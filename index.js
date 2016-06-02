62766300cddd3ccf7c5a2f289a7e1279557e31b8
Chuck Jazdzewski <chuckj@google.com>
2016-06-02 20:47:25 +0000

    13c39a5 chore(compiler): Expose types needed by the language service

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ var codegen_1 = require('./src/codegen');
 exports.CodeGenerator = codegen_1.CodeGenerator;
 var reflector_host_1 = require('./src/reflector_host');
 exports.NodeReflectorHost = reflector_host_1.NodeReflectorHost;
+var static_reflector_1 = require('./src/static_reflector');
+exports.StaticReflector = static_reflector_1.StaticReflector;
+exports.StaticSymbol = static_reflector_1.StaticSymbol;
 __export(require('@angular/tsc-wrapped'));
 //# sourceMappingURL=index.js.map
