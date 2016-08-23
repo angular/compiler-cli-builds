3dfb6e7baa829922e6ced37e4c8d4d1c55b744f1
Kara <kara@users.noreply.github.com>
2016-08-23 23:28:21 +0000

    5c93a88 fix(core): Share private types through an exported interface. (#11009)

--- src/core_private.d.ts
+++ src/core_private.d.ts
@@ -5,11 +5,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { __core_private_types__ as t } from '@angular/core';
-export declare type ReflectorReader = t.ReflectorReader;
-export declare var ReflectorReader: typeof t.ReflectorReader;
-export declare type ReflectionCapabilities = t.ReflectionCapabilities;
-export declare var ReflectionCapabilities: typeof t.ReflectionCapabilities;
-export declare type Console = t.Console;
-export declare var Console: typeof t.Console;
-export declare var reflector: typeof t.reflector;
+import { __core_private__ as r, __core_private_types__ as types } from '@angular/core';
+export declare let _compiler_cli_core_private_types: types;
+export declare type ReflectorReader = typeof _compiler_cli_core_private_types.ReflectorReader;
+export declare var ReflectorReader: typeof r.ReflectorReader;
+export declare type ReflectionCapabilities = typeof _compiler_cli_core_private_types.ReflectionCapabilities;
+export declare var ReflectionCapabilities: typeof r.ReflectionCapabilities;
+export declare type Console = typeof _compiler_cli_core_private_types.Console;
+export declare var Console: typeof r.Console;
+export declare var reflector: typeof r.reflector;
