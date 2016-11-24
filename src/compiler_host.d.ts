f5ea78f0c37460d90e8ea6c22f4d437c0b3d3e1b
vsavkin <avix1000@gmail.com>
2016-11-24 00:06:53 +0000

    2a3ca7b fix(compiler-cli): fix paths in source maps to be relative

--- src/compiler_host.d.ts
+++ src/compiler_host.d.ts
@@ -5,38 +5,29 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { ImportGenerator } from '@angular/compiler';
+import { AotCompilerHost } from '@angular/compiler';
 import { AngularCompilerOptions, MetadataCollector, ModuleMetadata } from '@angular/tsc-wrapped';
 import * as ts from 'typescript';
-import { StaticReflectorHost, StaticSymbol } from './static_reflector';
-export interface ReflectorHostContext {
+export interface CompilerHostContext {
     fileExists(fileName: string): boolean;
     directoryExists(directoryName: string): boolean;
     readFile(fileName: string): string;
+    readResource(fileName: string): Promise<string>;
     assumeFileExists(fileName: string): void;
 }
-export declare class ReflectorHost implements StaticReflectorHost, ImportGenerator {
+export declare class CompilerHost implements AotCompilerHost {
     protected program: ts.Program;
     protected compilerHost: ts.CompilerHost;
     protected options: AngularCompilerOptions;
     protected metadataCollector: MetadataCollector;
-    protected context: ReflectorHostContext;
+    protected context: CompilerHostContext;
     private isGenDirChildOfRootDir;
     protected basePath: string;
     private genDir;
-    constructor(program: ts.Program, compilerHost: ts.CompilerHost, options: AngularCompilerOptions, context?: ReflectorHostContext);
-    angularImportLocations(): {
-        coreDecorators: string;
-        diDecorators: string;
-        diMetadata: string;
-        diOpaqueToken: string;
-        animationMetadata: string;
-        provider: string;
-    };
+    private resolverCache;
+    constructor(program: ts.Program, compilerHost: ts.CompilerHost, options: AngularCompilerOptions, context?: CompilerHostContext);
     getCanonicalFileName(fileName: string): string;
-    protected resolve(m: string, containingFile: string): string;
-    protected normalizeAssetUrl(url: string): string;
-    protected resolveAssetUrl(url: string, containingFile: string): string;
+    moduleNameToFileName(m: string, containingFile: string): string;
     /**
      * We want a moduleId that will appear in import statements in the generated code.
      * These need to be in a form that system.js can load, so absolute file paths don't work.
@@ -52,34 +43,24 @@ export declare class ReflectorHost implements StaticReflectorHost, ImportGenerat
      *
      * NOTE: (*) the relative path is computed depending on `isGenDirChildOfRootDir`.
      */
-    getImportPath(containingFile: string, importedFile: string): string;
+    fileNameToModuleName(importedFile: string, containingFile: string): string;
     private dotRelative(from, to);
     /**
      * Moves the path into `genDir` folder while preserving the `node_modules` directory.
      */
     private rewriteGenDirPath(filepath);
-    findDeclaration(module: string, symbolName: string, containingFile: string, containingModule?: string): StaticSymbol;
-    private typeCache;
-    private resolverCache;
-    /**
-     * getStaticSymbol produces a Type whose metadata is known but whose implementation is not loaded.
-     * All types passed to the StaticResolver should be pseudo-types returned by this method.
-     *
-     * @param declarationFile the absolute path of the file where the symbol is declared
-     * @param name the name of the type.
-     */
-    getStaticSymbol(declarationFile: string, name: string, members?: string[]): StaticSymbol;
-    getMetadataFor(filePath: string): ModuleMetadata;
-    readMetadata(filePath: string): any;
-    private getResolverMetadata(filePath);
-    protected resolveExportedSymbol(filePath: string, symbolName: string): StaticSymbol;
+    protected getSourceFile(filePath: string): ts.SourceFile;
+    getMetadataFor(filePath: string): ModuleMetadata[];
+    readMetadata(filePath: string, dtsFilePath: string): ModuleMetadata[];
+    loadResource(filePath: string): Promise<string>;
 }
-export declare class NodeReflectorHostContext implements ReflectorHostContext {
+export declare class NodeCompilerHostContext implements CompilerHostContext {
     private host;
     constructor(host: ts.CompilerHost);
     private assumedExists;
     fileExists(fileName: string): boolean;
     directoryExists(directoryName: string): boolean;
     readFile(fileName: string): string;
+    readResource(s: string): Promise<string>;
     assumeFileExists(fileName: string): void;
 }
