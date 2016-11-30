ec9c39a2b2fb729d861e15b3b159a3e19a56f2a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-11-30 23:31:36 +0000

    d420080 docs(changelog): add changelog for 2.2.4

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-beta.0-2975d89');
+exports.VERSION = new core_1.Version('2.3.0-beta.0-d420080');
 //# sourceMappingURL=index.js.map
