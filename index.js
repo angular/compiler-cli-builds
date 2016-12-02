74a7633a825651cb319cd7df07cf0430c6f6ae9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:44:36 +0000

    dfd8140 Fix format

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-75d1617');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-dfd8140');
 //# sourceMappingURL=index.js.map
