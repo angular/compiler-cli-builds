f9afbb48d354f3d2d951650b977a35ce64a55b8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:55:18 +0000

    aa4bd14 docs(changelog): add instructions for people that depend on the `cache` operator (#13191)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-3ff6554');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-aa4bd14');
 //# sourceMappingURL=index.js.map
