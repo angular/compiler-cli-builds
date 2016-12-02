ec716f066409805f9926f2989d083fc306927210
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 23:31:50 +0000

    6cf7a1b refactor(build): fix build location of compiler-cli esm module (#13212)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-d46b8de');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-6cf7a1b');
 //# sourceMappingURL=index.js.map
