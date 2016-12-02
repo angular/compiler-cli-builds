64a9b135ce357ac908000b84ef4814ff2e8fb92c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 18:20:51 +0000

    75d1617 fix(compiler): include the summaries of reexported modules / directives / pipes (#13196)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-6c0b65b');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-75d1617');
 //# sourceMappingURL=index.js.map
