8fea8b8ead057fba9332385071bbe222ae205821
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 22:57:57 +0000

    51b0692 docs(core): correct HostListener typo (#13203)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-aa4bd14');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-51b0692');
 //# sourceMappingURL=index.js.map
