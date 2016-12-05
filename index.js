0ef1463fcec353986464bc170efe204f983fbed7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:39:45 +0000

    dea5916 docs(developer): add anchor to source code formatting (#13199)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-a132287');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-dea5916');
 //# sourceMappingURL=index.js.map
