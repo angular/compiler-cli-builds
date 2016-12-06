9fdc9966d94d98b47c454f60ccab436e977a14b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:55:20 +0000

    393c100 fix(tsc-wrapped): have UserError display the actual error

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-2893c2c');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-393c100');
 //# sourceMappingURL=index.js.map
