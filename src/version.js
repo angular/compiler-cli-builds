ab0c4078a31d59c34c87ba6569d07ffc2edebadb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 22:07:43 +0000

    13b41bd chore(release): update version of tsc-wrapped to 0.4.2

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-cf52284');
+exports.VERSION = new core_1.Version('2.3.0-13b41bd');
 //# sourceMappingURL=version.js.map
