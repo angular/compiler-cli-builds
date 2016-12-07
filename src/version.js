1c99a8be96a021e5174aaa8e322225edc920b45c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 18:19:34 +0000

    cf52284 docs(changelog): fix typo AplicationRef => ApplicationRef (#13284)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-4a09c81');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-cf52284');
 //# sourceMappingURL=version.js.map
