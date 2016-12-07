b780b9cd8e0b03018f312c173a662a7e181cf734
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 00:31:51 +0000

    25c2141 fix(language-service): remove incompletely used parameter from `createLanguageServiceFromTypescript()` (#13278)

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-rc.0-393c100');
+exports.VERSION = new core_1.Version('2.3.0-rc.0-25c2141');
 //# sourceMappingURL=index.js.map
