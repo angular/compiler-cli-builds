1c0dd5ffda6d162e8214a9029b922e03d977b0ff
Victor Berchet <victor@suumit.com>
2016-12-08 19:37:30 +0000

    9591a08 fix(router): Use T type in Resolve interface (#13242)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-65965c2');
+exports.VERSION = new core_1.Version('2.3.0-9591a08');
 //# sourceMappingURL=version.js.map
