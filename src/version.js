675081ff6657cc005c9fd89e46c30e54a43b9c6b
Victor Berchet <victor@suumit.com>
2016-12-09 19:33:06 +0000

    c8baf51 style: clang-format the code

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-9a8423d');
+exports.VERSION = new core_1.Version('2.3.0-c8baf51');
 //# sourceMappingURL=version.js.map
