452a5496d7d1b1e9fcc9b217864780ffe64c7458
Victor Berchet <victor@suumit.com>
2016-12-09 23:53:50 +0000

    8a8c532 fix(dom_adapter): remove logError from logGroup (#12925)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-08ff2e5');
+exports.VERSION = new core_1.Version('2.3.0-8a8c532');
 //# sourceMappingURL=version.js.map
