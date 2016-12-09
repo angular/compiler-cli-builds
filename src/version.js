e5953f1b438807714938c8451825187e5e3b8deb
Victor Berchet <victor@suumit.com>
2016-12-09 23:50:28 +0000

    a006c14 feat(router): routerLink add tabindex attribute (#13094)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-aaf6e05');
+exports.VERSION = new core_1.Version('2.3.0-a006c14');
 //# sourceMappingURL=version.js.map
