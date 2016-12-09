5cf00cee5691d2b751bd937bf8d0351dd8d710ff
Victor Berchet <victor@suumit.com>
2016-12-09 18:56:46 +0000

    56c361f test(compiler): test i18n explicit id

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-72361fb');
+exports.VERSION = new core_1.Version('2.3.0-56c361f');
 //# sourceMappingURL=version.js.map
