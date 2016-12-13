c4876392d94292bc6f23f25253e9072e8db82356
Victor Berchet <victor@suumit.com>
2016-12-13 19:43:48 +0000

    2b90cd5 fix(compiler): narrow the span reported for invalid pipes

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-9ec0a4e');
+exports.VERSION = new core_1.Version('2.3.0-2b90cd5');
 //# sourceMappingURL=version.js.map
