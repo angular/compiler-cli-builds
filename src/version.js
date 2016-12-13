c8e091496c6680e386c8ae43528c3e5114e0cd8e
Victor Berchet <victor@suumit.com>
2016-12-13 19:46:14 +0000

    714f2af ci(browser providers): update browsers in SL and BS (#13431)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-2b90cd5');
+exports.VERSION = new core_1.Version('2.3.0-714f2af');
 //# sourceMappingURL=version.js.map
