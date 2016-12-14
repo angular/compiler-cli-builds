9fd2958ec5835a5dc326f7910589e2513d42a285
Victor Berchet <victor@suumit.com>
2016-12-14 17:07:11 +0000

    821b8f0 fix(forms): ensure select[multiple] retains selections

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-7b0a867');
+exports.VERSION = new core_1.Version('2.3.0-821b8f0');
 //# sourceMappingURL=version.js.map
