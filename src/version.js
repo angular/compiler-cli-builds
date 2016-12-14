6a661d750e9dccfa1656c76a6e3827e034778122
Victor Berchet <victor@suumit.com>
2016-12-14 23:26:16 +0000

    c65b4fa refactor: format & lint

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-169ed82');
+exports.VERSION = new core_1.Version('2.3.0-c65b4fa');
 //# sourceMappingURL=version.js.map
