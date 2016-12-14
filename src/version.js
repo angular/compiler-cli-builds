1941da8d0b9c9d0bba5e857c782c6f1346b94c67
Victor Berchet <victor@suumit.com>
2016-12-14 22:45:31 +0000

    aa3769b fix(compiler): resolver should merge host bindings and listeners (#13474)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.3.0-d4ddb60');
+exports.VERSION = new core_1.Version('2.3.0-aa3769b');
 //# sourceMappingURL=version.js.map
