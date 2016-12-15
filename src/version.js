92dd82a8871c540f0fdb6c2d79b10929ef788724
GitHub <noreply@github.com>
2016-12-15 22:09:15 +0000

    5b6e8ea refactor(compiler): format update (#13506)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-732f446');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-5b6e8ea');
 //# sourceMappingURL=version.js.map
