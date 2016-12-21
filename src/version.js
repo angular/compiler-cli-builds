45e8187326652af251ea5e09e597f72a1771bef2
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:30:13 +0000

    f49ab56 fix(animations): always quote string map key values in AOT code (#13602)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-c0f750a');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-f49ab56');
 //# sourceMappingURL=version.js.map
