df1e2960ff8a7cdf32f8c786bfffa3071f58ae43
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 22:27:32 +0000

    842f52e fix(animations): always recover from a failed animation step (#13604)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-eb2ceff');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-842f52e');
 //# sourceMappingURL=version.js.map
