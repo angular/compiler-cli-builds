4eb1bf24423b6cb94fe31f0e6944da71601afe09
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:03:43 +0000

    c0f750a fix(compiler): ignore @import in comments (#13368)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-bcd37f5');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-c0f750a');
 //# sourceMappingURL=version.js.map
