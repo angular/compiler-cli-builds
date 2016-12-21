4da34eaec5acfe8bce2110e04b526f08c79950a5
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 21:01:18 +0000

    eb2ceff fix(router): should reset location if a navigation by location is successful (#13545)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-f49ab56');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-eb2ceff');
 //# sourceMappingURL=version.js.map
