36b5e20d15e62b2209c4fb8fae4f051ee00cfd34
GitHub <noreply@github.com>
2016-12-21 02:02:26 +0000

    e69c1fb refactor(platform-browser): resolver merge conflict for tslint (#13601)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-fcd116f');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-e69c1fb');
 //# sourceMappingURL=version.js.map
