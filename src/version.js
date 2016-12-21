0335f01a9546cdff46e77754bd5bd9d7c9b59dbe
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 02:03:02 +0000

    bcd37f5 Include bower instructions in DEVELOPER.md (#13591)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-e69c1fb');
+exports.VERSION = new core_1.Version('4.0.0-beta.0-bcd37f5');
 //# sourceMappingURL=version.js.map
