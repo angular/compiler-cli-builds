80fe1510245e0e5d57e339629b4fc190300f55e1
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 22:49:18 +0000

    28a92b2 docs(changelog): add changelog for 2.4.1

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.1-df8643f');
+exports.VERSION = new core_1.Version('2.4.1-28a92b2');
 //# sourceMappingURL=version.js.map
