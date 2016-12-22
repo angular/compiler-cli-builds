3776fe7d64aeb71253a1546bda01b95f12d4ff14
Chuck Jazdzewski <chuckj@google.com>
2016-12-22 01:10:07 +0000

    f114e40 docs(changelog): add changelog for 4.0.0-beta.1

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.0-842f52e');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-f114e40');
 //# sourceMappingURL=version.js.map
