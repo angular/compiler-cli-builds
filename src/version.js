fc1f93017b73e4d5fb3432767147ffadc694818f
Hans <hans@hansl.ca>
2016-12-22 20:50:04 +0000

    e85232a docs(ngIf): fix typos (#13630)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-67380d4');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-e85232a');
 //# sourceMappingURL=version.js.map
