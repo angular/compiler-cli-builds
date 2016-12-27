dbb7f54d1be980dd2ecf8db28f1884073fc02abb
Hans <hans@hansl.ca>
2016-12-27 17:49:07 +0000

    9c69703 feat(compiler): generate proper reexports in `.ngfactory.ts` files to not need transitive deps for compiling `.ngfactory.ts` files. (#13524)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-6976903');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-9c69703');
 //# sourceMappingURL=version.js.map
