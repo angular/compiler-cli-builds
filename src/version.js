6e887d3e01b44717c9cd37e8ca9f42b952a2cb2c
Hans <hans@hansl.ca>
2016-12-27 22:22:16 +0000

    69fa3bb feat(router): add an extra argument to CanDeactivate interface (#13560)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-445ed43');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-69fa3bb');
 //# sourceMappingURL=version.js.map
