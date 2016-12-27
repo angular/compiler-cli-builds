a5fbf8a91f3bcc811464fc7e18c870cb23d42880
Hans <hans@hansl.ca>
2016-12-27 23:35:47 +0000

    0eca960 fix(router): fix lazy loaded module with wildcard route (#13649)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-eed8344');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-0eca960');
 //# sourceMappingURL=version.js.map
