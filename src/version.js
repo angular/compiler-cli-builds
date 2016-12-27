e3dc949c63a0a76a6104fec9ec90e561e87d87c2
Hans <hans@hansl.ca>
2016-12-27 22:15:59 +0000

    445ed43 fix(compiler): throw an error for invalid provider (#13544)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-174334d');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-445ed43');
 //# sourceMappingURL=version.js.map
