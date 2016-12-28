59499f3dacc94281b505b6f5dbb6fe48a210b7d9
Hans Larsen <hans@hansl.ca>
2016-12-28 01:17:28 +0000

    2c0c86e fix(compiler): improve the error when template is not a string

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-b2ae7b6');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-2c0c86e');
 //# sourceMappingURL=version.js.map
