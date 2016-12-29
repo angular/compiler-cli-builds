d7f7979a14f38551d6ac5d8eb9e31a36bfb9c663
Hans <hans@hansl.ca>
2016-12-29 17:52:24 +0000

    1e6440e docs(Http): fix and extend samples for testing/MockBackend (#13689)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-6b02b80');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-1e6440e');
 //# sourceMappingURL=version.js.map
