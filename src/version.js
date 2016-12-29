5e65db421ea7bea337e1457ef86d705f01ca3027
Hans <hans@hansl.ca>
2016-12-29 17:54:58 +0000

    bb0d23f Typo (#13698)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-1e6440e');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-bb0d23f');
 //# sourceMappingURL=version.js.map
