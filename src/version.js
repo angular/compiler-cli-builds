ade7e3e6bfc79d705ed57ea4f6f495c079b83612
Igor Minar <iminar@google.com>
2017-01-05 17:39:27 +0000

    9898d8f fix(forms): Validators.required properly validate arrays (#13362)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.1-2dd6280');
+exports.VERSION = new core_1.Version('4.0.0-beta.1-9898d8f');
 //# sourceMappingURL=version.js.map
