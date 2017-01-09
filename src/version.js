d1ec61987ee04edab8757e6bdd52f57576cfccff
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 23:52:04 +0000

    6397824 fix(router): throw an error when navigate to null/undefined path

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.2-6164eb2');
+exports.VERSION = new core_1.Version('2.4.2-6397824');
 //# sourceMappingURL=version.js.map
