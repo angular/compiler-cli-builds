38246869090c5315f2e3df1c2ae2379acc236f2c
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 23:42:59 +0000

    5d9cbd7 fix(compiler-cli): add support for more than 2 levels of nested lazy routes

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.2-99aa49a');
+exports.VERSION = new core_1.Version('4.0.0-beta.2-5d9cbd7');
 //# sourceMappingURL=version.js.map
