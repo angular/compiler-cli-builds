677e1ea6af0b10a486d9077ecfe9f28e816fb344
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 01:43:14 +0000

    9aeb8c5 refactor(test): `<template>`/`<ng-container>`/*-directives

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.2-f14d549');
+exports.VERSION = new core_1.Version('4.0.0-beta.2-9aeb8c5');
 //# sourceMappingURL=version.js.map
