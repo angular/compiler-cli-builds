1d4cd47465bcf9737e644246e06913b66fed433c
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 23:56:10 +0000

    aeed737 fix(compiler-cli): avoid handling functions in loadChildren as lazy load routes paths

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.2-d43e5dd');
+exports.VERSION = new core_1.Version('4.0.0-beta.2-aeed737');
 //# sourceMappingURL=version.js.map
