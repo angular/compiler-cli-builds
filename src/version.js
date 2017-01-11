d12327cb9438724e5c255534949f7c1df43f6ca8
Miško Hevery <misko@hevery.com>
2017-01-11 21:36:39 +0000

    3a4b54d docs(release-schedule): add release schedule doc (#13827)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.2-95cbca2');
+exports.VERSION = new core_1.Version('4.0.0-beta.2-3a4b54d');
 //# sourceMappingURL=version.js.map
