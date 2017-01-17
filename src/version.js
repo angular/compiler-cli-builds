c8dfee0b66e551ecfec250d2dc59fe6f54bc041d
Miško Hevery <misko@hevery.com>
2017-01-17 23:14:04 +0000

    d169c24 feat(core): Add type information to injector.get() (#13785)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.3-5237b1c');
+exports.VERSION = new core_1.Version('4.0.0-beta.3-d169c24');
 //# sourceMappingURL=version.js.map
