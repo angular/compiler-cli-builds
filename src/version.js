84bc9e1596a41d68b125d361a0ea6e2aa3598da5
Miško Hevery <misko@hevery.com>
2017-01-18 05:32:59 +0000

    e8ea741 fix(router): routerLinkActive should not throw when not initialized (#13273)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.3-d169c24');
+exports.VERSION = new core_1.Version('4.0.0-beta.3-e8ea741');
 //# sourceMappingURL=version.js.map
