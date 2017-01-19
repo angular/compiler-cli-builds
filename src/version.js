3e239cf3e6645fc69689fe38b08a1ef179ee54d9
Miško Hevery <misko@hevery.com>
2017-01-19 00:14:52 +0000

    1200cf2 fix(http): don't create a blob out of ArrayBuffer when type is application/octet-stream (#13992)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.3-635bf02');
+exports.VERSION = new core_1.Version('4.0.0-beta.3-1200cf2');
 //# sourceMappingURL=version.js.map
