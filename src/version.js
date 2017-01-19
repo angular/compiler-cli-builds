a9a8f61d0078ea071c6a6e99a91ff60634c49973
Miško Hevery <misko@hevery.com>
2017-01-19 00:10:52 +0000

    635bf02 fix(router): enable loadChildren with function in aot (#13909)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.3-2d7b3a8');
+exports.VERSION = new core_1.Version('4.0.0-beta.3-635bf02');
 //# sourceMappingURL=version.js.map
