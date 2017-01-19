57e3406cc0024b844c1fb37bea9b54c0dc59aa7e
Miško Hevery <misko@hevery.com>
2017-01-19 00:58:55 +0000

    84542d8 docs(changelog): add changelog for 2.4.4

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.3-015878a');
+exports.VERSION = new core_1.Version('2.4.4-84542d8');
 //# sourceMappingURL=version.js.map
