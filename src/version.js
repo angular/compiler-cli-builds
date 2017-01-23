6537c17f5d79f687af4c4f44860a5c1c9dd10561
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:08:28 +0000

    dff6ee3 ci: validate the message of each new commit as part of the CI linting

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-0589f93');
+exports.VERSION = new core_1.Version('4.0.0-beta.4-dff6ee3');
 //# sourceMappingURL=version.js.map
