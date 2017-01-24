ead1f0c5ce7f18580933d8df19e515270a02d064
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 21:07:32 +0000

    0c7726d feat(tsc-wrapped): Support of vinyl like config file was added (#13987)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-83361d8');
+exports.VERSION = new core_1.Version('4.0.0-beta.4-0c7726d');
 //# sourceMappingURL=version.js.map
