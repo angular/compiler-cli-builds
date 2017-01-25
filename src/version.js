e1256b5c34b5e9cdf2cb916a84f8c6ffd84bcb7e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 18:44:58 +0000

    a733444 docs(compiler): add comment to warn about regexp changes (#14106)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-b2f9d56');
+exports.VERSION = new core_1.Version('4.0.0-beta.4-a733444');
 //# sourceMappingURL=version.js.map
