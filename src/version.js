071cb0a151576e635301258ae0888d68cd1ce614
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 00:02:50 +0000

    b2f9d56 fix(compiler): fix regexp to support firefox 31 (#14082)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-6152eb2');
+exports.VERSION = new core_1.Version('4.0.0-beta.4-b2f9d56');
 //# sourceMappingURL=version.js.map
