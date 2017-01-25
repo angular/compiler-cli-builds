99e070090a22b9ed2beebc3a7f35a28f338110a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 21:34:23 +0000

    d2859cd style(compiler): run format

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-4931a61');
+exports.VERSION = new core_1.Version('4.0.0-beta.4-d2859cd');
 //# sourceMappingURL=version.js.map
