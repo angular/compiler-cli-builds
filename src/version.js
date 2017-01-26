cd3848aa20c99bcca3451a6f1dfffb62d6fc10ee
Victor Berchet <victor@suumit.com>
2017-01-26 02:00:29 +0000

    1e729d7 feat(core): add query support to view engine

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-05b2b49');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-1e729d7');
 //# sourceMappingURL=version.js.map
