09fecfc5df28531b9460494e1f6fc1cadcacb9d9
Victor Berchet <victor@suumit.com>
2017-01-26 01:55:45 +0000

    4106d18 build(aio): add angular.io package.json and yarn.lock file

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.4-d2859cd');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-4106d18');
 //# sourceMappingURL=version.js.map
