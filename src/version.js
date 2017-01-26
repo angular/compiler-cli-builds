27ae038fb6b55f0412f2d2b7bc50d7105c6ab17b
GitHub <noreply@github.com>
2017-01-26 01:57:57 +0000

    05b2b49 fix(compiler): allow empty translations for attributes (#14085)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-3ef73c2');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-05b2b49');
 //# sourceMappingURL=version.js.map
