80b9bb2e45d48b2e2428db59f1b1ef3ca1d1f9a1
Miško Hevery <misko@hevery.com>
2017-01-28 05:06:58 +0000

    01da422 docs(public_api): change description (#13583)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-0854a5d');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-01da422');
 //# sourceMappingURL=version.js.map
