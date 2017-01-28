5e9c5ce8a396caea361129a285cd4d190974880e
Miško Hevery <misko@hevery.com>
2017-01-28 00:34:47 +0000

    3368f29 ci: update typings test to use standard typings (#14156)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-8960d49');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-3368f29');
 //# sourceMappingURL=version.js.map
