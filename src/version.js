72ab6ae1f8c09c6db129376480ed73ca28c4bf87
Igor Minar <iminar@google.com>
2017-01-28 05:38:23 +0000

    bc1320d docs: update COMITTER.md with info about pullapprove.com

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-01da422');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-bc1320d');
 //# sourceMappingURL=version.js.map
