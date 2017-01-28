2a2bed7c9bad4b0ee1d14ae49674568554711cc9
Miško Hevery <misko@hevery.com>
2017-01-28 04:45:10 +0000

    df7f5fc style: fix typo in CHANGELOG.md (#14009)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.5-24ea3f0');
+exports.VERSION = new core_1.Version('4.0.0-beta.5-df7f5fc');
 //# sourceMappingURL=version.js.map
