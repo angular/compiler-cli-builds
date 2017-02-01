6944589b784ef190d0dfd10c6336e9aca6582845
Miško Hevery <misko@hevery.com>
2017-02-01 05:14:35 +0000

    8d4aa82 fix(i18n): parse ICU messages while normalizing templates (#14153)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.5-14e9751');
+exports.VERSION = new core_1.Version('2.4.5-8d4aa82');
 //# sourceMappingURL=version.js.map
