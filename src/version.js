5ea05d22c9fb7a4f0e7bddf5c2a9bb3b5b31748c
Igor Minar <iminar@google.com>
2017-02-03 07:46:27 +0000

    a909643 feat(aio): delete unused kruft

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.6-559cf9d');
+exports.VERSION = new core_1.Version('4.0.0-beta.6-a909643');
 //# sourceMappingURL=version.js.map
