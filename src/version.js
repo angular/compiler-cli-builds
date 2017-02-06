b485b00ef91446062bdece5be910395cf194260f
Igor Minar <iminar@google.com>
2017-02-06 21:39:41 +0000

    80b66ed docs(zone): fix whitespace around backtick code

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.6-41db177');
+exports.VERSION = new core_1.Version('4.0.0-beta.6-80b66ed');
 //# sourceMappingURL=version.js.map
