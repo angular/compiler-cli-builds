e897d2fbc81f9882c41f26ab41f98d2fc379fc44
Igor Minar <iminar@google.com>
2017-02-07 08:22:20 +0000

    d4ffa47 fix(aio): correctly render decorator docs (#14328)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.6-80b66ed');
+exports.VERSION = new core_1.Version('4.0.0-beta.6-d4ffa47');
 //# sourceMappingURL=version.js.map
