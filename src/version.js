7176379e69c9e55447a85c1c4323146c2775c1d1
Miško Hevery <misko@hevery.com>
2017-02-07 18:06:07 +0000

    7036e04 docs: add 4.0.0-beta.7 to the release schedule (#14341)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.6-d4ffa47');
+exports.VERSION = new core_1.Version('4.0.0-beta.6-7036e04');
 //# sourceMappingURL=version.js.map
