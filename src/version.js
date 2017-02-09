6f10a731243f606cc1f23f2032434c25486d988f
Miško Hevery <misko@hevery.com>
2017-02-09 21:26:47 +0000

    56e2f84 ci: run 'yarn install' for doc_test task (#14384)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-600402d');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-56e2f84');
 //# sourceMappingURL=version.js.map
