0bcfb9da7e77cccb9abb72c2c12995eea78ade15
Miško Hevery <misko@hevery.com>
2017-02-09 18:22:08 +0000

    09b4bd0 docs(changelog): add changelog for 4.0.0-beta.7

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.6-d1feb47');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-09b4bd0');
 //# sourceMappingURL=version.js.map
