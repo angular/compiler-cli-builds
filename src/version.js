84bd6c4401fc1851f358277ef317daeb61e516dd
Misko Hevery <misko@hevery.com>
2017-02-09 04:38:58 +0000

    059085b docs(compiler): incorrect method reference (#14314)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require('@angular/core');
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('2.4.6-401ef74');
+exports.VERSION = new core_1.Version('2.4.6-059085b');
 //# sourceMappingURL=version.js.map
