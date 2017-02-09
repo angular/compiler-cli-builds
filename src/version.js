615ca9cb5354a2c382455d754c354583c50d70d1
Miško Hevery <misko@hevery.com>
2017-02-09 23:39:05 +0000

    44bb337 fix(compiler): disable non-components as an entry component (#14335)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-ac6f8e3');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-44bb337');
 //# sourceMappingURL=version.js.map
