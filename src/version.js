53e01baa0ecc7683b77ce6f5abbb234862d66952
Miško Hevery <misko@hevery.com>
2017-02-09 21:55:06 +0000

    45cc444 fix(core): Remove ChangeDetectorRef Paramter from KeyValueDifferFactory and IterableDifferFactory (#14311)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-56e2f84');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-45cc444');
 //# sourceMappingURL=version.js.map
