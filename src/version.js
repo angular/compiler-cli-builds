bfca2a7450e516cce61ee9448e10454dd5709a9c
Misko Hevery <misko@hevery.com>
2017-02-11 05:16:01 +0000

    d3f174a ci: fix validate-commit-messages

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-8dd16bb');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-d3f174a');
 //# sourceMappingURL=version.js.map
