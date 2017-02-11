21e5efd2264d8beb01919897bcd0bf812a7cce81
Miško Hevery <misko@hevery.com>
2017-02-11 01:55:51 +0000

    5279d06 ci: re-format sources (#14413)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-b988733');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-5279d06');
 //# sourceMappingURL=version.js.map
