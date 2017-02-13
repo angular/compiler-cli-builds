532819520915bd488cb7d426b574e2c06fce9c7e
Miško Hevery <misko@hevery.com>
2017-02-13 23:08:29 +0000

    1dc9be4 fix(aio): support `ValueModule` symbols in the TypeScript source (#14464)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-221b7a1');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-1dc9be4');
 //# sourceMappingURL=version.js.map
