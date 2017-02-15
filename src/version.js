e55e035af42088e8439b2fd879925513ec929af5
GitHub <noreply@github.com>
2017-02-15 06:01:34 +0000

    e9ba7aa docs: update schedule by adding one more beta (#14494)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-bb9c7ae');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-e9ba7aa');
 //# sourceMappingURL=version.js.map
