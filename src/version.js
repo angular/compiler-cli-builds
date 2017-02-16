911019dab1a0d0e48f9ccd28b1cbcf9cbc573272
Igor Minar <igor@angularjs.org>
2017-02-16 23:57:35 +0000

    96d06f7 fix(upgrade): Prevent property renaming for $inject.

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-8c20aaa');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-96d06f7');
 //# sourceMappingURL=version.js.map
