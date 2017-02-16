e2b31db98f9329884ce391e7549e59aeb9575547
Igor Minar <iminar@google.com>
2017-02-16 20:30:47 +0000

    ba17dcb refactor(animations): multiple group animations should tally-up delays correctly

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-3dbd9a0');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-ba17dcb');
 //# sourceMappingURL=version.js.map
