437339b09b5c12489952828b4ab670288854e131
Igor Minar <iminar@google.com>
2017-02-16 19:52:28 +0000

    b36f60c docs(router): fix broken link (#14431)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-57461e9');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-b36f60c');
 //# sourceMappingURL=version.js.map
