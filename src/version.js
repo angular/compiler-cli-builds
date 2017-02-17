078a002183e3dc7cc21aaa0ba3f1b69c57737034
Igor Minar <iminar@google.com>
2017-02-17 00:03:39 +0000

    b1a79fd ci: simplify validate-commit-message empty line logic

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-96d06f7');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-b1a79fd');
 //# sourceMappingURL=version.js.map
