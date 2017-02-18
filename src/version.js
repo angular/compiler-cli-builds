887d8067f1f5b77eda48733f445c18802388dc75
Igor Minar <iminar@google.com>
2017-02-18 01:38:08 +0000

    ab26b65 fix(platform-browser): allow to mix shadow dom with non shadow dom

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-74ce121');
+exports.VERSION = new core_1.Version('4.0.0-beta.7-ab26b65');
 //# sourceMappingURL=version.js.map
