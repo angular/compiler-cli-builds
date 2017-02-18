db54845f6b0e15b8a245b6c4b683120279176579
Igor Minar <igor@angularjs.org>
2017-02-18 23:36:26 +0000

    1bdf706 release: cut v4.0.0-beta.8

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.7-5129e8e');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-1bdf706');
 //# sourceMappingURL=version.js.map
