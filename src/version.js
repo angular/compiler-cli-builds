16ca0c5f2e655a817431864643367bc565f30671
GitHub <noreply@github.com>
2017-02-21 00:39:55 +0000

    d6a58f9 feat(core): make `new Inject()` optional for deps specified as `InjectionToken` (#14486)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-5f3c844');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-d6a58f9');
 //# sourceMappingURL=version.js.map
