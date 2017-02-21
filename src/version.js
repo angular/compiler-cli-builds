cc3f8a67ef22cec5abc9f6138a61061344964e01
Victor Berchet <victor@suumit.com>
2017-02-21 00:55:09 +0000

    551fe50 feat(forms): introduce AsyncValidator interface (#13483)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-d6a58f9');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-551fe50');
 //# sourceMappingURL=version.js.map
