95560cee43a83773ed0d8e2bd851faec54017cb6
Victor Berchet <victor@suumit.com>
2017-02-21 04:07:30 +0000

    2ddd1c3 Revert "fix(http): Make ResponseOptionsArgs an interface (#14607)"

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-2a191ca');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-2ddd1c3');
 //# sourceMappingURL=version.js.map
