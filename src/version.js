932cec515a0607029522fc70c4ad328146fa2db4
Igor Minar <iminar@google.com>
2017-02-22 19:49:37 +0000

    88755b0 build(aio): update dgeni-packages to 0.16.8 (#14597)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-fcc1d17');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-88755b0');
 //# sourceMappingURL=version.js.map
