004c0d781c3c04b4db91ee5ae31a3dcb13a407d3
Igor Minar <iminar@google.com>
2017-02-23 01:12:10 +0000

    a1d4769 feat: add a PLATFORM_ID token that provides a platform id Object. (#14647)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-88bc143');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-a1d4769');
 //# sourceMappingURL=version.js.map
