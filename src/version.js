bcd131d8cd660b7fa886279cbe3109fd0770cb80
Igor Minar <iminar@google.com>
2017-02-23 17:13:16 +0000

    e8d2743 feat(animations): noop animation module and zone fixes (#14661)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-6b7937f');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-e8d2743');
 //# sourceMappingURL=version.js.map
