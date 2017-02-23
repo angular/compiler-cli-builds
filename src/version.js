ee7e81e14a639ca0d243d61bfe6d244b93ee6528
Igor Minar <iminar@google.com>
2017-02-23 01:09:49 +0000

    88bc143 fix: app ids for better <style> management for ssr (#14632)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-88755b0');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-88bc143');
 //# sourceMappingURL=version.js.map
