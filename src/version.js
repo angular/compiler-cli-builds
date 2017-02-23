e74359d48c774d34211038bfc075f88b18b2d994
Igor Minar <iminar@google.com>
2017-02-23 05:17:05 +0000

    175dbce build: check yarn version in `check-environment` (#14499)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-801b090');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-175dbce');
 //# sourceMappingURL=version.js.map
