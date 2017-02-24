6e783bcadaf11e6915c4c1c1922f77afb6a3281a
Igor Minar <iminar@google.com>
2017-02-24 05:36:07 +0000

    01907ba build(platform-server): update parse5 to v3 (#14538)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-39f56fa');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-01907ba');
 //# sourceMappingURL=version.js.map
