98b077a294146e0b77c03eac494b9631d8dc9cbe
Igor Minar <iminar@google.com>
2017-02-24 07:15:39 +0000

    2da3844 build(language-service): support deep import bundling of language services (#14689)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-41da599');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-2da3844');
 //# sourceMappingURL=version.js.map
