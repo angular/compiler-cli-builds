6db7e3e244d1d2ef959df1b3c42c4bea2f52c3eb
GitHub <noreply@github.com>
2017-02-24 21:25:56 +0000

    3c9a46c fix(core): fix directive instantiation (#14715)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-436a179');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-3c9a46c');
 //# sourceMappingURL=version.js.map
