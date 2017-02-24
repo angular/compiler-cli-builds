ee745ea2fc465b05f5299c4fa6b3863f06920213
Igor Minar <iminar@google.com>
2017-02-24 23:38:09 +0000

    932a02f build(language-service): fix tsconfig needed for language-service rollup (#14717)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-beta.8-3c9a46c');
+exports.VERSION = new core_1.Version('4.0.0-beta.8-932a02f');
 //# sourceMappingURL=version.js.map
