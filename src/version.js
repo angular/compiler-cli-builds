ecca7ac712a1db5074cd6791ede1299fb6c7d69a
Igor Minar <igor@angularjs.org>
2017-02-25 02:43:41 +0000

    26ed262 docs: fix typo in the changelog

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-c7d1b36');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-26ed262');
 //# sourceMappingURL=version.js.map
