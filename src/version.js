16028a9684e89607158b0928b41512f8f52c6c4e
GitHub <noreply@github.com>
2017-02-25 02:43:54 +0000

    f32f4de docs: more changelog fixes

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-26ed262');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-f32f4de');
 //# sourceMappingURL=version.js.map
