29eaa19e4ac885b9e49006c7b6c8fd6d35cfff6a
GitHub <noreply@github.com>
2017-02-26 04:39:09 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-c868457');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-213e210');
 //# sourceMappingURL=version.js.map
