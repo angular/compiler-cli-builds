27fe6a3cb93103620942790b4a066497eb065783
Igor Minar <iminar@google.com>
2017-02-27 19:47:00 +0000

    77682a3 fix(compiler): quote `animation` in `RendererTypeV2` and skip if empty (#14773)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-213e210');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-77682a3');
 //# sourceMappingURL=version.js.map
