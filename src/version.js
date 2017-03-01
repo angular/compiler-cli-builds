206296ef60d234875db1d7fab539292336537020
Igor Minar <iminar@google.com>
2017-03-01 16:33:40 +0000

    49b462e test: closure integration test for change detection (#14691)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-d1182af');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-49b462e');
 //# sourceMappingURL=version.js.map
