5a6879bc8885397cb30f8a330b4ca8ae73a818b1
Igor Minar <iminar@google.com>
2017-03-01 15:11:52 +0000

    968995a fix(upgrade): fix upgrade component Closure optimization. (#14801)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-5ba55b0');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-968995a');
 //# sourceMappingURL=version.js.map
