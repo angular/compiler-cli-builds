634ab441e4992fb7c0485cf614bf4e2d89297f3e
Igor Minar <iminar@google.com>
2017-03-02 01:34:36 +0000

    9560ad8 fix(animations): make animations work in AOT (#14775)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.1-3168ef7');
+exports.VERSION = new core_1.Version('4.0.0-rc.1-9560ad8');
 //# sourceMappingURL=version.js.map
