343b17958c4e7447864b098a23d33dd42f8c0e73
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 17:53:45 +0000

    07122f0 fix(upgrade): populate upgraded component's view before creating the controller (#14289)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.2-ebd4463');
+exports.VERSION = new core_1.Version('4.0.0-rc.2-07122f0');
 //# sourceMappingURL=version.js.map
