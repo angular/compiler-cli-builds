6783a7de335f8bd93f5bc4ef3fc67b68bbeb8948
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 02:43:44 +0000

    174d4c8 ci(aio): do not deploy PR if preconditions not met

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.2-ad3b44a');
+exports.VERSION = new core_1.Version('4.0.0-rc.2-174d4c8');
 //# sourceMappingURL=version.js.map
