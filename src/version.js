4923d71a2b60e13abd317bf0a47abd935de51709
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:54:46 +0000

    75147ff fix(aio): remove invalid brace from CSS (#15122)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-018e5c9');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-75147ff');
 //# sourceMappingURL=version.js.map
