a5149e3aae60817262ba422067d9ea6f0978ba98
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:53:04 +0000

    71cd295 ci: add wardbell and gkalpak to aio approvers (#15030)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-6c8638c');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-71cd295');
 //# sourceMappingURL=version.js.map
