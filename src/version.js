de3f56edf2f1d5b78153546ce0b3db10a1e5d1b5
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 18:44:25 +0000

    6559425 fix(core): ErrorHandler should not rethrow an error by default (#15077)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-df914ef');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-6559425');
 //# sourceMappingURL=version.js.map
