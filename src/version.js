568795bbd26dd89a8f9a135c3798d9bdd4012242
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:40:00 +0000

    018e5c9 fix(platform-server): fix get/set title in parse5 adapter (#14965)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-e7dab7e');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-018e5c9');
 //# sourceMappingURL=version.js.map
