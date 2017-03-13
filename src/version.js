026baf66f24fda6ed9d72dfc5b359448bce55531
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:05:10 +0000

    ff71eff refactor(core): use flags in `Renderer2.setStyle` instead of booleans (#15045)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-fa1920a');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-ff71eff');
 //# sourceMappingURL=version.js.map
