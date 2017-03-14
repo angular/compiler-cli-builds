0eea6fc867df79fb2672a9fb58c6aa7f3dc1630e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 15:22:30 +0000

    423bfb0 ci(aio): whitelist 'aio-contributors' for deploying PR previews

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-2f250c1');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-423bfb0');
 //# sourceMappingURL=version.js.map
