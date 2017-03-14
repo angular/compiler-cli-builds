782bf034bc4eb54ffe6c8ce9a42c0d0436a09730
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 21:51:41 +0000

    2c5a671 fix: don't instantiate providers with ngOnDestroy eagerly. (#15070)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-0aad270');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-2c5a671');
 //# sourceMappingURL=version.js.map
