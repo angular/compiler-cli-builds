84664abccf9659cca05085206631e7aedc6584c9
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 19:09:07 +0000

    221899a ci: fix incorrect github credetials (#15146)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-1c1085b');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-221899a');
 //# sourceMappingURL=version.js.map
