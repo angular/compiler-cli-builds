80e3e544580d420de34c3dcaca9dbdfc80ca0a70
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:42 +0000

    80649ea fix(platform-server): correctly implement get href in parse5 adapter (#15022)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-778f7d6');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-80649ea');
 //# sourceMappingURL=version.js.map
