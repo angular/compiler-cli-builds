0b61d145f827300690235250fcd37681129fe2ab
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:11:05 +0000

    8850098 build(aio): ensure API docs are generated in the correct place (#15103)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-e6c81d2');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-8850098');
 //# sourceMappingURL=version.js.map
