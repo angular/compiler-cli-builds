4b0f3dbfad7dfd56bf15ae1e63549946733c6698
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:46:31 +0000

    62d5543 fix(animations): make sure easing values are applied to an empty animate() step (#15174)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-f1b33ab');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-62d5543');
 //# sourceMappingURL=version.js.map
