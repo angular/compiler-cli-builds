7724b96b116f92a19aeac2b7cf5818fba9e7394f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 23:32:45 +0000

    6772c91 fix(aio): scroll to hash fragment element on URL change

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-62d5543');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-6772c91');
 //# sourceMappingURL=version.js.map
