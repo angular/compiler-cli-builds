3c8648fde99eaa35aa0c120f182e1aff3eba8cc0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 22:42:17 +0000

    a9b1880 feat(aio): top bar typography, sidenav size

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-ec548ad');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-a9b1880');
 //# sourceMappingURL=version.js.map
