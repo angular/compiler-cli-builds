a83d99c9c026abaca631a8434fcd64f69bd29ac7
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:30:20 +0000

    3c15916 fix(compiler): Improve error message for missing annotations (#14724)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-959a03a');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-3c15916');
 //# sourceMappingURL=version.js.map
