30dd3080e5ef1e06ddf1d71a62f846b699042e90
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 16:30:52 +0000

    41f61b0 test: integration test that Angular apps compile with TS 2.2 (#15153)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-f92d644');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-41f61b0');
 //# sourceMappingURL=version.js.map
