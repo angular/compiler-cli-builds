675d04cf387c1e63ea9bad4740aaa857403964af
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 17:31:07 +0000

    26d4ce2 fix(forms): ensure observable validators are properly canceled (#15132)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-41f61b0');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-26d4ce2');
 //# sourceMappingURL=version.js.map
