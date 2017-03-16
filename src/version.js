40076320fa396f307430c763d3265f1c30af3a76
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:27:50 +0000

    6e98757 fix(core): use presence of .subscribe to detect observables rather then Symbol.observable (#15171)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.3-3131581');
+exports.VERSION = new core_1.Version('4.0.0-rc.3-6e98757');
 //# sourceMappingURL=version.js.map
