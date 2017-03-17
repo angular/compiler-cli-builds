2fa774f82e8d5235d6164dd0e62b68966e9da15a
Miško Hevery <misko@hevery.com>
2017-03-17 20:45:18 +0000

    f925910 fix(animations): make sure easing values work with web-animations (#15195)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-2d78c8c');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-f925910');
 //# sourceMappingURL=version.js.map
