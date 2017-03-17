781190577f4ec4fd1601200a8986451ecc2e1dcb
Miško Hevery <misko@hevery.com>
2017-03-17 22:06:43 +0000

    5c5c2ae fix(platform-server): setup NoopAnimationsModule in ServerModule by default (#15131)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-6e9264a');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-5c5c2ae');
 //# sourceMappingURL=version.js.map
