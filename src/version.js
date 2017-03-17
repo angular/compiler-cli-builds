f01de4f1752535c49b47f4361f9aabd4662b0cf4
GitHub <noreply@github.com>
2017-03-17 17:01:32 +0000

    7fb4528 fix(compiler-cli): update the tsc-wrapped dependency version (#15226)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.4-b7212f5');
+exports.VERSION = new core_1.Version('4.0.0-rc.4-7fb4528');
 //# sourceMappingURL=version.js.map
