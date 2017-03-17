2681971630501d5307191555172c59c4197547f2
Miško Hevery <misko@hevery.com>
2017-03-17 20:55:12 +0000

    d0bc83c build(aio): implement prerendering

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.4-fcaca45');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-d0bc83c');
 //# sourceMappingURL=version.js.map
