ee9782d200a6ef7a31e51bdcdcd2eb673cb94763
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 09:57:46 +0000

    d1e82b0 ci(aio): lower prerender browser instances

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-20f3b55');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-d1e82b0');
 //# sourceMappingURL=version.js.map
