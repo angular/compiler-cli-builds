9e87c7d6801fb9b769eb9a60f47fea57aaafa004
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 12:27:57 +0000

    6d4f5ce build(aio): update to dgeni-packages 0.17.0

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-d1e82b0');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-6d4f5ce');
 //# sourceMappingURL=version.js.map
