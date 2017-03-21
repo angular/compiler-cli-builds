35ef09b6f76532b1b129ab15b2d47706ece9a513
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 17:16:03 +0000

    f40f915 build(aio): upgrade to rxjs@5.2.0

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-11d5309');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-f40f915');
 //# sourceMappingURL=version.js.map
