a408b1ec633585918a88d92a5dfb86a157b772f7
Miško Hevery <misko@hevery.com>
2017-03-21 19:40:18 +0000

    8e6995c fix(core): stringify shouldn't throw when toString returns null/undefined (#14975)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-2489e4b');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-8e6995c');
 //# sourceMappingURL=version.js.map
