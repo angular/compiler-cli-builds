9a06d8c4e8894b0caabf244cb8631b2e1be68b0e
Miško Hevery <misko@hevery.com>
2017-03-21 23:30:32 +0000

    5efc860 fix(forms): make composition event buffering configurable (#15256)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-e59e5e2');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-5efc860');
 //# sourceMappingURL=version.js.map
