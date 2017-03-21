1b513c388a09b0f19e2d334d3c39979fc7be7cf0
Miško Hevery <misko@hevery.com>
2017-03-21 20:17:31 +0000

    64beae9 fix(core): mark components for check when host events trigger. (#15359)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-8e6995c');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-64beae9');
 //# sourceMappingURL=version.js.map
