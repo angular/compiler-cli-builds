df15fbf2475be3d825bfc7279f6c6d3845510eee
Miško Hevery <misko@hevery.com>
2017-03-22 16:57:36 +0000

    c584997 fix(tsc-wrapped): use windows friendly path normalization in bundler (#15374)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-1bcbcfd');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-c584997');
 //# sourceMappingURL=version.js.map
