066e9b00520521d100c58633520fcc8e64286af6
Igor Minar <iminar@google.com>
2017-03-22 20:44:32 +0000

    941f194 docs: spelling errors (#15375)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-8b4edcc');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-941f194');
 //# sourceMappingURL=version.js.map
