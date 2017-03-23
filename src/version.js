60844d1f1a7986dff23733dfc55a6b98b2fdcc25
Victor Berchet <victor@suumit.com>
2017-03-23 22:10:42 +0000

    b800a0c fix: prevent strictNullChecks support until #15432 is fixed (#15434)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.6-c8ab5cb');
+exports.VERSION = new core_1.Version('4.0.0-rc.6-b800a0c');
 //# sourceMappingURL=version.js.map
