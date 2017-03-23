b3ba3762900832d4751b9f860edcee6dfcd6097d
Victor Berchet <victor@suumit.com>
2017-03-23 17:44:10 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.6-73808dd');
+exports.VERSION = new core_1.Version('4.0.0-rc.6-376088d');
 //# sourceMappingURL=version.js.map
