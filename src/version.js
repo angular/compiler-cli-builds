f362888cad8388273692be5cdf6a7a6f462fd87e
Igor Minar <iminar@google.com>
2017-03-23 00:35:05 +0000

    9319b5f docs(common): update ngFor docs to new `as` syntax (#15166)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-edb2571');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-9319b5f');
 //# sourceMappingURL=version.js.map
