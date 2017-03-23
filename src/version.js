8fb9312411ad4e9428085e639d2f5a9535660842
Igor Minar <iminar@google.com>
2017-03-23 00:31:03 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-ea49a95');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-26f6bd4');
 //# sourceMappingURL=version.js.map
