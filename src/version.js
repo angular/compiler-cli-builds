25451ad7293e4b3f0e3e1aca53093894e8e04584
Igor Minar <iminar@google.com>
2017-03-23 04:44:51 +0000

    de87c47 docs: spelling errors

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.5-08d8675');
+exports.VERSION = new core_1.Version('4.0.0-rc.5-de87c47');
 //# sourceMappingURL=version.js.map
