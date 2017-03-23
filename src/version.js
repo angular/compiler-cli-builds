eedd1d93a05b789a5013a51130688217a993febc
Victor Berchet <victor@suumit.com>
2017-03-23 20:57:12 +0000

    c8ab5cb fix(compiler): assume queries with no matches as static (#15429)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-rc.6-0dda01e');
+exports.VERSION = new core_1.Version('4.0.0-rc.6-c8ab5cb');
 //# sourceMappingURL=version.js.map
