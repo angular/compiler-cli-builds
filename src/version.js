d004f99f9256a6a577ce7e2b3926a971dcdd2c77
GitHub <noreply@github.com>
2017-03-24 02:41:19 +0000

    b2b1195 docs: update the contributing link one more time :-)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-c65b754');
+exports.VERSION = new core_1.Version('4.0.0-b2b1195');
 //# sourceMappingURL=version.js.map
