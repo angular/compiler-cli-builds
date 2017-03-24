2a03c3d1a24736a51859bfe2ec51e6f2ebd60c01
Igor Minar <igor@angularjs.org>
2017-03-24 04:13:53 +0000

    c2892da docs: revert the move of CONTRIBUTING.md to docs/CONTRIBUTING.md

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-e1b09e3');
+exports.VERSION = new core_1.Version('4.0.0-c2892da');
 //# sourceMappingURL=version.js.map
