fb4730a1eba31251d8901519689d5a173038a284
Victor Berchet <victor@suumit.com>
2017-03-24 21:54:49 +0000

    8785b2b fix(router): shouldn't execute CanLoad when a route has been loaded

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-ea848f7');
+exports.VERSION = new core_1.Version('4.0.0-8785b2b');
 //# sourceMappingURL=version.js.map
