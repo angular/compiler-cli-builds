80aa749d5fcb07845a577b3645d514fb3baf9295
Victor Berchet <victor@suumit.com>
2017-03-27 19:13:04 +0000

    15662ef build(aio): update project config for @angular/cli v1.0.0

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-816b389');
+exports.VERSION = new core_1.Version('4.0.0-15662ef');
 //# sourceMappingURL=version.js.map
