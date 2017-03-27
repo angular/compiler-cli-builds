946056b5e171395a0162e6ddab9ee3095319a31d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 11:41:52 +0000

    837ed78 feat(aio): add code-example and code-tabs

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-52ea193');
+exports.VERSION = new core_1.Version('4.0.0-837ed78');
 //# sourceMappingURL=version.js.map
