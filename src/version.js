ab9683a3b4d035b190bc6b6a26dbe35006d74446
Victor Berchet <victor@suumit.com>
2017-03-28 21:58:36 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-aa16ccd');
+exports.VERSION = new core_1.Version('4.0.0-861953c');
 //# sourceMappingURL=version.js.map
