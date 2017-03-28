268d4bad3054501381401c59e7650664bc998961
Igor Minar <iminar@google.com>
2017-03-28 15:41:28 +0000

    eac99c1 build(aio): do not HTML format code-example contents (#15554)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-b7a89ce');
+exports.VERSION = new core_1.Version('4.0.0-eac99c1');
 //# sourceMappingURL=version.js.map
