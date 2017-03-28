13100acd635f91f86b02bb07b6de58a66e58cf1a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 11:11:36 +0000

    b7a89ce feat(aio): show top-nav with same styling as side-nav when window is narrow

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-9995191');
+exports.VERSION = new core_1.Version('4.0.0-b7a89ce');
 //# sourceMappingURL=version.js.map
