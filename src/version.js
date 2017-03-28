f22800db3a00546560a4dc39dcbf787ae077881d
Victor Berchet <victor@suumit.com>
2017-03-28 23:32:48 +0000

    0c36f23 perf(router): don't create new serializer every time UrlTree.toString is called (#15565)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-a580f8c');
+exports.VERSION = new core_1.Version('4.0.0-0c36f23');
 //# sourceMappingURL=version.js.map
