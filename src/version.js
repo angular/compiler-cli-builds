5d83deae09109b016e8de49b9b04a3694346efe7
Victor Berchet <victor@suumit.com>
2017-03-28 00:56:30 +0000

    920bf37 fix(language-service): guard access to `Symbol.members` (#15529)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-a9ae4da');
+exports.VERSION = new core_1.Version('4.0.0-920bf37');
 //# sourceMappingURL=version.js.map
