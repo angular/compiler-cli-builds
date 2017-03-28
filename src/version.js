ac264533a9a85f569c6f84651ec13d1353f4f7ca
Victor Berchet <victor@suumit.com>
2017-03-28 23:24:04 +0000

    a580f8c fix(animations): make sure style calculations are not computed too early (#15540)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-f368381');
+exports.VERSION = new core_1.Version('4.0.0-a580f8c');
 //# sourceMappingURL=version.js.map
