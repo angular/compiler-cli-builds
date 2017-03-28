0baaaaef0fb8978570ca3f5fea73a16e1fa9320b
Victor Berchet <victor@suumit.com>
2017-03-28 20:49:33 +0000

    6269d28 fix(language-service): improve performance of `updateModuleAnalysis()` (#15543)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-a5c972a');
+exports.VERSION = new core_1.Version('4.0.0-6269d28');
 //# sourceMappingURL=version.js.map
