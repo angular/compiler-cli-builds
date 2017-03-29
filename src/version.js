2639f8b34b75eca94f4ad7752ebee2bc7f18f7fb
Victor Berchet <victor@suumit.com>
2017-03-29 16:52:39 +0000

    910c0d9 fix(core): Update types for TypeScript nullability support (#15472)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-4916278');
+exports.VERSION = new core_1.Version('4.0.0-910c0d9');
 //# sourceMappingURL=version.js.map
