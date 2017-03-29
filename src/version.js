36ea23ad8cac6dcb5822ca877ce6f5cc89ac936d
GitHub <noreply@github.com>
2017-03-29 17:01:11 +0000

    d58a242 refactor(router): cleanup & simplification (#15436)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-910c0d9');
+exports.VERSION = new core_1.Version('4.0.0-d58a242');
 //# sourceMappingURL=version.js.map
