135f57b3fbd37266fcf6b460ae1b3f54cbecd39a
Victor Berchet <victor@suumit.com>
2017-03-29 22:27:50 +0000

    19cb503 docs(http): remove deprecated stuff and outdated plunkrs (#15598)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-9f2acf5');
+exports.VERSION = new core_1.Version('4.0.0-19cb503');
 //# sourceMappingURL=version.js.map
