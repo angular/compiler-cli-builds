2dc7508fffc629a48f0bf30d55ade90b196d253e
Victor Berchet <victor@suumit.com>
2017-03-29 23:46:08 +0000

    1dda05a docs: add changelog for 4.0.1

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.0.0-14fd78f');
+exports.VERSION = new core_1.Version('4.0.0-1dda05a');
 //# sourceMappingURL=version.js.map
