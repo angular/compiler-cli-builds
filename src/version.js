c46b4b834ec6cdb39f72658b7aa56c0d51fb6355
Igor Minar <iminar@google.com>
2017-03-30 07:13:01 +0000

    43a9619 feat(aio): overall margin, code, alert styles

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-3ccbe28');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-43a9619');
 //# sourceMappingURL=version.js.map
