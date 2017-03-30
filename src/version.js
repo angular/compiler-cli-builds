28f1212adb15a9d15ae4effb557942eb4e8b3563
Igor Minar <iminar@google.com>
2017-03-30 07:11:04 +0000

    3ccbe28 feat(aio): enable activating available ServiceWorker updates (#15561)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-67719f2');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-3ccbe28');
 //# sourceMappingURL=version.js.map
