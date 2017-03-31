f1b44d99fda8c026616cc1a01d18d135266cf667
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 15:38:13 +0000

    46f8a6d docs(aio): remove unnecessary blank lines

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-0aa90c6');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-46f8a6d');
 //# sourceMappingURL=version.js.map
