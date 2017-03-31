1a673157f78020be03f347eeb668e8d2ca37d8d7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 07:23:51 +0000

    8c925bc build(aio): update rho to new version

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-330a8c9');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-8c925bc');
 //# sourceMappingURL=version.js.map
