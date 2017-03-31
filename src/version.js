440a038b01f398ddc028985dcc54048f03ed2dab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:52:43 +0000

    5c38012 fix(aio): contributor.html typos

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-46f8a6d');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-5c38012');
 //# sourceMappingURL=version.js.map
