4ccbcdf45583444720161af546967b4950bebf4a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 07:39:41 +0000

    65bb237 fix(aio): remove "v.2" from index.html "title"

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-8c925bc');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-65bb237');
 //# sourceMappingURL=version.js.map
