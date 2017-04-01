c04005e291ec6ca935832e3e624ff10fc83fc045
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 11:03:51 +0000

    c8be396 style(aio): fix minor syntax error

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-97deb01');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-c8be396');
 //# sourceMappingURL=version.js.map
