6fa7b762e5545ef27ede781e1da5933cee6398f5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:01:03 +0000

    ad639d7 feat(aio): add contribute marketing page

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-e1c2e50');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-ad639d7');
 //# sourceMappingURL=version.js.map
