47bef40c89b45894ed612a8f8ea028617d2ca76e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 20:23:59 +0000

    62f9738 test(aio): enable linting of `transforms/` and add rules for jasmine

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-8c726ea');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-62f9738');
 //# sourceMappingURL=version.js.map
