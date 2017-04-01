616ebe2e306c61f19e388257290141edabe80ded
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:46:39 +0000

    08941aa build(aio): process marketing docs that have .md extension

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-03fb766');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-08941aa');
 //# sourceMappingURL=version.js.map
