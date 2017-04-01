39687b9f91fdcb74d5b2d7a070054232ad080cde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:30:39 +0000

    03fb766 feat(aio): shell code styling

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-2454a3b');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-03fb766');
 //# sourceMappingURL=version.js.map
