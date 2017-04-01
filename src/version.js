5f0d54e1710697fe3f70290c55c69fedd6ef46a5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:05:06 +0000

    97deb01 feat(aio): add about component and data

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-ad639d7');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-97deb01');
 //# sourceMappingURL=version.js.map
