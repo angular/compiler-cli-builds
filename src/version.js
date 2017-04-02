2a5b9abecb2069e6fca672cde3816278bde182ca
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 10:55:48 +0000

    34a1990 fix(aio): trim code blocks before rendering

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-e4c3882');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-34a1990');
 //# sourceMappingURL=version.js.map
