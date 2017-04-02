87514454a2464462e1b7213bb5952c4ac48a7bcb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 21:44:14 +0000

    ae70293 feat(aio): add DtComponent to view/change the raw contents

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-1e623a3');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-ae70293');
 //# sourceMappingURL=version.js.map
