dde84bdd2d243803b1579d0a88c25d2defd1caf8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 23:03:56 +0000

    25132bf feat(aio): marketing page styles

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-0e38bf9');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-25132bf');
 //# sourceMappingURL=version.js.map
