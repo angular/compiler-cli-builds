fa4d5c2d53db9e4183a50215b8fd2ee01ef61423
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-04 10:35:57 +0000

    c6917d9 build(aio): update boilerplate to new folder structure

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-25132bf');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-c6917d9');
 //# sourceMappingURL=version.js.map
