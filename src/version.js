2431749165ca85f764c7e2743f8c4441221a8a40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 21:52:37 +0000

    bde9771 fix(language-service): parse extended i18n forms

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-fe0d02f');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-bde9771');
 //# sourceMappingURL=version.js.map
