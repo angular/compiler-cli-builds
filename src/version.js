9f4486c1939ebe4ce4a45cac36c26a0fbf995068
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 20:30:51 +0000

    7b005aa style(router): fix typo in router preloader method

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-7764c5c');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-7b005aa');
 //# sourceMappingURL=version.js.map
