1a709836bc5b8aa6213b7c8d3ad6b945ec3b5150
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 08:28:54 +0000

    f4b5784 feat(animations): wip: introduce a wave of new animation features

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-294a205');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-f4b5784');
 //# sourceMappingURL=version.js.map
