6149a48625c08a29e6e523b40ab10e5524bdd2a2
Hans Larsen <hans@hansl.ca>
2017-04-10 22:59:03 +0000

    46ce331 test(router): enable running the campaign in non-ES6 browsers

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-83527fd');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-46ce331');
 //# sourceMappingURL=version.js.map
