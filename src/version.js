408d27965bab188aeac8f55f62bf361286e47e1c
Hans <hans@hansl.ca>
2017-04-11 21:41:31 +0000

    0799f18 fix(aio): v2 - when browser lacks service worker, disable sw feature

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-91b2e39');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-0799f18');
 //# sourceMappingURL=version.js.map
