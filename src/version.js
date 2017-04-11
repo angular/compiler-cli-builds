63f2069a8a2e17e4f2c813516a9215b52b3e59db
Hans <hans@hansl.ca>
2017-04-11 23:18:21 +0000

    04fb29b test(aio): add test for escaped newline in CodeComponent

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-3ced940');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-04fb29b');
 //# sourceMappingURL=version.js.map
