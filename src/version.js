2559f37050921fd0bf2418f60b16c430c0f4bc04
Hans <hans@hansl.ca>
2017-04-11 18:31:29 +0000

    a487563 fix(router): fix query param parsing

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-46ce331');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-a487563');
 //# sourceMappingURL=version.js.map
