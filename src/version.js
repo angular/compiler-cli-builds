114cdf85c521c7ff7f04024c229867fe665efa7f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:25:44 +0000

    909264f hr update

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-0614754');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-909264f');
 //# sourceMappingURL=version.js.map
