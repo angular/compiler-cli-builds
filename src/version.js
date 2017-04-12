ed2a3b68585f274dae599fbc11da919dccc53ebb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 14:03:40 +0000

    075f3f8 feat(aio): search results layer fix and responsive table

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-2798c1b');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-075f3f8');
 //# sourceMappingURL=version.js.map
