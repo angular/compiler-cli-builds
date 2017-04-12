787071bc3fec2faef70616f1c450af15eb17bf50
Hans Larsen <hans@hansl.ca>
2017-04-12 01:30:24 +0000

    38d75d4 feat(animations): Update types for TypeScript nullability support

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-04fb29b');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-38d75d4');
 //# sourceMappingURL=version.js.map
