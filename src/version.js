c8823ff7a918774f9b64818acd7910051316e9ca
Hans <hans@hansl.ca>
2017-04-12 18:52:39 +0000

    540581d fix(language-service): Update types for TypeScript nullability support

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-075f3f8');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-540581d');
 //# sourceMappingURL=version.js.map
