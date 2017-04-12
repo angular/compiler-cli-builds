6ac4bb6aeec96c70b638d7e907004e4f7c1d62d1
Igor Minar <iminar@google.com>
2017-04-12 19:43:29 +0000

    8659bd2 docs(aio): template -> ng-template (#15912)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-4a052cd');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-8659bd2');
 //# sourceMappingURL=version.js.map
