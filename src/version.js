558823324d5d69f6b33548c4fb63d8521c8e166c
Igor Minar <iminar@google.com>
2017-04-12 19:52:17 +0000

    8f1359d docs(aio): make events.html current (#15913)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-8659bd2');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-8f1359d');
 //# sourceMappingURL=version.js.map
