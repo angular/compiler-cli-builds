572d930390bd1699e1a86033fb8f21d8c00e2fd0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 20:18:03 +0000

    0614754 docs(aio): final migration from old site

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-8f1359d');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-0614754');
 //# sourceMappingURL=version.js.map
