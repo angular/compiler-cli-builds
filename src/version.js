56810e828ae7df0c4973fa599a22a64e95fb1ba2
GitHub <noreply@github.com>
2017-04-12 19:40:50 +0000

    4a052cd build(aio): upgrade to @angular/material#2.0.0-beta.3 (#15606)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.0-28c2a71');
+exports.VERSION = new core_1.Version('4.1.0-beta.0-4a052cd');
 //# sourceMappingURL=version.js.map
