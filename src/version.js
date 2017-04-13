ee1743b48e6ff604aeb04b47e6a793199bca79fc
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:12:43 +0000

    fdb3f26 refactor(compiler): cleanup (#15960)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-14b7dfa');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-fdb3f26');
 //# sourceMappingURL=version.js.map
