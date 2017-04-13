28a0711a1d928bb61984735a9f9c012da3a08715
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:11:01 +0000

    70b1d6d fix(tsc-wrapped): collect new expressions with no arguments (#15908)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-9394835');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-70b1d6d');
 //# sourceMappingURL=version.js.map
