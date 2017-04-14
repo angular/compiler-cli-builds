56b5412d5e3a1b5b6cddc41db9e037198a0e465a
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:21:29 +0000

    cb5a7ef fix(core): key-value differ changes iteration (#15968)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-8ad464d');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-cb5a7ef');
 //# sourceMappingURL=version.js.map
