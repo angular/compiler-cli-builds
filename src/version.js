01f4363f6687eb4055b3f7e93e110f992e2609b6
GitHub <noreply@github.com>
2017-04-14 01:18:52 +0000

    2e47a0d Revert "fix(forms): Update types for TypeScript nullability support (#15859)" (#15977)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-6a2e08d');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-2e47a0d');
 //# sourceMappingURL=version.js.map
