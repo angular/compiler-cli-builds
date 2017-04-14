ad67998d9288f070b116c8d812413359736a0d38
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:18:11 +0000

    ce47d33 fix(compiler): ignore calls to unresolved symbols in metadata (#15970)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-2e47a0d');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-ce47d33');
 //# sourceMappingURL=version.js.map
