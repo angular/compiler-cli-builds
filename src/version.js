6ef860e8e10481d03569bbd4ae7d424f4bf36fae
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:52:18 +0000

    a2bdcc9 feat(aio): callout, code ex header styles

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-16673fa');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-a2bdcc9');
 //# sourceMappingURL=version.js.map
