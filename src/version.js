8242eec1364133eded51b5fde59dd70c5db48331
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 16:31:09 +0000

    eda2a7b ci(aio): do not fail if PWA score testing fails on staging

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-196203f');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-eda2a7b');
 //# sourceMappingURL=version.js.map
