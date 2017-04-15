5eedeccecc9b49244957b78b51cdcff2bac29410
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 16:09:24 +0000

    196203f feat(aio): implement resources with resources.json

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-46b0c7a');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-196203f');
 //# sourceMappingURL=version.js.map
