5865f90afa2bce76ea976edb1ffc59af9d653c98
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:46:09 +0000

    b57c960 feat(aio): pwa bg color and name

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-49dfc9f');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-b57c960');
 //# sourceMappingURL=version.js.map
