2826337aedcd515140941bdcf2d2273de69909e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:54:29 +0000

    7d986ae feat(aio): smaller typography, code header pad

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-a2bdcc9');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-7d986ae');
 //# sourceMappingURL=version.js.map
