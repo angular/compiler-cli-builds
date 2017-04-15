955e0ac47b1e5d5de3635322d88b970e01255c5f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:37:40 +0000

    092f0df test(aio): remove dross from NavMenuComponent spec

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-dfc81c3');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-092f0df');
 //# sourceMappingURL=version.js.map
