1c7c0bc83b0ea039870cf0d952568adf00baa856
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 12:49:33 +0000

    b6a0098 ci(aio): make `deploy-staging.sh` executable

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-7d986ae');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-b6a0098');
 //# sourceMappingURL=version.js.map
