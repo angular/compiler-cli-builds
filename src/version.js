918ff229c8dddfaf0bc57a096a987d8e2fc81b33
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 21:44:45 +0000

    7433da5 build(aio): give intellisense to the examples

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-eda2a7b');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-7433da5');
 //# sourceMappingURL=version.js.map
