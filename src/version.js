8316321853168a46cdbe0ee3549187480b051e7d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:20 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-8479e9e');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-c0b1bbe');
 //# sourceMappingURL=version.js.map
