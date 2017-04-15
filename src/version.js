4c811c3ae7e749d9072af1302e93bc0ec73cabc7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:47:34 +0000

    624b5a5 feat(aio): filetree and top bar hover styles

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-b57c960');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-624b5a5');
 //# sourceMappingURL=version.js.map
