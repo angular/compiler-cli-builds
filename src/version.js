b793d9bdd178f0f68e873dc0d0c5fce1331d8c4e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:26:30 +0000

    4a3c66f feat(aio): fixed sidenav, moved footer inside of sidenav container (#16028)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-9a50844');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-4a3c66f');
 //# sourceMappingURL=version.js.map
