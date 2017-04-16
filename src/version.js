bd5f13487aa1490204934a9ec734dc37394fe52d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 07:28:16 +0000

    3f307ff style(aio): fix indentation on location service spec

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-7433da5');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-3f307ff');
 //# sourceMappingURL=version.js.map
