292d413a36a641bb50b936294609403f2fcd1372
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 21:33:05 +0000

    abecf3e build(aio): move the `transforms` folder into the `tools` folder

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-7c418a7');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-abecf3e');
 //# sourceMappingURL=version.js.map
