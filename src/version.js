380d7c9377a4e6c6f46dcb6b1103016511b3e37c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 07:08:52 +0000

    e20a722 feat(aio): when select node in sidebar close other expanded nodes

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-abecf3e');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-e20a722');
 //# sourceMappingURL=version.js.map
