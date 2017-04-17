5cd929c872b10787b5749ce115e62353a91f90ca
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:05:09 +0000

    0a3a9af fix(language-service): infer correct type of `?.` expressions

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-5a88d2f');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-0a3a9af');
 //# sourceMappingURL=version.js.map
