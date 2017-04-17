52ea00c46f6444a57f8febff16f29951be64a037
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:06:42 +0000

    2ddf3bc fix(language-service): look for type constructors on canonical symbol

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-0a3a9af');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-2ddf3bc');
 //# sourceMappingURL=version.js.map
