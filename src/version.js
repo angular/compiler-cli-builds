dff59f9f356783d4e8609e6a5b6e5ac324d95660
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:31:20 +0000

    5b141fb fix(compiler): support `<ng-container>` whatever the namespace

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-2688842');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-5b141fb');
 //# sourceMappingURL=version.js.map
