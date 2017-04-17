24ba43c5ca8ce60081af29a9d535150272fecebf
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:34:35 +0000

    82417b3 fix(router): prevent `RouterLinkActive` from causing an infinite CD loop

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-5b141fb');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-82417b3');
 //# sourceMappingURL=version.js.map
