e214ab7cb7bf0ea7a1a53ab8d8f6bcd865cd1d29
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:12:43 +0000

    2688842 Revert "fix(http): Update types for TypeScript nullability support"

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-2ddf3bc');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-2688842');
 //# sourceMappingURL=version.js.map
