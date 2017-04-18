a5bf4d82f18087418a5ce4d1679ab49c1ca6cc31
Tobias Bosch <tbosch1009@gmail.com>
2017-04-18 00:59:38 +0000

    7dfa0b9 fix(aio): about page

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-90814e4');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-7dfa0b9');
 //# sourceMappingURL=version.js.map
