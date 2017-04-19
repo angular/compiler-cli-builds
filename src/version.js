f0aba387617f2091abe6872d17fb587f096e47f6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 07:53:20 +0000

    b668c2c build(aio): IDEs should only parse the `src` files

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-ecd0348');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-b668c2c');
 //# sourceMappingURL=version.js.map
