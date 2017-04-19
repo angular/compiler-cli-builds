00909ce87f28383f61566176bb50cccd993b60fa
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:14:18 +0000

    c9983e6 docs(aio): Fix reference to the right example

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-51098c4');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-c9983e6');
 //# sourceMappingURL=version.js.map
