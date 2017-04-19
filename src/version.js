33bfba3f05f4b4c8138c1f7bee6dc4784e6d6662
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:48:09 +0000

    9d40ab9 docs(aio): rename ToH to match the guide name

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-691e86c');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-9d40ab9');
 //# sourceMappingURL=version.js.map
