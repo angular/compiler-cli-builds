0fa840e435a29bd9a261f8bd3fe182ededd0e5a3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:33:58 +0000

    8b31178 docs(aio): remove styling comments from cheatsheet

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-e951612');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-8b31178');
 //# sourceMappingURL=version.js.map
