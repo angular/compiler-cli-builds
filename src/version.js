86250ca36a24ccd02afafb37f5f5b5f8a6dd57dd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:12:44 +0000

    51098c4 fix(aio): allow empty titles for contents pages

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-50174d9');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-51098c4');
 //# sourceMappingURL=version.js.map
