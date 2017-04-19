f501925bc7cb91d3aacb79a3fe1e428ce3b24bbd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 08:37:00 +0000

    e951612 fix(aio): set the pageId to the file-not-found URL if the doc is not available

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-b668c2c');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-e951612');
 //# sourceMappingURL=version.js.map
