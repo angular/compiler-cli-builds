b829011963ed46767db4e7ed9b467c1eec5196d9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:44:16 +0000

    691e86c docs(aio): fix minor typos in deployment guide

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-93d8f8c');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-691e86c');
 //# sourceMappingURL=version.js.map
