8dd1b2f61855e6f05a7353ab316258206f6203ba
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:37:48 +0000

    8fa4453 docs(aio): Testing Guide: Second matcher parameter

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-c2fca3d');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-8fa4453');
 //# sourceMappingURL=version.js.map
