4e3e20677772617ab1d8709351831a51df4a49a5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:08 +0000

    a54fe63 docs(aio): move file to the right folder

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-800b1b0');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-a54fe63');
 //# sourceMappingURL=version.js.map
