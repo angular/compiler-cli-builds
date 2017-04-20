3ef7908d79b584ebe87b7f8dd6814f200773b6cb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:26:19 +0000

    d313aad docs(aio): remove js examples

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-ccb6c45');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-d313aad');
 //# sourceMappingURL=version.js.map
