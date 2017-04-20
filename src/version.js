45276cfbe78c713176b08755084c70682a13ab26
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:50:22 +0000

    134f542 docs(aio): fix spacing on l-sub-sections

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-d313aad');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-134f542');
 //# sourceMappingURL=version.js.map
