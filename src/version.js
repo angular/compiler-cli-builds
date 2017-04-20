dadcc46d47d4b671807e6e8c933529b8bc86aaab
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 10:06:14 +0000

    800b1b0 fix(aio): autoscroll & add #top-of-page

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-ab03852');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-800b1b0');
 //# sourceMappingURL=version.js.map
