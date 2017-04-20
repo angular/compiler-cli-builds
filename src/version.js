8d6f9db5c0cc9b312d3a1038c5eab8b8bf40b659
Miško Hevery <misko@hevery.com>
2017-04-20 03:55:58 +0000

    bfd5f27 feat(aio): add e2e (#15976)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-e07b7ea');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-bfd5f27');
 //# sourceMappingURL=version.js.map
