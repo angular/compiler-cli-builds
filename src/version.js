2b122c8e6ac75a142f6398ee5a84d2e694260c00
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:54:56 +0000

    ab03852 fix(aio): accessibility items in search and home

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-beta.1-f902581');
+exports.VERSION = new core_1.Version('4.1.0-beta.1-ab03852');
 //# sourceMappingURL=version.js.map
