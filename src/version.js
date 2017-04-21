967456becb84b8295e5b04c5f162a40b27162373
Miško Hevery <misko@hevery.com>
2017-04-21 17:27:12 +0000

    062fc4a docs(aio): change hardcoded links to the old site (#16196)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-cf034f7');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-062fc4a');
 //# sourceMappingURL=version.js.map
