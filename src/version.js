c373be252cf0ebd8318085ac9039550debb911c3
Miško Hevery <misko@hevery.com>
2017-04-21 20:53:17 +0000

    9c1318d fix(aio): strip leading slashes from path (and improve DRY-ness) (#16238)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-062fc4a');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-9c1318d');
 //# sourceMappingURL=version.js.map
