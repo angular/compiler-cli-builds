0b2937207fa7d1f257ea0e8d10a80ec7dbe823f0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:20:48 +0000

    cbe5f78 docs(aio): fix old jade on DI

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-4598af2');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-cbe5f78');
 //# sourceMappingURL=version.js.map
