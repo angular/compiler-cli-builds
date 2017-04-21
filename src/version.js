a1cfebb235346222720d2bcbde2c3b08cace7223
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 09:14:10 +0000

    4598af2 docs(aio): adjust zips/plunker paths

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-d855e90');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-4598af2');
 //# sourceMappingURL=version.js.map
