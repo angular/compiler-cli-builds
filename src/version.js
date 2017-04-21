9b0cba186ff4decff4d8349fdc1e122b7008eb99
Miško Hevery <misko@hevery.com>
2017-04-21 20:55:37 +0000

    7520ddc feat(aio): dont set query params during search #16125 (#16217)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-9c1318d');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-7520ddc');
 //# sourceMappingURL=version.js.map
