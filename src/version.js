36925daaad3c1d5c6d584eb9331f8e7a75471122
Miško Hevery <misko@hevery.com>
2017-04-24 17:14:10 +0000

    feda017 docs(aio): Fix decorator JS feature explanation (#16214)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-2991221');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-feda017');
 //# sourceMappingURL=version.js.map
