c0cfe48c35d89a9fc5591b511b215764bd84501c
Miško Hevery <misko@hevery.com>
2017-04-24 17:54:10 +0000

    d1fb066 fix(upgrade): use correct attribute name for upgraded component's bindings (#16128)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-feda017');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-d1fb066');
 //# sourceMappingURL=version.js.map
