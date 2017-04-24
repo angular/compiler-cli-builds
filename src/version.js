9d159fd64fd8ed617b83ef693585a9b7240fae96
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 13:37:25 +0000

    54e587a fix(aio): fix URL redirection for API pages

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-4ac5096');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-54e587a');
 //# sourceMappingURL=version.js.map
