1e90adb9d1846f1b1b0ef5ce18edfd1fc0b11bab
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 12:31:26 +0000

    c3fa880 fix(aio): use SVG icons for page load sensitive UI

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-3cad5da');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-c3fa880');
 //# sourceMappingURL=version.js.map
