72bab6b49df29645695455b6b0f5bf55781d9d9d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 14:01:44 +0000

    d7719aa perf(aio): improve unit test rebuild time

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-54e587a');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-d7719aa');
 //# sourceMappingURL=version.js.map
