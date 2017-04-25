e651328f30fcc3c8b67681102989cfb960c32d9c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:23:01 +0000

    ff0f53c build(aio): include missing packages in the docs

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-64ef69f');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-ff0f53c');
 //# sourceMappingURL=version.js.map
