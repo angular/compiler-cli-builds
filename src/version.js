dd2afc7a39c095db7bc45c69f112022abe53803d
Miško Hevery <misko@hevery.com>
2017-04-26 19:23:16 +0000

    b9723f9 docs: add changelog for 4.1.0

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-bb52e22');
+exports.VERSION = new core_1.Version('4.1.0-b9723f9');
 //# sourceMappingURL=version.js.map
