a86a760db943d33b68b3de10502aa0caaefb8030
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 13:52:10 +0000

    bb52e22 feat(aio): api pages styling

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-35a2dfc');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-bb52e22');
 //# sourceMappingURL=version.js.map
