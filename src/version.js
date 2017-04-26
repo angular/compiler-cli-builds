e0998c316c5ba75770b09fee22d149f811c1ab0f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:37:01 +0000

    ac5e6ba fix(aio): AppComponent should scroll only once when location changes

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-710b4a3');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-ac5e6ba');
 //# sourceMappingURL=version.js.map
