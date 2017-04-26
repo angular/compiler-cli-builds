80e6be23d5e627d8321292e17c32a3285e2c77ab
Miško Hevery <misko@hevery.com>
2017-04-26 22:10:43 +0000

    6bac4e6 build(aio): update dgeni-packages to 0.17.2 (#16358)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-a28f5ee');
+exports.VERSION = new core_1.Version('4.1.0-6bac4e6');
 //# sourceMappingURL=version.js.map
