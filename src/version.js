8da60c8edce8bf8bec65f2c4ffe2cff23a07aaf3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:15:11 +0000

    710b4a3 feat(aio): layout max width and design cleanup

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-rc.0-5331fc1');
+exports.VERSION = new core_1.Version('4.1.0-rc.0-710b4a3');
 //# sourceMappingURL=version.js.map
