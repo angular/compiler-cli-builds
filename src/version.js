7e98e1d32b52e9af94a1fd17885c0d82a0f12bf7
Miško Hevery <misko@hevery.com>
2017-04-30 16:28:17 +0000

    8d300ff fix(aio): header click should always toggle expand/collapse (#16439)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-065b76d');
+exports.VERSION = new core_1.Version('4.1.0-8d300ff');
 //# sourceMappingURL=version.js.map
