9b68264f2604dae46c5e17a54d38a81a3e6a0260
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:08:18 +0000

    ff32c53 docs(aio): fix lots of broken links

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-c8fd904');
+exports.VERSION = new core_1.Version('4.1.0-ff32c53');
 //# sourceMappingURL=version.js.map
