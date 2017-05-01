3d1ffd2459addc8483ce11fd8a175f70f5d28a32
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:07:35 +0000

    c8fd904 fix(aio): remove max width on marketing pages and footer

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-aaa5628');
+exports.VERSION = new core_1.Version('4.1.0-c8fd904');
 //# sourceMappingURL=version.js.map
