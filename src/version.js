46dcf1e23b050650e31f4813e3f53865c77d5b91
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:09:32 +0000

    235eb17 build(aio): improve info-bar template

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-ff32c53');
+exports.VERSION = new core_1.Version('4.1.0-235eb17');
 //# sourceMappingURL=version.js.map
