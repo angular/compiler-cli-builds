4894b371db6f4768647661d36fa2d5ab9105f14f
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:14:57 +0000

    d32bc5d feat(aio): add tooltip to nav node if none specified

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-efaf502');
+exports.VERSION = new core_1.Version('4.1.0-d32bc5d');
 //# sourceMappingURL=version.js.map
