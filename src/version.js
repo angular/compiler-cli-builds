5adca12cf5abf824b21e4ca49ea66437993c03a7
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:06:02 +0000

    65a8f7f fix(aio): search result styling

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-f32bfcd');
+exports.VERSION = new core_1.Version('4.1.0-65a8f7f');
 //# sourceMappingURL=version.js.map
