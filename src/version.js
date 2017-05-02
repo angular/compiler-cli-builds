79d31670c24af66f2f3c19b9cf666cc9b5a748f2
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 23:08:47 +0000

    c946a92 refactor(compiler): simplify AOT tests

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-21c96a5');
+exports.VERSION = new core_1.Version('4.1.0-c946a92');
 //# sourceMappingURL=version.js.map
