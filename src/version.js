02bcf05cc8ef88f07ca93626d2ffe190cb68ac90
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:07:42 +0000

    55b8de9 build(aio): lint examples

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-eb56ab3');
+exports.VERSION = new core_1.Version('4.1.0-55b8de9');
 //# sourceMappingURL=version.js.map
