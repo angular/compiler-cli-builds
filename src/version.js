b5c9bad5249d08c17733319b00c08cc6b3eb0e0c
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:03:04 +0000

    f29c6bb docs(aio): fix missing title warnings

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-464701a');
+exports.VERSION = new core_1.Version('4.1.0-f29c6bb');
 //# sourceMappingURL=version.js.map
