f0be8a8c7390fd5a36e449f584ddb2572db7ee97
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:47:20 +0000

    c8dc116 docs(aio): fixing new url on glossary date pipe

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-9684d78');
+exports.VERSION = new core_1.Version('4.1.0-c8dc116');
 //# sourceMappingURL=version.js.map
