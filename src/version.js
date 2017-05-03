4a21ef2b403ad7e0ab1f0fdd513a34c025b65012
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:58:11 +0000

    5b96fb9 build(aio): rename `src/content` to `src/generated`

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-1cfb263');
+exports.VERSION = new core_1.Version('4.1.0-5b96fb9');
 //# sourceMappingURL=version.js.map
