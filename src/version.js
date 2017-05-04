52240694713d17a75ab3ea0a2fa76e72f6c5fbb5
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 20:32:09 +0000

    1092292 test(compiler-cli): add test for missingTranslation parameter

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-8931e71');
+exports.VERSION = new core_1.Version('4.1.0-1092292');
 //# sourceMappingURL=version.js.map
