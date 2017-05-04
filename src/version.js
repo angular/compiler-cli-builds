71a66415a4f0a4e5251d690c07b114f49ac3db84
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:41:15 +0000

    aec65de refactor(aio): simplify DocViewer

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-919ff12');
+exports.VERSION = new core_1.Version('4.1.0-aec65de');
 //# sourceMappingURL=version.js.map
