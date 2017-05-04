b5bea57f0654b9af0c890f6c103b02b70a0f77f4
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:09:23 +0000

    9449eff docs(aio): improve tutorial next-steps prose/links

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-cd28df6');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-9449eff');
 //# sourceMappingURL=version.js.map
