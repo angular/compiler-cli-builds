94cee23ddf0cb33ad172b8e8d4310d749e73962d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:46:35 +0000

    895f47a refactor(aio): remove work-around for browsers without ServiceWorker support

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-aec65de');
+exports.VERSION = new core_1.Version('4.1.0-895f47a');
 //# sourceMappingURL=version.js.map
