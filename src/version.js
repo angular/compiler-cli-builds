6a97ec4a854f444823e3a576195a7bc40b008552
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 13:04:30 +0000

    8931e71 build(aio): ensure that tutorial index arrives in the tutorial search area

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.1.0-895f47a');
+exports.VERSION = new core_1.Version('4.1.0-8931e71');
 //# sourceMappingURL=version.js.map
