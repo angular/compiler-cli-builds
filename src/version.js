9e3d1071a4aeba39877bcf2f8dce57e4bb76ccfd
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 21:00:26 +0000

    518eb54 refactor(aio): tweak the CSS to account for new CSS classes on `aio-shell`

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-42dc2c1');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-518eb54');
 //# sourceMappingURL=version.js.map
