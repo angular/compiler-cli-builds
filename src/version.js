391168c8b883e4c1b60f476476365dd0ca5f850b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:03:29 +0000

    b7caa3e fix(aio): do not route `eplnkr` URLs to / from ServiceWorker

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-3e33482');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-b7caa3e');
 //# sourceMappingURL=version.js.map
