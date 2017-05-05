a007cd2bd98c86dff904436d0f30ff5adaecbb86
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 18:35:58 +0000

    547c363 feat: add .ngsummary.ts files to support AOT unit tests

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-d27588b');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-547c363');
 //# sourceMappingURL=version.js.map
