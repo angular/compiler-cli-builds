da3fd6d1854ea1841e3db21ac34c5f4b7fa7b173
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:53:09 +0000

    5cf6426 docs(aio): fix sizing of marketing doc images

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-03513e9');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-5cf6426');
 //# sourceMappingURL=version.js.map
