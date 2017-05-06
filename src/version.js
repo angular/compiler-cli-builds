f138af22c76aafcbeca6c2ac50642eda34dfe814
Matias Niemelä <matias@yearofmoo.com>
2017-05-06 00:24:17 +0000

    4874765 fix(compiler): avoid a `...null` spread in extraction

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-9da6340');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-4874765');
 //# sourceMappingURL=version.js.map
