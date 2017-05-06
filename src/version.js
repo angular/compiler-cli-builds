90da65bbb5d99be5b318e21a298cf4a3a76c2061
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:49:39 +0000

    b9ed97c fix(aio): do not display a Toc initially

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-799be9c');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-b9ed97c');
 //# sourceMappingURL=version.js.map
