ddeb6991b4ed059eced5201f13bb8740102fde44
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-07 07:31:09 +0000

    6dc6777 docs(aio): move links from nav groups to items

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-5cf6426');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-6dc6777');
 //# sourceMappingURL=version.js.map
