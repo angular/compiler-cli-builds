b973c793b5a8f40bf86ab5d0b326359fe99f5084
Miško Hevery <misko@hevery.com>
2017-05-08 18:07:08 +0000

    98dd609 docs(animations): remove duplicate word (#16508)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-6dc6777');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-98dd609');
 //# sourceMappingURL=version.js.map
