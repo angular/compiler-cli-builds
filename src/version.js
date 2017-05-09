44f488e420d260435195fb8df6656975d63f1074
GitHub <noreply@github.com>
2017-05-09 23:54:26 +0000

    a2dcb7b build: update key for pushing to PACKAGE-builds repos (#16667)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-c5ce040');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-a2dcb7b');
 //# sourceMappingURL=version.js.map
