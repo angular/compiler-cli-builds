4709155e3ea7275620d41b30efddd2196997cfa4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 11:12:24 +0000

    efa2928 docs(aio): add a progress reporter for the webpack guide

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-f5335d1');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-efa2928');
 //# sourceMappingURL=version.js.map
