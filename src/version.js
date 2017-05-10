e82cb4d8ea6cfa72bfa31cef078e5ae3dad7a2c8
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:09:03 +0000

    f795f64 refactor(aio): remove redundant styles

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-063db64');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-f795f64');
 //# sourceMappingURL=version.js.map
