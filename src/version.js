80921c6f43d713b6b11e4a6f4e29d38ecbe266e3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 10:57:21 +0000

    f5335d1 build(aio): fail the doc-gen if there is an invalid `{@link ...}` tag

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.0-dc7d242');
+exports.VERSION = new core_1.Version('4.2.0-beta.0-f5335d1');
 //# sourceMappingURL=version.js.map
