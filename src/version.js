6beb52db114558745d31b25bd969c325db06376c
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 17:47:45 +0000

    5afaa39 refactor(compiler): produce more dense generated code (#16666)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-ce1d7c4');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-5afaa39');
 //# sourceMappingURL=version.js.map
