a34b45c11e4c65618240f011d44b525eca79371a
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 21:12:46 +0000

    44c7ac0 fix(upgrade): use quote to prevent ClossureCompiler obfuscating $event. (#16724)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-abb36e3');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-44c7ac0');
 //# sourceMappingURL=version.js.map
