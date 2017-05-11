8492b4b9a17d6606547f17cb78e4ff99900bdf7b
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 20:40:33 +0000

    abb36e3 fix(aio): switch to ngu-sw-manifest gen to unblock critical path (#16709)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-84c30be');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-abb36e3');
 //# sourceMappingURL=version.js.map
