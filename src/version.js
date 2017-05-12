87b4657e5eb9a1efb334152ee1a8c7b1d3f98e8e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:47:19 +0000

    b016984 docs(aio): resize images

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-90bc5b2');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-b016984');
 //# sourceMappingURL=version.js.map
