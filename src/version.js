4eb970a1e106116c54462f1d3ff211dca193a9a8
Igor Minar <iminar@google.com>
2017-05-12 19:36:39 +0000

    0626464 build: use subshells when changing directories

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-21d213d');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-0626464');
 //# sourceMappingURL=version.js.map
