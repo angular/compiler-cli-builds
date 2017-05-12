8939776003615d33723d61b75c82feba72d3aab8
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-12 17:21:44 +0000

    569b1e0 fix(router): Wrap Promise-like instances in native Promises (#16759)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-b83fe6f');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-569b1e0');
 //# sourceMappingURL=version.js.map
