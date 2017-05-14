62300ea889a28b57f9fc30bfb59c55a4c25f5ec4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-14 15:05:18 +0000

    ecc356e fix(aio): set focus back to search box on "escape"

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-4a599ee');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-ecc356e');
 //# sourceMappingURL=version.js.map
