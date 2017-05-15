fba54472ee0dbe58480872fb4372f11cf2a2ebb9
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:29:47 +0000

    ea2b24f fix: add typescript 2.3.2 typings test (#16738)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-a9d9aa1');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-ea2b24f');
 //# sourceMappingURL=version.js.map
