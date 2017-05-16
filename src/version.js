552e53931dbd9806098a14bad0d9505a459d3314
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 17:38:42 +0000

    af99cf2 build(aio): compute `{@link ...} titles more effectively (#16813)

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-221f85a');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-af99cf2');
 //# sourceMappingURL=version.js.map
