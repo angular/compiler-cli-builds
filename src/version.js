c08ed336c41f39cec1797a2428b8f2ec20466f5d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:06:21 +0000

    2ee7662 test(aio): remove compileComponents from all tests

--- src/version.js
+++ src/version.js
@@ -15,5 +15,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-4419585');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-2ee7662');
 //# sourceMappingURL=version.js.map
