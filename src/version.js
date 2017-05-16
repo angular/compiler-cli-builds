21e13c1a2c91b23ac772adb9b59c4ac3892e88af
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 22:32:40 +0000

    c805082 fix(platform-server): wait for async app initializers to complete before removing server side styles (#16712)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-9a7f5d5');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-c805082');
 //# sourceMappingURL=version.js.map
