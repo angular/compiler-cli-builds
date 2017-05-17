e4ab93839c33b55f34d4001a4390dde2fa047a8b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-17 07:43:23 +0000

    238c523 fix(aio): remove searchbox shrink animation

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-593fe5e');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-238c523');
 //# sourceMappingURL=version.js.map
