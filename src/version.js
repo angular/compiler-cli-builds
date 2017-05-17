5f1b014527820013d4df1fe911587deb2dcc96ab
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 15:59:54 +0000

    6416e79 build: do not create the bundles when updating the public API

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-238c523');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-6416e79');
 //# sourceMappingURL=version.js.map
