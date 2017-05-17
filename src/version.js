e208959ffa288b3c897d2401eaaeba1f54422c42
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 00:57:46 +0000

    54a6e4f refactor(animations): make animation testing work with fixture.whenRenderingDone

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-d761059');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-54a6e4f');
 //# sourceMappingURL=version.js.map
