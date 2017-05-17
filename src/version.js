b9de47ff6ca66d1f78ee003ea03154eb28922947
Matias Niemelä <matias@yearofmoo.com>
2017-05-17 23:46:44 +0000

    712630c perf(animations): reduce size of animations bundle

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-6416e79');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-712630c');
 //# sourceMappingURL=version.js.map
