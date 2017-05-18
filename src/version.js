a5e02c8788057740cb4c46de2b896a7848da16d9
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:08:57 +0000

    0e13a59 fix(aio): add small delay before autoscrolling

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-3361a7b');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-0e13a59');
 //# sourceMappingURL=version.js.map
