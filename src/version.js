6804fd1376ea66e1ef9413002d370f939078eead
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 19:14:29 +0000

    86b7bd9 revert: refactor(router): cleanup, simplification

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-6531806');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-86b7bd9');
 //# sourceMappingURL=version.js.map
