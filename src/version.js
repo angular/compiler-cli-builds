1047157be793484113324eb7953abb6a5124e1ce
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:03:06 +0000

    3361a7b build(aio): minor `update-preview-server.sh` improvements

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-9466908');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-3361a7b');
 //# sourceMappingURL=version.js.map
