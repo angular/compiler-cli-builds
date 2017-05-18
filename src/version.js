46995658ecb1386a198cdfa4a8248ec6d3657024
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 15:10:07 +0000

    6531806 build(aio): do not ignore `is-on-https` in lighthouse check

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-0e13a59');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-6531806');
 //# sourceMappingURL=version.js.map
