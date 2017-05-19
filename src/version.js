8dace0a278d6040c11ea52a6dcf2cbd6c4a4dcc7
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 20:11:58 +0000

    b10029c docs(animations): add documentation for new animation features

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-5d4f543');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-b10029c');
 //# sourceMappingURL=version.js.map
