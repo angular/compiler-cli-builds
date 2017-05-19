a50a8c6232e250754a7371dd71e685dd676e8e4c
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 21:04:02 +0000

    f1a9e3c feat(animations): introduce routeable animation support

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-b10029c');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-f1a9e3c');
 //# sourceMappingURL=version.js.map
