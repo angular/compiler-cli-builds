a841680d21801a04c422110fe11aa7e48709e616
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:06:12 +0000

    8a0e565 docs(aio): tidy up docs that use float clearing

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-a42322d');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-8a0e565');
 //# sourceMappingURL=version.js.map
