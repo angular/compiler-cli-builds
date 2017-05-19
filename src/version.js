c66531eca8fa2dc8c88e4d8e72cc48d1dcbf2641
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 18:03:57 +0000

    6cb93c1 fix(animations): only require one flushMicrotasks call when testing animations

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-eed67dd');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-6cb93c1');
 //# sourceMappingURL=version.js.map
