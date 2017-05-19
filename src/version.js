b2cad817c3f88c7cc52b871bae317ca5f03b5f18
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 13:29:56 +0000

    eed67dd feat(aio): update aio-shell class when sidenav opens or closes

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-be9e8b9');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-eed67dd');
 //# sourceMappingURL=version.js.map
