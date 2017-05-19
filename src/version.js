9f1ef8ec8dad2696ae8a98a28ea5b44d5bef985e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:01:06 +0000

    a42322d feat(aio): code copy button has tooltip and aria-label

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-beta.1-b836aca');
+exports.VERSION = new core_1.Version('4.2.0-beta.1-a42322d');
 //# sourceMappingURL=version.js.map
