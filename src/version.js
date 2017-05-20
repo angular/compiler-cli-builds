9291e9a4f65b28f57981cf005d147f13285f4951
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-20 11:24:38 +0000

    c8b08f3 feat(aio): implement ScrollSpy service (to highlight the active section in ToC)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-3d382dc');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-c8b08f3');
 //# sourceMappingURL=version.js.map
