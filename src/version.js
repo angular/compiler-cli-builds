07c8f4b2d021afcfc1a8bb791a9bf3f68bb33d43
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-21 14:55:02 +0000

    670771d build(aio): make the postinstall script more Windows-friendly

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-c8b08f3');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-670771d');
 //# sourceMappingURL=version.js.map
