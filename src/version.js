de2f5666790a979ed0a00bae28101a02ab6c4211
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:40:20 +0000

    08dfe91 feat(compiler): add location note to extracted xliff2 files (#16791)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-85d4c4b');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-08dfe91');
 //# sourceMappingURL=version.js.map
