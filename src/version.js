ddf3dc54be6b1aa4ab877ad25719e32636dde99f
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 17:25:59 +0000

    98849de build: fix public-api:update gulp task (#16859)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-afd703d');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-98849de');
 //# sourceMappingURL=version.js.map
