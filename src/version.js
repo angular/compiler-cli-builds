173a2be2f8c892119f5cdc35a77e85c42376fec2
GitHub <noreply@github.com>
2017-05-22 18:36:47 +0000

    6e41add feat(tsc-wrapped): support template literals in metadata collection (#16880)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-11c10b2');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-6e41add');
 //# sourceMappingURL=version.js.map
