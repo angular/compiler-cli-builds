ce7535c66bce6bf7f2d36be8d8f496c05917a66f
GitHub <noreply@github.com>
2017-05-22 17:35:48 +0000

    11c10b2 fix(tsc-wrapped): resolve short-hand literal values to locals (#16873)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-98849de');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-11c10b2');
 //# sourceMappingURL=version.js.map
