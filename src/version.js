48cad6b47fb7f878b074fbf70b49e4b4d07a5b8b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-22 13:08:37 +0000

    ce18fdb fix(aio): use the current version in the version picker

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-670771d');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-ce18fdb');
 //# sourceMappingURL=version.js.map
