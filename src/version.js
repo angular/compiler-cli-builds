c144a545d052897f0aeb9e7d5cc2a8bc849dfb8d
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:39:04 +0000

    85d4c4b feat(core): update zone.js to 0.8.10 and expose the flush method (#16860)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-6e41add');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-85d4c4b');
 //# sourceMappingURL=version.js.map
