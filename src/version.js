97f4fd865689fa54ef80d0e06a35a1f49df41f32
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 18:09:13 +0000

    a80ac0a fix(core): make decorators closure safe (#16905)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-5af143e');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-a80ac0a');
 //# sourceMappingURL=version.js.map
