2d12cbc95cde74b7f5380653a0df329a956cf6e9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-23 07:17:00 +0000

    3b28c75 test(aio): avoid warnings from Material during tests

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-f5b2ce0');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-3b28c75');
 //# sourceMappingURL=version.js.map
