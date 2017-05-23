06e8632679e095381f5c94a1be12d9b1745c2bf2
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 18:10:26 +0000

    1651a8f fix(tsc-wrapped): ignore `|null` and `|undefined` when collecting types (#16222)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-a80ac0a');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-1651a8f');
 //# sourceMappingURL=version.js.map
