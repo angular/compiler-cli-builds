cab71f0f1ce9a3ded510418548ea6a775a67a18b
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:14:24 +0000

    e7d9fd8 fix(animations): repair flicker issues with WA polyfill (#16937)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-08dfe91');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-e7d9fd8');
 //# sourceMappingURL=version.js.map
