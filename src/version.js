fdb145662d990c8923da3d8136577996d9d1f50b
Chuck Jazdzewski <chuckj@google.com>
2017-05-24 18:41:21 +0000

    b0c5018 build(aio): better error message for invalid links (#16993)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-19a509a');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-b0c5018');
 //# sourceMappingURL=version.js.map
