d1e212d0e91112280a00b649a289d3a1e3dcd7ec
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 19:46:18 +0000

    cd5bc64 docs(aio): update headers for toc (#16969)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-5f723cb');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-cd5bc64');
 //# sourceMappingURL=version.js.map
