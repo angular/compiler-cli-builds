2dfce3e10fa2ea5952205935887b206aa79d4a37
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 16:31:40 +0000

    7ffb75f refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-c0981b8');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-7ffb75f');
 //# sourceMappingURL=version.js.map
