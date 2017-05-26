cdd291ebbb5d1fe28432b8ae2d9fd6cfaf1f3c5a
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 22:53:58 +0000

    51d7a65 docs: add changelog for 4.2.0-rc.1

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-d108479');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-51d7a65');
 //# sourceMappingURL=version.js.map
