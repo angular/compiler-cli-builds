eabc80a412e7dc68902717bf0e039d1634338829
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:02:14 +0000

    2538094 fix(aio): group topbar items into a single sidenav node when narrow screen (#17001)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-4c5e28e');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-2538094');
 //# sourceMappingURL=version.js.map
