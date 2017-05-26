88974fad155f8304ac75e919cd36e0981f0aed9b
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:00:51 +0000

    4c5e28e build(aio): less verbose boilerplate generator (#16970)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-b37a048');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-4c5e28e');
 //# sourceMappingURL=version.js.map
