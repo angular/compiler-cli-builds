b8accb6535948944b2556af271f3c097ffaa05c8
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:00:48 +0000

    b37a048 docs(aio): restore missing files (#16995)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-e10d763');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-b37a048');
 //# sourceMappingURL=version.js.map
