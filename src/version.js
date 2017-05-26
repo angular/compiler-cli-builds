1405b2d6e4df2c08cd8e28e3bd152245975e2955
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 01:11:12 +0000

    3ab86bd fix(animations): use a lightweight renderer for non-animation components (#17003)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.0-2538094');
+exports.VERSION = new core_1.Version('4.2.0-rc.0-3ab86bd');
 //# sourceMappingURL=version.js.map
