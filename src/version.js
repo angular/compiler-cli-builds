3b66634480f6e0593f50e670c74562991e047ec0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-27 10:36:37 +0000

    b8b91d3 feat(aio): marketing design refresh

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-0018acf');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-b8b91d3');
 //# sourceMappingURL=version.js.map
