1596aa5d5b1b08fd47d4b14f9516381a016761fc
Victor Berchet <victor@suumit.com>
2017-05-30 18:03:23 +0000

    18bf772 feat(compiler): emit typescript nodes from an output ast (#16823)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-160221c');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-18bf772');
 //# sourceMappingURL=version.js.map
