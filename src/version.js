9166c2f03b6f0f352768f6accf57e7d000288ca8
Victor Berchet <victor@suumit.com>
2017-05-30 22:34:05 +0000

    2f35392 fix(aio): make the search-pane larger (and improve the search-box) (#17105)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-b056adc');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-2f35392');
 //# sourceMappingURL=version.js.map
