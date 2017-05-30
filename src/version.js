4a1d904b6a8043e6f83402f07a2fc57c3589897d
Victor Berchet <victor@suumit.com>
2017-05-30 20:38:04 +0000

    e513808 build: update router package.json placeholder (#17060)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-8ffa483');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-e513808');
 //# sourceMappingURL=version.js.map
