e282f918e48af4a1f47b0fe22d045fa7ca37f39e
Victor Berchet <victor@suumit.com>
2017-05-30 18:08:25 +0000

    ea8a43d feat(tsc-wrapped): always convert shorthand imports (#16898)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-535d9da');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-ea8a43d');
 //# sourceMappingURL=version.js.map
