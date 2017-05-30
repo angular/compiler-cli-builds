78d9470b5420444ad8128baa23063e0368179fda
Victor Berchet <victor@suumit.com>
2017-05-30 20:37:31 +0000

    8ffa483 fix(compiler): enableLegacyTemplate should not be ignored (#17051)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-ea8a43d');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-8ffa483');
 //# sourceMappingURL=version.js.map
