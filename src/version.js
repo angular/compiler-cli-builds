c26c3e10d4adc5b6fc2980ca1d74c13ab6402ac5
Igor Minar <iminar@google.com>
2017-05-31 10:41:24 +0000

    11505fa fix(aio): disable search on browsers that don't support web workers

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-cb2cb7c');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-11505fa');
 //# sourceMappingURL=version.js.map
