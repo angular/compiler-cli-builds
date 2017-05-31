6372ed791afb5440267aae36180a40625a9be5ab
Victor Berchet <victor@suumit.com>
2017-05-31 22:53:33 +0000

    665e707 fix(animations): always change to desired animation state even if no transition fires (#17025)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-c20f60b');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-665e707');
 //# sourceMappingURL=version.js.map
