5539e56ff4bcd95d539175ba8b95ef5e5b50bfa9
Victor Berchet <victor@suumit.com>
2017-05-31 21:59:37 +0000

    c20f60b fix(router): make remove trailing slash consistent with URL params

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-1408357');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-c20f60b');
 //# sourceMappingURL=version.js.map
