10ab9ea0554a165ad376649f049f348aee81dfed
Victor Berchet <victor@suumit.com>
2017-06-01 18:58:00 +0000

    ad6a57e perf(animations): do not place enterId values on elements for querying purposes (#17150)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-230255f');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-ad6a57e');
 //# sourceMappingURL=version.js.map
