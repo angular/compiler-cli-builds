307101ba33e8ebf4e64c1382f3183227fddb4e3b
GitHub <noreply@github.com>
2017-06-01 20:43:42 +0000

    598fdad docs: update the release schedule (#17162)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-ad6a57e');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-598fdad');
 //# sourceMappingURL=version.js.map
