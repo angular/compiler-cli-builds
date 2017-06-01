b3bf4a13226c0739fe1a44f5ff54283a9a876015
Victor Berchet <victor@suumit.com>
2017-06-01 21:20:52 +0000

    068133e fix(animations): do not retain deleted nodes during an non-removal animation (#17153)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.1-598fdad');
+exports.VERSION = new core_1.Version('4.2.0-rc.1-068133e');
 //# sourceMappingURL=version.js.map
