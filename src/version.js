45caaa7823707491d082d96409f09b1920ff4ba4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 17:35:41 +0000

    d5ce086 build(aio): fix up API doc-gen templates

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-82ec02d');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-d5ce086');
 //# sourceMappingURL=version.js.map
