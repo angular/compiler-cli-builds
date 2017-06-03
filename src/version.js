ffa501f6520ae39c6cae3c2f1ef78e44c8dab727
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-03 00:22:40 +0000

    819514a fix(animations): ensure web-animations understands a numeric CSS perspective value

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-b55adee');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-819514a');
 //# sourceMappingURL=version.js.map
