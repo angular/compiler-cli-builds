c6c97da46bd81935c757f7f0a310f762db901b80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:35:27 +0000

    02d74ca ci: update pullapprove to reflect animation ownership

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-734f30d');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-02d74ca');
 //# sourceMappingURL=version.js.map
