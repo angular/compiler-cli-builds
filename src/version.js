7fe8d66f2addf0bb853e45df3af60021a2795539
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 14:13:39 +0000

    95f1ea2 fix(aio): topnav toolbar position styles

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-784347f');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-95f1ea2');
 //# sourceMappingURL=version.js.map
