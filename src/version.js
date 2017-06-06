46530da56fface70d93eb618a76b8c3493c7485a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:28:49 +0000

    b8979c8 fix(aio): fix and clean up topbar styling

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-d4e1960');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-b8979c8');
 //# sourceMappingURL=version.js.map
