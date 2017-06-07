a04e401b55146680844d82093e2fbcc07c52a548
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 08:29:49 +0000

    078a4b0 fix(aio): fix button alignment and jump

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-b00b80a');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-078a4b0');
 //# sourceMappingURL=version.js.map
