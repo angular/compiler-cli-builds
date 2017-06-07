36b597294fb8247d1fe7a048b8a1f3f5187a15bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:57:29 +0000

    a39f7d6 docs(aio): use h1 rather than divs and hide heading anchors

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-3ddd28d');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-a39f7d6');
 //# sourceMappingURL=version.js.map
