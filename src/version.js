d3a2551d6b65e5f610726e3392e05c5f5c267473
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 19:59:16 +0000

    a4a2901 fix(aio): remove `...` separator from search results

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-cba2b3c');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-a4a2901');
 //# sourceMappingURL=version.js.map
