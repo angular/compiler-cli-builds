20b1dd00bd49a5268617d8b05cbc3e340c05b2e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:51:32 +0000

    3ddd28d fix(aio): update contrast on mediumgray color

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-fe6b39d');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-3ddd28d');
 //# sourceMappingURL=version.js.map
