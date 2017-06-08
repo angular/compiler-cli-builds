996ba3bed3ed865cd2f0a3a50d05e99e0f60a8ad
Igor Minar <iminar@google.com>
2017-06-08 19:47:43 +0000

    391ed63 fix(aio): copy code button overlap fix on mobile (#17302)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-75a311f');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-391ed63');
 //# sourceMappingURL=version.js.map
