12e2d27749aa37bb9b709ae14b286ab92e8416ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 22:55:22 +0000

    cf0a9e0 docs: add changelog for 4.2.0

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-576d6d8');
+exports.VERSION = new core_1.Version('4.2.0-cf0a9e0');
 //# sourceMappingURL=version.js.map
