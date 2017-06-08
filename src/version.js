51e62f3b6de9090e3c024095e53b7bee8d526ea7
Igor Minar <iminar@google.com>
2017-06-08 19:25:49 +0000

    5364b51 fix(aio): scroll to top immediately when doc changes (#17310)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-680128b');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-5364b51');
 //# sourceMappingURL=version.js.map
