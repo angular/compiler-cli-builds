aeb1b556fef7a2ecccdd698991625386665e1fbb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 20:13:47 +0000

    7467354 docs(aio): document the non-null assertion operator

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-391ed63');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-7467354');
 //# sourceMappingURL=version.js.map
