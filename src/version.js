b33dc71f6dd2101b8d13c8ab06e9bd905b384153
Igor Minar <iminar@google.com>
2017-06-08 19:46:38 +0000

    75a311f fix(aio): style and fix hover color for features cta button (#17342)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-4d2ee51');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-75a311f');
 //# sourceMappingURL=version.js.map
