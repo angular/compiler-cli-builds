e15f7000564392e5b5182d2790b58a5877e49882
Igor Minar <iminar@google.com>
2017-06-08 19:20:26 +0000

    680128b fix(aio): alert, subsection and callout width fix (#17303)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-67ef0f0');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-680128b');
 //# sourceMappingURL=version.js.map
