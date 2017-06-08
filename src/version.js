9ce202105be9207b59599458a9e53a32e8a8f1e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 17:43:12 +0000

    e9886d7 fix(animations): evaluate substitutions on option param values

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-rc.2-022835b');
+exports.VERSION = new core_1.Version('4.2.0-rc.2-e9886d7');
 //# sourceMappingURL=version.js.map
