60712a6a6b50316e5e636465d32b18e398bd925a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 16:15:11 +0000

    2d56239 fix(aio): sidebar folder state after select item

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-a0b30e5');
+exports.VERSION = new core_1.Version('4.2.0-2d56239');
 //# sourceMappingURL=version.js.map
