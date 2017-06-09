a3bc40d1b9f521eae11c8bf7cb968cc5c5c1df69
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:05:51 +0000

    e894f5c fix(aio): expand the main content width when there is no ToC

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-8524187');
+exports.VERSION = new core_1.Version('4.2.0-e894f5c');
 //# sourceMappingURL=version.js.map
