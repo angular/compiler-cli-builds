18c0bca682782c3c82f57108afe8c5f45de1793b
Igor Minar <iminar@google.com>
2017-06-09 20:56:02 +0000

    3a99af2 docs(packaging): update homepage in @angular/tsc-wrapped (#17352)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-503fd1f');
+exports.VERSION = new core_1.Version('4.2.0-3a99af2');
 //# sourceMappingURL=version.js.map
