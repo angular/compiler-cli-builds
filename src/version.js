5752f7bdf10ea1eb55b6c79c527613d806ac3a1c
Igor Minar <iminar@google.com>
2017-06-09 22:09:40 +0000

    d56b7ed docs(aio): reorganise the sidenav menu (#16934)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-c2d31fb');
+exports.VERSION = new core_1.Version('4.2.0-d56b7ed');
 //# sourceMappingURL=version.js.map
