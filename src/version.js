7ebb248692b40e5003998b2b98d9ed08dbb5bfd2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 07:38:45 +0000

    f7422a9 fix(aio): animate hamburger in/out

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.0-cf0a9e0');
+exports.VERSION = new core_1.Version('4.2.0-f7422a9');
 //# sourceMappingURL=version.js.map
