07c78bc9af9bc31984fae9d0ebcdf03014736b18
Igor Minar <iminar@google.com>
2017-06-10 01:48:13 +0000

    afbc2b0 build(aio): upgrade purify to deal with messed up comment in animations code

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.1-15090a8');
+exports.VERSION = new core_1.Version('4.2.1-afbc2b0');
 //# sourceMappingURL=version.js.map
