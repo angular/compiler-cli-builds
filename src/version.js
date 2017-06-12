cfecd2a32dfff708c9308f0233d828ae11f37872
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-12 13:31:59 +0000

    3d5f520 test(aio): fix Chrome getting disconnected during unit tests

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.1-d961911');
+exports.VERSION = new core_1.Version('4.2.1-3d5f520');
 //# sourceMappingURL=version.js.map
