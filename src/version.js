a979d62d25ca813bb233826b484247bde190e8bf
Igor Minar <igor@angularjs.org>
2017-06-12 23:49:49 +0000

    5e79449 build(aio): make deploy-to-firebase.sh executable

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.1-0268818');
+exports.VERSION = new core_1.Version('4.2.2-5e79449');
 //# sourceMappingURL=version.js.map
