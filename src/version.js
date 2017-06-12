66069790e0ba494e187fe805490952e57384b4d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:18:26 +0000

    234268e ci(aio): deploy commits on the stable branch to production

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-7a2d2ef');
+exports.VERSION = new core_1.Version('4.2.1-234268e');
 //# sourceMappingURL=version.js.map
