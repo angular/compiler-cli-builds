ca2d97c09a757f5285007509662b04a42a39ec37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:57:14 +0000

    11dfb68 ci: update github templates (#17466)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-4164369');
+exports.VERSION = new core_1.Version('4.2.2-11dfb68');
 //# sourceMappingURL=version.js.map
