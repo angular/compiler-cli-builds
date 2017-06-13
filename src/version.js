9abea1df252fe7f333e748e8a4a5b156b8ad1b86
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:15:12 +0000

    052331f build(aio): add staging environment

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-541c9a9');
+exports.VERSION = new core_1.Version('4.2.2-052331f');
 //# sourceMappingURL=version.js.map
