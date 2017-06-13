8a54ce25da4fc67748f9f530e0e6369a47de9fb4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 07:12:27 +0000

    d647db2 fix(aio): ensure that API filter page can display 3 columns in wide view

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-0658e1d');
+exports.VERSION = new core_1.Version('4.2.2-d647db2');
 //# sourceMappingURL=version.js.map
