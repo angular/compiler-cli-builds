bf20e7f15ee8e70ac75c6e7227d12252aa8398ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:33:54 +0000

    b4cd20c fix(aio): make the footer links clickable on all browsers

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-f840afb');
+exports.VERSION = new core_1.Version('4.2.2-b4cd20c');
 //# sourceMappingURL=version.js.map
