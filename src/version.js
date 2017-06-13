a2052809b19a38fa9bdaf8c42d5d19ca1419db2b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:30:56 +0000

    f840afb ci: disable platform-server integration test as it is currently broken

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-93d1b4e');
+exports.VERSION = new core_1.Version('4.2.2-f840afb');
 //# sourceMappingURL=version.js.map
