894dafca26aad010390a1eee7eeee40ca03cc702
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 23:13:28 +0000

    0c07f8c test(platform-server): fix and re-enable integration tests

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-d8d21c7');
+exports.VERSION = new core_1.Version('4.2.2-0c07f8c');
 //# sourceMappingURL=version.js.map
