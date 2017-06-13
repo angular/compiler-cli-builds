d7f588ce9214f69c0e6045f08751bd46216c235e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 12:10:07 +0000

    541c9a9 docs(aio): i18n guide - updates for v4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-12452d4');
+exports.VERSION = new core_1.Version('4.2.2-541c9a9');
 //# sourceMappingURL=version.js.map
