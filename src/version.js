3a3dfccd4f06727419890bc5056712f0601fca41
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 15:03:46 +0000

    2bdf2fe test(aio): add async beforeEach to prevent Chrome disconnects

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-607fb1f');
+exports.VERSION = new core_1.Version('4.2.2-2bdf2fe');
 //# sourceMappingURL=version.js.map
