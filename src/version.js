5e2de6841808dc1857b95a5be9af67800f46e619
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:20:50 +0000

    53f57d7 feat(aio): add iphone pwa features

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-2b1de07');
+exports.VERSION = new core_1.Version('4.2.2-53f57d7');
 //# sourceMappingURL=version.js.map
