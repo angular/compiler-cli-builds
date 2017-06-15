a4f043daf6c84be4d222fbfa9faecbf1927cebc1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:04:25 +0000

    06a0cf2 docs(aio): http guide shows how to import toPromise operator from rxjs

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-a8afa65');
+exports.VERSION = new core_1.Version('4.2.2-06a0cf2');
 //# sourceMappingURL=version.js.map
