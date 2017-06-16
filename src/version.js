e0d520d6cbb5c91098b6dc3aff79ac60bc501927
Hans Larsen <hans@hansl.ca>
2017-06-16 19:34:11 +0000

    71a8ef5 fix(aio): improve no-javascript screen

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.3-623b9c6');
+exports.VERSION = new core_1.Version('4.2.3-71a8ef5');
 //# sourceMappingURL=version.js.map
