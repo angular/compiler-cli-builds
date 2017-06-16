7fc3fda78d08e335ac40f8da91002ad1777ae453
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:21:15 +0000

    2cc931e fix(aio): add missing redirect rule for `/styleguide`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-ce00fa3');
+exports.VERSION = new core_1.Version('4.2.2-2cc931e');
 //# sourceMappingURL=version.js.map
