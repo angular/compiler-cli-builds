97849d3dc2ae987dce0c909eb5f1c461816dfdf8
Hans <hans@hansl.ca>
2017-06-16 21:06:35 +0000

    3093c55 build(aio): update @angular/service-worker to 1.0.0-beta.16

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-5ac3919');
+exports.VERSION = new core_1.Version('4.2.2-3093c55');
 //# sourceMappingURL=version.js.map
