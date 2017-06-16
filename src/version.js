15ab26195befeba3df3c7b977b4938d19d5f2d04
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:23:29 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-b192dd5');
+exports.VERSION = new core_1.Version('4.2.2-dbc6a4c');
 //# sourceMappingURL=version.js.map
