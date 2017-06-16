ad42132a17b941a0ca8d6dc328868d36fb55bbf3
Hans Larsen <hans@hansl.ca>
2017-06-16 17:16:21 +0000

    8a547ee docs: add changelog for 4.2.3

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-b8c39cd');
+exports.VERSION = new core_1.Version('4.2.3-8a547ee');
 //# sourceMappingURL=version.js.map
