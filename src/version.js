0e42f239aca85721b10553780eee680ba357c2ff
Hans Larsen <hans@hansl.ca>
2017-06-19 22:12:40 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.3-3607a48');
+exports.VERSION = new core_1.Version('4.2.3-09512c7');
 //# sourceMappingURL=version.js.map
