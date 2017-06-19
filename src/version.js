0e7480bce77a09feb425507dab219943aa62e799
Hans Larsen <hans@hansl.ca>
2017-06-19 18:46:34 +0000

    afbb6bb docs: remove aio from changelog

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-4f37f86');
+exports.VERSION = new core_1.Version('4.2.2-afbb6bb');
 //# sourceMappingURL=version.js.map
