25e7214f97f932845bf4ed98fa57106036c18cef
Hans Larsen <hans@hansl.ca>
2017-06-19 23:02:34 +0000

    2e8efda fix(aio): leave results panel open when opening search result on new page

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.3-09512c7');
+exports.VERSION = new core_1.Version('4.2.3-2e8efda');
 //# sourceMappingURL=version.js.map
