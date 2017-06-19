f71688e9e194ad346172e5467a6034f84746ef23
Hans <hans@hansl.ca>
2017-06-19 18:04:23 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-3093c55');
+exports.VERSION = new core_1.Version('4.2.2-4f37f86');
 //# sourceMappingURL=version.js.map
