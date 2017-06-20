2c9f55bdc14e13af1ba39fbd78d4fb7ed32c9540
Hans <hans@hansl.ca>
2017-06-20 20:25:25 +0000

    009651e refactor(core): remove toString() method from DefaultKeyValueDiffer

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-c59c390');
+exports.VERSION = new core_1.Version('4.2.2-009651e');
 //# sourceMappingURL=version.js.map
