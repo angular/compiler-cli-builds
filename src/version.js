2af42558a758dabd213c8a8121b8c52f6ac47837
Hans <hans@hansl.ca>
2017-06-20 19:20:21 +0000

    4e6be15 fix(language-service): rollup `tslib` into the language service package

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-3e685f9');
+exports.VERSION = new core_1.Version('4.2.2-4e6be15');
 //# sourceMappingURL=version.js.map
