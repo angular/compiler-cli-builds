053de4ca5a3adeda98d24910432596f9dbf9cd3b
Hans <hans@hansl.ca>
2017-06-21 21:24:06 +0000

    97fd248 fix(aio): fix patch script on windows

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-bb804dd');
+exports.VERSION = new core_1.Version('4.2.2-97fd248');
 //# sourceMappingURL=version.js.map
