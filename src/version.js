1278d8efd27bc3fc060e4a7406a1ff63c87e74e7
Hans <hans@hansl.ca>
2017-06-21 21:11:52 +0000

    1d93cf2 build(aio): ensure all doc tests are run

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-0034bb2');
+exports.VERSION = new core_1.Version('4.2.2-1d93cf2');
 //# sourceMappingURL=version.js.map
