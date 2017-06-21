9b82367f5e3dbec12113c6fa1fa0667c6a3358c4
Hans <hans@hansl.ca>
2017-06-21 21:22:42 +0000

    bb804dd feat(aio): select contributor group with URL “about?group=gde”

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-1d93cf2');
+exports.VERSION = new core_1.Version('4.2.2-bb804dd');
 //# sourceMappingURL=version.js.map
