eb48165f3995a9e6370fe813fa4a27d0cb9119e6
Hans <hans@hansl.ca>
2017-06-21 23:38:23 +0000

    4352dd2 fix(compiler): avoid emitting self importing factories

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-34cc3f2');
+exports.VERSION = new core_1.Version('4.2.2-4352dd2');
 //# sourceMappingURL=version.js.map
