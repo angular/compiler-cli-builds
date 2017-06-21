555d92024e1fe49ddf4f397c359de10f5dc01796
Hans Larsen <hans@hansl.ca>
2017-06-21 22:02:18 +0000

    3634832 feat(aio): display “Searching ..." while building search index

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.3-572885d');
+exports.VERSION = new core_1.Version('4.2.3-3634832');
 //# sourceMappingURL=version.js.map
