0fce015f44e4a6d2d7e1652cd87942c17f3a45a4
Hans <hans@hansl.ca>
2017-06-21 18:56:33 +0000

    0034bb2 fix(aio): restore component-styles/exclude hidden children

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.2-d168528');
+exports.VERSION = new core_1.Version('4.2.2-0034bb2');
 //# sourceMappingURL=version.js.map
