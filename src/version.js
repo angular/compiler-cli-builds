5fa190cc9464636eaf79f6a87a5e21ee48be53c0
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:06:25 +0000

    8c129d7 docs: Fix some non-code being formatted as code in the router docs

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-5c576d3');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-8c129d7');
 //# sourceMappingURL=version.js.map
