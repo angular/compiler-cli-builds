d3ff3681c0d759817e9b59f2bd5161ea005569d1
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 21:58:08 +0000

    68f939e docs: Fix Stack Overflow being repeatedly misspelt as a single word

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-1fc0d05');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-68f939e');
 //# sourceMappingURL=version.js.map
