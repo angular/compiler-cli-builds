8e8bf3a7174ecd60d2f5443b515028ba86fba2d7
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:10:35 +0000

    68b64a2 docs: add missing colon in the Constants section of NAMING.md

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-8c129d7');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-68b64a2');
 //# sourceMappingURL=version.js.map
