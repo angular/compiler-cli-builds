969a51ada8bdcef91c415094e252f5bc06734e1a
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:03:40 +0000

    5c576d3 docs: add documentation for LTS versions

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-68f939e');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-5c576d3');
 //# sourceMappingURL=version.js.map
