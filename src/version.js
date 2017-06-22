9a043260f5be09020ab50151154821763a76ff39
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:09 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-92bcfef');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-133b5e6');
 //# sourceMappingURL=version.js.map
