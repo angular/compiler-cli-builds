ef6df52ba141defd685029783d8458df64dc0d59
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:19:21 +0000

    849200b docs: remove unnecessary newline

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-87206e1');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-849200b');
 //# sourceMappingURL=version.js.map
