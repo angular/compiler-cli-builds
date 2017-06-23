902cf58002752d5de20ed5ca6ec72b7f1783f748
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:00:35 +0000

    e80851d fix(core): add needed closure compiler warning suppression

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-30f4fe2');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-e80851d');
 //# sourceMappingURL=version.js.map
