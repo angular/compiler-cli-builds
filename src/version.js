9a4bcc463d6290dd7dbbffcd60313cbae91b0daf
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 20:09:09 +0000

    ffbdf74 docs(aio): cleanup rollup-config script

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.4-9344c86');
+exports.VERSION = new core_1.Version('4.2.4-ffbdf74');
 //# sourceMappingURL=version.js.map
