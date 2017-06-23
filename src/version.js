cc05e2bd3bcbd9b492b1d91939dd794e03d0a4f3
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:40:56 +0000

    3ce9d51 fix(aio): prefix `location.assign` with `window.`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-6191d53');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-3ce9d51');
 //# sourceMappingURL=version.js.map
