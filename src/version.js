ba4b7f93f74508b9b0da6e6a29ec5ef81b704cee
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:11:10 +0000

    b754e60 feat(compiler): add support `::ng-deep`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-e80851d');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-b754e60');
 //# sourceMappingURL=version.js.map
