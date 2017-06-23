0ddeb6330bb40ba2147505af253ce2b1a817f13e
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:36:10 +0000

    8b4acbb docs(aio): minor fixes for the upgrade guide

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-f2ee1dc');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-8b4acbb');
 //# sourceMappingURL=version.js.map
