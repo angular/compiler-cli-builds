9a9733daf5d70d13a3dbe7224aa22c727a475f8a
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:46:55 +0000

    7ba720a docs(aio): resources.json - replace “Angular 2” in titles/descriptions

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-eacc36b');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-7ba720a');
 //# sourceMappingURL=version.js.map
