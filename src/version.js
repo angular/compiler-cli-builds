8d08ef59f4787dd57a626cbbc1ee281e8d025135
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:54:24 +0000

    d6265df ci(aio): Change the firebase token

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-08ecfd8');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-d6265df');
 //# sourceMappingURL=version.js.map
