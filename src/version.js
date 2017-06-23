9fe07c706f17acdbb53b72f55f68555200e989f5
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:42:41 +0000

    eacc36b docs(aio): update Bash for Windows info

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-8b4acbb');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-eacc36b');
 //# sourceMappingURL=version.js.map
