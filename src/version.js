89547bed2e0d75aa448b15a37f232287fd8ad4ec
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 20:42:10 +0000

    448d9f9 test(compiler): add a test for components not part of any `NgModule`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-97bb374');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-448d9f9');
 //# sourceMappingURL=version.js.map
