715e2bd3536240790e5c2f80549a3b4bb66ac639
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 18:29:56 +0000

    34f3832 fix(animations): do not delay style() values before a stagger() runs

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-f162657');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-34f3832');
 //# sourceMappingURL=version.js.map
