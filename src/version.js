7899d44002f9255189484de11c942d3e3e1270e5
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 00:28:37 +0000

    68fc65d ci: add npm postinstall back to the lint step so node_modules doesn't get out of date

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-693f79e');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-68fc65d');
 //# sourceMappingURL=version.js.map
