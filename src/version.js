307df8402b310d15b1fe84cf74e31c0a2334b74e
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 20:54:10 +0000

    7205d7a fix(aio): build `scripts-js` before creating a new docker image for the preview server

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.4-ffbdf74');
+exports.VERSION = new core_1.Version('4.2.4-7205d7a');
 //# sourceMappingURL=version.js.map
