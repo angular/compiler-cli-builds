174e2a4b638f4f9ef9f0f1da91877b06527d1837
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 18:30:20 +0000

    d699c35 fix(animations): do not remove container nodes when children are queried by a parent animation

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-34f3832');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-d699c35');
 //# sourceMappingURL=version.js.map
