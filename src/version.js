980125c94f0052a5ded5dbb809a4b4cb306c8f52
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 01:48:06 +0000

    af14b1e test(animations): add proper tests to check multiple combinations of :enter/:leave animation queries

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-6c1a8da');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-af14b1e');
 //# sourceMappingURL=version.js.map
