b2ee3f6f04adbe03e3eb370d5805511937f4341f
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:05:34 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-e47a77f');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-96b1703');
 //# sourceMappingURL=version.js.map
