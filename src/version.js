95a1a1b7ab17ce1d66acb29bc8f3a6433d9bfcb2
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:00:12 +0000

    8d01db4 docs: Updated router guide content and examples for paramMap

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.0-4268c82');
+exports.VERSION = new core_1.Version('4.3.0-beta.0-8d01db4');
 //# sourceMappingURL=version.js.map
