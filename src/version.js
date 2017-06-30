37a5de353cbaeb0034c81bf7f19fdce1e9524e6e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:23:44 +0000

    b052ef5 fix(aio): do not include hidden content in window title

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-4459e0c');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-b052ef5');
 //# sourceMappingURL=version.js.map
