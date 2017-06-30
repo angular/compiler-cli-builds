06cf58fbb05ab72f35e935acd3ec67866ce9690d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 12:32:04 +0000

    01d4eae ci(aio): use correct URL for preview in PWA score test

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-154154d');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-01d4eae');
 //# sourceMappingURL=version.js.map
