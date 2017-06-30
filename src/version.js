cfd7be6c559f14db1edabf499a59413252c42114
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:47:19 +0000

    154154d docs(aio): fix quickstart-cli bad HTML escape chars

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-b052ef5');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-154154d');
 //# sourceMappingURL=version.js.map
