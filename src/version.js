c7d775aaf0b32a8e5f6f8a4dd77f1a3df4601e14
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 00:36:36 +0000

    40921bb docs: add changelog for 4.2.5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-dfcca66');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-40921bb');
 //# sourceMappingURL=version.js.map
