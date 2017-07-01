a7236fb2c21cece713ce67131d813aa10635c920
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:09:21 +0000

    8e00161 docs(aio): Add resources contributor info

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-01d4eae');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-8e00161');
 //# sourceMappingURL=version.js.map
