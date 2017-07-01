e5f71c1c69e85737e1bb9518702e80b1f5488870
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:11:34 +0000

    d5dc53e ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-8e00161');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-d5dc53e');
 //# sourceMappingURL=version.js.map
