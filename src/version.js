e9f18b1bc6db50b5d02cc0ecb8fccf235e01b2ba
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:55:30 +0000

    dc88e0a docs(aio): update quickstart to latest cli

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-fa34ed8');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-dc88e0a');
 //# sourceMappingURL=version.js.map
