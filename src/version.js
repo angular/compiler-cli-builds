e83b35e71c9216c7a4d823f9f1a1dab4a77883b1
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:31:30 +0000

    b51697c ci: add Chuck to the owners of core (#17892)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-92d7ecf');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-b51697c');
 //# sourceMappingURL=version.js.map
