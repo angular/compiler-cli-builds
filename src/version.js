8907ec718fc2feb23b53002b57132600244b0c4f
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 21:58:21 +0000

    0d7e1a9 docs(changelog): Correct typos (#16966)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-9d15d85');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-0d7e1a9');
 //# sourceMappingURL=version.js.map
