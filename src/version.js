0b10b460e44a59441f945b59e3acdaa60a6edb00
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:02:01 +0000

    c6b75b0 docs: add animations package to CONTRIBUTING.md (#15413)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-0d7e1a9');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-c6b75b0');
 //# sourceMappingURL=version.js.map
