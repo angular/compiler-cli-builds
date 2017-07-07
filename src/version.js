d3592e81c481a48ccc61d655e2aa0cdfdcf98109
GitHub <noreply@github.com>
2017-07-07 00:26:25 +0000

    ae27af7 fix(router): encode URLs the same way AngularJS did (closer to spec) (#17890)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-c69fff1');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-ae27af7');
 //# sourceMappingURL=version.js.map
