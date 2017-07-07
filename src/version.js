dbf30008f66090bb039db9daec4073b8d7753cba
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 19:19:27 +0000

    72747e5 ci: disable sauce-connect logging for realz (#17995)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-504500d');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-72747e5');
 //# sourceMappingURL=version.js.map
