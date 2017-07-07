038a0ef7035b20ebf390d0ab70bcb540c0ba6bec
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:36:04 +0000

    e1174f3 fix(aio): home & marketing styles cleanup (#17926)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-cb16e9c');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-e1174f3');
 //# sourceMappingURL=version.js.map
