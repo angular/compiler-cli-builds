1b9d51f7138c2d585feef3e0258c3b5e1466943f
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:18:11 +0000

    cb16e9c fix(tsc-wrapped): emit exports metadata in flat modules (#17893)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-6bae73c');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-cb16e9c');
 //# sourceMappingURL=version.js.map
