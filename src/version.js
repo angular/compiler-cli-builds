c406dfc306445a304ca50e27f1fa461498a15e3e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:14:40 +0000

    c81ad9d feat(common): two missing features in HttpClient (#17996)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-37797e2');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-c81ad9d');
 //# sourceMappingURL=version.js.map
