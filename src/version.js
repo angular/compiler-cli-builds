8a8f2ce2a3b4dfa757ec026236d9755da7c75507
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:38:08 +0000

    504500d fix(aio): activate ServiceWorker updates asap (#17699)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-e1174f3');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-504500d');
 //# sourceMappingURL=version.js.map
