a4c21470b1b837a6e4e4ace736abb64810acf7ff
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:51:57 +0000

    abbac4b feat(aio): use new ngo (#17977)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-671a175');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-abbac4b');
 //# sourceMappingURL=version.js.map
