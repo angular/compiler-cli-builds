0898526137e54efa582338af4fa9cb460d57d0e2
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:16:44 +0000

    8e28382 feat(animations): support disabling animations for sub elements

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-c81ad9d');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-8e28382');
 //# sourceMappingURL=version.js.map
