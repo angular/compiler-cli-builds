fdd060d51561643b6c24bcc3310371c809e4e9d7
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-08 00:26:25 +0000

    c723d42 refactor: fix typos (#18000)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-beta.1-9dd550f');
+exports.VERSION = new core_1.Version('4.3.0-beta.1-c723d42');
 //# sourceMappingURL=version.js.map
