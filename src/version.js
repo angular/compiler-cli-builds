3f5146a8187edc76a2c3f9f418f1e63d1e0608ec
Jason Aden <jasonaden@google.com>
2017-07-08 04:30:49 +0000

    8b8b4cb refactor(upgrade): move shareable functionality to `UpgradeHelper` class (#17971)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.5-3e61bf7');
+exports.VERSION = new core_1.Version('4.2.5-8b8b4cb');
 //# sourceMappingURL=version.js.map
