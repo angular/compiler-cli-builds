5b9bc6a2d75b8f465ee1be4bf810002cb3283de0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:02:02 +0000

    2055634 feat(aio): add API endpoint for notifying about PR updates

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-b3d9036');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-2055634');
 //# sourceMappingURL=version.js.map
