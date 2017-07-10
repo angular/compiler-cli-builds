4a0f49a3b60489ef190d3fc01f5dcbb6af7a63ae
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:16 +0000

    b3d9036 fix(aio): add new common/http package into API list

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-cb76091');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-b3d9036');
 //# sourceMappingURL=version.js.map
