f7c56f4068921f2dc8c91c0aa17f1596a228470b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:24:36 +0000

    e8bbf86 build(aio): add terms from heading to the search index

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-b30c5fc');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-e8bbf86');
 //# sourceMappingURL=version.js.map
