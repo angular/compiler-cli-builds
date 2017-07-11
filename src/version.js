7bb24875181e427ce544447fa607a03d3ac0b6a9
Igor Minar <iminar@google.com>
2017-07-11 19:21:44 +0000

    b9525ec docs: Unbreak README Formatting (#18043)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-d9b03be');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-b9525ec');
 //# sourceMappingURL=version.js.map
