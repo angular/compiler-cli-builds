d19f011f7e1ed1fc901816c4a4e5172ccfa54a70
Igor Minar <igor@angularjs.org>
2017-07-11 22:48:25 +0000

    afd5d72 docs(aio): Added resource link to amexio-ng-extensions [duplicate of #17785] (#17790)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.2.6-dbfd717');
+exports.VERSION = new core_1.Version('4.2.6-afd5d72');
 //# sourceMappingURL=version.js.map
