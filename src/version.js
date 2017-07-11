88188eb677a04520191885efb93f97dacf398293
Igor Minar <iminar@google.com>
2017-07-11 22:34:53 +0000

    e110a80 docs(aio): change Angular Module to NgModule (#16964)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-26b9492');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-e110a80');
 //# sourceMappingURL=version.js.map
