0b40816b6e2245815d62e017cc0760a93069fb04
Igor Minar <iminar@google.com>
2017-07-11 19:14:20 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-4d45fe6');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-b6aad07');
 //# sourceMappingURL=version.js.map
