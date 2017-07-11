99c7dcf86df27454290c15ea607117f0e7b90ab7
Igor Minar <iminar@google.com>
2017-07-11 19:34:21 +0000

    df06e8b fix(router): fix outdated homepage url in NPM package (#17899)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-db3bcc9');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-df06e8b');
 //# sourceMappingURL=version.js.map
