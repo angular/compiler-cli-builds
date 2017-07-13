48a01b124cde397fb5599e90847f4c85ab25188b
Igor Minar <iminar@google.com>
2017-07-13 19:39:03 +0000

    f0beb4d docs(aio): add Dart as a language option (#18080)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-ffda3e4');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-f0beb4d');
 //# sourceMappingURL=version.js.map
