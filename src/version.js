538b10e84b733f279c50f6e7dd3b1f946662f328
Igor Minar <iminar@google.com>
2017-07-13 22:48:36 +0000

    bc1ea8c docs: rewrite 'Server Communication' doc guide to use HttpClient (#18079)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-1bf7ba8');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-bc1ea8c');
 //# sourceMappingURL=version.js.map
