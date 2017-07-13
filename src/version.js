d1d79c1de25ef002aa0aee939ffe06b0a61cba4c
Igor Minar <iminar@google.com>
2017-07-13 19:33:36 +0000

    a7ea008 ci: fast-forward to master/HEAD on all circle jobs (#18101)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-edb8375');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-a7ea008');
 //# sourceMappingURL=version.js.map
