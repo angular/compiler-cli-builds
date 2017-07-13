3d987a94ea01c0d932e8d13c47e6340c4d4f5fed
Igor Minar <iminar@google.com>
2017-07-13 19:45:54 +0000

    4f1e4ff fix(common): don't guess Content-Type for FormData bodies (#18104)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-f0beb4d');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-4f1e4ff');
 //# sourceMappingURL=version.js.map
