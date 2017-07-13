8052d66e3ddaa1c6bc2fd5ba4701ba65d7e2a48f
Igor Minar <iminar@google.com>
2017-07-13 22:41:58 +0000

    1bf7ba8 ci(aio): exclude changes in aio/content folder

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-db96c96');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-1bf7ba8');
 //# sourceMappingURL=version.js.map
