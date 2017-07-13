7230ea88c012f9ea322a8cdfa351bbe43bf7c9a8
Igor Minar <iminar@google.com>
2017-07-13 19:34:57 +0000

    ffda3e4 build(aio): simplify the primary overload's anchor

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-a7ea008');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-ffda3e4');
 //# sourceMappingURL=version.js.map
