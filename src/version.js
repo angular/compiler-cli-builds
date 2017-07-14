3da36a564492ba57809eb965c20768b77c98593d
Igor Minar <iminar@google.com>
2017-07-14 00:39:04 +0000

    dd04f09 feat(common): on-by-default XSRF support in HttpClient (#18108)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-3d85f72');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-dd04f09');
 //# sourceMappingURL=version.js.map
