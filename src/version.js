817483b80d8803d888354766fb2946e8a37ea6f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 17:59:36 +0000

    01a2688 fix(animations): make sure @.disabled works in non-animation components

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-8e56c3c');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-01a2688');
 //# sourceMappingURL=version.js.map
