d951ca22340905f2a46f2e8f863dc0fd016d9067
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:02:17 +0000

    3ecc5e5 fix(common): rename HttpXsrfModule to HttpClientXsrfModule

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-rc.0-f7686d4');
+exports.VERSION = new core_1.Version('4.3.0-rc.0-3ecc5e5');
 //# sourceMappingURL=version.js.map
