3fa9ec50fdcd783b38bc1920260d740a36b2579c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 22:27:09 +0000

    55742e4 Revert "revert: revert: ci(aio): exclude changes in aio/content folder"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-a0b06be');
+exports.VERSION = new core_1.Version('4.3.0-55742e4');
 //# sourceMappingURL=version.js.map
