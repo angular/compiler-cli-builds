69a83c7a83af06c6fa6bc4d4e58307e1cc592302
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:52:56 +0000

    34834a9 fix(aio): remove unused `news.html` file

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-6e2ddcc');
+exports.VERSION = new core_1.Version('4.3.0-34834a9');
 //# sourceMappingURL=version.js.map
