20fff0c21551ad9a2f76d5f32560fea7ca7b1a6c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:44:47 +0000

    9f99f4f docs: fix HttpClient sample

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-c6ad212');
+exports.VERSION = new core_1.Version('4.3.0-9f99f4f');
 //# sourceMappingURL=version.js.map
