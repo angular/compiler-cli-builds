910cd66be2a32eced74910c3f73cc52aee11e531
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 22:10:01 +0000

    3f331b5 docs(aio): update jelbourn photo

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-375d598');
+exports.VERSION = new core_1.Version('4.3.0-3f331b5');
 //# sourceMappingURL=version.js.map
