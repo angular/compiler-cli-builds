85cafa78bd77ba8a1f30dc39624def12f2128149
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 18:12:37 +0000

    0bb8423 ci: add GK and PBD to aio content and marketing groups

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-95698d9');
+exports.VERSION = new core_1.Version('4.3.0-0bb8423');
 //# sourceMappingURL=version.js.map
