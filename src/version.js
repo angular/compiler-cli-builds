00df9307b1e4b193f5b74a1ac27be27f6546e9c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:33:59 +0000

    8c81c62 docs: fix HttpClient logging's sample

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-47b3ecd');
+exports.VERSION = new core_1.Version('4.3.0-8c81c62');
 //# sourceMappingURL=version.js.map
