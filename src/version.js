ec0ae32989a5982a730c772c5e53cd991f5a89bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:36:37 +0000

    ad3029e fix(router): should run resolvers for the same route concurrently

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-2a2fe11');
+exports.VERSION = new core_1.Version('4.3.0-ad3029e');
 //# sourceMappingURL=version.js.map
