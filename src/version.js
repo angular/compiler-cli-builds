93427fb0fb284d8c6e5a4aff8c9056be1f63ce5b
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:08:51 +0000

    c39e7d1 fix(aio): do not wrap `<code-tabs>` tab labels

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-799bffb');
+exports.VERSION = new core_1.Version('4.3.0-c39e7d1');
 //# sourceMappingURL=version.js.map
