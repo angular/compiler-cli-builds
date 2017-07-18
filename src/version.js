65222a22ce0d20cc4fc06861bda1e6ec8afa9743
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:12:42 +0000

    c453b7b build(aio): fail doc-gen if referenced images are missing

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-3f331b5');
+exports.VERSION = new core_1.Version('4.3.0-c453b7b');
 //# sourceMappingURL=version.js.map
