836f3b0bdb00220f22199e6fb0e8208c656512bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:17:53 +0000

    3b588fe docs: fix typo

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-e03adb9');
+exports.VERSION = new core_1.Version('4.3.0-3b588fe');
 //# sourceMappingURL=version.js.map
