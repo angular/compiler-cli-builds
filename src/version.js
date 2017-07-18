c6509fce61c42689bdeba530432a1e2db9e22afb
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 16:54:20 +0000

    799bffb docs(aio): fix cheatsheet layout for narrow screens

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-fda607c');
+exports.VERSION = new core_1.Version('4.3.0-799bffb');
 //# sourceMappingURL=version.js.map
