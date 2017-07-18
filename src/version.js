3abb72ea4df2969f1b48fba8f4c4077b2cc6e1bb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:11:16 +0000

    36faba1 fix(aio): add quote to module

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-92179bc');
+exports.VERSION = new core_1.Version('4.3.0-36faba1');
 //# sourceMappingURL=version.js.map
