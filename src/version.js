25401a1473f3d5c93f8187e27a7aeed30722a665
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:18:47 +0000

    a5205c6 fix(upgrade): allow accessing AngularJS injector from downgraded module

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-5c62e30');
+exports.VERSION = new core_1.Version('4.3.0-a5205c6');
 //# sourceMappingURL=version.js.map
