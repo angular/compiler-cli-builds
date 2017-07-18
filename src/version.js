23e6e9665c9a9fe9d52cc4d2338941b80d087f7e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:44:26 +0000

    f19bd5f docs(http): Make name of injected HttpTestingController consistent

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-17b7bc3');
+exports.VERSION = new core_1.Version('4.3.0-f19bd5f');
 //# sourceMappingURL=version.js.map
