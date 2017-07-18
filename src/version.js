518ecc90e17b51e756c1ce90db5f9bba455835cb
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:03:58 +0000

    fda607c build(aio): fail doc-gen if referenced images are missing

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-1390afe');
+exports.VERSION = new core_1.Version('4.3.0-fda607c');
 //# sourceMappingURL=version.js.map
