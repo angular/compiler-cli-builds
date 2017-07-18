47693f11a6ac90faaf1b32a4037748f547414bd3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:16:57 +0000

    7d0f2cd fix(aio): remove title attribute from `CodeExampleComponent`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-36faba1');
+exports.VERSION = new core_1.Version('4.3.0-7d0f2cd');
 //# sourceMappingURL=version.js.map
