813505c66749458c166b2d4111a4083afc2f60d8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:39:52 +0000

    d22f8f5 fix(animations): always camelcase style property names that contain auto styles

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.0-8076482');
+exports.VERSION = new core_1.Version('4.3.0-d22f8f5');
 //# sourceMappingURL=version.js.map
