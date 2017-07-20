783965fd96ed60f42b66ab97afee32584fd8cd66
Miško Hevery <misko@hevery.com>
2017-07-20 16:56:08 +0000

    31cb418 docs(aio): Fix http guide

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.0-8de44cf');
+exports.VERSION = new core_1.Version('5.0.0-beta.0-31cb418');
 //# sourceMappingURL=version.js.map
