9cbfd83cbad0843f8c12da375668e7374da52847
Miško Hevery <misko@hevery.com>
2017-07-21 17:48:04 +0000

    e0a9625 test: fix bad merge (#18267)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.0-a6c635e');
+exports.VERSION = new core_1.Version('5.0.0-beta.0-e0a9625');
 //# sourceMappingURL=version.js.map
