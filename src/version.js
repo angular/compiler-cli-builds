05f28d79937f3ac11ecf79e249a623797f88e655
Miško Hevery <misko@hevery.com>
2017-07-25 21:13:43 +0000

    6279e50 perf(core): use native addEventListener for faster rendering. (#18107)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.0-6fc5940');
+exports.VERSION = new core_1.Version('5.0.0-beta.0-6279e50');
 //# sourceMappingURL=version.js.map
