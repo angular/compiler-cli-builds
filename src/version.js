19c2d75d0b6526f10f1aa013cb92df56131dec38
Miško Hevery <misko@hevery.com>
2017-07-27 22:40:08 +0000

    5808153 docs: add changelog for 5.0.0.-beta.1

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.0-a7f2468');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-5808153');
 //# sourceMappingURL=version.js.map
