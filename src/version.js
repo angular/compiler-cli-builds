36c20b773c83b5d3b8d9f82fe84acc9cd820ba2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:38:38 +0000

    ebef5e6 feat(forms): add options arg to abstract controls

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-d71ae27');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-ebef5e6');
 //# sourceMappingURL=version.js.map
