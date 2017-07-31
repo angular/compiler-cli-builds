ec93e2c14528ae929ac2a7346a5a17ef58e59a6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 21:35:23 +0000

    cc2a4c4 build(aio): fix warning about missing `<h1>`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-a11542a');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-cc2a4c4');
 //# sourceMappingURL=version.js.map
