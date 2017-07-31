102803d32faabeee89225dfa4936733a3e3339ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:57:42 +0000

    a11542a docs(aio): fixed list format in FormArray section

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-b6c4af6');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-a11542a');
 //# sourceMappingURL=version.js.map
