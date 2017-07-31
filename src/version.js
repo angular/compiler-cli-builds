55bcd983ef1d247dc8d2b9e40bf84b32c3f58f2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:46:46 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-ebef5e6');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-381471d');
 //# sourceMappingURL=version.js.map
