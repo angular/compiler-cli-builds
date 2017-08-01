83317c09784c1bb35571f2570d1c634e476a3fc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 17:42:56 +0000

    82b067f build: ignore node_modules for tslint

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.1-9479a10');
+exports.VERSION = new core_1.Version('5.0.0-beta.1-82b067f');
 //# sourceMappingURL=version.js.map
