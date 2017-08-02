0811747757e01ab9b34e2d9ae97ea5787cecabc6
Victor Berchet <victor@suumit.com>
2017-08-02 23:57:01 +0000

    3923c30 docs(aio): fix missing anchor-open in i18n documentation (#18476)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-99017bf');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-3923c30');
 //# sourceMappingURL=version.js.map
