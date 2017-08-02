ebbd5fa61c2b85a142a8d1412986afbc5d880bff
Victor Berchet <victor@suumit.com>
2017-08-02 23:53:39 +0000

    99017bf fix(aio): correctly redirect `cookbook/a1-a2-quick-reference.html` (#18418)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-4d117fa');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-99017bf');
 //# sourceMappingURL=version.js.map
