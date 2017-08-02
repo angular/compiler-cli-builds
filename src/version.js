a0f24b88bc6f886ce9165a219b048fdb39775679
Victor Berchet <victor@suumit.com>
2017-08-02 22:58:12 +0000

    89c6161 docs: improve github labels by introducing "PR target" labels (#18436)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-1e1af7f');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-89c6161');
 //# sourceMappingURL=version.js.map
