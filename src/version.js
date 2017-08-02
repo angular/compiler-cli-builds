de53b32560f8ac6f8b167299b155d7e5e8e5b3ce
Victor Berchet <victor@suumit.com>
2017-08-02 23:11:07 +0000

    9adf40a build(aio): use cli 1.3.0-rc (#18290)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-89c6161');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-9adf40a');
 //# sourceMappingURL=version.js.map
