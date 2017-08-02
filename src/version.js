4ecdf9c8fc7bc614e58e5103fe36a2e54d803a75
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:00 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-dbc5c58');
+exports.VERSION = new core_1.Version('4.3.3-b65fe3e');
 //# sourceMappingURL=version.js.map
