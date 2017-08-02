51446ef5617212ab79a04914c240dc8448f3999a
GitHub <noreply@github.com>
2017-08-02 22:42:55 +0000

    baf4ce0 build: enable TSLint on the packages folder (#18459)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-24db1ed');
+exports.VERSION = new core_1.Version('4.3.3-baf4ce0');
 //# sourceMappingURL=version.js.map
