c87c8437ea08d00fa36770c4ce6cb682f115ad52
Victor Berchet <victor@suumit.com>
2017-08-02 23:42:21 +0000

    4d117fa test(common): skip some DatePipe tests in old Chrome where Intl is buggy (#15784)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-1d09838');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-4d117fa');
 //# sourceMappingURL=version.js.map
