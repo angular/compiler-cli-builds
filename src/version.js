706c51b6419a3a60e91785870a0f502ea055f78b
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:00 +0000

    5cc9913 docs(aio): replace old blog link in footer (#18448)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-9adf40a');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-5cc9913');
 //# sourceMappingURL=version.js.map
