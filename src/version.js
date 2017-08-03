0e823ce26ed0f5629009a031426b757dc5b222ea
Victor Berchet <victor@suumit.com>
2017-08-03 00:41:36 +0000

    ea07856 refactor(upgrade): compile upgrade cleanly with TypeScript 2.4 (#18461)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-7c47b62');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-ea07856');
 //# sourceMappingURL=version.js.map
