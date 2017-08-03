d1358376be12550453433afb23ebb216834574b5
Victor Berchet <victor@suumit.com>
2017-08-03 01:04:50 +0000

    81cb5bc docs(router): fix typo (#18479)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-1640d2a');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-81cb5bc');
 //# sourceMappingURL=version.js.map
