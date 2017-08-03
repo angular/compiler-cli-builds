836fe84a05f034288bc77c3dcda35ff695537e46
Victor Berchet <victor@suumit.com>
2017-08-03 00:08:48 +0000

    65d0888 fix(aio): fix layout of the webpack guide (#18493)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-afe3393');
+exports.VERSION = new core_1.Version('4.3.3-65d0888');
 //# sourceMappingURL=version.js.map
