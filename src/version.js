8020fd866d01572b40ccaf3d50fd53fefd65022d
Victor Berchet <victor@suumit.com>
2017-08-03 00:43:57 +0000

    1640d2a refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-5f501c7');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-1640d2a');
 //# sourceMappingURL=version.js.map
