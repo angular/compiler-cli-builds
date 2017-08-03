d0fbdef9be9dbd390ce30d447ce30bde63d981eb
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:04 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-4176832');
+exports.VERSION = new core_1.Version('4.3.3-e7e7622');
 //# sourceMappingURL=version.js.map
