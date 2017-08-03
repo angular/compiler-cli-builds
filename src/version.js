eb5222a7279037b3e6f7913d30682dacf9bd7eae
Victor Berchet <victor@suumit.com>
2017-08-03 01:36:59 +0000

    333a708 feat(forms): add updateOn blur option to FormControls (#18408)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-3a227a1');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-333a708');
 //# sourceMappingURL=version.js.map
