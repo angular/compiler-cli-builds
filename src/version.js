3f207191a8228533b4929798f174ea9a91dff19d
Victor Berchet <victor@suumit.com>
2017-08-03 00:19:33 +0000

    6a88659 test(upgrade): fix an IE9 timer issue in downgrade module tests (#18482)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-44ae6e9');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-6a88659');
 //# sourceMappingURL=version.js.map
