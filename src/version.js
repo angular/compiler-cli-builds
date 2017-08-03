d7e5dd6cac69a60f6c5e01af29d4d1a2c0829478
GitHub <noreply@github.com>
2017-08-03 21:59:46 +0000

    ca695e0 fix(compiler-cli): disable buggy expression lowering (#18513)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-9b015a9');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-ca695e0');
 //# sourceMappingURL=version.js.map
