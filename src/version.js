0a7a5ad5ae1521c1bfb18e4ebd17c814c322c059
Victor Berchet <victor@suumit.com>
2017-08-07 19:39:27 +0000

    04b18a9 docs(common): fix the DatePipe API docs (#18548)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-05472cb');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-04b18a9');
 //# sourceMappingURL=version.js.map
