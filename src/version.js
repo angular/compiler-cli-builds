06574859272f0b4eda8da416c96b7449ab68aa93
Victor Berchet <victor@suumit.com>
2017-08-07 19:24:52 +0000

    e0660b1 fix(animations): support persisting dynamic styles within animation states (#18468)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-3212f8c');
+exports.VERSION = new core_1.Version('4.3.3-e0660b1');
 //# sourceMappingURL=version.js.map
