a27da859f8504466c26701cb05fb749e756df5fb
Victor Berchet <victor@suumit.com>
2017-08-07 18:04:20 +0000

    3212f8c docs(aio): typo & update the bio (#18559)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-c421cca');
+exports.VERSION = new core_1.Version('4.3.3-3212f8c');
 //# sourceMappingURL=version.js.map
