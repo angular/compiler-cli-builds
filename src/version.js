9bdf3dd80b3840e2ec1c7ea049d59f72e76a8b1a
Victor Berchet <victor@suumit.com>
2017-08-07 23:00:54 +0000

    fcadbf4 perf: switch angular to use StaticInjector instead of ReflectiveInjector

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-685cc26');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-fcadbf4');
 //# sourceMappingURL=version.js.map
