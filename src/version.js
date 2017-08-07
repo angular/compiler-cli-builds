f6278a9c567fa8a18cde2ae7f086ab92ae67cbfd
Victor Berchet <victor@suumit.com>
2017-08-07 19:21:48 +0000

    c0c03dc fix(animations): revert container/queried animations accordingly during cancel (#18516)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-f9f8924');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-c0c03dc');
 //# sourceMappingURL=version.js.map
