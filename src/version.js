167fe6c781973c96d63357349c327d0920c391ca
Victor Berchet <victor@suumit.com>
2017-08-08 21:28:35 +0000

    1e18331 ci(aio): fix deploying to firebase (#18590)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-6f2038c');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-1e18331');
 //# sourceMappingURL=version.js.map
