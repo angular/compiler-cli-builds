6d33dfef7579fb47d137918911f3f85b3a36b242
GitHub <noreply@github.com>
2017-08-08 19:08:40 +0000

    f0a5501 fix(core): fix platform-browser-dynamic (#18576)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-f69561b');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-f0a5501');
 //# sourceMappingURL=version.js.map
