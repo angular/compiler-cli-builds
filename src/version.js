152fadbeb24a49f439cfb5aafb4d5aafcf4aa5f2
Victor Berchet <victor@suumit.com>
2017-08-09 23:10:15 +0000

    ff5c58b feat(forms): add default updateOn values for groups and arrays (#18536)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.2-dca50de');
+exports.VERSION = new core_1.Version('5.0.0-beta.2-ff5c58b');
 //# sourceMappingURL=version.js.map
