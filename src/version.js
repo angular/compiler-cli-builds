f0e27d5c679322e00c3832c12cfb5b9d6f4f5f76
Victor Berchet <victor@suumit.com>
2017-08-09 21:57:52 +0000

    3efc88f test(aio): fix the `deploy-to-firebase` tests

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-71f5e78');
+exports.VERSION = new core_1.Version('4.3.3-3efc88f');
 //# sourceMappingURL=version.js.map
