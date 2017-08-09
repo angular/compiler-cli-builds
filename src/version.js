37c6717c0fd4a8c4cd78a962cd5330bb90c90910
Victor Berchet <victor@suumit.com>
2017-08-09 22:01:22 +0000

    f4cb453 test(aio): fix running docs examples against local builds (#18520)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-3efc88f');
+exports.VERSION = new core_1.Version('4.3.3-f4cb453');
 //# sourceMappingURL=version.js.map
