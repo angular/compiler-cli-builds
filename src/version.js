0fa4cb7c2cbc29c4eb003d692ea4016dcb84510b
Victor Berchet <victor@suumit.com>
2017-08-10 19:00:32 +0000

    06e479f docs: add changelog for 4.3.4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-77fa3c3');
+exports.VERSION = new core_1.Version('4.3.4-06e479f');
 //# sourceMappingURL=version.js.map
