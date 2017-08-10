d745742931d87a0226b373ad83d4d1a6f25b0350
Victor Berchet <victor@suumit.com>
2017-08-10 03:48:28 +0000

    77fa3c3 test(aio): fix tests for example-boilerplate (#18619)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.3-f4cb453');
+exports.VERSION = new core_1.Version('4.3.3-77fa3c3');
 //# sourceMappingURL=version.js.map
