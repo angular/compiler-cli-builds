085f6fb45327c6b203212b3286c89acee6932c7c
Hans <hans@hansl.ca>
2017-08-10 23:26:44 +0000

    4d523fd fix(aio): fix compilation error by using the correct type for `providers`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-cea0241');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-4d523fd');
 //# sourceMappingURL=version.js.map
