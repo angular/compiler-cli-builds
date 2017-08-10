635b4b905a1cd30684240271b2feb4678dd05cd2
Hans <hans@hansl.ca>
2017-08-10 23:28:05 +0000

    06faac8 fix(aio): skip PWA test when redeploying non-public commit

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-4d523fd');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-06faac8');
 //# sourceMappingURL=version.js.map
