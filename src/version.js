354132cf0ccdcd184c657248abf0fc7200ab3315
Hans Larsen <hans@hansl.ca>
2017-08-11 20:58:10 +0000

    d7be4f1 perf(aio): update to new version of build-optimizer

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.4-06e479f');
+exports.VERSION = new core_1.Version('4.3.4-d7be4f1');
 //# sourceMappingURL=version.js.map
