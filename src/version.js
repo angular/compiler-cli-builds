55cecb02cc8cdc5e4d4a2ea9d90c9a556ea8a968
Hans <hans@hansl.ca>
2017-08-15 23:25:04 +0000

    38addac build(aio): switch from `@angular/http` to `@angular/common/http`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-856278c');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-38addac');
 //# sourceMappingURL=version.js.map
