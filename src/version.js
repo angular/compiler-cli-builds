3645a69c3228c0922ba017bb26055fb1cf66b615
Hans <hans@hansl.ca>
2017-08-15 23:57:34 +0000

    77747e1 refactor(core): remove toString() method from DefaultIterableDiffer

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-cce2ab2');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-77747e1');
 //# sourceMappingURL=version.js.map
