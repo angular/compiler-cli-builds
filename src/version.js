e473bfa69f1c9f8dfad729aaa2dba7f4c8a2e910
Hans <hans@hansl.ca>
2017-08-15 23:57:57 +0000

    1cfa79c feat(forms): add updateOn support to ngModelOptions

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-77747e1');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-1cfa79c');
 //# sourceMappingURL=version.js.map
