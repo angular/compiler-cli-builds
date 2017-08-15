30c9ffab72eea330093925634cd2957a786a4863
Hans Larsen <hans@hansl.ca>
2017-08-15 23:39:23 +0000

    cce2ab2 docs: add changelog for 4.3.4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-38addac');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-cce2ab2');
 //# sourceMappingURL=version.js.map
