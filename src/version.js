1c84ee10713520ea0af516323aa76ee1d0a68bed
Hans <hans@hansl.ca>
2017-08-15 19:48:07 +0000

    9320f34 docs(aio): add Metadata guide based on Chuck’s docs

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-d2c0d98');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-9320f34');
 //# sourceMappingURL=version.js.map
