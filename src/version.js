edbe29b7c98d79217acaa0473dfb2e7e9f02fe26
Hans <hans@hansl.ca>
2017-08-15 23:11:16 +0000

    856278c docs(core): correct code examples for ChangeDetectorRef

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-5e840e1');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-856278c');
 //# sourceMappingURL=version.js.map
