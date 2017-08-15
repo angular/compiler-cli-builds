e1bcae13eb505a2e2e4bf772655b5fec4d969642
Hans <hans@hansl.ca>
2017-08-15 22:57:44 +0000

    5e840e1 docs(forms): add api docs for AbstractControlDirective

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-233ef93');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-5e840e1');
 //# sourceMappingURL=version.js.map
