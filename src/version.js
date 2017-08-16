fd2b808a71f10fba489203cef3d13d499d10b8f1
Hans <hans@hansl.ca>
2017-08-16 00:16:35 +0000

    697c6ed docs: remove TypeScript to JavaScript guide & sample

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-64b4be9');
+exports.VERSION = new core_1.Version('5.0.0-beta.3-697c6ed');
 //# sourceMappingURL=version.js.map
