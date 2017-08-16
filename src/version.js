d4e3f616d53cc77090ba8b0a56a7f51a18c102e5
Hans Larsen <hans@hansl.ca>
2017-08-16 18:04:41 +0000

    a1624f2 fix(forms): re-assigning options should not clear select

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.4-b2f4d53');
+exports.VERSION = new core_1.Version('4.3.4-a1624f2');
 //# sourceMappingURL=version.js.map
