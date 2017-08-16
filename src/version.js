b347f750b2d9b0a7cc64f8de99699736e85f7958
Hans Larsen <hans@hansl.ca>
2017-08-16 20:57:27 +0000

    9aa0521 docs: add changelog for 5.0.0-beta.4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('5.0.0-beta.3-bc22ff1');
+exports.VERSION = new core_1.Version('5.0.0-beta.4-9aa0521');
 //# sourceMappingURL=version.js.map
