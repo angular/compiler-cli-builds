7ece68ea530b16acf0a1ad4949af119fe1d14e66
Miško Hevery <misko@hevery.com>
2017-08-18 18:45:01 +0000

    f9b2905 fix(animations): resolve error when using AnimationBuilder with platform-server (#18642)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-7e94405');
+exports.VERSION = new core_1.Version('4.3.5-f9b2905');
 //# sourceMappingURL=version.js.map
