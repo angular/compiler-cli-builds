c09e6d5336d412d3df8cbd9a9c0dd78ff60fc753
Miško Hevery <misko@hevery.com>
2017-08-18 00:15:17 +0000

    83713dd feat(common): add an empty DeprecatedI18NPipesModule module (#18737)

---
+++ src/main.d.ts
@@ -0,0 +1,3 @@
+import 'reflect-metadata';
+export declare function main(args: string[], consoleError?: (s: string) => void): Promise<number>;
+export declare function mainSync(args: string[], consoleError?: (s: string) => void): number;
---
+++ src/transformers/api.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var EmitFlags;
+(function (EmitFlags) {
+    EmitFlags[EmitFlags["DTS"] = 1] = "DTS";
+    EmitFlags[EmitFlags["JS"] = 2] = "JS";
+    EmitFlags[EmitFlags["Metadata"] = 4] = "Metadata";
+    EmitFlags[EmitFlags["I18nBundle"] = 8] = "I18nBundle";
+    EmitFlags[EmitFlags["Summary"] = 16] = "Summary";
+    EmitFlags[EmitFlags["Default"] = 3] = "Default";
+    EmitFlags[EmitFlags["All"] = 31] = "All";
+})(EmitFlags = exports.EmitFlags || (exports.EmitFlags = {}));
+//# sourceMappingURL=api.js.map
