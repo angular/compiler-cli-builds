13f7fb755ac5e1bb127a25b5e4393f80db946964
Miško Hevery <misko@hevery.com>
2017-08-18 23:52:28 +0000

    f2a2a6b refactor(core): remove deprecated `Testability#findBindings` (#18782)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-a56468c');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-f2a2a6b');
 //# sourceMappingURL=version.js.map
