d5e78a38d8815691d0939221b4742aa1cdf106cf
Miško Hevery <misko@hevery.com>
2017-08-19 14:33:43 +0000

    e1f45a3 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-77ebd2b');
+exports.VERSION = new core_1.Version('4.3.5-e1f45a3');
 //# sourceMappingURL=version.js.map
