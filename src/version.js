31ef7f0e89c9af9f21d6bb860acd77d9db09d33b
Miško Hevery <misko@hevery.com>
2017-08-19 14:29:17 +0000

    7062811 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-e228f2c');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-7062811');
 //# sourceMappingURL=version.js.map
