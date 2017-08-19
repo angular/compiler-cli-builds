90958e9f6a7980b3ceb9863f4e7ce5c5e707b10c
Miško Hevery <misko@hevery.com>
2017-08-19 00:04:42 +0000

    3b571a4 build(aio): do not auto-link code elements already inside a link (#18776)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-f9b2905');
+exports.VERSION = new core_1.Version('4.3.5-3b571a4');
 //# sourceMappingURL=version.js.map
