c274cdf06b25f4347e19147babb366e248db679b
Miško Hevery <misko@hevery.com>
2017-08-21 18:25:48 +0000

    5a1b9a3 style(animations): format integration spec (#18805)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-7062811');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-5a1b9a3');
 //# sourceMappingURL=version.js.map
