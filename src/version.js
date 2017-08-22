de6b5f9b3c2904607a1c28c282bb48a66f21b159
Miško Hevery <misko@hevery.com>
2017-08-22 01:53:23 +0000

    1f43713 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-14d34c9');
+exports.VERSION = new core_1.Version('4.3.5-1f43713');
 //# sourceMappingURL=version.js.map
