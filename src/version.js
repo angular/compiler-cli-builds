4a9eaf059acb9dea471b9031f528e93bfb40e96d
Miško Hevery <misko@hevery.com>
2017-08-22 02:02:04 +0000

    ec56760 refactor(common): remove deprecated `NgFor` (#18758)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-55d151a');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-ec56760');
 //# sourceMappingURL=version.js.map
