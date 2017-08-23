f30848800bfe4825efb85054f48b7f294bb6e5a1
Miško Hevery <misko@hevery.com>
2017-08-23 21:12:19 +0000

    f09a266 docs: add changelog for 4.3.6

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.5-641be64');
+exports.VERSION = new core_1.Version('4.3.6-f09a266');
 //# sourceMappingURL=version.js.map
