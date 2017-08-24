7bffa8b374ff041adc05fadd8b76523d073abe56
GitHub <noreply@github.com>
2017-08-24 14:56:52 +0000

    3db84c8 build: fix size limits for CLI to make CI green (#18849)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-3db84c8');
 //# sourceMappingURL=version.js.map
