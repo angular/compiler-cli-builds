24fe31b2b277a224687e359c1d6d25a512b64119
Misko Hevery <misko@hevery.com>
2017-08-24 17:04:59 +0000

    1c2f3d4 build: fix build by pinning angular-cli to specific version (#18865)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-3db84c8');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-1c2f3d4');
 //# sourceMappingURL=version.js.map
