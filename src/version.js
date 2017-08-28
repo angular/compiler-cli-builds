f0739f737b05eebb269a368c3a887ed078708219
Miško Hevery <misko@hevery.com>
2017-08-28 18:49:10 +0000

    f0c681a build: disable sass under bazel(#18848)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-3d2ee6c');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-f0c681a');
 //# sourceMappingURL=version.js.map
