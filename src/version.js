67f8b9126e141809b8199aba514cccfde9a38e50
Miško Hevery <misko@hevery.com>
2017-08-28 21:46:13 +0000

    655b495 build: reenable npm install for air builds

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.4-92dbaf3');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.4-655b495');
 //# sourceMappingURL=version.js.map
