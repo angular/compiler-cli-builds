af20343149a466493e40cb80ed53c0032f47b7fc
Miško Hevery <misko@hevery.com>
2017-08-29 00:15:24 +0000

    dfe2bad build: Add GitHub scripts for rebasing PRs (#18359)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-f09a266');
+exports.VERSION = new core_1.Version('4.3.6-dfe2bad');
 //# sourceMappingURL=version.js.map
