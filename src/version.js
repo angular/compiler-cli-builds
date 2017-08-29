1cb5b674bed4a30d92104fd595bb16cdc9ace06e
Miško Hevery <misko@hevery.com>
2017-08-29 16:55:20 +0000

    4878936 feat(common): generate `closure-locale.ts` to tree shake locale data (#18907)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-72c7b6e');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-4878936');
 //# sourceMappingURL=version.js.map
