fa61a50765fb7571a4ac94f4843108d83f14c6e9
Jason Aden <jasonaden@google.com>
2017-08-30 00:46:59 +0000

    a203a95 fix(common): fix improper packaging for @angular/common/http (#18613)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-8821723');
+exports.VERSION = new core_1.Version('4.3.6-a203a95');
 //# sourceMappingURL=version.js.map
