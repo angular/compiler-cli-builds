ba7ac296d45770be9d242a70101d2e00b7092ada
Jason Aden <jasonaden@google.com>
2017-08-30 02:20:39 +0000

    4ec5e28 fix(common): use correct group separator for currency pipe (#18932)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-452a7ae');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-4ec5e28');
 //# sourceMappingURL=version.js.map
