2a98e26c774e8a70cd545f0fffec4d0e57989d69
Jason Aden <jasonaden@google.com>
2017-08-31 01:15:53 +0000

    497e017 fix(compiler): normalize the locale name (#18963)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-a203a95');
+exports.VERSION = new core_1.Version('4.3.6-497e017');
 //# sourceMappingURL=version.js.map
