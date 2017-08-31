12a789ddb6db3223b6ee49e5c79a8610e6deedc9
GitHub <noreply@github.com>
2017-08-31 01:01:53 +0000

    043f104 fix(compiler): normalize the locale name (#18963)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-4ec5e28');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-043f104');
 //# sourceMappingURL=version.js.map
