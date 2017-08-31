666297f33ab5b8cc0497dfcabf21fe74d41f7196
Jason Aden <jasonaden@google.com>
2017-08-31 18:52:29 +0000

    57abe8d refactor(common): export locale data from closure-locale (#18973)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-cf7d47d');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-57abe8d');
 //# sourceMappingURL=version.js.map
