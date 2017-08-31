ec9ead172944814146367f8a29f08984370e365a
Jason Aden <jasonaden@google.com>
2017-08-31 02:31:37 +0000

    c7e1bda Revert "feat(compiler-cli): lower metadata `useValue` and `data` literal fields (#18905)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-2f2d5f3');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-c7e1bda');
 //# sourceMappingURL=version.js.map
