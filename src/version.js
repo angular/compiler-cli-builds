bff3d5c4debc4a5596407a70c479016593b2e1cc
Jason Aden <jasonaden@google.com>
2017-08-31 01:22:11 +0000

    2f2d5f3 feat(platform-server): provide a DOM implementation on the server

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-30d53a8');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-2f2d5f3');
 //# sourceMappingURL=version.js.map
