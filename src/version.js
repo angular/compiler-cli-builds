a21b4282c668a9d2a49bb5fca3febc4b63f4cf61
Olivier Combe <olivier.combe@gmail.com>
2017-08-31 09:30:23 +0000

    e1dc9bf style(changelog): improving readability

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-3725535');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-e1dc9bf');
 //# sourceMappingURL=version.js.map
