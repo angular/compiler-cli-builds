47823717d7b43951a04447fcfb75fd89fecb9221
Victor Berchet <victor@suumit.com>
2017-08-31 01:06:37 +0000

    30d53a8 fix(common): use v4 plurals when importing `DeprecatedI18NPipesModule` (#18955)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-043f104');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-30d53a8');
 //# sourceMappingURL=version.js.map
