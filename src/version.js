bf3cd90526590e1ec895492d61021407d24388c0
Jason Aden <jasonaden@google.com>
2017-09-01 17:53:46 +0000

    1cc3fe2 fix(animations): do not leak DOM nodes/styling for host triggered animations (#18853)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.3.6-ba7d70e');
+exports.VERSION = new core_1.Version('4.3.6-1cc3fe2');
 //# sourceMappingURL=version.js.map
