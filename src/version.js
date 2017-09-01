8c52514c897baef05bc785c07f8c1d9aae4e0ca8
Jason Aden <jasonaden@google.com>
2017-09-01 17:34:39 +0000

    56238fe feat(compiler): set `enableLegacyTemplate` to false by default (#18756)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-fd701b0');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-56238fe');
 //# sourceMappingURL=version.js.map
