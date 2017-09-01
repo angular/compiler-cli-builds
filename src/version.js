ab7e78e3c42939a577a3d94a1d723f95b5da3b58
Miško Hevery <misko@hevery.com>
2017-09-01 21:51:05 +0000

    185a941 test: support debugging unit tests in node (#18987)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.5-6af2396');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.5-185a941');
 //# sourceMappingURL=version.js.map
