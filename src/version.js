3353e1c54a47f378fde90ba45325baa4f0db28b1
Miško Hevery <misko@hevery.com>
2017-09-06 16:35:11 +0000

    29762f1 test(aio): fix docs example e2e test (#19070)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-d2707f1');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-29762f1');
 //# sourceMappingURL=version.js.map
