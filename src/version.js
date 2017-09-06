9c5175c716dfc4b0386112a0936ebb0ab9722f5f
Miško Hevery <misko@hevery.com>
2017-09-06 04:59:25 +0000

    d2707f1 build: cleanup duplicated code now in @bazel/typescript (#19004)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-15945c8');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-d2707f1');
 //# sourceMappingURL=version.js.map
