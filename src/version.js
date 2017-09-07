22521a38ad7aa2b30d41e5a13ae4bd2bdfe8e182
Matias Niemelä <matias@yearofmoo.com>
2017-09-07 20:17:53 +0000

    b6833d1 fix(upgrade): remove code setting id attribute.

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-3c480e4');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-b6833d1');
 //# sourceMappingURL=version.js.map
