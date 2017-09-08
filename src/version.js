c54873c67b852bb789da923bf8dc7c6f4250b511
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 23:51:18 +0000

    112e777 build(aio): update karma & systemjs config for HttpClient

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-a176cd6');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-112e777');
 //# sourceMappingURL=version.js.map
