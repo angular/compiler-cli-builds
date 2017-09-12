38108a90df7d29285f016af015d398c0fe7120e3
Matias Niemelä <matias@yearofmoo.com>
2017-09-12 21:25:59 +0000

    c8f742e test(packaging): added test for source map correctness

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-9ab9437');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-c8f742e');
 //# sourceMappingURL=version.js.map
