529160fee889de0b3a1601541983aa6e65f7a67e
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 21:01:13 +0000

    9b571a1 build(core): reduce payload size for cli-hello-world (#19159)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-bf94f87');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.6-9b571a1');
 //# sourceMappingURL=version.js.map
