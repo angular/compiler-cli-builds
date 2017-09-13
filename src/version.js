aa279f9944055ef8d8d7563ab6290fce4f45e505
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 21:30:24 +0000

    42c69d3 docs: add changelog for 5.0.0-beta.7

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.6-9b571a1');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-42c69d3');
 //# sourceMappingURL=version.js.map
