23520338d7fc7193f1d026143e3f3e83af6a60c5
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:51 +0000

    2373186 docs: update combined changelog for 4.4 stable

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-b6431c6');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-2373186');
 //# sourceMappingURL=version.js.map
