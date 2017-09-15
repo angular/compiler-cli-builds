06e06941339bb328fe31f0de238abc749a29dd3b
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:18:05 +0000

    0c72f7d docs: update combined changelog for 4.4 stable

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.1-818f4a7');
+exports.VERSION = new core_1.Version('4.4.1-0c72f7d');
 //# sourceMappingURL=version.js.map
