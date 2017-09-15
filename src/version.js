aa48479558e1bb50056cb6ee5e03e4509e2f3fe9
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:10:51 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.0-RC.0-e8d1858');
+exports.VERSION = new core_1.Version('4.4.0-RC.0-0bafd03');
 //# sourceMappingURL=version.js.map
