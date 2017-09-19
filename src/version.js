09734a2e2718f8b9666e4184119d276eb81d865d
Igor Minar <igor@angularjs.org>
2017-09-19 22:59:33 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.2-0371538');
+exports.VERSION = new core_1.Version('4.4.3-e2e8ba6');
 //# sourceMappingURL=version.js.map
