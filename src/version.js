578cbeb4892b33454b16b55082a79db462d819f5
Igor Minar <igor@angularjs.org>
2017-09-19 04:35:02 +0000

    b6b18c1 fix(tsc-wrapped): deduplicate metadata only when the module is the same (#19249)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-2373186');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-b6b18c1');
 //# sourceMappingURL=version.js.map
