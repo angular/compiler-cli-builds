b21978365328225c50e28d216079357216f443d1
Igor Minar <igor@angularjs.org>
2017-09-19 04:36:09 +0000

    49d122e release: cut the 4.4.2 release

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.1-ff87294');
+exports.VERSION = new core_1.Version('4.4.2-49d122e');
 //# sourceMappingURL=version.js.map
