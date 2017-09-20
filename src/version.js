c7f2f64ca80077b7b996fb4eb697f6627e964c77
Igor Minar <igor@angularjs.org>
2017-09-20 21:04:36 +0000

    b14c2d1 refactor(core): removed getter and changed to readonly variable (#19117)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-5751865');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-b14c2d1');
 //# sourceMappingURL=version.js.map
