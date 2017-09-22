1e481f7b828b0991d9a82b6c165e766e146d8f41
Igor Minar <igor@angularjs.org>
2017-09-22 01:20:41 +0000

    9bbf009 build(aio): change examples to CLI (#19248)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-6e1896b');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-9bbf009');
 //# sourceMappingURL=version.js.map
