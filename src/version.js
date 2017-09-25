96ff54d5a8420a57f22f157dc50974bd4946d3b7
Victor Berchet <victor@suumit.com>
2017-09-25 20:01:08 +0000

    e224e3d fix(compiler-cli): update ngtools2 EmitFlags (#19375)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-9ad4b3b');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-e224e3d');
 //# sourceMappingURL=version.js.map
