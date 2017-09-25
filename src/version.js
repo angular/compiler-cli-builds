d8a1a10e87f258b76ad0ba46711902c2099e28b1
Victor Berchet <victor@suumit.com>
2017-09-25 23:40:45 +0000

    3f100eb build(aio): fix the build script (#19390)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-ffceae0');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-3f100eb');
 //# sourceMappingURL=version.js.map
