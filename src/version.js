da9249cc6b1a4c29d32b10b18a7ff325dd45775d
Victor Berchet <victor@suumit.com>
2017-09-25 19:20:45 +0000

    adb0b76 build(aio): add metadata aliases for directives, components and pipes (#19317)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-0f5c70d');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-adb0b76');
 //# sourceMappingURL=version.js.map
