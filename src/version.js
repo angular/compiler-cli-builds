859e6f483aac0e93f4e5a4271cd4a9258c9ec33a
Victor Berchet <victor@suumit.com>
2017-09-26 22:22:29 +0000

    8a0e458 fix(http): introduce named type for HttpParams options (#19360)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-b21a1d1');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-8a0e458');
 //# sourceMappingURL=version.js.map
