db4d2831b68736a892c36122591a565dd50ac9b8
Victor Berchet <victor@suumit.com>
2017-09-26 22:21:19 +0000

    b21a1d1 fix: create proper externs so that closure does not clobber e.g. `ng` for internal variables (#19423)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-dfb8d21');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-b21a1d1');
 //# sourceMappingURL=version.js.map
