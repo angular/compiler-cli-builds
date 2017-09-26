b4e32379cc5481fee09b70d2196207c4446d210d
Victor Berchet <victor@suumit.com>
2017-09-26 17:36:36 +0000

    f57b7df fix(compiler): remove deprecated `Compiler.ngGetContentSelectors()` (#19347)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-2cf52c1');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-f57b7df');
 //# sourceMappingURL=version.js.map
