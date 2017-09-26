84bdc2b3da914ee3cc4375470f63f10296761699
Victor Berchet <victor@suumit.com>
2017-09-26 17:43:06 +0000

    2e1fec1 ci(aio): temporarily disable aot-compiler example e2e tests (#19410)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-f57b7df');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-2e1fec1');
 //# sourceMappingURL=version.js.map
