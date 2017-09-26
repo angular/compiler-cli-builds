6dcbc90ec0acfa2f6123ca01d180db8ceecfd62d
Victor Berchet <victor@suumit.com>
2017-09-26 19:22:49 +0000

    86ffacf feat(animations): support negative query option values

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-e889c68');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-86ffacf');
 //# sourceMappingURL=version.js.map
