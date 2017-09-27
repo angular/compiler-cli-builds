fc41366e5a194dea60a09f65dd2b254bb75a00a2
Victor Berchet <victor@suumit.com>
2017-09-27 21:08:42 +0000

    55a7443 docs(aio): edit summary and next step headers (#16962)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.3-bc81fbd');
+exports.VERSION = new core_1.Version('4.4.3-55a7443');
 //# sourceMappingURL=version.js.map
