fc96bcef12ca45cd486e8decf48cd17ea3009d0b
Victor Berchet <victor@suumit.com>
2017-09-27 22:44:08 +0000

    334caf8 docs(aio): add copy about NgForOf (#18686)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-48c6ece');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-334caf8');
 //# sourceMappingURL=version.js.map
