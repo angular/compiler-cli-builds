7988c55f3d244ae63da7b053248e792b5096cae0
Victor Berchet <victor@suumit.com>
2017-09-27 22:52:39 +0000

    0cefb0b docs(aio): add copy about NgForOf (#18686)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.3-55a7443');
+exports.VERSION = new core_1.Version('4.4.3-0cefb0b');
 //# sourceMappingURL=version.js.map
