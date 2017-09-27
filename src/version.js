af72261f30a32dd8d37630d41e3a70a7c37d36f0
Victor Berchet <victor@suumit.com>
2017-09-27 17:31:26 +0000

    627f048 fix(compiler): implement i18n with new compiler

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-8a0e458');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-627f048');
 //# sourceMappingURL=version.js.map
