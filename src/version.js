ac336bb90d53791f0ea28cb5d15d3482db6ee905
Victor Berchet <victor@suumit.com>
2017-09-28 19:42:55 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-a9d32a3');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-bed8ac7');
 //# sourceMappingURL=version.js.map
