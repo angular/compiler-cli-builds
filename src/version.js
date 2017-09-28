a51679779956898e42b77c8b3fc17dd19093edab
Victor Berchet <victor@suumit.com>
2017-09-28 23:44:29 +0000

    d442b68 feat(service-worker): introduce the @angular/service-worker package (#19274)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-7c1d3e0');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-d442b68');
 //# sourceMappingURL=version.js.map
