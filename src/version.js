3bbf1ec121be2fca724c057b2a5e85928e5d6cc2
Victor Berchet <victor@suumit.com>
2017-09-28 20:59:49 +0000

    14e8e88 Revert "refactor(core): Change `abstract get` to `readonly` (#19226)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-bed8ac7');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-14e8e88');
 //# sourceMappingURL=version.js.map
