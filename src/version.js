cb592ed12afbc12e3160da9f2f592918e8fa399c
Victor Berchet <victor@suumit.com>
2017-09-28 16:48:38 +0000

    5406a49 docs: add 'bazel' as an Angular component (#19346)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-8dbe2af');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-5406a49');
 //# sourceMappingURL=version.js.map
