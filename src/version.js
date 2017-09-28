3881867f0c58621e5ba308f07fd52420648ffdf2
Victor Berchet <victor@suumit.com>
2017-09-28 18:12:26 +0000

    ff5b050 fix(compiler): correctly map error message locations (#19424)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-2b84b86');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-ff5b050');
 //# sourceMappingURL=version.js.map
