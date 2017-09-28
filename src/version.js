51d3bde18c190fbc010b249bafdd305ffe92c309
Victor Berchet <victor@suumit.com>
2017-09-28 16:48:42 +0000

    82e4923 build: Give names to the AMD modules nested in Angular UMD distro (#19425)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-beta.7-5406a49');
+exports.VERSION = new compiler_1.Version('5.0.0-beta.7-82e4923');
 //# sourceMappingURL=version.js.map
