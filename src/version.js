202a7619166247a153c79235cca3e3d965561a8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-02 23:23:49 +0000

    eef7d8a fix(upgrade): call `ngOnInit()` after `ngOnChanges()` (on components with inputs)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-745b59f');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-eef7d8a');
 //# sourceMappingURL=version.js.map
