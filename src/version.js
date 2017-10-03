a3d851b231c77cbca0b95ba966d87ad99e38343b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:27:48 +0000

    7cecaf2 docs(aio): add ngVikings to the events page

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-eef7d8a');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-7cecaf2');
 //# sourceMappingURL=version.js.map
