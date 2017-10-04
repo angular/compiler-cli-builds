5c853e44bf0f436cb8e32c61b3408d07605d8679
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:27:20 +0000

    c80e02b docs(aio): document Custom Async validators

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-fbc9537');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-c80e02b');
 //# sourceMappingURL=version.js.map
