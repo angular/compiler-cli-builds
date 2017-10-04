70fa3a1bc223583dfff30ae381de40e9c6b55b08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:05:39 +0000

    d4d9009 docs(aio): fix broken link in toh-pt5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-0f43299');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-d4d9009');
 //# sourceMappingURL=version.js.map
