c9fcea3476ec561fed0fc2b3f2884a8e5cca6d19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:02:13 +0000

    9ae3742 docs(aio): fix typo in preview server config file comment

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-7c5ecb5');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-9ae3742');
 //# sourceMappingURL=version.js.map
