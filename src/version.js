fe670514a6b15d0c5d68393e6274d2b38d17e0f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-05 20:41:48 +0000

    b804d4b Revert "fix(platform-browser): support customEqualityTesters when overriding Jasmine toEqual"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.0-01f7112');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.0-b804d4b');
 //# sourceMappingURL=version.js.map
