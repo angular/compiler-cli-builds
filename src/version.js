d471c88c69ae2807c30d93a7c4f87c8f2ccf1b00
Tobias Bosch <tbosch1009@gmail.com>
2017-10-06 19:17:28 +0000

    03227e6 build(aio): example-boilerplate is no longer responsible for yarn install (#19511)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-81e9bdc');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-03227e6');
 //# sourceMappingURL=version.js.map
