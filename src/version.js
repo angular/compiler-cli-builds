02649c3dcecf6955db68ba2cfe63961cd3a10d10
Chuck Jazdzewski <chuckj@google.com>
2017-10-07 01:37:26 +0000

    b1ca5d4 Revert "perf(animations): reduce size of bundle by removing AST classes (#19539)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-a8920eb');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-b1ca5d4');
 //# sourceMappingURL=version.js.map
