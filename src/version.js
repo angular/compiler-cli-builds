cea493da01ce4f33fe0d7f84fb1e6f9b3b4399ff
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 22:08:19 +0000

    f393d86 build: add a Git .mailmap with my new name (#19550)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-6651541');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-f393d86');
 //# sourceMappingURL=version.js.map
