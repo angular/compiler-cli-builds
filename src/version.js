b46fe94db66881e91e607c69f799a217bf730a03
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 22:12:52 +0000

    32ca685 build(aio): do not fail on first `yarn setup` (#19603)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-f393d86');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-32ca685');
 //# sourceMappingURL=version.js.map
