eb1a4674b09216a0d7b1d26982994947c597656e
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 23:33:48 +0000

    79deeac docs(aio): add copy about ngNoForm to NgForm API doc (#19561)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-32ca685');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-79deeac');
 //# sourceMappingURL=version.js.map
