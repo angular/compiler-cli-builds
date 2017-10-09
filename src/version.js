7a4d80b6105050f48fea21d1cbcbe81fa340d4d1
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 20:06:50 +0000

    6651541 fix(router): do not call `location.go` when skipping a navigation (#19463)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-c3a5269');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-6651541');
 //# sourceMappingURL=version.js.map
