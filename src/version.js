5ddb5cd93d0597f1ef5a6cef0ae4b07b5b1457e8
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 22:00:08 +0000

    b0befd7 fix(compiler): `TestBed.overrideProvider` should keep imported `NgModule`s eager (#19624)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-dfa0973');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-b0befd7');
 //# sourceMappingURL=version.js.map
