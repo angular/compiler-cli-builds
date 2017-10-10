9b20c16521cc0385c8ebc66148e25b578eea1402
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 21:09:32 +0000

    dfa0973 docs(aio): update animations for CLI (#19551)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-ac50bd6');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-dfa0973');
 //# sourceMappingURL=version.js.map
