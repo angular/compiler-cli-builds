f26b424b4a430931b795bcbbd5c8921f6cb0973b
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 00:45:57 +0000

    d7eac7e Revert "fix(router): navigating to the current location works (#19463)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-79deeac');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-d7eac7e');
 //# sourceMappingURL=version.js.map
