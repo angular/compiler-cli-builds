58ad5554b4856c4518b5f5265cea74cbc33490be
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 18:17:44 +0000

    46992b4 Revert "fix(animations): ensure animateChild() works with all inner leave animations (#19006) (#19532)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-8b57130');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-46992b4');
 //# sourceMappingURL=version.js.map
