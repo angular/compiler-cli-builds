bfa5b450aee3f6202abcb4dad4a9a12cdc41d961
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 21:58:47 +0000

    3e3f918 ci: freeze yarn lockfile when installing dependencies (#19616)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.4-734378c');
+exports.VERSION = new core_1.Version('4.4.4-3e3f918');
 //# sourceMappingURL=version.js.map
