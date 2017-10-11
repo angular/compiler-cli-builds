7bcccc5c25b4bb6bef0d745eed32a9c3a1f56937
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 00:29:17 +0000

    8b57130 build(aio): append information about links in and out of docs (#19583)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-b0befd7');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-8b57130');
 //# sourceMappingURL=version.js.map
