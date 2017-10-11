c4dd038a2135c01e8287e18fc7c01ce6365e6719
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 22:54:55 +0000

    28c29d5 build: fix rxjs version for aio (#19585)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.4-65ca7fd');
+exports.VERSION = new core_1.Version('4.4.4-28c29d5');
 //# sourceMappingURL=version.js.map
