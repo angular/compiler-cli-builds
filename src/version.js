721a3500e3d6e622cceba81121d90c31d74e578e
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:54:55 +0000

    6b7cead Revert "Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)""

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-055b802');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-6b7cead');
 //# sourceMappingURL=version.js.map
