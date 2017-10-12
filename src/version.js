dd1d0ebbe255c5f1ab6cde39db6fb70d90f4f58a
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:11:42 +0000

    f27f6e4 docs(aio): improve the Angular Connect homepage banner (#19684)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.4-a15abbb');
+exports.VERSION = new core_1.Version('4.4.4-f27f6e4');
 //# sourceMappingURL=version.js.map
