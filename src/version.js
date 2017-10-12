3d303dec3dfbda883b43f11e27531aa21ac4209a
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 00:14:39 +0000

    a15abbb docs(aio): change Material 2 to Material (#19663)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.4-28c29d5');
+exports.VERSION = new core_1.Version('4.4.4-a15abbb');
 //# sourceMappingURL=version.js.map
