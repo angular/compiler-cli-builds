c0048e48932d72d785e6f7471a650720750ef705
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:09:26 +0000

    055b802 docs(aio): improve the Angular Connect homepage banner (#19684)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.1-41fec31');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.1-055b802');
 //# sourceMappingURL=version.js.map
