dc3ce7c9c6ceef5b2752af5c0358746eec9fe030
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 23:51:01 +0000

    836c889 fix(compiler): prepare for future Bazel semantics of += (#19717)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-43f9d91');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.2-836c889');
 //# sourceMappingURL=version.js.map
