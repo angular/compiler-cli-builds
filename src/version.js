b3c4b03c8285af4e8933cb5268c380ed3e0808b7
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:36:14 +0000

    591dcc2 docs(animations): add missing bracket to fadeAnimation

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.5-4acd322');
+exports.VERSION = new core_1.Version('4.4.5-591dcc2');
 //# sourceMappingURL=version.js.map
