1f0025e2e1dd1459dab9111ad5f8833ffa3c91fd
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:54:16 +0000

    43f9d91 build(aio): fix overwriting with local Angular packages that depend on other local ones (#19655)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-97969a8');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.2-43f9d91');
 //# sourceMappingURL=version.js.map
