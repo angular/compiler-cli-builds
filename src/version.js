303a38545fce5038369636dfeb9f278950a644dd
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 17:05:16 +0000

    f983a6c fix(animations): properly support boolean-based transitions and state changes (#19672)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.5-18f1b01');
+exports.VERSION = new core_1.Version('4.4.5-f983a6c');
 //# sourceMappingURL=version.js.map
