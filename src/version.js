3ff7364acaa559783886feab99652585586b9f85
Tobias Bosch <tbosch1009@gmail.com>
2017-10-14 00:50:46 +0000

    405ccc7 fix(bazel): fix the output directory for extractor to be genfiles/ instead of bin/ (#19716)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-836c889');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.2-405ccc7');
 //# sourceMappingURL=version.js.map
