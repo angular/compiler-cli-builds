229269e82e144d73d94b295e8cbfdbd432953dfc
Tobias Bosch <tbosch1009@gmail.com>
2017-10-16 18:35:11 +0000

    d035175 fix(animations): ensure inner :leave animations do not remove node when skipped (#19532) (#19693)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-405ccc7');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.2-d035175');
 //# sourceMappingURL=version.js.map
