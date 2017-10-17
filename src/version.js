02328737513fea60b9cb6991ad7d82bdc97d0187
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:03:33 +0000

    ce0ac46 style: fix formatting of check-node-modules (#19720)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.5-1be22df');
+exports.VERSION = new core_1.Version('4.4.5-ce0ac46');
 //# sourceMappingURL=version.js.map
