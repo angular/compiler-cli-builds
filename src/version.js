c986597f43f9577b88f9d77c07700bf7d5c04ccb
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:20:52 +0000

    b0c7ea8 Revert "fix(router): RouterLinkActive should update its state right after checking the children (#19449)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-5a9ed2d');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.2-b0c7ea8');
 //# sourceMappingURL=version.js.map
