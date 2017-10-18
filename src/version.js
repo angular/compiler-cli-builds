60fc13f3b89584d7374cd8eb3b810862c67873ce
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:47:46 +0000

    3342a82 fix(aio): make tests less flaky (#19784) PR Close #19784

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.5-630c19f');
+exports.VERSION = new core_1.Version('4.4.5-3342a82');
 //# sourceMappingURL=version.js.map
