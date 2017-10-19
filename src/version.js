ec12ebe2fef3d34401a8e5111f0aea910a351732
Tobias Bosch <tbosch1009@gmail.com>
2017-10-19 01:04:13 +0000

    230b98d docs: add changelog for 5.0.0-rc.3

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.2-b922743');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.3-230b98d');
 //# sourceMappingURL=version.js.map
