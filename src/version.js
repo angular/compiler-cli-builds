a7e09b52cdd4dbc8b887eadc05d441e6be00cd49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:01:37 +0000

    fc86352 build(aio): use http push for navigation.json

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.3-230b98d');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.3-fc86352');
 //# sourceMappingURL=version.js.map
