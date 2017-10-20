dfb5de9964d44559603fb09c99923acadc45ca22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:56 +0000

    14380ff build: add warning about changing ngtools_api2

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.3-441e01c');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.3-14380ff');
 //# sourceMappingURL=version.js.map
