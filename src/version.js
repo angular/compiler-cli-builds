772eb674f94db5e1e4a9bed19fd57e75499fd615
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:30 +0000

    441e01c fix(aio): simplify GaService to avoid e2e test failures

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.3-fc86352');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.3-441e01c');
 //# sourceMappingURL=version.js.map
