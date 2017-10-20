d6334065a3068691ba433371096731ea82809d1b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:40:44 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.3-820bb7b');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.3-910735d');
 //# sourceMappingURL=version.js.map
