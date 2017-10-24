146ae835a035a8ed9eeb2b4998727961c8bd51cc
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 05:17:25 +0000

    a0ae120 fix(compiler-cli): report all diagnostic error messages (#19886)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.4-bde5701');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.4-a0ae120');
 //# sourceMappingURL=version.js.map
