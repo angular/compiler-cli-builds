ca6a973be4171c1bfa30346a7ec71d4dc16aa71d
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:01:25 +0000

    fde5f2f build: update to rxjs@5.5.2 (#19931)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.5-d567246');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.5-fde5f2f');
 //# sourceMappingURL=version.js.map
