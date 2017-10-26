6b39e3834ffe89e211c837038621197e660fdd1f
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 20:37:16 +0000

    17142a7 fix(service-worker): don't block initialization on registration (#19936)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.6-703fcda');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.6-17142a7');
 //# sourceMappingURL=version.js.map
