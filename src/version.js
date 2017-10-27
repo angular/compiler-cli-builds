82bf6d3800ff9234f241f10097298989647ba1b9
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 03:11:31 +0000

    5542517 docs: add changelog for 5.0.0-rc.7

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.6-f4d5729');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.7-5542517');
 //# sourceMappingURL=version.js.map
