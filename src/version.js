cd5ccaa9a827c0b7e6aca1116c925279c436568b
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 07:11:59 +0000

    308fc8e docs: add changelog for 5.0.0-rc.8

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.7-394d951');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-308fc8e');
 //# sourceMappingURL=version.js.map
