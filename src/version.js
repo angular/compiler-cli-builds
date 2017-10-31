a3464869a8c25c71368bc8114e0efa6f93217735
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:58:57 +0000

    c295aee build: add `release` as a valid commit message subject (#19955)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.8-e3a16ed');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-c295aee');
 //# sourceMappingURL=version.js.map
