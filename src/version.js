27debe5dba6403714891c9beac470c9381870e8c
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:49:55 +0000

    f1248b6 refactor: make all rollup config ES5 compatible (#20028)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.8-812786f');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-f1248b6');
 //# sourceMappingURL=version.js.map
