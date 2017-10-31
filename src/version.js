31ed7c94f64dcf85efecd621596c40b88f70a593
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 06:06:24 +0000

    5225fdb build(aio): add i18n boilerplate type (#20004)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.9-f5b7f2b');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.9-5225fdb');
 //# sourceMappingURL=version.js.map
