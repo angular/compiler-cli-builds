a4b98b8f7d1f39224cc24fd167895b824ad52f60
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:31:06 +0000

    812786f fix: add missing globals from each rollup configuration (#20028)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.8-de24d54');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-812786f');
 //# sourceMappingURL=version.js.map
