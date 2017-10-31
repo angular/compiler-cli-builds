9a6d349e55d31111f8f42530b53538d12daf73a1
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:06:49 +0000

    cf5fce8 build: update rollup lint rule from bad merge (#20047)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.8-f1248b6');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-cf5fce8');
 //# sourceMappingURL=version.js.map
