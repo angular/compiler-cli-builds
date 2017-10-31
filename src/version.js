463440a6e9eaa5f7e3b0dbe8a2e83bac116c707f
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:39:08 +0000

    420852e fix(compiler): reexport less symbols in `.ngfactory.ts` files (#19884)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.8-04eb80c');
+exports.VERSION = new compiler_1.Version('5.0.0-rc.8-420852e');
 //# sourceMappingURL=version.js.map
