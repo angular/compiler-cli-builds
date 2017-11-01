71bb730feb6a0660365cc016fee0d774c68281cd
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:29:22 +0000

    e1fb65c docs(packaging): fix typo

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-rc.9-5225fdb');
+exports.VERSION = new compiler_1.Version('5.0.0-e1fb65c');
 //# sourceMappingURL=version.js.map
