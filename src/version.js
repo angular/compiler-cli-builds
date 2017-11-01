4ed168d7c10059de7d327bbbda96812a1bfca5df
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:42:58 +0000

    c30eff8 docs(aio): update universal for CLI

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-ca129ba');
+exports.VERSION = new compiler_1.Version('5.0.0-c30eff8');
 //# sourceMappingURL=version.js.map
