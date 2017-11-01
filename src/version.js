40b1d17218d1a01776333e87d1a277ab3e9543cd
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:53:40 +0000

    a1ec65b docs(aio): fix typo in attribute-directives

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-851e1cf');
+exports.VERSION = new compiler_1.Version('5.0.0-a1ec65b');
 //# sourceMappingURL=version.js.map
