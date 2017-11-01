3a190305e7288d11fd5b3d0594401850b3e1f8c1
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:45:44 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-171ae15');
+exports.VERSION = new compiler_1.Version('5.0.0-ca129ba');
 //# sourceMappingURL=version.js.map
