986e85abf3da3d126e46cd305cc8474f4debb41f
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:03:06 +0000

    27ae0f9 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-280dada');
+exports.VERSION = new compiler_1.Version('5.0.0-27ae0f9');
 //# sourceMappingURL=version.js.map
