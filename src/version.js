009a05971c2f73a48fb9427895b81e984b4459a3
Igor Minar <igor@angular.io>
2017-11-01 23:45:37 +0000

    2ea76cc build: change the version in package.json to 5.1.0-beta

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-27ae0f9');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-2ea76cc');
 //# sourceMappingURL=version.js.map
