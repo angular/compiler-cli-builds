ab11f4a125c4cece37dce9068a92341beb610374
Victor Berchet <victor@suumit.com>
2017-11-02 22:13:24 +0000

    17ed14f docs: fix changelog for v5 (#20097)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-7186c9c');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-17ed14f');
 //# sourceMappingURL=version.js.map
