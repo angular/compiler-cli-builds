2d467a12fdcf65e29610bf573fad007a37fbbf5e
Victor Berchet <victor@suumit.com>
2017-11-02 21:45:00 +0000

    132c071 docs(aio): updated i18n guide and example (#19975)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-3db7112');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-132c071');
 //# sourceMappingURL=version.js.map
