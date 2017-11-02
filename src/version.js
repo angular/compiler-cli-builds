602fd71166149ce4b828162f839f1a6e220272a6
Victor Berchet <victor@suumit.com>
2017-11-02 23:16:05 +0000

    37a740b build: remove local yarn (#19981)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-067e926');
+exports.VERSION = new compiler_1.Version('5.0.0-37a740b');
 //# sourceMappingURL=version.js.map
