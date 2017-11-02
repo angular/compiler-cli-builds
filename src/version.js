5cca7cec8bb8fa210953289c68b3b451d769a3a1
Igor Minar <igor@angular.io>
2017-11-02 00:25:59 +0000

    3db7112 docs(aio): fix linting for universal (#20086)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-2ea76cc');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-3db7112');
 //# sourceMappingURL=version.js.map
