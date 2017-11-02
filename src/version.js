81da05770e9bd7fe4251ac5bc27b255478594255
Victor Berchet <victor@suumit.com>
2017-11-02 23:36:09 +0000

    a28d616 fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-613a9e3');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-a28d616');
 //# sourceMappingURL=version.js.map
