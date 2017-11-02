fc1c8df4575873014d1fa6aac016ce9c070beaea
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:19 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-5b16ce9');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-068348e');
 //# sourceMappingURL=version.js.map
