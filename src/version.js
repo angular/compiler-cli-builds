8b74162283e076ff709fb3ce75fa5e991514edf6
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:53 +0000

    613a9e3 build: update Windows symlink scripts (#16761)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-65d57a0');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-613a9e3');
 //# sourceMappingURL=version.js.map
