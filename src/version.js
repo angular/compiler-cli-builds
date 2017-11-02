6f50330ea32abbde8ea1132fd77f01135166826f
Victor Berchet <victor@suumit.com>
2017-11-02 22:22:52 +0000

    d156e72 docs:  Add 0 in padding of 'dd' (#20107)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-17ed14f');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-d156e72');
 //# sourceMappingURL=version.js.map
