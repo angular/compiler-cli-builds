714b27913027367757a5cad4333c85883f96c433
Victor Berchet <victor@suumit.com>
2017-11-02 23:49:34 +0000

    dcf8840 feat(elements): implement `registerAsCustomElements()`

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-a28d616');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-dcf8840');
 //# sourceMappingURL=version.js.map
