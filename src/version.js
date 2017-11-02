26af27a94e6f6e985a5b3ce066a478c840ab6e31
Victor Berchet <victor@suumit.com>
2017-11-02 21:56:29 +0000

    7186c9c docs: AOT guide for CLI #19510 (#19818)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-a91252a');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-7186c9c');
 //# sourceMappingURL=version.js.map
