af8961abe751b0d3d7380ce4c729a39216bdcdb7
Victor Berchet <victor@suumit.com>
2017-11-03 19:31:06 +0000

    1d19d61 fix(platform-browser): support Symbols in custom jasmineToString() method (#19794)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-03f080b');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-1d19d61');
 //# sourceMappingURL=version.js.map
