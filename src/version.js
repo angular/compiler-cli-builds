5508aa9dbe8a0aed31756a95d2800d6f58fbecf7
Victor Berchet <victor@suumit.com>
2017-11-03 22:41:07 +0000

    200d92d fix(core): should support event.stopImmediatePropagation (#19222)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-dbec3ca');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-200d92d');
 //# sourceMappingURL=version.js.map
