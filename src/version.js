25af936f885b30bf4a3f30046203603896ac6f9a
Victor Berchet <victor@suumit.com>
2017-11-03 19:48:50 +0000

    faa6212 feat(core): add source to `StaticInjectorError` message (#19482)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-567cc26');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-faa6212');
 //# sourceMappingURL=version.js.map
