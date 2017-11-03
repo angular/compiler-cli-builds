8aab6e6d94cae119be9ccec3edb237992c1aa3cc
Victor Berchet <victor@suumit.com>
2017-11-03 22:40:14 +0000

    dbec3ca docs(aio): update pipes for CLI (#19553)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-f7c9b94');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-dbec3ca');
 //# sourceMappingURL=version.js.map
