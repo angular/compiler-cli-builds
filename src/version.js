d8cbc891fbf980a63529b094a20564553672f229
Victor Berchet <victor@suumit.com>
2017-11-03 19:32:20 +0000

    169cedd docs: Bootstrapping guide prose for CLI (#19777)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-1d19d61');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-169cedd');
 //# sourceMappingURL=version.js.map
