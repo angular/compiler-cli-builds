5ac2dcbe2b9682203ae0836bc349b6b4663b9c1c
Victor Berchet <victor@suumit.com>
2017-11-03 19:34:34 +0000

    567cc26 docs: Component Styles guide for CLI (#19791)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-169cedd');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-567cc26');
 //# sourceMappingURL=version.js.map
