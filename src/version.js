ac4ddd0eaf39c4062f055e13368d4ab545b30783
Victor Berchet <victor@suumit.com>
2017-11-03 19:57:08 +0000

    e4cd3b0 test(aio): make e2e tests less flakey

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-9811bef');
+exports.VERSION = new compiler_1.Version('5.0.0-e4cd3b0');
 //# sourceMappingURL=version.js.map
