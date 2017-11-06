d4e0ff963376e793f0ff94e7755084a88f7e4c5f
Victor Berchet <victor@suumit.com>
2017-11-06 18:47:14 +0000

    1f1741c build(aio): upgrade to @anglar/core@5.0.0 and cli@1.5.0

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-3ab0963');
+exports.VERSION = new compiler_1.Version('5.0.0-1f1741c');
 //# sourceMappingURL=version.js.map
