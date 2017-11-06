5425802ddc5fafbc1636a253fe13fed347751aa8
Victor Berchet <victor@suumit.com>
2017-11-06 18:24:11 +0000

    8802016 fix(aio): style correctly on Safari (#20157)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-8b50ed0');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-8802016');
 //# sourceMappingURL=version.js.map
