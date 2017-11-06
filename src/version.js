11bfcd948ff9cc0e01522d6c54f8c6d11a1c5ebd
Victor Berchet <victor@suumit.com>
2017-11-06 18:37:31 +0000

    3ab0963 docs(aio): update ToH for CLI (#19811)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-7083791');
+exports.VERSION = new compiler_1.Version('5.0.0-3ab0963');
 //# sourceMappingURL=version.js.map
