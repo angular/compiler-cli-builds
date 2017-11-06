7418369d2eff4af8d97eb2862ab587a1c642d79a
Victor Berchet <victor@suumit.com>
2017-11-06 19:50:47 +0000

    b489259 docs(common): fix mis-ordered lines (#20221)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-6b74883');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-b489259');
 //# sourceMappingURL=version.js.map
