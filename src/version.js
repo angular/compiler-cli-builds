23c6835d4377db1f116bac018fa8aff3761c3249
Victor Berchet <victor@suumit.com>
2017-11-06 22:54:32 +0000

    6e8e3bd refactor(core): misc changes and integrate review feedback on #19996

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-b489259');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-6e8e3bd');
 //# sourceMappingURL=version.js.map
