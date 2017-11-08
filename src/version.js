66ca5e38457bd090b599f0d6f3816c2c61c9740e
Victor Berchet <victor@suumit.com>
2017-11-08 19:23:39 +0000

    9de45fa docs: add changelog for 5.0.1

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.0-511e56d');
+exports.VERSION = new compiler_1.Version('5.0.1-9de45fa');
 //# sourceMappingURL=version.js.map
