cb4a928bc58098d43ffd4391f6028879ebaf062a
Victor Berchet <victor@suumit.com>
2017-11-09 00:14:43 +0000

    9bcd709 test(compiler): do not use `a` as a content selector

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-78ba39b');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-9bcd709');
 //# sourceMappingURL=version.js.map
