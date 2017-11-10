2e8fd3954dbccd4211da22509c866a2bf98c9277
Jason Aden <jasonaden@google.com>
2017-11-10 19:35:05 +0000

    fb4b90a docs(aio): typo fix (#20318)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-7830d74');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-fb4b90a');
 //# sourceMappingURL=version.js.map
