b1aabd012de1786ae12ae2e597d235dd59270be2
Jason Aden <jasonaden@google.com>
2017-11-10 23:05:25 +0000

    612f508 refactor(animations): uses a loop instead Array.map() which creates and (#19910) returns a new array that is discarded.

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.1-aa3d75b');
+exports.VERSION = new compiler_1.Version('5.0.1-612f508');
 //# sourceMappingURL=version.js.map
