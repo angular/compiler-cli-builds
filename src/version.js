295f8e4b21c188ce8a2b94416841c4f94acd8bd3
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:01 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.1-28985cb');
+exports.VERSION = new compiler_1.Version('5.0.1-424a323');
 //# sourceMappingURL=version.js.map
