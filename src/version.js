3b0f4357aad649734c21bd7f5f27ea968e564b7f
Jason Aden <jasonaden@google.com>
2017-11-10 22:14:19 +0000

    161f88f build: update to google-closure-compiler@20171023.0.1 (#20321)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-fb4b90a');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-161f88f');
 //# sourceMappingURL=version.js.map
