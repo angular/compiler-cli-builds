2005a162eec066350d9ec6edf00346619339aa14
Jason Aden <jasonaden@google.com>
2017-11-10 19:33:19 +0000

    2aefac8 docs(aio): empty line between HTML tag and content (#20341) PR Close #20341

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.1-424a323');
+exports.VERSION = new compiler_1.Version('5.0.1-2aefac8');
 //# sourceMappingURL=version.js.map
