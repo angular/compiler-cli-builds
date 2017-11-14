49641d2dffd5e9841cd79f89c47adf1674644118
Jason Aden <jasonaden@google.com>
2017-11-14 18:26:39 +0000

    7acbc19 docs(aio): fix broken link in guide/component-interaction (#20411)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.1-612f508');
+exports.VERSION = new compiler_1.Version('5.0.1-7acbc19');
 //# sourceMappingURL=version.js.map
