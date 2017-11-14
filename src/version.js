188aaa712a315e72e2300ec78e5b6ce3670c1cf2
Jason Aden <jasonaden@google.com>
2017-11-14 22:05:57 +0000

    12af6d3 docs(core): change from deprecated renderer to renderer2 (#19920)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-b9bd320');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-12af6d3');
 //# sourceMappingURL=version.js.map
