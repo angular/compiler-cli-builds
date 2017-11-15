352912ef34426eefb61f43c37c171456d05bfb2a
Jason Aden <jasonaden@google.com>
2017-11-15 20:20:41 +0000

    043e408 style(core): fix comment format for linter

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.0-d47b2a6');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.0-043e408');
 //# sourceMappingURL=version.js.map
