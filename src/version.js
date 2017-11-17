d4f3182c13868d684896a13bf5ac0a3f1716b1d8
Miško Hevery <misko@hevery.com>
2017-11-17 22:09:36 +0000

    365712e docs(aio): fix typo in Attribute Directives documentation (#20143)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-1861e41');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-365712e');
 //# sourceMappingURL=version.js.map
