9d7992a59066db2db965afb5e42d2db52b2e4668
Miško Hevery <misko@hevery.com>
2017-11-18 00:37:07 +0000

    69c53c3 fix(forms): updateOn should check if change occurred (#20358)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-365712e');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-69c53c3');
 //# sourceMappingURL=version.js.map
