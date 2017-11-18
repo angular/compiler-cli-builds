2b625c9393b9974b945b2fb34c56301c09210abd
Miško Hevery <misko@hevery.com>
2017-11-18 00:42:20 +0000

    82aace6 fix(core): should support event.stopImmediatePropagation (#20469)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.2-b1f8eb1');
+exports.VERSION = new compiler_1.Version('5.0.2-82aace6');
 //# sourceMappingURL=version.js.map
