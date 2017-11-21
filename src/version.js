0f549208c6bad9de8aaf9c7942caf63da5ec6d24
Miško Hevery <misko@hevery.com>
2017-11-21 17:07:40 +0000

    b53ead4 fix(compiler): support event bindings in `fullTemplateTypeCheck` (#20490)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.2-82aace6');
+exports.VERSION = new compiler_1.Version('5.0.2-b53ead4');
 //# sourceMappingURL=version.js.map
