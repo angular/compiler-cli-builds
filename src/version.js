cd2d20282432dfbb22f4dbff67d1eb89fcd41e61
Miško Hevery <misko@hevery.com>
2017-11-22 04:44:50 +0000

    7cf5e95 ci: update pullapprove (#20540)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-2a9d2ba');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-7cf5e95');
 //# sourceMappingURL=version.js.map
