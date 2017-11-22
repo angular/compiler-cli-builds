58a2562ed5f7e7194fce97a9055573ca5399526c
Miško Hevery <misko@hevery.com>
2017-11-22 20:12:17 +0000

    0444e13 docs(aio): add ngATL header (#20590)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-ffb6dbe');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-0444e13');
 //# sourceMappingURL=version.js.map
