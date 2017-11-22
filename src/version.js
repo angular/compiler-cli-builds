b72f41a08290f35e4b862e6139ea13d4f8c20e6b
Miško Hevery <misko@hevery.com>
2017-11-22 15:13:41 +0000

    ac93f12 ci: Update 1% payload size test (#20524)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-7cf5e95');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-ac93f12');
 //# sourceMappingURL=version.js.map
