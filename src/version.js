67fe0128b91cacee880358d858f9d63f7528cf83
Miško Hevery <misko@hevery.com>
2017-11-22 17:10:41 +0000

    efce396 docs(aio): add a French onsite training ressource (#19889) PR Close #19889

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.2-b53ead4');
+exports.VERSION = new compiler_1.Version('5.0.2-efce396');
 //# sourceMappingURL=version.js.map
