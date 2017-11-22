f3e9c80dc4fb24dc38618bdf86a633fbf671a8eb
Miško Hevery <misko@hevery.com>
2017-11-22 20:09:33 +0000

    ffb6dbe fix(animations): always fire start and done callbacks in order for noop animations (#20570)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.1-0e012c9');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.1-ffb6dbe');
 //# sourceMappingURL=version.js.map
