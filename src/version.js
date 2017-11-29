4c4ea90d944cdf9cea16bd704f08ed9fcdbd2ad8
Miško Hevery <misko@hevery.com>
2017-11-29 22:34:08 +0000

    ba850b3 Revert "fix(core): should use native addEventListener in ngZone (#20672)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.2-65a2cb8');
+exports.VERSION = new compiler_1.Version('5.1.0-beta.2-ba850b3');
 //# sourceMappingURL=version.js.map
