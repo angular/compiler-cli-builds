4f7964ad23cc7b39f6df60f3a16f9c8d763dcd5e
Misko Hevery <misko@hevery.com>
2017-11-30 04:50:39 +0000

    9e69d97 style: broken build due to missing new lines

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.3-b450c83');
+exports.VERSION = new compiler_1.Version('5.0.3-9e69d97');
 //# sourceMappingURL=version.js.map
