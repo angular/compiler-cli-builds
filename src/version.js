6a939efe3e2c1e07246014a4d347dc8098bb1318
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:10:15 +0000

    d507057 docs: add changelog for 5.0.5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.0-f582620');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.0-d507057');
 //# sourceMappingURL=version.js.map
