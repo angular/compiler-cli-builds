5cc36729c49f6c7bf548c2a68d52b481392c54b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:41:06 +0000

    b9a91a5 fix(service-worker): don't crash if SW not supported

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.0-60a3081');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.0-b9a91a5');
 //# sourceMappingURL=version.js.map
