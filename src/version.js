ee9ff02d3ab4e4435fcbbe03898b2ede25138760
Misko Hevery <misko@hevery.com>
2017-12-01 06:15:14 +0000

    60a3081 docs: add changelog for 5.1.0-rc.0

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-beta.2-47addd1');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.0-60a3081');
 //# sourceMappingURL=version.js.map
