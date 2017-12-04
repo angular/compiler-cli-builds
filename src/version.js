6667fd66b3a393a48dbdb24f13c04ca2dcd2a96d
Igor Minar <igor@angularjs.org>
2017-12-04 23:15:47 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.0-d507057');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.1-7e7ff2e');
 //# sourceMappingURL=version.js.map
