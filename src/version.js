b4d97b8e978c6e9b3007e5ed369be4da50f3900f
Igor Minar <igor@angularjs.org>
2017-12-05 00:35:21 +0000

    6911a25 build: set `preserveWhitespaces` to false by default on Bazel (#20783)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.1-7e7ff2e');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.1-6911a25');
 //# sourceMappingURL=version.js.map
