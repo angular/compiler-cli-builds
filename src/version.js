7a7a8343e2c82178c31247f13820561d57debac1
Igor Minar <igor@angular.io>
2017-12-06 20:59:30 +0000

    c2dbc55 release: cut the 5.1.0 release

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.1-b78ada1');
+exports.VERSION = new compiler_1.Version('5.1.0-c2dbc55');
 //# sourceMappingURL=version.js.map
