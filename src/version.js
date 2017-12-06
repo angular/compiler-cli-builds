86d2dac5ca88f91c9bb97dea24d191417d2312fd
Igor Minar <igor@angularjs.org>
2017-12-06 15:30:48 +0000

    eb57bc3 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.0.5-d824cc2');
+exports.VERSION = new compiler_1.Version('5.0.5-eb57bc3');
 //# sourceMappingURL=version.js.map
