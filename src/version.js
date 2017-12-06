f9d32b239fe0c1f6caca4fd17314e176c1931f87
Igor Minar <igor@angularjs.org>
2017-12-06 15:05:29 +0000

    be99449 docs(core): add docs for i18n tokens (#17920)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-rc.1-6911a25');
+exports.VERSION = new compiler_1.Version('5.1.0-rc.1-be99449');
 //# sourceMappingURL=version.js.map
