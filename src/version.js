04e1b5f61d937d49ba1f3a942b84addc02fad58e
Jason Aden <jasonaden@google.com>
2017-12-07 21:53:14 +0000

    ef534c0 build: upgrade bazel rules to latest (#20768)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-073f485');
+exports.VERSION = new compiler_1.Version('5.1.0-ef534c0');
 //# sourceMappingURL=version.js.map
