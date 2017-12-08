b3186742a93de5b5d4fcf7b391e004ea97f5505b
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:01 +0000

    baeec4d fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-9dd60a5');
+exports.VERSION = new compiler_1.Version('5.1.0-baeec4d');
 //# sourceMappingURL=version.js.map
