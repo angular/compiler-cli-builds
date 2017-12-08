f8f911fe414d99dbc6e3c7ab30cda83d0c1449ef
Jason Aden <jasonaden@google.com>
2017-12-08 18:25:09 +0000

    53b9de3 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-e6a2805');
+exports.VERSION = new compiler_1.Version('5.1.0-53b9de3');
 //# sourceMappingURL=version.js.map
