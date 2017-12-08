fbc3ba8b763baa1b05a37d5f21f293150f9affb1
Jason Aden <jasonaden@google.com>
2017-12-08 23:20:21 +0000

    13e663c fix(animations): ensure multi-level route leave animations are queryable (#20787)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-3ce3b4d');
+exports.VERSION = new compiler_1.Version('5.1.0-13e663c');
 //# sourceMappingURL=version.js.map
