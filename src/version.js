94de6c8a56a167bc883db7bded03e73d5769ac1d
Jason Aden <jasonaden@google.com>
2017-12-08 18:22:06 +0000

    e6a2805 fix(animations): properly recover and cleanup DOM when CD failures occur (#20719)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-baeec4d');
+exports.VERSION = new compiler_1.Version('5.1.0-e6a2805');
 //# sourceMappingURL=version.js.map
