a731a1bc20a0f85dc538ed4282d8365634fcb0f9
Jason Aden <jasonaden@google.com>
2017-12-08 20:31:50 +0000

    d713225 refactor(common): update i18n locale data to CLDR v32 (#20830)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-affa54d');
+exports.VERSION = new compiler_1.Version('5.1.0-d713225');
 //# sourceMappingURL=version.js.map
