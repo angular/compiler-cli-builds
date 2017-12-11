abb65e7df76b8bc895d75bfe456141f2e8c1da6d
Jason Aden <jasonaden@google.com>
2017-12-11 23:59:23 +0000

    8bcb093 build: update node version number in .nvmrc (#20832)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-9fbf850');
+exports.VERSION = new compiler_1.Version('5.1.0-8bcb093');
 //# sourceMappingURL=version.js.map
