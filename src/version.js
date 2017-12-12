54e86ae4d7eb2920515fddca9d12f3922f9d615a
Jason Aden <jasonaden@google.com>
2017-12-12 00:28:09 +0000

    add3589 ci: use container version in cache key (#20952)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-81d497c');
+exports.VERSION = new compiler_1.Version('5.1.0-add3589');
 //# sourceMappingURL=version.js.map
