63a269b3394d580360e812376c11ce9297759aca
Jason Aden <jasonaden@google.com>
2017-12-13 17:59:29 +0000

    b3eb1db build: update node version number in .nvmrc (#20832)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-8c52088');
+exports.VERSION = new compiler_1.Version('5.1.0-b3eb1db');
 //# sourceMappingURL=version.js.map
