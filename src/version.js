c43cdb45183a4e386b78b40a8194777d057d1d17
GitHub <noreply@github.com>
2017-12-13 22:17:47 +0000

    57e0535 Merge branch 'master' into alexeagle-patch-7

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-7fd441e');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-57e0535');
 //# sourceMappingURL=version.js.map
