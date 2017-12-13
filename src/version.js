28858ea3e7bf94a619232d72a4522d43db6bd4da
Jason Aden <jasonaden@google.com>
2017-12-13 18:02:23 +0000

    d6da798 fix(compiler): support referencing enums in namespaces (#20947)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-be9a737');
+exports.VERSION = new compiler_1.Version('5.1.0-d6da798');
 //# sourceMappingURL=version.js.map
