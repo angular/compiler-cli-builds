24e414db080a69ef78572878358df35d8ccbec20
Jason Aden <jasonaden@google.com>
2017-12-13 19:55:46 +0000

    e3140ae docs: add changelog for 5.1.1

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-d6da798');
+exports.VERSION = new compiler_1.Version('5.1.1-e3140ae');
 //# sourceMappingURL=version.js.map
