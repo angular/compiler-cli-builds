f012cbbc105470cb4eb43843b672f89777cd8eca
GitHub <noreply@github.com>
2017-12-13 19:43:28 +0000

    7fd441e ci: fix invalid bazel query expression

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.0-c8010f4');
+exports.VERSION = new compiler_1.Version('5.1.0-7fd441e');
 //# sourceMappingURL=version.js.map
