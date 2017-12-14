74a8da8915bcba7a00fb2ec2ed4d96eb1b9177e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 17:07:26 +0000

    b89e7c2 ci(aio): move e2e tests to non-optional job

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-cbe7e39');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-b89e7c2');
 //# sourceMappingURL=version.js.map
