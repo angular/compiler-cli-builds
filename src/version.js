91480bdde07ea713c402d4f805125c7771a83452
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:27:25 +0000

    a474276 test(aio): correct usage of fakeAsync and inject together in test

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-d91ff17');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-a474276');
 //# sourceMappingURL=version.js.map
