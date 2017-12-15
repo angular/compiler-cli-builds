411d98054d8e97c64f9a303ba12c90c72a0de468
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:13:21 +0000

    05ff6c0 fix(compiler): make tsx file aot compatible

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-2e7e935');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-05ff6c0');
 //# sourceMappingURL=version.js.map
