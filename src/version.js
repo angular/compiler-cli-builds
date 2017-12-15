fdb2cf0deeb3de480d842f7fe4efc88fe5ee3bd9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:10:14 +0000

    2e7e935 fix(common): fix a Closure compilation issue.

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-b89e7c2');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-2e7e935');
 //# sourceMappingURL=version.js.map
