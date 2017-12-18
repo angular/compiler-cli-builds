067fef1346966d33b95b9b687ceb8ece1b380be0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-18 20:30:25 +0000

    0575135 fix(upgrade): replaces get/setAngularLib with get/setAngularJSGlobal

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-3846f19');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-0575135');
 //# sourceMappingURL=version.js.map
