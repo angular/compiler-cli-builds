1f02f676d0c106fd9287257183adb7e6ea16435a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-18 17:03:50 +0000

    e48f477 ci: temporarily remove wardbell from pullapprove (2FA)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-20e1cc0');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-e48f477');
 //# sourceMappingURL=version.js.map
