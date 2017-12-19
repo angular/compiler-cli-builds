e94c609c4635b2546e56d0db94a227f4e5b3998d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-19 19:31:29 +0000

    f593552 docs(aio): Rename service worker files, update examples, move service worker under Techniques

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-8458647');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-f593552');
 //# sourceMappingURL=version.js.map
