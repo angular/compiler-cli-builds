1ba200faa2f1bbbdd51e80c7571fd85d9c97a1be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-19 19:05:03 +0000

    8ceffd8 fix(aio): improve transitions between pages

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.0-74e3115');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.0-8ceffd8');
 //# sourceMappingURL=version.js.map
