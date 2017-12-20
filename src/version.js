78945e60456bee11fcaf4d5bd1f2d829fe621dfc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 20:24:46 +0000

    a36dfd4 ci(router): update the public API guard for the router

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.1-57bed3f');
+exports.VERSION = new compiler_1.Version('5.1.1-a36dfd4');
 //# sourceMappingURL=version.js.map
