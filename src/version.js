ef7f4e4cd5ae20cb56201b69a57a38f825791348
Igor Minar <igor@angularjs.org>
2017-12-21 22:23:59 +0000

    4337768 build: fix pullapprove (#21140)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-dc53248');
+exports.VERSION = new compiler_1.Version('5.1.2-4337768');
 //# sourceMappingURL=version.js.map
