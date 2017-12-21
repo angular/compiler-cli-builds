00b4a6eb5c4fa3ea2c35e0bd109677f75102b28d
Igor Minar <igor@angularjs.org>
2017-12-21 21:35:23 +0000

    0487a9f build: update pullapprove: (#21117)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.1-871ece6');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.1-0487a9f');
 //# sourceMappingURL=version.js.map
