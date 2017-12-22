f53f0ed8024594a57452c55dc1ac63f2ac370b89
Igor Minar <igor@angular.io>
2017-12-22 23:47:29 +0000

    83c1383 build: upgrade circle/build into large class (more ram) (#21053)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.1-86a36ea');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.1-83c1383');
 //# sourceMappingURL=version.js.map
