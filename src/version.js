a14a1a27dce44678785f12643589b84174cf6ce6
Igor Minar <igor@angularjs.org>
2017-12-22 17:43:17 +0000

    3e34fa8 fix(animations): avoid infinite loop with multiple blocked sub triggers (#21119)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-8c99175');
+exports.VERSION = new compiler_1.Version('5.1.2-3e34fa8');
 //# sourceMappingURL=version.js.map
