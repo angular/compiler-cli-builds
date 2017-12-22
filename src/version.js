a2c62b75db7ff44135f42ca3ed9cdf1f56418123
Igor Minar <igor@angularjs.org>
2017-12-22 17:22:28 +0000

    fa0e8ef fix(common): handle JS floating point errors in percent pipe (#20329)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-0aa6341');
+exports.VERSION = new compiler_1.Version('5.1.2-fa0e8ef');
 //# sourceMappingURL=version.js.map
