0754dd18e2e23d12bbc837d5cd1958840b1d2cea
Igor Minar <igor@angularjs.org>
2017-12-22 07:00:48 +0000

    5a7bf36 build: fix circular dep between interface and l_node by merging (#20855)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.1-6a58184');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.1-5a7bf36');
 //# sourceMappingURL=version.js.map
