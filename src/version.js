780cc787664eab5afab63c22e5b558f62428994b
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:59 +0000

    6a58184 build: make umd.min.js source map paths relative (#21147)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.1-bc66d27');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.1-6a58184');
 //# sourceMappingURL=version.js.map
