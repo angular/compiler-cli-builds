854d2aa85cd26829129ad5c3aadbd6edc604734f
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:16 +0000

    41abcc3 docs(service-worker): fix word wrap (#21114)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-4337768');
+exports.VERSION = new compiler_1.Version('5.1.2-41abcc3');
 //# sourceMappingURL=version.js.map
