98d935b9740ca564b356038b79fb3b4a326eba19
Igor Minar <igor@angular.io>
2017-12-23 05:56:46 +0000

    5de91fe docs(forms): add text about min() and max() as functions (#21110)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-d787b55');
+exports.VERSION = new compiler_1.Version('5.1.2-5de91fe');
 //# sourceMappingURL=version.js.map
