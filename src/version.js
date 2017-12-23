408f03fd5dfd379c01371257754a60408d368925
Igor Minar <igor@angular.io>
2017-12-23 05:58:21 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-5de91fe');
+exports.VERSION = new compiler_1.Version('5.1.2-12702b2');
 //# sourceMappingURL=version.js.map
