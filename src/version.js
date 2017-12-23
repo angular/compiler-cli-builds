31faa25644fb427f704281cbd74f0d1dafff186e
Igor Minar <igor@angular.io>
2017-12-23 05:54:49 +0000

    d787b55 build(common): generate ts declarations for i18n locale files (#21127)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-3e34fa8');
+exports.VERSION = new compiler_1.Version('5.1.2-d787b55');
 //# sourceMappingURL=version.js.map
