af76b654d686a2b1538e012582e212ef84a23226
Miško Hevery <misko@hevery.com>
2018-01-02 19:02:48 +0000

    3e47ea2 fix(language-service): ignore null metadatas (#20557)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-beta.1-bc7a6d7');
+exports.VERSION = new compiler_1.Version('5.2.0-beta.1-3e47ea2');
 //# sourceMappingURL=version.js.map
