37019a039461dc5e4da2ba1d133c15d57cab566f
Miško Hevery <misko@hevery.com>
2018-01-02 22:25:26 +0000

    8006be8 docs(aio): fix trailing space breaking lint

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-48a1f32');
+exports.VERSION = new compiler_1.Version('5.1.2-8006be8');
 //# sourceMappingURL=version.js.map
