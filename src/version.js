4d6d1354e6a2ba51b4b116b8271f6751b1517d94
Misko Hevery <misko@hevery.com>
2018-01-03 22:31:40 +0000

    00308fc build: update rules_typescript in integration test

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-a7a7bd3');
+exports.VERSION = new compiler_1.Version('5.1.2-00308fc');
 //# sourceMappingURL=version.js.map
