5e109713df5cc4eebbdfe810032134391169d4ca
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:12 +0000

    d138b38 docs: add changelog for 5.1.3

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.2-00308fc');
+exports.VERSION = new compiler_1.Version('5.1.3-d138b38');
 //# sourceMappingURL=version.js.map
