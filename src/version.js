7f196e0dc85f974acdd2a16a0d43972bbeef5bda
Kara Erickson <karakara@google.com>
2018-01-04 23:00:12 +0000

    dcc3eba fix(aio): fix test message's mistake. (#21254)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-6588ca8');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-dcc3eba');
 //# sourceMappingURL=version.js.map
