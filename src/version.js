4ffc53876366694ada0d907065d379cb66317305
Kara Erickson <karakara@google.com>
2018-01-04 01:28:47 +0000

    fc217f5 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-d138b38');
+exports.VERSION = new compiler_1.Version('5.1.3-fc217f5');
 //# sourceMappingURL=version.js.map
