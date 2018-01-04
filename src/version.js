f8c62bc09d74971ac26eff91303350c26bd8a89d
Kara Erickson <karakara@google.com>
2018-01-04 23:38:38 +0000

    d3c0425 docs(router): fix missing export in docs for UrlMatcher (#21095)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-5efb751');
+exports.VERSION = new compiler_1.Version('5.1.3-d3c0425');
 //# sourceMappingURL=version.js.map
