f29606ea5ab8b41e0299d742ca510b8f9bf82c08
Kara Erickson <karakara@google.com>
2018-01-04 22:38:35 +0000

    6588ca8 fix(aio): do not redirect pages on "next" deployment (#21027)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-81823e8');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-6588ca8');
 //# sourceMappingURL=version.js.map
