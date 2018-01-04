4a2a3be801317440bdec7431a7875375b67b0577
Kara Erickson <karakara@google.com>
2018-01-04 23:53:14 +0000

    03d16fc docs(aio): add missing imports to interceptor example (#21259)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-b5934fc');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-03d16fc');
 //# sourceMappingURL=version.js.map
