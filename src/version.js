2e176c81ebee0fc8acc93a081e022692b55de82c
Kara Erickson <karakara@google.com>
2018-01-04 20:24:27 +0000

    ef78538 fix(aio): don't use Object.keys on NamedNodeMap to prevent SEO errors (#21305)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-ccea372');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-ef78538');
 //# sourceMappingURL=version.js.map
