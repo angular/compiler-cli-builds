3ff2f75f277b90212cd111baa16edbfd818c8832
Kara Erickson <karakara@google.com>
2018-01-04 19:40:11 +0000

    ccea372 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-cdc66f6');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-ccea372');
 //# sourceMappingURL=version.js.map
