dcec0727b80342f0104421f8d9980841fbb0f0ea
Kara Erickson <karakara@google.com>
2018-01-05 23:46:00 +0000

    8f3ea33 build(core): update payload size of hello-world in patch branch (#21343)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-839d24e');
+exports.VERSION = new compiler_1.Version('5.1.3-8f3ea33');
 //# sourceMappingURL=version.js.map
