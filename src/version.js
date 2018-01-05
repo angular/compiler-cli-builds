f46b3708479df99a6d5036bbdfaf1ae9ba8d4ad7
Kara Erickson <karakara@google.com>
2018-01-05 21:43:24 +0000

    f9436f7 docs(aio): delete duplicated word (#21129)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-e726d81');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-f9436f7');
 //# sourceMappingURL=version.js.map
