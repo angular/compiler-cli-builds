c4aa309ed06383532628c69c5a5ca2440c3ee3b4
Kara Erickson <karakara@google.com>
2018-01-05 20:16:16 +0000

    24df08e build(aio): fix plunker for custom plunkers (#21291)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-4f1a571');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-24df08e');
 //# sourceMappingURL=version.js.map
