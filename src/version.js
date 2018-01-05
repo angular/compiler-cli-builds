8eb7863f0b634b19327af36b4f2db7b8d5152a64
Kara Erickson <karakara@google.com>
2018-01-05 19:53:45 +0000

    c809545 test(aio): wait for Angular to avoid flake in e2e test (#21047)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-d3c0425');
+exports.VERSION = new compiler_1.Version('5.1.3-c809545');
 //# sourceMappingURL=version.js.map
