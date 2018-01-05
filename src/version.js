f15d92f550415fdc9a657079a301edad5f19fb68
Kara Erickson <karakara@google.com>
2018-01-05 20:24:28 +0000

    6cd4b1b build(aio): add `@nodoc` alias to the `@internal` tag (#21024)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-24df08e');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-6cd4b1b');
 //# sourceMappingURL=version.js.map
