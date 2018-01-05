8c826cde3ddb9ec48958d6c3f4633f1c3f395235
Kara Erickson <karakara@google.com>
2018-01-05 20:16:30 +0000

    8dd93f8 docs: remove broken issuestats.com badges (#21334) PR Close #21334

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-17e85cd');
+exports.VERSION = new compiler_1.Version('5.1.3-8dd93f8');
 //# sourceMappingURL=version.js.map
