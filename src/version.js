0a0022eab1228c85d160f972d927f02af7ba7eae
Kara Erickson <karakara@google.com>
2018-01-05 22:45:35 +0000

    fecf768 fix(common): export currencies via `getCurrencySymbol` (#20983)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-2fc4cf6');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-fecf768');
 //# sourceMappingURL=version.js.map
