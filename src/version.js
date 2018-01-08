ab0b1f58235d2d6e6f3705a71c1ffd8ee975f9eb
Kara Erickson <karakara@google.com>
2018-01-08 20:30:41 +0000

    4f7c13d build(core): reduce payload limit back to normal (#21395)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-4947454');
+exports.VERSION = new compiler_1.Version('5.1.3-4f7c13d');
 //# sourceMappingURL=version.js.map
