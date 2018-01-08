2f798c31c988f00ae597f5b5fbfd37d25f8d36ff
Kara Erickson <karakara@google.com>
2018-01-08 21:27:38 +0000

    fda3815 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-4f7c13d');
+exports.VERSION = new compiler_1.Version('5.1.3-fda3815');
 //# sourceMappingURL=version.js.map
