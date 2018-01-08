4695d0adc598dcc95e3fd990e394985b22d3bd9e
Kara Erickson <karakara@google.com>
2018-01-08 21:29:21 +0000

    6040ee3 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-74dbf7b');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-6040ee3');
 //# sourceMappingURL=version.js.map
