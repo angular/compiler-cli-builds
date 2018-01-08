923fa1950ee36db5427f9b266d83df40d88bf243
Kara Erickson <karakara@google.com>
2018-01-08 19:32:59 +0000

    7269fe5 ci: reformat itegration/_payload-limits.json to be easier to read (#21344)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-e994752');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-7269fe5');
 //# sourceMappingURL=version.js.map
