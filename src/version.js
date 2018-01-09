7b2f08bdec220b8ea3e704c43aed89817fec9753
Kara Erickson <karakara@google.com>
2018-01-09 18:50:19 +0000

    39f81b2 build: upgrade Bazel to 0.9.0 (#21335)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-0602aaa');
+exports.VERSION = new compiler_1.Version('5.1.3-39f81b2');
 //# sourceMappingURL=version.js.map
