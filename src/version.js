a71e39df7b4b33aff4374d7d912843ac429c2206
Kara Erickson <karakara@google.com>
2018-01-09 00:36:00 +0000

    fa03ae1 fix(benchpress): work around missing events from Chrome 63 (#21396)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-27196b6');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-fa03ae1');
 //# sourceMappingURL=version.js.map
