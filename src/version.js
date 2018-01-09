78569a79ada61bc4353d48751a8ea5a426108419
Kara Erickson <karakara@google.com>
2018-01-09 19:13:50 +0000

    9728dce fix(bazel): Give correct module names for ES6 output (#21320)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-463e287');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-9728dce');
 //# sourceMappingURL=version.js.map
