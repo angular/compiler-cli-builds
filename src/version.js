5408ffcaf3715ed96f1bbdb1223fe74023faef52
Kara Erickson <karakara@google.com>
2018-01-09 20:43:17 +0000

    229b76c ci: temporary work-around for Travis issue (#21422)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-80a5b91');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-229b76c');
 //# sourceMappingURL=version.js.map
