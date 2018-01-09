2ac0825501720e2fbf0784d9596c8aa6debf17f0
Kara Erickson <karakara@google.com>
2018-01-09 22:36:51 +0000

    3db91ff feat(core): add ngAfterViewInit and ngAfterViewChecked support to render3 (#21266)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-229b76c');
+exports.VERSION = new compiler_1.Version('5.2.0-rc.0-3db91ff');
 //# sourceMappingURL=version.js.map
