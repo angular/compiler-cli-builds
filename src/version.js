88b08f69d44f212a81b5bd4c681a4fdb5b86b776
Kara Erickson <karakara@google.com>
2018-01-09 19:26:43 +0000

    7c34807 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.1.3-84f6b14');
+exports.VERSION = new compiler_1.Version('5.1.3-7c34807');
 //# sourceMappingURL=version.js.map
