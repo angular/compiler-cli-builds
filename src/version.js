17e6da67ef1cc2c405ff932fbfcd2cabc572a7f9
Alex Eagle <alexeagle@google.com>
2018-01-10 22:49:05 +0000

    7b463df fix(aio): preserve static Observable methods (#21351)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-650f5fb');
+exports.VERSION = new compiler_1.Version('5.2.0-7b463df');
 //# sourceMappingURL=version.js.map
