a38a905904c6dc32037f922079d68ba9385ce91c
Kara Erickson <karakara@google.com>
2018-01-10 19:32:22 +0000

    70e8802 docs(core): provide note for unit test changes with the `ng-star-inserted` className (#21450)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-04dd971');
+exports.VERSION = new compiler_1.Version('5.2.0-70e8802');
 //# sourceMappingURL=version.js.map
