5d4ca3561cc24f2d73505ac09f72cf58f0836dd0
Alex Eagle <alexeagle@google.com>
2018-01-10 21:08:54 +0000

    3804d20 docs(bazel): document an installation issue (#21401)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-0a5a878');
+exports.VERSION = new compiler_1.Version('5.2.0-3804d20');
 //# sourceMappingURL=version.js.map
