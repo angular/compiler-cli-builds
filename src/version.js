5ee0acb48a93db9189ae7a901d5b9f87062c3933
Alex Eagle <alexeagle@google.com>
2018-01-10 20:45:42 +0000

    a76ab1f docs(router): remove extra brackets (#21294)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-72246d4');
+exports.VERSION = new compiler_1.Version('5.2.0-a76ab1f');
 //# sourceMappingURL=version.js.map
