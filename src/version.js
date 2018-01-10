78f308c866f0f9942aa13a12e4a7692ff3c8caff
Alex Eagle <alexeagle@google.com>
2018-01-10 23:40:01 +0000

    aad1126 docs: update the release schedule with v6 info (#21435)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-7b463df');
+exports.VERSION = new compiler_1.Version('5.2.0-aad1126');
 //# sourceMappingURL=version.js.map
