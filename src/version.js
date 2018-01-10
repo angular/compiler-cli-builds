cd4c5cc1f823a1bd003db867063ac958517d3779
Alex Eagle <alexeagle@google.com>
2018-01-10 21:24:53 +0000

    c4f02e2 build: move repeated tsconfig attributes to a macro (#20964)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-f15ca6c');
+exports.VERSION = new compiler_1.Version('5.2.0-c4f02e2');
 //# sourceMappingURL=version.js.map
