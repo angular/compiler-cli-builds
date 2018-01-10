bb5e5932c633a288b108ba89fd39d076bccfa2f5
Alex Eagle <alexeagle@google.com>
2018-01-10 21:11:49 +0000

    f15ca6c fix(aio): support multibyte character in heading (#21414)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-0de219a');
+exports.VERSION = new compiler_1.Version('5.2.0-f15ca6c');
 //# sourceMappingURL=version.js.map
