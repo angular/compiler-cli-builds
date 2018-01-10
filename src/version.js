b521c7d48bcfbc727cbff5638cdda4814e548cd5
Alex Eagle <alexeagle@google.com>
2018-01-10 20:25:22 +0000

    d2aa8ac fix(animations): fix increment/decrement aliases example (#18323)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-ef956a2');
+exports.VERSION = new compiler_1.Version('5.2.0-d2aa8ac');
 //# sourceMappingURL=version.js.map
