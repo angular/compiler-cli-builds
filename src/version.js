2bf502c55efbef654fe44367bce7b2c00b1ebc86
Alex Eagle <alexeagle@google.com>
2018-01-10 20:58:21 +0000

    ff6047c build(aio): upgrade to uglify-es@3.3.5 (#21350)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-a76ab1f');
+exports.VERSION = new compiler_1.Version('5.2.0-ff6047c');
 //# sourceMappingURL=version.js.map
