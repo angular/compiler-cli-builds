8221304bb5c925e1829fa2960b2c9889c7ca5c66
Alex Eagle <alexeagle@google.com>
2018-01-10 21:49:53 +0000

    c32e833 fix(compiler): cache external reference resolution (#21359)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-7bdd9ae');
+exports.VERSION = new compiler_1.Version('5.2.0-c32e833');
 //# sourceMappingURL=version.js.map
