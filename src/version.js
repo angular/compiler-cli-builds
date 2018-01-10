7f325d4b199fc6007a5ded96a2ef60fff9f7564a
Kara Erickson <karakara@google.com>
2018-01-10 20:17:29 +0000

    fc3e7e0 refactor(core): create consistent naming scheme across classes (#21403)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-908129e');
+exports.VERSION = new compiler_1.Version('5.2.0-fc3e7e0');
 //# sourceMappingURL=version.js.map
