e469e2b3698d44fdb07dad3f7fdceb072961eeac
Alex Eagle <alexeagle@google.com>
2018-01-10 20:18:55 +0000

    ef956a2 build: add bazel rulse for benchmarks (#21436)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-fc3e7e0');
+exports.VERSION = new compiler_1.Version('5.2.0-ef956a2');
 //# sourceMappingURL=version.js.map
