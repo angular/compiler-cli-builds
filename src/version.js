3df639b2a7dcf1e65389b302e8ff3c0d5f29db9d
Alex Eagle <alexeagle@google.com>
2018-01-10 04:34:18 +0000

    a0166bd docs(bazel): document an installation issue

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-rc.0-0d1611f');
+exports.VERSION = new compiler_1.Version('5.2.0-a0166bd');
 //# sourceMappingURL=version.js.map
