92034e70b3c52f8e512c62d4ef01471f1e55ea0b
Alex Eagle <alexeagle@google.com>
2018-01-11 00:44:46 +0000

    456f57d docs: fix lazy-loading example (#20306)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-e64b1e9');
+exports.VERSION = new compiler_1.Version('5.2.0-456f57d');
 //# sourceMappingURL=version.js.map
