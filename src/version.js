799b4d903b497e17f96336d7ce7d5e46f62c9fe7
Alex Eagle <alexeagle@google.com>
2018-01-11 00:32:38 +0000

    6be9c04 refactor(core): split up interface files in render3 (#21433)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-a33ff2c');
+exports.VERSION = new compiler_1.Version('5.2.0-6be9c04');
 //# sourceMappingURL=version.js.map
