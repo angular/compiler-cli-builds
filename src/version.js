bdf5f6d4c64057e4134def520ea889f3b4f4290d
Alex Eagle <alexeagle@google.com>
2018-01-11 15:22:30 +0000

    00300f6 docs(forms): clarify note in reactive forms doc (#21134)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-5eaaac3');
+exports.VERSION = new compiler_1.Version('5.2.0-00300f6');
 //# sourceMappingURL=version.js.map
