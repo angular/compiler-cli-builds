2585ebd48211f13165a5939eca9f822790e1ba45
Alex Eagle <alexeagle@google.com>
2018-01-11 19:17:47 +0000

    3c6a506 fix(benchpress): should still support selenium_webdriver < 3.6.0 (#21477)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-b49d54e');
+exports.VERSION = new compiler_1.Version('5.2.0-3c6a506');
 //# sourceMappingURL=version.js.map
