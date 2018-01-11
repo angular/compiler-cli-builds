17efe2d54ac21b1656fa8587bc3f86c2ef94b8bb
Alex Eagle <alexeagle@google.com>
2018-01-11 15:04:49 +0000

    c0080d7 docs: fix release schedule date (#21469) PR Close #21469

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-c6eb9ee');
+exports.VERSION = new compiler_1.Version('5.2.0-c0080d7');
 //# sourceMappingURL=version.js.map
