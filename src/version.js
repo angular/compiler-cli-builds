acbd1a82851c6d2f0a09db81bf64ed3217072b88
Alex Eagle <alexeagle@google.com>
2018-01-11 01:25:13 +0000

    c6eb9ee build: update polyfill size (#21461)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-456f57d');
+exports.VERSION = new compiler_1.Version('5.2.0-c6eb9ee');
 //# sourceMappingURL=version.js.map
