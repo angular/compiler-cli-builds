b196b7b0c9c4fb5367b861cd839d60ed2a1ab8a8
Alex Eagle <alexeagle@google.com>
2018-01-11 15:21:08 +0000

    5eaaac3 refactor(ivy): remove `type` from `DirectiveDef` (#21374)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-c0080d7');
+exports.VERSION = new compiler_1.Version('5.2.0-5eaaac3');
 //# sourceMappingURL=version.js.map
