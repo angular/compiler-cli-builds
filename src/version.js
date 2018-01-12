8b0d2e903d8bfcbbfcdc0b9149daa20b42263e40
Alex Eagle <alexeagle@google.com>
2018-01-12 18:24:52 +0000

    9005a6f feat(aio): implement survey notification link (#21371)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-3c6a506');
+exports.VERSION = new compiler_1.Version('5.2.0-9005a6f');
 //# sourceMappingURL=version.js.map
