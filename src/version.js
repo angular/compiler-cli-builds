c3563a9d1f544beacaed0de623d065090a1a3fdb
Alex Eagle <alexeagle@google.com>
2018-01-12 23:03:39 +0000

    43e1520 fix(language-service): Clear caches when program changes (#21337)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-2d44a2a');
+exports.VERSION = new compiler_1.Version('5.2.0-43e1520');
 //# sourceMappingURL=version.js.map
