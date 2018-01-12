69e795bd48be4825804fc681a0c0c6ebdd4844ee
Alex Eagle <alexeagle@google.com>
2018-01-12 19:05:57 +0000

    9b5a485 perf(ivy): add missing dom element in render3_function tree benchmark (#21476)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-50ca01e');
+exports.VERSION = new compiler_1.Version('5.2.0-9b5a485');
 //# sourceMappingURL=version.js.map
