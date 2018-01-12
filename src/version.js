93594abd9dbbb288d6aebfaf09ba78a55e8abb09
Alex Eagle <alexeagle@google.com>
2018-01-12 22:55:58 +0000

    2d44a2a docs(compiler): document the $any type cast function (#20968)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-c5c6d84');
+exports.VERSION = new compiler_1.Version('5.2.0-2d44a2a');
 //# sourceMappingURL=version.js.map
