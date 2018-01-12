cc01b4e032c132700fa8a64955188b3016dd90bd
Alex Eagle <alexeagle@google.com>
2018-01-12 22:11:26 +0000

    eb70966 fix(aio): correctly handle redirects (#21416)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-cf4bea5');
+exports.VERSION = new compiler_1.Version('5.2.0-eb70966');
 //# sourceMappingURL=version.js.map
