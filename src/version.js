bf084fd7874b700cb06e809fda15a545f06b71a0
Alex Eagle <alexeagle@google.com>
2018-01-12 18:48:48 +0000

    50ca01e ci: update pullapprove rules now that we allow selfapprovals (#21494)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-63478d7');
+exports.VERSION = new compiler_1.Version('5.2.0-50ca01e');
 //# sourceMappingURL=version.js.map
