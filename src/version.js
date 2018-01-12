d126fb89ff3ba1a1b3709f74c1f4327c75daac66
Alex Eagle <alexeagle@google.com>
2018-01-12 18:49:29 +0000

    0e59d18 ci: add angular robot config file (#21489)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-54c8a32');
+exports.VERSION = new compiler_1.Version('5.2.0-0e59d18');
 //# sourceMappingURL=version.js.map
