bed3ccd7e9a7f6489c989c18ac897d3fadc0645c
Alex Eagle <alexeagle@google.com>
2018-01-12 21:55:16 +0000

    8e9cd57 fix(common): set correct timezone for ISO8601 dates in Safari (#21506)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-f23896f');
+exports.VERSION = new compiler_1.Version('5.2.0-8e9cd57');
 //# sourceMappingURL=version.js.map
