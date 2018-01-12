ebd6282f18c4aef66ce0e0b5ed699df0e209b3c0
Alex Eagle <alexeagle@google.com>
2018-01-12 22:19:59 +0000

    977978e test(common): make date pipe tests work in more timezones (#21379)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-eb70966');
+exports.VERSION = new compiler_1.Version('5.2.0-977978e');
 //# sourceMappingURL=version.js.map
