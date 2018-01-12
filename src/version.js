c269fee1918dcad3ee473e5b0f9f8dbaa57458ca
Alex Eagle <alexeagle@google.com>
2018-01-12 20:28:07 +0000

    bb62458 docs: fix release schedule date (#21474)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-cffa0fe');
+exports.VERSION = new compiler_1.Version('5.2.0-bb62458');
 //# sourceMappingURL=version.js.map
