9da5e006ac44a32c3751adebd907da5d5e73672f
Alex Eagle <alexeagle@google.com>
2018-01-12 22:04:22 +0000

    cf4bea5 ci(aio): run e2e tests in `production` mode (#21470)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-8e9cd57');
+exports.VERSION = new compiler_1.Version('5.2.0-cf4bea5');
 //# sourceMappingURL=version.js.map
