6b5a6668f7ebd56ef3807e50ac35839ebc5ba628
Alex Eagle <alexeagle@google.com>
2018-01-12 23:20:12 +0000

    7c414fc ci: disable integration/cli-hello-world test (#21492)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-43e1520');
+exports.VERSION = new compiler_1.Version('5.2.0-7c414fc');
 //# sourceMappingURL=version.js.map
