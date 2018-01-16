9557d764bfb25c374c4a9e8f27aa157116538347
Alex Eagle <alexeagle@google.com>
2018-01-16 15:30:53 +0000

    a771ee5 build(aio): move file cleaning to later in the doc gen (#21540)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-a4cbe35');
+exports.VERSION = new compiler_1.Version('5.2.0-a771ee5');
 //# sourceMappingURL=version.js.map
