0bcee60a0d94e90a76f716997ccee8a57bad5a76
Alex Eagle <alexeagle@google.com>
2018-01-17 17:27:59 +0000

    420f5c4 build(bazel): remove spurious file (#21455)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-b773a4a');
+exports.VERSION = new compiler_1.Version('5.2.0-420f5c4');
 //# sourceMappingURL=version.js.map
