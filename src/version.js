feecdaa4d9ef8a7f22df594e23f0359f9f4b9e8f
Alex Eagle <alexeagle@google.com>
2018-01-17 15:34:57 +0000

    54e7576 fix(core): fix chained http call (#20924)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-d3333f0');
+exports.VERSION = new compiler_1.Version('5.2.0-54e7576');
 //# sourceMappingURL=version.js.map
