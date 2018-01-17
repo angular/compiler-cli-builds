9df31652233a47b108bb9652359dec39e16bba06
Alex Eagle <alexeagle@google.com>
2018-01-17 18:23:58 +0000

    af4eb00 docs: add changelog for 5.2.1

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-420f5c4');
+exports.VERSION = new compiler_1.Version('5.2.1-af4eb00');
 //# sourceMappingURL=version.js.map
