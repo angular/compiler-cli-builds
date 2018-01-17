c1ebbf284a7cc2c0d5b1d4726ee66c506fa323c3
Alex Eagle <alexeagle@google.com>
2018-01-17 16:50:31 +0000

    b773a4a docs(aio): change `df-question` to `app-question` (#21438)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-55f15c5');
+exports.VERSION = new compiler_1.Version('5.2.0-b773a4a');
 //# sourceMappingURL=version.js.map
