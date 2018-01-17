a72f7e2ef6b8c797cb7ae448917bc15b28c61fb4
Alex Eagle <alexeagle@google.com>
2018-01-17 15:59:27 +0000

    4556532 feat(core): add binding name to content changed error (#20352)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-54e7576');
+exports.VERSION = new compiler_1.Version('5.2.0-4556532');
 //# sourceMappingURL=version.js.map
