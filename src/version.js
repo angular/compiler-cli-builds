e7f2eabc627d235b2c8c6fec5e256e7e9f32f355
Alex Eagle <alexeagle@google.com>
2018-01-17 18:52:34 +0000

    8503cc1 docs: add changelog for 6.0.0-beta.0

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.0-bb3b93c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.0-8503cc1');
 //# sourceMappingURL=version.js.map
