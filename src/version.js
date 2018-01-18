dece013d34b99549521a95afa937159997fcf908
Miško Hevery <misko@hevery.com>
2018-01-18 20:02:28 +0000

    879756d fix(common): fallback to last defined value for named date and time formats (#21299)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.9.9-6-beta.0-2cf9d6d');
+exports.VERSION = new compiler_1.Version('5.9.9-6-beta.0-879756d');
 //# sourceMappingURL=version.js.map
