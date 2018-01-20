39ad1a6533aab117888025da056eb4821e4b6bed
Misko Hevery <misko@hevery.com>
2018-01-20 04:51:51 +0000

    554129d feat(aio): update metatags to control search engine crawling (#21665)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.1-c828e56');
+exports.VERSION = new compiler_1.Version('5.2.1-554129d');
 //# sourceMappingURL=version.js.map
