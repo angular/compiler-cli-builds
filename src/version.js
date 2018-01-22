6199432078d2779d3dc1dfc77e8e25aeed68eb32
Miško Hevery <misko@hevery.com>
2018-01-22 21:17:39 +0000

    c12ea3a fix(common): A null value should remove the style on IE (#21679)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.1-d7dbdc5');
+exports.VERSION = new compiler_1.Version('5.2.1-c12ea3a');
 //# sourceMappingURL=version.js.map
