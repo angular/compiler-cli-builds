645b8fc36ae1b73400e7f49052b184759dc82ed7
Miško Hevery <misko@hevery.com>
2018-01-22 21:16:59 +0000

    a64af40 build(aio): generate sitemap from the generated pages (#21689)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.9.9-6-beta.0-86d9612');
+exports.VERSION = new compiler_1.Version('5.9.9-6-beta.0-a64af40');
 //# sourceMappingURL=version.js.map
