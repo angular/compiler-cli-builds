4f5c01bc1d0768b5dfbbe72302cdeab6a2a215c8
Miško Hevery <misko@hevery.com>
2018-01-24 20:55:20 +0000

    de6c644 fix(compiler): Don't strip `/*# sourceURL ... */` (#16088)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.1-2f61d3c');
+exports.VERSION = new compiler_1.Version('5.2.1-de6c644');
 //# sourceMappingURL=version.js.map
