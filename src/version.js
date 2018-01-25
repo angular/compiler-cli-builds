345de8e3be9fcecf7d1dc1b71c335684a74a5819
Misko Hevery <misko@hevery.com>
2018-01-25 06:42:44 +0000

    5269ce2 feat(ivy): support deep queries through view boundaries (#21700)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-3e03dbe');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-5269ce2');
 //# sourceMappingURL=version.js.map
