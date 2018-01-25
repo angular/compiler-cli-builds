33ffc89a61011ce3d44e33460620b55abe3c732e
Misko Hevery <misko@hevery.com>
2018-01-25 05:09:46 +0000

    da9e57b build: Update to latest rules_typescript. (#21675)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.1-5423882');
+exports.VERSION = new compiler_1.Version('5.2.1-da9e57b');
 //# sourceMappingURL=version.js.map
