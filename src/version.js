b425cd2837fe4bb57eab7f792a24550dd85a94eb
Jason Aden <jasonaden@google.com>
2018-01-26 23:56:42 +0000

    81d64d6 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-7410941');
+exports.VERSION = new compiler_1.Version('5.2.2-81d64d6');
 //# sourceMappingURL=version.js.map
