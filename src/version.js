ee602121cb6e47de6f40e2e543ae897604f4f5f3
Misko Hevery <misko@hevery.com>
2018-01-26 07:04:00 +0000

    bbb8f38 feat(ivy): implement template variables (#21760)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-08aa54e');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-bbb8f38');
 //# sourceMappingURL=version.js.map
