2ac70ba1fda9b0ea4efbfe8f6d49cec65d4e8f56
Jason Aden <jasonaden@google.com>
2018-01-26 23:14:00 +0000

    250c8da fix(language-service): ensure correct paths are passed to TypeScript (#21812)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-d159ad8');
+exports.VERSION = new compiler_1.Version('5.2.2-250c8da');
 //# sourceMappingURL=version.js.map
