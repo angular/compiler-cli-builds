8a7d0d9983d28cf6d4bdbd91faa845168507674b
Misko Hevery <misko@hevery.com>
2018-01-26 05:53:10 +0000

    f21eb65 build: autosquashes SHAs as part of merge-pr script (#21791)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-27ecd07');
+exports.VERSION = new compiler_1.Version('5.2.2-f21eb65');
 //# sourceMappingURL=version.js.map
