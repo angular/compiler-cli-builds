3edeeb2509948487b9236eef2cca66248579a270
Miško Hevery <misko@hevery.com>
2018-01-26 19:02:00 +0000

    35a0721 fix(router): remove @internal tag on ParamInheritanceType (#21773)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-67806a7');
+exports.VERSION = new compiler_1.Version('5.2.2-35a0721');
 //# sourceMappingURL=version.js.map
