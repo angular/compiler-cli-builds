0978b675e4309c1575707e46a594d4f7a659c40e
Miško Hevery <misko@hevery.com>
2018-01-26 18:48:19 +0000

    e608052 docs: add notes on email used for CLA (#21754)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-f9381e4');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-e608052');
 //# sourceMappingURL=version.js.map
