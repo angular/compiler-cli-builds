74d5a26fddf56ba37cf0ade28a42a1d321724cb6
Jason Aden <jasonaden@google.com>
2018-01-26 23:19:44 +0000

    e0b31db build: merge-pr now checks that PR status is green before proceeding (#21810)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-8df56fe');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-e0b31db');
 //# sourceMappingURL=version.js.map
