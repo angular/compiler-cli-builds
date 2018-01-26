815518e918f83c4aba5d42097681335e5b335aea
Miško Hevery <misko@hevery.com>
2018-01-26 19:03:29 +0000

    ba045e8 docs: add notes on email used for CLA (#21754)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-35a0721');
+exports.VERSION = new compiler_1.Version('5.2.2-ba045e8');
 //# sourceMappingURL=version.js.map
