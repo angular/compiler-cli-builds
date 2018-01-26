3d2db9622e1ec9075da33e1b8fa32ce83b73dd1e
Misko Hevery <misko@hevery.com>
2018-01-26 07:05:47 +0000

    f9381e4 feat(ivy): implement QueryList array-related methods (#21778)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-bbb8f38');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-f9381e4');
 //# sourceMappingURL=version.js.map
