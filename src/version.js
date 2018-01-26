463c81a1bb4307174e5198ed78b9d6d40f346a6d
Jason Aden <jasonaden@google.com>
2018-01-26 23:13:29 +0000

    d159ad8 build(aio): prevent Windows error on `serve-and-sync` (#21806)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-ba045e8');
+exports.VERSION = new compiler_1.Version('5.2.2-d159ad8');
 //# sourceMappingURL=version.js.map
