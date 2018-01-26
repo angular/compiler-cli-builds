23b22e97aeb3d41a42836439bdcbe1d0069c8c15
Misko Hevery <misko@hevery.com>
2018-01-26 06:59:25 +0000

    08aa54e ci: Add back the CLI integration test with pinning (#21555)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-fac4d8d');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-08aa54e');
 //# sourceMappingURL=version.js.map
