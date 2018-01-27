bc5f2289b2692c9bb401a7e3c60127ae7bdde15c
Jason Aden <jasonaden@google.com>
2018-01-27 00:29:38 +0000

    696ba01 fix(aio): don't set noindex metatag in the static index.html (#21816)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-81d64d6');
+exports.VERSION = new compiler_1.Version('5.2.2-696ba01');
 //# sourceMappingURL=version.js.map
