fdd1417ef65ab9884feb225e7ef4fe65a4d9fa77
Misko Hevery <misko@hevery.com>
2018-01-27 19:10:43 +0000

    5778bb8 fix(ivy): fix issues found producing "Hello, World" example (#21790)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-d364117');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-5778bb8');
 //# sourceMappingURL=version.js.map
