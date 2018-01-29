1369139de6b33ad0edbbc789ab79bb812bb1953f
Jason Aden <jasonaden@google.com>
2018-01-29 20:16:28 +0000

    3f5ead3 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-a89e709');
+exports.VERSION = new compiler_1.Version('5.2.2-3f5ead3');
 //# sourceMappingURL=version.js.map
