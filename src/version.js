b57a469e367fee29258cb9ab62b2b5576770f35b
Jason Aden <jasonaden@google.com>
2018-01-29 20:07:25 +0000

    18174e5 feat(ivy): support ng-content projection in the ivy compiler (#21764)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-72265f7');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-18174e5');
 //# sourceMappingURL=version.js.map
