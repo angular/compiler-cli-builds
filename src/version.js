b53021761b4c39f291d7bf3f88a9fcfdb5816b8d
Jason Aden <jasonaden@google.com>
2018-01-30 19:58:07 +0000

    bb577c6 ci: unblock master by ignoring date pipe tests while we fix it (#21906)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-02483a0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-bb577c6');
 //# sourceMappingURL=version.js.map
