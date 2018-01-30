541e2563071beb06ebc2d625f7943117b8b7aaca
Jason Aden <jasonaden@google.com>
2018-01-30 00:32:42 +0000

    fad99cc fix(forms): inserting and removing controls should work in re-bound form arrays (#21822)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-3f5ead3');
+exports.VERSION = new compiler_1.Version('5.2.2-fad99cc');
 //# sourceMappingURL=version.js.map
