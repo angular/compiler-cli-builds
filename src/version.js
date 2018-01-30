b79f4a1ac43bc02b69827cc632b3231a700ce1c0
Jason Aden <jasonaden@google.com>
2018-01-30 00:33:11 +0000

    11b1267 docs: document debugging a Node test in VSCode/Bazel (#21868)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-dd48df1');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-11b1267');
 //# sourceMappingURL=version.js.map
