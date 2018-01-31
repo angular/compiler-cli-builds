9ded7eccb967be1a0c6023719c0b6477f1e4dfef
Jason Aden <jasonaden@google.com>
2018-01-31 21:31:41 +0000

    579bed1 docs: add changelog for 5.2.3

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.2-2aa460b');
+exports.VERSION = new compiler_1.Version('5.2.3-579bed1');
 //# sourceMappingURL=version.js.map
