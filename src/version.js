be9660a1803d695308a5a9d9f45e111e3e1d31e6
Jason Aden <jasonaden@google.com>
2018-01-31 21:52:26 +0000

    61abba4 docs: add changelog for 6.6.0-beta.2

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-3db75b3');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.2-61abba4');
 //# sourceMappingURL=version.js.map
