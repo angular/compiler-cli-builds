037808f6b4b8247522d31601f31b87d895f1eb27
Jason Aden <jasonaden@google.com>
2018-01-31 19:15:23 +0000

    db3e65f docs(service-worker): describe safety-worker.js in SW guide (#21921)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-ccd0298');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-db3e65f');
 //# sourceMappingURL=version.js.map
