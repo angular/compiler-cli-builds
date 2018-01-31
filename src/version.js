c819a863116dba901a3a62b8518819d956887a48
Jason Aden <jasonaden@google.com>
2018-01-31 18:51:42 +0000

    eeab433 docs: Fix platform-detection example for Universal (#21796)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-c83c416');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-eeab433');
 //# sourceMappingURL=version.js.map
