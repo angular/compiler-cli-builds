4cd2f04d1885d0a0494ebff4b4f531712a8a00e2
Jason Aden <jasonaden@google.com>
2018-01-31 18:45:19 +0000

    c83c416 build(aio): upgrade to dgeni-packages 0.24.0 (#21802)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.1-9dca5f2');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.1-c83c416');
 //# sourceMappingURL=version.js.map
