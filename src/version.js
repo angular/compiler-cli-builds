5c76724e36eaa17ee17737ef30cfafffdaf12e6c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:33 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.3-965eecc');
+exports.VERSION = new compiler_1.Version('5.2.3-75eecdc');
 //# sourceMappingURL=version.js.map
