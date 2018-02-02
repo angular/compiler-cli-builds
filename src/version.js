73fec08bf045a9278e7eb7adabf708f455c5d6e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:31:49 +0000

    447783e docs: add docs for IE (#21824)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.2-743d8bc');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.2-447783e');
 //# sourceMappingURL=version.js.map
