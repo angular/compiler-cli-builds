426dc2a1ea6d3d53da6eaf4c0aed5799b4f85b03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:53:55 +0000

    3a86940 fix(core): should check Zone existance when scheduleMicroTask (#20656)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.2-7b120b5');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.2-3a86940');
 //# sourceMappingURL=version.js.map
