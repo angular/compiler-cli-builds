388e5622cb20294df6a9f3ee567448f1befe4bc9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:17:13 +0000

    f0396f1 docs(aio): fix swap value (#20905)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.3-adb1d62');
+exports.VERSION = new compiler_1.Version('5.2.3-f0396f1');
 //# sourceMappingURL=version.js.map
