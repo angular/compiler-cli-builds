d454915eb06340833563ba4cc3e7dca0b851ec77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:32:05 +0000

    b155ae1 ci: add config for g3 status (#21996)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.3-d38e088');
+exports.VERSION = new compiler_1.Version('5.2.3-b155ae1');
 //# sourceMappingURL=version.js.map
