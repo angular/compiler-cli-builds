a62573fb5b7db5f6947013b2d767caa67ce746b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:46:29 +0000

    973607f ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.3-b155ae1');
+exports.VERSION = new compiler_1.Version('5.2.3-973607f');
 //# sourceMappingURL=version.js.map
