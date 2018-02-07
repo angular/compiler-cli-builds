1e8201cec9746e73cc6cde968071b73f88d91036
Miško Hevery <misko@hevery.com>
2018-02-07 20:25:08 +0000

    44ea80b docs(forms): Custom Validator example selector name incorrect. (#20464)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-530b824');
+exports.VERSION = new compiler_1.Version('5.2.4-44ea80b');
 //# sourceMappingURL=version.js.map
