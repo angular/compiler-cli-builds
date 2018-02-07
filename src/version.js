dcf166ecc1b15aa775f7661f89fd6ef7fca9ac34
Miško Hevery <misko@hevery.com>
2018-02-07 20:51:55 +0000

    0bcfae7 fix(forms): prevent event emission on enable/disable when emitEvent is false (#12366) (#21018)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-140e7c0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-0bcfae7');
 //# sourceMappingURL=version.js.map
