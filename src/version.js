1bfc0bf8edb2ecc543c00560a364d44c570d9f8b
Miško Hevery <misko@hevery.com>
2018-02-07 20:31:11 +0000

    b0ddb5a build(aio): blacklist unwanted URLs from the generated sitemap.xml (#22061)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-44ea80b');
+exports.VERSION = new compiler_1.Version('5.2.4-b0ddb5a');
 //# sourceMappingURL=version.js.map
