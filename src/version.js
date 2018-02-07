91b2ae84c6904e90b73798e5badc545f3b6797a0
Miško Hevery <misko@hevery.com>
2018-02-07 22:05:45 +0000

    b58c352 test(ivy): add canonical spec for object literals (#22045)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-efc67ee');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-b58c352');
 //# sourceMappingURL=version.js.map
