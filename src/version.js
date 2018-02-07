0e676a1d4cbaebee6ca54f2461440de5b93aec70
Miško Hevery <misko@hevery.com>
2018-02-07 21:07:19 +0000

    7e95802 fix(aio): ignore `.header-link` when selecting the heading text (#21695)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-9744a1c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-7e95802');
 //# sourceMappingURL=version.js.map
