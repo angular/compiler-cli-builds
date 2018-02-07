4c6aea6f88b8fa3dbdab058fa5915aed22065d4b
Miško Hevery <misko@hevery.com>
2018-02-07 21:35:06 +0000

    e442881 feat(bazel): allow explicit specification of factories (#22003)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-b37cee3');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-e442881');
 //# sourceMappingURL=version.js.map
