20ca48d26a98848182faee0aa411ea0ff4173058
Miško Hevery <misko@hevery.com>
2018-02-07 21:25:30 +0000

    07769e5 test(common): disable deprecated date pipe tests on chrome mobile (#21933)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-9fdb804');
+exports.VERSION = new compiler_1.Version('5.2.4-07769e5');
 //# sourceMappingURL=version.js.map
