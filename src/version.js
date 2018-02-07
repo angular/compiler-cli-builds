c535038619c4618c67caf8fa3fd3c7d726401af9
Miško Hevery <misko@hevery.com>
2018-02-07 22:01:26 +0000

    75897d4 feat(aio): report `logger.error` calls to Google Analytics (#22011)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-0b23573');
+exports.VERSION = new compiler_1.Version('5.2.4-75897d4');
 //# sourceMappingURL=version.js.map
