62d4a657ccad4a301455022d59720677c86fe280
Miško Hevery <misko@hevery.com>
2018-02-07 21:56:24 +0000

    0b23573 fix(language-service): correct instructions to install the language service (#22000)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-47b73fd');
+exports.VERSION = new compiler_1.Version('5.2.4-0b23573');
 //# sourceMappingURL=version.js.map
