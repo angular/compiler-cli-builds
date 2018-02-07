68357765ce9caa66a4f2702279c53f26f8270f24
Miško Hevery <misko@hevery.com>
2018-02-07 21:32:00 +0000

    e56de10 fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-54a1431');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-e56de10');
 //# sourceMappingURL=version.js.map
