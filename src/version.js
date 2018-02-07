7a34cbceac94773b55eaeaccf81d5b3cd8d3bfe7
Miško Hevery <misko@hevery.com>
2018-02-07 21:10:15 +0000

    9fdb804 test(forms): update test name with correct wording (#21833)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-9d02db3');
+exports.VERSION = new compiler_1.Version('5.2.4-9fdb804');
 //# sourceMappingURL=version.js.map
