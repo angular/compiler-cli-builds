9154eacf4e3d68c0bdce428013c026a418e841c4
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:32:50 +0000

    16e5b86 test(ivy): also track the size of the compressed hello world bundle (#22056)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-cd25939');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-16e5b86');
 //# sourceMappingURL=version.js.map
