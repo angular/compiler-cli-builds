e81dcba857ebfe9ce28b7cce0491243c53d617c8
Miško Hevery <misko@hevery.com>
2018-02-08 18:21:49 +0000

    c5ec8d9 fix(bazel): improve error message for missing assets (#22096)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-de561f3');
+exports.VERSION = new compiler_1.Version('5.2.4-c5ec8d9');
 //# sourceMappingURL=version.js.map
