7a1fb02d737f40672b717280e8bb9ea4ca71e028
Miško Hevery <misko@hevery.com>
2018-02-08 18:25:14 +0000

    dcf64a0 fix(bazel): improve error message for missing assets (#22096)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-a9545ab');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-dcf64a0');
 //# sourceMappingURL=version.js.map
