dc13cad7ff1e10fd2de12b137d4667e4380ae71a
Miško Hevery <misko@hevery.com>
2018-02-08 22:59:05 +0000

    0b1f5d2 Revert "docs(common): add `HttpParamsOptions` to the public API (#20332)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-dcf64a0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-0b1f5d2');
 //# sourceMappingURL=version.js.map
