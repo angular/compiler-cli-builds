754c8661bdf6e4dc9395e15c3c64c1c379077b5e
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:51:31 +0000

    22d548f docs(aio): remove ngATL banner from homepage (#22060)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-799edd1');
+exports.VERSION = new compiler_1.Version('5.2.4-22d548f');
 //# sourceMappingURL=version.js.map
