9f0e6dcfe2a10ad3cea7c1ad504de1f73bbbc221
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:29:35 +0000

    ffcb5dd build(aio): update examples to CLI to 1.6.5 (#21222)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-75897d4');
+exports.VERSION = new compiler_1.Version('5.2.4-ffcb5dd');
 //# sourceMappingURL=version.js.map
