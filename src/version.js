a9e95041525e626140233b827843f4963b57eb57
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 17:26:28 +0000

    47b71d9 fix(core): use appropriate inert document strategy for Firefox & Safari (#17019)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-22d548f');
+exports.VERSION = new compiler_1.Version('5.2.4-47b71d9');
 //# sourceMappingURL=version.js.map
