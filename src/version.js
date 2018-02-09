7e1d82509708b82c58525ee8f310c34e6a2350f9
Miško Hevery <misko@hevery.com>
2018-02-09 21:56:05 +0000

    854f0ff feat(aio): enable data driven homepage announcements (#22043)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-d4a9db2');
+exports.VERSION = new compiler_1.Version('5.2.4-854f0ff');
 //# sourceMappingURL=version.js.map
