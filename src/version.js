d58c34a7941b8eb7a356222ab1875b100770a7b0
Miško Hevery <misko@hevery.com>
2018-02-09 21:30:55 +0000

    7007f51 feat(aio): first pass API docs redesign (#21874)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-cf8d512');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-7007f51');
 //# sourceMappingURL=version.js.map
