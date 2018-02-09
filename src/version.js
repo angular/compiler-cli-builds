e2b873cbd460d09f4a13d07e61ca469a238cd4b2
Miško Hevery <misko@hevery.com>
2018-02-09 21:31:15 +0000

    bc1e229 docs(aio): several fix for ngmodule guides (#21517)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-7007f51');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-bc1e229');
 //# sourceMappingURL=version.js.map
