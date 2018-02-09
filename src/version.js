c0442d108c4b569c0c3a1e6e6e4f3cc5c0f6ed1a
Miško Hevery <misko@hevery.com>
2018-02-09 21:42:10 +0000

    c348445 docs: fix typo in http.md (#22058)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-fbef94a');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-c348445');
 //# sourceMappingURL=version.js.map
