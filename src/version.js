58c2d8026517f8c233f00eb18160c8e1d74b3fd9
Miško Hevery <misko@hevery.com>
2018-02-09 18:34:27 +0000

    cf8d512 Revert "fix(forms): set state before emitting a value from ngModelChange (#21514)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-0b1f5d2');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-cf8d512');
 //# sourceMappingURL=version.js.map
