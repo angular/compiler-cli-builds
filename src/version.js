194a390aa258ad353722e6c4ed5dfbab80a0f9c3
Miško Hevery <misko@hevery.com>
2018-02-10 01:43:40 +0000

    d57fd0b fix(bazel): allow TS to read ambient typings (#21876)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-9b280ee');
+exports.VERSION = new compiler_1.Version('5.2.4-d57fd0b');
 //# sourceMappingURL=version.js.map
