6417cb7ba627888fd43d52902e43b8ba31d35ea4
Miško Hevery <misko@hevery.com>
2018-02-10 01:36:22 +0000

    c878d55 docs: add VSCode interaction issue to bazel docs (#22128)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-263a2ec');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-c878d55');
 //# sourceMappingURL=version.js.map
