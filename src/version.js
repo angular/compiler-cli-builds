7b8ac8a080602b97a3417cd6efcbe7773edf1aaa
Miško Hevery <misko@hevery.com>
2018-02-10 01:48:27 +0000

    7e9b120 build: update to latest bazel rules (#22127)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-b081dfe');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-7e9b120');
 //# sourceMappingURL=version.js.map
