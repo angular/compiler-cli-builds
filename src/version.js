a4d20aced4305252013d616f4a4dc32355210b1d
Miško Hevery <misko@hevery.com>
2018-02-10 01:35:50 +0000

    9b280ee build: merge-pr new checks that all requested changes have been addressed (#21817)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-ac815f7');
+exports.VERSION = new compiler_1.Version('5.2.4-9b280ee');
 //# sourceMappingURL=version.js.map
