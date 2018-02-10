10eaf839989c43e05d8b0ca2289ae2492a0947a9
Miško Hevery <misko@hevery.com>
2018-02-10 00:28:57 +0000

    263a2ec build(bazel): allow ng_modules to elide .ngsummary.closure.js files (#22107)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-44154e7');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-263a2ec');
 //# sourceMappingURL=version.js.map
