190bebb50749f043acbe98d69705e6ea2cdadfda
Miško Hevery <misko@hevery.com>
2018-02-12 18:33:40 +0000

    a616dd6 fix(aio): remove broken span closing tag (#22146)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-d57fd0b');
+exports.VERSION = new compiler_1.Version('5.2.4-a616dd6');
 //# sourceMappingURL=version.js.map
