517fe8c1d758984deaa55f30e3707c7567f89311
Miško Hevery <misko@hevery.com>
2018-02-13 19:03:30 +0000

    4f19491 ci: remove conditional clause for bazel install (#22170)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-2de0d4c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-4f19491');
 //# sourceMappingURL=version.js.map
