f770cbf718c9ba0dd8444a35dabc84ee86cb4e68
Miško Hevery <misko@hevery.com>
2018-02-13 00:30:47 +0000

    e97afae docs(aio): fix typo in "preserveWhitespaces" example (#22182)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-c6bdc83');
+exports.VERSION = new compiler_1.Version('5.2.4-e97afae');
 //# sourceMappingURL=version.js.map
