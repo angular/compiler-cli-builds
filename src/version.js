c0316eb40f1a59078839e4bab5d6f643d85e6368
Miško Hevery <misko@hevery.com>
2018-02-13 21:58:59 +0000

    a589ca0 test(ivy): clean up canonical spec (#22188)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-72f8abd');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-a589ca0');
 //# sourceMappingURL=version.js.map
