270ef9ccfeb85ac1b826b050f6adef0946ab1d26
Miško Hevery <misko@hevery.com>
2018-02-13 00:17:45 +0000

    be59c3a fix(common): weaken AsyncPipe transform signature (#22169)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-235a235');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.3-be59c3a');
 //# sourceMappingURL=version.js.map
