e1f5660e621c430ded60857ac7e667abb75db251
Misko Hevery <misko@hevery.com>
2018-02-14 06:06:46 +0000

    fa7d890 docs: add changelog for 6.0.0-beta.4

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.3-a589ca0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-fa7d890');
 //# sourceMappingURL=version.js.map
