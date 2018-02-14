4b7c0fb180e413b745e44c5cd5c9d47661b9b118
Misko Hevery <misko@hevery.com>
2018-02-14 05:53:24 +0000

    106b435 docs: add changelog for 5.2.5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-c4f841f');
+exports.VERSION = new compiler_1.Version('5.2.5-106b435');
 //# sourceMappingURL=version.js.map
