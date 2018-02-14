04033f06978419ee75990f36bec86f6fa425d968
Miško Hevery <misko@hevery.com>
2018-02-14 05:21:42 +0000

    c4f841f fix(core): add stacktrace in log when error during cleanup component in TestBed (#22162)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.4-1b8ea10');
+exports.VERSION = new compiler_1.Version('5.2.4-c4f841f');
 //# sourceMappingURL=version.js.map
