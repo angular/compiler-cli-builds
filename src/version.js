fe0ac96a2d1a594b5ad7a77901f9799e2873def8
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:48:59 +0000

    5bb9fca build: comment-out chromium version checking code temporarily (#22232)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-fa7d890');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-5bb9fca');
 //# sourceMappingURL=version.js.map
