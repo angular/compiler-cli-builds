7d67c1e2d10dad578265fafac42cb8cde7f8178c
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 00:26:27 +0000

    1e3e0fa fix(aio): improve printing styles (#19651)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-0c88d5d');
+exports.VERSION = new compiler_1.Version('5.2.5-1e3e0fa');
 //# sourceMappingURL=version.js.map
