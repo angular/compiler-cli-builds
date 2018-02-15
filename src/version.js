4093102c034ddc29fe5c10df7f508978d2dc4a3a
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 04:52:52 +0000

    0b68312 refactor(ivy): assertion (#22189)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-831592c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-0b68312');
 //# sourceMappingURL=version.js.map
