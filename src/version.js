4f958ffbb7764576c7e71ad634a874ae77e0dbaf
Victor Berchet <victor@suumit.com>
2018-02-15 22:27:22 +0000

    6a57264 Revert: "build: allow bazel build ... (#22168)"

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-1e3e0fa');
+exports.VERSION = new compiler_1.Version('5.2.5-6a57264');
 //# sourceMappingURL=version.js.map
