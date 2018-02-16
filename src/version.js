7068aea5d8d09cbb07d94d4bda7808a19fc9895f
Victor Berchet <victor@suumit.com>
2018-02-16 23:15:53 +0000

    185a6ab build: add esm5 build (#22258)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-6b45784');
+exports.VERSION = new compiler_1.Version('5.2.5-185a6ab');
 //# sourceMappingURL=version.js.map
