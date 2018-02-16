d0e759a03cb4a1c74bbce391fd224cef7199f99f
Victor Berchet <victor@suumit.com>
2018-02-16 23:11:00 +0000

    6b45784 test(aio): increase `docs-test` timeouts to prevent flakes on Travis (#22261)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-5f52ea3');
+exports.VERSION = new compiler_1.Version('5.2.5-6b45784');
 //# sourceMappingURL=version.js.map
