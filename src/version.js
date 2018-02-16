9f8d6fb4c8f23d1188b0e41d7092cc86443b7d81
Victor Berchet <victor@suumit.com>
2018-02-16 23:30:30 +0000

    ce5e8fa fix(common): correct mapping of Observable methods (#20518)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-185a6ab');
+exports.VERSION = new compiler_1.Version('5.2.5-ce5e8fa');
 //# sourceMappingURL=version.js.map
