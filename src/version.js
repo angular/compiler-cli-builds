568ac5151452f9321363565bd2ec97c7fceb03eb
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:12 +0000

    e1bf067 fix(animations): report correct totalTime value even during noOp animations (#22225)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-884de18');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-e1bf067');
 //# sourceMappingURL=version.js.map
