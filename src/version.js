d81044066bb2a49c54335af96cf5024f79319b21
Victor Berchet <victor@suumit.com>
2018-02-17 02:26:37 +0000

    f693be3 feat(ivy): add pureFunction0 instruction (#22214)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-e1bf067');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-f693be3');
 //# sourceMappingURL=version.js.map
