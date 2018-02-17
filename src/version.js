9c9f113e0811b1c1ed18319642f6402e2d89b45f
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:31 +0000

    861250b docs: fix changelog errors (#22228)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-81c1e0a');
+exports.VERSION = new compiler_1.Version('5.2.5-861250b');
 //# sourceMappingURL=version.js.map
