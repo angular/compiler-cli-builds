f4f769844d2cee479181be570ef1ffcdbf25d554
Victor Berchet <victor@suumit.com>
2018-02-18 23:32:23 +0000

    c726d1d docs(aio): add angular-buch to resources (#22163)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-9dae97c');
+exports.VERSION = new compiler_1.Version('5.2.5-c726d1d');
 //# sourceMappingURL=version.js.map
