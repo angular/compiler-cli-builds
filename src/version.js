ad959958fb7051112ebf0ed6a88511e324219d0d
Victor Berchet <victor@suumit.com>
2018-02-18 23:33:47 +0000

    c30d329 fix(ivy): fix merge errors (master is broken) (#22291)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-ac2b04a');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-c30d329');
 //# sourceMappingURL=version.js.map
