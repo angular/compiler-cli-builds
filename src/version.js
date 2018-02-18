affc32c1c262f0a4dd40f28fdf04fe3fa1bebb3e
Victor Berchet <victor@suumit.com>
2018-02-18 21:33:26 +0000

    396bc0d build: use authenticated mode for the merge script (#22269)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-861250b');
+exports.VERSION = new compiler_1.Version('5.2.5-396bc0d');
 //# sourceMappingURL=version.js.map
