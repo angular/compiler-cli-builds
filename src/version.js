f34ff3e94208a30142d672b51eed08427eef9321
Victor Berchet <victor@suumit.com>
2018-02-19 03:11:45 +0000

    a8b5465 fix(ivy): update master with renamings (#22268)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-3ceee99');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-a8b5465');
 //# sourceMappingURL=version.js.map
