daf90bc88a363c09382ebb4a89dfc9f7f9123ecf
Victor Berchet <victor@suumit.com>
2018-02-19 04:10:26 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-c726d1d');
+exports.VERSION = new compiler_1.Version('5.2.5-cdfedc1');
 //# sourceMappingURL=version.js.map
