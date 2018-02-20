9accb3dc87fb34a173e2688b47c26c690cb12e18
Victor Berchet <victor@suumit.com>
2018-02-20 20:03:15 +0000

    ee60bb5 fix(ivy): pureFunction8 should update the right bindings (#22313)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-f6120c0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-ee60bb5');
 //# sourceMappingURL=version.js.map
