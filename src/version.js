a26e8086ccd90ecbe356d0224c7669e7aed98b05
Victor Berchet <victor@suumit.com>
2018-02-20 22:34:53 +0000

    99909bb feat(ivy): generate pipe references and definitions (#22034)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-ee60bb5');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-99909bb');
 //# sourceMappingURL=version.js.map
