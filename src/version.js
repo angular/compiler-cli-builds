c60fb20a914ee8e000c957747ba0f2b31ff8f474
Victor Berchet <victor@suumit.com>
2018-02-21 19:36:52 +0000

    dfdade2 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-c2f78e1');
+exports.VERSION = new compiler_1.Version('5.2.5-dfdade2');
 //# sourceMappingURL=version.js.map
