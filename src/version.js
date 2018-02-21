80ebbba47ed02c5591c876cc46f2a38960042736
Victor Berchet <victor@suumit.com>
2018-02-21 00:02:29 +0000

    5bdb3ac build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-a346d28');
+exports.VERSION = new compiler_1.Version('5.2.5-5bdb3ac');
 //# sourceMappingURL=version.js.map
