1fa8e743d72d8d7d5f30954fa0954298e2797f70
Victor Berchet <victor@suumit.com>
2018-02-21 23:22:19 +0000

    a7ebf5a fix(core): properly handle function without prototype in reflector (#22284)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-b42921b');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-a7ebf5a');
 //# sourceMappingURL=version.js.map
