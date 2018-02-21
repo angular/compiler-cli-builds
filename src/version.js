6da9e03f04155337a3538ec975253c9cd76d689f
Victor Berchet <victor@suumit.com>
2018-02-21 00:18:24 +0000

    a2cb010 docs(aio): Fix name of component (#22332) PR Close #22332

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.5-d20a08b');
+exports.VERSION = new compiler_1.Version('5.2.5-a2cb010');
 //# sourceMappingURL=version.js.map
