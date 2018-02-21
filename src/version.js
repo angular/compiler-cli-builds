a50e847143462571ada1171d130482265aa715f3
Victor Berchet <victor@suumit.com>
2018-02-21 19:31:12 +0000

    9e6268b docs(http): fix a typo in code comment (#22327)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-1c1cbba');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-9e6268b');
 //# sourceMappingURL=version.js.map
