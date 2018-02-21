789fd2c6a2c6cf84c800ab4c66d61cd953934796
Victor Berchet <victor@suumit.com>
2018-02-21 00:18:41 +0000

    69a0578 docs(aio): fix the css of the heroes component's buttons (#22333)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-b5ca275');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-69a0578');
 //# sourceMappingURL=version.js.map
