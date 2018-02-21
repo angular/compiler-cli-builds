bf64ce20ec50b7f839d5a82885a29cdc23f7c98f
Victor Berchet <victor@suumit.com>
2018-02-21 19:40:40 +0000

    722dec1 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-435f6ee');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-722dec1');
 //# sourceMappingURL=version.js.map
