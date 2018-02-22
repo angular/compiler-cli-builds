3be2a24343ef3a6d071940270027c1279b29e2ca
Victor Berchet <victor@suumit.com>
2018-02-22 19:41:14 +0000

    da1b4d5 docs: fix ngmodules-jsmodules pre-req (#22316)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-aa100f6');
+exports.VERSION = new compiler_1.Version('5.2.6-da1b4d5');
 //# sourceMappingURL=version.js.map
