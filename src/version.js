80d37a4f0de2a83cb700fb087bfbdb4a6f53ac5d
Victor Berchet <victor@suumit.com>
2018-02-22 22:03:28 +0000

    1776239 build: disable bazel-out symlink (#22375)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-da1b4d5');
+exports.VERSION = new compiler_1.Version('5.2.6-1776239');
 //# sourceMappingURL=version.js.map
