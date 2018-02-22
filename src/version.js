21a6d89565f0f1564120555280a5c49c5c4b1741
Victor Berchet <victor@suumit.com>
2018-02-22 22:23:31 +0000

    9eecb0b docs: fix deployment sample path (#22048)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-45eff4c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-9eecb0b');
 //# sourceMappingURL=version.js.map
