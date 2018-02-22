6edf6b032fb3705408bf2c1f30d22ffea3ec2301
Victor Berchet <victor@suumit.com>
2018-02-22 01:10:13 +0000

    5c89d6b feat(core): support metadata reflection for native class types (#22356)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.4-3e6a86f');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.4-5c89d6b');
 //# sourceMappingURL=version.js.map
