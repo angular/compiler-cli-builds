3a543866a7492de98b544e970bd6e904ff4b317a
Victor Berchet <victor@suumit.com>
2018-02-22 18:44:41 +0000

    9cca5a8 build: allow passing node options to ngc. (#22245)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-7c3b95b');
+exports.VERSION = new compiler_1.Version('5.2.6-9cca5a8');
 //# sourceMappingURL=version.js.map
