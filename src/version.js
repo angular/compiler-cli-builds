9af66b958e3b5f6f2f812717ec0c5ffcda70acef
Victor Berchet <victor@suumit.com>
2018-02-23 18:12:54 +0000

    aad4316 refactor(ivy): rename componentRefresh to directiveRefresh (#22395)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-a81d599');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-aad4316');
 //# sourceMappingURL=version.js.map
