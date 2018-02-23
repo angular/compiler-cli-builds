0c49e92dba4f37b27f07bf75d0d1a26b1443951d
Victor Berchet <victor@suumit.com>
2018-02-23 21:43:25 +0000

    094666d fix(router): fix URL serialization so special characters are only encoded where needed (#22337)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-3a809cb');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-094666d');
 //# sourceMappingURL=version.js.map
