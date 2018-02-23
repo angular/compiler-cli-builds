84ab3e5cd0559f62662e9ac8444d5e5c8ed0ffa4
Victor Berchet <victor@suumit.com>
2018-02-23 18:16:25 +0000

    17c1577 ci: don't use bazel git_repository rule (#22406)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-150bac3');
+exports.VERSION = new compiler_1.Version('5.2.6-17c1577');
 //# sourceMappingURL=version.js.map
