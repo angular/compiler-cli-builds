a3c2e645cdfd0ac8e57f0b709bab8d4ccfa0a700
Victor Berchet <victor@suumit.com>
2018-02-23 19:39:28 +0000

    cf91906 docs(aio): Essential JS 2 url updated (#19739) PR Close #19739

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-2b71889');
+exports.VERSION = new compiler_1.Version('5.2.6-cf91906');
 //# sourceMappingURL=version.js.map
