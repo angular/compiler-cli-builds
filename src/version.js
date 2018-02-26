26f9e26e28e1b31c0f78e565fb9358c11b197254
Alex Eagle <alexeagle@google.com>
2018-02-26 23:19:45 +0000

    d3827a0 feat(platform-server): bump Domino to v2.0 (#22411)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-a7e1f23');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-d3827a0');
 //# sourceMappingURL=version.js.map
