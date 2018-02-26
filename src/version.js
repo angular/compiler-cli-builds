29609da0a42aeac901d0aef04cca6b641e321c89
Alex Eagle <alexeagle@google.com>
2018-02-26 21:56:03 +0000

    2a38d93 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-a9a0e27');
+exports.VERSION = new compiler_1.Version('5.2.6-2a38d93');
 //# sourceMappingURL=version.js.map
