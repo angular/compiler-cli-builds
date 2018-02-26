40c3763db83b47c087dcdb371486cfb631490574
Alex Eagle <alexeagle@google.com>
2018-02-26 22:05:18 +0000

    25a4304 build(aio): group API members by type in overview (#22438)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-c593d69');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-25a4304');
 //# sourceMappingURL=version.js.map
