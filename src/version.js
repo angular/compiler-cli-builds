c3fc63c1100f4daa24f32054fefe353ed0fe7e7b
Alex Eagle <alexeagle@google.com>
2018-02-27 02:11:39 +0000

    ef99126 test(aio): move forgotten e2e test to the correct folder (#22445)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-40ba009');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-ef99126');
 //# sourceMappingURL=version.js.map
