b9c74e68c726906b142808523b205b325953b5bc
Alex Eagle <alexeagle@google.com>
2018-02-28 19:37:41 +0000

    0ec11e3 docs: fix dynamic component loader example (#22181)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.6-089769d');
+exports.VERSION = new compiler_1.Version('5.2.6-0ec11e3');
 //# sourceMappingURL=version.js.map
