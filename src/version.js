2dbf0d49f57ab003c20fbc02c5d07ec05c5caed4
Alex Eagle <alexeagle@google.com>
2018-02-28 03:05:34 +0000

    8a85888 fix(upgrade): correctly destroy nested downgraded component (#22400)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-83b32a0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-8a85888');
 //# sourceMappingURL=version.js.map
