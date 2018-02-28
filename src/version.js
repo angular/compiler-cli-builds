36507336ffe8b1c1b53d85df4a3a3e62e6cb5808
Alex Eagle <alexeagle@google.com>
2018-02-28 03:23:11 +0000

    e95b61d docs: fix community tab in GitHub by copying CoC

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-8a85888');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-e95b61d');
 //# sourceMappingURL=version.js.map
