84a534c7c8e35af210ba5ab2d567fe85d6d49f04
Alex Eagle <alexeagle@google.com>
2018-02-28 22:10:47 +0000

    9eaf1bb feat(ivy): support injecting ChangeDetectorRef (#22469)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.5-aabe16c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.5-9eaf1bb');
 //# sourceMappingURL=version.js.map
