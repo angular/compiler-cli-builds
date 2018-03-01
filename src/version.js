9ef27708f51684c3beb1e7ffe4321df6ebcba034
Alex Eagle <alexeagle@google.com>
2018-03-01 17:09:41 +0000

    dd53447 fix(compiler): allow tree-shakeable injectables to depend on string tokens (#22376)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-0e311e3');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-dd53447');
 //# sourceMappingURL=version.js.map
