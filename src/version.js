5dec510f5a92c18c1c54cf417330306dfb4f1377
Alex Eagle <alexeagle@google.com>
2018-03-01 16:38:00 +0000

    f8e70fb build(aio): initialise `exampleMap` correctly (#22502)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.7-27962f8');
+exports.VERSION = new compiler_1.Version('5.2.7-f8e70fb');
 //# sourceMappingURL=version.js.map
