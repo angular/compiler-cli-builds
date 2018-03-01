a78d47db1492795fcf739520c7509d561633d351
Alex Eagle <alexeagle@google.com>
2018-03-01 16:46:28 +0000

    8bb2f5c docs(compiler): ivy separate compilation design document (#22480)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-94707fe');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-8bb2f5c');
 //# sourceMappingURL=version.js.map
