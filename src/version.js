f20752c54e4ae2061b5d124a5548bd59eaf317ef
Alex Eagle <alexeagle@google.com>
2018-03-01 17:50:40 +0000

    ec445b5 ci: speed up lint job on CircleCI (#22526)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-ab790f3');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-ec445b5');
 //# sourceMappingURL=version.js.map
