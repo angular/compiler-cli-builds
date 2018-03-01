64ac21790a0bd82c1395412b65c9dc7e9efbbee8
Alex Eagle <alexeagle@google.com>
2018-03-01 16:48:30 +0000

    0e311e3 build(aio): improve accuracy of code auto-linking (#22494)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-8bb2f5c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-0e311e3');
 //# sourceMappingURL=version.js.map
