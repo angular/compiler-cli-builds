41040681de7375d65c2189b1889fe5927375a82e
Alex Eagle <alexeagle@google.com>
2018-03-02 22:09:32 +0000

    ba8df8a build: update to latest bazel rules (#22558)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-0451fd9');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-ba8df8a');
 //# sourceMappingURL=version.js.map
