11cca216b51950ec8b22747651f1335e8dfb2c21
Alex Eagle <alexeagle@google.com>
2018-03-02 22:43:31 +0000

    d7e5d45 release: packageGroup should use scoped package names (#22538)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-ba8df8a');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-d7e5d45');
 //# sourceMappingURL=version.js.map
