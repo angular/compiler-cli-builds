71fd720622c54d433bfced171983c67a279e64cb
Alex Eagle <alexeagle@google.com>
2018-03-05 19:12:59 +0000

    58932c7 build(aio): indicate whether properties are read-only in API pages (#22584)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-5bb9f64');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-58932c7');
 //# sourceMappingURL=version.js.map
