698484e805b814c32f67206f6e3902a94fe35f1e
Alex Eagle <alexeagle@google.com>
2018-03-05 19:02:38 +0000

    5bb9f64 fixup! feat(aio): allow template to position embedded ToC (#22565)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-51ca643');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-5bb9f64');
 //# sourceMappingURL=version.js.map
