378e7a67ac344a5ad61e2449911be99a5a7c4f39
Alex Eagle <alexeagle@google.com>
2018-03-05 18:32:21 +0000

    51ca643 test(ivy): add injectAttribute spec (#22510)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-4f60968');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-51ca643');
 //# sourceMappingURL=version.js.map
