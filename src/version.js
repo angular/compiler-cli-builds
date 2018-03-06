eb9abf87f662c55db7dea6d7cd6cd4cac17b23af
Alex Eagle <alexeagle@google.com>
2018-03-06 18:01:48 +0000

    d0db9de docs: fix cli-quickstart doc and specs (#22338)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-4c40812');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-d0db9de');
 //# sourceMappingURL=version.js.map
