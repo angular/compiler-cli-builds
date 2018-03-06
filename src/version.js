b288e27e69c79d39945469e435866ac3359e3680
Alex Eagle <alexeagle@google.com>
2018-03-06 05:45:00 +0000

    94bc277 fix(aio): tidy up embedded ToC styling (#22570)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-2a1e3d1');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-94bc277');
 //# sourceMappingURL=version.js.map
