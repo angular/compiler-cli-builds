6b06a54a26a37c2643121cc19b80efc0abc80b63
Alex Eagle <alexeagle@google.com>
2018-03-06 15:17:07 +0000

    2c75acc feat(ivy): add support for the ngProjectAs attribute (#22498)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-f86d8ae');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-2c75acc');
 //# sourceMappingURL=version.js.map
