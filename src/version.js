edd8109e44638782bba1aaf3e4ad800ac5cf9abb
Alex Eagle <alexeagle@google.com>
2018-03-06 23:26:46 +0000

    5876fb0 docs(aio): update deprecated Http reference to HttpClientModule, remove Http reference because another context is used (#21984)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.7-5b7b208');
+exports.VERSION = new compiler_1.Version('5.2.7-5876fb0');
 //# sourceMappingURL=version.js.map
