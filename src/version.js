ef1c070eeb8645d625e47a0deffabef31fae797e
Alex Eagle <alexeagle@google.com>
2018-03-06 16:17:28 +0000

    4c40812 fix(bazel): fixes for ng_package on Windows (#22597)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-fa974c7');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-4c40812');
 //# sourceMappingURL=version.js.map
