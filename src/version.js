12d72b3975e7b0cc84475fd2d49fd11e36848b3b
Alex Eagle <alexeagle@google.com>
2018-03-07 22:58:45 +0000

    ad8fb84 test: include router in public_api_guard tests (#22628)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.6-7816791');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.6-ad8fb84');
 //# sourceMappingURL=version.js.map
