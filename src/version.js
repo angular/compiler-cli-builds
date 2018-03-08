a84f9c9307e00e7a0aaaa847f07c90054b01ce92
Kara Erickson <karakara@google.com>
2018-03-08 05:09:30 +0000

    8fb34bc refactor(forms): deprecate ngModel usage on same field as formControl (#22633)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-6d1367d');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-8fb34bc');
 //# sourceMappingURL=version.js.map
