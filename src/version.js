d43bc424869e898557acb8df7fe15bf045bd4585
Kara Erickson <karakara@google.com>
2018-03-08 05:20:24 +0000

    f64ee15 feat(ivy): implement pipes (#22254)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-5d4fa7f');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f64ee15');
 //# sourceMappingURL=version.js.map
