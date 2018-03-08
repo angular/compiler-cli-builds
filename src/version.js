e437266a1ccc27828e3646a8e6c2c10c34458cfe
Kara Erickson <karakara@google.com>
2018-03-08 19:23:01 +0000

    2ca77d8 docs: refactor revert() and call to lifecylce hook, edit doc to changes (#22094)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-73c203f');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-2ca77d8');
 //# sourceMappingURL=version.js.map
