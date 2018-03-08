ee3714a0ef094948d195c5d76599d7c1b1d11729
Kara Erickson <karakara@google.com>
2018-03-08 05:17:03 +0000

    5d4fa7f test(ivy): add canonical examples of bindings on elements (#22403)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-8fb34bc');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-5d4fa7f');
 //# sourceMappingURL=version.js.map
