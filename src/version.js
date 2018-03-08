502f37ac6935cde2b57c797fb120e1ba878b9600
Kara Erickson <karakara@google.com>
2018-03-08 20:31:29 +0000

    2fee5cc test(ivy): add injection canonical specs (#22595)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f13f4db');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-2fee5cc');
 //# sourceMappingURL=version.js.map
