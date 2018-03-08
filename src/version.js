9ab8ae3841f799f3660fb1473ba7f13cc07f6c7e
Kara Erickson <karakara@google.com>
2018-03-08 19:20:18 +0000

    73c203f feat(ivy): support host attribute and property bindings (#22334)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-c499c8f');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-73c203f');
 //# sourceMappingURL=version.js.map
