be74ed49ed35d4b6a71781ee36708aaefa50b285
Kara Erickson <karakara@google.com>
2018-03-08 05:28:52 +0000

    8407fcc ci: double our cores on CircleCI (#22641)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f64ee15');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-8407fcc');
 //# sourceMappingURL=version.js.map
