271528dcfac37a681a8d4951019023aad1ac4daa
Kara Erickson <karakara@google.com>
2018-03-08 02:45:37 +0000

    6d1367d feat(ivy): provide sanitization methods which can be tree shaken (#22540)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-065bcc5');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-6d1367d');
 //# sourceMappingURL=version.js.map
