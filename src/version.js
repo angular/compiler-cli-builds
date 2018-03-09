90c46bb6f58345d0460fd7b60d37dc0b3f8d0f13
Kara Erickson <karakara@google.com>
2018-03-09 07:02:39 +0000

    b0b9ca3 feat(ivy): produce Renderer2 back-patching and factories. (#22506)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-5412e10');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-b0b9ca3');
 //# sourceMappingURL=version.js.map
