75a2ae98904beff16db47704b72dc7bc46875e4f
Kara Erickson <karakara@google.com>
2018-03-10 04:48:41 +0000

    fa451bc feat(ivy): support markForCheck (#22690)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-0d8deb0');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-fa451bc');
 //# sourceMappingURL=version.js.map
