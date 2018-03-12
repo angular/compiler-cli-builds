5c7400ca19eaf759baf07774ad2630362ca05016
Kara Erickson <karakara@google.com>
2018-03-12 05:40:38 +0000

    aa7dba2 feat(ivy): support checkNoChanges (#22710)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-40315be');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-aa7dba2');
 //# sourceMappingURL=version.js.map
