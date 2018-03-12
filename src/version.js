c980a81007e2b603ffb07e18c7e48c8be0cfaf95
Kara Erickson <karakara@google.com>
2018-03-12 23:46:30 +0000

    21e44c6 ci: add alexeagle to pullapprove root group (#22724) PR Close #22724

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f5d75d8');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-21e44c6');
 //# sourceMappingURL=version.js.map
