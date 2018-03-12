dec440d060ca29275bc0fe992ba72f119dce3b60
Kara Erickson <karakara@google.com>
2018-03-12 05:39:17 +0000

    86517f2 fix(router): correct over-encoding of URL fragment (#22687)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.8-6d9a4f8');
+exports.VERSION = new compiler_1.Version('5.2.8-86517f2');
 //# sourceMappingURL=version.js.map
