37c67ff951ba709159f97d862fb9634129e8f288
Kara Erickson <karakara@google.com>
2018-03-13 17:05:51 +0000

    58b94e6 feat(animations): expose `element` and `params` within transition matchers (#22693)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-db56836');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-58b94e6');
 //# sourceMappingURL=version.js.map
