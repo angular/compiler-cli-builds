aeeeb9889899399dd834b48082258e35c0a7dcc8
Kara Erickson <karakara@google.com>
2018-03-13 20:49:37 +0000

    bd9d4df refactor(ivy): remove inputsPropertyName (#22716)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-34e355a');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-bd9d4df');
 //# sourceMappingURL=version.js.map
