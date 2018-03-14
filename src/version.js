152d917ffa0f90946febaaf2a6be904b81798b5d
Kara Erickson <karakara@google.com>
2018-03-14 22:52:29 +0000

    5298b2b docs: add changelog for 5.2.9

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.8-fc6dfc2');
+exports.VERSION = new compiler_1.Version('5.2.9-5298b2b');
 //# sourceMappingURL=version.js.map
