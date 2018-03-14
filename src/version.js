109e1990ebde2a6a288a56b829241b2014d31126
Kara Erickson <karakara@google.com>
2018-03-14 18:13:25 +0000

    fe96caf fix(aio): constrain error logging to improve reporting (#22713)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.8-ad674da');
+exports.VERSION = new compiler_1.Version('5.2.8-fe96caf');
 //# sourceMappingURL=version.js.map
