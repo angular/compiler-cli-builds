4f18f1dcef97c512918643d0aba00eb864ea62af
Miško Hevery <misko@hevery.com>
2018-03-15 22:14:50 +0000

    02e6ac2 docs: incorporate suggestions and corrections from gkalpak (#21569)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-a011654');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-02e6ac2');
 //# sourceMappingURL=version.js.map
