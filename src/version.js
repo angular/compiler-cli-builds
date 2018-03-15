54afb1c7031707cf81a11ee9e4301ab8b4273ce9
Miško Hevery <misko@hevery.com>
2018-03-15 19:18:00 +0000

    88b3198 feat(bazel): change ng_package rule to APF v6 (#22782)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-6e5e819');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-88b3198');
 //# sourceMappingURL=version.js.map
