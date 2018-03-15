081b4867b86de16fbe8a253267b697b8316ff12f
Miško Hevery <misko@hevery.com>
2018-03-15 22:42:45 +0000

    44de10e feat: mark angular packages as side-effect free (#22785)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-0ebd577');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-44de10e');
 //# sourceMappingURL=version.js.map
