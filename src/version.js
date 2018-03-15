4e6575822680d1345d81d05383f6d22b88719f83
Miško Hevery <misko@hevery.com>
2018-03-15 19:22:59 +0000

    a011654 build: update to tsickle@0.27.2 (#22789)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-88b3198');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-a011654');
 //# sourceMappingURL=version.js.map
