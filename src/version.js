508393a8181fb4f016fa7c9e93ee11c91c9717b1
Miško Hevery <misko@hevery.com>
2018-03-16 04:40:32 +0000

    b149424 build(bazel): make ng_package auto generate package.json for secondary entry-points (#22806)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f9247e4');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-b149424');
 //# sourceMappingURL=version.js.map
