b00536a139f8f615fd1e9599217c60b96924ef5d
Miško Hevery <misko@hevery.com>
2018-03-16 04:38:57 +0000

    f9247e4 build: enable importHelpers in tsconfig (#22812)

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -6,11 +6,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./collector"));
-__export(require("./schema"));
-__export(require("./bundle_index_host"));
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./collector"), exports);
+tslib_1.__exportStar(require("./schema"), exports);
+tslib_1.__exportStar(require("./bundle_index_host"), exports);
 //# sourceMappingURL=index.js.map
--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-44de10e');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f9247e4');
 //# sourceMappingURL=version.js.map
