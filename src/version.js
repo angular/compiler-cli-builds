cdda39bfed438f822e7d0fae134f2d0bdcd17814
Miško Hevery <misko@hevery.com>
2018-03-16 18:50:50 +0000

    cedc04c docs(ivy): document project status (#22751)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-269c3a1');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-cedc04c');
 //# sourceMappingURL=version.js.map
