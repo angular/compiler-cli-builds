03941ebd963fb13f5a49d6e41c5fd41846d62fb9
Miško Hevery <misko@hevery.com>
2018-03-16 20:04:13 +0000

    688096b feat(elements): remove attribute/input from config (#22413)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-bafdad9');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-688096b');
 //# sourceMappingURL=version.js.map
