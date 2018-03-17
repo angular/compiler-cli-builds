9d7c18aee27d3f367dd04b80b1552fb268abb2cb
Miško Hevery <misko@hevery.com>
2018-03-17 05:41:20 +0000

    f258ec6 docs(ivy): update status of ivy (#22834)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-5d82d8d');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f258ec6');
 //# sourceMappingURL=version.js.map
