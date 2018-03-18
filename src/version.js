b71eecd42e76ae3c917784d830b021455dc18e07
Igor Minar <igor@angular.io>
2018-03-18 16:54:34 +0000

    ce63dc6 feat: update the package output of build.sh to APF v6 (#22808)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-f258ec6');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-ce63dc6');
 //# sourceMappingURL=version.js.map
