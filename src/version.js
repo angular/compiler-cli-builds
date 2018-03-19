5d3f77bb884983727c0d514a99600344847fc0ff
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:52:30 +0000

    1a0cb21 refactor(ivy): remove unnecessary binding from hello world (#22848)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-0bede54');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-1a0cb21');
 //# sourceMappingURL=version.js.map
