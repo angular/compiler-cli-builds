554c834ffacddd58d60773a85f3597a1e4a5e9b5
Miško Hevery <misko@misko-ubi.cbf.corp.google.com>
2018-03-19 21:58:23 +0000

    bfe077a fix(bazel): correct expected outs for external sources in ng_module (#22755)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-1a0cb21');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-bfe077a');
 //# sourceMappingURL=version.js.map
