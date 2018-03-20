cc3c2a5c23896fd9c5823aa8dce2208b513eaead
Igor Minar <igor@angular.io>
2018-03-20 20:54:55 +0000

    689f351 build: expose flatModuleOutFile option on ng_module (#22814)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-67f570c');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-689f351');
 //# sourceMappingURL=version.js.map
