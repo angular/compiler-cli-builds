d32ae3fcfe22c83ee61b71b4ab2d0c3c7a290063
Miško Hevery <misko@hevery.com>
2018-03-20 04:08:11 +0000

    2b3de63 fix(upgrade): two-way binding and listening for event (#22772)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-5c387a7');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-2b3de63');
 //# sourceMappingURL=version.js.map
