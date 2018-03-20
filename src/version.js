7300f44bcafbc21573da1c860ac10923e8d2a8f6
Igor Minar <igor@angular.io>
2018-03-20 20:56:40 +0000

    7d095b9 fix: correct several esm2015 entry-points in package.jsons (#22892)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-689f351');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-7d095b9');
 //# sourceMappingURL=version.js.map
