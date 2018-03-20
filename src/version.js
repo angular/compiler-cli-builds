cb91ade993fa16c155c94a71d6ba2121de1b3745
Igor Minar <igor@angular.io>
2018-03-20 22:49:41 +0000

    3cc5c2e build: update to rxjs@6.0.0-beta.0 (#22887)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-e6c731f');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-3cc5c2e');
 //# sourceMappingURL=version.js.map
