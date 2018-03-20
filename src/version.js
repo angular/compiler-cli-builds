9886edfbb426a9f4f46533eb3f06457f8bd847ef
Igor Minar <igor@angular.io>
2018-03-20 05:13:47 +0000

    4648597 build(aio): temporarily pin elements to 6.0.0-beta.8 (#22573)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-beta.7-2b3de63');
+exports.VERSION = new compiler_1.Version('6.0.0-beta.7-4648597');
 //# sourceMappingURL=version.js.map
