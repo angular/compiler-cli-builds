a62018de908b0cc3c02109c350750a5d26213dd3
Igor Minar <igor@angular.io>
2018-03-21 06:34:18 +0000

    7cd8e8d build: update version numbers in ./scripts/release/post-check-next

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-73261a8');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-7cd8e8d');
 //# sourceMappingURL=version.js.map
