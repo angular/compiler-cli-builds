b5c5140f727c6620e9b1dc27b1ab9bba773e49ff
Igor Minar <igor@angular.io>
2018-03-21 20:35:47 +0000

    8c2a578 fix(service-worker): fix LruList bugs (#22769)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-4f2c51f');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-8c2a578');
 //# sourceMappingURL=version.js.map
