d52234e0de8cf55604d1b02142037a077d4343b8
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:48:59 +0000

    3498106 build: ts-api-guardian reuses root node_modules (#22894)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-e44f69c');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-3498106');
 //# sourceMappingURL=version.js.map
