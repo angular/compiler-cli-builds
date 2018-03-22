235171a08c281ea9915da9004ee464013fbd50a8
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:26:24 +0000

    e5e1b0d fix(animations): treat numeric state name values as strings (#22923)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-99711b1');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-e5e1b0d');
 //# sourceMappingURL=version.js.map
