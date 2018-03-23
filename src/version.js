5df04f08160e73d8c7419e23ecd8591d6ee1c042
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:28:29 +0000

    8d1e640 docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-49f6d1d');
+exports.VERSION = new compiler_1.Version('5.2.9-8d1e640');
 //# sourceMappingURL=version.js.map
