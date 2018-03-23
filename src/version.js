8cc8de86347ef0f634d8b543094b82d29a9b54c1
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:47:44 +0000

    49396ca refactor(ivy): move directives into separate array (#22918)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-8ca26a9');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-49396ca');
 //# sourceMappingURL=version.js.map
