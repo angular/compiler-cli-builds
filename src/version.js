aab9b040a12e278402a1a81519d5275e085c47c7
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:50:13 +0000

    5ab9d4d test(compiler): allow asserting matching identifier names (#22835)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-49396ca');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-5ab9d4d');
 //# sourceMappingURL=version.js.map
