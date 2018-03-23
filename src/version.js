459f76d60a8f3ad9fd4d03257de87a554d715dad
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:34:33 +0000

    4be8b3f docs: update available platforms for `test.sh` (#22958)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-8d1e640');
+exports.VERSION = new compiler_1.Version('5.2.9-4be8b3f');
 //# sourceMappingURL=version.js.map
