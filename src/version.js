25e4807a838bb196e91610b4e243513b957df4eb
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:11:16 +0000

    8ca26a9 ci(compiler): run compiler tests in bazel (#22900)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-3498106');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-8ca26a9');
 //# sourceMappingURL=version.js.map
