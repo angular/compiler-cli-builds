df3a37d5101354227311b8df8fc732dbe5c52d25
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 19:30:19 +0000

    65f296a test(compiler): assert translation names pattern (#22960)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-b9cbe83');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-65f296a');
 //# sourceMappingURL=version.js.map
