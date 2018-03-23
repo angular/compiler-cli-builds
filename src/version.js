e98dc0b179dce9592134f82dda90c0c30e0e7566
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 19:27:44 +0000

    b9cbe83 docs(ivy): update STATUS.md with compiler work breakdown (#22874)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-60d9983');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-b9cbe83');
 //# sourceMappingURL=version.js.map
