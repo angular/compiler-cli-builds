cb9db9cb3d134bf10f723c0887c1c81e1bb7a1c5
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 20:53:13 +0000

    d7f4aa6 docs(common): add `HttpParamsOptions` to the public API (#20332)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-4be8b3f');
+exports.VERSION = new compiler_1.Version('5.2.9-d7f4aa6');
 //# sourceMappingURL=version.js.map
