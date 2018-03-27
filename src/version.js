8445854ce60fde5f74dc5faeaabe02979535be97
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 19:34:07 +0000

    776bb82 build: update browserstack key (#23026)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-d7f4aa6');
+exports.VERSION = new compiler_1.Version('5.2.9-776bb82');
 //# sourceMappingURL=version.js.map
