d4f60accbb07df602d03708481f958f1bcd2490b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:23:14 +0000

    d9dc46e fix(service-worker): ignore invalid `only-if-cached` requests (#22883)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-e2e80ec');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-d9dc46e');
 //# sourceMappingURL=version.js.map
