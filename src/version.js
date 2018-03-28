7e63c232b9740f147b89a3b9ac736584f19c702a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:26:39 +0000

    4e004f3 ci(compiler-cli): run compiler-cli tests in bazel (#22997)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-d9dc46e');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-4e004f3');
 //# sourceMappingURL=version.js.map
