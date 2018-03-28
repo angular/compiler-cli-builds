4972dcacd22d140b0bb24725cb4a13fd2ae364a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 16:38:43 +0000

    bd024c0 feat(compiler): lower @NgModule ids if needed (#23031)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-0b348c8');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-bd024c0');
 //# sourceMappingURL=version.js.map
