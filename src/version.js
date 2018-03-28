fc8847363bf634ca7df9e445d366f002da817765
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 16:24:44 +0000

    0b348c8 build: fix bazel stamping (#22965)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-e1ea7ed');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-0b348c8');
 //# sourceMappingURL=version.js.map
