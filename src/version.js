16a06e7ee89430b96d7125f0c8ad75de76ee1f81
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:19:32 +0000

    4f7c369 fix(compiler): fix support for html-like text in translatable attributes (#23053)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-0d4fe38');
+exports.VERSION = new compiler_1.Version('5.2.9-4f7c369');
 //# sourceMappingURL=version.js.map
