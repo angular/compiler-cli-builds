8b4cc7b8df8f75fdf01876c8f7ab4954ffbc19a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:54:15 +0000

    de0b13d docs: update for hero service name change (#22920)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-28058b7');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-de0b13d');
 //# sourceMappingURL=version.js.map
