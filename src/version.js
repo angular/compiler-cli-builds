978b48dfe9e9645911448af9697b23fe2e91f9d5
Igor Minar <igor@angular.io>
2018-03-30 01:07:31 +0000

    45e090b fix(bazel): pass --global option to rollup (#23073)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-0d9140c');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-45e090b');
 //# sourceMappingURL=version.js.map
