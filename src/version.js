c5188c55688778daa3447e294df180d9ae2bcef3
Igor Minar <igor@angular.io>
2018-03-30 21:26:23 +0000

    1527878 release(bazel): change publish-next script to publish bazel artifacts (#23097)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-22cb2c9');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-1527878');
 //# sourceMappingURL=version.js.map
