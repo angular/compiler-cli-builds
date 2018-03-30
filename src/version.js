74218adcbb7b58c61db6c3d9cb17cc94a8e50137
Igor Minar <igor@angular.io>
2018-03-30 03:58:37 +0000

    439030f fix(bazel): complete the rollup globals list for all angular entrypoints (#23080)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.0-481b22e');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.0-439030f');
 //# sourceMappingURL=version.js.map
