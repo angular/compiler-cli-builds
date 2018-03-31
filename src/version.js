088eba564ab9f2c7a24bddbccc453d213aaff9b1
Igor Minar <igor@angular.io>
2018-03-31 00:52:08 +0000

    43d6202 build(aio): update to angular@6.0.0-rc.1 (#23101)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-d6bf71a');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-43d6202');
 //# sourceMappingURL=version.js.map
