dd0f0d709e0c079ed98e9d2062573bc8d8651502
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:43:02 +0000

    ae76eec fix(upgrade): propagate return value of resumeBootstrap (#22754)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-f43fba6');
+exports.VERSION = new compiler_1.Version('5.2.9-ae76eec');
 //# sourceMappingURL=version.js.map
