b74eefe209b7e9e67b9c07481782aceb42edf086
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 18:13:58 +0000

    7ca7720 fix(common): `locales` are not being shipped (#23136)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-580f05b');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-7ca7720');
 //# sourceMappingURL=version.js.map
