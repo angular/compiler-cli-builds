03cd1c3ffa49c318da2873cd156912bee1977f01
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:50:22 +0000

    5c8340a refactor(ivy): misc refactoring (#23154)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-46eadb5');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-5c8340a');
 //# sourceMappingURL=version.js.map
