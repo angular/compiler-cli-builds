07127b508df643517e6f2b6647ea8adb3ccf249e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:53:44 +0000

    23cc3ef fix(forms): improve error message for invalid value accessors (#22731)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-550433a');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-23cc3ef');
 //# sourceMappingURL=version.js.map
