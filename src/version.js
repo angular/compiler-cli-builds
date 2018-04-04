30c9c89f9a82dcf4a09ae709134c5098f8dbfd66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:52:24 +0000

    f48e215 build(service-worker): properly build npm_package with Bazel (#23090)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-f99cb5c');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-f48e215');
 //# sourceMappingURL=version.js.map
