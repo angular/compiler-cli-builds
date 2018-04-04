a842474ba8b99e5de6c2a97e66fa19c34b3bc77f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:11:48 +0000

    46eadb5 build: fix common locales in npm package (#23153)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-412b85b');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-46eadb5');
 //# sourceMappingURL=version.js.map
