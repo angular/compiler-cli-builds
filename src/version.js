745d37b7c2691c120c246da9d6ce0e211fb4da27
Igor Minar <igor@angular.io>
2018-04-05 17:48:44 +0000

    a50ce65 refactor(aio): remove unused images (#23018)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-8aa49ac');
+exports.VERSION = new compiler_1.Version('5.2.9-a50ce65');
 //# sourceMappingURL=version.js.map
