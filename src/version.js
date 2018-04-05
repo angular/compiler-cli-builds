389b8152f1ecaf20afaf69eb704b455a76c0eccc
Igor Minar <igor@angular.io>
2018-04-05 17:38:23 +0000

    79cecf9 fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-ac316be');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-79cecf9');
 //# sourceMappingURL=version.js.map
