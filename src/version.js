ecfcc3b50332792252227d2e8687d8bbc6a9e460
GitHub <noreply@github.com>
2018-04-05 06:39:08 +0000

    5db9ab1 docs: fix skipTemplateCodeGen -> skipTemplateCodegen typo in the AOT compiler guide

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-7c03961');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-5db9ab1');
 //# sourceMappingURL=version.js.map
