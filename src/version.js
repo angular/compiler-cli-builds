ef265053d3e3f36cc9c60adff07af6109d12e5cd
Igor Minar <igor@angular.io>
2018-04-05 17:29:26 +0000

    8aa49ac fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-3900c36');
+exports.VERSION = new compiler_1.Version('5.2.9-8aa49ac');
 //# sourceMappingURL=version.js.map
