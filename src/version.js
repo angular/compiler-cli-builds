9b75fbd3944c4ac4554e900612d105ca4a37ce34
Igor Minar <igor@angular.io>
2018-04-05 16:54:38 +0000

    92821e3 docs(forms): remove unnecessary `{@link Injector}` jsdoc tags (#23186)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.1-8ea15b4');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.1-92821e3');
 //# sourceMappingURL=version.js.map
