1df753f0dce2fd34673bc3ddcfaebe4212ab17b2
Igor Minar <igor@angular.io>
2018-04-06 21:58:33 +0000

    195193b docs(compiler): update ngtsc/ngcc design doc with more details (#23174)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.2-3a9d916');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.2-195193b');
 //# sourceMappingURL=version.js.map
