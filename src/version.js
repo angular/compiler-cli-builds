0e253e7cc009fc0204bdb0704dfea7f3b95bb8aa
Igor Minar <igor@angular.io>
2018-04-07 05:16:25 +0000

    1fac5f4 docs: recommend best practice for providers (#22934)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-33f8120');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-1fac5f4');
 //# sourceMappingURL=version.js.map
