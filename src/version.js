f7d5113fdae4f4f5313200312e8e3a711faa083a
Igor Minar <igor@angular.io>
2018-04-09 22:41:24 +0000

    5a298b1 fix(common): workaround UMD/webpack limitation (#23271)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-30a6861');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-5a298b1');
 //# sourceMappingURL=version.js.map
