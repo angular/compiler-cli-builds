2217dd2c33090e961edb633f231147e5bcb98aae
Igor Minar <igor@angular.io>
2018-04-09 22:46:34 +0000

    60e8392 docs(core): update directives documentation (#23255)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-f4c56f4');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-60e8392');
 //# sourceMappingURL=version.js.map
