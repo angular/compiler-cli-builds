13b26e2677553e176c2f3ca20120154876bcdb47
Igor Minar <igor@angular.io>
2018-04-10 20:41:15 +0000

    c973830 refactor(ivy): clean projection support (#23287)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-f1db789');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-c973830');
 //# sourceMappingURL=version.js.map
