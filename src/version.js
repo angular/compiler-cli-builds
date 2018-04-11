f48516b4247944596f5fe4d8dddcf767d7f1ad6f
Victor Berchet <victor@suumit.com>
2018-04-11 22:56:57 +0000

    a4ac872 build: use rxjs@6.0.0-terrific-rc.3 (#23327)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-d1e33d2');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-a4ac872');
 //# sourceMappingURL=version.js.map
