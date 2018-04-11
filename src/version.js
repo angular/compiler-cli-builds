fb3e4936115fa1a58f6d0a21a13189d415b8139a
Victor Berchet <victor@suumit.com>
2018-04-11 22:55:26 +0000

    d1e33d2 build: disambiguate rollup progress message (#23318)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-0d516f1');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-d1e33d2');
 //# sourceMappingURL=version.js.map
