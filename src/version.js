2eaf02fd0976a977168b072eb8a055f901473351
Igor Minar <igor@angular.io>
2018-04-11 06:22:30 +0000

    b3a10e0 build: update Bazel dependency to 0.11.1 (#23297)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-0cb4f12');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-b3a10e0');
 //# sourceMappingURL=version.js.map
