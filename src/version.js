aa926e069a25dc99d103be028fd7bac7cbab7595
Igor Minar <igor@angular.io>
2018-04-12 01:38:09 +0000

    37d2cb4 feat(elements): add schematics (#23298)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.3-ce40e85');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.3-37d2cb4');
 //# sourceMappingURL=version.js.map
