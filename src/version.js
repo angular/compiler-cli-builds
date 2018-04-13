cdb207c22832cfe82c02d4c599f5a4b43601b5a2
Igor Minar <igor@angular.io>
2018-04-13 20:46:07 +0000

    21f3301 refactor: ensure all 'TODO's are consistent (#23252)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-a3204f8');
+exports.VERSION = new compiler_1.Version('5.2.9-21f3301');
 //# sourceMappingURL=version.js.map
