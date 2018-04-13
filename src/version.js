8ea559016ab0b3ef258d0aaca2aeb42626b053fc
Igor Minar <igor@angular.io>
2018-04-13 15:38:36 +0000

    eac36d7 test(aio): fix `DocViewerComponent` tests (#23359)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-74b203a');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-eac36d7');
 //# sourceMappingURL=version.js.map
