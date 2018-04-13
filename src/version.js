7aa32f5d9da497a141959d57ed5edb69c5f93f88
Igor Minar <igor@angular.io>
2018-04-13 08:27:29 +0000

    74b203a docs(elements): add ng-add docs to elements guide (#23350)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-c2a53bb');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-74b203a');
 //# sourceMappingURL=version.js.map
