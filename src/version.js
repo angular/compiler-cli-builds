c09a9963497203f2422510a644b90c3819e290b5
Igor Minar <igor@angular.io>
2018-04-13 06:43:32 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-993eeab');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-75febe7');
 //# sourceMappingURL=version.js.map
