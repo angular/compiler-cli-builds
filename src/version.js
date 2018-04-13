5abba39834e885b24da16a5d39ff09ca0f616687
Igor Minar <igor@angular.io>
2018-04-13 06:38:14 +0000

    dabc076 docs: fix typo in injected variable name (#23315)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var compiler_1 = require("@angular/compiler");
 /**
  * @stable
  */
-exports.VERSION = new compiler_1.Version('5.2.9-4ea8b17');
+exports.VERSION = new compiler_1.Version('5.2.9-dabc076');
 //# sourceMappingURL=version.js.map
