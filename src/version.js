9d688c218c200229d1b3399c2c2a66547a22a2bd
Igor Minar <igor@angular.io>
2018-04-13 21:42:34 +0000

    490772e docs(aio): Update file tree to match Angular CLI V6 structure (#23367)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-e7ef027');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-490772e');
 //# sourceMappingURL=version.js.map
