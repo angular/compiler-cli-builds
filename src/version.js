f47728079c0a187353a51caa75bdd6bcbd4de92d
Igor Minar <igor@angular.io>
2018-04-13 20:43:56 +0000

    6c2c958 fix(service-worker): let `*` match 0 characters in globs (#23339)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-639d52f');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-6c2c958');
 //# sourceMappingURL=version.js.map
