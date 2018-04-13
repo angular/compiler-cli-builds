60258deb67d9f0b3bec50974b657ca727ec250db
Igor Minar <igor@angular.io>
2018-04-13 07:51:55 +0000

    3bd682f docs(aio): Add link to Japanese localization (#20630) PR Close #20630

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-2bb7838');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-3bd682f');
 //# sourceMappingURL=version.js.map
