92beba7208fa197f2f1f7319eed79010f6a66ee2
Igor Minar <igor@angular.io>
2018-04-14 11:11:43 +0000

    815ae29 build: add '@angular/elements' the framework package group

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-8f690c9');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-815ae29');
 //# sourceMappingURL=version.js.map
