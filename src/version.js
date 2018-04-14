f88a731135854971fc32745a6c620a13af444acc
Igor Minar <igor@angular.io>
2018-04-14 10:53:42 +0000

    8f690c9 docs: add changelog for 6.0.0-rc.5

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-674c3de');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-8f690c9');
 //# sourceMappingURL=version.js.map
