9205424bf9eac074e0aa0070d70bed499163432d
Igor Minar <igor@angular.io>
2018-04-14 04:53:31 +0000

    33630dd fix(ivy): workaround for tsickle bug (#23379)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.4-d1177c7');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.4-33630dd');
 //# sourceMappingURL=version.js.map
