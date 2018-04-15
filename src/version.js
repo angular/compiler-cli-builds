b74b1111ffcd6aec4e212d946eca4bdc583c11dd
Igor Minar <igor@angular.io>
2018-04-15 04:02:43 +0000

    b64a276 refactor(ivy): make return value of define(Component|Directive|Pipe|Injector|Injectable) private (#23371) (#23383)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-815ae29');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-b64a276');
 //# sourceMappingURL=version.js.map
