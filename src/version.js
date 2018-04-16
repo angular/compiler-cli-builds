d8f98cac79d4c242a9df27083d2de208583eb85b
Igor Minar <igor@angular.io>
2018-04-16 09:33:49 +0000

    84fc1a3 docs: add changelog for the 4.4.7 release

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.6-2c5cf19');
+exports.VERSION = new core_1.Version('4.4.7-84fc1a3');
 //# sourceMappingURL=version.js.map
