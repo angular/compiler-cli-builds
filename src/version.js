c783daf99da403ce84b1fac66a086a47d6b33652
Igor Minar <igor@angular.io>
2018-04-16 16:21:37 +0000

    9c29127 docs: document how to enable service worker using ng add command (#23348)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-4361560');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-9c29127');
 //# sourceMappingURL=version.js.map
