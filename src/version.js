a3a7a9730bf72903398acb69c9269f08317b1ed5
Jason Aden <jasonaden@google.com>
2018-04-17 21:23:57 +0000

    5a1ddee refactor(ivy): speed up bound text nodes (#23386)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-7f612fc');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-5a1ddee');
 //# sourceMappingURL=version.js.map
