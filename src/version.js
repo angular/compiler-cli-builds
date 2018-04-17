115d52b21250b2b14c6352bb1310844f17b9fb60
Jason Aden <jasonaden@google.com>
2018-04-17 21:12:22 +0000

    010a4ef docs(aio): Fixed typo in 'comparing observables -> Observabled compared (#23407) to arrays' section.

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-c3280b2');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-010a4ef');
 //# sourceMappingURL=version.js.map
