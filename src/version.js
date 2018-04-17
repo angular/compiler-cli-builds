a90cac56daed6d1ffc405b9d3eb1994c31d2899d
Jason Aden <jasonaden@google.com>
2018-04-17 21:35:54 +0000

    06f8656 docs(aio): Cleanup examples with edits from Igor/George (#23234)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-7de69ba');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-06f8656');
 //# sourceMappingURL=version.js.map
