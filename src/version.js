b441c44ff366c960694aa4da5a1dd03e7ca35594
Jason Aden <jasonaden@google.com>
2018-04-17 21:39:33 +0000

    fd48e53 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-fe312cc');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-fd48e53');
 //# sourceMappingURL=version.js.map
