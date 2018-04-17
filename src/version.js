f85bafb0c9691808990357443d3bbc5500c64dcf
Jason Aden <jasonaden@google.com>
2018-04-17 21:28:13 +0000

    7be7abd refactor(animations): use a const enum to avoid compilation side effects (#23402)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-5a1ddee');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-7be7abd');
 //# sourceMappingURL=version.js.map
