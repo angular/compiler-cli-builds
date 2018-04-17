83b68b503dde61cdf7b7580f79f69db8ac3d541d
Jason Aden <jasonaden@google.com>
2018-04-17 21:17:02 +0000

    7de69ba ci(aio): fix `aio-monitoring` tests (#23390)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-010a4ef');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-7de69ba');
 //# sourceMappingURL=version.js.map
