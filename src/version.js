bb612c7489d301e7710a32f0e4bd0c02e496baaf
Jason Aden <jasonaden@google.com>
2018-04-17 21:35:39 +0000

    fe312cc docs(aio): Cleanup examples with edits from Igor/George (#23234)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-7be7abd');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-fe312cc');
 //# sourceMappingURL=version.js.map
