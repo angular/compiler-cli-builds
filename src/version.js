0a470e655a82ac47ee7ad74deccaa7af8f85c1ac
Victor Berchet <victor@suumit.com>
2018-04-18 22:12:44 +0000

    acf6781 test(core): add a symbols test for renderer2 code (#23436)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-fd48e53');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-acf6781');
 //# sourceMappingURL=version.js.map
