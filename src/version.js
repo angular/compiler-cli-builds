7c83fd47a66ffd7e040b0e1803bd188b97555c49
Victor Berchet <victor@suumit.com>
2018-04-19 23:55:35 +0000

    9757347 feat(ivy): add an IVY local the compiler which avoids analyzeModule (#23441)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-1d1e75e');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-9757347');
 //# sourceMappingURL=version.js.map
