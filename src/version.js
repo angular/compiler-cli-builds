5d99aeae4d96e97582b60456a041a248c460e856
Victor Berchet <victor@suumit.com>
2018-04-20 02:22:57 +0000

    ca776c5 fix(compiler): handle undefined annotation metadata (#23349)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-f2563ca');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-ca776c5');
 //# sourceMappingURL=version.js.map
