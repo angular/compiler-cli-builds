1d1bedc71e1c3610380c6e24c15c608736cd0c8f
Victor Berchet <victor@suumit.com>
2018-04-20 23:33:37 +0000

    4662878 refactor(compiler): refactor template binding parsing (#23460)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-ca776c5');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-4662878');
 //# sourceMappingURL=version.js.map
