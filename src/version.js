9141cebead3b004c4439fd0b5e7298b892960a59
Victor Berchet <victor@suumit.com>
2018-04-22 19:20:23 +0000

    ba47997 style(compiler): fix lint issues (#23480)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-6761a64');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-ba47997');
 //# sourceMappingURL=version.js.map
