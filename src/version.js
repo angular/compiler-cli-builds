042927f55fa442464b64bd6870c95c80484a6d9e
Victor Berchet <victor@suumit.com>
2018-04-23 21:03:58 +0000

    391bfce docs(aio): Add UpgradingAngularJS to education resources (#23169)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-151fb66');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-391bfce');
 //# sourceMappingURL=version.js.map
