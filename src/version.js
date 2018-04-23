257bc146377ebf069e57897514f335fc8be2060d
Victor Berchet <victor@suumit.com>
2018-04-23 17:31:28 +0000

    999ab0a ci: add alxhub as owner to a few packages (#23510)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-ba47997');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-999ab0a');
 //# sourceMappingURL=version.js.map
