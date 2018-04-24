94939d1d6c728b6be8e4c7a53fc8bb83d3661f3b
Victor Berchet <victor@suumit.com>
2018-04-24 22:10:54 +0000

    04c18ac docs: fix typo (#23514)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-1b26dd8');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-04c18ac');
 //# sourceMappingURL=version.js.map
