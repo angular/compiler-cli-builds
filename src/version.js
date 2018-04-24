836c20e52cdd1988d9a055d476e85dc1da66d9ed
Victor Berchet <victor@suumit.com>
2018-04-24 21:24:33 +0000

    0bc8443 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-db17231');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-0bc8443');
 //# sourceMappingURL=version.js.map
