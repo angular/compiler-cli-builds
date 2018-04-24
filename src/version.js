87b8099fe444a82dc31f58d3b6ef8d69f545ac80
Victor Berchet <victor@suumit.com>
2018-04-24 22:30:46 +0000

    adf6235 docs: corrected spelling of "ambient".

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-04c18ac');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-adf6235');
 //# sourceMappingURL=version.js.map
