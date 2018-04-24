c6392e9cffe82a77cde36ff9f10a2498efa8ada9
Victor Berchet <victor@suumit.com>
2018-04-24 21:58:48 +0000

    f721b06 style: format code

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-0bc8443');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-f721b06');
 //# sourceMappingURL=version.js.map
