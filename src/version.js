0aa86e009b97241a2d6b7f6f4fe2392c9cf877fd
Victor Berchet <victor@suumit.com>
2018-04-24 18:36:53 +0000

    540626a build(common): mark locales files as side-effect-full (#23509)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-391bfce');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-540626a');
 //# sourceMappingURL=version.js.map
