bc1b8338b65c87ed4953c2e6c9d64cba88ac4f20
Igor Minar <igor@angular.io>
2018-04-27 14:47:47 +0000

    5021bdd ci: add Brandon Roberts as an aio approver (#23417)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-64bf6ed');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-5021bdd');
 //# sourceMappingURL=version.js.map
