612b66bb159882174e789c51ed043e8eccd0cf4c
Igor Minar <igor@angular.io>
2018-04-27 15:27:07 +0000

    693a7ba test: add i18n to cli-hello-world integration test (#23527)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-91bf598');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-693a7ba');
 //# sourceMappingURL=version.js.map
