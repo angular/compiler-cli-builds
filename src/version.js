21b0b60299da67b6c8d1fc3c567c5796628f53c6
Igor Minar <igor@angular.io>
2018-04-27 15:10:09 +0000

    08e9290 build(aio): add support for faster, unoptimized `serve` (#23569)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.5-098a3ef');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.5-08e9290');
 //# sourceMappingURL=version.js.map
