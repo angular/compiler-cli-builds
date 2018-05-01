4239232ff530954cab076242daa56866f2a6019f
Igor Minar <igor@angular.io>
2018-05-01 17:53:49 +0000

    31a435e docs: fix typo in tag name (`my-child` --> `app-child`) (#23606)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.6-3e92b22');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.6-31a435e');
 //# sourceMappingURL=version.js.map
