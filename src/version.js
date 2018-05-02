e4e99d9bb9e7186856d008f25661f48de13581b6
Igor Minar <igor@angular.io>
2018-05-02 22:40:17 +0000

    22eb8e2 build(aio): align stackblitz files with Angular CLI V6 (#23521)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.6-14138f6');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.6-22eb8e2');
 //# sourceMappingURL=version.js.map
