96aa1fd9daf1ede44426c7df6075023ff15b611d
Igor Minar <igor@angular.io>
2018-05-02 22:22:59 +0000

    14138f6 docs(elements): add intro connecting angular elements to custom elements (#23638)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-rc.6-f11daa2');
+exports.VERSION = new compiler_1.Version('6.0.0-rc.6-14138f6');
 //# sourceMappingURL=version.js.map
