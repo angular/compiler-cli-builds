9834951066ab295405427f98d57f295df0a893f8
Igor Minar <igor@angular.io>
2018-05-03 23:26:37 +0000

    4ddeb03 build(aio): use Angular 6.0.0 (#23687)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-947ea17');
+exports.VERSION = new compiler_1.Version('6.0.0-4ddeb03');
 //# sourceMappingURL=version.js.map
