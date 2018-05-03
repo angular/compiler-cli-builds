01aad45b8cd23c5eb38eeedd554de9935ae03f55
Igor Minar <igor@angular.io>
2018-05-03 22:29:58 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ var core_1 = require("@angular/core");
 /**
  * @stable
  */
-exports.VERSION = new core_1.Version('4.4.7-84fc1a3');
+exports.VERSION = new core_1.Version('4.4.7-b19216d');
 //# sourceMappingURL=version.js.map
