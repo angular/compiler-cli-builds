bdb7c888287cd15dd37f31293a71fe26144629b2
Igor Minar <igor@angular.io>
2018-05-03 21:49:15 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-5bd8c68');
+exports.VERSION = new compiler_1.Version('6.0.0-9027818');
 //# sourceMappingURL=version.js.map
