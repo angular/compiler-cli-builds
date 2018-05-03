a3a46d94c76ffbc4da3c209d7e74e6cd27410f29
Igor Minar <igor@angular.io>
2018-05-03 20:53:46 +0000

    5bd8c68 docs: improve the GitHub README.md, update links, etc

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-d0ccf5f');
+exports.VERSION = new compiler_1.Version('6.0.0-5bd8c68');
 //# sourceMappingURL=version.js.map
