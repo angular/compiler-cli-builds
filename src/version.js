09e0467b5d6d9d66e99df5314e1f3a12309df800
Igor Minar <igor@angular.io>
2018-05-04 23:00:35 +0000

    07f2098 feat(aio): add v6 release notification (#23690)

--- src/version.js
+++ src/version.js
@@ -16,5 +16,5 @@ const compiler_1 = require("@angular/compiler");
 /**
  *
  */
-exports.VERSION = new compiler_1.Version('6.0.0-9b53a6e');
+exports.VERSION = new compiler_1.Version('6.0.0-07f2098');
 //# sourceMappingURL=version.js.map
