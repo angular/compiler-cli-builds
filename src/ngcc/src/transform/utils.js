364453a29bf5cda869cbdf3b5c27f2f151335ed8
Misko Hevery <misko@hevery.com>
2018-09-05 20:13:54 +0000

    dcad054 build: update ngcontainer to node 10.9.0 (#25812)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.4+57.sha-397b047';
+    exports.NGCC_VERSION = '7.0.0-beta.4+59.sha-dcad054';
     /**
      * Represents an entry point to a package or sub-package.
      *
