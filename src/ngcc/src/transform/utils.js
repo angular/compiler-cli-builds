e33ac581d955eac79eba6168a3bcde612217865d
Misko Hevery <misko@hevery.com>
2018-09-05 20:13:01 +0000

    1bc6f64 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.4+56.sha-d96e962';
+    exports.NGCC_VERSION = '7.0.0-beta.4+58.sha-1bc6f64';
     /**
      * Represents an entry point to a package or sub-package.
      *
