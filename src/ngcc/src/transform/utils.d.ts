8738ec75970b69678fd4fe74c9de9ab2425187e7
Igor Minar <igor@angular.io>
2018-09-06 22:40:52 +0000

    34ec924 build: update to Node 10 (#25822)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5";
+export declare const NGCC_VERSION = "7.0.0-beta.5+1.sha-34ec924";
 /**
  * Represents an entry point to a package or sub-package.
  *
