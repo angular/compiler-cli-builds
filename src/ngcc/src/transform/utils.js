3e3bb796e9770b09bbc97bb4bae286a26d17730a
Igor Minar <igor@angular.io>
2018-09-06 23:02:19 +0000

    d6cd041 docs(service-worker): update http-server command (#25845) PR Close #25845

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+1.sha-34ec924';
+    exports.NGCC_VERSION = '7.0.0-beta.5+4.sha-d6cd041';
     /**
      * Represents an entry point to a package or sub-package.
      *
