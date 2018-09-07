996b8bd98810fb18ed83d869dc65fce961b46398
Igor Minar <igor@angular.io>
2018-09-07 23:46:26 +0000

    83a1334 refactor(ivy): migrate previousOrParentNode to use TNodes (#25829)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+8.sha-2a21ca0';
+    exports.NGCC_VERSION = '7.0.0-beta.5+9.sha-83a1334';
     /**
      * Represents an entry point to a package or sub-package.
      *
