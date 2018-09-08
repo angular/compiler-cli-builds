b3279ddbcee0502bba5934536620e8942a1aebdd
Igor Minar <igor@angular.io>
2018-09-08 00:11:59 +0000

    96eb79b build: add support for running builds outside of sandbox on Mac. (#25870)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+9.sha-83a1334";
+export declare const NGCC_VERSION = "7.0.0-beta.5+10.sha-96eb79b";
 /**
  * Represents an entry point to a package or sub-package.
  *
