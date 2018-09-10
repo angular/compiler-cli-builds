8ee297325738bbb970b92f1c82badcdf1aed5fe2
Igor Minar <igor@angular.io>
2018-09-10 19:14:28 +0000

    ed266da docs(aio): add ng-sq-ui to resources (#25874)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+11.sha-91d7993';
+    exports.NGCC_VERSION = '7.0.0-beta.5+13.sha-ed266da';
     /**
      * Represents an entry point to a package or sub-package.
      *
