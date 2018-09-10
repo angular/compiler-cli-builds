e4fc4b5bc998093f915ca91d1e51390a782398e3
Igor Minar <igor@angular.io>
2018-09-10 22:07:09 +0000

    e363388 feat(ivy): support animation @triggers in templates (#25849)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+13.sha-ed266da";
+export declare const NGCC_VERSION = "7.0.0-beta.5+14.sha-e363388";
 /**
  * Represents an entry point to a package or sub-package.
  *
--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+13.sha-ed266da';
+    exports.NGCC_VERSION = '7.0.0-beta.5+14.sha-e363388';
     /**
      * Represents an entry point to a package or sub-package.
      *
