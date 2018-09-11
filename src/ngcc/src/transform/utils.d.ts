06c1bd204ba1116a199e7edb4c9ba3b6841eb400
Igor Minar <igor@angular.io>
2018-09-11 15:11:15 +0000

    c5b594e docs(bazel): add info on options for nodejs_test rule (#25877)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+22.sha-86a3be8";
+export declare const NGCC_VERSION = "7.0.0-beta.5+23.sha-c5b594e";
 /**
  * Represents an entry point to a package or sub-package.
  *
