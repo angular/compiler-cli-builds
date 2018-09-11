18cfef741d5e2e73a2007fc1df6c8be7d8986581
Igor Minar <igor@angular.io>
2018-09-11 14:52:38 +0000

    d5bd86a fix(ivy): don't accidently read the inherited definition (#25736)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+20.sha-a9099e8';
+    exports.NGCC_VERSION = '7.0.0-beta.5+21.sha-d5bd86a';
     /**
      * Represents an entry point to a package or sub-package.
      *
