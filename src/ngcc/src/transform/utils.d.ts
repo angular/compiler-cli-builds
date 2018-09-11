d2e2bc129066d762c6927d76e27bc68e78e94810
Igor Minar <igor@angular.io>
2018-09-11 14:51:16 +0000

    d5bd86a fix(ivy): don't accidently read the inherited definition (#25736)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+20.sha-a9099e8";
+export declare const NGCC_VERSION = "7.0.0-beta.5+21.sha-d5bd86a";
 /**
  * Represents an entry point to a package or sub-package.
  *
