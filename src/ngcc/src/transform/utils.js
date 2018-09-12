bcd6ebaef4e30071807ec1ee0d7212deda7ca536
Igor Minar <igor@angular.io>
2018-09-12 00:02:25 +0000

    f2ba55f docs(router): change description of parameter usage for navigation functions (#21840)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var path_1 = require("path");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+24.sha-f3b552f';
+    exports.NGCC_VERSION = '7.0.0-beta.5+28.sha-f2ba55f';
     /**
      * Represents an entry point to a package or sub-package.
      *
