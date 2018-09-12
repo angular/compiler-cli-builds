7fb8bc3ec44b14af4fcaf554760f6129173f4b67
Igor Minar <igor@angular.io>
2018-09-12 00:24:56 +0000

    21009b0 fix(ivy): use proper sanitizer names (#25817)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+27.sha-00d3666";
+export declare const NGCC_VERSION = "7.0.0-beta.5+26.sha-21009b0";
 /**
  * Represents an entry point to a package or sub-package.
  *
