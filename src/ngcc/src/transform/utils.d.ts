50cefd36bd678d6aa43258e817967cda6fa489db
Ben Lesh <ben@benlesh.com>
2018-09-18 21:07:59 +0000

    f47f262 refactor(ivy): remove LNode.view (#25988)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+44.sha-9b1bb37";
+export declare const NGCC_VERSION = "7.0.0-beta.5+39.sha-f47f262";
 /**
  * Represents an entry point to a package or sub-package.
  *
