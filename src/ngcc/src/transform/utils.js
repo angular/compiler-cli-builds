c0b0b055967b728dba1a387a4539fd8cc16d8b02
Ben Lesh <ben@benlesh.com>
2018-09-18 21:15:05 +0000

    a53a559 ci: use locally built skylint and buildifier (#25917)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var fs_1 = require("fs");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+46.sha-56283ed';
+    exports.NGCC_VERSION = '7.0.0-beta.5+48.sha-a53a559';
     /**
      * Represents an entry point to a package or sub-package.
      *
