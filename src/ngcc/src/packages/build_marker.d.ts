534aad155d5caad58d49a3af204d946e9147e400
Ben Lesh <ben@benlesh.com>
2018-09-18 21:12:32 +0000

    56283ed build: update .nvmrc file to correct node version (#25992)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.5+50.sha-96ee898";
+export declare const NGCC_VERSION = "7.0.0-beta.5+46.sha-56283ed";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var fs_1 = require("fs");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+50.sha-96ee898';
+    exports.NGCC_VERSION = '7.0.0-beta.5+46.sha-56283ed';
     /**
      * Represents an entry point to a package or sub-package.
      *
