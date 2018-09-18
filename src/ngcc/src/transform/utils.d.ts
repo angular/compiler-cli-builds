4d30dd47b3ec9fae53165e559c0cc7f3230ee54f
Ben Lesh <ben@benlesh.com>
2018-09-18 21:26:21 +0000

    cbbad1b refactor(ivy): pre-factor: set explicit type parameters for ModuleWithProviders (#25970)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+53.sha-e952c65";
+export declare const NGCC_VERSION = "7.0.0-beta.5+51.sha-cbbad1b";
 /**
  * Represents an entry point to a package or sub-package.
  *
