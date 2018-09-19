fd95d98b3b0380eb3872f28500023d1d871c1477
Ben Lesh <ben@benlesh.com>
2018-09-19 18:26:19 +0000

    d0e710d docs: copy edit (#25582)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.5+56.sha-dd5e35e";
+export declare const NGCC_VERSION = "7.0.0-beta.5+59.sha-d0e710d";
 /**
  * Represents an entry point to a package or sub-package.
  *
