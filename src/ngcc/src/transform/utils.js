da3da35e8f810e8c9e43d863f59507c3a960ba11
Ben Lesh <ben@benlesh.com>
2018-09-19 18:19:18 +0000

    f91b045 docs(animations): updated animation docs (#24206)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var fs_1 = require("fs");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.5+54.sha-e8bab13';
+    exports.NGCC_VERSION = '7.0.0-beta.5+55.sha-f91b045';
     /**
      * Represents an entry point to a package or sub-package.
      *
