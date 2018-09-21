fbac545f7de5c7422834b3b8268261628b4163ae
Kara Erickson <karakara@google.com>
2018-09-21 18:05:29 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.6+27.sha-e7ade38";
+export declare const NGCC_VERSION = "7.0.0-beta.6+28.sha-e42c1b0";
 /**
  * Represents an entry point to a package or sub-package.
  *
