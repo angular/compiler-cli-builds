b73e86a823310984844b7642bfcc0deecda1b753
Kara Erickson <karakara@google.com>
2018-09-21 18:10:22 +0000

    a880686 fix(docs-infra): ensure that only search is removed from URL on click (#26056)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.6+29.sha-4809483";
+export declare const NGCC_VERSION = "7.0.0-beta.6+34.sha-a880686";
 /**
  * Represents an entry point to a package or sub-package.
  *
