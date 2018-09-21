298dad72019a2d11157ba38f4cbaa9a07376238a
Kara Erickson <karakara@google.com>
2018-09-21 01:41:06 +0000

    d5f47d6 refactor(ivy): special injection tokens should not be cached (#26048)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.6+25.sha-64aa670";
+export declare const NGCC_VERSION = "7.0.0-beta.6+26.sha-d5f47d6";
 /**
  * Represents an entry point to a package or sub-package.
  *
