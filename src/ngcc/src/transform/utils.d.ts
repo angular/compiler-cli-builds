3b7cb396de0c0be056f6701a2ddbc610befe49b0
Kara Erickson <karakara@google.com>
2018-09-21 18:05:13 +0000

    9931bd7 build(docs-infra): do not include license comment in first API doc (#26050)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.6+33.sha-026b60c";
+export declare const NGCC_VERSION = "7.0.0-beta.6+30.sha-9931bd7";
 /**
  * Represents an entry point to a package or sub-package.
  *
