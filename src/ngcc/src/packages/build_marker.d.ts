f5d4360209eaf9a7884ffa35758c0fd9775ec08e
Kara Erickson <karakara@google.com>
2018-09-21 18:03:57 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.6+27.sha-e7ade38";
+export declare const NGCC_VERSION = "7.0.0-beta.6+28.sha-e42c1b0";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
