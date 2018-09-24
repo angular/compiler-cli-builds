23afcd23b7d0ab2f8104853ecc7e70ce8e1d52f6
Kara Erickson <karakara@google.com>
2018-09-24 16:54:17 +0000

    47eb212 docs: fix Sajee info in contributors (#26063)

--- src/ngcc/src/transform/utils.d.ts
+++ src/ngcc/src/transform/utils.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/transform/utils" />
-export declare const NGCC_VERSION = "7.0.0-beta.6+51.sha-b8422b4";
+export declare const NGCC_VERSION = "7.0.0-beta.6+52.sha-47eb212";
 /**
  * Represents an entry point to a package or sub-package.
  *
