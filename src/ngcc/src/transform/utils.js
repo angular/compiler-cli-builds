c61ed063d00e3c4507f819b98b7366b858f090c7
Kara Erickson <karakara@google.com>
2018-09-24 16:56:12 +0000

    1e02cd9 docs: Fixes typo in FormArray (#26031) PR Close #26031

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var fs_1 = require("fs");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.6+52.sha-47eb212';
+    exports.NGCC_VERSION = '7.0.0-beta.6+54.sha-1e02cd9';
     /**
      * Represents an entry point to a package or sub-package.
      *
