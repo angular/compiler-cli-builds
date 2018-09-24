0aebacb8789314468138150ff474c81399c178fa
Kara Erickson <karakara@google.com>
2018-09-24 19:15:19 +0000

    82a14dc feat(ivy): provide groundwork for animations in core (#25234)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.6+25.sha-64aa670";
+export declare const NGCC_VERSION = "7.0.0-beta.6+35.sha-82a14dc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
