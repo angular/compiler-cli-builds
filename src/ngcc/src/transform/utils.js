6ffb8a90c450ac903a446fa64f4b068556be4690
Kara Erickson <karakara@google.com>
2018-09-24 19:12:24 +0000

    82a14dc feat(ivy): provide groundwork for animations in core (#25234)

--- src/ngcc/src/transform/utils.js
+++ src/ngcc/src/transform/utils.js
@@ -20,7 +20,7 @@
     var fs_1 = require("fs");
     var shelljs_1 = require("shelljs");
     var utils_1 = require("@angular/compiler-cli/src/ngcc/src/utils");
-    exports.NGCC_VERSION = '7.0.0-beta.6+25.sha-64aa670';
+    exports.NGCC_VERSION = '7.0.0-beta.6+35.sha-82a14dc';
     /**
      * Represents an entry point to a package or sub-package.
      *
