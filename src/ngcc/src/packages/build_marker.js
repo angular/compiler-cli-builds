278c497506c760e4807dc62cfb916ba4a0c1c286
Kara Erickson <karakara@google.com>
2018-09-25 18:51:50 +0000

    482e12c build: remove obsolete comment in `env.sh` (#25819)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-beta.6+58.sha-0c34471';
+    exports.NGCC_VERSION = '7.0.0-beta.6+59.sha-482e12c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
