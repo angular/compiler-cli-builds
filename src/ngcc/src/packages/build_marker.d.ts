ea97a84df1525ac1acfe7f5ebc024ee25aadc817
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 21:42:52 +0000

    63b795a refactor(ivy): make sure that test bed symbols are imported from ivy_switch (#26121)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.6+80.sha-15a2b8f";
+export declare const NGCC_VERSION = "7.0.0-beta.7+30.sha-63b795a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
