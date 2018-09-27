5b992788ae38bd6f7e13ed81720358e0badcc728
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 21:43:50 +0000

    9523991 refactor(router): cleanup to navigation stream for readability and documentation (#25740)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.7+35.sha-4d544bc";
+export declare const NGCC_VERSION = "7.0.0-beta.7+46.sha-9523991";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
