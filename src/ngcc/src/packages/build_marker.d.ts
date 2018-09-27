d80177133a857869f4ad020c8e142548b8f19d1d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 20:00:36 +0000

    4d544bc style: update gulp task to format untracked and diff files separately (#24969)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-beta.7+31.sha-aac08e0";
+export declare const NGCC_VERSION = "7.0.0-beta.7+35.sha-4d544bc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
