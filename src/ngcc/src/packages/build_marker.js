06ce6c7e58ddb310fe4d667f4d6af7b9bde608df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 16:49:38 +0000

    eeebe28 ci(docs-infra): run the  script in the correct folder

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-beta.7+19.sha-ffc6e19';
+    exports.NGCC_VERSION = '7.0.0-beta.7+20.sha-eeebe28';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
