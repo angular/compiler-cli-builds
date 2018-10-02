7e56cb124f8ff968b7fba14a028a8145ed60246c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-02 21:25:54 +0000

    d216a46 release: ts_api_guardian 0.4.0 (#26206)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.0+1.sha-68fadd9";
+export declare const NGCC_VERSION = "7.0.0-rc.0+24.sha-d216a46";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
