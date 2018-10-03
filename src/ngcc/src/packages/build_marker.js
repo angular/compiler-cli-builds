d06cb7b0e505b151254c0d81d69e57b88ad790fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 20:07:25 +0000

    cb59d87 feat(ivy): compiler support to generate QUERY_READ_FROM_NODE calls (#26171)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.0+24.sha-d216a46';
+    exports.NGCC_VERSION = '7.0.0-rc.0+25.sha-cb59d87';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
