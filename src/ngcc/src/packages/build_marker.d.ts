2f70659430d285f723215d18c8152090e0749a79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 23:12:10 +0000

    7306799 refactor(ivy): flatten LInjector into LViewData (#26220)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.0+25.sha-cb59d87";
+export declare const NGCC_VERSION = "7.0.0-rc.0+26.sha-7306799";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
