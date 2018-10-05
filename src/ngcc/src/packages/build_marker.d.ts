eef0f7cfda5091240ac9d9bca6874052d0b06f68
Jason Aden <jasonaden@google.com>
2018-10-05 23:17:52 +0000

    ccceff5 docs: add fakeAsync test new feature document (#23117)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.0+59.sha-f543d71";
+export declare const NGCC_VERSION = "7.0.0-rc.0+62.sha-ccceff5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
