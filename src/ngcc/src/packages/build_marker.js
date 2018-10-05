54def1a7ca34800bd09e3085792e51d88a190daa
Jason Aden <jasonaden@google.com>
2018-10-05 21:44:28 +0000

    4006c9b docs: fix spelling errors (#26213)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.0+48.sha-8f25321';
+    exports.NGCC_VERSION = '7.0.0-rc.0+55.sha-4006c9b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
