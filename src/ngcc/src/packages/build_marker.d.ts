7effbee334500794331a983129bdb94213e8dad7
Jason Aden <jasonaden@google.com>
2018-10-05 21:43:20 +0000

    4006c9b docs: fix spelling errors (#26213)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.0+48.sha-8f25321";
+export declare const NGCC_VERSION = "7.0.0-rc.0+55.sha-4006c9b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
