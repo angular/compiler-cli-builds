4265795955c4cadac0e9af95c30e1e3eddabcdf6
Jason Aden <jasonaden@google.com>
2018-10-05 23:14:30 +0000

    ab6f055 ci(docs-infra): show custom 404 page on preview server (for consistency) (#26199)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.0+58.sha-3a5cb1c';
+    exports.NGCC_VERSION = '7.0.0-rc.0+57.sha-ab6f055';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
