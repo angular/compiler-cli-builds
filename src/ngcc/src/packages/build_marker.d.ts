d2873b193a8faa30e1f8879893dfec59c26637ad
Jason Aden <jasonaden@google.com>
2018-10-05 23:17:30 +0000

    55d54c7 fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.0+62.sha-ccceff5";
+export declare const NGCC_VERSION = "7.0.0-rc.0+63.sha-55d54c7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
