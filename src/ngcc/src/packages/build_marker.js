2eb3f7318028d0d2fc39e55137f9b1ac9c908871
Jason Aden <jasonaden@google.com>
2018-10-08 21:12:25 +0000

    9320ec0 build(docs-infra): upgrade Angular to 7.0.0-rc.0 and TypeScript to 3.1.1 (#26202)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.0+65.sha-5a31bde';
+    exports.NGCC_VERSION = '7.0.0-rc.0+71.sha-9320ec0';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
