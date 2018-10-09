803d78374cd5aed74942a3b41d6238f3cd252e6e
Jason Aden <jasonaden@google.com>
2018-10-09 00:04:18 +0000

    83302d1 fix(ivy): ensure ngcc compiles `@angular/core` with correct internal imports (#26236)

--- src/ngcc/src/host/esm5_host.d.ts
+++ src/ngcc/src/host/esm5_host.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/host/esm5_host" />
 import * as ts from 'typescript';
 import { ClassMember, Decorator, FunctionDefinition } from '../../../ngtsc/host';
-import { Fesm2015ReflectionHost } from './fesm2015_host';
+import { Fesm2015ReflectionHost, ParamInfo } from './fesm2015_host';
 /**
  * ESM5 packages contain ECMAScript IIFE functions that act like classes. For example:
  *
@@ -27,7 +27,7 @@ import { Fesm2015ReflectionHost } from './fesm2015_host';
  *
  */
 export declare class Esm5ReflectionHost extends Fesm2015ReflectionHost {
-    constructor(checker: ts.TypeChecker);
+    constructor(isCore: boolean, checker: ts.TypeChecker);
     /**
      * Check whether the given node actually represents a class.
      */
@@ -44,28 +44,41 @@ export declare class Esm5ReflectionHost extends Fesm2015ReflectionHost {
      * - The function declaration inside the IIFE, which is eventually returned and assigned to the
      *   outer variable.
      *
-     * @param node The top level declaration that represents an exported class or the function
+     * @param node the top level declaration that represents an exported class or the function
      *     expression inside the IIFE.
-     * @returns The symbol for the node or `undefined` if it is not a "class" or has no symbol.
+     * @returns the symbol for the node or `undefined` if it is not a "class" or has no symbol.
      */
     getClassSymbol(node: ts.Node): ts.Symbol | undefined;
     /**
      * Parse a function declaration to find the relevant metadata about it.
+     *
      * In ESM5 we need to do special work with optional arguments to the function, since they get
      * their own initializer statement that needs to be parsed and then not included in the "body"
      * statements of the function.
+     *
      * @param node the function declaration to parse.
+     * @returns an object containing the node, statements and parameters of the function.
      */
     getDefinitionOfFunction<T extends ts.FunctionDeclaration | ts.MethodDeclaration | ts.FunctionExpression>(node: T): FunctionDefinition<T>;
     /**
      * Find the declarations of the constructor parameters of a class identified by its symbol.
+     *
      * In ESM5 there is no "class" so the constructor that we want is actually the declaration
      * function itself.
+     *
+     * @param classSymbol the class whose parameters we want to find.
+     * @returns an array of `ts.ParameterDeclaration` objects representing each of the parameters in
+     * the class's constructor or null if there is no constructor.
      */
     protected getConstructorParameterDeclarations(classSymbol: ts.Symbol): ts.ParameterDeclaration[];
     /**
-     * Constructors parameter decorators are declared in the body of static method of the constructor
-     * function in ES5. Note that unlike ESM2105 this is a function expression rather than an arrow
+     * Get the parameter type and decorators for the constructor of a class,
+     * where the information is stored on a static method of the class.
+     *
+     * In this case the decorators are stored in the body of a method
+     * (`ctorParatemers`) attached to the constructor function.
+     *
+     * Note that unlike ESM2015 this is a function expression rather than an arrow
      * function:
      *
      * ```
@@ -76,7 +89,28 @@ export declare class Esm5ReflectionHost extends Fesm2015ReflectionHost {
      *   { type: undefined, decorators: [{ type: Inject, args: [INJECTED_TOKEN,] },] },
      * ]; };
      * ```
+     *
+     * @param paramDecoratorsProperty the property that holds the parameter info we want to get.
+     * @returns an array of objects containing the type and decorators for each parameter.
      */
-    protected getConstructorDecorators(classSymbol: ts.Symbol): (Map<string, ts.Expression> | null)[];
+    protected getParamInfoFromStaticProperty(paramDecoratorsProperty: ts.Symbol): ParamInfo[] | null;
+    /**
+     * Reflect over a symbol and extract the member information, combining it with the
+     * provided decorator information, and whether it is a static member.
+     * @param symbol the symbol for the member to reflect over.
+     * @param decorators an array of decorators associated with the member.
+     * @param isStatic true if this member is static, false if it is an instance property.
+     * @returns the reflected member information, or null if the symbol is not a member.
+     */
     protected reflectMember(symbol: ts.Symbol, decorators?: Decorator[], isStatic?: boolean): ClassMember | null;
+    /**
+     * Find statements related to the given class that may contain calls to a helper.
+     *
+     * In ESM5 code the helper calls are hidden inside the class's IIFE.
+     *
+     * @param classSymbol the class whose helper calls we are interested in. We expect this symbol
+     * to reference the inner identifier inside the IIFE.
+     * @returns an array of statements that may contain helper calls.
+     */
+    protected getStatementsForClass(classSymbol: ts.Symbol): ts.Statement[];
 }
