6137a5e01a631f44df13b057aae07e63bba3ad1e
Jason Aden <jasonaden@google.com>
2018-10-10 21:46:10 +0000

    912f3d1 fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.0+102.sha-e26cb21';
+    exports.NGCC_VERSION = '7.0.0-rc.0+103.sha-912f3d1';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
