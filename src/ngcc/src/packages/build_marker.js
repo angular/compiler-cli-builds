4e8781abdbe276d618e5c1bec7252d747481e015
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:04 +0000

    062fe5c docs: fix transpiles link in dependency injection (#26250)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+21.sha-6a64ac4';
+    exports.NGCC_VERSION = '7.0.0-rc.1+20.sha-062fe5c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
