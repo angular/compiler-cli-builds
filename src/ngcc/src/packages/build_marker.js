6c0a59fff46b854cb90c4e5620b0a010ca2f98fe
Miško Hevery <misko@hevery.com>
2018-10-11 21:52:36 +0000

    0a3f817 refactor(platform-browser): remove type assertion on console.profileEnd (#26303)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+18.sha-bd186c7';
+    exports.NGCC_VERSION = '7.0.0-rc.1+22.sha-0a3f817';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
