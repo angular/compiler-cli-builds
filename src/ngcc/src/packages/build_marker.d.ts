4cc1c8611c85d8650c8f3213c80d12fd0cdf830f
Miško Hevery <misko@hevery.com>
2018-10-11 21:53:27 +0000

    062fe5c docs: fix transpiles link in dependency injection (#26250)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+21.sha-6a64ac4";
+export declare const NGCC_VERSION = "7.0.0-rc.1+20.sha-062fe5c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
