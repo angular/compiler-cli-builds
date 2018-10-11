3b5fcfab29fb7c577915e417a352fbe208fb13fd
Miško Hevery <misko@hevery.com>
2018-10-11 20:27:54 +0000

    053bf27 refactor(ivy): use context discovery in TestBed implementation (#26211)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+4.sha-39f42ba";
+export declare const NGCC_VERSION = "7.0.0-rc.1+5.sha-053bf27";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
