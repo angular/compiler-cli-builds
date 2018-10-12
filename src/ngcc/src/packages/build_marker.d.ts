f891ab3d59341cc7afb8b3b67a8ab9a8e1862626
Miško Hevery <misko@hevery.com>
2018-10-12 16:41:20 +0000

    ea20ae6 build(docs-infra): only show name in 'inherited from' section (#26387)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+32.sha-7777a99";
+export declare const NGCC_VERSION = "7.0.0-rc.1+33.sha-ea20ae6";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
