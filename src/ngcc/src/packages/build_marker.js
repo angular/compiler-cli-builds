05476cdcb7e89823972d429c1b7a76b31a2e513d
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:25 +0000

    9e32dc7 build: upgrade `@types/jasminewd2` to 2.0.4 (#26139)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+36.sha-bab5b68';
+    exports.NGCC_VERSION = '7.0.0-rc.1+38.sha-9e32dc7';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
