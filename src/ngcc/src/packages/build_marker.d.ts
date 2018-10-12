4c07b8af73a0be1fc4200d6dcf81c76059d883e5
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:31 +0000

    b2db32b docs: getting started updated for accuracy and style (#26093)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+37.sha-a19b690";
+export declare const NGCC_VERSION = "7.0.0-rc.1+39.sha-b2db32b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
