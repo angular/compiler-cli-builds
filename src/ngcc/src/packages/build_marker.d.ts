bdb54b4ea575048b5612d8671e6cef67438ebd72
Miško Hevery <misko@hevery.com>
2018-10-12 04:32:41 +0000

    735dfd3 refactor(ivy): replace LNode.dynamicLContainerNode with flat LContainers (#26407)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+27.sha-071934e";
+export declare const NGCC_VERSION = "7.0.0-rc.1+26.sha-735dfd3";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
