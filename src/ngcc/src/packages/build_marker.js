16ee0d7217b1378ee0f5d3b5237a34af95b9de1f
Miško Hevery <misko@hevery.com>
2018-10-12 16:31:52 +0000

    7777a99 fix(docs-infra): highlight current CLI command in navigation menu (#26388)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+31.sha-9ebb4c0';
+    exports.NGCC_VERSION = '7.0.0-rc.1+32.sha-7777a99';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
