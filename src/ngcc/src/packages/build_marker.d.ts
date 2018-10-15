3ba99dbd434e91d1d8a4e7e9022e5dfdf07154d3
Miško Hevery <misko@hevery.com>
2018-10-15 18:55:37 +0000

    e76a570 refactor(ivy): remove LNode (#26426)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+41.sha-931e603";
+export declare const NGCC_VERSION = "7.0.0-rc.1+43.sha-e76a570";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
