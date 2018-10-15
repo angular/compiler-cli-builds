d567cb285da2da90edf135bae29aeff38937882e
Miško Hevery <misko@hevery.com>
2018-10-15 17:39:55 +0000

    45732e5 fix(ivy): fix reference to pipeBind instruction with 2 args (#26451)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+39.sha-b2db32b";
+export declare const NGCC_VERSION = "7.0.0-rc.1+40.sha-45732e5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
