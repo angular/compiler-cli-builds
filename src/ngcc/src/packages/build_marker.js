8ee3ee2daca79b02a46fb608740166a0db96f908
Miško Hevery <misko@hevery.com>
2018-10-16 22:11:27 +0000

    b115374 build: fix deps for running modules/benchmarks/src/largetable/render3:perf (#26482)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+51.sha-7d82053';
+    exports.NGCC_VERSION = '7.0.0-rc.1+52.sha-b115374';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
