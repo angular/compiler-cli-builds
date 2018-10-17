981cd4278754384ab2d65b1835612a5e723c6e38
Misko Hevery <misko@hevery.com>
2018-10-17 19:12:06 +0000

    516515d build(docs-infra): do not add extra space after links in tables (#26505)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+67.sha-948f507';
+    exports.NGCC_VERSION = '7.0.0-rc.1+68.sha-516515d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
