1709578c01666e792546b313d5550dded83e6c94
Misko Hevery <misko@hevery.com>
2018-10-17 19:27:08 +0000

    b29e709 fix(docs-infra): rename "title" by "header" to avoid unwanted tooltips (#26396)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+64.sha-d769b44";
+export declare const NGCC_VERSION = "7.0.0-rc.1+62.sha-b29e709";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
