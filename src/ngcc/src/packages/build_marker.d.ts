db4b9cf50674ab22aa48c7d7e7cd3b9372ae4ecc
Miško Hevery <misko@hevery.com>
2018-10-17 03:49:25 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+43.sha-e76a570";
+export declare const NGCC_VERSION = "7.0.0-rc.1+55.sha-2326b9c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
