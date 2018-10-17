d813f4dff13047db256f64bcdfeaf599f72b8132
Miško Hevery <misko@hevery.com>
2018-10-17 20:44:31 +0000

    b49b274 fix(aio): add relative to app level routes section (#26504)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+72.sha-9997ab5";
+export declare const NGCC_VERSION = "7.0.0-rc.1+74.sha-b49b274";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
