3b2ee3fd568480bf6f7d2d47188ac9a906cb7253
Misko Hevery <misko@hevery.com>
2018-10-17 20:38:55 +0000

    8a3fd58 feat(ivy): i18n compiler support for i18nStart and i18nEnd instructions (#26442)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+64.sha-fa8e633";
+export declare const NGCC_VERSION = "7.0.0-rc.1+71.sha-8a3fd58";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
