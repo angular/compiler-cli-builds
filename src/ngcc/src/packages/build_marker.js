f7ad4ed5a768015e25341a74e468d444684dc9f3
Miško Hevery <misko@hevery.com>
2018-10-17 21:10:27 +0000

    f8195e5 fix(ivy): stub TestBed.compileComponents implementation (#26506)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+71.sha-8a3fd58';
+    exports.NGCC_VERSION = '7.0.0-rc.1+85.sha-f8195e5';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
