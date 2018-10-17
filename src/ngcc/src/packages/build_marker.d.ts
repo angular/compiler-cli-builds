68d3964fe31067b10ed37b2a8d5a1acc4eeae497
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:10 +0000

    ecd473b fix(docs-infra): don't hide contributor links on devices that cannot hover (#26410)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+69.sha-3fa876c";
+export declare const NGCC_VERSION = "7.0.0-rc.1+72.sha-ecd473b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
