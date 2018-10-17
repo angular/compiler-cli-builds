7b914b705be76403818ff9effe01d99168d78f5b
Miško Hevery <misko@hevery.com>
2018-10-17 20:42:03 +0000

    a609bf5 docs: fix formatting of PULL_REQUEST_TEMPLATE.md (#26512)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+85.sha-f8195e5";
+export declare const NGCC_VERSION = "7.0.0-rc.1+86.sha-a609bf5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
