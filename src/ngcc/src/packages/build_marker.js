d3bf4f458462c2d10fcaa8c4a62344980440f022
Misko Hevery <misko@hevery.com>
2018-10-17 19:26:19 +0000

    beebf7f docs: change links to cli wiki to link to new aio docs (#26489)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+74.sha-0ef1f7e';
+    exports.NGCC_VERSION = '7.0.0-rc.1+75.sha-beebf7f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
