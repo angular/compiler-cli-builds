98de6a9b60d16ce6caf24c7958b590797003964e
Misko Hevery <misko@hevery.com>
2018-10-17 04:52:15 +0000

    fa8e633 feat(ivy): enhance [style] and [class] bindings to be animation aware (#26096)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+55.sha-2326b9c";
+export declare const NGCC_VERSION = "7.0.0-rc.1+64.sha-fa8e633";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
