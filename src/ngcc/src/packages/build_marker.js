1225ade25e4d75fec4869bd41431b3adc57b59d2
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:53 +0000

    0ef1f7e docs: change links to cli wiki to link to new aio docs files m to z (#26492)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+72.sha-ecd473b';
+    exports.NGCC_VERSION = '7.0.0-rc.1+74.sha-0ef1f7e';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
