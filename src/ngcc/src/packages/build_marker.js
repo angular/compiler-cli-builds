aee62b70812ea6acb7ba6ff742276b1cc06b9c7a
Misko Hevery <misko@hevery.com>
2018-10-17 19:25:20 +0000

    ab9e114 docs: remove for example code (#26519)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+62.sha-b29e709';
+    exports.NGCC_VERSION = '7.0.0-rc.1+66.sha-ab9e114';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
