bd3bdf7817669234898a2edca4b1971e430e4a5a
Miško Hevery <misko@hevery.com>
2018-10-17 20:43:17 +0000

    b49b274 fix(aio): add relative to app level routes section (#26504)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+72.sha-9997ab5';
+    exports.NGCC_VERSION = '7.0.0-rc.1+74.sha-b49b274';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
