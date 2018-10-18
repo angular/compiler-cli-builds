cf5c4d02ebe0e5227ee7a2dcf96a4f471a058a60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:42:18 +0000

    95914a0 docs: update http to https where possible (#26509)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+93.sha-09cc458";
+export declare const NGCC_VERSION = "7.0.0-rc.1+96.sha-95914a0";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
