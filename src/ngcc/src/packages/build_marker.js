f7a4675a9cf6ca1c77cb1b5f41fa6f5faffaee52
Miško Hevery <misko@hevery.com>
2018-10-18 20:55:59 +0000

    331989c docs: add release announcement URL

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+100.sha-bbf96db';
+    exports.NGCC_VERSION = '7.0.0-rc.1+101.sha-331989c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
