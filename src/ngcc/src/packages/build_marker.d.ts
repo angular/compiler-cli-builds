3d5a370ea56feebce14477297036066be11acf77
Miško Hevery <misko@hevery.com>
2018-10-18 17:31:24 +0000

    3b183ce build(docs-infra): allow `""` as empty region in `{@example}` tags (#26514)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+75.sha-1035c6a";
+export declare const NGCC_VERSION = "7.0.0-rc.1+78.sha-3b183ce";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
