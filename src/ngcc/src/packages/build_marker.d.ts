c0e4baf6422bcfd3498ce482b1aac44055878e63
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:42:09 +0000

    09cc458 docs: type fix (#26386)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+92.sha-efe0c5f";
+export declare const NGCC_VERSION = "7.0.0-rc.1+93.sha-09cc458";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
