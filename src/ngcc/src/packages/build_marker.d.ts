a7d29c5fead693a9907e29063b4761baa49b2a29
Miško Hevery <misko@hevery.com>
2018-10-18 19:46:49 +0000

    f85a969 docs: fix incomplete sentence in CHANGELOG.md

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0+5.sha-4f14a09";
+export declare const NGCC_VERSION = "7.0.0+7.sha-f85a969";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
