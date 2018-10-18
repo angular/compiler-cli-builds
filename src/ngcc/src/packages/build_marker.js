8fef330f21184f59412d1c5ddb91a3c73515e1c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:29 +0000

    8d28fe9 docs: update http to https where possible (#26509)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0+1.sha-a403c4f';
+    exports.NGCC_VERSION = '7.0.0+4.sha-8d28fe9';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
