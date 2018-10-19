8dd2a9691c204dcad59b1768edfc2deeeaa8e56d
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:58:06 +0000

    31022cb feat(ivy): generate .ngsummary.js shims (#26495)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+115.sha-1700bd6';
+    exports.NGCC_VERSION = '7.0.0-rc.1+118.sha-31022cb';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
