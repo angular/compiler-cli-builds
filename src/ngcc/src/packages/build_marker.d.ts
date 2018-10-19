3a704131b5ed87f2e0684d4034d263480a43e5b7
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:12:34 +0000

    07fc4c2 docs(service-worker): updated browser support for Service Worker (#26408)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+108.sha-aa55d88";
+export declare const NGCC_VERSION = "7.0.0-rc.1+107.sha-07fc4c2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
