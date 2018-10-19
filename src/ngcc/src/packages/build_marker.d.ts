d0ea833ec0dbfc0b843269b5ad1038a34a58feb4
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:20:11 +0000

    716d887 docs: add Katerina Skroumpelou to GDE resources (#26547)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+110.sha-4c0ad52";
+export declare const NGCC_VERSION = "7.0.0-rc.1+112.sha-716d887";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
