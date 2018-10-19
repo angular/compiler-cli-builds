fd2a2b1eedd6b079aca15f15fe3b0ec0ffbe6c7b
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:34:51 +0000

    5b4cf38 refactor(bazel): remove workaround for TS bug (#26516)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+102.sha-d4cee51";
+export declare const NGCC_VERSION = "7.0.0-rc.1+111.sha-5b4cf38";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
