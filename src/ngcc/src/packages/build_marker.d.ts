2ce1c24e37e601f8df0f8b3540a836f9b7ce90ab
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:56:54 +0000

    31022cb feat(ivy): generate .ngsummary.js shims (#26495)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+115.sha-1700bd6";
+export declare const NGCC_VERSION = "7.0.0-rc.1+118.sha-31022cb";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
