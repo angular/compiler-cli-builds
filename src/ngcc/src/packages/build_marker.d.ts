9c412a9de3f7d905482332f58c59f8bc9ea30ed2
Igor Minar <igor@angular.io>
2018-10-20 06:18:50 +0000

    c8adef2 build: remove manual tags from platform jasmine tests (#26606)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0+52.sha-778e1c2";
+export declare const NGCC_VERSION = "7.0.0+53.sha-c8adef2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
