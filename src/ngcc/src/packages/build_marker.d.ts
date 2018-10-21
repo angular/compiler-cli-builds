31f725d693d2806b992d8a42529917d0c8036f81
Igor Minar <igor@angular.io>
2018-10-21 04:28:07 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+137.sha-4c2ce4e";
+export declare const NGCC_VERSION = "7.0.0-rc.1+156.sha-100c7ef.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
