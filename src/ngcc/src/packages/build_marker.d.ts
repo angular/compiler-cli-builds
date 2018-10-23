56307c143b0d0528a6e05f8674a6ea5329db7719
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:57:24 +0000

    929739c Revert "feat(router): add prioritizedGuardValue operator optimization and allowing UrlTree return from guard (#26478)" (#26700)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0+53.sha-c8adef2";
+export declare const NGCC_VERSION = "7.0.0+67.sha-929739c.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
