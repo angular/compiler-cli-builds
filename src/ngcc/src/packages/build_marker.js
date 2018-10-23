2c64c4b6e426a77e8e28ddd5f904034c1b98ee18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:57:06 +0000

    b52c97c ci: disable ivy CI jobs on the patch branch (#26702)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0+67.sha-929739c.with-local-changes';
+    exports.NGCC_VERSION = '7.0.0+68.sha-b52c97c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
