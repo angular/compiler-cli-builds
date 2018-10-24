1c14b513a77dd33cdfaa3650eca2a88f7f7768cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:40:45 +0000

    67789f1 release: cut the v7.1.0-beta.0 release

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0-rc.1+194.sha-6e16a17';
+    exports.NGCC_VERSION = '7.1.0-beta.0';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
