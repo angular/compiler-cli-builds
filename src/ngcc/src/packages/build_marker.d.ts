c23f045d7a19ef8549048855efc862298976ec32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:58:21 +0000

    4e91ead style: format (#26708)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0-rc.1+178.sha-ee0b857.with-local-changes";
+export declare const NGCC_VERSION = "7.0.0-rc.1+192.sha-4e91ead.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
