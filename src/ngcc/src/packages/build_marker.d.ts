5c963494a343cbbd706ef3502b55c9024d2ed011
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:11:39 +0000

    0238744 test(upgrade): make e2e tests for upgrade docs examples less flaky (#26726)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.0+87.sha-062af7a.with-local-changes";
+export declare const NGCC_VERSION = "7.0.1+4.sha-0238744.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.0+87.sha-062af7a.with-local-changes';
+    exports.NGCC_VERSION = '7.0.1+4.sha-0238744.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
