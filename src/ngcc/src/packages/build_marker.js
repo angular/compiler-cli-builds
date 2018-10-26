bc2d64c15b60e0e7bf5f508103077b14fbe856e5
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:59:58 +0000

    95993e1 fix(ivy): ensure pipes indices are referenced in styling bindings (#26755)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+42.sha-d2e6d69';
+    exports.NGCC_VERSION = '7.1.0-beta.0+44.sha-95993e1';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
