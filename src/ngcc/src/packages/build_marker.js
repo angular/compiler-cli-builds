3fa000d22fa78ab08162cfef429a91cb8249b233
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:45:51 +0000

    d2e6d69 docs: add ant design mobile of angular in resources (#26562)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+43.sha-d52d82d';
+    exports.NGCC_VERSION = '7.1.0-beta.0+42.sha-d2e6d69';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
