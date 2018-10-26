6585aac7dbd891d3c147780e8cf74e65a39f83f8
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:43:49 +0000

    d52d82d fix(ivy): injecting optional TemplateRef on element should not throw (#26664)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+41.sha-da4a28e";
+export declare const NGCC_VERSION = "7.1.0-beta.0+43.sha-d52d82d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
