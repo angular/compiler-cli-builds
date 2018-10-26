f5c7bbde3c7402000ac56d0ec9c58d570ff3f25c
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:43:18 +0000

    da4a28e docs: fix indentation (#26623)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+37.sha-56f44be';
+    exports.NGCC_VERSION = '7.1.0-beta.0+41.sha-da4a28e';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
