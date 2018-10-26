da2fd99f468cefd787944f51ecf0433b6bacfc20
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 17:45:00 +0000

    cc5e4fb ci: move CircleCI env variables definition to shell script (#26786)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.1+11.sha-bda1e52.with-local-changes';
+    exports.NGCC_VERSION = '7.0.1+14.sha-cc5e4fb.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
