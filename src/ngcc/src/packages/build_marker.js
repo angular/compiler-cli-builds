3efff47e7eeab4613275e73aa6482eb5660c67c3
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 01:27:20 +0000

    2309650 build(docs-infra): upgrade cli command docs sources to 4faa81e25 (#26741)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.1+1.sha-8da6c36.with-local-changes';
+    exports.NGCC_VERSION = '7.0.1+5.sha-2309650.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
