c191f4e3ba339e73b3e023858b7b07487e6dd1ff
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:32:57 +0000

    c0bf222 ci: exclude ngcc from g3sync check (#26740)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+54.sha-d72d50d";
+export declare const NGCC_VERSION = "7.1.0-beta.0+55.sha-c0bf222";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
