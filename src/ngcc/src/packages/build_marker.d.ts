6fb431be190bbabd045d71f3cf961dda6c1a1031
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:30:40 +0000

    ef6f160 ci(docs-infra): notify caretaker about `aio_monitoring` failures (#26649)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+44.sha-95993e1";
+export declare const NGCC_VERSION = "7.1.0-beta.0+53.sha-ef6f160";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
