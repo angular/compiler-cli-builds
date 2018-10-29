0547b9f696d2256489b02bcac2bd819be9802630
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 18:23:37 +0000

    2a86927 fix(ivy): move HostListeners generation to factory function (#26480)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+55.sha-c0bf222';
+    exports.NGCC_VERSION = '7.1.0-beta.0+56.sha-2a86927';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
