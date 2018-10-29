6486ddd06a77ba25a5973960b1e3b8a83756851c
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 18:39:04 +0000

    1130e48 fix(ivy): host injection flag should not throw for embedded views (#26795)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+56.sha-2a86927';
+    exports.NGCC_VERSION = '7.1.0-beta.0+57.sha-1130e48';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
