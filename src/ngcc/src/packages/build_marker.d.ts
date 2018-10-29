d75d584278d3f30f87e9c81f513be140bf4ac1e9
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 23:13:14 +0000

    578e4c7 fix(ivy): compile queries in JIT mode (#26816)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+60.sha-ede65db";
+export declare const NGCC_VERSION = "7.1.0-beta.0+59.sha-578e4c7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+60.sha-ede65db';
+    exports.NGCC_VERSION = '7.1.0-beta.0+59.sha-578e4c7';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
