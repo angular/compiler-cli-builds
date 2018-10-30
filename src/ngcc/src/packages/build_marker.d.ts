538607b423cdb8a75f38a1c3235c7fef5bffc7e8
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:46:52 +0000

    8fc4ae5 build: use bazel version from node modules (#26691)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+67.sha-b95089d";
+export declare const NGCC_VERSION = "7.1.0-beta.0+69.sha-8fc4ae5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
