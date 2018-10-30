e68dc6d0139631c491feec1b580f6a837fed3ada
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:55:34 +0000

    634e9e9 docs: Removing duplicate lines (#26754)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+70.sha-87a7f2d";
+export declare const NGCC_VERSION = "7.1.0-beta.0+72.sha-634e9e9";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
