694182c2ea377b0350e6b50787ccb826465804b9
GitHub <noreply@github.com>
2018-10-31 20:32:21 +0000

    257ac83 fix(core): Remove static dependency from @angular/core to @angular/compiler (#26734) (#26879)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.1+54.sha-bc93d47.with-local-changes";
+export declare const NGCC_VERSION = "7.0.1+55.sha-257ac83";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
