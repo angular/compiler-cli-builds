69bb73fe6b55c79fa01ba7acafd5b5761019d331
GitHub <noreply@github.com>
2018-10-31 22:29:49 +0000

    2ac7292 fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.0+80.sha-774f6fb";
+export declare const NGCC_VERSION = "7.1.0-beta.0+81.sha-2ac7292";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
