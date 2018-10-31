d95421444a69c63a002813cf8550630e0703c5c7
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:24:59 +0000

    3041240 build: remove problematic `yarn` prefix for running bazel

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.1+55.sha-257ac83";
+export declare const NGCC_VERSION = "7.0.1+56.sha-3041240";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
