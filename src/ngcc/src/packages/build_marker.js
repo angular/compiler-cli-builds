88333b97af81af5a874635c5f4338558f23ab9c5
GitHub <noreply@github.com>
2018-10-31 22:29:29 +0000

    774f6fb fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.0+81.sha-2ac7292';
+    exports.NGCC_VERSION = '7.1.0-beta.0+80.sha-774f6fb';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
