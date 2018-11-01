b9f7d2bb2126a9e2790eb66160a6b5472d268f9f
Kara Erickson <karakara@google.com>
2018-11-01 22:13:33 +0000

    ea0a996 fix(ivy): init hooks should be called before host bindings (#26802)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+36.sha-18b6d58';
+    exports.NGCC_VERSION = '7.1.0-beta.1+39.sha-ea0a996';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
