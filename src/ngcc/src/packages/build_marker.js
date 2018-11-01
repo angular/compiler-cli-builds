c44030d6626f930472b64c31687da18441c3704e
Kara Erickson <karakara@google.com>
2018-11-01 21:09:59 +0000

    683d53d docs: add `Function Inlining` rules to `PERF_NOTES.md` (#26876)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+17.sha-a2929df';
+    exports.NGCC_VERSION = '7.1.0-beta.1+18.sha-683d53d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
