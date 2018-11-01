3d9921feec2629bc54e764a8dbdebacc28957afd
Kara Erickson <karakara@google.com>
2018-11-01 22:58:33 +0000

    e9e804f feat(forms): add updateOn option to FormBuilder (#24599)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+50.sha-aed95fd";
+export declare const NGCC_VERSION = "7.1.0-beta.1+51.sha-e9e804f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
