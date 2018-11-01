12a3188c216d9342c6da86bc39072a9c00e372c8
Kara Erickson <karakara@google.com>
2018-11-01 21:51:37 +0000

    5eb540c docs: Webcomponents activated as of firefox 63 (#26889) PR Close #26889

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.2+6.sha-7697650";
+export declare const NGCC_VERSION = "7.0.2+7.sha-5eb540c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
