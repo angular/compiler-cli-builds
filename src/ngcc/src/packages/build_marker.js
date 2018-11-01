ababfd7d9f05cb879549cf9058ec07f9042fee07
Kara Erickson <karakara@google.com>
2018-11-01 21:14:34 +0000

    2810482 build: update `canonical-path` dependency (#26719)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.2';
+    exports.NGCC_VERSION = '7.0.2+1.sha-2810482';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
