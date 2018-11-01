5d1642d4495a1b8bd7413590286681de4fb4ca3a
Kara Erickson <karakara@google.com>
2018-11-01 21:47:37 +0000

    3b9bc73 fix(ivy): host bindings after dirs without host bindings should work (#26801)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+31.sha-bc68b59";
+export declare const NGCC_VERSION = "7.1.0-beta.1+37.sha-3b9bc73";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
