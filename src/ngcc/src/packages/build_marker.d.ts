f21c7204e3321d6c38e106fdeb4341ef3b0db225
Kara Erickson <karakara@google.com>
2018-11-01 22:14:48 +0000

    ea0a996 fix(ivy): init hooks should be called before host bindings (#26802)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+36.sha-18b6d58";
+export declare const NGCC_VERSION = "7.1.0-beta.1+39.sha-ea0a996";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
