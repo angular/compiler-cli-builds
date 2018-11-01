ab3678f78cc86da489229102cdba145c4ba8bf3c
Kara Erickson <karakara@google.com>
2018-11-01 21:13:00 +0000

    eb5d308 build: update `canonical-path` dependency (#26719)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+18.sha-683d53d";
+export declare const NGCC_VERSION = "7.1.0-beta.1+19.sha-eb5d308";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
