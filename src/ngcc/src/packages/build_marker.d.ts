fb5e4fdcd41c33ca774e0b7b06af2e789507e130
Kara Erickson <karakara@google.com>
2018-11-01 23:22:47 +0000

    dc05385 fix(router): remove type bludgeoning of context and outlet when running CanDeactivate (#26496)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.2+7.sha-5eb540c";
+export declare const NGCC_VERSION = "7.0.2+9.sha-dc05385";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
