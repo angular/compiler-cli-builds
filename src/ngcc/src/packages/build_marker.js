030460812afb6352aef1cf95e178aadd9bd2cb0b
Kara Erickson <karakara@google.com>
2018-11-02 23:20:36 +0000

    7d2a746 build: remove ivy JIT mode (#26863)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+64.sha-516af6c';
+    exports.NGCC_VERSION = '7.1.0-beta.1+66.sha-7d2a746';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
