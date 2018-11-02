a92a2369df8627268c0e1859a70a8e83a4fe6d9c
Kara Erickson <karakara@google.com>
2018-11-02 18:01:04 +0000

    fe1c0d7 feat(docs-infra): add getting started widgets (#26059)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.2+9.sha-dc05385";
+export declare const NGCC_VERSION = "7.0.2+10.sha-fe1c0d7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
