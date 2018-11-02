d285f4e5f957426fef0d9da89562afd72819a628
Kara Erickson <karakara@google.com>
2018-11-02 18:04:18 +0000

    5c5b62c ci: remove redundant `start-xvfb` step (#26869)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.2+11.sha-0c10b57";
+export declare const NGCC_VERSION = "7.0.2+17.sha-5c5b62c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
