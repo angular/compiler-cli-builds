2b3dfc569cbad69f6c289e64727bd8448eb126e1
Kara Erickson <karakara@google.com>
2018-11-05 18:17:40 +0000

    86d41a1 docs: new GitHub Issue templates (#26918)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+67.sha-e6a0c45';
+    exports.NGCC_VERSION = '7.1.0-beta.1+68.sha-86d41a1';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
