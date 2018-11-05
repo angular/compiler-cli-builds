86c92b2412bf3a756b9a4bc9c7352ed8dc5a266d
Kara Erickson <karakara@google.com>
2018-11-05 19:41:50 +0000

    4e4bca6 Revert "fix(ivy): correct ngtsc path handling in Windows (#26703)"

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+70.sha-d0037b2';
+    exports.NGCC_VERSION = '7.1.0-beta.1+71.sha-4e4bca6';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
