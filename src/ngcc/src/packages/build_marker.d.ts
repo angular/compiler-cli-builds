52de9caf01143e5d3f3ef59e9b107ea948a65857
Kara Erickson <karakara@google.com>
2018-11-05 20:01:33 +0000

    f9a4abb fix: missing semi-colon and empty line (#26945)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+71.sha-4e4bca6";
+export declare const NGCC_VERSION = "7.1.0-beta.1+72.sha-f9a4abb";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
