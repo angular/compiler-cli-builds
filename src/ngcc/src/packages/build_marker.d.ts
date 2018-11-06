aa8b54e86755ececb71007e3a8010f9ab2a52208
Kara Erickson <karakara@google.com>
2018-11-06 20:10:32 +0000

    297c54e fix(ivy): support env where requestAnimationFrame is not available (#26779)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.1+78.sha-dc2464e";
+export declare const NGCC_VERSION = "7.1.0-beta.1+79.sha-297c54e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
