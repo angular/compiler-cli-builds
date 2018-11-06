6904b14e8f900bfd9eac3b6b7f353805ae62edf3
Kara Erickson <karakara@google.com>
2018-11-06 19:45:05 +0000

    dc2464e fix(ivy): jit should handle undefined type in constructor deps (#26956)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.1+77.sha-7dbc103';
+    exports.NGCC_VERSION = '7.1.0-beta.1+78.sha-dc2464e';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
