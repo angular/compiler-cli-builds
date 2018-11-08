81bec325ff68bb3d42febfc0febaac2cb8ad6f2a
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:46:09 +0000

    d284fa6 docs: update hero search component to use input event (#26440)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.3+3.sha-8e4dca0';
+    exports.NGCC_VERSION = '7.0.3+4.sha-d284fa6';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
