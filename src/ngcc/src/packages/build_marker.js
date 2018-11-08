970d5a8ac2d888c2c805c19db8bb6e6f21d5a649
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:28:14 +0000

    791d192 docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.2+2.sha-15e671e';
+    exports.NGCC_VERSION = '7.1.0-beta.2+3.sha-791d192';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
