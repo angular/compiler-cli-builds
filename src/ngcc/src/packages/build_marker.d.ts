bae47092b97041a778b941c2e84de542f10990cd
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:31:35 +0000

    19546c2 build: add 'yarn list-fixme-ivy-targets' to list all the fixme-ivy-* targest (#26834)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.2+3.sha-791d192";
+export declare const NGCC_VERSION = "7.1.0-beta.2+4.sha-19546c2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
