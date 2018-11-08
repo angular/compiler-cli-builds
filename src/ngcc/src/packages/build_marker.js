8c714d0e5fd28e7f4da6f53af11862175cad2809
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:30:48 +0000

    0475b8d ci: exclude symbol golden files from build-and-ci pullapprove group (#27004)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.3+6.sha-6be6125';
+    exports.NGCC_VERSION = '7.0.3+7.sha-0475b8d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
