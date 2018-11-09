9dd2ec7202258f505d0d85a3e89a382b7eb8db25
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:08:09 +0000

    b00588a docs: add missing instruction in HTTP section (#25715)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.3+10.sha-fb0d93d";
+export declare const NGCC_VERSION = "7.0.3+11.sha-b00588a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
