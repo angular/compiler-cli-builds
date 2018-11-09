ee9b867fa7e1823b68e5042590bb2b6fdfb517b7
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:47:42 +0000

    9729e8f docs: removed unused pull-left classes from hero form component (#26178)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.2+19.sha-78b6f88";
+export declare const NGCC_VERSION = "7.1.0-beta.2+20.sha-9729e8f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
