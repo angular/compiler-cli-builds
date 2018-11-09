c8a4f784f8872ca76b063bef8ea722d944c0c2e1
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:04:36 +0000

    682034f docs: Fix links (#27027)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.3+7.sha-0475b8d";
+export declare const NGCC_VERSION = "7.0.3+8.sha-682034f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
