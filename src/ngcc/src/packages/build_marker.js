2c587af6a98893cd586713030d2937ec06c96814
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:08:44 +0000

    f33307b docs(router): add RxJS filter pipe operator to example (#25686)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.3+11.sha-b00588a';
+    exports.NGCC_VERSION = '7.0.3+12.sha-f33307b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
