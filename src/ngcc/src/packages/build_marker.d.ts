5bcaf7a74ce75ef76b9d8837408c7c17f87ebd17
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:23:24 +0000

    0ada23a fix(compiler-cli): only pass canonical genfile paths to compiler host (#27062)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-beta.2+36.sha-9741f5b";
+export declare const NGCC_VERSION = "7.1.0-beta.2+34.sha-0ada23a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
