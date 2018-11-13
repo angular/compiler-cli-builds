04d5cd8d757fe78992c2d2e7fc79681ced555f35
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:12:40 +0000

    bff5c24 docs: fix typo (#26878) PR Close #26878

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.3+17.sha-53197de";
+export declare const NGCC_VERSION = "7.0.3+18.sha-bff5c24";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.0.3+17.sha-53197de';
+    exports.NGCC_VERSION = '7.0.3+18.sha-bff5c24';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
