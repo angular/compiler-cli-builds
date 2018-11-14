616ac8aedd4ea3afb6011f593453ae5ac875ab52
Andrew Kushnir <akushnir@google.com>
2018-11-14 00:08:47 +0000

    a63fd2d feat(ivy): support for i18n & ICU expressions (#26275)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.2+40.sha-bc652a2';
+    exports.NGCC_VERSION = '7.1.0-beta.2+43.sha-a63fd2d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
