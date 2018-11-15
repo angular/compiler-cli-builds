7530bb4b6855412a79759bd3a761c27104413f70
Filipe Silva <filipematossilva@gmail.com>
2018-11-15 11:32:39 +0000

    d3f9b3a ci: test ts-api-guardian on windows

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-beta.2+45.sha-a752971';
+    exports.NGCC_VERSION = '7.1.0-beta.2+46.sha-d3f9b3a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
