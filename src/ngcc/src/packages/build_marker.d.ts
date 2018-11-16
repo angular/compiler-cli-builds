fdece54b6ccd5261e5c44f1d3fa4713aa6cd2fd6
Misko Hevery <misko@hevery.com>
2018-11-16 21:35:28 +0000

    91bffa9 fix(ivy): set encapsulation metadata (#27115) PR Close #27115

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+3.sha-ee12e72";
+export declare const NGCC_VERSION = "7.1.0-rc.0+10.sha-91bffa9";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
