7d0948f3725ef67d8a8ff00d62c5aa0dc66cd81f
Misko Hevery <misko@hevery.com>
2018-11-16 20:47:00 +0000

    b07bd30 feat(bazel): Bazel workspace schematics (#26971)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+7.sha-4f965ad";
+export declare const NGCC_VERSION = "7.1.0-rc.0+8.sha-b07bd30";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
