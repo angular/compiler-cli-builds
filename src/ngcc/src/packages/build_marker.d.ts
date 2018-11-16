3f9b70960e8b519f4d585b7abba6950a28730ef9
Miško Hevery <misko@hevery.com>
2018-11-16 17:49:28 +0000

    848f414 fix(ivy): DI should  work when no element injector on starting node (#27133)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+5.sha-65943b4";
+export declare const NGCC_VERSION = "7.1.0-rc.0+6.sha-848f414";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
