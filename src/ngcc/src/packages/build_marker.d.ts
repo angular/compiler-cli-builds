2dfef949f136350ba2014ce61e41c4525028f140
Misko Hevery <misko@hevery.com>
2018-11-17 20:41:24 +0000

    81e975a build: update to latest Bazel rules_typescript (#27138)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+15.sha-20ea5b5";
+export declare const NGCC_VERSION = "7.1.0-rc.0+16.sha-81e975a.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
