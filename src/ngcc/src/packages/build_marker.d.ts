6b48dd90cfefa22ff62744285a94c1cfed4e4187
Misko Hevery <misko@hevery.com>
2018-11-17 00:32:21 +0000

    92e80af feat(ivy): ICU support for Ivy (#26794)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+11.sha-a4934a7";
+export declare const NGCC_VERSION = "7.1.0-rc.0+12.sha-92e80af";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
