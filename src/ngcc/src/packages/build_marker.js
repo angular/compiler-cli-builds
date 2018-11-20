7666c9f3a2898bc71f3d7e4d460500cabaceb275
Misko Hevery <misko@hevery.com>
2018-11-20 20:05:57 +0000

    ca40565 fix(ivy): hack implementation of host styles (#27180)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-rc.0+26.sha-34306c3.with-local-changes';
+    exports.NGCC_VERSION = '7.1.0-rc.0+28.sha-ca40565.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
