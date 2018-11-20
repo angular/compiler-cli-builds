a0fd962aab1a364f2a621c9f70cfd548ad5ffc25
Misko Hevery <misko@hevery.com>
2018-11-20 19:16:44 +0000

    2a7210e build: release scripts should use bazel from yarn (#26899)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+20.sha-a43998c.with-local-changes";
+export declare const NGCC_VERSION = "7.1.0-rc.0+25.sha-2a7210e.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
