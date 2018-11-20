9d5e13dc5e782b6cf266bfc42e5f6d3e7da2cf41
Misko Hevery <misko@hevery.com>
2018-11-20 20:07:53 +0000

    e56c8bf fix(ivy): align discovery methods for consistency (#27117)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+28.sha-ca40565.with-local-changes";
+export declare const NGCC_VERSION = "7.1.0-rc.0+29.sha-e56c8bf.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
