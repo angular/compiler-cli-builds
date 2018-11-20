fd038429c40c9564b331e6c2c244689869fbd815
Misko Hevery <misko@hevery.com>
2018-11-20 19:18:54 +0000

    391767f build: fix size artifacts not measured by github robot (#27042)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+21.sha-859da3a.with-local-changes";
+export declare const NGCC_VERSION = "7.1.0-rc.0+24.sha-391767f.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
