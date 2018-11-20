bc3fc21c60ab9ac989c6283a01c27d659a005365
Misko Hevery <misko@hevery.com>
2018-11-20 19:16:59 +0000

    975c269 docs(forms): language correction (#27177)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-rc.0+25.sha-2a7210e.with-local-changes';
+    exports.NGCC_VERSION = '7.1.0-rc.0+27.sha-975c269.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
