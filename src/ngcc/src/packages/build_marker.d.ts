5ceaf0033819deafdb289d8bbc02d93cb896ae1e
Miško Hevery <misko@hevery.com>
2018-11-21 17:44:57 +0000

    573fb78 docs: add formatting productivity hints to `DEVELOPER.md` (#27211)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+33.sha-d666370";
+export declare const NGCC_VERSION = "7.1.0-rc.0+34.sha-573fb78";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
