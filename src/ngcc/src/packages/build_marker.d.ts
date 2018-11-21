92879603327cf00a8c0573f443888e1c1a39a28b
Miško Hevery <misko@hevery.com>
2018-11-21 16:10:19 +0000

    026b7e3 build: update yarn version (#27193)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+30.sha-4390e10.with-local-changes";
+export declare const NGCC_VERSION = "7.1.0-rc.0+31.sha-026b7e3";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0-rc.0+30.sha-4390e10.with-local-changes';
+    exports.NGCC_VERSION = '7.1.0-rc.0+31.sha-026b7e3';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
