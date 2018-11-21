0f49a1fbf6778f2d954352f3cab0b7c167bb9040
Miško Hevery <misko@hevery.com>
2018-11-21 16:09:13 +0000

    a72250b build(bazel): Add Bazel builders (#27141)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0-rc.0+31.sha-026b7e3";
+export declare const NGCC_VERSION = "7.1.0-rc.0+32.sha-a72250b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
