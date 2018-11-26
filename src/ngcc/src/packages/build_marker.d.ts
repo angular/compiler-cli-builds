7ed8aaada0bbb123f3f95955dd0e2ebee230825a
Jason Aden <jasonaden@google.com>
2018-11-26 20:39:04 +0000

    b37a2fb build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.0.4+12.sha-106b85d";
+export declare const NGCC_VERSION = "7.0.4+16.sha-b37a2fb";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
