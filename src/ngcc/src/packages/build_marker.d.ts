d713cae7dc194737fca626b9cd187cab38a544ab
Jason Aden <jasonaden@google.com>
2018-11-27 18:49:16 +0000

    d62da4d test(platform-server): add root causes for TestBed tests failures (#27268)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+14.sha-c2f3054";
+export declare const NGCC_VERSION = "7.1.0+18.sha-d62da4d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
