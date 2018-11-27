7241c3fd23de46f17097681fa80bd496068a73f2
Jason Aden <jasonaden@google.com>
2018-11-27 18:47:40 +0000

    c2f3054 fix(ivy): should support components without selector (#27169)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+13.sha-d767e0b';
+    exports.NGCC_VERSION = '7.1.0+14.sha-c2f3054';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
