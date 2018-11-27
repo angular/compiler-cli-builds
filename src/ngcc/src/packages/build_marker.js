4fb86cd5f825ba1c60d49f3d733695fa51cbf410
Jason Aden <jasonaden@google.com>
2018-11-27 18:48:05 +0000

    d62da4d test(platform-server): add root causes for TestBed tests failures (#27268)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+14.sha-c2f3054';
+    exports.NGCC_VERSION = '7.1.0+18.sha-d62da4d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
