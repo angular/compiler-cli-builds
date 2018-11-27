caeff24e48afda8b83bf1937ce40b75c4e579319
Jason Aden <jasonaden@google.com>
2018-11-27 18:46:17 +0000

    d767e0b fix(ivy): consider providers for view/content queries (#27156)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+11.sha-f45aedc';
+    exports.NGCC_VERSION = '7.1.0+13.sha-d767e0b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
