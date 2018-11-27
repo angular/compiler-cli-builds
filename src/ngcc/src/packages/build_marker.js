02159b504425f60981f0553eaf58866cd4b0b888
Jason Aden <jasonaden@google.com>
2018-11-27 18:50:39 +0000

    d0e8020 fix(ivy): blueprints should be synced whenever they are off (#27281)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+19.sha-a7ba05a';
+    exports.NGCC_VERSION = '7.1.0+20.sha-d0e8020';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
