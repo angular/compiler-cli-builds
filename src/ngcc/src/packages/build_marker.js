dc6f3baad248ef6641de54737985e7f0fdaac32b
Jason Aden <jasonaden@google.com>
2018-11-27 18:48:50 +0000

    a7ba05a fix(ivy): fix directive instantiation at slots above 2^12 (#27280)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+18.sha-d62da4d';
+    exports.NGCC_VERSION = '7.1.0+19.sha-a7ba05a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
