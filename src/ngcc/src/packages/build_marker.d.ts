8ed91d62e18b59d8370e8d9dfbccc4315e50226a
Jason Aden <jasonaden@google.com>
2018-11-29 23:08:15 +0000

    11a8bd8 fix(router): update URL after redirects when urlHandlingStrategy='eager' (#27356)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+44.sha-57dae16";
+export declare const NGCC_VERSION = "7.1.0+45.sha-11a8bd8";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
