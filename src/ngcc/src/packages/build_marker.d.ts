b9b70d528b5449bf92a15572663b01394d5324ba
Igor Minar <igor@angular.io>
2018-11-30 22:03:50 +0000

    d40af0c feat(router): add a Navigation type available during navigation (#27198)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+75.sha-1b84b11";
+export declare const NGCC_VERSION = "7.1.0+79.sha-d40af0c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
