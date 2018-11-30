c5a49d1903dae90e7b1e60cdfe1e140e0d856a2e
Igor Minar <igor@angular.io>
2018-11-30 22:48:31 +0000

    973ebdc fix(ivy): avoid creating self-closing i18n instructions in case we have styling instructions (#27330)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+97.sha-a088b8c";
+export declare const NGCC_VERSION = "7.1.0+98.sha-973ebdc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
