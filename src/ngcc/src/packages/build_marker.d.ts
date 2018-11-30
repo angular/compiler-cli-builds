909091659d6566ff979c5deb32bdb9c3ec2360d2
Igor Minar <igor@angular.io>
2018-11-30 06:07:54 +0000

    5d0e114 docs: move Minko to Angular (#27339)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.1+4.sha-a0f239f";
+export declare const NGCC_VERSION = "7.1.1+5.sha-5d0e114";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
