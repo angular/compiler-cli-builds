5b8f948404af08fb28f79a9bcbd88ef859e4f0a6
Igor Minar <igor@angular.io>
2018-11-30 22:03:53 +0000

    fc2c23e fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.1+12.sha-184d63d";
+export declare const NGCC_VERSION = "7.1.1+14.sha-fc2c23e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.1+12.sha-184d63d';
+    exports.NGCC_VERSION = '7.1.1+14.sha-fc2c23e';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
