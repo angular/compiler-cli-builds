fdefa705066498dc90331aa41fb1ca98a69c1c4f
Igor Minar <igor@angular.io>
2018-11-30 06:10:26 +0000

    816ec0b refactor(ivy): treate LView as the primary global state (#27282)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+52.sha-2d51579';
+    exports.NGCC_VERSION = '7.1.0+55.sha-816ec0b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
