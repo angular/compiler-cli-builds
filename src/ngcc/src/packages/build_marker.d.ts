a3eab247c43521e98f8cba9ffce0e3d3b886a297
Igor Minar <igor@angular.io>
2018-11-30 06:10:30 +0000

    8ceea0f test(ivy): update root causes for @angular/core TestBed failures (#27351)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+53.sha-653eb59";
+export declare const NGCC_VERSION = "7.1.0+50.sha-8ceea0f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
