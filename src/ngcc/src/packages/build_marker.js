2e5275616fe39d3597ac79628bb761544d8cd863
Igor Minar <igor@angular.io>
2018-11-30 22:47:35 +0000

    64a3461 fix(ivy): support string tokens in dependency injection (#27383)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+84.sha-c96dea2';
+    exports.NGCC_VERSION = '7.1.0+85.sha-64a3461';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
