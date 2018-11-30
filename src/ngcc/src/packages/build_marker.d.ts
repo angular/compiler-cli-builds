b6422d39da5c453d369544ade3899018aee59aeb
Igor Minar <igor@angular.io>
2018-11-30 23:22:15 +0000

    35e02ad test(ivy): add root causes for router TestBed failures (#27325)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+99.sha-01fd0cd";
+export declare const NGCC_VERSION = "7.1.0+101.sha-35e02ad";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
