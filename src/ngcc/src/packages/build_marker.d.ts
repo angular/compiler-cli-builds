7a79f59a71538b117d1755bac379f980d0fdc7bd
Igor Minar <igor@angular.io>
2018-11-30 06:09:53 +0000

    419fa17 docs: move past events on events page (#27342)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+49.sha-e024f2f";
+export declare const NGCC_VERSION = "7.1.0+51.sha-419fa17";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
