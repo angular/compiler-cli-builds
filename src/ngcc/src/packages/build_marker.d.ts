c3a9d71fc3e9efa347e3f4792d09c14c27e7d35a
Igor Minar <igor@angular.io>
2018-11-30 06:16:51 +0000

    1402175 test(upgrade): re-enable tests that have been fixed (#27305)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+59.sha-65775a3";
+export declare const NGCC_VERSION = "7.1.0+60.sha-1402175";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
