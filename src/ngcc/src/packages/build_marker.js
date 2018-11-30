2491a942e26c2bc27329b6e34ef4725a4f2ad505
Igor Minar <igor@angular.io>
2018-11-30 22:04:08 +0000

    1ddc34c build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.1+14.sha-fc2c23e';
+    exports.NGCC_VERSION = '7.1.1+13.sha-1ddc34c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
