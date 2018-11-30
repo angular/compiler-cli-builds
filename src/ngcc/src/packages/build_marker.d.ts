b91e884b7b218de3ea3c41197aa8d4927f8814dc
Igor Minar <igor@angular.io>
2018-11-30 22:02:52 +0000

    184d63d docs: fix dynamic-component-loader example for Adblock Plus + EasyList (#27212)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.1+11.sha-236ac06";
+export declare const NGCC_VERSION = "7.1.1+12.sha-184d63d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
