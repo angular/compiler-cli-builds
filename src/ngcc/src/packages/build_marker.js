9567730a3b11ade20f2c5b9ed08b411fbb424dba
Igor Minar <igor@angular.io>
2018-11-30 22:49:15 +0000

    01fd0cd fix(ivy): proper accounting of bound vars in i18nExp fns (FW-747) (#27338)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+98.sha-973ebdc';
+    exports.NGCC_VERSION = '7.1.0+99.sha-01fd0cd';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
