fed69146490b4f985165af72e25664901ac93df9
Igor Minar <igor@angular.io>
2018-11-30 06:08:49 +0000

    653eb59 build: add kara to approvers for packages needing ivy TestBed updates (#27337)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+51.sha-419fa17";
+export declare const NGCC_VERSION = "7.1.0+53.sha-653eb59";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
