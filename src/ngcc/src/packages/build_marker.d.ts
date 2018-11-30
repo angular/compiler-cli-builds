2cbfc5d3cd6b78f957e67b0696a2e553f6d387bb
Igor Minar <igor@angular.io>
2018-11-30 22:36:50 +0000

    c96dea2 test(ivy): turn ivy test selectors into real functions (#27372)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+82.sha-eb17502";
+export declare const NGCC_VERSION = "7.1.0+84.sha-c96dea2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
