1091f476b1204d5fb1d1774f6bc762388f36abf8
Igor Minar <igor@angular.io>
2018-11-30 06:09:36 +0000

    013c0ae style: change style css syntax (#27253)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.1+6.sha-56254b5";
+export declare const NGCC_VERSION = "7.1.1+8.sha-013c0ae";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
