1c940a3a710f5a7d502cbc11172f7b273a7dfc4a
Igor Minar <igor@angular.io>
2018-11-30 06:20:56 +0000

    c331fc6 docs(forms): update API reference for common APIs and template-driven directives (#27033)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+62.sha-4effb89";
+export declare const NGCC_VERSION = "7.1.0+68.sha-c331fc6";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
