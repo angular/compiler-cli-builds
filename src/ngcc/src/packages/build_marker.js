fd28107c2b4a929c0b6271abd15509e7028f93f6
Igor Minar <igor@angular.io>
2018-12-01 00:30:54 +0000

    f5f323d build: only stamp version info when releasing (#27362)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+101.sha-35e02ad';
+    exports.NGCC_VERSION = '7.1.0+102.sha-f5f323d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
