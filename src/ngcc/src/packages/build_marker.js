0e3efc8d41564ca1b90df324c0faa70639e74654
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 17:42:56 +0000

    6f5c124 fix(ivy): proper accounting of host vars in case of inherited Directives (#27392)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+107.sha-931a636';
+    exports.NGCC_VERSION = '7.1.0+108.sha-6f5c124';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
