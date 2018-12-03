4336f8a52b792ad24a8de0e84b36cd6938f50892
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 19:29:09 +0000

    2a39425 fix(ivy): temporary hack to set host styles and static classes (#27385)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+109.sha-7d89cff";
+export declare const NGCC_VERSION = "7.1.0+110.sha-2a39425";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
