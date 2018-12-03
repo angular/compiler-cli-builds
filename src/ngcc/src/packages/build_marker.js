c020e3069648e3763506fd4c89ac39a17002739b
Igor Minar <igor@angular.io>
2018-12-03 16:50:32 +0000

    0378792 docs: fix typo in toh-pt6.md (#27403)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.1+17.sha-1022db1';
+    exports.NGCC_VERSION = '7.1.1+18.sha-0378792';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
