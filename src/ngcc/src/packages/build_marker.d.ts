fb48c7b6187a2b1623b7da235247eb00a366eaf7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:59 +0000

    06d4a0c feat(bazel): ng-new schematics with Bazel (#27277)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+112.sha-7ec05b4";
+export declare const NGCC_VERSION = "7.1.0+113.sha-06d4a0c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
