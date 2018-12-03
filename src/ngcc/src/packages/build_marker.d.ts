e4d903490b886264a8bd77b3f95822c6941ac26e
Igor Minar <igor@angular.io>
2018-12-03 16:53:28 +0000

    a082f64 fix(ivy): fix style prop instructions to account for zero-based values (#27270)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+105.sha-d847051";
+export declare const NGCC_VERSION = "7.1.0+106.sha-a082f64";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
