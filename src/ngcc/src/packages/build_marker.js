021e008d38b7586e6bb75a9aca54b1426143fe41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:09:39 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+114.sha-75723d5';
+    exports.NGCC_VERSION = '7.1.0+115.sha-6552471';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
