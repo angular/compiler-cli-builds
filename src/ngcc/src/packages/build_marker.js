484f813683668d6f4818c36c5cb42f4fe7764822
Igor Minar <igor@angular.io>
2018-12-04 22:33:17 +0000

    7ad6b03 ci(docs-infra): manually trigger the preview server webhook (#27458)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+116.sha-0cf49c8';
+    exports.NGCC_VERSION = '7.1.0+117.sha-7ad6b03';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
