2bdf25b3084223b4143e6b5736ea1a0ecef99145
Igor Minar <igor@angular.io>
2018-12-04 22:39:57 +0000

    1597886 fix(ivy): resolve resources using TS module resolution semantics (#27357)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+128.sha-8e644d9';
+    exports.NGCC_VERSION = '7.1.0+127.sha-1597886';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
