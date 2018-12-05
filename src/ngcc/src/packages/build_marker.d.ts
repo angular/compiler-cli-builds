f88b9a4824d5180a374b54981c86cf51cbcbf001
Igor Minar <igor@angular.io>
2018-12-05 21:51:18 +0000

    4b5cd72 refactor(docs-infra): remove unused custom element (#27250)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+164.sha-24ebdbd";
+export declare const NGCC_VERSION = "7.1.0+176.sha-4b5cd72";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
