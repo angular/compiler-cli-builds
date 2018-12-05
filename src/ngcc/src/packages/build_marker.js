e7264920461c8fb2893f4208de2ce158f4d6bdfd
Igor Minar <igor@angular.io>
2018-12-05 19:48:38 +0000

    821fecb test(ivy): update root causes for @angular/core TestBed failures (#27479)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+162.sha-2643350';
+    exports.NGCC_VERSION = '7.1.0+163.sha-821fecb';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
