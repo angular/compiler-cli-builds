1d86de2d46609666c2d76d437c1a447576bea483
Igor Minar <igor@angular.io>
2018-12-05 19:08:16 +0000

    2bc3986 feat(ivy): support inputs & outputs with aliases in component decorators (#27350) PR Close #27350

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+153.sha-20cef50";
+export declare const NGCC_VERSION = "7.1.0+155.sha-2bc3986";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
