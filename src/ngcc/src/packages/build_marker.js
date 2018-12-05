c38b4f80124abbfcf08f8b5ee1619b95cc59a07f
Igor Minar <igor@angular.io>
2018-12-05 04:46:28 +0000

    5c57920 fix(docs-infra): log the successful creation of previews (#27436)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.1+24.sha-9b4d959';
+    exports.NGCC_VERSION = '7.1.1+25.sha-5c57920';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
