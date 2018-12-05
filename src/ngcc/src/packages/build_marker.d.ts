811a38701a41a32aa49a2071e8abac9b654519da
Igor Minar <igor@angular.io>
2018-12-05 04:39:56 +0000

    9b4d959 fix(bazel): do not throw if ts compile action does not create esm5 outputs (#27401)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.1+23.sha-8087b6b";
+export declare const NGCC_VERSION = "7.1.1+24.sha-9b4d959";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
