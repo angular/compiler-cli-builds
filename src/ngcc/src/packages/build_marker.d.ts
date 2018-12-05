a5f5ec8ebad1fd3d7ae908d951c23e9c5b5e8f72
Igor Minar <igor@angular.io>
2018-12-05 04:43:31 +0000

    f0b0d64 fix(ivy): adding `projectDef` instructions to all templates where <ng-content> is present (FW-745) (#27384)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+130.sha-b2d6f43";
+export declare const NGCC_VERSION = "7.1.0+129.sha-f0b0d64";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
