4e3de77b83819100df06c2e0fa9d0b1c4a3ef54e
Igor Minar <igor@angular.io>
2018-12-05 07:43:21 +0000

    4b9948c fix(core): More precise return type for `InjectableDecorator` (#27360)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+149.sha-faca8b1";
+export declare const NGCC_VERSION = "7.1.0+150.sha-4b9948c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
