fc2a51da10150b07de56ed3acc107ab467f36a29
Igor Minar <igor@angular.io>
2018-12-06 22:11:55 +0000

    8fa7e93 test(ivy): enable router tests with pending timers (FW-768) (#27509)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+207.sha-cad6714";
+export declare const NGCC_VERSION = "7.1.0+208.sha-8fa7e93";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
