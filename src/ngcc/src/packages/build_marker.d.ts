9695def4209ce4e587973790529eaa5121643f96
Igor Minar <igor@angular.io>
2018-12-06 18:04:24 +0000

    d63d53f ci: test ts-api-guardian on windows (#27205)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+199.sha-7c52d10";
+export declare const NGCC_VERSION = "7.1.0+200.sha-d63d53f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
