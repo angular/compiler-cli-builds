232c5b57f9d4b3f5b2b63c9f2f37313f8d29cc8f
Igor Minar <igor@angular.io>
2018-12-06 18:00:36 +0000

    7c52d10 Revert "ci: test ts-api-guardian on windows (#27205)"

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+198.sha-160cace";
+export declare const NGCC_VERSION = "7.1.0+199.sha-7c52d10";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
