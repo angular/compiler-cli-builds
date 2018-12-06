6c3d2638b240243d48002951bdedf25e0369dcc0
Igor Minar <igor@angular.io>
2018-12-06 19:46:13 +0000

    ad106ab build: introduce remote bazel caching (#27358)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.1+56.sha-84f2928';
+    exports.NGCC_VERSION = '7.1.1+57.sha-ad106ab';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
