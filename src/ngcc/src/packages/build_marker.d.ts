938d998df90736a32ee34ce0611534e2031e00e3
Igor Minar <igor@angular.io>
2018-12-06 17:50:57 +0000

    ca1c430 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+196.sha-091a504";
+export declare const NGCC_VERSION = "7.1.0+197.sha-ca1c430";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
