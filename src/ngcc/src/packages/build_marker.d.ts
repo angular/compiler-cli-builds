5f78764a14ae4284310fba7ef40843adc7a4d60a
Igor Minar <igor@angular.io>
2018-12-06 05:18:57 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+185.sha-a315ded";
+export declare const NGCC_VERSION = "7.1.0+194.sha-6740d0d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
