56d6ebdde33b5cb6b8e4e7318886641376b12555
Igor Minar <igor@angular.io>
2018-12-06 22:10:40 +0000

    c71d7b5 fix(ivy): setting up animation properties correctly (FW-643) (#27496)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+205.sha-4da739a";
+export declare const NGCC_VERSION = "7.1.0+206.sha-c71d7b5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
