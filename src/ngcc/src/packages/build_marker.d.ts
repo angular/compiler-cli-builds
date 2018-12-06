60e31b16f4568225f3f1aa014fb771f1f4fc9868
Miško Hevery <misko@hevery.com>
2018-12-06 21:43:31 +0000

    3fa2a5f fix(ivy): add missing AttributeMarker export

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+163.sha-6fa5383";
+export declare const NGCC_VERSION = "7.1.0+202.sha-3fa2a5f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+163.sha-6fa5383';
+    exports.NGCC_VERSION = '7.1.0+202.sha-3fa2a5f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
