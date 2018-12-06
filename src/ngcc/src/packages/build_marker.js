71517428895db0cd61c0b5552ec635cad3b93be8
Igor Minar <igor@angular.io>
2018-12-06 18:01:50 +0000

    7c52d10 Revert "ci: test ts-api-guardian on windows (#27205)"

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+198.sha-160cace';
+    exports.NGCC_VERSION = '7.1.0+199.sha-7c52d10';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
