ef3903e112394eba3805cc1fdb92970f8b3515cd
Igor Minar <igor@angular.io>
2018-12-06 18:03:04 +0000

    d63d53f ci: test ts-api-guardian on windows (#27205)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.0+199.sha-7c52d10';
+    exports.NGCC_VERSION = '7.1.0+200.sha-d63d53f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
