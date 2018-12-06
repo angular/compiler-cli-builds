eb254ad65b2def15e0bc06b654bcb2c1c657d984
Igor Minar <igor@angular.io>
2018-12-06 23:00:25 +0000

    913563a test(ivy): finish root cause analysis for @angular/core TestBed failing tests (#27510)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+196.sha-091a504";
+export declare const NGCC_VERSION = "7.1.0+209.sha-913563a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
