18078e273ec511632ca51fb8e22be82d5b3e339b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:58:46 +0000

    6316051 build(docs-infra): upgrade cli command docs sources to 5eef7122c (#27539)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.1+3.sha-aa48810";
+export declare const NGCC_VERSION = "7.2.0-beta.1+5.sha-6316051";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
