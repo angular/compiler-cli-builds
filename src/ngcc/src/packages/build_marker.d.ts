06a768c34bc310c047ce77d99f7f3020481e0fc1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:16:10 +0000

    b0c7561 fix(forms): typed argument for FormBuilder group (#26985)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.0+209.sha-913563a";
+export declare const NGCC_VERSION = "7.2.0-beta.1+12.sha-b0c7561";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
