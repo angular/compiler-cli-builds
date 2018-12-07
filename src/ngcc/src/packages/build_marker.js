4b9e195840a615c3bf19fcf681b5995b280c7f04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:02:44 +0000

    055dc2d docs: file new issue needs to have opener choose type from template (#27400)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.1+11.sha-2a35471';
+    exports.NGCC_VERSION = '7.2.0-beta.1+10.sha-055dc2d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
