72465bcc006a5401185a16d2aa86ec90980e6eb9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 22:50:50 +0000

    05cdfb9 fix(ivy): make ViewRef.detectChanges work with embedded views (FW-749) (#27521)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.1+21.sha-16d26e5';
+    exports.NGCC_VERSION = '7.2.0-beta.1+22.sha-05cdfb9';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
