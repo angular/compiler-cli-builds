649859630cbe1bc57ef84e4093a69eab98f1ea00
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 18:58:50 +0000

    54e4bdb docs: remove AngularDart from the version picker (#27550)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.1+16.sha-44dd764';
+    exports.NGCC_VERSION = '7.2.0-beta.1+18.sha-54e4bdb';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
