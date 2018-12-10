b74859ea690f018c6de8007fec4395a806e11d9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 22:44:25 +0000

    575ef9f docs: add webucator to resources (#26895)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.2+8.sha-11996cd";
+export declare const NGCC_VERSION = "7.1.2+9.sha-575ef9f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
