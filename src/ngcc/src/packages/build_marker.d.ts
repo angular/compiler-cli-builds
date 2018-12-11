6b57ec282321389d54e33c00c22ac14adde4ecc5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 00:57:49 +0000

    50687e1 build: fix type-check errors introduced during rules_ts 0.21 (#27586)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.1+22.sha-05cdfb9";
+export declare const NGCC_VERSION = "7.2.0-beta.1+24.sha-50687e1";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
