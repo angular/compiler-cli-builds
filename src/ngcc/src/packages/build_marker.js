243da865d26f758f1ce0e9aaecc9274cd096fe1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:58:10 +0000

    3cb6dad docs: update npm scripts to support angular 7 (#27334)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.1+29.sha-ceb14de';
+    exports.NGCC_VERSION = '7.2.0-beta.1+28.sha-3cb6dad';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
