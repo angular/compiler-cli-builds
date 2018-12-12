c86880e614af5d2062711b153057a94127d7404b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 00:54:00 +0000

    d0e6eeb build: bump @bazel/ibazel to 0.9.0 (#27601)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.3+2.sha-93078e3';
+    exports.NGCC_VERSION = '7.1.3+3.sha-d0e6eeb';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
