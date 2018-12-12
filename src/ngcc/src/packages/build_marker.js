74eba9a7e08bd4ebd75680212b8004d45d8ee43e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:24:47 +0000

    fc6dc78 fix(docs-infra): fix top menu item clickable area (#27620)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+3.sha-cabf1c7';
+    exports.NGCC_VERSION = '7.2.0-beta.2+5.sha-fc6dc78';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
