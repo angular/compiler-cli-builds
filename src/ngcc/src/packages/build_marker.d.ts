50924ec8dbb1ff9b9b30b03b84acb53da1dab6bd
Miško Hevery <misko@hevery.com>
2018-12-12 21:29:37 +0000

    37c05bd fix(ivy): avoid destroy renderer method invocation for child views (#27592)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+6.sha-28ceca0";
+export declare const NGCC_VERSION = "7.2.0-beta.2+9.sha-37c05bd";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
