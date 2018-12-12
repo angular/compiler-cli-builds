82d2646f17920d6edfcc3881c97ccfe45dbb45b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:32:15 +0000

    9c7fb0d docs: add Zenika trainings in resources.json (#27216)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+4.sha-042463f';
+    exports.NGCC_VERSION = '7.2.0-beta.2+8.sha-9c7fb0d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
