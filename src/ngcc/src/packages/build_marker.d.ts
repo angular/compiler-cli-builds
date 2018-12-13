66af20d13ae675d444d54e80e37bc45b3c296630
Miško Hevery <misko@hevery.com>
2018-12-13 19:36:03 +0000

    3ed1e84 fix(bazel): fix major/minor semver check between @angular/bazel npm packager version and angular bazel repo version (#27635)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+10.sha-97eb858";
+export declare const NGCC_VERSION = "7.1.3+13.sha-3ed1e84";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
