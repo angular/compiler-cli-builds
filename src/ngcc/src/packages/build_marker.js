53d3a27a959c9b55f4f0f1757ae36a85ff0fea8e
Miško Hevery <misko@hevery.com>
2018-12-13 19:39:46 +0000

    52fd4a2 build: disable postinstall-patch.js log output (#27619)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.3+14.sha-8c32a2e';
+    exports.NGCC_VERSION = '7.1.3+15.sha-52fd4a2';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
