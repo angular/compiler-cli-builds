fcb5fe00eae712c961b86c1c9e26aca844d5a619
Miško Hevery <misko@hevery.com>
2018-12-13 19:49:00 +0000

    e269274 build: ts-api-guardian tsconfig warning with bazel (#27583)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+18.sha-8313ffc';
+    exports.NGCC_VERSION = '7.2.0-beta.2+19.sha-e269274';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
