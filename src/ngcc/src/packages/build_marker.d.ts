c7350848240c8fbeca94cf51e46cdc4a7430f204
Miško Hevery <misko@hevery.com>
2018-12-13 19:47:47 +0000

    e269274 build: ts-api-guardian tsconfig warning with bazel (#27583)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+18.sha-8313ffc";
+export declare const NGCC_VERSION = "7.2.0-beta.2+19.sha-e269274";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
