809873a6008e8fd62be9d6e03cf7a7c0acf17f2a
Miško Hevery <misko@hevery.com>
2018-12-13 19:48:11 +0000

    e318081 ci: remove packages/bazel from compiler-cli group (#27641)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+17.sha-ffe1b4d";
+export declare const NGCC_VERSION = "7.2.0-beta.2+13.sha-e318081";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
