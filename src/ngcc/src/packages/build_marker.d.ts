045e151a63956e7e3cbc70a2c0166af45f2d568c
Miško Hevery <misko@hevery.com>
2018-12-14 23:57:40 +0000

    b224057 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+27.sha-453589f";
+export declare const NGCC_VERSION = "7.1.3+29.sha-b224057";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
