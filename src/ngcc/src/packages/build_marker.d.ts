fe712918825a5a72bc134403b1e6260d59bfa97b
Miško Hevery <misko@hevery.com>
2018-12-14 23:50:55 +0000

    c220328 docs: adding ngAtlanta 2019 (#27522)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+26.sha-fa3af8b";
+export declare const NGCC_VERSION = "7.1.3+25.sha-c220328";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
