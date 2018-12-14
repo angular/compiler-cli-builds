877b21cea0753a3e8096a47b92db1e81a5a982de
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:25 +0000

    78bd3c7 docs(animations): rename animateChild() to animate() (#26184)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+29.sha-b224057";
+export declare const NGCC_VERSION = "7.1.3+30.sha-78bd3c7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
