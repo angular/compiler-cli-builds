593a13fe82ea901b674ba0210c1f9f53707088ca
Miško Hevery <misko@hevery.com>
2018-12-14 19:02:41 +0000

    d32939d build: restrict visibility of npm_package targets (#27611)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+28.sha-3680aef";
+export declare const NGCC_VERSION = "7.2.0-beta.2+34.sha-d32939d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
