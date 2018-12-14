0f62f5ed1690a92e68ae6a17f5da48c40baef75e
Miško Hevery <misko@hevery.com>
2018-12-14 22:39:58 +0000

    650c5a0 Revert "fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)"

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+26.sha-ad26cd6';
+    exports.NGCC_VERSION = '7.2.0-beta.2+40.sha-650c5a0';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
