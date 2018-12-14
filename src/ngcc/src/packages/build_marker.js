d54ecf83bbe8b1a168e440edef76208d56bf3a14
Miško Hevery <misko@hevery.com>
2018-12-14 18:49:35 +0000

    f5e7208 build: re-enable saucelabs non-verbose logging (#27657)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.1.3+15.sha-52fd4a2';
+    exports.NGCC_VERSION = '7.1.3+20.sha-f5e7208';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
