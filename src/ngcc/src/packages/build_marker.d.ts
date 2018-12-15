9850461075844b984c2fef881f390f7ba8e2457e
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:58 +0000

    f4a797d fix(ivy): prevent ngcc from inadvertently dropping Ivy definitions (#27159)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+49.sha-4df82bd";
+export declare const NGCC_VERSION = "7.2.0-beta.2+50.sha-f4a797d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
