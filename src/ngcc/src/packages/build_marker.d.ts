2b0daff056aef48a668d0e751963298e7fb1e509
Miško Hevery <misko@hevery.com>
2018-12-15 00:03:44 +0000

    df123e0 fix(animations): mark actual descendant node as disabled (#26180)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+44.sha-b39efdd";
+export declare const NGCC_VERSION = "7.2.0-beta.2+46.sha-df123e0";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
