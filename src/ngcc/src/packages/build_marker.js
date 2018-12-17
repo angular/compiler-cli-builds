b9856c390d699d70fee8bbf35407e8803f43af91
Miško Hevery <misko@hevery.com>
2018-12-17 18:11:36 +0000

    ea10a3a test(ivy): enable all test for @angular/common (#27700)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+52.sha-f8096d4';
+    exports.NGCC_VERSION = '7.2.0-beta.2+53.sha-ea10a3a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
