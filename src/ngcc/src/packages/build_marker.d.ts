d9f51f5586f43aa71d8387c2cadb0d298d230523
Miško Hevery <misko@hevery.com>
2018-12-17 18:07:31 +0000

    d1a14f6 docs: remove misplaced code in Universal guide (#26865)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+31.sha-ce4cff6";
+export declare const NGCC_VERSION = "7.1.3+32.sha-d1a14f6";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
