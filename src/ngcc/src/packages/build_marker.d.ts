e0c294525516c9f26dc432b87720aa7c1e242aa6
Miško Hevery <misko@hevery.com>
2018-12-17 18:04:53 +0000

    ce4cff6 docs: Add Smart UI to UI Components section in resources (#27515)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+30.sha-78bd3c7";
+export declare const NGCC_VERSION = "7.1.3+31.sha-ce4cff6";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
