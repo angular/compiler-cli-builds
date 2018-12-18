336a7b1f74e045c8950707ad8e8b3ad6d07f0ea2
Miško Hevery <misko@hevery.com>
2018-12-18 18:43:10 +0000

    4774a1a docs(bazel): Update VSCode config (#27733)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+80.sha-8d0a3ce';
+    exports.NGCC_VERSION = '7.2.0-beta.2+81.sha-4774a1a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
