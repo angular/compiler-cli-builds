54536a2e4f058e6930cd9038f09b4213cb73f1b8
Miško Hevery <misko@hevery.com>
2018-12-18 18:00:53 +0000

    8fdd702 docs: Fixed typo (#27669) PR Close #27669

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+77.sha-d766ad0';
+    exports.NGCC_VERSION = '7.2.0-beta.2+79.sha-8fdd702';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
