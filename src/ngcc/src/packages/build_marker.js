f3468a0f9986993bdeefa7f54362a8fbc8cc4852
Miško Hevery <misko@hevery.com>
2018-12-18 01:17:24 +0000

    8042140 test(ivy): enable more @angular/core tests (#27654)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+68.sha-87d7b74';
+    exports.NGCC_VERSION = '7.2.0-beta.2+70.sha-8042140';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
