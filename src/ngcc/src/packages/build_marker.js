9c019ae5e87ce817939997fa0a41e6305008efdb
Miško Hevery <misko@hevery.com>
2018-12-18 05:17:21 +0000

    e98c57a refactor(core): rename spec file to match case of src file

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+74.sha-4bf8d64';
+    exports.NGCC_VERSION = '7.2.0-beta.2+75.sha-e98c57a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
