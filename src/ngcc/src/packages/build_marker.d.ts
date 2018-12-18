42c87452300c5e20ead4a2140dcef2cd30b6861d
Miško Hevery <misko@hevery.com>
2018-12-18 01:16:51 +0000

    85866de fix(bazel): Set module_name and enable ng test (#27715)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+66.sha-c986d3d";
+export declare const NGCC_VERSION = "7.2.0-beta.2+67.sha-85866de";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
