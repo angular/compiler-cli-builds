63b0a6034e58eaf74a24f08e290a9c84ca723131
Miško Hevery <misko@hevery.com>
2018-12-18 21:48:46 +0000

    7901cd8 fix(ivy): add missing AttributeMarker export (#27485) PR Close #27485

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+89.sha-69198ba";
+export declare const NGCC_VERSION = "7.2.0-beta.2+90.sha-7901cd8";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
