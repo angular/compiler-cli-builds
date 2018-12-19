01ee76cdff5e9c5be245761c04de5c9e28f4cfec
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:44:43 +0000

    0604527 Revert "build: update gulp-clang-format dependency (#27712)" (#27759)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+3.sha-8f8572f";
+export declare const NGCC_VERSION = "7.2.0-rc.0+4.sha-0604527";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
