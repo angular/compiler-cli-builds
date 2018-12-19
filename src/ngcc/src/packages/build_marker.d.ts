e249dfcbb899b64dd4fd4cb55eab088e0cdd622e
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:40:06 +0000

    a20b2f7 fix(ivy): process creation mode deeply before running update mode (#27744)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0";
+export declare const NGCC_VERSION = "7.2.0-rc.0+1.sha-a20b2f7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
