4bfaf9c2985223b8d3ebb24c4b884b3d1c66bb8c
Igor Minar <igor@angular.io>
2018-12-19 18:24:29 +0000

    01a5c74 turns out that github silently converts '/ in team names to '-'

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-beta.2+77.sha-4d71c4c";
+export declare const NGCC_VERSION = "7.2.0-beta.2+78.sha-01a5c74";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-beta.2+77.sha-4d71c4c';
+    exports.NGCC_VERSION = '7.2.0-beta.2+78.sha-01a5c74';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
