b183ddff433137053a7c9759fbab978d858ef6b3
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:41:09 +0000

    880e8a5 refactor: fix broken formatting

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+11.sha-a833b98";
+export declare const NGCC_VERSION = "7.2.0-rc.0+13.sha-880e8a5";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+11.sha-a833b98';
+    exports.NGCC_VERSION = '7.2.0-rc.0+13.sha-880e8a5';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
