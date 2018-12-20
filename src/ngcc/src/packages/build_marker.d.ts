bbad636e0b6d266f9e802876c9eeafd9a4b6d1de
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:54:13 +0000

    50a91ba docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+14.sha-07ada7f";
+export declare const NGCC_VERSION = "7.2.0-rc.0+19.sha-50a91ba";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
