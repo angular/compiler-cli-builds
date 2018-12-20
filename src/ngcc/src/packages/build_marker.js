e0b656551bbeefd9121019dce4c401bf6714cdaa
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:52:56 +0000

    50a91ba docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+14.sha-07ada7f';
+    exports.NGCC_VERSION = '7.2.0-rc.0+19.sha-50a91ba';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
