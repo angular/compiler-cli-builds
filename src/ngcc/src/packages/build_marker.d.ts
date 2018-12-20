6acfe1b7a7a96e69b7db08fc8d0e5c16dda3614a
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:27:25 +0000

    a0585c9 fix(ivy): correct content projection with nested templates (#27755)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+10.sha-4b70a4e";
+export declare const NGCC_VERSION = "7.2.0-rc.0+12.sha-a0585c9";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
