ac1bd4546dae5071b5eaa3f4b8ca38617d1f369c
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:34:48 +0000

    5da55d6 test(ivy): add root cause analysis for failing router tests (#27792)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+19.sha-50a91ba';
+    exports.NGCC_VERSION = '7.2.0-rc.0+20.sha-5da55d6';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
