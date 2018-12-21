a5ce66497b5b156f65bc85ae3863be638080e129
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 23:40:36 +0000

    13eb57a fix(ivy): merge static style rendering across elements, directives and components (#27661)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+26.sha-0b3ae3d";
+export declare const NGCC_VERSION = "7.2.0-rc.0+27.sha-13eb57a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
