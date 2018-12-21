54e88dc5d8ccd1ef89e9bf2460328b44f20691aa
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 21:06:54 +0000

    509aa61 fix(ivy): apply all overrides from TestBed, not the last one only (#27734)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+23.sha-c5ce4e6";
+export declare const NGCC_VERSION = "7.2.0-rc.0+22.sha-509aa61";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+23.sha-c5ce4e6';
+    exports.NGCC_VERSION = '7.2.0-rc.0+22.sha-509aa61';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
