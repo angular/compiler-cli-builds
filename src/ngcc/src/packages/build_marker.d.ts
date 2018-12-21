a65d46df11aea38d39e1a22ddcc6fe251ff757a1
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:41:40 +0000

    0ae1e93 docs: fix code example to ensure consistency in file (#26577)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+11.sha-98a617e";
+export declare const NGCC_VERSION = "7.2.0-rc.0+10.sha-0ae1e93";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
