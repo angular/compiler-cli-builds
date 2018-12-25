bb1c496bbc88706e90164571d4fd167a744eb96c
Matias Niemelä <matias@yearofmoo.com>
2018-12-25 01:19:29 +0000

    9129112 test(bazel): fix `bazel-workspace` schematics test

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.1.3+35.sha-be08611";
+export declare const NGCC_VERSION = "7.1.4+1.sha-9129112";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
