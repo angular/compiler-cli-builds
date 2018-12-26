eaeca2b7e5c0511f4cae5064de33afac928082d8
Ben Lesh <ben@benlesh.com>
2018-12-26 19:57:34 +0000

    92d3942 build: update to latest rules_nodejs (#27764)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+14.sha-4ad7e21";
+export declare const NGCC_VERSION = "7.2.0-rc.0+15.sha-92d3942";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
