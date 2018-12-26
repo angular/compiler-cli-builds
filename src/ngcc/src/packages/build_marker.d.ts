7fe3ee35e8ac38d19add6274d26e69771c26c79e
Matias Niemelä <matias@yearofmoo.com>
2018-12-26 12:24:24 +0000

    9b91bee docs: fix typo explictly (#27798) Closes #27796

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+27.sha-13eb57a";
+export declare const NGCC_VERSION = "7.2.0-rc.0+29.sha-9b91bee";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
