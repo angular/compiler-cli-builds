3dbb2cf75fb6fa1a0967a91e90871802706e64cc
Ben Lesh <ben@benlesh.com>
2019-01-03 18:02:28 +0000

    061615c test(docs-infra): increase timeout for redirection to external URL (#27903)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+18.sha-e9de47c";
+export declare const NGCC_VERSION = "7.2.0-rc.0+17.sha-061615c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
