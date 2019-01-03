1b9df8800a1e801b2fb5ccab35e9b22dd0d932a7
Ben Lesh <ben@benlesh.com>
2019-01-03 18:18:14 +0000

    1dc95c4 docs: fix typo in TOH http section (#26127)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+41.sha-5638c1d";
+export declare const NGCC_VERSION = "7.2.0-rc.0+44.sha-1dc95c4";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
