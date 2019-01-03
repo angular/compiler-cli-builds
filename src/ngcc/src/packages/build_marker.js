00150050bf2e8f09583fd32e433a3cb9473fbabb
Ben Lesh <ben@benlesh.com>
2019-01-03 18:04:38 +0000

    d8aeb59 docs: fix npm description in architecture modules guide (#27818)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+17.sha-061615c';
+    exports.NGCC_VERSION = '7.2.0-rc.0+20.sha-d8aeb59';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
