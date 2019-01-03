be2464e78489af4d365d543feff20c3b5a43a718
Ben Lesh <ben@benlesh.com>
2019-01-03 18:18:31 +0000

    7df9040 docs: add missing comma to environment config in build guide (#27476)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+44.sha-1dc95c4";
+export declare const NGCC_VERSION = "7.2.0-rc.0+42.sha-7df9040";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
