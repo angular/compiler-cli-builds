7e4509e53790c4e132f9679d869403c4a848b039
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:20 +0000

    a0a3648 docs: add more info on what is being cached (#27343)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+40.sha-755c809";
+export declare const NGCC_VERSION = "7.2.0-rc.0+43.sha-a0a3648";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
