c9f51d59f224707d916ecf203d935ec95812ca46
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:29 +0000

    1dc95c4 docs: fix typo in TOH http section (#26127)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+41.sha-5638c1d';
+    exports.NGCC_VERSION = '7.2.0-rc.0+44.sha-1dc95c4';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
