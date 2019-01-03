2641ce203057818ec463cf688dc07cd42904829f
Ben Lesh <ben@benlesh.com>
2019-01-03 18:00:36 +0000

    e9de47c docs(service-worker): update service worker configuration doc (#27868)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+16.sha-213e01b";
+export declare const NGCC_VERSION = "7.2.0-rc.0+18.sha-e9de47c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
