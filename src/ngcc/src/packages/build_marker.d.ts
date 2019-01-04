3313af868533948bffee6fba92ef07a1119b1893
Kara Erickson <karakara@google.com>
2019-01-04 20:46:47 +0000

    0a54825 ci: make `integration_test` job logs less verbose (#27934)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+26.sha-970e3a0";
+export declare const NGCC_VERSION = "7.2.0-rc.0+27.sha-0a54825";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
