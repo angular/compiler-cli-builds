dfccf18dcae7b86bc501ae30a3e8a035e39a6fc1
Kara Erickson <karakara@google.com>
2019-01-04 20:49:04 +0000

    7eb2c41 fix(ivy): ng-content tags in re-inserted templates should walk declaration tree (#27783)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+55.sha-38b4d15";
+export declare const NGCC_VERSION = "7.2.0-rc.0+57.sha-7eb2c41";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
