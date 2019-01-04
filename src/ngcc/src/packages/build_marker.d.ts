8e47ea12c40840f637321701c4fd4896bcdc0009
Kara Erickson <karakara@google.com>
2019-01-04 20:55:20 +0000

    48555f9 fix(ivy): ensure unique template and listener function identifiers (#27766)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+54.sha-176b3f1";
+export declare const NGCC_VERSION = "7.2.0-rc.0+59.sha-48555f9";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
