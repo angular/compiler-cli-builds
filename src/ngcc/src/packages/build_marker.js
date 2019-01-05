22cafaea2dd1d51e62902676f27f5be939edd372
Kara Erickson <karakara@google.com>
2019-01-05 03:03:59 +0000

    411de25 fix(ivy): disable test which rely on DebugElement in WebWorker (#27895)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+65.sha-b61dafa';
+    exports.NGCC_VERSION = '7.2.0-rc.0+66.sha-411de25';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
