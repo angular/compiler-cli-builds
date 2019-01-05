b547c658133797f4ef0464465a640348ba9ed096
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:41:32 +0000

    784453c ci: use image based cache

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+53.sha-9576e48";
+export declare const NGCC_VERSION = "7.2.0-rc.0+53.sha-784453c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
