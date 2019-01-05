d3fc045e10aa2e43eff2ee2a1610749b1af9f195
Kara Erickson <karakara@google.com>
2019-01-05 03:03:20 +0000

    6cc864a build: enforce minimum Bazel version of 0.21.0 in WORKSPACE so cache works with `yarn bazel` (#27935)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+27.sha-0a54825';
+    exports.NGCC_VERSION = '7.2.0-rc.0+29.sha-6cc864a';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
