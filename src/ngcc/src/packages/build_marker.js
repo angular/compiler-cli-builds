c350ac4bc29c43d64fc21187e96a9d5786cf3154
Kara Erickson <karakara@google.com>
2019-01-06 02:06:53 +0000

    224d787 ci: replace our pullapprove setup with GitHub CODEOWNERS (#27690)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+68.sha-7adcbb3';
+    exports.NGCC_VERSION = '7.2.0-rc.0+69.sha-224d787';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
