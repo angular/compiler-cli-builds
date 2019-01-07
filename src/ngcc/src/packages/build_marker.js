c4cad6a517b88668f42129bea1f59a7fdd1a2c1c
Kara Erickson <karakara@google.com>
2019-01-07 23:52:48 +0000

    c1dacdd fix(ivy): QueryList not instance of exported QueryList (#27942)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+14.sha-ac5f5ed';
+    exports.NGCC_VERSION = '7.2.0+17.sha-c1dacdd';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
