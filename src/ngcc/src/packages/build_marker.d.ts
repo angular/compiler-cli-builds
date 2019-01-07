c80d2e446802bad51738d458f469d51d3aa82b35
Kara Erickson <karakara@google.com>
2019-01-07 17:06:49 +0000

    654055b ci: add brandonroberts to @angular/framework-global-approvers-for-docs-only-changes (#27949)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+2.sha-e94e7ee";
+export declare const NGCC_VERSION = "7.2.0+1.sha-654055b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
