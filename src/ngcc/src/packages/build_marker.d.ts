cdd852dd2e953bdb70d9beec3e3562644e2d62e2
Kara Erickson <karakara@google.com>
2019-01-08 18:56:47 +0000

    1cca9cc ci: compute commit range for rerun workflows (#27775)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+51.sha-575cf3d";
+export declare const NGCC_VERSION = "7.2.0-rc.0+52.sha-1cca9cc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
