84c8649f881e06a908a3bb3a4015cc936de37784
Kara Erickson <karakara@google.com>
2019-01-08 22:22:10 +0000

    1a7f92c test(ivy): re-enable passing animation tests (#27997)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+43.sha-a084024';
+    exports.NGCC_VERSION = '7.2.0+44.sha-1a7f92c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
