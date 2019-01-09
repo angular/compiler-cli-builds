7af9cf80cb49314b2b52660ba378e048d073b13a
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:06:44 +0000

    eb3345b docs: fix typo PageNotFoundCompponent (#27799) closes: #27795

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+72.sha-a06469c";
+export declare const NGCC_VERSION = "7.2.0-rc.0+71.sha-eb3345b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
