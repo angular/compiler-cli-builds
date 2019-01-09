5481536181b5e88a6c9f6f545ca3edb86dcaccfa
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:55:57 +0000

    11325ba test(ivy): mark jit_summaries_integration_spec as obsolete in Ivy (#28027)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+79.sha-d0c3e25';
+    exports.NGCC_VERSION = '7.2.0+80.sha-11325ba';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
