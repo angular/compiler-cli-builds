052267348f306dde56021f1533cdd133520a4d66
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:55:31 +0000

    d0c3e25 docs: remove deprecated `fromPromise` from RxJS guide (#28015)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+78.sha-e1e4887";
+export declare const NGCC_VERSION = "7.2.0+79.sha-d0c3e25";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+78.sha-e1e4887';
+    exports.NGCC_VERSION = '7.2.0+79.sha-d0c3e25';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
