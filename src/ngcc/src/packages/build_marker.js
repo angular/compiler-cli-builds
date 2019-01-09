faf66175e32e565c0b9c88cc8394381623c58306
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:01:33 +0000

    9b2b9b3 docs(core): add missing closing backtick (`) (#27908) PR Close #27908

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+71.sha-76580b9';
+    exports.NGCC_VERSION = '7.2.0+74.sha-9b2b9b3';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
