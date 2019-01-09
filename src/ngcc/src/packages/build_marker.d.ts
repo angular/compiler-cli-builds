8a5d1de511397d21282de6eb4d4a4d35cd2dc999
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:02:04 +0000

    91a8a4f docs(forms): remove duplicated link (#27884) PR Close #27884

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+72.sha-582395b";
+export declare const NGCC_VERSION = "7.2.0+73.sha-91a8a4f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
