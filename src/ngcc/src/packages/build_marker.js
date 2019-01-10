172cf6ae03ef89002a2fd50a4f8e3ee6883f0914
Andrew Kushnir <akushnir@google.com>
2019-01-10 02:00:25 +0000

    5a0deb8 fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+84.sha-4694c93';
+    exports.NGCC_VERSION = '7.2.0+85.sha-5a0deb8';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
