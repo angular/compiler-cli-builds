b8c60a00c3615841d132726f325130095d4f9573
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:29:50 +0000

    a64a7a4 test(upgrade): log more info to help debug CI flakes (#28045)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+77.sha-9bda446";
+export declare const NGCC_VERSION = "7.2.0-rc.0+78.sha-a64a7a4";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
