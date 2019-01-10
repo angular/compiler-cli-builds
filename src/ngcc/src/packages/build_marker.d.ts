3dba084b74f7e76d294ea9fa8af23396aab1e0a8
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:27:44 +0000

    4694c93 refactor(ivy): avoid `transitiveScopesFor` function duplication in TestBed (#27860)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+83.sha-94c0b7a";
+export declare const NGCC_VERSION = "7.2.0+84.sha-4694c93";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
