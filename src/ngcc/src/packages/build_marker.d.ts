1d70fc29f218e2ac79cdaaa948aec7a7728ac112
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:31:53 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+92.sha-65e72e9";
+export declare const NGCC_VERSION = "7.2.0+94.sha-ee60c76";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+92.sha-65e72e9';
+    exports.NGCC_VERSION = '7.2.0+94.sha-ee60c76';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
