c2ff0fb359ca12114ae69dcac4659db73d8090b7
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:06 +0000

    5a0deb8 fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+84.sha-4694c93";
+export declare const NGCC_VERSION = "7.2.0+85.sha-5a0deb8";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
