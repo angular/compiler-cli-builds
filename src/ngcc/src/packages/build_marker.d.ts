726a2b0506db4d8617b015a7458b8c88803afdf4
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:51:51 +0000

    feebe03 fix(ivy): pass ngContentSelectors through to `defineComponent()` calls (#27867)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+98.sha-a6ba789";
+export declare const NGCC_VERSION = "7.2.0+100.sha-feebe03";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
