fe40ed00088a1447ba02809a73cbb492d27e14ce
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:56:23 +0000

    b05baa5 fix(bazel): Add /bazel-out to .gitignore (#27874)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+100.sha-feebe03';
+    exports.NGCC_VERSION = '7.2.0+102.sha-b05baa5';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
