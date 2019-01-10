fd6e57fc36e90619915f4bbd308dc203e28bc662
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:54:57 +0000

    d4b476f fix(bazel): Add /bazel-out to .gitignore (#27874)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+78.sha-a64a7a4";
+export declare const NGCC_VERSION = "7.2.0-rc.0+79.sha-d4b476f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
