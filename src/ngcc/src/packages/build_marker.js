f5e05b8a13acbe944796eae87f9c3370a2c84429
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:34:24 +0000

    e8a57f0 test(upgrade): log more info to help debug CI flakes (#28045)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+95.sha-dffcb9c';
+    exports.NGCC_VERSION = '7.2.0+99.sha-e8a57f0';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
