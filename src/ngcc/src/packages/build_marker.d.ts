5d3023d3342dbe65dcf6581b338d963ff7143c54
Andrew Kushnir <akushnir@google.com>
2019-01-11 22:45:45 +0000

    b73d678 build: support external angular in ng_rollup_bundle (#28049)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+132.sha-0c6fa1d";
+export declare const NGCC_VERSION = "7.2.0+133.sha-b73d678";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
