3c78bb1c8e36b2e7a7f0b2ee9c61b0f2cfe82b5b
Keen Yee Liau <kyliau@umich.edu>
2019-01-11 01:34:15 +0000

    7a9e3ad docs(bazel): Getting started with Bazel + CLI

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+103.sha-7de7e1b";
+export declare const NGCC_VERSION = "7.2.0+112.sha-7a9e3ad";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
