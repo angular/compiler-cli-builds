92dfc3bbcfed58fb5e619effa5be5f0e606c77d1
Andrew Kushnir <akushnir@google.com>
2019-01-11 22:48:53 +0000

    3c08b86 docs(forms): fix up `@see` tags for `AbstractContol` (#28069)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+93.sha-0465dc6";
+export declare const NGCC_VERSION = "7.2.0-rc.0+95.sha-3c08b86";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
