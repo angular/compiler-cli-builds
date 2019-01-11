209ba38f6c545215673fb85cede4dba2621a1186
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:45:34 +0000

    8c3f98f docs(service-worker): fix example of manually checking for updates (#28020)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+117.sha-3ea9f09';
+    exports.NGCC_VERSION = '7.2.0+122.sha-8c3f98f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
