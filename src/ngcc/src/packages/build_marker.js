e5e42cdd8c101322ed164124540c9831b270654b
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:43:45 +0000

    3ea9f09 docs(router): remove obsolete sections in README.md (#27880)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+114.sha-e31afb7';
+    exports.NGCC_VERSION = '7.2.0+117.sha-3ea9f09';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
