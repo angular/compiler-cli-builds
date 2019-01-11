146ae95ebf636a145ffbc54f8e278e9e3e82266c
Andrew Kushnir <akushnir@google.com>
2019-01-11 18:37:58 +0000

    f854eb7 test(ivy): mark tests where checkNoChanges results are different in the view engine and ivy (#28073)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+111.sha-b0f3c20';
+    exports.NGCC_VERSION = '7.2.0+112.sha-f854eb7';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
