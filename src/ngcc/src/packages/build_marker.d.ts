ea9f3d31d1c7a3d45a66cb4e9af6ac9beb934c7a
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:25:23 +0000

    9447b85 refactor(core): improve type for EventEmitter (#27228)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+82.sha-c18c46a";
+export declare const NGCC_VERSION = "7.2.0-rc.0+81.sha-9447b85";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
