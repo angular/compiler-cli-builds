7151b429714dd221ee5fc8ee50456c67aa2fc7ed
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:15:46 +0000

    01d475d fix(bazel): Add ibazel to deps of Bazel project (#28090)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+95.sha-3c08b86';
+    exports.NGCC_VERSION = '7.2.0-rc.0+96.sha-01d475d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
