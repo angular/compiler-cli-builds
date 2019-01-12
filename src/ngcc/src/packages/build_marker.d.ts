ec90ac8a18d4eba774d82d2e18a1dac1e94fd961
Keen Yee Liau <kyliau@umich.edu>
2019-01-12 00:25:51 +0000

    6eeb19c docs(bazel): Getting started with Bazel + CLI

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+112.sha-7a9e3ad";
+export declare const NGCC_VERSION = "7.2.0+139.sha-6eeb19c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+112.sha-7a9e3ad';
+    exports.NGCC_VERSION = '7.2.0+139.sha-6eeb19c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
