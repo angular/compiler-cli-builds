bbb547f53c727de8be82d0254aa20e0b32141ecb
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:55:26 +0000

    da2880d fixup! fix(ivy): add missing @nocollapse annotation to __NG_ELEMENT_ID__ fields (#28050) (#28066)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+137.sha-9260b5e";
+export declare const NGCC_VERSION = "7.2.0+143.sha-da2880d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
