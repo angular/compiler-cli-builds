54aa5fe66d2c6178eb237fcb55f17f4fb37cfdf4
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:11:46 +0000

    5f616ca docs: fix typo in testing guide (#26828)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+100.sha-b47ea97";
+export declare const NGCC_VERSION = "7.2.0-rc.0+101.sha-5f616ca";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
