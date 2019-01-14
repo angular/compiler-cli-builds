3bb89694c7374c66874f96d1f265d8bc539e54ec
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:24:46 +0000

    5e6e96d docs: fix GitHub pages deployment command (#26896)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+150.sha-638375b';
+    exports.NGCC_VERSION = '7.2.0+152.sha-5e6e96d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
