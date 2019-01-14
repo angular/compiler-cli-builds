8db70c7c636706f0a2201fa71db7a61fc5f723ff
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:29:15 +0000

    0897eba docs: fix typo in docs.md (#27484)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+157.sha-4bb5f63";
+export declare const NGCC_VERSION = "7.2.0+156.sha-0897eba";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
