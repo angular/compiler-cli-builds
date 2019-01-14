373178cd78a0901500cfc10428ee18ccc90cb62f
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:31:11 +0000

    1ec01d6 docs: change in(what should I import? answer) NgModule FAQs (#27677)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+156.sha-0897eba";
+export declare const NGCC_VERSION = "7.2.0+158.sha-1ec01d6";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+156.sha-0897eba';
+    exports.NGCC_VERSION = '7.2.0+158.sha-1ec01d6';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
