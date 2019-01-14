1da2aa6420db2905e53fec292269ffe762c3030a
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:31:38 +0000

    78bc21c docs: fix scripts section and some minor issues in universal documentation (#26444)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+147.sha-48a03fc';
+    exports.NGCC_VERSION = '7.2.0+148.sha-78bc21c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
