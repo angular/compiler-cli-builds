b6108298b2a8033c19c9f9307340d84c5d0c8c56
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:28:54 +0000

    4bb5f63 docs: fix typo in dependency injection guide (#27616)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+154.sha-d6aca79";
+export declare const NGCC_VERSION = "7.2.0+157.sha-4bb5f63";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
