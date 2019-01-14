e2f814bb9d7b32d86669958165966427e83a7e65
Andrew Kushnir <akushnir@google.com>
2019-01-14 22:30:28 +0000

    d12db4e fix(bazel): incorrectly always uses ngc-wrapped from "npm" workspace (#28137)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+161.sha-e8495b4";
+export declare const NGCC_VERSION = "7.2.0+169.sha-d12db4e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
