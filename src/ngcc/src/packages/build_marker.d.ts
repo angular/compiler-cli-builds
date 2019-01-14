4ccf63714b116d68f0828b4d39abfb82427ce61b
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:18:58 +0000

    5a8c14b test(docs-infra): increase timeout for all redirection tests (#28103)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+97.sha-31556fa";
+export declare const NGCC_VERSION = "7.2.0-rc.0+98.sha-5a8c14b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
