18220a0b2f2d8cafc7833273a64c25b6e5fe4bc8
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:22:01 +0000

    48a03fc test(docs-infra): increase timeout for all redirection tests (#28103)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+146.sha-6fff74e';
+    exports.NGCC_VERSION = '7.2.0+147.sha-48a03fc';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
