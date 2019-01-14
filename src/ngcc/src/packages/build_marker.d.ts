3be6e5a56ee4c7b87905ae7a5561ab819d5474d5
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:20:28 +0000

    2558e68 docs: add browser polyfills for IE11 with hash-based routing (#27135)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+101.sha-5f616ca";
+export declare const NGCC_VERSION = "7.2.0-rc.0+104.sha-2558e68";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
