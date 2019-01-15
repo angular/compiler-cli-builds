cbbfeb97f54a5197db41d9294c3d6aa15b4eb07d
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:06:51 +0000

    f1fb62d fix(core): proper @internal and @nocollapse combined usage fix (#28138)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+128.sha-091a8a6';
+    exports.NGCC_VERSION = '7.2.0+170.sha-f1fb62d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
