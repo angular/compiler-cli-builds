9275cb938728002241bc70f3e4d278113a9ba03b
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:04:36 +0000

    6930451 refactor(ivy): remove def.attributes in favor of the `elementHostAttrs` instruction (#28089)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+178.sha-e62eeed";
+export declare const NGCC_VERSION = "7.2.0+179.sha-6930451";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
