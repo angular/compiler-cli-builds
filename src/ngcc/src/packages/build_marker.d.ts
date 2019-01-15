d497639e89fc60254ce74fe3706b9dcafd612c95
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:11:05 +0000

    d49d1e7 fix(service-worker): navigation urls backwards compatibility (#27244)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+181.sha-2e5752e";
+export declare const NGCC_VERSION = "7.2.0+182.sha-d49d1e7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
