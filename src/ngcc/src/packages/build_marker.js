d7def2852354dd7b60455bc25b5033177816fc6a
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:34:00 +0000

    67e6b63 fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27680)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0-rc.0+121.sha-88d4155';
+    exports.NGCC_VERSION = '7.2.0-rc.0+122.sha-67e6b63';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
