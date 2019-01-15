0b222aaceaded9d8d38ff249033cb18192068f2d
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:33:40 +0000

    60fecc1 fixup! refactor(ivy): simplify context discover based on feedback (#28101)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+174.sha-fa53150";
+export declare const NGCC_VERSION = "7.2.0+176.sha-60fecc1";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
