e6e027561823d6122d58dcb570a65d8b9aa837bb
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:09:05 +0000

    da8ee29 fix(ivy): throw meaningful error for uninitialized output (#28085)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+179.sha-6930451';
+    exports.NGCC_VERSION = '7.2.0+180.sha-da8ee29';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
