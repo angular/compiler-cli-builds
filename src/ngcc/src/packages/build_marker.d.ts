ca18a9bb9102057cceb9d5da5beb22b7f5d3224f
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:02:23 +0000

    35ec2dc fix(ivy): normalize summary and factory shim files paths (#28173)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0-rc.0+128.sha-332dcb2";
+export declare const NGCC_VERSION = "7.2.0-rc.0+129.sha-35ec2dc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
