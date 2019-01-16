495a47585ba0c70833f48ae0acf15bb840bbc78c
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:12:31 +0000

    bac71ef fix(ivy): componentFactories not populated in ModuleWithComponentFactories (#28112)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.0+196.sha-9a81f0d';
+    exports.NGCC_VERSION = '7.2.0+198.sha-bac71ef';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
