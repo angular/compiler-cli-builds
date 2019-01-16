4172a5e293c1ce2306fc29691604482896501ed2
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:11:25 +0000

    9a81f0d fix(ivy): update i18n/i18nStart and i18nAttributes instruction order (#28163)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.0+193.sha-3b7a571";
+export declare const NGCC_VERSION = "7.2.0+196.sha-9a81f0d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
