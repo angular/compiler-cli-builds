3d87a802956575365a9ce426b25ae702eaa04aca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:00:44 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.0+5.sha-80967ce';
+    exports.NGCC_VERSION = '8.0.0-beta.0+7.sha-8a08ff1';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
