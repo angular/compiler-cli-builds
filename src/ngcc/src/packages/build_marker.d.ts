3c3984ad0d950d896807c1fca1441fb9443a0143
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:34:39 +0000

    38343a2 build: set a default module_name for ts_library rules (#28051)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+21.sha-45bf911";
+export declare const NGCC_VERSION = "8.0.0-beta.0+22.sha-38343a2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
