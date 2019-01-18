9c3b735da9d37c16d06a4b3e58724f16b110ef71
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:28:15 +0000

    808898d fix(ivy): properly project individual nodes (#28152)

--- src/ngtsc/util/src/path.d.ts
+++ src/ngtsc/util/src/path.d.ts
@@ -7,3 +7,4 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/util/src/path" />
 export declare function relativePathBetween(from: string, to: string): string | null;
+export declare function normalizeSeparators(path: string): string;
