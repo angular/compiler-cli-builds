3c5b65ec34372ee0943c863e2a06639bbbcca29f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:04:03 +0000

    4c00059 feat(compiler-cli): resolve generated Sass/Less files to .css inputs (#28166)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.1+7.sha-93d78c9";
+export declare const NGCC_VERSION = "7.2.1+8.sha-4c00059";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.1+7.sha-93d78c9';
+    exports.NGCC_VERSION = '7.2.1+8.sha-4c00059';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
