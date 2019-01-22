85741f426e93e1e7b3fe364b19edcdbb86cd3b4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:17:09 +0000

    351ef2a fix(docs-infra): removal of the use of the ChildNode.remove() method that it isn't supported by IE (#28188)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+31.sha-058aafc";
+export declare const NGCC_VERSION = "8.0.0-beta.0+33.sha-351ef2a";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
