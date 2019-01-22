9b5f19a01389a08aac5b008ba1cb1c879a0887e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 23:30:15 +0000

    50df897 fix(router): `skipLocationChange` with named outlets (#28300)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+63.sha-ea1b5c1";
+export declare const NGCC_VERSION = "8.0.0-beta.0+65.sha-50df897";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
