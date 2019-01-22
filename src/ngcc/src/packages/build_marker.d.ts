3ff83520e11fd43624b0d7dedf803bb9e0be6b78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:35:10 +0000

    54565ed docs: fix crisis-detail milestone files (#27025)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.1+14.sha-ab08385";
+export declare const NGCC_VERSION = "7.2.1+15.sha-54565ed";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.1+14.sha-ab08385';
+    exports.NGCC_VERSION = '7.2.1+15.sha-54565ed';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
