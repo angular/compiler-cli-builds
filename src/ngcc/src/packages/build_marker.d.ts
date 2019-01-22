4d0677eb5ec477887ae596d7546c31490be052e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:58:12 +0000

    5b08a88 style(docs-infra): Active tslint rule `semicolon` (#28282)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+26.sha-7db0358";
+export declare const NGCC_VERSION = "8.0.0-beta.0+27.sha-5b08a88";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
