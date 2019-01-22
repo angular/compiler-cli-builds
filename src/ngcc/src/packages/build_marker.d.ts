7a7b5a82f0c7ebd65bac3d104d0e286c6fc865c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:52:42 +0000

    cf71668 build: hide vscode settings e.g. debug launch config (#28299)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.1+15.sha-54565ed";
+export declare const NGCC_VERSION = "7.2.1+16.sha-cf71668";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
