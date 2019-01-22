7f368361d44c1b27366aab72ab66c4a471f7d3c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:30:36 +0000

    7642308 docs: correct array to map (#27379)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.1+16.sha-cf71668';
+    exports.NGCC_VERSION = '7.2.1+19.sha-7642308';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
