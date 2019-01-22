de141d87e3637bc3ba143bd4d5ce57277b397997
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:37:54 +0000

    18a9afc docs(core): deprecate ViewEncapsulation.Native (#26361)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+59.sha-9882434";
+export declare const NGCC_VERSION = "8.0.0-beta.0+62.sha-18a9afc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
