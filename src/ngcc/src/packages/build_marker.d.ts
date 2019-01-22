495779c959ce2a6dd16560d6ecf65f62e3c3a7a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:42:02 +0000

    9dabeea ci: add a rebase check to the merge-pr script (#28250)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.0+24.sha-73dcd72";
+export declare const NGCC_VERSION = "8.0.0-beta.0+25.sha-9dabeea";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
