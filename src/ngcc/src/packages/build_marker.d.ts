e176094a12c82e9ad3284165fade087c117d4877
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:08:11 +0000

    cb93027 build: Fix gulp format for Node >= 10.14 (#28213)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.1+10.sha-f4ac96d";
+export declare const NGCC_VERSION = "7.2.1+12.sha-cb93027";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
