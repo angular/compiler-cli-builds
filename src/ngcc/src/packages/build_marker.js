690e94128916106c23771f309ffdb375ae458bb6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:11:05 +0000

    eb8ccf6 fix(docs-infra): removal of the use of the ChildNode.remove() method that it isn't supported by IE (#28188)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.1+11.sha-607fd8e';
+    exports.NGCC_VERSION = '7.2.1+13.sha-eb8ccf6';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
