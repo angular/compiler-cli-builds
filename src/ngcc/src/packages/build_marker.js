4b56b604f2d88554affc83af37f9d69ec4a5342a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:17:51 +0000

    5877b3f refactor: remove unused parameter in _main method invocation (#28203)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.2';
+    exports.NGCC_VERSION = '7.2.2+3.sha-5877b3f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
