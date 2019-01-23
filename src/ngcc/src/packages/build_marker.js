7877302b8e4de0e6695f59bf364eb0cdebb3c844
Jason Aden <jasonaden@google.com>
2019-01-23 23:22:10 +0000

    cbd6264 fix(ivy): link correct ngModule's injector to the bootstrapped component (#28183)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.1+17.sha-317cc92';
+    exports.NGCC_VERSION = '8.0.0-beta.1+18.sha-cbd6264';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
