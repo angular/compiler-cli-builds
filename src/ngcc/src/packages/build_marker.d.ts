5dd90d32502b816c1a81be6a9fcf06018aecce54
Jason Aden <jasonaden@google.com>
2019-01-23 23:21:46 +0000

    317cc92 test(ivy): turn on passing tests in platform-browser-dynamic (#28307)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+15.sha-32c61f4";
+export declare const NGCC_VERSION = "8.0.0-beta.1+17.sha-317cc92";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
