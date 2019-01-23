db5ea4c8fadd2e8694f4b92a588e9331b2871739
Jason Aden <jasonaden@google.com>
2019-01-23 23:16:16 +0000

    32c61f4 fix(ivy): ngUpgrade should distinguish element and module injectors (#28313)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+14.sha-22a43cf";
+export declare const NGCC_VERSION = "8.0.0-beta.1+15.sha-32c61f4";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
