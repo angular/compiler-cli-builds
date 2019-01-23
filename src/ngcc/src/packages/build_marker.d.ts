5c29d67b74ed2a14e94b0d005e2812e1a279727a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:22:13 +0000

    5430d2b fix(ivy): NgOnChangesFeature no longer included in hello_world (#28187)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+3.sha-cf8770f";
+export declare const NGCC_VERSION = "8.0.0-beta.1+10.sha-5430d2b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
