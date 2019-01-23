ebbf164c45f85b3f01671fcb6728e18a2cab61bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:34:10 +0000

    d8f2318 fix(ivy): generating incorrect tag name with namespace (#28298)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+1.sha-1fd6735";
+export declare const NGCC_VERSION = "8.0.0-beta.1+2.sha-d8f2318";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.1+1.sha-1fd6735';
+    exports.NGCC_VERSION = '8.0.0-beta.1+2.sha-d8f2318';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
