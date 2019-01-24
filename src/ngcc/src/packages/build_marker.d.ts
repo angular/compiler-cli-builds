8731d1c7dd0ca51616010cf75da03cbab6721ef1
Jason Aden <jasonaden@google.com>
2019-01-24 18:49:43 +0000

    465abab docs(animations): fix group and sequence function usage examples (#28305)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+21.sha-c1fb9c2";
+export declare const NGCC_VERSION = "8.0.0-beta.1+22.sha-465abab";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
