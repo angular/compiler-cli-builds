e66549e3da69b0c404668a0260fd2534cd242297
Jason Aden <jasonaden@google.com>
2019-01-25 21:23:47 +0000

    7496630 test(docs-infra): increase timeout for redirection tests more (#28290)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.1+29.sha-b2811e5';
+    exports.NGCC_VERSION = '8.0.0-beta.1+39.sha-7496630';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
