8b452c7fe587e16723d546d3a4f512f67914e2ad
Jason Aden <jasonaden@google.com>
2019-01-25 21:25:52 +0000

    fdc2b0b fix(ivy): queries should register matches from top to bottom (#28319)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+43.sha-3d5a919";
+export declare const NGCC_VERSION = "8.0.0-beta.1+35.sha-fdc2b0b";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
