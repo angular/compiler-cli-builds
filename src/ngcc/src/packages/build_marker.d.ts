f60e8688651231194f0a7755fc8e05f27130da91
Jason Aden <jasonaden@google.com>
2019-01-25 21:05:54 +0000

    a1b9995 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.2+15.sha-35f7ff0";
+export declare const NGCC_VERSION = "7.2.2+16.sha-a1b9995";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
