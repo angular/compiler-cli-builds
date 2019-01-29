88a02693efa775368f0301f99367846dd39ba50f
Jason Aden <jasonaden@google.com>
2019-01-29 18:01:18 +0000

    22f76df feat(compiler-cli): expose ngtsc as a TscPlugin (#28431)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+83.sha-e18a52e";
+export declare const NGCC_VERSION = "8.0.0-beta.1+84.sha-22f76df";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
