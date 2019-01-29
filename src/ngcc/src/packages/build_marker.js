402145905c5a74f50f25f993661fa9593bb6b5c2
Jason Aden <jasonaden@google.com>
2019-01-29 19:46:47 +0000

    227f7e4 Revert "feat(compiler-cli): expose ngtsc as a TscPlugin" (#28433)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.1+84.sha-22f76df';
+    exports.NGCC_VERSION = '8.0.0-beta.1+85.sha-227f7e4';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
