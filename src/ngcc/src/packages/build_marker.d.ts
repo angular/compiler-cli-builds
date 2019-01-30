bce9de7a965e052ece575f56924d1c3e252c3788
Jason Aden <jasonaden@google.com>
2019-01-30 00:32:57 +0000

    ea2a3f8 Revert "build: switch example e2e tests to bazel (#28402)" (#28438)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.2+29.sha-12b8a6e";
+export declare const NGCC_VERSION = "7.2.2+43.sha-ea2a3f8";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.2+29.sha-12b8a6e';
+    exports.NGCC_VERSION = '7.2.2+43.sha-ea2a3f8';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
