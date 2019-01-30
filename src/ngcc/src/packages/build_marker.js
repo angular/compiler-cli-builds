1271cdb3dec7c15f9f9a880af5a14f6855a5f211
Jason Aden <jasonaden@google.com>
2019-01-30 01:05:57 +0000

    d11c2f9 docs: fix typo in lifecycle-hooks.md (#28206)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.2+44.sha-0ff48a1';
+    exports.NGCC_VERSION = '7.2.2+47.sha-d11c2f9';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
