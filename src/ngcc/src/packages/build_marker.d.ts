39a657e73eb3c5041e231a81a0de140efde60ce7
Jason Aden <jasonaden@google.com>
2019-01-30 00:50:38 +0000

    41e68f7 style: change to American English (#27266)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+94.sha-dabcb3e";
+export declare const NGCC_VERSION = "8.0.0-beta.1+95.sha-41e68f7";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
