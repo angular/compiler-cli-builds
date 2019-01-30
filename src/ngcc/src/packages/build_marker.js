d6cc473f0e6c36f989b32562318c5a7652be3b0e
Jason Aden <jasonaden@google.com>
2019-01-30 00:49:18 +0000

    41e68f7 style: change to American English (#27266)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.1+94.sha-dabcb3e';
+    exports.NGCC_VERSION = '8.0.0-beta.1+95.sha-41e68f7';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
