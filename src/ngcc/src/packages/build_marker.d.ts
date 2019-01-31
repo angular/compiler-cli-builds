6a06bdece138c9eadde821869fb52ff95fe3fe0e
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 21:51:26 +0000

    35e45dc fix(ivy): prevent errors from views being destroyed twice (#28413)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.1+109.sha-a227c52";
+export declare const NGCC_VERSION = "8.0.0-beta.2+3.sha-35e45dc";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
