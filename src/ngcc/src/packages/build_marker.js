3d218ad8249768b9d37d162beb7b7b163cadfc6f
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 17:48:27 +0000

    c0b3835 ci: fix symbol test to handle duplicate symbols (#28459)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.2+3.sha-35e45dc';
+    exports.NGCC_VERSION = '8.0.0-beta.2+4.sha-c0b3835';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
