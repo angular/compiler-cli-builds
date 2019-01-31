2507b65c765bfd0e6b58d10272f65a001483699f
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 17:46:56 +0000

    c0b3835 ci: fix symbol test to handle duplicate symbols (#28459)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.2+3.sha-35e45dc";
+export declare const NGCC_VERSION = "8.0.0-beta.2+4.sha-c0b3835";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
