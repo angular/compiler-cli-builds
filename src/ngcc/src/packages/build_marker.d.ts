69d4c1b8447b611996dfd4682d6e3d3b9227fd0c
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:11:27 +0000

    ec6e730 perf: pngcrush all pngs (#28479)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.2+8.sha-1b6d8a7";
+export declare const NGCC_VERSION = "8.0.0-beta.2+10.sha-ec6e730";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
