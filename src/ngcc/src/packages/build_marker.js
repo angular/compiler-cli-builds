75e5c2dc604991019d260402316b0b3543ed95b0
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:14:32 +0000

    1a25144 perf: pngcrush all pngs (#28479)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.3+3.sha-d85d396';
+    exports.NGCC_VERSION = '7.2.3+2.sha-1a25144';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
