18f53a4263a79653d1318aa5f41e7b48ebe5f1be
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:42:11 +0000

    8930f60 refactor(ivy): create an Ivy version of tree-shakable providers test (#28477)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.2+10.sha-ec6e730";
+export declare const NGCC_VERSION = "8.0.0-beta.2+12.sha-8930f60";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
