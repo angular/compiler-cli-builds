ff13c01aabf216d9fa34aef2e8f131c55db464f6
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:07:41 +0000

    5f9d574 build(docs-infra): update terser version in lockfile (#28512)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.3+2.sha-1a25144";
+export declare const NGCC_VERSION = "7.2.3+9.sha-5f9d574";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
