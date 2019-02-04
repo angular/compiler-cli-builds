07cfacb6ab5b5951835789fd28c137340ebec703
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:15:28 +0000

    c64e666 test: log cli version in `cli-hello-world[-ivy]` integration projects (#28421)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.3+9.sha-5f9d574';
+    exports.NGCC_VERSION = '7.2.3+16.sha-c64e666';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
