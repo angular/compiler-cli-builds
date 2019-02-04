4c92a5ffef17a61fb756d3184203c38e7e111807
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 21:53:55 +0000

    dad1bc7 build: bump year (#28220)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.3+18.sha-2109c30';
+    exports.NGCC_VERSION = '7.2.3+19.sha-dad1bc7';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
