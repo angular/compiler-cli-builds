498373cd4feb95224cad53b2e65d98537ba96228
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:11:55 +0000

    c1aa1bf build: `gulp format` only changed lines by default (#28411)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.3+19.sha-dad1bc7";
+export declare const NGCC_VERSION = "7.2.3+20.sha-c1aa1bf";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
