c96a58dc7d68c642e44e138f4c8c7fbbcb2e61e3
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:16:24 +0000

    2109c30 docs: fix shadow variable in tutorial example (#28499) PR Close #28499

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.3+16.sha-c64e666";
+export declare const NGCC_VERSION = "7.2.3+18.sha-2109c30";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
