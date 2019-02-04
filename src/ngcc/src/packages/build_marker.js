0bfb48652e34abc7d37582fd55dfafd28c0c836f
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:05:44 +0000

    fc88a79 fix(ivy): errors not being logged to ErrorHandler (#28447)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.2+23.sha-3d52271';
+    exports.NGCC_VERSION = '8.0.0-beta.2+26.sha-fc88a79';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
