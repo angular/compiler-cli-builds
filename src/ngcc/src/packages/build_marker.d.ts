5c8b0945fdc4db9f1b12e025f35735f2808c16ca
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:44:30 +0000

    a6fd118 build: switch to typescript's es2015 typings (#28134)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.3+24.sha-fe6e76c";
+export declare const NGCC_VERSION = "7.2.3+25.sha-a6fd118";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.3+24.sha-fe6e76c';
+    exports.NGCC_VERSION = '7.2.3+25.sha-a6fd118';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
