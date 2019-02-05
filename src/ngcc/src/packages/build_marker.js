978b9a15fe083810d335edd0ca3e2a6a8b2f331d
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:55:00 +0000

    744b020 build: fix failing compiler-cli tests on windows (#28352)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.2+39.sha-9ef8d2b';
+    exports.NGCC_VERSION = '8.0.0-beta.2+48.sha-744b020';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
