a7bdda9612cfb188aa9d571bca9561deb6010beb
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 05:35:27 +0000

    22d3226 revert: fix(ivy): remove query results from destroyed embedded views (#28445)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+14.sha-5ebc0da";
+export declare const NGCC_VERSION = "8.0.0-beta.3+15.sha-22d3226";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
