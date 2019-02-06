122f6944c43bfddfb9016210e511a5428bbee0f3
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:22:21 +0000

    a0e3a8e build: fix revert issue with bitmap.ts (#28576)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+11.sha-be8a403";
+export declare const NGCC_VERSION = "7.2.4+21.sha-a0e3a8e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
