51c2d9e442b74ee0085e8463dffcdd60c9e01fa7
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:45:59 +0000

    47f5b5f docs(service-worker): note about isStable/setInterval (#28102)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+21.sha-a0e3a8e";
+export declare const NGCC_VERSION = "7.2.4+23.sha-47f5b5f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+21.sha-a0e3a8e';
+    exports.NGCC_VERSION = '7.2.4+23.sha-47f5b5f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
