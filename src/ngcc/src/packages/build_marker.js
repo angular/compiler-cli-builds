97d0b18a935b7f5d244de325a3901637dbe0920d
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 19:52:00 +0000

    0e6f799 test(ivy): update e2e tests to use Renderer2 instead of Renderer (#28558)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+25.sha-6709db9';
+    exports.NGCC_VERSION = '8.0.0-beta.3+26.sha-0e6f799';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
