e6113da17a7f2c15cbbcabb4926fbe793c55be00
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:17:51 +0000

    5a6f0d0 docs: release notes for the v7.2.4 release

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.2+60.sha-979582a";
+export declare const NGCC_VERSION = "8.0.0-beta.2+61.sha-5a6f0d0";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
