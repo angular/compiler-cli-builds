0be606e3b968a16bf4d517055f530800f17c26f5
Misko Hevery <misko@hevery.com>
2019-02-07 05:38:33 +0000

    3de06dd build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+28.sha-851cf16";
+export declare const NGCC_VERSION = "8.0.0-beta.3+34.sha-3de06dd";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
