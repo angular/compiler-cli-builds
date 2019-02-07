c51ab51dc8ff18f61a139b4c95dd934e5970acfa
Miško Hevery <misko@hevery.com>
2019-02-07 20:55:40 +0000

    7660d0d fix(ivy): extended next pointer lookup while traversing tNode tree (#28533)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+42.sha-264ef72";
+export declare const NGCC_VERSION = "8.0.0-beta.3+43.sha-7660d0d";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
