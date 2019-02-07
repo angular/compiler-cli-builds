963d3b51716c39d71d9fcaa4422c1b0699497a5d
Misko Hevery <misko@hevery.com>
2019-02-07 05:38:38 +0000

    8d90142 build: disable sandbox when running protractor through bazel (#28557)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+34.sha-3de06dd';
+    exports.NGCC_VERSION = '8.0.0-beta.3+29.sha-8d90142';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
