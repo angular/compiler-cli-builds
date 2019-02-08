1e507eee06b974aacd8694363234a0c9a359567d
Miško Hevery <misko@hevery.com>
2019-02-08 01:03:06 +0000

    c3fadad docs: update HttpHeader documentation (#26233)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+47.sha-b65fe62';
+    exports.NGCC_VERSION = '8.0.0-beta.3+48.sha-c3fadad';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
