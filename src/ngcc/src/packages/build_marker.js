92f34845f687aa9833198c20ab91a969d4bc9e0c
Miško Hevery <misko@hevery.com>
2019-02-08 17:34:11 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+56.sha-d91ea2c';
+    exports.NGCC_VERSION = '8.0.0-beta.3+57.sha-ceada77';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
