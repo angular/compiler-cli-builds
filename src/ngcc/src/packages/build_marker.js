9ef430175fb2a4715d2eaff11240a6f677a4d300
Miško Hevery <misko@hevery.com>
2019-02-08 01:23:01 +0000

    4b7264f ci: use fallback caches (#28545)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+49.sha-6f9dd1b';
+    exports.NGCC_VERSION = '8.0.0-beta.3+50.sha-4b7264f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
