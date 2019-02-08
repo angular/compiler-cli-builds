107dfa210586e65e2b59a75df7598fb21a91613e
Miško Hevery <misko@hevery.com>
2019-02-08 23:24:17 +0000

    2f73c55 build: fix invalid bazel target referenced by benchmarks (#28626)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+39.sha-e4b1bdb.with-local-changes";
+export declare const NGCC_VERSION = "7.2.4+44.sha-2f73c55";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
