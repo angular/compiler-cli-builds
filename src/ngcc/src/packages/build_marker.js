6693c24e96a8c016cc749a40fd0b1e175f7b4874
Miško Hevery <misko@hevery.com>
2019-02-08 01:14:10 +0000

    1b6fb5b refactor(common): Remove deprecated FileType (#28583)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+32.sha-57a5ba3';
+    exports.NGCC_VERSION = '7.2.4+31.sha-1b6fb5b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
