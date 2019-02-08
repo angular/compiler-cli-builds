d5c557cf1df230186fb3ad670237e4f2463ebc15
Miško Hevery <misko@hevery.com>
2019-02-08 17:35:28 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+56.sha-d91ea2c";
+export declare const NGCC_VERSION = "8.0.0-beta.3+57.sha-ceada77";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
