8622c4c7a24ad56dd0a0bc8e78180c3b07ee8a4f
Misko Hevery <misko@hevery.com>
2019-02-08 06:05:32 +0000

    9067025 test: update Material blocklist (#28605)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+53.sha-94f042b";
+export declare const NGCC_VERSION = "8.0.0-beta.3+54.sha-9067025";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
