2f87fa0fca3441d15698f8cccb1d1eaa9c82455f
Miško Hevery <misko@hevery.com>
2019-02-08 01:11:41 +0000

    57a5ba3 ci: use fallback caches (#28545)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+30.sha-bd9da26";
+export declare const NGCC_VERSION = "7.2.4+32.sha-57a5ba3";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
