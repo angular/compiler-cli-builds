65f7a848fee73e8d20c5352af71f4513ebbb2017
Miško Hevery <misko@hevery.com>
2019-02-11 17:27:28 +0000

    9ec8aa5 docs: re-enable E2E tests for Http guide (#28592)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+71.sha-81329c8';
+    exports.NGCC_VERSION = '8.0.0-beta.3+88.sha-9ec8aa5';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
