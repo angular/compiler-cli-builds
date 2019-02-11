3df4449cd1bd291410ac033f2d6a5609b358d8bf
Miško Hevery <misko@hevery.com>
2019-02-11 17:29:32 +0000

    aa163be refactor(compiler): allow disabling external symbol factory reexports (#28594)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+44.sha-2f73c55";
+export declare const NGCC_VERSION = "7.2.4+45.sha-aa163be";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+44.sha-2f73c55';
+    exports.NGCC_VERSION = '7.2.4+45.sha-aa163be';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
