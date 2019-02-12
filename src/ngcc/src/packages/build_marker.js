fae6da5d4b77e45b0e49749761f178d2929e849b
Miško Hevery <misko@hevery.com>
2019-02-12 18:47:56 +0000

    2bf526b docs: add a README for third_party usage (#28651)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+46.sha-8bd7d5b';
+    exports.NGCC_VERSION = '7.2.4+47.sha-2bf526b';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
