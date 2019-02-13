2ee5e3aec67d92e9159fb914305531007e1ce46b
Miško Hevery <misko@hevery.com>
2019-02-13 18:19:25 +0000

    fc8f4f8 refactor(ivy): remove unused parameter in postProcessBaseDirective (#28631)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+126.sha-ac58d01";
+export declare const NGCC_VERSION = "8.0.0-beta.3+128.sha-fc8f4f8";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
