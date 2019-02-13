8960f999181545d7b6cf7eca6d1c1dcd5f0af366
Miško Hevery <misko@hevery.com>
2019-02-13 18:19:31 +0000

    91b7152 feat(compiler-cli): no longer re-export external symbols by default (#28633)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+130.sha-06ec95f';
+    exports.NGCC_VERSION = '8.0.0-beta.3+129.sha-91b7152';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
