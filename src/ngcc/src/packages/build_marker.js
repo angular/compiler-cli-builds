b42f4e6aa90aab34029d13513168ff568d12346d
Miško Hevery <misko@hevery.com>
2019-02-13 04:52:01 +0000

    73f9db5 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+47.sha-2bf526b';
+    exports.NGCC_VERSION = '7.2.4+48.sha-73f9db5';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
