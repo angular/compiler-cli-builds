6a3208c2d8ee9fd8f798b65ec8fadbff8d2db48b
Miško Hevery <misko@hevery.com>
2019-02-13 20:23:00 +0000

    644e7a2 docs: add di-related api doc (#27731)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+134.sha-f8b9e61";
+export declare const NGCC_VERSION = "8.0.0-beta.3+135.sha-644e7a2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
