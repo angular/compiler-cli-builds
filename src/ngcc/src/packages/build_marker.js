e41b322a31ab6045392a7cfa41142c1d1efe6db6
Miško Hevery <misko@hevery.com>
2019-02-13 19:41:32 +0000

    06ec95f fix(ivy): allow directive inheritance in strict mode (#28634)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+124.sha-08de52b';
+    exports.NGCC_VERSION = '8.0.0-beta.3+130.sha-06ec95f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
