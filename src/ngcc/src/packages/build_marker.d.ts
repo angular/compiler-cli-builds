10284badd9ef44c77562082be57d7fc0eaeacb8b
Misko Hevery <misko@hevery.com>
2019-02-14 03:51:20 +0000

    8cec4b3 build: make api-extractor work in google3 (#28588)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+168.sha-b0afc4c";
+export declare const NGCC_VERSION = "8.0.0-beta.3+166.sha-8cec4b3";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
