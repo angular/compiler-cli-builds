31bc4b5799dc4de3f6d0bec14f67eea606544528
Misko Hevery <misko@hevery.com>
2019-02-14 03:39:21 +0000

    b23582d docs(animations): fixed some grammar (#28708) PR Close #28708

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.4+73.sha-d3c6e51';
+    exports.NGCC_VERSION = '7.2.4+74.sha-b23582d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
