caf2b768d41a75d4f58f27125a37327a5955968f
Miško Hevery <misko@hevery.com>
2019-02-14 19:52:58 +0000

    63e5d27 fix(ivy): rethrow event handler errors in tests (#28707)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+178.sha-7cbc36f";
+export declare const NGCC_VERSION = "8.0.0-beta.3+177.sha-63e5d27";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
