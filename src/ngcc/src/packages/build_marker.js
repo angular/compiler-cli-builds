a95cea8e956a7032f601d9e85a9013ba61699f0d
Miško Hevery <misko@hevery.com>
2019-02-14 19:47:51 +0000

    627cecd fix(ivy): ensure host bindings and host styling works on a root component (#28664)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+174.sha-b41da03';
+    exports.NGCC_VERSION = '8.0.0-beta.3+175.sha-627cecd';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
