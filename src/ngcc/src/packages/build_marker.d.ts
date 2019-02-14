3147842f61227fbd808e2b0246568a95b9a89c9c
Miško Hevery <misko@hevery.com>
2019-02-14 19:56:42 +0000

    80a5934 fix(ivy): support schemas at runtime (#28637)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.3+177.sha-63e5d27";
+export declare const NGCC_VERSION = "8.0.0-beta.3+179.sha-80a5934";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
