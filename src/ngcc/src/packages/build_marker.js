e838a1ac1654e687660fadc2c4461d2d575417fb
Misko Hevery <misko@hevery.com>
2019-02-14 03:49:31 +0000

    b0afc4c refactor(ivy): remove unused constructor argument (#28677)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+161.sha-36df905';
+    exports.NGCC_VERSION = '8.0.0-beta.3+168.sha-b0afc4c';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
