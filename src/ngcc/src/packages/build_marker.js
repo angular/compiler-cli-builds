c1f806bc944996da92dde22026a95cc3f688d353
Miško Hevery <misko@hevery.com>
2019-02-14 19:45:24 +0000

    b41da03 build(docs-infra): upgrade cli command docs sources to 1631f7d62 (#28727)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+173.sha-65c2dea';
+    exports.NGCC_VERSION = '8.0.0-beta.3+174.sha-b41da03';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
