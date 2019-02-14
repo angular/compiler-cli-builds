f63c8f71886ac268ea76090899d33d943c0d90c5
Misko Hevery <misko@hevery.com>
2019-02-14 03:46:45 +0000

    5e701a2 build(bazel): rollback to chromium 69 (latest version that still works on OSX) (#28698)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.4+74.sha-b23582d";
+export declare const NGCC_VERSION = "7.2.4+78.sha-5e701a2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
