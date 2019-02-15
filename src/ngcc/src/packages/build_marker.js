341d43608032fc6f78c28dff0e268b230c473b26
Miško Hevery <misko@hevery.com>
2019-02-15 22:41:52 +0000

    fcc8c56 docs: release notes for the v7.2.5 release

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.3+181.sha-8accc98';
+    exports.NGCC_VERSION = '8.0.0-beta.3+182.sha-fcc8c56';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
