dbfe5730fe7dd9eb29a0673394255451b70d5dc9
Misko Hevery <misko@hevery.com>
2019-02-17 05:22:22 +0000

    2e3cc45 build(bazel): specify tsconfig-test dependency (#28696)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+2.sha-c1094cf";
+export declare const NGCC_VERSION = "8.0.0-beta.4+1.sha-2e3cc45";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
