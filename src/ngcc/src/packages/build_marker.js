f6a31fddb752361f472f9dce36dab2bcf2dd459f
Igor Minar <igor@angular.io>
2019-02-19 21:17:05 +0000

    692ddfc fix(ivy): allign DebugNode.componentInstance semantics with view engine (#28756)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.4+21.sha-00a8b07';
+    exports.NGCC_VERSION = '8.0.0-beta.4+23.sha-692ddfc';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
