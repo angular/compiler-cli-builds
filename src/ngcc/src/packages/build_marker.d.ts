e00ba0e0b6774409c1af8d879097983821983756
Igor Minar <igor@angular.io>
2019-02-19 21:51:44 +0000

    7fc38d4 docs: fix wrong filename in text (#28832)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+19.sha-ff2c342";
+export declare const NGCC_VERSION = "7.2.5+22.sha-7fc38d4";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
