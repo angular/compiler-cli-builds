e11162b306ffa2e677d6d2e2916c7ffde2dc801e
Igor Minar <igor@angular.io>
2019-02-19 20:49:34 +0000

    f487121 ci: add /aio/content/images/bios/** to @angular/fw-docs-marketing group (#28835)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.5+5.sha-2a23eca';
+    exports.NGCC_VERSION = '7.2.5+6.sha-f487121';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
