f77573cf9d1d0407b299414fa949a455b2ef45ec
Igor Minar <igor@angular.io>
2019-02-19 23:50:59 +0000

    3c1a162 fix(ivy): support static ContentChild queries (#28811)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+32.sha-ae16378";
+export declare const NGCC_VERSION = "8.0.0-beta.4+34.sha-3c1a162";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
