d044cdc1ba415cc86cbfb227f24c3b4cddf6a402
Igor Minar <igor@angular.io>
2019-02-19 21:18:19 +0000

    3b78f9e build(compiler-cli): upgrade chokidar to latest version (#28797)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+16.sha-f6ca619";
+export declare const NGCC_VERSION = "7.2.5+17.sha-3b78f9e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
