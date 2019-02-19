c75302e028977b3ebdcd33e5bdbe104d3b7e156e
Igor Minar <igor@angular.io>
2019-02-19 20:59:31 +0000

    cdcbb53 docs: capitalize karma (#28775)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+7.sha-1df6b16";
+export declare const NGCC_VERSION = "7.2.5+9.sha-cdcbb53";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
