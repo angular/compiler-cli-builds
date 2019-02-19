db9a796d7e9b63a9fe810f9d9d81ad888ac2e8bf
Igor Minar <igor@angular.io>
2019-02-19 21:08:37 +0000

    00a8b07 ci: combine "define_env_vars" and "download_yarn" anchor (#28788)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+20.sha-80c7aff";
+export declare const NGCC_VERSION = "8.0.0-beta.4+21.sha-00a8b07";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
