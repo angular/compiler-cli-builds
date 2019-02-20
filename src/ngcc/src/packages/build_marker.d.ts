1c7b855c296c9b1d7da3be3aa6bd7735487d7e73
Ben Lesh <ben@benlesh.com>
2019-02-20 22:38:34 +0000

    a7e1c0c feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+28.sha-19afb79";
+export declare const NGCC_VERSION = "8.0.0-beta.4+56.sha-a7e1c0c";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
