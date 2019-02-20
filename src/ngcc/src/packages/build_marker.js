c28cfaa9140aaf099efdf9b1624e7e6bca81605d
Igor Minar <igor@angular.io>
2019-02-20 17:44:30 +0000

    04ae125 fix(docs-infra): prevent extra space at the bottom of the page in Chrome (#28864)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.4+50.sha-1832e0f';
+    exports.NGCC_VERSION = '8.0.0-beta.4+51.sha-04ae125';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
