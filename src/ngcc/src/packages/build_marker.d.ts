1b572a4831f98c980ed8271265a89a67a88e620e
Igor Minar <igor@angular.io>
2019-02-20 21:30:38 +0000

    27eb8f2 build: update deps in ./scripts/release/post-check

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+29.sha-843fc7d";
+export declare const NGCC_VERSION = "7.2.5+31.sha-27eb8f2";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
