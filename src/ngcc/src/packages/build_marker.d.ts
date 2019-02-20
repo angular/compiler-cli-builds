dd2960ab03a7ddc02627ac2db5c480b017e1b895
Igor Minar <igor@angular.io>
2019-02-20 19:17:11 +0000

    b9eb662 test(bazel): Upgrade CLI dependencies for schematics test (#28872)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+51.sha-04ae125";
+export declare const NGCC_VERSION = "8.0.0-beta.4+52.sha-b9eb662";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
