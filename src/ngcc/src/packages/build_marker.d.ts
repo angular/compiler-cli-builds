7782cfbc212cd44fbe8bb258be6d93d410c216ed
Igor Minar <igor@angular.io>
2019-02-20 00:14:33 +0000

    ad6475f fix(ivy): re-create node links after creating/moving/removing nodes with i18n (#28827)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.4+34.sha-3c1a162";
+export declare const NGCC_VERSION = "8.0.0-beta.4+35.sha-ad6475f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
