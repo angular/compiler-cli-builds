57a7d8870a73832cb88408be396fd950d2584485
Igor Minar <igor@angular.io>
2019-02-20 21:39:47 +0000

    81c3104 docs: release notes for the v7.2.6 release

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.4+53.sha-68da465';
+    exports.NGCC_VERSION = '8.0.0-beta.4+54.sha-81c3104';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
