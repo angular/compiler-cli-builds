d09835bd50c7da7aba80ff635ab4f378da86b7a6
Igor Minar <igor@angular.io>
2019-02-20 20:21:59 +0000

    843fc7d Revert "feat(router): add hash-based navigation option to setUpLocationSync (#28609)"

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.5+27.sha-48cabb4';
+    exports.NGCC_VERSION = '7.2.5+29.sha-843fc7d';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
