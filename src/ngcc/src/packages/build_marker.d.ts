eabd4ff516a2eac351769ea1ef8837b620956f53
Igor Minar <igor@angular.io>
2019-02-20 00:35:47 +0000

    fc895ba feat(router): add hash-based navigation option to setUpLocationSync (#28609)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+23.sha-9a51528";
+export declare const NGCC_VERSION = "7.2.5+24.sha-fc895ba";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.5+23.sha-9a51528';
+    exports.NGCC_VERSION = '7.2.5+24.sha-fc895ba';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
