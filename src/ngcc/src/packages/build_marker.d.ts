5181e453ed77a53bb03f0bc3a29278d7d554f581
Igor Minar <igor@angular.io>
2019-02-20 17:38:59 +0000

    cfe4732 fix(docs-infra): prevent extra space at the bottom of the page in Chrome (#28864)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.5+25.sha-069146e";
+export declare const NGCC_VERSION = "7.2.5+28.sha-cfe4732";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
