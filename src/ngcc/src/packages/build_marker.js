611cc6d9d5e948db53b01f084d580018e416189c
Igor Minar <igor@angular.io>
2019-02-20 06:45:51 +0000

    669b441 test(ivy): update material golden file to turn on passing tests (#28848)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.4+44.sha-1eccf64';
+    exports.NGCC_VERSION = '8.0.0-beta.4+45.sha-669b441';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
