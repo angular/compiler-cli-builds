06170e77cec54a4f5805e9c7b55a65e40c925720
Igor Minar <igor@angular.io>
2019-02-21 20:57:59 +0000

    d8704a3 test(ivy): turn on passing Material tests for paginator (#28890)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+18.sha-64e6aaa';
+    exports.NGCC_VERSION = '8.0.0-beta.5+19.sha-d8704a3';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
