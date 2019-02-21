5574564d05a453b254e3456094a606e92ef4589d
Igor Minar <igor@angular.io>
2019-02-21 08:41:20 +0000

    72d043f fix(ivy): check the presence of .css resource for styleUrls (#28770)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+13.sha-95d9aa2";
+export declare const NGCC_VERSION = "8.0.0-beta.5+11.sha-72d043f";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+13.sha-95d9aa2';
+    exports.NGCC_VERSION = '8.0.0-beta.5+11.sha-72d043f';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
