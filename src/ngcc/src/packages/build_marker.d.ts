32dcac9c6b61c745c0c20b3ee725720607e3556f
Ben Lesh <ben@benlesh.com>
2019-02-21 21:01:47 +0000

    3f2b51b ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+19.sha-d8704a3";
+export declare const NGCC_VERSION = "8.0.0-beta.5+20.sha-3f2b51b.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
