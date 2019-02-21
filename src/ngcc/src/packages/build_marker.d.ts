890c3fdcf354479d36de03a8081363b64831f926
Ben Lesh <ben@benlesh.com>
2019-02-21 02:44:38 +0000

    58436fd fix(ivy): unable to import shim factory files on case-insensitive platforms (#28831)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+7.sha-d0b6622";
+export declare const NGCC_VERSION = "8.0.0-beta.5+9.sha-58436fd";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+7.sha-d0b6622';
+    exports.NGCC_VERSION = '8.0.0-beta.5+9.sha-58436fd';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
