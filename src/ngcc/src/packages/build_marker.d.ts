c3f92f5857cc9b24929963bb4023f1d1f78c6fdb
Igor Minar <igor@angular.io>
2019-02-21 17:54:56 +0000

    e1aaa7e fix(ivy): component destroy hook called twice when configured as provider (#28470)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+15.sha-ebffde7";
+export declare const NGCC_VERSION = "8.0.0-beta.5+17.sha-e1aaa7e";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
