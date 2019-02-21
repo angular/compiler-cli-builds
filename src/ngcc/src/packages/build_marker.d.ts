3bb53ccf93b5dcefdb91f0ecb4eaf448a9fd932d
Igor Minar <igor@angular.io>
2019-02-21 08:33:43 +0000

    be121bb fix(ivy): restore @fileoverview annotations for Closure (#28723)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+9.sha-58436fd";
+export declare const NGCC_VERSION = "8.0.0-beta.5+10.sha-be121bb";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
