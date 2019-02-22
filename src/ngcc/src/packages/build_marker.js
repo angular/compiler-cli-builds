4a44c81c00c463d4f45af5b0769197747c373b97
Igor Minar <igor@angular.io>
2019-02-22 02:19:22 +0000

    9defc00 build(bazel): cleanup the jasmine bootstrap code (#28906)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+23.sha-1145bdb.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+24.sha-9defc00.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
