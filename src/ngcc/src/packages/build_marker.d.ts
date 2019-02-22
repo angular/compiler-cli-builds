6ff6d81f67cf0fc5fc2241eb64ff25e1b7d753ef
Igor Minar <igor@angular.io>
2019-02-22 06:32:53 +0000

    9b77434 ci: update circleci post checkout to v2 syntax (#28472)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.6+13.sha-ce219cc.with-local-changes";
+export declare const NGCC_VERSION = "7.2.6+14.sha-9b77434.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
