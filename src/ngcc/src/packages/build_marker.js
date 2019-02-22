217f3adc63ce36f0eaadc74efa663d4c930da345
Ben Lesh <ben@benlesh.com>
2019-02-22 21:48:14 +0000

    f0989b7 ci: share data between jobs using workspaces (#28928)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+49.sha-c1392ce.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+58.sha-f0989b7.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
