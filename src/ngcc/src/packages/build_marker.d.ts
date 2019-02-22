3d6cb49a13870cf28ce5ea1a5e535b9b1170b93a
Ben Lesh <ben@benlesh.com>
2019-02-22 20:21:25 +0000

    43181ea fix(ivy): host listeners being inherited twice (#28902)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+44.sha-9dac04f.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.5+45.sha-43181ea.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
