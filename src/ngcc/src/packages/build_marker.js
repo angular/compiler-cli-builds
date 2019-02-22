73e96124ad3081a0bac1bc5af805644fb9250f72
Igor Minar <igor@angular.io>
2019-02-22 07:47:36 +0000

    599e2e2 feat(docs-infra): add meta[name]=Description to angular.io's index.html (#28916)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+28.sha-94223a0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+32.sha-599e2e2.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
