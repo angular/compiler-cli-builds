4aceff1ceeb6617be384abba7f008815e8adfdb1
Ben Lesh <ben@benlesh.com>
2019-02-22 20:35:51 +0000

    fafabc0 build(bazel): use http_server rule from rules_nodejs (#28905)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+45.sha-43181ea.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+47.sha-fafabc0.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
