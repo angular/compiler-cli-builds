5da4b42c407d40b68904980d959b3b88ab63c73c
Ben Lesh <ben@benlesh.com>
2019-02-22 19:14:13 +0000

    7f3e3a8 docs(bazel): Use CLI 8 for Bazel schematics (#28908)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+41.sha-230a941.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
