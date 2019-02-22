6ed2b0ae613058c75f04a9200e94e269c7ddd99d
Igor Minar <igor@angular.io>
2019-02-22 05:12:52 +0000

    ce219cc docs(common): update docs for HttpClient methods (#26143)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.6+12.sha-53bbb01.with-local-changes";
+export declare const NGCC_VERSION = "7.2.6+13.sha-ce219cc.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
