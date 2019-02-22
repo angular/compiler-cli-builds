8e20fdfce2d679a3fbc3b38dc29c3849bc57af4e
Igor Minar <igor@angular.io>
2019-02-22 02:18:54 +0000

    32ae84d fixup! fix(ivy): incorrectly remapping certain properties that refer to inputs (#28765)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+24.sha-9defc00.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.5+26.sha-32ae84d.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
