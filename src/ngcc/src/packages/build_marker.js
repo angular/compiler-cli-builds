e6f660f78da66e2bb52a38a0959d5b05305ee2b5
Ben Lesh <ben@benlesh.com>
2019-02-22 18:25:06 +0000

    230a941 refactor(ivy): remove superfluous isComponentDef function (#28880)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+40.sha-7c20bf8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+41.sha-230a941.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
