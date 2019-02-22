cbd1981387f41c139170e520afcc0698d27dbec4
Igor Minar <igor@angular.io>
2019-02-22 02:09:12 +0000

    f4d6525 build: use uname instead of relying on sed --version (#28900)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+20.sha-3f2b51b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+22.sha-f4d6525.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
