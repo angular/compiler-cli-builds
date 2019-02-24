df64793741aea741f5728e069be4539e4cc4e15c
Ben Lesh <ben@benlesh.com>
2019-02-24 21:00:43 +0000

    f79cd59 docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+62.sha-350802b.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.5+63.sha-f79cd59.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
