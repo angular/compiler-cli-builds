f7797861e9f1c7354b4ea249e019a6da7ae84a5a
Ben Lesh <ben@benlesh.com>
2019-02-25 18:51:10 +0000

    ef1b9e6 docs: add FancyGrid to resources page (#28949)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+71.sha-65d3dda.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
