e20760dfa19aa2884ab6e97eb981a8aa7897d153
Ben Lesh <ben@benlesh.com>
2019-02-25 18:43:00 +0000

    1e97d51 docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.6+29.sha-7e1e00c.with-local-changes";
+export declare const NGCC_VERSION = "7.2.6+30.sha-1e97d51.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
