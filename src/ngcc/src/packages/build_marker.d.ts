37f2a3a570991bdec7d0d27d35e425a7f0d7e707
Ben Lesh <ben@benlesh.com>
2019-02-25 18:47:15 +0000

    0071048 docs: Updated TOH Heros documentation (#28347)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.5+68.sha-9f7a9c6.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.5+69.sha-0071048.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
