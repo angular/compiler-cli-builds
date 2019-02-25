5068b66173190a3cc563fbe65ca24c2696529d0c
Ben Lesh <ben@benlesh.com>
2019-02-25 18:36:52 +0000

    7e1e00c docs: add ngVikings 2019 to events page (#28501)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.6+28.sha-481f4b7.with-local-changes';
+    exports.NGCC_VERSION = '7.2.6+29.sha-7e1e00c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
