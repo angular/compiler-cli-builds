b7391d9a28043b12d50a9a4e551456e723ae8b42
Ben Lesh <ben@benlesh.com>
2019-02-25 21:23:26 +0000

    7a11242 docs: add Nebular to UI Components section in resources (#28838) PR Close #28838

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+73.sha-65de0d6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+74.sha-7a11242.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
