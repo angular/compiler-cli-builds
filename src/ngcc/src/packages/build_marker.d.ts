48d94551b79484fac91db3bae7891d8ebe14fc5c
Ben Lesh <ben@benlesh.com>
2019-02-25 18:49:56 +0000

    d374787 ci: add bazel_repository_cache to CircleCI workspace (#28935) (#28965)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.6+30.sha-1e97d51.with-local-changes";
+export declare const NGCC_VERSION = "7.2.6+34.sha-d374787.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
