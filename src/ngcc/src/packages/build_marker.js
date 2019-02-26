5a8b532fc135bf67f343a56f76b9ac3ef22de865
Miško Hevery <misko@hevery.com>
2019-02-26 02:21:32 +0000

    3cb497c refactor(ivy): simplify differentiation of LView, `RNode`, `LView`, `LContainer`, `StylingContext` (#28947)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+75.sha-1930e8a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+77.sha-3cb497c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
