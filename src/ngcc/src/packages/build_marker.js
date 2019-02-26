1963156d01d3d425feebfd6c5f4f332d6c352944
Ben Lesh <ben@benlesh.com>
2019-02-26 21:50:45 +0000

    dc9f0af fix(ivy): static host classes and styles should work on root component (#28926)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+82.sha-25a2fef.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+84.sha-dc9f0af.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
