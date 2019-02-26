51009ec4518ca79667e00f5261d3a159acc54bb3
Ben Lesh <ben@benlesh.com>
2019-02-26 17:03:20 +0000

    0e73657 docs: re-phrase note in architecture guide for clarity (#28543)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.6+40.sha-5b5b989.with-local-changes";
+export declare const NGCC_VERSION = "7.2.6+39.sha-0e73657.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
