8a5a1e3a0665ae256b56bbf1e78856855eff4840
Ben Lesh <ben@benlesh.com>
2019-02-27 00:53:19 +0000

    5a4c402 docs: release notes for the v7.2.7 release

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.5+85.sha-262ba67.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.5+86.sha-5a4c402.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
