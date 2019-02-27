752efa52762036e2263033a91e81aedd0bc9fd3c
Ben Lesh <ben@benlesh.com>
2019-02-27 18:50:32 +0000

    f16fca4 build(docs-infra): upgrade cli command docs sources to 3b1fe6437 (#29003)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.6+7.sha-d127d05.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.6+9.sha-f16fca4.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
