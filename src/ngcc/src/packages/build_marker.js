f03c8b53b6e2b0c8a6f681a6ddd413e5fdb2fc2b
Ben Lesh <ben@benlesh.com>
2019-02-27 20:16:45 +0000

    40833ba fix(ivy): process property bindings in i18n blocks similar to non-i18n bindings (#28969)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.6+12.sha-34bdebc.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.6+13.sha-40833ba.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
