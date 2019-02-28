2ae36b0b98e7843f748a7c4d7659228af45e1bbe
Andrew Kushnir <akushnir@google.com>
2019-02-28 19:11:33 +0000

    d629f2c build(bazel): revert back to non-vendored yarn in node_repositories() (#29034)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.7+8.sha-6ee47d5.with-local-changes';
+    exports.NGCC_VERSION = '7.2.7+9.sha-d629f2c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
