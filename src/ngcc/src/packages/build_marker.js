6bb91f566b4ee41837d662737ca5d575b5e53a45
Igor Minar <igor@angular.io>
2019-02-28 11:12:24 +0000

    03d2e5c refactor: Consistently use index access on index signature types. (#28937)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.6+22.sha-cb20b3b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.6+25.sha-03d2e5c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
