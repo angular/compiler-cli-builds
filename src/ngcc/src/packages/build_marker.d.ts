a5ba48850b35103317708999898f03462740e2d1
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:43:30 +0000

    ba602db build: update Bazel to 0.23 (#29058)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.6+47.sha-b1df9a3.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.6+48.sha-ba602db.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
