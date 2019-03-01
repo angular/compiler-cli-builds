9426b5a28b628ef0f3152ea5b9b36c869d42405d
Andrew Kushnir <akushnir@google.com>
2019-03-01 18:39:51 +0000

    f01d1c4 docs(ivy): add link to recursive benchmark (#29060)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.6+40.sha-b5629d9.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.6+41.sha-f01d1c4.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
