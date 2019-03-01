bbfc256d2afaa6da53fc9d728a8dc2fa5878fff0
Andrew Kushnir <akushnir@google.com>
2019-03-01 01:21:21 +0000

    5e38ec8 ci(docs-infra): disable failing ivy jit systemjs examples (#29046)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.7+9.sha-d629f2c.with-local-changes";
+export declare const NGCC_VERSION = "7.2.7+14.sha-5e38ec8.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
