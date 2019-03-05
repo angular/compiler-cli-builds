6512a0fa990411e44adc42bac44fde20561bb9e3
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:06:47 +0000

    72ecc45 fix(router): removed obsolete TODO comment (#29085)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.6+72.sha-7d17496.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.6+73.sha-72ecc45.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
