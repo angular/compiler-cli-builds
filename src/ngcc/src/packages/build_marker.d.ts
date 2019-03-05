2de827600ea363d16b691670442db8161f8e5476
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:07:17 +0000

    2256920 docs(router): align examples in interfaces docs (#29093)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.7+28.sha-2a25ac2.with-local-changes";
+export declare const NGCC_VERSION = "7.2.7+29.sha-2256920.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.7+28.sha-2a25ac2.with-local-changes';
+    exports.NGCC_VERSION = '7.2.7+29.sha-2256920.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
