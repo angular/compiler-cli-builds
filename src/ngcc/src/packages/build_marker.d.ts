7cbd1c0a004d2ce730a878d614f6fc119f43ad02
Andrew Kushnir <akushnir@google.com>
2019-03-05 20:06:50 +0000

    b446095 refactor: remove unused functions and classes in diagnostics (#28923)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.6+79.sha-1efad37.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.6+80.sha-b446095.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.6+79.sha-1efad37.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.6+80.sha-b446095.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
