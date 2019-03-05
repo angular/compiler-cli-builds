fbae5d273b5ca1f28ca81963deed5bd459ba9582
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:11:20 +0000

    36b6110 docs: order upcoming events in chronological order (#29115)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.7+30.sha-24a1e14.with-local-changes";
+export declare const NGCC_VERSION = "7.2.7+31.sha-36b6110.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
