7b448dbdabe0e6ac6b00b643ba7540873ea76aa5
Andrew Kushnir <akushnir@google.com>
2019-03-06 00:25:26 +0000

    20a9dbe ci: saucelabs-legacy job should not run schematic core tests (#29124)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.6+84.sha-25166d4.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.6+85.sha-20a9dbe.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
