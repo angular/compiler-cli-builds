67d1b855154e4d2eb7d8f735e045879b1e1eebfd
Andrew Kushnir <akushnir@google.com>
2019-03-06 00:51:46 +0000

    6945f79 build(docs-infra): ivy prebuild script should not run ngcc for all formats (#29122)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.7+31.sha-36b6110.with-local-changes";
+export declare const NGCC_VERSION = "7.2.7+33.sha-6945f79.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
