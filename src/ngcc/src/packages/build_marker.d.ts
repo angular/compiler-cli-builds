46f3f795ad64d3daa5f727c83647469d2bd3d971
Kara Erickson <karakara@google.com>
2019-03-07 18:56:25 +0000

    29f57e3 build: enable bundle_dts for router package (#28833)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.7+7.sha-22ddbf4.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.7+16.sha-29f57e3.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
