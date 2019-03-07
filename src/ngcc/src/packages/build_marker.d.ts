cebf4b340cf20c2477d58aa064254fd019b0da8d
Kara Erickson <karakara@google.com>
2019-03-07 19:05:53 +0000

    9a19592 build: remove now redundant `bundle_dts = True` attribute (#29128)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.7+18.sha-479ae51.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.7+22.sha-9a19592.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
