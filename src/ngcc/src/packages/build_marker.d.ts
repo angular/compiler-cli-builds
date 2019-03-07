3cdc82d1e71c467b5a7094a4f94cd7bb9d5f2ca4
Kara Erickson <karakara@google.com>
2019-03-07 17:16:11 +0000

    ea495d9 build: rules_nodejs 0.26.0 & use @npm instead of @ngdeps now that downstream angular build uses angular bundles (#29063)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.7+33.sha-6945f79.with-local-changes";
+export declare const NGCC_VERSION = "7.2.8+2.sha-ea495d9.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
