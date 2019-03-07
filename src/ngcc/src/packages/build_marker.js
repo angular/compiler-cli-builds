edc8cefe76686fed527fd6bb553f981b95d515b8
Kara Erickson <karakara@google.com>
2019-03-07 15:45:40 +0000

    15e8495 test(ivy): diagnose root causes for MatInput (#29153)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.7+12.sha-14ce8a9.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.7+14.sha-15e8495.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
