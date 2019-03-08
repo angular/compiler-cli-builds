454c0c30e1bbc0f590f9698b218a128c1b7f40f1
Kara Erickson <karakara@google.com>
2019-03-08 20:11:43 +0000

    37c5a26 perf(ivy): switch ngtsc to use single-file emit (#29147)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.7+43.sha-142ac41.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.7+44.sha-37c5a26.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
