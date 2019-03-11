476004039556cb5e3b05aecaf08a00dbf68f8339
Kara Erickson <karakara@google.com>
2019-03-11 22:55:22 +0000

    ed4675e docs: update Universal guide (#28956)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.7+61.sha-146256a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.7+63.sha-ed4675e.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
