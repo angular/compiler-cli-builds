e9b472ae3ba4257703b2c83e9e1ed671d55c7281
Kara Erickson <karakara@google.com>
2019-03-11 18:30:42 +0000

    99aa967 test(ivy): remove chip list tests from blocklist (#29228)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.7+58.sha-6b6fdff.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.7+59.sha-99aa967.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
