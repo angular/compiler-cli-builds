7223ae6e1b9e0afb27ee94a5f6e0f66359271cb7
Kara Erickson <karakara@google.com>
2019-03-12 18:51:17 +0000

    e6117a3 ci: rebase PRs on target branch (#29215)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.7+71.sha-37b9f06.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.7+73.sha-e6117a3.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
