30f3791810c376dfe4a3cf99e2067c6017e10b46
Kara Erickson <karakara@google.com>
2019-03-12 18:06:27 +0000

    b4ee746 docs: fix typo in directive decorator description (#29207)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.8+11.sha-09e55ea.with-local-changes';
+    exports.NGCC_VERSION = '7.2.8+13.sha-b4ee746.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
