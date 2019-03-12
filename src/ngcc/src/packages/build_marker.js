bf7a1e68b4d0b9428a5721277c4a8c521c15f8d8
Kara Erickson <karakara@google.com>
2019-03-12 17:53:21 +0000

    67444e7 docs: update enableIvy link (#29238) PR Close #29238

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.8+10.sha-1c838eb.with-local-changes';
+    exports.NGCC_VERSION = '7.2.8+12.sha-67444e7.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
