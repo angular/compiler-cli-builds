63e1af34419210962d09772e8e6e8b5c0a4e9722
Kara Erickson <karakara@google.com>
2019-03-12 21:51:03 +0000

    c5daaa9 refactor(core): static-query schematic should not run multiple times (#29133)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.7+60.sha-1d88c2b.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.7+83.sha-c5daaa9.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
