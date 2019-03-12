a04dd6a33e9e3e5f9a09dc5ace5c7dc434d2a5cc
Kara Erickson <karakara@google.com>
2019-03-12 21:21:37 +0000

    3f32c0e test(ivy): diagnose root causes of failures in CdkTable and MatTable (#29253)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.7+84.sha-b7c17ff.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.7+85.sha-3f32c0e.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
