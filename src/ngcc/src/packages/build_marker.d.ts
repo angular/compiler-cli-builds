0bcf52b51b095f350a66b3db9b463d929101ca16
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:20:54 +0000

    4e6efb3 docs: remove 'PR target: review' label (#29227)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.9+14.sha-090b6d9.with-local-changes";
+export declare const NGCC_VERSION = "7.2.9+16.sha-4e6efb3.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
