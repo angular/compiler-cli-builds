3f3b73e37b327ec4ad528c9d008b341860540641
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 15:09:26 +0000

    0c59342 build(docs-infra): automatically create previews for members of `aio-auto-previews` (#29293)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.8+17.sha-018477e.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.8+18.sha-0c59342.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
