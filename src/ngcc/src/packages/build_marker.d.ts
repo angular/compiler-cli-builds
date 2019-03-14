94b9ef946a31f2a913c6b3262eba9773ea5e1429
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:15:59 +0000

    090b6d9 docs: remove configuration instructions at the top of the i18n guide (#29313)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.9+13.sha-a5cbfa2.with-local-changes";
+export declare const NGCC_VERSION = "7.2.9+14.sha-090b6d9.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
