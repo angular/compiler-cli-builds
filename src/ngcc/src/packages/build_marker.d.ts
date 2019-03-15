7f4bbb20b147d7fe3109f0668af6ed25aa4108ac
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 02:50:41 +0000

    fc8048d build: update to nodejs rules 0.27 (#29210)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.8+40.sha-8ed13a3.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.8+42.sha-fc8048d.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
