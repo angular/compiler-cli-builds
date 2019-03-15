ff325b27a92a0cb5ff4244ca8e46c014843bdb52
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 04:30:25 +0000

    1db8bf3 fix(bazel): Remove @angular/upgrade from dev dependencies (#29319)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.8+43.sha-f7738ad.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.8+44.sha-1db8bf3.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
