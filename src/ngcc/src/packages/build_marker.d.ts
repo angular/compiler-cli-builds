3af738aec9bac73030d4a536685844843d3e08ad
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 16:59:32 +0000

    67f4852 docs: fix a typo in creating libraries guide (#29154)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.9+20.sha-08f6a64.with-local-changes";
+export declare const NGCC_VERSION = "7.2.9+21.sha-67f4852.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
