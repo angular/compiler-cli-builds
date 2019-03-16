94658fc9f6aa6966428ee5af313718f4f7676a03
Matias Niemelä <matias@yearofmoo.com>
2019-03-16 00:42:28 +0000

    604f37b test: revert to earlier @bazel/jasmine without sharding support (#29347)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.8+50.sha-bf2db12.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.8+52.sha-604f37b.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
