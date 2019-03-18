8926f36d249f2b3131b200e90645ac4386caa518
Jason Aden <jasonaden@google.com>
2019-03-18 14:31:40 +0000

    e8df000 refactor(ivy): update TNodeType (#29343)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.8+52.sha-604f37b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.8+53.sha-e8df000.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
