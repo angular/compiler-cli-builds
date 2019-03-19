02dcdbfd2da7e2a6db1cfeb0e7e64f5c8a00cedd
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 07:01:27 +0000

    e9fab63 ci: add `.vscode/**` to the `@angular/fw-dev-infra` group (#29302)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.8+71.sha-0244a24.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.8+73.sha-e9fab63.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
