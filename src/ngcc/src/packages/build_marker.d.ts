f5bd7373bc8b0fc0b3b567bb40efaf7bf4579300
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:50:55 +0000

    d6e27a4 test(ivy): remove passing snack bar test from blocklist (#29423)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+15.sha-d6e27a4.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+15.sha-d6e27a4.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
