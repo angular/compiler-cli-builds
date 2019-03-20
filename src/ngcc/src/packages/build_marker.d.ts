5b92ad25fc91e55cd448e628d105fd0f92688431
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:48:49 +0000

    1a4d4a0 docs: fix typo (culd -> could) (#29412)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+13.sha-3121409.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
