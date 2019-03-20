5d5e42a383b5349385c9c4da2d6eaaf89c90c2f9
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 04:31:41 +0000

    dafbbf8 fix(core): parse incorrect ML open tag as text (#29328)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.8+83.sha-067657c.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+2.sha-dafbbf8.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.8+83.sha-067657c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+2.sha-dafbbf8.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
