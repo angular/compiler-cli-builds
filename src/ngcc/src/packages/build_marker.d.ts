77dc507375a972849bd62c14a5a186123a80c58f
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:29:41 +0000

    9a364a8 docs: add Justin Schwartzenberger to GDE page (#29401)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+10.sha-a530ed1.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+11.sha-9a364a8.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
