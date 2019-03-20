fe71ea9ea3065efc228a758cfb2bf0f3f257a8c7
Kara Erickson <karakara@google.com>
2019-03-20 14:14:31 +0000

    a530ed1 ci: do not cache modified "node_modules" in "material-unit-tests" job (#29416)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+9.sha-2d7435d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+10.sha-a530ed1.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
