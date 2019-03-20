8bcf4acac5100667ce38eb62e2dff45e521dc0f3
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 18:02:24 +0000

    d5c815f docs: add alexeagle to @angular/fw-global-approvers (#29287)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.9+29.sha-fd1d161.with-local-changes';
+    exports.NGCC_VERSION = '7.2.10+2.sha-d5c815f.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
