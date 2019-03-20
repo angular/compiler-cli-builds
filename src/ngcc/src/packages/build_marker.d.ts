79eee25938c70f09f4cb0f2f0dfe3bc840e52a9e
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 20:27:42 +0000

    f64de2e docs: add Justin Schwartzenberger to GDE page (#29430)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.10+3.sha-86ad77c.with-local-changes";
+export declare const NGCC_VERSION = "7.2.10+4.sha-f64de2e.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
