a6fcb9ef9ab55d55fc6fb50eb6c6d99ee352891b
Filipe Silva <filipematossilva@gmail.com>
2019-03-21 18:38:25 +0000

    d0d8ce0 ci: add codefresh

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.8+18.sha-4b7ed54.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.8+18.sha-d0d8ce0.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
