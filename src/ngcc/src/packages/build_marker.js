e84c0e4e2a904690cd0bc673e111c202623da1c9
Miško Hevery <misko@hevery.com>
2019-03-21 23:16:19 +0000

    a2cf677 docs: add Siddharth Ajmera to GDE resources (#28456)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.10+11.sha-a00c2a9.with-local-changes';
+    exports.NGCC_VERSION = '7.2.10+13.sha-a2cf677.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
