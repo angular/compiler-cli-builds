be8307dd48b60cb8eddeb17490f6fcb185aec45c
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 06:48:39 +0000

    afe3e72 docs: add Luis Aviles to GDE resources (#29405)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+44.sha-9eb8274.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+47.sha-afe3e72.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
