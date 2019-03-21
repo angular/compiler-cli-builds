ef429908aef4b1f35d507e515d931c4fd508f82e
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 16:23:58 +0000

    6cd3743 refactor(service-worker): use `Adapter#parseUrl()` for all URL parsing (#27080)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+48.sha-41737bb.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+55.sha-6cd3743.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
