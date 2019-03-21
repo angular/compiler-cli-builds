7806c0d0b9345b692a47746ce6b9d1ea50e64e7c
Miško Hevery <misko@hevery.com>
2019-03-21 23:11:00 +0000

    9e1ce31 docs: add NG-DE 2019 to events page (#29079)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.10+10.sha-a677d8b.with-local-changes";
+export declare const NGCC_VERSION = "7.2.10+12.sha-9e1ce31.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
