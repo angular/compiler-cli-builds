d62951cfc3f5f7a04da9930ac5eab067aad089b6
Miško Hevery <misko@hevery.com>
2019-03-22 20:35:45 +0000

    9bcc1e8 ci: add `.codefresh/` to the `fw-dev-infra` group (#29478)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+78.sha-a06f034.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+79.sha-9bcc1e8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
