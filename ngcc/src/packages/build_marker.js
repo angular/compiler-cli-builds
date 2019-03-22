5137762284cc1d000743b4dc21e0049925645942
Miško Hevery <misko@hevery.com>
2019-03-22 23:59:17 +0000

    cfe6581 test(ivy): remove passing test from blocklist (#29484)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+83.sha-bef5043.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+84.sha-cfe6581.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
