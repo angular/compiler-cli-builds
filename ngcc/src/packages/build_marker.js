d8b3946095260a0796155b0c106c9e4f90210e0f
Misko Hevery <misko@hevery.com>
2019-03-25 16:45:37 +0000

    4122528 ci: add codefresh badge (#29475)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+87.sha-3612ddb.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.9+88.sha-4122528.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
