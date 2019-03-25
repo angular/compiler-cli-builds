2a77866d3994a69af3020ad853fb2058f4de437c
Misko Hevery <misko@hevery.com>
2019-03-25 16:39:49 +0000

    3612ddb refactor: remove extra line break in gulpfile (#29489)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+85.sha-b3102b9.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+87.sha-3612ddb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
