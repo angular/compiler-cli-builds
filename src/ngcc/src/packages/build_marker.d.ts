2789811217d063b7a304af5a9a49da356dae5d0d
Misko Hevery <misko@hevery.com>
2019-03-25 16:38:24 +0000

    dbbfab3 docs: changing "struture" to "structure" (#29497) PR Close #29497

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.10+16.sha-8b1d28f.with-local-changes";
+export declare const NGCC_VERSION = "7.2.10+18.sha-dbbfab3.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
