e7b850ee307321fe681057ba119e6e4949b6eda5
Misko Hevery <misko@hevery.com>
2019-03-25 16:39:21 +0000

    b3102b9 docs: changing "struture" to "structure" (#29497) PR Close #29497

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.9+84.sha-cfe6581.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.9+85.sha-b3102b9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
