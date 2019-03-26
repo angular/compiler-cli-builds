b0f02fb9f8002c3ce7359ed1d1a63876e9792249
Miško Hevery <misko@hevery.com>
2019-03-26 23:28:19 +0000

    495a4c1 docs: release notes for the v7.2.11 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.9+99.sha-17f7bdb.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
