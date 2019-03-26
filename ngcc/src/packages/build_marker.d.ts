b2d858725b655a3a1576c1421c232a6947491314
Miško Hevery <misko@hevery.com>
2019-03-26 23:27:14 +0000

    a28b3e3 release: cut the v8.0.0-beta.10 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+1.sha-a28b3e3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
