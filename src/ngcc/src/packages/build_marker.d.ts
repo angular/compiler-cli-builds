09f1860f8d4a4b988f67f2e9cfb3ea8987f57c67
Miško Hevery <misko@hevery.com>
2019-03-27 17:17:32 +0000

    4a73dfc docs: add Bonnie to GDE page (#29432)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.10+22.sha-f4be046.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+4.sha-4a73dfc.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
