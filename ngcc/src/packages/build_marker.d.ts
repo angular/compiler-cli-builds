2dec5672ed82e0662d6bf38c8473369f2ff83637
Miško Hevery <misko@hevery.com>
2019-03-27 20:59:41 +0000

    80161d5 build: remove unused dependency from `common` example (#29532)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+24.sha-c981006.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+27.sha-80161d5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
