e246ae5d3adaec131e0922c081064241f002107b
Miško Hevery <misko@hevery.com>
2019-03-27 17:23:41 +0000

    29d2986 docs: replaced hero with heroes component property (#29487) PR Close #29487

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+4.sha-4a73dfc.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+5.sha-29d2986.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
