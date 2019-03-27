78e0109dcc0fddd7d69b30ff783e38c119895bac
Miško Hevery <misko@hevery.com>
2019-03-27 22:28:25 +0000

    9419165 build: add version name pre release check [patch] (#29552)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.11+12.sha-1f749fe.with-local-changes';
+    exports.NGCC_VERSION = '7.2.11+13.sha-9419165.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
