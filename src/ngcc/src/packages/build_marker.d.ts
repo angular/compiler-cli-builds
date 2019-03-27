12769b7eb0bb9e1d93c21c00d141c6dd11d30d1b
Miško Hevery <misko@hevery.com>
2019-03-27 17:27:01 +0000

    2d12af3 docs: update description text (#28507)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+5.sha-29d2986.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+9.sha-2d12af3.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
