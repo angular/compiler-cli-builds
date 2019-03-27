07019cb3596a324e11f68b6f303fb243d0658498
Miško Hevery <misko@hevery.com>
2019-03-27 17:26:10 +0000

    b17d1a9 docs: update description text (#28507)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+3.sha-3c11646.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+16.sha-b17d1a9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
