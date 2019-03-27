90018e94b26d610154a881388f0e83fb96579ffb
Miško Hevery <misko@hevery.com>
2019-03-27 21:06:42 +0000

    1e5a818 fix(ivy): ngtsc is unable to detect flat module entry-point on windows (#29453)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+30.sha-1e5a818.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
