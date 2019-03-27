2cbf8df309b612a242e3872135b2881934603d74
Miško Hevery <misko@hevery.com>
2019-03-27 19:57:44 +0000

    96b800c feat(ivy): select instruction now generated in front of all relevant instructions (#29546)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+19.sha-401b8ee.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+21.sha-96b800c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
