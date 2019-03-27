312da286b74f34fd70ef89e68f1c5ba9c2689a43
Miško Hevery <misko@hevery.com>
2019-03-27 17:19:06 +0000

    6c33058 docs: add library structure (#29376)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+4.sha-06056b4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+11.sha-6c33058.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
