a17946390e57a39eec54d986a22554d47ae6ee6a
Miško Hevery <misko@hevery.com>
2019-03-27 17:41:32 +0000

    f8c7c3c docs: add schematic details and links to config page (#27272)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
