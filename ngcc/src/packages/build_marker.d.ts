21d051bd93c5afb8bbe316a16d07857ea04d6c3d
Miško Hevery <misko@hevery.com>
2019-03-27 19:57:26 +0000

    401b8ee fix(bazel): Update schematics to support routing (#29548)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+23.sha-1727fe2.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+19.sha-401b8ee.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
