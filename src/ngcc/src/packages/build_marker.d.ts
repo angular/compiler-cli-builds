12981b024041a3e4c7d480f0a2e694b549e1df6c
Jason Aden <jasonaden@google.com>
2019-03-28 22:49:44 +0000

    baec8ab docs: reorg deployment guide with serve-from-disk info from CLI story (#28217)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+14.sha-1e3c015.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+15.sha-baec8ab.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
