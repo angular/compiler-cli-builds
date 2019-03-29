26e9ae9ef8762ebd2797173ac27b810b5e69028b
Jason Aden <jasonaden@google.com>
2019-03-29 17:55:16 +0000

    cfa6ab1 docs: remove coremodule references (#28434)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+23.sha-6d7a4ff.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+22.sha-cfa6ab1.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
