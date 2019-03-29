13b95f958cd34eb58eb44cfb26e248a81e07ab89
Jason Aden <jasonaden@google.com>
2019-03-29 17:50:06 +0000

    71daa11 docs: add note about ngModel usage in structural directives guide (#29522)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+45.sha-a81fd5f.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+46.sha-71daa11.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
