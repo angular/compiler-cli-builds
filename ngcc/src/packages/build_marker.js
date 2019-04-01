9c7925cf02488ba5c27cf6c42b6ed3d0be8d22ff
Jason Aden <jasonaden@google.com>
2019-04-01 22:29:09 +0000

    3e56976 fix(ivy): avoid remote scoping if it's not actually required (#29404)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+83.sha-d7e5535.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+87.sha-3e56976.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
