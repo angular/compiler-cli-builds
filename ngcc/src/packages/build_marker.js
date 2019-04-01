d1279e28c8a60d2ec70280f1a7571157ac681062
Jason Aden <jasonaden@google.com>
2019-04-01 18:47:01 +0000

    9cd9053 docs: add missing window key shortcut to language service guide (#29613)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+77.sha-ff2a55f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+78.sha-9cd9053.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
