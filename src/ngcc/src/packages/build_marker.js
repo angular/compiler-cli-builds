dfdb97acd0c87d4c0fe2fc61ec8d00e121d038ed
Jason Aden <jasonaden@google.com>
2019-04-01 18:48:04 +0000

    94b0e8f docs: update Jorge Cano GDE bio (#29622)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.11+40.sha-157b948.with-local-changes';
+    exports.NGCC_VERSION = '7.2.11+41.sha-94b0e8f.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
