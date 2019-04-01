43dd89b0e4973c802e7261f39f8a34c8208917cb
Jason Aden <jasonaden@google.com>
2019-04-01 19:23:30 +0000

    ab72e06 docs: add custom-element ref to library overview (#28476)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+43.sha-5218f5a.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+44.sha-ab72e06.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
