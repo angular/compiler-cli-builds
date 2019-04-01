7621a8776cc43237fc78335edb07577db9a0ae4d
Jason Aden <jasonaden@google.com>
2019-04-01 18:37:40 +0000

    157b948 test(docs-infra): add e2e test for the contributors page (#29601)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+38.sha-e4eed73.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+40.sha-157b948.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
