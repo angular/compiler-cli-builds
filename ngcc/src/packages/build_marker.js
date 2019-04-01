dfb8f5405bff034f79b7c99ac30be68d3c2bb882
Jason Aden <jasonaden@google.com>
2019-04-01 18:17:59 +0000

    fd4c939 build(docs-infra): upgrade cli command docs sources to f63f8c60f (#29618)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+66.sha-6759aa6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+67.sha-fd4c939.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
