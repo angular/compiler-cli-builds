8bd29ef7f89bab6ca64753d3deb32000fad43056
Jason Aden <jasonaden@google.com>
2019-04-01 18:30:59 +0000

    76110d7 docs(http): add information about body content type to flush method for testing (#29214)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+70.sha-2ff3d2d.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+74.sha-76110d7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
