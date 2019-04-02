a60687ead907ba3f79aa28a89ff0f700b13a7aa0
Jason Aden <jasonaden@google.com>
2019-04-02 23:28:40 +0000

    afd4a4e feat(core): Add "AbstractType<T>" interface (#29295)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+120.sha-60afe88.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+121.sha-afd4a4e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+120.sha-60afe88.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+121.sha-afd4a4e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
