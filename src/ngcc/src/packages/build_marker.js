a02baff0b396e6c7c3c3c7fba8c77df0c335e7a2
Jason Aden <jasonaden@google.com>
2019-04-02 18:03:40 +0000

    91f2b1e refactor(platform-browser): rename _singleTagWhitelist (#29592)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.11+57.sha-6bf8e37.with-local-changes';
+    exports.NGCC_VERSION = '7.2.11+56.sha-91f2b1e.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
