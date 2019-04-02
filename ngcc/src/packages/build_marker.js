686bf9feb1f563571261f595315f66951870b8ab
Jason Aden <jasonaden@google.com>
2019-04-02 23:15:37 +0000

    c579949 ci: fix CODEOWNERS for ngcc (#29660)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+116.sha-c579949.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
