8c4e57da5d49b0b14d56c740b48fccb4642399e5
Jason Aden <jasonaden@google.com>
2019-04-02 23:21:20 +0000

    60afe88 feat(ivy): do not emit empty providers/imports for defineInjector (#29598)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+117.sha-6b39c9c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+120.sha-60afe88.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
