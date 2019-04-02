33910451865b3477a3bb159325c8f1e356176ae9
Jason Aden <jasonaden@google.com>
2019-04-02 17:48:51 +0000

    3580a6c fix(docs-infra): fix scroll position restoration error in `ScrollService` (#29658)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+48.sha-d727561.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+51.sha-3580a6c.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.11+48.sha-d727561.with-local-changes';
+    exports.NGCC_VERSION = '7.2.11+51.sha-3580a6c.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
