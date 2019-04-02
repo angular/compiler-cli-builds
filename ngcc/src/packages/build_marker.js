73c6fadad68cde8f76c94cd57a23a0cd548f9144
Jason Aden <jasonaden@google.com>
2019-04-02 17:57:37 +0000

    d11b0c0 refactor(compiler): update docs (#29599)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+102.sha-1c07061.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+101.sha-d11b0c0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
