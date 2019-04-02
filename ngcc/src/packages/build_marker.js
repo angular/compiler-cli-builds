4175676cd931b927a549a6e509584b63df14bb8c
Jason Aden <jasonaden@google.com>
2019-04-02 17:49:11 +0000

    9e4c0bd docs: fix typos in getting-started (#29665)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+94.sha-deca6a6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
