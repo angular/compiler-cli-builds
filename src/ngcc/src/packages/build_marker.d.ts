96b5e04a71395a29741a80d731cf9ea0c90e6cb9
Jason Aden <jasonaden@google.com>
2019-04-02 23:36:09 +0000

    a357dae docs: update location API docs (#27010)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+60.sha-ec4dced.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+61.sha-a357dae.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.11+60.sha-ec4dced.with-local-changes';
+    exports.NGCC_VERSION = '7.2.11+61.sha-a357dae.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
