c4d6e4b5c5bd95beb76cad3f75d445dfc34dd1e3
Jason Aden <jasonaden@google.com>
2019-04-02 18:03:28 +0000

    6bf8e37 docs: add info regarding injecting custom pipes (#28291)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.11+55.sha-c7075fe.with-local-changes";
+export declare const NGCC_VERSION = "7.2.11+57.sha-6bf8e37.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
