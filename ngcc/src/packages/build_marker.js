89f95a789b3ac3277c1db7aba3df9f2346921dd8
Jason Aden <jasonaden@google.com>
2019-04-02 23:35:45 +0000

    a6809e0 docs: update location API docs (#27010)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+121.sha-afd4a4e.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+123.sha-a6809e0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
