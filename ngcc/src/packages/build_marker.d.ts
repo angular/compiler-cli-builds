4183aa7ef991b35020e46d0eb04acefbf182c3e1
Jason Aden <jasonaden@google.com>
2019-04-02 17:50:58 +0000

    7c1f73a build(bazel): update to nodejs rules 0.27.9 (#29647)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.10+99.sha-7c1f73a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.10+99.sha-7c1f73a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
