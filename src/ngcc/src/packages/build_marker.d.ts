4dc443415021e2c637272d716b7415bd1c541cb3
Jason Aden <jasonaden@google.com>
2019-04-03 23:00:23 +0000

    0930820 docs: add angular.de to the workshops page (#28856)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.12+3.sha-96ff497.with-local-changes";
+export declare const NGCC_VERSION = "7.2.12+4.sha-0930820.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
