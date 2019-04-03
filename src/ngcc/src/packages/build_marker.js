b664e727a72c17750b5ac71842e6cefd7ac1278a
Jason Aden <jasonaden@google.com>
2019-04-03 22:55:12 +0000

    c9b9be6 docs: update announcements for home page (#29671)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.12+1.sha-736afea.with-local-changes';
+    exports.NGCC_VERSION = '7.2.12+2.sha-c9b9be6.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
