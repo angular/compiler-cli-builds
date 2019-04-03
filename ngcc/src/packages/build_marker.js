6fa87a26ce8e50693f79c511a62658ed1460571d
Jason Aden <jasonaden@google.com>
2019-04-03 22:57:22 +0000

    d96f62f docs: add ngx-smart-modal to doc resources (#29661)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+4.sha-9810c6c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+7.sha-d96f62f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
