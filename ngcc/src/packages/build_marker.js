e2097d2baba778e3fa203ad19cc9eebc1bafc7cf
Jason Aden <jasonaden@google.com>
2019-04-03 23:58:13 +0000

    699ecac refactor(ivy): Add i18n create op codes debug info (#29348)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.10+123.sha-a6809e0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+9.sha-699ecac.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
