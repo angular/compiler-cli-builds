21760b4edd95811366e45af3d7f6675aec1d7456
Igor Minar <igor@angular.io>
2019-04-04 21:44:17 +0000

    2bd9214 fix(bazel): remove karma-jasmine from ts_web_test_suite (#29695)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+13.sha-c3aea73.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+17.sha-2bd9214.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
