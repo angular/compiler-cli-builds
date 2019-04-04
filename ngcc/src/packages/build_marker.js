4a1568784e4c18dbba3cded339d1afd030ee06ae
Jason Aden <jasonaden@google.com>
2019-04-04 18:10:39 +0000

    afd2417 docs: add ibm carbon ui library (#29706)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+9.sha-699ecac.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+10.sha-afd2417.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
