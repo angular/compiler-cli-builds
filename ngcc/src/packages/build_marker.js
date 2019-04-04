2c93efc264251c299eb2355b6571ef8ffc5d0cc3
Jason Aden <jasonaden@google.com>
2019-04-04 18:12:35 +0000

    433b7b0 test(docs-infra): fix eslint warnings (#29673)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+10.sha-afd2417.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+16.sha-433b7b0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
