06302f10f229a0204d3c12d86eebd7fc1e193a00
Igor Minar <igor@angular.io>
2019-04-04 23:48:28 +0000

    609024f fix(core): Deprecate TestBed.get(...):any (#29290)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.11+9.sha-699ecac.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.11+24.sha-609024f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
