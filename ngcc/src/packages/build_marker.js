3bac297f2bf5ba793ad7fcb245fc79dbd653495c
Igor Minar <igor@angular.io>
2019-04-05 16:53:44 +0000

    f8cdda6 refactor: uniformly access all index sigs with index access. (#29390)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+25.sha-b6f48db.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+26.sha-f8cdda6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
