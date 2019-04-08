a7b56f173d345c3e5ed1d3f476a938009061f624
Igor Minar <igor@angular.io>
2019-04-08 19:22:57 +0000

    84be7c5 refactor(ivy): rename `styling_instructions` to `styling` (#29770)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+41.sha-66b87ce.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+45.sha-84be7c5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
