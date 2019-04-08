1a20fa610ca203c88e57b656115663af70361fe8
Igor Minar <igor@angular.io>
2019-04-08 19:21:29 +0000

    66b87ce fix(ivy): add dev mode counter information to styling (#29579)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.11+38.sha-cf40105.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.11+41.sha-66b87ce.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
