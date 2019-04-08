8d90d4e93057e8fca0cab1dfec51e27b15b1f3b2
Igor Minar <igor@angular.io>
2019-04-08 19:22:34 +0000

    d144a3b fix(ivy): not throwing error for unknown properties on container nodes (#29691)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+43.sha-cb9ee34.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+42.sha-d144a3b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
