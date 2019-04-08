221e85be54a4f30f357fdfdd28794c0633e5d90b
Igor Minar <igor@angular.io>
2019-04-08 17:10:14 +0000

    c65ac7f perf(ivy): ngcc - exit early if the targeted package has been compiled (#29740)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+36.sha-902a53a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+40.sha-c65ac7f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
