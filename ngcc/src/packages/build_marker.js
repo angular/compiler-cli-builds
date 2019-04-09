cb8a7a76ff23280ca48c037b884e67852f10a408
Igor Minar <igor@angular.io>
2019-04-09 00:40:36 +0000

    1102b02 refactor(core): static-query schematic should handle function callbacks (#29663)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+47.sha-632847d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+50.sha-1102b02.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
