30352c7e16569e38f22d21ac00bd6189cca20856
Igor Minar <igor@angular.io>
2019-04-09 00:34:57 +0000

    632847d docs: typo fix, change "insure" to "ensure" (#29737) PR Close #29737

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+46.sha-e682b44.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+47.sha-632847d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
