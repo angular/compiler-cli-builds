4c3ba140a17eb8212c9d5456a2ca2154c97470a9
Igor Minar <igor@angular.io>
2019-04-09 00:43:30 +0000

    82c77ce docs(core): update API doc examples to use static injector (#29729)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+50.sha-1102b02.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+48.sha-82c77ce.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
