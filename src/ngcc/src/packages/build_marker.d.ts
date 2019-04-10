727b4e606a7b9fce0cf783cde426da6bcdafeba7
Igor Minar <igor@angular.io>
2019-04-10 19:42:34 +0000

    19fe45d docs: update path mappings and exports (#29810)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.12+25.sha-7f1c5f6.with-local-changes";
+export declare const NGCC_VERSION = "7.2.12+27.sha-19fe45d.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
