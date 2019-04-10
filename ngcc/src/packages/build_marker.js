51c65490f30282091cdf070d29e42e030039a34c
Igor Minar <igor@angular.io>
2019-04-10 19:43:15 +0000

    17c4ed9 docs: update path mappings and exports (#29810)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+60.sha-0997512.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+65.sha-17c4ed9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
