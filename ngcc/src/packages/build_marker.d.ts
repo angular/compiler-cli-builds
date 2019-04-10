25801552376ee78bdc54d49ffef2a2793b100673
Igor Minar <igor@angular.io>
2019-04-10 19:42:38 +0000

    0997512 docs: fix a grammatical error in the building and serving documentation (#29768)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.11+59.sha-39bfd1a.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.11+60.sha-0997512.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
