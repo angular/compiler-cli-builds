293abe4f8d3c3e9d595bd6ba4fe1c906ca7505d6
Kara Erickson <karakara@google.com>
2019-04-10 02:48:15 +0000

    db62ccf build(bazel): pin versions of nodejs rules in @angular/bazel peer deps and @bazel/schematics (#29800)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+54.sha-b0eefc8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+55.sha-db62ccf.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
