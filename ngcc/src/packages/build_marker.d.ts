1b6417a4c902b5ce8cb25bfb5bf34c83b51b70f3
Igor Minar <igor@angular.io>
2019-04-11 15:19:56 +0000

    f98093a docs(aio): add missing mentors for collaborators (#29142)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.11+73.sha-712d60e.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.11+75.sha-f98093a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
