3efa5206cc25579fffa6cf0e6a24996b23836a0c
Igor Minar <igor@angular.io>
2019-04-11 15:35:04 +0000

    ddadb8e refactor(ivy): Update @publicApi to @codeGenApi on ivy instructions (#29820)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+76.sha-def73a6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+77.sha-ddadb8e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
