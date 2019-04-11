f427965960727b4b3d53dfba4a413c6c3cfadcf8
Igor Minar <igor@angular.io>
2019-04-11 15:42:13 +0000

    91c7b45 feat(ivy): support i18n without closure (#28689)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+82.sha-387fbb8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+83.sha-91c7b45.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
