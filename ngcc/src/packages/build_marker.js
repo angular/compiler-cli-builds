9419fbb6b06816f771e0d7b8df221cf5048ff10c
Igor Minar <igor@angular.io>
2019-04-11 15:41:21 +0000

    5b32f55 refactor(core): static-query schematic should check templates (#29713)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.11+81.sha-5b32f55.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
