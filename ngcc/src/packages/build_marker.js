a7654886a56ccb01f0c353aacae3e5992b1820fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 18:06:51 +0000

    b2962db test: add additional test for codeGenApi (#29844)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.12+6.sha-6a8cca7.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.12+8.sha-b2962db.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
