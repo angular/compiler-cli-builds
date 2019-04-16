71fb3d2ea8dc9637d3e4e63609d95b7d90f10349
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:04:37 +0000

    5d824c4 fix(ivy): DebugNode.properties not preserving type of values (#29914)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.12+18.sha-2deac0a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.12+20.sha-5d824c4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
