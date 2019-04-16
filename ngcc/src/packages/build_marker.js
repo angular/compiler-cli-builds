d32ab76a78f65c5e6e9c7eba522a877142fdcb78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 16:11:01 +0000

    9147092 Revert "feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)" (#29918)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.11+85.sha-b057806.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.12+17.sha-9147092.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
