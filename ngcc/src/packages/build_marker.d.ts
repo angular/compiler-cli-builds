ab837087ec0b7509e031b6b78e2764db82450549
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:05:23 +0000

    ca755a6 build: Fix label name (#29777)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.12+20.sha-5d824c4.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.12+19.sha-ca755a6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
