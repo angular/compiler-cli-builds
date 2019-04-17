13683150c8a6ce00ed32ef0c88573ad5e2509e42
Ben Lesh <ben@benlesh.com>
2019-04-17 19:36:16 +0000

    5fee9da fix(ivy): properly coalesce event handler in presence of queries (#29957)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+6.sha-1a56cd5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+7.sha-5fee9da.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
