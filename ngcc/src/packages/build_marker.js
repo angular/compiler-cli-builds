39ce983b9b221c42a43731bfe25925a0e6bde8fb
Ben Lesh <ben@benlesh.com>
2019-04-18 21:04:39 +0000

    45c2429 docs: create skeleton deprecation summary to land so other writers can contribute in parallel (#29979)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+21.sha-78146c1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+22.sha-45c2429.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
