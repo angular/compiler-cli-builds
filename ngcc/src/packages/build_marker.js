d662facf33e2b3ac927ec6295f6780a43dc763ff
Ben Lesh <ben@benlesh.com>
2019-04-18 00:40:45 +0000

    e1f51ea feat(compiler-cli): export tooling definitions (#29929)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+18.sha-b9251fd.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+20.sha-e1f51ea.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
