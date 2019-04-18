2a556ec7142593fc8d9ef8a18051f4508578f22c
Ben Lesh <ben@benlesh.com>
2019-04-18 21:05:46 +0000

    45c2429 docs: create skeleton deprecation summary to land so other writers can contribute in parallel (#29979)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.13+21.sha-78146c1.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.13+22.sha-45c2429.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
