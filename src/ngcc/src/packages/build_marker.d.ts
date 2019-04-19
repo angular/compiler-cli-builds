567ad394ff171665e2b89ba8f0be6cffc8a6aff3
Ben Lesh <ben@benlesh.com>
2019-04-19 21:29:04 +0000

    a593b3f ci(docs-infra): do not build with Ivy on 7.2.x (#29993)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.13+18.sha-2bb4263.with-local-changes";
+export declare const NGCC_VERSION = "7.2.13+19.sha-a593b3f.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
