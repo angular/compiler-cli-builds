d097b945064a41d3917d98b2a738c45327915f6c
Ben Lesh <ben@benlesh.com>
2019-04-19 17:17:10 +0000

    2bb4263 ci(docs-infra): increase wait for SW on all origins to avoid CI flakes (#29988)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.13+17.sha-0c4430b.with-local-changes";
+export declare const NGCC_VERSION = "7.2.13+18.sha-2bb4263.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
