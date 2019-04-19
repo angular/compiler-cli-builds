e5928c06aa818e5282fff038d01f6570d4a0d824
Ben Lesh <ben@benlesh.com>
2019-04-19 01:40:34 +0000

    c1d5fbd refactor(core): static-query template strategy should not parse stylesheets (#29876)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.13+23.sha-0629ebd.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
