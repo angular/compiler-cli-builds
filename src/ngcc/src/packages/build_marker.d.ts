ef1715506a8455faecb9931fefcd64672a5d6663
Ben Lesh <ben@benlesh.com>
2019-04-19 01:36:48 +0000

    4994527 docs: add Muhammad Ahsan Ayaz to GDE contributors group (#29838)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.13+13.sha-dcfde77.with-local-changes";
+export declare const NGCC_VERSION = "7.2.13+12.sha-4994527.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
