893605d6ccdb2bc674f96c6ff84dcdb81939dfe6
Ben Lesh <ben@benlesh.com>
2019-04-19 21:30:59 +0000

    4e13700 fix(ivy): set ng-reflect properties for unbound directive inputs (#29973)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+51.sha-d9ce8a4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+52.sha-4e13700.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
