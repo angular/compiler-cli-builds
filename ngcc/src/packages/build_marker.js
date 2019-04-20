3d4f4826736fcc96147c4acfcea2b25a909d1b47
Ben Lesh <ben@benlesh.com>
2019-04-20 02:46:39 +0000

    c3c0df9 fix(ivy): let ngtsc evaluate default parameters in the callee context (#29888)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+57.sha-f348dea.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+60.sha-c3c0df9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
