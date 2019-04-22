151e9b8e59cdee7d8ac4af27efb54473a818549b
Ben Lesh <ben@benlesh.com>
2019-04-22 16:01:44 +0000

    8cba4e1 fix(ivy): ngcc - do not copy declaration files into bundle clone (#30020)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+63.sha-8cba4e1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
