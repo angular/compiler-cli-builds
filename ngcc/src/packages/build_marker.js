45d5e1802fbcd6a2f17a81f5a7af5438ecef8322
Ben Lesh <ben@benlesh.com>
2019-04-22 23:44:19 +0000

    54289ae docs: cleanup code snippets for getting started example app (#29837)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+73.sha-8c80b85.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+75.sha-54289ae.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
