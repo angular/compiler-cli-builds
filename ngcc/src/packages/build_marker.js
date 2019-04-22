08afb683aecea8dde6b5ef6bd2280c5ff3bf7e7f
Ben Lesh <ben@benlesh.com>
2019-04-22 16:01:55 +0000

    5a07aa4 docs: add ng-japan 2019 event (#30021) PR Close #30021

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+63.sha-8cba4e1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+62.sha-5a07aa4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
