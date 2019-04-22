439d995ab82d0e6c0cc28fae24e7f0870743cdb2
Ben Lesh <ben@benlesh.com>
2019-04-22 18:39:42 +0000

    7f15c01 docs: change doc for address form group in reactive forms guide (#30007)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.13+22.sha-903aef0.with-local-changes";
+export declare const NGCC_VERSION = "7.2.13+21.sha-7f15c01.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
