605f4d15dc0299e3288036eb83ea2508080b5ad0
Ben Lesh <ben@benlesh.com>
2019-04-22 18:39:29 +0000

    903aef0 docs: fix typo in routing section of Tour of Heroes (#29961)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.13+20.sha-546299b.with-local-changes";
+export declare const NGCC_VERSION = "7.2.13+22.sha-903aef0.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
