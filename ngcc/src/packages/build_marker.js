1015b0de5756b594d555bcfdf377a2ad5f70035d
Ben Lesh <ben@benlesh.com>
2019-04-22 16:10:05 +0000

    645e305 feat(core): add schematics to move deprecated DOCUMENT import (#29950)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+65.sha-96a8289.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+67.sha-645e305.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
