69865062e8750ab038a0fe73ddc04412ae3da956
Ben Lesh <ben@benlesh.com>
2019-04-22 23:46:25 +0000

    57c8f78 docs: add Differential Loading section to deployment guide (#29932)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+75.sha-54289ae.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+74.sha-57c8f78.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
