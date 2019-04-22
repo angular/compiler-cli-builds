6e5997636b19bc6e63e73ab4ef6486c873fda27a
Ben Lesh <ben@benlesh.com>
2019-04-22 18:34:43 +0000

    63523f7 fix(ivy): avoid generating instructions for empty style and class bindings (#30024)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.13+68.sha-94aeeec.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.13+70.sha-63523f7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
