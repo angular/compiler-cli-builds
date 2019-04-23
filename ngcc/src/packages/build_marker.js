c8eee9ba5471f616a9ceda6cc3d5f1356af443b2
Ben Lesh <ben@benlesh.com>
2019-04-23 15:47:06 +0000

    00ce9aa fix(ivy): explicitly compile ngModuleDef for RootScopeModule in R3TestBed (#30037)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+82.sha-8e73f9b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.13+83.sha-00ce9aa.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
