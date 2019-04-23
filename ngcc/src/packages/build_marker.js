0532b16ccfa831375f11ded3e0bcb05b27d8efe0
Ben Lesh <ben@benlesh.com>
2019-04-23 23:51:22 +0000

    d92fb25 feat(ivy): add injectable-pipe schematic (#29847)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.13+88.sha-8e8e89a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+3.sha-d92fb25.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
