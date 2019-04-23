56584060104997cacd39fcd102692e099a3e2aed
Ben Lesh <ben@benlesh.com>
2019-04-23 15:52:04 +0000

    cd48a53 build(docs-infra): pin versions of packages installed in preview server docker image (#29976)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.13+24.sha-4382cea.with-local-changes';
+    exports.NGCC_VERSION = '7.2.13+27.sha-cd48a53.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
