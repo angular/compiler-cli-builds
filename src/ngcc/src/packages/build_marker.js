040e96d373580889b87121b2f8514e010a0b1cec
Ben Lesh <ben@benlesh.com>
2019-04-23 00:48:18 +0000

    4382cea docs: fix grammatical errors in the guides and API documentation (#29928)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.13+23.sha-9a81e52.with-local-changes';
+    exports.NGCC_VERSION = '7.2.13+24.sha-4382cea.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
