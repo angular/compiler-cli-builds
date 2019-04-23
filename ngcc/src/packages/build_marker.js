9d4101eddcb7c143e4f4f007a5f71ade597d5e7e
Ben Lesh <ben@benlesh.com>
2019-04-23 22:04:37 +0000

    aaf8145 fix(ivy): support module.id as @NgModule's "id" field value (#30040)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+2.sha-aaf8145.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
