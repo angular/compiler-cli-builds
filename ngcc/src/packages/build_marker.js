b023fec4bc87328326c2b2f6622c8b42511a13f7
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:03:04 +0000

    2905bf5 fix(bazel): Make sure only single copy of `@angular/bazel` is installed (#30072)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14+8.sha-e8d3246.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+9.sha-2905bf5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
