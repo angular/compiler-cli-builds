83316542db72f33d2d9f8a0597ae182f75e59c92
Andrew Kushnir <akushnir@google.com>
2019-04-24 17:53:14 +0000

    c7f1b0a fix(ivy): queries not being inherited from undecorated classes (#30015)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.14+5.sha-8ca208f.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.14+6.sha-c7f1b0a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
