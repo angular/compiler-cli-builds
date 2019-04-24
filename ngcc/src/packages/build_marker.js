dff21e47e910e98d6112b34b4c91a2b4e5e11a24
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:00:49 +0000

    e8d3246 build(docs-infra): upgrade RxJS in docs examples to 6.5.1 (#30048)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14+6.sha-c7f1b0a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+8.sha-e8d3246.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
