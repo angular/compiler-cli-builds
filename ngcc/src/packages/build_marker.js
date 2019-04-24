ea2295e6898b1a689db630e77c05e0fa67cbd3f0
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:04:25 +0000

    0b5f480 fix(bazel): make name param in ng add optional (#30074)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14+9.sha-2905bf5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+10.sha-0b5f480.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
