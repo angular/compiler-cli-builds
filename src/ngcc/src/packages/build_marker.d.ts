a4311fe151724acbbc6fbdf110fa52d585feaf52
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:49:47 +0000

    7368dc1 docs(docs-infra): fix typo in the aio/tools/examples/README (#30041)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.14+3.sha-644f898.with-local-changes";
+export declare const NGCC_VERSION = "7.2.14+4.sha-7368dc1.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
