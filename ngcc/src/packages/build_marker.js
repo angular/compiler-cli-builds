5ab057cf20cccd70cde6cc1368eb81cf4660f174
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:49:14 +0000

    2e21997 perf(ivy): remove megamorphic read from renderStringify (#30082)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14+15.sha-24c61cb.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+16.sha-2e21997.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
