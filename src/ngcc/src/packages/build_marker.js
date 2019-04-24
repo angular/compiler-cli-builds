0e5af95423f32dc8b52a75c74d9d3a2aa49af163
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:10:02 +0000

    451d4bf ci: add `aio/content/examples/*` to `docs-infra` group (#30087)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.14+1.sha-8de9d57.with-local-changes';
+    exports.NGCC_VERSION = '7.2.14+2.sha-451d4bf.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
