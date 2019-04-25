ae2f05b38c4763765b504cfaf368789c92d5a36a
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:31:53 +0000

    07f0385 test(upgrade): rename `angular.module()` to `angular.module_()` (#30107)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.14+10.sha-d025159.with-local-changes';
+    exports.NGCC_VERSION = '7.2.14+13.sha-07f0385.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
