ed18a23f8ccfdd74c4eb2c9ad1ac28ea2c8fbbc3
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:22:27 +0000

    28fd5ab fix(compiler): Fix compiler crash due to isSkipSelf of null (#30075)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.14+39.sha-c61df39.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.14+40.sha-28fd5ab.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
