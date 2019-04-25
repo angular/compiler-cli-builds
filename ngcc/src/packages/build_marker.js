4bb8cf5cfc3ff3b9f0832b3d111fb3b4d0ce377c
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:46:45 +0000

    909557d feat(core): add missing ARIA attributes to html sanitizer (#29685)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-beta.14+54.sha-2236ea4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-beta.14+62.sha-909557d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
