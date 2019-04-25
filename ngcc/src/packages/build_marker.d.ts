3fd7d9b7d01beab8e2aa3b030b021073c6e9b3c7
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:16:39 +0000

    a9379e0 docs(bazel): add aio/guide/bazel (#30042)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-beta.14+36.sha-6da1959.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-beta.14+37.sha-a9379e0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
