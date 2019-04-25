d104dd0921c36460b5840862231c483fa2d35733
Andrew Kushnir <akushnir@google.com>
2019-04-25 18:16:33 +0000

    d025159 ci(docs-infra): loosen conditions to reduce flakiness on CI (#29757)

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.14+7.sha-8275442.with-local-changes";
+export declare const NGCC_VERSION = "7.2.14+10.sha-d025159.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
