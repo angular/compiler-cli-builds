84dd417edd5b5343d24a86d100ad6e1f98d7b411
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:53:04 +0000

    b945bc3 test(ivy): pin deps on hello world size tests (#30152)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+13.sha-b945bc3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
