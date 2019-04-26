ab3cfb804014a3f954e7d4a78ef8757de40cc196
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:35:48 +0000

    8d6d2c6 build: bazel ts-api-guardian usage fails on workspaces which don't depend on `chalk` (#30138)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.0+6.sha-8dd9192.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+8.sha-8d6d2c6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
