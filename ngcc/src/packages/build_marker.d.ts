19534912d60ad88f02a2e96b1dde2ba3a7e40f5f
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:51:55 +0000

    b945bc3 test(ivy): pin deps on hello world size tests (#30152)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+13.sha-b945bc3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
