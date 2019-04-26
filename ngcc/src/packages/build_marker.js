e19a9378cb5939ea50cf5160ed1c2c85b7bdfd72
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:37:41 +0000

    ba2a359 fix(ivy): output should not be subscribe twice when 2 listeners (#30144)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.0+10.sha-a50bfe5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+11.sha-ba2a359.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
