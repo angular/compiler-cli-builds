9c15595d02a50d9c606a27549f0378ecf4a85ac0
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:29:39 +0000

    b9f0720 refactor(ivy): undeprecate inject (#30132)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.0+5.sha-71b8b35.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+6.sha-b9f0720.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
