0cdaae93a3dd0053b5ca5752430c59fa03a008e8
Alex Eagle <alexeagle@angular.io>
2019-04-27 00:08:51 +0000

    e5c3695 Revert "refactor: use new Http library in playground (#29355)"

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.0+18.sha-c99d379.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+38.sha-e5c3695.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
