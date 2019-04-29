9dbef027ae9c54ccdbc5360578eac94132069a59
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:42:55 +0000

    28a1caa docs: add in-page nav, change title to match left nav better (#30093)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+58.sha-f015dbe.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+59.sha-28a1caa.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
