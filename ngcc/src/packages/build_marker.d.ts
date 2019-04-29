4bcfc6e5e07b22521f0577704843c1b04ee76f69
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:45:43 +0000

    21418ea test: fix ngc-wrapped bazel tests in windows (#30111)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.2+3.sha-6748392.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.2+5.sha-21418ea.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
