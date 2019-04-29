ca6b919e376f5571b30a0c87775ff31815b531b9
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:44:43 +0000

    71c5d80 build: add size-tracking bazel test (#30070)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.2.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.2+1.sha-71c5d80.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
