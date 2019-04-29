e710f8c060f98396e45fbcef3efee8875a2bb80c
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:39:48 +0000

    a44b510 docs: release notes for the v8.0.0-rc.2 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+41.sha-572b549.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+42.sha-a44b510.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
