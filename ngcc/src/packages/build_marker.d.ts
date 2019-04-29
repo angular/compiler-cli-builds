b2f51758653250b9571914ad38b8fc1b89f919b2
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:51:17 +0000

    1353bf0 fix(bazel): Bump ibazel to 0.10.1 for windows fixes (#30196)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+62.sha-05eabb1.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+64.sha-1353bf0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
