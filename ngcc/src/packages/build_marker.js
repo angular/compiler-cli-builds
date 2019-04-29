b5db22dd4a93c16dae4d57b243e15e8d371544c0
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:52:23 +0000

    1353bf0 fix(bazel): Bump ibazel to 0.10.1 for windows fixes (#30196)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+62.sha-05eabb1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+64.sha-1353bf0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
