b90b124da261226c63ade7a1d622fa7a3510f61b
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:47:44 +0000

    1195dab build(docs-infra): update the aio app to framework and cli 8.0.0-rc.1 (#30183)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+43.sha-2945f47.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+44.sha-1195dab.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
