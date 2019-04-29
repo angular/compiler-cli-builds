9f536e449d969f85caffaa5cdf8044b9765739c6
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:53:17 +0000

    6dc884f test(language-service): Load language service from tsconfig (#30153)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+64.sha-1353bf0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+63.sha-6dc884f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
