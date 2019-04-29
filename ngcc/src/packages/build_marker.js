e547a7f7a93919e74a5ad595bcc74919573c12f7
Andrew Kushnir <akushnir@google.com>
2019-04-29 16:57:35 +0000

    4b2fcfd fix: disable injectable-pipe migration (#30180)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.1+27.sha-b4d291a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
