370b57179e2bfecbe220831913cd4ecca7e89459
Kara Erickson <karakara@google.com>
2019-04-30 02:02:54 +0000

    e53cf81 build(docs-infra): upgrade material and cdk to 8.0.0-rc.0 (#30202)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+66.sha-68ff2cc.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+68.sha-e53cf81.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
