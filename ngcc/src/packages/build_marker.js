9309a09ae7665718eb9001d106c174511f740c51
Kara Erickson <karakara@google.com>
2019-04-30 23:31:59 +0000

    37c598d fix(ivy): inherited listeners from grand super classes invoked multiple times (#30169)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+70.sha-ad94e02.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+71.sha-37c598d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
