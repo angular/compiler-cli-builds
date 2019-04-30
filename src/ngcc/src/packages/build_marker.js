0d5e71e76b1ed652d3ccbce8999fc60f06b8fe23
Kara Erickson <karakara@google.com>
2019-04-30 23:31:08 +0000

    b4a2cbb ci(docs-infra): use the tests from the stable branch in `aio_monitoring_stable` CircleCI job (#30110)

--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.14+13.sha-07f0385.with-local-changes';
+    exports.NGCC_VERSION = '7.2.14+17.sha-b4a2cbb.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
