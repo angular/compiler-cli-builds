b3199d0f163894f8015b50f2acb23a8f2ef06ea8
Kara Erickson <karakara@google.com>
2019-04-30 03:17:28 +0000

    00ffc03 fix(ivy): support forward refs in provider deps (#30201)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+68.sha-e53cf81.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+69.sha-00ffc03.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
