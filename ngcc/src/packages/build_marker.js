5a2301cc6dcb833a8cb88da698b76939f5c287e2
Kara Erickson <karakara@google.com>
2019-04-30 23:38:14 +0000

    6a61d37 ci(docs-infra): re-enable `aio_monitoring` CircleCI jobs and change time (#30168)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.2+9.sha-8d2e92b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.2+10.sha-6a61d37.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
