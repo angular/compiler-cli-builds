b1164d41d94d8f5e557a8c243381c4be4a8e9f68
Kara Erickson <karakara@google.com>
2019-05-01 20:57:28 +0000

    7ec8806 docs: explicitly state purpose of components (#29879)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+74.sha-7d6f488.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+75.sha-7ec8806.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
