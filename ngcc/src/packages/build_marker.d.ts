2fcc64ea60e83f65deb7807188262096699cba02
Kara Erickson <karakara@google.com>
2019-05-01 20:56:15 +0000

    7ec8806 docs: explicitly state purpose of components (#29879)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+74.sha-7d6f488.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+75.sha-7ec8806.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
