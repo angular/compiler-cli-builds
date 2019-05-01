b341c62499ad0182c42671d9d52603e9245b5d74
Kara Erickson <karakara@google.com>
2019-05-01 22:57:55 +0000

    5b80ab3 fix(ivy): use `CompilerHost.resolveModuleNames()` if available (#30017)

--- ngcc/src/packages/ngcc_compiler_host.d.ts
+++ ngcc/src/packages/ngcc_compiler_host.d.ts
@@ -2,8 +2,8 @@
 import * as ts from 'typescript';
 import { FileSystem } from '../file_system/file_system';
 export declare class NgccCompilerHost implements ts.CompilerHost {
-    private fs;
-    private options;
+    protected fs: FileSystem;
+    protected options: ts.CompilerOptions;
     private _caseSensitive;
     constructor(fs: FileSystem, options: ts.CompilerOptions);
     getSourceFile(fileName: string, languageVersion: ts.ScriptTarget): ts.SourceFile | undefined;
@@ -16,4 +16,16 @@ export declare class NgccCompilerHost implements ts.CompilerHost {
     getNewLine(): string;
     fileExists(fileName: string): boolean;
     readFile(fileName: string): string | undefined;
+}
+/**
+ * Represents a compiler host that resolves a module import as a JavaScript source file if
+ * available, instead of the .d.ts typings file that would have been resolved by TypeScript. This
+ * is necessary for packages that have their typings in the same directory as the sources, which
+ * would otherwise let TypeScript prefer the .d.ts file instead of the JavaScript source file.
+ */
+export declare class NgccSourcesCompilerHost extends NgccCompilerHost {
+    protected entryPointPath: string;
+    private cache;
+    constructor(fs: FileSystem, options: ts.CompilerOptions, entryPointPath: string);
+    resolveModuleNames(moduleNames: string[], containingFile: string, reusedNames?: string[], redirectedReference?: ts.ResolvedProjectReference): Array<ts.ResolvedModule | undefined>;
 }
