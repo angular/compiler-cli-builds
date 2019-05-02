d5bb81284052a64d75a8bc70a3678f2c8c6b76a7
Kara Erickson <karakara@google.com>
2019-05-02 17:52:13 +0000

    3fe3a84 fix(core): fix interpolate identifier in AOT (#30243)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.2+13.sha-28e4187.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.2+14.sha-3fe3a84.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
