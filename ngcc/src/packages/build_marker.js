ab4e0a976b7bc4d2d80a0c55e4f8cf3371026896
Kara Erickson <karakara@google.com>
2019-05-06 16:42:53 +0000

    bf73fb7 docs: update Wikipedia link to Polyfill description (#30214)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+79.sha-b70d20b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+82.sha-bf73fb7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
