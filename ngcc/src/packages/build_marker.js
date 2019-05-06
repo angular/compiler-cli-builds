2024fc9d4ffdd32b8a407f481dd9d9daec9424d8
Kara Erickson <karakara@google.com>
2019-05-06 19:05:47 +0000

    d6538eb docs: fix naming miss and improve anchor reference in router guide (#30225)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+86.sha-0926119.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+87.sha-d6538eb.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
