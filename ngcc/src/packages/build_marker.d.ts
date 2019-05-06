655626915c1bb34e6ff976f3850719cec8e4066c
Kara Erickson <karakara@google.com>
2019-05-06 16:51:26 +0000

    fed07c7 docs(forms): fix ControlValueAccessor registerOnChange code example (#30276)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.2+17.sha-390cac6.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.2+18.sha-fed07c7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
