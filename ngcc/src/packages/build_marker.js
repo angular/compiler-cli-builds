779831ac5eabb8bb10afedaaac91e5c3bfe42162
Kara Erickson <karakara@google.com>
2019-05-06 17:36:07 +0000

    0926119 docs: add explanation on what lazy loading is (#29667)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+84.sha-60a8888.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+86.sha-0926119.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
