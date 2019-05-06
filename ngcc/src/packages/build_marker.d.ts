f60ace7769a5add637cb2b3f84c700ec7b3ae54d
Kara Erickson <karakara@google.com>
2019-05-06 23:22:27 +0000

    ea9a381 docs(forms): fix import line ending (#30290) PR Close #30290

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+88.sha-b2437c4.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+89.sha-ea9a381.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+88.sha-b2437c4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+89.sha-ea9a381.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
