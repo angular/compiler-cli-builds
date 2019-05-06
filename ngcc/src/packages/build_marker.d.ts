b1f4160a1d7ce11b807cf50db44cabd331260c6d
Kara Erickson <karakara@google.com>
2019-05-06 16:43:58 +0000

    4537816 docs: fix `targetEntryPointPath` description (#30237)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+82.sha-bf73fb7.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+83.sha-4537816.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+82.sha-bf73fb7.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+83.sha-4537816.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
