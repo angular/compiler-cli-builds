93050c181627340c4d2ef358a5d41e41d787e0b3
Kara Erickson <karakara@google.com>
2019-05-06 23:29:08 +0000

    132f01c fix(router): fix a problem with router not responding to back button (#30160)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.2+22.sha-6a987f1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.2+23.sha-132f01c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
