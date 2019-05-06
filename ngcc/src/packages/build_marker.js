aff45458ce83cdf602f0113752dec73f68625c69
Kara Erickson <karakara@google.com>
2019-05-06 16:45:25 +0000

    1ec092b docs: fix example header in singleton services guide to match styleguide (#30097)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+83.sha-4537816.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+81.sha-1ec092b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
