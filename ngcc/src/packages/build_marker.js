dae9e21b2b35e37c5e846f5c8dec198e8e80c7a0
Kara Erickson <karakara@google.com>
2019-05-06 21:30:13 +0000

    b2437c4 perf(ivy): avoid unnecessary function calls when bound value doesn't change (#30255)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+87.sha-d6538eb.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+88.sha-b2437c4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
