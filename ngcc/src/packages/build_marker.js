e911f2641b4dab1368a33e0a1af0f14754763895
Kara Erickson <karakara@google.com>
2019-05-07 22:09:53 +0000

    bd37622 docs: release notes for the v8.0.0-rc.3 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+99.sha-48de6e8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+100.sha-bd37622.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
