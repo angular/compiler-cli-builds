7e72487c7d6850b1174c11fe9b9ab2b068a939ab
Kara Erickson <karakara@google.com>
2019-05-07 17:45:31 +0000

    4f055d4 refactor: remove tslint no-use-before-declare rule (#30288)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+92.sha-1660b34.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+95.sha-4f055d4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
