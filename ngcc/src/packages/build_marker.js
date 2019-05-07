7568794fafc35f77f4348eff3adfec2212ab4965
Kara Erickson <karakara@google.com>
2019-05-07 18:28:30 +0000

    be8fbac refactor(ivy): break apart stylingMap into styleMap and classMap instructions (#30293)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+96.sha-98a38ec.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+97.sha-be8fbac.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
