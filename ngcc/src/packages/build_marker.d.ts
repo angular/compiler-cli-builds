4f8967037302c263389fb7b19682301206f13633
Kara Erickson <karakara@google.com>
2019-05-07 17:46:58 +0000

    f6d7271 refactor: remove tslint no-use-before-declare rule (#30288)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.2+25.sha-c1f3faf.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.2+26.sha-f6d7271.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
