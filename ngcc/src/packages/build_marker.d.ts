59cc669664bd5ab3162fd6065b086432da19ebbd
Kara Erickson <karakara@google.com>
2019-05-07 17:51:23 +0000

    98a38ec docs: update date pipe example for minutes and seconds (#30281) (#30295)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+95.sha-4f055d4.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+96.sha-98a38ec.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
