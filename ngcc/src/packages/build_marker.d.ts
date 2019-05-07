606c92c0ae7d25c8a1d4c8c6343e190ecedd99f5
Kara Erickson <karakara@google.com>
2019-05-07 20:35:23 +0000

    3b551e0 docs(aio): add awade jigsaw into resources (#30204)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+97.sha-be8fbac.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+98.sha-3b551e0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+97.sha-be8fbac.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+98.sha-3b551e0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
