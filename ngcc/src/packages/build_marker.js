80d43f689eca19763affb2e249c8a3584761f396
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 20:17:41 +0000

    509352f fix(core): static-query migration should gracefully exit if AOT compiler throws (#30269)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+113.sha-29786e8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+116.sha-509352f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
