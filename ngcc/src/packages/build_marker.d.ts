bc444f058d6b10fa33e63d2816d375d1f96b1842
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:31:55 +0000

    4a96ddf build(docs-infra): upgrade cli command docs sources to 8dd9b98ac (#30325)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+104.sha-8ced321.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+105.sha-4a96ddf.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+104.sha-8ced321.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+105.sha-4a96ddf.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
