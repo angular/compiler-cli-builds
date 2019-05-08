553cab900b45f2dcb4125b3a18a7671634e86166
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:53:43 +0000

    bcc72b0 test(ivy): move host binding tests to acceptance (#30308)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+7.sha-12fb639.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+5.sha-bcc72b0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
