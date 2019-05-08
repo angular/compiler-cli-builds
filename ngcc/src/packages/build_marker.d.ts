624668d15e10656a7ae4cf0cadbd9fbf74b50fb1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 19:16:15 +0000

    a71d8a8 fix(core): static-query migration should gracefully exit if AOT compiler throws (#30269)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.3+9.sha-6857533.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.3+12.sha-a71d8a8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
