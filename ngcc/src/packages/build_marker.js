74d14a5a91643f41996a2c2574bed4b0d7fe41c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 22:14:03 +0000

    c016e2c refactor(ivy): migrate all host-specific styling instructions to element-level styling instructions (#30336)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+120.sha-452f121.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+121.sha-c016e2c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
