8d25b363c8a9ff6f2ceade58212ba3dff3cbf6e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:50:07 +0000

    4c12d74 fix(core): static-query migration should not prompt if no queries are used (#30254)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+109.sha-04a9f28.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+110.sha-4c12d74.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
