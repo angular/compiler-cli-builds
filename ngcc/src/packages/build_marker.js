c35d1c3b95c491c59df87d4f367d67b01be2e0a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:53:11 +0000

    bf6bedd fix(docs-infra): remove scroll position from sessionStorage when a ServiceWorker update has been activated (#29958)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+111.sha-f094bb5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+112.sha-bf6bedd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
