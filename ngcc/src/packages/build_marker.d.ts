7eb96393efb9d7242e8d1a97508ad0241f651704
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:46:47 +0000

    04a9f28 build: add jasonaden to angular/upgrade in CODEOWNERS (#30307)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+108.sha-d9b9ed5.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+109.sha-04a9f28.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
