036da0d609f4b2537a2df8e806d0f79be9525281
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 21:43:15 +0000

    dd8651d fix(core): CSS sanitizer now allows parens in file names (#30322)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+13.sha-5bbbe3f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+14.sha-dd8651d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
