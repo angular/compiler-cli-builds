498593ae7fd7ec482770e5c324de70e5fb50926e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 00:44:10 +0000

    08841e3 Revert "fix(router): fix a problem with router not responding to back button (#30160)" (#30320)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+2.sha-d1fcc2b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+3.sha-08841e3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
