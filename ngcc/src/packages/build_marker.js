55c27b6370f092f52d5d10579fe3fd823deaa9e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 01:03:42 +0000

    d8665e6 refactor(ivy): drop `element` prefixes for all styling-related instructions (#30318)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+116.sha-509352f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+122.sha-d8665e6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
