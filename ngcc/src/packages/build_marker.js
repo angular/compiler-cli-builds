f9ad813754bbe26443a5f34d69bc5df574b6ae6b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:56:03 +0000

    dd299f9 fix(core): static-query migration should handle queries on accessors (#30327)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+17.sha-49ec3f3.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+18.sha-dd299f9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
