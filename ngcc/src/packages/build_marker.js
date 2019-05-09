c66aafa779c7749896bd83ff8da137c4663be0c0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:03:34 +0000

    0cefa9e docs: fix filename in example headers for provider guides (#29889)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+20.sha-994d48a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+22.sha-0cefa9e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
