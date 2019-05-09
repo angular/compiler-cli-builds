52b5a89f316f327ba578eb427ca7da470a87aade
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:55:49 +0000

    cb6ad97 test(ivy): add onlyInIvy perf counter expectations (#30339)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+127.sha-f26f036.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+125.sha-cb6ad97.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
