b80cd1c8e28b663c17b956affacc091311e31b9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 21:57:14 +0000

    8d3365e fix(core): static-query usage migration strategy should detect ambiguous query usage (#30215)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+134.sha-8cec8f5.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+135.sha-8d3365e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
