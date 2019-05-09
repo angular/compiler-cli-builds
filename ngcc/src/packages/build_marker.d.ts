304d9269cb6b7db6e35f9e68d8467c17be656529
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:50:06 +0000

    d70b1ff fix(docs-infra): Handle search criteria from Chrome search providers (#30345)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+121.sha-c016e2c.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+124.sha-d70b1ff.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
