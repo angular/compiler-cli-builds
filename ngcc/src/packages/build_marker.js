94935648eca3d6b3ce856d81289e238f9b37419f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:04:12 +0000

    cabe03c docs(forms): properly convert number to string for formControlName input (#29473)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+22.sha-0cefa9e.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+23.sha-cabe03c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
