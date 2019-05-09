609a70e4ce3ec5137605f3367ecdfb8710274051
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:54:32 +0000

    49ec3f3 fix(docs-infra): Handle search criteria from Chrome search providers (#30345)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+16.sha-71eba45.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+17.sha-49ec3f3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
