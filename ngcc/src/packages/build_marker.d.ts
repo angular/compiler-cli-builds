a4f2dc21dd121f79f678539d3ae171b6318f5ca2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 16:38:36 +0000

    dbe845e test(ivy): use `TestBed` for render3 component tests (#30282) PR Close #30282

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.3+27.sha-e562acc.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.3+28.sha-dbe845e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
