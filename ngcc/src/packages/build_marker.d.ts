c59d3c8f0af874fbabeb6aed7ed1cba6a75ed6a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 21:55:29 +0000

    3c1ffba test(ivy): move pipe tests into acceptance (#30389)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.3+33.sha-14c0017.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.3+34.sha-3c1ffba.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+33.sha-14c0017.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+34.sha-3c1ffba.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
