2e8e62b84e76a300c3acee895a35c4fb061cf8d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 17:36:33 +0000

    4feb9b1 build: ignore `.devcontainer/` directory (#30417)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+148.sha-cb6dea4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+149.sha-4feb9b1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
