8497d220e39e1692f11714a87727fd4fe37f3720
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:06:41 +0000

    877b228 docs: change reference to jQuery programmers (#30386)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+149.sha-4feb9b1.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+151.sha-877b228.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+149.sha-4feb9b1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+151.sha-877b228.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
