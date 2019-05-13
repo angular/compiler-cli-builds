418fec1cc5a81ee3d0ee38cfec4a04a3fae9d36c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:59:26 +0000

    197584d fix(router): IE 11 bug can break URL unification when comparing objects (#30393)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+160.sha-18c0ba5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+161.sha-197584d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
