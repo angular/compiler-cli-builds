aa92d61431c33713808b905513759b713b2443dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:16:38 +0000

    7569a2e test(ivy): pure function acceptance tests (#30406)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+152.sha-0cf09fc.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+154.sha-7569a2e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
