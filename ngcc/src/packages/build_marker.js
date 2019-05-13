72b2b71fc156fad4befb07a72a8fad1129446397
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:10:43 +0000

    0cf09fc test(ivy): use `TestBed` for render3 i18n tests (#30352) PR Close #30352

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+151.sha-877b228.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+152.sha-0cf09fc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
