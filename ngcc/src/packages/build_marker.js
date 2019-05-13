29bdbedbe8bee0e7e5dfb46265293af7dd4a7906
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:15:27 +0000

    7569a2e test(ivy): pure function acceptance tests (#30406)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+152.sha-0cf09fc.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+154.sha-7569a2e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
