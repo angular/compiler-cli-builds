35766e8993682a19d3b511faaa5ffbe98cf48fb2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:31:37 +0000

    f74373f fix(ivy): align NgModule registration timing with ViewEngine (#30244)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+156.sha-2f35dbf.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+157.sha-f74373f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
