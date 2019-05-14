7d1d6e3dfdd41ec1183ae55ebc32c34f364579cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 16:59:20 +0000

    1b6f3c1 test(ivy): move render3 renderer_factory tests to acceptance (#30435)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.3+41.sha-c383491.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.3+42.sha-1b6f3c1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
