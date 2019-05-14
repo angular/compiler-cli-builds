0cbb3f215f2c5b949f5695d36aa87c1a2928a1c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 17:16:45 +0000

    96baff3 test(ivy): move change detection tests into acceptance (#30425)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+164.sha-090eac0.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+165.sha-96baff3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
