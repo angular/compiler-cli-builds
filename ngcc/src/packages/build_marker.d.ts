2555eb6cbba4de66791b9dbb0287840e4ca0b99f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 17:04:54 +0000

    090eac0 test(ivy): move property render3 tests to acceptance (#30426)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+162.sha-a57f3e7.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+164.sha-090eac0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
