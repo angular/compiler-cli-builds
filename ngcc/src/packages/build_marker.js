8b83373edc1affbf56adbca7984a64c6be53b886
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 21:25:24 +0000

    9e5377a refactor(core): improve messages for static-query migrations (#30458)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+173.sha-f9fb921.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+176.sha-9e5377a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
