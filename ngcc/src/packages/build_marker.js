a0a1dd8ecd0227bc3d389c8b35b957175d39f50a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 17:15:31 +0000

    96baff3 test(ivy): move change detection tests into acceptance (#30425)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+164.sha-090eac0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+165.sha-96baff3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
