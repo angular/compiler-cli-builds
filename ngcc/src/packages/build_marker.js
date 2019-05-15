a31aeacc6a15e31002cfa06d5a05f1f810cbf26b
Jason Aden <jasonaden@google.com>
2019-05-15 21:49:55 +0000

    32daa93 fix(router): type cast correctly for IE 11 bug breaking URL Unification when comparing objects (#30464)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.4+4.sha-766615c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.4+1.sha-32daa93.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
