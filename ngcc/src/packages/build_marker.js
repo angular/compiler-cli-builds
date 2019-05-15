59a32a91359da753115618e6ac30de62d390f732
Jason Aden <jasonaden@google.com>
2019-05-15 21:45:39 +0000

    35c1750 test(ivy): port TemplateRef tests over to acceptance tests (#30443)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+184.sha-53f3564.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+185.sha-35c1750.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
