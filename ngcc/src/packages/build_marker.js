1ece59dcaba97f5a09bba7f26ec5276f7b8050d8
Jason Aden <jasonaden@google.com>
2019-05-15 21:47:14 +0000

    8555016 docs: fix the payload in event-binding sample code (#30429)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+186.sha-1a0e500.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+188.sha-8555016.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
