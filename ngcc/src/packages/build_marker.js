b532ea76dca140a1b88f95bf04489d8a0faa4371
Jason Aden <jasonaden@google.com>
2019-05-15 22:02:45 +0000

    0051ddf docs: correct types in transform method to match specification. (#29812)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+191.sha-79903b1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+190.sha-0051ddf.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
