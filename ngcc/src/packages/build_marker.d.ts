18565a492f371abccd3187393eab58dbd172d3d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-15 20:10:48 +0000

    d20b0f4 docs: release notes for the v8.0.0-rc.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+182.sha-79d4b16.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+183.sha-d20b0f4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
