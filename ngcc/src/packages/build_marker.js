b53ff84c4235f6618470c80c67ae85266fc0d6a3
Jason Aden <jasonaden@google.com>
2019-05-16 19:06:06 +0000

    1b613c3 fix(ivy): evaluate external declaration usages as dynamic (#30247)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+199.sha-e9ead2b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+201.sha-1b613c3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
