b97966deebaf91e890b104b8c480d9ec9e095825
Jason Aden <jasonaden@google.com>
2019-05-16 19:05:09 +0000

    3f7e823 test(ivy): add exhaustive inheritance tests (#30442)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+201.sha-1b613c3.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+200.sha-3f7e823.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
