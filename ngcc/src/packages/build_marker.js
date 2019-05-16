156379df95999303038d944b25b6a57b65f0bc4f
Jason Aden <jasonaden@google.com>
2019-05-16 17:05:37 +0000

    0778093 test: fix compiler tests in windows ci (#30482)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+193.sha-c62c5e2.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+196.sha-0778093.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
