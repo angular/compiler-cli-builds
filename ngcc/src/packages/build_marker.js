9f27aababeebe7dff1161c366fd19f86b71a9ddc
Jason Aden <jasonaden@google.com>
2019-05-17 17:17:53 +0000

    eda09e6 fix(ivy): ngtsc - do not wrap arguments unnecessarily (#30349)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+243.sha-0937062.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+244.sha-eda09e6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
