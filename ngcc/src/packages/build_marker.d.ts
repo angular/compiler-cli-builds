9aa993ab8e733644b7fd91204dc4e30cfd89be67
Jason Aden <jasonaden@google.com>
2019-05-17 17:17:07 +0000

    01919fb test(ivy): Add TODOs (#30522)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+244.sha-eda09e6.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+246.sha-01919fb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
