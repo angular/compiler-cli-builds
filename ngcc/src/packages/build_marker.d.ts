cc27afdac932592717929b951852e5631ca0a2cc
Jason Aden <jasonaden@google.com>
2019-05-17 17:14:27 +0000

    f03475c refactor(ivy): evaluate prop-based styling bindings with a new algorithm (#30469)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+240.sha-848e53e.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+241.sha-f03475c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
