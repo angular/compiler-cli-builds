e4b4f68d10264f741a8735261f65db11d7be8184
Jason Aden <jasonaden@google.com>
2019-05-17 21:45:33 +0000

    c1135ee build: update version to 8.0.0-rc.4 in `package.json` (#30493)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+254.sha-661a57d.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+257.sha-c1135ee.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
