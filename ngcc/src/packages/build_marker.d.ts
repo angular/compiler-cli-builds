5f26287025fa800f4a35b49f30097df2bc7ef3f3
Jason Aden <jasonaden@google.com>
2019-05-17 17:16:53 +0000

    0937062 fix(ivy): type-checking should infer string type for interpolations (#30177)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+241.sha-f03475c.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+243.sha-0937062.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+241.sha-f03475c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+243.sha-0937062.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
