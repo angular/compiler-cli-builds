9ef31729d39742f509837688bf11073e04e89bac
Jason Aden <jasonaden@google.com>
2019-05-17 20:55:47 +0000

    8e2e9dc test(ivy): move render3 view_container_ref tests to acceptance (#30488)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+253.sha-660a091.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+249.sha-8e2e9dc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
