672a5abd73e01842a95478a0bcb1fb819028ebd4
Jason Aden <jasonaden@google.com>
2019-05-17 21:46:44 +0000

    c1135ee build: update version to 8.0.0-rc.4 in `package.json` (#30493)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+254.sha-661a57d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+257.sha-c1135ee.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
