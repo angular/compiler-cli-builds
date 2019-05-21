fab22fa2dc5d5740f18b99412c01b20023c57511
Jason Aden <jasonaden@google.com>
2019-05-21 20:44:21 +0000

    c9f5f3d docs: add section to upgrade guide on lazy loading AngularJS (#30541)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+273.sha-a96976e.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
