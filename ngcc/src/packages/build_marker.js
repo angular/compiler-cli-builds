6dba8f1e3b73199f57867c1cfb27fd3a4573e834
Jason Aden <jasonaden@google.com>
2019-05-21 20:49:29 +0000

    a50c1bb docs: fix spelling of 'its' (#30455)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+275.sha-f3c69e7.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+276.sha-a50c1bb.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
