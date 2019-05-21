8a96e1add35db092f6a1ec4c2013b12a87f62d3c
Jason Aden <jasonaden@google.com>
2019-05-21 00:02:25 +0000

    e688e02 fix(bazel): ng test should run specific ts_web_test_suite (#30526)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+262.sha-d7eaae6.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+263.sha-e688e02.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
