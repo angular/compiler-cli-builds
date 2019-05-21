906e6adb3ef48fa1719d7bc433e6d00baabfc3df
Jason Aden <jasonaden@google.com>
2019-05-21 20:54:09 +0000

    f6bf892 fix(core): temporarily remove @deprecated jsdoc tag for a TextBed.get overload (#30514)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+276.sha-a50c1bb.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+282.sha-f6bf892.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
