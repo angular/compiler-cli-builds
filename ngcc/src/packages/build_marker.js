d44bad64159f1247f44c6934f0197eb0822b8b7d
Jason Aden <jasonaden@google.com>
2019-05-21 20:55:37 +0000

    66f269c refactor(ivy): rename query-related functions for clarity (#30587)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+281.sha-6debe9d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+287.sha-66f269c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
