124f0ed91763b2eef7fe3be6cee1d91c3510eac2
Jason Aden <jasonaden@google.com>
2019-05-21 16:36:28 +0000

    e1af6e3 docs: use dynamic import syntax in examples using lazy loading (#30563)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+269.sha-0d97143.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+271.sha-e1af6e3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
