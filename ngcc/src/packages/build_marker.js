1456b283a46813a8811a8639c84360bab63b14a4
Jason Aden <jasonaden@google.com>
2019-05-22 23:35:59 +0000

    62c936a docs: update node and typescript versions mentioned in docs (#30601)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.4+45.sha-77e1b34.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.4+46.sha-62c936a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
