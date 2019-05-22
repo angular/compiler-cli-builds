c8c4d4dfa294115d438bad50b6ce80c532b6b4ba
Jason Aden <jasonaden@google.com>
2019-05-22 00:31:56 +0000

    f1ea78b build(docs-infra): upgrade cli command docs sources to f960a8e4a (#30530)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+289.sha-87a2366.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+290.sha-f1ea78b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+289.sha-87a2366.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+290.sha-f1ea78b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
