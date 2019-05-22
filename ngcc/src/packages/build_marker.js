64c4cbeff5ceddd5cdee5453f8cdf00b16d776cc
Jason Aden <jasonaden@google.com>
2019-05-22 23:56:44 +0000

    bf38df4 fix(bazel): allow ts_library interop with list-typed inputs (#30600)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.4+47.sha-79a0253.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.4+48.sha-bf38df4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
