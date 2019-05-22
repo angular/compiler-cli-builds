7a987e37e26e264dc9eed9c444ad4c00e45cc2bf
Jason Aden <jasonaden@google.com>
2019-05-22 23:46:49 +0000

    79a0253 docs: add new cli builder api guide (#29964)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.4+46.sha-62c936a.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.4+47.sha-79a0253.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
