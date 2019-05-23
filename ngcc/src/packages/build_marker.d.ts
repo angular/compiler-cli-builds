9f26e0befebfc33dd82db693887901a41a8e98e4
Jason Aden <jasonaden@google.com>
2019-05-23 00:02:19 +0000

    2cdbe9b refactor(ivy): ensure new attribute instructons are available in JIT (#30503)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+305.sha-d4e6263.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+311.sha-2cdbe9b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+305.sha-d4e6263.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+311.sha-2cdbe9b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
