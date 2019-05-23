60a1c78ab527f405dd1b719a76eabb5ff88a48c0
Jason Aden <jasonaden@google.com>
2019-05-23 22:29:28 +0000

    ccc76f7 feat(platform-webworker): deprecate platform-webworker (#30642)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+327.sha-b5c8939.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+329.sha-ccc76f7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
