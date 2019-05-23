8244b84e34fe0e20d86cb0e3fc5b92550202b792
Jason Aden <jasonaden@google.com>
2019-05-23 18:11:35 +0000

    21e8ac1 refactor(ivy): createNodeAtIndex no longer depends on L* (#30453)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+326.sha-132c61d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+320.sha-21e8ac1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
