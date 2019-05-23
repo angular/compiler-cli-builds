b35322ce79b80bf7f311858fde77ad52dcfd3620
Jason Aden <jasonaden@google.com>
2019-05-23 18:12:05 +0000

    faac51f test(core): update query-specific tests in core (#30626)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+320.sha-21e8ac1.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+324.sha-faac51f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
