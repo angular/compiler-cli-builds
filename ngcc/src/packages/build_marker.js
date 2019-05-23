eb02e4239ba5424eae0379f2f67040af496d7d39
Jason Aden <jasonaden@google.com>
2019-05-23 20:53:48 +0000

    b5c8939 docs: add v8 deprecations (#30598)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+324.sha-faac51f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+327.sha-b5c8939.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
