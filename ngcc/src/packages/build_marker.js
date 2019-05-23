6e083f28b7a90232f2d9433a47e2fd7989b89818
Jason Aden <jasonaden@google.com>
2019-05-23 18:13:43 +0000

    256fe24 refactor(core): static-query migration should warn about syntax failures (#30628)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.4+49.sha-8abcf04.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.4+51.sha-256fe24.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
