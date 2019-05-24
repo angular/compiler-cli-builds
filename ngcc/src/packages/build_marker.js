d4b6f7472d6925ff45ef0cb32f5b5150b1d14008
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:22:56 +0000

    68cd0ca fix(ivy): don't throw on unmatched placeholder replacements in `i18nPostprocess` (#30632)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+342.sha-deb77bd.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+340.sha-68cd0ca.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
