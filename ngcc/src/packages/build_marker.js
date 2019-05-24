105d495c09fa15c7b4552b5683b357890d0a4023
Jason Aden <jasonaden@google.com>
2019-05-24 06:10:48 +0000

    bfedfa7 docs: release notes for the v8.0.0-rc.5 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+333.sha-911a816.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+334.sha-bfedfa7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
