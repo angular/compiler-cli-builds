919f5a0255953ff0ecfb4d16628ad6cef97eb0a6
Jason Aden <jasonaden@google.com>
2019-05-24 06:09:46 +0000

    d135bb5 release: cut the v8.0.0-rc.5 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.4+63.sha-8b9a3e4.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
