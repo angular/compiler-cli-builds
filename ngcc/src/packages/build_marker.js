1f083f511c5ed9c9243220cdbd090499603a9b1f
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:19:56 +0000

    d5f96a8 refactor(ivy): refactor getBeforeNodeForView for readability (#30614)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+339.sha-9b3af14.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+341.sha-d5f96a8.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
