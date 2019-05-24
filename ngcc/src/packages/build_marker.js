c98028007a5402a91cd71001cc293ccbfa141051
Igor Minar <igor@angular.io>
2019-05-24 15:33:42 +0000

    6bf5ec2 docs(core): static-query migration should use permalink for migration guide (#30649)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+334.sha-bfedfa7.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
