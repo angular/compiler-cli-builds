a420eb6b8d43e5c0f70e8f21e00188bf45d1fd01
Igor Minar <igor@angular.io>
2019-05-28 16:57:27 +0000

    c61c14a docs: remove core.module.ts from file tree in style guide (#30679)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.5+6.sha-45718b3.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.5+11.sha-c61c14a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.5+6.sha-45718b3.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.5+11.sha-c61c14a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
