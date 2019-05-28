96a18e987cebc1a65db623478c00c1908c9c9583
Igor Minar <igor@angular.io>
2019-05-28 17:01:28 +0000

    4761d40 docs: fix typo in a declaration alert (#30545)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.5+9.sha-002fbdd.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.5+8.sha-4761d40.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
