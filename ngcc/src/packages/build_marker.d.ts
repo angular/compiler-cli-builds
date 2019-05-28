3f597db93c33e21468ec7f5b016fb7e8450b56b2
Miško Hevery <misko@hevery.com>
2019-05-28 23:41:45 +0000

    51cb592 release: cut the v8.0.0 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+352.sha-a36caca.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+353.sha-51cb592.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
