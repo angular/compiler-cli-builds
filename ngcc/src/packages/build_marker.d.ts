9eba1804162f86dd49b34ab20e9b3ed3a7ab5af2
Miško Hevery <misko@hevery.com>
2019-05-28 23:36:43 +0000

    d076c51 test: improve language service tests performance (#30683)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.5+12.sha-b0d3ad6.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.5+13.sha-d076c51.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0-rc.5+12.sha-b0d3ad6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.5+13.sha-d076c51.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
