403aed06b95b3be038301cb7255dc998fa5abcf1
Matias Niemelä <matias@yearofmoo.com>
2019-05-29 18:24:34 +0000

    f8fa2f2 docs(core): template-var-assignment schematic should link to deprecation guide (#30702)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+2.sha-f3ee9a6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+3.sha-f8fa2f2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
