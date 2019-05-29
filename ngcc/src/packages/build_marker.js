697ef8012d8df1c33229507134de3c32dd67b1e3
Matias Niemelä <matias@yearofmoo.com>
2019-05-29 16:55:50 +0000

    dd08150 feat(ivy): add ɵɵtextInterpolateX instructions (#30011)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+354.sha-4809382.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+355.sha-dd08150.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
