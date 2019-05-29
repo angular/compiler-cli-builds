8cb4600589505767426b74a45c3e89dc919eb7cc
Matias Niemelä <matias@yearofmoo.com>
2019-05-29 16:57:06 +0000

    dd08150 feat(ivy): add ɵɵtextInterpolateX instructions (#30011)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+354.sha-4809382.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+355.sha-dd08150.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
