eb1aeecb4d96bdbe6f00442922ce7c5f431656e2
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:36:40 +0000

    31aba96 docs: improve glossary for entry-point. (#29433)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+13.sha-facce2c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+14.sha-31aba96.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
