c82f397d19275ba1e2b3c758e120b299c675b7bc
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:24:12 +0000

    fd23663 docs: fix closing tags in deprecation summary (#30735)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+358.sha-1a98d51.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+372.sha-fd23663.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
