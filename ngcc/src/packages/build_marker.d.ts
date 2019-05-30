c20b33dfdfdb907c482bfdfd7258f1e874c89173
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:19:38 +0000

    7757826 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+368.sha-0b23adc.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+360.sha-7757826.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
