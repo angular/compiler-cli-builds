71ee833a495707ff805eb6a1d967742a737e190d
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 22:08:31 +0000

    282abdb release: cut the v8.1.0-beta.0 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+386.sha-19bbf4b.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
