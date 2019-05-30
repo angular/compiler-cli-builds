279259b66b967f015dcd7e9d5f0b868904c4e81c
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:32:57 +0000

    99ddec1 docs(ivy): fix symbol in example AST (#30745)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+11.sha-4bff2d0.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+15.sha-99ddec1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
