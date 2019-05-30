0ab0c069365d0e6fa34fb6368f382c999ece0697
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:22:40 +0000

    cfc608a docs: merge duplicated platform-webworker content in Deprecation Guide (#30651)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+6.sha-1c67b90.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+10.sha-cfc608a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+6.sha-1c67b90.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+10.sha-cfc608a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
