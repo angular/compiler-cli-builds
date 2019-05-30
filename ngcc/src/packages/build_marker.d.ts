508ee8447fa548166b993026d5db4cecee816816
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 05:25:34 +0000

    82682bb refactor(ivy): enable sanitization support for the new styling algorithm (#30667)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+356.sha-d72479b.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+357.sha-82682bb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
