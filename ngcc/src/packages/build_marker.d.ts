e6e02ae28e7448819dea03cb8816e4fdfe99b67d
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:22:19 +0000

    1a98d51 docs: change wording for more clarity in TypeScript Configuration guide (#30748)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+362.sha-24ca8ba.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+358.sha-1a98d51.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
