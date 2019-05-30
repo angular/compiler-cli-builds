15986bb5e22be9cfaa5772e4c6a3ecf7410b0821
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:10:33 +0000

    9a9ae60 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+3.sha-f8fa2f2.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+5.sha-9a9ae60.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
