b2bd90200bdf053ea4fed34670e4ff5f531c7d3e
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 19:29:18 +0000

    7a0f8ac fix(ivy): generate explicit type annotation for NgModuleFactory calls in ngfactories (#30708)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+378.sha-5e0f982.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+378.sha-5e0f982.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
