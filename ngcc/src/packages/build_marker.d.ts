c26a32255fe41dc42fa8547555f5d15a5b77424a
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 20:16:38 +0000

    41f372f ci: fix payload limits value

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+383.sha-41f372f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+383.sha-41f372f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
