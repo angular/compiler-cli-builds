489beb2e48cb0b2aa86d5870adf4f7d5e74df3e2
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:18:05 +0000

    b19a05c docs(common): fix typo in ngSwitch directive description (#30483)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+4.sha-ae6fa92.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+8.sha-b19a05c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+4.sha-ae6fa92.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+8.sha-b19a05c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
