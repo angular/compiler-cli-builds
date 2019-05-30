cfe32ce84d7a8e48d64e0163123c5655ca2317c0
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:35:54 +0000

    facce2c docs: fix typo in upgrade guide example (#30428)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+15.sha-99ddec1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+13.sha-facce2c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
