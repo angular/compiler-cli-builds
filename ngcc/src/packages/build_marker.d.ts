00dfe54360cb8ce8a9c3ea1c16e4920465314e5e
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 21:27:42 +0000

    19bbf4b test(ivy): fix sanitization state leaking issue

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0-rc.0+385.sha-53c6b78.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0-rc.0+386.sha-19bbf4b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.0.0-rc.0+385.sha-53c6b78.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0-rc.0+386.sha-19bbf4b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
