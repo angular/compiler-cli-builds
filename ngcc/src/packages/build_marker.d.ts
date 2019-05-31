44b392e051528d500b2f9bee7a50839dda6a8d69
Misko Hevery <misko@hevery.com>
2019-05-31 02:57:46 +0000

    0f352b6 docs: add final release date and update future schedule (#30712)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+16.sha-06bdecf.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+17.sha-0f352b6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
