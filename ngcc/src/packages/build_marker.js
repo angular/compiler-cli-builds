8d9a3358826faa6d11ab135caefa38246d66f33e
Miško Hevery <misko@hevery.com>
2019-05-31 22:50:25 +0000

    81b7559 docs(docs-infra): compile module first for Ivy when loading element modules (#30704)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+23.sha-f88f941.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+25.sha-81b7559.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
