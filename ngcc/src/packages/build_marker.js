14707416d7c1b4fa6be737c9c31ed30263eb0af0
Miško Hevery <misko@hevery.com>
2019-05-31 22:44:17 +0000

    0e17958 fix(docs-infra): add v7 in versions dropdown (#30775)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+21.sha-e82b45c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+22.sha-0e17958.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
