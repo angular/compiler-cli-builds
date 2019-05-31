9da3e471613ebfc589a0908b05caf016fdad5418
Miško Hevery <misko@hevery.com>
2019-05-31 17:22:57 +0000

    e82b45c docs(docs-infra): remove deprecated `Buffer` usage from stackblitz builder (#30369)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+20.sha-8422ef2.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+21.sha-e82b45c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
