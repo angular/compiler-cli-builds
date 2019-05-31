43359c86a224546e8cf1ebf170ee0f8d8ca9a95c
Miško Hevery <misko@hevery.com>
2019-05-31 23:23:56 +0000

    f936590 fix(ivy): DebugNode.queryAll matching sibling nodes (#30788)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+10.sha-aca339e.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+12.sha-f936590.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
