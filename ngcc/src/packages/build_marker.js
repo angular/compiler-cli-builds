c97ae6772de65212ff88f52cba87f1fca8856c86
Miško Hevery <misko@hevery.com>
2019-05-31 17:16:42 +0000

    8422ef2 fix(docs-infra): update tooltip of events link (#30643)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+19.sha-cdf586d.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+20.sha-8422ef2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
