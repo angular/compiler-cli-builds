2164ec4ff44627c12192bad34fcbf07c6084fa3a
Misko Hevery <misko@hevery.com>
2019-05-31 06:48:39 +0000

    e0c3662 test: resotre "async" around tests where "await" is used (#30762)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+3.sha-fa35488.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+6.sha-e0c3662.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
