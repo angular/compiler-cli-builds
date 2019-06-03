e95dfb24eb78b412da1cd3d7457e2ef93a379a68
Misko Hevery <misko@hevery.com>
2019-06-03 16:28:49 +0000

    21328f2 fix(ivy): add back ngDevMode to r3_injector (#30707)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+17.sha-8bedf50.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+19.sha-21328f2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
