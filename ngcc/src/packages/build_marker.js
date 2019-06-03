baf7e89a4f6ed7b28bfe1b3990da64aa5be4c279
Misko Hevery <misko@hevery.com>
2019-06-03 19:28:40 +0000

    06ffed5 docs: document UrlTree in guard return type in cheatsheet (#30822)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+28.sha-5a15126.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+29.sha-06ffed5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
