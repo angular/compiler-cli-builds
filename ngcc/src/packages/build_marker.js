640fd83c74742ca5be2e940e8a0d9d3d65333365
Misko Hevery <misko@hevery.com>
2019-06-03 16:35:36 +0000

    680d385 fix(ivy): correctly project bare ICU expressions (#30696)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+18.sha-dd8cf19.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+20.sha-680d385.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
