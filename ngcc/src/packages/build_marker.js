43f6da7a7705767d7a83f272e5637c209ed301d1
Misko Hevery <misko@hevery.com>
2019-06-03 16:33:42 +0000

    393529d docs: update example references from "my-app" to "app-root" (#30789)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+27.sha-9e4b2f1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+26.sha-393529d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
