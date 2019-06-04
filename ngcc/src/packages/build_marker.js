3bcc3e891e0837dcc1e6c8f679346b81d72492ce
Misko Hevery <misko@hevery.com>
2019-06-04 19:15:00 +0000

    97268b9 docs: update example dependencies to 8.x release (#30755)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+29.sha-06ffed5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+33.sha-97268b9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
