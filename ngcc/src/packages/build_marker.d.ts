bce07467308b2ea5140db6e9098f35abb5f73229
Misko Hevery <misko@hevery.com>
2019-06-04 19:15:22 +0000

    3ed7463 docs: rename component for constructor example (#30845) PR Close #30845

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-beta.0+28.sha-812c231.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-beta.0+27.sha-3ed7463.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
