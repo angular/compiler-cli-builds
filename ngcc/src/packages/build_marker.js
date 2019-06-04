31720d19e6198be6356388205303e6791baad3fa
Misko Hevery <misko@hevery.com>
2019-06-04 19:37:01 +0000

    ccc1c27 refactor: Move away from index signature based "updateCache". (#30518)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+35.sha-19caace.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+34.sha-ccc1c27.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
