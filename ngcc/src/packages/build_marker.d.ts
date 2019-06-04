11731507e9d9a2e9cb689f0afe5ba513c7666932
Misko Hevery <misko@hevery.com>
2019-06-04 19:29:17 +0000

    19caace docs(service-worker): mention that HTTPS is required, unless on `localhost` (#30847)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+32.sha-7c41abe.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+35.sha-19caace.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+32.sha-7c41abe.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+35.sha-19caace.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
