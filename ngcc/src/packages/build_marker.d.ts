a9272742a3ac9e5f5b531ced07182342bb95238b
Misko Hevery <misko@hevery.com>
2019-06-04 19:12:56 +0000

    812c231 docs: update example dependencies to 8.x release (#30755)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-beta.0+26.sha-8154433.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-beta.0+28.sha-812c231.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
