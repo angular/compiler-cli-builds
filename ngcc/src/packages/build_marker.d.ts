50d32539eecc395a5f34e071e920c97102332c97
Miško Hevery <misko@hevery.com>
2019-06-05 16:27:50 +0000

    337b6fe build: remove unreferenced tsconfig-build.json files (#30858)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-beta.0+34.sha-b74df20.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-beta.0+37.sha-337b6fe.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
