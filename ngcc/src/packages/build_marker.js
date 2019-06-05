49951e99d93520826ee0825495059074c7ce636d
Miško Hevery <misko@hevery.com>
2019-06-05 16:39:59 +0000

    00cc905 feat(ivy): support `ng-content` in runtime i18n translations (#30782)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+39.sha-d1df0a9.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+38.sha-00cc905.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
