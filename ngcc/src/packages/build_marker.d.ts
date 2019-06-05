9d7dcbeaced08990adbffbea84ff7e80ab6e037e
Miško Hevery <misko@hevery.com>
2019-06-05 16:45:36 +0000

    8852b79 build: unable to run build-packages-dist script on windows (#30853)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-beta.0+38.sha-00cc905.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-beta.0+40.sha-8852b79.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
