ffa2157eaca2c32a20a88e8178da02b7736cb173
Miško Hevery <misko@hevery.com>
2019-06-05 19:59:38 +0000

    540b014 release: cut the v8.1.0-next.1 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+40.sha-8852b79.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
