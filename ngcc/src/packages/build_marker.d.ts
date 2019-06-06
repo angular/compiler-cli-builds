ff3a5ed574b2f952be45fcc1a379f7c4725019ec
Miško Hevery <misko@hevery.com>
2019-06-06 16:15:30 +0000

    26e3615 fix(core): TypeScript related migrations should cater for BOM (#30719)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+39.sha-65f2010.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+40.sha-26e3615.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+39.sha-65f2010.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+40.sha-26e3615.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
