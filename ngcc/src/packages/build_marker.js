b8eb830fd1c316a74910f2c773873f5215a2a8b5
Miško Hevery <misko@hevery.com>
2019-06-06 23:04:31 +0000

    11a4454 perf(ivy): remove check for function type in renderStringify (#30838)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.1+6.sha-3859bcc.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.1+10.sha-11a4454.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
