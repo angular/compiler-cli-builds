d4154072c6750d3fb01f9d793de03eb6b35f58ef
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:23 +0000

    05a43ca fix(bazel): exclude components schematics from build (#30825)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.1+5.sha-30efb6b.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.1+4.sha-05a43ca.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
