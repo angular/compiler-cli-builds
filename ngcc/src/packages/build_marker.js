e4c1c17cbab7f14a01665b32395ee0f361c55bc2
Miško Hevery <misko@hevery.com>
2019-06-06 16:11:55 +0000

    f440bd1 build(docs-infra): fix CLI command github links (#30889)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.1+6.sha-3859bcc.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.1+7.sha-f440bd1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
