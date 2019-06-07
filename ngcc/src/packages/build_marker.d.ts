1f10d6a1c55240aeb396616236632e4e35b5fc4a
Misko Hevery <misko@hevery.com>
2019-06-07 16:27:46 +0000

    5c18f23 fix(common): expose the `HttpUploadProgressEvent` interface as public API (#30852)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.1+14.sha-297222f.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.1+17.sha-5c18f23.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
