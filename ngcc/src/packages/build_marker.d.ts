bc41f7741f1d28e28b8a78c0f6fc79ebc0fe36c3
Misko Hevery <misko@hevery.com>
2019-06-07 16:12:20 +0000

    8aef446 build(docs-infra): upgrade cli command docs sources to d09f130ce (#30909)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+46.sha-9aeef0a.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+45.sha-8aef446.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
