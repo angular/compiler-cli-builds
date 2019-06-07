e514922443e34b85a080f2d95925223c2f64526d
Misko Hevery <misko@hevery.com>
2019-06-07 16:19:15 +0000

    08c38a1 fix(service-worker): avoid uncaught rejection warning when registration fails (#30876)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.0+48.sha-9ace748.with-local-changes";
+export declare const NGCC_VERSION = "8.0.0+47.sha-08c38a1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
