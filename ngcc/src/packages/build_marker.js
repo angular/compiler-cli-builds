fc45b6f8262ebc63f4fc2cd5a2dfe78d9a111284
Misko Hevery <misko@hevery.com>
2019-06-07 16:11:08 +0000

    9aeef0a build(docs-infra): fail hard if the CLI source is not what we expect (#30901)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+44.sha-f7ee91a.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+46.sha-9aeef0a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
