3e95cd593ccf1122a8f0121ea4686185ac266c44
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-10 19:46:51 +0000

    c265475 test: update resolve path for windows

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+33.sha-4f1008b.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+34.sha-c265475.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
