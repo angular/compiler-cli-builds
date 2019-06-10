2f5ec6cd64fca867a9c22459358e77af633212f1
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-10 19:14:46 +0000

    4f1008b test: modify for windows

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-beta.0+32.sha-99331c0.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-beta.0+33.sha-4f1008b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-beta.0+32.sha-99331c0.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-beta.0+33.sha-4f1008b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
