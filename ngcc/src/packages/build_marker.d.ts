f69a8493b6a6125179c604341a15e9713c182e31
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:44:52 +0000

    699283c feat(docs-infra): layout ui polish (#30883)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.1+40.sha-c15e675.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.1+42.sha-699283c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.1+40.sha-c15e675.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.1+42.sha-699283c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
