5fb3e28054eb9a066aae7a2eb3d18eabc1c48075
Igor Minar <igor@angular.io>
2019-06-11 00:20:06 +0000

    6a48485 build: update rules_nodejs and clean up bazel warnings (#30370) (#30763)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+49.sha-4e8614b.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+51.sha-6a48485.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
