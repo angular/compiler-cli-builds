b4f9d0c8d417acfb780292b8bc9e3cdba5760469
Igor Minar <igor@angular.io>
2019-06-11 00:35:34 +0000

    dcf9f05 docs: use `Node.js` consistently (#30934) PR Close #30934

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+51.sha-6a48485.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+55.sha-dcf9f05.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
