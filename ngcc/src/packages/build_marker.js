10c1a6f2176bae2298c4f5d9b13e969da7fc88a5
Andrew Kushnir <akushnir@google.com>
2019-06-11 18:14:39 +0000

    501bacd build: update golden for //packages/core/test/bundling/injection:symbol_test (#30965)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+57.sha-fe02462.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+59.sha-501bacd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
