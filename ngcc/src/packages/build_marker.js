a2edcbe1440d7426fbf35653d6f5414c23555d98
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:47:10 +0000

    762fc28 feat(docs-infra): layout ui polish (#30883)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+61.sha-7e2ed89.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+63.sha-762fc28.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
