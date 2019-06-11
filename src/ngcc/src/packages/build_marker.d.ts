3030c3e6e06df9710e85b3def473aecee3915360
Igor Minar <igor@angular.io>
2019-06-11 00:26:54 +0000

    41e56c9 fixup! refactor(docs-infra): make archive redirection tests DRY

--- src/ngcc/src/packages/build_marker.d.ts
+++ src/ngcc/src/packages/build_marker.d.ts
@@ -7,7 +7,7 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngcc/src/packages/build_marker" />
 import { EntryPoint, EntryPointFormat } from './entry_point';
-export declare const NGCC_VERSION = "7.2.15+1.sha-b62a9d0.with-local-changes";
+export declare const NGCC_VERSION = "7.2.15+5.sha-41e56c9.with-local-changes";
 /**
  * Check whether there is a build marker for the given entry point and format.
  * @param entryPoint the entry point to check for a marker.
--- src/ngcc/src/packages/build_marker.js
+++ src/ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var canonical_path_1 = require("canonical-path");
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '7.2.15+1.sha-b62a9d0.with-local-changes';
+    exports.NGCC_VERSION = '7.2.15+5.sha-41e56c9.with-local-changes';
     function getMarkerPath(entryPointPath, format) {
         return canonical_path_1.resolve(entryPointPath, "__modified_by_ngcc_for_" + format + "__");
     }
