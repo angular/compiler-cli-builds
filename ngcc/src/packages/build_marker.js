aa4d676d0c561d79a3acd8cc74c1e7f19aad2ca9
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:08:42 +0000

    cbb3794 fix(docs-infra): enable only vertical scrolling for contributors bio (#30991)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.0+67.sha-77a5790.with-local-changes';
+    exports.NGCC_VERSION = '8.0.0+68.sha-cbb3794.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
