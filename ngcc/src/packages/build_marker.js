82f07b5bf3c5dca6147942374a3ee52778498893
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:13:19 +0000

    85d38ae docs: rewrite property binding section and add example (#25770)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.2+14.sha-4ad323a.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.2+15.sha-85d38ae.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
