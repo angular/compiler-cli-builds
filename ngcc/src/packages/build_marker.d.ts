b37657deb36dbd453e83f8b01ac3a8ef5522427c
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:21:30 +0000

    e76690b docs: edit codeowners for new Template Syntax examples (#31060)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+19.sha-f41242f.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+21.sha-e76690b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
