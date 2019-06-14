58fa7550b5f297dc9d397f2af6a1676f2d06e49e
Andrew Kushnir <akushnir@google.com>
2019-06-14 23:23:50 +0000

    ce7131f ci(docs-infra): disable payload size tracking for `test_aio_local` and `test_aio_local_ivy` (#31064)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.1+18.sha-2abb54c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.1+21.sha-ce7131f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
