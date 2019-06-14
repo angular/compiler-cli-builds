0c8a707053f3b6d8206996e360b5d5c5f895fbf6
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:14:16 +0000

    a4601ec fix(language-service): Remove 'any' in getQuickInfoAtPosition (#31014)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+11.sha-8f5c396.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+13.sha-a4601ec.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
