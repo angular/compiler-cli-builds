90ee3b9d1c45e0b223cc6bbb4592ffb517f05046
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:11:46 +0000

    85d38ae docs: rewrite property binding section and add example (#25770)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+14.sha-4ad323a.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+15.sha-85d38ae.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
