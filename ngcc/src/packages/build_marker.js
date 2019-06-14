8103e1fc2de5d79da18a8ea19954de8af5920a43
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:21:20 +0000

    e2fd628 docs: edit template ref vars copy and example (#27371)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.2+17.sha-2c2135d.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.2+18.sha-e2fd628.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
