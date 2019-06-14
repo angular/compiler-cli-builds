eae7c330efe21b212b524bdfae6536d39b75e7d2
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:08:29 +0000

    5d24a75 docs: minor fix in `get-commit-range.js` docs (#31049)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+7.sha-799d1fd.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+8.sha-5d24a75.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
