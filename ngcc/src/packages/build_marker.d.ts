bb1fefb3bf1f33ed0e3e31861eee04dea16a43ea
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:39:53 +0000

    16aa6ce refactor(ivy): update ɵɵtextBinding to not take index (#30792)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+20.sha-6c4d912.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+23.sha-16aa6ce.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
