a36e81d9a68c72156610898d8b7b6e696c5c75e8
Andrew Kushnir <akushnir@google.com>
2019-06-17 21:16:18 +0000

    4adf95ed6 ci: publish snapshots job is unable to decode github token (#31099)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.2+26.sha-c596795.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.2+29.sha-4adf95e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
