432486546653a4f8ca8822f9d9f50958637b889c
Andrew Kushnir <akushnir@google.com>
2019-06-18 18:18:37 +0000

    f4cd2b75b docs: add missing word in build and deploy guide to clarify sentence (#31093)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+41.sha-5909c44.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+42.sha-f4cd2b7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
