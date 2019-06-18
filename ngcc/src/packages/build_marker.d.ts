dcda0cc32baf27f77136410f4f7c79538f002ce4
Andrew Kushnir <akushnir@google.com>
2019-06-18 21:36:21 +0000

    bf9de8ca5 test: update side-effects test to be more descriptive (#31005)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.0.1+34.sha-df37c47.with-local-changes";
+export declare const NGCC_VERSION = "8.0.1+35.sha-bf9de8c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.1+34.sha-df37c47.with-local-changes';
+    exports.NGCC_VERSION = '8.0.1+35.sha-bf9de8c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
