88ec4543cc5f6db25254affa3c2b14fd61bb5a3d
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:10:55 +0000

    7e49beb8c docs: rewrite Observable examples with pipable operators (#31074)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+31.sha-98515e1.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+33.sha-7e49beb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
