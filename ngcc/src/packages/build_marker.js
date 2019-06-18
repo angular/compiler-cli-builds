6fcebd7fdc0f638945da03f963fe0df2a8d2aee7
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:21:55 +0000

    87168acf3 refactor(ivy): move `bind` instruction into `i18nExp` (#31089)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.2+37.sha-beaab27.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.2+36.sha-87168ac.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
--- src/ngtsc/indexer/src/api.d.ts
+++ src/ngtsc/indexer/src/api.d.ts
@@ -6,23 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/indexer/src/api" />
-import { InterpolationConfig, ParseSourceFile } from '@angular/compiler';
-import { ParseTemplateOptions } from '@angular/compiler/src/render3/view/template';
+import { ParseSourceFile, ParseSpan } from '@angular/compiler';
 import * as ts from 'typescript';
 /**
  * Describes the kind of identifier found in a template.
  */
 export declare enum IdentifierKind {
-    Property = 0,
-    Method = 1
-}
-/**
- * Describes the absolute byte offsets of a text anchor in a source code.
- */
-export declare class AbsoluteSourceSpan {
-    start: number;
-    end: number;
-    constructor(start: number, end: number);
 }
 /**
  * Describes a semantically-interesting identifier in a template, such as an interpolated variable
@@ -30,7 +19,7 @@ export declare class AbsoluteSourceSpan {
  */
 export interface TemplateIdentifier {
     name: string;
-    span: AbsoluteSourceSpan;
+    span: ParseSpan;
     kind: IdentifierKind;
     file: ParseSourceFile;
 }
@@ -46,14 +35,4 @@ export interface IndexedComponent {
         identifiers: Set<TemplateIdentifier>;
         usedComponents: Set<ts.ClassDeclaration>;
     };
-}
-/**
- * Options for restoring a parsed template. See `template.ts#restoreTemplate`.
- */
-export interface RestoreTemplateOptions extends ParseTemplateOptions {
-    /**
-     * The interpolation configuration of the template is lost after it already
-     * parsed, so it must be respecified.
-     */
-    interpolationConfig: InterpolationConfig;
 }
