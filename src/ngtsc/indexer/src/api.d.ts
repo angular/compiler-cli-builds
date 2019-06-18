58ea5cbec80c0376720b29943d1fe8e20c92c178
Andrew Kushnir <akushnir@google.com>
2019-06-18 20:50:25 +0000

    beaab27a4 feat(ivy): index identifiers discovered in templates (#30963)

--- src/ngtsc/indexer/src/api.d.ts
+++ src/ngtsc/indexer/src/api.d.ts
@@ -6,12 +6,23 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/indexer/src/api" />
-import { ParseSourceFile, ParseSpan } from '@angular/compiler';
+import { InterpolationConfig, ParseSourceFile } from '@angular/compiler';
+import { ParseTemplateOptions } from '@angular/compiler/src/render3/view/template';
 import * as ts from 'typescript';
 /**
  * Describes the kind of identifier found in a template.
  */
 export declare enum IdentifierKind {
+    Property = 0,
+    Method = 1
+}
+/**
+ * Describes the absolute byte offsets of a text anchor in a source code.
+ */
+export declare class AbsoluteSourceSpan {
+    start: number;
+    end: number;
+    constructor(start: number, end: number);
 }
 /**
  * Describes a semantically-interesting identifier in a template, such as an interpolated variable
@@ -19,7 +30,7 @@ export declare enum IdentifierKind {
  */
 export interface TemplateIdentifier {
     name: string;
-    span: ParseSpan;
+    span: AbsoluteSourceSpan;
     kind: IdentifierKind;
     file: ParseSourceFile;
 }
@@ -35,4 +46,14 @@ export interface IndexedComponent {
         identifiers: Set<TemplateIdentifier>;
         usedComponents: Set<ts.ClassDeclaration>;
     };
+}
+/**
+ * Options for restoring a parsed template. See `template.ts#restoreTemplate`.
+ */
+export interface RestoreTemplateOptions extends ParseTemplateOptions {
+    /**
+     * The interpolation configuration of the template is lost after it already
+     * parsed, so it must be respecified.
+     */
+    interpolationConfig: InterpolationConfig;
 }
