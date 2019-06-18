e8fcaa0ed1f1db98103523cdb84a7362df13a5b3
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:13:23 +0000

    eb970b462 docs: rewrite Observable examples with pipable operators (#31074)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.1+26.sha-6a01fa5.with-local-changes';
+    exports.NGCC_VERSION = '8.0.1+28.sha-eb970b4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
