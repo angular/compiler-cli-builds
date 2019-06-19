0bc90e1d5b539df2e4c9c9fa32fc297a920b0b1a
Kara Erickson <karakara@google.com>
2019-06-19 22:40:05 +0000

    4ec50811d refactor(language-service): Remove NgLSHost -> NgLS dependency (#31122)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+2.sha-c34abf2.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+3.sha-4ec5081.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
