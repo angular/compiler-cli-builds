def4504cca526da6b35d407bd18aef54cc829077
Kara Erickson <karakara@google.com>
2019-06-19 22:51:57 +0000

    ab86a58f2 docs: remove inappropriate link (#31091)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+3.sha-4ec5081.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+4.sha-ab86a58.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
