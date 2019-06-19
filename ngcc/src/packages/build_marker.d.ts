fbe7c0d54f3b790a7daac89ae0918f57a827a59c
Andrew Kushnir <akushnir@google.com>
2019-06-19 16:11:29 +0000

    0bf810022 docs: add core and cli version alignment note (#30976) docs: add core and cli version alignment note PR Close #30976

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.2+46.sha-bc04d18.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.2+47.sha-0bf8100.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
