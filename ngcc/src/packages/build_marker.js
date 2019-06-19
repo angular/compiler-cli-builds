05c46aae83c59dd9c807b37ccd1cbb415f1dc13f
Kara Erickson <karakara@google.com>
2019-06-19 23:19:40 +0000

    9c222350f ci: re-enable payload size tracking (#31138)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+7.sha-c36edff.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+8.sha-9c22235.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
