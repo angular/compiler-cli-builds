8c4c19779c5616de273a34d674906fc6ce62efff
Andrew Kushnir <akushnir@google.com>
2019-06-19 16:11:53 +0000

    2fe8f2b1e build: update buildifier to version that supports windows (#31129)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.1+36.sha-f1c08c6.with-local-changes';
+    exports.NGCC_VERSION = '8.0.1+37.sha-2fe8f2b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
