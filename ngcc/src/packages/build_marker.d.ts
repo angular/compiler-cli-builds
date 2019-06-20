352b574b9c7c2b28fa29624a77f8dddc8c99a211
Kara Erickson <karakara@google.com>
2019-06-20 18:55:12 +0000

    5eb742621 build: move zone.js to angular repo (#30962)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+18.sha-7b3bcc2.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+19.sha-5eb7426.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
