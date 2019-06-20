a8bdc2f8ad4dce68320fe9a5e1b592d6ab4603c7
Kara Erickson <karakara@google.com>
2019-06-20 18:51:18 +0000

    07f6b2c01 build: user bazelrc should be able to overwrite all flags (#31164)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+8.sha-9c22235.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+10.sha-07f6b2c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
