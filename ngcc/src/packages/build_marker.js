cebc09d7f97e789c1147dabde8d367cb2692d439
Kara Erickson <karakara@google.com>
2019-06-20 01:47:21 +0000

    029f1be20 perf(ivy): avoid unnecessary function call in i18n instruction (#31106)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+6.sha-f039583.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+8.sha-029f1be.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
