0ba736f067c2c7fa281a160014e9d4dd9f89a44c
Kara Erickson <karakara@google.com>
2019-06-21 17:54:20 +0000

    de0474186 docs: clarify wording in Template Syntax (#31119)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+12.sha-15d138f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+15.sha-de04741.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
