71bad33b269057868e7a8cda0fcd23eb6f4bd3bd
Kara Erickson <karakara@google.com>
2019-06-21 22:52:51 +0000

    7fe95b188 ci: send failure notifications from `saucelabs_tests` to `dev-infra-ci-failures` (#31202)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+18.sha-ff59345.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+22.sha-7fe95b1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
