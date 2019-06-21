8b14f2af108bb4919e87d6d30adb933367883719
Kara Erickson <karakara@google.com>
2019-06-21 17:47:06 +0000

    ecd7f6ecd docs: fix javascript module link (#31181)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+28.sha-1f1cf1a.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+30.sha-ecd7f6e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
