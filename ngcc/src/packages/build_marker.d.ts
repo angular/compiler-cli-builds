3eefbc4a6a6009cc99c774ff9fa59e444c8f84d0
Kara Erickson <karakara@google.com>
2019-06-21 02:14:08 +0000

    3fb78aaac feat(upgrade): provide unit test helpers for wiring up injectors (#16848)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+16.sha-f221908.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+24.sha-3fb78aa.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+16.sha-f221908.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+24.sha-3fb78aa.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
