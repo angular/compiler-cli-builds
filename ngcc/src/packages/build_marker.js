9d93cc191e18c022a6a18ed4497db029637e4bb1
Kara Erickson <karakara@google.com>
2019-06-21 17:42:57 +0000

    1699b5486 docs: add Mosaic library to resources (#31026)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+10.sha-07f6b2c.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+11.sha-1699b54.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
