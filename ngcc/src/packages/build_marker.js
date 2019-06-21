1fd66932e524bdd7e56e8e04a2b9869a4193bee0
Kara Erickson <karakara@google.com>
2019-06-21 19:27:35 +0000

    0bb83b2ba docs: update node minimum version (#31192)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+14.sha-8ea9188.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+16.sha-0bb83b2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
