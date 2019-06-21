c2f93b8c41c4b7f201e788486bcbbfa87d6c24e9
Kara Erickson <karakara@google.com>
2019-06-21 02:07:58 +0000

    dc613b336 refactor(ivy): removing unused properties from i18n interfaces (#31171)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+25.sha-7ff628f.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+26.sha-dc613b3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+25.sha-7ff628f.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+26.sha-dc613b3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
