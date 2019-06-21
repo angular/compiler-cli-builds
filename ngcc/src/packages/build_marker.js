665d7a7d83731c79e25b702e595d68d5c73e20f1
Kara Erickson <karakara@google.com>
2019-06-21 17:49:35 +0000

    a9502886b docs: mark interfaces as public (#30955)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+32.sha-bd3b056.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+34.sha-a950288.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
