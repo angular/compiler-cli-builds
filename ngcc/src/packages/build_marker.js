2de62c5c1dac0a4fa25ce1b93c5a1421ba755faf
Kara Erickson <karakara@google.com>
2019-06-21 22:52:31 +0000

    ff5934597 build: Set up Build Event Service Configuration for RBE executed bazel (#31197)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+17.sha-09b60ed.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+18.sha-ff59345.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
