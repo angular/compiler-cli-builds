1cace1d239c17ed8c29a710759816f924af83538
Kara Erickson <karakara@google.com>
2019-06-21 22:55:32 +0000

    274833ee9 ci: add josephperrott to `dev-infra` owners (#31205)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+22.sha-7fe95b1.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+19.sha-274833e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
