262bd9c355eb55e88607004c58f69b994cea917b
Kara Erickson <karakara@google.com>
2019-06-21 21:41:21 +0000

    65974154e feat(docs-infra): change typography font sizes to be rem based (#31118)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+37.sha-7035f22.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+38.sha-6597415.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
