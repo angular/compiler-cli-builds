66eae1ce7bc4edf6928aae37041d36817a5f9d98
Kara Erickson <karakara@google.com>
2019-06-24 22:18:15 +0000

    e83667ad7 fix(bazel): remove unsupported Css pre-processors from ng new (#31234)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+45.sha-23c0171.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+47.sha-e83667a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
