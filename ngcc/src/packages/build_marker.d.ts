e1c7cec38e6eae3b2a718641d1a434bbf984a64f
Kara Erickson <karakara@google.com>
2019-06-24 22:23:53 +0000

    a5dd4edab fix(service-worker): registration failed on Safari (#31140)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+47.sha-e83667a.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+48.sha-a5dd4ed.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
