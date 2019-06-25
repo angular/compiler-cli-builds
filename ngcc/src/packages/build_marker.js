1056ade34e6dc98e1d78c50f6351c18bee5f5cc4
Kara Erickson <karakara@google.com>
2019-06-25 17:52:45 +0000

    8a1bd2c40 docs: fix formatting (#31120)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+29.sha-4ea231f.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+36.sha-8a1bd2c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
