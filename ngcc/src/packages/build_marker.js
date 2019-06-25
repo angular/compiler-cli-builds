93283fd36099b86af3e00de5e7d1abb12df2f9e2
Kara Erickson <karakara@google.com>
2019-06-25 22:01:24 +0000

    280e8563f Revert "build(bazel): update to bazel 0.27.0 and fix compat in @angular/bazel package (#31019)" (#31267)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+72.sha-98685e6.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+80.sha-280e856.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
