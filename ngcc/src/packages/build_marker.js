49996630c2e5941f6e05ff12ce6985437a70e4f3
Kara Erickson <karakara@google.com>
2019-06-25 02:11:53 +0000

    6bad2ca58 fix(bazel): exclude all angular schematics folders from metadata build (#31237)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+27.sha-dabf132.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+28.sha-6bad2ca.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
