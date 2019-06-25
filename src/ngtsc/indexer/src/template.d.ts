2140291007cb3873285fd51c59a3483533056981
Kara Erickson <karakara@google.com>
2019-06-25 02:06:10 +0000

    74f4f5dfa feat(ivy): integrate indexing pipeline with NgtscProgram (#31151)

--- src/ngtsc/indexer/src/template.d.ts
+++ src/ngtsc/indexer/src/template.d.ts
@@ -6,12 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/indexer/src/template" />
-import { TmplAstNode } from '@angular/compiler';
-import { RestoreTemplateOptions, TemplateIdentifier } from './api';
+import { BoundTarget, DirectiveMeta } from '@angular/compiler';
+import { TemplateIdentifier } from './api';
 /**
  * Traverses a template AST and builds identifiers discovered in it.
- * @param template template to extract indentifiers from
- * @param options options for restoring the parsed template to a indexable state
+ *
+ * @param boundTemplate bound template target, which can be used for querying expression targets.
  * @return identifiers in template
  */
-export declare function getTemplateIdentifiers(template: TmplAstNode[], options: RestoreTemplateOptions): Set<TemplateIdentifier>;
+export declare function getTemplateIdentifiers(boundTemplate: BoundTarget<DirectiveMeta>): Set<TemplateIdentifier>;
