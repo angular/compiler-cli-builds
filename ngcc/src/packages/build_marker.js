643215d34279b0c12d12f316c1ecc658dce6e0a7
Kara Erickson <karakara@google.com>
2019-06-25 17:59:51 +0000

    00ecfc7f9 fix(docs-infra): separate vendor-specific CSS selectors (#31252)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+63.sha-1d5c445.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+65.sha-00ecfc7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
