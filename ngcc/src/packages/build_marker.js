5fafebe4b7485e4753528c406991e92fdda5e008
Kara Erickson <karakara@google.com>
2019-06-25 19:34:37 +0000

    98685e6f8 docs: change note about cli (#31216) PR Close #31216

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+68.sha-a1d436e.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+72.sha-98685e6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
