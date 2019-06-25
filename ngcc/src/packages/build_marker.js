f8dee25d5715c6fe6929b56c0e9e17cda3f88ff6
Kara Erickson <karakara@google.com>
2019-06-25 17:54:49 +0000

    b11a2057c docs(core): change from CSS Event to DOM event (#31229) PR Close #31229

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+52.sha-f7e9659.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+61.sha-b11a205.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
