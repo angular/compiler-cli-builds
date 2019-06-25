45420af81c8c5db01588f3fc046f67397ae3475c
Kara Erickson <karakara@google.com>
2019-06-25 22:04:55 +0000

    ae1ac4598 docs(core): update code sample for "outputs" attribute (#31199)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/path';
 import { FileSystem } from '../file_system/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+80.sha-280e856.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+81.sha-ae1ac45.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
