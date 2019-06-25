152cc4aa7b67212551443c6243183a29a0da6ada
Kara Erickson <karakara@google.com>
2019-06-25 19:29:32 +0000

    e9ae88598 ci: add owners for zone.js to CODEOWNERS (#31203)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs_1 = require("fs");
-    exports.NGCC_VERSION = '8.0.2+30.sha-00242e8.with-local-changes';
+    exports.NGCC_VERSION = '8.0.2+37.sha-e9ae885.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
