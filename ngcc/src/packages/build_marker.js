c33dbe650815873e0bfcb885147fdaab5eb71e2c
Kara Erickson <karakara@google.com>
2019-06-25 03:38:58 +0000

    f7e9659c4 fix(docs-infra): return full width highlight to sidenav (#31246)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+51.sha-16717fa.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+52.sha-f7e9659.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
