ab5d02541a65947818fa819b3cd34e88c9d8226c
Kara Erickson <karakara@google.com>
2019-06-25 22:04:52 +0000

    1e7e06542 docs: correct indention for code example in deployment guide (#31255)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -16,7 +16,7 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NGCC_VERSION = '8.1.0-next.3+81.sha-ae1ac45.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+82.sha-1e7e065.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
