6b5225e8cbefa4de02fa2e2487ae9c634e7e5afd
Kara Erickson <karakara@google.com>
2019-06-26 21:19:11 +0000

    da4f7fbe1 docs: release notes for the v8.0.3 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+105.sha-3851544.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+108.sha-da4f7fb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-next.3+105.sha-3851544.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+108.sha-da4f7fb.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
