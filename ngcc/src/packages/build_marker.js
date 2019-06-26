45b2408eb2b1e8811b445bbb5083c8cb67b7f8db
Kara Erickson <karakara@google.com>
2019-06-26 20:52:36 +0000

    9c87d223e ci: target default pool for linux RBE executions (#31297)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-next.3+106.sha-2be061a.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+107.sha-9c87d22.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
