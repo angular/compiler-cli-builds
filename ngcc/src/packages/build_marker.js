b753b366a415e0ba0c7be30f299e7cee9cd49d87
Kara Erickson <karakara@google.com>
2019-06-26 15:29:54 +0000

    32c760f5e fix(ivy): don't mask errors by calling lifecycle hooks after a crash (#31244)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-next.3+96.sha-f690a4e.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-next.3+97.sha-32c760f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
