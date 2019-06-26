436eb77c98bebfa5956cc208e0677a97476a2f90
Kara Erickson <karakara@google.com>
2019-06-26 15:25:33 +0000

    f690a4e0a fix(ivy): ngcc - do not analyze files outside the current package (#30591)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-next.3+98.sha-3788ebb.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-next.3+96.sha-f690a4e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
