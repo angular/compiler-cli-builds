79b5ed3e10863da9ce16a51ce74fe2480ca8a9fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 17:10:49 +0000

    6c0cca093 docs: couple of small UI fixes throughout some documents (#31155)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+1.sha-d7b4172.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+3.sha-6c0cca0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
