bf601f1b0ea4e15613c9769b0bd7175b2d5265cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 19:54:56 +0000

    d17100608 fix(ivy): ngtsc - NgtscCompilerHost should cope with directories that look like files (#31289)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-rc.0+11.sha-19e8570.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-rc.0+12.sha-d171006.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
