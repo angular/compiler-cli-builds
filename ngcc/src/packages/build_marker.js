540a4ab20bf01f99e131141e3e5a8ea89c2897be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:47:25 +0000

    2b5d52fbd docs: add tests for lazy loading angularjs example (#30622)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-rc.0+21.sha-f57e77e.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0-rc.0+20.sha-2b5d52f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
