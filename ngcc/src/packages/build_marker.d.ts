7d3b4fe2675b74dc8a29185f319774578c17ac27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:47:56 +0000

    81332150a perf(ivy): chain host binding instructions (#31296)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+20.sha-2b5d52f.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+19.sha-8133215.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
