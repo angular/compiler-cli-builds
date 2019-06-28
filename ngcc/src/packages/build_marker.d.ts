8a0acb01dd5160ffc7e5af733a75c612b0090bd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:41:29 +0000

    7ca611cd1 fix(ivy): properly handle re-projection with an empty set of nodes to re-project (#31306)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+24.sha-c12b6fa.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+25.sha-7ca611c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
