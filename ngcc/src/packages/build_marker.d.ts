1125ea7a4d8d938594b75d5ce5ee608d9b66273c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:34:03 +0000

    80ccd6c19 fix(core): handle `undefined` meta in `injectArgs` (#31333)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+19.sha-b7e3d80.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+20.sha-80ccd6c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
