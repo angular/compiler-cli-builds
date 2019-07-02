dbf4ed61ada856e28f13af1c6b567a56e974665d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:50:41 +0000

    0fe6110b9 build(docs-infra): update dgeni-packages dependency (#31368)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+31.sha-a57ea26.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+29.sha-0fe6110.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
