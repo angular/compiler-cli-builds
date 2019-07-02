93fa8f0937b42143dcc08912ecab9440bfe05f88
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:50:29 +0000

    a57ea2640 test(zone.js): fix typos (#31358)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+28.sha-ffe7050.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+31.sha-a57ea26.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
