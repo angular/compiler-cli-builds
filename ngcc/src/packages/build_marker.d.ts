94ece0ac0c29934745afb7f0f8efcf7674c87d9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 19:03:33 +0000

    376c5fceb docs: add accessibility guide (#30851)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0-rc.0+32.sha-d0c32e0.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0-rc.0+34.sha-376c5fc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
