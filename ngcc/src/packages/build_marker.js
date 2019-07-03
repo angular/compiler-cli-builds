7a884ecfd1a99b2eda16cb29118d740b2e34fa7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-03 16:15:25 +0000

    dcf9c13fc ci: update gcp_token (#31405)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0-rc.0+34.sha-376c5fc.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0+1.sha-dcf9c13.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
