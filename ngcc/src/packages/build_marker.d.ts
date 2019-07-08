7d17399cf10b25a962a9def01682b03c9947dc35
Jason Aden <jasonaden@google.com>
2019-07-08 17:40:27 +0000

    8052de07e docs: update polyfill support doc (#31262)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.0+7.sha-40aaa42.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.0+8.sha-8052de0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
