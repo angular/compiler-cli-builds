3ca9529de0d6ed6c87f35e2289b6eecb45cd044d
Jason Aden <jasonaden@google.com>
2019-07-08 17:41:45 +0000

    8052de07e docs: update polyfill support doc (#31262)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.0+7.sha-40aaa42.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.0+8.sha-8052de0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
