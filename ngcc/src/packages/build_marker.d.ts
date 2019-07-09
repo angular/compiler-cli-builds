ccf81739487bb5c00a2b5e822f334a8b33f9b0c2
Jason Aden <jasonaden@google.com>
2019-07-09 21:48:18 +0000

    2b44be984 fix(ivy): i18n should not alloc expando slots when there is no new var (#31451)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.0+20.sha-7724f74.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.0+24.sha-2b44be9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
