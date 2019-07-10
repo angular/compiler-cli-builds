69c526c2c3c69cf9a9cce5ec57bf2f08c08d3318
Jason Aden <jasonaden@google.com>
2019-07-10 17:49:15 +0000

    1863254e0 ci: install Bazel MSYS2 packages on Windows (#31486)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0+13.sha-61cba26.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0+12.sha-1863254.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
