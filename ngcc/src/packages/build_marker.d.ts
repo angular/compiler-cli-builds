ee65832a1a231f0409ebd2c1d6bcd54c8ad303eb
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:09:21 +0000

    103a5b42e fix(ivy): ngcc - resolve `main` property paths correctly (#31509)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.1+8.sha-1cba5d4.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.1+7.sha-103a5b4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
