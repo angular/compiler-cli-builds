6e27e52379e72badb81bec27cc3a32689c710d20
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:02:52 +0000

    b591035e4 docs: add angular-in-depth to community curations (#31212)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0+14.sha-a395cb1.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0+16.sha-b591035.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
