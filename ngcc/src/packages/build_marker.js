e79b49515b6dcceae5a1d64f3a54e64f41c6a0ec
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 22:46:44 +0000

    d545bbeee fix(ivy): handle ModuleWithProviders in R3TestBed correctly (#31415)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.1+34.sha-4bb283c.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.1+35.sha-d545bbe.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
