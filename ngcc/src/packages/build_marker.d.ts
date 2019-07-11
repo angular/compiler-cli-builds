f90bacfd8aa1c7c0fa5ef5b57d60e08624ed0936
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 17:17:20 +0000

    af418b33e fix(docs-infra): fix get started button rendering (#31470)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0+24.sha-96f2d78.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0+25.sha-af418b3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
