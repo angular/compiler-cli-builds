dc41321b76a84a3f564d051c600b3cf4bf849865
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 19:27:14 +0000

    7c62a8f9c docs: remove heroes reference in i18n example (#31515)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.1+21.sha-a55c6df.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.1+22.sha-7c62a8f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
