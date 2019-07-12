ea29840fbf28f3a52185a41e01ef97aa1ccb8a6e
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:17:14 +0000

    2ed812745 docs: add microsyntax details (#31517)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0+43.sha-6737a8d.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0+44.sha-2ed8127.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
