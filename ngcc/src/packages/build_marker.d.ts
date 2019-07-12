69aed4bb2364d78fe3f797bf75efbf44641dbffa
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:57:28 +0000

    fa69e99bd build: add tsconfig-test to dependency for tsconfig in core/test/strict_types (#31471)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.0+36.sha-58260fc.with-local-changes";
+export declare const NGCC_VERSION = "8.1.0+41.sha-fa69e99.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
