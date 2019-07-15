156e1d7bb20d89fba56ca1e6182c2eecd341b604
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 16:55:20 +0000

    d6c80871f docs: remove and update resource list (#31469)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.1+47.sha-b65e11e.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.1+50.sha-d6c8087.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
