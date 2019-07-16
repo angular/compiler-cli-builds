4b1a7e751ee6301b29f2ff14e3cdc31dc3895421
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 17:25:16 +0000

    09576e968 fix(ivy): use goog.LOCALE for Closure Compiler to define default LOCALE_ID (#31519)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.1+56.sha-8853f13.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.1+60.sha-09576e9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
