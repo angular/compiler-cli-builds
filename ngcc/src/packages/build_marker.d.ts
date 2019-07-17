efe0f6f9c6bfc9ceb73fce2855504fd4c5918007
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 05:13:08 +0000

    12fd06916 fix(ivy): don't match directives against attribute bindings (#31541)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.1+60.sha-09576e9.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.1+67.sha-12fd069.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
