ccd4020b40b31fe962c12d544dea2354d96a766e
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:52:05 +0000

    f025fe6c2 docs: remove prerequisite section in NgModules docs (#31169)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.0+58.sha-e1cfa41.with-local-changes';
+    exports.NGCC_VERSION = '8.1.0+56.sha-f025fe6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
