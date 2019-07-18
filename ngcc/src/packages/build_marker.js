703dd42b915d96d18c1ec7345641bff67b0ee0fa
Miško Hevery <misko@hevery.com>
2019-07-18 21:47:27 +0000

    1b6e8411b docs: fix typo in workspace config section (#31630)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+24.sha-376ad9c.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+26.sha-1b6e841.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
