ef0184f3aa20b7e9dc2e61140c627cf36396d074
Miško Hevery <misko@hevery.com>
2019-07-18 21:45:41 +0000

    376ad9c3c refactor(ivy): remove deep imports into the compiler (#31376)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+22.sha-60f58bf.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+24.sha-376ad9c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
