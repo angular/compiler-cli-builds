0346f4bd551e9c99b21ceb962fc18120a6823af5
Miško Hevery <misko@hevery.com>
2019-07-19 00:06:55 +0000

    4495a46b9 refactor(ivy): clean up RNode retrieval; add better debug support for TNode (#31040)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+28.sha-7b6ee5e.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+30.sha-4495a46.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
