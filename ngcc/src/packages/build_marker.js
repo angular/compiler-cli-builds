3fd4f9fee4f97514dd724becbdb470c88648b76e
Kara Erickson <karakara@google.com>
2019-07-19 22:39:52 +0000

    f14693b9a refactor(ivy): correct circular dependency which causes g3 failures (#31665)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+31.sha-f10d6c6.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+32.sha-f14693b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
