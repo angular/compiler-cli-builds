d90bb8d8663e84d29846a3d2d9122ee129b5778c
Miško Hevery <misko@hevery.com>
2019-07-22 17:14:45 +0000

    a9f3547ef fix(docs-infra): fix mobile toc styles (#31533)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.2+16.sha-03050de.with-local-changes';
+    exports.NGCC_VERSION = '8.1.2+15.sha-a9f3547.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
