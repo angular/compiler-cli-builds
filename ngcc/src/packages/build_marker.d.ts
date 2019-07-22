82285792321af76ff261bcf29ff27c1cc8755985
Miško Hevery <misko@hevery.com>
2019-07-22 17:14:39 +0000

    03050deca fix(docs-infra): improve small screen experience for docs cards layout (#31576)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.2+13.sha-157456b.with-local-changes";
+export declare const NGCC_VERSION = "8.1.2+16.sha-03050de.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
