46237771d0e73ee453a08f84d6911f91f5f5a3d2
Miško Hevery <misko@hevery.com>
2019-07-22 17:13:16 +0000

    b9a94c6d0 fix(docs-infra): improve small screen experience for docs cards layout (#31576)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+40.sha-ce19610.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+39.sha-b9a94c6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+40.sha-ce19610.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+39.sha-b9a94c6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
