00e28a2e6603ed86fa1a4268072c1bcc68ca0460
Kara Erickson <karakara@google.com>
2019-07-23 23:09:51 +0000

    9eefe25e2 fix(ivy): components created with wrong context when passing root node to component factory (#31661)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+50.sha-b31a292.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+53.sha-9eefe25.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
