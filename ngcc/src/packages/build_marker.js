34791a08bca288d0e4e1159baa4886596c43e03b
Kara Erickson <karakara@google.com>
2019-07-23 23:10:59 +0000

    b31a29295 test: update size-tracking golden for "core_all" bundling test (#31779)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+51.sha-24ca582.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+50.sha-b31a292.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
