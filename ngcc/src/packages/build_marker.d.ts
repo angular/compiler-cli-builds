b6821ca7c2d594219e09dc9f7edfdf071886bf14
Kara Erickson <karakara@google.com>
2019-07-23 23:05:48 +0000

    f50dede8f refactor(ivy): remove all old styling code prior to refactor (#31193)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+46.sha-0e68c7e.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+47.sha-f50dede.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
