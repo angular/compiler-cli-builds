84907156f20e232a83a55481047867a71114e0eb
Miško Hevery <misko@hevery.com>
2019-07-24 22:29:22 +0000

    3d7303efc perf(ivy): avoid extra parameter in query instructions (#31667)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+84.sha-2172368.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+78.sha-3d7303e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
