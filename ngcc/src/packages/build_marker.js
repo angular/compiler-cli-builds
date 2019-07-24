057a4fed73b30392b6dd24b467f84d358ec12bbd
Misko Hevery <misko@hevery.com>
2019-07-24 04:33:24 +0000

    7f7033bbd fix(zone.js): update dart zone link (#31646)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+65.sha-a445826.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+64.sha-7f7033b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
