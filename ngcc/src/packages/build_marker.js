fcc602864b1d88cedc662c7a68df1df5547b319f
Miško Hevery <misko@hevery.com>
2019-07-24 22:14:52 +0000

    53286d1ef fix(zone.js): patch shadydom (#31717)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.1.2+28.sha-c73b75f.with-local-changes';
+    exports.NGCC_VERSION = '8.1.2+30.sha-53286d1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
