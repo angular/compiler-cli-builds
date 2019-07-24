4fe0f1c111160decfeeb6a2e1f3dc9cddc332293
Miško Hevery <misko@hevery.com>
2019-07-24 21:55:28 +0000

    29e1c53a3 feat(upgrade): support $element in upgraded component template/templateUrl functions (#31637)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+72.sha-2bb9a65.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+74.sha-29e1c53.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
