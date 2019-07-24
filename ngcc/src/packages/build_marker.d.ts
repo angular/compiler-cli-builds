476829bb6ef201d1e5a35433d58cc28e586cb244
Misko Hevery <misko@hevery.com>
2019-07-24 04:35:50 +0000

    5ae9b76a9 docs: add ngxs to library section (#31792) PR Close #31792

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+64.sha-7f7033b.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+68.sha-5ae9b76.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
