2d0167b1791ace209a6baa29f99763af3ed885b0
Miško Hevery <misko@hevery.com>
2019-07-24 21:52:43 +0000

    2bb9a6535 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+69.sha-b6aeace.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+72.sha-2bb9a65.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
