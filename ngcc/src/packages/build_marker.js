c8fb4acbc415f489f387034899480a72bc0da78b
Miško Hevery <misko@hevery.com>
2019-07-24 22:04:21 +0000

    2c402d5c9 fix(zone.js): handle MSPointer event correctly (#31722)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+77.sha-716af10.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+83.sha-2c402d5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
