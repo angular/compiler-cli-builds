b20f652da542fe1f8c04575dbb471cbb5452cb08
Miško Hevery <misko@hevery.com>
2019-07-24 22:27:49 +0000

    3d7303efc perf(ivy): avoid extra parameter in query instructions (#31667)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+84.sha-2172368.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+78.sha-3d7303e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
