3a9ce2b923444d312cd4cc9ff2bf9282d2ab32c2
Misko Hevery <misko@hevery.com>
2019-07-24 04:43:16 +0000

    5407b4397 fix(zone.js): update dart zone link (#31646)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.1.2+24.sha-77143b5.with-local-changes";
+export declare const NGCC_VERSION = "8.1.2+22.sha-5407b43.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
