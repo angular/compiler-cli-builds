3889fd1c1e137edfc5735336133a196d5f55c376
Miško Hevery <misko@hevery.com>
2019-07-25 20:27:52 +0000

    85d051f8d perf(ivy): introduce new benchmark for view create, destroy, traverse (#31797)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+78.sha-3d7303e.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+86.sha-85d051f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
