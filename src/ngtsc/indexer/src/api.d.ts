65b0843d357fc1d317bd2194638254f55d55d5ef
Miško Hevery <misko@hevery.com>
2019-07-25 21:54:57 +0000

    6b67cd562 feat(ivy): index template references, variables, bound attributes/events (#31535)

--- src/ngtsc/indexer/src/api.d.ts
+++ src/ngtsc/indexer/src/api.d.ts
@@ -16,7 +16,10 @@ export declare enum IdentifierKind {
     Property = 0,
     Method = 1,
     Element = 2,
-    Attribute = 3
+    Template = 3,
+    Attribute = 4,
+    Reference = 5,
+    Variable = 6
 }
 /**
  * Describes a semantically-interesting identifier in a template, such as an interpolated variable
@@ -27,12 +30,20 @@ export interface TemplateIdentifier {
     span: AbsoluteSourceSpan;
     kind: IdentifierKind;
 }
+/** Describes a template expression, which may have a template reference or variable target. */
+interface ExpressionIdentifier extends TemplateIdentifier {
+    /**
+     * ReferenceIdentifier or VariableIdentifier in the template that this identifier targets, if
+     * any. If the target is `null`, it points to a declaration on the component class.
+     * */
+    target: ReferenceIdentifier | VariableIdentifier | null;
+}
 /** Describes a property accessed in a template. */
-export interface PropertyIdentifier extends TemplateIdentifier {
+export interface PropertyIdentifier extends ExpressionIdentifier {
     kind: IdentifierKind.Property;
 }
 /** Describes a method accessed in a template. */
-export interface MethodIdentifier extends TemplateIdentifier {
+export interface MethodIdentifier extends ExpressionIdentifier {
     kind: IdentifierKind.Method;
 }
 /** Describes an element attribute in a template. */
@@ -44,23 +55,48 @@ interface DirectiveReference {
     node: ClassDeclaration;
     selector: string;
 }
+/** A base interface for element and template identifiers. */
+interface BaseElementOrTemplateIdentifier extends TemplateIdentifier {
+    /** Attributes on an element or template. */
+    attributes: Set<AttributeIdentifier>;
+    /** Directives applied to an element or template. */
+    usedDirectives: Set<DirectiveReference>;
+}
 /**
  * Describes an indexed element in a template. The name of an `ElementIdentifier` is the entire
  * element tag, which can be parsed by an indexer to determine where used directives should be
  * referenced.
  */
-export interface ElementIdentifier extends TemplateIdentifier {
+export interface ElementIdentifier extends BaseElementOrTemplateIdentifier {
     kind: IdentifierKind.Element;
-    /** Attributes on an element. */
-    attributes: Set<AttributeIdentifier>;
-    /** Directives applied to an element. */
-    usedDirectives: Set<DirectiveReference>;
+}
+/** Describes an indexed template node in a component template file. */
+export interface TemplateNodeIdentifier extends BaseElementOrTemplateIdentifier {
+    kind: IdentifierKind.Template;
+}
+/** Describes a reference in a template like "foo" in `<div #foo></div>`. */
+export interface ReferenceIdentifier extends TemplateIdentifier {
+    kind: IdentifierKind.Reference;
+    /** The target of this reference. If the target is not known, this is `null`. */
+    target: {
+        /** The template AST node that the reference targets. */
+        node: ElementIdentifier | TemplateIdentifier;
+        /**
+         * The directive on `node` that the reference targets. If no directive is targeted, this is
+         * `null`.
+         */
+        directive: ClassDeclaration | null;
+    } | null;
+}
+/** Describes a template variable like "foo" in `<div *ngFor="let foo of foos"></div>`. */
+export interface VariableIdentifier extends TemplateIdentifier {
+    kind: IdentifierKind.Variable;
 }
 /**
  * Identifiers recorded at the top level of the template, without any context about the HTML nodes
  * they were discovered in.
  */
-export declare type TopLevelIdentifier = PropertyIdentifier | MethodIdentifier | ElementIdentifier;
+export declare type TopLevelIdentifier = PropertyIdentifier | MethodIdentifier | ElementIdentifier | TemplateNodeIdentifier | ReferenceIdentifier | VariableIdentifier;
 /**
  * Describes the absolute byte offsets of a text anchor in a source code.
  */
