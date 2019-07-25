5e1ac27333b85ce891301f7b67038014ad2ddbfb
Miško Hevery <misko@hevery.com>
2019-07-25 20:28:29 +0000

    0cd4c019c build: lock material unit tests job to specific commit (#31569)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+86.sha-85d051f.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+88.sha-0cd4c01.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
