a1c9de7a1cdf1ea35f8f28889a47ff7ef90bc841
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:23:58 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+97.sha-3067309.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+98.sha-859ebdd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
