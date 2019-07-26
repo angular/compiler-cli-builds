84f7a7e16e6fa6ae1de649c0e65527cf68432434
Miško Hevery <misko@hevery.com>
2019-07-26 18:59:23 +0000

    fa15814d7 build(zone.js): add changelog gulptask for zone.js (#31852)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+95.sha-7a62530.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-rc.0+1.sha-fa15814.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
