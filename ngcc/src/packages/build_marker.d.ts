e79108bd878a6f1391d3d131b30c11438fa8826c
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:26:27 +0000

    5296c04f6 fix(ivy): set LOCALE_ID when using the injector (#31566)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+101.sha-4da8052.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+103.sha-5296c04.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+101.sha-4da8052.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+103.sha-5296c04.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
