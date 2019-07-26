da455e2741db290825b09ee956a07cb5bd421b78
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:20:55 +0000

    155f40c17 docs: fix header mistake (#31811)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-rc.0+3.sha-8c446b0.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-rc.0+4.sha-155f40c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-rc.0+3.sha-8c446b0.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-rc.0+4.sha-155f40c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
