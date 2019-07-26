79f9d5db9ff5adbbf4fffc1e4be3494c7be1c9d8
Andrew Kushnir <akushnir@google.com>
2019-07-26 20:18:23 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+94.sha-6ece7db.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+98.sha-859ebdd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
