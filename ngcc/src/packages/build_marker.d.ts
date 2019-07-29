73a797a777f06058097a7b75bb3988ff122f424e
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:20:35 +0000

    5f0d5e9cc build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+98.sha-859ebdd.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+104.sha-5f0d5e9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
