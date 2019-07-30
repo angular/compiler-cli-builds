925dd78349810874a404721e18058a6f9bf41272
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:18:28 +0000

    5c9a8961d fix(zone.js): don't rely on global node typings outside of node/ directory (#31783)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0-next.2+113.sha-3479fdd.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0-next.2+114.sha-5c9a896.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
