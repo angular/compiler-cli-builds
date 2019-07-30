5205be208ccb3b5d342689c5ee1c19d9d847ab9e
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:17:17 +0000

    3479fddf6 docs: fix typo in Animation guide (#31888)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0-next.2+111.sha-cbcbe23.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0-next.2+113.sha-3479fdd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
