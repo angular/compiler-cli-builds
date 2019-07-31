a350eed992e4db201eab0e154e9b6bc763e50f4f
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:06:28 +0000

    ecffbda66 test: fix outDir in TS integration tests (#29284)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+2.sha-8e7a0d4.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+5.sha-ecffbda.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
