ff70c78d35506c079de3bb52131995d3ef4ea0ad
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:00:11 +0000

    8e7a0d4ff docs: add ReactiveConf 2019 to events (#31895) PR Close #31895

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+1.sha-4db9592.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+2.sha-8e7a0d4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
