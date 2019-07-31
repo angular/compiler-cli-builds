49ac01be0ebcdb236d7e170bb01195581623830b
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:58:35 +0000

    4db959260 docs(ivy): Add README to indexer module (#31260)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+1.sha-4db9592.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
