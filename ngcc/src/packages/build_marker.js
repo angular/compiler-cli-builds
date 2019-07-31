b281342898650bbb9ddca55e5cede6c38d2fd8e4
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:11:57 +0000

    584b42343 build: Do not generate *.umd.min.js for language service (#31917)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+5.sha-ecffbda.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+3.sha-584b423.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
