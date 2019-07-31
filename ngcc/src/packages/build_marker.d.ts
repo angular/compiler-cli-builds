c0e05c3dba1a36e4281eda8862d97c746e0554f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-31 23:40:40 +0000

    b8f269414 fix(ivy): speed up ngtsc if project has no templates to check (#31922)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+2.sha-8ed83ca.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+3.sha-b8f2694.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
