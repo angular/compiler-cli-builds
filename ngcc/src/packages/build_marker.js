13206fe9186b0795515dc61ebdb9882b03adffe2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:32:32 +0000

    9e9179e91 docs: update Zenika trainings URL in resources.json (#31910) PR Close #31910

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+8.sha-c1ae612.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+9.sha-9e9179e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
