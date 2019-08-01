3409230ffe9dc692424684e504efa84a808fb87d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:24 +0000

    d0d875a3f fix(ivy): pass `schemas` field to nested views (#31913)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+10.sha-e8b8f6d.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+11.sha-d0d875a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
