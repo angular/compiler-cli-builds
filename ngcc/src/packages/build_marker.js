6b9c27c9ef059bccb1099c8060bb04a43e72c2ed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:30:29 +0000

    c1ae6124c fix(upgrade): compile downgraded components synchronously (if possible) (#31840)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+11.sha-d0d875a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+8.sha-c1ae612.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
