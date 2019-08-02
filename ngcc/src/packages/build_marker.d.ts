ebe38c05d1ae3c4fad6a3e8428a613aaf5cadfd9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:04:45 +0000

    44b5bf486 docs: add links to web.dev/angular guides (#31804)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+5.sha-04ebd59.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+7.sha-44b5bf4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
