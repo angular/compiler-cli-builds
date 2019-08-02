5f53968f154c457130263f4cbae12908b2bedd04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:05:26 +0000

    a2183ddb7 fix(ivy): directive matching not working in some cases when preceded by styling attributes (#31942)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+18.sha-3122f34.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+19.sha-a2183dd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
