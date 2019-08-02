f76e104a739c9f73ca7427176062999d10f19480
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:02:48 +0000

    3122f3415 perf(ivy): remove global state acces from postProcessDirective (#31946)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+14.sha-f2d47c9.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+18.sha-3122f34.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
