70dec4f84c1439c8396c1b0680c16c3d185cf66a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 21:46:23 +0000

    2a6e6c02e build(zone.js): update gulp task to gen changelog automatically (#31915)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+22.sha-ebb2772.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+24.sha-2a6e6c0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
