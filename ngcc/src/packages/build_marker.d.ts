c64b3bf0e8b0f2477b9d2b3b5d16ae4a2d484d70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:10:40 +0000

    c00e7ff12 fix(docs-infra): ui polish (#31013)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+13.sha-2cabced.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+15.sha-c00e7ff.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
