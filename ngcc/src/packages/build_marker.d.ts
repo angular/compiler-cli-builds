30aaa4200a00f922747ffa444baa19b0b30cfd3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 17:17:56 +0000

    2cabced9f build(docs-infra): fix StackBlitz projects and ZIPs for `testing` guide examples (#31937)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+10.sha-dde0a32.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+13.sha-2cabced.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
