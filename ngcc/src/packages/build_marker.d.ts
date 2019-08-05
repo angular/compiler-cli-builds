b8601aa54c279346e4051306deb915107c6aa007
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:23:23 +0000

    6ba31b9b8 docs: updated doc for timezone param in DatePipe (#31827) PR Close #31827

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+19.sha-83d67b4.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+18.sha-6ba31b9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0+19.sha-83d67b4.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0+18.sha-6ba31b9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
