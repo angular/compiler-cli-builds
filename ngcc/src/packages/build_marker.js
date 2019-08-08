5ac53cfc2307b42ef90f58d791adb63e868ec6a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:05:35 +0000

    038d82394 docs: remove extraneous word from sentence in styleguide (#32038)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.0+27.sha-1909583.with-local-changes';
+    exports.NGCC_VERSION = '8.2.0+28.sha-038d823.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
