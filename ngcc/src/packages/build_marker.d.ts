62c994fb06eb3d15bd9ab64b746ff61fb696e67f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:37:56 +0000

    bb4627786 docs: update node:8-browsers to 10 in testing (#32049)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+21.sha-afd76a8.with-local-changes";
+export declare const NGCC_VERSION = "8.2.0+20.sha-bb46277.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
