6d0796478bd5e237cc1a78ff01b298a983cff156
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:32:04 +0000

    17e289c39 docs: correct description of output decorator and add links to guide (#31780)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+69.sha-f8c27d4.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+71.sha-17e289c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
