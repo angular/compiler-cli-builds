f1d55539b89b24449ed0e56a5ef2b2a16782de43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:31:09 +0000

    9106271f2 refactor(ivy): remove dependency on global state from isCreationMode (#31959)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+63.sha-a8e2ee1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+67.sha-9106271.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
