126c7dab2177a8f171ab265ac26d0c5ff6e0ada0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:35:36 +0000

    f7eebd022 build(docs-infra): upgrade cli command docs sources to bb4be27da (#32036)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+53.sha-e5a89e0.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+55.sha-f7eebd0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
