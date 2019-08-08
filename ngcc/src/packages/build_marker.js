4bf3378161c341c96501c592215382924d41c6ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:02:02 +0000

    22d3cabc1 docs: add missing Oxford comma in styleguide (#32040)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+59.sha-a06043b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+60.sha-22d3cab.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
