bf07424e842058895184cae437d70ff73ec4122e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:32:57 +0000

    f8c27d42e docs: add note on compatibility mode (#31808)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+67.sha-9106271.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+69.sha-f8c27d4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
