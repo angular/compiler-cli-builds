e7d8df85c3bcda720f2e4abe6e7e69ed3c99c94b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:35:56 +0000

    e5a89e047 docs: update node:8-browsers to 10 in testing (#32049)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.0+54.sha-8af2cc1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.0+53.sha-e5a89e0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
