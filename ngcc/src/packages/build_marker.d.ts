b726ee2106f2807bbe7b765f9a16340b46b256ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:23:12 +0000

    a8e2ee134 fix(language-service): Make Definition and QuickInfo compatible with TS LS (#31972)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.0+62.sha-e906a4f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.0+63.sha-a8e2ee1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
