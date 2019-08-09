e8326720a3ad4e30bcc9fa7428079e28fcbf0187
Kara Erickson <karakara@google.com>
2019-08-09 21:33:30 +0000

    eccb60cd6 test: update golden files (#32069)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.1+4.sha-6ec91dd.with-local-changes";
+export declare const NGCC_VERSION = "8.2.1+6.sha-eccb60c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
