afd53568d8e96846929606db63b30929ba8ccf9e
Kara Erickson <karakara@google.com>
2019-08-09 18:05:58 +0000

    695f322dc docs: clarify pipe naming (#31806)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.1+7.sha-9896d43.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.1+5.sha-695f322.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
