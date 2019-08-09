e6c42ac672d270795d167779ce00f71854b1e2a8
Kara Erickson <karakara@google.com>
2019-08-09 18:14:19 +0000

    90ddee7a7 ci: remove codefresh config and supporting files (#32058)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.1+2.sha-3300331.with-local-changes";
+export declare const NGCC_VERSION = "8.2.1+3.sha-90ddee7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
