5e030f955e25cd778e030cdfd475db11e3d1946e
Kara Erickson <karakara@google.com>
2019-08-09 18:05:50 +0000

    6f3414bd6 docs: fix cli builder doc (#31305)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.0+34.sha-cfaefa9.with-local-changes";
+export declare const NGCC_VERSION = "8.2.1+1.sha-6f3414b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
