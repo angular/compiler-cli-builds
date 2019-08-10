765c7551f815def7137376de761fc5e314733826
Kara Erickson <karakara@google.com>
2019-08-10 02:22:19 +0000

    37de490e2 Revert "feat(compiler): allow selector-less directives as base classes (#31379)" (#32089)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.1+18.sha-9a37e82.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.1+20.sha-37de490.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
