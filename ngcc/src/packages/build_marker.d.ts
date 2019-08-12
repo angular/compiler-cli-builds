cdca450dc6aa0bf16e7d03ffc0c6b2fabbe06c97
Kara Erickson <karakara@google.com>
2019-08-12 23:24:46 +0000

    18bac15dd ci: add ivy commits to generated CHANGELOG (#32114)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+1.sha-a1fe52b.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+2.sha-18bac15.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
