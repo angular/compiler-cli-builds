cb76ffbcfda93456737a0faa73761ae8e9f7db70
Kara Erickson <karakara@google.com>
2019-08-12 14:27:08 +0000

    972550ee0 style: fix inline comment typo (#32090)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.1+10.sha-c67f490.with-local-changes";
+export declare const NGCC_VERSION = "8.2.1+11.sha-972550e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
