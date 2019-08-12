fea0265f2242effde8e31015e404c47cc8047683
Kara Erickson <karakara@google.com>
2019-08-12 17:57:51 +0000

    4ea3e7e00 refactor(ivy): combine query load instructions (#32100)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.1+22.sha-6eb9c2f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.1+23.sha-4ea3e7e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
