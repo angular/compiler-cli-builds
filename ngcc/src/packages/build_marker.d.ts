f5958b905bdf9d3679349b5f9dc37cbe459d58c0
Kara Erickson <karakara@google.com>
2019-08-12 20:37:42 +0000

    76854287e Revert "fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)" (#32110)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.1+11.sha-972550e.with-local-changes";
+export declare const NGCC_VERSION = "8.2.1+12.sha-7685428.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
