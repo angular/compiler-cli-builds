4e5da5f1ce21c416d27fc9c4e62d453f97831f88
Kara Erickson <karakara@google.com>
2019-08-12 21:44:26 +0000

    9406104c0 release: cut the v9.0.0-next.2 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.1+25.sha-c0194e0.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
