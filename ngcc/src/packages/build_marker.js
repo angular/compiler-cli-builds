86adfbdd2d4204d82719ad412efa94fa32cd3274
Kara Erickson <karakara@google.com>
2019-08-13 18:41:06 +0000

    a91ab1552 fix(language-service): Remove 'context' used for module resolution (#32015)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+7.sha-a95f860.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+8.sha-a91ab15.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
