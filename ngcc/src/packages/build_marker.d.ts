9bb10f24d7a171d88d0116f68ffe36c11b1aacda
Kara Erickson <karakara@google.com>
2019-08-13 22:02:54 +0000

    4d96cf519 ci: update material-unit-tests job to latest commit (#31650)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+14.sha-bef27f2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+17.sha-4d96cf5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
