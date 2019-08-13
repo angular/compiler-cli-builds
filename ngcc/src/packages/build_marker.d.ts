a2f5cf396b7eb8d0d46f1dffb04ba36b55915743
Kara Erickson <karakara@google.com>
2019-08-13 17:13:42 +0000

    ebcf79d7f docs(zone.js): update DEVELOPER.md for changelog instruction (#32016)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+2.sha-48487cb.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+4.sha-ebcf79d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
