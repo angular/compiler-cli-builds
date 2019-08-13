4f452c02018971051b890ddc34641ff964a8216d
Kara Erickson <karakara@google.com>
2019-08-13 19:28:06 +0000

    3d1b82be6 docs: correct description of animation example (#32009)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+8.sha-7b3bd21.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+5.sha-3d1b82b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
