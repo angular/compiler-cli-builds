f80b30c285e4dc9f53be3a909412711971c4b3e5
Kara Erickson <karakara@google.com>
2019-08-13 17:15:21 +0000

    3cd8a2b90 docs: update events page and fix ordering (#32106)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+4.sha-ebcf79d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+5.sha-3cd8a2b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
