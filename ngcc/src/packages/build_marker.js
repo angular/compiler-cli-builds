17268c27ceed5e7103e2294d000955605060142c
Kara Erickson <karakara@google.com>
2019-08-13 17:20:33 +0000

    2f812f31d docs: update events page and fix ordering (#32106)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+4.sha-dec7e52.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+3.sha-2f812f3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
