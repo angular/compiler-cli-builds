72f08a864a7450ebf1c1ea51d9e3c3bc5e5a35ff
Andrew Kushnir <akushnir@google.com>
2019-08-14 21:31:36 +0000

    d6bbc4d76 docs(router): fix router description (#32136)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+31.sha-5a562d8.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+30.sha-d6bbc4d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
