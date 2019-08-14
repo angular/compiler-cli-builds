7f8355895bc5d43eb57f441d23abe1067ec2fe8b
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:20:34 +0000

    984d23f68 fix(docs-infra): fix broken toc ul styles (#32124)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+23.sha-9e6c677.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+26.sha-984d23f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
