8e8484b1ad3ae437deaa876f75657af9000e2ad2
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:26:40 +0000

    84a3daf60 docs: doc browser support for service workers (#32046)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+14.sha-0757702.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+10.sha-84a3daf.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+14.sha-0757702.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+10.sha-84a3daf.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
