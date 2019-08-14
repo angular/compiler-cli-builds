6c7bcc0ce30aa912309e18dbd5958f90c70c19a7
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:23:34 +0000

    8af85d812 docs: add info about reviewing PRs from code owners (#32108)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+28.sha-4d43680.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+25.sha-8af85d8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
