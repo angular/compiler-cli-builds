8fc776ee478250a14ba8f4e469245978835f4350
Andrew Kushnir <akushnir@google.com>
2019-08-14 21:30:56 +0000

    5a562d8a0 refactor(language-service): Return ts.CompletionInfo for getCompletionsAt() (#32116)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+29.sha-4055150.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+31.sha-5a562d8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
