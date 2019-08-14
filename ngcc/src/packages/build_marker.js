c953356e161b646342443b2dcce5c6240f40310f
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:24:04 +0000

    0757702d6 docs: add guide to reproduce material-unit-test failures locally (#32138)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+11.sha-da44498.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+14.sha-0757702.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
