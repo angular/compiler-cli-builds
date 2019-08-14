6c8ad68d7eabee6147cea2c77a4531dd6eaab83e
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:19:47 +0000

    9e6c67713 test(language-service): Remove test for external template (#32017)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+24.sha-8438811.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+23.sha-9e6c677.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
