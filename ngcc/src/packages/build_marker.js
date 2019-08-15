278c2c64c9aa0a8a4302744eae822b14d13480a9
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:02:08 +0000

    1c6516199 docs: clarify hierarchical injectors (#28700)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+39.sha-2e4d17f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+41.sha-1c65161.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
