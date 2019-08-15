a9e467d6eef084aaefe25e44f0b041146e1b9c1d
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:57:14 +0000

    adc869ff8 build: add `ngcc` as a valid commit message scope (#32144)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+16.sha-46f2dcc.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+17.sha-adc869f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+16.sha-46f2dcc.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+17.sha-adc869f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
