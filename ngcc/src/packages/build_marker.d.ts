711492a479379f70554fe49f1f0096a49a382512
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:05:38 +0000

    699c705a8 docs: clarify hierarchical injectors (#28700)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+19.sha-6f2e8af.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+18.sha-699c705.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+19.sha-6f2e8af.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+18.sha-699c705.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
