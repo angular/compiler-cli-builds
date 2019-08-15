19835977bc2a10f829dfed7d399f8614400bd5ae
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:55:12 +0000

    2e4d17f3a perf(core): make sanitization tree-shakable in Ivy mode (#31934)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+40.sha-3cf2005.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+39.sha-2e4d17f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
