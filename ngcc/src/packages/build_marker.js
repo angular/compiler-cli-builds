34d59d3bf849eb14f3b475daeb37cdddf9abe8c2
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:11:32 +0000

    46f2dcc47 build(docs-infra): upgrade cli command docs sources to 43fc440c0 (#32148)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+15.sha-2d9c4c1.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+16.sha-46f2dcc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
