b9f65f604a70f3128f0dbe65517a803d87edb0a7
Andrew Kushnir <akushnir@google.com>
2019-08-16 17:28:10 +0000

    abb44f7db perf(ivy): avoid for-of loops at runtime (#32157)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+51.sha-71ada48.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+50.sha-abb44f7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
