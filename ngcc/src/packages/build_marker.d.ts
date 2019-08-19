7465a81fe19b7bf18f066ee02c22cc03f675167c
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:28:53 +0000

    adb4f7c7c build(docs-infra): upgrade cli command docs sources to 6aae7f563 (#32174)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+52.sha-5f76de1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+53.sha-adb4f7c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
