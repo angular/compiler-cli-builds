8c7902fc340e708f2fe359bfb679db6dd0cc556d
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:07:06 +0000

    5da5ca5c2 fix(bazel): pin `@microsoft/api-extractor` (#32187)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+65.sha-639b732.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+64.sha-5da5ca5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+65.sha-639b732.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+64.sha-5da5ca5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
