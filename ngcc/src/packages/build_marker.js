c2a29a8cf3e731a1334a02796f094cae11992276
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:33:54 +0000

    994264c0b refactor(ivy): simplify `walkTNodeTree` method for readability (#31065)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+55.sha-e85ec23.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+58.sha-994264c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
