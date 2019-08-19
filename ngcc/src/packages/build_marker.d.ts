fc05fab871ae9de24b81516c4add72bd5b993748
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:03:55 +0000

    639b73202 refactor(core): remove disabled injectable-pipe migration (#32184)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+63.sha-0b1bf14.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+65.sha-639b732.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
