3ab02b433092e0a5bb90ce27e33d6eabfeceb412
Andrew Kushnir <akushnir@google.com>
2019-08-20 18:08:30 +0000

    3dbc4ab57 fix(ivy): get name directly from nativeNode (#32198)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+68.sha-cfed0c0.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+69.sha-3dbc4ab.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
