cae82b787ed736bedbe5bcf4bfd47876c757dd6f
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:15:58 +0000

    f8b995dbf fix(ngcc): ignore format properties that exist but are undefined (#32205)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+67.sha-bb3c684.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+66.sha-f8b995d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
