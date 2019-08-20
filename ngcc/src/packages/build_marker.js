ad325156327128fdd5042f9c3edc9161a8562d21
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:07:40 +0000

    a01ed0d7b refactor(core): remove disabled injectable-pipe migration (#32184) (#32206)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+23.sha-a7b9478.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+24.sha-a01ed0d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
