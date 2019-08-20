605e6a143d7ff0eaa2e878140114a1b882febe09
Andrew Kushnir <akushnir@google.com>
2019-08-20 23:58:36 +0000

    388578fec Revert "ci: remove material-unit-tests failure blocklist (#32138)" (#32226)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.2+70.sha-dd6070a.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.2+71.sha-388578f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
