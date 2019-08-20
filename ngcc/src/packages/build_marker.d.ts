3c16d8233ce48f6839d3837c5ca45c29dd3d41f6
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:15:05 +0000

    af170d2ae ci: exclude the upstream g3 branch from building on CI (#32202)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+24.sha-a01ed0d.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+25.sha-af170d2.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.2+24.sha-a01ed0d.with-local-changes';
+    exports.NGCC_VERSION = '8.2.2+25.sha-af170d2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
