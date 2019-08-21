8b4d1e3a171a33486e98e1dec362eba0bf6b3161
atscott <atscott@google.com>
2019-08-21 22:41:46 +0000

    7fed0faa4 docs: edit and organize aot doc (#32028)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.3+4.sha-5c94833.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.3+2.sha-7fed0fa.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
