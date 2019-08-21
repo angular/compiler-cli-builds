c0f24d10ccb0ac30c53393161fb957ed3562125b
Andrew Kushnir <akushnir@google.com>
2019-08-21 16:34:47 +0000

    e7c4e94c9 docs: update collaborators page (#32229)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+27.sha-26dc826.with-local-changes";
+export declare const NGCC_VERSION = "8.2.2+28.sha-e7c4e94.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
