0c70414c80023a155335fef4529ffe3b0121c732
Andrew Kushnir <akushnir@google.com>
2019-08-21 21:15:34 +0000

    e8c09e3b5 docs: release notes for the v8.2.3 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.2+88.sha-53bfa7c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.2+89.sha-e8c09e3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
