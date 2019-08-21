a23e791b73d666b24313455dcbefbb26a349af96
atscott <atscott@google.com>
2019-08-21 22:43:33 +0000

    b85ac0313 docs(upgrade): position old setup guide as legacy until it can be removed (#32193)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.3+2.sha-7fed0fa.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.3+3.sha-b85ac03.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
