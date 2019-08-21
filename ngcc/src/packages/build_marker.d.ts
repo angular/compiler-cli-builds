0a91e69689c5b341f949650e2ad1721932aeb22d
atscott <atscott@google.com>
2019-08-21 22:36:26 +0000

    44f233c02 docs: edit and organize aot doc (#32028)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.2+29.sha-e3c9f9d.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+1.sha-44f233c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
