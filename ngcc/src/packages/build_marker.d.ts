4c73ba4d316b30a088f93ddb4f87afac54d9ae19
atscott <atscott@google.com>
2019-08-21 22:47:29 +0000

    f661f460b docs(upgrade): position old setup guide as legacy until it can be removed (#32193)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+1.sha-44f233c.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+2.sha-f661f46.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.3+1.sha-44f233c.with-local-changes';
+    exports.NGCC_VERSION = '8.2.3+2.sha-f661f46.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
