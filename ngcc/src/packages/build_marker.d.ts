34736ed0859bd9497ec788e427982d1cc38f3636
atscott <atscott@google.com>
2019-08-22 20:52:33 +0000

    21bb74d16 docs: improve structure (#32227)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+13.sha-6d11154.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+19.sha-21bb74d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
