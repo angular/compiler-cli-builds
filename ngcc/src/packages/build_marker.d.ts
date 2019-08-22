3a109bed2ce93869a18eec902d871d28373b6445
atscott <atscott@google.com>
2019-08-22 17:36:26 +0000

    6b245a39e fix(ivy): reset binding index before executing a template in `refreshView` call (#32201)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+10.sha-0677cf0.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+12.sha-6b245a3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
