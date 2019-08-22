bf27b2a98ad46a01dc605ffbe8543a40bdc202c5
atscott <atscott@google.com>
2019-08-22 14:00:56 +0000

    5e4babeaa docs: update http guide (#32045)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+2.sha-f661f46.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+3.sha-5e4babe.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.3+2.sha-f661f46.with-local-changes';
+    exports.NGCC_VERSION = '8.2.3+3.sha-5e4babe.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
