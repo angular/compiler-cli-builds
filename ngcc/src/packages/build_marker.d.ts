7bb84ec1905a27ced100056d8c048da81beb59bd
atscott <atscott@google.com>
2019-08-22 13:59:40 +0000

    21edc6a82 docs: update http guide (#32045)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+8.sha-b6fa929.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+7.sha-21edc6a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
