ff604795a716f28c1cc95f25b94338a85300e53e
atscott <atscott@google.com>
2019-08-26 19:06:22 +0000

    6b0b7d01b docs: fix typo in compiler options guide (#32312)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+16.sha-b5cb120.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+18.sha-6b0b7d0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
