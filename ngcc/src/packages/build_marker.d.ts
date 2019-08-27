075286f31214adb557c1f3dfa832c57fdc92bab2
atscott <atscott@google.com>
2019-08-27 23:38:56 +0000

    65aaffcfe docs(service-worker): add example app for `SwPush` API docs (#32139)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+58.sha-e3f4281.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+60.sha-65aaffc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
