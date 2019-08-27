ec466e9410a73330c26c6e677b5261e4c0af198a
atscott <atscott@google.com>
2019-08-27 16:31:53 +0000

    55eaa5fb6 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.3+50.sha-70cf8ed.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.3+51.sha-55eaa5f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
