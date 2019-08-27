1691dd017b309b5c5d84b63e243e18c14cd33f44
atscott <atscott@google.com>
2019-08-27 16:29:12 +0000

    f57c17de2 build: remove the deprecated `aio` commit message scope (#32341)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+49.sha-c7c7f9f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+47.sha-f57c17d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
