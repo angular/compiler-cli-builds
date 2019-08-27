2db1316c4aff6cf63c53f3ba6bd41646dc77fba9
atscott <atscott@google.com>
2019-08-27 21:17:22 +0000

    14feb5613 fix(ivy): debug node names should match user declaration (#32328)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+52.sha-f209aac.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+53.sha-14feb56.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
