78f665b93395630daaa178369b11f8fbdc0a1e9d
atscott <atscott@google.com>
2019-08-27 16:30:20 +0000

    55eaa5fb6 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.3+50.sha-70cf8ed.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.3+51.sha-55eaa5f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
