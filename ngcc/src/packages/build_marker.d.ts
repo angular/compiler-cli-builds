4449b283cc9fca1d867ea6b139290bfedd3e0a88
atscott <atscott@google.com>
2019-08-28 16:25:12 +0000

    6aaf4eac8 docs(docs-infra): add note about setting up examples on Windows (#32355)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+28.sha-aa8dd74.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+32.sha-6aaf4ea.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
