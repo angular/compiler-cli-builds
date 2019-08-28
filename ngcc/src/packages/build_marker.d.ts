a20710ac5ca5085a3b6c3127e7ca2ed201bcf522
Andrew Scott <atscott01@gmail.com>
2019-08-28 19:26:05 +0000

    261593aab docs: update release notes for 8.2.4 to indicate a docs only release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+35.sha-b7f95be.with-local-changes";
+export declare const NGCC_VERSION = "8.2.4+1.sha-261593a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.3+35.sha-b7f95be.with-local-changes';
+    exports.NGCC_VERSION = '8.2.4+1.sha-261593a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
