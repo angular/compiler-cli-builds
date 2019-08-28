3e3840df9d02b001ab3d9d94aea21ab59ca51001
atscott <atscott@google.com>
2019-08-28 16:58:26 +0000

    d02573d05 docs: fix a minor typo in the HTTP guide (#32359)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.3+32.sha-6aaf4ea.with-local-changes";
+export declare const NGCC_VERSION = "8.2.3+34.sha-d02573d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.3+32.sha-6aaf4ea.with-local-changes';
+    exports.NGCC_VERSION = '8.2.3+34.sha-d02573d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
