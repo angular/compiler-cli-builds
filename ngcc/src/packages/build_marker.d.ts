8828e163ec802b290796a507a84ad8748862b01c
Miško Hevery <misko@hevery.com>
2019-08-29 00:40:31 +0000

    4b1251106 build: bump yarn requirement to 1.17.3 (#32344)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+15.sha-a1e91b0.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+16.sha-4b12511.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
