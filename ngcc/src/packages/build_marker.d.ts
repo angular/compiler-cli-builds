8f0329206a849b4a300ca50051ace61e1049e29a
Miško Hevery <misko@hevery.com>
2019-08-29 00:31:55 +0000

    60a056d5d refactor(core): undecorated classes migration should not decorate classes if not needed (#32319)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+12.sha-581b837.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+11.sha-60a056d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
