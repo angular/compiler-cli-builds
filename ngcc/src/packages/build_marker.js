673cbced60b495e821f2d7d23b5596da3ec8ba90
Miško Hevery <misko@hevery.com>
2019-08-29 00:31:36 +0000

    581b837e8 perf(ivy): remove repeated memory read / write in addComponentLogic (#32339)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.4+9.sha-d0f3539.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.4+12.sha-581b837.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
