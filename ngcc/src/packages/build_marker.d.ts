34391d7d90e1eeb9f99f17909e6c10154f4c14c9
Miško Hevery <misko@hevery.com>
2019-08-29 19:57:24 +0000

    266c267b6 test: export TableModule in benchmarks (#32404)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+33.sha-b4d3468.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+32.sha-266c267.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.4+33.sha-b4d3468.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.4+32.sha-266c267.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
