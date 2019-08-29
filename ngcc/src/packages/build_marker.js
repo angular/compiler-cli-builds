7134250ebfdcf8ce5762af2eeb0707b442d94a06
Miško Hevery <misko@hevery.com>
2019-08-29 00:39:01 +0000

    4b1251106 build: bump yarn requirement to 1.17.3 (#32344)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.4+15.sha-a1e91b0.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.4+16.sha-4b12511.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
