45dd6cf5541860f531d97b657fcde7b3ee482f57
Miško Hevery <misko@hevery.com>
2019-08-29 19:48:19 +0000

    45c909a23 ci: temporarily disable chrome mobile tests on android (#32403)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.4+2.sha-5b76b93.with-local-changes";
+export declare const NGCC_VERSION = "8.2.4+3.sha-45c909a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
