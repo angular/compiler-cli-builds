a6550ecc96ec32e437578bed35c4833a876993a6
Miško Hevery <misko@hevery.com>
2019-08-29 00:32:19 +0000

    a1e91b00d perf(ivy): remove renderStringify calls for text nodes creation (#32342)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+11.sha-60a056d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+15.sha-a1e91b0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
