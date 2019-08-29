3b21292ae559cbbe6b615e3784850a902a297eeb
Miško Hevery <misko@hevery.com>
2019-08-29 00:46:00 +0000

    97fc45f32 fix(language-service): Invalidate Reflector caches when program changes (#32357)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+19.sha-d4703d9.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+17.sha-97fc45f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
