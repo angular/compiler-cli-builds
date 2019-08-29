0f4f27b3618079b0fe38ed3f5227bc6b14c4bf32
Miško Hevery <misko@hevery.com>
2019-08-29 19:55:30 +0000

    4c5cfecc5 fix: update all files/directories owned by DevInfra to new DevInfra Framework team alias (#32247)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.4+3.sha-45c909a.with-local-changes";
+export declare const NGCC_VERSION = "8.2.4+6.sha-4c5cfec.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
