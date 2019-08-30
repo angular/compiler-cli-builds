9d51adc24a3b8a3ae23c5db728d0bd7650cc0171
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:15:19 +0000

    ba5e07efc perf(ivy): add a micro benchmark for map-based style and class bindings (#32401)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+48.sha-9bbe67f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+46.sha-ba5e07e.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
