f0e2376acc3995c33d3d5e63a7b563f4822acd84
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:54:31 +0000

    ad13520b2 docs: create Issue and Pull Request markdown doc, explaining automatic locking policy (#32405)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+50.sha-350ea47.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+51.sha-ad13520.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
