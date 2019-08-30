27600065db9f0c3a80ecc5cbee41a8dab8a881e6
Misko Hevery <misko@hevery.com>
2019-08-30 05:09:12 +0000

    1537791f0 perf(core): Make `PlatformLocation` tree-shakable (#32154)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+39.sha-3758978.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+44.sha-1537791.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
