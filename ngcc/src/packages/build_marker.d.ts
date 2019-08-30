f97ae45dbebef3aa63fc24bacb8ed7c75622203a
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:13:55 +0000

    8c388e373 build(docs-infra): upgrade cli command docs sources to d68a96224 (#32413)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+44.sha-1537791.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+49.sha-8c388e3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
