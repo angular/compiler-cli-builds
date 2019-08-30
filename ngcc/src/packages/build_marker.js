7ceab796405c9454f44aec8e99478b4f7cf421ca
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:14:47 +0000

    9bbe67f28 docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.4+49.sha-8c388e3.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.4+48.sha-9bbe67f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
