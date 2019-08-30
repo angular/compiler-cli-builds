b15314756cbdcb1d7f3cea4bac6be1d860553447
Misko Hevery <misko@hevery.com>
2019-08-30 20:11:52 +0000

    60f963986 ci: update codeowners for docs guides/examples/images (#32360)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+56.sha-c56c241.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+59.sha-60f9639.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
