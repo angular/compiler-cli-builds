978db714fca8f5a5112301e10524d07b1b5b2f65
Misko Hevery <misko@hevery.com>
2019-09-03 18:51:52 +0000

    f5bec3ff5 feat(common): HttpParams fromObject accepts ReadonlyArray<string> (#31072)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+61.sha-260217a.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+70.sha-f5bec3f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
