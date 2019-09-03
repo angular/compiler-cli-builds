e90f03dc0cbf0ffedb1c910a7d7e661c92975a3b
Miško Hevery <misko@hevery.com>
2019-09-03 19:20:52 +0000

    e8f9ba4b6 fix(ivy): add missing closure extern for \$localize (#32460)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+73.sha-a20fcbb.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+71.sha-e8f9ba4.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
