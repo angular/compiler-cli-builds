65b06f98b7f15b04a1f53e8dc476e669f2ebc944
Miško Hevery <misko@hevery.com>
2019-09-04 18:57:45 +0000

    cfa09b84d docs: fix typo in structural directive example (#32459) Fixes #32458

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.4+80.sha-e213080.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.4+81.sha-cfa09b8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
