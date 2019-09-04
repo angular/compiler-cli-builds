c78611f746869746f14e6c97f875e64b8f734f96
Miško Hevery <misko@hevery.com>
2019-09-04 18:54:57 +0000

    e213080c0 docs: add app-shell production reference (#32471)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.4+79.sha-04d4fea.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.4+80.sha-e213080.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
