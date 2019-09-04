9aca1a46f90c3ed0d2701b4e6c6468f3de132a6f
Miško Hevery <misko@hevery.com>
2019-09-04 19:07:47 +0000

    5112669d2 refactor(benchpress): remove two mutable exports from largetable/util.ts (#32425)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.4+18.sha-3d453fe.with-local-changes";
+export declare const NGCC_VERSION = "8.2.4+19.sha-5112669.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
