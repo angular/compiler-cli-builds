bf973f958368fc82f8b25d94686255b4e6a47021
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:32:23 +0000

    da42a7648 fix(ivy): node placed in incorrect order inside ngFor with ng-container (#32324)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+18.sha-098feec.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+16.sha-da42a76.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
