667cccd0cdad8ce9b919bfb3d2f49ded1ca6b8e0
Miško Hevery <misko@hevery.com>
2019-09-05 00:43:10 +0000

    094538c0c feat(service-worker): recover from `EXISTING_CLIENTS_ONLY` mode when there is a valid update (#31865)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+2.sha-a731119.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+6.sha-094538c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
