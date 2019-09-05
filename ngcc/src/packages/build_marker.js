4897bc6fb7a99510835f64d4e2371e10eb7a52a7
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:59 +0000

    c426a2595 docs: add Alexey Zuev to GDE resources (#32440)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.5+3.sha-896a8a4.with-local-changes';
+    exports.NGCC_VERSION = '8.2.5+4.sha-c426a25.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
