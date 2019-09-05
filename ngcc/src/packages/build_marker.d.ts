d6686b1d3d4495dd3a7c0df455d8964c2d53dcd1
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:29:24 +0000

    62d92f809 fix(ivy): unable to bind to properties that start with class or style (#32421)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+20.sha-497d6b1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+19.sha-62d92f8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
