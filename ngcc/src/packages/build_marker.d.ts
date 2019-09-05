08dbf021fcb4a5c5ad3c5394dd1e610ff05af94e
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:56:32 +0000

    d4003452c fix(docs-infra): do not include GitHub links in Table of Content (#32418)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+10.sha-e505fa6.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+8.sha-d400345.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
