4661c878a09634419470f4908bf6efbfb4405c4f
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:52:46 +0000

    be30f25da docs: update default path for xi18n (#32480)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.4+20.sha-8be20f3.with-local-changes";
+export declare const NGCC_VERSION = "8.2.5+5.sha-be30f25.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
