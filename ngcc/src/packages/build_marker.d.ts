721f72ca604986dce2a49f2158d1b4863813ebed
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:30:27 +0000

    497d6b132 ci: re-run flaky docs examples e2e tests in `test_docs_examples[_ivy]` jobs (#32497)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+14.sha-fed6b25.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+20.sha-497d6b1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
