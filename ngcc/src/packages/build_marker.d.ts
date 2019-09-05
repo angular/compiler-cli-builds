301847b97a4a4236b2b12637b49bbe1d6fd269b7
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 18:46:29 +0000

    7bbc3523b ci: set up angular-automatic-lock-bot for the repo (#32500)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+12.sha-f00d033.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+13.sha-7bbc352.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
