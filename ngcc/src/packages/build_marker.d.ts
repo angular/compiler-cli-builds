7b255af11c115298427db00e705177c4a704a409
Matias Niemelä <matias@yearofmoo.com>
2019-09-06 19:17:45 +0000

    e742edca7 docs: add akita to the list of data libraries (#32505)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.5+7.sha-8304343.with-local-changes";
+export declare const NGCC_VERSION = "8.2.5+8.sha-e742edc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
