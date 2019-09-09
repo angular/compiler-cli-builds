26943982640a8ed94ff7ce903721e7b66efa49e2
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 16:41:18 +0000

    a65d3fa1d fix(language-service): Return empty external files during project initialization (#32519)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.5+26.sha-df5924a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.5+28.sha-a65d3fa.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
