19d2642133b6fe5fcb172e6a8885fefafcf69bf6
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:27:37 +0000

    e38351fce docs: fixed typo for toh-pt5.md (#32362)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.5+58.sha-f00bb85.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.5+59.sha-e38351f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
