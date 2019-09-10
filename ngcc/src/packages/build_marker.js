77715a644eabd59ce92912b32d19fb8d564358a1
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 22:13:21 +0000

    a813ae07e test: make expanding_rows test work in g3 (#32568)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.5+72.sha-6ccb4e0.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.5+73.sha-a813ae0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
