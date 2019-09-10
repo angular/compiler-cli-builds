88f8be187fe0e5bc2a8ef77d299a43ebefc32481
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 12:16:41 +0000

    5b2408f0a docs: fix typos in the routing and testing documentation (#32329)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.5+55.sha-6052b12.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.5+57.sha-5b2408f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
