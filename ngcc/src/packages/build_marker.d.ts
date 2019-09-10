3d2f747387af94987e71a8f6dc500f919f820ae6
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 12:17:42 +0000

    39302ba92 docs: fix typos in the routing and testing documentation (#32329)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.5+13.sha-1c71db8.with-local-changes";
+export declare const NGCC_VERSION = "8.2.5+14.sha-39302ba.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
