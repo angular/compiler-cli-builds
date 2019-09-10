6908e0fb192e0869bde537c502630cf919a9fffe
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 19:11:32 +0000

    21245887e fix(ivy): unable to override ComponentFactoryResolver provider in tests (#32512)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.5+62.sha-7280710.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.5+63.sha-2124588.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
