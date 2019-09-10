0e8f5be0315b4cf3bddb124bdbf011a3b4aecf8b
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 20:16:39 +0000

    3b3746973 refactor(ivy): remove styling state storage and introduce direct style writing (#32259)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.5+67.sha-8a7129e.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.5+68.sha-3b37469.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
