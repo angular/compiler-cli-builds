1c2f132b489a458634f0c73d5949d30d5af9468c
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 16:54:43 +0000

    1775498b4 ci: pin docker images by ID for hermeticity (#32602)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.5+18.sha-2565f67.with-local-changes";
+export declare const NGCC_VERSION = "8.2.5+22.sha-1775498.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
