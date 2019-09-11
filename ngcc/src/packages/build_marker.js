b7eee79c73967a0d3bd86813e8cd5376dcc716dd
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:39:53 +0000

    4f033235b perf(language-service): keep analyzedModules cache when source files don't change (#32562)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+6.sha-f5982fd.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+5.sha-4f03323.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
