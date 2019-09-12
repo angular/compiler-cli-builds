d21bf79e20160eaf22bceaa8215446190522f72d
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:49:13 +0000

    5f095a501 fix(core): initialize global ngDevMode without toplevel side effects (#32079)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+14.sha-a85eccd.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+15.sha-5f095a5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
