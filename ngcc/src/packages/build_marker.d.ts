208b4476941c48a79f5e7188f227ec8f47f9a43c
Kara Erickson <karakara@google.com>
2019-09-12 17:45:25 +0000

    8d322c89b docs: modify lazy-load module instructions for new cli flag (#32588)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.6+5.sha-31f06ee.with-local-changes";
+export declare const NGCC_VERSION = "8.2.6+7.sha-8d322c8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.6+5.sha-31f06ee.with-local-changes';
+    exports.NGCC_VERSION = '8.2.6+7.sha-8d322c8.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
