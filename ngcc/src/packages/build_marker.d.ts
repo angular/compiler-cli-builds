0cf3465788ac2b6c1c68a2d8d36e1fed77e680fa
Kara Erickson <karakara@google.com>
2019-09-12 23:45:30 +0000

    2bf5606bb feat(ivy): i18n - reorganize entry-points for better reuse (#32488)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+40.sha-e82f56b.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+41.sha-2bf5606.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
