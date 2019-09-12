7252153ecee24bee68372b544976ba84029aeed6
Kara Erickson <karakara@google.com>
2019-09-12 17:42:53 +0000

    eab959aa1 build(docs-infra): upgrade cli command docs sources to 5d72355fc (#32634)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+18.sha-0a0489d.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+22.sha-eab959a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
