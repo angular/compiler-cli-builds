056d4b9ed623fe31e5934b9068ce3570063c1dda
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:43:06 +0000

    08a7c6f0b ci: update codeowners to reflect GitHub team members (#32569)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.6+4.sha-1c5b157.with-local-changes";
+export declare const NGCC_VERSION = "8.2.6+6.sha-08a7c6f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
