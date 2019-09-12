863fe89a0ecac5f8b7284fc6bdcd98301e141444
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:50:40 +0000

    5f095a501 fix(core): initialize global ngDevMode without toplevel side effects (#32079)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+14.sha-a85eccd.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+15.sha-5f095a5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
