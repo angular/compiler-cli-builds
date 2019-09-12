cf1f22cf3e5c33fdc7c1e46dd98df4e2dd6f63c1
Kara Erickson <karakara@google.com>
2019-09-12 20:39:52 +0000

    3c7da767d fix(ngcc): resolve imports in `.d.ts` files for UMD/CommonJS bundles (#32619)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+37.sha-5a830c4.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+39.sha-3c7da76.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
