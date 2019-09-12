baa39573c2bf43df0985c6981c0cf9ead6ff8f45
Kara Erickson <karakara@google.com>
2019-09-12 17:52:36 +0000

    1771d6ff2 fix(language-service): Lazily instantiate MetadataResolver (#32631)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+27.sha-527ce3b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+21.sha-1771d6f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
