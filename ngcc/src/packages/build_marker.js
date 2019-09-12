3cf7c2b49d3b99dad8ddd0950948e053731f1da4
Kara Erickson <karakara@google.com>
2019-09-12 20:25:45 +0000

    5a830c49c refactor(ivy): improve micro-benchmark profiling (#32647)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+36.sha-bd67958.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+37.sha-5a830c4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
