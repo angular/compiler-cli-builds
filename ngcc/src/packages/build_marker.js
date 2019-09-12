b8829713a7004d06d88ae98b100eedf8601252ae
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:40:07 +0000

    1c5b157f1 fix(language-service): cache module resolution (#32483)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.6+3.sha-38fe473.with-local-changes';
+    exports.NGCC_VERSION = '8.2.6+4.sha-1c5b157.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
