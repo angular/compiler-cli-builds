29c77ca7a80a61b78e44b5251f48f3665982efd7
Kara Erickson <karakara@google.com>
2019-09-12 17:44:21 +0000

    eab959aa1 build(docs-infra): upgrade cli command docs sources to 5d72355fc (#32634)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+18.sha-0a0489d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+22.sha-eab959a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
