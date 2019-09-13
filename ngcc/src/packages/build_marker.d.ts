935ec30a2a9310132fd87bdd4e9e341592f6988b
Kara Erickson <karakara@google.com>
2019-09-13 16:11:00 +0000

    c442c0334 refactor(language-service): remove non-null assertion on MockHost member (#32666)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+42.sha-bbb2798.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+44.sha-c442c03.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
