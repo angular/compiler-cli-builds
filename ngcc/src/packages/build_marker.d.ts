842172efddf904100ad4cfb267ffc115b219d690
Kara Erickson <karakara@google.com>
2019-09-13 16:17:46 +0000

    e1065eec5 refactor(language-service): let -> const (#32668)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+45.sha-082b0d3.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+46.sha-e1065ee.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
