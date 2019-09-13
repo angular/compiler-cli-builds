e6393517ba4922ac5d2b0620797a45cd5ca72398
Kara Erickson <karakara@google.com>
2019-09-13 17:34:23 +0000

    bfb399586 fix(ivy): DebugNode throws exceptions when querying some properties (#32622)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+48.sha-5328bb2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+49.sha-bfb3995.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
