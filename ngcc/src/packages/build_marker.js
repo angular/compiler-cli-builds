a69f69828c2d150fd8dd6fb367667828c273dfe5
Kara Erickson <karakara@google.com>
2019-09-13 17:21:33 +0000

    5328bb223 fix(ivy): avoid unnecessary i18n instructions generation for <ng-template> with structural directives (#32623)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+47.sha-0477bfc.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+48.sha-5328bb2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
