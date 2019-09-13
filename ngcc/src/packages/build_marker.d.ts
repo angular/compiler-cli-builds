a9fae62dbc10513d92cf8be3ea9bfc97374c0e63
Kara Erickson <karakara@google.com>
2019-09-13 16:15:47 +0000

    6cd61aeb1 ci: add @josephperrott to global approvers in `CODEOWNERS` (#32667)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.6+10.sha-581b991.with-local-changes";
+export declare const NGCC_VERSION = "8.2.6+11.sha-6cd61ae.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
