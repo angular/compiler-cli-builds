78b7fae5a286b2a8757148dcb627e7efa01781bf
Kara Erickson <karakara@google.com>
2019-09-13 16:09:28 +0000

    c442c0334 refactor(language-service): remove non-null assertion on MockHost member (#32666)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+42.sha-bbb2798.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+44.sha-c442c03.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
