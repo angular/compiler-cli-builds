b79a94e2f1104ae4e9ed50c7c529e75381e01a6b
Kara Erickson <karakara@google.com>
2019-09-16 18:07:19 +0000

    f213c7a64 docs: add autoprefixer backward compatability instruction (#32109)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.6+13.sha-947c076.with-local-changes";
+export declare const NGCC_VERSION = "8.2.6+14.sha-f213c7a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.6+13.sha-947c076.with-local-changes';
+    exports.NGCC_VERSION = '8.2.6+14.sha-f213c7a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
