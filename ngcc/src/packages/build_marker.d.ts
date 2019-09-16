29f5a057871eb214ee677266e37e2768c72db769
Kara Erickson <karakara@google.com>
2019-09-16 16:26:34 +0000

    f6d66671b fix(compiler-cli): fix typo in diagnostic template info. (#32684)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+53.sha-3a90a7c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+54.sha-f6d6667.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
