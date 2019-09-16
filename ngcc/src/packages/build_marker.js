2f008f049a1d6259e792348429f0f96ba8802de9
Kara Erickson <karakara@google.com>
2019-09-16 16:56:26 +0000

    52a6da043 fix(ivy): correct debug array names (#32691)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+55.sha-1748aeb.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+56.sha-52a6da0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
