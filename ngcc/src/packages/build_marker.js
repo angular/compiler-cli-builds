cf5609ea6b62cabf4a2817c13eccadb7a4ae029b
Kara Erickson <karakara@google.com>
2019-09-16 16:52:59 +0000

    1748aeb9c perf(ivy): convert all node-based benchmark to use a testing harness (#32699)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+54.sha-f6d6667.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.6+55.sha-1748aeb.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
