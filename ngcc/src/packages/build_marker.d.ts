870346ed1a9f17bbd6d9f0415e0290c8882e0935
Andrew Kushnir <akushnir@google.com>
2019-09-17 16:42:57 +0000

    b741a1c3e fix(ivy): i18n - update the compiler to output `MessageId`s (#32594)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+66.sha-e5a3de5.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+71.sha-b741a1c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
