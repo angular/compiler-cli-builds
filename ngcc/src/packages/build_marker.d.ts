eed524caf238564ca966e5767511ea59b7fcf7c5
Andrew Kushnir <akushnir@google.com>
2019-09-17 20:51:55 +0000

    a0d04c628 fix(ivy): ensure that `window.ng` utilities are published when NgModules are used (#32725)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+71.sha-b741a1c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+72.sha-a0d04c6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
