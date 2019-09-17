8a0de36eee4a49f3c32c901404a5bf9231e3c08d
Andrew Kushnir <akushnir@google.com>
2019-09-17 22:34:01 +0000

    acebf6464 ci: build bazel saucelabs tests remotely (#32719)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.6+16.sha-e367aa2.with-local-changes";
+export declare const NGCC_VERSION = "8.2.6+17.sha-acebf64.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
