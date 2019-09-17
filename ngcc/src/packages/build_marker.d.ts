ed032c1cf738111900dc55a0004d582fec37927e
Andrew Kushnir <akushnir@google.com>
2019-09-17 23:25:33 +0000

    df1c45634 fix(bazel): ng_package(data) should support non-text files (#32721)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.6+77.sha-4726ac2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.6+80.sha-df1c456.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
