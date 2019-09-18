ea81a14872d0f555ad7fe1bfc684a56da989aa7d
Andrew Kushnir <akushnir@google.com>
2019-09-18 18:22:56 +0000

    ab29874f0 release: cut the v9.0.0-next.7 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.6+81.sha-9b0861c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
