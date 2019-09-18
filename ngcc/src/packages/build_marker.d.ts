87bfa909bdf8647d95c3db5a07f3ab61d6bb506e
Andrew Kushnir <akushnir@google.com>
2019-09-18 20:29:25 +0000

    9d8dc793d test(language-service): Create proper test project (#32653)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.7+1.sha-2846505.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.7+2.sha-9d8dc79.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
