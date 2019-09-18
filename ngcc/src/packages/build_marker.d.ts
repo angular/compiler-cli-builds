fd72e16c20377ecf55be830f8b2352f4cb207380
Andrew Kushnir <akushnir@google.com>
2019-09-18 22:29:16 +0000

    0450f3962 refactor(ivy): move all styling util code into `utils/styling_utils.ts` (#32731)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.7+5.sha-708ae4c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.7+10.sha-0450f39.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
