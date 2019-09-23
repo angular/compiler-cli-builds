f6562af0305ec27eb2be5f0c2eaf3b912431262a
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:55:06 +0000

    52552b052 perf(ivy): remove extra SafeStyle detection code (#32775)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.7+15.sha-5651fa3.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.7+16.sha-52552b0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
