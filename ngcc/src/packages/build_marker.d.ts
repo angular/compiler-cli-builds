69de0c24d8143e038c774c06a3e58ae2dbc4ba42
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:52:11 +0000

    5651fa3a9 fix(ivy): ensure `window.ng.getDebugNode` returns debug info for component elements (#32780)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.7+14.sha-353368c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.7+15.sha-5651fa3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
