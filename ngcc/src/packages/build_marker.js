53be559ff8f1a74635f6126382a46c7c1c0df2e6
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:46:29 +0000

    353368ccc docs: correct default for router extra option (#32702)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.7+13.sha-7a00c67.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.7+14.sha-353368c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
