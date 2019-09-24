7062e480b3df0efa230d78ea490add4c4d2e2e49
Andrew Kushnir <akushnir@google.com>
2019-09-24 21:19:35 +0000

    32b6c2285 docs: add comment markers for internal goog.define replacement (#32795)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.7+19.sha-86fd571.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.7+20.sha-32b6c22.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
