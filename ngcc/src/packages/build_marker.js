666832a36a10e1a13de3bfc13d44d28a9f51be73
Andrew Kushnir <akushnir@google.com>
2019-09-25 18:46:36 +0000

    c8be987b4 fix(ivy): ensure TestBed restores fields to the most original value (#32823)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -10,7 +10,7 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
-    exports.NGCC_VERSION = '9.0.0-next.8.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+3.sha-c8be987.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
