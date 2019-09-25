079424439581fb269d1d16afb3fc0716b9f7f2ad
Andrew Kushnir <akushnir@google.com>
2019-09-25 17:49:06 +0000

    4dddee742 docs: release notes for the v8.2.8 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.7+25.sha-39bc6f7.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.7+26.sha-4dddee7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
