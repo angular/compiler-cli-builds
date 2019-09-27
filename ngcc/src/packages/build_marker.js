c3b90a2eb59a7f3c4d20fc3c039058c23b1b8fd1
atscott <atscott@google.com>
2019-09-27 17:51:05 +0000

    5ae48d52e docs: fix typo in deployment guide (#32777)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+14.sha-966c2a3.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+15.sha-5ae48d5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
