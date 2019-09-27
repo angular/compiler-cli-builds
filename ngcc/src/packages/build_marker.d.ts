cbea9f2dc56d6956fc442ad385eb2120c0ee19a3
atscott <atscott@google.com>
2019-09-27 18:57:41 +0000

    84feda1db build(docs-infra): do not ngcc the 'module' entry points (#32679)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.8+15.sha-5ae48d5.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.8+17.sha-84feda1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
