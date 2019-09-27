00286a5e06fb2d6233a5983a3b4598c7e93a483a
atscott <atscott@google.com>
2019-09-27 00:27:58 +0000

    85990c321 fix(docs-infra): correctly run on IE11 (#32871)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+8.sha-fbad4ff.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+9.sha-85990c3.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
