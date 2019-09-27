27c08103c1e51ac07f3957a5d42071dd57e56611
atscott <atscott@google.com>
2019-09-27 00:26:33 +0000

    e81fc14da fix(docs-infra): correctly run on IE11 (#32871)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.8+2.sha-d5c210a.with-local-changes";
+export declare const NGCC_VERSION = "8.2.8+3.sha-e81fc14.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
