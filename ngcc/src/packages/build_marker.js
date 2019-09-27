320ce17661ff11bb0291f8eb54563cbad634548e
atscott <atscott@google.com>
2019-09-27 17:23:44 +0000

    a7d090d22 build(docs-infra): upgrade cli command docs sources to 0a36071b8 (#32878)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+11.sha-2e73028.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+10.sha-a7d090d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
