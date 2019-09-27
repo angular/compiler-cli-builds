843be5198116df69587cbb48dd29ea687b5c9b2c
atscott <atscott@google.com>
2019-09-27 21:40:37 +0000

    2397144e0 docs(router): make `InitialNavigation` part of the public API (#32707)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.8+7.sha-466c754.with-local-changes";
+export declare const NGCC_VERSION = "8.2.8+8.sha-2397144.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
