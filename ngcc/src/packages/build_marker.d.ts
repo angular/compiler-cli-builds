9c368d812617368a5173a93173107e4844bccfab
atscott <atscott@google.com>
2019-09-30 21:33:23 +0000

    0818e2c79 docs(service-worker): add info about recovering from degraded Driver state (#32682)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.8+13.sha-7b8b2d4.with-local-changes";
+export declare const NGCC_VERSION = "8.2.8+14.sha-0818e2c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.8+13.sha-7b8b2d4.with-local-changes';
+    exports.NGCC_VERSION = '8.2.8+14.sha-0818e2c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
