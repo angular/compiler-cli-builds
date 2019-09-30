ff3d38848480f6e0336b5cd03971569538dd0011
atscott <atscott@google.com>
2019-09-30 17:48:58 +0000

    f6526a087 docs: add filename headers to code examples (#32701)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.8+9.sha-bdea243.with-local-changes";
+export declare const NGCC_VERSION = "8.2.8+12.sha-f6526a0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
