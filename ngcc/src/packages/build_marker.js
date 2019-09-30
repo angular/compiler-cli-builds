08d0def80ef230221a7cdd85c8aa60d6f9c602a2
atscott <atscott@google.com>
2019-09-30 21:37:02 +0000

    45c893d0e docs(service-worker): add info about recovering from degraded Driver state (#32682)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+28.sha-53b32f1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+27.sha-45c893d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
