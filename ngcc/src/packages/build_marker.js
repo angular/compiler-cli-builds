5df0a41baaaa1d0cdc9399b252d5d85cded497f8
atscott <atscott@google.com>
2019-09-30 21:30:57 +0000

    002a97d85 fix(ngcc): ensure private exports are added for `ModuleWithProviders` (#32902)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+23.sha-f438ae8.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+24.sha-002a97d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
