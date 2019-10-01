f6391a735599b13306dc84befc1b9911688a9e36
atscott <atscott01@google.com>
2019-10-01 22:02:49 +0000

    42f741952 docs: emphasize headers for code examples (#32938)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.8+16.sha-06e7272.with-local-changes";
+export declare const NGCC_VERSION = "8.2.8+18.sha-42f7419.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
