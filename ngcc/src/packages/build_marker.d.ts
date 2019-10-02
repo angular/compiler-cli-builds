34492686570643c43970f940932013a99f44d578
atscott <atscott01@google.com>
2019-10-02 16:57:40 +0000

    948b01ce5 feat(core): default to dynamic queries (#32720)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.8+52.sha-25219ba.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.8+51.sha-948b01c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
