988ab4007cf74239a814c79f10715d386f1fb14f
atscott <atscott01@gmail.com>
2019-10-02 20:44:08 +0000

    3de59d48b docs(docs-infra): use recommended type assertion (#31042)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+4.sha-41ed694.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+6.sha-3de59d4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
