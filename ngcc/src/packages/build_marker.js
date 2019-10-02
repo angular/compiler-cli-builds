082cc86be8bc55d43882054f972c46ef46a0101a
atscott <atscott01@gmail.com>
2019-10-02 20:47:54 +0000

    e7729b686 docs: add comment about newEvent utility function (#32001)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+5.sha-3c7ac5f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+8.sha-e7729b6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
