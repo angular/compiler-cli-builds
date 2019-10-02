2d76bc443bcd0860e4a4ecb78bbf1430c1c60ce4
atscott <atscott01@gmail.com>
2019-10-02 20:49:18 +0000

    e7729b686 docs: add comment about newEvent utility function (#32001)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+5.sha-3c7ac5f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+8.sha-e7729b6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
