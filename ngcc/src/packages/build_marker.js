d6da82e35a7d7d6e74574c12b971b4151bfc615f
atscott <atscott01@google.com>
2019-10-02 16:53:46 +0000

    0f3a48e4d ci: refactor notification commands into a single parametrized one (#32745)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+43.sha-090732f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+50.sha-0f3a48e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
