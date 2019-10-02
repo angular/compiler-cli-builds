c35991857a2314190d234b8dd0bfd11221609b81
atscott <atscott01@gmail.com>
2019-10-02 21:52:30 +0000

    052cae642 docs: fix typo in attribute-directives.md (#32943)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+7.sha-0d186dd.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+9.sha-052cae6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
