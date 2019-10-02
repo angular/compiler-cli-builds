99247b9d33f8c87b309402e92847d191ef3bcc8f
atscott <atscott01@google.com>
2019-10-02 00:36:24 +0000

    63256b511 fix(ivy): Only restore registered modules if user compiles modules with TestBed (#32944)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.8+39.sha-66658c4.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.8+41.sha-63256b5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.8+39.sha-66658c4.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.8+41.sha-63256b5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
