e817ef8e698af532612d5da26917f6f17032140d
atscott <atscott01@gmail.com>
2019-10-02 22:11:54 +0000

    32e315755 test(ivy): add runtime translations to integration test app (#32867)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+9.sha-052cae6.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+15.sha-32e3157.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
