fdf41cb636bc59653fdcf8b3a29bb68a0e7ba82e
atscott <atscott01@gmail.com>
2019-10-02 20:42:41 +0000

    41ed694d0 ci: update size benchmarks for core with 9.0.0-next.8 (#32947)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+3.sha-954b8e5.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+4.sha-41ed694.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
