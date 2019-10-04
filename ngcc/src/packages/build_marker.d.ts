557b08c128aeb986fade2d58dce4c360c74332c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-04 19:07:39 +0000

    60047037a perf(ivy): attempt rendering initial styling only if present (#32979)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+41.sha-53d13c3.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+44.sha-6004703.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+41.sha-53d13c3.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+44.sha-6004703.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
