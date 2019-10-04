79e57978de7b70c910b2e9e694ac6d756b077111
atscott <atscott01@gmail.com>
2019-10-04 16:00:46 +0000

    442f323a3 ci: shorten the display name of `init_environment` steps that are shown in all jobs (#32998)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+39.sha-480b775.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+40.sha-442f323.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+39.sha-480b775.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+40.sha-442f323.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
