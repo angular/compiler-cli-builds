141e99cdefc10374fe0c6b206cf4a0fb081e16ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:44:06 +0000

    40d87dd80 docs: fix accessibility lint rules (#32661)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+62.sha-0ce8621.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+63.sha-40d87dd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+62.sha-0ce8621.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+63.sha-40d87dd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
