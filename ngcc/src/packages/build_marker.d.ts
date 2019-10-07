dd613572419f1b0a9e5173c5c3d03e5008673167
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 17:43:57 +0000

    d18289fa9 fix(ivy): missing schematics field in localize package (#33025)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+46.sha-3a53e2c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+52.sha-d18289f.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+46.sha-3a53e2c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+52.sha-d18289f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
