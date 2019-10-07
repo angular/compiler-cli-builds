5d075ece051f5d3cba0ec35821a837adf0890978
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:13:59 +0000

    393398e6f build(docs-infra): update in-memory-we-api and karma-jasmine-html-reporter version (#32892)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.9+55.sha-e007918.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.9+56.sha-393398e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
