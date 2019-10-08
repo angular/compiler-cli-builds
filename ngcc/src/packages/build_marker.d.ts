f4fabad2a24ce284de8df25de19089a429fbd4b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 16:48:45 +0000

    2265cb593 refactor(core): remove deprecated Renderer (#33019)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+64.sha-c507dda.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+65.sha-2265cb5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
