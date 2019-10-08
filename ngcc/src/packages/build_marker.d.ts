fdd679e8b71d0341ee805f9c8503ef8ae87596fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 17:46:12 +0000

    c653a16b5 build: load build_bazel_rules_nodejs through external (#33041)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+71.sha-64823f5.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+72.sha-c653a16.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
