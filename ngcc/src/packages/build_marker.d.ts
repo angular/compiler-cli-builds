1d996cbdf438de3e1b97c9f0589e31f6d9baa40b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-09 21:01:22 +0000

    305f3686c build: fix compilation error in benchmark (#33067)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.9+76.sha-b2b917d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.9+81.sha-305f368.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
