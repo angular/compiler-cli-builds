65fa809672a0cc3a0bd83031552528bb42b68627
Miško Hevery <misko@hevery.com>
2019-10-10 21:34:29 +0000

    f640a4a49 fix(ivy): i18n - turn on legacy message-id support by default (#33053)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+21.sha-728cd84.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+20.sha-f640a4a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
