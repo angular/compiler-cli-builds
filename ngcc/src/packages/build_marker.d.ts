fa64c5cad90e8ab604ea89ae66e708ec79c7399c
Miško Hevery <misko@hevery.com>
2019-10-10 22:55:19 +0000

    9c153cfb3 docs(router): clarify that createUrlTree only uses some NavigationExtras (#33029)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+34.sha-b934898.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+33.sha-9c153cf.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
