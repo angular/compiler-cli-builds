14b1e7ec98717adcc9bd641f37250a2ff268c5bf
Miško Hevery <misko@hevery.com>
2019-10-10 21:24:20 +0000

    b2decf026 perf(ivy): speed up bindings when no directives are present (#32919)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+2.sha-a3ef3e1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+17.sha-b2decf0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
