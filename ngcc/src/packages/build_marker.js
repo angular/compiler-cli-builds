7a0820dce3081dd237a4ad3e590ea7b9cb83f7e7
Miško Hevery <misko@hevery.com>
2019-10-10 22:44:51 +0000

    f54adf10b refactor(forms): replace instanceof Array (#33078)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+29.sha-b0834fe.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+30.sha-f54adf1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
