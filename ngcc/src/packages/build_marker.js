ad8b738881bc333cd642565822df0f2cfbd64e63
Miško Hevery <misko@hevery.com>
2019-10-10 21:36:49 +0000

    5ede5b780 ci: run bazel for package-builder via yarn rather than binary directly (#32983)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+20.sha-f640a4a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+19.sha-5ede5b7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
