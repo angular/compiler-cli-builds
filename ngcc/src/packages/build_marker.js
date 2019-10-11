373f13766347a0ee475edc7dff1aa8d6d80f0190
Miško Hevery <misko@hevery.com>
2019-10-11 23:03:30 +0000

    d4d07233d perf(ivy): guard host binding execution with a TNode flag (#33102)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+37.sha-15e3b5f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+41.sha-d4d0723.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
