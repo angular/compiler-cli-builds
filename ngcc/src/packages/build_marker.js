3641cc2471700e53565f0ddeefe660066a0b8427
Miško Hevery <misko@hevery.com>
2019-10-11 23:04:24 +0000

    64fd0d6db refactor(core): rename ngComponentDef to ɵcmp (#33088)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+43.sha-c88305d.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+42.sha-64fd0d6.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
