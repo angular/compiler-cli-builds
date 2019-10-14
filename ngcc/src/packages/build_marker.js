682854c4c002d4694d659af0e0cb9c7628109aad
Miško Hevery <misko@hevery.com>
2019-10-14 23:58:52 +0000

    64aae3a9d test(language-service): test project cleanup (#33157)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+79.sha-10dcbcf.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+77.sha-64aae3a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
