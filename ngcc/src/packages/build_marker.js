514e143dca5f8208349c84b78da1bab1ac3e27b2
Miško Hevery <misko@hevery.com>
2019-10-14 23:27:49 +0000

    fc93dafab refactor(core): rename ngModuleDef to ɵmod (#33142)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+70.sha-c3aaa52.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+75.sha-fc93daf.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
