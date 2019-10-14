b2c1b4143b8c601996fa09e56c7e5541f916722e
Miško Hevery <misko@hevery.com>
2019-10-14 16:43:58 +0000

    1a67d70bf refactor(core): rename ngDirectiveDef to ɵdir (#33110)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+46.sha-84ba1f0.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+45.sha-1a67d70.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
