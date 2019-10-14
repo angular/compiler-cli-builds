03e7c0953f42f315bd33673bae264f1f23a71585
Miško Hevery <misko@hevery.com>
2019-10-14 16:50:48 +0000

    127cec021 docs: add command for creating angular workspace (#32804)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+50.sha-4b30108.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+51.sha-127cec0.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
