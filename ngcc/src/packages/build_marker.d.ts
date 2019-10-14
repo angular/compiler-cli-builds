afd79a8e7f0b703e250003237c95a08ae6bfeda3
Miško Hevery <misko@hevery.com>
2019-10-14 23:59:40 +0000

    5265b7413 docs: fix typo in guide (#33155)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+76.sha-4c0726d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+78.sha-5265b74.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
