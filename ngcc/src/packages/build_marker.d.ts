0ab73a7fce9bb2f554552d3b3f2aae5ed4525d3f
Miško Hevery <misko@hevery.com>
2019-10-14 16:55:56 +0000

    868b3f946 test(ivy): i18n - run integration test in es2015 mode (#33097)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+56.sha-e2d5bc2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+55.sha-868b3f9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
