0754a50cdb34113f598e1cda3cd70b0c5ca06585
Miško Hevery <misko@hevery.com>
2019-10-14 16:48:11 +0000

    1a34fbce2 fix(ngcc): rename the executable from `ivy-ngcc` to `ngcc` (#33140)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+49.sha-25af147.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+48.sha-1a34fbc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
