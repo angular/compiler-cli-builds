1cba4265fcd15c900a07b85b00722c866b746d45
Miško Hevery <misko@hevery.com>
2019-10-14 20:34:48 +0000

    b04488d69 feat(compiler): record absolute span of template expressions in parser (#31897)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+57.sha-68f06c8.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+58.sha-b04488d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
