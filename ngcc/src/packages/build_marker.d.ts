987d04a03e16256736d0b3d60021ebc7f0485216
Miško Hevery <misko@hevery.com>
2019-10-14 21:02:46 +0000

    f433d6604 feat(ivy): i18n - support source locale inlining (#33101)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+66.sha-cd7b199.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+73.sha-f433d66.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
