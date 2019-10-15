0ead8033db4506c33135e81d71ecb0fdbd54ebbf
Miško Hevery <misko@hevery.com>
2019-10-15 21:14:47 +0000

    ec6a9f2a0 docs: add postinstall script removal to Ivy guide (#32999)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+84.sha-8b0cb2f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+93.sha-ec6a9f2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
