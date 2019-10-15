af02537b64688d7ac5489d9ab750af49bc0ec137
Miško Hevery <misko@hevery.com>
2019-10-15 21:16:21 +0000

    ec6a9f2a0 docs: add postinstall script removal to Ivy guide (#32999)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.10+84.sha-8b0cb2f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.10+93.sha-ec6a9f2.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
