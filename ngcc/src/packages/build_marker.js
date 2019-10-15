5b3303270c18733f7efd5ec95756f9a17d18b09b
Miško Hevery <misko@hevery.com>
2019-10-15 21:29:47 +0000

    ad72c9044 fix(ivy): i18n - add XLIFF aliases for legacy message id support (#33160)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.10+94.sha-11e04b1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.10+95.sha-ad72c90.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
