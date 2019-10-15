bdaf19af07aa76a3a84982d8225a156cf97fd580
Miško Hevery <misko@hevery.com>
2019-10-15 21:29:57 +0000

    b71ccc295 docs: fix JDK url (#33168)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.10+13.sha-9409dce.with-local-changes";
+export declare const NGCC_VERSION = "8.2.10+18.sha-b71ccc2.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
