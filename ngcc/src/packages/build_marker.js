514a37cc88e4027a76381811e5eae15dacec4ca7
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 17:13:04 +0000

    4c63e6ba0 build: remove vendored Babel typings (#33176)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.11+4.sha-4659cc2.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.11+5.sha-4c63e6b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
