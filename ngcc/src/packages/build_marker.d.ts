00cad2bcc5ad05a0ba0f9cc12ff7a15629847c7f
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:09:13 +0000

    becd62d4a fix(upgrade): remove unused version export (#33180)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+10.sha-86104b8.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+11.sha-becd62d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
