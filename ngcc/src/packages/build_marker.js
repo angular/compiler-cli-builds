da2be746db88fb83af9e1754442d675638f068e9
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 15:15:34 +0000

    4659cc26e style: emove unreachable 'return null' statement (#33174)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.11+3.sha-67c9148.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.11+4.sha-4659cc2.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
