6c1653bfb4404a605f470c10c1661d9656dff507
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:30:26 +0000

    cc7c2a81d build: update webdriver-manager to support latest Chrome (#33206)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.11+25.sha-3dc6490.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.11+30.sha-cc7c2a8.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
