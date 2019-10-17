af46322feb0abedbc417aab35424293defbb5518
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 21:05:06 +0000

    f45c43188 fix(ivy): ensure errors are thrown during checkNoChanges for style/class bindings (#33103)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+43.sha-9d54679.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+44.sha-f45c431.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
