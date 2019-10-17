54ac01641fe70acde6462106b511b5ba8d054ca4
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:35:00 +0000

    9d54679e6 test: clean up explicit dynamic query usages (#33015)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+42.sha-7e64bbe.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+43.sha-9d54679.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
