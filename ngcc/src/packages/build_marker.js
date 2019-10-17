cce685c7e7ade4e2b8e147768bdc39b97a3085c6
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:34:01 +0000

    4800fa1c0 refactor(core): tweek micro-benchmarks to make them more consistent (#33207)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.11+26.sha-0e260d2.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.11+31.sha-4800fa1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
