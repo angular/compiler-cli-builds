df444d3ac653c7d820dd7b819b57f188ee3cecac
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:33:34 +0000

    7e64bbe5a fix(ivy): use container i18n meta if a message is a single ICU (#33191)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+41.sha-1a8bd22.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+42.sha-7e64bbe.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
