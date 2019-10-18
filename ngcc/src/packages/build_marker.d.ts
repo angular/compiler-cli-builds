1994edfc7030a88e59be08732d1dc25d01190907
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 20:49:11 +0000

    d9581bd6a4 ci: fix dev-infra incorrectly matching all ".bzl" files as codeowner (#33249)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.11+8.sha-19ada81.with-local-changes";
+export declare const NGCC_VERSION = "8.2.11+9.sha-d9581bd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.11+8.sha-19ada81.with-local-changes';
+    exports.NGCC_VERSION = '8.2.11+9.sha-d9581bd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
