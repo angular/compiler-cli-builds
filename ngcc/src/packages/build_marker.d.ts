700eb144daa267b03d9fe493cee59de138ffd912
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:08:26 +0000

    e0059c7d51 refactor: format files (#31248)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+65.sha-6958d11.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+74.sha-e0059c7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
