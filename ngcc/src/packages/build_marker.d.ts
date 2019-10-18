839bac3edd7d9e55cdde69b2c8b33d5b3566303f
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:04:34 +0000

    6958d11d95 feat(ivy): type checking of event bindings (#33125)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.11+67.sha-c44a1a7.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.11+65.sha-6958d11.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
