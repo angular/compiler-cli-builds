31b80b5ec826bde11f8fa1ec94f4a765bb4bfcf4
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:08:15 +0000

    19ada81a08 build: fix tslint issue with String instantiation

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.11+6.sha-f13e40d.with-local-changes";
+export declare const NGCC_VERSION = "8.2.11+8.sha-19ada81.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
