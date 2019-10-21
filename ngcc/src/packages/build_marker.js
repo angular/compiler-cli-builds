746b785044543927f51ae0ef4474e6acd2059744
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:45:20 +0000

    1b4eaea6d4 feat(ivy): input type coercion for template type-checking (#33243)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+9.sha-755a80c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+8.sha-1b4eaea.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
