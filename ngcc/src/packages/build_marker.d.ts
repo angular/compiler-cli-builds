6231068df774e11e18b52535c83d1ea24f347396
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 21:14:00 +0000

    bb284066ca build: update bazel remote build config and documentation (#33304)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.11+13.sha-3f3e1bf.with-local-changes";
+export declare const NGCC_VERSION = "8.2.11+15.sha-bb28406.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
