3d619cca302108d43fd1b25fe87fe5e20c9b1cfd
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 17:54:22 +0000

    5d86e4a9b1 fix(compiler): ensure that legacy ids are rendered for ICUs (#33318)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.12+38.sha-65a0d2b.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.12+45.sha-5d86e4a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
