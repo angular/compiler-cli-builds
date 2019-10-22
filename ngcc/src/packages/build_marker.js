1e60a00f04facfdc7ff6db64d98d8d4607ad2f34
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 17:55:50 +0000

    5d86e4a9b1 fix(compiler): ensure that legacy ids are rendered for ICUs (#33318)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+38.sha-65a0d2b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+45.sha-5d86e4a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
