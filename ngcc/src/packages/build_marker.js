2105ffebc1f9f17c556f4a8d29334464ecc8da31
Kara Erickson <karakara@google.com>
2019-10-22 21:44:50 +0000

    b3d6d500be docs: add ngcc postinstall migration doc (#33328)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+48.sha-5632424.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+50.sha-b3d6d50.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
