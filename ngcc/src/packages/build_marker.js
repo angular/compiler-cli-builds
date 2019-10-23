dc5d689ddd0496ee44f63abe06dee598eb7edacb
Andrew Kushnir <akushnir@google.com>
2019-10-23 18:27:59 +0000

    2ecc4c7886 docs: Fix appHighlightColor typo (#33331)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+62.sha-3f257e9.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+63.sha-2ecc4c7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
