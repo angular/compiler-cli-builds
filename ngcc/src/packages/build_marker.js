088dc829bfbd18097cae28df854f1c97a7187839
Andrew Kushnir <akushnir@google.com>
2019-10-23 17:36:06 +0000

    3f257e96c6 fix(language-service): Add global symbol for $any() (#33245)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+61.sha-8bc5fb2.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+62.sha-3f257e9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
