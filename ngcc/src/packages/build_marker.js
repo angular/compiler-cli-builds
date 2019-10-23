b8926c25e95b7211da493c164933ae93fa49b0ff
Andrew Kushnir <akushnir@google.com>
2019-10-23 19:07:27 +0000

    70238f7d7d docs: fix spelling error in angular component (#32971)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.11+20.sha-704e354.with-local-changes';
+    exports.NGCC_VERSION = '8.2.11+21.sha-70238f7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
