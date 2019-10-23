94e1b36b1699bbb69fe489798bc843f8d6e8c09d
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:29:20 +0000

    ed4d96f858 docs: add migrating to version 9 guide (#33339)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+57.sha-f013515.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+55.sha-ed4d96f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
