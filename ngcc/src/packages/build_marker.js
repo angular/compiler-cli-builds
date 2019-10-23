4a01557a52f84664876b1baac30c66a1119bec45
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:27:49 +0000

    383457f898 docs: clean up deprecation guide (#33338)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+51.sha-398ff1e.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+54.sha-383457f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
