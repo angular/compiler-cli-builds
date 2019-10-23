02c10a2895b66f5f80649b4bc61b0f3e53b5b279
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:18:42 +0000

    ba29e4d953 feat(docs-infra): implement figure styles (#33259)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.12+65.sha-355e54a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.12+66.sha-ba29e4d.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
