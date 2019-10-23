97d4f76e608a367ebb4d13416a1ebed864acec2c
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:21:39 +0000

    2d465ee37b feat(docs-infra): implement figure styles (#33259)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.11+21.sha-70238f7.with-local-changes";
+export declare const NGCC_VERSION = "8.2.11+22.sha-2d465ee.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
