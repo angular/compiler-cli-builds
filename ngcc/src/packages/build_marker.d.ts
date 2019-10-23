9b119e4ca2f46844ee59f45bb7eee042c1caed4d
Andrew Kushnir <akushnir@google.com>
2019-10-23 23:58:47 +0000

    d40ee6a259 perf(ivy): improve styling performance (#33326)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+1.sha-c79d6ec.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+2.sha-d40ee6a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
