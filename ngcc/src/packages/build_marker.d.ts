d2591b435417df266e82756bd1da4ddcd9cb1b95
Andrew Kushnir <akushnir@google.com>
2019-10-24 22:00:00 +0000

    09a2bb839f refactor(ivy): Intruduce LFrame to store global instruction information (#33178)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+24.sha-d883007.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+27.sha-09a2bb8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
