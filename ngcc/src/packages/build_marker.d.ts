a282c2cef32072327c819a8a35ae2fd84dcc96e1
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:30:26 +0000

    93a0b1ba13 refactor(language-service): Remove AttrInfo interface (#33380)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+21.sha-73530a9.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+22.sha-93a0b1b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
