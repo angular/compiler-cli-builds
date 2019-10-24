fabf5f02e9b7ce4b1969953053894a0dc9681383
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:37:08 +0000

    f76b370d70 test: update ivy i18n integration test (#33314)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+10.sha-efbbae5.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+15.sha-f76b370.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
