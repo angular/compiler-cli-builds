bf18357901efa0ab1e5b82c5742a3cd70e87c7c2
Andrew Kushnir <akushnir@google.com>
2019-10-24 18:31:54 +0000

    63f0ded5cf fix(ivy): fix broken typechecking test on Windows (#33376)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+18.sha-9a5e08f.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+17.sha-63f0ded.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
