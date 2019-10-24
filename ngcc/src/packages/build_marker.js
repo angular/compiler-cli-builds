c7f843943f7afcd53e73df6ec6ce534ccc13f165
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:07:53 +0000

    2805af900f docs: fix typo in getting started guide (#33372)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+5.sha-2f3812a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+6.sha-2805af9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
