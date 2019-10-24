079ce32a27a96da99c86606cee9df57923928ecb
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:28:43 +0000

    93a0b1ba13 refactor(language-service): Remove AttrInfo interface (#33380)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+21.sha-73530a9.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+22.sha-93a0b1b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
