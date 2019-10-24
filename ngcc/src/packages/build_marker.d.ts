367219e83a191c35f6aeef4dfd5f8ce3250300e3
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:19:37 +0000

    efbbae5a48 ci: publish tarballs for all Angular packages as build artifacts on PR builds (#33321)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+7.sha-3d11355.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+10.sha-efbbae5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
