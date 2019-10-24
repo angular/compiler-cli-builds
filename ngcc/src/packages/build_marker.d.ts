1ce1d9de951fd82119b4f201074629929d419c91
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:32:36 +0000

    d883007cc6 build: update CLI and related dependencies (#33382)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+23.sha-4c4217c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+24.sha-d883007.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+23.sha-4c4217c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+24.sha-d883007.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
