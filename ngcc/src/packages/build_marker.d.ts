e439a003a0f15d09ccdca2e8adc6db2d86026d16
Andrew Kushnir <akushnir@google.com>
2019-10-25 19:32:19 +0000

    8d15bfa6ee fix(ivy): allow abstract directives to have an invalid constructor (#32987)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+51.sha-e4e8dbd.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+52.sha-8d15bfa.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
