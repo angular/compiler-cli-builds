0dd109502202f6dba154e7e10346d186503e0cb0
Andrew Kushnir <akushnir@google.com>
2019-10-25 16:41:01 +0000

    aa36b55bd9 build: address review comments in .bazelrc (#33367)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+42.sha-b381497.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+47.sha-aa36b55.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.13+42.sha-b381497.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.13+47.sha-aa36b55.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
