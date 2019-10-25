174db7807fd62a44b75d711091e28074ec5d1055
Andrew Kushnir <akushnir@google.com>
2019-10-25 20:17:24 +0000

    3505692f75 docs: add @next to update instructions (#33410)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+52.sha-8d15bfa.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+53.sha-3505692.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
