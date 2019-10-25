5338409949c093345fe4efbb4c96c1ca1f52580b
Andrew Kushnir <akushnir@google.com>
2019-10-25 21:18:30 +0000

    5607ad8c62 perf(ivy): apply static styles/classes directly to an element's style/className properties (#33364)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+58.sha-335854f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+59.sha-5607ad8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
