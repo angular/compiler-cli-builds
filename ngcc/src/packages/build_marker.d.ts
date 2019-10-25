f26375c804cc77806a83226c45f1db3d0b2c4c9c
Andrew Kushnir <akushnir@google.com>
2019-10-25 00:24:39 +0000

    ee4fc12e42 refactor(language-service): find expression ASTs using absolute spans (#33387)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.13+33.sha-3f195fe.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.13+34.sha-ee4fc12.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
