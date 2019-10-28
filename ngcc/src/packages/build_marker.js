f052448bd00f2dd5228ccf7aa9bfdbc8faf60f6f
Andrew Kushnir <akushnir@google.com>
2019-10-28 23:38:50 +0000

    2c623fde16 feat(ivy): i18n - support inlining of XTB formatted translation files (#33444)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.14+10.sha-e483aca.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.14+12.sha-2c623fd.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
