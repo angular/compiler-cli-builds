701866006ee4ea059a75ad5f275c8ebcab8abe81
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:28:04 +0000

    c3e93564d0 perf(ivy): avoid generating selectors array for directives without a selector (#33431)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.14+24.sha-d1246a1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.14+25.sha-c3e9356.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
