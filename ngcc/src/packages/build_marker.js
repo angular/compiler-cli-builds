bd2b7e9e8603d200ea6fa3cc8a0dec4ebf026e3b
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:09:53 +0000

    d1246a1d10 test(ivy): i18n - reenable ivy i18n runtime integration test (#33462)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.14+22.sha-936700a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.14+24.sha-d1246a1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
