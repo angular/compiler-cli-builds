bdf90786a2a0587c286f4f55ef1454edda1b1529
Andrew Kushnir <akushnir@google.com>
2019-10-29 21:25:05 +0000

    bf913cc39b feat(bazel): update bazel-schematics to use Ivy and new rollup_bundle (#33435)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.14+28.sha-649093c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.14+27.sha-bf913cc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
