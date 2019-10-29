7997a5d716cf50dd28b0481c8125564128a1ff8b
Andrew Kushnir <akushnir@google.com>
2019-10-29 23:42:08 +0000

    540d104b17 feat(bazel): update @bazel/schematics to Bazel 1.0.0 (#33476)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.14+30.sha-1de7579.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.14+32.sha-540d104.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
