ab664765136e6b454fcee2538d6008dffae40dd8
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:00:32 +0000

    afa50838a7 docs: update instructions for v9 to reflect cli update (#33473)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.14+16.sha-27745c5.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.14+18.sha-afa5083.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
