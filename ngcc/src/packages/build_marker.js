fbafbac8088b6b03ebd464302d10cd5b28bdb007
Andrew Kushnir <akushnir@google.com>
2019-10-29 21:34:34 +0000

    1fc778644a build: remove --nowatchfs flag as --watchfs is a noop on windows now (#33471)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.12+5.sha-f97c464.with-local-changes';
+    exports.NGCC_VERSION = '8.2.12+7.sha-1fc7786.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
