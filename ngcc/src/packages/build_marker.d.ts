7c809f5a06389fa1c490f517ac0460ae95ffcba1
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:09:16 +0000

    f97c464a9e fix(compiler): i18n - ignore `alt-trans` tags in XLIFF 1.2 (#33464)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.12+3.sha-26a6db2.with-local-changes";
+export declare const NGCC_VERSION = "8.2.12+5.sha-f97c464.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
