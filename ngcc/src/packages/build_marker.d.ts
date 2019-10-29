1045b84f668848ee8759a62f51f00e1b6d282402
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:04:29 +0000

    bd40c89688 fix(ivy): handle elements with local refs in i18n blocks (#33415)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-next.14+19.sha-da4eb91.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-next.14+20.sha-bd40c89.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-next.14+19.sha-da4eb91.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-next.14+20.sha-bd40c89.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
