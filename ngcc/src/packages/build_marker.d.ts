5bda2ca3ebecad7be8ef3257f126f4c81d7b4a0f
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:04:10 +0000

    38e9959cf0 docs: remove redundant whitespaces and fix minor typos (#33483)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.12+7.sha-1fc7786.with-local-changes";
+export declare const NGCC_VERSION = "8.2.12+8.sha-38e9959.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
