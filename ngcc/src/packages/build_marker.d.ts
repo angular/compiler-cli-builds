ff23e1676ea5906536ba2c86f697a14f5729b6ef
atscott <atscott01@gmail.com>
2019-10-31 23:08:53 +0000

    4452d6d848 perf(ivy): avoid repeated tNode.initialInputs reads (#33322)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+4.sha-66ab701.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+11.sha-4452d6d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
