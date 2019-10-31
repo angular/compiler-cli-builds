7c93001aaad9a3f37412a42964e7a02bf543e30f
atscott <atscott01@gmail.com>
2019-10-31 23:14:38 +0000

    7dbf716673 fix(service-worker): ensure initialization before handling messages (#32525)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+7.sha-a0f6b58.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+14.sha-7dbf716.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
