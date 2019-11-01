9ecfb5101edab8c64c21a61fcc81b87388faf23d
atscott <atscott01@gmail.com>
2019-11-01 17:58:17 +0000

    feb5f27d09 docs: Add Mike Brocchi to the list of GDEs (#33466)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+21.sha-31116f0c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+22.sha-feb5f27.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
