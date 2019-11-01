35aa5c7329f0cdb25ecafdfff9a2d867c2997e11
atscott <atscott01@gmail.com>
2019-11-01 22:43:14 +0000

    67aedd8366 docs: remove erroneous brack  in animations guide (#33525)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+29.sha-d77453d.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+31.sha-67aedd8.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
