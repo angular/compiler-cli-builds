f2f3f29c0e1a40cf5616695c05532e69d8835191
atscott <atscott01@gmail.com>
2019-11-01 18:06:40 +0000

    09536423e8 fix(zone.js): work around TS3.7 issue (#33294)

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,5 @@ export * from './src/transformers/entry_points';
 export * from './src/perform_compile';
 export * from './src/tooling';
 export { CompilerOptions as AngularCompilerOptions } from './src/transformers/api';
-export { NgTools_InternalApi_NG_2 as __NGTOOLS_PRIVATE_API_2 } from './src/ngtools_api';
 export { ngToTsDiagnostic } from './src/transformers/util';
 export { NgTscPlugin } from './src/ngtsc/tsc_plugin';
