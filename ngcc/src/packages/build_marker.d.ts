8484d3e1956ed241f5f3ba8b7d31d7474f804988
atscott <atscott01@gmail.com>
2019-11-01 18:10:49 +0000

    fc8eecad3f fix(compiler-cli): remove unused CLI private exports (#33242)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+25.sha-f02213a.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+23.sha-fc8eeca.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
