2ba8ddd0dcca37230d3b95773c85695e9a346114
atscott <atscott01@gmail.com>
2019-11-01 18:11:44 +0000

    a8f4f14bdc ci: always run the publish_packages_as_artifacts jobs (#33519)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+23.sha-fc8eeca.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+26.sha-a8f4f14.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
