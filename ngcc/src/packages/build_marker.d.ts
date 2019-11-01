70a4e5a8bbd0db2e5a2d02f2bde699c84ea5dacd
atscott <atscott01@gmail.com>
2019-11-01 00:12:20 +0000

    4d4b527474 docs(common): Remove async pipe from ngFor example (#33378)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+17.sha-602eeee.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+18.sha-4d4b527.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
