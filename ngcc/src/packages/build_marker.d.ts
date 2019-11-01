52ffb6fa9ceda08c9eb7811e10a3fd0d91f04ea6
atscott <atscott01@gmail.com>
2019-11-01 18:07:04 +0000

    f02213a661 build: restore functionality of the micro benchmarks `profile_all.js` script (#33494)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+24.sha-0953642.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+25.sha-f02213a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
