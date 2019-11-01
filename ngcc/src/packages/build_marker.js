9fdd037076b96b9f421646a9d4e05e95638e6419
atscott <atscott01@gmail.com>
2019-11-01 18:08:10 +0000

    09536423e8 fix(zone.js): work around TS3.7 issue (#33294)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+20.sha-9ebac71.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+24.sha-0953642.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
