249bab24bdb43d6bfaceaa1df55cc4a9c3ad0f55
atscott <atscott01@gmail.com>
2019-11-02 01:12:05 +0000

    ac9d044cad docs: add GDE profile of Fabian Gosebrink (#33438)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+31.sha-9e83814.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+32.sha-ac9d044.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
