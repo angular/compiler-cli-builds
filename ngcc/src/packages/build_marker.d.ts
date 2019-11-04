4c0b8c5fd304a493e8b81a350ec3a0427feb3d1b
atscott <atscott01@gmail.com>
2019-11-04 16:34:36 +0000

    5437e2da29 refactor(core): typo in undecorated-classes-with-di AOT failure message (#33018)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+33.sha-c83f501.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+34.sha-5437e2d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
