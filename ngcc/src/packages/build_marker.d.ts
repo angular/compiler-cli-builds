458b7e629734f32053c93e1765e64b720272d0a8
atscott <atscott01@gmail.com>
2019-11-04 16:52:56 +0000

    66725b7b37 perf(ivy): move local references into consts array (#33129)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+34.sha-5437e2d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+35.sha-66725b7.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
