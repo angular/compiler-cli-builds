5507feaa5bb7bed51ff858113670942a6513379a
atscott <atscott01@gmail.com>
2019-11-04 20:29:39 +0000

    3992e7f05b docs: Update link to angular + bazel example app (#33409)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+35.sha-316b30b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+37.sha-3992e7f.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
