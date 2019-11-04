7f4c9f8a3aecce329bb9b53e53d3547c282e5df9
atscott <atscott01@gmail.com>
2019-11-04 20:29:29 +0000

    e3189f97ff docs: Update link to angular + bazel example app (#33409)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+36.sha-4ff43e1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+37.sha-e3189f9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+36.sha-4ff43e1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+37.sha-e3189f9.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
