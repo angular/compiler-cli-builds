862139b65f56c5471162622b90dbafe3c8ded8c9
atscott <atscott01@gmail.com>
2019-11-05 17:48:26 +0000

    c5894e08bc feat(common): support loading locales from a global (#33523)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+42.sha-25aaff2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+53.sha-c5894e0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
