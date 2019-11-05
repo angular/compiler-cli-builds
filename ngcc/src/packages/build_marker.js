da02fcfa8b7af5a08d98d401162a8de65ae96004
atscott <atscott01@gmail.com>
2019-11-05 01:12:34 +0000

    1b08f7c919 docs: Adding Doug Parker to contributors list. (#33516)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+40.sha-8de3fcf.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+42.sha-1b08f7c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
