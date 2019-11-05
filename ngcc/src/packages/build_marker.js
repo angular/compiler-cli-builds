6eff6d91db2478c9995823442873c79cdc98cd3a
atscott <atscott01@gmail.com>
2019-11-05 23:07:52 +0000

    3de72e4124 revert: build: remove deps on legacy nodejs rules rollup_bundle internals (#33201) (#33604)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+62.sha-d751ca7.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+63.sha-3de72e4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
