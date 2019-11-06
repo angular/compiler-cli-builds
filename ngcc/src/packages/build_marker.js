53001ae1270823fead9b7d0bdc02287400b45729
atscott <atscott01@gmail.com>
2019-11-06 20:09:53 +0000

    e2d7b25e0d fix(ivy): avoid implicit any errors in event handlers (#33550)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+70.sha-2a4061a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+72.sha-e2d7b25.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
