0d00635daf340458d33d03cdd2833ea66a73deae
atscott <atscott01@gmail.com>
2019-11-06 01:06:37 +0000

    8c6fb17d29 build: reference zone.js from source directly instead of npm. (#33046)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+63.sha-3de72e4.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+64.sha-8c6fb17.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
