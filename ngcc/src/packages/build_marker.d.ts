c60fa623b063e2e29746f22f702623b533823c6c
atscott <atscott01@gmail.com>
2019-11-06 20:11:37 +0000

    e2d7b25e0d fix(ivy): avoid implicit any errors in event handlers (#33550)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+70.sha-2a4061a.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+72.sha-e2d7b25.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
