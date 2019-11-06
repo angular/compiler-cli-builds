7f699545f9dc2126d45b4047e073b7d005e988a9
atscott <atscott01@gmail.com>
2019-11-06 18:23:06 +0000

    e89c2dd8d0 perf(ivy): add ngIf-like directive to the ng_template benchmark (#33595)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.0+67.sha-3297a76.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.0+68.sha-e89c2dd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
