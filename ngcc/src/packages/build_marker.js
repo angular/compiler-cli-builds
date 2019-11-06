64bd1bf9d1aa2856b1e67541e4063c02f0a2b538
atscott <atscott01@gmail.com>
2019-11-06 16:48:57 +0000

    3297a76195 refactor(ivy): remove TStylingContext locking in favor of firstUpdatePass flag (#33521)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+66.sha-5453c4c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.0+67.sha-3297a76.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
