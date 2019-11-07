efd60c944de530525e184428f6931b38fd93b08a
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:21:14 +0000

    1aa3053b12 perf(ivy): allign (browser, benchpress, G3) the depth param for the tree benchmark (#33629)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+13.sha-7c5c213.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+14.sha-1aa3053.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
