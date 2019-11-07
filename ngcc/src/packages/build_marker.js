18aa63b182f172000170a06902fdd2d6df25a2ef
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:54:13 +0000

    71b400eb7b test(ivy): introduce a benchmark for duplicate map-based style/class bindings (#33608)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+16.sha-e8d43d8.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+18.sha-71b400e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
