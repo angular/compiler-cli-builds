d06d72dfc98f9e6bc90548fedeca9be625c17bca
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:15:38 +0000

    cd8333cf0d fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+7.sha-99ead47.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+13.sha-cd8333c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
