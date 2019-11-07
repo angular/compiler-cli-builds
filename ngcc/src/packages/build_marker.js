25287cbff22280a410f776857999bb53f9ee8839
atscott <atscott01@gmail.com>
2019-11-07 00:16:47 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.0+76.sha-814ed31.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
