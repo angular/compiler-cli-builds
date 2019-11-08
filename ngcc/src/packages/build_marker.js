c29b7f492601f9a2c7814c921ba4319a97c5abd5
Kara Erickson <karakara@google.com>
2019-11-08 19:14:59 +0000

    1db86500de style: increase timeout to give blaze sufficient time to build (#33678) PR Close #33678

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+28.sha-afbaaf9.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+27.sha-1db8650.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
