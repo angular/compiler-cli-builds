5fdf7f0fc631ab357d35812834683799de7bc6a5
Kara Erickson <karakara@google.com>
2019-11-08 01:09:04 +0000

    811275cd15 fix(ivy): properly determine the first native node of a view (#33627)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+21.sha-c1bd3bc.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+24.sha-811275c.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+21.sha-c1bd3bc.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+24.sha-811275c.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
