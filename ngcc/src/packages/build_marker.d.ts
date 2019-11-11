67fa649d61f20b985a17155168f90d983b7798b4
Kara Erickson <karakara@google.com>
2019-11-11 23:09:17 +0000

    2c74996b7c docs: reorganize get-started to emphasize ready-made app

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.13+2.sha-b43ae44.with-local-changes";
+export declare const NGCC_VERSION = "8.2.13+4.sha-2c74996.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
