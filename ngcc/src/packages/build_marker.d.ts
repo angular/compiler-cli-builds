72e14d27cb1d9f1673d19c0b793d2880ec19cf36
Kara Erickson <karakara@google.com>
2019-11-11 21:20:57 +0000

    c5400616f8 fix(ngcc): ensure that adjacent statements go after helper calls (#33689)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+47.sha-9396b19.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+51.sha-c540061.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
