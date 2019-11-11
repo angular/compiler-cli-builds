83ba88201b74831b93a8ca955b80be6d906f70f5
Kara Erickson <karakara@google.com>
2019-11-11 21:04:51 +0000

    c6ebcd1eb9 docs: fix numbering (#33736)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+47.sha-eece138.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
