8649a35253e042cd94d23fbe1baf5846cd6ce7b8
Kara Erickson <karakara@google.com>
2019-11-12 18:30:29 +0000

    e31f62045d perf(ivy): chain listener instructions (#33720)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+61.sha-f1b0547.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+65.sha-e31f620.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
