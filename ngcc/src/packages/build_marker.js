3d95925f58cdab79caadbd7a79681fdf92589297
Kara Erickson <karakara@google.com>
2019-11-12 20:15:04 +0000

    641c671bac fix(core): support `ngInjectableDef` on types with inherited `ɵprov` (#33732)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+68.sha-641c671.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
