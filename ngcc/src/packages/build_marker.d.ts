b14ee8c5ce07cdb0222207cd148ba28c1680df35
Kara Erickson <karakara@google.com>
2019-11-12 00:13:41 +0000

    942e2ebe44 fix: generate the new locale files (#33682)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+51.sha-d8be830.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+56.sha-942e2eb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
