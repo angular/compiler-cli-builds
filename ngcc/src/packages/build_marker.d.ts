ef8f9a7bb4d8e00150d6481bc2608285326e33db
Kara Erickson <karakara@google.com>
2019-11-12 20:13:30 +0000

    641c671bac fix(core): support `ngInjectableDef` on types with inherited `ɵprov` (#33732)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+68.sha-641c671.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
