9106eea51336e3c06bf5473cd941b2e0c32f3247
Kara Erickson <karakara@google.com>
2019-11-13 21:31:42 +0000

    f1b32840d0 docs: release notes for the v9.0.0-rc.2 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+88.sha-18a8abb.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+89.sha-f1b3284.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
