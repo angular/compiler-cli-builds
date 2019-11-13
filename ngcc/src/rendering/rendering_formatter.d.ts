175b9d7f7925c203a97a4e4558e7754065d08cc6
Kara Erickson <karakara@google.com>
2019-11-13 22:15:20 +0000

    b30bb8dd91 fix(router): make routerLinkActive work with query params which contain arrays (#22666)

--- ngcc/src/rendering/rendering_formatter.d.ts
+++ ngcc/src/rendering/rendering_formatter.d.ts
@@ -6,6 +6,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import { Statement } from '@angular/compiler';
 import MagicString from 'magic-string';
 import * as ts from 'typescript';
 import { Reexport } from '../../../src/ngtsc/imports';
@@ -35,4 +36,5 @@ export interface RenderingFormatter {
     removeDecorators(output: MagicString, decoratorsToRemove: RedundantDecoratorMap): void;
     rewriteSwitchableDeclarations(outputText: MagicString, sourceFile: ts.SourceFile, declarations: SwitchableVariableDeclaration[]): void;
     addModuleWithProvidersParams(outputText: MagicString, moduleWithProviders: ModuleWithProvidersInfo[], importManager: ImportManager): void;
+    printStatement(stmt: Statement, sourceFile: ts.SourceFile, importManager: ImportManager): string;
 }
