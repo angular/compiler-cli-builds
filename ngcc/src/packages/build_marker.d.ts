dfb2cd5230fc3f52365353e322d105517ddfdee9
Kara Erickson <karakara@google.com>
2019-11-13 05:54:31 +0000

    cf10b336e7 fix(ivy): ComponentFactory.create should clear host element content (#33487)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+80.sha-2be0a1d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+79.sha-cf10b33.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
