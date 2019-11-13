df7c8aa3c4971a171bc81e807273d077004ee93c
Kara Erickson <karakara@google.com>
2019-11-13 20:38:04 +0000

    03b3a3881a build: set up all packages to publish via wombot proxy

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -17,7 +17,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '8.2.13+4.sha-2c74996.with-local-changes';
+    exports.NGCC_VERSION = '8.2.13+5.sha-03b3a38.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
