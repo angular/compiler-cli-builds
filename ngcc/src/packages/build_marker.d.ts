9a44a30236daedbd3dca98adf69d074703617a63
Kara Erickson <karakara@google.com>
2019-11-13 19:30:09 +0000

    1e1e242570 fix(ngcc): support minified ES5 scenarios (#33777)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+79.sha-cf10b33.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+86.sha-1e1e242.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+79.sha-cf10b33.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+86.sha-1e1e242.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
