cca157700ea22d8b3c386a710a9206bb039eacfe
Kara Erickson <karakara@google.com>
2019-11-13 19:53:32 +0000

    b4d7a684f8 build: set up all packages to publish via wombot proxy (#33747)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+86.sha-49e517d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+87.sha-b4d7a68.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
