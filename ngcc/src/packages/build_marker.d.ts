7d7b31a3ddc91047ae673e3704db995f4c1db242
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 17:57:15 +0000

    aaa38cf3f3 build: minor improvements to build script output (#33823)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.2+11.sha-8e5ed20.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.2+17.sha-aaa38cf.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
