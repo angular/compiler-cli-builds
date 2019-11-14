f081e8d64b1493364db4e1b73c65a2503edc1c18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 19:13:35 +0000

    eca85d5880 feat(bazel): update ng-add to use bazel v1.1.0 (#33813)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+106.sha-e25b256.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+108.sha-eca85d5.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+106.sha-e25b256.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+108.sha-eca85d5.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
