4edeeea4efdcfca9a403d3077746a2103eabb308
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 00:20:18 +0000

    84524583e9 fix(ivy): extend assertion in `directiveInject` function to support IcuContainers (#33832)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.2+20.sha-a61fb76.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.2+21.sha-8452458.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
