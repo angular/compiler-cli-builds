fde8a961ea3cad28d3e56305e814f7b0d08cfad5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-16 00:44:44 +0000

    7eb3e3bce6 build: fix build scripts on macOS (#33854)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+135.sha-a48573e.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+128.sha-7eb3e3b.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
