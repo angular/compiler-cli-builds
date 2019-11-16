cbf06ec9aee2ec8034ae308d0611f0e59fb47ef2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-16 00:37:27 +0000

    7567f2b8c8 Revert "build: add ngJitMode to ng_rollup_bundle terser config (#33773)"

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.2+37.sha-3074cdf.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.2+45.sha-7567f2b.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
