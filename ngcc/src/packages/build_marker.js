7905fda03cacfa2680727327c0c54b2056558571
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 20:22:50 +0000

    6bf2531b19 fix(ivy): properly insert views before ng-container with injected ViewContainerRef (#33853)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+164.sha-c68200c.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+177.sha-6bf2531.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
