4fdc59c76e6743bfaf703873071cc6abd83fc065
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:18:53 +0000

    fd83d9479a fix(ivy): avoid using stale cache in TestBed if module overrides are defined (#33787)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+197.sha-55748db.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+199.sha-fd83d94.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
