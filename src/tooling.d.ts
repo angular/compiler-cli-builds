e56a1eb30d4e6c36e0a94efa974fbfd91c4af391
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 22:18:16 +0000

    b54ed980ed fix(ivy): retain JIT metadata unless JIT mode is explicitly disabled (#33671)

--- src/tooling.d.ts
+++ src/tooling.d.ts
@@ -19,3 +19,8 @@ export declare const GLOBAL_DEFS_FOR_TERSER: {
     ngDevMode: boolean;
     ngI18nClosureMode: boolean;
 };
+export declare const GLOBAL_DEFS_FOR_TERSER_WITH_AOT: {
+    ngJitMode: boolean;
+    ngDevMode: boolean;
+    ngI18nClosureMode: boolean;
+};
