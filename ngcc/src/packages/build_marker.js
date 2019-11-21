348994b258f0cd52848c2bcbd523641d4713b9be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:04:07 +0000

    df71aa425f docs: clarify that `--prod` should be used to publish a library (#33959)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+207.sha-715d02a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+208.sha-df71aa4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
