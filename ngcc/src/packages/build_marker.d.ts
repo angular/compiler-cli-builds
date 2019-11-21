677f417bcc37e4e7faa3765c406ed52b49a641e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 17:37:13 +0000

    1394781cef build: update @schematics/angular to 9.0.0-rc.3 (#33955)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.3+14.sha-fc6ad19.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.3+16.sha-1394781.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.3+14.sha-fc6ad19.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.3+16.sha-1394781.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
