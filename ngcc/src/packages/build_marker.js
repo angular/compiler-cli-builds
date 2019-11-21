9aa867470e3cd8f3bbc77b35ea246150775e8cd0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:17:07 +0000

    bf1bcd1e08 fix(ngcc): render localized strings when in ES5 format (#33857)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+209.sha-346e1c1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
