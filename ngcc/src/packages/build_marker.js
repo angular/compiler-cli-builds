4d868777aca84d0670deb02f50bf5c91bf4edf24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:04:41 +0000

    715d02aa14 fix(ngcc): report errors from `analyze` and `resolve` processing (#33964)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+206.sha-e53375b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+207.sha-715d02a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
