50847638d4d029f684a8cd0ca73b92fa2c4c2e40
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:18:17 +0000

    fd5f101bab docs: add CSRF to XSRF section of http guide (#32933)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+215.sha-2303dc4.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+216.sha-fd5f101.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
