d900a9125169b4d517f08ff38b4010d8fcd63ec8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:15:34 +0000

    bf1bcd1e08 fix(ngcc): render localized strings when in ES5 format (#33857)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+209.sha-346e1c1.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
