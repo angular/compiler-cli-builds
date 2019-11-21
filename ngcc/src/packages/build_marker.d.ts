8b93cb001a38aa24eac218615327e18d09c86dc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 20:01:41 +0000

    250e6fdd39 fix(service-worker): throw when using the unsupported `versionedFiles` option in config (#33903)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.3+20.sha-c6695fa.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.3+22.sha-250e6fd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
