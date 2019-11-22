d72af6415e32178980e8470eba7431624e12bf62
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 17:55:20 +0000

    5024e217d7 docs: update npm install dev command to `npm install --save-dev` (#33985)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+221.sha-2dd572b.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+222.sha-5024e21.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
