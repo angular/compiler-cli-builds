e183a3e34d7bc0175b24f9e8febd8fa9dccd233d
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 18:33:39 +0000

    3c335c3590 fix(bazel): update to tsickle 0.37.1 to fix peerDep warnings (#33788)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+226.sha-a37bf9a.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+227.sha-3c335c3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
