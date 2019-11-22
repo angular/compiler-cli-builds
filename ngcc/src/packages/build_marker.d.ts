01d220212309e4760b6b62bfb10a13ff14547325
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 01:25:26 +0000

    f1bf5b26d1 fix(router): make routerLinkActive work with query params which contain arrays (#22666)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.3+27.sha-90a9043.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.3+30.sha-f1bf5b2.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
