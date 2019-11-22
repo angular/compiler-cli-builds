eddbfb03a2569d975714f48b48637a86b49ebec4
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:59:48 +0000

    8c76e78ec4 ci: add JoostK to @angular/fw-compiler (#33994)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+243.sha-fe91700.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+245.sha-8c76e78.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
