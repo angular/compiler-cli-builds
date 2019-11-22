cc60b84b6fcfd86967bc9ca59bc51a4b0d80b944
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 18:42:57 +0000

    dca6ad87fe fix(docs-infra): update manifest short_name (#30194)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+229.sha-621b659.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+231.sha-dca6ad8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
