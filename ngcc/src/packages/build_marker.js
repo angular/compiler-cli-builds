32d33db041d913f3eb05af4c6e170b1ba6c867d5
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:10:17 +0000

    de043a4bcb refactor(bazel): use getattr instead of hasattr in ng_module.bzl (#33765)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+232.sha-485ce64.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+237.sha-de043a4.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
