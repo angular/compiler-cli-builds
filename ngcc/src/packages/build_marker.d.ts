9fc6077941690ba977509d5e2823aeacde1bae5c
Matias Niemelä <matias@yearofmoo.com>
2019-11-23 00:01:20 +0000

    8c76e78ec4 ci: add JoostK to @angular/fw-compiler (#33994)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+243.sha-fe91700.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+245.sha-8c76e78.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
