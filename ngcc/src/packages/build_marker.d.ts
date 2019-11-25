5a074b9e68618bc2614caf6b6012846aea8d7404
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 19:32:58 +0000

    56f33bc266 docs: initial Ivy i18n guide updates (#33762)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+254.sha-31e2bdf.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+255.sha-56f33bc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
