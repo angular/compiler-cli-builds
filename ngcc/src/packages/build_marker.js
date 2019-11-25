2b6f3bc9283c71eb6599459d70794b41b9d5e8a4
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:04:32 +0000

    04b12fc0d6 refactor(ivy): remove isRootView check from getRenderParent (#33988)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+251.sha-478f882.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+250.sha-04b12fc.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
