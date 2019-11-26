52f8e8b0181491a794c1a5305eb619f9bfcc542b
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 03:58:20 +0000

    d655093123 docs: add Animations import in Router doc (#33740)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+266.sha-953365d.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+267.sha-d655093.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
