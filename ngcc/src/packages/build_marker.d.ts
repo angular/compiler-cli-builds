e35461bf36690d7c11d75c61baf8ec1538ae7965
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 03:58:03 +0000

    953365d090 refactor(ivy): remove tsickle workaround in chainable instruction (#34019)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+265.sha-7cd16b9.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+266.sha-953365d.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
