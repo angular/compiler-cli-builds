db686fd565ceb139606dd1e5cf5d51da00573bf0
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 00:04:10 +0000

    9ba5344f3e docs: fix typos in compiler API comments (#33977)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+259.sha-25dcc76.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+260.sha-9ba5344.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
