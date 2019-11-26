dc52572599d93dcf06e7f4a9103205c5fa741897
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 04:05:08 +0000

    b659aa32c9 fix(ivy): take styles extracted from template into account in JIT mode (#34017)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.3+73.sha-bbc8b0f.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.3+76.sha-b659aa3.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
