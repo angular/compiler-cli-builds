704c6aebfb9d7117ff637b20065eaa64cda0b460
Miško Hevery <misko@hevery.com>
2019-11-27 01:15:19 +0000

    9e5065a778 ci: do not run ng_benchmarks on CI (#34057)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.3+76.sha-b659aa3.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.3+98.sha-9e5065a.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
