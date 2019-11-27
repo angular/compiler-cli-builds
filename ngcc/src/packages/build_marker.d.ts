0e56fe5d5387b062483b60eb6ea8bd8ca33cc693
Miško Hevery <misko@hevery.com>
2019-11-27 00:41:00 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+282.sha-49804fe.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+288.sha-005c4a1.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
