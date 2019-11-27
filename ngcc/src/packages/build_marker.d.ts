88a59bf15b0b92d503d2f4884af69ee403287cc0
Miško Hevery <misko@hevery.com>
2019-11-27 23:38:23 +0000

    168abc6d6f docs: release notes for the v9.0.0-rc.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+302.sha-6108962.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+303.sha-168abc6.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
