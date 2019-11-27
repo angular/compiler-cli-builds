93f3ad7eef517d711363307c94c9e3bd08ef4c23
Miško Hevery <misko@hevery.com>
2019-11-27 00:39:51 +0000

    c291c8e2cb ci: do not run ng_benchmarks on CI (#34057)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+288.sha-005c4a1.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+289.sha-c291c8e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
