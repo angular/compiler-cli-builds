513ae4ef2dbdfe3be390d62608f078cb7fd718d4
Miško Hevery <misko@hevery.com>
2019-11-27 00:39:28 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+282.sha-49804fe.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+288.sha-005c4a1.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
