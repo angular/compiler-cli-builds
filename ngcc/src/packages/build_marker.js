1dd1ef48b5e4c751699b9e587f30f12536bfef7f
Miško Hevery <misko@hevery.com>
2019-12-02 19:49:36 +0000

    1425e63029 fix(language-service): determine correct type for ngFor exported values (#34089)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+310.sha-bbce2ad.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+312.sha-1425e63.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
