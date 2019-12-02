8b4c3d681059533db7ce3ad1e1f154e4e0f3bd26
Miško Hevery <misko@hevery.com>
2019-12-02 20:04:11 +0000

    e6dbcd0f46 docs: remove redundant dashes (#34172) PR Close #34172

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+322.sha-b8eb2f2.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+327.sha-e6dbcd0.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
