afa3c195f86e7cbab0efe4feb5511c5f48152d4f
Miško Hevery <misko@hevery.com>
2019-12-02 19:50:08 +0000

    3408468121 build: remove fsevents from package.json (#34058)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+303.sha-168abc6.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+308.sha-3408468.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
