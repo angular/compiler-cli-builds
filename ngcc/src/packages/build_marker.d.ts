619666205a9b5b4a9ae808f2ce8b2f54c0b0e614
Miško Hevery <misko@hevery.com>
2019-12-02 20:03:11 +0000

    d529b55651 fix(ivy): i18n - correctly parse XLIFF placeholders (#34155)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+320.sha-e7cc1d6.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+326.sha-d529b55.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
