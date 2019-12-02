2eec8163ff806d6adc7429acff77a22c52228fd0
Miško Hevery <misko@hevery.com>
2019-12-02 19:57:18 +0000

    7eccbcd30d fix(language-service): Make missing module suggestion instead of error (#34115)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+314.sha-c16a79d.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+317.sha-7eccbcd.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
