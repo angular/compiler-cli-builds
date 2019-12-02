a35884eb4c370ee3b3c19b495f4102fab4908eda
Miško Hevery <misko@hevery.com>
2019-12-02 19:51:10 +0000

    1425e63029 fix(language-service): determine correct type for ngFor exported values (#34089)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+310.sha-bbce2ad.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+312.sha-1425e63.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
