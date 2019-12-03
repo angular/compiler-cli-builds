6a4a496387805d700b52c01b3c4222a1479fdcbf
Miško Hevery <misko@hevery.com>
2019-12-03 00:50:40 +0000

    716fc845f5 fix(bazel): don't rely on @angular/core being as a depedency to install @angular/bazel (#34181)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.4.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.4+28.sha-716fc84.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
