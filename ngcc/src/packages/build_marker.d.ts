5684928f7f7276b1f546d6e667a7ea0d6763a24f
Miško Hevery <misko@hevery.com>
2019-12-03 18:33:35 +0000

    56f4e56094 docs: Ivy i18n guide updates for localize option (#34053)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+334.sha-dd944ef.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+335.sha-56f4e56.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
