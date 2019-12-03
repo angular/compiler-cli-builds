59d19a123d297ccd34da7e16a9815eb5434ac760
Miško Hevery <misko@hevery.com>
2019-12-03 18:30:28 +0000

    c60d7563a8 style: enforce disallowance of object constructor (#33211)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+332.sha-d4b8368.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+333.sha-c60d756.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
