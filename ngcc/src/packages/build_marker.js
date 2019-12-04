bc3b1fe22265752d56078dabb25ea48a6adcbeb0
Miško Hevery <misko@hevery.com>
2019-12-04 00:45:55 +0000

    a295255e58 perf(ivy): avoid duplicate state lookup and default function parameters (#34183)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+362.sha-e315215.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+361.sha-a295255.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
