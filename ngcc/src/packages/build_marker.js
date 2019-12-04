8e291ba9d459aceffeb084f3ae5b33b01bca375c
Miško Hevery <misko@hevery.com>
2019-12-04 23:23:03 +0000

    1183075d16 Revert "fix(common): update closure locales to include directionality data (#34240)"

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.5.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.5+2.sha-1183075.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
