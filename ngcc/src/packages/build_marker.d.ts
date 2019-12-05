59841a19525c130120b70ae3c5c53e6c35de16f6
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:32:03 +0000

    c0869ecfb3 fix(docs-infra): add deprecated-api-item class to remaining deprecated items (#34192)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+373.sha-28ef1af.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+374.sha-c0869ec.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
