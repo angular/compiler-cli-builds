a72aa13e82ab195ec72c42705c0ef663e69131c6
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:28:50 +0000

    e71d151571 docs: edit copy of getting started step 2 (#32776)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+371.sha-f9e959e.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+372.sha-e71d151.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
