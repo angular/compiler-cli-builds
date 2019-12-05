d4a96fffa1f7cb3fcbe789199afd66365e9449ab
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:34:21 +0000

    0e911f87b0 fix(language-service): Remove getExternalFiles() (#34260)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.5+12.sha-81c8b83.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.5+22.sha-0e911f8.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
