514583cc50c6939e798d06c33acb1155cc404f93
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:44:39 +0000

    ecb29de82f build: update to rules_nodejs 0.42.2 (#34243)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+381.sha-023c9be.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+380.sha-ecb29de.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
