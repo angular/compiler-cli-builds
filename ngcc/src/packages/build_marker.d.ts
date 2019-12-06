b3965d9c5b0adce5b538c3f023f8dccc671c1870
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:22:24 +0000

    6e944acaae build: remove compile build variable (#34280)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.5+25.sha-7dfd327.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.5+28.sha-6e944ac.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
