26abf123d5e986af89bf31b61f7745326f4cf8a3
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:27:46 +0000

    a12b5f930a fix(compiler): ensure localized strings are ES5 compatible for JIT mode (#34265)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+400.sha-b342a69.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+401.sha-a12b5f9.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
