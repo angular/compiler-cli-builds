0af19109a1b2736f58fcaa29bb830b7522f94db5
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:07:49 +0000

    7a6e326ec6 docs: update CHANGELOG to add v9.0.0-rc.5 release notes (#34282)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+394.sha-613dc11.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+395.sha-7a6e326.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
