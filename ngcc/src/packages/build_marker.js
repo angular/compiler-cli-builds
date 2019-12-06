9c693a7079915f708818a3cebd363fe7190f822a
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:28:39 +0000

    a1d0f1e5d2 fix(ivy): align TestBed.overrideProvider with what happens with providers in TestBed providers array (#33769)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+401.sha-a12b5f9.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
