d3b8daa0bd3b1dffd7d5b2b77aae14ff55b43186
Andrew Kushnir <akushnir@google.com>
2019-12-09 20:34:49 +0000

    60051ebb59 fix(compiler): switch to modern diagnostic formatting (#34234)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.5+35.sha-1347064.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.5+37.sha-60051eb.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
