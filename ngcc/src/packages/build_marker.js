d8eba4345c5fafd426857279243dc253541c34ce
Andrew Kushnir <akushnir@google.com>
2019-12-09 18:55:49 +0000

    d3069dbec6 refactor(ivy): minor cleanup in the listener instruction (#34255)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+404.sha-d3069db.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
