610a8ed42e58233a6ec9552ee517e40ae46bff27
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:36:43 +0000

    7c2bb37e4e refactor(docs-infra): give more descriptive variable name (#34284)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.5+40.sha-24d1f9e.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.5+44.sha-7c2bb37.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
