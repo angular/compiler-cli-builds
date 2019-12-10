3143b933dd11112fac591595ea3f97de7bd553df
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:36:06 +0000

    f95e2c7e7e docs: fix broken links (#34311)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+411.sha-ead169a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+413.sha-f95e2c7.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
