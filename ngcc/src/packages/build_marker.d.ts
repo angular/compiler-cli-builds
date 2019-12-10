4a5ea4ed3b578fe87227a44a7a0d807c33c1b57d
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:43:56 +0000

    8683c0335b docs: Fix links to Webpack docs (#34326)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+414.sha-419b153.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+415.sha-8683c03.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
