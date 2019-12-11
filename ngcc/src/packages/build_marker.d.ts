1c30ef70fca2d6472f02976ad29feeb79fd94e5c
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:50:35 +0000

    0984fbc748 fix(compiler-cli): allow declaration-only template type check members (#34296)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+421.sha-c9df9af.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+423.sha-0984fbc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
