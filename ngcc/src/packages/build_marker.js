64f73997d57d12b07280e41545f8e27b78f9b242
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:15:05 +0000

    d7c459a81a fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.5+58.sha-ad82ed8.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.5+61.sha-d7c459a.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
