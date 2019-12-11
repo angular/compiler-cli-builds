232ebaca9c18167e37e7b427679aaf9081802b4d
Kara Erickson <karakara@google.com>
2019-12-11 21:43:23 +0000

    6ce940315b fix(ivy): i18n - remove `translate` function when clearing translations (#34346)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+436.sha-5f2897b.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+435.sha-6ce9403.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
