7d6bc1d0906abc99165c58e6a4cab064cc89042d
Kara Erickson <karakara@google.com>
2019-12-12 00:17:08 +0000

    ad987021ce Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+435.sha-6ce9403.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+442.sha-ad98702.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
