0d4ef85fd5d2c5c93fa3d7eabe6faf04dbf73e0c
Kara Erickson <karakara@google.com>
2019-12-12 21:44:37 +0000

    6ba5fdc208 fix(ivy): generate a better error for template var writes (#34339)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+450.sha-6e7ca05.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
