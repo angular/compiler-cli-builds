acb79e45ddd8f48cfa2047993034ccf6392a0e4b
Kara Erickson <karakara@google.com>
2019-12-12 17:30:18 +0000

    1583293694 docs: ng-conf 2020 the musical (#34342)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+444.sha-05c1398.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+443.sha-1583293.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
