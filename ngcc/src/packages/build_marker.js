5d4b4682be2588905704ced13acf7497282ca009
Kara Erickson <karakara@google.com>
2019-12-12 00:19:38 +0000

    de71f9152c Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.6+8.sha-81c75cf.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.6+13.sha-de71f91.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
