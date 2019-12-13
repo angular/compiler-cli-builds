b553788d7cbe0ded607d48cd0df9da7c0fc9e012
Kara Erickson <karakara@google.com>
2019-12-13 19:13:07 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+460.sha-0fba79c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+463.sha-ea57587";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
