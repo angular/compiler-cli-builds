f8871f56de45d3757be2aa63aeeda968532b5ecf
Kara Erickson <karakara@google.com>
2019-12-13 22:02:27 +0000

    17f7f06ca5 Revert "build: update to yarn@1.21.1" (#34402)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+463.sha-ea57587';
+    exports.NGCC_VERSION = '9.0.0-rc.1+464.sha-17f7f06.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
