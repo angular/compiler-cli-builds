c587222ec0bb0b8901fa2f3f0fe6057e95e51009
Kara Erickson <karakara@google.com>
2019-12-16 16:02:35 +0000

    3bbd12d560 build: update to yarn@1.21.1 (#34384)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+472.sha-f79110c.with-local-changes";
+export declare const NGCC_VERSION = "9.0.0-rc.1+476.sha-3bbd12d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
