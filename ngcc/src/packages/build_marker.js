56f43bec914416e4a2f43c396b58f81de4b7b2f4
Kara Erickson <karakara@google.com>
2019-12-16 22:32:37 +0000

    28b4f4abce build: remove unused `polyfills-runtime.ts` file (#34424)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+483.sha-23cf11a';
+    exports.NGCC_VERSION = '9.0.0-rc.1+488.sha-28b4f4a';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
