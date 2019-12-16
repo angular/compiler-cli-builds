b9ba822281c4d86e6175ffe863a64f5794c869c3
Kara Erickson <karakara@google.com>
2019-12-16 16:05:28 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+476.sha-3bbd12d";
+export declare const NGCC_VERSION = "9.0.0-rc.1+478.sha-04ab036";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
