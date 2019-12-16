abc4304feae2344dea418262576c2d4a204efe73
Miško Hevery <misko@hevery.com>
2019-12-16 20:42:23 +0000

    a71965671c refactor(ivy): add `insertTStylingBinding` to keep track of bindings.

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+421.sha-7cd465a.with-local-changes';
+    exports.NGCC_VERSION = '9.0.0-rc.1+422.sha-a719656.with-local-changes';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
