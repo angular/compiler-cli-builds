f3767ddc0c679477577ad3e767597f66fa1c5927
Kara Erickson <karakara@google.com>
2019-12-16 16:34:13 +0000

    ddc229b69b fix(ivy): record correct absolute source span for ngForOf expressions (#31813)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+480.sha-12444a8";
+export declare const NGCC_VERSION = "9.0.0-rc.1+481.sha-ddc229b";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
