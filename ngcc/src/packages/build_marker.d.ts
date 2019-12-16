5fb2cb38754e48a71d28d8319e482b4266e84a1a
Kara Erickson <karakara@google.com>
2019-12-16 16:52:12 +0000

    d0a04bf309 ci: fix saucelabs_view_engine master-only failing (#34429)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+481.sha-ddc229b";
+export declare const NGCC_VERSION = "9.0.0-rc.1+482.sha-d0a04bf";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
