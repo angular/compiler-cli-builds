541cc309fb6fd07267d312cf556e8f95ce986cd0
Kara Erickson <karakara@google.com>
2019-12-16 23:51:55 +0000

    b1d4c58b81 feat(forms): expand NgModel disabled type to work with strict template type checking (#34438)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.6+60.sha-76e4870';
+    exports.NGCC_VERSION = '9.0.0-rc.6+61.sha-b1d4c58';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
