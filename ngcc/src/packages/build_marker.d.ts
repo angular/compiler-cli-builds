eaa2072494e66e96badb6012f84754c0bccfbc93
Kara Erickson <karakara@google.com>
2019-12-17 23:00:57 +0000

    3e201181bb fix(ivy): correctly associate output bound events with directives (#31938)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+497.sha-7938ff3";
+export declare const NGCC_VERSION = "9.0.0-rc.1+498.sha-3e20118";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+497.sha-7938ff3';
+    exports.NGCC_VERSION = '9.0.0-rc.1+498.sha-3e20118';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
