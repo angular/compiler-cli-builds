5ca2f0f8fb20d4c6cb6c0fd65572cf938844c7a3
Kara Erickson <karakara@google.com>
2019-12-17 22:59:35 +0000

    82dce68e13 refactor(compiler-cli): avoid unnecessarily calling `getSourceFile()` twice in `PartialEvaluator` (#34441)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.6+67.sha-deeb87d";
+export declare const NGCC_VERSION = "9.0.0-rc.6+68.sha-82dce68";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
