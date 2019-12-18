45c7160fca3993fabcad447c300f44747285254e
Kara Erickson <karakara@google.com>
2019-12-18 23:13:25 +0000

    3959511b80 fix(ivy): avoid duplicate errors in safe navigations and template guards (#34417)

--- src/ngtsc/typecheck/src/oob.d.ts
+++ src/ngtsc/typecheck/src/oob.d.ts
@@ -6,9 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/typecheck/src/oob" />
-import { AbsoluteSourceSpan, BindingPipe, PropertyWrite, TmplAstReference, TmplAstVariable } from '@angular/compiler';
+import { BindingPipe, PropertyWrite, TmplAstReference, TmplAstVariable } from '@angular/compiler';
 import * as ts from 'typescript';
-import { TcbSourceResolver } from './diagnostics';
+import { TemplateId } from './api';
+import { TemplateSourceResolver } from './diagnostics';
 /**
  * Collects `ts.Diagnostic`s on problems which occur in the template which aren't directly sourced
  * from Type Check Blocks.
@@ -28,7 +29,7 @@ export interface OutOfBandDiagnosticRecorder {
      * reference.
      * @param ref the `TmplAstReference` which could not be matched to a directive.
      */
-    missingReferenceTarget(templateId: string, ref: TmplAstReference): void;
+    missingReferenceTarget(templateId: TemplateId, ref: TmplAstReference): void;
     /**
      * Reports usage of a `| pipe` expression in the template for which the named pipe could not be
      * found.
@@ -36,19 +37,16 @@ export interface OutOfBandDiagnosticRecorder {
      * @param templateId the template type-checking ID of the template which contains the unknown
      * pipe.
      * @param ast the `BindingPipe` invocation of the pipe which could not be found.
-     * @param sourceSpan the `AbsoluteSourceSpan` of the pipe invocation (ideally, this should be the
-     * source span of the pipe's name). This depends on the source span of the `BindingPipe` itself
-     * plus span of the larger expression context.
      */
-    missingPipe(templateId: string, ast: BindingPipe, sourceSpan: AbsoluteSourceSpan): void;
-    illegalAssignmentToTemplateVar(templateId: string, assignment: PropertyWrite, assignmentSpan: AbsoluteSourceSpan, target: TmplAstVariable): void;
+    missingPipe(templateId: TemplateId, ast: BindingPipe): void;
+    illegalAssignmentToTemplateVar(templateId: TemplateId, assignment: PropertyWrite, target: TmplAstVariable): void;
 }
 export declare class OutOfBandDiagnosticRecorderImpl implements OutOfBandDiagnosticRecorder {
     private resolver;
     private _diagnostics;
-    constructor(resolver: TcbSourceResolver);
+    constructor(resolver: TemplateSourceResolver);
     readonly diagnostics: ReadonlyArray<ts.Diagnostic>;
-    missingReferenceTarget(templateId: string, ref: TmplAstReference): void;
-    missingPipe(templateId: string, ast: BindingPipe, absSpan: AbsoluteSourceSpan): void;
-    illegalAssignmentToTemplateVar(templateId: string, assignment: PropertyWrite, assignmentSpan: AbsoluteSourceSpan, target: TmplAstVariable): void;
+    missingReferenceTarget(templateId: TemplateId, ref: TmplAstReference): void;
+    missingPipe(templateId: TemplateId, ast: BindingPipe): void;
+    illegalAssignmentToTemplateVar(templateId: TemplateId, assignment: PropertyWrite, target: TmplAstVariable): void;
 }
