ef8592c4c70fca0b07a5be60068652ce8d524444
Kara Erickson <karakara@google.com>
2019-12-18 19:53:54 +0000

    23595272fe build: use vendored yarn version for bazel node toolchain (#34472)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+509.sha-9264f43';
+    exports.NGCC_VERSION = '9.0.0-rc.1+510.sha-2359527';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
