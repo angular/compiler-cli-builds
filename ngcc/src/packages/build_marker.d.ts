534a256567a80a0f9ef3c0d3bd3d2f7c379cddbe
Kara Erickson <karakara@google.com>
2019-12-18 18:45:04 +0000

    5ec4fb2f2a refactor: TypeScript 3.7 fixes. (#34372)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.6+72.sha-1ebb405";
+export declare const NGCC_VERSION = "9.0.0-rc.6+73.sha-5ec4fb2";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.6+72.sha-1ebb405';
+    exports.NGCC_VERSION = '9.0.0-rc.6+73.sha-5ec4fb2';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
