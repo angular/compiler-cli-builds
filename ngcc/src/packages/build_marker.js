8982063489860fe75005edf286d925e362285590
Kara Erickson <karakara@google.com>
2019-12-18 17:32:51 +0000

    1eae7c81e9 refactor(language-service): Append missing AttrAst to AstPath (#34459)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+498.sha-3e20118';
+    exports.NGCC_VERSION = '9.0.0-rc.1+501.sha-1eae7c8';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
