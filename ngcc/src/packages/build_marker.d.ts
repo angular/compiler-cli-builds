a8444612e0c1ec89722e7722e1e72c39021cf0fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-19 19:55:16 +0000

    2dffe65cfd fix(language-service): completions after "let x of |" in ngFor (#34473)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+519.sha-260a061";
+export declare const NGCC_VERSION = "9.0.0-rc.1+520.sha-2dffe65";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
