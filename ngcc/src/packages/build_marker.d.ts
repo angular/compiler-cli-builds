ffd6b11db60bc4e22bdbdede30dcb92b57b1fe91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-20 22:59:44 +0000

    9b9116c79d feat(language-service): Append symbol type to hover tooltip (#34515)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+520.sha-2dffe65";
+export declare const NGCC_VERSION = "9.0.0-rc.1+521.sha-9b9116c";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
