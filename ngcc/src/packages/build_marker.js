277252898c93bc8caced12c5a925cbd15c1381d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-20 23:01:24 +0000

    9b9116c79d feat(language-service): Append symbol type to hover tooltip (#34515)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+520.sha-2dffe65';
+    exports.NGCC_VERSION = '9.0.0-rc.1+521.sha-9b9116c';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
