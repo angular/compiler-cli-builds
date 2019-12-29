0fa884acc42cb6823023b83b379794a4acb428f6
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:23:17 +0000

    0fae3d0395 build(docs-infra): bump handlebars from 4.0.12 to 4.5.3 in /aio/tools/examples/shared

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+522.sha-1aed00d";
+export declare const NGCC_VERSION = "9.0.0-rc.1+522.sha-0fae3d0";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+522.sha-1aed00d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+522.sha-0fae3d0';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
