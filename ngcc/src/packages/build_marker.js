4f5d247dc0b3879c0adc44840f7417a29a2c18f1
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:16:02 +0000

    61c805fa1b build(docs-infra): bump handlebars from 4.1.2 to 4.5.3 in /aio

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+522.sha-22ac0e9';
+    exports.NGCC_VERSION = '9.0.0-rc.1+522.sha-61c805f';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
