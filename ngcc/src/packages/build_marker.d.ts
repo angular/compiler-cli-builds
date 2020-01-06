3e59943f7755b96b039ff8e6f8fc787b9e1764cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:39:41 +0000

    e116816131 refactor(ivy): let `strictTemplates` imply `fullTemplateTypeCheck` (#34195)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+525.sha-1de49ba";
+export declare const NGCC_VERSION = "9.0.0-rc.1+527.sha-e116816";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
