601f31e0b9e3629551d00a95b500a7a2c51db681
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:29:49 +0000

    19944c2424 feat(compiler): record end of expression Token (#33549)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+522.sha-8410278";
+export declare const NGCC_VERSION = "9.0.0-rc.1+523.sha-19944c2";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
