3ddb9cb352d31b86dbc61b2ae17aa72af3568587
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:22:40 +0000

    8410278b6d docs: copy edit getting started step 4 (#32839)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+521.sha-9b9116c";
+export declare const NGCC_VERSION = "9.0.0-rc.1+522.sha-8410278";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
