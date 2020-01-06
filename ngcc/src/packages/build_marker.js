e764bcdee962a1d62ffefe78b36f6dc39107378b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:31:27 +0000

    19944c2424 feat(compiler): record end of expression Token (#33549)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+522.sha-8410278';
+    exports.NGCC_VERSION = '9.0.0-rc.1+523.sha-19944c2';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
