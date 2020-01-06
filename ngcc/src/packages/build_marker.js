37947c24bf40252739f44605286a82605fcbcc32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 20:28:11 +0000

    5b864ede13 fix(ivy): TestBed not unwrapping imports array function when overriding provider (#34629)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+530.sha-ba2fd31';
+    exports.NGCC_VERSION = '9.0.0-rc.1+533.sha-5b864ed';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
