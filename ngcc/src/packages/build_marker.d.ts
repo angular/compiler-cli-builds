637818416d21a16a23e0e82cc3681c1cc59c57bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:38:00 +0000

    1de49ba369 refactor(ivy): consistently translate types to `ts.TypeNode` (#34021)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+523.sha-19944c2";
+export declare const NGCC_VERSION = "9.0.0-rc.1+525.sha-1de49ba";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
