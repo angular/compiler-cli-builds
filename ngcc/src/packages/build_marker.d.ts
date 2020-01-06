cc82b5beae455a8b371e15856258fed89d9bbd03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:54:25 +0000

    ba2fd31e62 fix(language-service): The pipe method should not include parentheses (#34485)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+529.sha-eab7f9f";
+export declare const NGCC_VERSION = "9.0.0-rc.1+530.sha-ba2fd31";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
