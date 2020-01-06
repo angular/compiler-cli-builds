28d2a27f961e42eb14a4ee5133750ffd85f219bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 20:29:45 +0000

    5b864ede13 fix(ivy): TestBed not unwrapping imports array function when overriding provider (#34629)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+530.sha-ba2fd31";
+export declare const NGCC_VERSION = "9.0.0-rc.1+533.sha-5b864ed";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
