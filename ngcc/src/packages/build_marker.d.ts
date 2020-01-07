91d38254519e6ed46306a9be608198b8030c26da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 18:48:39 +0000

    effb92dfae fix(ivy): skip field inheritance if InheritDefinitionFeature is present on parent def (#34244)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+533.sha-5b864ed";
+export declare const NGCC_VERSION = "9.0.0-rc.1+534.sha-effb92d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
