5b7f11f7c340ea5090aa3cd3708611e1c236e6e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 21:42:20 +0000

    39bf104a03 build: update `firebase-tools` to the latest version (#34682)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+555.sha-b3398ee";
+export declare const NGCC_VERSION = "9.0.0-rc.1+556.sha-39bf104";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
