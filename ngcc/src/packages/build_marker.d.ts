d2ca7feb7442e107a3c67d52528b7a5646635485
atscott <atscott@google.com>
2020-01-09 23:33:59 +0000

    9c5510b28e docs(ivy): document breaking change with <select> and binding order (#34709)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+583.sha-c3f14bb";
+export declare const NGCC_VERSION = "9.0.0-rc.1+585.sha-9c5510b";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
