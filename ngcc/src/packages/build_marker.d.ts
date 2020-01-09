8cf04f3355e53bf81c5f7530bc3e7feee7eb6bb6
atscott <atscott@google.com>
2020-01-09 22:36:29 +0000

    c3f14bb7a5 feat(ivy): improve `ExpressionChangedAfterChecked` error message for attributes (#34505)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+575.sha-0083443";
+export declare const NGCC_VERSION = "9.0.0-rc.1+583.sha-c3f14bb";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
