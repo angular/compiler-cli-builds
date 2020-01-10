98808ebc745d8ffffdab07399b2a86e31a90c90b
atscott <atscott@google.com>
2020-01-10 00:35:02 +0000

    25eaff447a refactor(core): remove duplicate InjectableType declaration (#34679)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+585.sha-9c5510b';
+    exports.NGCC_VERSION = '9.0.0-rc.1+586.sha-25eaff4';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
