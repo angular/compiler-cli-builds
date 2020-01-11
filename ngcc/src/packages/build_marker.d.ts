8937b5bce3e686d3cbec47dfaf4d8dc17de43262
atscott <atscott@google.com>
2020-01-11 00:23:54 +0000

    08c25818d8 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.8+75.sha-a7d8dcd";
+export declare const NGCC_VERSION = "9.0.0-rc.8+76.sha-08c2581";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
