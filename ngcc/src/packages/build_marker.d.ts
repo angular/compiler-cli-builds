7f00997373f9771d61e074a464d78bc4d37ac42d
atscott <atscott@google.com>
2020-01-13 15:45:48 +0000

    ebcd59ae4f docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+632.sha-6d534f1";
+export declare const NGCC_VERSION = "9.0.0-rc.1+641.sha-ebcd59a";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+632.sha-6d534f1';
+    exports.NGCC_VERSION = '9.0.0-rc.1+641.sha-ebcd59a';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
