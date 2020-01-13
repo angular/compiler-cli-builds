2b2eabd0ea6bd441a0398e436afe939374854621
atscott <atscott@google.com>
2020-01-13 18:07:28 +0000

    ff02ddfd9b fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.8+85.sha-a0eb57f";
+export declare const NGCC_VERSION = "9.0.0-rc.8+88.sha-ff02ddf";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
