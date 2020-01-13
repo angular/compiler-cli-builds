89e7c3b25d60a69bb8f4dabbf496d2ef0c9ffc67
atscott <atscott@google.com>
2020-01-13 23:24:02 +0000

    15b4173a76 refactor(language-service): create attr for missing attr, bound tmpl (#34743)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+650.sha-7d40185';
+    exports.NGCC_VERSION = '9.0.0-rc.1+651.sha-15b4173';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
