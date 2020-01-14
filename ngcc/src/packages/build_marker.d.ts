4ecb1633bdce9abaac8fd8c8ed4fb0c7adb9fca0
atscott <atscott@google.com>
2020-01-14 18:50:29 +0000

    ec147bd839 docs: remove old contributors (#34486)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+659.sha-280aae6";
+export declare const NGCC_VERSION = "9.0.0-rc.1+668.sha-ec147bd";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
