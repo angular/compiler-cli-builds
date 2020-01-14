9d7e77be566575468f7878364ca4d0346a1bdccd
atscott <atscott@google.com>
2020-01-14 22:02:09 +0000

    23cbfa791c docs: expand doc for app_initializer token (#34760)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+671.sha-d863526';
+    exports.NGCC_VERSION = '9.0.0-rc.1+669.sha-23cbfa7';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
