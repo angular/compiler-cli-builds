eaee0085d6329113cc7523a5f084b7b20f20fa8c
atscott <atscott@google.com>
2020-01-14 23:14:31 +0000

    3113bb7ad2 fix(forms): accept number length in length validators (#32057)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.8+115.sha-e100bbc";
+export declare const NGCC_VERSION = "9.0.0-rc.8+116.sha-3113bb7";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.8+115.sha-e100bbc';
+    exports.NGCC_VERSION = '9.0.0-rc.8+116.sha-3113bb7';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
