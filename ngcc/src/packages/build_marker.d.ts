69771dc5c2999a3dcbeca9035e33a55eaedccb41
atscott <atscott@google.com>
2020-01-14 17:52:17 +0000

    1a453872c8 fix(language-service): apply suggested change. (#34564)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+652.sha-7325053";
+export declare const NGCC_VERSION = "9.0.0-rc.1+657.sha-1a45387";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
