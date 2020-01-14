61ee651bca64decf9955781267240e19403b1f4e
atscott <atscott@google.com>
2020-01-14 18:52:04 +0000

    ec147bd839 docs: remove old contributors (#34486)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+659.sha-280aae6';
+    exports.NGCC_VERSION = '9.0.0-rc.1+668.sha-ec147bd';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
