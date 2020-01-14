58122dbfe5c23e4fed5362294fc0889f29826250
atscott <atscott@google.com>
2020-01-14 23:49:03 +0000

    c4d035e886 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.8+116.sha-3113bb7";
+export declare const NGCC_VERSION = "9.0.0-rc.8+118.sha-c4d035e";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
