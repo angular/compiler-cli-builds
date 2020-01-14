965cf253134b3b3f598ecac30f60ffcf67c8b719
atscott <atscott@google.com>
2020-01-14 18:06:27 +0000

    277681096d fix(ivy): properly bootstrap components with attribute selectors (#34450)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+657.sha-1a45387";
+export declare const NGCC_VERSION = "9.0.0-rc.1+658.sha-2776810";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
