221a6df4ace67e1314ea73881a03fe8b74598df0
atscott <atscott@google.com>
2020-01-14 18:29:26 +0000

    280aae6fc5 docs: edit injectable decorator doc (#34218)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+658.sha-2776810";
+export declare const NGCC_VERSION = "9.0.0-rc.1+659.sha-280aae6";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
