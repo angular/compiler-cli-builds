00a776b60b06099258361f761ccea0481a2111de
GitHub <noreply@github.com>
2020-01-15 21:35:01 +0000

    1fd4d4f0d4 fixup! docs: fix public api tool name

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+692.sha-966cb35";
+export declare const NGCC_VERSION = "9.0.0-rc.1+693.sha-1fd4d4f";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
