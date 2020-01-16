ccd937f13bf91e6104a5d894eacf89465bfeb384
GitHub <noreply@github.com>
2020-01-16 11:32:16 +0000

    9d66345226 build: disable link to PUBLIC_API docs when running ts-api-guardian in angular repo

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+693.sha-1fd4d4f";
+export declare const NGCC_VERSION = "9.0.0-rc.1+695.sha-9d66345";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+693.sha-1fd4d4f';
+    exports.NGCC_VERSION = '9.0.0-rc.1+695.sha-9d66345';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
