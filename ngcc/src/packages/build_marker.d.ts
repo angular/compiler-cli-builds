40b52cc3e83444f53b005384a41e69a34798edd8
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:18:44 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+746.sha-e7cf37d";
+export declare const NGCC_VERSION = "9.0.0-rc.1+747.sha-61ad50d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+746.sha-e7cf37d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+747.sha-61ad50d';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
