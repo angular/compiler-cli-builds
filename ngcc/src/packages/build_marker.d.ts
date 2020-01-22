7d042ecd223d8c915bd946baa5484134a19660de
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:51:04 +0000

    a4cbb86360 docs: fix typo on router guide (#34911)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.10+14.sha-34c30ae";
+export declare const NGCC_VERSION = "9.0.0-rc.10+15.sha-a4cbb86";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
