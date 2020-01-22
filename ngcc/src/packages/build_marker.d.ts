07a7be23b899035bd9b7cf69f874737339799fee
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:19:55 +0000

    92c411f86d docs: update payload size limits for angular.io application (#34913)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+750.sha-e1fc44f";
+export declare const NGCC_VERSION = "9.0.0-rc.1+755.sha-92c411f";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
