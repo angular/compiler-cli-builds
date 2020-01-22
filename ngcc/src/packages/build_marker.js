c9a8246bc7f7f8d0349c1e5931971732983d8177
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:26:04 +0000

    34c30ae0aa build(docs-infra): always specify a landing file for StackBlitz projects (#34553)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.10+9.sha-20dc436';
+    exports.NGCC_VERSION = '9.0.0-rc.10+14.sha-34c30ae';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
