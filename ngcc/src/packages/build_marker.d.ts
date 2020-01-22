2dbec9de088a130b7e6e0a8e21614b61c823ef08
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:07:33 +0000

    e7cf37d99f docs: clarify where the `async` utility function is coming from (#34876) inside the `beforeEach` for out testing guide.

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+740.sha-0d83095";
+export declare const NGCC_VERSION = "9.0.0-rc.1+746.sha-e7cf37d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
