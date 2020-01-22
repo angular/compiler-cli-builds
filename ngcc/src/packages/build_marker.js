01300d0aa2ba61f01e951807177650fbd12540e0
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:19:29 +0000

    20dc436585 fix(language-service): rename getDiagnostics to getSemanticDiagnostics (#34909)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '0.0.0';
+    exports.NGCC_VERSION = '9.0.0-rc.10+9.sha-20dc436';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
