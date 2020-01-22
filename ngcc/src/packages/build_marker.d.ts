25811313c52a29d240d210b42de6c0863afee9fc
Andrew Kushnir <akushnir@google.com>
2020-01-22 21:56:47 +0000

    13ce3756c2 docs: add differential loading term to glossary (#34884)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+748.sha-32489c7";
+export declare const NGCC_VERSION = "9.0.0-rc.1+749.sha-13ce375";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+748.sha-32489c7';
+    exports.NGCC_VERSION = '9.0.0-rc.1+749.sha-13ce375';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
