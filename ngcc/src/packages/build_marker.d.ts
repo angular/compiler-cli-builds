4d8af41e42da77160196733f43e0f1ae8dad4ca6
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:19:08 +0000

    32489c7426 revert: refactor(ivy): remove styleSanitizer instruction in favor of an inline param (#34480) (#34910)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+747.sha-61ad50d";
+export declare const NGCC_VERSION = "9.0.0-rc.1+748.sha-32489c7";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+747.sha-61ad50d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+748.sha-32489c7';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
