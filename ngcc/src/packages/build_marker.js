52d65d3e2d6d5d1878341d8c34bef763a1f1f40d
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:09:12 +0000

    e7cf37d99f docs: clarify where the `async` utility function is coming from (#34876) inside the `beforeEach` for out testing guide.

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+740.sha-0d83095';
+    exports.NGCC_VERSION = '9.0.0-rc.1+746.sha-e7cf37d';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
