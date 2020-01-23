572eb627270419fadb041508c4c00a325832b2d9
Andrew Kushnir <akushnir@google.com>
2020-01-23 23:11:08 +0000

    080a8bfd43 fix(ngcc): do not attempt compilation when analysis fails (#34889)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.10+45.sha-c80392b';
+    exports.NGCC_VERSION = '9.0.0-rc.10+47.sha-080a8bf';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
