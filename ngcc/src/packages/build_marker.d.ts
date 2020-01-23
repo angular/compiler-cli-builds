3758b77bd23d55c7d359410a047f3d0cf680a733
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:43:58 +0000

    055533b6a7 feat(common): expose getLocaleCurrencyCode publicly (#34810)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+777.sha-4ba4782";
+export declare const NGCC_VERSION = "9.0.0-rc.1+778.sha-055533b";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+777.sha-4ba4782';
+    exports.NGCC_VERSION = '9.0.0-rc.1+778.sha-055533b';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
