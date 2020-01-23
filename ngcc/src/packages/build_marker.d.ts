b1c63c270a6783ba7d3b1a7b8410c71f139f06d3
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:46:13 +0000

    4ba478267e feat(language-service): specific suggestions for template context diags (#34751)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+776.sha-eb4b7d2";
+export declare const NGCC_VERSION = "9.0.0-rc.1+777.sha-4ba4782";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
