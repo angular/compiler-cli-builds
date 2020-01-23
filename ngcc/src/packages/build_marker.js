ec67b8d42f886b6c88cc10f5a2f40d2d67fed9f9
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:59:10 +0000

    970b22f98e test: setup circular dependency tests for all entry points (#34774)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+773.sha-0c8d085';
+    exports.NGCC_VERSION = '9.0.0-rc.1+775.sha-970b22f';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
