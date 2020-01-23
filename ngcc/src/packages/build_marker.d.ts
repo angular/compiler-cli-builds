0957440d521cce71adadff7409eddd8d771d9139
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:34:36 +0000

    1ea04ffc05 feat(language-service): support multiple symbol definitions (#34782)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+762.sha-48f8ca5";
+export declare const NGCC_VERSION = "9.0.0-rc.1+763.sha-1ea04ff";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
