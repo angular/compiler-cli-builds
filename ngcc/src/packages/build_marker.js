1f1f58f76273a9be57accf246d6c8408a66dd8a4
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:06:29 +0000

    0c8d085666 fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+767.sha-6b468f9';
+    exports.NGCC_VERSION = '9.0.0-rc.1+773.sha-0c8d085';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
