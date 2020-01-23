368481b5ce9df7c5f389a16fff8fb65204cd14ab
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:57:17 +0000

    5aa0507f6a docs(ivy): move incremental package README file to the correct location (#34912)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+780.sha-fa4ea34';
+    exports.NGCC_VERSION = '9.0.0-rc.1+782.sha-5aa0507';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
