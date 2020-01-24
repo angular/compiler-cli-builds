9ec71b924b5b4a5a1236951c90714e1e0415ce69
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:17:14 +0000

    b50ed5c22c fix(language-service): Make metadata in Declaration non-optional (#34936)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+787.sha-7659f2e';
+    exports.NGCC_VERSION = '9.0.0-rc.1+788.sha-b50ed5c';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
