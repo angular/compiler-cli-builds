3ca109344cf76fcabf6bd7a9bb21f10c36ea2144
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:16:18 +0000

    0223aa6121 fix(language-service): Diagnostic span should point to class name (#34932)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+788.sha-b50ed5c';
+    exports.NGCC_VERSION = '9.0.0-rc.1+789.sha-0223aa6';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
