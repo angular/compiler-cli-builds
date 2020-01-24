10b0fa5e4247b2d58e8ce046eef3f7ccd733458a
Andrew Kushnir <akushnir@google.com>
2020-01-24 19:52:18 +0000

    ed24100976 ci(docs-infra): use the correct version of yarn in `aio_monitoring_stable` (#34954)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+791.sha-24b2f1d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+792.sha-ed24100';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
