7df471ec6d6ffa47ed6a2b78e54d0bd1e3d31785
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:14:24 +0000

    c9db7bdb8a fix(language-service): Diagnostic span should point to class name (#34932)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.10+47.sha-080a8bf";
+export declare const NGCC_VERSION = "9.0.0-rc.10+49.sha-c9db7bd";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
