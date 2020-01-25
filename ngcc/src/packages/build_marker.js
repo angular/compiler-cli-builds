e2068b72e75134440513999ebf4fe76be6d9a067
Igor Minar <igor@angular.io>
2020-01-25 06:47:29 +0000

    bdd3865314 build(docs-infra): update CLI to 9.0.0-rc.10

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+4.sha-8fca9b6';
+    exports.NGCC_VERSION = '9.0.0-rc.11+5.sha-bdd3865';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
