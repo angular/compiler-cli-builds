82c245039e3e004b42d888d1db53a18bed2a05cb
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:04:21 +0000

    8926f764a5 docs: release notes for the v9.0.0-rc.11 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+806.sha-9bd9590';
+    exports.NGCC_VERSION = '9.0.0-rc.1+807.sha-8926f76';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
