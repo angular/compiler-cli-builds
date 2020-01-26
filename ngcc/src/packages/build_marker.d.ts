009b72dd72f5dfe509171c4ee2a3fd7e3498080d
Igor Minar <igor@angular.io>
2020-01-26 23:18:53 +0000

    3d4b1431ec test: add 'localize' as an allowed commit message scope (#34974)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+809.sha-be8d06c";
+export declare const NGCC_VERSION = "9.0.0-rc.1+814.sha-3d4b143";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
