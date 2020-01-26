9316a12c427defc291df2fe1adea08daedf8b882
Igor Minar <igor@angular.io>
2020-01-26 23:14:25 +0000

    4a6093d002 test: add 'localize' as an allowed commit message scope (#34974)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+5.sha-bdd3865';
+    exports.NGCC_VERSION = '9.0.0-rc.11+10.sha-4a6093d';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
