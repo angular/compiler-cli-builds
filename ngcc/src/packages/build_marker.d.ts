fa40883cd524cbf93436b9ea9d6965e2c25bdf59
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:25:39 +0000

    c0e73e0d67 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+15.sha-efb17ae";
+export declare const NGCC_VERSION = "9.0.0-rc.11+23.sha-c0e73e0";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+15.sha-efb17ae';
+    exports.NGCC_VERSION = '9.0.0-rc.11+23.sha-c0e73e0';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
