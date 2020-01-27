1168e5a182821cbf05863b25bb4315595d3923d0
Andrew Kushnir <akushnir@google.com>
2020-01-27 21:32:37 +0000

    304584c291 perf(ivy): remove unused argument in hostBindings function (#34969)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+828.sha-7069a83";
+export declare const NGCC_VERSION = "9.0.0-rc.1+829.sha-304584c";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
