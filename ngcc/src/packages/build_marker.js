e6943101c23c478e22e7d8fce33214f6b17b60d6
Andrew Kushnir <akushnir@google.com>
2020-01-27 18:02:54 +0000

    efb17ae0c8 build: update API Golden files after node 12 update (#34955)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+10.sha-4a6093d';
+    exports.NGCC_VERSION = '9.0.0-rc.11+15.sha-efb17ae';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
