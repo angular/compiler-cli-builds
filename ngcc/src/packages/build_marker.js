b3138a8d01e0c080155551ded8ec71d8acb79e99
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:10:22 +0000

    09869af90f fix(language-service): remove repeated symbol definitions for structural directive (#34847)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+819.sha-b3bd6ca';
+    exports.NGCC_VERSION = '9.0.0-rc.1+823.sha-09869af';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
