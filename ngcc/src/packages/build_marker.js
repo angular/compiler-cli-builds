7c7d52c50c071252e0e3c9830712b8c3b8ff711d
Andrew Kushnir <akushnir@google.com>
2020-01-27 20:44:16 +0000

    c7c7b20684 fix(localize): re-enable filename in code-frame error messages (#34994)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+23.sha-c0e73e0';
+    exports.NGCC_VERSION = '9.0.0-rc.11+24.sha-c7c7b20';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
