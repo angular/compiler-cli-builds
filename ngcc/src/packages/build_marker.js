eb1967cabfcd61f6fd32d98be8f6f0db3495c11e
Andrew Kushnir <akushnir@google.com>
2020-01-27 21:37:07 +0000

    71a3c7208f perf(ivy): remove unused argument in hostBindings function (#34969)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+24.sha-c7c7b20';
+    exports.NGCC_VERSION = '9.0.0-rc.11+25.sha-71a3c72';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
