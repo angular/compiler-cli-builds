a92a4668abe93cf1fc240550fdcf52a789b5ddfe
Andrew Kushnir <akushnir@google.com>
2020-01-28 00:15:41 +0000

    bd79a16869 refactor(language-service): make findOutputBinding a utility function (#34997)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+25.sha-71a3c72";
+export declare const NGCC_VERSION = "9.0.0-rc.11+26.sha-bd79a16";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
