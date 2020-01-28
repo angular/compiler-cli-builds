11f34627618f9702c35c6cc490d6c339eaff116f
Andrew Kushnir <akushnir@google.com>
2020-01-28 22:06:25 +0000

    dff4e1e19c refactor: simplify bazel saucelabs targets using karma pre-test wrapper and shared saucelabs connection between tests (#34769)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+837.sha-a5a5981";
+export declare const NGCC_VERSION = "9.0.0-rc.1+838.sha-dff4e1e";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
