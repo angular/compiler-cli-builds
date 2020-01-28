6802d416d777a16bdd8e710fd9eb859f0d0284f1
Andrew Kushnir <akushnir@google.com>
2020-01-28 17:27:56 +0000

    e7c74cbd69 feat(language-service): completions for output $event properties in (#34570)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+830.sha-90b303f";
+export declare const NGCC_VERSION = "9.0.0-rc.1+832.sha-e7c74cb";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+830.sha-90b303f';
+    exports.NGCC_VERSION = '9.0.0-rc.1+832.sha-e7c74cb';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
