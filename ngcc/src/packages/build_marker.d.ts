c6f1c1e00c4778fe36ab5bd53a3a1e5549237386
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:39:07 +0000

    296dc0622f ci: Migrate from CODEOWNERS to PullApprove for PR review management (#34814)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+832.sha-e7c74cb";
+export declare const NGCC_VERSION = "9.0.0-rc.1+834.sha-296dc06";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+832.sha-e7c74cb';
+    exports.NGCC_VERSION = '9.0.0-rc.1+834.sha-296dc06';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
