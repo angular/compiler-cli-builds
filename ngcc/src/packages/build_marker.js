00c3d549552c6e54b23fa4950072d732d7251d3c
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 18:46:57 +0000

    1cee9b32c3 ci: remove draft condition from pullapprove (#35012)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+835.sha-d15d6b7';
+    exports.NGCC_VERSION = '9.0.0-rc.1+836.sha-1cee9b3';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
