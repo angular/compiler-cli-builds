f5df484b46a9ae1131b3b8949b12c5b6e79d2830
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:55:31 +0000

    d15d6b7a63 ci: fix pullapprove_conditions (#35011)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+834.sha-296dc06';
+    exports.NGCC_VERSION = '9.0.0-rc.1+835.sha-d15d6b7';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
