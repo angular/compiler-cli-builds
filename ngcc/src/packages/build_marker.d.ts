0629ad9448fd73436e44dff006a1b6956c1c2a8f
George Kalpakas <kalpakas.g@gmail.com>
2020-01-29 20:30:40 +0000

    8b88269ae1 docs: update events page

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
  */
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { EntryPointJsonProperty, EntryPointPackageJson } from './entry_point';
-export declare const NGCC_VERSION = "8.2.14+3.sha-015f833.with-local-changes";
+export declare const NGCC_VERSION = "8.2.14+6.sha-8b88269.with-local-changes";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
