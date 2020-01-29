20d334f25d2bc84267a9d6107822c97be4c5166a
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:33:44 +0000

    7ca74375cb ci: add missing codeowners for `aio/` files (#34988)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+857.sha-a892054";
+export declare const NGCC_VERSION = "9.0.0-rc.1+860.sha-7ca7437";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
