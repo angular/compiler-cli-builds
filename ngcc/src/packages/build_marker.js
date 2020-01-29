39464b7a1c4cd0bc020eddd442ab05f5842e099a
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:46:41 +0000

    fb852f7252 build(localize): sync up babel dependencies (#35008)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+847.sha-53c4c8d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+848.sha-fb852f7';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
