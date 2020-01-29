02e5a8e67e1642399790359d1982d84385da90ed
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:36:57 +0000

    95383ee6a9 ci: don't run benchmark_tests on remote executors (#34996)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+858.sha-d5205a0";
+export declare const NGCC_VERSION = "9.0.0-rc.1+861.sha-95383ee";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+858.sha-d5205a0';
+    exports.NGCC_VERSION = '9.0.0-rc.1+861.sha-95383ee';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
