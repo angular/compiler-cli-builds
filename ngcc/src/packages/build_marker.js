e51280d21c8bb84456e35bcad57781717bf53909
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:48:33 +0000

    d43187f7ef ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job (for real) (#35033)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+848.sha-fb852f7';
+    exports.NGCC_VERSION = '9.0.0-rc.1+849.sha-d43187f';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
