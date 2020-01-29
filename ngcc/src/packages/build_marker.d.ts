f4e748070a39f71f643b946c5a3090bcdeb24ff0
Andrew Kushnir <akushnir@google.com>
2020-01-29 23:36:16 +0000

    0390d20d60 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+58.sha-9b7d703";
+export declare const NGCC_VERSION = "9.0.0-rc.11+59.sha-0390d20";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
