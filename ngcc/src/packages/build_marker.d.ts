f9ec5093d4963a7d0a586b6908f9e7cc212bd3ec
Andrew Kushnir <akushnir@google.com>
2020-01-29 19:14:51 +0000

    e554f2d3ed docs: ng-conf 2020 date change (#35019)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+46.sha-2d377b0";
+export declare const NGCC_VERSION = "9.0.0-rc.11+47.sha-e554f2d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
