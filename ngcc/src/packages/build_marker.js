a341bdc62b67c0e3af16ceab0b4a6c6178b223f3
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:35:25 +0000

    7ca74375cb ci: add missing codeowners for `aio/` files (#34988)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+857.sha-a892054';
+    exports.NGCC_VERSION = '9.0.0-rc.1+860.sha-7ca7437';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
