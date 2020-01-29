65afdcbfce71455b80b17b62970ef962f089f802
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:35:39 +0000

    a3de110b52 build(docs-infra): update aio to @angular/cli@9.0.0-rc.11 (#35043)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+861.sha-95383ee";
+export declare const NGCC_VERSION = "9.0.0-rc.1+859.sha-a3de110";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
