bba2e574ece3594978bf8ddc357b8e881b2ca532
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:16:09 +0000

    669df70da5 fix(ivy): ensure `DebugNode`/`DebugElement` are tree-shakeable in Ivy (#35003)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+840.sha-bd61cd0";
+export declare const NGCC_VERSION = "9.0.0-rc.1+844.sha-669df70";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
