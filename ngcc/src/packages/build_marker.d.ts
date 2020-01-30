5260375b08d50925ae79142fe8446498b2e80f85
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:38:09 +0000

    c1cf46c5c3 refactor(ivy): clean of  #34804 from previous merge (#35022)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+862.sha-1765052";
+export declare const NGCC_VERSION = "9.0.0-rc.1+865.sha-c1cf46c";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+862.sha-1765052';
+    exports.NGCC_VERSION = '9.0.0-rc.1+865.sha-c1cf46c';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
