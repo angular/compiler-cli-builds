c7547f23e7891ccd83f5dd055c5d50823305d60e
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:00:51 +0000

    36b8c0320b refactor(ivy): clean of  #34804 from previous merge (#35022)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+61.sha-19c4895";
+export declare const NGCC_VERSION = "9.0.0-rc.11+62.sha-36b8c03";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
