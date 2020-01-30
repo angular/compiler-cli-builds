d4b8e2c64872cd7e66c8bcd9f574be3963469758
Andrew Kushnir <akushnir@google.com>
2020-01-30 17:28:23 +0000

    3b612afff3 build(compiler-cli): update to chokidar 3.x (#35047)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.11+62.sha-36b8c03";
+export declare const NGCC_VERSION = "9.0.0-rc.11+63.sha-3b612af";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.11+62.sha-36b8c03';
+    exports.NGCC_VERSION = '9.0.0-rc.11+63.sha-3b612af';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
