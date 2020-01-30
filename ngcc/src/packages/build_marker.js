21bd12934881ec0d38d5136a3bb5b15c251ead51
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:28:43 +0000

    374a8ec263 ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+870.sha-9f5b490';
+    exports.NGCC_VERSION = '9.0.0-rc.1+872.sha-374a8ec';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
