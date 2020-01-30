659996b06aa794c877120aefc4c8e612e5941f57
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:04:24 +0000

    ee8b8f52aa feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+859.sha-a3de110';
+    exports.NGCC_VERSION = '9.0.0-rc.1+864.sha-ee8b8f5';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
