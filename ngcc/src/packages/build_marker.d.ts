46d79b290fd6ab5ac9d04af7b48a2bf9ac39b74d
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:43:41 +0000

    a25cb2d1e0 docs: release notes for the v9.0.0-rc.12 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+872.sha-374a8ec";
+export declare const NGCC_VERSION = "9.0.0-rc.1+871.sha-a25cb2d";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
