2c5ac1be8ac9ba5802ad8e2f9ce7a2274fea09ea
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:30:18 +0000

    374a8ec263 ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+870.sha-9f5b490";
+export declare const NGCC_VERSION = "9.0.0-rc.1+872.sha-374a8ec";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
