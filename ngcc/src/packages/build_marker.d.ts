e83ea6c556655d1679e2e997f4ee71b3646cf45e
Andrew Kushnir <akushnir@google.com>
2020-01-30 19:49:42 +0000

    cd9ae66b35 refactor(ivy): remove markDirty from global utilities (#34453)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+867.sha-d72cfc9";
+export declare const NGCC_VERSION = "9.0.0-rc.1+869.sha-cd9ae66";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+867.sha-d72cfc9';
+    exports.NGCC_VERSION = '9.0.0-rc.1+869.sha-cd9ae66';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
