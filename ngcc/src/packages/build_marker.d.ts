45e4d7a5f7c154ded41a95a57bd91b15a2461a0e
Andrew Kushnir <akushnir@google.com>
2020-01-30 21:08:57 +0000

    9f5b490800 Revert "refactor(compiler): refactor and export Bazel host creation. (#34686)" (#35063)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+869.sha-cd9ae66";
+export declare const NGCC_VERSION = "9.0.0-rc.1+870.sha-9f5b490";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
