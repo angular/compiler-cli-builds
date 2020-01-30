775da229860e6b89c48a045f4b01f3774f4c39e7
Andrew Kushnir <akushnir@google.com>
2020-01-30 18:44:22 +0000

    d72cfc9d57 docs: remove deployment section on golang because of errors (#34099)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+866.sha-37f39d6';
+    exports.NGCC_VERSION = '9.0.0-rc.1+867.sha-d72cfc9';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
