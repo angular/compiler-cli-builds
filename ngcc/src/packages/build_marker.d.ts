94014fbe8fbe9a1fedd13a18b34c6fbef06c4715
Misko Hevery <misko@hevery.com>
2020-01-31 21:10:33 +0000

    e672b1f2ac feat(docs-infra): change navigation in resources page (#34756)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+877.sha-6b710f7";
+export declare const NGCC_VERSION = "9.0.0-rc.1+880.sha-e672b1f";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
