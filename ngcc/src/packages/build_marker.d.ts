992be0d43a1485ed05826fb472f3d84e60515b67
Misko Hevery <misko@hevery.com>
2020-01-31 22:54:12 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+887.sha-58b29f1";
+export declare const NGCC_VERSION = "9.0.0-rc.1+888.sha-6d11a81";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
