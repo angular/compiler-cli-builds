d939195316f2064f6e8f139b011aba35550d2509
Misko Hevery <misko@hevery.com>
2020-01-31 21:07:21 +0000

    fcfce99e9e fix(common): remove extra & in http params (#34896)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+876.sha-c070037";
+export declare const NGCC_VERSION = "9.0.0-rc.1+878.sha-fcfce99";
 /**
  * Check whether ngcc has already processed a given entry-point format.
  *
