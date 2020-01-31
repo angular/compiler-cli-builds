2878e8a2b2a161df9442a57591ffea58ee1b587d
Misko Hevery <misko@hevery.com>
2020-01-31 19:33:55 +0000

    68f51e0adc ci: update pullapprove.yml to cover all files in the repo (#35072)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+871.sha-a25cb2d';
+    exports.NGCC_VERSION = '9.0.0-rc.1+873.sha-68f51e0';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
