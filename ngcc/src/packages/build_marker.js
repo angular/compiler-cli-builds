36cf8802a9870e44f620d5597b022524ffeaaa0f
Misko Hevery <misko@hevery.com>
2020-01-31 19:39:16 +0000

    69b52903f0 build: update to @bazel/protractor@1.2.0 (#35068)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+873.sha-68f51e0';
+    exports.NGCC_VERSION = '9.0.0-rc.1+874.sha-69b5290';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
