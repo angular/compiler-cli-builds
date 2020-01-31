79da5fbf26fb025ef95520aea74d2b898714c1a4
Misko Hevery <misko@hevery.com>
2020-01-31 22:55:47 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -18,7 +18,7 @@
      * found in the LICENSE file at https://angular.io/license
      */
     var file_system_1 = require("@angular/compiler-cli/src/ngtsc/file_system");
-    exports.NGCC_VERSION = '9.0.0-rc.1+887.sha-58b29f1';
+    exports.NGCC_VERSION = '9.0.0-rc.1+888.sha-6d11a81';
     /**
      * Check whether ngcc has already processed a given entry-point format.
      *
