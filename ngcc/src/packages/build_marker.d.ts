c73b810c8500dee340fb71ccc86c20398e18b98a
Misko Hevery <misko@hevery.com>
2020-02-01 03:04:10 +0000

    ab12529941 release: cut the v9.0.0-rc.13 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.12+24.sha-58dbe66";
+export declare const NGCC_VERSION = "9.0.0-rc.13";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
