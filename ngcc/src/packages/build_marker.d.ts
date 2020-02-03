6b0f880db58bd960df45ad754629045096bd7ecc
Miško Hevery <misko@hevery.com>
2020-02-03 22:25:31 +0000

    3c30474417 docs: update zone-bluebird patch document for angular (#34536)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+904.sha-9601b5d";
+export declare const NGCC_VERSION = "9.0.0-rc.1+905.sha-3c30474";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
