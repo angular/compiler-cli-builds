7e0fce3d4f9076b7bbbe3488bf79ab99288c34a1
Miško Hevery <misko@hevery.com>
2020-02-03 16:58:13 +0000

    a3c7ab99b7 feat: performance improvement for eventListeners (#34613)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.0.0-rc.1+896.sha-4d36b2f';
+    exports.NGCC_VERSION = '9.0.0-rc.1+897.sha-a3c7ab9';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
