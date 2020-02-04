af9e5aab6922508f187ad2ba89895576b557f5f9
Miško Hevery <misko@hevery.com>
2020-02-04 19:57:30 +0000

    7fb66eca25 docs: changes AoT to AOT for consistency (#35112)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+916.sha-6ddf550";
+export declare const NGCC_VERSION = "9.0.0-rc.1+919.sha-7fb66ec";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
