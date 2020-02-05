0d3ba94a5056fe218a86b93deb75609efb2b4da3
Miško Hevery <misko@hevery.com>
2020-02-05 00:20:54 +0000

    1f6da8c7d5 docs: fix typo in router animation (#35100)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+922.sha-aed5c7c";
+export declare const NGCC_VERSION = "9.0.0-rc.1+926.sha-1f6da8c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
