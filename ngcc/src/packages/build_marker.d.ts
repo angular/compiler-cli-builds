2a7cd36d47a53c586506303fa6c3935b9162a535
Misko Hevery <misko@hevery.com>
2020-02-06 23:58:56 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+944.sha-3c69442";
+export declare const NGCC_VERSION = "9.0.0-rc.1+948.sha-6fb538e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
