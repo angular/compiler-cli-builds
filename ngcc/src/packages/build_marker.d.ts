ffcf64b364e7fb681eb2a9113e307e5bfb0d9377
Misko Hevery <misko@hevery.com>
2020-02-06 17:54:50 +0000

    c286fbb9dc docs(ivy): add size debugging section (#35178)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+933.sha-7526ef7";
+export declare const NGCC_VERSION = "9.0.0-rc.1+934.sha-c286fbb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
