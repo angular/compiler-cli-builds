fb404c4792498013d119df4029f02190500a5793
Misko Hevery <misko@hevery.com>
2020-02-06 23:16:53 +0000

    49dd7b6a0a docs: consilidate release notes for the v8.0.0 release (#35197)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+941.sha-532ccb6";
+export declare const NGCC_VERSION = "9.0.0-rc.1+942.sha-49dd7b6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
