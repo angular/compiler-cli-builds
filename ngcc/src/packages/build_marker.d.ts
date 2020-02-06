90c0e44faedcd7d94b5ee9e6cee31f847c8391e1
Misko Hevery <misko@hevery.com>
2020-02-06 22:07:22 +0000

    053dabb7ed docs: consolidate and remove rc/next notes from the v9.0.0 release (#35195)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+937.sha-22acdc2";
+export declare const NGCC_VERSION = "9.0.0-rc.1+938.sha-053dabb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
