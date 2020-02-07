f80dc94a340f49fb300c8337e4a7a380b3906752
Misko Hevery <misko@hevery.com>
2020-02-07 00:38:35 +0000

    14fc1812f3 ci: re-enable android 7 saucelabs tests (#35171)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0+10.sha-60b8870";
+export declare const NGCC_VERSION = "9.0.0+19.sha-14fc181";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
