236c70a687ca6062b98f9962f6d92da966522d0d
Kara Erickson <karakara@google.com>
2020-02-07 19:57:26 +0000

    f7e1c21596 docs: fix awkard space due to margin in sections with `alert` class (#35113)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+961.sha-1c85c24";
+export declare const NGCC_VERSION = "9.0.0-rc.1+965.sha-f7e1c21";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
