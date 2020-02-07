544083e020cdb572202f270313b44b47f0db7571
Misko Hevery <misko@hevery.com>
2020-02-07 00:02:00 +0000

    10ad984334 docs: Clarifies code section is a continuation from the section above (#35111)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+948.sha-6fb538e";
+export declare const NGCC_VERSION = "9.0.0-rc.1+946.sha-10ad984";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
