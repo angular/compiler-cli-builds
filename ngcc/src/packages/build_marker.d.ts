9e09f5321ad101a4da0b98ed990060a8590967a6
Kara Erickson <karakara@google.com>
2020-02-07 20:01:53 +0000

    daac33cdc8 feat: add basic jest support (#35080)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+965.sha-f7e1c21";
+export declare const NGCC_VERSION = "9.0.0-rc.1+966.sha-daac33c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
