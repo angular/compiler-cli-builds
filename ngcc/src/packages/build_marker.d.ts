dd4768430e14f8c383ec02374d03203a87847e96
Kara Erickson <karakara@google.com>
2020-02-08 00:35:02 +0000

    d5d9971c28 refactor(benchpress): delete outdated/unused folder (#35147)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+968.sha-26b17ae";
+export declare const NGCC_VERSION = "9.0.0-rc.1+972.sha-d5d9971";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
