e557b8dacd7d09b6ab94d10533670fc03827614d
Kara Erickson <karakara@google.com>
2020-02-11 21:51:05 +0000

    b9b38f0ad2 docs: Repetition on getting started tutorial (#35290)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+988.sha-afdd405";
+export declare const NGCC_VERSION = "9.0.0-rc.1+989.sha-b9b38f0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
