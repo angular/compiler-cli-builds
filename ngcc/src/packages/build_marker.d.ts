ff699783335194d9ec24e5523426805d377626d2
Kara Erickson <karakara@google.com>
2020-02-11 00:55:46 +0000

    a92d97cda7 fix(compiler): report errors for missing binding names (#34595)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+977.sha-76b77ec";
+export declare const NGCC_VERSION = "9.0.0-rc.1+982.sha-a92d97c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
