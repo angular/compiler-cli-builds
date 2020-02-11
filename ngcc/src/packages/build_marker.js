32762a1bbad4c32c9d8735bb5e90aed79fd932ff
Kara Erickson <karakara@google.com>
2020-02-11 00:45:01 +0000

    e1160f19be feat: make jasmine patch as zone module (#34676)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.0.0-rc.1+977.sha-76b77ec';
+    exports.NGCC_VERSION = '9.0.0-rc.1+981.sha-e1160f1';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
