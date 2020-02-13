cf1d262709cd42384b8472541c0e3d03fb1fb3b4
Kara Erickson <karakara@google.com>
2020-02-13 01:36:38 +0000

    a30fd2993b fix(ivy): error if directive with synthetic property binding is on same node as directive that injects ViewContainerRef (#35343)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.1+6.sha-a84093a";
+export declare const NGCC_VERSION = "9.0.1+7.sha-a30fd29";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
