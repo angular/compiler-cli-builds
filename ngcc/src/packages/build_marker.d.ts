bfb9272bd914b113f2cf3df98ef723fc4388e585
Kara Erickson <karakara@google.com>
2020-02-13 01:08:24 +0000

    e8ea3be843 docs: remove service from region where it was added before it was created (#35354)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.0-rc.1+991.sha-4aeb6c4";
+export declare const NGCC_VERSION = "9.1.0-next.0+6.sha-e8ea3be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
