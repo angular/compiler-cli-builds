ab4ece7bfa58512cbbde150f7d87140c641f7ffe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:38:18 +0000

    b9b512f729 fix(ivy): `LFrame` needs to release memory on `leaveView()` (#35156)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.0+20.sha-27cd01f";
+export declare const NGCC_VERSION = "9.1.0-next.0+21.sha-b9b512f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
