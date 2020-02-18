1c662f3817b8a124db4cc3db30e85619fc8a9442
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:11:28 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.0+26.sha-d05629b";
+export declare const NGCC_VERSION = "9.1.0-next.0+29.sha-d7c4f40";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
