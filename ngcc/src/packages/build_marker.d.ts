24c4985d073ef4bf084472647787993128a5d71b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 22:18:08 +0000

    183a8629a4 fix(docs-infra): set `th`/`td` to proper width (#35437)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.0+36.sha-2f18e85";
+export declare const NGCC_VERSION = "9.1.0-next.0+37.sha-183a862";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
