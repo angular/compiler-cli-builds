0ba80e6470f1da1ba86f2aa001b06554e0b08692
Miško Hevery <misko@hevery.com>
2020-02-24 17:33:21 +0000

    89f6b341a3 docs: add amazon s3 builder package (#34783)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.2+27.sha-bb09cd0";
+export declare const NGCC_VERSION = "9.0.2+41.sha-89f6b34";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
