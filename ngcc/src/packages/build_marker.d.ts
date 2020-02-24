c4eee2e7a8b0eef570a92a5fa3a6aae53d4ba47e
Miško Hevery <misko@hevery.com>
2020-02-24 17:30:53 +0000

    53f059ee8f fix(localize): improve placeholder mismatch error message (#35593)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.1+45.sha-71b5970";
+export declare const NGCC_VERSION = "9.1.0-next.1+44.sha-53f059e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
