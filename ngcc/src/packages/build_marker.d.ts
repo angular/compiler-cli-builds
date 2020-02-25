210eddd20cd5a18bd151c6a8c23e627c5dc35cff
Miško Hevery <misko@hevery.com>
2020-02-25 21:47:35 +0000

    d6904882d0 fix(ivy): error when accessing NgModuleRef.componentFactoryResolver in constructor (#35637)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.2+47.sha-8cac5fe";
+export declare const NGCC_VERSION = "9.0.2+54.sha-d690488";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
