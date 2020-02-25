2eaa1be9c598b163754b59c50bdaab6aaa42bc1e
Miško Hevery <misko@hevery.com>
2020-02-25 21:40:16 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.1+61.sha-1e20b2c";
+export declare const NGCC_VERSION = "9.1.0-next.1+59.sha-8e354da";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
