372cd800e68dd9f4952e8dc52200cf0799a15e59
Miško Hevery <misko@hevery.com>
2020-02-25 01:44:41 +0000

    c839c05620 fix(router): removed unused ApplicationRef dependency (#35642)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.1+46.sha-3893799";
+export declare const NGCC_VERSION = "9.1.0-next.1+48.sha-c839c05";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
