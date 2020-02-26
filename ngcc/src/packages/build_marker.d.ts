68c4f0645cb10aa0a7d72af6afbeb3e0c52a54a1
Miško Hevery <misko@hevery.com>
2020-02-26 21:13:58 +0000

    8fbb966ca7 docs(core): describe your change... (#34513)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.1+64.sha-6f68eae";
+export declare const NGCC_VERSION = "9.1.0-next.1+65.sha-8fbb966";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
