f82cf97db0bd5e7a0f0891deab168309ccc7d27e
Miško Hevery <misko@hevery.com>
2020-02-26 17:27:19 +0000

    406ce8c884 refactor(platform-browser): Hoist functions to workaround optimization bug. (#32230)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.2+54.sha-d690488";
+export declare const NGCC_VERSION = "9.0.2+55.sha-406ce8c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
