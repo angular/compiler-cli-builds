3fa562c4e63b5efd37c058038606975da8b1bdd5
atscott <atscott@google.com>
2020-02-27 22:28:22 +0000

    287d841486 build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.3+9.sha-9b53054";
+export declare const NGCC_VERSION = "9.0.4+3.sha-287d841";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
