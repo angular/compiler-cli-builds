a3cf3ca1f913518d7ac0bc6e0fb773439f9a9452
Matias Niemelä <matias@yearofmoo.com>
2020-02-27 21:30:30 +0000

    9b53054ea8 refactor(ngcc): guard against a crash if source-map flattening fails (#35718)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.3+6.sha-bf6fbf5";
+export declare const NGCC_VERSION = "9.0.3+9.sha-9b53054";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
