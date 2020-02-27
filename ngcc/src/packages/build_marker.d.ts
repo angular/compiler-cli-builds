65cb1d02c6f2de8cf28195f33bcaef0763cfad17
atscott <atscott@google.com>
2020-02-27 00:54:10 +0000

    91934ebb5c ci: increase AIO payload size limit (#35702)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.1+72.sha-975a11b";
+export declare const NGCC_VERSION = "9.1.0-next.1+85.sha-91934eb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
