0d58525361a6250d594b2f67d17004c5a8a51292
atscott <atscott@google.com>
2020-02-27 22:22:12 +0000

    8fed1fe792 perf(core): adding micro benchmark for host bindings (#35705)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+10.sha-bc7b589";
+export declare const NGCC_VERSION = "9.1.0-next.2+11.sha-8fed1fe";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
