13a38b8e320ab50ad2c10bf53eee063c7c86419e
atscott <atscott@google.com>
2020-02-28 20:49:43 +0000

    ef75875ca3 fix(core): handle `<ng-template>` with local refs in i18n blocks (#35758)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+23.sha-4003538";
+export declare const NGCC_VERSION = "9.1.0-next.2+24.sha-ef75875";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
