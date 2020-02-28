6950c1caeff864b570db3179dd35ef238616174b
atscott <atscott@google.com>
2020-02-28 15:41:28 +0000

    208ef7bd62 refactor(compiler): Remove NullAstVisitor and visitAstChildren (#35619)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+18.sha-73ee57a";
+export declare const NGCC_VERSION = "9.1.0-next.2+20.sha-208ef7b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
