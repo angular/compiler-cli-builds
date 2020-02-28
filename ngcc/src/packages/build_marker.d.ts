9b689d3c3dc9dde5a25441e89d744b8a0c94604e
atscott <atscott@google.com>
2020-02-28 16:03:33 +0000

    fcad075ade fix(ivy): narrow `NgIf` context variables in template type checker (#35125)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.4+4.sha-52fc087";
+export declare const NGCC_VERSION = "9.0.4+10.sha-fcad075";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
