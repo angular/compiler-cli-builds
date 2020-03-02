f69ac8bee68dea312f6f91b9f6f9deba3f879042
atscott <atscott@google.com>
2020-03-02 16:46:36 +0000

    40da51f641 fix(compiler): support i18n attributes on `<ng-template>` tags (#35681)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+32.sha-35c9f0d";
+export declare const NGCC_VERSION = "9.1.0-next.2+33.sha-40da51f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
