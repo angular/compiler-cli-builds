d4962f04599a18c4fe63c200bff7d7d09cb1c19b
atscott <atscott@google.com>
2020-03-02 16:50:51 +0000

    a153b61098 fix(core): treat `[class]` and `[className]` as unrelated bindings (#35668)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+33.sha-40da51f";
+export declare const NGCC_VERSION = "9.1.0-next.2+35.sha-a153b61";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
