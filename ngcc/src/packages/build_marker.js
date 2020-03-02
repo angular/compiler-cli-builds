025c51a2f452e058de08997c0a6043d0c07a85b7
atscott <atscott@google.com>
2020-03-02 21:53:29 +0000

    8eb4a9d395 docs: fix docs and associated code snippets for enabling more macro tasks in `fakeAsync()` (#35778)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+38.sha-4ba4d90';
+    exports.NGCC_VERSION = '9.1.0-next.2+40.sha-8eb4a9d';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
