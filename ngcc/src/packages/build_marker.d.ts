8aef1085865464d36b6204d91cf21fc5d0dffb05
atscott <atscott@google.com>
2020-03-02 17:53:04 +0000

    44e47da4cf docs: reword explanation for using singleton services decorative to be more understandable (#35002)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+35.sha-a153b61";
+export declare const NGCC_VERSION = "9.1.0-next.2+36.sha-44e47da";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
