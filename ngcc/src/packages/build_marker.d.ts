8fce9aace1ec09ef2214907374aeed63455297f6
atscott <atscott@google.com>
2020-03-03 18:04:46 +0000

    c7d0567d37 Revert "docs: add comment markers for internal goog.define replacement (#32795)" (#35830)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+54.sha-5615928";
+export declare const NGCC_VERSION = "9.1.0-next.2+55.sha-c7d0567";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
