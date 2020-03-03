cb5e59f35fd6b5b50c28d0e7077ba32ef6eef780
atscott <atscott@google.com>
2020-03-03 17:28:49 +0000

    89b5b97368 build: avoid running duplicate tests in language service (#35816)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+48.sha-0653db1";
+export declare const NGCC_VERSION = "9.1.0-next.2+51.sha-89b5b97";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
