ec474ee293c233fbf8dd82290ba09fb35eef7016
atscott <atscott@google.com>
2020-03-03 17:16:15 +0000

    c195d22f68 fix(core): remove side effects from `ɵɵgetInheritedFactory()` (#35769)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+41.sha-47a1811";
+export declare const NGCC_VERSION = "9.1.0-next.2+46.sha-c195d22";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
