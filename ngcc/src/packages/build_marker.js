68e27e9d27e00203921962eadd9ea138c7a42ec0
atscott <atscott@google.com>
2020-03-04 20:14:27 +0000

    b57be3bb41 docs: release notes for the v9.1.0-next.3 release

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+72.sha-dcf28bf';
+    exports.NGCC_VERSION = '9.1.0-next.2+73.sha-b57be3b';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
