fe1435e8c78065c95f7cae29d3351605ef019faf
Andrew Scott <atscott01@gmail.com>
2020-03-04 19:19:04 +0000

    1b8a66893e Revert "build(docs-infra): use local version of Zone.js when testing against local packages (#35780)" (#35857)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+62.sha-2e728f7";
+export declare const NGCC_VERSION = "9.1.0-next.2+71.sha-1b8a668";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
