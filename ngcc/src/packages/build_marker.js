5ba5e666f9cc1b0ba094e02805983200291c9e37
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:21:54 +0000

    e0a35e13d5 perf(ngcc): reduce directory traversing (#35756)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+75.sha-a1d0396';
+    exports.NGCC_VERSION = '9.1.0-next.2+77.sha-e0a35e1';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
