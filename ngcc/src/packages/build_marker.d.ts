83b12ab880974f20fafc771bb81ec653426abb75
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:07:19 +0000

    4e1d780554 fix(language-service): resolve the real path for symlink (#35895)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+89.sha-59078c4";
+export declare const NGCC_VERSION = "9.1.0-next.2+88.sha-4e1d780";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
