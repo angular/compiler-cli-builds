23a73dd57aa71bc948cee8b667360213d5d275ca
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:17:25 +0000

    84c2458ea7 refactor: cleanup zone.js test instruction (#34860)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+93.sha-c06f9a1";
+export declare const NGCC_VERSION = "9.1.0-next.2+94.sha-84c2458";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
