8f3c0eb9fe2d4857fcb891f135b96eb61efb530f
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:57:58 +0000

    958165888c fix(bazel): do not use manifest paths for generated imports within compilation unit (#35841)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+96.sha-82a6fc5';
+    exports.NGCC_VERSION = '9.1.0-next.2+98.sha-9581658';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
