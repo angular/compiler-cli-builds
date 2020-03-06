f1f99d8d67c5f50ecfd80bdca9a82a4f5f8aa22c
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 23:01:46 +0000

    9e23a69d1b refactor: export silentLogger not SilentLoggerFn (#34079)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.5+12.sha-28d2bf7";
+export declare const NGCC_VERSION = "9.0.5+22.sha-9e23a69";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
