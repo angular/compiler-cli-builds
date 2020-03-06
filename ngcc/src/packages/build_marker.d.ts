b8d480d11a83e07f1794a6094a704437b3c89159
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 00:17:03 +0000

    2cccf59415 build: create dev-infra-private npm package (#35862)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.5+5.sha-2a9b254";
+export declare const NGCC_VERSION = "9.0.5+6.sha-2cccf59";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
