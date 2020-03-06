b4c38977ece1eb7f379ee62b110dc01a73622fd0
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:04:38 +0000

    59078c444c docs: move ngIndia 2020 to past events on events page (#35901)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+91.sha-59607dc";
+export declare const NGCC_VERSION = "9.1.0-next.2+89.sha-59078c4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+91.sha-59607dc';
+    exports.NGCC_VERSION = '9.1.0-next.2+89.sha-59078c4';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
