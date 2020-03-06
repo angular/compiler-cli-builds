c2c96a26c44328d6c51485b320faabf38f3b83ee
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:59:48 +0000

    958165888c fix(bazel): do not use manifest paths for generated imports within compilation unit (#35841)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+96.sha-82a6fc5";
+export declare const NGCC_VERSION = "9.1.0-next.2+98.sha-9581658";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
