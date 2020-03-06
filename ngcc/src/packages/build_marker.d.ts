f5a5dd2defd7dbf4b7bef247ca0055779cc0dd5a
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:56:42 +0000

    55b3f97be0 fix(zone.js): `tickOptions`'s `processNewMacroTasksSynchronously` should default to true when flag omitted (#35814)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+95.sha-86a45ff";
+export declare const NGCC_VERSION = "9.1.0-next.2+99.sha-55b3f97";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
