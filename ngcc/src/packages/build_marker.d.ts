5a03da32b351fed2f4ccdc5832d3bf93d1e1938e
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:57:29 +0000

    82a6fc5ef9 build(docs-infra): use local version of Zone.js when testing against local packages (#35858)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+99.sha-55b3f97";
+export declare const NGCC_VERSION = "9.1.0-next.2+96.sha-82a6fc5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
