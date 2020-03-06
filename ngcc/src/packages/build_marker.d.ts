0e749cf7909886e8a4790a06efe22dfc0af2ad2b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:23:37 +0000

    86a45ff561 docs(zone.js): update coments of zone configuration API definition (#35849)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+94.sha-84c2458";
+export declare const NGCC_VERSION = "9.1.0-next.2+95.sha-86a45ff";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
