c6abc659242f9a0edc5c21552c0df9cd626aefee
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:14:18 +0000

    28d2bf7d7c docs(zone.js): update comment and chapter format of ngzone doc. (#35738)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.5+10.sha-eaf5b58";
+export declare const NGCC_VERSION = "9.0.5+12.sha-28d2bf7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
