c8f7bf6ccdb4b476a580abc240b1e4ec23e2e5c2
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:28:20 +0000

    15482e7367 Revert "feat(bazel): transform generated shims (in Ivy) with tsickle (#35848)" (#35970)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+110.sha-fc4c3c3";
+export declare const NGCC_VERSION = "9.1.0-next.2+112.sha-15482e7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
