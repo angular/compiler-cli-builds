98b795a1a8e7f5514d2bf9a3ff6b8f3e87d39c4b
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 18:47:13 +0000

    fc4c3c3eb5 fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+111.sha-9ff9a07";
+export declare const NGCC_VERSION = "9.1.0-next.2+110.sha-fc4c3c3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
