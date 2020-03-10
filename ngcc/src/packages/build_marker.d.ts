4c255efaa5df259821d7901ed8f6f261e142ebab
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:14:49 +0000

    64d6f13b87 fix(platform-browser): add missing peerDependency on `@angular/animations` (#35949)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+120.sha-287bfef";
+export declare const NGCC_VERSION = "9.1.0-next.2+119.sha-64d6f13";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+120.sha-287bfef';
+    exports.NGCC_VERSION = '9.1.0-next.2+119.sha-64d6f13';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
