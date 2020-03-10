d68a294356207aecb2cb6b7684a834073eb249e0
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:00:17 +0000

    e4b1e6c622 build: Add pullapprove verification tool to dev-infra-private package (#35911)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+122.sha-983f481";
+export declare const NGCC_VERSION = "9.1.0-next.2+127.sha-e4b1e6c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
