9ae091ea3c94dc62b8239f5028e66336df4b174d
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:58:21 +0000

    983f48136a test(compiler): add a public API guard for the public compiler options (#35885)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+119.sha-64d6f13";
+export declare const NGCC_VERSION = "9.1.0-next.2+122.sha-983f481";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
