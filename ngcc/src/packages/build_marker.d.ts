c9a5b5a06235ae7e5132765a77a667043956b1bc
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:14:35 +0000

    287bfefade ci: update components-repo-unit-tests job commit (#35961)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.2+116.sha-3d46a45";
+export declare const NGCC_VERSION = "9.1.0-next.2+120.sha-287bfef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
