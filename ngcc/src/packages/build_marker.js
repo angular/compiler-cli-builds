8ba882068fbb93bdb1b6256befc6ac90a219edaf
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:05:34 +0000

    406419bc0f fix(language-service): fix calculation of pipe spans (#35986)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.2+113.sha-71309d2';
+    exports.NGCC_VERSION = '9.1.0-next.2+115.sha-406419b';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
