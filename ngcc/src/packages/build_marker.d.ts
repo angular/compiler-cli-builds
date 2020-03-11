31854d1c868170da4432c60d048c7571ade6a76b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:17:07 +0000

    77da6887f7 build(dev-infra): handle async failures in `get-data.ts` (#36007)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+22.sha-d8f9ec5";
+export declare const NGCC_VERSION = "9.1.0-next.4+16.sha-77da688";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
