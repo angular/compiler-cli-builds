5841e548c54087e37ac5700e485a9ab94e6f1edf
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:05:45 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+34.sha-99ea5d7";
+export declare const NGCC_VERSION = "9.1.0-next.4+35.sha-2b6028b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
