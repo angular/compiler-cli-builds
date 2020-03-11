84aad1fd881b4a81e97e5a412b862c360c602216
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:39:12 +0000

    13495c64f7 docs(dev-infra): update triage and contributing docs for dev-infra (#35995)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+3.sha-6f95bc9";
+export declare const NGCC_VERSION = "9.1.0-next.4+7.sha-13495c6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
