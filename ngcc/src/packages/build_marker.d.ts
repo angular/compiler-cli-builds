cffc4cc311e2df32e964fe98b3a56bc493c8c00d
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:10:28 +0000

    b99b80c18b fix(dev-infra): do not run the `lock-closed` GitHub action on forks (#36000)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+11.sha-1f89c61";
+export declare const NGCC_VERSION = "9.1.0-next.4+17.sha-b99b80c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
