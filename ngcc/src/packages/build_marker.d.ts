a8e6ea60f3a0fb65879096d9927ee030f84c4e04
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:14:40 +0000

    9a1240360b build: add more info to SauceLabs README (#35704)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+12.sha-10f1e94";
+export declare const NGCC_VERSION = "9.1.0-next.4+18.sha-9a12403";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
