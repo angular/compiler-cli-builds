551ac2ae8dba35484ef3f44835a932a784691f39
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:33:27 +0000

    0d806c84e2 docs: add developer survey 2020 (#35655)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+35.sha-2b6028b";
+export declare const NGCC_VERSION = "9.1.0-next.4+36.sha-0d806c8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.4+35.sha-2b6028b';
+    exports.NGCC_VERSION = '9.1.0-next.4+36.sha-0d806c8';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
