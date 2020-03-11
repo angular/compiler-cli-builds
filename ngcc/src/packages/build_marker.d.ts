9b8b247023dc205fc9b5d9eba17c52e7096ea88b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:26:06 +0000

    146ef48a6e docs: change grammar in architecture guide (#35541)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+25.sha-32f099a";
+export declare const NGCC_VERSION = "9.1.0-next.4+19.sha-146ef48";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
