fdb8a990a871403a3ae7a6a91492bb144db55736
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:10:21 +0000

    1f89c6130e fix(ngcc): show helpful error when providing an invalid option (#36010)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+8.sha-42375c4";
+export declare const NGCC_VERSION = "9.1.0-next.4+11.sha-1f89c61";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
