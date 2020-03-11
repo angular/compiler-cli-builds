4764c09e23948ce475aadcfb47b5a6415c7db385
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:38:10 +0000

    19cfaf7f4c fix(core): don't re-invoke pure pipes that throw and arguments are the same (#35827)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+1.sha-3aa5ae9";
+export declare const NGCC_VERSION = "9.1.0-next.4+4.sha-19cfaf7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
