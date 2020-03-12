d83e9c5a849a2d56b568a81c8f2d590558edf6bf
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:38:41 +0000

    63815b54f3 test(core): Add benchpress test for transplanted view change detection (#36001)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+40.sha-372a56a";
+export declare const NGCC_VERSION = "9.1.0-next.4+41.sha-63815b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
