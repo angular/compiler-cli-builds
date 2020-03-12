7e16cea80ece3106071bb075698255133694aad3
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:11:32 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+36.sha-0d806c8";
+export declare const NGCC_VERSION = "9.1.0-next.4+37.sha-37a4839";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
