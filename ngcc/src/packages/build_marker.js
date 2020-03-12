0fb1a99f76cfcdf0df78e50305f874c208bcc2fd
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:50:10 +0000

    f699c962bb test(core): Add benchpress test for transplanted view change detection (#36001)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.0.6+27.sha-dec80b0';
+    exports.NGCC_VERSION = '9.0.6+28.sha-f699c96';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
