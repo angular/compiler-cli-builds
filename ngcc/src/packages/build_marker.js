f4abd1cd1e5bdca9469a671b586720917351334f
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:23:18 +0000

    db74f2074b build: enable forms tests on IE (#35971)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.4+49.sha-772bb5e';
+    exports.NGCC_VERSION = '9.1.0-next.4+50.sha-db74f20';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
