42d7898c09e4f399b5c153483abe4ae52109b541
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:45:47 +0000

    9e698b7cca fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.6+36.sha-a8cc6a2";
+export declare const NGCC_VERSION = "9.0.6+37.sha-9e698b7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
