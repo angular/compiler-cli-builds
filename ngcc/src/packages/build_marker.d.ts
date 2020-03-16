c9f683bb65fee5339a0185f1c8b455d91f431823
Andrew Kushnir <akushnir@google.com>
2020-03-16 23:08:24 +0000

    e552591768 release: cut the zone.js-0.10.3 release (#34508)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+60.sha-46fe8fb";
+export declare const NGCC_VERSION = "9.1.0-next.4+61.sha-e552591";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
