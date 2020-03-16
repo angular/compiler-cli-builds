5636b5c5750b242cc02b002aa247ac3308b86979
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:17:51 +0000

    a52b5680cb fix(compiler): do not recurse to find static symbols of same module (#35262)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.6+39.sha-1342668";
+export declare const NGCC_VERSION = "9.0.6+41.sha-a52b5680";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
