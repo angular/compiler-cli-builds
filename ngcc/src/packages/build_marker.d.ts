fa5789f5b39c8b85cce1bca2f0c1a81e570ee8da
Andrew Kushnir <akushnir@google.com>
2020-03-17 20:58:06 +0000

    76d7188872 fix(ngcc): consistently delegate to TypeScript host for typing files (#36089)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.6+56.sha-3c19a5d";
+export declare const NGCC_VERSION = "9.0.6+58.sha-76d7188";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
