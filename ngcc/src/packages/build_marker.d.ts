9ceddc1985126e3053171dd17b6991ea49f0e331
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:51:52 +0000

    acc483e2eb feat(language-service): improve non-callable error message (#35271)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+64.sha-61c3795";
+export declare const NGCC_VERSION = "9.1.0-next.4+63.sha-acc483e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
