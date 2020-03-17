a29dd9f388f769925214410fc7e64837c157751e
Andrew Kushnir <akushnir@google.com>
2020-03-17 00:06:08 +0000

    ec750caff0 refactor(core): misc refactoring to support upcoming changes to improve synthetic host bindings (#35884)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.6+41.sha-a52b5680";
+export declare const NGCC_VERSION = "9.0.6+42.sha-ec750ca";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
