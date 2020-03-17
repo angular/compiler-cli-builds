fbe01c3fd2efb1d2a85662a723b5cab8ab843ec8
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:49:21 +0000

    61c37954c6 docs: remove browserstack badge from readme (#35684)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+62.sha-168a393";
+export declare const NGCC_VERSION = "9.1.0-next.4+64.sha-61c3795";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-next.4+62.sha-168a393';
+    exports.NGCC_VERSION = '9.1.0-next.4+64.sha-61c3795';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
