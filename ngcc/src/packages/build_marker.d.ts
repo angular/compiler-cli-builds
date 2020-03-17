7670b6d72a45098502b34b05bb9b317d435cc0d6
Andrew Kushnir <akushnir@google.com>
2020-03-17 20:03:12 +0000

    3c19a5d49f build: fix ts-api-guardian golden approval not working on windows (#36115)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.0.6+47.sha-70425d2";
+export declare const NGCC_VERSION = "9.0.6+56.sha-3c19a5d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
