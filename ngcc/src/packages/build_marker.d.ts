9fb81117744b47ed8bd31a5252fccf470d1f2dd8
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:21:26 +0000

    67b9adfd1a docs(upgrade): remove outdated/non-applicable AOT section from upgrade guide (#36015)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+80.sha-9e70bcb";
+export declare const NGCC_VERSION = "9.1.0-next.4+101.sha-67b9adf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
