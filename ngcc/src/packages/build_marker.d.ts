90eb5fb8a1d4833400cabe8942d0bf012af8d949
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:28:29 +0000

    1aa5783a45 docs: adding a note on extra text interpolations and HTML tags in translations to Ivy guide (#36029)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-next.4+101.sha-67b9adf";
+export declare const NGCC_VERSION = "9.1.0-next.4+102.sha-1aa5783";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
