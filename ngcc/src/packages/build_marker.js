8c8fa5a266c427b93d6e171d33ed58507b64cc31
Misko Hevery <misko@hevery.com>
2020-03-20 17:48:40 +0000

    912692137a fix(docs-infra): fix image name in example (#36127)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-rc.0+2.sha-df890d7';
+    exports.NGCC_VERSION = '9.1.0-rc.0+6.sha-9126921';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
