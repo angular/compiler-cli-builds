9a00a3cfcc33783fc66f56a62c2c8587d5257575
Misko Hevery <misko@hevery.com>
2020-03-23 18:55:47 +0000

    e81ad3a1bc docs: Add asterisk info in template syntax guide (#36176)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-rc.0+13.sha-0ce8ad3';
+    exports.NGCC_VERSION = '9.1.0-rc.0+16.sha-e81ad3a';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
