70f793e49d3209428fcbb5d5ce2dd2dcc69dda4a
Misko Hevery <misko@hevery.com>
2020-03-24 17:37:03 +0000

    58f4254fba fix(dev-infra): use @angular/dev-infra-private package for pullapprove verification (#35996)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+26.sha-380de1e";
+export declare const NGCC_VERSION = "9.1.0-rc.0+22.sha-58f4254";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
