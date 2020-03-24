59ca5a05a4a523046ec3ef931e1179a3e4e4f74e
Misko Hevery <misko@hevery.com>
2020-03-24 17:36:30 +0000

    380de1e7b4 fix(ngcc): use path-mappings from tsconfig in dependency resolution (#36180)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+21.sha-4419907";
+export declare const NGCC_VERSION = "9.1.0-rc.0+26.sha-380de1e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
