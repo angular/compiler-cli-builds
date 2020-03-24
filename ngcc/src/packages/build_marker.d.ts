09d3ed2b9228b97bde0b8f8b36eabeb1a807466c
Misko Hevery <misko@hevery.com>
2020-03-24 17:51:54 +0000

    9ba46d9f88 fix(elements): correctly handle setting inputs to `undefined` (#36140)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+28.sha-55dac05";
+export declare const NGCC_VERSION = "9.1.0-rc.0+30.sha-9ba46d9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
