c779f4ebd64b760ed52d7ba07ad41f0fdf756688
Misko Hevery <misko@hevery.com>
2020-03-25 19:12:44 +0000

    d37dad82f1 build: ensure that refs and shas for PRs only need to be requested once (#36207)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+36.sha-22710fc";
+export declare const NGCC_VERSION = "9.1.0-rc.0+37.sha-d37dad8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
