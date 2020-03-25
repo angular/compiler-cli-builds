90d110b317536bb8bd0b4909920602d94346d9ad
Misko Hevery <misko@hevery.com>
2020-03-25 21:37:36 +0000

    fc3e5cb6d3 docs: coalesce release notes for the v9.1.0 release (#36247)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+37.sha-d37dad8";
+export declare const NGCC_VERSION = "9.1.0-rc.0+38.sha-fc3e5cb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
