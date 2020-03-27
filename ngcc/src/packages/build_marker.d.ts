012cd448d1e1dc518d3f3aafff169d8221c3c79d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:48:49 +0000

    0baab0f3db docs: update release nots with blog post (#36268)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0+2.sha-294c629";
+export declare const NGCC_VERSION = "9.1.0+14.sha-0baab0f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
