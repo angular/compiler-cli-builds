3a010ca94f946fa541a802a1b1479b6ec4cc1a76
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 21:35:35 +0000

    5cee709266 fix(ngcc): do not spawn more processes than intended in parallel mode (#36280)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+53.sha-2d0847c";
+export declare const NGCC_VERSION = "9.1.0-rc.0+54.sha-5cee709";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
