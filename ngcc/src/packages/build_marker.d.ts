cb4de1d31819ff607397741d608cbb6776e445ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:10 +0000

    8e0dec538e docs: clarify observables doc with new titles and tooltips (#36023)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+46.sha-bfa7b1a";
+export declare const NGCC_VERSION = "9.1.0-rc.0+45.sha-8e0dec5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
