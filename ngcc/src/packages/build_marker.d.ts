318e3d732361fc4510a71796900e6be2ec10ec8a
Igor Minar <igor@angular.io>
2020-03-29 06:42:41 +0000

    c3b297a318 refactor(dev-infra): fix lint warnings for Pullapprove-related scripts (#36287)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0+15.sha-6ea232e";
+export declare const NGCC_VERSION = "9.1.0+16.sha-c3b297a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
