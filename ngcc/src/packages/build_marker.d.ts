f3aa8c59bdf27c78b329f0e7f4cccd50db835f12
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:54:40 +0000

    0ab244f39e docs: fix typo in Schematics guide (#36328)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0+20.sha-15cffa2";
+export declare const NGCC_VERSION = "9.1.0+22.sha-0ab244f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
