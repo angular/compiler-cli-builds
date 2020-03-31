dab5e97a65b68f19b6404a69e8a1cc8f5cbd669f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:29:02 +0000

    08348fc2e8 build(docs-infra): rename duplicate test name (#36348)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+65.sha-63fbc71";
+export declare const NGCC_VERSION = "9.1.0-rc.0+66.sha-08348fc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
