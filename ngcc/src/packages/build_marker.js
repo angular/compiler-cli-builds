7ae54f32366d9b2977700ac512337547c8fb6d6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:33:41 +0000

    e8f3c47b03 build(docs-infra): rename duplicate test name (#36348)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0+22.sha-0ab244f';
+    exports.NGCC_VERSION = '9.1.0+26.sha-e8f3c47';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
