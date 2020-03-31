5a693617dbfb6b23d80872ce3047c4314e7e11f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:23:30 +0000

    c5df9ce474 build(zone.js): update zone.js version to 0.10.3 (#36214)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+64.sha-b44f7b5";
+export declare const NGCC_VERSION = "9.1.0-rc.0+63.sha-c5df9ce";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
