1426860c8b99fc8bd9415161f566c1c0dd82a9d2
Kara Erickson <karakara@google.com>
2020-04-01 23:01:28 +0000

    a5eb0e56b6 docs(zone.js): fix typos and align formatting (#35907)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+86.sha-24a9247";
+export declare const NGCC_VERSION = "9.1.0-rc.0+88.sha-a5eb0e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
