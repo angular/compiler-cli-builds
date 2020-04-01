16013f7d5e55ef792b56d7e1e05c7d9337446801
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:53:54 +0000

    cc4b813e75 fix(ngcc): handle bad path mappings when finding entry-points (#36331)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+82.sha-5516802";
+export declare const NGCC_VERSION = "9.1.0-rc.0+81.sha-cc4b813";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
