e8b2723dc9427a2bd30f442206bf9695ba01b068
Kara Erickson <karakara@google.com>
2020-04-01 20:33:20 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+67.sha-ec8bae1";
+export declare const NGCC_VERSION = "9.1.0-rc.0+68.sha-4d1d0fa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
