b22ca8026884d427134fe1310a8a7653a58742fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:40:53 +0000

    c5c57f6737 build: update to clang 1.4.0 and only run clang format on changed files (#36203)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+68.sha-4d1d0fa";
+export declare const NGCC_VERSION = "9.1.0-rc.0+72.sha-c5c57f6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
