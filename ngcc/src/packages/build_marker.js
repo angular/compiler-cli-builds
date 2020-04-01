f701d35f898623f013f204a13c7c908df31472dc
Kara Erickson <karakara@google.com>
2020-04-01 20:35:13 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.0-rc.0+67.sha-ec8bae1';
+    exports.NGCC_VERSION = '9.1.0-rc.0+68.sha-4d1d0fa';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
