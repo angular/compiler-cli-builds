35497a89ba25426493c6d96fbb5265d033c2a5c8
Kara Erickson <karakara@google.com>
2020-04-01 18:32:15 +0000

    beb3f19dc0 docs: update end date of survey to middle of April (#36339)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0+26.sha-e8f3c47";
+export declare const NGCC_VERSION = "9.1.0+27.sha-beb3f19";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
