a5bbe9aaa997de57c66ddac2f768b9f7c3b861e3
Kara Erickson <karakara@google.com>
2020-04-03 19:18:17 +0000

    ca25c957bf fix(ngcc): correctly detect imported TypeScript helpers (#36284)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+106.sha-6402a9a";
+export declare const NGCC_VERSION = "9.1.0-rc.0+105.sha-ca25c95";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
