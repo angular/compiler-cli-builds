c1e479dfe290cfe529f5cf0005d66591d8566e66
Kara Erickson <karakara@google.com>
2020-04-03 19:02:40 +0000

    6402a9ae2a build: rebuild yarn lock from scratch (#36377)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+108.sha-a555fdb";
+export declare const NGCC_VERSION = "9.1.0-rc.0+106.sha-6402a9a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
