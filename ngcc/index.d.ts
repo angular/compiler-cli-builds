91e1b8191bd4955781224cc6cedb9cb551dfbf4c
Kara Erickson <karakara@google.com>
2020-04-06 16:57:35 +0000

    1140bbc25c refactor(language-service): reformat using clang-format (#36426)

--- ngcc/index.d.ts
+++ ngcc/index.d.ts
@@ -1,7 +1,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc" />
 import { AsyncNgccOptions, SyncNgccOptions } from './src/main';
 export { ConsoleLogger } from './src/logging/console_logger';
-export { LogLevel, Logger } from './src/logging/logger';
+export { Logger, LogLevel } from './src/logging/logger';
 export { AsyncNgccOptions, NgccOptions, SyncNgccOptions } from './src/main';
 export { PathMappings } from './src/utils';
 export declare function process(options: AsyncNgccOptions): Promise<void>;
