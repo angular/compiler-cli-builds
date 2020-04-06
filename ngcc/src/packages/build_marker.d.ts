87b74c234564ee2fccfd040f16e9619e14df6a7b
Kara Erickson <karakara@google.com>
2020-04-06 22:54:32 +0000

    eb8c6c7eff test(language-service): Move pipe tests to TEST_TEMPLATE (#36407)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+123.sha-09c8416";
+export declare const NGCC_VERSION = "9.1.0-rc.0+128.sha-eb8c6c7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
