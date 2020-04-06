90aa6206595345995092278adcb963f066012de7
Kara Erickson <karakara@google.com>
2020-04-06 20:43:54 +0000

    ee70a18a75 fix(ngcc): don't crash on cyclic source-map references (#36452)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+120.sha-76a8cd5";
+export declare const NGCC_VERSION = "9.1.0-rc.0+121.sha-ee70a18";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
