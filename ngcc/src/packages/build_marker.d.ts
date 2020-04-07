0bcb348de526ebfe3e33b6f34dc901a2232c0031
Kara Erickson <karakara@google.com>
2020-04-07 17:50:46 +0000

    af4269471d fix(core): ngOnDestroy on multi providers called with incorrect context (#35840)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0+89.sha-f48a065";
+export declare const NGCC_VERSION = "9.1.0+95.sha-af42694";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
