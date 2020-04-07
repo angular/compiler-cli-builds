34b54ca37e9e76af05874e7715b7adff6dccbd61
Kara Erickson <karakara@google.com>
2020-04-07 17:42:26 +0000

    e145fa13b1 test(language-service): delete expression-cases.ts (#36468)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+131.sha-7d0af17";
+export declare const NGCC_VERSION = "9.1.0-rc.0+136.sha-e145fa1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
