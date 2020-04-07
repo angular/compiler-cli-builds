94111f5b88f19df6ebd388b5a2fe1a2c4cded84c
Kara Erickson <karakara@google.com>
2020-04-07 18:58:07 +0000

    7f28845f58 test(language-service): remove ng-for-cases.ts (#36470)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+138.sha-e0415db";
+export declare const NGCC_VERSION = "9.1.0-rc.0+140.sha-7f28845";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
