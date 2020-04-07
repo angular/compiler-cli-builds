d557dd50b2099e1e9bbac5a0ab3fe1e27518dae1
Kara Erickson <karakara@google.com>
2020-04-07 17:08:52 +0000

    7d0af179e3 style(forms): reformat of the `forms` package after clang update (#36466)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.0-rc.0+129.sha-93302b7";
+export declare const NGCC_VERSION = "9.1.0-rc.0+131.sha-7d0af17";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
