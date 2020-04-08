2477ebfaa41c3e0bab70fd3986ef32770dc1fcdc
atscott <atscott@google.com>
2020-04-08 23:20:30 +0000

    333b8679ad refactor(bazel): use runfiles helper in ts-api-guardian (#36471)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.1+12.sha-5da621d";
+export declare const NGCC_VERSION = "9.1.1+13.sha-333b867";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
