c1fa0b9b8d8c8fd30160527e1ed4317b49aabc20
atscott <atscott@google.com>
2020-04-09 17:06:29 +0000

    e526f74dfd refactor(compiler): create a new root `BindingScope` for each template (#36362)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+7.sha-9e78f55";
+export declare const NGCC_VERSION = "10.0.0-next.1+8.sha-e526f74";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
