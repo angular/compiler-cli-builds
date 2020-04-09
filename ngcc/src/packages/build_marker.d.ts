d3354d780344cd7cc11630b8194913438cec91a7
atscott <atscott@google.com>
2020-04-09 23:51:10 +0000

    fee316161d build: update `REQUIRED_BASE_SHA` in merge script to clang 1.4.0 upgrade commit (#36547)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+18.sha-2e4244d";
+export declare const NGCC_VERSION = "10.0.0-next.1+19.sha-fee3161";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
