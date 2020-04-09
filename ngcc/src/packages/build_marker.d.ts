4ffea0b0f1d04d3fb1d753e6a7aec68412579fc7
atscott <atscott@google.com>
2020-04-09 18:52:36 +0000

    13ae2d7271 style: format forms validators to fix lint error (#36546)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+13.sha-078b0be";
+export declare const NGCC_VERSION = "10.0.0-next.1+12.sha-13ae2d7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
