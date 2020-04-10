d2a545d53ac2510a3a72427a79eef56f8a9127ac
atscott <atscott@google.com>
2020-04-10 21:14:17 +0000

    3992341d34 fix(core): undecorated-classes-with-decorated-fields migration should avoid error if base class has no value declaration (#36543)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.1+28.sha-8c559ef";
+export declare const NGCC_VERSION = "9.1.1+29.sha-3992341";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
