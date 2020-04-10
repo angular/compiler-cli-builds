8ffc6ebde3a0656bca3fff7f29ff74f4780e0c0e
atscott <atscott@google.com>
2020-04-10 16:28:41 +0000

    8c559ef104 fix(ngcc): correctly detect external files from nested `node_modules/` (#36559)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.1+27.sha-0bac2b0";
+export declare const NGCC_VERSION = "9.1.1+28.sha-8c559ef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
