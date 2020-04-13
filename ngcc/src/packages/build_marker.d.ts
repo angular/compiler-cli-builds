eda2d24ba0fe6b6571ec183ea9809029c060c0ac
atscott <atscott@google.com>
2020-04-13 15:41:05 +0000

    713bce19f4 docs: move ng-conf 2020 to the already presented section (#36413)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+22.sha-5737e65";
+export declare const NGCC_VERSION = "10.0.0-next.1+26.sha-713bce1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
