5d27b887133962eafe46371e764ac2a2b8ee2eff
atscott <atscott@google.com>
2020-04-13 15:42:34 +0000

    5737e65700 docs(zone.js): fix typos in NgZone guide code example (#36597)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+23.sha-9181b73";
+export declare const NGCC_VERSION = "10.0.0-next.1+22.sha-5737e65";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
