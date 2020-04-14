ccee6a43a211a3dc998f82d196a1f9ebeaf4419b
atscott <atscott@google.com>
2020-04-14 17:39:23 +0000

    5e80e7e216 refactor(language-service): clean up and exports and consolidate types (#36533)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.1+31.sha-d50cb30";
+export declare const NGCC_VERSION = "10.0.0-next.1+32.sha-5e80e7e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
