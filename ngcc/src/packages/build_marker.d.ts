368409b76dfbdf1952fcae39169f141e7160377c
atscott <atscott@google.com>
2020-04-14 00:55:03 +0000

    c8f2ca2349 docs: edit to setup-local (#36168)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.1+34.sha-fd76982";
+export declare const NGCC_VERSION = "9.1.1+36.sha-c8f2ca2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
