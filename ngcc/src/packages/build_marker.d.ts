d05710da46dcb3e6692aa289d9c24c9ffefd3c5f
atscott <atscott@google.com>
2020-04-15 23:38:41 +0000

    5c3774cfe6 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#36648)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.1+45.sha-12266b2";
+export declare const NGCC_VERSION = "9.1.1+46.sha-5c3774c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
