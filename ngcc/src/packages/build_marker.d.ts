29a7579d93607e1e221be866a28138348bc4c4ae
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:34:08 +0000

    4480ba3e29 ci: remove reliance on Github API for CI setup (#36500)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+31.sha-5e79799";
+export declare const NGCC_VERSION = "10.0.0-next.2+32.sha-4480ba3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
