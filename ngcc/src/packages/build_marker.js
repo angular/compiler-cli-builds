e3ce63e1c2da215cfb17021d28b8a8de5a3f763f
atscott <atscott@google.com>
2020-04-16 17:57:35 +0000

    305b059809 docs: refactor routing doc (#35566)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.2+17.sha-297b7da';
+    exports.NGCC_VERSION = '10.0.0-next.2+18.sha-305b059';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
