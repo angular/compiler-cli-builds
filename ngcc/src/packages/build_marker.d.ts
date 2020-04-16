8f26cd01b7c5bb8d09e89cdb788514cac984f54a
atscott <atscott@google.com>
2020-04-16 18:02:45 +0000

    1e4fb74ec8 docs: refactor routing doc (#35566)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.2+17.sha-797c306";
+export declare const NGCC_VERSION = "9.1.2+18.sha-1e4fb74";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
