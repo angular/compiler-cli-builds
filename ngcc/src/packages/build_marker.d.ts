e1c269ebe149583127155ff8984ab586bae8f6fc
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:35:39 +0000

    0fa5ac8d0d ci: remove reliance on Github API for CI setup (#36500)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.2+29.sha-f2fca3e";
+export declare const NGCC_VERSION = "9.1.2+30.sha-0fa5ac8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.2+29.sha-f2fca3e';
+    exports.NGCC_VERSION = '9.1.2+30.sha-0fa5ac8';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
