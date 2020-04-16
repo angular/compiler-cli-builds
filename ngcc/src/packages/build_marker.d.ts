68c0fd507edc5c7b6ec3ad85500f09ec9833af3e
atscott <atscott@google.com>
2020-04-16 17:06:05 +0000

    4a9f0bebc3 fix(core): prevent unknown property check for AOT-compiled components (#36072)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+3.sha-b1f1d3f";
+export declare const NGCC_VERSION = "10.0.0-next.2+14.sha-4a9f0be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
