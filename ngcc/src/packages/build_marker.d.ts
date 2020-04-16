dfaf3a62d81d22cc926b825995ffb2952c0c441d
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:29:49 +0000

    78840e59a7 docs: remove rob (#36285)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+29.sha-8be0972";
+export declare const NGCC_VERSION = "10.0.0-next.2+28.sha-78840e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
