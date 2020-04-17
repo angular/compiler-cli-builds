fd4822998293a520590509e892b3bc24ab81659a
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:55:48 +0000

    bb150c2704 fixup!: build(docs-infra): Ensures that only member docs are linked (#36316)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+34.sha-4da901f";
+export declare const NGCC_VERSION = "10.0.0-next.2+40.sha-bb150c2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
