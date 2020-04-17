13f863f5c74ba74721f2b8cecf8e2b2db9194012
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:56:45 +0000

    95f45e8070 refactor(compiler): remove unused CachedFileSystem (#36687)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.2+37.sha-fd7c39e";
+export declare const NGCC_VERSION = "9.1.2+41.sha-95f45e8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
