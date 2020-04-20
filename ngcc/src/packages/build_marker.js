d67f9ac00f16f3289d3a883b66477ce21baa3774
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:42:14 +0000

    458dc10ddc docs: place download section in accessibility to the top (#36561)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.2+49.sha-4341743';
+    exports.NGCC_VERSION = '10.0.0-next.2+50.sha-458dc10';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
