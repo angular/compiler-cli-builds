3ba8fdb492ed051f16097dff223c18d3ab68905e
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:51:55 +0000

    2a53f47159 fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.2+45.sha-722d939";
+export declare const NGCC_VERSION = "9.1.2+46.sha-2a53f47";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
