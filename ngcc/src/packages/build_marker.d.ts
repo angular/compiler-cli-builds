ecf524b2f74bcc444d458b4989133fa6bc185710
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:12:07 +0000

    f186c32245 docs: place download section in architecture to the top (#36565)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+50.sha-458dc10";
+export declare const NGCC_VERSION = "10.0.0-next.2+53.sha-f186c32";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
