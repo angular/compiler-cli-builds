5c0e9b0ade7bf8297868c98e84bdf92a2cee45ba
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:31:47 +0000

    1b3ee17888 build: move circular deps golden to a subfolder (#36630)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+3.sha-fc1b557";
+export declare const NGCC_VERSION = "10.0.0-next.3+6.sha-1b3ee17";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
