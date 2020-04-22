ab8232e18083bd6f663be98b9bd5609fa523ccb0
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:37:30 +0000

    4275b3412f build: add AndrewKushnir to the `fw-testing` group (#36744)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+6.sha-1b3ee17";
+export declare const NGCC_VERSION = "10.0.0-next.3+7.sha-4275b34";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
