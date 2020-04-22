8c16c69904731bf068293389fc78a578590189cc
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 04:48:25 +0000

    5db8d93fe5 build: fix the compare master and patch script output (#36749)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+57.sha-535c249";
+export declare const NGCC_VERSION = "10.0.0-next.2+58.sha-5db8d93";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
