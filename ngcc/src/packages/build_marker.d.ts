9ece1cc5a9301ae4be90a4683da1cc4ef8bacdcc
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:23:40 +0000

    535c249f3a build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.2+56.sha-942b986";
+export declare const NGCC_VERSION = "10.0.0-next.2+57.sha-535c249";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
