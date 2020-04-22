37e23981507213a2a9436d73209b1bcd665a9eaa
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 16:48:05 +0000

    8bd5374cfd revert: fix(common): format day-periods that cross midnight (#36611) (#36751)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.2+54.sha-a6e10ef";
+export declare const NGCC_VERSION = "9.1.2+56.sha-8bd5374";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
