e10d2227eec71d32b85859c534e83247a35f9ccb
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:29:13 +0000

    8306da5ce1 release(benchpress): bump version of benchpress to 0.2.0 (#36457)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+2.sha-47f9867";
+export declare const NGCC_VERSION = "10.0.0-next.3+5.sha-8306da5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
