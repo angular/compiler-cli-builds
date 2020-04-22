bbf448b68691d068e8238505c49bdc66fab74cbf
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 20:54:48 +0000

    47f98673c7 fix(localize): include legacy ids when describing messages (#36761)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+1.sha-781f561";
+export declare const NGCC_VERSION = "10.0.0-next.3+2.sha-47f9867";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
