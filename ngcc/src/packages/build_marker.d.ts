5f287dc0a47474712f12cda83d0520626d4157f6
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:40:16 +0000

    e37f2663c2 build: require a commit body in commit messages (#36632)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+19.sha-f0ec4ae";
+export declare const NGCC_VERSION = "10.0.0-next.3+21.sha-e37f266";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
