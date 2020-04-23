d138efbdad41338f2de5be2155aba355485fe8f2
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:36:08 +0000

    7743c43529 test(router): fix router test failing on IE (#36742)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.3+6.sha-d40bcce";
+export declare const NGCC_VERSION = "9.1.3+9.sha-7743c43";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
