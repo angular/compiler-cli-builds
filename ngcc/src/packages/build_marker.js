4674ced88daf5a370539c1a85868eae22e948062
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:56:42 +0000

    d840503d35 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.3+15.sha-f898c9a';
+    exports.NGCC_VERSION = '9.1.3+18.sha-d840503';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
