eef2a7545145bc0bbd6aa7e92f6e05138dc8f86f
Andrew Kushnir <akushnir@google.com>
2020-04-24 19:56:31 +0000

    180a32894e build: migrate from gulp to ng-dev for running formatting (#36726)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.3+24.sha-cc71116";
+export declare const NGCC_VERSION = "9.1.3+26.sha-180a328";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
