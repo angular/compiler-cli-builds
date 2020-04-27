af1d85db76be79149fcaac01d8dd1611bbc8f20e
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:18:20 +0000

    0f389fa5ae fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.3+27.sha-2a27f69";
+export declare const NGCC_VERSION = "9.1.3+28.sha-0f389fa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
