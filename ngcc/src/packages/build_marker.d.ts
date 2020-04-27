7e0b901d2483561ccce77e6437c397f94bbb1653
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:05:39 +0000

    00e6cb1d62 feat(router): allow CanLoad guard to return UrlTree (#36610)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+33.sha-b34fb04";
+export declare const NGCC_VERSION = "10.0.0-next.3+35.sha-00e6cb1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
