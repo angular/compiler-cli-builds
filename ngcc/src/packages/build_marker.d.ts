0bba8989df54d60a48ee20184cb68f70551e077a
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:41:47 +0000

    0ce96f1d78 refactor(localize): remove unused code (#36834)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.3+28.sha-0f389fa";
+export declare const NGCC_VERSION = "9.1.3+34.sha-0ce96f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
