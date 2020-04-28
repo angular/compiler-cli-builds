2bac3f4402900a6af74363ad0ae5e184103a04ff
Andrew Kushnir <akushnir@google.com>
2020-04-28 22:33:23 +0000

    a6a7e1bb99 build: enable platform-browser tests on Saucelabs (#36797)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.3+43.sha-70dd27f';
+    exports.NGCC_VERSION = '10.0.0-next.3+44.sha-a6a7e1b';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
