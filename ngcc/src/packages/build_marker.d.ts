0ce975538ce901c9387889918885983dc553a50f
Andrew Kushnir <akushnir@google.com>
2020-04-28 22:31:17 +0000

    a6a7e1bb99 build: enable platform-browser tests on Saucelabs (#36797)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+43.sha-70dd27f";
+export declare const NGCC_VERSION = "10.0.0-next.3+44.sha-a6a7e1b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
