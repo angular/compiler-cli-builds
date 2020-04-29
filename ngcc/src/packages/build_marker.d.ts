e7b1817532822a3c0650edb306035d5446791f2c
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:40:27 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.3+48.sha-c89421b";
+export declare const NGCC_VERSION = "10.0.0-next.4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
