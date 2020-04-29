4eafbb67a116fafb5bb7cdd883ba00d3da11ccb4
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:20:44 +0000

    9b961a410a release: cut the v9.1.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.3+38.sha-420c73c";
+export declare const NGCC_VERSION = "9.1.4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
