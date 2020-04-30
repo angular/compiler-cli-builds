de2738cd016a7c0d9fbf3084cba38460b470c6a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-30 19:07:03 +0000

    49be32c931 refactor(common): remove WrappedValue from AsyncPipe (#36633)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+14.sha-1786586";
+export declare const NGCC_VERSION = "10.0.0-next.4+15.sha-49be32c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
