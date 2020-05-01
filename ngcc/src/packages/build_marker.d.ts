b5573fd7917163319da6b9fe6c2b06d226566d8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:24:49 +0000

    e9300c979e build: use @angular/dev-infra-private for bazel stamping (#36844)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+30.sha-95a407d";
+export declare const NGCC_VERSION = "10.0.0-next.4+29.sha-e9300c9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
