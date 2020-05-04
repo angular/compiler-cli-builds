8ddd0ec8ad02cbd9f7dd4e89358aec8aa247651e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:02:21 +0000

    9d9d46f52b fix(core): log error instead of warning for unknown properties and elements (#36399)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.4+32.sha-d9c4840';
+    exports.NGCC_VERSION = '10.0.0-next.4+33.sha-9d9d46f';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
