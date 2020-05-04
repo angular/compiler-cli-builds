5c59c807eeea81b2a0e9f5e1b36b322b33e067d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 19:59:40 +0000

    d9c4840a9c fix(router): cancel navigation when at least one resolver completes with no "next" emission (#24621)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+29.sha-e9300c9";
+export declare const NGCC_VERSION = "10.0.0-next.4+32.sha-d9c4840";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
