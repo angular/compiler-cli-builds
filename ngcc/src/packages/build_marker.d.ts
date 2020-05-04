978c8b0bcf56f7188e80a799548729a2bb5c7dee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:09:01 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+34.sha-0f3831b";
+export declare const NGCC_VERSION = "10.0.0-next.4+36.sha-297dab8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
