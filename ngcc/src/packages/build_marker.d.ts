0d4625d9053dc8a758d52af3c69fdac73c1f742f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:04:18 +0000

    9d9d46f52b fix(core): log error instead of warning for unknown properties and elements (#36399)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+32.sha-d9c4840";
+export declare const NGCC_VERSION = "10.0.0-next.4+33.sha-9d9d46f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
