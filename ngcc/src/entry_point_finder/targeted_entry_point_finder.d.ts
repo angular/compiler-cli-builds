45ed3949bb74abd6707c8ca27964c49b9b6aaeb4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:15:16 +0000

    42c58d9fe8 docs(dev-infra): update format commands described in DEVELOPER.md (#36910)

--- ngcc/src/entry_point_finder/targeted_entry_point_finder.d.ts
+++ ngcc/src/entry_point_finder/targeted_entry_point_finder.d.ts
@@ -31,23 +31,30 @@ export declare class TargetedEntryPointFinder implements EntryPointFinder {
     private unprocessedPaths;
     private unsortedEntryPoints;
     private basePaths;
+    private getBasePaths;
     constructor(fs: FileSystem, config: NgccConfiguration, logger: Logger, resolver: DependencyResolver, basePath: AbsoluteFsPath, targetPath: AbsoluteFsPath, pathMappings: PathMappings | undefined);
     findEntryPoints(): SortedEntryPointsInfo;
     targetNeedsProcessingOrCleaning(propertiesToConsider: EntryPointJsonProperty[], compileAllFormats: boolean): boolean;
     private processNextPath;
     private getEntryPoint;
+    private computePackagePath;
     /**
-     * Search down to the `entryPointPath` from each `basePath` for the first `package.json` that we
-     * come to. This is the path to the entry-point's containing package. For example if `basePath` is
-     * `/a/b/c` and `entryPointPath` is `/a/b/c/d/e` and there exists `/a/b/c/d/package.json` and
-     * `/a/b/c/d/e/package.json`, then we will return `/a/b/c/d`.
+     * Search down to the `entryPointPath` from the `containingPath` for the first `package.json` that
+     * we come to. This is the path to the entry-point's containing package. For example if
+     * `containingPath` is `/a/b/c` and `entryPointPath` is `/a/b/c/d/e` and there exists
+     * `/a/b/c/d/package.json` and `/a/b/c/d/e/package.json`, then we will return `/a/b/c/d`.
      *
      * To account for nested `node_modules` we actually start the search at the last `node_modules` in
-     * the `entryPointPath` that is below the `basePath`. E.g. if `basePath` is `/a/b/c` and
-     * `entryPointPath` is `/a/b/c/d/node_modules/x/y/z`, we start the search at
+     * the `entryPointPath` that is below the `containingPath`. E.g. if `containingPath` is `/a/b/c`
+     * and `entryPointPath` is `/a/b/c/d/node_modules/x/y/z`, we start the search at
      * `/a/b/c/d/node_modules`.
      */
-    private computePackagePath;
+    private computePackagePathFromContainingPath;
+    /**
+     * Search up the directory tree from the `entryPointPath` looking for a `node_modules` directory
+     * that we can use as a potential starting point for computing the package path.
+     */
+    private computePackagePathFromNearestNodeModules;
     /**
      * Split the given `path` into path segments using an FS independent algorithm.
      * @param path The path to split.
