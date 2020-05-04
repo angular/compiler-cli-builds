d74ef1f83a63784cfb10cf97b1478c9f2ae217d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:10:56 +0000

    58ea040570 test(language-service): add new mock host for testing ivy (#36879)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.4+41.sha-e037840";
+export declare const NGCC_VERSION = "10.0.0-next.4+38.sha-58ea040";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
