0fb48285d03bc509a9df32286dd71a327aa6edc5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:37:44 +0000

    e30e1325f3 fix(core): properly get root nodes from embedded views with <ng-content> (#36051)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.5+20.sha-fc6c3ae";
+export declare const NGCC_VERSION = "10.0.0-next.5+21.sha-e30e132";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.5+20.sha-fc6c3ae';
+    exports.NGCC_VERSION = '10.0.0-next.5+21.sha-e30e132';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
