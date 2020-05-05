f0f58bae7324d7c83d509373c3408556477e11b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:23:52 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.5+7.sha-306f46c";
+export declare const NGCC_VERSION = "10.0.0-next.5+18.sha-b95a336";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
