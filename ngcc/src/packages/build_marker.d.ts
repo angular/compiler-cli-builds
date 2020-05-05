663f2b471632abb89eeb779c5dad244ea8b4b6ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:41:48 +0000

    9e755f30f0 docs: improve startup nav, naming, and terminology (#35533) (#36822)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.4+26.sha-7537010";
+export declare const NGCC_VERSION = "9.1.4+43.sha-9e755f3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
