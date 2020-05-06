f1c35fd5b38faa7496257734e33194e1f4c17ff4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:49:50 +0000

    f7815cf96d test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36859)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.5+61.sha-f930e75";
+export declare const NGCC_VERSION = "10.0.0-next.5+71.sha-f7815cf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
