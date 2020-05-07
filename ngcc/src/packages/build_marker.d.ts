c793f91de117da068626b6ca1c8a6e68f8d5f082
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:44:15 +0000

    97d6d909e7 fix(forms): number input fires valueChanges twice. (#36087)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.5+71.sha-f7815cf";
+export declare const NGCC_VERSION = "10.0.0-next.5+72.sha-97d6d90";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
