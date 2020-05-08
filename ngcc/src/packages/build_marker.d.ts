71a7c870f27ac1b5d64ae0f22a1418f7d9d3e35a
Misko Hevery <misko@hevery.com>
2020-05-08 22:06:20 +0000

    ed1b4a8f19 docs: add migration guides to sidenav in updating to Version 9 (#34979)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+17.sha-bd30c37";
+export declare const NGCC_VERSION = "10.0.0-next.6+18.sha-ed1b4a8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
