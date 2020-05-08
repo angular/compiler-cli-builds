29e45dca8311fc03515bb57b60c1ac3e55f84d42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:05:17 +0000

    14be55c9fa docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+7.sha-39e066a";
+export declare const NGCC_VERSION = "10.0.0-next.6+5.sha-14be55c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
