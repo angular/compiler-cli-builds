1fd8100e6d22b0039758524be68074b0331a3766
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:01:19 +0000

    7cc332efe6 docs: update aot compiler docs (#35487)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6";
+export declare const NGCC_VERSION = "10.0.0-next.6+3.sha-7cc332ef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
