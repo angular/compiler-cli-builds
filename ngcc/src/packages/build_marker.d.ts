df66a5518ce1eba23da2f55b34a11a5a4c4a6180
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:27:18 +0000

    b6bc0aeb61 release: cut the v10.0.0-next.6 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.5+74.sha-42d1091";
+export declare const NGCC_VERSION = "10.0.0-next.6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
