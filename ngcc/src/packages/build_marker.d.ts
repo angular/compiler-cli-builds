3d329a36cc829453bea34a4e86218d7dacdf6140
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:28:39 +0000

    d1b3af697c release: cut the v9.1.5 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.4+67.sha-dcf1dcb";
+export declare const NGCC_VERSION = "9.1.5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
