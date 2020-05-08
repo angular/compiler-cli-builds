97bfbd62cc8d41f60b0153ab9eafaefaa7e1b8f5
Misko Hevery <misko@hevery.com>
2020-05-08 22:03:55 +0000

    bd30c37040 fix(docs-infra): add visual signal for external links (#36601)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.6+16.sha-eb34aa5';
+    exports.NGCC_VERSION = '10.0.0-next.6+17.sha-bd30c37';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
