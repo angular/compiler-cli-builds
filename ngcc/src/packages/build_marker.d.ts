5844d4de428c215ff406b4ad266ffc176eadabdf
Misko Hevery <misko@hevery.com>
2020-05-08 22:05:44 +0000

    eb34aa551a feat(compiler): add name spans for property reads and method calls (#36826)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+11.sha-df71c7c";
+export declare const NGCC_VERSION = "10.0.0-next.6+16.sha-eb34aa5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
