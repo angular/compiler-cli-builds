c2d50d91fe470d28c8cf39019ae935a6ef9028e7
Misko Hevery <misko@hevery.com>
2020-05-08 22:00:02 +0000

    c184de175d build: migrate to typescript file for ng-dev config (#37017)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+8.sha-de0c71c";
+export declare const NGCC_VERSION = "10.0.0-next.6+10.sha-c184de1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
