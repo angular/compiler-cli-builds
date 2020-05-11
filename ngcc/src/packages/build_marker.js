88f4616cf120a46ee3c60775b2041cb06f9cbf71
Misko Hevery <misko@hevery.com>
2020-05-11 18:27:06 +0000

    286fbf42c6 fix: add aikidave as reviewer for DOCS: Marketing (#37014)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.6+21.sha-7eb1a58';
+    exports.NGCC_VERSION = '10.0.0-next.6+23.sha-286fbf4';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
