6fb4d4d3a21ed85e9721e3f86a920849c5615451
Misko Hevery <misko@hevery.com>
2020-05-11 18:16:47 +0000

    28f3c1c96a refactor(core): rename `instructions/container.ts` -> `instructions/template.ts` (#34715)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+18.sha-ed1b4a8";
+export declare const NGCC_VERSION = "10.0.0-next.6+20.sha-28f3c1c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
