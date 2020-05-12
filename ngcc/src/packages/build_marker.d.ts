150cd649b54961d3d02321ef43f345ff072b322c
Misko Hevery <misko@hevery.com>
2020-05-12 22:45:21 +0000

    b91b3d7462 docs(common): fix CLDR website link in FormStyle doc (#37069)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.6+15.sha-468aeb4";
+export declare const NGCC_VERSION = "9.1.6+19.sha-b91b3d7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
