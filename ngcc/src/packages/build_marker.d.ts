4ffd632821c2d9c69b14b6ab52bac91646e900e5
Misko Hevery <misko@hevery.com>
2020-05-12 18:10:29 +0000

    1e208e8c87 docs: refactor dynamic forms topic as tutorial (#36465)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+22.sha-40025f5";
+export declare const NGCC_VERSION = "10.0.0-next.6+24.sha-1e208e8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
