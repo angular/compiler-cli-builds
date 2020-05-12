adcc45e09704fc633851a249d95694cbfa161856
Misko Hevery <misko@hevery.com>
2020-05-12 22:03:19 +0000

    2cddeabce0 docs: fixes `ngOnit` routing variable (#36905)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+33.sha-4f3ac1d";
+export declare const NGCC_VERSION = "10.0.0-next.6+31.sha-2cddeab";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
