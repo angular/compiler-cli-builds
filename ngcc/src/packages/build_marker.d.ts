c448346106efb2d5f78434f4c5cf3fbf7958f298
Misko Hevery <misko@hevery.com>
2020-05-12 18:10:49 +0000

    468aeb454a docs: minor typo fix in the bazel documentation (#37043)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.6+11.sha-efe1713";
+export declare const NGCC_VERSION = "9.1.6+15.sha-468aeb4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
