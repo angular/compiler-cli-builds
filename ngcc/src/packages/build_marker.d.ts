46beb3c389f0474d092bee83e5897e4910718404
Misko Hevery <misko@hevery.com>
2020-05-12 18:12:03 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.6+28.sha-681741c";
+export declare const NGCC_VERSION = "10.0.0-next.6+27.sha-8fe532e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
