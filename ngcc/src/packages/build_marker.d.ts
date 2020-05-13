a0f3f6d3cf4aa3a8818eb7939d930806acf4332c
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:05 +0000

    57a868627f test(docs-infra): re-enable upgrade-phonecat-2-hybrid example e2e tests (#36969)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+7.sha-45f4a47";
+export declare const NGCC_VERSION = "10.0.0-next.7+15.sha-57a8686";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
