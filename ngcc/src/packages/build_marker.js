1af77b890784229f3a9145761589d7f3332c225f
Misko Hevery <misko@hevery.com>
2020-05-13 23:29:02 +0000

    90205079d7 docs(common): English grammar (#36908)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.7+11.sha-9edea0bb';
+    exports.NGCC_VERSION = '10.0.0-next.7+16.sha-9020507';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
