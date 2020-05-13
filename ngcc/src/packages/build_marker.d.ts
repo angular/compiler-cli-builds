fde55b0fa4e8ca484e0000420aceea98f7d76d25
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:52 +0000

    2cacab92e6 docs: correct typos in doc for template type guards (#37090)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+15.sha-57a8686";
+export declare const NGCC_VERSION = "10.0.0-next.7+14.sha-2cacab9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
