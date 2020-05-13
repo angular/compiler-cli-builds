c8bf99af87f7606dd9fda09f5cbb80b6e2a4505c
Misko Hevery <misko@hevery.com>
2020-05-13 23:12:58 +0000

    141fcb95a4 refactor(localize): use the `FileSystem` from ngtsc (#36843)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+2.sha-019835c";
+export declare const NGCC_VERSION = "10.0.0-next.7+6.sha-141fcb9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- src/ngtsc/file_system/src/types.d.ts
+++ src/ngtsc/file_system/src/types.d.ts
@@ -34,7 +34,8 @@ export declare type PathSegment = BrandedPath<'PathSegment'>;
 export interface FileSystem {
     exists(path: AbsoluteFsPath): boolean;
     readFile(path: AbsoluteFsPath): string;
-    writeFile(path: AbsoluteFsPath, data: string, exclusive?: boolean): void;
+    readFileBuffer(path: AbsoluteFsPath): Buffer;
+    writeFile(path: AbsoluteFsPath, data: string | Buffer, exclusive?: boolean): void;
     removeFile(path: AbsoluteFsPath): void;
     symlink(target: AbsoluteFsPath, path: AbsoluteFsPath): void;
     readdir(path: AbsoluteFsPath): PathSegment[];
