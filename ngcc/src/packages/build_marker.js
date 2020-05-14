ce845aa3e403251bfa56a119854a92e36ab778e9
Kara Erickson <karakara@google.com>
2020-05-14 22:45:49 +0000

    7a30153aa1 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.7+49.sha-7acd330';
+    exports.NGCC_VERSION = '10.0.0-next.7+50.sha-7a30153';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
