9c66da8105795cd814e23f330f09a5613b6976ff
Kara Erickson <karakara@google.com>
2020-05-14 19:23:45 +0000

    77c0ef38be docs(router): filter the event to subscribe (#37027)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+46.sha-5f64e52";
+export declare const NGCC_VERSION = "10.0.0-next.7+47.sha-77c0ef3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
