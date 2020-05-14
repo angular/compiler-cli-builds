3171caccce9ff2ee090072ff73e5d8905cdc3687
Kara Erickson <karakara@google.com>
2020-05-14 20:25:10 +0000

    7acd33007d fix(dev-infra): do not require a commit body for release commits (#37110)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+48.sha-8a56c99";
+export declare const NGCC_VERSION = "10.0.0-next.7+49.sha-7acd330";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
