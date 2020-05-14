4054f9115e11d84204a377a7edc62e94c5738862
Kara Erickson <karakara@google.com>
2020-05-14 22:41:13 +0000

    4de546eef5 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.6+36.sha-719ca52";
+export declare const NGCC_VERSION = "9.1.6+37.sha-4de546e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
