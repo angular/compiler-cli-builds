0331b8b7a0726e40f46109c0f49d4f839624c672
Kara Erickson <karakara@google.com>
2020-05-15 17:43:38 +0000

    55c2433171 docs(changelog): remove duplicate features and bug fixes (#37101)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+61.sha-03afa36";
+export declare const NGCC_VERSION = "10.0.0-next.7+62.sha-55c2433";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
