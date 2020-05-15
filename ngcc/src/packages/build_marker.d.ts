cb3b790ed5e9d55b44699a197e325c78224dadd3
Kara Erickson <karakara@google.com>
2020-05-15 17:34:03 +0000

    5ffa4f12e5 refactor(docs-infra): refactors `checkUnbalancedBackTicks` (#37065)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.6+41.sha-d808333";
+export declare const NGCC_VERSION = "9.1.6+47.sha-5ffa4f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
