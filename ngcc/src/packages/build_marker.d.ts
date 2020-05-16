6f56d64bec1e38a69500a9800917a566a555cd69
Kara Erickson <karakara@google.com>
2020-05-16 00:20:10 +0000

    3f9b3d7666 build: add kara to public-api approvers (#37143)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.7+64.sha-7f58c57";
+export declare const NGCC_VERSION = "10.0.0-next.7+66.sha-3f9b3d7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
