f95378da69016ffee8727a1b0743eac22b9a256a
Kara Erickson <karakara@google.com>
2020-05-18 22:16:31 +0000

    178a78f9ed docs(router): add docs for RouterLink inputs (#37018)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.6+67.sha-ba9e63a";
+export declare const NGCC_VERSION = "9.1.6+70.sha-178a78f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
