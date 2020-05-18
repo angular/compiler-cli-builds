44220c8b16f60ecaa200db5442a8d307c58c3aa6
Kara Erickson <karakara@google.com>
2020-05-18 17:49:07 +0000

    22d80a6780 refactor(docs-infra): refactors `createOverviewDump` (#37141)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.8+9.sha-593e07b";
+export declare const NGCC_VERSION = "10.0.0-next.8+10.sha-22d80a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
