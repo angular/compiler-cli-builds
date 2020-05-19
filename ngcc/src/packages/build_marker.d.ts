af03feba05e7fc5d3041b0a24d00c0b8b1c40548
Kara Erickson <karakara@google.com>
2020-05-19 17:43:02 +0000

    deac994517 refactor(docs-infra): remove `rev` property from `Resource` interface and data (#37181)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.8+24.sha-b754ab2";
+export declare const NGCC_VERSION = "10.0.0-next.8+25.sha-deac994";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
