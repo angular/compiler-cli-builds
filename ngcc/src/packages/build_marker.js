86ca2cbeba84dc67a9600bf6e97d755a625a5186
Kara Erickson <karakara@google.com>
2020-05-19 22:56:51 +0000

    211d33ad59 build: fix deprecation notice in scripts/github/merge-pr script (#37208)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.8+29.sha-383f04b';
+    exports.NGCC_VERSION = '10.0.0-next.8+30.sha-211d33a';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
