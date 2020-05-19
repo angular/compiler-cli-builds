0f2857c870fe152681353ac85ddd2d93b3c46902
Kara Erickson <karakara@google.com>
2020-05-19 17:31:07 +0000

    b754ab298a build(docs-infra): upgrade cli command docs sources to 200a21f8a (#37200)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-next.8+23.sha-3a4e1b8';
+    exports.NGCC_VERSION = '10.0.0-next.8+24.sha-b754ab2';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
