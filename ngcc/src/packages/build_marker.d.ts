f87d3efa628549bd602a88100e0dc1f81c7a6a25
Kara Erickson <karakara@google.com>
2020-05-19 22:55:34 +0000

    362fba4bde fix(dev-infra): make commit-message-filter script executable for merge (#37209)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.8+30.sha-211d33a";
+export declare const NGCC_VERSION = "10.0.0-next.8+31.sha-362fba4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
