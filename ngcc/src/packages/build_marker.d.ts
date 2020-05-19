3894541eeff2e57e6a4edd8807cd600f6ce0d9a6
Kara Erickson <karakara@google.com>
2020-05-19 22:26:13 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.8+28.sha-4e96cdc";
+export declare const NGCC_VERSION = "10.0.0-next.8+27.sha-772c5b8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
