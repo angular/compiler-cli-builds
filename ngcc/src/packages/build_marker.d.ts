3af824e5dd72ae23037775ad18b7b405b95765fb
Kara Erickson <karakara@google.com>
2020-05-19 22:54:48 +0000

    211d33ad59 build: fix deprecation notice in scripts/github/merge-pr script (#37208)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.8+29.sha-383f04b";
+export declare const NGCC_VERSION = "10.0.0-next.8+30.sha-211d33a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
