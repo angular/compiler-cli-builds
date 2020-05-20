25f4aeaefcf90c075cd1f6865ce01868c57ead37
Kara Erickson <karakara@google.com>
2020-05-20 23:06:05 +0000

    6ca3fb19fd docs: release notes for the v${releaseVersion} release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-next.9+21.sha-267f844";
+export declare const NGCC_VERSION = "10.0.0-next.9+22.sha-6ca3fb1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
