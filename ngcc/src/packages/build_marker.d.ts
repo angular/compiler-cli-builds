bc4256bef06a40eaa1211ac5a3f4a5f22ea34014
Kara Erickson <karakara@google.com>
2020-05-20 23:17:42 +0000

    b797913d10 release: cut the v9.1.9 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.8+19.sha-1465372";
+export declare const NGCC_VERSION = "9.1.9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
