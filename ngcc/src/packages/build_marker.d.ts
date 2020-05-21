d2eac7fc539a670ab279afd15ec3bdd154c8a993
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:41:22 +0000

    93be1f1543 build: use static patch value for targetting branches in merge config (#37245)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+4.sha-87b1aea";
+export declare const NGCC_VERSION = "10.0.0-rc.0+3.sha-93be1f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
