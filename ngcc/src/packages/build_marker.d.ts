42fac6f584ac2d87df0b18a42be7c4fae89c02a7
Matias Niemelä <matias@yearofmoo.com>
2020-05-26 18:47:42 +0000

    6f829180f7 build: update license headers to reference Google LLC (#37205)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -1,7 +1,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+1.sha-441073b";
+export declare const NGCC_VERSION = "10.0.0-rc.0+4.sha-6f82918";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
