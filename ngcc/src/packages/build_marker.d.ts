79be17c6afac8ef1f4b04460a4cab61d1ca5b8f8
Matias Niemelä <matias@yearofmoo.com>
2020-05-26 18:54:01 +0000

    ecce99470a build: deprecate old merge script (#37247)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -1,7 +1,7 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/build_marker" />
 /**
  * @license
- * Copyright Google LLC All Rights Reserved.
+ * Copyright Google Inc. All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+17.sha-d1ea1f4";
+export declare const NGCC_VERSION = "10.0.0-rc.0+15.sha-ecce994";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
