24e86d841f82619eb4f94b3d8131e302446b51db
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:52:40 +0000

    82761ec50e docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+11.sha-270da1f";
+export declare const NGCC_VERSION = "10.0.0-rc.0+22.sha-82761ec";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
