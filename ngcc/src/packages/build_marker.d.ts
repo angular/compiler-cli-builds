81f7223cf69092cc8f8c411e15d8b39a464e39a5
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 23:19:16 +0000

    4d0e175a65 fix(core): reenable decorator downleveling for Angular npm packages (#37317)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+34.sha-bd7393f";
+export declare const NGCC_VERSION = "10.0.0-rc.0+35.sha-4d0e175";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
