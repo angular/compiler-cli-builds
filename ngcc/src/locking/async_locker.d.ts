9526f0719122f5843f7231c14a41d661cf8c2435
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:55:55 +0000

    044a19966d ci: temporarily disable Android 10 browser unit tests on Saucelabs (#37399)

--- ngcc/src/locking/async_locker.d.ts
+++ ngcc/src/locking/async_locker.d.ts
@@ -25,4 +25,6 @@ export declare class AsyncLocker {
      */
     lock<T>(fn: () => Promise<T>): Promise<T>;
     protected create(): Promise<void>;
+    protected isProcessRunning(pid: string): boolean;
+    private lockFileMessage;
 }
