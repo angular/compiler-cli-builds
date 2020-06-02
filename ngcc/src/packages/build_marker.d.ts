ffd3dad4d45576e1a689e225008f478b3ba117c0
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:50:39 +0000

    561c0f81a0 perf(ngcc): allow immediately reporting a stale lock file (#37250)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+44.sha-1b55da1";
+export declare const NGCC_VERSION = "10.0.0-rc.0+46.sha-561c0f8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
