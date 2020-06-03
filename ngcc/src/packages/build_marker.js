a4fa9549cce3649a70c67544a1bd18535fc67e96
atscott <atscott@google.com>
2020-06-03 22:49:34 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.2+21.sha-60a03b7';
+    exports.NGCC_VERSION = '10.0.0-rc.2+22.sha-e35269d';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
