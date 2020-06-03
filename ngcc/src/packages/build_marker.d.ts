8bab36f1637e946f308f96ca08fcbc2e3215221a
atscott <atscott@google.com>
2020-06-03 20:47:18 +0000

    965a688c97 fix(compiler-cli): use ModuleWithProviders type if static eval fails (#37126)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+55.sha-ab3f4c9";
+export declare const NGCC_VERSION = "10.0.0-rc.0+56.sha-965a688";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
