b6b09e6f81e8184773b09f25a513940c715dd06c
atscott <atscott@google.com>
2020-06-03 20:37:34 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+50.sha-06a8ad2";
+export declare const NGCC_VERSION = "10.0.0-rc.0+54.sha-40f3bb5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
