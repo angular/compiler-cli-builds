c089fe65b5b41154ab16ad831d09b61c80d8dcad
atscott <atscott@google.com>
2020-06-03 19:56:43 +0000

    06a8ad28a7 refactor(forms): use a type guard to get rid of casts (#32541)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+49.sha-382aa50";
+export declare const NGCC_VERSION = "10.0.0-rc.0+50.sha-06a8ad2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
