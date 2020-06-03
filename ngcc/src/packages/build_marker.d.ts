6e79b3769296a3a18d10a0a72fac94fd0fa17d34
atscott <atscott@google.com>
2020-06-03 19:49:12 +0000

    382aa50c54 docs: refactor template-driven forms doc as a tutorial (#36732)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+48.sha-044a199";
+export declare const NGCC_VERSION = "10.0.0-rc.0+49.sha-382aa50";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
