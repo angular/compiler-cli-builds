fd6e39383c1d1bc8d1c1f1597ff861819a250509
atscott <atscott@google.com>
2020-06-04 16:44:55 +0000

    c61decf7e5 docs: add blank line before header (#37391)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+61.sha-2021ad1";
+export declare const NGCC_VERSION = "10.0.0-rc.0+63.sha-c61decf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
