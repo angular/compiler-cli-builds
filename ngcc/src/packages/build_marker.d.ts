e399e6b4c680ee784e18a5293c229e10f74965b5
atscott <atscott@google.com>
2020-06-04 16:59:19 +0000

    c025357fb8 feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+67.sha-57411c8";
+export declare const NGCC_VERSION = "10.0.0-rc.0+68.sha-c025357";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
