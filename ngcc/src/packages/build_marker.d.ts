92f8335ebedef250793f00171c8672d18476d3c8
atscott <atscott@google.com>
2020-06-04 16:39:07 +0000

    6e7bd939f6 perf(ngcc): cache parsed tsconfig between runs (#37417)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+60.sha-c4f4675";
+export declare const NGCC_VERSION = "10.0.0-rc.0+62.sha-6e7bd93";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
