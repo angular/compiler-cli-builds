c3f45bc2db27781417d671f1a029de9248f98e84
atscott <atscott@google.com>
2020-06-04 16:41:29 +0000

    2021ad12cf ci(docs-infra): skip deploying RC version when lexicographically smaller than stable (#37426)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+62.sha-6e7bd93";
+export declare const NGCC_VERSION = "10.0.0-rc.0+61.sha-2021ad1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
