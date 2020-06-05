71eb5c81c53d0162ddc3174a26132bac95bdfe4f
atscott <atscott@google.com>
2020-06-05 16:41:44 +0000

    2cb3b66640 fix(ngcc): find decorated constructor params on IIFE wrapped classes (#37436)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.2+50.sha-fff424a";
+export declare const NGCC_VERSION = "10.0.0-rc.2+53.sha-2cb3b66";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
