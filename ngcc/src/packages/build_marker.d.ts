da5d765520bd2b35a962e66555403f919e4338a9
atscott <atscott@google.com>
2020-06-05 16:43:28 +0000

    e31208beb1 docs: fix minor error in the "Structural directives" guide (#37452)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+75.sha-3569fdf";
+export declare const NGCC_VERSION = "10.0.0-rc.0+76.sha-e31208b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
