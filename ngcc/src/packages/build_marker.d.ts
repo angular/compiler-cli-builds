87ce65c1c990986659220c4ff02af2d92e18b827
atscott <atscott@google.com>
2020-06-08 16:52:08 +0000

    31b3888a2f style(ngcc): post-merge review tidy up (#37461)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.2+59.sha-75294e7";
+export declare const NGCC_VERSION = "10.0.0-rc.2+64.sha-31b3888";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
