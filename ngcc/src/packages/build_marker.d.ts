1cfd68fcd0f8c4c43e0c1e54b2679b2b88bc2974
atscott <atscott@google.com>
2020-06-08 22:26:54 +0000

    87fdd23157 docs: add note on publishing libraries in ivy (#36556)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+96.sha-713fb3b";
+export declare const NGCC_VERSION = "10.0.0-rc.0+97.sha-87fdd23";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
