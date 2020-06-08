59a67430afef3c212007d93861666b6bb4995553
atscott <atscott@google.com>
2020-06-08 17:46:22 +0000

    8bead6bfdd test(language-service): Remove all markers from test project (#37475)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.2+65.sha-52dda73";
+export declare const NGCC_VERSION = "10.0.0-rc.2+66.sha-8bead6b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
