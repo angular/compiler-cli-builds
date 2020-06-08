52be0f754d339ebd1512cc867ea5c28d02744234
atscott <atscott@google.com>
2020-06-08 17:48:35 +0000

    650974e1b3 test(language-service): Remove all markers from test project (#37475)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.0+91.sha-ed3c549';
+    exports.NGCC_VERSION = '10.0.0-rc.0+92.sha-650974e';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
