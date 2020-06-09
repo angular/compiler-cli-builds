35b0dc1a48d2c2d90733b633004c38d8858fc0bf
atscott <atscott@google.com>
2020-06-09 15:52:59 +0000

    f6ee911cbb docs(core): fix path referenced in comments of both compiler facade interface files (#37370)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+103.sha-7edb026";
+export declare const NGCC_VERSION = "10.0.0-rc.0+108.sha-f6ee911";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
