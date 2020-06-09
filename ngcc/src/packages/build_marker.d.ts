a38f05fb2a13c1b66dcf1a7165511d548ed76584
atscott <atscott@google.com>
2020-06-09 00:46:50 +0000

    6280cf95b4 fix(language-service): Improve signature selection by finding exact match (#37494)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+98.sha-946f117";
+export declare const NGCC_VERSION = "10.0.0-rc.0+100.sha-6280cf9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
