131a1b82ae21c55bf89ed32e562fd2f8f0f21e08
atscott <atscott@google.com>
2020-06-09 00:44:49 +0000

    55979fe0ae feat(language-service): TS references from template items (#37437)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+100.sha-6280cf9";
+export declare const NGCC_VERSION = "10.0.0-rc.0+101.sha-55979fe";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
