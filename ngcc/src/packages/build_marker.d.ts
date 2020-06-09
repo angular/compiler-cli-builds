0ae9cf6d4ce836af434df37ed447f3ecfdf9d66d
atscott <atscott@google.com>
2020-06-09 20:54:14 +0000

    d494f7bd5e docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.3+11.sha-ec6a7ab";
+export declare const NGCC_VERSION = "10.0.0-rc.3+12.sha-d494f7b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
