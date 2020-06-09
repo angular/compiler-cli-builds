f7fdded41b3bc9e3a38c04c3dbeb68fed3ecc6c0
atscott <atscott@google.com>
2020-06-09 18:36:56 +0000

    cb1373106b docs: wrong links in lifecycle hooks api documentaion (#36557)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+108.sha-f6ee911";
+export declare const NGCC_VERSION = "10.0.0-rc.0+109.sha-cb13731";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
