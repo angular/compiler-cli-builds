fb5f61597f0ea636dea4abdc4aeb683659a38604
atscott <atscott@google.com>
2020-06-09 15:49:56 +0000

    7edb026619 fix(common): prevent duplicate URL change notifications (#37459)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+107.sha-a331370";
+export declare const NGCC_VERSION = "10.0.0-rc.0+103.sha-7edb026";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
