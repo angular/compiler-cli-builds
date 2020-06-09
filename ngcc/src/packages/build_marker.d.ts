073279eef77e93b45a60b66ff6f7b46c185dbae0
atscott <atscott@google.com>
2020-06-09 20:59:06 +0000

    b2ccc34f9c perf(core): avoid pulling in jit-specific code in aot bundles (#37372)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+110.sha-eaa38a5";
+export declare const NGCC_VERSION = "10.0.0-rc.0+111.sha-b2ccc34";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
