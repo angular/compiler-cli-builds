7da8ef5fec5bfb014c47488431f7ddb90794ce76
atscott <atscott@google.com>
2020-06-10 19:11:09 +0000

    d76a872efb docs: add Mike Huang as a GDE (#36472)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+117.sha-021dd33";
+export declare const NGCC_VERSION = "10.0.0-rc.0+122.sha-d76a872";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
