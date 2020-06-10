5282d6a303b43af0ccac0d4e64b38a7f24751557
atscott <atscott@google.com>
2020-06-10 15:40:05 +0000

    0a1d078a74 Revert "build: remove wombot proxy registry from package.jsons for release (#37378)" (#37495)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+112.sha-35502c7";
+export declare const NGCC_VERSION = "10.0.0-rc.0+113.sha-0a1d078";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
