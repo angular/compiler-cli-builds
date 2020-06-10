62826e6d16d623650a465cf7914161f8a0e70e66
atscott <atscott@google.com>
2020-06-10 18:52:09 +0000

    684726cdac docs: release notes for the v9.1.11 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+115.sha-7b005bb";
+export declare const NGCC_VERSION = "10.0.0-rc.0+116.sha-684726c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
