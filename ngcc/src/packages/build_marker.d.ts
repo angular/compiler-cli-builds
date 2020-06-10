b22b07a5a688a8ea9235bf81925623ed94b956b4
atscott <atscott@google.com>
2020-06-10 20:21:15 +0000

    20346ff192 build: remove ngcontainer Docker (#36421)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+123.sha-bd9c1e6";
+export declare const NGCC_VERSION = "10.0.0-rc.0+124.sha-20346ff";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
