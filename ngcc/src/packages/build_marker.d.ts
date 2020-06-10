6791c98af915a85639d41b0e8dfe267966af5cdc
atscott <atscott@google.com>
2020-06-10 19:13:59 +0000

    e836eaf448 fix(docs-infra): preconnect to analytics origins (#37400)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+121.sha-a398d6a";
+export declare const NGCC_VERSION = "10.0.0-rc.0+120.sha-e836eaf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
