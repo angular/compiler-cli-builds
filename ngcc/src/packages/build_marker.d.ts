a160231f0e09c4bf58c27fab989da2bfdf130915
Misko Hevery <misko@hevery.com>
2020-06-11 20:32:40 +0000

    a937889c3b refactor(elements): add accessor workaround for build-optimizer (#37456)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+130.sha-8c682c5";
+export declare const NGCC_VERSION = "10.0.0-rc.0+134.sha-a937889";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
