3438ef05f9ede08dcfbddb371c16ba9969965575
Misko Hevery <misko@hevery.com>
2020-06-11 18:25:31 +0000

    39f973430a docs: remove out of date GDEs (#36467)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+124.sha-20346ff";
+export declare const NGCC_VERSION = "10.0.0-rc.0+125.sha-39f9734";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
