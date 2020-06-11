f56fbabafb75d049dced141cca471965052a2ab7
Misko Hevery <misko@hevery.com>
2020-06-11 20:53:12 +0000

    97dc85ba5e feat(core): support injection token as predicate in queries (#37506)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+134.sha-a937889";
+export declare const NGCC_VERSION = "10.0.0-rc.0+135.sha-97dc85b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
