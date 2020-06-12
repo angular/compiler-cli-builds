c757db7ed1f8577ac1062fe0b2a62b0e3fe99bda
Misko Hevery <misko@hevery.com>
2020-06-12 22:31:59 +0000

    62e4acc3d1 docs: fix typo in control_value_accessor.ts (#37057)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+164.sha-1bc807c";
+export declare const NGCC_VERSION = "10.0.0-rc.0+168.sha-62e4acc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
