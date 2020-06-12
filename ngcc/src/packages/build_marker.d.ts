97be7c24ccff97c2055069000ac66511b1dbb393
Misko Hevery <misko@hevery.com>
2020-06-12 22:25:19 +0000

    1bc807c81a style(common): enforce format on newly included files (#36940)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+161.sha-8dcf618";
+export declare const NGCC_VERSION = "10.0.0-rc.0+164.sha-1bc807c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
