5334ca5920db49ddfad33af9e78192d323c40506
crisbeto <crisbeto@abv.bg>
2020-06-12 23:03:32 +0000

    6c1ab479ee fix(core): infinite loop if injectable using inheritance has a custom decorator

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -11,7 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '9.1.11';
+    exports.NGCC_VERSION = '9.1.11+1.sha-6c1ab47';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
