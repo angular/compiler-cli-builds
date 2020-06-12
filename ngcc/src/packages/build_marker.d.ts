b13443cd538af6d928fdbd8f8ff600e1735082f8
Misko Hevery <misko@hevery.com>
2020-06-12 16:12:34 +0000

    aba33de5f5 build: fix outdated payload size (#37551)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+159.sha-a075260";
+export declare const NGCC_VERSION = "10.0.0-rc.0+158.sha-aba33de";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
