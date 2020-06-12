e6a9f8bddfae680804021d6e081c66feb337e2fc
Misko Hevery <misko@hevery.com>
2020-06-12 16:12:00 +0000

    a075260751 docs: document how external link icons work (#37025)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+138.sha-284123c";
+export declare const NGCC_VERSION = "10.0.0-rc.0+159.sha-a075260";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
