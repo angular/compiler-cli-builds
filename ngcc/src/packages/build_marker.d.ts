96490abc8259e6748550512debd2bd564b8a2b5e
Misko Hevery <misko@hevery.com>
2020-06-15 21:56:01 +0000

    87a679b210 build: import in-memory-web-api project (#37182)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+185.sha-2a145f2";
+export declare const NGCC_VERSION = "10.0.0-rc.0+182.sha-87a679b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
