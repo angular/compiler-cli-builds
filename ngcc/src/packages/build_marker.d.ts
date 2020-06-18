89d111631799cd9ec251082ab547155152904843
Misko Hevery <misko@hevery.com>
2020-06-18 23:24:10 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+193.sha-71f008f";
+export declare const NGCC_VERSION = "10.0.0-rc.0+198.sha-bd7f440";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
