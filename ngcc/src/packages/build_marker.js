d50b36c7ed1b0ccb937e5eb231cf1bbddfb22f7a
Misko Hevery <misko@hevery.com>
2020-06-18 23:29:12 +0000

    1d844b9bd8 docs(core): correct type for opts.read (#37626)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.0+195.sha-53e95233';
+    exports.NGCC_VERSION = '10.0.0-rc.0+196.sha-1d844b9';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
