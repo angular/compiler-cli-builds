599290878e05317619e1fc558e6a0f122625e3df
Misko Hevery <misko@hevery.com>
2020-06-22 18:25:15 +0000

    c8c0063499 ci: disable saucelabs tests on Firefox ESR while investigating failures (#37647)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+208.sha-fd65958";
+export declare const NGCC_VERSION = "10.0.0-rc.0+212.sha-c8c0063";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
