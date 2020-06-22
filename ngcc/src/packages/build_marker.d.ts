a6550336895ba14deb31489755cb9c95a9b9cc30
Misko Hevery <misko@hevery.com>
2020-06-22 18:18:12 +0000

    fd65958b88 test: update symbol goldens to reflect optimized application (#37623)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+200.sha-7e0eccc";
+export declare const NGCC_VERSION = "10.0.0-rc.0+208.sha-fd65958";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
