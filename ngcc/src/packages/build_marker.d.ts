0c6b4b22b6839d1f99cc4323237edd3378a042e9
Misko Hevery <misko@hevery.com>
2020-06-22 23:51:55 +0000

    4c1edd52c5 docs: Add content for new `strict` mode for Angular CLI (#37486)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.6+13.sha-d512e27";
+export declare const NGCC_VERSION = "10.0.0-rc.6+14.sha-4c1edd5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.6+13.sha-d512e27';
+    exports.NGCC_VERSION = '10.0.0-rc.6+14.sha-4c1edd5';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
