293697ea5be6b93447bad822a139eec41a4ce2c9
Misko Hevery <misko@hevery.com>
2020-06-22 18:18:09 +0000

    d512e27979 docs: fix invalid anchor for CLI flags in deprecation guide (#37662)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.6+8.sha-0d29259";
+export declare const NGCC_VERSION = "10.0.0-rc.6+13.sha-d512e27";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
