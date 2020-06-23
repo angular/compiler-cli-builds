e98ae0b403fbce301b99dc747aa3541d5fca50d3
Misko Hevery <misko@hevery.com>
2020-06-23 20:34:04 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.6+17.sha-82c8b44";
+export declare const NGCC_VERSION = "10.0.0-rc.6+20.sha-36a00a2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
