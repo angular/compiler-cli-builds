59a11af85e18b1cb9b55787d78bfbc9ebce13af5
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:21:16 +0000

    5229809adb docs: Initial commit of update guide for v10 release. (#37152)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+216.sha-5c40fd6";
+export declare const NGCC_VERSION = "10.0.0-rc.0+220.sha-5229809";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
