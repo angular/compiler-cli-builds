ce91d2994ac521394ff126718bef7ed94384f7f6
Misko Hevery <misko@hevery.com>
2020-06-24 23:23:11 +0000

    96891a076f release: sort the v10.0.0 release in `CHANGELOG.md`

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.6+23.sha-c49507b";
+export declare const NGCC_VERSION = "10.0.0+2.sha-96891a0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
