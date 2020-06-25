b9c0c5bd2880f9f55c8cb5a4188f3ace01c3adaf
Igor Minar <igor@angular.io>
2020-06-25 21:06:52 +0000

    0feb560136 fixup! feat(docs-infra): update deploy-to-firebase.sh script to support v9 multisite setup

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.11+1.sha-6c1ab47";
+export declare const NGCC_VERSION = "9.1.11+3.sha-0feb560";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
