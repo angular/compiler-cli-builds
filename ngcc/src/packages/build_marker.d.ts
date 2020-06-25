30d1e829016d7576a36688a67158b3737c7422fb
Misko Hevery <misko@hevery.com>
2020-06-25 03:17:05 +0000

    f5bbfd7b8c docs(changelog): fix v10 announcement url (#37722)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+226.sha-41ce524";
+export declare const NGCC_VERSION = "10.0.0-rc.0+227.sha-f5bbfd7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
