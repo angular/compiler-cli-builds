a3cd3f07a058b608ea202af024db556c3be38957
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:15:24 +0000

    4967997536 ci(compiler-cli): exempt compiler-cli .bazel files from dev-infra approval (#37558)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+237.sha-1c1eb30";
+export declare const NGCC_VERSION = "10.0.0-rc.0+236.sha-4967997";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.0+237.sha-1c1eb30';
+    exports.NGCC_VERSION = '10.0.0-rc.0+236.sha-4967997';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
