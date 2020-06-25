00a32c0d328587449f9140dc6d41ba61321da12a
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:56:42 +0000

    9118f49a63 fix(core): cleanup DOM elements when root view is removed (#37600)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+260.sha-d12cdb5";
+export declare const NGCC_VERSION = "10.0.0-rc.0+262.sha-9118f49";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
