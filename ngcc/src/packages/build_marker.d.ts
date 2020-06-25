81efe96ac58e51bd737479d69c69941cd4f9c95a
Igor Minar <igor@angular.io>
2020-06-25 22:56:56 +0000

    20c44a6240 fix(docs-infra): fix typo in the deploy-to-firebase.sh script

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "9.1.11+3.sha-0feb560";
+export declare const NGCC_VERSION = "9.1.11+4.sha-20c44a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
