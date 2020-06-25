835340de1d0f57f89240b9b08184f2277d32e073
Misko Hevery <misko@hevery.com>
2020-06-25 03:12:42 +0000

    0d2cdf6165 docs: add v9.angular.io to the angular.io version picker (#37719)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0+2.sha-96891a0";
+export declare const NGCC_VERSION = "10.0.0+4.sha-0d2cdf6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
