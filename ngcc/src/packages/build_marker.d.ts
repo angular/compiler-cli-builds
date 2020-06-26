eca07c8a74ef0a5b379447669122ed70fd6acb5b
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:31:07 +0000

    638dd15f61 build: add wombot proxy for publish config for @angular/benchpress (#37752)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+268.sha-9c668e0";
+export declare const NGCC_VERSION = "10.0.0-rc.0+269.sha-638dd15";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
