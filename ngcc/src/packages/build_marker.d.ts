c95d7e8eb917aed9fbc59bdcf39b79df2b960b6a
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:23:58 +0000

    c942662d79 fix(language-service): reinstate getExternalFiles() (#37750)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+273.sha-f954ab6";
+export declare const NGCC_VERSION = "10.0.0-rc.0+275.sha-c942662";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
