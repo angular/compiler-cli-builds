b233a872e98476753d6d954e8f6fa975d5cdd62d
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:39:34 +0000

    acf3cff9ee ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+276.sha-e0eeb4a";
+export declare const NGCC_VERSION = "10.0.0-rc.0+281.sha-acf3cff";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
