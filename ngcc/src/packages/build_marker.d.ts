11b1cc20a2572dee94b4e0a7631bb96111c7a988
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:20:59 +0000

    2038568f3e ci: decrease expected AIO and integration payload sizes (#36578) (#36578)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+283.sha-ae5257c";
+export declare const NGCC_VERSION = "10.0.0-rc.0+287.sha-2038568";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
