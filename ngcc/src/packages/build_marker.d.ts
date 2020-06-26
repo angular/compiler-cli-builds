830631026ec33e687a84990d3f634bf476a2169e
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:13:02 +0000

    3ee666580a fix(dev-infra): merge script should not always require full repo permissions (#37718)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+270.sha-eee2fd2";
+export declare const NGCC_VERSION = "10.0.0-rc.0+272.sha-3ee6665";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
