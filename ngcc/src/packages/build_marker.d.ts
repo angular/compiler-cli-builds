3c957dee211b6b001673594e9837e9c7dbcedfcb
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:24:08 +0000

    6341a837c1 docs: correct outdated dev instructions for public api golds (#37026)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+271.sha-dbc2364";
+export declare const NGCC_VERSION = "10.0.0-rc.0+274.sha-6341a83";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
