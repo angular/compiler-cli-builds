23cd9f8187d20c3dcf69faa3c58168539b4871df
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:21:26 +0000

    873330b82c docs: correct the spelling mistake in observables error handling code (#36437)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+300.sha-435a28e";
+export declare const NGCC_VERSION = "10.0.0-rc.0+302.sha-873330b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
