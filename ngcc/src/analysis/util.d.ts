4f6cfac92fdf267614f3ef4a5930666acde0c702
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:43:15 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- ngcc/src/analysis/util.d.ts
+++ ngcc/src/analysis/util.d.ts
@@ -6,8 +6,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as ts from 'typescript';
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { DependencyTracker } from '../../../src/ngtsc/incremental/api';
-export declare function isWithinPackage(packagePath: AbsoluteFsPath, sourceFile: ts.SourceFile): boolean;
+export declare function isWithinPackage(packagePath: AbsoluteFsPath, filePath: AbsoluteFsPath): boolean;
 export declare const NOOP_DEPENDENCY_TRACKER: DependencyTracker;
