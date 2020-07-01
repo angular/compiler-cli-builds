001b9b950c53269799201a23933b3d558207ff81
Andrew Kushnir <akushnir@google.com>
2020-07-01 16:48:14 +0000

    1d5ca55aab docs(changelog): remove empty sections (#36264)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.0-rc.0+309.sha-9206a26';
+    exports.NGCC_VERSION = '10.0.0-rc.0+311.sha-1d5ca55';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
