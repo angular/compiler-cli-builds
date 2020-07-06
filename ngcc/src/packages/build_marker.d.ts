8f165629139b517d528541c970ccbacf97565d50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 17:00:32 +0000

    a5ffca0576 fix(router): fix navigation ignoring logic to compare to the browser url (#37716)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+312.sha-196bfa8";
+export declare const NGCC_VERSION = "10.0.0-rc.0+313.sha-a5ffca0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
