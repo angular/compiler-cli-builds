1e6fe9bfcd29de19ca8a37eec764e17f54344df3
atscott <atscott@google.com>
2020-07-08 23:01:06 +0000

    879b2273c1 ci: decrease the minBodyLength commit message limit to 20 chars (#37949)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.3";
+export declare const NGCC_VERSION = "10.0.3+1.sha-879b227";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
