a2703770e94b1f1582b76c0f301b75d333ae6ab6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:31:21 +0000

    82a2207f4a build: filter out duplicate cherry-picked commits in changelog (#37956)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.0-rc.0+325.sha-289a96b";
+export declare const NGCC_VERSION = "10.0.0-rc.0+326.sha-82a2207";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
