9902307907ebfdafcc3b7ae072a04c16e3619ff9
atscott <atscott@google.com>
2020-07-09 16:42:51 +0000

    b98314e306 docs: add Formly library to the list of resources (#37257)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+9.sha-6b731a0";
+export declare const NGCC_VERSION = "10.1.0-next.0+11.sha-b98314e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
