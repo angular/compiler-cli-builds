a7de70d83c38ce8f23a44cff9688f781af0abe52
atscott <atscott@google.com>
2020-07-09 17:35:59 +0000

    8ee23ba67b docs: add explanation for providedIn any (#35283)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.3+4.sha-9f2393f";
+export declare const NGCC_VERSION = "10.0.3+18.sha-8ee23ba";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
