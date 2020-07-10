4f975cbdfb6273058c893708e4fcccb5b964ae4a
Andrew Scott <atscott01@gmail.com>
2020-07-10 17:26:10 +0000

    4b4b74548d fix(dev-infra): include `bazel` utility files in npm package (#37891)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.0+19.sha-1550663';
+    exports.NGCC_VERSION = '10.1.0-next.0+20.sha-4b4b745';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
