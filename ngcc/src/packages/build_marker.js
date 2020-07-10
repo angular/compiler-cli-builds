f182b0408a919fa8585b06e916e1cac85a49a84f
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:21:30 +0000

    9bd4b74b06 feat(dev-infra): commit message validation should skip lines consisting of URLs (#37890)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.0+20.sha-4b4b745';
+    exports.NGCC_VERSION = '10.1.0-next.0+22.sha-9bd4b74';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
