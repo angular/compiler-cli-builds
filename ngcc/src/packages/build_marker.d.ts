ca8b29448776c8b1d1fe7f682adb5921edb0d38f
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:31:37 +0000

    36dd817913 docs: fixed typo in https://angular.io/guide/glossary (#36220)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+33.sha-6b31155";
+export declare const NGCC_VERSION = "10.1.0-next.0+37.sha-36dd817";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
