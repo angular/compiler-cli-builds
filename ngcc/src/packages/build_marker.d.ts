7c0e5a783d3f8ea3842990b52e67dc8712b46d27
Andrew Scott <atscott01@gmail.com>
2020-07-13 19:31:56 +0000

    6b311552f0 fix(compiler-cli): ensure file_system handles mixed Windows drives (#37959)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+32.sha-66947cf";
+export declare const NGCC_VERSION = "10.1.0-next.0+33.sha-6b31155";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
