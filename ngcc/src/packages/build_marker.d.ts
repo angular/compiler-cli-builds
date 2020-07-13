b65d4bbec45e2345153b041228945d434287e436
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:41:39 +0000

    69472a1ed0 refactor(dev-infra): allow for consumption with rules_nodejs v2.0.0 (#37968)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+38.sha-10aba15";
+export declare const NGCC_VERSION = "10.1.0-next.0+39.sha-69472a1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
