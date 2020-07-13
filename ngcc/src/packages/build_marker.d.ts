411386dfacc370a550eba29bf6d8c391e0721019
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:40:46 +0000

    10aba15154 fix(language-service): remove completion for string (#37983)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+36.sha-0eaa084";
+export declare const NGCC_VERSION = "10.1.0-next.0+38.sha-10aba15";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
