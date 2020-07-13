4af04d970d8859d3dd61b7eed150baed77c5bd71
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:54:05 +0000

    9cf78d5701 docs(core): Fixed typo in Type JSdoc (#37930)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.3+45.sha-53e4ff7";
+export declare const NGCC_VERSION = "10.0.3+52.sha-9cf78d5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.3+45.sha-53e4ff7';
+    exports.NGCC_VERSION = '10.0.3+52.sha-9cf78d5';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
