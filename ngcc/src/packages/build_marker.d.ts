d6234344d501be934fca3ddd2c19ee94136322fc
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:34:30 +0000

    0eaa084ced docs(router): fix typo 'containa' to 'contains' (#36764)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+35.sha-62e19c8";
+export declare const NGCC_VERSION = "10.1.0-next.0+36.sha-0eaa084";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
