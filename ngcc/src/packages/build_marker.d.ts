3c5783002ba61a1ff9f95a0fb5f559162bbbe4ae
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-14 18:39:59 +0000

    e52ac490c8 docs(core): fixes minor typo in initNgDevMode function docs

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+42.sha-2dd22b7";
+export declare const NGCC_VERSION = "10.1.0-next.0+42.sha-e52ac49";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
