b1798e7c078c428cb9151462d0868c4c1fb20fde
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:26:04 +0000

    5fd2c8f2d6 refactor(forms): remove unnecessary `!` operators from validators (#36805)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.0+44.sha-f01b033";
+export declare const NGCC_VERSION = "10.1.0-next.0+47.sha-5fd2c8f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
