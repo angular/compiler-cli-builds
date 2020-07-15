d63ee6cca23d2ef36cf33a28d003b2feb8293c4e
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:00:15 +0000

    45d79efc3f docs: remove all references to Angular Console (#37608)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+6.sha-09eee5b";
+export declare const NGCC_VERSION = "10.1.0-next.1+2.sha-45d79ef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
