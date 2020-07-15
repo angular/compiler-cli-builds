d35ea787d3e1d2a40b5936119cef5f50ce9e6382
Andrew Kushnir <akushnir@google.com>
2020-07-15 19:58:29 +0000

    09eee5b312 fix(dev-infra): add missing BUILD file to dev-infra/bazel:files (#38026)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+3.sha-b00121c";
+export declare const NGCC_VERSION = "10.1.0-next.1+6.sha-09eee5b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
