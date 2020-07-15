b76c6bf779720080fbf0a55d7b3cc3011846a007
Andrew Kushnir <akushnir@google.com>
2020-07-15 19:58:07 +0000

    b00121c9a6 docs: update reference doc for router guards and resolvers (#38079)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+1.sha-1d9e56e";
+export declare const NGCC_VERSION = "10.1.0-next.1+3.sha-b00121c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
