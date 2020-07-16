f263f1fe20599c39fa38ba46450648d5767d77fa
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:31:47 +0000

    8b25a64200 build(language-service): remove typescript from ivy bundle (#38088)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+19.sha-019a696";
+export declare const NGCC_VERSION = "10.1.0-next.1+18.sha-8b25a64";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
