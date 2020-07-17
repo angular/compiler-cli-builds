8dee28e9a459a3b1850104d6619aa4cf55d7afeb
Andrew Kushnir <akushnir@google.com>
2020-07-17 23:58:05 +0000

    136acdfab6 docs: correct flag default values in `--strict` (#37982)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+28.sha-095cfd9";
+export declare const NGCC_VERSION = "10.1.0-next.1+29.sha-136acdf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
