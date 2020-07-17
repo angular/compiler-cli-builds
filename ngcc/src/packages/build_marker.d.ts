e142af2e6b4999912d1f5b7ce831eb003f41f202
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:10:12 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+24.sha-58f6079";
+export declare const NGCC_VERSION = "10.1.0-next.1+25.sha-fd45f74";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
