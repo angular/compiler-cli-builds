5d73fd8389fdb70907d0d509d3659b3ba307e185
Andrew Kushnir <akushnir@google.com>
2020-07-17 21:00:41 +0000

    095cfd961d build: Ignore .history for the xyz.local-history VSCode extension (#38121)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+27.sha-38a7021";
+export declare const NGCC_VERSION = "10.1.0-next.1+28.sha-095cfd9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
