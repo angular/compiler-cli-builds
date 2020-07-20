1532fd0b73966bab6a69ab065f619c7a8cb4d412
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:56:22 +0000

    655da32837 build: update pullapprove to remove @matsko from reviewer lists (#38146)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+36.sha-5b31a0a";
+export declare const NGCC_VERSION = "10.1.0-next.1+37.sha-655da32";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
