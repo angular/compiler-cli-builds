285e963b2008c3943e919401b3d361e007635d06
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:24:15 +0000

    e80278cf02 fix(compiler): properly associate source spans for implicitly closed elements (#38126)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.4+23.sha-8e305e7";
+export declare const NGCC_VERSION = "10.0.4+27.sha-e80278c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
