7cd89aab08debd14e4ebe51fa4afe27d8cf10883
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:44:49 +0000

    9cb318f5a2 docs: separate template syntax into multiple docs (#36954)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.4+27.sha-e80278c";
+export declare const NGCC_VERSION = "10.0.4+31.sha-9cb318f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
