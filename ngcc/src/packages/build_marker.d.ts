528b7b58cf81698c3d7fa725e9c39431257e8f83
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:39:06 +0000

    5d3d8b8330 docs: add known issue for bazel (#38074)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+34.sha-c8c7415";
+export declare const NGCC_VERSION = "10.1.0-next.1+35.sha-5d3d8b8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.1+34.sha-c8c7415';
+    exports.NGCC_VERSION = '10.1.0-next.1+35.sha-5d3d8b8';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
