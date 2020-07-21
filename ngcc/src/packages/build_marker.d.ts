3c458ea816a86d8c75823c08bb611db87aec297c
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:33:06 +0000

    eae13e42f3 docs: add routing terms to glossary (#38053)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.1+39.sha-4a6dcd0";
+export declare const NGCC_VERSION = "10.1.0-next.1+41.sha-eae13e4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
