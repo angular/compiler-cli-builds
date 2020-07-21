2f99c4e218f0c3a7d461ff21d822cb1282b32e48
Andrew Kushnir <akushnir@google.com>
2020-07-21 21:53:37 +0000

    3f8f3a2daa docs: fix type in lazy-load callout (#38153)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.4+37.sha-f5eeb1a";
+export declare const NGCC_VERSION = "10.0.4+38.sha-3f8f3a2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
