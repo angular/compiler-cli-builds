b200d7632c2b370e426d1345a74a45ab69fd0025
Andrew Kushnir <akushnir@google.com>
2020-07-21 17:08:03 +0000

    2af3d9c040 refactor(core): rename synthetic host property and listener instructions (#38150)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.4+35.sha-4664acc";
+export declare const NGCC_VERSION = "10.0.4+36.sha-2af3d9c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
