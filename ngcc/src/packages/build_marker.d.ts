3ce82b4ee4674021363c3c5e483cdf32260e7e54
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:56:09 +0000

    f5eeb1a714 docs: fix DOCUMENT import path (#38158) (#38159)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.4+36.sha-2af3d9c";
+export declare const NGCC_VERSION = "10.0.4+37.sha-f5eeb1a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
