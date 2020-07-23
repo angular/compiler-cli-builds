3909fb71e9d651c3c3720ddb5d15e320007afe38
Misko Hevery <misko@hevery.com>
2020-07-23 18:31:39 +0000

    88d4b269b5 build(docs-infra): simplify `ExampleZipper` by removing `PackageJsonCustomizer` (#38192)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.2+18.sha-dd09fb5';
+    exports.NGCC_VERSION = '10.1.0-next.2+20.sha-88d4b26';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
