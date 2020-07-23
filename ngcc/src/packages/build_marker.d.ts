441b5e274719105c48cdaa550c0c28c9e22f41aa
Misko Hevery <misko@hevery.com>
2020-07-23 04:01:17 +0000

    8df888dfb4 fix(elements): run strategy methods in correct zone (#37814)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+9.sha-778ad37";
+export declare const NGCC_VERSION = "10.1.0-next.2+10.sha-8df888d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
