0f07306705d552ddc0a4f7ca6070d41c48891d9b
Misko Hevery <misko@hevery.com>
2020-07-23 18:32:39 +0000

    d0060dcfdc refactor(dev-infra): Add support for groups in the conditions evaluator (#38164)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+20.sha-88d4b26";
+export declare const NGCC_VERSION = "10.1.0-next.2+17.sha-d0060dc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
