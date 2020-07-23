f1ed6dc0c5ebe34580fdc0d32cbbbb354747e3e1
Misko Hevery <misko@hevery.com>
2020-07-23 04:21:34 +0000

    3c1866b5bb docs: update api reference for router outlet directive (#38166)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+11.sha-062b8d9";
+export declare const NGCC_VERSION = "10.1.0-next.2+12.sha-3c1866b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
