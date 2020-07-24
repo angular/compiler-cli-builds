eeec157c61af4f2367aca389d4c172152aa067b7
Misko Hevery <misko@hevery.com>
2020-07-24 22:33:48 +0000

    253337dc0a feat(zone.js): move MutationObserver/FileReader to different module (#31657)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+23.sha-6e2db22";
+export declare const NGCC_VERSION = "10.1.0-next.2+24.sha-253337d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
