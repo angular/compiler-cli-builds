c2a393cb7214f3610bdefd02c4d9ddbc243f6c9f
Misko Hevery <misko@hevery.com>
2020-07-24 23:16:20 +0000

    6a3e68b606 docs(zone.js): update zone.js bundle doc since the APF change (#37919)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.2+26.sha-1822cbc';
+    exports.NGCC_VERSION = '10.1.0-next.2+27.sha-6a3e68b';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
