9a9f8e1319a6a26eff64992fd9da44b428f777c4
Misko Hevery <misko@hevery.com>
2020-07-24 15:38:30 +0000

    b8af10902f docs: fixed that class attribute is not closed (#38219)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.5+15.sha-f411c9e";
+export declare const NGCC_VERSION = "10.0.5+16.sha-b8af109";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
