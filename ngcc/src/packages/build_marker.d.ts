5da8dd7458e1746425d8eebb1a57044b859c4b5b
Misko Hevery <misko@hevery.com>
2020-07-24 17:14:54 +0000

    cce5583e75 docs(core): Fix incorrectly rendered code example in structural directives guide (#38207)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+21.sha-307db74";
+export declare const NGCC_VERSION = "10.1.0-next.2+22.sha-cce5583";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
