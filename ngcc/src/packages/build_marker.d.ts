e8705a48bee990ee64c6813c8360ec2c43b966c9
Misko Hevery <misko@hevery.com>
2020-07-25 01:22:23 +0000

    a123ef58b1 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.5+20.sha-c4e6f58";
+export declare const NGCC_VERSION = "10.0.5+23.sha-a123ef5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
