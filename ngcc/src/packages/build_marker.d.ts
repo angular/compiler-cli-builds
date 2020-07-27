77bf300de04b1f9987801a9c1524144b0f07f2a7
Misko Hevery <misko@hevery.com>
2020-07-27 22:45:57 +0000

    174aac68f7 refactor(platform-browser): specify return type of parseEventName (#38089)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+42.sha-5d3ba8d";
+export declare const NGCC_VERSION = "10.1.0-next.2+43.sha-174aac6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
