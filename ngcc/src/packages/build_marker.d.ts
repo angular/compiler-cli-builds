7ac9cef528a619d08330d4a3ce58229906d4cfa2
Misko Hevery <misko@hevery.com>
2020-07-27 19:37:58 +0000

    96aa14df01 fix(zone.js): zone patch rxjs should return null _unsubscribe correctly. (#37091)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+38.sha-2b6ab57";
+export declare const NGCC_VERSION = "10.1.0-next.2+39.sha-96aa14d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
