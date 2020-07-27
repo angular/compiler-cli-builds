619830d3729abc3a79e7bbefa856ce771eec8d04
Misko Hevery <misko@hevery.com>
2020-07-27 17:44:32 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+35.sha-8e5969b";
+export declare const NGCC_VERSION = "10.1.0-next.2+36.sha-f74cfad";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
