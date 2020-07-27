78fcf6f83803f5bff8e7af4218f1ac4ab7aa507b
Misko Hevery <misko@hevery.com>
2020-07-27 17:10:35 +0000

    1296003445 docs: add ng-add save option (#38198)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.2+30.sha-67e3ecc";
+export declare const NGCC_VERSION = "10.1.0-next.2+32.sha-1296003";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
