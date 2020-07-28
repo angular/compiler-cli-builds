5f79cefb127a07831c2ca035803f11ed68fbd7db
Misko Hevery <misko@hevery.com>
2020-07-28 22:27:45 +0000

    cdda60a430 release: cut the v10.0.6 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.5+51.sha-7570356";
+export declare const NGCC_VERSION = "10.0.6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
