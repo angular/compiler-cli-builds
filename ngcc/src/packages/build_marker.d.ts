d3b8eb619e6251c641c4d8cca5cb3d9765bfc129
Misko Hevery <misko@hevery.com>
2020-07-29 17:56:57 +0000

    103a95c182 docs: update bio picture (#38272)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+12.sha-b280d54";
+export declare const NGCC_VERSION = "10.1.0-next.3+14.sha-103a95c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
