b6fdd6030393e7acddb0a4b9106e360a564f5518
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:11:13 +0000

    1eebb7f189 test(compiler-cli): disable one TypeChecker test on Windows due to path sensitivity issue (#38294)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+16.sha-7f8c222";
+export declare const NGCC_VERSION = "10.1.0-next.3+18.sha-1eebb7f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
