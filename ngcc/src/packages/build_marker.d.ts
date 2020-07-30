4c163e2472447cfcf52d343e2f1c9ff35e41d618
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:44:04 +0000

    fd51e01335 fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+19.sha-3a525d1";
+export declare const NGCC_VERSION = "10.1.0-next.3+20.sha-fd51e01";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
