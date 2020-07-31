691bc88f2d9fa93c388b27a5ccfff7d1d076172e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 18:26:52 +0000

    9c5fc1693a refactor(docs-infra): update docs examples `tslint.json` to match CLI and fix failures (#38143)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.7+2.sha-7ff5ef2";
+export declare const NGCC_VERSION = "10.0.7+17.sha-9c5fc16";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
