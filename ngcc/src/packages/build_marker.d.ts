9725ce80862c8ee3b145b42f3cd0467e3fb7cba5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:20:50 +0000

    e8896b9de3 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+22.sha-6f6102d";
+export declare const NGCC_VERSION = "10.1.0-next.3+23.sha-e8896b9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
