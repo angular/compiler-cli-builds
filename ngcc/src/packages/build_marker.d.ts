2426e53816a5913c84826ccf5106f2a6d532f3a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:59:08 +0000

    cea46786a2 fix(compiler): update unparsable character reference entity error messages (#38319)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.7+17.sha-9c5fc16";
+export declare const NGCC_VERSION = "10.0.7+18.sha-cea4678";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
