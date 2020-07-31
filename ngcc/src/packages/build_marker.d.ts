9f79ec65d3fad69e1ed41f7641d7817821a97221
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:21:15 +0000

    7ff5ef27c7 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.6+21.sha-879ff08f";
+export declare const NGCC_VERSION = "10.0.7+2.sha-7ff5ef2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.6+21.sha-879ff08f';
+    exports.NGCC_VERSION = '10.0.7+2.sha-7ff5ef2';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
