76122ab823024a8b3f87630374de5c43da3a5c95
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:17:04 +0000

    4c7f233fa8 docs: release notes for the v10.0.7 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+20.sha-fd51e01";
+export declare const NGCC_VERSION = "10.1.0-next.3+21.sha-4c7f233";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
