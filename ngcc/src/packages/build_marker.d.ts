a3109a6f4ba1c92c76f3275d9087d2fea9a7992c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:18:00 +0000

    87baa06cc6 revert: docs(core): correct SomeService to SomeComponent (#38325)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+41.sha-e49b053";
+export declare const NGCC_VERSION = "10.1.0-next.3+40.sha-87baa06";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
