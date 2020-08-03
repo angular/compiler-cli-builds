2feb079305b3fcfd09c00c2daad5d20e8955fa4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:14:52 +0000

    e49b053dac build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.3+39.sha-ff9f4de";
+export declare const NGCC_VERSION = "10.1.0-next.3+41.sha-e49b053";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
