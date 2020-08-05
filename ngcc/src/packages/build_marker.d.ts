a3eac0dc1f0906ff1756cf639a0e2c5684a74019
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:21:30 +0000

    ba175be41f fix(compiler-cli): match wrapHost parameter types within plugin interface (#38004)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+6.sha-7630234";
+export declare const NGCC_VERSION = "10.1.0-next.4+5.sha-ba175be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
