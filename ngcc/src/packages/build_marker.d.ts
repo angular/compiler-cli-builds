0c29c69ea7513e4bf160de7fbf50c45cede2f012
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:19:22 +0000

    415131413d fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.8";
+export declare const NGCC_VERSION = "10.0.8+6.sha-4151314";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
