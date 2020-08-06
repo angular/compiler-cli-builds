ff07c13a43fa2e57c01effbdd70281cc51351348
Andrew Kushnir <akushnir@google.com>
2020-08-06 23:33:53 +0000

    df7f3b04b5 fix(service-worker): fix the chrome debugger syntax highlighter (#38332)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+21.sha-7525f3a";
+export declare const NGCC_VERSION = "10.1.0-next.4+22.sha-df7f3b0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
