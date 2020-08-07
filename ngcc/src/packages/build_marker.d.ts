432f6065ec382656f0b9d850039a6b3a757f2574
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:39:52 +0000

    4107abba2d refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#38372)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.8+20.sha-ef7b70a";
+export declare const NGCC_VERSION = "10.0.8+22.sha-4107abb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
