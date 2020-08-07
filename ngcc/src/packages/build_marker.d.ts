629bd36459095c05946875fa27d9bb042e9bffd3
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:26:46 +0000

    445ac15a78 docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+25.sha-856db56";
+export declare const NGCC_VERSION = "10.1.0-next.4+26.sha-445ac15";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
