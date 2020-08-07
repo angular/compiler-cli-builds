3038fdce979605f40a1861a7dc5de57006c93bf6
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:05:05 +0000

    856db56cca refactor(forms): get rid of duplicate functions (#38371)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+24.sha-354e66e";
+export declare const NGCC_VERSION = "10.1.0-next.4+25.sha-856db56";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
