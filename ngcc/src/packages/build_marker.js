b12c8910a4c7a990f222731cfb0dde1ad9c9fd12
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:21:05 +0000

    ef7b70a375 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.8+19.sha-deb290b';
+    exports.NGCC_VERSION = '10.0.8+20.sha-ef7b70a';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
