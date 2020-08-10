7a61e60cae05446744f1cd4268167317780ae68b
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:37:54 +0000

    e34c33cd46 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.4+29.sha-6d8c73a';
+    exports.NGCC_VERSION = '10.1.0-next.4+30.sha-e34c33c';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
