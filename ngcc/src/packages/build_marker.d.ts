31a156a06cac7426e61ef1cfd6a88942f7057a41
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:54:16 +0000

    32109dc414 fix(core): queries not matching string injection tokens (#38321)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.8+28.sha-7f11149";
+export declare const NGCC_VERSION = "10.0.8+31.sha-32109dc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
