d59724f49f6b74eef515f910fc629df0cf5fa455
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:22:26 +0000

    0de93fd402 docs: update team contributors page (#38384)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+28.sha-6ff28ac";
+export declare const NGCC_VERSION = "10.1.0-next.4+27.sha-0de93fd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
