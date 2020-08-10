9c11b0b4abdfdd4d2c03356b3402826ce443c4bc
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:15:44 +0000

    6ff28ac944 docs: Remove redundant sentence from Router (#38398) PR Close #38398

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+26.sha-445ac15";
+export declare const NGCC_VERSION = "10.1.0-next.4+28.sha-6ff28ac";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
