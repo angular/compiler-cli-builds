00d5635648edd4af3bbe0da937c85733d19d0d3c
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:07:54 +0000

    6d8c73a4d6 fix(core): Store the currently selected ICU in `LView` (#38345)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+27.sha-0de93fd";
+export declare const NGCC_VERSION = "10.1.0-next.4+29.sha-6d8c73a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
