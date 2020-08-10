203671b82ff7a144aefd1c60d88e1008da94b88d
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:41:24 +0000

    8f708b561c fix(router): defer loading of wildcard module until needed (#38348)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+30.sha-e34c33c";
+export declare const NGCC_VERSION = "10.1.0-next.4+31.sha-8f708b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
