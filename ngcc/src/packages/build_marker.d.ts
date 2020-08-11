1c32cca71fa0688950bec454a3b8dbe39a71dccf
Andrew Kushnir <akushnir@google.com>
2020-08-11 16:03:41 +0000

    df76a2048b fix(router): restore 'history.state' object for navigations coming from Angular router (#28108) (#28176)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.4+35.sha-3d15616";
+export declare const NGCC_VERSION = "10.1.0-next.4+36.sha-df76a20";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
