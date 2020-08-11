526ab6d66034a7ff593965be903a84f7a081f00b
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:55:12 +0000

    f68a1d3708 build: run formatting automatically on pre-commit hook (#38402)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.8+37.sha-ca2b4bc";
+export declare const NGCC_VERSION = "10.0.8+39.sha-f68a1d3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.0.8+37.sha-ca2b4bc';
+    exports.NGCC_VERSION = '10.0.8+39.sha-f68a1d3';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
