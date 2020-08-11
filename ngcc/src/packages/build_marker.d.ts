da81025e2e0dd6e3b2ef17d1c6d688c5baebe562
Andrew Kushnir <akushnir@google.com>
2020-08-11 18:29:25 +0000

    f6cfc9294b ci: update payload size limits for Closure tests (#38411)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.8+32.sha-02aca13";
+export declare const NGCC_VERSION = "10.0.8+36.sha-f6cfc92";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
