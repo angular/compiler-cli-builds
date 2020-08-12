ae66ebc2fd32c40930b7fa7eab71f55729659ed5
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:52:04 +0000

    2f53bbba20 docs: remove unused Input decorator (#38306)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.9";
+export declare const NGCC_VERSION = "10.0.9+3.sha-2f53bbb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
