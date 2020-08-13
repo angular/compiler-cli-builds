cf8d3a065cc913a925ea30231ed1c2c78f0a78a7
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:09:40 +0000

    f4ced74e3a feat(dev-infra): save invalid commit message attempts to be restored on next commit attempt (#38304)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.5+4.sha-8366eff';
+    exports.NGCC_VERSION = '10.1.0-next.5+5.sha-f4ced74';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
