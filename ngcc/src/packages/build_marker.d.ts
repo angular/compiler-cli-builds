1c53f61e999e32a5fac3d5173f7f89f897239c94
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:04:33 +0000

    ffd1691ba9 feat(dev-infra): save invalid commit message attempts to be restored on next commit attempt (#38304)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.9+4.sha-afd4417";
+export declare const NGCC_VERSION = "10.0.9+5.sha-ffd1691";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
