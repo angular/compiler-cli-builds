e0f1d8e77760f6f0fa756b4873541b991d22cee0
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:04:17 +0000

    71079ce47e fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.5+19.sha-ca79880";
+export declare const NGCC_VERSION = "10.1.0-next.5+20.sha-71079ce";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
