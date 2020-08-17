ac90553d5761f70a1a4289b550b4fca735c1a807
Andrew Scott <atscott@google.com>
2020-08-17 17:40:07 +0000

    cb05c0102f fix(core): move generated i18n statements to the `consts` field of ComponentDef (#38404)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.5+20.sha-71079ce";
+export declare const NGCC_VERSION = "10.1.0-next.5+22.sha-cb05c01";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
