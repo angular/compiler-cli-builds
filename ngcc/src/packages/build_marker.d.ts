d76420da82031376719036b0f0f88086e9ceebe5
Andrew Scott <atscott@google.com>
2020-08-18 17:44:27 +0000

    0af95332e9 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.10";
+export declare const NGCC_VERSION = "10.0.10+6.sha-0af9533";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
