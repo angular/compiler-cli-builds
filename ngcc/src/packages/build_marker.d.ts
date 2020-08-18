0a40e34d734addcbdde9c6dd3a5f5abf6fdcaadf
Andrew Scott <atscott@google.com>
2020-08-18 17:31:38 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.6+3.sha-e472f5f";
+export declare const NGCC_VERSION = "10.1.0-next.6+4.sha-723a9ff";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
