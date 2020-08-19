abf1dd2dd9d71c736457c6bb4963d8b6f780e660
Andrew Scott <atscott@google.com>
2020-08-19 00:26:18 +0000

    398118f708 feat(dev-infra): create a wizard for building commit messages (#38457)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.10+7.sha-dcc3f6d";
+export declare const NGCC_VERSION = "10.0.10+8.sha-398118f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
