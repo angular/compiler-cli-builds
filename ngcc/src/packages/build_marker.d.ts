c891b7d43212d49121b78e45dfdf24c1795b297a
Misko Hevery <misko@hevery.com>
2020-08-19 21:38:27 +0000

    68a9a01a64 fix(localize): parse all parts of a translation with nested HTML (#38452)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+4.sha-0b54c0c";
+export declare const NGCC_VERSION = "10.1.0-next.7+6.sha-68a9a01";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
