0f7f0cd4c2317026d66faea156cd7e43fff3e0d3
Misko Hevery <misko@hevery.com>
2020-08-19 21:43:52 +0000

    ac461e1efd fix(localize): extract the correct message ids (#38498)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+7.sha-f245c6b";
+export declare const NGCC_VERSION = "10.1.0-next.7+8.sha-ac461e1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
