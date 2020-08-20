80194fec8a82b004396ec73fadee35384ec54e5e
Misko Hevery <misko@hevery.com>
2020-08-20 04:25:39 +0000

    87bbf69ce8 Revert "fix(core): remove closing body tag from inert DOM builder (#38454)"

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.11+5.sha-5528536";
+export declare const NGCC_VERSION = "10.0.11+6.sha-87bbf69";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
