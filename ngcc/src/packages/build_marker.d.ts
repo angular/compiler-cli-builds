f29c798c3c7c9dd7c59cd968b053382d681fd277
Misko Hevery <misko@hevery.com>
2020-08-20 20:28:39 +0000

    7f2a3e4b6b docs: apply code styling in template reference variables guide (#38522)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.11+10.sha-1775f35";
+export declare const NGCC_VERSION = "10.0.11+11.sha-7f2a3e4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
