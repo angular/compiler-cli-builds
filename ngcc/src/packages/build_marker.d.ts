e98ef4306e5856dc12f77c5d70efa3f7685c840a
Misko Hevery <misko@hevery.com>
2020-08-20 16:50:35 +0000

    4ee5e730ab build: upgrade cli command docs sources to ef770f1cb (#38546)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+8.sha-ac461e1";
+export declare const NGCC_VERSION = "10.1.0-next.7+11.sha-4ee5e73";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
