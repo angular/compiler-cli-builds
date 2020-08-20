525ed165d717e2c96c11f1c99ea40624530d6796
Misko Hevery <misko@hevery.com>
2020-08-20 17:03:31 +0000

    8017ca4db3 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+10.sha-6442875";
+export declare const NGCC_VERSION = "10.1.0-next.7+13.sha-8017ca4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
