20d850e6c44a8827788f435ddfdfd0f29288667d
Misko Hevery <misko@hevery.com>
2020-08-21 19:39:33 +0000

    2a643e1ab6 docs: change function name from async -> waitForAsync (#38548)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+14.sha-956b25a";
+export declare const NGCC_VERSION = "10.1.0-next.7+16.sha-2a643e1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
