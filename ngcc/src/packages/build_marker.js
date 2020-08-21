d751a35e464f45d02a23922adc7bd4872fe2938d
Misko Hevery <misko@hevery.com>
2020-08-21 19:52:52 +0000

    874792dc43 feat(compiler): support unary operators for more accurate type checking (#37918)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.7+17.sha-e7da404';
+    exports.NGCC_VERSION = '10.1.0-next.7+18.sha-874792d';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
