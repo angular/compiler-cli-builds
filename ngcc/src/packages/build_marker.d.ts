c16f3915da5d276576bb1f4bd770bebb9598c5ad
Misko Hevery <misko@hevery.com>
2020-08-24 16:52:06 +0000

    201a546af8 perf(forms): use internal `ngDevMode` flag to tree-shake error messages in prod builds (#37821)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+20.sha-4985267";
+export declare const NGCC_VERSION = "10.1.0-next.7+22.sha-201a546";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
