f7f7b11703f17e1b31fe6f9beb09d5900f29de64
Misko Hevery <misko@hevery.com>
2020-08-24 16:48:36 +0000

    8224e65e70 docs(localize): fix angular.json syntax (#38553)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.11+13.sha-814b436";
+export declare const NGCC_VERSION = "10.0.11+16.sha-8224e65";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
