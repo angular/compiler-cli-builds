9970f152a74995a3011f0957017b9c47cc7979c8
Misko Hevery <misko@hevery.com>
2020-08-24 16:51:51 +0000

    b48cc6ead5 feat(language-service): introduce hybrid visitor to locate AST node (#38540)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+22.sha-201a546";
+export declare const NGCC_VERSION = "10.1.0-next.7+19.sha-b48cc6e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
