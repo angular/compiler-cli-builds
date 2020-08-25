1c659d6432c4e5dd1ae3af9856e0e699bbb0dad0
Misko Hevery <misko@hevery.com>
2020-08-25 22:37:14 +0000

    841dfa68f9 docs: typo fixes in the component testing scenarios guide (#38574)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.8+10.sha-14e90be";
+export declare const NGCC_VERSION = "10.1.0-next.8+12.sha-841dfa6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
