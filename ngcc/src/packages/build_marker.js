6d0f0a02fd536852f17b50ef5f781782ed642846
Misko Hevery <misko@hevery.com>
2020-08-25 22:41:14 +0000

    f0af387f6c fix(localize): ensure required XLIFF parameters are serialized (#38575)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-next.8+12.sha-841dfa6';
+    exports.NGCC_VERSION = '10.1.0-next.8+11.sha-f0af387';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
