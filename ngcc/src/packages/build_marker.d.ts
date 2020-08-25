e893f8c564a4f92e4c2703fff2806b872a0719e7
Misko Hevery <misko@hevery.com>
2020-08-25 22:35:53 +0000

    14e90bef58 fix(localize): render text of extracted placeholders (#38536)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-next.7+31.sha-94a3e0e";
+export declare const NGCC_VERSION = "10.1.0-next.8+10.sha-14e90be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
