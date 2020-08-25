a45a559ecaa964e1d656390ec5d3633c24e1b644
Misko Hevery <misko@hevery.com>
2020-08-25 22:44:06 +0000

    91689193bf docs: Minor fixes in NgModules section (#36177)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.12+3.sha-d066368";
+export declare const NGCC_VERSION = "10.0.12+4.sha-9168919";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
