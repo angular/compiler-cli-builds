417291934930da0bea7e572028f7559c837e9264
Joey Perrott <josephperrott@gmail.com>
2020-08-27 16:32:39 +0000

    94fd5c772b test(docs-infra): add commands to run the unit tests (#34537)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.14+3.sha-89ff446";
+export declare const NGCC_VERSION = "10.0.14+5.sha-94fd5c7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
