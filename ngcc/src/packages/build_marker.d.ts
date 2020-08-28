8162ae78b27348d319e6a5e7d06490dd876eb83c
Joey Perrott <josephperrott@gmail.com>
2020-08-28 18:39:40 +0000

    cb6bf40550 fix(bazel): fix integration test for bazel building (#38629)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.14+6.sha-43ddfba";
+export declare const NGCC_VERSION = "10.0.14+13.sha-cb6bf40";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
