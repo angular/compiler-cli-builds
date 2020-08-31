9e94ed193551350b6b40d2986c000af5cd93443f
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:40 +0000

    c1bc070b40 refactor(dev-infra): remove `style` type from commit style guide (#38639)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-rc.0+20.sha-52c7a4b";
+export declare const NGCC_VERSION = "10.1.0-rc.0+16.sha-c1bc070";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
