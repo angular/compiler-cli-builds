fa69464476bef58d9f81ac5c3a890c456f69ca9a
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:59:32 +0000

    ebc0e46501 refactor(dev-infra): improve error message for unexpected version branches (#38622)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.0-rc.0+23.sha-d5fabc3';
+    exports.NGCC_VERSION = '10.1.0-rc.0+22.sha-ebc0e46';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
