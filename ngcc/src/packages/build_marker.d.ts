693ebed3512e9f8d9915263b90845c8be42d943c
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:54:21 +0000

    91dfb18840 refactor(forms): remove extra space in error message (#38637)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-rc.0+12.sha-6b1a505";
+export declare const NGCC_VERSION = "10.1.0-rc.0+14.sha-91dfb18";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
