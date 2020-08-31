fcc67bcd716e2e9a28791a940585c47ea619851c
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:51:34 +0000

    b060837e70 docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.14+21.sha-9a64980";
+export declare const NGCC_VERSION = "10.0.14+22.sha-b060837";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
