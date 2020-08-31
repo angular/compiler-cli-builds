ed134f21b8a2ae3f055f97fdbdbf4a310020e0d6
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:04:35 +0000

    5d5caf21b8 docs: fix broken markdown in start/start-data (#38644) PR Close #38644

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-rc.0+15.sha-930eeaf";
+export declare const NGCC_VERSION = "10.1.0-rc.0+17.sha-5d5caf2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
