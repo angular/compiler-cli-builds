bfb79aaf6ba2afa566cfcd63f54b7716ed7a7e41
Joey Perrott <josephperrott@gmail.com>
2020-08-31 20:48:23 +0000

    a938180688 fix(compiler): incorrectly inferring namespace for HTML nodes inside SVG (#38477)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.0.14+23.sha-7d5ea55";
+export declare const NGCC_VERSION = "10.0.14+24.sha-a938180";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
