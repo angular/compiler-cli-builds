9f49ea6e9553ed34e1f3de079ed3ad5f27c0dd32
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:56:10 +0000

    5e4aeaa348 refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-rc.0+15.sha-cbbf8b5";
+export declare const NGCC_VERSION = "10.1.0-rc.0+16.sha-5e4aeaa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
