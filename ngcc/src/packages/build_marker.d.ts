8e8fb953376aa2c906d734f8773bd0a948008206
Joey Perrott <josephperrott@gmail.com>
2020-09-01 18:01:24 +0000

    d9fea857db fix(forms): ensure to emit `statusChanges` on subsequent value update/validations (#38354)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.0-rc.0+34.sha-03dbcc7";
+export declare const NGCC_VERSION = "10.1.0-rc.0+35.sha-d9fea85";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
