c35b3c3fea510e98361fa1ddd9ab278e7a2f3010
atscott <atscott01@gmail.com>
2020-09-03 19:21:13 +0000

    1c7e5cef3e docs: add dayjs date adapter to resources page (#38031)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+8.sha-44bb85a";
+export declare const NGCC_VERSION = "11.0.0-next.0+10.sha-1c7e5ce";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
