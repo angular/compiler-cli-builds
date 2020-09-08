10a57dcbe5c85f63cbcc40c111ef36cf0fce9f5e
Andrew Scott <atscott@google.com>
2020-09-08 16:33:14 +0000

    73001b42fe docs: remove reverted bug fix from 10.1 change log (#38718)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.0+12.sha-c90eb54';
+    exports.NGCC_VERSION = '11.0.0-next.0+13.sha-73001b4';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
