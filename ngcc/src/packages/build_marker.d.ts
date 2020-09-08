576ac67e47608bc683857c8bbb521d8946f9a429
atscott <atscott01@gmail.com>
2020-09-08 21:35:17 +0000

    a32a317ea1 fix(compiler-cli): ensure that a declaration is available in type-to-value conversion (#38684)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+41.sha-b0c79f2";
+export declare const NGCC_VERSION = "11.0.0-next.0+40.sha-a32a317";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
