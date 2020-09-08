8269cf4988d5cc7d8a6519642458042978cf6e87
atscott <atscott01@gmail.com>
2020-09-08 21:31:20 +0000

    b0c79f2373 docs: Describe a scenario in which ngOnChanges is not called before ngOnInit. (#38625)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+39.sha-bfb7eec";
+export declare const NGCC_VERSION = "11.0.0-next.0+41.sha-b0c79f2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
