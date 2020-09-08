2b5f7556fc310f7416634e031d3ee6bfe6112db5
atscott <atscott01@gmail.com>
2020-09-08 17:28:30 +0000

    3e57ca1d98 docs: fix typos in deployment guide (#38727)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+18.sha-79d8795";
+export declare const NGCC_VERSION = "11.0.0-next.0+17.sha-3e57ca1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
