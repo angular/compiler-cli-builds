ebd6bac994e7e5234aaa0f4043263a2652bcac0b
atscott <atscott01@gmail.com>
2020-09-08 17:28:14 +0000

    9f28f82598 docs: add space between icon and text in issue template (#38712)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+16.sha-c2d017d";
+export declare const NGCC_VERSION = "11.0.0-next.0+22.sha-9f28f82";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
