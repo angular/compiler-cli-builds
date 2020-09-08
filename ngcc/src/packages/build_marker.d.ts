cfa22648081861856979749dc172394840dca70e
atscott <atscott01@gmail.com>
2020-09-08 17:28:31 +0000

    97475d7408 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+22.sha-9f28f82";
+export declare const NGCC_VERSION = "11.0.0-next.0+24.sha-97475d7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
