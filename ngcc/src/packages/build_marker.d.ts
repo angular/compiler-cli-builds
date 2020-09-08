3fc438c5095c86e838d61c169b5307e4b36d87e5
atscott <atscott01@gmail.com>
2020-09-08 17:29:18 +0000

    a29f9a9fb3 fix(dev-infra): change logging of commit message restoration to debug (#38704)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+17.sha-3e57ca1";
+export declare const NGCC_VERSION = "11.0.0-next.0+23.sha-a29f9a9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
