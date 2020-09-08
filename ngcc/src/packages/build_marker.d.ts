8f4ef14fd7cdbfe8d00145fd296267e438c37f62
atscott <atscott01@gmail.com>
2020-09-08 21:46:35 +0000

    4360eed9b7 fix(localize): enable whitespace preservation marker in XLIFF files (#38737)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+42.sha-c880e39";
+export declare const NGCC_VERSION = "11.0.0-next.0+43.sha-4360eed";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
