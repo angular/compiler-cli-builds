b271d18dc0fae296e457428ec380841ca5d15131
atscott <atscott01@gmail.com>
2020-09-09 17:06:50 +0000

    3d77b64fc3 docs: add Andrew Grekov to GDE resources (#36690)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+50.sha-7997fc5";
+export declare const NGCC_VERSION = "11.0.0-next.0+53.sha-3d77b64";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
