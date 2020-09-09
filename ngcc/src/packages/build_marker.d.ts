c8388e199c196d72476306e6e1960eb7d4d3efb7
atscott <atscott01@gmail.com>
2020-09-09 16:31:24 +0000

    19a484302d docs: Fix component decorator closing brackets (#38754) PR Close #38754

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+49.sha-3cb2a79";
+export declare const NGCC_VERSION = "11.0.0-next.0+47.sha-19a4843";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
