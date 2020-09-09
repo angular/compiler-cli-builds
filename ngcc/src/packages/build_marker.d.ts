c15db52db9bb2932922ef036033f56f970d49225
atscott <atscott01@gmail.com>
2020-09-09 16:30:04 +0000

    f645d26e3f docs(docs-infra): add The Deep Dive podcast, update Angular inDepth URL (#37621)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.0+46.sha-ded075a";
+export declare const NGCC_VERSION = "11.0.0-next.0+52.sha-f645d26";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
