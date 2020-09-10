d7b996a594f4d962722b29a484add8aa85d37b71
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:59:13 +0000

    8f349b2375 fix(compiler): source span for microsyntax text att should be key span (#38766)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+21.sha-03447ba";
+export declare const NGCC_VERSION = "11.0.0-next.1+20.sha-8f349b2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
