936f1c6519b1683f64c6a4a1587c2cebeb552326
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:20:35 +0000

    a47383d1e8 fix(localize): ensure that `formatOptions` is optional (#38787)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.1+11.sha-9078187";
+export declare const NGCC_VERSION = "10.1.1+12.sha-a47383d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
