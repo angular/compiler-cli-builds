beb3c4cb726186e42e56b838277990e788aed678
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:18:52 +0000

    281865bbcf refactor(router): Remove `defer` as it's not needed anymore (#38781)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+5.sha-1373a98";
+export declare const NGCC_VERSION = "11.0.0-next.1+9.sha-281865b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
