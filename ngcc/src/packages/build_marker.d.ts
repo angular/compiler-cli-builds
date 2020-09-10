9ec2471295d219a052fd7547fd06d274ea1bc0e4
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:32:40 +0000

    edb7f90363 fix(core): clear the `RefreshTransplantedView` when detached (#38768)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.1+5.sha-7669bd8";
+export declare const NGCC_VERSION = "10.1.1+9.sha-edb7f90";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
