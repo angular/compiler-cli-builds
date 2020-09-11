a0a0c1a46bbf66c5afda6f4f00a558f28a6c599d
Andrew Kushnir <akushnir@google.com>
2020-09-11 19:32:14 +0000

    15207e3c9c fix(compiler): detect pipes in ICUs in template binder (#38810)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+26.sha-66129f8";
+export declare const NGCC_VERSION = "11.0.0-next.1+27.sha-15207e3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
