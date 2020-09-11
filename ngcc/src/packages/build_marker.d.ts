34bbb6cd06fd98002edf85c55f9d4910581f009f
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:05:21 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.1+13.sha-31d0ee4";
+export declare const NGCC_VERSION = "10.1.1+15.sha-8096c63";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
