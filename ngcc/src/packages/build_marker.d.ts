51bc39a066242571b5665bba9b27e6b90faca435
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:21 +0000

    6768fe9927 fix(dev-infra): correct build setup for dev-infra (#38815)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+29.sha-57c442f";
+export declare const NGCC_VERSION = "11.0.0-next.1+30.sha-6768fe9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
