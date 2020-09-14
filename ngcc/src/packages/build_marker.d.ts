7e9e86fce331922c5420602ef09632f1dab20dc0
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:54:42 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+34.sha-297c060";
+export declare const NGCC_VERSION = "11.0.0-next.1+35.sha-3934f0a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
