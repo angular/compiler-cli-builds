b6bd82998160ac726605ad61786abc519f1b2e04
Andrew Kushnir <akushnir@google.com>
2020-09-15 01:25:08 +0000

    593bd594e3 build: create temporary script for symbol extractor tests (#38819)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+36.sha-78e1ecb";
+export declare const NGCC_VERSION = "11.0.0-next.1+38.sha-593bd59";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
