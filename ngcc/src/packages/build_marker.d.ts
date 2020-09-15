3bbefdd6e4600d259d9c100f6dee1943e8c74067
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:53:28 +0000

    3817e5f1df fix(router): Fix arguments order for call to shouldReuseRoute (#26949)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.1+44.sha-171a0d0";
+export declare const NGCC_VERSION = "11.0.0-next.1+45.sha-3817e5f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
