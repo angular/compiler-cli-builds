5eab1a5c7bcdaf65729b61faf574cd0cc8bf5cb3
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:53:40 +0000

    af3b401e15 docs: add ngc to glossary (#36781)

--- ngcc/src/packages/entry_point_bundle.d.ts
+++ ngcc/src/packages/entry_point_bundle.d.ts
@@ -1,8 +1,17 @@
 /// <amd-module name="@angular/compiler-cli/ngcc/src/packages/entry_point_bundle" />
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as ts from 'typescript';
 import { AbsoluteFsPath, FileSystem } from '../../../src/ngtsc/file_system';
 import { PathMappings } from '../path_mappings';
 import { BundleProgram } from './bundle_program';
 import { EntryPoint, EntryPointFormat } from './entry_point';
+import { SharedFileCache } from './source_file_cache';
 /**
  * A bundle of files and paths (and TS programs) that correspond to a particular
  * format of a package entry-point.
@@ -21,6 +30,8 @@ export interface EntryPointBundle {
  * Get an object that describes a formatted bundle for an entry-point.
  * @param fs The current file-system being used.
  * @param entryPoint The entry-point that contains the bundle.
+ * @param sharedFileCache The cache to use for source files that are shared across all entry-points.
+ * @param moduleResolutionCache The module resolution cache to use.
  * @param formatPath The path to the source files for this bundle.
  * @param isCore This entry point is the Angular core package.
  * @param format The underlying format of the bundle.
@@ -31,4 +42,4 @@ export interface EntryPointBundle {
  * @param enableI18nLegacyMessageIdFormat Whether to render legacy message ids for i18n messages in
  * component templates.
  */
-export declare function makeEntryPointBundle(fs: FileSystem, entryPoint: EntryPoint, formatPath: string, isCore: boolean, format: EntryPointFormat, transformDts: boolean, pathMappings?: PathMappings, mirrorDtsFromSrc?: boolean, enableI18nLegacyMessageIdFormat?: boolean): EntryPointBundle;
+export declare function makeEntryPointBundle(fs: FileSystem, entryPoint: EntryPoint, sharedFileCache: SharedFileCache, moduleResolutionCache: ts.ModuleResolutionCache, formatPath: string, isCore: boolean, format: EntryPointFormat, transformDts: boolean, pathMappings?: PathMappings, mirrorDtsFromSrc?: boolean, enableI18nLegacyMessageIdFormat?: boolean): EntryPointBundle;
