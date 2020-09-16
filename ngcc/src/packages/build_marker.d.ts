1d3bd8de71b3bce06cdd7f0742961244e8419f62
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:46:13 +0000

    49ee90b1b5 docs: fix typo in architecture.md guide (#38853)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+9.sha-e62a918";
+export declare const NGCC_VERSION = "11.0.0-next.2+8.sha-49ee90b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
