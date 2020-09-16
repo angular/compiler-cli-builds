f9ec20bfcea57983a391d49c157d0dc4fc421411
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:44:28 +0000

    e62a918542 docs(forms): fix grammar in first sentence of reset function docs (#38872)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+7.sha-7849fdd";
+export declare const NGCC_VERSION = "11.0.0-next.2+9.sha-e62a918";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
