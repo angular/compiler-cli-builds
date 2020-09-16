697faf37c30bf4e3cffcf9f865d3e6cb52983724
Joey Perrott <josephperrott@gmail.com>
2020-09-16 22:13:10 +0000

    fbcb66e70c release: cut the v10.1.2 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.1+33.sha-3d94919";
+export declare const NGCC_VERSION = "10.1.2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
