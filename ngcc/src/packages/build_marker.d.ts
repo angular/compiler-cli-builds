929bdce3a6543991aa3de171333eb409028b8252
Misko Hevery <misko@hevery.com>
2020-09-17 16:57:26 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+12.sha-1291071";
+export declare const NGCC_VERSION = "11.0.0-next.2+15.sha-a33d630";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
