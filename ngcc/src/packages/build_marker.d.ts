ed2606e5f96704999529d197a0c46c7fd2f8e74d
GitHub <noreply@github.com>
2020-09-17 16:12:13 +0000

    2a8c65bc73 Update aio/content/marketing/events.json

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+12.sha-4a21eb8";
+export declare const NGCC_VERSION = "11.0.0-next.2+13.sha-2a8c65b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
