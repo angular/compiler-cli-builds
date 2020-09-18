f576e92d537dc542571582248202cd02d6c53e99
Misko Hevery <misko@hevery.com>
2020-09-18 15:26:23 +0000

    d795a00137 refactor(compiler): replace Comment nodes with leadingComments property (#38811)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+20.sha-e442486";
+export declare const NGCC_VERSION = "11.0.0-next.2+19.sha-d795a00";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
