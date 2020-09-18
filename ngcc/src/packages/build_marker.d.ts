46f8589c197cb1c09d1a1ecb21c5808dd9460062
Misko Hevery <misko@hevery.com>
2020-09-18 18:42:02 +0000

    55485713a3 fix(dev-infra): skip husky git commit hooks while merging pull requests (#38888)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+18.sha-7fb388f";
+export declare const NGCC_VERSION = "11.0.0-next.2+22.sha-5548571";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
