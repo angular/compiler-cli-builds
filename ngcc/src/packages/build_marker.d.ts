bda6e193553516d4781a6d95fda5d0b7acb8d757
Misko Hevery <misko@hevery.com>
2020-09-18 18:45:19 +0000

    88d7bb8386 fix(http): Fix error message when we call jsonp without importing HttpClientJsonpModule (#38756)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+25.sha-2d6105a";
+export declare const NGCC_VERSION = "11.0.0-next.2+26.sha-88d7bb8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
