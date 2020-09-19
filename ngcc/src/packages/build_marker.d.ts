d9bf20583d054d20457f37d661e32ae8176c6af6
Misko Hevery <misko@hevery.com>
2020-09-19 00:17:21 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.2+21.sha-3902ec0";
+export declare const NGCC_VERSION = "10.1.2+24.sha-7d5b515";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
