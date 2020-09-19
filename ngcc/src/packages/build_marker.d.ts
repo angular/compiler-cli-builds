8b871dd4021df0f5364972024c629ccf009772e7
Misko Hevery <misko@hevery.com>
2020-09-19 00:08:47 +0000

    b68fab547a ci: do not require g3 checks for the changes in `packages/zone.js/dist` folder (#38901)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+23.sha-d92a0dd";
+export declare const NGCC_VERSION = "11.0.0-next.2+28.sha-b68fab5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
