89e524de82a38c9bb5fd70d995ac2f6c8123e0c7
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:13 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+36.sha-fdd4fa0";
+export declare const NGCC_VERSION = "11.0.0-next.2+37.sha-2b1b718";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
