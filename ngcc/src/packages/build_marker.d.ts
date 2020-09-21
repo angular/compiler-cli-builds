a94a7de68f6dde9b5496d9d86eb5d20897d92945
Misko Hevery <misko@hevery.com>
2020-09-21 19:53:02 +0000

    dfbfabc052 fix(docs-infra): remove scrollbar styles for accessibility (#38852)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.2+35.sha-984ed39";
+export declare const NGCC_VERSION = "11.0.0-next.2+33.sha-dfbfabc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
