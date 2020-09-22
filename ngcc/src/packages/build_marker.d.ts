a2037a1a1defa2ada22bbfdf62e0da9979c23e74
Misko Hevery <misko@hevery.com>
2020-09-22 22:32:06 +0000

    32e32e5bdc refactor(dev-infra): simplify runBenchmark (#38941)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.2+24.sha-7d5b515";
+export declare const NGCC_VERSION = "10.1.2+33.sha-32e32e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
