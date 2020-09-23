40db31e240b4f84c38b17fbf8d277f12cbfd3177
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:13:48 +0000

    ffe89fb07d test(docs-infra): remove usage of blacklist in test naming (#38927)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+1.sha-15ea811";
+export declare const NGCC_VERSION = "11.0.0-next.3+2.sha-ffe89fb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
