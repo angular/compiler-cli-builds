fa43c8e1a73928d03af1ad930fc8548af7718f49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:58:31 +0000

    323be39297 fix(language-service): hybrid visitor returns parent node of BoundAttribute (#38995)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+20.sha-239968d";
+export declare const NGCC_VERSION = "11.0.0-next.3+22.sha-323be39";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
