228a649f502a4c06d8e50cd7a711112c92f0bad2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:56:02 +0000

    3ba97ab391 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+65.sha-c5e1789";
+export declare const NGCC_VERSION = "11.0.0-next.3+77.sha-3ba97ab";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
