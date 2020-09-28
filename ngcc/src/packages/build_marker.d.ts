01df42177a77b9b8dc097e7e2a49ff91b0404102
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 16:48:32 +0000

    4744c229db fix(common): correct typing and implementation of `SlicePipe` (#37447)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+22.sha-323be39";
+export declare const NGCC_VERSION = "11.0.0-next.3+29.sha-4744c22";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
