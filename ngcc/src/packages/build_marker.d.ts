e23f01e4381ed0f6432195e71a54d8d672791e61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:52:23 +0000

    393ce5574b ci: do not require g3 checks for the changes in `ngtsc/sourcemaps` folder (#39035)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+88.sha-8f66540";
+export declare const NGCC_VERSION = "11.0.0-next.3+90.sha-393ce55";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
