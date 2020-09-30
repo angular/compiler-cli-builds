0d662bf25cfc653da89531fbe82352fde47fe5e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:58:53 +0000

    8b7acc4f8f refactor(compiler): Binding parser sets binding span as source span in Ivy (#39036)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+93.sha-e8084ff";
+export declare const NGCC_VERSION = "11.0.0-next.3+91.sha-8b7acc4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
