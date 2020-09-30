7543bf5ea789d411c9b4f124db4eab7ff5b1ce7d
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:19:06 +0000

    79620f5139 refactor(localize): avoid free-standing `FileSystem` functions (#39006)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+9.sha-bd5e425";
+export declare const NGCC_VERSION = "11.0.0-next.4+8.sha-79620f5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
