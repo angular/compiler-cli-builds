2a1ecb7804a270d09da39f45e566e9635355d853
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:53:40 +0000

    2aeaedc519 docs(docs-infra): replace a comma with a period at the end of a sentence (#39034)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+87.sha-8f11b51";
+export declare const NGCC_VERSION = "11.0.0-next.3+89.sha-2aeaedc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
