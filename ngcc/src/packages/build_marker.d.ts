de276974d59aeaade4ac2a1f1fd283fa2a3cce91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:14:56 +0000

    a1bf0de711 release: cut the zone.js-0.11.2 release (#39032)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.3+60.sha-69302ad";
+export declare const NGCC_VERSION = "10.1.3+72.sha-a1bf0de";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
