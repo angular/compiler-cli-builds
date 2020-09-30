a48e1efa14bb524280c3a052017007c1c4da7f35
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:53:00 +0000

    5b15e5eeae ci: remove errant comma from angular robot config (#39066)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+8.sha-79620f5";
+export declare const NGCC_VERSION = "11.0.0-next.4+10.sha-5b15e5e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
