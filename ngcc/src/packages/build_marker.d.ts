8653363fac3a9aa676fab04d8f01fdc29cae395e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:43:36 +0000

    ded9aeb447 refactor(language-service): Move two-way binding logic to visitBoundEvent (#38985)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+86.sha-c8958d7";
+export declare const NGCC_VERSION = "11.0.0-next.3+82.sha-ded9aeb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
