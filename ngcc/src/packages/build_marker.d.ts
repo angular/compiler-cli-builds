c6219738348587f11bc79c368de9100bdcd0166f
Joey Perrott <josephperrott@gmail.com>
2020-09-30 22:22:03 +0000

    ea1968317e fix(router): update getRouteGuards to check if the context outlet is activated (#39049)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+10.sha-5b15e5e";
+export declare const NGCC_VERSION = "11.0.0-next.4+11.sha-ea19683";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
