16545dcba3f8aefffebbae218402accd17211445
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:38:38 +0000

    1b70dc931d docs: edit template-statements doc (#38742)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.3+80.sha-a6971ba";
+export declare const NGCC_VERSION = "11.0.0-next.3+78.sha-1b70dc9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
