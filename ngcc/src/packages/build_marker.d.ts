22fff252dd99ee9a212f038c9af5f712cd3a698e
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:54:31 +0000

    21213e1531 fix(dev-infra): run git fetch in quiet mode (#39068)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+12.sha-fb12c61";
+export declare const NGCC_VERSION = "11.0.0-next.4+13.sha-21213e1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
