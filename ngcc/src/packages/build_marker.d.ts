c50b26348509b2e2898aadee599734dfe2d604d9
Joey Perrott <josephperrott@gmail.com>
2020-10-01 19:53:34 +0000

    4bce21358d feat(dev-infra): add a command to verify NgBot YAML config syntax (#39071)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+25.sha-6205ed0";
+export declare const NGCC_VERSION = "11.0.0-next.4+26.sha-4bce213";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
