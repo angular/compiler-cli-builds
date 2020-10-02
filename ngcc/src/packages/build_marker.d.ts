4c44dfc3691484bcad37fd60e30d44293197b9b0
Joey Perrott <josephperrott@gmail.com>
2020-10-02 19:04:20 +0000

    f50313f54d feat(compiler): Recover on malformed keyed reads and keyed writes (#39004)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.4+20.sha-30433a0";
+export declare const NGCC_VERSION = "10.1.4+21.sha-f50313f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
