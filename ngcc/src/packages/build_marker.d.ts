66fded6bc786b591263e9327bab24ab6dc49dec1
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:41:14 +0000

    89c5255b8c refactor(compiler): iteratively parse interpolations (#38977)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+35.sha-5dbf357";
+export declare const NGCC_VERSION = "11.0.0-next.4+36.sha-89c5255";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.4+35.sha-5dbf357';
+    exports.NGCC_VERSION = '11.0.0-next.4+36.sha-89c5255';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
