86fb0f7b4070995d6e98b1de7cb7a7c47ebc920e
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:45:46 +0000

    82d54fe8c3 feat(zone.js): add jest fakeTimers support (#39016)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.4+40.sha-26afcdd';
+    exports.NGCC_VERSION = '11.0.0-next.4+43.sha-82d54fe';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
