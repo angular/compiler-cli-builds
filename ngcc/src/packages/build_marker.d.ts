aecf0373ac9ff5068e8a6692a5b82e2b7f555d2a
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:30:35 +0000

    4beaddcb83 docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+36.sha-89c5255";
+export declare const NGCC_VERSION = "11.0.0-next.4+37.sha-4beaddc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
