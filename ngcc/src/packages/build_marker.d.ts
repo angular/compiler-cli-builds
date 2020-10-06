2da2b1baa25b7c4fb82adbabcd4b2cbf815bf46f
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:34:22 +0000

    3c474ecf56 fix(common): add boolean to valid json for testing (#37893)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+49.sha-ee03408";
+export declare const NGCC_VERSION = "11.0.0-next.4+50.sha-3c474ec";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
