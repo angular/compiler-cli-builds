94109564fe74aa38abe9ec93b4839b2242458696
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:32:22 +0000

    0845d1148f build: update bazelversion (#39123)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.4+29.sha-f80c220";
+export declare const NGCC_VERSION = "10.1.4+31.sha-0845d11";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
