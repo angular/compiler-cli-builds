d8a778d87ed453102bdf04d4b890a0ebab01e613
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:20:35 +0000

    f2fca6d58e refactor(compiler-cli): add a global autocompletion API (#39048)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+60.sha-aeec223";
+export declare const NGCC_VERSION = "11.0.0-next.4+59.sha-f2fca6d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
