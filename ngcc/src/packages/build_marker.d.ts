b8caee61b3c5c987e0ecab88f710dc2e6b57889a
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:00:13 +0000

    d8c05347c3 fix(router): properly assign ExtraOptions to Router in RouterTestingModule (#39096)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+47.sha-faa81dc";
+export declare const NGCC_VERSION = "11.0.0-next.4+48.sha-d8c0534";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
