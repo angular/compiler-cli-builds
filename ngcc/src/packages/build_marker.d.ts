bdd01cf803ede9ab07b5de02983147631dbf7cb9
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:56:36 +0000

    293270696b build(docs-infra): render optional decorator options with a ? (#39167)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.4+61.sha-5fa012e";
+export declare const NGCC_VERSION = "11.0.0-next.4+63.sha-2932706";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
