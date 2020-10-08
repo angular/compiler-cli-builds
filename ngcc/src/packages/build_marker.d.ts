63d667ddeda873f3baa81bed7b10f907f26b99d8
atscott <atscott01@gmail.com>
2020-10-08 19:21:12 +0000

    0a16e60afa fix(compiler-cli): type checking of expressions within ICUs (#39072)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+13.sha-4a1c12c";
+export declare const NGCC_VERSION = "11.0.0-next.5+11.sha-0a16e60";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
