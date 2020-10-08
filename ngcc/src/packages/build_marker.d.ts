032a57b29d2120f46ee3b4b6ff20c8ee03b3a099
atscott <atscott01@gmail.com>
2020-10-08 19:22:10 +0000

    7628c36f49 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#39182)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.5+5.sha-b417dd7";
+export declare const NGCC_VERSION = "10.1.5+6.sha-7628c36";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
