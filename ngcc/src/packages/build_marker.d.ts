2c0391924a11d54e4b25a490869cc18da0d39823
atscott <atscott01@gmail.com>
2020-10-09 17:03:23 +0000

    b2342d4116 feat(dev-infra): Add github links to caretaker checks (#39185)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+15.sha-7f689a2";
+export declare const NGCC_VERSION = "11.0.0-next.5+16.sha-b2342d4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
