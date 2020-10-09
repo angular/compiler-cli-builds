b00be4d01146a02381627501e60ada30d3422cde
atscott <atscott01@gmail.com>
2020-10-09 15:17:42 +0000

    ca4ef61c06 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+11.sha-0a16e60";
+export declare const NGCC_VERSION = "11.0.0-next.5+14.sha-ca4ef61";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
