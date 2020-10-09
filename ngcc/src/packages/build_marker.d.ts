0a31da0a397737a28ff5f2c6878677d228ca0d17
atscott <atscott01@gmail.com>
2020-10-09 17:24:14 +0000

    9f3388e491 build(docs-infra): upgrade cli command docs sources to 7208ef964 (#39181)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+18.sha-bf717b1";
+export declare const NGCC_VERSION = "11.0.0-next.5+17.sha-9f3388e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
