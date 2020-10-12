d23e7189442883a94134147c6a0de395bfe586af
atscott <atscott01@gmail.com>
2020-10-12 16:04:01 +0000

    2d37e47e95 build(docs-infra): support passing args to `ng serve` via `serve-and-sync` (#39201)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.5+10.sha-952fd86";
+export declare const NGCC_VERSION = "10.1.5+12.sha-2d37e47";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
