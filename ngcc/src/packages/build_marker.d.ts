92ae620320f1694b6570e02b9123468869807a2f
atscott <atscott01@gmail.com>
2020-10-12 18:09:10 +0000

    5471789664 refactor(animations): do not assign to innerHTML (#37397)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+40.sha-952710b";
+export declare const NGCC_VERSION = "11.0.0-next.5+41.sha-5471789";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
