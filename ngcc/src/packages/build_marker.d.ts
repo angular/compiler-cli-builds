d79b3f92a325daec7e6d4bba4b87e49182848efe
atscott <atscott01@gmail.com>
2020-10-12 20:03:29 +0000

    72f4497f11 docs: move Inputs and Outputs topic to Components section (#38984)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+45.sha-96dd78e";
+export declare const NGCC_VERSION = "11.0.0-next.5+46.sha-72f4497";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
