18dc1c52a1c98603a47070c1bd0dfdac1ac0300d
atscott <atscott01@gmail.com>
2020-10-12 18:09:16 +0000

    c76dda6aeb fix(dev-infra): correctly check for commit that closes PR (#39135)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+41.sha-5471789";
+export declare const NGCC_VERSION = "11.0.0-next.5+43.sha-c76dda6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
