4eec65b7f15181f343cc064aa12748abb59f04d5
atscott <atscott01@gmail.com>
2020-10-13 16:15:46 +0000

    437e563507 refactor(language-service): Allow "go to definition" for directives in Ivy (#39228)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+47.sha-b0b4953";
+export declare const NGCC_VERSION = "11.0.0-next.5+48.sha-437e563";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
