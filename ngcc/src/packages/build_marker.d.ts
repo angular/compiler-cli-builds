092437eaac9e7680485af0107045561e83a58644
atscott <atscott01@gmail.com>
2020-10-13 17:20:20 +0000

    5ce71e0fbc feat(core): add automated migration to replace async with waitForAsync (#39212)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+48.sha-437e563";
+export declare const NGCC_VERSION = "11.0.0-next.5+49.sha-5ce71e0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
