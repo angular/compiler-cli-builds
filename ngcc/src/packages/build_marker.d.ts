df495f72762ac22358d83555f23877f92121c62b
atscott <atscott01@gmail.com>
2020-10-14 16:58:24 +0000

    898be92f70 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+62.sha-8fd25d9";
+export declare const NGCC_VERSION = "11.0.0-next.5+68.sha-898be92";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
