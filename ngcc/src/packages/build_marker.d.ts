4289b73e9d497a385962d94f1ab3e74026b91663
atscott <atscott01@gmail.com>
2020-10-14 17:01:36 +0000

    f6d5cdfbd7 fix(core): use Trusted Types policy in named_array_type (#39209)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.5+68.sha-898be92";
+export declare const NGCC_VERSION = "11.0.0-next.5+67.sha-f6d5cdf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
