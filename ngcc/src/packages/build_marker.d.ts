4d6b9537cbce5662e4158b19ef2e952a9dacf7ec
atscott <atscott01@gmail.com>
2020-10-14 21:38:42 +0000

    a83693ddd9 refactor(core): use relative import paths in micro benchmarks (#39142)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+1.sha-cdf642b";
+export declare const NGCC_VERSION = "11.0.0-next.6+5.sha-a83693d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
