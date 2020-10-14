c2a6db16d349b1b1e4a9a1495558faad4fd71aa7
atscott <atscott01@gmail.com>
2020-10-14 17:47:39 +0000

    df0941554b docs: add Discord as an official communication channel (#39149)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.5+22.sha-5221df8";
+export declare const NGCC_VERSION = "10.1.5+23.sha-df09415";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
