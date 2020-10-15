64823fd66501a79eb4f5403efc6661632942a931
Andrew Kushnir <akushnir@google.com>
2020-10-15 23:27:49 +0000

    5bda62c51d refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+22.sha-7669827";
+export declare const NGCC_VERSION = "11.0.0-next.6+24.sha-5bda62c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
