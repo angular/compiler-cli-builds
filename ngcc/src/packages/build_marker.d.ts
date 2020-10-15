f6cec8e0fd76cd450266a7f14351e940701b27dc
atscott <atscott01@gmail.com>
2020-10-15 16:31:08 +0000

    497af77d7a build(zone.js): zone.js should output esm format for fesm2015 bundles (#39203)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+13.sha-6e18d2d";
+export declare const NGCC_VERSION = "11.0.0-next.6+11.sha-497af77";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
