9b15ce3223a4fe4d877e4c17db755cab67b338eb
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:30:21 +0000

    10db99588f refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+21.sha-8fd59da";
+export declare const NGCC_VERSION = "10.1.6+22.sha-10db995";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
