676b1ed3d8bd65f8b324b7bbd36c78d0b1e99cc9
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:10:00 +0000

    8fd59dabba ci: separate the windows CI tests into build and test (#39289)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+20.sha-d8e313b";
+export declare const NGCC_VERSION = "10.1.6+21.sha-8fd59da";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
