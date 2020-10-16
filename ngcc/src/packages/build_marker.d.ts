095f00d54c9c03085e29e9bd539c783bded75906
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:21:59 +0000

    94d7ef3909 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+14.sha-af17038";
+export declare const NGCC_VERSION = "10.1.6+16.sha-94d7ef3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
