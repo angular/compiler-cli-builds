e2664e0b797fd1c3f1d4f1e88140cc59a9ae5125
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:30:07 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+34.sha-34b74ce";
+export declare const NGCC_VERSION = "11.0.0-next.6+36.sha-4dc8d83";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
