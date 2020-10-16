b48e135e75a54370549d90e27199fc1b4a8db603
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:56:54 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+41.sha-1b21350";
+export declare const NGCC_VERSION = "11.0.0-next.6+39.sha-563fb6c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
