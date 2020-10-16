bd04f0a3f39c41dd47a4a9d002a9ac6a13242a15
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:40:43 +0000

    34b74cecb6 build(docs-infra): add a tool to create new examples (#39283)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+33.sha-81aa119";
+export declare const NGCC_VERSION = "11.0.0-next.6+34.sha-34b74ce";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
