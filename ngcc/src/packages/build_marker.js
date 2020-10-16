db44bc2e579945329b33649f31330f649743ac16
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:44:49 +0000

    4a5f17cd8e build(docs-infra): add a tool to create new examples (#39283)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '10.1.6+16.sha-94d7ef3';
+    exports.NGCC_VERSION = '10.1.6+18.sha-4a5f17c';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
