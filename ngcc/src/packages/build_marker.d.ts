cae8e31ac988c003da3a78bb26e164323eb8a6a9
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:57:54 +0000

    d2ea9b05d8 test(language-service): Make project service a singleton (#39308)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+30.sha-a0fde29";
+export declare const NGCC_VERSION = "11.0.0-next.6+32.sha-d2ea9b0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
