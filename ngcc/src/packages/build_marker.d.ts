ca275cd2f27b67304bb0d3aa94afc5cd85955d39
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:47:01 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+24.sha-38db976";
+export declare const NGCC_VERSION = "10.1.6+25.sha-7827d3e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
