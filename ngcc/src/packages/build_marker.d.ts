3f45104906c0c7ce37815367a908b4d912d5cea4
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:54:09 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+26.sha-32a8e72";
+export declare const NGCC_VERSION = "10.1.6+27.sha-02b8750";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
