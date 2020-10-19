5f0733a461ee14bf73dacb06bfd9eaea9a83d48e
Andrew Kushnir <akushnir@google.com>
2020-10-19 15:28:45 +0000

    d30911c4eb docs: add Johannes Hoppe to GDE resources (#34694)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+34.sha-8a744d5";
+export declare const NGCC_VERSION = "11.0.0-next.6+36.sha-d30911c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
