a6727d83d2e2b9f3aa2303a1299b2d61cbd88c48
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:29:32 +0000

    32a8e72a14 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.1.6+25.sha-7827d3e";
+export declare const NGCC_VERSION = "10.1.6+26.sha-32a8e72";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
