be113f9c061b143250a3b913eb23cc854f81c1cd
Andrew Kushnir <akushnir@google.com>
2020-10-19 15:33:49 +0000

    a93b7fd674 build(docs-infra): upgrade cli command docs sources to 04ae7cbf5 (#39318)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+43.sha-79d67dc";
+export declare const NGCC_VERSION = "11.0.0-next.6+47.sha-a93b7fd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
