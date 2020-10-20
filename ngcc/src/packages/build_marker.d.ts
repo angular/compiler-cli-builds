0c47fa610de3d952d199d0f5e6c32cd423388255
Andrew Kushnir <akushnir@google.com>
2020-10-20 00:07:37 +0000

    7040f1714b docs: add Jessica Janiuk to contributors.json (#39253)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+52.sha-41c2228";
+export declare const NGCC_VERSION = "11.0.0-next.6+54.sha-7040f17";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
