c72fcb5c508ec1018adf4097ea81593e365b86ba
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:58:15 +0000

    9fb7bdea89 fix(router): incorrect signature for createUrlTree (#39347)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+59.sha-a7964f4";
+export declare const NGCC_VERSION = "11.0.0-next.6+60.sha-9fb7bde";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
