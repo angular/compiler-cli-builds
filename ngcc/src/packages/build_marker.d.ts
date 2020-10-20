4077a6f397c6f8db003d1ce6941110ad2b7235fa
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:21:11 +0000

    595062e415 docs: Add Component Overview topic to angular.io (#39186)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+43.sha-5167eea";
+export declare const NGCC_VERSION = "11.0.0-next.6+46.sha-595062e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
