915cd8cd26d1ae35e4970503a7b51ad60296f34b
Andrew Kushnir <akushnir@google.com>
2020-10-21 19:28:28 +0000

    eb4c05d97a build: update bazelversion (#39351)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+67.sha-3525b2f";
+export declare const NGCC_VERSION = "11.0.0-next.6+69.sha-eb4c05d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
