da57881a91fa59fdebfe0a8291f53bff8cf80260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-22 01:57:08 +0000

    08f3d62391 refactor(core): clean up circular dependencies (#39233)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.6+69.sha-eb4c05d';
+    exports.NGCC_VERSION = '11.0.0-next.6+81.sha-08f3d62';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
