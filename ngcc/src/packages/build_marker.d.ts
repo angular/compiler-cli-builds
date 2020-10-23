ae9748eb663d7e699c71c8d4ad98f0d900b562b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-23 17:59:02 +0000

    b989ba2502 fix(core): update expected size for cli-hello-world-ivy-i18n integration test (#39402)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+81.sha-08f3d62";
+export declare const NGCC_VERSION = "11.0.0-next.6+95.sha-b989ba2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
