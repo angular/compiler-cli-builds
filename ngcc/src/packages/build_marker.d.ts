edd1bf1398ffb477f13be65d914dd92f3b98a717
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:14:56 +0000

    35fdc6d92f build(docs-infra): ignore `doc.basePath` when generating keywords for each document (#39409)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+100.sha-cf60e0e";
+export declare const NGCC_VERSION = "11.0.0-next.6+103.sha-35fdc6d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
