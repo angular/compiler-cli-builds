0945c0705e40736e93bcc52b0670ec2151b85ecb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 17:40:13 +0000

    90c0772743 fix(zone.js): remove global declaration (#37861)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+95.sha-b989ba2";
+export declare const NGCC_VERSION = "11.0.0-next.6+99.sha-90c0772";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
