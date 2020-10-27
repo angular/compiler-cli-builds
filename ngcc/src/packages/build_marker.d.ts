a7f2d8a11218fb5769121ee4f7851cd7fc6c06b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:21:28 +0000

    055f7ebecb ci: bust cache of stored node_modules on CircleCI (#39461)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.2.0+17.sha-308b930";
+export declare const NGCC_VERSION = "10.2.0+31.sha-055f7eb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
