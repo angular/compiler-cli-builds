9c3f1b609291c9ddac178418634ae52d250fa0b4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:24:26 +0000

    fdb6b98d80 docs: add `enableI18nLegacyMessageIdFormat` option to Angular compiler options guide (#39453)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+136.sha-371fb9a";
+export declare const NGCC_VERSION = "11.0.0-next.6+142.sha-fdb6b98";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
