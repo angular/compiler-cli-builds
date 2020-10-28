c8a4a3aa1df2f6158a244427813b9e7c9edb62d8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:45:26 +0000

    bdec818b3a docs: release notes for the v11.0.0-rc.1 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+146.sha-e42d3ec";
+export declare const NGCC_VERSION = "11.0.0-next.6+147.sha-bdec818";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
