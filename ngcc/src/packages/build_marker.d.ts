6b12d6e3b6876a47e573a56fdd47eb37c0674c91
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:09:21 +0000

    dd0ba3f4ab docs: rename `ng xi18n` to `ng extract-i18n` (#39337)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+149.sha-e3d2f46";
+export declare const NGCC_VERSION = "11.0.0-next.6+150.sha-dd0ba3f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
