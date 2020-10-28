52334221f7d2a3071c4c83802c571adfb3590ae2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 17:30:58 +0000

    366be11150 refactor(core): add top 10 runtime error codes (#39188)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-rc.0+58.sha-399f491";
+export declare const NGCC_VERSION = "11.0.0-rc.0+60.sha-366be11";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
