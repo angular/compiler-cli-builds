87dd74304e3ab241a595fc26dbc7b0e48aafb1b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:15:15 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+132.sha-e6ca3d3";
+export declare const NGCC_VERSION = "11.0.0-next.6+133.sha-b9358c2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
