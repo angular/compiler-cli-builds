460227742e5e41306bd529dc707ea4e32e289987
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:35:07 +0000

    ddc070e778 docs(core): add jessicajaniuk to pullapprove list (#39473)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-rc.0+60.sha-366be11";
+export declare const NGCC_VERSION = "11.0.0-rc.0+70.sha-ddc070e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
