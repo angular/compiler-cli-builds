2a399b70695f50894b0f76bb0e003acafffba7b5
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:39:57 +0000

    4a8d5ae970 fix(core): markDirty() should only mark flags when really scheduling tick. (#39316)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+162.sha-170af07";
+export declare const NGCC_VERSION = "11.0.0-next.6+163.sha-4a8d5ae";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
