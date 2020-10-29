f330e1d41b3d791f197f0d099d7ad32c32f16dae
Joey Perrott <josephperrott@gmail.com>
2020-10-29 20:07:38 +0000

    e3365724f2 feat(compiler): recover expression parsing in more malformed pipe cases (#39437)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.6+158.sha-8d324ec';
+    exports.NGCC_VERSION = '11.0.0-next.6+159.sha-e336572';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
