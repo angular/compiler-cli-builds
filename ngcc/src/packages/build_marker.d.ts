3a158d356526d82ed541b07d13dcb9a6c0a3d690
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:43:01 +0000

    8af32fcb16 docs(forms): update ngModel documentation (#39481)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+155.sha-d6a7659";
+export declare const NGCC_VERSION = "11.0.0-next.6+156.sha-8af32fc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
