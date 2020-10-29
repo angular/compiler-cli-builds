3cad20b407fb60dee1cce0cb3b8e5f90d485dfd9
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:52:32 +0000

    c83b2ad87f docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+163.sha-4a8d5ae";
+export declare const NGCC_VERSION = "11.0.0-next.6+165.sha-c83b2ad";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
