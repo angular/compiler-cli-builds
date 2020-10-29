612faa74befe74aeb07976dd0a21b0b8e972b560
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:48:10 +0000

    fb96c3d624 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.2.1+9.sha-f9d3588";
+export declare const NGCC_VERSION = "10.2.1+10.sha-fb96c3d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
