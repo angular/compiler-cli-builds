2e8596155c6aa8d5d25b9cd21dea263a4d74a175
Joey Perrott <josephperrott@gmail.com>
2020-10-29 19:21:16 +0000

    f54662e931 fix(core): mark Trusted Types as declarations (#39471)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+156.sha-8af32fc";
+export declare const NGCC_VERSION = "11.0.0-next.6+157.sha-f54662e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
