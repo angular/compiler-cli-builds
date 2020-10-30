237c08c02cbeed25c2562dd316af3d7b4d4bd892
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:05:53 +0000

    3241d922fc refactor(compiler): parse bindings "by hand" rather than via regex (#39375)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+165.sha-c83b2ad";
+export declare const NGCC_VERSION = "11.0.0-next.6+166.sha-3241d92";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
