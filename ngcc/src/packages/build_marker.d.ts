017c0c0d2b0bdf1d6e1b34d7af88c8a2368dbaa8
Joey Perrott <josephperrott@gmail.com>
2020-10-30 18:17:14 +0000

    cbc0907bfd fix(compiler): preserve this.$event and this.$any accesses in expressions (#39323)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+168.sha-4e68254";
+export declare const NGCC_VERSION = "11.0.0-next.6+169.sha-cbc0907";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
