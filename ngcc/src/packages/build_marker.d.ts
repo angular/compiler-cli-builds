b5e2c9edd34880f3fbba60367305dd2ed1608b30
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:49:11 +0000

    4e68254514 fix(common): change the week-numbering year format from `r` -> `Y` (#39495)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+167.sha-beb9356";
+export declare const NGCC_VERSION = "11.0.0-next.6+168.sha-4e68254";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
