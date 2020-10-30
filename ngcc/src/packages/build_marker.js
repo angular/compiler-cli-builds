250efe79193af82742b7a1f7a4b4c8bd9788a6b8
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:48:42 +0000

    b2f3952946 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-rc.1+19.sha-eb625b9';
+    exports.NGCC_VERSION = '11.0.0-rc.1+21.sha-b2f3952';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
