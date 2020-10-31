cab234ab64cf2bd207c818a43416c54a096e3081
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:24:23 +0000

    3b0b7d2210 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+172.sha-3af585c";
+export declare const NGCC_VERSION = "11.0.0-next.6+173.sha-3b0b7d2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- src/ngtsc/typecheck/src/type_check_block.d.ts
+++ src/ngtsc/typecheck/src/type_check_block.d.ts
@@ -121,7 +121,7 @@ export declare class Context {
      * might change depending on the type of data being stored.
      */
     allocateId(): ts.Identifier;
-    getPipeByName(name: string): ts.Expression | null;
+    getPipeByName(name: string): Reference<ClassDeclaration<ts.ClassDeclaration>> | null;
 }
 /**
  * Local scope within the type check block for a particular template.
