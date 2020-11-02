6a3c6d9d2b0726f731f58ccb115cfc6cafbf4050
Joey Perrott <josephperrott@gmail.com>
2020-11-02 21:00:43 +0000

    5f1e9758f5 build(docs-infra): ensure that deployment works on CI (#39535)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+187.sha-ad62eda";
+export declare const NGCC_VERSION = "11.0.0-next.6+188.sha-5f1e975";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
