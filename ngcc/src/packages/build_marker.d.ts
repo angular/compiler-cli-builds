d8547a230dae84da1914bd5933d3b26c6823c532
Joey Perrott <josephperrott@gmail.com>
2020-11-02 22:03:31 +0000

    3cab823222 docs(bazel): Mention Alex Eagle's article on migration doc (#39507)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+188.sha-5f1e975";
+export declare const NGCC_VERSION = "11.0.0-next.6+189.sha-3cab823";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
