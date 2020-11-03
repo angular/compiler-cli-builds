0eb6fb1419e88959aaf397676d62208deeee9d43
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:11:23 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.2.1+15.sha-71d0063";
+export declare const NGCC_VERSION = "10.2.1+29.sha-aa3bd27";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
