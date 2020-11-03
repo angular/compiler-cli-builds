f3c9fab19d2d18f634071272fdd099f4220b84b1
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:04:36 +0000

    db1c48ff36 ci: only check testing directories within packages/ for fw-testing (#39536)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+191.sha-be64f98";
+export declare const NGCC_VERSION = "11.0.0-next.6+190.sha-db1c48f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
