ccad6e34f79afa09efef0675164a7784b249b6ed
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:42:16 +0000

    1e91f84697 docs: fix Android 10 codename (#39399)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+193.sha-d0dd0e8";
+export declare const NGCC_VERSION = "11.0.0-next.6+194.sha-1e91f84";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.6+193.sha-d0dd0e8';
+    exports.NGCC_VERSION = '11.0.0-next.6+194.sha-1e91f84';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
