88ec3d5838a70906cc5f5219dd06a4253bc62add
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:57:15 +0000

    7ed0f0f7d0 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.2.1+30.sha-250858a";
+export declare const NGCC_VERSION = "10.2.1+32.sha-7ed0f0f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
