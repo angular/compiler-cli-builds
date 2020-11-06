3d7186bf21595e4fc9e5cf1b59148c491cda3fb1
Misko Hevery <misko@hevery.com>
2020-11-06 17:38:02 +0000

    8e384d9f0d docs: typo fix for 'Intall' (#39585)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+214.sha-1f95618";
+export declare const NGCC_VERSION = "11.0.0-next.6+215.sha-8e384d9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
