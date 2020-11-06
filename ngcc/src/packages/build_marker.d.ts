7426484a8ef23082a381c2159f07df1fe4239d30
Misko Hevery <misko@hevery.com>
2020-11-06 17:28:08 +0000

    1f956184c4 fix(compiler): skipping leading whitespace should not break placeholder source-spans (#39486)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+210.sha-27358eb";
+export declare const NGCC_VERSION = "11.0.0-next.6+214.sha-1f95618";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
