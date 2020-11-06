a8b3297aca3aa942a1e8945adeee4c80d85e97a6
Misko Hevery <misko@hevery.com>
2020-11-06 17:40:32 +0000

    8e384d9f0d docs: typo fix for 'Intall' (#39585)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.6+214.sha-1f95618';
+    exports.NGCC_VERSION = '11.0.0-next.6+215.sha-8e384d9';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
