05ebed1754f0a3d7720ee8995c09d4d67a946966
Misko Hevery <misko@hevery.com>
2020-11-06 17:57:54 +0000

    dd28855455 fix(elements): update the view of an `OnPush` component when inputs change (#39452)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-rc.2+5.sha-b8e9b3d';
+    exports.NGCC_VERSION = '11.0.0-rc.2+14.sha-dd28855';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
