90abe5080f867b142304900863590e530308c59f
Misko Hevery <misko@hevery.com>
2020-11-06 17:43:53 +0000

    3a1d36cce3 refactor(language-service): Use compiler APIs in Ivy to get definitions for external resources (#39476)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+215.sha-8e384d9";
+export declare const NGCC_VERSION = "11.0.0-next.6+216.sha-3a1d36c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
