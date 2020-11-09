30e814ede900f0279b9a32c963914f99eb5cb9d7
Misko Hevery <misko@hevery.com>
2020-11-09 20:43:51 +0000

    28112ca55c docs: add update information for v11 release (#39607)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-rc.2+18.sha-a17ae55";
+export declare const NGCC_VERSION = "11.0.0-rc.2+19.sha-28112ca";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
