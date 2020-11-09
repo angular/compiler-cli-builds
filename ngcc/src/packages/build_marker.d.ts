5f8f99510f9eeef1317f8d9d3d6b0063476413bc
Misko Hevery <misko@hevery.com>
2020-11-09 16:02:05 +0000

    ae71b76089 ci: log commands output when deploying angular.io to Firebase (#39596)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "10.2.2+8.sha-861e4fa";
+export declare const NGCC_VERSION = "10.2.2+10.sha-ae71b76";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
