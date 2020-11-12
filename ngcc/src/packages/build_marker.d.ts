8f245f96769ddb6a62ceeda864fae7da7826ca02
atscott <atscott01@gmail.com>
2020-11-12 18:05:24 +0000

    1bc53eb303 fix(forms): more precise control cleanup (#39623)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+238.sha-4476324";
+export declare const NGCC_VERSION = "11.0.0-next.6+239.sha-1bc53eb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
