8faa66c5b56e16cb3a64eb6c70aa2191680db776
atscott <atscott01@gmail.com>
2020-11-12 22:19:23 +0000

    c8ed497211 docs: clarify what we inline when font optimization is enabled (#39656)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.0-next.6+242.sha-f8956ad';
+    exports.NGCC_VERSION = '11.0.0-next.6+244.sha-c8ed497';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
