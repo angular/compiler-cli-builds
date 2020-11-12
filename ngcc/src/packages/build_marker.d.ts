1f13f08dbf9d91cec475d7a6862913c84c279c0f
atscott <atscott01@gmail.com>
2020-11-12 22:18:44 +0000

    f8956adb73 build: update zone.js version to 0.11.3 (#39317)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+241.sha-a872362";
+export declare const NGCC_VERSION = "11.0.0-next.6+242.sha-f8956ad";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
