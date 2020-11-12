0b40db7b6da398510dc01924875492bba0690f71
Misko Hevery <misko@hevery.com>
2020-11-12 00:05:44 +0000

    6d1347363e docs: update the roadmap with our priorities at Q4 (#39635)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+233.sha-8a03e3e";
+export declare const NGCC_VERSION = "11.0.0-next.6+234.sha-6d13473";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
