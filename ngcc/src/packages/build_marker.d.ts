6dc1add28cdad3c1bf7913ebeed9dfee26ec83f8
atscott <atscott01@gmail.com>
2020-11-12 22:13:33 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+240.sha-b015d3e";
+export declare const NGCC_VERSION = "11.0.0-next.6+241.sha-a872362";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
