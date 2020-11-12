7b8ae95a2d2e09aacbe91b850dad44cd03ba49ac
atscott <atscott01@gmail.com>
2020-11-12 22:48:44 +0000

    21651d362d refactor(compiler-cli): add keySpan to text attributes (#39613)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+247.sha-ade6da9";
+export declare const NGCC_VERSION = "11.0.0-next.6+248.sha-21651d3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
