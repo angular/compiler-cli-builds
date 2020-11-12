05f3e0a5dbdb82c018b02ce1571bf0525d40bc46
atscott <atscott01@gmail.com>
2020-11-12 22:24:43 +0000

    ade6da95e4 perf(compiler-cli): reduce filesystem hits during resource resolution (#39604)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+245.sha-8e17dc0";
+export declare const NGCC_VERSION = "11.0.0-next.6+247.sha-ade6da9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
