c4174a8ae3d08ff01e3b5ba964fc2cf19526cafc
atscott <atscott01@gmail.com>
2020-11-13 19:48:31 +0000

    eea5d6b38f test(compiler-cli): convert element related compliance tests (#39617)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+258.sha-3f66a84";
+export declare const NGCC_VERSION = "11.0.0-next.6+257.sha-eea5d6b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
