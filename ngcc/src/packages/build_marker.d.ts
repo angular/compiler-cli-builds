95e137bb3f1336f1fa31fd937fbfcc09e5e4e4b9
atscott <atscott01@gmail.com>
2020-11-13 19:58:10 +0000

    25d6fcacc4 docs: add doc with list of example apps currently in aio (#39295)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+251.sha-9e53c6e";
+export declare const NGCC_VERSION = "11.0.0-next.6+259.sha-25d6fca";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
