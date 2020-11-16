a69910f239886d53927fb6cb43271ed90358a333
atscott <atscott01@gmail.com>
2020-11-16 17:40:25 +0000

    b0c4ecf563 docs: improve commit message format documentation (#39705)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+263.sha-5e92d64";
+export declare const NGCC_VERSION = "11.0.0-next.6+265.sha-b0c4ecf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
