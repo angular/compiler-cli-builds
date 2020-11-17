557c7a6cb64058135bd5ba77e86ea4c07fb51fed
atscott <atscott01@gmail.com>
2020-11-17 18:39:40 +0000

    d281ea820b perf(compiler): use raw bytes to represent utf-8 encoded strings (#39694)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+280.sha-9f20942";
+export declare const NGCC_VERSION = "11.0.0-next.6+282.sha-d281ea8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
