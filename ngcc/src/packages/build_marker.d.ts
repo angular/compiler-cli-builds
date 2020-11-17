4e4a51f8b8346251a851f03442523b9fdeb298bb
atscott <atscott01@gmail.com>
2020-11-17 21:34:30 +0000

    3fcfc5fcd1 docs: add spanish lang (#39502)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0+39.sha-a88bc64";
+export declare const NGCC_VERSION = "11.0.0+41.sha-3fcfc5f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
