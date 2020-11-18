8d7afd64329508645d0875043a8eb6ac863ca339
atscott <atscott01@gmail.com>
2020-11-18 17:36:28 +0000

    e4028ae5c4 build(docs-infra): compute previous version list (#39689)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+295.sha-b33b89d";
+export declare const NGCC_VERSION = "11.0.0-next.6+297.sha-e4028ae";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
