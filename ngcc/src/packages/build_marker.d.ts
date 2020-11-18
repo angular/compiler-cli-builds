8b5488b094e65b8e591af3778c6aa60aa784f8fc
atscott <atscott01@gmail.com>
2020-11-18 00:36:07 +0000

    724dc93080 docs: fix dead CSRF link (#35929)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0+41.sha-3fcfc5f";
+export declare const NGCC_VERSION = "11.0.0+42.sha-724dc93";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
