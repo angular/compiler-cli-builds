c464ee683d6e2fd035b14822582208e46169ea34
atscott <atscott01@gmail.com>
2020-11-18 17:37:16 +0000

    b33b89d441 fix(common): add `HttpParamsOptions` to the public api (#35829)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+294.sha-0ab833f";
+export declare const NGCC_VERSION = "11.0.0-next.6+295.sha-b33b89d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
