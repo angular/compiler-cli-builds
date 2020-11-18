cbb153f2e5855fa092a5a611005905287d8b29f2
atscott <atscott01@gmail.com>
2020-11-18 17:45:48 +0000

    3b2e5be6cb refactor(core): clean up circular dependencies (#39722)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+296.sha-7d68f92";
+export declare const NGCC_VERSION = "11.0.0-next.6+299.sha-3b2e5be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
