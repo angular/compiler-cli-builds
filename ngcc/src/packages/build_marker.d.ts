9070f338c005eff532eddcf9d137ba942ca36801
atscott <atscott01@gmail.com>
2020-11-18 18:48:34 +0000

    7261e393f1 refactor(dev-infra): extract the parsing of a range of commits into a util function (#39726)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+306.sha-d20cab1";
+export declare const NGCC_VERSION = "11.0.0-next.6+300.sha-7261e39";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
