4d9894206019311fff10b93e075a7a31607f0a2e
atscott <atscott01@gmail.com>
2020-11-18 18:47:30 +0000

    d20cab1448 docs: add links to corresponding commits in v11.0.0 breaking changes in `CHANGELOG.md` (#39704)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.0-next.6+298.sha-6fbe219";
+export declare const NGCC_VERSION = "11.0.0-next.6+306.sha-d20cab1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
