a8c865c0f478c644459c88beb676cb030d6618d6
Andrew Kushnir <akushnir@google.com>
2020-11-19 17:24:10 +0000

    61506404a9 docs: move di-in-action doc to conceptual ref section (#39544)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+15.sha-1951342";
+export declare const NGCC_VERSION = "11.1.0-next.0+16.sha-6150640";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
