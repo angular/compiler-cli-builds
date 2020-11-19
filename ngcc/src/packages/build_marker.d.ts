c8970c1716d9e389429854f368d306ee1c853a25
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:25:30 +0000

    d001ecc5a0 docs: fix confusing description for strictTemplates (#39745)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+12.sha-f8c6230";
+export declare const NGCC_VERSION = "11.1.0-next.0+13.sha-d001ecc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
