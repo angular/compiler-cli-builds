5fa392d9f28788e536bb0d048e26a352c7425fd5
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:37:07 +0000

    066126ae2f fix(core): remove duplicated noop function (#39761)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+18.sha-0e85938";
+export declare const NGCC_VERSION = "11.1.0-next.0+22.sha-066126a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
