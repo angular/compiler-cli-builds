4f3523e1921843f057975120b88bc7093f7bf7ae
Andrew Kushnir <akushnir@google.com>
2020-11-20 21:36:13 +0000

    3e1e5a15ba docs: update links to use HTTPS as protocol (#39718)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+46.sha-ff0a90e";
+export declare const NGCC_VERSION = "11.1.0-next.0+51.sha-3e1e5a1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
