0c573fb42517ae96dcc7b24d4eb4a539b1b10d06
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:05:03 +0000

    1cba56e11f refactor(core): remove unused fakeAsyncFallback and asyncFallback (#37879)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+38.sha-66d863f";
+export declare const NGCC_VERSION = "11.1.0-next.0+39.sha-1cba56e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
