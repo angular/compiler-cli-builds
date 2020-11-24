ddd44c79170203d4498baeab5fcc72aa2266e662
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:05:41 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+64.sha-6046419";
+export declare const NGCC_VERSION = "11.1.0-next.0+67.sha-2c275f6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
