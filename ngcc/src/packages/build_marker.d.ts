42cf619dcfac8a40d7d9a688a7474a58d4a5d616
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:09:11 +0000

    b5c0f9d7d1 feat(platform-browser): add doubletap HammerJS support (#26362)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+66.sha-88fbc06";
+export declare const NGCC_VERSION = "11.1.0-next.0+69.sha-b5c0f9d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
