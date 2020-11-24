85e8557002f8029efe715858aad641a63a14ccf9
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:10:34 +0000

    8634611e37 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.2+43.sha-7bf73d7";
+export declare const NGCC_VERSION = "11.0.2+48.sha-8634611";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
