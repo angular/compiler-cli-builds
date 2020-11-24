1174fbc205d3d8da9570eec5a8731992ad0f9db8
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:03:27 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.0+64.sha-6046419';
+    exports.NGCC_VERSION = '11.1.0-next.0+67.sha-2c275f6';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
