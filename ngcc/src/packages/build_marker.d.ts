efe3cc64a98f537db4211b2e1c6bc2f4a563239b
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:31:30 +0000

    75b53690f6 docs: explain foreign function/value constraints in Ivy compatibility examples (#36092)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.2+49.sha-9cb12ca";
+export declare const NGCC_VERSION = "11.0.2+51.sha-75b5369";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
