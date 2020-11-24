409bfb80372bf38a7173cdcf539408b3b1786d17
Andrew Kushnir <akushnir@google.com>
2020-11-24 23:19:04 +0000

    23c36a24ed test(docs-infra): disable the Selenium Promise Manager in docs examples e2e tests (#39818)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+84.sha-8758006";
+export declare const NGCC_VERSION = "11.1.0-next.0+89.sha-23c36a2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
