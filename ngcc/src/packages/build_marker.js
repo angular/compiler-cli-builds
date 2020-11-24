8391025514bbb82328b4af627507bee684f01d12
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:06:00 +0000

    a1b6ad07a8 fix(core): Allow passing AbstractType to the inject function (#37958)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.0+70.sha-453b32f';
+    exports.NGCC_VERSION = '11.1.0-next.0+71.sha-a1b6ad0';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
