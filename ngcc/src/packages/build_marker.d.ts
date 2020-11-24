6ab0539b03bdc0d55e40ea80d4f777cecbf26805
Andrew Kushnir <akushnir@google.com>
2020-11-24 22:47:37 +0000

    8758006acc refactor(language-service): create getReferencesAtPosition stubs (#39829)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+83.sha-3a6e7b5";
+export declare const NGCC_VERSION = "11.1.0-next.0+84.sha-8758006";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
