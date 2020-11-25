8d0d8a69fb22f52d8586b9e3da906eb356ec431a
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:24:40 +0000

    bfa197f564 fix(animations): replace copy of query selector node-list from "spread" to "for" (#39646)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+91.sha-5684ac5";
+export declare const NGCC_VERSION = "11.1.0-next.0+92.sha-bfa197f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
