d68d2e80ff3098e034a6d29b9181e12462f79164
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 21:21:24 +0000

    ba6945bfa9 docs(docs-infra): display angulararchitects.io in trainings (#37139)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+99.sha-4def19f";
+export declare const NGCC_VERSION = "11.1.0-next.0+101.sha-ba6945b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
