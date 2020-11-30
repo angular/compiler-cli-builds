05fe4d39f6569899355122653641781ce6bd295d
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 23:26:49 +0000

    1de59d2818 docs: edit step 2 of getting started start-routing.md (#39593)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+114.sha-dd3ed3c";
+export declare const NGCC_VERSION = "11.1.0-next.0+115.sha-1de59d2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
