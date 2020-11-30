17a44ccab275fe45015a81c2c427569c274b8799
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:31:20 +0000

    93d689ffa8 docs: edit dependency-injection.md (#39255)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+104.sha-68d4a74";
+export declare const NGCC_VERSION = "11.1.0-next.0+105.sha-93d689f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
