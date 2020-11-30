82bbfd22bbdfed90c9d2815913ba60f5c140aa4e
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:46:58 +0000

    c5619ca56e test(compiler-cli): convert styling compliance tests (#39881)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.0+108.sha-e2040af';
+    exports.NGCC_VERSION = '11.1.0-next.0+111.sha-c5619ca';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
