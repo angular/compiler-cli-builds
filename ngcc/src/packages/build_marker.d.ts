1d0ad5836824064a918dc3bdad76caf1f89d1795
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:32:58 +0000

    288a0a668a docs: fix builder section of generated angular.json file for libs (#39888)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.2+68.sha-20ab5d8";
+export declare const NGCC_VERSION = "11.0.2+70.sha-288a0a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
