032b3ce3beaf60a7b58dfe46dba05fee16e3f567
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:25:24 +0000

    3d19645208 docs: edit binding-syntax doc (#38773)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.2+76.sha-518fbea";
+export declare const NGCC_VERSION = "11.0.2+77.sha-3d19645";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
