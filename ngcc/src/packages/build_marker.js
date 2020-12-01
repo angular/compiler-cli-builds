d4b25b8ef96145a8bfd8a541ea9d27fa8968d8fc
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:21:41 +0000

    50962c1b0b test(compiler-cli): migrate template compliance tests (#39871)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.0+123.sha-40b127c';
+    exports.NGCC_VERSION = '11.1.0-next.0+124.sha-50962c1';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
