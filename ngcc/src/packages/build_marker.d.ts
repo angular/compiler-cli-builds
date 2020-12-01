dc8bacbd99f333046622fa207996a6ce9d12a2ff
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:00:30 +0000

    518fbeac9b ci: enable the Selenium Promise Manager in AIO e2e tests to avoid flakiness on CI (#39905)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.2+74.sha-425e329";
+export declare const NGCC_VERSION = "11.0.2+76.sha-518fbea";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
