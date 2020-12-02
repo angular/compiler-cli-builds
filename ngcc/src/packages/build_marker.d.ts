2d566b77c876d5f21866e434ebcbb3ad7926f35d
Misko Hevery <misko@hevery.com>
2020-12-02 23:21:47 +0000

    8d613c1d42 feat(compiler): allow trailing comma in array literal (#22277)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+22.sha-744f46c";
+export declare const NGCC_VERSION = "11.1.0-next.1+25.sha-8d613c1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
