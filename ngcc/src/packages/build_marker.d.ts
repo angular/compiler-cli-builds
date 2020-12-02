0f96f562b73d995c475093e26423b3ff9e1354d4
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 21:04:11 +0000

    c923aaf64b docs: add wes grimes to GDE resources (#34741)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+2.sha-943ae37";
+export declare const NGCC_VERSION = "11.1.0-next.1+6.sha-c923aaf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
