3a507455f252a448c9fafa04fd9113680782ce39
Joey Perrott <josephperrott@gmail.com>
2020-12-02 19:02:57 +0000

    784667ca12 release: cut the v11.1.0-next.1 release (#39932)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+126.sha-eac85b4";
+export declare const NGCC_VERSION = "11.1.0-next.0+127.sha-784667c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
