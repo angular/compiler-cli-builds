a1862c0e4cb97f77d5992948d0c9e20fb50872cd
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 19:40:11 +0000

    7851a254d8 docs(common): fix typo in HttpClient docs (#39904)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.0+127.sha-784667c";
+export declare const NGCC_VERSION = "11.1.0-next.1+3.sha-7851a25";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
