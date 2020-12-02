0335e536f83e8c9ba22e86f772e687c5d267f3e1
Misko Hevery <misko@hevery.com>
2020-12-02 23:29:20 +0000

    efd0d33391 test(compiler-cli): convert bindings compliance tests (#39862)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+21.sha-790ca09";
+export declare const NGCC_VERSION = "11.1.0-next.1+24.sha-efd0d33";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
