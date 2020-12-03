b12e9fa824759b2974c735c355312da655a504f4
Misko Hevery <misko@hevery.com>
2020-12-03 22:08:12 +0000

    f01c713ee2 ci: fix AIO deployment to multiple Firebase sites (#39948)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+37.sha-3b70098";
+export declare const NGCC_VERSION = "11.1.0-next.1+34.sha-f01c713";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
