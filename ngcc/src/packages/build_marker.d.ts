fbba8591c05e965dddeb93c6fc4882e6869fd82c
Misko Hevery <misko@hevery.com>
2020-12-03 19:08:44 +0000

    93564ca137 docs: add alvarocamillont to GDE resources (#39138)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+18.sha-6b6fcd7";
+export declare const NGCC_VERSION = "11.0.3+19.sha-93564ca";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
