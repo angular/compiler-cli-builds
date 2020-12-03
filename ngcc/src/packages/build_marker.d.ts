d784de1352acd8144551c6a49c1f1549251d6996
Misko Hevery <misko@hevery.com>
2020-12-03 00:58:19 +0000

    3680ad147b fix(core): remove application from the testability registry when the root view is removed (#39876)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+5.sha-57642e8";
+export declare const NGCC_VERSION = "11.0.3+17.sha-3680ad1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
