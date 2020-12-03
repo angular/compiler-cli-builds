1e33b80fed56060084477b48df0b7d8bf96b3d6c
Misko Hevery <misko@hevery.com>
2020-12-03 22:06:51 +0000

    5a3a154cd8 fix(core): unsubscribe from the `onError` when the root view is removed (#39940)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+33.sha-aa8bd73";
+export declare const NGCC_VERSION = "11.1.0-next.1+35.sha-5a3a154";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
