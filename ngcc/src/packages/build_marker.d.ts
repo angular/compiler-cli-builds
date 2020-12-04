73b44cd4010dff37ee9fdb70e29055551e5e6349
Misko Hevery <misko@hevery.com>
2020-12-04 23:32:16 +0000

    1e3534f74f perf(core): use `ngDevMode` to tree-shake warnings (#39959)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+24.sha-4aeb65d";
+export declare const NGCC_VERSION = "11.0.3+26.sha-1e3534f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.3+24.sha-4aeb65d';
+    exports.NGCC_VERSION = '11.0.3+26.sha-1e3534f';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
