429b5e066f887d1c73971844ab6a9807b9c04516
Misko Hevery <misko@hevery.com>
2020-12-08 17:23:31 +0000

    2ffd739aed docs: rephrased two sentences (#37230)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+70.sha-f06a50f";
+export declare const NGCC_VERSION = "11.1.0-next.1+71.sha-2ffd739";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
