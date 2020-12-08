3d0c5cdd63c6a5fce21075e3d06ac353ea1b1676
Misko Hevery <misko@hevery.com>
2020-12-08 19:32:51 +0000

    19309b5797 Revert "build: update bazel rules_nodejs to version 2.3.1 (#39636)"

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+54.sha-8ab1231";
+export declare const NGCC_VERSION = "11.0.3+56.sha-19309b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
