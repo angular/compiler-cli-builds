f9079cf09a7a27548d7499dcdb9dba65986bf778
Misko Hevery <misko@hevery.com>
2020-12-08 00:50:01 +0000

    b8714c384f test(compiler-cli): add source-map compliance tests for inline-templates (#39939)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+55.sha-7901eba";
+export declare const NGCC_VERSION = "11.1.0-next.1+62.sha-b8714c3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
