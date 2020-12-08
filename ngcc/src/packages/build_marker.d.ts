aad5eb5e8b70422ef08a6ef02a5778a3735eb512
Misko Hevery <misko@hevery.com>
2020-12-08 20:33:35 +0000

    74e42cf7d5 fix(service-worker): handle error with ErrorHandler (#39990)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+85.sha-6dc43a4";
+export declare const NGCC_VERSION = "11.1.0-next.1+86.sha-74e42cf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
