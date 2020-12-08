5c77c1b273a8bac5d78d9b8e28ce54b7d8cfdb8a
Misko Hevery <misko@hevery.com>
2020-12-08 20:38:31 +0000

    55c21fadc8 ci: add atscott to language-service approvers (#40027)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+86.sha-74e42cf";
+export declare const NGCC_VERSION = "11.1.0-next.1+89.sha-55c21fa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
