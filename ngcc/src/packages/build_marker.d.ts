9640ea9cca9ac90e644ba57b2d3acbb2a9a6bd98
Misko Hevery <misko@hevery.com>
2020-12-08 20:21:57 +0000

    5848439a48 docs(platform-browser): fix spelling of deserialized (#36102) PR Close #36102

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+78.sha-bc6e669";
+export declare const NGCC_VERSION = "11.1.0-next.1+79.sha-5848439";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
