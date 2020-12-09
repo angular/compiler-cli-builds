8ec55e772c89fa5f6636f1baa4f061a17e342f51
Misko Hevery <misko@hevery.com>
2020-12-09 03:39:10 +0000

    cfb094dbaf test(compiler-cli): temporary disable source mapping tests which have errors (#40033)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.1+92.sha-2f8a420';
+    exports.NGCC_VERSION = '11.1.0-next.1+95.sha-cfb094d';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
