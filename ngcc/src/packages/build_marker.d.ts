f47bb2bf08433ae1ae20ebb26920d576db2adbb3
Misko Hevery <misko@hevery.com>
2020-12-09 17:33:13 +0000

    b2090fdf93 docs(common): use table layout for Pre-defined format options (#40036)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+66.sha-5a765f0";
+export declare const NGCC_VERSION = "11.0.3+73.sha-b2090fd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
