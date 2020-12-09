8406e8100117260d2f30ae5f05bd742edaffbe6c
Misko Hevery <misko@hevery.com>
2020-12-09 17:21:03 +0000

    bb3f207f44 docs: add reason to use Angular in README (#36927)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.1+95.sha-cfb094d";
+export declare const NGCC_VERSION = "11.1.0-next.1+96.sha-bb3f207";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
