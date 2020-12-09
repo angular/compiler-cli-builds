1ccde8dd665c46ce940452d9aaf6334642fc3969
Misko Hevery <misko@hevery.com>
2020-12-09 00:49:07 +0000

    5a765f0be9 fix(animations): implement getPosition in browser animation builder (#39983)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.3+63.sha-65e93ea";
+export declare const NGCC_VERSION = "11.0.3+66.sha-5a765f0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
