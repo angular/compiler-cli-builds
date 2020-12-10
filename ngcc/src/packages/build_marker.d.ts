350fc84ab04d0412fcb9965779107b26e10ca665
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:07:07 +0000

    b4b21bdff4 fix(upgrade): fix HMR for hybrid applications (#40045)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+10.sha-76e3de2";
+export declare const NGCC_VERSION = "11.1.0-next.2+13.sha-b4b21bd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
