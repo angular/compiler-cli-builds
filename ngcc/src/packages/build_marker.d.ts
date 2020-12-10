956f5e39f224981b7f738c83eeeb8a7c83ffb461
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 21:51:53 +0000

    1f73af77a7 refactor(compiler-cli): use `ngDevMode` guard for `setClassMetadata` call (#39987)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+8.sha-85b07ad";
+export declare const NGCC_VERSION = "11.1.0-next.2+9.sha-1f73af7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- src/ngtsc/annotations/src/metadata.d.ts
+++ src/ngtsc/annotations/src/metadata.d.ts
@@ -11,7 +11,8 @@ import { DefaultImportRecorder } from '../../imports';
 import { DeclarationNode, ReflectionHost } from '../../reflection';
 /**
  * Given a class declaration, generate a call to `setClassMetadata` with the Angular metadata
- * present on the class or its member fields.
+ * present on the class or its member fields. An ngDevMode guard is used to allow the call to be
+ * tree-shaken away, as the `setClassMetadata` invocation is only needed for testing purposes.
  *
  * If no such metadata is present, this function returns `null`. Otherwise, the call is returned
  * as a `Statement` for inclusion along with the class.
