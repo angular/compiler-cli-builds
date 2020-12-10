03a273fe8a92c5b8a3a473b158701b0b1e24018f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:44:12 +0000

    112324a614 feat(router): add `relativeTo` as an input to `routerLink` (#39720)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+6.sha-dc6d40e";
+export declare const NGCC_VERSION = "11.1.0-next.2+7.sha-112324a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
