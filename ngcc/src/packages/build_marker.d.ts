7adec10870dd721aa2704ad66410c441ada94249
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:17:58 +0000

    3e68b79106 Revert "Revert "ci: update pullapprove config to add comp labels"" (#40059)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+18.sha-2a38641";
+export declare const NGCC_VERSION = "11.1.0-next.2+19.sha-3e68b79";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
