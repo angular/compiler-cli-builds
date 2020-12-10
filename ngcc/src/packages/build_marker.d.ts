d91a19ce5f20719c52041fdca9c7727f6c7221a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:11:49 +0000

    2a386415ce build(language-service): install @angular/language-service in root node_modules (#40058)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+17.sha-0817758";
+export declare const NGCC_VERSION = "11.1.0-next.2+18.sha-2a38641";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
