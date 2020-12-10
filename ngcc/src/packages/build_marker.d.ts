687107c10671b174b8dedca11a29d88a79f4a0cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:55:58 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+21.sha-79a42d9";
+export declare const NGCC_VERSION = "11.1.0-next.2+23.sha-91fb830";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
