ca40484c71c253fb229f97c40f7cb78deb72c694
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:28:21 +0000

    29298f4f3d docs: fix the code sample displayed in the "reusable animations" guide (#37262)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+1.sha-d39d64c";
+export declare const NGCC_VERSION = "11.1.0-next.2+2.sha-29298f4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
