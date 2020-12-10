63a4464d53162c86fa80c968c56699d74f4eb9b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:37:45 +0000

    f5aab2b972 fix(compiler): handle strings inside bindings that contain binding characters (#39826)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.4+1.sha-eba7de6";
+export declare const NGCC_VERSION = "11.0.4+3.sha-f5aab2b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
