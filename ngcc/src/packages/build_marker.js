63f9f048673dbc37a38d9f56d00370f2980dce11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-11 22:19:46 +0000

    6e4e68cb30 refactor(compiler-cli): Add flag to TCB for non-diagnostic requests (#40071)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.2+24.sha-b08fd0c';
+    exports.NGCC_VERSION = '11.1.0-next.2+26.sha-6e4e68c';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
