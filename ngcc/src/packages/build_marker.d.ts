e2f1221d9c82042d9184fc0bf21157d892912b36
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:02:01 +0000

    35a1975697 build: update to use npm_package.pack rule (#39636)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+34.sha-1e05109";
+export declare const NGCC_VERSION = "11.1.0-next.2+30.sha-35a1975";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
