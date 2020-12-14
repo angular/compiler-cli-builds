0c9bee12d116aab99ab0ca3c1e1d67c166029145
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:09:55 +0000

    c18a9d5c79 ci: reduce flakiness of docs-infra `deploy-to-firebase.js` tests (#40088)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+33.sha-c977e1b";
+export declare const NGCC_VERSION = "11.1.0-next.2+36.sha-c18a9d5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
