cf4885bd4385cf36b6e51b16653b3a9a2f317234
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:03:41 +0000

    c977e1b629 test(compiler): use `yarn bazel` instead of `bazel` in error message (#40078)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+32.sha-044cd52";
+export declare const NGCC_VERSION = "11.1.0-next.2+33.sha-c977e1b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
