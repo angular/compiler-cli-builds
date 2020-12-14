e57eff0d50220c723ca0e28a272d35439dd62eaa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:03:15 +0000

    044cd52996 docs: update CLI MAN page format (#40038)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.2+35.sha-4f4f318";
+export declare const NGCC_VERSION = "11.1.0-next.2+32.sha-044cd52";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
