0f59b772548d3be1446cd9bdcc4dfa1c1dc8d765
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 21:58:26 +0000

    a1c82e772d build: update config flags for snapshot builds (#40095)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.4+22.sha-7d9ac37";
+export declare const NGCC_VERSION = "11.0.4+24.sha-a1c82e7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
