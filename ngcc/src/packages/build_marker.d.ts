c5f829a971fd673dc78f9b4b1b5e776a832a85ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:41:40 +0000

    922f492a3b fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.4+26.sha-0aa220b";
+export declare const NGCC_VERSION = "11.0.4+27.sha-922f492";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
