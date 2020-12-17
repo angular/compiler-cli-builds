af890176489e47419a1800784b0db6c3ffdf6c2f
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:13:05 +0000

    475468cc8f refactor(core): remove custom globalThis (#40123)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+4.sha-effc079";
+export declare const NGCC_VERSION = "11.1.0-next.3+5.sha-475468c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
