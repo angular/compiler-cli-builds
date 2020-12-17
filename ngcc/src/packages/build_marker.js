a7ab3bcc250f26c6bea0b8c3538f51f8e4ad224f
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:05:00 +0000

    c53bae839d ci: fail CI jobs when rebasing on master fails (#40161)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.3+1.sha-ee6b8a7';
+    exports.NGCC_VERSION = '11.1.0-next.3+2.sha-c53bae8';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
