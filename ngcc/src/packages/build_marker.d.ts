629a94554025fc217fdad9b0e138e673f4e0c9fa
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:24:01 +0000

    ab34211eed refactor(docs-infra): enable tslint rules for the angular.io app (#39307)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.4+28.sha-e0e2157";
+export declare const NGCC_VERSION = "11.0.5+2.sha-ab34211";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.4+28.sha-e0e2157';
+    exports.NGCC_VERSION = '11.0.5+2.sha-ab34211';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
