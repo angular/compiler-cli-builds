03657541fc874d7bea97bb4255864629f69fb4ef
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:35:19 +0000

    5831d2b685 ci: correctly rebase PRs for branches contain a slash (/) (#40184)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+6.sha-362f45c";
+export declare const NGCC_VERSION = "11.1.0-next.3+8.sha-5831d2b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
