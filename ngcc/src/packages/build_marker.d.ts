43e4d6afc6e5f7b3790c6f1bd7b43238b905be11
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:44:41 +0000

    f7642ceff3 docs: update App Shell guide to reflect its actual behavior (#40210) PR Close #40210

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+7.sha-10a468a";
+export declare const NGCC_VERSION = "11.1.0-next.3+9.sha-f7642ce";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
