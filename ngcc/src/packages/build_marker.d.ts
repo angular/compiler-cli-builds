67cd00c7b7974a59a4e9014e27f2883887614467
Joey Perrott <josephperrott@gmail.com>
2020-12-21 19:05:04 +0000

    3f4a1d6154 build(docs-infra): update Lighthouse to v7.0.0 (#40198)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.5+5.sha-2075cf7";
+export declare const NGCC_VERSION = "11.0.5+11.sha-3f4a1d6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
