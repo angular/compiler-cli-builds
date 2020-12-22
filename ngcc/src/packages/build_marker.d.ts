9c9334c96d860725cbe495e06916f493936a987d
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:16:34 +0000

    e4fbab9ec8 fix(core): error if detectChanges is called at the wrong time under specific circumstances (#40206)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+25.sha-12cb39c";
+export declare const NGCC_VERSION = "11.1.0-next.3+27.sha-e4fbab9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
