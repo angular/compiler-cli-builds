4517cdab633173c7885a5618c1cbd271b155bc05
Joey Perrott <josephperrott@gmail.com>
2020-12-23 21:28:46 +0000

    d7665fdc96 build(docs-infra): upgrade cli command docs sources to 4cefc7dc1 (#40250)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -16,7 +16,7 @@ export declare class PartialLinkerSelector<TExpression> {
      * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
      * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
      *
-     * Finally, note that we always start with the current version (i.e. `11.1.0-next.3+32.sha-c22d005`). This
+     * Finally, note that we always start with the current version (i.e. `11.1.0-next.3+33.sha-d7665fd`). This
      * allows the linker to work on local builds effectively.
      */
     private linkers;
