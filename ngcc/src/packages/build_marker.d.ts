c36b64cc30900e55e13b8dda95771fcb8decd3ac
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:25:40 +0000

    c83163ecfe docs(router): fix typo (#40244)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.5+24.sha-9a149d4";
+export declare const NGCC_VERSION = "11.0.5+26.sha-c83163e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
