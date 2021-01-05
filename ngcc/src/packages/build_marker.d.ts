17e71614a46461b94420186cee8344aa4606b837
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:25:04 +0000

    6a9d7e5969 refactor(core): express trusted constants with tagged template literals (#40082)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+53.sha-8cdfd77";
+export declare const NGCC_VERSION = "11.1.0-next.3+54.sha-6a9d7e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
