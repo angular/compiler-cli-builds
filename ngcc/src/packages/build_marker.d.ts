facc3b29fd91cb8d2335cc48a759341019a2bce2
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:23:41 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+52.sha-c1d7805";
+export declare const NGCC_VERSION = "11.1.0-next.3+53.sha-8cdfd77";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
