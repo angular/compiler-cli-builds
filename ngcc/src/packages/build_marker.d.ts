0c96bbfb70c43a43cd806e1e1b3001248b5386cb
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:10:31 +0000

    9248ee2c64 docs: fix interchanged values (#40302) PR Close #40302

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+33.sha-d7665fd";
+export declare const NGCC_VERSION = "11.1.0-next.3+34.sha-9248ee2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
