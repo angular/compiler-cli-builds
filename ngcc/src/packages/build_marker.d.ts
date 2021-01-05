62b338599fd8000aa6e50d314e5f10510b4fad85
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:11:18 +0000

    4eac7e6436 fix(router): Router should focus element after scrolling (#40241)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+34.sha-9248ee2";
+export declare const NGCC_VERSION = "11.1.0-next.3+37.sha-4eac7e6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
