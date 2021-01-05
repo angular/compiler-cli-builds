d4cd5a775b30bc07f83bc3fa4e4db99f3408d7e7
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:34:35 +0000

    805b4f936b feat(core): Add schematic to fix invalid `Route` configs (#40067)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+38.sha-d466db8";
+export declare const NGCC_VERSION = "11.1.0-next.3+40.sha-805b4f9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
