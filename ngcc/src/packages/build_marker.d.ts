ca7142b0b21cfbd109965cdf830731f835c70c56
Joey Perrott <josephperrott@gmail.com>
2021-01-06 17:09:58 +0000

    fdec650ef2 docs(forms): typo word "property" was missing (#40323)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+64.sha-d4327d5";
+export declare const NGCC_VERSION = "11.1.0-next.3+65.sha-fdec650";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
