bc9041c3ce244da1cb9607b5a4be23b3f1918f95
Joey Perrott <josephperrott@gmail.com>
2021-01-06 20:17:42 +0000

    b6a9717597 test(core): fix invalid symbol used in html sanitizier spec (#40325)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.5+51.sha-bad1717";
+export declare const NGCC_VERSION = "11.0.5+55.sha-b6a9717";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
