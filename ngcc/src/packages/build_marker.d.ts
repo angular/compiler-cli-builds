ea25eced1c898c932123e9794bfcfea9a9cf64e5
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:31:12 +0000

    d5a7a268cf ci: add jessicajaniuk to pullapprove groups (#40176)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.3+58.sha-0a317f3";
+export declare const NGCC_VERSION = "11.1.0-next.3+57.sha-d5a7a26";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
