e000bdeafcb8a91a5ddb0312446afe6b4da8d21a
atscott <atscott01@gmail.com>
2021-01-07 20:10:32 +0000

    6429be1c15 fix(router): Remove usage of `Object.entries` to avoid the need for a polyfill (#40340)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.6+5.sha-48526cc";
+export declare const NGCC_VERSION = "11.0.6+6.sha-6429be1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
