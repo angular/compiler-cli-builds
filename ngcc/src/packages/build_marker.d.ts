8ca6ae31d8dd55edfec61f16f4bab0a933e7694b
atscott <atscott01@gmail.com>
2021-01-07 21:34:28 +0000

    335c1ab37f docs: add Craig Spence to contributor list (#40335)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+10.sha-1437f4c";
+export declare const NGCC_VERSION = "11.1.0-next.4+11.sha-335c1ab";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
