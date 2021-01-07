341be22182f4d8c63d023ce35e6e35bf3b9d4286
atscott <atscott01@gmail.com>
2021-01-07 21:51:42 +0000

    104546569e fix(compiler): incorrectly interpreting some HostBinding names (#40233)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+31.sha-da6c739`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+22.sha-1045465`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+31.sha-da6c739";
+export declare const NGCC_VERSION = "11.1.0-next.4+22.sha-1045465";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
