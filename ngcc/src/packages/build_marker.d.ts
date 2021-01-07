1b98c0a1933797e3251cfb65487789de9b108043
atscott <atscott01@gmail.com>
2021-01-07 21:32:27 +0000

    9be9e466b1 docs: remove duplicated 'the' (#40333)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+13.sha-aea265b";
+export declare const NGCC_VERSION = "11.1.0-next.4+12.sha-9be9e46";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
