ac5e1ead96541f2bb271463db1ea5ef2f34b8beb
atscott <atscott01@gmail.com>
2021-01-07 21:44:36 +0000

    57043721db docs(language-service): Add comment about approach to quick info for two-way bindings (#40185)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+21.sha-266cc9b";
+export declare const NGCC_VERSION = "11.1.0-next.4+29.sha-5704372";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
