ab3f914dc30d3995eec3d5fad49cd6ae5acb535b
Andrew Scott <atscott@google.com>
2021-01-08 18:21:42 +0000

    e05ac395fc docs: add @christianliebel to list of GDEs (#39788)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+39.sha-4f73820";
+export declare const NGCC_VERSION = "11.1.0-next.4+43.sha-e05ac39";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
