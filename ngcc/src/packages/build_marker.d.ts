9d87202872c540171cf7a83f1e24cab632c3adfc
Andrew Scott <atscott@google.com>
2021-01-08 18:16:35 +0000

    14389755e6 docs: import in-memory-web-api readme (#40203)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+38.sha-fdbd3ca";
+export declare const NGCC_VERSION = "11.1.0-next.4+41.sha-1438975";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
