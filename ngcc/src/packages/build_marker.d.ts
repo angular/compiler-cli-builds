3914a6fbc9975f76dae9c3e3ab79b47e546b0403
Andrew Scott <atscott@google.com>
2021-01-10 01:01:30 +0000

    c44dd84a71 fix(router): Remove usage of `Object.values` to avoid the need for a polyfill (#40370)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.7+11.sha-6ccdb2d";
+export declare const NGCC_VERSION = "11.0.7+13.sha-c44dd84";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
