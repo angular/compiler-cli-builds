5f5c2179567edf90ed52ffab01f42aecb04dbb36
atscott <atscott01@gmail.com>
2021-01-11 18:20:02 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+50.sha-1e46b73";
+export declare const NGCC_VERSION = "11.1.0-next.4+53.sha-3d7e207";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
