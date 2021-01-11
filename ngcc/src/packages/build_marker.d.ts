514e2f78b76965ee5d78fcff4090be7f518461ce
atscott <atscott01@gmail.com>
2021-01-11 22:41:32 +0000

    4a7a649852 docs(router): clarify the description of legacy relativeLinkResolution (#40384)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+66.sha-7695468";
+export declare const NGCC_VERSION = "11.1.0-next.4+68.sha-4a7a649";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
