f699aea2cfb3297763c70af6b7f82057133bd2bc
atscott <atscott01@gmail.com>
2021-01-11 18:22:43 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+50.sha-1e46b73`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+53.sha-3d7e207`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
