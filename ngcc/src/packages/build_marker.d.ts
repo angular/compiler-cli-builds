98b4c3bcdffd49cf590c483e6d28f03aa8f3802b
atscott <atscott01@gmail.com>
2021-01-11 18:19:04 +0000

    6438a29eb4 docs(router): fix minor grammatical issue (#40380)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.7+13.sha-c44dd84";
+export declare const NGCC_VERSION = "11.0.8+6.sha-6438a29";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
