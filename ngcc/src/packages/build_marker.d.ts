ee634e5981d5b23b628b4825e3ed8c0b6c807ca7
atscott <atscott01@gmail.com>
2021-01-11 18:21:43 +0000

    34d2988d14 docs: Fix compiler option name reference (#40362)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+53.sha-3d7e207";
+export declare const NGCC_VERSION = "11.1.0-next.4+51.sha-34d2988";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
