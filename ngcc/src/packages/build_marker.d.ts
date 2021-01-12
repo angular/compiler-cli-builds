d19d2a0ee50c06ff32607f132d689a90c13b3653
atscott <atscott01@gmail.com>
2021-01-12 00:07:59 +0000

    b48eabddb8 refactor(compiler-cli): include `template` source directly inside declaration object (#40383)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+79.sha-bd17610";
+export declare const NGCC_VERSION = "11.1.0-next.4+80.sha-b48eabd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
