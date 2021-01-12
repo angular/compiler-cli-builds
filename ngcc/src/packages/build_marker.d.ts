f62f6957348537f586e834537d8f364f02054596
atscott <atscott01@gmail.com>
2021-01-12 00:04:10 +0000

    bd17610c29 feat(dev-infra): make ts-api-guardian compatible with rules_nodejs version 3 (#40353)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+78.sha-66c27ff";
+export declare const NGCC_VERSION = "11.1.0-next.4+79.sha-bd17610";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
