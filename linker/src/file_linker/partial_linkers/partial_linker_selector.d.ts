651a0d76d69128e45e11435648b90a02865107db
atscott <atscott01@gmail.com>
2021-01-13 22:35:28 +0000

    0664d750b6 perf(core): make DI decorators tree-shakable when used for `useFactory` deps config (#40145)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.5+1.sha-ebd94ef`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-rc.0+7.sha-0664d75`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.5+1.sha-ebd94ef";
+export declare const NGCC_VERSION = "11.1.0-rc.0+7.sha-0664d75";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
