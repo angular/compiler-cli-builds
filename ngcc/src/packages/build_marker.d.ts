ece57cc3979cebfa98c236fc59046a79211b9036
GitHub <noreply@github.com>
2021-01-13 21:44:40 +0000

    47b3b08118 Update next branch to reflect new release-train "v11.2.0-next.0". (#40418)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+83.sha-391e0eb";
+export declare const NGCC_VERSION = "11.1.0-next.4+84.sha-47b3b08";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
