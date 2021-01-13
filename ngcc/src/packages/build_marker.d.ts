085141ccf42b2874dc2e618ea127234c200fcad1
atscott <atscott01@gmail.com>
2021-01-13 23:13:53 +0000

    5aa264f17c docs: update identity-revealed.directive.ts to use `AbstractControl` instead of `FormGroup` (#40132)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.9+7.sha-3f6279d";
+export declare const NGCC_VERSION = "11.0.9+9.sha-5aa264f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.0.9+7.sha-3f6279d';
+    exports.NGCC_VERSION = '11.0.9+9.sha-5aa264f';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
