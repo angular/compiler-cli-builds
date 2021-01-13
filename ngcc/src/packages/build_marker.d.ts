f6b3e78b1e4c414ea6ae8466d07c52531c2226a0
atscott <atscott01@gmail.com>
2021-01-13 22:34:30 +0000

    3f6279d394 ci: add mgechev to docs-marketing approvers (#40396)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.8+31.sha-c6ca7b4";
+export declare const NGCC_VERSION = "11.0.9+7.sha-3f6279d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
