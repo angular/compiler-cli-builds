abe108ec14835b2f63860f457a81c40d13f222cc
atscott <atscott01@gmail.com>
2021-01-14 18:52:40 +0000

    72e8899d17 docs(core): add Ivy note to entryComponents page (#35479)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.9+11.sha-f3a96a5";
+export declare const NGCC_VERSION = "11.0.9+12.sha-72e8899";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
