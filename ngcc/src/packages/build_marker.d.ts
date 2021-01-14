2aaae4666fb7619b9c8f0fbcb72b1b78dc8bed77
Andrew Scott <atscott@google.com>
2021-01-14 17:38:33 +0000

    f4ed12211e docs(common): updated differential loading section (#37913)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-rc.0+9.sha-c6c7e9a";
+export declare const NGCC_VERSION = "11.1.0-rc.0+10.sha-f4ed122";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
