6bd67689a5381b9ae91ddddb91a2caf2fb0bfb0b
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:57:25 +0000

    e4df94214b docs: add guides for the top 10 errors (#40060)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.4+98.sha-10994ae';
+    exports.NGCC_VERSION = '11.1.0-next.4+100.sha-e4df942';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
