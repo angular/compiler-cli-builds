11c8e511fc88703f89739de893cafb2a01af6825
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:58:46 +0000

    2f7e315157 perf(ngcc): do not copy files that have been processed (#40429)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.0.9+12.sha-72e8899";
+export declare const NGCC_VERSION = "11.0.9+17.sha-2f7e315";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
