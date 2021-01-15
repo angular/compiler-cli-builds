fc9a1bb055b911ce73618edfd2045cd66e0cbe60
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:14:59 +0000

    54579362ae docs: add UI Bakery to IDEs section in resources (#40444)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.4+107.sha-9a5ac47';
+    exports.NGCC_VERSION = '11.1.0-next.4+108.sha-5457936';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
