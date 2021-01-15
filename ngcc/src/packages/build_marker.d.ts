9e7939d396ff061cf5ff9e775161a3d6748993ff
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:12:18 +0000

    54579362ae docs: add UI Bakery to IDEs section in resources (#40444)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+107.sha-9a5ac47";
+export declare const NGCC_VERSION = "11.1.0-next.4+108.sha-5457936";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
