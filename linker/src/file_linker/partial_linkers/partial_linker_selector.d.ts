b04a42d84b7fef8cb5c189529dd788d70747c5dd
Andrew Kushnir <akushnir@google.com>
2021-01-15 00:49:24 +0000

    9a5ac47331 feat(language-service): initial implementation for `findRenameLocations` (#40140)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+106.sha-d516113`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+107.sha-9a5ac47`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
