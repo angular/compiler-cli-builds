3f8201787bfcdbbf236362b9679af7347eac8614
Andrew Kushnir <akushnir@google.com>
2021-01-15 00:47:01 +0000

    9a5ac47331 feat(language-service): initial implementation for `findRenameLocations` (#40140)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+106.sha-d516113";
+export declare const NGCC_VERSION = "11.1.0-next.4+107.sha-9a5ac47";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
