15291b3dea8ca1456a857e048e176d6391f71ef4
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:47:35 +0000

    0568c7387f docs: add Angular Builders to community resources (#40475)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+120.sha-2c95f57";
+export declare const NGCC_VERSION = "11.1.0-next.4+121.sha-0568c73";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
