b4cd63dd483cc5f85d77ba1c9e938d2dfd6f9c2d
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:39:24 +0000

    f49447df20 fix(core): fix fakeAsync() error messages (#40442)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+115.sha-4e8198d";
+export declare const NGCC_VERSION = "11.1.0-next.4+116.sha-f49447d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
