0720a7cc8eacc0cb802a387bf67fafcc831eb181
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:48:37 +0000

    2c95f57266 feat(docs-infra): use Record type instead of key-value (#40463)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+119.sha-b636406";
+export declare const NGCC_VERSION = "11.1.0-next.4+120.sha-2c95f57";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
