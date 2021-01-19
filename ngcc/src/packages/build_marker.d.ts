241146df1fb773ce542c9a4817cb5e51fcb2da69
Andrew Kushnir <akushnir@google.com>
2021-01-19 21:25:19 +0000

    3e97a1ea43 fix(language-service): fix go to definition for template variables and references (#40455)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+124.sha-1d1304c";
+export declare const NGCC_VERSION = "11.1.0-next.4+125.sha-3e97a1e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
