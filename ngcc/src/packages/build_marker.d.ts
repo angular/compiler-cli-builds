5a14a39bab012803232a2226030f828c02f42203
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:42:35 +0000

    2731a4b633 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-rc.0+35.sha-2c5ad5c";
+export declare const NGCC_VERSION = "11.1.0-rc.0+37.sha-2731a4b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
