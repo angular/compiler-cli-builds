845ff478fd31014f3f76bfc74e2d3ecdb227915f
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:35:45 +0000

    0b46f814d0 docs: release notes for the v11.1.0 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+129.sha-3cfb4b4";
+export declare const NGCC_VERSION = "11.1.0-next.4+130.sha-0b46f81";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
