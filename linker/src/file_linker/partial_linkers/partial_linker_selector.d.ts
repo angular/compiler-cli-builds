a67b524872241a19ff5f1f7ebc32d12621accf8f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:52:28 +0000

    315b272f9f docs: add missing type parameter to testing example (#40479)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+130.sha-0b46f81`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+137.sha-315b272`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
