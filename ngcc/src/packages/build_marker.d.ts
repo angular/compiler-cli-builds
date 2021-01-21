c87fcd86db230cc663d803980527fdb07937cf25
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:49:26 +0000

    a8a27efb77 fix(router): always stringify matrix parameters (#25095)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0";
+export declare const NGCC_VERSION = "11.1.0+9.sha-a8a27ef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
