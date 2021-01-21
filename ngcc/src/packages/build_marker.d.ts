d7b3df24f40a3bf5115acd220ecb4bc4011bbe51
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:24:19 +0000

    d482f5cdd3 feat(language-service): view template typecheck block (#39974)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+150.sha-575a2d1";
+export declare const NGCC_VERSION = "11.1.0-next.4+152.sha-d482f5c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
