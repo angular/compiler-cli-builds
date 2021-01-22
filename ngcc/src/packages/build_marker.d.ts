e4610355b16d70714b6b7d4c1265342a812c0dc0
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:44:59 +0000

    07b7af332f fix(compiler): support multiple `:host-context()` selectors (#40494)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+151.sha-2b2a847";
+export declare const NGCC_VERSION = "11.1.0-next.4+154.sha-07b7af3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
