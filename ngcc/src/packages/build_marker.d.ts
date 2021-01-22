fdfcbc34ced75f7f57da4906c36a397b322df6db
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 22:39:10 +0000

    3bf6a9908c fix(core): fix possible XSS attack in development through SSR (#40525)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0+23.sha-6298b3e";
+export declare const NGCC_VERSION = "11.1.0+24.sha-3bf6a99";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
