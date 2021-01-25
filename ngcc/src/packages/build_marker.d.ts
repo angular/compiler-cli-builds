8a94eb27bfde6372695cbabb712494145e4a960d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:37:27 +0000

    21bc16d4d8 fix(service-worker): handle error with console.error (#40236)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+160.sha-fc64fa8";
+export declare const NGCC_VERSION = "11.1.0-next.4+164.sha-21bc16d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
