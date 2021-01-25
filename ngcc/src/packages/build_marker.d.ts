27eaca5313b8dfc42990c3e4a4baa0e4f8ef7a1c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:26:05 +0000

    8b9f6b504e fix(localize): include meaning in generated ARB files (#40546)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+165.sha-1f53301";
+export declare const NGCC_VERSION = "11.1.0-next.4+171.sha-8b9f6b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.4+165.sha-1f53301';
+    exports.NGCC_VERSION = '11.1.0-next.4+171.sha-8b9f6b5';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
