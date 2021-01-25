2ac3973b89b1caf22313417a94b92b84758989d7
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:07:02 +0000

    1f53301fd3 refactor(zone.js): refactor zone rollup config (#40481)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+164.sha-21bc16d`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+165.sha-1f53301`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
