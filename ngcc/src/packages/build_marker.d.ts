c4304ce1ca5b3a10a1e1450fa8823d25d8be49d0
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:02:27 +0000

    67c4ab616d build: update .bazelversion (#40579)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0+47.sha-a01a9d2";
+export declare const NGCC_VERSION = "11.1.0+48.sha-67c4ab6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
