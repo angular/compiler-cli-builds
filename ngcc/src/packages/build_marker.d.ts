6b391afbd483151034a9140d2d9736c5ee5ceed9
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 00:50:06 +0000

    956f75f068 docs: fix the docs because the interface does not exist (#40308)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+176.sha-b630b09";
+export declare const NGCC_VERSION = "11.1.0-next.4+177.sha-956f75f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
