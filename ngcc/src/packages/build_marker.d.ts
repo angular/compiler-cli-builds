ddff1eba0b4fa71aa7239b607cadabaf22920d14
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:31:17 +0000

    cffb00ec11 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+182.sha-39b3cbb";
+export declare const NGCC_VERSION = "11.1.0-next.4+183.sha-cffb00e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
