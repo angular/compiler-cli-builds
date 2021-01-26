f4c3e66f9268da469622f27e3c1cbc53e47202c0
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 19:25:16 +0000

    a01a9d221e build: update to yarn v1.22.10 (#40562)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0+45.sha-97ec6e4";
+export declare const NGCC_VERSION = "11.1.0+47.sha-a01a9d2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
