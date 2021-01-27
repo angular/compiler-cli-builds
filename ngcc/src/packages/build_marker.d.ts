018191b8d142fa87bd798656d095c2f9a96688fb
GitHub <noreply@github.com>
2021-01-27 21:21:59 +0000

    8772fbd4bb release: bump the version to 12.0.0-next.0 (#40604)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+195.sha-e75a4f2";
+export declare const NGCC_VERSION = "11.1.0-next.4+196.sha-8772fbd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
