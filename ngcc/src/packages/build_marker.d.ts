81832038b155fb8297f5bc3a14d839d237e7d65b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 23:45:11 +0000

    de124856f6 docs: update release notes for the 11.1.1 release (#40602)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0+60.sha-63275ae";
+export declare const NGCC_VERSION = "11.1.1+1.sha-de12485";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
