8397c979da9bc9f62798096371da5e33c2150c95
Misko Hevery <misko@hevery.com>
2021-01-28 21:26:05 +0000

    9d396f837c fix(compiler): Don't set expression text to synthetic `$implicit` when empty (#40583)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0-next.0+1.sha-e11e6cf";
+export declare const NGCC_VERSION = "11.2.0-next.0+10.sha-9d396f8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.0-next.0+1.sha-e11e6cf';
+    exports.NGCC_VERSION = '11.2.0-next.0+10.sha-9d396f8';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
