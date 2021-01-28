876b3d34891485d7a31b8531fd2419825cffccec
Misko Hevery <misko@hevery.com>
2021-01-28 23:00:03 +0000

    e706f46a16 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.1+1.sha-de12485";
+export declare const NGCC_VERSION = "11.1.1+10.sha-e706f46";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
