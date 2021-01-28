e6023a5d4f54634097b87eef11c914b50bc0718c
Misko Hevery <misko@hevery.com>
2021-01-28 23:09:52 +0000

    88f8ddd3d3 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+197.sha-d4bb4a0`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+208.sha-88f8ddd`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+197.sha-d4bb4a0";
+export declare const NGCC_VERSION = "11.1.0-next.4+208.sha-88f8ddd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
