99cf1654478cd894a6353872f9b7fd237d716493
Misko Hevery <misko@hevery.com>
2021-01-29 00:24:13 +0000

    4c5f18dfda fix(common): parse `YYYY-MM` strings as UTC dates (#40620)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0-next.0+11.sha-552009e";
+export declare const NGCC_VERSION = "11.2.0-next.0+15.sha-4c5f18d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
