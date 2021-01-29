415ca637a9778e40a03fd3c956e9e8a23f989e09
Misko Hevery <misko@hevery.com>
2021-01-29 00:15:55 +0000

    2ba62e80fd ci: add pete to the fw-http group (#40619)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+209.sha-22f9e45`). This
+ * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+211.sha-2ba62e8`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
