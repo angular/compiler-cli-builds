645c8e5f52a26a34ab10ac282c7510f1e8493c3c
Misko Hevery <misko@hevery.com>
2021-02-01 17:37:21 +0000

    caafac2248 fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0-next.0+15.sha-4c5f18d";
+export declare const NGCC_VERSION = "11.2.0-next.0+18.sha-caafac2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
