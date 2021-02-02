ce89e624084cae13f84dab5d2e2f3dcef5bbd6b9
Misko Hevery <misko@hevery.com>
2021-02-02 20:22:56 +0000

    20654c95d8 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.1+17.sha-00eeebf4";
+export declare const NGCC_VERSION = "11.1.1+29.sha-20654c9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.1+17.sha-00eeebf4';
+    exports.NGCC_VERSION = '11.1.1+29.sha-20654c9';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
