affd4d402c75363e629ec137b2d6c632d1c0b464
Misko Hevery <misko@hevery.com>
2021-02-03 00:49:16 +0000

    a3b0864428 refactor(compiler-cli): remove the overrideComponentTemplate API (#40585)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.4+229.sha-11ca2f0';
+    exports.NGCC_VERSION = '11.1.0-next.4+231.sha-a3b0864';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
