d026449ee9ab4738c39d05695edb5e8480dc3b07
Misko Hevery <misko@hevery.com>
2021-02-04 00:21:02 +0000

    836b1764eb docs: release notes for the v11.2.0-rc.0 release

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -37,7 +37,7 @@
      * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
      * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
      *
-     * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+239.sha-a9322b7`). This
+     * Finally, note that we always start with the current version (i.e. `11.1.0-next.4+240.sha-836b176`). This
      * allows the linker to work on local builds effectively.
      */
     var PartialLinkerSelector = /** @class */ (function () {
@@ -83,11 +83,11 @@
             var partialComponentLinkerVersion1 = new partial_component_linker_1_1.PartialComponentLinkerVersion1(environment, get_source_file_1.createGetSourceFile(sourceUrl, code, environment.sourceFileLoader), sourceUrl, code);
             var linkers = new Map();
             linkers.set(exports.ɵɵngDeclareDirective, [
-                { range: '11.1.0-next.4+239.sha-a9322b7', linker: partialDirectiveLinkerVersion1 },
+                { range: '11.1.0-next.4+240.sha-836b176', linker: partialDirectiveLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialDirectiveLinkerVersion1 },
             ]);
             linkers.set(exports.ɵɵngDeclareComponent, [
-                { range: '11.1.0-next.4+239.sha-a9322b7', linker: partialComponentLinkerVersion1 },
+                { range: '11.1.0-next.4+240.sha-836b176', linker: partialComponentLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialComponentLinkerVersion1 },
             ]);
             return linkers;
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+239.sha-a9322b7";
+export declare const NGCC_VERSION = "11.1.0-next.4+240.sha-836b176";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
