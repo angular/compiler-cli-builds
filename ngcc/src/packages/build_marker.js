5520576e0dda77ef9423e5dc4d40317227e679ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:03:57 +0000

    d9e4d751f0 docs: add Taiga UI to "EXPLORE ANGULAR RESOURCES" page (#40572) PR Close #40572

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.1.0-next.4+247.sha-348d365';
+    exports.NGCC_VERSION = '11.1.0-next.4+248.sha-d9e4d75';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
