b8bc3a054673d0e4066e6e2e9667ada97474d55f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:01:24 +0000

    d9e4d751f0 docs: add Taiga UI to "EXPLORE ANGULAR RESOURCES" page (#40572) PR Close #40572

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+247.sha-348d365";
+export declare const NGCC_VERSION = "11.1.0-next.4+248.sha-d9e4d75";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
