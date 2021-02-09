90470744b804b009c9ed36c7942fcfbcc88315a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:08:24 +0000

    0652b29f62 fix(zone.js): setTimeout patch should clean tasksByHandleId cache. (#40586)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+250.sha-2282069";
+export declare const NGCC_VERSION = "11.1.0-next.4+249.sha-0652b29";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
