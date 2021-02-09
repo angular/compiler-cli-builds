3e7ad813f87cb3d9630f1e395ec706caa4117bdb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 18:55:58 +0000

    348d365666 docs(service-worker): copyedit getting started guide (#40543)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+246.sha-8fb83ea";
+export declare const NGCC_VERSION = "11.1.0-next.4+247.sha-348d365";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
