4ed16248cc64edd889bc62f310be1725508c9d14
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:05:29 +0000

    3b0c51e045 docs: edit example to fetch hero before sending message (#40419)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.0-rc.0+65.sha-c0d1e34';
+    exports.NGCC_VERSION = '11.2.0-rc.0+67.sha-3b0c51e';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
