9fdf49b7cd100136a82a45602eb19f01fac910a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:10:57 +0000

    46ab85996c build(docs-infra): upgrade cli command docs sources to c61df5d30 (#40758)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+305.sha-4e4fcac";
+export declare const NGCC_VERSION = "11.1.0-next.4+307.sha-46ab859";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
