4e72a100f82814b2cc346027b7161ad8a3ad261f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:07:59 +0000

    61a0b6de6d fix(http): complete the request on timeout (#39807)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+311.sha-2a851c0";
+export declare const NGCC_VERSION = "11.1.0-next.4+309.sha-61a0b6d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
