be83e7046694beaa35adc57f58dca487ffb2fda4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:45:44 +0000

    f76f2eb381 docs: update docs-style-guide (#36281)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+306.sha-ce44a3d";
+export declare const NGCC_VERSION = "11.1.0-next.4+308.sha-f76f2eb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
