9d7fc8c1e6b5c487752cdfd5cec3844c3711fa23
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:58:01 +0000

    cfbb1faa6f docs: add Angular + NgRx book by the asian BPB Publications (#39680)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0-rc.0+67.sha-3b0c51e";
+export declare const NGCC_VERSION = "11.2.0-rc.0+79.sha-cfbb1fa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
