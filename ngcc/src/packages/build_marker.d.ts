a72d38e13fc1c42bfa62a7d6d5facb7b6186a990
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:45:24 +0000

    7a8821c9aa docs: add link to component-less routes description. (#40741)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.1.0-next.4+324.sha-fface51";
+export declare const NGCC_VERSION = "11.1.0-next.4+321.sha-7a8821c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
