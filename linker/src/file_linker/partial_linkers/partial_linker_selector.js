35af7a1a2e3c4e9e0a5a1736accea8360835c0a9
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:00:26 +0000

    925e2b7434 build: remove jpm dependency (#40807)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -37,7 +37,7 @@
      * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
      * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
      *
-     * Finally, note that we always start with the current version (i.e. `11.2.0+3.sha-9cbd9be`). This
+     * Finally, note that we always start with the current version (i.e. `11.2.0+13.sha-925e2b7`). This
      * allows the linker to work on local builds effectively.
      */
     var PartialLinkerSelector = /** @class */ (function () {
@@ -83,11 +83,11 @@
             var partialComponentLinkerVersion1 = new partial_component_linker_1_1.PartialComponentLinkerVersion1(environment, get_source_file_1.createGetSourceFile(sourceUrl, code, environment.sourceFileLoader), sourceUrl, code);
             var linkers = new Map();
             linkers.set(exports.ɵɵngDeclareDirective, [
-                { range: '11.2.0+3.sha-9cbd9be', linker: partialDirectiveLinkerVersion1 },
+                { range: '11.2.0+13.sha-925e2b7', linker: partialDirectiveLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialDirectiveLinkerVersion1 },
             ]);
             linkers.set(exports.ɵɵngDeclareComponent, [
-                { range: '11.2.0+3.sha-9cbd9be', linker: partialComponentLinkerVersion1 },
+                { range: '11.2.0+13.sha-925e2b7', linker: partialComponentLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialComponentLinkerVersion1 },
             ]);
             return linkers;
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.0+3.sha-9cbd9be';
+    exports.NGCC_VERSION = '11.2.0+13.sha-925e2b7';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
