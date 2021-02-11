72439b80918530304001f359fe1b73a268ebcb62
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:33:04 +0000

    4c6d557ca9 build: update browserstacktunnel-wrapper (#40810)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0+13.sha-925e2b7";
+export declare const NGCC_VERSION = "11.2.0+16.sha-4c6d557";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
