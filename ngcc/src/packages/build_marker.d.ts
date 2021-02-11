4c5881e2e84101fb681d10178711af0b1d98b65d
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:34:27 +0000

    68da05dc47 Revert "ci: Remove language-service from testing group (#40787)" (#40794)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.0+3.sha-c56ecab";
+export declare const NGCC_VERSION = "12.0.0-next.0+4.sha-68da05d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
