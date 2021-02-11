5112fad94bcbc29e899de4e5a4174b527b6cf269
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:33:25 +0000

    9cbd9bed55 Revert "ci: Remove language-service from testing group (#40787)" (#40794)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -17,7 +17,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.0-rc.0+80.sha-e3f09ce`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.0+3.sha-9cbd9be`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0-rc.0+80.sha-e3f09ce";
+export declare const NGCC_VERSION = "11.2.0+3.sha-9cbd9be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
