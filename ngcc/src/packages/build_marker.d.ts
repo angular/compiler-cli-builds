3b5140964c47d7492b639543bc89cdb003000bfe
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:48:33 +0000

    e9a19a6152 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.0+8.sha-c22ae5d";
+export declare const NGCC_VERSION = "12.0.0-next.0+9.sha-e9a19a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
