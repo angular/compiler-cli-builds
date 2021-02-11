4578d76123252449688b3821ded698b0e4d285a3
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:00:04 +0000

    e27787a583 build: remove jpm dependency (#40807)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.0+12.sha-fcf00de';
+    exports.NGCC_VERSION = '12.0.0-next.0+13.sha-e27787a';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
