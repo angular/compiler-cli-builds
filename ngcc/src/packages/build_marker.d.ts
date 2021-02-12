28d3cfc16f32141f934c198bb8c945145143223c
Joey Perrott <josephperrott@gmail.com>
2021-02-12 21:49:58 +0000

    44b8b0ffe1 release: cut the zone.js-0.11.4 release (#40775)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.0+28.sha-b4c8143";
+export declare const NGCC_VERSION = "12.0.0-next.0+30.sha-44b8b0f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
