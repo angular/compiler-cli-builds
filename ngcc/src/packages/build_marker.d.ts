10c7c8780307e7c1e4b5ae6d4373ad5f72b6aa9f
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:18:47 +0000

    9482a4df4c docs: copy paste error (#40812)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0+16.sha-4c6d557";
+export declare const NGCC_VERSION = "11.2.0+18.sha-9482a4d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
