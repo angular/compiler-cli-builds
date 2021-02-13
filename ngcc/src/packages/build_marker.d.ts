267ddc0ba16837c3a703305794461a289b8b46a7
Joey Perrott <josephperrott@gmail.com>
2021-02-13 00:33:20 +0000

    bbbfa3e6bd docs: clarify that support schedule is subject to change (#40770)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.0+31.sha-ccd10ea";
+export declare const NGCC_VERSION = "12.0.0-next.0+32.sha-bbbfa3e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
