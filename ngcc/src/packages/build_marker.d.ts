28f7d4e6cf241825cf47e79b29a49520760c97cb
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:24:11 +0000

    91c68f498e docs: add a style recommendation for initializing inputs (#40698)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0+25.sha-a2fba9e";
+export declare const NGCC_VERSION = "11.2.0+29.sha-91c68f4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
