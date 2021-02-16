ce3ef92af8cef45767a5b3bac5dfbb9d2273220b
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:00:59 +0000

    5f0c219883 docs: fix schematics' package name for `ng generate` sub-commands (#40846)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.0+36.sha-72a00dc';
+    exports.NGCC_VERSION = '12.0.0-next.0+38.sha-5f0c219';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
