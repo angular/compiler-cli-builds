c28375acacffd363f326074ce32a13e86f1b6481
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:24:45 +0000

    53865c4517 docs(forms): fix link for browser compatibility of using selects in IE (#40855)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.0+37.sha-7f09945`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.0+61.sha-53865c4`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
