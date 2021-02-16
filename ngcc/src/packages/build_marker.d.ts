88fa65d3757cc076505d02534c76a3703c03156b
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:21:56 +0000

    7f09945771 docs: Add documentation about the CLI failing (#35385)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.0+29.sha-91c68f4";
+export declare const NGCC_VERSION = "11.2.0+37.sha-7f09945";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
