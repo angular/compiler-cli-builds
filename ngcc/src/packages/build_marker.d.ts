fb3aed26576705dbad010cd7c29363306e6b8f42
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:23:19 +0000

    75db0c7445 docs: edit builtin-directives doc (#39816)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.0+66.sha-95ad452";
+export declare const NGCC_VERSION = "12.0.0-next.0+67.sha-75db0c7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
