4b0aff9dfb0f047a766a32a15dbde787b89e1534
Zach Arend <zachzach@google.com>
2021-02-18 01:26:04 +0000

    65baac6055 docs: update attribute-binding.md to correctly describe [attr.binding] rules (#40878)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+8.sha-2375b85";
+export declare const NGCC_VERSION = "12.0.0-next.1+9.sha-65baac6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
