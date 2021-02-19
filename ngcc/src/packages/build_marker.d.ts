6597307f61ca279d83ff012c781d14c0f8df3890
atscott <atscott01@gmail.com>
2021-02-19 22:56:41 +0000

    ff86f919ab refactor(core): use RuntimeError to throw provider not found error (#40901)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.1+32.sha-3b884ed";
+export declare const NGCC_VERSION = "11.2.1+33.sha-ff86f91";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
