c0a57f68c5ba297b22776b1e232ee40b90df6c5b
atscott <atscott01@gmail.com>
2021-02-19 17:34:25 +0000

    09e1e1935a refactor(docs-infra): convert Sass mixin from camelCase to kebab-case to follow Sass conventions (#40881)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+14.sha-b4301c3";
+export declare const NGCC_VERSION = "12.0.0-next.1+32.sha-09e1e19";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
