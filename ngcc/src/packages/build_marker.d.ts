7046579079044cf1365d027520ac7e7b2a9ac1a6
atscott <atscott01@gmail.com>
2021-02-19 17:37:44 +0000

    a82fddf1ce feat(router): Allow for custom router outlet implementations (#40827)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+32.sha-09e1e19";
+export declare const NGCC_VERSION = "12.0.0-next.1+16.sha-a82fddf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
