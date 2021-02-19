bfbeb15d147a488941406f0325e101e77694a6d6
Andrew Scott <atscott@google.com>
2021-02-19 16:33:26 +0000

    30d5e25a28 docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+10.sha-542ba1f";
+export declare const NGCC_VERSION = "12.0.0-next.1+11.sha-30d5e25";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
