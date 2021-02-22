1f8fd359a02a05978c1d2bd35ed14f3faf96dc5a
atscott <atscott01@gmail.com>
2021-02-22 17:05:39 +0000

    0d5fbea5dc refactor(zone.js): update Object.create params to match web platform (#34287)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.1+33.sha-ff86f91";
+export declare const NGCC_VERSION = "11.2.1+36.sha-0d5fbea";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
