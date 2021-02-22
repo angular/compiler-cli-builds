e54e9be0c2993bbdda5408df2b8c7e486113fec0
Andrew Scott <atscott@google.com>
2021-02-22 21:19:14 +0000

    4a32579a4d fix(http): emit error on XMLHttpRequest abort event (#40786)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.1+37.sha-8beead1";
+export declare const NGCC_VERSION = "11.2.1+38.sha-4a32579";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
