6808157f85cbe132383423e695700c9331803537
Andrew Scott <atscott@google.com>
2021-02-22 20:56:20 +0000

    b84f719747 docs: update year (#40925)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+41.sha-dc9fd1a";
+export declare const NGCC_VERSION = "12.0.0-next.1+42.sha-b84f719";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
