2ae878962256e3aedfbdf3a2ddd330ff078ccd7a
atscott <atscott01@gmail.com>
2021-02-23 19:10:09 +0000

    1f7921be33 docs: improve accessibility of tohpt3 (#40617)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+55.sha-1f91672";
+export declare const NGCC_VERSION = "12.0.0-next.1+54.sha-1f7921b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
