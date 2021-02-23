77f9a2e21f4fc1098eed770b958ce08de119cd84
atscott <atscott01@gmail.com>
2021-02-23 19:05:39 +0000

    c6e2234fa6 docs: improve accessibility for toh-pt1 example (#40560)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.2+1.sha-09b92d7`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.2+10.sha-c6e2234`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
