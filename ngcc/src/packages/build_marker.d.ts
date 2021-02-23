15bb6d27789e9ab3e13e9cffa3d4fb6685a926cd
atscott <atscott01@gmail.com>
2021-02-23 21:44:13 +0000

    db28ed2341 refactor(language-service): add type guard for NgLanguageService (#40954) (#40968)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.2+10.sha-c6e2234";
+export declare const NGCC_VERSION = "11.2.2+13.sha-db28ed2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
