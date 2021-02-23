fa35a77b7048df14b108177c15511d71df2ff006
atscott <atscott01@gmail.com>
2021-02-23 19:04:02 +0000

    596dfb88a6 test(bazel): use bazel runfiles helper for resolving file names (#40955)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+47.sha-d3705b3";
+export declare const NGCC_VERSION = "12.0.0-next.1+50.sha-596dfb8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
