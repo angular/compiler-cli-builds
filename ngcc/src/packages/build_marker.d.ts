0411041641f1a643163b674233318590d32d13c2
atscott <atscott01@gmail.com>
2021-02-23 19:10:22 +0000

    06d3267824 docs: improve accessibility for toh-pt1 example (#40560)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.1+54.sha-1f7921b";
+export declare const NGCC_VERSION = "12.0.0-next.1+56.sha-06d3267";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
