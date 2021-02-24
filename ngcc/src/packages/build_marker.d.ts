753085d58c88a1e029a55caaee6bdde76956bde1
Zach Arend <zachzach@google.com>
2021-02-24 23:26:25 +0000

    55eb7b5681 fix(compiler): ensure JIT compilation of ɵɵngDeclarePipe() works (#40929)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.3+3.sha-ca271b8";
+export declare const NGCC_VERSION = "11.2.3+4.sha-55eb7b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
