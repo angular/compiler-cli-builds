45c8d7b346641ff1d25d007e5b48af1a81f42c9d
Zach Arend <zachzach@google.com>
2021-02-24 23:52:11 +0000

    6c05c80f19 feat(router): Add more find-tuned control in `routerLinkActiveOptions` (#40303)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+6.sha-29d8a0a";
+export declare const NGCC_VERSION = "12.0.0-next.2+7.sha-6c05c80";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
