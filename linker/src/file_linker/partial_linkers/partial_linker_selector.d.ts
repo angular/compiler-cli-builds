9658fad8116017c902f28cacd79e90ac781ce690
GitHub <noreply@github.com>
2021-02-24 17:52:40 +0000

    d7133a8e4e release: cut the v11.2.3 release (#40981)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.2+25.sha-3fe759e`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.2+26.sha-d7133a8`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
