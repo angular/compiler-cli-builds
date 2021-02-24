eda9968878b47b66a89190e9a56e16da150e4e41
Zach Arend <zachzach@google.com>
2021-02-24 23:04:44 +0000

    51a79772b2 test(localize): ensure tests pass on Windows (#40952)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+1.sha-8d13f63";
+export declare const NGCC_VERSION = "12.0.0-next.2+3.sha-51a7977";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
