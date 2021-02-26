52d4f6e3cd76309905c913c132c5c2d922293df3
Zach Arend <zachzach@google.com>
2021-02-26 16:45:29 +0000

    6206d99b86 docs(forms): add array example to get method (#41008)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+9.sha-953e98e";
+export declare const NGCC_VERSION = "12.0.0-next.2+8.sha-6206d99";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
