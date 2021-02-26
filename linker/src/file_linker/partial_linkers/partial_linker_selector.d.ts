8cc8ecfd7c07ec4743a416ee4b0c4aeda5e0b04b
Zach Arend <zachzach@google.com>
2021-02-26 16:50:39 +0000

    89ac771652 fix(docs-infra): add links to glossary for terms on the "Press kit" page (#40945)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.3+4.sha-55eb7b5`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.3+10.sha-89ac771`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.3+4.sha-55eb7b5";
+export declare const NGCC_VERSION = "11.2.3+10.sha-89ac771";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
