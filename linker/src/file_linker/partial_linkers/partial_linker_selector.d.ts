685be1b47d1ad965b647886e03424bedb226f2bc
Zach Arend <zachzach@google.com>
2021-02-26 23:56:51 +0000

    272b5645c4 fix(docs-infra): improve the layout of the "Resources" page (#40944)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.2+15.sha-fb82558`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.2+29.sha-272b564`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
