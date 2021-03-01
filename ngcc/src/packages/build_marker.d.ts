6f9747611713276e401f16f9e8a7d035cbb6e314
Zach Arend <zachzach@google.com>
2021-03-01 23:45:43 +0000

    8110cf0ed2 fix(language-service): can't provide the Input and Output custom binding property name (#41005)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+35.sha-e12d9de";
+export declare const NGCC_VERSION = "12.0.0-next.2+37.sha-8110cf0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
