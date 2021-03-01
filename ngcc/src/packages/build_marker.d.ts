7235488e8605ab46f40a0250a10b0f00ebbbe19a
Zach Arend <zachzach@google.com>
2021-03-01 17:19:53 +0000

    e12d9dec64 fix(core): remove duplicated EMPTY_ARRAY constant (#40991)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+34.sha-be8893f";
+export declare const NGCC_VERSION = "12.0.0-next.2+35.sha-e12d9de";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
