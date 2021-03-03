bfb3f5a71c1fab7a70200aca74a2c0c7e37a4b97
Zach Arend <zachzach@google.com>
2021-03-03 23:18:48 +0000

    4606a6097b docs: release notes for the v11.2.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+58.sha-bb6e5e2";
+export declare const NGCC_VERSION = "12.0.0-next.2+59.sha-4606a60";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
