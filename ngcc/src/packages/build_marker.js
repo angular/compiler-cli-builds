f47154907f0843f8f4ea498aa38e7393c434654a
Zach Arend <zachzach@google.com>
2021-03-03 21:57:44 +0000

    bb6e5e2dd0 fix(language-service): don't show external template diagnostics in ts files (#41070)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.2+57.sha-0847a03';
+    exports.NGCC_VERSION = '12.0.0-next.2+58.sha-bb6e5e2';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
