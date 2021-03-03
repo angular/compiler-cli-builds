289cf72bd0cdf6da224d66d362f3c29ba010e776
Zach Arend <zachzach@google.com>
2021-03-03 23:12:24 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.3+49.sha-9322e6a";
+export declare const NGCC_VERSION = "11.2.4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
