6670cf90ea11f64fe6c3a514ef18edd505571544
Zach Arend <zachzach@google.com>
2021-03-03 18:15:02 +0000

    ffbacbbc98 build(docs-infra): upgrade cli command docs sources to d62203a9f (#41046)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+50.sha-e9e7c33";
+export declare const NGCC_VERSION = "12.0.0-next.2+51.sha-ffbacbb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
