c4350bcfc9be2f07253a3e2914ad939b6bbd6b13
Zach Arend <zachzach@google.com>
2021-03-03 21:33:19 +0000

    0847a0353b fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.2+55.sha-7765b64";
+export declare const NGCC_VERSION = "12.0.0-next.2+57.sha-0847a03";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
