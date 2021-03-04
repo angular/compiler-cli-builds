1260fc4fb2f675a189bb9e93ee59dbef2573fa77
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:09:07 +0000

    45216ccc0d fix(language-service): Only provide dom completions for inline templates (#41078)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+13.sha-38524c4";
+export declare const NGCC_VERSION = "12.0.0-next.3+14.sha-45216cc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
