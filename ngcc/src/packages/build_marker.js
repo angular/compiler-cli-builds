2ca485adc249003608de17fc916a50e17772c668
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:10:37 +0000

    a05eb13caa fix(language-service): Only provide dom completions for inline templates (#41078)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.4+11.sha-1d4fc94';
+    exports.NGCC_VERSION = '11.2.4+12.sha-a05eb13';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
