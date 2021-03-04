88de9f4550f2e9c17d106c1f7b9e6969c52c3374
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:29 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+3.sha-ad40fca";
+export declare const NGCC_VERSION = "12.0.0-next.3+4.sha-d9acaa8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
