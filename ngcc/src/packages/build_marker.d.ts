b58001fc21a0d634f70bd138030a52f43c049ba2
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:04:55 +0000

    2de7eef92e docs: fix issue #40941 (#40942)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+12.sha-a05eb13";
+export declare const NGCC_VERSION = "11.2.4+14.sha-2de7eef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
