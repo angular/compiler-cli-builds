f4686fbf0e22761c24ffcc022bd577dcb5ac7e4f
Andrew Kushnir <akushnir@google.com>
2021-03-06 01:06:15 +0000

    4fd5d688ce docs: fix some typos in angular.io examples (#41093)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+14.sha-2de7eef";
+export declare const NGCC_VERSION = "11.2.4+15.sha-4fd5d68";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
