39012b05dcb7d5eac930ae54fbdf3d9ac9a0b0b4
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:25:29 +0000

    531f0bfb4a ci: improve error message when payload size check fails (#41116)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+34.sha-5c4914d";
+export declare const NGCC_VERSION = "12.0.0-next.3+32.sha-531f0bf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
