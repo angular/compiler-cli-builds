29a4351f59807465cd001d34a00835b4e6a36848
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:19:43 +0000

    c1a93fcf32 docs: remove augury resource (#41107)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+29.sha-fed6a7c";
+export declare const NGCC_VERSION = "12.0.0-next.3+31.sha-c1a93fc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
