279469bdd55a5d541e011d76b1c2c1bff7a8bcfe
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:14:28 +0000

    2d69f0c9ca docs: extend :host selector documentation (#41055)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+23.sha-7854c4d";
+export declare const NGCC_VERSION = "12.0.0-next.3+26.sha-2d69f0c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
