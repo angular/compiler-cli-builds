b1fa35f7ee3eba9834142c22fa5342b1521ad84a
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:27:13 +0000

    925746b6d8 fix(bazel): fix incorrect rollup plugin method signature (#41101)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+24.sha-e35ecea";
+export declare const NGCC_VERSION = "11.2.4+26.sha-925746b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
