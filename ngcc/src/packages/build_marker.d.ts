d4b4ab5d60334836632a1f04de54d1fc4ac8b40a
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:25:46 +0000

    1735430476 feat(localize): add scripts to migrate away from legacy message IDs (#41026)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+32.sha-531f0bf";
+export declare const NGCC_VERSION = "12.0.0-next.3+33.sha-1735430";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
