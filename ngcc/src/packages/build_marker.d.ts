ba34065b25d2685fb250b7be29460ac28e499850
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:24:42 +0000

    e7ce857bc6 docs(common): change misspelled constant (#41136)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+31.sha-c5ead38";
+export declare const NGCC_VERSION = "11.2.4+50.sha-e7ce857";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.4+31.sha-c5ead38';
+    exports.NGCC_VERSION = '11.2.4+50.sha-e7ce857';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
