35c461124e31363c591cd5f7d3cfb5eec1d39a9d
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:50:04 +0000

    c4ea0ab4e0 fix(docs-infra): print info to help debugging SW cache issue (#41106) (#41139)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+51.sha-16f90ca";
+export declare const NGCC_VERSION = "11.2.4+52.sha-c4ea0ab";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
