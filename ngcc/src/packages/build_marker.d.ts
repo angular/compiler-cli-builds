bd80f66ab05b6f45fb442adf570a496dcfc9acc4
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:15:20 +0000

    7a27cd262f refactor(docs-infra): simplify code snippet indentation in the "Cheat sheet" guide (#41051)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+43.sha-fa3689f";
+export declare const NGCC_VERSION = "12.0.0-next.3+57.sha-7a27cd2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.3+43.sha-fa3689f';
+    exports.NGCC_VERSION = '12.0.0-next.3+57.sha-7a27cd2';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
