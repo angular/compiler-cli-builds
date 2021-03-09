64ef9b73d803b97b62a68456ca48369df811c4fd
Andrew Kushnir <akushnir@google.com>
2021-03-09 22:14:55 +0000

    41b8f98744 build: update the lock closed sha to the latest sha (#41140)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+61.sha-b50d880";
+export declare const NGCC_VERSION = "12.0.0-next.3+62.sha-41b8f98";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
