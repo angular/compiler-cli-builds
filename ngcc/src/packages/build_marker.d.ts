577e0c7be9cff11f41f1d2530d72ed7fbab30804
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:22:00 +0000

    e29a29ce9f docs(common): change misspelled constant (#41136)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+58.sha-f281310";
+export declare const NGCC_VERSION = "12.0.0-next.3+60.sha-e29a29c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
