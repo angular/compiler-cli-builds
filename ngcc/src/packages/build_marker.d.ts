fc4b75b0d9d190ed5550a8ae245e2a76c4f7ab86
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:26:33 +0000

    ba81e8cc94 build: remove commit message max line length limit (#41157)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.3+64.sha-1fb0dbb";
+export declare const NGCC_VERSION = "12.0.0-next.4+2.sha-ba81e8c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
