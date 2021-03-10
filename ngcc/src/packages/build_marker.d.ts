74952ad7000274e10f0c56138c7b8d135512c14a
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:41:22 +0000

    2fa231a83c docs: Remove of unnecessary Input (#41150)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.4+53.sha-01c1677";
+export declare const NGCC_VERSION = "11.2.5+4.sha-2fa231a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
