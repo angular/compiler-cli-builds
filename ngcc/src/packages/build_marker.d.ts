5931a508ab5ccdd348b96d52652f20eb58a264a4
Andrew Kushnir <akushnir@google.com>
2021-03-10 20:16:24 +0000

    3c66b100dd perf(common): remove unused methods from DomAdapter (#41102)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+3.sha-4c79b8a";
+export declare const NGCC_VERSION = "12.0.0-next.4+4.sha-3c66b10";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
