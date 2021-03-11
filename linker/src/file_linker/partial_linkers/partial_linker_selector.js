4ff039b33653aa3ac25dc2e1a78ce0517c1df770
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:51:30 +0000

    f2ca29cd27 build(docs-infra): upgrade cli command docs sources to edc1d0bc7 (#41175)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -39,7 +39,7 @@
      * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
      * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
      *
-     * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+7.sha-e1e766a`). This
+     * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+8.sha-f2ca29c`). This
      * allows the linker to work on local builds effectively.
      */
     var PartialLinkerSelector = /** @class */ (function () {
@@ -86,15 +86,15 @@
             var partialPipeLinkerVersion1 = new partial_pipe_linker_1_1.PartialPipeLinkerVersion1();
             var linkers = new Map();
             linkers.set(exports.ɵɵngDeclareDirective, [
-                { range: '12.0.0-next.4+7.sha-e1e766a', linker: partialDirectiveLinkerVersion1 },
+                { range: '12.0.0-next.4+8.sha-f2ca29c', linker: partialDirectiveLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialDirectiveLinkerVersion1 },
             ]);
             linkers.set(exports.ɵɵngDeclareComponent, [
-                { range: '12.0.0-next.4+7.sha-e1e766a', linker: partialComponentLinkerVersion1 },
+                { range: '12.0.0-next.4+8.sha-f2ca29c', linker: partialComponentLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialComponentLinkerVersion1 },
             ]);
             linkers.set(exports.ɵɵngDeclarePipe, [
-                { range: '12.0.0-next.4+7.sha-e1e766a', linker: partialPipeLinkerVersion1 },
+                { range: '12.0.0-next.4+8.sha-f2ca29c', linker: partialPipeLinkerVersion1 },
                 { range: '>=11.1.0-next.1', linker: partialPipeLinkerVersion1 },
             ]);
             return linkers;
