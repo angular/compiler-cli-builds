8d0c8d488126736a2572ad90de4a45c7005b58a6
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:52:14 +0000

    78e67130f7 docs: add stepan suvorov to GDE resources (#41178)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+4.sha-2fa231a";
+export declare const NGCC_VERSION = "11.2.5+6.sha-78e6713";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
