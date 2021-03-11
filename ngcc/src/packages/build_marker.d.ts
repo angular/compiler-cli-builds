247a65eafed3ac4793d1fbe608c6c6a04c20a2c7
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-11 23:21:52 +0000

    012a2b55e1 build(language-service): use 'export =' syntax for default export (#41165)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+10.sha-69afeb3";
+export declare const NGCC_VERSION = "12.0.0-next.4+11.sha-012a2b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
