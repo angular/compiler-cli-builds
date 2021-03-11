812efa46a032ebe47c440a6d2e4eae998946cea2
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:45:23 +0000

    79e21b966e fix(docs-infra): fix angular.io on IE 11 (#41162)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+4.sha-3c66b10`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+6.sha-79e21b9`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
