24f27f36891f8a2bfff42ebed17258b346758792
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:49:30 +0000

    69afeb3808 fix(localize): trim extracted `equiv-text` values (#41180)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+9.sha-94e8f5d";
+export declare const NGCC_VERSION = "12.0.0-next.4+10.sha-69afeb3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
