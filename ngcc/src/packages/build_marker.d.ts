d3bac4bda8c0e626c0231cd8040538b48cc8cd45
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:48:43 +0000

    ed6c09ae45 fix(localize): trim extracted `equiv-text` values (#41180)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+6.sha-78e6713";
+export declare const NGCC_VERSION = "11.2.5+7.sha-ed6c09a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.5+6.sha-78e6713';
+    exports.NGCC_VERSION = '11.2.5+7.sha-ed6c09a';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
