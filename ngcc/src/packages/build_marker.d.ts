ff6e08fdb1a8dc3b3787561924098920529e81d9
Andrew Kushnir <akushnir@google.com>
2021-03-11 20:50:04 +0000

    74aba139ed fix(docs-infra): fix angular.io on IE 11 (#41162) (#41184)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+7.sha-ed6c09a";
+export declare const NGCC_VERSION = "11.2.5+9.sha-74aba13";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
