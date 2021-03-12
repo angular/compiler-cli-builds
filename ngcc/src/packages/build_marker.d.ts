682ad1ebc9310bf6348d686b54cc48783b9e3ee5
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 19:15:31 +0000

    70962465b5 fix(core): Switch `emitDistinctChangesOnlyDefaultValue` to true (#41121)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+11.sha-012a2b5";
+export declare const NGCC_VERSION = "12.0.0-next.4+12.sha-7096246";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
