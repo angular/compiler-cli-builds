9cd1554153f5d92a66c2143c9343a52a130b58cf
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:32:54 +0000

    da5f5a4910 docs: add youtube to social icons (#40987) (#41202)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+19.sha-b448412";
+export declare const NGCC_VERSION = "11.2.5+20.sha-da5f5a4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
