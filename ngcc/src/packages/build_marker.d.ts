50b1ba1e85b1637de788b91f8e7b0a68f2971051
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:52:14 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+18.sha-5908eda";
+export declare const NGCC_VERSION = "11.2.5+19.sha-b448412";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
