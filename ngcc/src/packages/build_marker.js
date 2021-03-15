649cc0b6870df218d0aa644f0a6e828ea73aab12
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:36:45 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.4+24.sha-1644d64';
+    exports.NGCC_VERSION = '12.0.0-next.4+25.sha-3bd1992';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
