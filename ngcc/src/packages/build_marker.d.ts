d4fa7d0078833301bf63e6d2883ea03f3c30788a
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 19:40:02 +0000

    bec919ace9 docs: add contributors guide to aio (#41061)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+20.sha-da5f5a4";
+export declare const NGCC_VERSION = "11.2.5+22.sha-bec919a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
