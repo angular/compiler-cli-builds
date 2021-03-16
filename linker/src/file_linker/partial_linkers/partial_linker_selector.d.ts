569edf10d6ee02dff09464ae7e8a6227f94764c2
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:52:38 +0000

    7d6db009e9 docs: update docs to reflect production mode by default (#41173)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -20,7 +20,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+34.sha-1a9f526`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+35.sha-7d6db00`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+34.sha-1a9f526";
+export declare const NGCC_VERSION = "12.0.0-next.4+35.sha-7d6db00";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
