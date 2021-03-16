17050e9871b10768c4fc578661efb7d3cea20186
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:56:09 +0000

    e88a9c6350 perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+35.sha-7d6db00";
+export declare const NGCC_VERSION = "12.0.0-next.4+38.sha-e88a9c6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
