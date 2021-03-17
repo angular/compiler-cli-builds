a8db3343cfe95f21ba3136b20876d57c63aa7427
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:25:45 +0000

    6bc22a7557 build(docs-infra): upgrade cli command docs sources to 5f5448301 (#41239)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -20,7 +20,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+43.sha-7d5b60e`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.4+46.sha-6bc22a7`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
