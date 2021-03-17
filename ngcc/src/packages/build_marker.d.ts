006131fd0f2d5bd1bcd87d279a0237c5eb307f3c
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:24:19 +0000

    78038027fc docs: update assets configuration to include `followSymlinks` option (#41238)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+28.sha-18cd7bb";
+export declare const NGCC_VERSION = "11.2.5+30.sha-7803802";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
