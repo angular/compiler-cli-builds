6338a9fc9ad3ebef2db9a79f5b888af5ff7738b3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:28:05 +0000

    98de4cb1bf docs: add deprecation note next to IE 11 in browser-support page (#41147)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.4+44.sha-0f0f34f";
+export declare const NGCC_VERSION = "12.0.0-next.4+47.sha-98de4cb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
