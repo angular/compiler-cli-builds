8b287c482f3fe54eb18274868f6330a44ffa578d
Misko Hevery <misko@hevery.com>
2021-03-18 17:38:56 +0000

    e23994191c docs: make `HeroService#gerHero()` in `toh-pt5` compatible with TS strict mode (#41234)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+7.sha-ac9a250";
+export declare const NGCC_VERSION = "12.0.0-next.5+5.sha-e239941";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
