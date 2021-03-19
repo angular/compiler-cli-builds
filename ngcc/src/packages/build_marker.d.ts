8c9eea225f8dc8edc2f3dea700f327f40f1c4502
Misko Hevery <misko@hevery.com>
2021-03-19 20:15:54 +0000

    3a55698402 fix(core): remove obsolete check for [class] and [className] presence (#41254)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+10.sha-2ccb579";
+export declare const NGCC_VERSION = "12.0.0-next.5+11.sha-3a55698";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
