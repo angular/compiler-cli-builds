11cfaae5bf19525d312100c747510f5f3c9b6030
Misko Hevery <misko@hevery.com>
2021-03-19 20:13:54 +0000

    72c9565c34 docs: specify date format for reviewed tag (#41269)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.6+5.sha-4690b6d";
+export declare const NGCC_VERSION = "11.2.6+12.sha-72c9565";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
