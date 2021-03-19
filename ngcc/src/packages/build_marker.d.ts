f3922728c759ab83067be297e2d84bc90459f996
Misko Hevery <misko@hevery.com>
2021-03-19 20:21:44 +0000

    aa039a13f0 fix(compiler): correctly process multiple rules containing `:host` selectors (#41261)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+14.sha-e8cae22";
+export declare const NGCC_VERSION = "12.0.0-next.5+12.sha-aa039a1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
