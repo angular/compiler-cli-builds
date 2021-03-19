b4326dee4046c8cb680d7cd5ef7c6e8ad2a9fcbd
Misko Hevery <misko@hevery.com>
2021-03-19 19:16:07 +0000

    4690b6dbca docs: update link to schematics schema (#41258)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.5+31.sha-cbba715";
+export declare const NGCC_VERSION = "11.2.6+5.sha-4690b6d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
