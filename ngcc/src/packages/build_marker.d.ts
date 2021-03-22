79283f386b5793fb9169fb2d31ecb694607a0243
Misko Hevery <misko@hevery.com>
2021-03-22 16:15:28 +0000

    efe02d8b04 docs: update contributors.json (#41284)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+16.sha-ed5bfe8";
+export declare const NGCC_VERSION = "12.0.0-next.5+19.sha-efe02d8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
