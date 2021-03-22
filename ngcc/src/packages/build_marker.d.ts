03359c883143e1af9c54cce20a335525b58d5b4b
Misko Hevery <misko@hevery.com>
2021-03-22 16:17:41 +0000

    e7b1d434c8 refactor(compiler): use `ɵɵInjectorDeclaration` rather than `ɵɵInjectorDef` in compiled output (#41119)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+19.sha-efe02d8";
+export declare const NGCC_VERSION = "12.0.0-next.5+23.sha-e7b1d43";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
