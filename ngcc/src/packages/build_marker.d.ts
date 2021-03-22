e67abff6ec7f19f8145a81780f4b2536ca33a474
Misko Hevery <misko@hevery.com>
2021-03-22 16:13:56 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+15.sha-5eb7f34";
+export declare const NGCC_VERSION = "12.0.0-next.5+18.sha-5bc0a47";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
