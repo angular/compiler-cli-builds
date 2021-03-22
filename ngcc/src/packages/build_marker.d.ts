f0b0e3a58c1b03024c5fa4a80ff4e01151c1f462
Misko Hevery <misko@hevery.com>
2021-03-22 20:06:26 +0000

    b5551609fe fix(router): fragment can be null (#37336)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+23.sha-e7b1d43";
+export declare const NGCC_VERSION = "12.0.0-next.5+24.sha-b555160";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
