3e10fafb1b1537975b4dde5c3575cda08c15b808
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:09 +0000

    80f11a9b86 ci: check that there are no unused contributor images (#41290)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+27.sha-e340d19";
+export declare const NGCC_VERSION = "12.0.0-next.5+29.sha-80f11a9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
