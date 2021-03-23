e001846f3c214c770dd198b1db69a76dc3dc6549
Joey Perrott <josephperrott@gmail.com>
2021-03-23 21:39:09 +0000

    d8d77ed00d refactor(dev-infra): use conventional-commits-parser for commit parsing (#41329)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -18,7 +18,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `11.2.6+25.sha-a68f6b0`). This
+ * Finally, note that we always start with the current version (i.e. `11.2.6+26.sha-d8d77ed`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
