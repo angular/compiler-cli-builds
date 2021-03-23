47cbc22045e30cec55d00196675107135e661a57
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:08:59 +0000

    31dc483206 docs: add alisa duncan to GDE resources (#41295)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+25.sha-c49b280";
+export declare const NGCC_VERSION = "12.0.0-next.5+26.sha-31dc483";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
