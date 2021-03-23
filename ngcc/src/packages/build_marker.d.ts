d1ad103331529b02979f0cadf60a2f8f94a7237f
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:47:07 +0000

    274dc15452 fix(core): detect synthesized constructors that have been downleveled using TS 4.2 (#41305)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+33.sha-65f7d53";
+export declare const NGCC_VERSION = "12.0.0-next.5+35.sha-274dc15";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
