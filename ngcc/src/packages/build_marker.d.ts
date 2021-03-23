7261ae1a580a282187220127641e1c48881b0fdc
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:41:51 +0000

    65f7d5380d docs: add link to fork repo how-to (#41266)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+31.sha-190fa07";
+export declare const NGCC_VERSION = "12.0.0-next.5+33.sha-65f7d53";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
