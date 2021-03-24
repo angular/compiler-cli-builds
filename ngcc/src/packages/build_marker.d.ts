5dc6bde8fddd61b4558f58a06cdd28bce9cf7b84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 20:55:17 +0000

    dd82c8e9f4 docs(docs-infra): add Zach Arend to contributors.json (#41325)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.5+41.sha-9da7ea9";
+export declare const NGCC_VERSION = "12.0.0-next.6+1.sha-dd82c8e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
