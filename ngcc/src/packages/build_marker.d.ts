4108e389f118f35b48a22430d0ce288f9f5e491a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 17:16:12 +0000

    723bfc717b Revert "feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)"

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.7+9.sha-06e5132";
+export declare const NGCC_VERSION = "11.2.7+13.sha-723bfc7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
