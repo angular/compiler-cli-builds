1f2f2cdbe3a5d70457720ea3f8a2aa193aaca68e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:56:13 +0000

    3470ea17d5 fix(docs-infra): increase the max width of the `file-not-found` page (#41275)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -21,7 +21,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.6+38.sha-db55f8e`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.6+40.sha-3470ea1`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
