83d02212d04871c7704ba62e131096ccd8a689af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:13:38 +0000

    c6d9a193d6 fix(dev-infra): Correctly place comments in dockerfile (#41374)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.6+42.sha-ff9470b";
+export declare const NGCC_VERSION = "12.0.0-next.6+43.sha-c6d9a19";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
