bfa6f62faee1bf3551cac0b8070d75509219d3ed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 22:05:16 +0000

    154129b9de release: cut the v11.2.8 release (#41419)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.7+32.sha-4bac297";
+export declare const NGCC_VERSION = "11.2.7+33.sha-154129b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
