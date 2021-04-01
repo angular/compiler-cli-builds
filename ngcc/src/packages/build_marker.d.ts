a414582c391472ff1d42426b5583f539753754a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 21:03:04 +0000

    4bac297aa0 build: update yargs dependency to 16.2.0. (#41392)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.7+31.sha-bef92f9";
+export declare const NGCC_VERSION = "11.2.7+32.sha-4bac297";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
