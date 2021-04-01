918a952618a12795702dd4f1cc5d9aeece700af6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:52:49 +0000

    db55f8e671 ci: update commit-message verification range parameters (#41341)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.6+36.sha-18bc9ff";
+export declare const NGCC_VERSION = "12.0.0-next.6+38.sha-db55f8e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
