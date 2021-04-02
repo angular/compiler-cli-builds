249e23406fb73b82fc3d0be1144119f41e8bc4bf
atscott <atscott01@gmail.com>
2021-04-02 17:46:37 +0000

    46a97b985e build: Bump minimum required node version to 10.19.0 (#41390)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.7+1.sha-c5e9944";
+export declare const NGCC_VERSION = "12.0.0-next.7+2.sha-46a97b9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
