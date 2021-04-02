50ca0fa49eb9f33df5c349383e5bcec6a5d84141
atscott <atscott01@gmail.com>
2021-04-02 18:11:23 +0000

    03a46afc91 docs: Including Visual Studio (#41414)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.7+3.sha-a43f36b";
+export declare const NGCC_VERSION = "12.0.0-next.7+5.sha-03a46af";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
