a7aff04789af1d870085e93061b7a6ee7fbec1af
atscott <atscott01@gmail.com>
2021-04-02 17:48:55 +0000

    dc8a0818e7 fix(dev-infra): require npm login for all publishes via wombat proxy (#41422)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.7+33.sha-154129b";
+export declare const NGCC_VERSION = "11.2.8+2.sha-dc8a081";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
