40716950a50fde3b744aa44f1770aa95d92d242c
atscott <atscott01@gmail.com>
2021-04-02 18:12:42 +0000

    03a46afc91 docs: Including Visual Studio (#41414)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -21,7 +21,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.7+3.sha-a43f36b`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.7+5.sha-03a46af`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
