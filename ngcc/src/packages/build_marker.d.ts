b4b7961750f8ab9e2c697eef26ed5ba22dcd301f
atscott <atscott01@gmail.com>
2021-04-05 16:20:26 +0000

    2cef385e43 feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.7+11.sha-ab47f41";
+export declare const NGCC_VERSION = "12.0.0-next.7+14.sha-2cef385";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
