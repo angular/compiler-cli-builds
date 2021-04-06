2200a8db05cef972dd7ee583ab490c45afe8eb16
atscott <atscott01@gmail.com>
2021-04-06 17:30:36 +0000

    ba3344ddbe feat(dev-infra): add utility method to GitClient to get latest SemVer tag (#41455)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.7+22.sha-f7c294e";
+export declare const NGCC_VERSION = "12.0.0-next.7+23.sha-ba3344d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
