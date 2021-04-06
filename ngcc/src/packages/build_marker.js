6183cb0b31738329b73e76494d2029e766931baa
atscott <atscott01@gmail.com>
2021-04-06 20:06:42 +0000

    ff9253b4a6 ci: add cache for windows run (#41467)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.7+23.sha-ba3344d';
+    exports.NGCC_VERSION = '12.0.0-next.7+32.sha-ff9253b';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
