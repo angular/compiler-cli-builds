e2ab7cb20e0339dd8fe8edd3719e8ce212040da8
atscott <atscott01@gmail.com>
2021-04-06 00:00:27 +0000

    6eb3dc770d build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.8+10.sha-754247f";
+export declare const NGCC_VERSION = "11.2.8+11.sha-6eb3dc7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
