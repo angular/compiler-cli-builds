339fd49ebda73d0a306e22f9879405188e1e215b
atscott <atscott01@gmail.com>
2021-04-06 20:05:09 +0000

    ff9253b4a6 ci: add cache for windows run (#41467)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.7+23.sha-ba3344d";
+export declare const NGCC_VERSION = "12.0.0-next.7+32.sha-ff9253b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
