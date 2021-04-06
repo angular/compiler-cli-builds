afbbbf053af4246d586944870cd9fe71c41d7899
atscott <atscott01@gmail.com>
2021-04-06 20:05:50 +0000

    3f12a0dbfa build: update package.json engines range for nodes in preparation for upgrade (#41472)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.8+23.sha-bc74735";
+export declare const NGCC_VERSION = "11.2.8+26.sha-3f12a0d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
