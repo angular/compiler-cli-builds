dfac64a53c4936c48604654f9d3e920f5e6e2549
atscott <atscott01@gmail.com>
2021-04-07 19:01:49 +0000

    4b8bbe2cf5 docs: cherry-pick the v11.2.9 release changelog

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -21,7 +21,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.7+41.sha-eda27c9`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.7+40.sha-4b8bbe2`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
