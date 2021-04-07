c238af58631030ccd4d356e4fa5e80d591f8ef5c
Zach Arend <zachzach@google.com>
2021-04-07 23:03:38 +0000

    646f4a1fe6 fix(router): Remove information about attached component when deactivating route (#41381)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+8.sha-41636e6";
+export declare const NGCC_VERSION = "11.2.9+12.sha-646f4a1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
