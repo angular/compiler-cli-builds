206c776b60b9fe36dc1843766e9902e169ff8f61
Zach Arend <zachzach@google.com>
2021-04-08 22:39:54 +0000

    7a40d8cefd fix(dev-infra): use base ref name rather than sha for the githubTargetBranch (#41523)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.d.ts
@@ -22,7 +22,7 @@ export declare const declarationFunctions: string[];
  * Also, ranges are matched to include "pre-releases", therefore if the range is `>=11.1.0-next.1`
  * then this includes `11.1.0-next.2` and also `12.0.0-next.1`.
  *
- * Finally, note that we always start with the current version (i.e. `12.0.0-next.8+19.sha-51bb922`). This
+ * Finally, note that we always start with the current version (i.e. `12.0.0-next.8+20.sha-7a40d8c`). This
  * allows the linker to work on local builds effectively.
  */
 export declare class PartialLinkerSelector<TStatement, TExpression> {
