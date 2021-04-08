d377b78160dc255e25b9d2b35b4011be934bd1bb
Zach Arend <zachzach@google.com>
2021-04-08 22:41:16 +0000

    74d1769a48 fix(dev-infra): use base ref name rather than sha for the githubTargetBranch (#41523)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+12.sha-646f4a1";
+export declare const NGCC_VERSION = "11.2.9+13.sha-74d1769";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
