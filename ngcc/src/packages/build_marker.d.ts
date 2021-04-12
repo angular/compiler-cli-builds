c9723779dee194b9be1c7e89ffa74ff971a671bc
Zach Arend <zachzach@google.com>
2021-04-12 17:51:39 +0000

    419c189ad3 fix(core): error if DebugRenderer2.destroyNode is called twice in a row (#41565)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+17.sha-7946f63";
+export declare const NGCC_VERSION = "11.2.9+21.sha-419c189";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
