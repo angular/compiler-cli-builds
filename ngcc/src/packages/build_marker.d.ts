3f17de7c4b7ff2f1d2712956132fc1a1a9947b5e
Zach Arend <zachzach@google.com>
2021-04-12 17:41:43 +0000

    ebc80b3b5c build: add breakingChangesLabel to merge configuration (#41546)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+29.sha-4810f5c";
+export declare const NGCC_VERSION = "12.0.0-next.8+31.sha-ebc80b3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.8+29.sha-4810f5c';
+    exports.NGCC_VERSION = '12.0.0-next.8+31.sha-ebc80b3';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
