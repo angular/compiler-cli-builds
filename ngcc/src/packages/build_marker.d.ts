1f039a9faad8e6f6e98076be7e9b733c706d032b
Zach Arend <zachzach@google.com>
2021-04-13 20:28:20 +0000

    04235fa079 docs: replace Codelyzer recommendation with Angular-ESLint in accessibility guide (#41294)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+36.sha-4847c14";
+export declare const NGCC_VERSION = "11.2.9+37.sha-04235fa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
