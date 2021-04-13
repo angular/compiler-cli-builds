dce5768971aff4c41a880d35bcf7081a19de3a47
Zach Arend <zachzach@google.com>
2021-04-13 00:09:04 +0000

    528e7d787c test: update CLI integration tests to run only for Ivy (#41434)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+37.sha-9bf8e51";
+export declare const NGCC_VERSION = "12.0.0-next.8+45.sha-528e7d7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
