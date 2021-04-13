7e2f994e604ae91c4ba31dabbe5ba3555fba7c71
Zach Arend <zachzach@google.com>
2021-04-13 00:01:52 +0000

    99c9f7c709 ci: remove old compliance tests (#41556) (#41585)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+21.sha-419c189";
+export declare const NGCC_VERSION = "11.2.9+23.sha-99c9f7c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
