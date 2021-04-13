0fe9811b077e9aa53666e0cba308a36708a85733
Zach Arend <zachzach@google.com>
2021-04-13 04:35:31 +0000

    9cda866703 docs: change (+) operator to `Number` function to match code (#41570) PR Close #41570

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.9+23.sha-99c9f7c";
+export declare const NGCC_VERSION = "11.2.9+30.sha-9cda866";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
