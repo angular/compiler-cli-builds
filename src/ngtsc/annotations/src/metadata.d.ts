75474289fd98e07280237532c2a0a7a2a53fc0a6
Zach Arend <zachzach@google.com>
2021-04-13 02:29:39 +0000

    1381301afe refactor(compiler-cli): track a dependency on a default import on `WrappedNodeExpr` (#41557)

--- src/ngtsc/annotations/src/metadata.d.ts
+++ src/ngtsc/annotations/src/metadata.d.ts
@@ -7,7 +7,6 @@
  */
 /// <amd-module name="@angular/compiler-cli/src/ngtsc/annotations/src/metadata" />
 import { R3ClassMetadata } from '@angular/compiler';
-import { DefaultImportRecorder } from '../../imports';
 import { DeclarationNode, ReflectionHost } from '../../reflection';
 /**
  * Given a class declaration, generate a call to `setClassMetadata` with the Angular metadata
@@ -17,4 +16,4 @@ import { DeclarationNode, ReflectionHost } from '../../reflection';
  * If no such metadata is present, this function returns `null`. Otherwise, the call is returned
  * as a `Statement` for inclusion along with the class.
  */
-export declare function extractClassMetadata(clazz: DeclarationNode, reflection: ReflectionHost, defaultImportRecorder: DefaultImportRecorder, isCore: boolean, annotateForClosureCompiler?: boolean): R3ClassMetadata | null;
+export declare function extractClassMetadata(clazz: DeclarationNode, reflection: ReflectionHost, isCore: boolean, annotateForClosureCompiler?: boolean): R3ClassMetadata | null;
