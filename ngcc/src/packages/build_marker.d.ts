0f0406fe474c6da196d9f2738db14cf983eaff77
Zach Arend <zachzach@google.com>
2021-04-14 23:09:58 +0000

    2af894d164 refactor: format renovate configuration with 2 spaces (#41619)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+99.sha-eaeb7ef";
+export declare const NGCC_VERSION = "12.0.0-next.8+101.sha-2af894d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
