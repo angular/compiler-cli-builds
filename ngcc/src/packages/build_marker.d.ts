d6059ecf4dcd3701c39c935e8a69e13ea44f3ea3
Zach Arend <zachzach@google.com>
2021-04-14 18:32:50 +0000

    3923201f95 refactor(compiler-cli): change how partial-linkers are matched to declaration versions (#41578)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+96.sha-5a8bc1b";
+export declare const NGCC_VERSION = "12.0.0-next.8+97.sha-3923201";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
