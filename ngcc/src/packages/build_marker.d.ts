abaf09f47a9e026064861552ed134d8a7448732b
Zach Arend <zachzach@google.com>
2021-04-14 23:56:37 +0000

    e655846627 docs: add rx-angular to resources.json (#39291)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+99.sha-886bf37";
+export declare const NGCC_VERSION = "12.0.0-next.9+9.sha-e655846";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
