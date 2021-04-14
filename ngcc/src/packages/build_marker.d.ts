fd9554d82e3124a51734b506f16340754ec9881b
Zach Arend <zachzach@google.com>
2021-04-14 17:11:25 +0000

    beafa22de9 feat(upgrade): update supported range of node versions (#41544)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+78.sha-0bc539a";
+export declare const NGCC_VERSION = "12.0.0-next.8+95.sha-beafa22";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
