1dd7cce87bf2cc9c77987cdd52899476668d6392
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:05:29 +0000

    a99aa29040 fix(animations): allow animations on elements in the shadow DOM (#40134)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+116.sha-1aebf16";
+export declare const NGCC_VERSION = "12.0.0-next.8+115.sha-a99aa29";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
