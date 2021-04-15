b288fa78f875f9509710090eef314511df54179e
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:10:38 +0000

    8d0511d009 ci: disable dependency updates of Angular JS packages (#41642)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+115.sha-a99aa29";
+export declare const NGCC_VERSION = "12.0.0-next.8+117.sha-8d0511d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
