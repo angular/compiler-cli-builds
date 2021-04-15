07c09462254b9824a0bb8536b819f7ce56a6bedf
Andrew Kushnir <akushnir@google.com>
2021-04-15 22:01:41 +0000

    6c43a949b8 docs: update copy in schematics.md (#41521)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.10+7.sha-3aa9235";
+export declare const NGCC_VERSION = "11.2.10+8.sha-6c43a94";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
