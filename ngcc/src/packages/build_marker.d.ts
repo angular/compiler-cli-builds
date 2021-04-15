6abecb90055237febda585b02de824d773672c5b
Zach Arend <zachzach@google.com>
2021-04-15 02:12:38 +0000

    5332a4a919 refactor: update `$schema` and `id` keywords (#41574)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+108.sha-4329cca";
+export declare const NGCC_VERSION = "12.0.0-next.8+110.sha-5332a4a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
