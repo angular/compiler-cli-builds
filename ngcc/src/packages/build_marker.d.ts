57152db71078ea177de99f65e1fc15dd8925b496
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:11:09 +0000

    f4376fc0d4 fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.10+5.sha-c4b0753";
+export declare const NGCC_VERSION = "11.2.10+6.sha-f4376fc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
