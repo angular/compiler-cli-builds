29e78b28fffadcf7f6f059595e8aaa24eb7a9bb1
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:06:24 +0000

    1aebf165db fix(common): viewport scroller not finding elements inside the shadow DOM (#41644)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+114.sha-6bceb70";
+export declare const NGCC_VERSION = "12.0.0-next.8+116.sha-1aebf16";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
