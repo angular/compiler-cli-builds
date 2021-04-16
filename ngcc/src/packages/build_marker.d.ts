c863d3c1dad124da2a49714b712acc3b78635899
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:16:48 +0000

    fe5bf7f53f fix(compiler-cli): autocomplete literal types in templates. (#41456) (#41645)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+123.sha-5d73b33";
+export declare const NGCC_VERSION = "12.0.0-next.8+126.sha-fe5bf7f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
