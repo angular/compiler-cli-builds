f55c658f8de3e6a492b94beb52b1c5dc1a5a85e4
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:24:20 +0000

    d3edc5c0f5 test(docs-infra): run all example tests in IVY runtime (#41638)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+124.sha-b23850e";
+export declare const NGCC_VERSION = "12.0.0-next.8+130.sha-d3edc5c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
