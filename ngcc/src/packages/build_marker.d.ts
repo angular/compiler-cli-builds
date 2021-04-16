9ad669a4f594fa6bef832469522a6b1ea351186c
Andrew Kushnir <akushnir@google.com>
2021-04-16 00:02:28 +0000

    c85710da4f docs: edits to remove jargon and abbreviations (#41530)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.9+20.sha-9684cd3";
+export declare const NGCC_VERSION = "12.0.0-next.9+21.sha-c85710d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
