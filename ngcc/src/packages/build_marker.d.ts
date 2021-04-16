6b26b323d6d41c777ef905c946414f15a095a3ba
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:14:03 +0000

    5d73b33ba2 build: update angular packages (#41633)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+121.sha-391d08f";
+export declare const NGCC_VERSION = "12.0.0-next.8+123.sha-5d73b33";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
