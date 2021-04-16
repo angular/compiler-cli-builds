8ea664a018dee4698f03b1fd596a5509dc716810
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:55:03 +0000

    ab257b3701 fix(compiler): non-literal inline templates incorrectly processed in partial compilation (#41583)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.9+26.sha-8b2b5ef";
+export declare const NGCC_VERSION = "12.0.0-next.9+32.sha-ab257b3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
