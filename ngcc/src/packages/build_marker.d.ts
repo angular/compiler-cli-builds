a67792171fadd722cc4550da74d71e23bf679111
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:42:20 +0000

    d5a8302dfd docs: separate steps for github actions (#41594) PR Close #41594

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+133.sha-83d6a96";
+export declare const NGCC_VERSION = "12.0.0-next.8+135.sha-d5a8302";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
