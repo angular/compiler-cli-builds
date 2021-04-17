cf336ed27a4ed8fe762928dd2fe4c8e36672c3b8
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:46:32 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.8+136.sha-96175fd';
+    exports.NGCC_VERSION = '12.0.0-next.8+138.sha-de25cda';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
