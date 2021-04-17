1b947b8a54bd85e302bb8887504b24639d2b867a
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:47:44 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+136.sha-96175fd";
+export declare const NGCC_VERSION = "12.0.0-next.8+138.sha-de25cda";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
