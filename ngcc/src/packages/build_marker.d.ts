30aa1ac780b29cd49634e3854c505473b4fc8e88
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:42:58 +0000

    96175fdda2 docs: fix the property name in the example (#41410)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+135.sha-d5a8302";
+export declare const NGCC_VERSION = "12.0.0-next.8+136.sha-96175fd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
