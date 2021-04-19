c650bbffc9e4d7826722a60d389a0406cfaf6f98
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:18:36 +0000

    3738dbba05 ci: disable automatic updates of rollup (#41672)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+142.sha-cf6c5c1";
+export declare const NGCC_VERSION = "12.0.0-next.8+145.sha-3738dbb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
