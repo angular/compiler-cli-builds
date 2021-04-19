ec89d1a2f1777d45d21022ead289b44b224222bc
Andrew Kushnir <akushnir@google.com>
2021-04-19 22:42:57 +0000

    b333a1fe5c build: update firebase-tools to version 9.10.0 (#41686)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.9+75.sha-2114a13";
+export declare const NGCC_VERSION = "12.0.0-next.9+77.sha-b333a1f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
