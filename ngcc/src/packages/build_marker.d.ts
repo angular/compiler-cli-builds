7ef2ddb6e9cd08a55abc53e94454d25ba2f3234a
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:34:47 +0000

    ef4cf878b1 build: update google-closure-compiler to version 20210406.0.0 (#41687)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+169.sha-5e34317";
+export declare const NGCC_VERSION = "12.0.0-next.8+170.sha-ef4cf87";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
