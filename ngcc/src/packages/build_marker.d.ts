d29e6f791c4b797675358c3ab268be2edf726bc1
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:21:19 +0000

    13913231f1 test(docs-infra): change URL separator from `\t` to `-->` in `URLS_TO_REDIRECT.txt` (#41680)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+144.sha-75168a2";
+export declare const NGCC_VERSION = "12.0.0-next.8+147.sha-1391323";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
