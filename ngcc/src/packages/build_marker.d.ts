fa6ca54029e82924221eb2e757d496343de420bd
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:29:50 +0000

    343b9d0ddc build: update jasmine-spec-reporter to version 7.0.0 (#41692)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+159.sha-322db17";
+export declare const NGCC_VERSION = "12.0.0-next.8+155.sha-343b9d0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
