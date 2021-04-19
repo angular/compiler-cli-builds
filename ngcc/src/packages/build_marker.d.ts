4d6fde6cce3671a961c076f92c646018a9559548
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:38:18 +0000

    e256d9274c build: update @octokit/types to version 6.13.0 (#41678)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+155.sha-343b9d0";
+export declare const NGCC_VERSION = "12.0.0-next.8+165.sha-e256d92";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
