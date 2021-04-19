09ece5c6382a8b1a06bb629bbd24a03651afe447
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:28:59 +0000

    b64b6b3944 build: update mocha to version 8.3.2 (#41699)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+154.sha-0c23c8b";
+export declare const NGCC_VERSION = "12.0.0-next.8+160.sha-b64b6b3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.8+154.sha-0c23c8b';
+    exports.NGCC_VERSION = '12.0.0-next.8+160.sha-b64b6b3';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
