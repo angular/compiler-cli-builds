a3fe63331ab14c3d92401fa2a4128950fcde7c18
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:22:19 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+147.sha-1391323";
+export declare const NGCC_VERSION = "12.0.0-next.8+149.sha-6a153a2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
