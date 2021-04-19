e6206971e85fbd578fbdb8e29e8b5bba2134117b
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:25:32 +0000

    185090b15d docs: add missing backticks in `schematics-for-libraries.md` (#41681)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+149.sha-6a153a2";
+export declare const NGCC_VERSION = "12.0.0-next.8+148.sha-185090b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
