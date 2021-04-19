2e55d9eed63fa064527b359b9f2ed0b1953032c3
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:33:27 +0000

    5e343173e8 docs: fix styling issues in example CSS (#41222)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+167.sha-d58747d";
+export declare const NGCC_VERSION = "12.0.0-next.8+169.sha-5e34317";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
