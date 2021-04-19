f26126a3edce5d12e4ae9f977f929b964a627925
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:38:35 +0000

    c617f1f768 build: update jsdom to version 16.5.3 (#41695)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+170.sha-ef4cf87";
+export declare const NGCC_VERSION = "12.0.0-next.8+171.sha-c617f1f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
