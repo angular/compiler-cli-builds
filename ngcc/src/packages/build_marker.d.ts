40d0fc3a6cd75f53c6443381445a14347110e22b
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:29 +0000

    7459f6d705 docs: rewrites animation query () method use (#41710)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.10+35.sha-a76f07b";
+export declare const NGCC_VERSION = "11.2.10+38.sha-7459f6d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
