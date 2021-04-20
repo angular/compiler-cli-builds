649e6c5dd0daa6195866ea16163c860c8dedf6f4
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:02:00 +0000

    c2011c0ba1 build: update semver to version 7.3.5 (#41715)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+182.sha-e238f2c";
+export declare const NGCC_VERSION = "12.0.0-next.8+184.sha-c2011c0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
