5f3a2a38762d01be93b26aca50c8050a1df200d9
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:19:10 +0000

    7bd8741109 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.9+87.sha-87b5026";
+export declare const NGCC_VERSION = "12.0.0-next.9+88.sha-7bd8741";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
