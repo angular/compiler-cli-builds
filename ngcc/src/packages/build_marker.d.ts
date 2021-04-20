7d4c35da936619425fb48c7d1531b826c2e648d4
Andrew Kushnir <akushnir@google.com>
2021-04-20 18:55:46 +0000

    87b5026e48 docs: add margin to button boilerplate CSS for examples (#41413)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.9+86.sha-2bcbbda";
+export declare const NGCC_VERSION = "12.0.0-next.9+87.sha-87b5026";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
