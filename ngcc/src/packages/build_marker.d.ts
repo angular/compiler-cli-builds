b6eac673160e7ec05d0f19f17ac4b71a66590edd
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:02:04 +0000

    e0fb18a133 build: update @babel/cli to version 7.13.16 (#41734)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+202.sha-c3a512a";
+export declare const NGCC_VERSION = "12.0.0-next.8+203.sha-e0fb18a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
