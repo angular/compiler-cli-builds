8b1fa0e429eadf3b448c91920d22e2f9755c7c40
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 16:03:40 +0000

    6221227e10 build(docs-infra): upgrade cli command docs sources to 8a5dfd9bb (#41766)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+6.sha-2acb8d1";
+export declare const NGCC_VERSION = "12.0.0-rc.0+10.sha-6221227";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
