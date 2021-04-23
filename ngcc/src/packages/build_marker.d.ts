13dd5d8e8397c0729c18eed6845fc89b39a2ba6b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:57:43 +0000

    163c076f97 docs: Indicate `HttpClientModule` being imported (#41749)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+9.sha-dd46b87";
+export declare const NGCC_VERSION = "11.2.11+10.sha-163c076";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
