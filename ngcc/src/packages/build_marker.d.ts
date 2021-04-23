e9e9e4adc7f1f7e4d89f5a91afb0e89e27671c6f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 17:06:06 +0000

    dd46b87bc7 fix(upgrade): preserve $interval.flush when ngMocks is being used (#30229)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+8.sha-42c64aa";
+export declare const NGCC_VERSION = "11.2.11+9.sha-dd46b87";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
