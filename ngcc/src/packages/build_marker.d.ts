c1706ac25e4d6cc5cdd0c2fdb61823be241cebbc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:39:11 +0000

    f9c1f08957 fix(core): update supported range of node versions to only include LTS versions (#41822)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+42.sha-8d4460d";
+export declare const NGCC_VERSION = "12.0.0-rc.0+61.sha-f9c1f08";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
