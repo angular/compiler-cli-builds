8a0a6876665260df6cc5370406de01bbc89f4a55
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 17:03:43 +0000

    21f1466d84 build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+10.sha-163c076";
+export declare const NGCC_VERSION = "11.2.11+15.sha-21f1466";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
