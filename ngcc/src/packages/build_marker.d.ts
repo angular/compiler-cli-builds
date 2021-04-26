967d311751ad5507879ad176cbc7d337a8130d39
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:33:57 +0000

    aa578f56bf docs: fix links to docs with ambiguous paths (#41788)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+15.sha-21f1466";
+export declare const NGCC_VERSION = "11.2.11+18.sha-aa578f5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
