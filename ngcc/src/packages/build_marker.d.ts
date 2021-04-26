8d6c534c98e063a4ef4565de6940a44fd5b2b178
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:51:17 +0000

    990067a0c6 perf(core): avoid storing LView in __ngContext__ (#41358)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+24.sha-e6ec22f";
+export declare const NGCC_VERSION = "12.0.0-rc.0+34.sha-990067a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
