5354cf47cba5c2596e2990c83ce9ca95132d662e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:02:05 +0000

    295889ed2e ci: prevent CI cache from growing indefinitely (#41814)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+226.sha-a938849";
+export declare const NGCC_VERSION = "12.0.0-next.8+228.sha-295889e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
