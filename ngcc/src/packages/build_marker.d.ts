bd79bd5befdb48b78991e295395e40eede730c34
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 23:19:38 +0000

    7756ead0bf docs: add metadata documentation to httpClient (#41706)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+18.sha-aa578f5";
+export declare const NGCC_VERSION = "11.2.11+20.sha-7756ead";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
