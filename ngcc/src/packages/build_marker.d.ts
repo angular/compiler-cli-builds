1f19279648b52ee9bef6ec4b2691fedfd9a8982c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:37:50 +0000

    01e546f116 docs: improve accessibility of architecture example (#41314)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+216.sha-3268847";
+export declare const NGCC_VERSION = "12.0.0-next.8+217.sha-01e546f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
