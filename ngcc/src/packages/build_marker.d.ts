effc68808873f6a965b2d5670f9a1dedeb5b6d8a
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:35:47 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+253.sha-f978976";
+export declare const NGCC_VERSION = "12.0.0-next.8+255.sha-3783976";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
