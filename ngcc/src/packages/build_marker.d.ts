1d0eabec1c4ed2052273239dff1ff67d96097f58
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:37:08 +0000

    f978976970 build: update @types/jasmine to version 3.6.10 (#41830)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+252.sha-da6ed15";
+export declare const NGCC_VERSION = "12.0.0-next.8+253.sha-f978976";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
