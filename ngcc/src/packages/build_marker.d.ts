7ce893efb59158b72e1ccdf3accf7ce93a49d3fd
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:13:57 +0000

    eedbb758d2 build(docs-infra): update dgeni-packages to fix version list (#41841)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+24.sha-e758658";
+export declare const NGCC_VERSION = "11.2.11+25.sha-eedbb75";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
