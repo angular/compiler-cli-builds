dadd3779454a235552654b48f2498786a6046df4
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:56:14 +0000

    e7586581af ci: add Zach Arend to pullapprove groups (#41839)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "11.2.11+23.sha-84a44a9";
+export declare const NGCC_VERSION = "11.2.11+24.sha-e758658";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '11.2.11+23.sha-84a44a9';
+    exports.NGCC_VERSION = '11.2.11+24.sha-e758658';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
