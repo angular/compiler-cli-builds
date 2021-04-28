b3a12ee0fc747aba533f505720af196f60db7ca7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:18:26 +0000

    a66421e643 ci: re-enable the `@angular/components` unit tests (#41816)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+79.sha-77a5157";
+export declare const NGCC_VERSION = "12.0.0-rc.0+80.sha-a66421e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
