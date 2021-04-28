23e3821fafeba08ee5457979579288210c8f20d5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 18:53:55 +0000

    444775c352 docs: release notes for the v11.2.12 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+270.sha-641fa69";
+export declare const NGCC_VERSION = "12.0.0-next.8+271.sha-444775c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
