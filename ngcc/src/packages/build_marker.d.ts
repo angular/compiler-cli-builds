b691b3d4b8252c5c2e508b999a685a2feedaeb2e
Alan Agius <alan.agius4@gmail.com>
2021-04-28 09:20:36 +0000

    ab88957d68 fix(dev-infra): replace `chalk` require with namespace import

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+266.sha-c31d3c0";
+export declare const NGCC_VERSION = "12.0.0-next.8+267.sha-ab88957";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
