fe6262d31875a87fac10f2a28df6e4e6555f7e54
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:53:07 +0000

    45792f145c docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.1+6.sha-c691b66";
+export declare const NGCC_VERSION = "12.0.0-rc.1+9.sha-45792f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
