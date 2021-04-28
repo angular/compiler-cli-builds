69df210b2ff47fa776f0a2a157a4ad800080e272
GitHub <noreply@github.com>
2021-04-28 19:17:39 +0000

    98de217640 release: cut the v12.0.0-rc.1 release (#41864)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+80.sha-a66421e";
+export declare const NGCC_VERSION = "12.0.0-rc.0+81.sha-98de217";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
