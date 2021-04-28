bea8d2df97a0c91a6bc728ef99c6cdd8738e86b0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 20:12:29 +0000

    c691b668d1 docs: remove mention of scheduleTick in markForCheck (#41845)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.0+81.sha-98de217";
+export declare const NGCC_VERSION = "12.0.0-rc.1+6.sha-c691b66";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
