b502521a36a0fde59dbeb7962bf1a8b1c69f5649
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 22:46:44 +0000

    55d9713664 docs: Edits to remove jargon (#41536)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+281.sha-c31d6ea";
+export declare const NGCC_VERSION = "12.0.0-next.8+282.sha-55d9713";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
