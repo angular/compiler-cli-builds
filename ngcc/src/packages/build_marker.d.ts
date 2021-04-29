f8e100ed4e390742308c5ce1c7e13b6c12bf885a
Misko Hevery <misko@hevery.com>
2021-04-29 21:13:31 +0000

    0255a2c631 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.1+16.sha-77b77ff";
+export declare const NGCC_VERSION = "12.0.0-rc.1+20.sha-0255a2c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
