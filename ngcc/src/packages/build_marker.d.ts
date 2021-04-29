6512b2e1761d17279a82cfb597c02590938eb71b
Misko Hevery <misko@hevery.com>
2021-04-29 17:26:59 +0000

    595a75cda4 build: update dependency karma to v6 (#41696)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+282.sha-55d9713";
+export declare const NGCC_VERSION = "12.0.0-next.8+283.sha-595a75c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
