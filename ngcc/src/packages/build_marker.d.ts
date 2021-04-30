e2124a22d9162562b2570ad45175e530d27afc0d
Misko Hevery <misko@hevery.com>
2021-04-30 21:57:43 +0000

    74da3724e4 docs: Update link to Testing Angular online book (#41906)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+303.sha-b5134e2";
+export declare const NGCC_VERSION = "12.0.0-next.8+307.sha-74da372";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
