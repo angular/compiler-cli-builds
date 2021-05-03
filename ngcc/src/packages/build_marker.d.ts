f73ef08fb7f9cc8d284d55296b0d2b43643d9f75
Misko Hevery <misko@hevery.com>
2021-05-03 21:49:06 +0000

    3777cf55aa Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+313.sha-3b58903";
+export declare const NGCC_VERSION = "12.0.0-next.8+316.sha-3777cf5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
