60c9d73b09f08978ec2494c2f54bfb359d3fbe2f
Misko Hevery <misko@hevery.com>
2021-05-03 21:50:48 +0000

    741c295129 docs: update file structure document (#41923)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+315.sha-f42796a');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+314.sha-741c295');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.0.0-next.8+315.sha-f42796a') {
+            if (version === '12.0.0-next.8+314.sha-741c295') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
