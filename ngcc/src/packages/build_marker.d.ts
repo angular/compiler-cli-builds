02646da5eab122f15872a224ed3c939423903c54
Misko Hevery <misko@hevery.com>
2021-05-04 15:35:05 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+314.sha-741c295";
+export declare const NGCC_VERSION = "12.0.0-next.8+318.sha-b7db98f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
