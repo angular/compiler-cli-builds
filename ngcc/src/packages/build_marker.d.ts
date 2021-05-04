a80c96e3629264f43b1cb40711fa1be0389b8cc6
Misko Hevery <misko@hevery.com>
2021-05-04 15:53:28 +0000

    dac78d1ba1 build: update dependency yargs to v17 (#41920)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+320.sha-4dae97d";
+export declare const NGCC_VERSION = "12.0.0-next.8+321.sha-dac78d1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
