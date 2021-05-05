180fc282ce27b2200465ba24c0f09d56ac1ea1d8
Misko Hevery <misko@hevery.com>
2021-05-05 21:09:23 +0000

    df2ff0497d docs: initial commit of content projection topic. (#41143)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.1+58.sha-33c9579";
+export declare const NGCC_VERSION = "12.0.0-rc.1+64.sha-df2ff04";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
