6084f1daf255db6d6be972a5b5410d99119707ba
Misko Hevery <misko@hevery.com>
2021-05-05 21:03:56 +0000

    fb38175a40 docs: remove ts-node and tslint form dev deps (#41950)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+333.sha-753fb06";
+export declare const NGCC_VERSION = "12.0.0-next.8+334.sha-fb38175";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
