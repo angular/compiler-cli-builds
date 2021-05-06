75abcee0c8bbf6c61fe450166d4005e5e8a086e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:00:13 +0000

    5c9bd237e3 build: update release configuration to use new release notes system (#41905)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+345.sha-35fbd4f";
+export declare const NGCC_VERSION = "12.0.0-next.8+350.sha-5c9bd23";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
