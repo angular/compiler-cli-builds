9e26f760185e6e53a316bbe1962f2a407d658687
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:09:31 +0000

    4d7294d9f7 feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+354.sha-640ec78";
+export declare const NGCC_VERSION = "12.0.0-next.8+355.sha-4d7294d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
