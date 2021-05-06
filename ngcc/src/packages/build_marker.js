74f09e519b36d83569eafae38d6292e5069c82bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:02:32 +0000

    90d4b2277b build(docs-infra): upgrade cli command docs sources to 094a1b674 (#41958)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.0.0-next.8+350.sha-5c9bd23';
+    exports.NGCC_VERSION = '12.0.0-next.8+348.sha-90d4b22';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
