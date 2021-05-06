84317d85724efa8ea8986762501ba7ba306e48d8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 19:24:11 +0000

    a4e3ea0311 feat(docs-infra): add support for unformatted code-snippets (#41335)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+348.sha-90d4b22";
+export declare const NGCC_VERSION = "12.0.0-next.8+351.sha-a4e3ea0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
