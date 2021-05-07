acee6ee92540c1f699d433f6e49e8fa56bc93706
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 14:26:07 +0000

    3927e2529a build: update angular (#41955)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+364.sha-e29eb8a";
+export declare const NGCC_VERSION = "12.0.0-next.8+365.sha-3927e25";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
