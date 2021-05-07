d90d9209b1a8ee7d505bde7719278140248bef4f
Andrew Scott <atscott@google.com>
2021-05-07 01:30:23 +0000

    e29eb8abfb docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+355.sha-4d7294d";
+export declare const NGCC_VERSION = "12.0.0-next.8+364.sha-e29eb8a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
