9ca1ee08fde4aa7b50e3a4d113464a8d653c7fec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:49:07 +0000

    23f6b76d1a fix(docs-infra): update glossary to include the term template-input-variable (#41632)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+373.sha-cd252b9";
+export declare const NGCC_VERSION = "12.0.0-next.8+376.sha-23f6b76";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
