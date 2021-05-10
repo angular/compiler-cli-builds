8559122f6779db37210d88cf07c7e4f59caf5589
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:49:03 +0000

    d59f2b0d0c refactor(core): remove unnecessary cast (#41960)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+373.sha-cd252b9";
+export declare const NGCC_VERSION = "12.0.0-next.8+380.sha-d59f2b0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
