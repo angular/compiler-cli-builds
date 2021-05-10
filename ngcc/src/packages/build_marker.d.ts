5e578dabd12d754d57f615e6d1ab9114312ac1a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 20:49:18 +0000

    96624b71a7 fix(platform-browser): prevent memory leak of style nodes if shadow DOM encapsulation is used (#42005)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+391.sha-6ebb86f";
+export declare const NGCC_VERSION = "12.0.0-next.8+394.sha-96624b7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
