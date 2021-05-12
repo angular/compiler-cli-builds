fab2ed8b66fe4804dfea50b00c4819b784d56e89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:54:35 +0000

    751491701e build(docs-infra): update docs examples to Angular v12.0.0 (#42071)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-rc.3+2.sha-0a2191f";
+export declare const NGCC_VERSION = "12.0.0+2.sha-7514917";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
