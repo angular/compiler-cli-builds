84886aae126f24ad9b4cc4846cdce57b19d33856
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 18:36:28 +0000

    a74eb52589 Revert "refactor(core): optimize calls to `split` and `slice` while computing version parts (#41208)"

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+395.sha-eac42e9');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+396.sha-a74eb52');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.0.0-next.8+395.sha-eac42e9') {
+            if (version === '12.0.0-next.8+396.sha-a74eb52') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
