379a65952f979a3925208ee4cc9caf766500c21d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:29:24 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+396.sha-a74eb52";
+export declare const NGCC_VERSION = "12.0.0-next.8+397.sha-dad205e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
