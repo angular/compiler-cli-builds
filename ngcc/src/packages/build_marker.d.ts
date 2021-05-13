cbb1b5f3bb6f994293a3c8ce37c0f08ca135eb17
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 01:05:58 +0000

    53cd7e5eb5 docs: update the angular roadmap (#42050)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+399.sha-d34ce6b";
+export declare const NGCC_VERSION = "12.0.0-next.8+401.sha-53cd7e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
