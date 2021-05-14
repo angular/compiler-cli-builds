bcceeeb4de19657590127cb46447579cac2fb70d
Zach Arend <zachzach@google.com>
2021-05-14 21:24:08 +0000

    7fb8527e1e refactor(dev-infra): remove invoke bazel clean command from release tool (#42101)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+412.sha-3aec55b";
+export declare const NGCC_VERSION = "12.0.0-next.8+413.sha-7fb8527";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
