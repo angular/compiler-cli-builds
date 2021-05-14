9a3106ac7a2e72946ace683336533a6465742849
Zach Arend <zachzach@google.com>
2021-05-14 21:26:07 +0000

    6d87895ffd test(dev-infra): add testing for integration of ReleaseNotes in publishing tooling (#41967)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+414.sha-e89e3cb";
+export declare const NGCC_VERSION = "12.0.0-next.8+415.sha-6d87895";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
