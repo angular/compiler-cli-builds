ee6b309c2163c86e8471369c9c44467aa4f6ed9d
Andrew Scott <atscott@google.com>
2021-05-18 22:58:19 +0000

    ec78e68a36 fix(dev-infra): correct FormatConfig interface (#42154)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+435.sha-b7d1614";
+export declare const NGCC_VERSION = "12.0.0-next.8+437.sha-ec78e68";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
