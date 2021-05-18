57a7effc38035ae7973a49b21f5d6b44ace203c2
atscott <atscott01@gmail.com>
2021-05-18 15:56:04 +0000

    68619112e9 docs: update bazel migration doc link (#42142) PR Close #42142

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0+22.sha-edfcbb4";
+export declare const NGCC_VERSION = "12.0.0+26.sha-6861911";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
