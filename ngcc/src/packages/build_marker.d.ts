540be73856ccd65b7ff005f1b5af2f4a9f5e4b90
atscott <atscott01@gmail.com>
2021-05-18 21:24:00 +0000

    a20ef16d82 feat(dev-infra): publish major versions to "next" NPM dist tag (#42133)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0+32.sha-c58e15a";
+export declare const NGCC_VERSION = "12.0.0+34.sha-a20ef16";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
