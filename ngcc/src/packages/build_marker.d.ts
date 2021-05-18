e2cd306be438071346f30e028bc3dd5507e05fdd
Andrew Scott <atscott@google.com>
2021-05-18 18:38:34 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0+26.sha-6861911";
+export declare const NGCC_VERSION = "12.0.0+32.sha-c58e15a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
