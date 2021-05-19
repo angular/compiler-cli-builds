2938cbbe91914cbb017a2fe819b7c01d7a9e8abf
GitHub <noreply@github.com>
2021-05-19 18:31:37 +0000

    4259945128 release: cut the v12.1.0-next.2 release (#42178)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+442.sha-8d20bb9');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+445.sha-4259945');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.0.0-next.8+442.sha-8d20bb9') {
+        if (version === '12.0.0-next.8+445.sha-4259945') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.0.0-next.8+442.sha-8d20bb9';
+export const NGCC_VERSION = '12.0.0-next.8+445.sha-4259945';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
