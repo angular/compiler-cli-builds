34a48e27af4b4a3b784e11f9173ddd7ba8ed9759
atscott <atscott01@gmail.com>
2021-05-19 16:39:56 +0000

    d0958e8254 build(docs-infra): upgrade cli command docs sources to 9cbb4059f (#42172)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+438.sha-2dd9564";
+export declare const NGCC_VERSION = "12.0.0-next.8+439.sha-d0958e8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.0.0-next.8+438.sha-2dd9564';
+export const NGCC_VERSION = '12.0.0-next.8+439.sha-d0958e8';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
