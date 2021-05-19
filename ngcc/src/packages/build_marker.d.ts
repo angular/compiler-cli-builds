1529af234fcc6e7c3137c1912f40b0f4506d2d87
atscott <atscott01@gmail.com>
2021-05-19 18:39:11 +0000

    ef676bf946 build: reformat yml files (#42136)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+445.sha-4259945";
+export declare const NGCC_VERSION = "12.1.0-next.2+2.sha-ef676bf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
