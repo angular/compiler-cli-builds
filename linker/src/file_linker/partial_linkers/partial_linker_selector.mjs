15027eb510f6c03350c317cc4ff6bedeb52fb019
atscott <atscott01@gmail.com>
2021-05-19 16:38:33 +0000

    d0958e8254 build(docs-infra): upgrade cli command docs sources to 9cbb4059f (#42172)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+438.sha-2dd9564');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.0.0-next.8+439.sha-d0958e8');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.0.0-next.8+438.sha-2dd9564') {
+        if (version === '12.0.0-next.8+439.sha-d0958e8') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
