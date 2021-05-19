d762ab7265f15fece42214757a3abcea8fdb0288
atscott <atscott01@gmail.com>
2021-05-19 16:46:32 +0000

    8d20bb9855 ci: update to latest feature request triage bot commit (#42157)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.0-next.8+439.sha-d0958e8";
+export declare const NGCC_VERSION = "12.0.0-next.8+442.sha-8d20bb9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
