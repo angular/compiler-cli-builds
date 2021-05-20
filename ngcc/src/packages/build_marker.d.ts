6556d55c8640342c17e0396d2a52d6c1489e74a1
Zach Arend <zachzach@google.com>
2021-05-20 17:15:20 +0000

    640578fe16 docs: embed the devtools video in the corresponding guide (#42161)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.1+3.sha-390d24b";
+export declare const NGCC_VERSION = "12.0.1+4.sha-640578f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
