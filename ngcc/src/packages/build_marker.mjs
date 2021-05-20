555ae7128b36ef5ac55a8e455db469f179cca68c
Zach Arend <zachzach@google.com>
2021-05-20 17:44:09 +0000

    d6bc61ab5f build: update angular (#42090)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+6.sha-a01fe8d';
+export const NGCC_VERSION = '12.1.0-next.2+8.sha-d6bc61a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
