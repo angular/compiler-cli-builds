a967152b80db72b48cb4969522fe8a3502b6205a
Zach Arend <zachzach@google.com>
2021-05-20 17:45:37 +0000

    d6bc61ab5f build: update angular (#42090)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.2+6.sha-a01fe8d');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.2+8.sha-d6bc61a');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.1.0-next.2+6.sha-a01fe8d') {
+        if (version === '12.1.0-next.2+8.sha-d6bc61a') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
