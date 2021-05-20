1cbb7139488f05c6a1756feaef7ad91947b91708
Zach Arend <zachzach@google.com>
2021-05-20 16:30:30 +0000

    04ab5edf65 test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+2.sha-ef676bf";
+export declare const NGCC_VERSION = "12.1.0-next.2+3.sha-04ab5ed";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
