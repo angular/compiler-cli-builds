02596971f3b0a2b841d09c6f661785c398163285
Zach Arend <zachzach@google.com>
2021-05-21 18:25:57 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+14.sha-8c158ba';
+export const NGCC_VERSION = '12.1.0-next.2+15.sha-62becc8';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
