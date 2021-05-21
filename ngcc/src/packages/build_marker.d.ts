7ba6429eeda5149f2177f383947c335b18db0681
Zach Arend <zachzach@google.com>
2021-05-21 22:30:25 +0000

    4d56c10584 docs: move helpful alert regarding built-in directives to more logical place (#42226)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+18.sha-30c461d";
+export declare const NGCC_VERSION = "12.1.0-next.2+19.sha-4d56c10";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
