9a070ac890dae6f65e17a8eb95e28ff0359ab93b
Zach Arend <zachzach@google.com>
2021-05-25 20:49:08 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+68.sha-d7768c6";
+export declare const NGCC_VERSION = "12.1.0-next.2+69.sha-3a6af8e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
