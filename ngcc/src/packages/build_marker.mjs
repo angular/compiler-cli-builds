d557cc96effe69c5494c2ae67c66bfd70eaa0b33
Zach Arend <zachzach@google.com>
2021-05-25 22:32:55 +0000

    b0581f3117 docs: add link to the style precedence guide in ivy compatibility (#42317)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+73.sha-9f50495';
+export const NGCC_VERSION = '12.1.0-next.2+78.sha-b0581f3';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
