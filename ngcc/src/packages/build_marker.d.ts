9648aec657f24bcfc8cde2aaedcf40eab0ae2bcf
Zach Arend <zachzach@google.com>
2021-05-25 18:14:30 +0000

    2ff5bfe139 docs: fix documentation of guard execution order (#42288)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+60.sha-dc6bff1";
+export declare const NGCC_VERSION = "12.1.0-next.2+61.sha-2ff5bfe";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
