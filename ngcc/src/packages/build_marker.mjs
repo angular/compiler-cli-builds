d9a83786332b43043a64e293d217cebadf0fd7b4
Zach Arend <zachzach@google.com>
2021-05-25 18:20:19 +0000

    f74fd64523 fix(docs-infra): increase the opacity of background-color for inline `code` blocks (#42297)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+62.sha-ef33806';
+export const NGCC_VERSION = '12.1.0-next.2+65.sha-f74fd64';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
