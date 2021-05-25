c334278f72b6ebd8f67a5835c65b0d5b03ed84e9
Zach Arend <zachzach@google.com>
2021-05-25 20:49:24 +0000

    3d9062dad7 fix(forms): Add float number support for min and max validator (#42223)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+69.sha-3a6af8e';
+export const NGCC_VERSION = '12.1.0-next.2+70.sha-3d9062d';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
