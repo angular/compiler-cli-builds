7d62a15668735be7940423057781d720eb771895
Zach Arend <zachzach@google.com>
2021-05-25 03:03:47 +0000

    1c3fe69f92 docs: change Zeit -> Vercel (#42285)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+38.sha-a391698";
+export declare const NGCC_VERSION = "12.1.0-next.2+40.sha-1c3fe69";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
