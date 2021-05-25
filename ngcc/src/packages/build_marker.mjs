9c928ec208702a4615c02690fc425ac8ac8bca49
Zach Arend <zachzach@google.com>
2021-05-25 18:14:18 +0000

    29e5d509aa docs: clarify expectations for `ActivatedRoute` observables (#42316)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+56.sha-9f09c3b';
+export const NGCC_VERSION = '12.1.0-next.2+57.sha-29e5d50';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
