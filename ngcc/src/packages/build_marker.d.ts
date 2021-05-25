f0160a72608d2afa20318d15bc311237654518c2
Zach Arend <zachzach@google.com>
2021-05-25 22:32:04 +0000

    063d1789c2 docs: move "Browser Support" page in navigation tree (#42326)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+72.sha-bdd3cca";
+export declare const NGCC_VERSION = "12.1.0-next.2+71.sha-063d178";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
