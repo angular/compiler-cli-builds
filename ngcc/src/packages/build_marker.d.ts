72dc4b3099acab7329f1adf6becc0bd63a964146
Zach Arend <zachzach@google.com>
2021-05-25 22:36:00 +0000

    6da0c97423 docs: add note about redirects after absolute redirect (#42277)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+78.sha-b0581f3";
+export declare const NGCC_VERSION = "12.1.0-next.2+80.sha-6da0c97";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
