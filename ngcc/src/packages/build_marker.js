1bfc126ccc5ef94758b5d3810bc70d0091c800fb
Zach Arend <zachzach@google.com>
2021-05-25 18:15:32 +0000

    dc6bff10ac docs: clarify the definition of the bundle bundget defaults with strict mode (#42289)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.1.0-next.2+58.sha-d15a6a3';
+    exports.NGCC_VERSION = '12.1.0-next.2+60.sha-dc6bff1';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
