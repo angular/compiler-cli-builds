c50f59a8c66af41ccef4be51fc38298974ca39ef
Zach Arend <zachzach@google.com>
2021-05-25 22:32:28 +0000

    9f50495f28 docs: update date pipe api reference to indicate it is a pure pipe (#42320)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+77.sha-4a2cc3e';
+export const NGCC_VERSION = '12.1.0-next.2+73.sha-9f50495';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
