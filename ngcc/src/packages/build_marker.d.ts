f69a67f6911b2073ff3491ce58f51fd2d644f06f
Zach Arend <zachzach@google.com>
2021-05-25 22:40:13 +0000

    8228170919 docs: document shallow equality emit for ActivatedRoute (#42280)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.2+82.sha-850028d";
+export declare const NGCC_VERSION = "12.1.0-next.2+85.sha-8228170";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
