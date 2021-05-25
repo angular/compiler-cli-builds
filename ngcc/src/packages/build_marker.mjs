4977060f98c33d3cb1c81295686d10cb8e4cf519
Zach Arend <zachzach@google.com>
2021-05-25 22:37:58 +0000

    850028d000 docs: remove VS 2015 guide (#42253)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.2+84.sha-b7721aa';
+export const NGCC_VERSION = '12.1.0-next.2+82.sha-850028d';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
