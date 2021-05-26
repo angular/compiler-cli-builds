efa6f367e2e4a77219ff2ee4255b247c0d05adbb
Zach Arend <zachzach@google.com>
2021-05-26 20:24:47 +0000

    40bbcaa12d docs: move and update browser compatibility docs to browser-support guide (#42334)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+2.sha-0166391');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+5.sha-40bbcaa');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.1.0-next.3+2.sha-0166391') {
+        if (version === '12.1.0-next.3+5.sha-40bbcaa') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
