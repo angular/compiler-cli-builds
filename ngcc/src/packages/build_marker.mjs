0070e968bb6ff1517f09c56fc4eef72463a87750
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:43:05 +0000

    516923e3dc docs: clarify when the `ngDoBootstrap` hook is invoked (#42362)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+16.sha-1572659';
+export const NGCC_VERSION = '12.1.0-next.3+18.sha-516923e';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
