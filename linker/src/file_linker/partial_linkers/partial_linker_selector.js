72853e63197dffc3fbfd311521f49fd7a50bf4ba
Zach Arend <zachzach@google.com>
2021-05-26 20:26:12 +0000

    40bbcaa12d docs: move and update browser compatibility docs to browser-support guide (#42334)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+2.sha-0166391');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+5.sha-40bbcaa');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.1.0-next.3+2.sha-0166391') {
+            if (version === '12.1.0-next.3+5.sha-40bbcaa') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+2.sha-0166391';
+export const NGCC_VERSION = '12.1.0-next.3+5.sha-40bbcaa';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
