527d248d786749082e0363031d58aa4cb4132653
Zach Arend <zachzach@google.com>
2021-05-26 20:30:44 +0000

    33e1a18530 build(docs-infra): allow only Node.js v14 and above (#42093)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+7.sha-14abc68';
+export const NGCC_VERSION = '12.1.0-next.3+12.sha-33e1a18';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
