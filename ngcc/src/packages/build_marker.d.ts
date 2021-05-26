2299ab2007f98b4021fa17e605913ca58326dec0
Zach Arend <zachzach@google.com>
2021-05-26 20:32:14 +0000

    bd51762d6e docs: add section on binding to passive events (#42292)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+9.sha-de8a6ae";
+export declare const NGCC_VERSION = "12.1.0-next.3+10.sha-bd51762";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
