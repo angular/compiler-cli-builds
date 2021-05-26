6cd36e4941eaa7e0e027448d4e97a91e79466bb3
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:39:32 +0000

    4e9b61aaf9 docs: remove/update broken links in resources appearing on aio (#42232)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+13.sha-9193ea2";
+export declare const NGCC_VERSION = "12.1.0-next.3+14.sha-4e9b61a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+13.sha-9193ea2';
+export const NGCC_VERSION = '12.1.0-next.3+14.sha-4e9b61a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
