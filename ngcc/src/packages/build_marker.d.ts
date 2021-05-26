7a79b98615b6c55d01ae5701ece5b98b57a1851e
Zach Arend <zachzach@google.com>
2021-05-26 20:24:13 +0000

    0166391402 docs: edit project structure description (#42346)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+1.sha-74ebdf6";
+export declare const NGCC_VERSION = "12.1.0-next.3+2.sha-0166391";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
