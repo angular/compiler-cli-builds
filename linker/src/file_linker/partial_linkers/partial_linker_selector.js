910d07f99f8c95171a224a03d48e41aca05a6b73
Zach Arend <zachzach@google.com>
2021-05-26 20:32:09 +0000

    33e1a18530 build(docs-infra): allow only Node.js v14 and above (#42093)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+9.sha-de8a6ae');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.3+12.sha-33e1a18');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.1.0-next.3+9.sha-de8a6ae') {
+            if (version === '12.1.0-next.3+12.sha-33e1a18') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+9.sha-de8a6ae";
+export declare const NGCC_VERSION = "12.1.0-next.3+12.sha-33e1a18";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
