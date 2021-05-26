4d0afe0827c5dbcbc1f216c5089646aeb7647c80
Andrew Kushnir <akushnir@google.com>
2021-05-26 23:07:12 +0000

    75a33e5fa9 docs: remove lightbox gif from Use Directives to Watch the DOM section. (#42369) This gif is out of date and causing reader confusion.

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.2+21.sha-765854e";
+export declare const NGCC_VERSION = "12.0.2+23.sha-75a33e5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
