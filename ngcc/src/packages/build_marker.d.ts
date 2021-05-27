907c4654c2daec647517e792575539ca84ba5c37
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:34:11 +0000

    2513a533ed docs: add note to clarify that ngOnChanges is only called if inputs are bound (#42374)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.2+23.sha-75a33e5";
+export declare const NGCC_VERSION = "12.0.2+25.sha-2513a53";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
