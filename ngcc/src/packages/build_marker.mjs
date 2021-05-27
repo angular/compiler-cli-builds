50c3164e6b8cf17549e6810ab56ef109651f61b4
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:35:23 +0000

    141791e070 docs: add a note about the ngc command and command line options (#42373)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+23.sha-7a6358b';
+export const NGCC_VERSION = '12.1.0-next.3+22.sha-141791e';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
