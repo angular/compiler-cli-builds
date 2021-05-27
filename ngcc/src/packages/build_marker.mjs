ff5c3c7119ce28069833505622c43e0e35ee2890
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:14:02 +0000

    e87dbafba1 docs: fix a typo (#42384)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+25.sha-38378ce';
+export const NGCC_VERSION = '12.1.0-next.3+26.sha-e87dbaf';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
