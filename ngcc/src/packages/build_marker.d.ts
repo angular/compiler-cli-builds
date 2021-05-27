01ff71cadadc4c7a7b1f789f5188b1064d7319e3
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:53:25 +0000

    d33fdea1e3 docs: clarify behavior of strict templates flags (#42392)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+39.sha-fa0144f";
+export declare const NGCC_VERSION = "12.1.0-next.3+41.sha-d33fdea";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
