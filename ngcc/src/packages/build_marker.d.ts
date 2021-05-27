cfa4fa89eea50a77b2c9ef9856efb96d137bba7b
Andrew Kushnir <akushnir@google.com>
2021-05-27 18:43:37 +0000

    a86a6b95c5 feat(docs-infra): implement dark mode (#41129)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.2+25.sha-2513a53";
+export declare const NGCC_VERSION = "12.0.2+39.sha-a86a6b9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
