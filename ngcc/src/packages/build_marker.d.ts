aa994bfaab144ad30bdaa4418b28ddfb056b6f35
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:22:01 +0000

    62b5a6cb07 docs: add docs for DebugElement (#42352)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+46.sha-4cd2cc4";
+export declare const NGCC_VERSION = "12.1.0-next.3+48.sha-62b5a6c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
