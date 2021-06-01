6a1d6c5bef5f8485b1da3bc617097fe3e5a6c6c7
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:30:52 +0000

    e0381a87c9 docs: update CLI builder to be platform agnostic (#42371)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+49.sha-e798bfa";
+export declare const NGCC_VERSION = "12.1.0-next.3+52.sha-e0381a8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
