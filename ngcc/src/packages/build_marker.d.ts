a2cbf02819c5c289a727c7411252e1a356cab116
Andrew Kushnir <akushnir@google.com>
2021-06-01 23:54:02 +0000

    574f64041e docs: specify correct interceptor class type (#42437)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+65.sha-6a2e3e2";
+export declare const NGCC_VERSION = "12.1.0-next.3+66.sha-574f640";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+65.sha-6a2e3e2';
+export const NGCC_VERSION = '12.1.0-next.3+66.sha-574f640';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
