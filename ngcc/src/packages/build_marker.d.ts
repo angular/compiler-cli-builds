01e0e1ff6bd860f297bbe7aec71c92cef7a25555
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:32:13 +0000

    e798bfabb0 build(docs-infra): fix linting warnings (#42359)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.3+48.sha-62b5a6c";
+export declare const NGCC_VERSION = "12.1.0-next.3+49.sha-e798bfa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
