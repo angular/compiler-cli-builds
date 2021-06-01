6bd00345e4950b3116184f1a869d1b2c431ba502
Andrew Kushnir <akushnir@google.com>
2021-06-01 21:31:33 +0000

    6a2e3e2b2c docs: change Navigation from type to interface (#42281)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.3+63.sha-5babdb2';
+export const NGCC_VERSION = '12.1.0-next.3+65.sha-6a2e3e2';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
