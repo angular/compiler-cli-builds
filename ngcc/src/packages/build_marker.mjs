21f1681f366298db10295bcb83b439e8714a9ec5
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:34:51 +0000

    4c39869566 docs: add a note about javascript mime-types to the deployment guide (#42339)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+4.sha-f3f1014';
+export const NGCC_VERSION = '12.1.0-next.4+5.sha-4c39869';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
