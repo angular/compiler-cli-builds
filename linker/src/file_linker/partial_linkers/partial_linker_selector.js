fb91999e1402b7960747ecf6c3263c6399f9d089
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:07:46 +0000

    0a1d8e5102 refactor(docs-infra): clean up AIO Sass files (#42442)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.0.3');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.0.3+11.sha-0a1d8e5');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.0.3') {
+            if (version === '12.0.3+11.sha-0a1d8e5') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
