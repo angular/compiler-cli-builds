bfbfdc79566a5ed73d0ccd037be8e04ee14d335a
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:37:58 +0000

    1fe1dd1f19 docs: clarify the wording around live examples for all testing guides (#42406)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+5.sha-4c39869';
+export const NGCC_VERSION = '12.1.0-next.4+6.sha-1fe1dd1';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
