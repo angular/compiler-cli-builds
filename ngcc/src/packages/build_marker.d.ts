66f5d51f9390eefe713a54fcd6377afc71a6438c
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:44:38 +0000

    0ac193c86c docs: fix typo in the Router guide (#42455) PR Close #42455

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.4+8.sha-c0b2eeb";
+export declare const NGCC_VERSION = "12.1.0-next.4+13.sha-0ac193c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+8.sha-c0b2eeb';
+export const NGCC_VERSION = '12.1.0-next.4+13.sha-0ac193c';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
