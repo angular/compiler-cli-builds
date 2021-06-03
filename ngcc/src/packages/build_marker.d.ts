722ad34f32409f092d2371fbd50f3a3021bb9e85
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:29:11 +0000

    374fa2c26f fix(common): initialize currencyCode in currencyPipe (#40505)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.4+14.sha-3d1c905";
+export declare const NGCC_VERSION = "12.1.0-next.4+23.sha-374fa2c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
