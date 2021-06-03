afb4b9540a2c1c7e1461eb2b0235309964378ec6
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 21:03:20 +0000

    a6dec05e82 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+22.sha-50c87e8";
+export declare const NGCC_VERSION = "12.0.3+26.sha-a6dec05";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
