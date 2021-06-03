98f69cdf712a17d108a69d6712174d21c4d8acdd
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:51:11 +0000

    751cd83ae3 fix(forms): the `min` and `max` validators should work correctly with `0` as a value (#42412)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+23.sha-374fa2c';
+export const NGCC_VERSION = '12.1.0-next.4+28.sha-751cd83';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
