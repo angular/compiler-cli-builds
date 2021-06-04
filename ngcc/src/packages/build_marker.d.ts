655faf0d95795f9769a128fce441588e3a4f4643
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:51:55 +0000

    6080db1839 build: suppress errors in git hooks (#42484)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+31.sha-beae425";
+export declare const NGCC_VERSION = "12.0.3+33.sha-6080db1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
