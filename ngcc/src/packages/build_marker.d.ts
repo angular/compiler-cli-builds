231075bff9f2a7868ee1ad264ebec73af5749900
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 17:20:23 +0000

    f5d81d3c80 docs(core): clarify deprecation of `entryComponents` (#42248)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+37.sha-770883f";
+export declare const NGCC_VERSION = "12.0.3+38.sha-f5d81d3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
