4602571676c7a19bdbe1ca5ae094c7b5615f5d14
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:50:50 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+47.sha-3005917';
+export const NGCC_VERSION = '12.1.0-next.4+48.sha-cd22c7a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
