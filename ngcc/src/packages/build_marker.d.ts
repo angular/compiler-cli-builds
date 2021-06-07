821a2b6e67874c45209dbba900d4e7ec95ea9f74
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:12:22 +0000

    65d59305d5 build: remove unused no-strict tsconfig file (#42506)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+38.sha-f5d81d3";
+export declare const NGCC_VERSION = "12.0.3+41.sha-65d5930";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
