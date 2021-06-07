0ec976cd7004e09d57c4c4a9eb8ac21215bae238
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:55:20 +0000

    770883ff4a fix(docs-infra): fix search results font color (#42488)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+34.sha-ad9dd2a";
+export declare const NGCC_VERSION = "12.0.3+37.sha-770883f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
