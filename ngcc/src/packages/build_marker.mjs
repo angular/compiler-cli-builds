9742ba3752c5896125190977839c5551ef08dd60
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:27:27 +0000

    66f49c2497 Revert "refactor: remove checked-in locale files" (#42521)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+64.sha-f1d58ef';
+export const NGCC_VERSION = '12.1.0-next.4+70.sha-66f49c2';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
