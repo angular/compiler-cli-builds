e80d7e501304efc42e6d1e748b12e1c5b8bb66e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 01:43:38 +0000

    f1d58efdb1 docs: Edits to remove jargon in Reference Observables, Dependency Injection (#42185)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.4+63.sha-b762820";
+export declare const NGCC_VERSION = "12.1.0-next.4+64.sha-f1d58ef";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
