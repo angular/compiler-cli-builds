fe7d07ec484ed2582b399c34d5a06a737ae96b70
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:06:56 +0000

    43b4a8c07c docs: Edited to remove jargon in Reference Errors (#42186)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+86.sha-f3fde4b';
+export const NGCC_VERSION = '12.1.0-next.4+85.sha-43b4a8c';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
