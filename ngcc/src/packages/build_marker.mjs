be24adcc74903d18fe1edda8c15993332420d90f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:02:07 +0000

    d10c38a8f8 docs: add Roman Sedov to contributors page (#42515)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+73.sha-645cad5';
+export const NGCC_VERSION = '12.1.0-next.4+72.sha-d10c38a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
