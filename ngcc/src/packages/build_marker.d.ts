117e2cd7257d39aa66b51d02207cd7018c28b5bb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:01:59 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.4+82.sha-386550c";
+export declare const NGCC_VERSION = "12.1.0-next.4+84.sha-5c0541f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
