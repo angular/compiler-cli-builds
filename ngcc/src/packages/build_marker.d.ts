2e7865c344e4b742d2e413e8f212ecc0e6e72921
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:10 +0000

    4cf442c879 docs: add links to examples for ComponentFactoryResolver (#42496)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.4+76.sha-c44ab4f";
+export declare const NGCC_VERSION = "12.1.0-next.4+74.sha-4cf442c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+76.sha-c44ab4f';
+export const NGCC_VERSION = '12.1.0-next.4+74.sha-4cf442c';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
