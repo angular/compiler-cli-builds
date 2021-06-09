e6d6a688e429874fea430516b3968cec138b3993
GitHub <noreply@github.com>
2021-06-09 18:48:58 +0000

    28a4631b63 release: cut the v12.1.0-next.5 release (#42534)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.1.0-next.4+96.sha-7b5e2fe';
+    exports.NGCC_VERSION = '12.1.0-next.4+97.sha-28a4631';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
