ce469d7b0ec02905a5e97e83edba729b915a3029
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:10:38 +0000

    3abf365fdc docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+63.sha-5f6492e";
+export declare const NGCC_VERSION = "12.0.3+64.sha-3abf365";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
