e681e90491028681b3096150193f75821fcb3077
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 17:03:56 +0000

    6203f5e3a7 docs: clarify onSameUrlNavigation behavior (#42275)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.3+64.sha-3abf365";
+export declare const NGCC_VERSION = "12.0.3+65.sha-6203f5e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
