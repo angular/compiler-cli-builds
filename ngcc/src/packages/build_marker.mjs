213a1b8bed3560c2b65267081c6f2a671a32b8a3
GitHub <noreply@github.com>
2021-06-09 18:28:32 +0000

    7b5e2fef98 docs: release notes for the v12.0.4 release (#42532)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.4+95.sha-536c373';
+export const NGCC_VERSION = '12.1.0-next.4+96.sha-7b5e2fe';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
