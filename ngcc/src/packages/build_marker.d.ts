6c8cecdbc5a7366fd9db3767bc3f93bb02dfbaef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:22:02 +0000

    ec90d8d5e5 docs: change field name from id to $id in schema.json file (#42528)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+9.sha-cddb3a8";
+export declare const NGCC_VERSION = "12.1.0-next.5+8.sha-ec90d8d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
