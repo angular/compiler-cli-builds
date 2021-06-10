abb83ee985aa59b991ba62c389d1e2160bd2cdc0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:45:41 +0000

    299f92c3b6 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.5+4.sha-665b986';
+export const NGCC_VERSION = '12.1.0-next.5+5.sha-299f92c';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
