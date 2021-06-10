89129cee09a0d7d2e410571da9088ecc4ff90a45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:22:20 +0000

    5e4a886259 docs: fix i18n guide typo (#42545)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+8.sha-ec90d8d";
+export declare const NGCC_VERSION = "12.1.0-next.5+10.sha-5e4a886";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
