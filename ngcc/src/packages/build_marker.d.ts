35f47ed5736b71f2e1c4e62c2467d8e87aa3441d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:42:21 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.4+4.sha-a75874c";
+export declare const NGCC_VERSION = "12.0.4+5.sha-e0158a1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
