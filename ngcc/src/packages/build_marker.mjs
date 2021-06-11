00c545202863ff5aa3a909fe2c7912b672ad3356
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 15:18:55 +0000

    0c13e2bf97 build(packaging): remove rehype (#42557)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.5+12.sha-efb440e';
+export const NGCC_VERSION = '12.1.0-next.5+13.sha-0c13e2b';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
