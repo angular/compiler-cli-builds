715f663c45f49472233f3baa5adaf39eee522d79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 15:20:27 +0000

    0c13e2bf97 build(packaging): remove rehype (#42557)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.5+12.sha-efb440e');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.5+13.sha-0c13e2b');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.1.0-next.5+12.sha-efb440e') {
+        if (version === '12.1.0-next.5+13.sha-0c13e2b') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
