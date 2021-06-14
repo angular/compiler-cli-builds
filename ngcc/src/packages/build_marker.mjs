dffaaa28a12e103cd75967aef5a815bcea1a6a18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:20:45 +0000

    044e0229bd build: generate alias locale data for closure locale (#42230)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.5+19.sha-56a0582';
+export const NGCC_VERSION = '12.1.0-next.5+28.sha-044e022';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
