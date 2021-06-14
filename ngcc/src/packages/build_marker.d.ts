1e40911b69ff89c2d25bff2387ca10fb49784b9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:31:25 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+30.sha-228beea";
+export declare const NGCC_VERSION = "12.1.0-next.5+31.sha-4001e9d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
