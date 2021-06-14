ab7d7210bd47293a1d047dd4583f52ca8217d3f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:33 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+21.sha-e7c69b8";
+export declare const NGCC_VERSION = "12.1.0-next.5+19.sha-56a0582";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
