d1d4894635b094056355e7fb669dbde6a5826732
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 21:21:25 +0000

    6bf4cb6d44 refactor(language-service): add missing helper method (#42579)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.0.4+21.sha-11e0f53');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.0.4+25.sha-6bf4cb6');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.0.4+21.sha-11e0f53') {
+            if (version === '12.0.4+25.sha-6bf4cb6') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.4+21.sha-11e0f53";
+export declare const NGCC_VERSION = "12.0.4+25.sha-6bf4cb6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
