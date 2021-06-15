04d3c65483a02655e6c8a48d00c585ce131ab1c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:10:36 +0000

    11e0f53352 fix(language-service): Use last child end span for parent without close tag (#42554)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.4+19.sha-18d8322";
+export declare const NGCC_VERSION = "12.0.4+21.sha-11e0f53";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
