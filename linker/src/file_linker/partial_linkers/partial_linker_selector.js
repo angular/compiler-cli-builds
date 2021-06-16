3eeedce139f01912749e470f0469ed6151776e27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:47 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.5+40.sha-2d1347b');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.5+41.sha-62aca30');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.1.0-next.5+40.sha-2d1347b') {
+            if (version === '12.1.0-next.5+41.sha-62aca30') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
