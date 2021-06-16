a72a217c64d0320c7d5ff99479deabefb1d4be03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:23:48 +0000

    7b85a4d457 fix(docs-infra): add horizontal margins to nav menu (#42561)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+43.sha-3b81528";
+export declare const NGCC_VERSION = "12.1.0-next.5+46.sha-7b85a4d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
