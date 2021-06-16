cd8684d8d44206c57097b0fd85a1dc8107b0a8ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:53:52 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.5+49.sha-7961e2f';
+export const NGCC_VERSION = '12.1.0-next.5+50.sha-0e33521';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
