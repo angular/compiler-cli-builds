34a78624b6a5a5350cdef609e40370d5a2953b2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:03:00 +0000

    7db8e1f82e release: cut the v12.0.5 release (#42585)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.0.4+32.sha-f9a5d25');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.0.4+33.sha-7db8e1f');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.0.4+32.sha-f9a5d25') {
+            if (version === '12.0.4+33.sha-7db8e1f') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
