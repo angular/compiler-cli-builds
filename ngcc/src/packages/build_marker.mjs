50a38492b901d196f26b60048ccb49d0a6616382
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:24:53 +0000

    eb04684ed3 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.5+46.sha-7b85a4d';
+export const NGCC_VERSION = '12.1.0-next.5+47.sha-eb04684';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
