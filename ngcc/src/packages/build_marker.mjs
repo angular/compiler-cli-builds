7d9579c3e2fce5bcd56275f4d388a8106182c886
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 23:21:33 +0000

    983c540191 docs: fix pipe params (#42593)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.6+7.sha-b037df2';
+export const NGCC_VERSION = '12.1.0-next.6+8.sha-983c540';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
