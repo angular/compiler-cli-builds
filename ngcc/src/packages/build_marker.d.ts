8bcd63e41e5319ec3d1c9e5b81ab4ac216f0045a
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:27:22 +0000

    0af9b89b86 build: update angular to v12.0.5 (#42588)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.6+1.sha-873229f";
+export declare const NGCC_VERSION = "12.1.0-next.6+2.sha-0af9b89";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
