ed0e9bee5701128bf41f3a2b357d52b1b0866f44
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:27:39 +0000

    166e98a594 build: update all non-major dependencies (#42544)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.6+2.sha-0af9b89';
+export const NGCC_VERSION = '12.1.0-next.6+3.sha-166e98a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
