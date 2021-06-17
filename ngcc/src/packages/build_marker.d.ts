5db58e27763852787f023394a6996c7de534f5ac
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:34:21 +0000

    b037df28fd Revert "fix(docs-infra): do not redirect disambiguated URLs" (#42414)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.6+4.sha-07c1ddc";
+export declare const NGCC_VERSION = "12.1.0-next.6+7.sha-b037df2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
