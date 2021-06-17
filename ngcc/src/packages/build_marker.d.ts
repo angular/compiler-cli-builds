915f2d1618ed4ba2e0ca44bca75556491ded680e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-17 00:04:39 +0000

    f8e17c83e9 release: cut the v12.1.0-next.6 release (#42587)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.5+50.sha-0e33521";
+export declare const NGCC_VERSION = "12.1.0-next.5+51.sha-f8e17c8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
