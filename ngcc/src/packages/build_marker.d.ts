3680a18e1c122905529f2ad7714dcf78e5b64ea4
Dylan Hunn <dylhunn@gmail.com>
2021-06-18 17:52:27 +0000

    44291889ee docs(docs-infra): document how to configure redirects (#42452)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.6+13.sha-0c105c3";
+export declare const NGCC_VERSION = "12.1.0-next.6+23.sha-4429188";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
