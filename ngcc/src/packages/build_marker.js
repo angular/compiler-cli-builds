97bad7dd70d7aeb404771222bb94f0f37207e0d6
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:03:42 +0000

    d77f560403 build: update to typescript 4.3.4 (#42600)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.1.0-next.6+23.sha-4429188';
+    exports.NGCC_VERSION = '12.1.0-next.6+25.sha-d77f560';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
