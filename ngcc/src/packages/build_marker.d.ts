dd83b875b47a5a3a96bf5e6fb1dec000ebc73940
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:58:06 +0000

    2dc7b45275 fix(compiler): should not break a text token on a non-valid start tag (#42605)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.5+28.sha-1ce5c20";
+export declare const NGCC_VERSION = "12.0.5+34.sha-2dc7b45";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
