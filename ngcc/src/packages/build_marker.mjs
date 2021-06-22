dc6a7207ac6dbfa93994b84349cef93914ccf8b5
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 00:00:57 +0000

    cc672f05bf feat(compiler): add support for shorthand property declarations in templates (#42421)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.6+33.sha-6b2a475';
+export const NGCC_VERSION = '12.1.0-next.6+35.sha-cc672f0';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
