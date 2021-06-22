6f4d84799512b22bc30e5081f145e0b45126b58e
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:47:02 +0000

    75bbcf7c2f docs: improve styles of the roadmap done section (#42616)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0-next.6+35.sha-cc672f0";
+export declare const NGCC_VERSION = "12.1.0-next.6+36.sha-75bbcf7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
