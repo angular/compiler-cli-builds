34fc6b6ec4a1b3eaeac8e128a3a5023200781f45
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 17:01:19 +0000

    9de65dbdce fix(compiler): should not break a text token on a non-valid start tag (#42605)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.6+39.sha-8a67770';
+export const NGCC_VERSION = '12.1.0-next.6+41.sha-9de65db';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
