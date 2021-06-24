ca2ef5468412e0e526454d29489fbeba2a706786
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 15:47:12 +0000

    8da6f66f7b ci: replace RBE instance name for components-repo-unit-tests job (#42636)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0-next.6+41.sha-9de65db';
+export const NGCC_VERSION = '12.1.0-next.6+45.sha-8da6f66';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
