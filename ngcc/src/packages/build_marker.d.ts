d3f08f8779953c5145b9298442eed9dc8ece2a27
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 16:49:24 +0000

    4459741d9b fix(compiler-cli): change default ngcc  hash algorithm to be FIPS compliant (#42582)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.0.5+34.sha-2dc7b45";
+export declare const NGCC_VERSION = "12.0.5+39.sha-4459741";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
