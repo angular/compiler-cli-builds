bf5b6e822f47b63d326c1ac68f7cac5a67951c18
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 16:40:57 +0000

    874de59d35 fix(compiler-cli): change default ngcc  hash algorithm to be FIPS compliant (#42582)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.6+45.sha-8da6f66');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.1.0-next.6+48.sha-874de59');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.1.0-next.6+45.sha-8da6f66') {
+        if (version === '12.1.0-next.6+48.sha-874de59') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
