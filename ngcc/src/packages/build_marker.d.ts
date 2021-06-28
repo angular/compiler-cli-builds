55e2ead9ba5ea2d1904409d16bd4603f7f2d2b1b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:09:11 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.0+11.sha-1142b51";
+export declare const NGCC_VERSION = "12.2.0-next.0+12.sha-c93df7d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
