8a01dd770e188abe6144e99b5766f44e355c01df
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:09:53 +0000

    0f27f89d38 feat(dev-infra): add command for printing release train information (#42644)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0+11.sha-a1e036c';
+export const NGCC_VERSION = '12.1.0+12.sha-0f27f89';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
