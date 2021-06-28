3cf4b1eee0fe681d92b69bec26c128b61fd40b56
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:02:40 +0000

    a1e036cfd1 docs: correct literal string example (#42623)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0+2.sha-f6b828e';
+export const NGCC_VERSION = '12.1.0+11.sha-a1e036c';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
