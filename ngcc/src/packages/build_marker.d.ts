cc58164855078152476483612ab5eaf6a26eb030
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:52:23 +0000

    4bea630baa docs(core): add context jsdoc param to createEmbeddedView (#42675) PR Close #42675

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.0+9.sha-2a260dc";
+export declare const NGCC_VERSION = "12.2.0-next.0+10.sha-4bea630";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
