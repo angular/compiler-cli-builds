d6e613789c12352c852a120cba7263411539c1b5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:51:02 +0000

    56fb4eb08d docs(compiler): interally typo fixed (#42676)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.0+3.sha-37a740c');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.0+4.sha-56fb4eb');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.0+3.sha-37a740c') {
+            if (version === '12.2.0-next.0+4.sha-56fb4eb') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
