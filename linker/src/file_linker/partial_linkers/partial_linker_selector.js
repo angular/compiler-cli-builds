55d2dbbb964b8f5b86c1050bef387e83160862cb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:37:09 +0000

    8528073f6c fix(dev-infra): remove bots from special thanks section (#42697)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.0+19.sha-f29fe5c');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.0+20.sha-8528073');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.0+19.sha-f29fe5c') {
+            if (version === '12.2.0-next.0+20.sha-8528073') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
