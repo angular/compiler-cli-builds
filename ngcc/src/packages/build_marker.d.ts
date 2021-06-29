74b49d4c06b072898f086af22dd04d2a1d015349
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:38:38 +0000

    8528073f6c fix(dev-infra): remove bots from special thanks section (#42697)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.0+19.sha-f29fe5c";
+export declare const NGCC_VERSION = "12.2.0-next.0+20.sha-8528073";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
