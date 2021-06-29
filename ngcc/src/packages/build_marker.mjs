5db10ab2ee856ed931caaf2ec654c98c37f20ef7
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:33:30 +0000

    f84d826a3a fix(dev-infra): remove bots from special thanks section (#42697)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0+15.sha-78cf10b';
+export const NGCC_VERSION = '12.1.0+16.sha-f84d826';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
