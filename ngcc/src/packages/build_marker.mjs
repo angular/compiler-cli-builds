e0e51bc62a1d65b0acbc270f08f99a0eaa4f9715
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:51:20 +0000

    02742552f3 refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.0+15.sha-2c490d5';
+export const NGCC_VERSION = '12.2.0-next.0+17.sha-0274255';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
