1a0833f7c1630c16f1fca3355d984dc5ca8c731c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:55:36 +0000

    234b5edcc7 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.0+21.sha-f3965ff';
+export const NGCC_VERSION = '12.2.0-next.0+22.sha-234b5ed';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
