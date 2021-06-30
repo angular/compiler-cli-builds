14e1d3a024e8f833a92723f79c53527f0538a3ec
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 21:51:05 +0000

    41c6877c01 fix(core): error in TestBed if module is reset mid-compilation in ViewEngine (#42669)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.0+23.sha-12e0cfa";
+export declare const NGCC_VERSION = "12.1.1+1.sha-41c6877";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.0+23.sha-12e0cfa';
+export const NGCC_VERSION = '12.1.1+1.sha-41c6877';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
