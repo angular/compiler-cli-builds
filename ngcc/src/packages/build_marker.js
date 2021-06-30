baa38ea1f09fa8a46c5c19c1bd68ae7cdf4dfc4f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:54:01 +0000

    d19ddd1a87 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.1.0+17.sha-c380d56';
+    exports.NGCC_VERSION = '12.1.0+18.sha-d19ddd1';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
