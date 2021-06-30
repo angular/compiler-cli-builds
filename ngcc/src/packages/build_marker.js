8c23d7ab7d1a88098709bd0309b0a664d1b99f2b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:03:41 +0000

    0e20b05f7d docs: add last updated date to roadmap (#42615)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.0-next.0+23.sha-9f5cc7c';
+    exports.NGCC_VERSION = '12.2.0-next.0+24.sha-0e20b05';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
