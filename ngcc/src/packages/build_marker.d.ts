576c3826cf8dd88ac9d5fdc640040b6466b89327
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:53:06 +0000

    d997303cf4 build: update dependency husky to v7 (#42730)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+1.sha-0f23f73";
+export declare const NGCC_VERSION = "12.2.0-next.1+2.sha-d997303";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
