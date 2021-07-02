f6a1f32839279a7e9bd16cea179473baa45b4262
atscott <atscott01@gmail.com>
2021-07-02 17:09:19 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+7.sha-19bcdfb";
+export declare const NGCC_VERSION = "12.2.0-next.1+9.sha-e81efe6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+7.sha-19bcdfb';
+export const NGCC_VERSION = '12.2.0-next.1+9.sha-e81efe6';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
