624e11dec3b4fd5afcb93fa7917738f668fcfa3e
atscott <atscott01@gmail.com>
2021-07-02 17:08:08 +0000

    19bcdfb028 fix(docs-infra): correctly configure top-menu nav-item in the sidenav (#42747)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+5.sha-ffeea63';
+export const NGCC_VERSION = '12.2.0-next.1+7.sha-19bcdfb';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
