c66e789763ac1d5bc6a42275376b887535959174
atscott <atscott01@gmail.com>
2021-07-02 17:27:56 +0000

    e99af184ab build: update API goldens after api-extractor update (#42737)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+9.sha-e81efe6';
+export const NGCC_VERSION = '12.2.0-next.1+12.sha-e99af18';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
