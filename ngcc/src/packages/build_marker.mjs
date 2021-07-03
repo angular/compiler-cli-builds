1d24bf3da39dce0a1c1d5ada3dfd2ff871e49c49
atscott <atscott01@gmail.com>
2021-07-03 00:35:49 +0000

    c9b47f2182 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+15.sha-4c78984';
+export const NGCC_VERSION = '12.2.0-next.1+16.sha-c9b47f2';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
