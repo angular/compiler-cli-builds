e26c90d3c899c8f4d9519aeef9b40bca871d3ed6
Andrew Scott <atscott@google.com>
2021-07-07 20:56:26 +0000

    8b95816fc4 fix(docs-infra): log more SW debug info (#42776)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+19.sha-24ac378';
+export const NGCC_VERSION = '12.2.0-next.1+24.sha-8b95816';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
