5f89dda1d921c74068c08e461592a706c568a7ed
Andrew Scott <atscott@google.com>
2021-07-07 20:59:29 +0000

    95ba5b4edb fix(docs-infra): ensure the sidenav backdrop covers the floating ToC (#42787)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.mjs
@@ -53,7 +53,7 @@ export const declarationFunctions = [
  */
 export function createLinkerMap(environment, sourceUrl, code) {
     const linkers = new Map();
-    const LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+24.sha-8b95816');
+    const LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+25.sha-95ba5b4');
     linkers.set(ɵɵngDeclareDirective, [
         { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) },
     ]);
@@ -122,7 +122,7 @@ export class PartialLinkerSelector {
             throw new Error(`Unknown partial declaration function ${functionName}.`);
         }
         const linkerRanges = this.linkers.get(functionName);
-        if (version === '12.2.0-next.1+24.sha-8b95816') {
+        if (version === '12.2.0-next.1+25.sha-95ba5b4') {
             // Special case if the `version` is the same as the current compiler version.
             // This helps with compliance tests where the version placeholders have not been replaced.
             return linkerRanges[linkerRanges.length - 1].linker;
