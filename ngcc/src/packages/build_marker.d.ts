86a0fca94ec363811fd2bad02538d6ef74e5d5ac
atscott <atscott01@gmail.com>
2021-07-07 22:37:39 +0000

    30c82cd177 fix(compiler-cli): inline type checking instructions no longer prevent incremental reuse (#42759)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+25.sha-95ba5b4";
+export declare const NGCC_VERSION = "12.2.0-next.1+26.sha-30c82cd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
