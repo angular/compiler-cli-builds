9723a8e577a51f7bc578a01f1b6ac3a80552b8b4
atscott <atscott01@gmail.com>
2021-07-08 17:51:25 +0000

    cb2ca9a66e fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+29.sha-ad08b17';
+export const NGCC_VERSION = '12.2.0-next.1+39.sha-cb2ca9a';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
