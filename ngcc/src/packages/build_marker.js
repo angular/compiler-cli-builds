ba6e5c40f88d3c543b82f248369c0c682c6e03f8
atscott <atscott01@gmail.com>
2021-07-08 22:11:49 +0000

    e7832a6b2f docs(core): fix spelling error (#42783)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.0-next.1+42.sha-c30502e';
+    exports.NGCC_VERSION = '12.2.0-next.1+43.sha-e7832a6';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
