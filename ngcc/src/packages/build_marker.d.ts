27b947feaa1f97749130a1ce6ef95c64462e112d
atscott <atscott01@gmail.com>
2021-07-08 17:47:15 +0000

    ad08b17b05 docs(forms): add diff between add and set control (#42638)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+26.sha-30c82cd";
+export declare const NGCC_VERSION = "12.2.0-next.1+29.sha-ad08b17";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+26.sha-30c82cd';
+export const NGCC_VERSION = '12.2.0-next.1+29.sha-ad08b17';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
