ced5b71e87f69af2ca3d19f306d1c5f37e7a7901
atscott <atscott01@gmail.com>
2021-07-08 22:10:18 +0000

    e7832a6b2f docs(core): fix spelling error (#42783)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+42.sha-c30502e";
+export declare const NGCC_VERSION = "12.2.0-next.1+43.sha-e7832a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+42.sha-c30502e';
+export const NGCC_VERSION = '12.2.0-next.1+43.sha-e7832a6';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
