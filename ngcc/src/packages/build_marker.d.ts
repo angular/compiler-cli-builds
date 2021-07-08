cdcbe9d4e54f7c4b35c67bc6e1689e36c91fad6e
atscott <atscott01@gmail.com>
2021-07-08 21:27:12 +0000

    f6bce3158a ci: use issue forms for issue creation (#42099)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+39.sha-d879175";
+export declare const NGCC_VERSION = "12.1.1+41.sha-f6bce31";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.1.1+39.sha-d879175';
+export const NGCC_VERSION = '12.1.1+41.sha-f6bce31';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
