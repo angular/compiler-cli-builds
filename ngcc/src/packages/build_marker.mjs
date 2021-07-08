f1e13ba769cbc3100dd79e08c3be7b880c14a121
atscott <atscott01@gmail.com>
2021-07-08 22:12:39 +0000

    89084ffcf9 docs(core): initializeApp method should return function (#42743)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+43.sha-e7832a6';
+export const NGCC_VERSION = '12.2.0-next.1+44.sha-89084ff';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
