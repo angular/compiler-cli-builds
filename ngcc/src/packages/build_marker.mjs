f3dc1950ddd9ac847dde1ad69d1803a4111fcf19
atscott <atscott01@gmail.com>
2021-07-08 22:18:35 +0000

    dfe9e1d077 build: lock file maintenance (#42770)

--- ngcc/src/packages/build_marker.mjs
+++ ngcc/src/packages/build_marker.mjs
@@ -1,5 +1,5 @@
 import { NGCC_PROPERTY_EXTENSION } from '../writing/new_entry_point_file_writer';
-export const NGCC_VERSION = '12.2.0-next.1+45.sha-1f3747b';
+export const NGCC_VERSION = '12.2.0-next.1+46.sha-dfe9e1d';
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
