536b3040d7398246d4cf8104347757394883ffb4
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:45:05 +0000

    c1c1cda866 Revert "refactor(dev-infra): add spawnSync to child process utils, normalize naming of child-process utils (#42394)" (#42829)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+91.sha-9456eca";
+export declare const NGCC_VERSION = "12.2.0-next.1+98.sha-c1c1cda";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
