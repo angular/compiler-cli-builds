a791de063717fbfcdfd07d88be4a2b484065e2d8
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:09:38 +0000

    40da386a4d docs(core): fix `ViewChildren` code examples to avoid TS error (#42816)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+61.sha-4e73b88";
+export declare const NGCC_VERSION = "12.2.0-next.1+62.sha-40da386";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
