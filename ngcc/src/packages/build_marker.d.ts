8cf9c7250cd386006f1fcf0029d281082116b55e
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:46:25 +0000

    88b15d572f docs: clarify voting phase for feature requests (#42663)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+98.sha-c1c1cda";
+export declare const NGCC_VERSION = "12.2.0-next.1+99.sha-88b15d5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
