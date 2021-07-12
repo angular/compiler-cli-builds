daa5bbf38a969328b9b112f8a03ba45a9b27d5e0
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:23:39 +0000

    91a576db20 refactor(core): optimize the implementation about finding context from error in ErrorHandler (#42581)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+55.sha-2ed5a2d";
+export declare const NGCC_VERSION = "12.1.1+60.sha-91a576d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
