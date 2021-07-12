0e8aecc607e0f2d597f2a67d9a741d9c89764558
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:33:53 +0000

    b5ab7aff43 refactor: add override keyword to members implementing abstract declarations (#42512)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+67.sha-e8be045');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+89.sha-b5ab7af');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.1+67.sha-e8be045') {
+            if (version === '12.2.0-next.1+89.sha-b5ab7af') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
