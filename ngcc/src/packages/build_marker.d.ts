2ecaaebcdb8224e17d3836c49cb556c6b7569a30
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:58:52 +0000

    e42aa6c13b fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+100.sha-81dce5c";
+export declare const NGCC_VERSION = "12.2.0-next.1+101.sha-e42aa6c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
