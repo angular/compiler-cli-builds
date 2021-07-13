fa62261adc52f21f7bb6ce794510a05f5f6f4cdc
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:33:56 +0000

    9ebd41e39c fix(core): allow proper type inference when `ngFor` is used with a `trackBy` function (#42692)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+93.sha-d654c79";
+export declare const NGCC_VERSION = "12.1.1+95.sha-9ebd41e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
