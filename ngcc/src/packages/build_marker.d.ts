309389a58932a5310e0e27095433051f99b970e9
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:42:47 +0000

    437759ba47 revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+2.sha-48c1c81";
+export declare const NGCC_VERSION = "12.2.0-next.2+3.sha-437759b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
