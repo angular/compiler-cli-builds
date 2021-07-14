981608e604a492899d06b36f0c633403377bb6a2
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:33:23 +0000

    64bb7cd704 docs: update incorrect bullet text (#42839)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+106.sha-0b429d3";
+export declare const NGCC_VERSION = "12.2.0-next.2+1.sha-64bb7cd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
