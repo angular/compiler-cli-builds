a9a9db76c11e6a5ccdafb15aaa5aee11fd5979c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:54:46 +0000

    642362a9c7 build: add configuration for caretaker handoff (#42859)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+12.sha-f5baa55";
+export declare const NGCC_VERSION = "12.2.0-next.2+15.sha-642362a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
