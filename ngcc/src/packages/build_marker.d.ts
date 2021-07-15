edc8965087590e9b62361bfa82dbb50837b2c6e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:43:08 +0000

    ea936720a6 docs(docs-infra): remove unnecessary "Podcast" from happy-angular podcast's title (#42756)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+10.sha-670300e";
+export declare const NGCC_VERSION = "12.2.0-next.2+9.sha-ea93672";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
