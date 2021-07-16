35ee9052eb81da3ee448b1c43271df6c0c72703a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 17:23:41 +0000

    3da71467e9 build(docs-infra): upgrade Lighthouse to version 8.1.0 (#42846)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+13.sha-7422821";
+export declare const NGCC_VERSION = "12.2.0-next.2+16.sha-3da7146";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
