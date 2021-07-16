597cc21dbe5a7e6bfe5a30910d70518898bbb1e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 20:07:00 +0000

    3b2f607cda build: generate closure locale files using hard-coded list of locales (#42230)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.2+16.sha-3da7146');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.2+24.sha-3b2f607');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.2+16.sha-3da7146') {
+            if (version === '12.2.0-next.2+24.sha-3b2f607') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
