6ad67e46f9964cd1a46aaaa54e0857822b3d9354
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:54:36 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.2+35.sha-7271ad1');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.2+38.sha-722eb5d');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.2+35.sha-7271ad1') {
+            if (version === '12.2.0-next.2+38.sha-722eb5d') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
