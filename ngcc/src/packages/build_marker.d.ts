ba95c38df2e8163b349ff2b18c26983282215e3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:16:06 +0000

    29f50976db fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.2+29.sha-e6b7472";
+export declare const NGCC_VERSION = "12.1.2+33.sha-29f5097";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
