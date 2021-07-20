c964480b9e3b5b1c032cd6c105a6f9245d3e803c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:36:40 +0000

    96d31d8c9f fix(dev-infra): prevent verbose messaging of git checkout and commit for release (#42871)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+28.sha-0ce8f6e";
+export declare const NGCC_VERSION = "12.2.0-next.2+32.sha-96d31d8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
