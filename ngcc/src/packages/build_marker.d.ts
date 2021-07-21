6e0eac0b993f3947fe78aae6ec059b86a3cc0f2a
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:54:56 +0000

    4d86ea6471 ci: no longer use deprecated `pullapprove_conditions` option (#42895)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+46.sha-23d6133";
+export declare const NGCC_VERSION = "12.2.0-next.3+3.sha-4d86ea6.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
