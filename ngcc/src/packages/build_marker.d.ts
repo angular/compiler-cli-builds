9d72015d1f03e0eb68b5665e4af748ec8dad0da0
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:02:14 +0000

    5fb23eccea perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.3+5.sha-a502279.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.3+6.sha-5fb23ec.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
