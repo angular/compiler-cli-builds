a7d35fe96dff0d32e1b938185c1661c1d4400e86
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:55:11 +0000

    992dc93ea3 refactor(dev-infra): remove usages and dependency on shelljs (#42911)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.3+3.sha-4d86ea6.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.3+2.sha-992dc93.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
