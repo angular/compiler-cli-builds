c01200c63eb5e6329f8cfe9a83afa48d48990662
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:30:19 +0000

    8fc08c5ab1 ci: update pullapprove config to properly handle global approvals (#42929)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+71.sha-9073633.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.1+73.sha-c74927d.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
