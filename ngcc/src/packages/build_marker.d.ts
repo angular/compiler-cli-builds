3213bf4187479097a06e5c9bbc6bbecd255e7f7c
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 21:43:57 +0000

    ee1f824c40 test(router): move computed state restoration tests to own file (#42933)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+49.sha-8267d25.with-local-changes";
+export declare const NGCC_VERSION = "12.1.1+51.sha-d8d7784.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
