47a76ff93c6cfeec67446645f94680c466186022
Dylan Hunn <dylhunn@gmail.com>
2021-07-24 00:31:29 +0000

    e8eb87ff58 docs: Edits to remove jargon (#42861)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+51.sha-d8d7784.with-local-changes";
+export declare const NGCC_VERSION = "12.1.1+52.sha-7b5405b.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
