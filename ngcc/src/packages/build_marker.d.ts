92581f13bb4f8afd36057da8b27a281cba605877
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:22:08 +0000

    dbae00195e feat(router): ability to provide custom route reuse strategy via DI for `RouterTestingModule` (#42434)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+79.sha-ff87da3.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.1+80.sha-22290af.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
