1c04d5ee703508c8d3751921e95dfafe423abdb0
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:14:14 +0000

    c7e4f14bc8 docs: include interpolations in the "What is Angular" example (#42702)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+60.sha-91a576d.with-local-changes";
+export declare const NGCC_VERSION = "12.1.1+62.sha-cf27076.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
