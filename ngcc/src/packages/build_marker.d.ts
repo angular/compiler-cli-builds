d3df85093f1f77c2f5427cd2895c0062da553e80
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:16:12 +0000

    8ce1ac603a docs: Add josmar-crwdstffng to contributors list. (#42972)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+94.sha-08444c6.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.1+95.sha-f0d857e.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
