1584f6a598ada4f02545187f67f2eeeba480ad99
Joey Perrott <josephperrott@gmail.com>
2021-07-28 18:55:04 +0000

    6ef06bbb38 release: cut the v12.1.4 release (#42981)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.1.1+62.sha-cf27076.with-local-changes";
+export declare const NGCC_VERSION = "12.1.1+63.sha-0c9c8f5.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
