7e24834011e445b641c2c7e48ba65673de35b4a8
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 20:25:27 +0000

    2a0e28cf61 fix(dev-infra): ensure that building environment stamp fails silently (#42985)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.1+93.sha-e6593ad.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.1+94.sha-08444c6.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
