eff02ff9eb3ee295875bfad9d397cdd647acf8d4
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:14:39 +0000

    8ce1ac603a docs: Add josmar-crwdstffng to contributors list. (#42972)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+94.sha-08444c6.with-local-changes');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.0-next.1+95.sha-f0d857e.with-local-changes');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.0-next.1+94.sha-08444c6.with-local-changes') {
+            if (version === '12.2.0-next.1+95.sha-f0d857e.with-local-changes') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
