55d971c137b416ebd9b11a09000c744ffb37a6a1
atscott <atscott01@gmail.com>
2021-08-02 22:55:16 +0000

    fbfec946a7 docs: amend links in content-projection guide (#43009)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+7.sha-c11bcb7.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.2+8.sha-c556128.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
