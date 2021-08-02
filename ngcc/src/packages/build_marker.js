c2cd8ada2ac348c5f37276a6894f79271d32debf
atscott <atscott01@gmail.com>
2021-08-02 17:17:22 +0000

    e4f62c12f5 test(compiler): add a test for parsing multiline expressions in attributes (#42062)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.0-next.1+98.sha-c1c1cda.with-local-changes';
+    exports.NGCC_VERSION = '12.2.0-next.2+6.sha-ca1fde9.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
