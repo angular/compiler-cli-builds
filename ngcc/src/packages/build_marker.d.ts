3dba720db3605d4e84c66ffbcb18523275bd351a
GitHub <noreply@github.com>
2021-08-04 17:25:54 +0000

    82c8393d37 docs: release notes for the v12.1.5 release (#43046)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0-next.2+29.sha-5327926.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0-next.2+30.sha-89509df.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
