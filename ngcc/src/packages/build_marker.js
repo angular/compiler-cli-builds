ea6f123085f36368a3252136adab2857763c4dff
Andrew Kushnir <akushnir@google.com>
2021-08-05 23:25:48 +0000

    4db80d69d7 ci: stop publishing snapshots to angular/dev-infra-private-builds (#43059)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.0-next.2+35.sha-7271ad1.with-local-changes';
+    exports.NGCC_VERSION = '12.2.0-next.2+36.sha-7eddd12.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
