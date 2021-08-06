8294329d76de3fc53a0569cacb319bcd3c0b370d
Andrew Kushnir <akushnir@google.com>
2021-08-06 18:08:06 +0000

    784671597e fix(router): Allow question marks in query param values (#31187)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.0+5.sha-d8690d8.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.0+6.sha-7846715.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
