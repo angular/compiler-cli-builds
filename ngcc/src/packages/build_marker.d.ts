d9885606b1df79f114f62f367201cbf8765e628a
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:46:10 +0000

    bbad42310b fix(router): ensure check for match options is compatible with property renaming (#43086)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.0+8.sha-9359089.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.0+10.sha-bbad423.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
