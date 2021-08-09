a886d7f79a2623f48fb971c6957eb83f7d48bd2b
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:00:39 +0000

    d93427b806 docs: fix cli link (#43085) PR Close #43085

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.0+4.sha-4371ff9.with-local-changes";
+export declare const NGCC_VERSION = "12.2.0+7.sha-d93427b.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
