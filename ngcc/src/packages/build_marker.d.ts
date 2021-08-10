8934367a9886eaf23e08fe5315e4d3f1f35c317e
Andrew Kushnir <akushnir@google.com>
2021-08-10 23:14:20 +0000

    e25d35f4cb refactor(compiler-cli): add test for `BananaInBoxCheck` (#42984)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.0+18.sha-e3415e5.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.0+26.sha-e25d35f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
