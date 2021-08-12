9f0de3880205226f3f12f580a6eed8b528e531d0
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:51:12 +0000

    a2a9b917b5 docs: adjust description to code example (#43127)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.1+2.sha-d5f9890.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.1+3.sha-a2a9b91.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
