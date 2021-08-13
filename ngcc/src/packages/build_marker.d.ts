608f7d3295a1f8b5c6dce9a17628ce239f61a6a9
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:17:58 +0000

    19fd2f4125 ci: add breaking changes auto labeling action (#43141)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.1+3.sha-7a67597.with-local-changes";
+export declare const NGCC_VERSION = "12.2.1+4.sha-19fd2f4.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
