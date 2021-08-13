0a99a1a72a037300e7d9cd58bdacca56fc477eee
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:17:49 +0000

    2a1aa5867d ci: add breaking changes auto labeling action (#43141)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '13.0.0-next.1+6.sha-470371f.with-local-changes';
+    exports.NGCC_VERSION = '13.0.0-next.1+7.sha-2a1aa58.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
