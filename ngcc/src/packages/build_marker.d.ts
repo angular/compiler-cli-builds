5dd5a9d4fd7b5c58ace619ba6059d99b29510345
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:25:01 +0000

    c91cecddaa ci: update circle cache key (#43177)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.1+17.sha-d743dd9.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.1+20.sha-c91cecd.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
