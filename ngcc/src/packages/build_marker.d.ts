d0d758598d3bc3d67038438b29ad5eb7904cb6cf
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:01:25 +0000

    0c84f6fd52 refactor(compiler): fix lateron typo in i18n ExtractorHost (#43160)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.1+8.sha-b0b7ef2.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.1+9.sha-0c84f6f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
