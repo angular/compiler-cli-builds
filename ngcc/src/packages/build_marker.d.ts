f1a583a0ef1061284b60df724e26b59b58104436
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:04:57 +0000

    83fa3ee581 docs: add tmdavis to contributors (#43135)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.1+9.sha-e0a8ca7.with-local-changes";
+export declare const NGCC_VERSION = "12.2.1+12.sha-83fa3ee.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
