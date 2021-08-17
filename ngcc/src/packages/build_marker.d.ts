6aa89b9a10ae3957e8f49b243005271fd537657f
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:42:48 +0000

    bb6555979d fix(animations): add pure annotations to static property initializers (#43064)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.1+19.sha-cd10a4c.with-local-changes";
+export declare const NGCC_VERSION = "12.2.1+26.sha-bb65559.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
