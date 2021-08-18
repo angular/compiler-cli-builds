90b24f33ec605a84dda78e0643459fb32c49e11e
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 18:11:22 +0000

    513b17043e docs: fix typo (#43190)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.1+32.sha-c501e86.with-local-changes";
+export declare const NGCC_VERSION = "12.2.2+1.sha-513b170.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.1+32.sha-c501e86.with-local-changes';
+    exports.NGCC_VERSION = '12.2.2+1.sha-513b170.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
