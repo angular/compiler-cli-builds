b532eb61eda4a75cc3073e43eae5d02cb946f32a
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 23:57:01 +0000

    692e8ab21e test(compiler-cli): add tests for `NullishCoalescingNotNullableCheck` (#43232)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.3+6.sha-30d959a.with-local-changes";
+export declare const NGCC_VERSION = "12.2.3+13.sha-692e8ab.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
