34549aafa09f88b0336a3512e1a33a8805a32901
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 20:03:50 +0000

    c33ba8d4de build: update issue template to include core package (#43272)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.3+8.sha-78690ee.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.3+9.sha-c33ba8d.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
