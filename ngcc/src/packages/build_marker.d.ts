ec8cb3b8ca0922b9580cf601eac71174a15c52c0
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:26:51 +0000

    fdcb420bd7 docs(router): Fix incorrect name for url match options (#43268)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.3+3.sha-fb64f71.with-local-changes";
+export declare const NGCC_VERSION = "12.2.3+5.sha-fdcb420.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
