7d026e3ef1b2f763bb22a2044b4f919298112d14
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-30 20:56:22 +0000

    dcfabf08af docs: add step to declare `ProductAlertsComponent` in tutorial (#43229)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.3+18.sha-f3c8408.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.3+19.sha-dcfabf0.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
