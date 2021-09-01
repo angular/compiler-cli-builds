b7bf69dbc95048b4efcd31ed98181485cad28326
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:40:04 +0000

    a0bd6e90f9 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.3+17.sha-caecd18.with-local-changes";
+export declare const NGCC_VERSION = "12.2.4+1.sha-a0bd6e9.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
