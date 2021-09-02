c7eaea2346ad2548d877bf5d1c85c1d56895acb5
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:12:48 +0000

    c6a93001eb fix(common): synchronise location mock behavior with the navigators (#41730)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.4+11.sha-fc3b50e.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.4+12.sha-c6a9300.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
