57768f9b63f74a5146ecbeffe0781b7f7946c603
Andrew Scott <atscott01@gmail.com>
2021-09-07 23:35:50 +0000

    2efc18e675 fix(migrations): migration failed finding tsconfig file (#43343)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.4+25.sha-9c7d18e.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.4+26.sha-2efc18e.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
