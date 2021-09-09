f387e031f4f9eb3605017e1728aba13e532682ec
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:54:21 +0000

    f57f0edd23 refactor(router): clean up unnecessary flag in `restoreHistory` function (#43382)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.5+8.sha-6c84c5f.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.5+9.sha-f57f0ed.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
