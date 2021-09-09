05d211abf817c0f76e64337e85e4423f3fbd21d7
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:42:49 +0000

    6c84c5f513 fix(animations): emit pure annotations to static property initializers (#43344)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '13.0.0-next.5+6.sha-4c307ad.with-local-changes');
+        var LATEST_VERSION_RANGE = getRange('<=', '13.0.0-next.5+8.sha-6c84c5f.with-local-changes');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '13.0.0-next.5+6.sha-4c307ad.with-local-changes') {
+            if (version === '13.0.0-next.5+8.sha-6c84c5f.with-local-changes') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
