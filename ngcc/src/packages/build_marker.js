44d3edfd1b06ab1ebbf82c6b1ac6679a28ff7746
Andrew Kushnir <akushnir@google.com>
2021-09-09 21:42:53 +0000

    134e4f5f6a Revert "refactor(router): clean up unnecessary flag in `restoreHistory` function" (#43409)

--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.5+8.sha-8cc93b7.with-local-changes';
+    exports.NGCC_VERSION = '12.2.5+9.sha-134e4f5.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
