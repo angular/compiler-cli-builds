41ba7b9db56a8fbe2a39b8783d3fb9f446f95a6e
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:53:34 +0000

    8cc93b723c docs: add Angular Projects book in resources (#43130)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.5+4.sha-2658219.with-local-changes";
+export declare const NGCC_VERSION = "12.2.5+8.sha-8cc93b7.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
