380630bbc353f73f44990f223de76fd59d7b5666
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:39:39 +0000

    2d93e9c0fd docs: add Michele Stieven to GDE resources (#43233)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.5+19.sha-99f63dc.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.5+20.sha-2d93e9c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
