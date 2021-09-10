628d5f4c9c6068b3ef26709f0b1b446733ea874e
Andrew Kushnir <akushnir@google.com>
2021-09-10 16:45:24 +0000

    c8179353ce fix(docs-infra): bump aio size limit (#43406)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.5+11.sha-cbf360d.with-local-changes";
+export declare const NGCC_VERSION = "12.2.5+23.sha-c817935.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
