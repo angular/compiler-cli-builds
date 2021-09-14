066e5560b0d1dbad88ea7aaadf47505661913c28
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:31:17 +0000

    3ec9ee65b9 refactor(router): Remove TODO and slightly adjust eager browserUrlTree update (#43426)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.5+45.sha-71e4323.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.5+47.sha-3ec9ee6.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
