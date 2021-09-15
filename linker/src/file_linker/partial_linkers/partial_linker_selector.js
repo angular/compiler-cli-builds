7fd711c4356e2e570ad00334581dfd23d85e44a4
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:07:55 +0000

    1986407ab5 docs: move i18n angular.json snippets to external file (#43216)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '13.0.0-next.5+56.sha-5e9eb7a.with-local-changes');
+        var LATEST_VERSION_RANGE = getRange('<=', '13.0.0-next.6+11.sha-1986407.with-local-changes');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '13.0.0-next.5+56.sha-5e9eb7a.with-local-changes') {
+            if (version === '13.0.0-next.6+11.sha-1986407.with-local-changes') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
