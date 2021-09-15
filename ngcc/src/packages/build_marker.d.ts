af530acc0177a99d6f068cd76e0729638b642c94
GitHub <noreply@github.com>
2021-09-15 17:58:36 +0000

    5e9eb7abbd release: cut the v13.0.0-next.6 release (#43464)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.5+55.sha-bab963f.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.5+56.sha-5e9eb7a.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
