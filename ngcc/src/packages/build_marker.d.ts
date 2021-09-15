e6b093693bcdfe48c7c449cdea44b1eb927c8b64
Andrew Kushnir <akushnir@google.com>
2021-09-15 20:42:23 +0000

    def6315a76 docs: update reviewed tag (#43452)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.5+40.sha-2050b08.with-local-changes";
+export declare const NGCC_VERSION = "12.2.6+15.sha-def6315.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
