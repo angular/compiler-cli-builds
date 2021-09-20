69ab54bf549d8018b4fd5f70df7b19d99e8eb879
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 22:15:50 +0000

    2bb4bf1468 fix(common): titlecase pipe incorrectly handling numbers (#43476)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.6+42.sha-defb02f.with-local-changes";
+export declare const NGCC_VERSION = "12.2.6+43.sha-2bb4bf1.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
--- ngcc/src/packages/build_marker.js
+++ ngcc/src/packages/build_marker.js
@@ -12,7 +12,7 @@
     exports.markAsProcessed = exports.hasBeenProcessed = exports.cleanPackageJson = exports.needsCleaning = exports.NGCC_VERSION = void 0;
     var tslib_1 = require("tslib");
     var new_entry_point_file_writer_1 = require("@angular/compiler-cli/ngcc/src/writing/new_entry_point_file_writer");
-    exports.NGCC_VERSION = '12.2.6+42.sha-defb02f.with-local-changes';
+    exports.NGCC_VERSION = '12.2.6+43.sha-2bb4bf1.with-local-changes';
     /**
      * Returns true if there is a format in this entry-point that was compiled with an outdated version
      * of ngcc.
