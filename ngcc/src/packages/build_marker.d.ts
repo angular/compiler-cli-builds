1f7823ade1d07a5a97325c19118bb657cfe2e168
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 16:22:15 +0000

    020f0d3858 fix(docs-infra): improve aio contributors page responsiveness (#43355)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.6+37.sha-909c706.with-local-changes";
+export declare const NGCC_VERSION = "12.2.6+38.sha-020f0d3.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
