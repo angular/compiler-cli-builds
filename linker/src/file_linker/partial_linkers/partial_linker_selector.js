01e960e627f44f6a42b172c9506ac85554a92543
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 23:09:25 +0000

    aa8e8db047 docs: Edits to remove jargon (#42928)

--- linker/src/file_linker/partial_linkers/partial_linker_selector.js
+++ linker/src/file_linker/partial_linkers/partial_linker_selector.js
@@ -66,7 +66,7 @@
      */
     function createLinkerMap(environment, sourceUrl, code) {
         var linkers = new Map();
-        var LATEST_VERSION_RANGE = getRange('<=', '12.2.6+43.sha-2bb4bf1.with-local-changes');
+        var LATEST_VERSION_RANGE = getRange('<=', '12.2.6+46.sha-aa8e8db.with-local-changes');
         linkers.set(exports.ɵɵngDeclareDirective, [
             { range: LATEST_VERSION_RANGE, linker: new partial_directive_linker_1_1.PartialDirectiveLinkerVersion1(sourceUrl, code) },
         ]);
@@ -137,7 +137,7 @@
                 throw new Error("Unknown partial declaration function " + functionName + ".");
             }
             var linkerRanges = this.linkers.get(functionName);
-            if (version === '12.2.6+43.sha-2bb4bf1.with-local-changes') {
+            if (version === '12.2.6+46.sha-aa8e8db.with-local-changes') {
                 // Special case if the `version` is the same as the current compiler version.
                 // This helps with compliance tests where the version placeholders have not been replaced.
                 return linkerRanges[linkerRanges.length - 1].linker;
