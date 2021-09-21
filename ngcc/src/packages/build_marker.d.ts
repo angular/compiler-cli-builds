49c1088bd9fa311ede5be26c95dc13ac0571e820
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-21 16:32:28 +0000

    d1baf7cdfb fix(docs-infra): add a "skip to content" link for better accessibility (#43460)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.6+48.sha-7d6ba72.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.6+53.sha-d1baf7c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
