bfaafe4be02698c51803a4b07524ccb69e7a4d58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 17:57:31 +0000

    d9d8f950e9 feat(forms): allow disabling min/max validators dynamically (by setting the value to `null`) (#42978)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.7+11.sha-ea61ec2.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.7+29.sha-d9d8f95.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
