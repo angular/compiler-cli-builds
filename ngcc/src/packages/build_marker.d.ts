cdb6c1d93f7b42a07525dc2e2ce3b9378b116a91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 16:09:36 +0000

    c008e0fa90 refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.8+18.sha-4555d8a.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.8+21.sha-c008e0f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
