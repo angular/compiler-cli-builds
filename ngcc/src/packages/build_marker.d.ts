f8f43556631ef10ca25f13eba7f638bee1add06f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:34:17 +0000

    09d325a9e6 test(router): refactor tests to not use deprecated loadChildren (#43578)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.8+27.sha-6a3492b.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.8+29.sha-09d325a.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
