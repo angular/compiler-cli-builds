3e85eb65bb1e878727d30221b93beed951d74f6f
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 17:44:36 +0000

    b64f796cc7 docs: add developer survey 2021 (#43670)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4451,7 +4451,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.9+85.sha-c721135.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.10+1.sha-b64f796.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
