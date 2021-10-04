a38f7fbbb10aae346dfce43c064462124e37a225
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:14:32 +0000

    e5c3017df3 docs: move angular-compiler-options tsconfig snippets to external file (#43545)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.10+7.sha-dbfb2d2.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.10+8.sha-e5c3017.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
