cf9ef8c69a4fe5521e3f91d957ea63c6b5996e8e
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:12:58 +0000

    2cc5cc9d1f build(docs-infra): upgrade cli command docs sources to afd86f0cd (#43678)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.10+6.sha-16f504b.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.10+5.sha-2cc5cc9.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
