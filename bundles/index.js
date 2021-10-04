94aa555703cf716b330f339f69513b59aa29487c
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:11:28 +0000

    0d43148f58 build: update to the latest version of @angular/dev-infra-private (#43541)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.10+3.sha-f513b17.with-local-changes");
+var VERSION = new Version("13.0.0-next.10+4.sha-0d43148.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
