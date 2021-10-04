8626a1249efae2abd86d6c80ffa53dc757bfbc54
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 23:22:08 +0000

    a268c4471f refactor(router): restore history in a consistent way on canceled navigations (#43651)

--- bundles/linker/index.js
+++ bundles/linker/index.js
@@ -795,7 +795,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.10+8.sha-e5c3017.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.10+9.sha-a268c44.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -842,7 +842,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-next.10+8.sha-e5c3017.with-local-changes") {
+    if (version === "13.0.0-next.10+9.sha-a268c44.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
