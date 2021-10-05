e02f5d23ee0a1f6efc7ec59baba4ea3eea5f4a35
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 20:58:51 +0000

    fddb50b597 fix(service-worker): make `ngsw.json` generation deterministic and correct (#43679)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.11+11.sha-d585fbc.with-local-changes");
+var VERSION = new Version("13.0.0-next.11+10.sha-fddb50b.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
