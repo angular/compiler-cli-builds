c7b0d7848fc9664a6a50faf26b1dd539952b1dfb
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 01:27:19 +0000

    7acf3bece4 release: cut the v13.0.0-next.12 release (#43726)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.11+14.sha-c14085e.with-local-changes");
+var VERSION = new Version("13.0.0-next.11+16.sha-7acf3be.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.11+14.sha-c14085e.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.11+16.sha-7acf3be.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
