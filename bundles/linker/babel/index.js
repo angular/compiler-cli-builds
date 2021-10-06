821dde8ad75b6db52967833e8892c03d0403ae5e
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:43:40 +0000

    52587e5b43 test: remove `progress` option from `dev-server` builder (#43730)

--- bundles/linker/babel/index.js
+++ bundles/linker/babel/index.js
@@ -1086,7 +1086,7 @@ var \u0275\u0275ngDeclareNgModule = "\u0275\u0275ngDeclareNgModule";
 var \u0275\u0275ngDeclarePipe = "\u0275\u0275ngDeclarePipe";
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.12+4.sha-6ac35d0.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.12+5.sha-52587e5.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -1133,7 +1133,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-next.12+4.sha-6ac35d0.with-local-changes") {
+    if (version === "13.0.0-next.12+5.sha-52587e5.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.12+4.sha-6ac35d0.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.12+5.sha-52587e5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
