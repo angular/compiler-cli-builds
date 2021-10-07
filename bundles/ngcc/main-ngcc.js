0b82db20105fd62dc300c7547b196c4bdd9515f4
Andrew Scott <atscott01@gmail.com>
2021-10-07 17:52:22 +0000

    d977701a43 feat(bazel): allow for custom conditions to be set in `ng_package` targets (#43764)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4451,7 +4451,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.13+5.sha-48aa003.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.13+6.sha-d977701.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
