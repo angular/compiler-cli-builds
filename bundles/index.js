d4df47d66cd39fb8a72684ee8dd247f849456f33
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:35:19 +0000

    300230c255 fix(docs-infra): avoid prepending version info to error messages twice (#43767)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+8.sha-bbb91aa.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+12.sha-300230c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
