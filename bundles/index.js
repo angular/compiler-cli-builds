7281746ec6ad09cc34fadf5d7c9c3f7472d96cfc
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:34:54 +0000

    bbb91aae91 build: update to latest @angular/dev-infra-private (#43785)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+7.sha-4eedf77.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+8.sha-bbb91aa.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4451,7 +4451,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+7.sha-4eedf77.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+8.sha-bbb91aa.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
