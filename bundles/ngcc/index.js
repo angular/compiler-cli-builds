b79eee27d273d810a2a8f34713385bd496413bbe
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:59:23 +0000

    4c00e92023 build: update dependency @microsoft/api-extractor to v7.18.15 (#43771)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+15.sha-1317e19.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+17.sha-4c00e92.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
