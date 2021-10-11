4143834b52fff08eda8fdaad476c92b8814f9282
Andrew Scott <atscott01@gmail.com>
2021-10-11 16:59:35 +0000

    8b2971417a docs: add missing declaration of `CartComponent` in tutorial (#43774)

--- bundles/linker/babel/index.js
+++ bundles/linker/babel/index.js
@@ -1086,7 +1086,7 @@ var \u0275\u0275ngDeclareNgModule = "\u0275\u0275ngDeclareNgModule";
 var \u0275\u0275ngDeclarePipe = "\u0275\u0275ngDeclarePipe";
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+17.sha-4c00e92.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+19.sha-8b29714.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -1133,7 +1133,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-next.14+17.sha-4c00e92.with-local-changes") {
+    if (version === "13.0.0-next.14+19.sha-8b29714.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
