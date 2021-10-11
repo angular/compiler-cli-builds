ae077f4f35434a6574104dbff109a390e36efb9f
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:01:26 +0000

    4daf6318bc build: update dependency jsdom to v18 (#43786)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+20.sha-33d6b58.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+21.sha-4daf631.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
