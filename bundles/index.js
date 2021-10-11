522ba938550bf4c604199f692c84b3345bd37593
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:00:32 +0000

    33d6b5888c docs: Formalize language describing HttpClient (#43778) PR Close #43778

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+19.sha-8b29714.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+20.sha-33d6b58.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
