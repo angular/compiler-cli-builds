a3be3d92c77fb12ad36f547e6efbed6f8b40ada8
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:01:10 +0000

    baf8da0f0f ci: re-enable dev-app building in components-repo-unit-tests job (#43798)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+30.sha-17f5299.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+27.sha-baf8da0.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+30.sha-17f5299.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+27.sha-baf8da0.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
