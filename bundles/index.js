ad9964fd076d0a228fbabca530dc52643c298bbe
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:03:43 +0000

    fde329da80 docs: update ngUpgrade support message (#43704)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+33.sha-d151a3c.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+34.sha-fde329d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
