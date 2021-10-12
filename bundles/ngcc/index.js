fd7baefc2d156af6898c4bef829dc7a03c9f46f2
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:07:11 +0000

    16e5a314b1 build: lock file maintenance (#43793)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+37.sha-325c6e4.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+36.sha-16e5a31.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
