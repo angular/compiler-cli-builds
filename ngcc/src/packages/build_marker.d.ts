f6b302a162e787a6c8d283a706a475547b5acff2
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:02:29 +0000

    62494c9f2a docs: update how to remove a configuration item (#43795)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+27.sha-baf8da0.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+32.sha-62494c9.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
