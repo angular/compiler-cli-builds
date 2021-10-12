30819f927931b2d64c22f474891f42aafad75b0e
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:02:56 +0000

    d151a3c375 refactor(language-service): enable extended template diagnostics when strict templates is forced. (#43708)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+32.sha-62494c9.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+33.sha-d151a3c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
