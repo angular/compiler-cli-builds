2ec96774d982c81f16e355186cc381e0afe6f504
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:10:08 +0000

    512f6438ab build: update dependency @typescript-eslint/eslint-plugin to v5 (#43806)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+40.sha-85c0ce7.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+44.sha-512f643.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
