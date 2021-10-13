1687396027f4b5d14c076fd0b04c38c0340b4211
GitHub <noreply@github.com>
2021-10-13 18:43:16 +0000

    b5eccde447 release: cut the v13.0.0-rc.0 release (#43828)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.15+41.sha-7ba9591.with-local-changes");
+var VERSION = new Version("13.0.0-next.15+42.sha-b5eccde.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4451,7 +4451,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.15+41.sha-7ba9591.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.15+42.sha-b5eccde.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
