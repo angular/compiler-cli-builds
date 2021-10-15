9f5beb835eafeddd050f2df9f7019762751b7d95
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:53:15 +0000

    70c14ed66d build(docs-infra): add a box around CLI command line reference syntax (#43850)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-rc.0+11.sha-426a3ec.with-local-changes");
+var VERSION = new Version("13.0.0-rc.0+14.sha-70c14ed.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4451,7 +4451,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.0+11.sha-426a3ec.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.0+14.sha-70c14ed.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
