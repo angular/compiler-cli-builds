c900880f76675ad57346abc9ee94af8af6eab258
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:50:28 +0000

    25b9c8cc5a docs: update getting started links to provide a more consistent user experience (#43872)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+67.sha-efeb5ad.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+71.sha-25b9c8c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+67.sha-efeb5ad.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+71.sha-25b9c8c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
