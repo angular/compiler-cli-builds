9f11d2975ca86ca33591193359dbfa79270d8812
Andrew Kushnir <akushnir@google.com>
2021-10-18 17:59:16 +0000

    83f857c4fb docs: fix a couple of random typos (#43878)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+72.sha-a3ee474.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+74.sha-83f857c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
