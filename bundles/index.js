cfb13c0e39542d67886c36e96c8cacb20fe5f50f
Andrew Kushnir <akushnir@google.com>
2021-10-18 20:50:33 +0000

    fc6519c25a Revert "fix(compiler): support i18n interpolated only attribute bindings (#43815)" (#43882)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-rc.0+20.sha-db57e39.with-local-changes");
+var VERSION = new Version("13.0.0-rc.0+25.sha-fc6519c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4452,7 +4452,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.0+20.sha-db57e39.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.0+25.sha-fc6519c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
