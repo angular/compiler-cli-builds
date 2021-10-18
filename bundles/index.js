f609c72545d1ac94a714d1fddf95946c808f4a88
Andrew Kushnir <akushnir@google.com>
2021-10-18 18:22:33 +0000

    c944fee11b fix(compiler): do not error if $any is used inside a listener (#43866)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+74.sha-83f857c.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+75.sha-c944fee.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
