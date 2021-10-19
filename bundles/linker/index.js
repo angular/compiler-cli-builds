057cfdb5e0ebb7a062d3dc0ed9305e53188b0342
Andrew Kushnir <akushnir@google.com>
2021-10-19 18:02:19 +0000

    9d9346c57a docs: add docs about build cache (#43843)

--- bundles/linker/index.js
+++ bundles/linker/index.js
@@ -795,7 +795,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.0+39.sha-812a33d.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.0+40.sha-9d9346c.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -842,7 +842,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-rc.0+39.sha-812a33d.with-local-changes") {
+    if (version === "13.0.0-rc.0+40.sha-9d9346c.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
