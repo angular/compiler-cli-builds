98bb055f557a8ddd4269ed9ce86737a7d2d92556
Andrew Kushnir <akushnir@google.com>
2021-10-19 04:09:33 +0000

    9f350a8473 ci: update payload size limit (#43885)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.9+17.sha-c7c285c.with-local-changes";
+export declare const NGCC_VERSION = "12.2.10+15.sha-9f350a8.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
