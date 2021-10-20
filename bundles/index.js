1be16440e68fd46283dd3ffb8f7518bae793035a
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:42:17 +0000

    665466def3 docs: move deprecations example snippets to external file (#43700)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-rc.0+43.sha-b31a9a7.with-local-changes");
+var VERSION = new Version("13.0.0-rc.1+2.sha-665466d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
