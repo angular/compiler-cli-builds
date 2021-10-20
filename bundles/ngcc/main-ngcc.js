895d55aec676e25e912cc42cdcdafa9013a284cc
GitHub <noreply@github.com>
2021-10-20 17:15:10 +0000

    b31a9a75ad release: cut the v13.0.0-rc.1 release (#43911)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4457,7 +4457,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.0+42.sha-7d33af0.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.0+43.sha-b31a9a7.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
