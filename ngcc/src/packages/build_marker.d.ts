d1515df442c78728fe8202aed79f974ef5df9f5e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 20:28:17 +0000

    7817892684 refactor(core): made comment structure consistent in index.ts (#43684)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.11+3.sha-cef01b8.with-local-changes";
+export declare const NGCC_VERSION = "12.2.11+6.sha-7817892.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
