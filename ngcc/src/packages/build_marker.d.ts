f652685f0754b5844d7eaec3d5227121d578ac53
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 16:45:43 +0000

    cef01b804a ci: update `angular/dev-infra/github-actions` sha (#43904)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.11+1.sha-e09556f.with-local-changes";
+export declare const NGCC_VERSION = "12.2.11+3.sha-cef01b8.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
