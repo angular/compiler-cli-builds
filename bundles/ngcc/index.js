26ef35d9f7fdb9db5911aaa27462bb665a50fb06
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:16:54 +0000

    bf1a2fc2d5 test(platform-browser): complete component bootstrap before switching to the next test (#43933)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4458,7 +4458,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+113.sha-6e37c93.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+114.sha-bf1a2fc.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
