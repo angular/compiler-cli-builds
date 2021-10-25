c86334e4444d02b66924c743674ef39d99cc13fd
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 19:49:00 +0000

    087fae5ea9 refactor(migrations): remove no longer accessible migrations (#43942)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4457,7 +4457,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.1+17.sha-8277c38.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.1+18.sha-087fae5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
