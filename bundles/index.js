ff85888789dfc8d9ea351e7648b299813f2b0b50
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:18:46 +0000

    0873ae2580 Revert "refactor(migrations): remove no longer accessible migrations" (#43943)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+115.sha-0b4350c.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+116.sha-0873ae2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
