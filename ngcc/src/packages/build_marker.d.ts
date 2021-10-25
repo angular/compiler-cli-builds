3e244008894d93b7d44d61f57f3f59bb19618da7
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:24:16 +0000

    940eafa351 docs(localize): fix spelling of documentation link (#43941)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-rc.1+18.sha-087fae5.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-rc.1+20.sha-940eafa.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
