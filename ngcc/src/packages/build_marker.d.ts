563f4b8ac46a6be738dc297142ee088b1d48d176
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:33:52 +0000

    8fcfd87d99 ci: update commit-message-based-labels job to be called labels (#43944)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.11+12.sha-bf59401.with-local-changes";
+export declare const NGCC_VERSION = "12.2.11+14.sha-8fcfd87.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
