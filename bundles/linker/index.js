a6bf8c1b3f60782cf045696ed0bd2813929f0ad7
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:37:16 +0000

    5d66b4aee6 ci: update commit-message-based-labels job to be called labels (#43944)

--- bundles/linker/index.js
+++ bundles/linker/index.js
@@ -795,7 +795,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+122.sha-143bb9f.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+123.sha-5d66b4a.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -842,7 +842,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-next.14+122.sha-143bb9f.with-local-changes") {
+    if (version === "13.0.0-next.14+123.sha-5d66b4a.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
