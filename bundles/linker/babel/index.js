64b6e2fe9cd3d4d73ab1638b0ff14dfc598f289c
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:18:30 +0000

    081a4c2722 build: update all non-major dependencies (#43917)

--- bundles/linker/babel/index.js
+++ bundles/linker/babel/index.js
@@ -1086,7 +1086,7 @@ var \u0275\u0275ngDeclareNgModule = "\u0275\u0275ngDeclareNgModule";
 var \u0275\u0275ngDeclarePipe = "\u0275\u0275ngDeclarePipe";
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.1+27.sha-c44d1cb.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.1+28.sha-081a4c2.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -1133,7 +1133,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-rc.1+27.sha-c44d1cb.with-local-changes") {
+    if (version === "13.0.0-rc.1+28.sha-081a4c2.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
