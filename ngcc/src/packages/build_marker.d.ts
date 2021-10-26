2b81db3b67e1283af77706a148b6fd54c3ea43a7
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:35:50 +0000

    143bb9f339 build: create issue selector configuration (#43923)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+121.sha-58beafb.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+122.sha-143bb9f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
