a94714569570520916d4641cc419e81ec04e5938
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:42:22 +0000

    5c22dfe861 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43835)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4457,7 +4457,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+123.sha-5d66b4a.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+124.sha-5c22dfe.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+123.sha-5d66b4a.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+124.sha-5c22dfe.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
