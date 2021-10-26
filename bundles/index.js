f0e950ada1a3cca6ca4e2bac680f29cc10be38bd
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 23:34:59 +0000

    0dbe1c695a docs: reviewed tag added (#43488)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-rc.1+28.sha-081a4c2.with-local-changes");
+var VERSION = new Version("13.0.0-rc.1+29.sha-0dbe1c6.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
