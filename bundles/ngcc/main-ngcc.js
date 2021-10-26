ff12e4ad75487c7fdf8569223fb8e2e8a9b43bf5
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 23:59:26 +0000

    284f58546a docs: reviewed tag added (#43488)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4457,7 +4457,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+125.sha-6c47139.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+126.sha-284f585.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
