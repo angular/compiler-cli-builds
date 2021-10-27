8e2d558db827bfd71e9aa370cdd74c6fc736c778
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:36:39 +0000

    515949e7e6 docs: review tag added (#43465)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.11+14.sha-8fcfd87.with-local-changes";
+export declare const NGCC_VERSION = "12.2.11+20.sha-515949e.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
