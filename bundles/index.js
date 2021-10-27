c58ebb064a530fc9c942d3fc3ada7256ca5674c5
GitHub <noreply@github.com>
2021-10-27 18:53:11 +0000

    a84983efea docs: release notes for the v13.0.0-rc.2 release (#43969)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+134.sha-96944eb.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+135.sha-a84983e.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
