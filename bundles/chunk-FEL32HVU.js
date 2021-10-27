00e513dd304405ee9770a0dc74c42ebf4fa8b9e3
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:30:25 +0000

    b393188899 fix(docs-infra): fix heritage links (#43956)

--- bundles/chunk-FEL32HVU.js
+++ bundles/chunk-FEL32HVU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+131.sha-57cbb4f.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+133.sha-b393188.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+131.sha-57cbb4f.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+133.sha-b393188.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
