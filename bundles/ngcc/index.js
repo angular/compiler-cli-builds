29c48a4ffddb0636ce72ba91050a36dbcb5f155b
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:30:25 +0000

    7de7dc89bf fix(docs-infra): fix heritage links (#43956)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4458,7 +4458,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.1+31.sha-cfc509c.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.1+33.sha-7de7dc8.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
