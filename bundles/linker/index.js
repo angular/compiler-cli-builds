d02db65d7dab816049bc5c622b2257c28b62e865
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 21:42:13 +0000

    96b99c443b docs: update `creating libraries` guide (#43982)

--- bundles/linker/index.js
+++ bundles/linker/index.js
@@ -795,7 +795,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.2+9.sha-3faef71.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-rc.2+10.sha-96b99c4.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -842,7 +842,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-rc.2+9.sha-3faef71.with-local-changes") {
+    if (version === "13.0.0-rc.2+10.sha-96b99c4.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
