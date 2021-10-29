baca32439c6330641b9a2b75a1c2dc57a5f00c78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 21:41:48 +0000

    6b8cdc9339 docs: update `creating libraries` guide (#43982)

--- bundles/chunk-XR5J3AKU.js
+++ bundles/chunk-XR5J3AKU.js
@@ -787,7 +787,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+144.sha-7b35a34.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.0-next.14+145.sha-6b8cdc9.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -834,7 +834,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.0-next.14+144.sha-7b35a34.with-local-changes") {
+    if (version === "13.0.0-next.14+145.sha-6b8cdc9.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
