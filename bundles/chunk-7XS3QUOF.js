5fed64479148a569d2c8004cb912f3e27b8381fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:05:47 +0000

    408e4dafbd fix(docs-infra): search box input line-height (#43983)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+171.sha-a01e02d.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+175.sha-408e4da.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
