75bae040a03bc50844f87099f8fc073abd9b8000
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:03:00 +0000

    a01e02d4e2 refactor(docs-infra): remove obsolete way of disabling `DocViewer` animations (#43977)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-next.14+164.sha-967283e.with-local-changes";
+var NGCC_VERSION = "13.0.0-next.14+171.sha-a01e02d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-next.14+164.sha-967283e.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-next.14+171.sha-a01e02d.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
