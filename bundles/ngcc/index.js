bcd2f364e678a691da7ac1df927416eabca9198c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 20:31:08 +0000

    3faef7180d refactor(docs-infra): align docs examples with default CLI TS config (#43880)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4458,7 +4458,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0-rc.2+5.sha-f225bfd.with-local-changes";
+var NGCC_VERSION = "13.0.0-rc.2+9.sha-3faef71.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
