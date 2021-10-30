4119de9d06cc122124365af90a80a76d73505e32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-30 00:09:26 +0000

    0382f863db docs: update left-nav (#43964)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.0-rc.2+10.sha-96b99c4.with-local-changes";
+export declare const NGCC_VERSION = "13.0.0-rc.2+50.sha-0382f86.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
