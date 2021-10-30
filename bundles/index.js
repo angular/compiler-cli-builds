4721d3f54752bed1378a8c22e9367ac7447a4a37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-30 00:09:08 +0000

    3d1ac6a009 docs: update left-nav (#43964)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+169.sha-6247efa.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+185.sha-3d1ac6a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
