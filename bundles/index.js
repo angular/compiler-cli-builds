ec0843027d4efb66974400bd15eee10cab4632bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-01 16:45:33 +0000

    d015ac214f Revert "refactor(router): restore history in a consistent way on canceled navigations (#43651)" (#44006)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-next.14+185.sha-3d1ac6a.with-local-changes");
+var VERSION = new Version("13.0.0-next.14+186.sha-d015ac2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
