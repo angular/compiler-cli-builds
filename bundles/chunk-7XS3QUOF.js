676f2eb81f448de34ebcb3b9500a634987259ac9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 23:36:20 +0000

    8020457ce2 build(docs-infra): target webcontainers for stackblitz "live-example" links (#44041)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.0+1.sha-595a7e3.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.0+2.sha-8020457.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
