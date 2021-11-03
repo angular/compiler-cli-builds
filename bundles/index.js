3a9bad33be83971cd416cc8e2f286fc8bc55e97d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 17:10:48 +0000

    b6f2a55147 fix(migrations): prevent migrations from updating external templates multiple times (#44013)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0-rc.2+55.sha-d24cbce.with-local-changes");
+var VERSION = new Version("13.0.0-rc.3+3.sha-b6f2a55.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
