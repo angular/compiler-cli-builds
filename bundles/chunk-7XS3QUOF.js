5755872a30cdca6fea2919f617fd81e7e0781f09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:41:33 +0000

    a0c2e0780e docs: add blog post link to CHANGELOG (#44049)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.0+3.sha-96e84cc.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.0+4.sha-a0c2e07.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+3.sha-96e84cc.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+4.sha-a0c2e07.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
