f131a7c07d8cf90af8ec67c6d5ebf9441fdbb226
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:19:52 +0000

    96e84ccdcd docs: update CHANGELOG with deprecations (#44048)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+2.sha-8020457.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+3.sha-96e84cc.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
