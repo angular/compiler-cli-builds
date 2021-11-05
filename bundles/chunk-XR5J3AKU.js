d6595091ca45512dea258c3b931bbcd2ace99857
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:49:33 +0000

    64af4b9c1b docs: fix typo in the CHANGELOG (#44058)

--- bundles/chunk-XR5J3AKU.js
+++ bundles/chunk-XR5J3AKU.js
@@ -787,7 +787,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.0+28.sha-3507279.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.0+29.sha-64af4b9.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -834,7 +834,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.0-next.0+28.sha-3507279.with-local-changes") {
+    if (version === "13.1.0-next.0+29.sha-64af4b9.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
