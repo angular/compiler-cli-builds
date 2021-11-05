658853e2a8e757d9bb9bcdc11a475024bb814190
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:39:57 +0000

    58f453da15 docs: improve documentation (#44031) PR Close #44031

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.0+7.sha-ef15db0.with-local-changes");
+var VERSION = new Version("13.0.0+29.sha-58f453d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4458,7 +4458,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0+7.sha-ef15db0.with-local-changes";
+var NGCC_VERSION = "13.0.0+29.sha-58f453d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
