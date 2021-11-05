4e478f83e3195c1c2abf9542bfa04fbecc50cfc3
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:39:25 +0000

    22edf0263f docs: update install guide for angular elements (#44077)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+26.sha-bac03a9.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+23.sha-22edf02.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
