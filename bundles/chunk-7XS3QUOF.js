02b4539f3cafb6c4baf1ed516bb0fa6bc0785fcc
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:38:44 +0000

    bac03a95d7 ci: do not capture nested bazel integration test workspace within Bazel (#44061)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.0+22.sha-523e8fd.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.0+26.sha-bac03a9.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
