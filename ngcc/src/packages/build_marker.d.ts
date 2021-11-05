f9ac6361a62c6618382249f84b31846fb131e119
Andrew Scott <atscott01@gmail.com>
2021-11-05 21:48:31 +0000

    2c269c390a refactor(core): correct typing when getting the Nearest LContainer (#44087)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.0+36.sha-1543aae.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.0+39.sha-2c269c3.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
