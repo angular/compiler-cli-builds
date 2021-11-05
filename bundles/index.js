91f9ebb2f81cb8894626de41217395de30c5828b
Andrew Scott <atscott01@gmail.com>
2021-11-05 20:27:08 +0000

    1543aaedd6 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43998)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+31.sha-544c26d.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+36.sha-1543aae.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
