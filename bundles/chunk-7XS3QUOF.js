89c6b3464f3f46a9f46334825bbff3d784af4136
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:41:22 +0000

    49964c89e6 docs: add Angular 13 support information (#44098)

--- bundles/chunk-7XS3QUOF.js
+++ bundles/chunk-7XS3QUOF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.0+46.sha-baab456.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.0+47.sha-49964c8.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
