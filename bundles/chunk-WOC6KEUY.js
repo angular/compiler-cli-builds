dd8718ef2888c99dd8791bd9484d4fb9828a504a
Andrew Scott <atscott01@gmail.com>
2021-11-10 20:57:56 +0000

    4809bbd83c build: update angular (#44074)

--- bundles/chunk-WOC6KEUY.js
+++ bundles/chunk-WOC6KEUY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.0+75.sha-790dfe5.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.0+76.sha-4809bbd.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+75.sha-790dfe5.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+76.sha-4809bbd.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
