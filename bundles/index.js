b261fd033c529722b737633a83474fe7f3f43cac
Andrew Scott <atscott01@gmail.com>
2021-11-10 19:46:42 +0000

    790dfe5e25 docs: update review tag to topic (#44008)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.0+74.sha-393efa5.with-local-changes");
+var VERSION = new Version("13.1.0-next.0+75.sha-790dfe5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
