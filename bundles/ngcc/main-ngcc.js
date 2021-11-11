d3179bebe34774bdf7b37714892253409106c2bd
Andrew Scott <atscott01@gmail.com>
2021-11-11 00:02:43 +0000

    ca0873fca9 build: update to latest @angular/dev-infra-private to address issues related to verifying environment (#44138)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4468,7 +4468,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.0+73.sha-ee2031d.with-local-changes";
+var NGCC_VERSION = "13.0.0+78.sha-ca0873f.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
