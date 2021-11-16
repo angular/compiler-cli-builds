e2314fcaf1de024748fb6542481b6c6218f599bd
Andrew Kushnir <akushnir@google.com>
2021-11-16 22:27:06 +0000

    158ad09945 docs: Adding type annotation to ngOnInit() to make it consistent with the class structure produced by the CLI (#43579)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4469,7 +4469,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.1+6.sha-aa98cf1.with-local-changes";
+var NGCC_VERSION = "13.0.1+12.sha-158ad09.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
