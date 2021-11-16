35f572d2d302dfdceb67083f17f86a6b1a972d03
Andrew Kushnir <akushnir@google.com>
2021-11-16 18:34:08 +0000

    00ae55c3fb docs(animations): improve wording for animation example (#44168)

--- bundles/chunk-WOC6KEUY.js
+++ bundles/chunk-WOC6KEUY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.1+3.sha-b7fd9eb.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.1+6.sha-00ae55c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
