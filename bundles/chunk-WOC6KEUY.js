8907700bbd7d887fe03e99d9f4239e8a2dc28509
Andrew Kushnir <akushnir@google.com>
2021-11-17 17:36:12 +0000

    6c618de22e docs: Adding type annotation to ngOnInit() to make it consistent with the class structure produced by the CLI (#43579)

--- bundles/chunk-WOC6KEUY.js
+++ bundles/chunk-WOC6KEUY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.1+8.sha-5a24f96.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.1+12.sha-6c618de.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.1+8.sha-5a24f96.with-local-changes");
+var VERSION = new Version("13.1.0-next.1+12.sha-6c618de.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
