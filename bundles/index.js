2be942d81de454f47f613c85672f2caaa57445b4
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-18 22:49:12 +0000

    76833aa85d build(docs-infra): upgrade cli command docs sources to b8e198ba5 (#44212)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+12.sha-213105c.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+14.sha-76833aa.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
