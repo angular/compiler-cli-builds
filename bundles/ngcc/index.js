6b12738857f226b300db328b8c95affe849b3037
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-18 21:31:31 +0000

    427822e6c3 refactor(animations): improve some animations comments (#44203)

--- bundles/ngcc/index.js
+++ bundles/ngcc/index.js
@@ -4469,7 +4469,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.2+10.sha-16d026e.with-local-changes";
+var NGCC_VERSION = "13.0.2+11.sha-427822e.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
