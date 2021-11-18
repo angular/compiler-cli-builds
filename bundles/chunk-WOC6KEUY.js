1630b66920f362d0b7a3c243f4b223a46b8d3ad2
Andrew Kushnir <akushnir@google.com>
2021-11-18 18:36:19 +0000

    8330520a0d build: update dependency lighthouse to v9 (#44184)

--- bundles/chunk-WOC6KEUY.js
+++ bundles/chunk-WOC6KEUY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+7.sha-38c02e1.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+10.sha-8330520.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+7.sha-38c02e1.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+10.sha-8330520.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
