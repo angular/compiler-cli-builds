860527abb02f55c5add5624930af2852ee64a37b
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 20:25:55 +0000

    53bdbc6d2c build: update angular (#44221)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+21.sha-48ca7dc.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+24.sha-53bdbc6.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
