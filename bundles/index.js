22e0eee40a84cd05e83d069065d6ee3eabb267f3
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 19:01:51 +0000

    7e121e4b55 build: update dependency @angular-eslint/builder to v13 (#44222)

--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+18.sha-894aaea.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+16.sha-7e121e4.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
