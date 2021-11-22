db8f6d64dd8c55e131fdbf3430f3554bc28f5382
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-22 22:00:33 +0000

    d9c99f7d44 build: update dependency @angular-eslint/eslint-plugin-template to v13 (#44224)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.2+26.sha-6f5c0c1.with-local-changes");
+var VERSION = new Version("13.0.2+27.sha-d9c99f7.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4468,7 +4468,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.2+26.sha-6f5c0c1.with-local-changes";
+var NGCC_VERSION = "13.0.2+27.sha-d9c99f7.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
