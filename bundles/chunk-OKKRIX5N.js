0d3ad31e2aa1cc59d3b0fbf1be3e5a573118bae6
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 17:29:36 +0000

    40eb2f57f4 build: update dependency source-map-support to v0.5.21 (#44225)

--- bundles/chunk-OKKRIX5N.js
+++ bundles/chunk-OKKRIX5N.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+33.sha-a0177c6.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+34.sha-40eb2f5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.2+33.sha-a0177c6.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.2+34.sha-40eb2f5.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
