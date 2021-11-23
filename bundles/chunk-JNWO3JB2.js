b275d77e30f5a0a0b62f43256f9cbac5f73d2f06
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 18:55:11 +0000

    f44cb57c12 fix(router): prevent componentless routes from being detached (#44240)

--- bundles/chunk-JNWO3JB2.js
+++ bundles/chunk-JNWO3JB2.js
@@ -787,7 +787,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+34.sha-40eb2f5.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+36.sha-f44cb57.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -834,7 +834,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.0-next.2+34.sha-40eb2f5.with-local-changes") {
+    if (version === "13.1.0-next.2+36.sha-f44cb57.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
