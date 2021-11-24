706898600f242919d11308902de01814e6c236c2
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 00:22:38 +0000

    84ef01d9d2 refactor: delete the View Engine runtime (#44218)

--- bundles/linker/index.js
+++ bundles/linker/index.js
@@ -795,7 +795,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.0.2+32.sha-d265d0d.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.0.2+35.sha-84ef01d.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -842,7 +842,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.0.2+32.sha-d265d0d.with-local-changes") {
+    if (version === "13.0.2+35.sha-84ef01d.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
