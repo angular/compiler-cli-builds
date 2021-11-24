379293124d41d505c87ef1e4bc0fa79e2e5d987e
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:20:41 +0000

    2bf131a8a0 fix(docs-infra): move aio footer inside mat-sidenav-container (#44236)

--- bundles/chunk-XYWTIQAK.js
+++ bundles/chunk-XYWTIQAK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+40.sha-414d5fe.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+43.sha-2bf131a.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+40.sha-414d5fe.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+43.sha-2bf131a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
