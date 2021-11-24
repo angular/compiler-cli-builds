baaa6f2aae711cdd2d00f95ea168e8bda6f22e81
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:14:27 +0000

    b06e3981bc fix(core): add missing info about a component in the "pipe could not be found" error message (#44081)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.2+44.sha-6ae3858.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.2+50.sha-b06e398.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
