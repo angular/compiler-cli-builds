8ab634af9b66c052393cca936d53269d7b0fbe9a
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:37 +0000

    ef140a76d9 fix(docs-infra): fix the main content layout (#44282)

--- bundles/chunk-B7H7G4MU.js
+++ bundles/chunk-B7H7G4MU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+66.sha-708eb8c.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+65.sha-ef140a7.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
