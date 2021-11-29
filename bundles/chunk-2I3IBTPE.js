0476c4d34be3c0ba476dfe5dc78838000bedbe4a
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 17:52:55 +0000

    2c954bb75d docs: fix filename of the file containing the 'routes'-array in lazy loading guide (#44297)

--- bundles/chunk-2I3IBTPE.js
+++ bundles/chunk-2I3IBTPE.js
@@ -787,7 +787,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+55.sha-594bd12.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+54.sha-2c954bb.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -834,7 +834,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.0-next.2+55.sha-594bd12.with-local-changes") {
+    if (version === "13.1.0-next.2+54.sha-2c954bb.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+55.sha-594bd12.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+54.sha-2c954bb.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
