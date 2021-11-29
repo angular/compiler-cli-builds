7f3541638d721d9ebcfb003d8d5beb311c7421e5
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:39:44 +0000

    3aafa765ee fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- bundles/chunk-B7H7G4MU.js
+++ bundles/chunk-B7H7G4MU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+65.sha-ef140a7.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+67.sha-3aafa76.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+65.sha-ef140a7.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+67.sha-3aafa76.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
