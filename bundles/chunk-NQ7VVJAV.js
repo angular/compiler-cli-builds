90a7eaf52ad677c974386804b49932231c15efea
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 17:52:38 +0000

    594bd12d5d test(core): clean up tests relying on entryComponents (#44276)

--- bundles/chunk-NQ7VVJAV.js
+++ bundles/chunk-NQ7VVJAV.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+53.sha-b31973c.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+55.sha-594bd12.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
