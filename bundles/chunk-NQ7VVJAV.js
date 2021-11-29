5f897668be36bec1f1e2e297ebb85120a48a05ba
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 17:53:43 +0000

    47f1c0fa33 fix(docs-infra): ensure deprecated styling is not overridden (#44275)

--- bundles/chunk-NQ7VVJAV.js
+++ bundles/chunk-NQ7VVJAV.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+54.sha-2c954bb.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+56.sha-47f1c0f.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
