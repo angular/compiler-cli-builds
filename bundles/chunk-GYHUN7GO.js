3999c5238210c08a30b486edf5987f7f90d41cc2
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:35:35 +0000

    96dfc7af7c refactor(core): make the error messages tree shakable (#44219)

--- bundles/chunk-GYHUN7GO.js
+++ bundles/chunk-GYHUN7GO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+80.sha-f058109.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+82.sha-96dfc7a.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -103,7 +103,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.2+80.sha-f058109.with-local-changes");
+var VERSION = new Version("13.1.0-next.2+82.sha-96dfc7a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
