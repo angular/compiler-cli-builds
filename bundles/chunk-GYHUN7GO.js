ff29681bad3ee2c839ba020d08061dd6c6543df1
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 21:48:48 +0000

    a532040d97 Revert "feat(core): add migration to remove entryComponents (#44308)" (#44318)

--- bundles/chunk-GYHUN7GO.js
+++ bundles/chunk-GYHUN7GO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+83.sha-24baa5e.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+84.sha-a532040.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
