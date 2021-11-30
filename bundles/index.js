cc033a2b97f472860404db26c439f5b2d5add56c
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 21:58:44 +0000

    98430d678e refactor(forms): minor refactoring of `SelectMultipleControlValueAccessor` (#44261)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.2+76.sha-53bdf11.with-local-changes");
+var VERSION = new Version("13.0.2+78.sha-98430d6.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.0.2+76.sha-53bdf11.with-local-changes";
+export declare const NGCC_VERSION = "13.0.2+78.sha-98430d6.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
