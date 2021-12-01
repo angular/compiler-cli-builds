c6c1681ecef99c6215d90aab309e81836d7a42c2
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:31:32 +0000

    f1f6e7104f docs: release notes for the v13.0.3 release (#44333)

--- bundles/chunk-MFCLQBFK.js
+++ bundles/chunk-MFCLQBFK.js
@@ -787,7 +787,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+93.sha-e65a245.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.0-next.2+94.sha-f1f6e71.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -834,7 +834,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.0-next.2+93.sha-e65a245.with-local-changes") {
+    if (version === "13.1.0-next.2+94.sha-f1f6e71.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
