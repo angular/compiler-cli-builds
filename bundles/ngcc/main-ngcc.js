0f631a26fe37fb1ffb48ff18e2bd597d5dab4e27
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:21:47 +0000

    257a30d7b3 release: cut the v13.0.3 release (#44332)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4561,7 +4561,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.2+81.sha-0c9d33c.with-local-changes";
+var NGCC_VERSION = "13.0.2+82.sha-257a30d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
