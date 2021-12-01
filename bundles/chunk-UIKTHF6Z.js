0c4b7adc77b56ebbaccad718fa1e159ce285170f
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:12:28 +0000

    4192ab41bf build: update dependency saucelabs to v7 (#44321)

--- bundles/chunk-UIKTHF6Z.js
+++ bundles/chunk-UIKTHF6Z.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.2+89.sha-1922032.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.2+90.sha-4192ab4.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.2+89.sha-1922032.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.2+90.sha-4192ab4.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
