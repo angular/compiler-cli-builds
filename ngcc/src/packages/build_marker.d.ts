3d487b26297447f0afcd31833266c7200b27435e
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:04:55 +0000

    360867b654 build: update dependency tsec to ^0.2.0 (#44274)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.3+1.sha-f3898f8.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.3+2.sha-360867b.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
