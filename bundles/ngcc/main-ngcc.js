4b9ce1a33746685dae9a0d8f6e1e76d48a0fd97b
Andrew Kushnir <akushnir@google.com>
2021-12-02 20:11:17 +0000

    a8f96e8aaa build(docs-infra): update `.browserslistrc` to align with new CLI apps (#44352)

--- bundles/ngcc/main-ngcc.js
+++ bundles/ngcc/main-ngcc.js
@@ -4561,7 +4561,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.0.3+4.sha-7fb4309.with-local-changes";
+var NGCC_VERSION = "13.0.3+5.sha-a8f96e8.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
