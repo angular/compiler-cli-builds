6c2696cb2949486b7f7d2578ec129ad9ccad9a44
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:04:49 +0000

    927bfb9e7b build: update dependency eslint to v8 (#43789)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.3+5.sha-5109369.with-local-changes");
+var VERSION = new Version("13.1.0-next.3+6.sha-927bfb9.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
