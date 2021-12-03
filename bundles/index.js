3d45a7641ab7006bf80146485cb22754f5cd1f0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:06:01 +0000

    e8e21ac9d0 build: update dependency jsdom to v19 (#44362)

--- bundles/index.js
+++ bundles/index.js
@@ -446,7 +446,7 @@ import { StaticReflector, StaticSymbol } from "@angular/compiler";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.0.3+5.sha-a8f96e8.with-local-changes");
+var VERSION = new Version("13.0.3+7.sha-e8e21ac.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/metadata/collector.mjs
 import ts4 from "typescript";
