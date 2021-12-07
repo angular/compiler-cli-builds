ca857df50612efb806e9d148a2e6efe8f4b79603
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:23 +0000

    41265919aa fix(ngcc): correctly resolve UMD dependencies (#44381)

--- bundles/chunk-3E25K6DN.js
+++ bundles/chunk-3E25K6DN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.3+13.sha-52a73c6.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.3+10.sha-4126591.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
