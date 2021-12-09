8c00a740b04c1ae3b845df962b679adfe831dbd3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 18:29:59 +0000

    79baf7a87a Revert "release: cut the v13.2.0-next.0 release (#44421)"

--- bundles/chunk-3E25K6DN.js
+++ bundles/chunk-3E25K6DN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.3+26.sha-e3f4360.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.3+27.sha-79baf7a.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
