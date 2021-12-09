af42b9990705a2766dcaa27ac4ddbe98039063e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 18:42:48 +0000

    a7c117ab44 release: cut the v13.2.0-next.0 release (#44422)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0-next.3+27.sha-79baf7a.with-local-changes");
+var VERSION = new Version("13.1.0-next.3+28.sha-a7c117a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
