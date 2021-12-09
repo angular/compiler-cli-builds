0019073b3c821e6501f81992ec747172057ac102
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 17:18:27 +0000

    b11d3c3759 release: cut the v13.1.0 release (#44418)

--- bundles/chunk-3E25K6DN.js
+++ bundles/chunk-3E25K6DN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.0-next.3+24.sha-08694e3.with-local-changes";
+var NGCC_VERSION = "13.1.0-next.3+31.sha-b11d3c3.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.0-next.3+24.sha-08694e3.with-local-changes";
+export declare const NGCC_VERSION = "13.1.0-next.3+31.sha-b11d3c3.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
