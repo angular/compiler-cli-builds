5e11b2605c1a5fa27fc703392e7abf30d10f93b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:55:25 +0000

    7b6ca4c5d2 build: update angular (#44273)

--- bundles/chunk-3E25K6DN.js
+++ bundles/chunk-3E25K6DN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.0+3.sha-9249506.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.0+4.sha-7b6ca4c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+3.sha-9249506.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+4.sha-7b6ca4c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
