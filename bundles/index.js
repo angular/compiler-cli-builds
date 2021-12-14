2bf5335cadfa1746f32dd6d96fb81a9ed6e07042
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:19 +0000

    e4839939b0 test(ngcc): don't use the workspace typescript version to generate integration test fixtures (#44448)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+12.sha-12f7c85.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+14.sha-e483993.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
