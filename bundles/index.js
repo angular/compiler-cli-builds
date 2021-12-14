d5a7d8d265b62968e3424cdf8c2a868dfe4192e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:23:52 +0000

    12f7c85fd0 docs: fix typo (#44427)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+9.sha-38ddae1.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+12.sha-12f7c85.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.0+9.sha-38ddae1.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.0+12.sha-12f7c85.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
