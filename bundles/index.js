bd7b3cda484028981501c226bc318443ccac7d4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:26:47 +0000

    815d4ceeef fix(forms): I indroduced a minor error in a previous PR: pendingValue is a value not a boolean flag. (#44450)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+16.sha-6571799.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+15.sha-815d4ce.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
