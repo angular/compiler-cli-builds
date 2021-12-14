dca940ecacb65fe6d10ef1854593834ab9f4bdbe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:30 +0000

    ab2248e773 docs(docs-infra): change variable names in route guards examples (#44444)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.0+14.sha-e483993.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.0+13.sha-ab2248e.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
