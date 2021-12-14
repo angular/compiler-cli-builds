7de36be5d73ecc467eb8fa7380e7dc9d58830193
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:22:18 +0000

    030747a688 docs: update Aristeidis Bampakos bio (#44410) PR Close #44410

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+8.sha-bab7ed3.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+10.sha-030747a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
