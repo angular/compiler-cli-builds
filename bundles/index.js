5696feb2bcc7e946d8875ff96fa5e94f7d44d263
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:30:00 +0000

    f394831ec4 refactor(animations): remove the unused enter and leave selectors (#44460)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+20.sha-d640fa7.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+19.sha-f394831.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
