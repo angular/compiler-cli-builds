aba7618bcded327d7743a6a5604f605b155e7737
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 17:43:01 +0000

    6a0dadf46f fix(forms): `_reduceValue` arrow function now has correct types. (#44483)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+21.sha-093fd39.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+24.sha-6a0dadf.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
