8fb518fe869eff005ba161f4b1f02a83eb0bb8aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:37:05 +0000

    9921b2ca35 feat(core): Add a migration for typed forms. (#44449)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+25.sha-835987b.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+26.sha-9921b2c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
