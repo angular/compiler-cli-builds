bf07694e1cfca31ad783e2745a90d228ab1462c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:38:08 +0000

    c2ed1ae945 fix(docs-infra): update examples to Angular 13.1 (#44487)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.0+25.sha-bce108a.with-local-changes");
+var VERSION = new Version("13.1.0+26.sha-c2ed1ae.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
