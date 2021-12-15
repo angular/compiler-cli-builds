ebd2575e8b680b2d8c0bb8ffab005e1083ab10d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:37:55 +0000

    7c246d513b fix(docs-infra): update examples to Angular 13.1 (#44487)

--- bundles/chunk-DF2WBACT.js
+++ bundles/chunk-DF2WBACT.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.0+26.sha-9921b2c.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.0+27.sha-7c246d5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.0+26.sha-9921b2c.with-local-changes");
+var VERSION = new Version("13.2.0-next.0+27.sha-7c246d5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
