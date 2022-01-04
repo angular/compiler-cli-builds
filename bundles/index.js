f493a1a12e1ef171d12440291438df137e326e13
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 18:55:09 +0000

    58e8f5e06a ci: remove atscott for unavailable users (#44539)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+1.sha-8ebc946.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+2.sha-58e8f5e.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
