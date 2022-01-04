74669c9e85f9f9ae5bfe33129b2fb879d4779d2f
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:19:54 +0000

    af512deeed docs: fix invalid link of library support for npm packages guide (#44612)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+3.sha-c6c75c9.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+4.sha-af512de.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
