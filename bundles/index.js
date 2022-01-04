d2140ec640001b094b03cf385aa9352a6d54ae60
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:26:17 +0000

    756761d534 fix(language-service): revert the test files for Ivy (#44528)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+13.sha-7cb2999.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+12.sha-756761d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
