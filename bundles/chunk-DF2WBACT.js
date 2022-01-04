c5ea5d9628a0c3c98c967448c7d10eaeaa343381
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 18:55:07 +0000

    e6beeab7dd ci: remove atscott for unavailable users (#44539)

--- bundles/chunk-DF2WBACT.js
+++ bundles/chunk-DF2WBACT.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.1+1.sha-beb1828.with-local-changes";
+var NGCC_VERSION = "13.1.1+2.sha-e6beeab.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
