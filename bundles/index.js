2283a41788b0037d32bc33b5ccea59c363b74e2d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:22:04 +0000

    79d1afaf5b build: bump license year (#44590)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+5.sha-a01c077.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+8.sha-79d1afa.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
