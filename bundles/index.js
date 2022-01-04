bb6261f8dddb5e2fd106fb5e00f48ad22c2ac0f8
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:23:42 +0000

    4f7a838e9d docs: Add breaking changes note to 13.0 release (#44518)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+9.sha-62a5027.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+11.sha-4f7a838.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
