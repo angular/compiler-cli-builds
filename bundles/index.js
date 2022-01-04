f8421522f58001cf6e1309fc29b13b76de1c555d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:33:06 +0000

    3a29c57f60 test: update size goldens to reflect latest CLI devkit update (#44490)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+26.sha-f0b4c70.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+23.sha-3a29c57.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
