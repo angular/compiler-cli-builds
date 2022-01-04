cf4d628dafed17eec2f46fb05d541be02057e70d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:22:54 +0000

    62a5027482 docs: fixed meetup URL (#44585)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+8.sha-79d1afa.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+9.sha-62a5027.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
