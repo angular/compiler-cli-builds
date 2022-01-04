52a237705c6180b48cc572915fb85d1a39d39caf
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 19:17:08 +0000

    2469b88af2 docs: fix typo (#44592) PR Close #44592

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.1+2.sha-e6beeab.with-local-changes";
+export declare const NGCC_VERSION = "13.1.1+3.sha-2469b88.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
