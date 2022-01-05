16ac6ad731339f7b18753e393562ccb772902678
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:38:12 +0000

    e39f804f1e test(upgrade): fix flaky tests (#44597)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+61.sha-2d14724.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+60.sha-e39f804.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
