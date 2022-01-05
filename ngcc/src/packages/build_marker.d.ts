7c02264e6fb27c3ec57ca944bb4f912e67cb43bb
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:51 +0000

    2d14724965 docs(common): url link format updated (#44638)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.1+59.sha-12e0424.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.1+61.sha-2d14724.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
