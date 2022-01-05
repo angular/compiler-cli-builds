d68c726041fbfe8b89e36f9e29a69a51b9b28748
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:38 +0000

    12e042405b ci: temporarily disable components-repo-unit-tests job (#44505)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+52.sha-6a55aff.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+59.sha-12e0424.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.1+52.sha-6a55aff.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.1+59.sha-12e0424.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
