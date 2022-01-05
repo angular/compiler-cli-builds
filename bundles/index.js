3cf65aec51556d8e3bf8687d14d39545b2e86ab8
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:09:22 +0000

    d4015f762f test: remove newlines to make a test more stable (#44616)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+31.sha-f2b858d.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+30.sha-d4015f7.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
