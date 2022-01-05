55d76d20447965888f3664fe8513c0f709545623
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:04 +0000

    7278a9c584 docs(animations): fix filter-animations docregion (#44544)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+50.sha-d5ced77.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+51.sha-7278a9c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.1+50.sha-d5ced77.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.1+51.sha-7278a9c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
