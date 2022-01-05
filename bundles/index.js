caa28323576f96052743c21d7d504c901680a701
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:50:26 +0000

    d5ced77d31 refactor(docs-infra): fix eslint issues for aio ugrade-* examples (#44534)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+49.sha-c7ac2df.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+50.sha-d5ced77.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
