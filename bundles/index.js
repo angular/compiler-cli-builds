c80c0ef3f6b6c1093c6999e865813572e4e2b994
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:40 +0000

    359103274b test: clean up more entryComponents usages (#44561)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+65.sha-7f51f1d.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+64.sha-3591032.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
