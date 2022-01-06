53dacf81e1db1a27626f8ba80ab1fabb969b9aa6
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:40:08 +0000

    9b9042c8d1 docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.1+57.sha-98592b0.with-local-changes");
+var VERSION = new Version("13.1.1+58.sha-9b9042c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
