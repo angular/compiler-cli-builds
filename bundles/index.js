50c4cdbaccfb0696c8ecc0d3422f744534410ab2
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:14 +0000

    7f51f1dcae docs(docs-infra): improve the content of attribute directives (#44628)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+62.sha-b69a514.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+65.sha-7f51f1d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
