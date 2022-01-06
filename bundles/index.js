382f7159d49ad1c24660bb1375d4e19a55384199
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:56:27 +0000

    5017bf92fe docs(animations): fix grammar (#44607)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.1+58.sha-9b9042c.with-local-changes");
+var VERSION = new Version("13.1.1+62.sha-5017bf9.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
