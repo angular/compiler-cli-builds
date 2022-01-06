9dd020f0dc4164a77b10fc8e71fa22b4520001ba
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:39:45 +0000

    ea91edb55a docs: fix typos in using libraries guide (#44634)

--- bundles/chunk-C626OOH7.js
+++ bundles/chunk-C626OOH7.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.1+60.sha-e39f804.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.1+63.sha-ea91edb.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
