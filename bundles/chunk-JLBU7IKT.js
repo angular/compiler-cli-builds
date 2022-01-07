33b33dd44752a65d3fbb11a28656fd2ddce3f971
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:27:49 +0000

    c57e22a21b refactor(docs-infra): remove IE11 code (#44570)

--- bundles/chunk-JLBU7IKT.js
+++ bundles/chunk-JLBU7IKT.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.1+81.sha-277d2af.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.1+83.sha-c57e22a.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
