26f00952bead1632a666a181264a1efb52309457
Andrew Scott <atscott01@gmail.com>
2022-01-07 19:33:04 +0000

    919f8ae4e8 build: update dependency google-closure-compiler to v20220104 (#44655)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.1+78.sha-63c8e56.with-local-changes");
+var VERSION = new Version("13.1.1+79.sha-919f8ae.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.1+78.sha-63c8e56.with-local-changes";
+export declare const NGCC_VERSION = "13.1.1+79.sha-919f8ae.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
