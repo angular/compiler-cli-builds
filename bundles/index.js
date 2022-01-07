9f0e36043a578bfb26b0b0f38333bbc9511f667c
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:35 +0000

    abc217b28e fix(animations): retain triggers values for moved tracked list items (#44578)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.1+79.sha-919f8ae.with-local-changes");
+var VERSION = new Version("13.1.1+80.sha-abc217b.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
