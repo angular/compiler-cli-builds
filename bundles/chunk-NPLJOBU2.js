587636abb7e885b2427d1421264a8224a2263ddc
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:25:37 +0000

    21f8d12c3c release: cherry-pick the v12.2.15 release notes (#44619)

--- bundles/chunk-NPLJOBU2.js
+++ bundles/chunk-NPLJOBU2.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.1+77.sha-0cf5501.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.1+79.sha-21f8d12.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+77.sha-0cf5501.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+79.sha-21f8d12.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
