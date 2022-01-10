664e9e8fabf6e8b9da8d51ef30cdbb7e87cd40eb
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:00:18 +0000

    5a4ddfd4f5 feat(router): Allow symbol keys for `Route` `data` and `resolve` properties (#44519)

--- bundles/chunk-W5MCEQ45.js
+++ bundles/chunk-W5MCEQ45.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.1+86.sha-8ba43a1.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.1+87.sha-5a4ddfd.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+86.sha-8ba43a1.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+87.sha-5a4ddfd.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
