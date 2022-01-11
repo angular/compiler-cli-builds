3c171ffed51943da7cefff5b7a29d8d64d00404b
Andrew Scott <atscott01@gmail.com>
2022-01-11 22:20:05 +0000

    de0ae1b36a build(docs-infra): upgrade cli command docs sources to 9ac3df5d1 (#44682)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+105.sha-92b23f4.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+106.sha-de0ae1b.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
