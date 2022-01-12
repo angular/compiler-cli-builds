3df69523f3c83332599c4dcf3e8e163c8f015e53
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:57:20 +0000

    57d0ca18e7 ci: add golden files for runtime error codes (#44677)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+1.sha-c888dfd.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+3.sha-57d0ca1.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
