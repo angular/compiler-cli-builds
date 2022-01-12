a76928c608118da47f3255a16bc2baef4fa5290b
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:56:40 +0000

    c888dfdea0 docs: add Decoded Frontend YT channel to resources list (#44278)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+109.sha-42ce3ac.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+1.sha-c888dfd.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.1+109.sha-42ce3ac.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.2+1.sha-c888dfd.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
