f4ea0287ad22763b9791f3529dd77e647dd191a6
GitHub <noreply@github.com>
2022-01-12 19:11:50 +0000

    555a57d505 docs: release notes for the v13.1.2 release (#44699)

--- bundles/chunk-NU7DCJLY.js
+++ bundles/chunk-NU7DCJLY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.1+107.sha-e98b8f5.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.1+108.sha-555a57d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.1+107.sha-e98b8f5.with-local-changes");
+var VERSION = new Version("13.2.0-next.1+108.sha-555a57d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
