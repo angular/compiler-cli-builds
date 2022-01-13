99651d95ce0f3c8378f145098455d265cd2e8742
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:20:14 +0000

    af0a152a2c fix(animations): apply setStyles to only rootTimelines (#44515)

--- bundles/chunk-UESD6FVI.js
+++ bundles/chunk-UESD6FVI.js
@@ -785,7 +785,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.2+10.sha-44ff4fb.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.2+12.sha-af0a152.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -832,7 +832,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.2+10.sha-44ff4fb.with-local-changes") {
+    if (version === "13.1.2+12.sha-af0a152.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.2+10.sha-44ff4fb.with-local-changes");
+var VERSION = new Version("13.1.2+12.sha-af0a152.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
