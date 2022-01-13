cc41f8bae16648fcb80256e01ab714333f4f2cd7
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:18:13 +0000

    c2da290659 feat(docs-infra): Add a new processor for `@overriddenImplementation`. (#44689)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+10.sha-f06b0d9.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+13.sha-c2da290.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
