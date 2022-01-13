e1e81b709cf4e0b4bb4225c346cb4d813078971a
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:18:17 +0000

    d460e02d9e build: update all non-major dependencies (#44705)

--- bundles/chunk-NU7DCJLY.js
+++ bundles/chunk-NU7DCJLY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.2+13.sha-c2da290.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.2+11.sha-d460e02.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+13.sha-c2da290.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+11.sha-d460e02.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
