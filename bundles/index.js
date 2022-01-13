266ee748877f3459f3fba7a32f16cd5542b191bb
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:43:39 +0000

    432e518931 refactor(elements): remove the createCustomEvent function (#44703)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+6.sha-2189588.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+5.sha-432e518.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.2+6.sha-2189588.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.2+5.sha-432e518.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
