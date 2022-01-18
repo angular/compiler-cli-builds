b4f7bf417e6e09382f25fcb889bc9f441d712dbc
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 18:47:45 +0000

    4307b82058 refactor(common): make the error messages tree shakable (#44663)

--- bundles/chunk-B7OIMLXY.js
+++ bundles/chunk-B7OIMLXY.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.2+17.sha-ccb73c0.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.2+18.sha-4307b82.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+17.sha-ccb73c0.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+18.sha-4307b82.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
