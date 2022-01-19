571bf7953d55c9debac96ba983b065bc03f13c16
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 17:26:41 +0000

    b49ffcd50e feat(core): allow for injector to be specified when creating an embedded view (#44666)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+26.sha-fb27867.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+27.sha-b49ffcd.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
