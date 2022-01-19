d4ef90db84c821ea8497f98fb3e45c1addade1ae
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 17:47:52 +0000

    7cb3b78acc release: cut the v13.1.3 release (#44757)

--- bundles/chunk-22IGP26G.js
+++ bundles/chunk-22IGP26G.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.2+21.sha-3797d10.with-local-changes";
+var NGCC_VERSION = "13.1.2+22.sha-7cb3b78.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.2+21.sha-3797d10.with-local-changes");
+var VERSION = new Version("13.1.2+22.sha-7cb3b78.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
