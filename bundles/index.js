cf10630949ac71c118f4972468e03df861b59aa1
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 17:41:19 +0000

    34d2292323 docs: update deprecated form of the tap operator in http example (#44738)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+27.sha-b49ffcd.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+28.sha-34d2292.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
