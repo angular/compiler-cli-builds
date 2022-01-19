7c89aac1b01419a1d081ae5f557431338f17b56a
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 04:10:52 +0000

    fb27867ab8 Revert "refactor(forms): Move FormControl to an overridden exported constructor. (#44316)" (#44750)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+25.sha-bc03a2e.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+26.sha-fb27867.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
