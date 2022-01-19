db81ab06aed1bcde133743853552c276d0eb0602
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:35:34 +0000

    41b8ecc9b8 refactor(forms): Make a couple small cleanups encountered while rebasing typed forms. (#44748)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+23.sha-ece530f.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+24.sha-41b8ecc.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
