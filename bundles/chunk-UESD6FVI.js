a30bc9aa29f953976207701ee7f34251b189ed42
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:08:28 +0000

    929788d9a8 refactor(animations): change errors type from any to string (#44726)

--- bundles/chunk-UESD6FVI.js
+++ bundles/chunk-UESD6FVI.js
@@ -785,7 +785,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.1.2+19.sha-e9bd16a.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.1.2+20.sha-929788d.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -832,7 +832,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.1.2+19.sha-e9bd16a.with-local-changes") {
+    if (version === "13.1.2+20.sha-929788d.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
