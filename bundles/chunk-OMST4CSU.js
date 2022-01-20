b6505440b145d75fb38b6302c055d0a9f93542dd
Joey Perrott <josephperrott.github@gmail.com>
2022-01-20 02:01:05 +0000

    3c14eb559f docs: release notes for the v13.2.0-rc.0 release

--- bundles/chunk-OMST4CSU.js
+++ bundles/chunk-OMST4CSU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.2+35.sha-ed21f5c.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.2+37.sha-3c14eb5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
