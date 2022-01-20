01da6a9e7cf38071ea7c404afe5fa40bb56f7de2
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 17:38:20 +0000

    011d0f27b8 docs: correct (possible) typo (#44759)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+37.sha-3c14eb5.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+38.sha-011d0f2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
