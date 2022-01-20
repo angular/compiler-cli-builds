777ee612848e2215035e9003f78187b680fea713
Andrew Kushnir <akushnir@google.com>
2022-01-20 19:32:59 +0000

    b7ff6f06de refactor(compiler): pass rootDir to tsickle (#44768)

--- bundles/chunk-OMST4CSU.js
+++ bundles/chunk-OMST4CSU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-rc.0+1.sha-6b75263.with-local-changes";
+var NGCC_VERSION = "13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-rc.0+1.sha-6b75263.with-local-changes");
+var VERSION = new Version("13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
