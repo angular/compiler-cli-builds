04b9dd10630d8c96ce68ca992c7fe2736f71cc64
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 01:04:13 +0000

    d509a0d75f fix(docs-infra): fix date parsing in a flaky test (#44763)

--- bundles/chunk-22IGP26G.js
+++ bundles/chunk-22IGP26G.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.2+22.sha-7cb3b78.with-local-changes";
+var NGCC_VERSION = "13.1.3+1.sha-d509a0d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
