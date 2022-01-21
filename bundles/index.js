977434d3c0a43bd29a5197a4d019b152801acc84
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:35:11 +0000

    fb9163ae50 fix(core): consistently use namespace short name rather than URI (#44766)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+40.sha-197f3f4.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+43.sha-fb9163a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
