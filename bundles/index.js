d4725404532eb598e06900e169945e359d738747
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:39:38 +0000

    e2920d98aa build: update all non-major dependencies (#44713)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+44.sha-d8bff1a.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+45.sha-e2920d9.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
