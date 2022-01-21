67e2db490e7226c4a2a43ec52d62256c3731c1ea
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:38:48 +0000

    ef5486fab5 build: update all non-major dependencies (#44713)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.3+4.sha-a9f5c87.with-local-changes");
+var VERSION = new Version("13.1.3+7.sha-ef5486f.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.1.3+4.sha-a9f5c87.with-local-changes";
+export declare const NGCC_VERSION = "13.1.3+7.sha-ef5486f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
