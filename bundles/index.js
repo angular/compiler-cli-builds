d9c4b0a8184d82ce38babef923c125f048210cf0
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:35:45 +0000

    9d4af65e34 feat(language-service): Provide plugin to delegate rename requests to Angular (#44696)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+43.sha-fb9163a.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+41.sha-9d4af65.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
