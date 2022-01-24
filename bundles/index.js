75bdc5c8b8e7cb654e6f7623a80c3e17d361f57d
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:07:28 +0000

    ec0a0e0669 feat(service-worker): add `cacheOpaqueResponses` option for data-groups (#44723)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+58.sha-a534a78.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+62.sha-ec0a0e0.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.2+58.sha-a534a78.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.2+62.sha-ec0a0e0.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
