53980dae8667e2865d3b1b8f810dbfc43c5df870
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:11:25 +0000

    7e93513425 docs: add link to AbstractControlOptions reference (#44764) PR Close #44764

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-rc.1+9.sha-a4aa9b3.with-local-changes");
+var VERSION = new Version("13.2.0-rc.1+22.sha-7e93513.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
