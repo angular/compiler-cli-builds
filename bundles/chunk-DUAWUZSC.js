c75ecad30c4581c13e16953674a67313aa9d460a
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:10:25 +0000

    6a68bab58e docs: add link to AbstractControlOptions reference (#44764) PR Close #44764

--- bundles/chunk-DUAWUZSC.js
+++ bundles/chunk-DUAWUZSC.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-next.2+63.sha-c9fcc4f.with-local-changes";
+var NGCC_VERSION = "13.2.0-next.2+74.sha-6a68bab.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+63.sha-c9fcc4f.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+74.sha-6a68bab.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
