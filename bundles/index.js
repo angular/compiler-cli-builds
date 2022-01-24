bfecac56631e6f4ee0638391d99af1b654bb13de
Andrew Kushnir <akushnir@google.com>
2022-01-24 18:59:17 +0000

    8363db4189 docs(router): fixing `pathMatch` doc to style code correctly (#44796) PR Close #44796

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-next.2+54.sha-ec2e6f6.with-local-changes");
+var VERSION = new Version("13.2.0-next.2+55.sha-8363db4.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0-next.2+54.sha-ec2e6f6.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0-next.2+55.sha-8363db4.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
