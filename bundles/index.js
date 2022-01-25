b481f5f4e5a6373c33a058dc6774b4de6b6afb3d
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:41:32 +0000

    6cb7c3ebe3 build(docs-infra): update `dgeni-packages` to v0.29.3 (#44810)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-rc.1+22.sha-7e93513.with-local-changes");
+var VERSION = new Version("13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
