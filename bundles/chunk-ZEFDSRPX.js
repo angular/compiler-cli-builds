338517cc71a180241a1fd8b524ffc31059c386f4
Andrew Kushnir <akushnir@google.com>
2022-01-25 01:26:30 +0000

    dd7364fc16 ci: update payload size limit for the Forms test app (#44813)

--- bundles/chunk-ZEFDSRPX.js
+++ bundles/chunk-ZEFDSRPX.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.1.3+13.sha-a52685b.with-local-changes";
+var NGCC_VERSION = "13.1.3+27.sha-dd7364f.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.1.3+13.sha-a52685b.with-local-changes");
+var VERSION = new Version("13.1.3+27.sha-dd7364f.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
