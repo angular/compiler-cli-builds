90db8d5986214d5a4bb322b8d1a9d2957275b654
Andrew Kushnir <akushnir@google.com>
2022-01-26 21:09:40 +0000

    112a6cdec3 test(animations): Add bundling size regression test for animations package (#44826)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1.sha-e46ac81.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+2.sha-112a6cd.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
