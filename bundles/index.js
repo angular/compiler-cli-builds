dae896aab9a8b265116240576db6a91eb7da0395
Joey Perrott <josephperrott@gmail.com>
2022-01-26 22:36:50 +0000

    8fb3aed874 docs(devtools): update comment to better reflect what angular version is running in g3.

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+2.sha-112a6cd.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1015.sha-8fb3aed.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
