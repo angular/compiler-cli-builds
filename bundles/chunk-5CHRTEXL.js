d0605c517cb0382ec7d78bced27a78a30f4f893a
GitHub <noreply@github.com>
2022-01-26 20:01:51 +0000

    de1e35b944 release: cut the v13.2.0 release (#44838)

--- bundles/chunk-5CHRTEXL.js
+++ bundles/chunk-5CHRTEXL.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0-rc.1+26.sha-a38ae0a.with-local-changes";
+var NGCC_VERSION = "13.2.0-rc.1+27.sha-de1e35b.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/chunk-UESD6FVI.js
+++ bundles/chunk-UESD6FVI.js
@@ -785,7 +785,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.2.0-rc.1+26.sha-a38ae0a.with-local-changes");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.2.0-rc.1+27.sha-de1e35b.with-local-changes");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -832,7 +832,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.2.0-rc.1+26.sha-a38ae0a.with-local-changes") {
+    if (version === "13.2.0-rc.1+27.sha-de1e35b.with-local-changes") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
