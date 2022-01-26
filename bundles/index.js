0e3ab4fb57917d81730c076fd1d891233ab018b7
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:52:20 +0000

    dc0cac235f docs: cleanup CHANGELOG.md file (#44842)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-rc.1+27.sha-de1e35b.with-local-changes");
+var VERSION = new Version("13.2.0+1.sha-dc0cac2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
