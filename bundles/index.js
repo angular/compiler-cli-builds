c0b8fe2a1af515c93acfd896b7462de6ed540826
Andrew Kushnir <akushnir@google.com>
2022-01-26 01:20:07 +0000

    a38ae0aefa refactor(forms): Temporarily disable the typed forms migration. (#44828)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes");
+var VERSION = new Version("13.2.0-rc.1+26.sha-a38ae0a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
