f62c569456b29a1306b349b50512a2668c484db4
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-27 22:11:03 +0000

    7b7d644286 refactor: fix type checking error with devtools state-serializer spec (#44735)

--- bundles/chunk-45VX3OX3.js
+++ bundles/chunk-45VX3OX3.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1028.sha-c12110f.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1027.sha-7b7d644.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
