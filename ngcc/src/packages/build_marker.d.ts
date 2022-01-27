a948c6031f7373ab804310d6dbb097a02c9b78eb
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-27 22:08:48 +0000

    c12110f2f8 docs: update survey id (#44865)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1023.sha-1312ac1.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1028.sha-c12110f.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
