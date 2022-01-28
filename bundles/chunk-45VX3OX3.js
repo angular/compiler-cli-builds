f60042c03efcd671cab80e34399b91728f0aa604
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:25:18 +0000

    c577a1dc93 fix(docs-infra): fix links to CLI commands source code (#44835)

--- bundles/chunk-45VX3OX3.js
+++ bundles/chunk-45VX3OX3.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1031.sha-f1a5145.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1032.sha-c577a1d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
