53ba53c9a5bcc2a014d74c0235e70cd206ba2d2b
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 17:53:02 +0000

    f398717502 docs: fix typo in update guide (#44853)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0+13.sha-c9abf34.with-local-changes");
+var VERSION = new Version("13.2.0+14.sha-f398717.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
