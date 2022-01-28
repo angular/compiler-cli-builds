a7d1e701e07a2920830539bef9907c6efcb4ee28
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 18:19:20 +0000

    9229511b1d build: bump version for `@angular/benchpress` to v0.2.2 (#44879)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1036.sha-c2e09e0.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1037.sha-9229511.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
