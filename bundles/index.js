85ed2d96e7dc64dfbe7f2ef9df38cad0db413957
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 22:15:29 +0000

    6b7fffcbeb fix(forms): Update the typed forms migration schematic to find all files. (#44881)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0+18.sha-641f27a.with-local-changes");
+var VERSION = new Version("13.2.0+19.sha-6b7fffc.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0+18.sha-641f27a.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0+19.sha-6b7fffc.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
