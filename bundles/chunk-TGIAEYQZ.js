79ad7bbfb369bc72912e8fce2b6c4fc30bb4f582
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:48:50 +0000

    8fb2e3b71f docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- bundles/chunk-TGIAEYQZ.js
+++ bundles/chunk-TGIAEYQZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0+28.sha-07185f4.with-local-changes";
+var NGCC_VERSION = "13.2.0+34.sha-8fb2e3b.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.0+28.sha-07185f4.with-local-changes";
+export declare const NGCC_VERSION = "13.2.0+34.sha-8fb2e3b.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
