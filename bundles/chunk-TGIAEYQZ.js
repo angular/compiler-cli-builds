4c34db6c5877d361dbae705baa6c07c2293c3063
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 23:02:03 +0000

    4644886aaf perf(animations): remove no longer needed CssKeyframes classes (#44903) (#44919)

--- bundles/chunk-TGIAEYQZ.js
+++ bundles/chunk-TGIAEYQZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0+39.sha-f5ce6cf.with-local-changes";
+var NGCC_VERSION = "13.2.0+40.sha-4644886.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0+39.sha-f5ce6cf.with-local-changes");
+var VERSION = new Version("13.2.0+40.sha-4644886.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
