5131209e3bb81ea7416f98ccba017d17d20b8d6d
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:48:54 +0000

    5e099b5e6c docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1061.sha-7155e9e.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1062.sha-5e099b5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1061.sha-7155e9e.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1062.sha-5e099b5.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
