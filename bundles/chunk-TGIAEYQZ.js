a29b03bf80bb352f6427ed8681e8c0601162ba0f
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:55:28 +0000

    9eff8908af refactor(core): strict templates type-checking compatibility for perf tests (#44905)

--- bundles/chunk-TGIAEYQZ.js
+++ bundles/chunk-TGIAEYQZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1065.sha-889de82.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1066.sha-9eff890.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
