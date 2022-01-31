b05eaeb716e581ec9e6be065e6324fbf375246a4
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:17:45 +0000

    a8686c64b4 build: update dependency @types/chrome to ^0.0.177 (#44889)

--- bundles/chunk-YNDUWN2K.js
+++ bundles/chunk-YNDUWN2K.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1046.sha-e62f010.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1047.sha-a8686c6.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1046.sha-e62f010.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1047.sha-a8686c6.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
