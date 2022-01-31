9be8dbf6b5e99130170f4d306574ad9e0ec2936e
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:17:06 +0000

    d614a97af9 build: update legacy saucelabs bundle generation to account for esbuild update (#44830)

--- bundles/chunk-45VX3OX3.js
+++ bundles/chunk-45VX3OX3.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1038.sha-deb9531.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1041.sha-d614a97.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
