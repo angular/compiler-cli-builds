d3ab1fb9950b906172138b66c289f74157978fcc
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:22:59 +0000

    390a653b7b build: update dependency d3 to v7 (#44892)

--- bundles/chunk-YNDUWN2K.js
+++ bundles/chunk-YNDUWN2K.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1048.sha-dea30d4.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1049.sha-390a653.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
