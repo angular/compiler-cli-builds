0972f2b64b333939180a2c3d8e35c84906fa2143
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:47:11 +0000

    4c83395508 build: update all non-major dependencies (#44855)

--- bundles/chunk-TGIAEYQZ.js
+++ bundles/chunk-TGIAEYQZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1059.sha-53a4a03.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1060.sha-4c833955.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1059.sha-53a4a03.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1060.sha-4c833955.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
