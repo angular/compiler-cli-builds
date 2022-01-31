72e95b6f11f5d61f758b95705c532fd7c6a698a9
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:54:17 +0000

    39c614c438 fix(core): flush delayed scoping queue while setting up TestBed (#44814)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1063.sha-6642e3c.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1064.sha-39c614c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
