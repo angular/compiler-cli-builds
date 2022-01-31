a4b1ec2d11ae69d2a6241abed518204179152028
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:53:45 +0000

    10092f24fc build: fix formatting in zone.js BUILD file (#44912)

--- bundles/chunk-45VX3OX3.js
+++ bundles/chunk-45VX3OX3.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1042.sha-0650066.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1043.sha-10092f2.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1042.sha-0650066.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1043.sha-10092f2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
