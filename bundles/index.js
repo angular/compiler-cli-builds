6bdb8c6b263574d2c55b05e6e74b9495e22c972e
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:42:40 +0000

    53a4a03144 build: bump version for `@angular/benchpress` to v0.3.0 (#44893)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1057.sha-366e424.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1059.sha-53a4a03.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1057.sha-366e424.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1059.sha-53a4a03.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
