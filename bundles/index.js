f3a0ddf3180e8d49f3f388d1736b622f5d241a4f
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:02:05 +0000

    3f0a2d3120 fix(devtools): show date type property value in preview (#44864)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1044.sha-b1b4547.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1045.sha-3f0a2d3.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
