9967d495220e79852db620d9df46b13f89237431
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:56:28 +0000

    f5ce6cfa1e refactor(compiler-cli): remove unused `canonical-path` dependency (#44918)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0+34.sha-8fb2e3b.with-local-changes");
+var VERSION = new Version("13.2.0+39.sha-f5ce6cf.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
