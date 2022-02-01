6f590db9c79481300b5fd58a4c6848c06e936e92
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 21:44:41 +0000

    d41dedb85b fix(devtools): highlighter should only show the hovered component's name (#44863)

--- bundles/chunk-R5CO7F4W.js
+++ bundles/chunk-R5CO7F4W.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1079.sha-c89cf63.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1081.sha-d41dedb.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
