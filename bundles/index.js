64ea00fc1a34ddc05daef63272a21d82988492ff
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 00:32:26 +0000

    2916ac9e1a refactor(core): the `RuntimeError` class should support more compact syntax (#44783)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.0+40.sha-4644886.with-local-changes");
+var VERSION = new Version("13.2.0+41.sha-2916ac9.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
