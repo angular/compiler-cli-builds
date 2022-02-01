ad79f5e0d816349fee9bf2dbcb264970af880e73
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 04:08:14 +0000

    bc61cc2449 refactor(migrations): remove old migrations (#44857)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1071.sha-ed1732c.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1072.sha-bc61cc2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.0+1071.sha-ed1732c.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.0+1072.sha-bc61cc2.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
