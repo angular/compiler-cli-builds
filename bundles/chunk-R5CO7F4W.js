75cb9b50bae3ef8d607f6dbf2866691eca048eca
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:33:06 +0000

    5d7ffd5c0c docs: correct the demo code of NgZone.run (#44733)

--- bundles/chunk-R5CO7F4W.js
+++ bundles/chunk-R5CO7F4W.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1075.sha-e0a340e.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1077.sha-5d7ffd5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.0+1075.sha-e0a340e.with-local-changes");
+var VERSION = new Version("14.0.0-next.0+1077.sha-5d7ffd5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
