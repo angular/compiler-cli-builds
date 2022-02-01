27b35c9806d6e656ba6779057745304d165a9caf
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 23:56:14 +0000

    5a75721835 build: fix test for useUnknownInCatch (#44935)

--- bundles/chunk-PMUO5RFT.js
+++ bundles/chunk-PMUO5RFT.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0+49.sha-812c1ba.with-local-changes";
+var NGCC_VERSION = "13.2.0+52.sha-5a75721.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
