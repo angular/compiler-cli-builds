9cbd634e37886543c530ddd55c40b72b4ad4e55f
Dylan Hunn <dylhunn@gmail.com>
2022-02-02 23:53:05 +0000

    9cce12cbf5 refactor(compiler): add `@suppress {msgDescriptions}` if no description is present on an i18n message (#44787)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.1+3.sha-b9aab0c.with-local-changes";
+export declare const NGCC_VERSION = "13.2.1+4.sha-9cce12c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
