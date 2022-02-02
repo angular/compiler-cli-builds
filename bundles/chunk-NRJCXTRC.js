f9ea243a8907995dc0975231c8cb690630929722
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 17:24:36 +0000

    98a1da9382 build(docs-infra): upgrade cli command docs sources to 0f51d5c70 (#44943)

--- bundles/chunk-NRJCXTRC.js
+++ bundles/chunk-NRJCXTRC.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.0+56.sha-165aa4f.with-local-changes";
+var NGCC_VERSION = "13.2.0+57.sha-98a1da9.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
