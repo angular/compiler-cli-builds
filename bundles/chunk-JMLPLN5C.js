31fb016699f59dbd8b55f5296a4115b91ce8ca06
GitHub <noreply@github.com>
2022-02-02 20:08:26 +0000

    0c36f82134 release: cut the v14.0.0-next.1 release (#44951)

--- bundles/chunk-JMLPLN5C.js
+++ bundles/chunk-JMLPLN5C.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.0+1092.sha-38bb2be.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.0+1093.sha-0c36f82.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
