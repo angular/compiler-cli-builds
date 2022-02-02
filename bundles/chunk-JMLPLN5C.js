4798f289266d28d5bcd71aace624a35557c504c5
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:21:11 +0000

    a01bcb8e7e fix(platform-browser): do not run change detection when loading Hammer (#44921)

--- bundles/chunk-JMLPLN5C.js
+++ bundles/chunk-JMLPLN5C.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+1.sha-39b90b4.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+2.sha-a01bcb8.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
