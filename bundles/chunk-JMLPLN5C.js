19db4e4549bacee56ae8baea2d48ab4892b30a0a
Dylan Hunn <dylhunn@gmail.com>
2022-02-02 23:49:35 +0000

    85ba38aecb refactor(compiler): add `@suppress {msgDescriptions}` if no description is present on an i18n message (#44787)

--- bundles/chunk-JMLPLN5C.js
+++ bundles/chunk-JMLPLN5C.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+6.sha-bf42877.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+7.sha-85ba38a.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
