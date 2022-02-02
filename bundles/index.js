4852dfc7e0b5b146c53bd7fa60f61c3451689975
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:27:23 +0000

    202a1a5631 fix(upgrade): Do not trigger duplicate navigation events from Angular Router (#43441)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+2.sha-a01bcb8.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+4.sha-202a1a5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
