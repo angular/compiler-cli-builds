18c1c97619cb191ae24bcc7495c3928375f9f9d7
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:58:11 +0000

    226fac67bd docs: describe your change... (#42899)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+18.sha-f7ea275.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+19.sha-226fac6.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
