601d0fc3dae9c3d81496d69aa44ed15a0e9f9c46
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 17:09:59 +0000

    1aae414b36 docs: remove duplicate closing tags in View Encapsulation file (#44958)

--- bundles/chunk-JMLPLN5C.js
+++ bundles/chunk-JMLPLN5C.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+8.sha-077593f.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+9.sha-1aae414.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+8.sha-077593f.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+9.sha-1aae414.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
