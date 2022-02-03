464cbe7a3dfeae7bc55ba0cc9b179a3ff097918e
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:11:46 +0000

    1b216297f4 docs: update input-output-diagram.svg (#39900)

--- bundles/chunk-NRJCXTRC.js
+++ bundles/chunk-NRJCXTRC.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.1+13.sha-f092bad.with-local-changes";
+var NGCC_VERSION = "13.2.1+15.sha-1b21629.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.1+13.sha-f092bad.with-local-changes");
+var VERSION = new Version("13.2.1+15.sha-1b21629.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
