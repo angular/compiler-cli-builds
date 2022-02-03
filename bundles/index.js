0a549b33eba6cc3bdc3f9b29af29834b80e948fe
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 01:50:13 +0000

    077593f3bc docs: remove old commits from CHANGELOG.md (#44956)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+7.sha-85ba38a.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+8.sha-077593f.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
