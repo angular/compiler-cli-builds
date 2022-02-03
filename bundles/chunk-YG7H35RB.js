bba8d77b3301ff14fcb12ccb9296233c7a8a8503
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:56:43 +0000

    48d430d7c2 docs: remove item from changelog that does not work (#44961)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+16.sha-791bd31.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+14.sha-48d430d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+16.sha-791bd31.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+14.sha-48d430d.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
