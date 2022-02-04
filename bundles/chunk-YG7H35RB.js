a8506df6d1ab334c30c46acd3ef480a2956de8da
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 07:05:08 +0000

    b32647dc68 fix(platform-browser): Make transfer state key typesafe. (#23020)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+28.sha-64f837d.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+29.sha-b32647d.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
