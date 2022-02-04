7f5edb141893e780bc562691deb4d1fc89397296
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:28 +0000

    459cbed33b refactor(dev-infra): convert nullish coalescing operator to plain javascript (#44968)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+30.sha-8dd3f82.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+31.sha-459cbed.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
