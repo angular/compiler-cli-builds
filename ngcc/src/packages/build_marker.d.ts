ff174d1de355489195a706e2ce0e16bb4fe39092
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:02:59 +0000

    28393031b1 perf(http): remove IE special status handling (#44354)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.1+34.sha-9917152.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.1+35.sha-2839303.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
