0e8274f56d17e62f4cfd3f461f209f5657352f95
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 19:01:08 +0000

    99171522cb refactor(core): cleanup `DebugNode` and `DebugElement` implementations (#44270)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+33.sha-38c03a2.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+34.sha-9917152.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+33.sha-38c03a2.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+34.sha-9917152.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
