a69c6403b2b444938167cb5ed9ba2d1eff2ed3fb
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 21:32:57 +0000

    ebf2fc5224 fix(forms): incorrectly keeping track of ngModel with ngFor inside a form (#40459)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.1+43.sha-b5cce71.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.1+44.sha-ebf2fc5.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+43.sha-b5cce71.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+44.sha-ebf2fc5.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
