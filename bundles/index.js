f49bdc28e70fc8493c086c35f43cf55c3dbef38c
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 19:06:28 +0000

    37af6abb49 fix(compiler): allow banana-in-a-box bindings to end with non-null assertion (#37809)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.1+23.sha-604a67f.with-local-changes");
+var VERSION = new Version("13.2.1+24.sha-37af6ab.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
