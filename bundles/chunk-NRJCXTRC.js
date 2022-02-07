bdd3d68bdc0c9deb1a0e530b97b294346105f27b
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 23:48:47 +0000

    d40ac2056e refactor(core): drop all `.ngfactory` and `.ngsummary` imports (#44957)

--- bundles/chunk-NRJCXTRC.js
+++ bundles/chunk-NRJCXTRC.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.1+27.sha-1368f58.with-local-changes";
+var NGCC_VERSION = "13.2.1+28.sha-d40ac20.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.1+27.sha-1368f58.with-local-changes");
+var VERSION = new Version("13.2.1+28.sha-d40ac20.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
