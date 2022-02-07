85126f581b5ced68062508e823d65481e1236d24
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 19:11:00 +0000

    d5719c2e0f fix(core): input coercion (#42803)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.1+40.sha-db6cf7e.with-local-changes");
+var VERSION = new Version("14.0.0-next.1+41.sha-d5719c2.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
