9e820c205c9ccb282509a77d432fff54dfd95740
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:12:59 +0000

    635683ae97 docs(common): mark lifecycle methods as nodoc (#45017)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+4.sha-df6f616.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+5.sha-635683a.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
