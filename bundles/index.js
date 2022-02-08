ffff734af6a97009305909b5b41beb817761ee11
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:56:34 +0000

    28e943634a refactor: one more CHANGELOG fixup. (#45010)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+1.sha-4d63b80.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+2.sha-28e9436.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
