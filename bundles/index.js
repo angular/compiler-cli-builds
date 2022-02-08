7eb8f890ae9ae98ba57782d657b064ed5a190183
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 19:32:28 +0000

    a599fa8d7f build: update dependency google-closure-compiler to v20220202 (#45013)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+10.sha-226cb08.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+11.sha-a599fa8.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
