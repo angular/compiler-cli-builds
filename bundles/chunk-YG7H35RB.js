fe494d435585395e9888dae26e72f3652800cb95
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 05:35:00 +0000

    486cade596 docs: fix command to clear cache on Windows (#44988) PR Close #44988

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.2+2.sha-28e9436.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.2+3.sha-486cade.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+2.sha-28e9436.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+3.sha-486cade.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
