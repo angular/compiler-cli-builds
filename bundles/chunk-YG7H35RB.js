5cd40e3fb72aeb5d2d89c8c85faace79ac46e969
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 20:03:29 +0000

    bbababe590 refactor(http): deprecated symbol used (#44985)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.2+20.sha-4c778cd.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.2+21.sha-bbababe.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.2+20.sha-4c778cd.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.2+21.sha-bbababe.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
