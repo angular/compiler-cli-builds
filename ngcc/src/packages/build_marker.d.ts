916b0aac41e6cf87bc263e5be59611e5bb7f4223
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:39:09 +0000

    b8f8e8cf63 build: update scorecard action dependencies to 7f9dca2 (#45026)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.2+17.sha-7d9413e.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.2+15.sha-b8f8e8c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
