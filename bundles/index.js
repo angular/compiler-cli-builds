7bca9e73272ddb4c29b0518b06ffd61b189d0f4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-10 17:33:04 +0000

    8793dbd8a2 build: update angular (#45011)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+21.sha-bbababe.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+22.sha-8793dbd.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
