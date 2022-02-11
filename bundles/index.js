440effa6c2e56438e06e1c8f66c54d37f04762c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:49:17 +0000

    b4157449c4 refactor(docs-infra): replace deprecated subscribe usages (#45046)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+22.sha-8793dbd.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+24.sha-b415744.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
