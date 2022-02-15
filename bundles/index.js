47cb8b9ceb6f0d86c6e90d9fc90acd316bde4728
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 00:00:49 +0000

    787eaa7a4b test(animations): Fix e2e intermittent failures (#45083)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+29.sha-bedb257.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+34.sha-787eaa7.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
