0e8f4ea01c5e7da4cedaa819cfa289cac4f188ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:45:52 +0000

    13172421f9 docs: fix grammatical error (#45092)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.2+34.sha-787eaa7.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.2+35.sha-1317242.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
