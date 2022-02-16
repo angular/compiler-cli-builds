ccd1d2115992a6541bce7c793e9fa80dfb652e7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:01:58 +0000

    81bb0eb333 docs: clarify install of build schematics deps (#45104)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+40.sha-80585f4.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+41.sha-81bb0eb.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
