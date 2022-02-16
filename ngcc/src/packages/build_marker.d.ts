73635710496d3276340949d5b9b5e3873a85235b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:02:01 +0000

    1eae65ead7 build: update .pullapprove.yml (#45061)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.2+40.sha-80585f4.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.2+43.sha-1eae65e.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
