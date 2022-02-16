0c8a474a10a085af3ece4be6f065ff6c2ccc41b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:05:34 +0000

    5c7c56bc85 perf(animations): improve algorithm to balance animation namespaces (#45057)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.2+43.sha-1eae65e.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.2+44.sha-5c7c56b.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+43.sha-1eae65e.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+44.sha-5c7c56b.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
