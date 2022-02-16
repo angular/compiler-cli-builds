8add46b11ba2b4dcc5b6767e3e0a05cd227c604a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:08:47 +0000

    782f678236 build: update scorecard action dependencies to c4e058a (#45036)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.2+24.sha-aeeb1b5.with-local-changes");
+var VERSION = new Version("13.2.2+31.sha-782f678.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
