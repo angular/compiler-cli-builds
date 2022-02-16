c5a5bcc13c4e143f39325f505960a6231bc229eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:07:45 +0000

    ee381dadc2 test(zone.js): add integration test for zone.js Mocha patch (#45047)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.2+45.sha-6769f94.with-local-changes");
+var VERSION = new Version("14.0.0-next.2+47.sha-ee381da.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
