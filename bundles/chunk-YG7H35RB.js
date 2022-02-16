8cbbae6d35af2ab16b58ae972d0a2808f500c4c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:06:59 +0000

    6769f94b14 build: update all non-major dependencies (#45037)

--- bundles/chunk-YG7H35RB.js
+++ bundles/chunk-YG7H35RB.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.2+44.sha-5c7c56b.with-local-changes";
+var NGCC_VERSION = "14.0.0-next.2+45.sha-6769f94.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
