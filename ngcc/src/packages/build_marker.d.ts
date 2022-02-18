71af770658a18c50ab633e778acadfa02fe24339
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:50:44 +0000

    561bd750b5 build: update dependency io_bazel_rules_sass to v1.49.8 (#45128)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.3+5.sha-4ba95ea.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.3+4.sha-561bd75.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
