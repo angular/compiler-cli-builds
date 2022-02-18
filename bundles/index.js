d06ce4bcbda5733da16ae08754f170c942a37cf4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:50:57 +0000

    be50401090 docs(animations): remove query call in animations example (#45126)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.3+4.sha-561bd75.with-local-changes");
+var VERSION = new Version("14.0.0-next.3+6.sha-be50401.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
