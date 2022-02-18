4cf48c28025a34965632c82ba2aaeb08d951058f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:49:11 +0000

    66d45b0079 docs: fix test examples to use withContext (#45100)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.3+1.sha-071c8af.with-local-changes");
+var VERSION = new Version("14.0.0-next.3+2.sha-66d45b0.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
