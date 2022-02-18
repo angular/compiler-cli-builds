176b8526eeb772ea3659040eaca06f6d504b4566
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:49:31 +0000

    32f83ab848 docs: solve the problem with Dynamic Component guide example (#45123)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.3+2.sha-66d45b0.with-local-changes");
+var VERSION = new Version("14.0.0-next.3+3.sha-32f83ab.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
