e6f9a6de5b2a655917bf9a1aa7549dbb2ea2d7a6
Andrew Scott <atscott01@gmail.com>
2022-02-22 21:04:03 +0000

    d607d0b93c build: update ossf/scorecard-action commit hash to 3662744 (#45162)

--- bundles/chunk-NRJCXTRC.js
+++ bundles/chunk-NRJCXTRC.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.3+10.sha-c02e4d7.with-local-changes";
+var NGCC_VERSION = "13.2.3+11.sha-d607d0b.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
