bd47718a38cd39a0b31410af224705d66f89eaf7
GitHub <noreply@github.com>
2022-02-23 21:06:48 +0000

    ded96ca6cb release: cut the v14.0.0-next.4 release (#45184)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.3+22.sha-f58ad88.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.3+24.sha-ded96ca.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
