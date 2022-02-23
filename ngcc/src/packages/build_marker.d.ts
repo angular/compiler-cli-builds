3c199b86a60d6d55d6b8bbdd74892a516b742e01
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:15:47 +0000

    6550c59df5 docs: fix test description to match code example (#45101)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.2.3+12.sha-0322762.with-local-changes";
+export declare const NGCC_VERSION = "13.2.3+13.sha-6550c59.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
