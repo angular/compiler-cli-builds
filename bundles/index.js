d9e5d1924056d7ae1a40898bec0a14fe9e9b734f
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:56:24 +0000

    8d634ac84d ci: add permissions to current github action workflows (#45177)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.4+9.sha-451bc27.with-local-changes");
+var VERSION = new Version("14.0.0-next.4+10.sha-8d634ac.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
