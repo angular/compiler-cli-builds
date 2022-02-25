e53c6202d323584fafc37fdcf301816962b4e34a
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:23:54 +0000

    e01820c45f build: update dependency @types/chrome to ^0.0.179 (#45201)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.4+21.sha-63e9cc7.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.4+22.sha-e01820c.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
