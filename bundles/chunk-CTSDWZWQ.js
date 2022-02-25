abb6923dd67c4b91909ce0f700eadfaf105dce4c
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 22:29:07 +0000

    76c6dca93f ci: instruct renovate to not update/override merge ready PRs (#45204)

--- bundles/chunk-CTSDWZWQ.js
+++ bundles/chunk-CTSDWZWQ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.4+15.sha-8554659.with-local-changes";
+var NGCC_VERSION = "13.2.4+16.sha-76c6dca.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.4+15.sha-8554659.with-local-changes");
+var VERSION = new Version("13.2.4+16.sha-76c6dca.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
