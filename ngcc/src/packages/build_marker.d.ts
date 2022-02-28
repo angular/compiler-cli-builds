42ddb1fc0bc530bced849ff3c8eef55a7c746278
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 17:55:39 +0000

    f4fd488d4d build: update tooling to use new entry-point for dev-infra exports (#45206)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.4+26.sha-4c80ca3.with-local-changes";
+export declare const NGCC_VERSION = "14.0.0-next.4+28.sha-f4fd488";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
