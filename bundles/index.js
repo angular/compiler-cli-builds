8ad3a6a76cb4cbf8aae60187aafeb9b9c8f82066
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 17:29:53 +0000

    c9679760b2 refactor(router): take only the first emitted value of every resolver to make it consistent with guards (#44573)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.4+36.sha-bd12f9e");
+var VERSION = new Version("14.0.0-next.4+37.sha-c967976");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
