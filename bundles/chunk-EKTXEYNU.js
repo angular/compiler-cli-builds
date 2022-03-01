8f0773047c21db8b0fa4c3de74b838a75283e20f
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 18:58:35 +0000

    1b85811c00 refactor(zone.js): for legacy browser, still use hard coding eventNames (#40962)

--- bundles/chunk-EKTXEYNU.js
+++ bundles/chunk-EKTXEYNU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.4+37.sha-c967976";
+var NGCC_VERSION = "14.0.0-next.4+39.sha-1b85811";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.4+37.sha-c967976");
+var VERSION = new Version("14.0.0-next.4+39.sha-1b85811");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
