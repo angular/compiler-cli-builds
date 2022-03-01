16b9a53d896357ff3454601fa06f4b381eb1c0c7
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 19:32:43 +0000

    e8fd452bd2 fix(core): remove individual commands for updating gold files (#45198)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.4+21.sha-71ff12c.with-local-changes");
+var VERSION = new Version("13.2.4+22.sha-e8fd452.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
