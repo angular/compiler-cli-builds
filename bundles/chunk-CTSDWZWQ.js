88dcdcadf4377753ee66ab17f152a992754437c3
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 18:55:52 +0000

    71ff12c1cc perf(core): make `LOCALE_ID` and other tokens from `ApplicationModule` tree-shakable (#45102) (#45222)

--- bundles/chunk-CTSDWZWQ.js
+++ bundles/chunk-CTSDWZWQ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.4+18.sha-8907fbb.with-local-changes";
+var NGCC_VERSION = "13.2.4+21.sha-71ff12c.with-local-changes";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.4+18.sha-8907fbb.with-local-changes");
+var VERSION = new Version("13.2.4+21.sha-71ff12c.with-local-changes");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
