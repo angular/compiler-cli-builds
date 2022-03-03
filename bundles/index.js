d09cccbbae067d88afa2efed50225b1ee8d184c7
Andrew Kushnir <akushnir@google.com>
2022-03-03 20:39:26 +0000

    b437d1238d fix(zone.js): defineProperties should also set symbol props (#45098)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+2.sha-5883e05");
+var VERSION = new Version("14.0.0-next.5+3.sha-b437d12");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
