3d9c845a5705257e091aa1f0020ee7b32eb8dea0
Andrew Kushnir <akushnir@google.com>
2022-03-03 20:44:21 +0000

    2e105a1118 docs: add Greek localization for Angular Docs (#45152) PR Close #45152

--- bundles/chunk-EKTXEYNU.js
+++ bundles/chunk-EKTXEYNU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+3.sha-b437d12";
+var NGCC_VERSION = "14.0.0-next.5+4.sha-2e105a1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+3.sha-b437d12");
+var VERSION = new Version("14.0.0-next.5+4.sha-2e105a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
