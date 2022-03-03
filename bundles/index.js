ae2daae01d64d49b55714c315f5a9da2e511c8bc
Andrew Kushnir <akushnir@google.com>
2022-03-03 17:30:30 +0000

    5883e0536c refactor(router): Remove special logic for hybrid apps (#45240)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+1.sha-30c52ac");
+var VERSION = new Version("14.0.0-next.5+2.sha-5883e05");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
