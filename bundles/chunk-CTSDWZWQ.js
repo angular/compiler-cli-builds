5f366ea3342d60bd7609343198bd3cdd9e5abc1e
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:53:28 +0000

    f261fa7c08 test(compiler): fix deprecation warning (#45285)

--- bundles/chunk-CTSDWZWQ.js
+++ bundles/chunk-CTSDWZWQ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.5+11.sha-de67c9f";
+var NGCC_VERSION = "13.2.5+13.sha-f261fa7";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.5+11.sha-de67c9f");
+var VERSION = new Version("13.2.5+13.sha-f261fa7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
