f1ffc24dbbcbfbaa42bc688fb0c756ed74353138
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:59:07 +0000

    8390e12bdd docs: update `flush` description to include microtasks (#45237)

--- bundles/chunk-CTSDWZWQ.js
+++ bundles/chunk-CTSDWZWQ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.2.5+15.sha-43bbaf8";
+var NGCC_VERSION = "13.2.5+16.sha-8390e12";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
