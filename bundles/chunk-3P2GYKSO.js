09d429b29a6abfa0818c6d5f6228f795f148e351
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:50:22 +0000

    aa7b857be8 fix(forms): Property renaming safe code (#45271)

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+12.sha-8c171da";
+var NGCC_VERSION = "14.0.0-next.5+13.sha-aa7b857";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
