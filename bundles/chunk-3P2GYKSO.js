c2dceabfd1339f533f26ad3cb6b97fab87e3d5a0
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:50:10 +0000

    8c171da29f refactor(compiler): specify `@angular/core` as peer dependency (#45267)

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+11.sha-94bba76";
+var NGCC_VERSION = "14.0.0-next.5+12.sha-8c171da";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+11.sha-94bba76");
+var VERSION = new Version("14.0.0-next.5+12.sha-8c171da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
