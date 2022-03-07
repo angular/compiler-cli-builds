7ff546e2073fb87759a600601cdf5d4d5a8f4f58
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:58:07 +0000

    a659d15aa0 docs: Change tag name to TD from TR in context of colspan usage (#45257) PR Close #45257

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+18.sha-68eda48";
+var NGCC_VERSION = "14.0.0-next.5+21.sha-a659d15";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+18.sha-68eda48");
+var VERSION = new Version("14.0.0-next.5+21.sha-a659d15");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
