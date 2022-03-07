a9a5b9e88bec00c779a68f3cf977bb7cf312c4e9
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:50:56 +0000

    0bb55f8cb3 build(docs-infra): upgrade cli command docs sources to 9aa2eb03f (#45275)

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+13.sha-aa7b857";
+var NGCC_VERSION = "14.0.0-next.5+14.sha-0bb55f8";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
