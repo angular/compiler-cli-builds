4ab9bd5b537ef3cb94bcd0282d3f6c2a47edc965
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:29:57 +0000

    ba0efd1f80 refactor: replace deprecated `HTMLDocument` with `Document` (#45282)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+30.sha-6eaaefd");
+var VERSION = new Version("14.0.0-next.5+32.sha-ba0efd1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
