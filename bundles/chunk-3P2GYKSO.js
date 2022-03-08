57b992966b84b8807c5daeb4909f6e491f376dab
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:44:56 +0000

    2ca4ef7a22 build: update actions/checkout action to v3 (#45248)

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+26.sha-9e56e40";
+var NGCC_VERSION = "14.0.0-next.5+27.sha-2ca4ef7";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+26.sha-9e56e40");
+var VERSION = new Version("14.0.0-next.5+27.sha-2ca4ef7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
