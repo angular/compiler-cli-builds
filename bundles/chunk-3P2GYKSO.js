ee12ff0a2244e26ddcf5ac4b07c19f78bb603d69
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:52:10 +0000

    f58b8a94d4 build: update all non-major dependencies (#45250)

--- bundles/chunk-3P2GYKSO.js
+++ bundles/chunk-3P2GYKSO.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.5+28.sha-f25c8be";
+var NGCC_VERSION = "14.0.0-next.5+29.sha-f58b8a9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+28.sha-f25c8be");
+var VERSION = new Version("14.0.0-next.5+29.sha-f58b8a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
