c02b92bf8ba503f51ac30692be2b60156a597f2c
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:31:01 +0000

    8227eb914e refactor(migrations): update `isReferenceToImport` to not use `valueDeclaration` (#45292)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.5+21.sha-23003f8");
+var VERSION = new Version("13.2.5+22.sha-8227eb9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
