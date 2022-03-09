cb0846cb16adb47236924bd37e6ff5ba82a0b386
GitHub <noreply@github.com>
2022-03-09 17:21:02 +0000

    51b67eba93 docs: release notes for the v13.2.6 release (#45300)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.5+36.sha-be220fa");
+var VERSION = new Version("14.0.0-next.5+37.sha-51b67eb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
