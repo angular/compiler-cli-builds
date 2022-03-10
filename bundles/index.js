9ad6ebaa552aa11996814ecc9aa19f36d57f7c97
Andrew Scott <atscott01@gmail.com>
2022-03-10 21:09:05 +0000

    78781a998d refactor(core): Switch over to new closure LOCALE vs getLocale() (#45302)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.6+2.sha-6336b8e");
+var VERSION = new Version("13.2.6+3.sha-78781a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
