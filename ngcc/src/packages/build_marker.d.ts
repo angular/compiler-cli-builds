37120438df1b4018636f35417ca0d24e39fc2f4a
Andrew Scott <atscott01@gmail.com>
2022-03-10 21:07:51 +0000

    5f7c4f67d0 refactor(core): Switch over to new closure LOCALE vs getLocale() (#45302)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.6+7.sha-8848d2d";
+export declare const NGCC_VERSION = "14.0.0-next.6+8.sha-5f7c4f6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
