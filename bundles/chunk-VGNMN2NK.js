b9db35b763509553f71ba67674182571909bc8f3
Andrew Scott <atscott01@gmail.com>
2022-03-14 20:28:21 +0000

    c12b3276ec docs: fix small English typo (#45340)

--- bundles/chunk-VGNMN2NK.js
+++ bundles/chunk-VGNMN2NK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.6+14.sha-c305112";
+var NGCC_VERSION = "14.0.0-next.6+15.sha-c12b327";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.6+14.sha-c305112");
+var VERSION = new Version("14.0.0-next.6+15.sha-c12b327");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
