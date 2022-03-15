3ad7a950b5dec8a47b3d413e35b800030413ecd4
Andrew Scott <atscott01@gmail.com>
2022-03-15 20:43:50 +0000

    eac94f0945 fix(forms): improve error message for invalid value accessors (#45192)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.2.6+5.sha-16eda01");
+var VERSION = new Version("13.2.6+7.sha-eac94f0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
