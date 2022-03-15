68118e7a0397a2991074104081eed2ea3a5788fa
Andrew Scott <atscott01@gmail.com>
2022-03-15 23:46:17 +0000

    e47ba49b5f docs: add ngx-gantt to "EXPLORE ANGULAR RESOURCES" page (#45351)

--- bundles/chunk-VGNMN2NK.js
+++ bundles/chunk-VGNMN2NK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.6+17.sha-9db9091";
+var NGCC_VERSION = "14.0.0-next.6+18.sha-e47ba49";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.6+17.sha-9db9091");
+var VERSION = new Version("14.0.0-next.6+18.sha-e47ba49");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
