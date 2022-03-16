85c630a471490dc67a5ea17e14bf8cd4b0f3aba3
GitHub <noreply@github.com>
2022-03-16 19:58:39 +0000

    2b6749f5a0 release: cut the v14.0.0-next.7 release (#45368)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.6+21.sha-34c48a3");
+var VERSION = new Version("14.0.0-next.6+22.sha-2b6749f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
