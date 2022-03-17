d5cb53ed5136a2227525a508c10794b2ecf8c53a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-17 21:21:10 +0000

    e5300d6d75 build(docs-infra): move to circleci v2 api for aio-builds (#45349)

--- bundles/chunk-LUJ2SCC7.js
+++ bundles/chunk-LUJ2SCC7.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.0+2.sha-3700323";
+var NGCC_VERSION = "13.3.0+3.sha-e5300d6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.0+2.sha-3700323");
+var VERSION = new Version("13.3.0+3.sha-e5300d6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
