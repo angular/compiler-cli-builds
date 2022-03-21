b07246df84dbd61a3b0b94bcea8d16b3d13c2ccf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 22:07:30 +0000

    aec9abd374 fix(docs-infra): don't wrap subcommands in code element (#45343)

--- bundles/chunk-VGNMN2NK.js
+++ bundles/chunk-VGNMN2NK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.7+6.sha-c26915c";
+var NGCC_VERSION = "14.0.0-next.7+7.sha-aec9abd";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.7+6.sha-c26915c");
+var VERSION = new Version("14.0.0-next.7+7.sha-aec9abd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
