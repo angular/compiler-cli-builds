5d52fb1c90e2887bc8a6c4881da97c5977d944ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 21:58:57 +0000

    c26915c8cf refactor(core): Make the typed forms migration apply to all usages of the symbols. (#45311)

--- bundles/chunk-VGNMN2NK.js
+++ bundles/chunk-VGNMN2NK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.7+5.sha-79d334b";
+var NGCC_VERSION = "14.0.0-next.7+6.sha-c26915c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.7+5.sha-79d334b";
+export declare const NGCC_VERSION = "14.0.0-next.7+6.sha-c26915c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
