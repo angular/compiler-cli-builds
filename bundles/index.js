7c6b3c2565c8aefaa29b782ae331bafedf659efc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:17:24 +0000

    9d8ff5d022 build: update dependency @types/chrome to ^0.0.180 (#45357)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.7+12.sha-6507150");
+var VERSION = new Version("14.0.0-next.7+13.sha-9d8ff5d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
