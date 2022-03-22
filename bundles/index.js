70b38561de96ce67a069886a82de5c531f70bab6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:13:46 +0000

    15dbb4dc7d docs(docs-infra): fix TypeScript casing (#45307)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.7+9.sha-e80df0d");
+var VERSION = new Version("14.0.0-next.7+11.sha-15dbb4d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
