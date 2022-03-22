a4b5e404de39b26ca255ebec8061d8cb5637a4b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:13:29 +0000

    e80df0d149 build: update to bazel v5 for new runfiles API used in dev-infra (#45407)

--- bundles/chunk-VGNMN2NK.js
+++ bundles/chunk-VGNMN2NK.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.7+8.sha-fffa023";
+var NGCC_VERSION = "14.0.0-next.7+9.sha-e80df0d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
