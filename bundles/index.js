bf22bf7273fe82c5b095e90cd0c51ac50d1de8ca
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:13:49 +0000

    dc72f3007a fix(bazel): ng module compilation workers are subject to linker race-conditions (#45393)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.7+25.sha-c9d566c");
+var VERSION = new Version("14.0.0-next.7+26.sha-dc72f30");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
