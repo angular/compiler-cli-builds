76d5c4a5b103f8d0b417c3f8874fc74670ac8afa
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:09:16 +0000

    fc97499676 build(docs-infra): remove `defaultProject` workspace option (#45410)

--- bundles/chunk-GBI7TUYX.js
+++ bundles/chunk-GBI7TUYX.js
@@ -792,7 +792,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.7+24.sha-0dca774");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.7+23.sha-fc97499");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -839,7 +839,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.7+24.sha-0dca774") {
+    if (version === "14.0.0-next.7+23.sha-fc97499") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
