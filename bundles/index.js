ce8b783f518e48592935439d61e188a0aed4d98b
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:47:01 +0000

    0f9b3c67b4 docs: update the public roadmap (#45415)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.7+33.sha-638e860");
+var VERSION = new Version("14.0.0-next.7+34.sha-0f9b3c6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.7+33.sha-638e860";
+export declare const NGCC_VERSION = "14.0.0-next.7+34.sha-0f9b3c6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
