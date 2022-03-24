e956530926b4b921b4f316d766632c5d973b89c0
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:12:58 +0000

    b481eab39d fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.0+6.sha-ee20410");
+var VERSION = new Version("13.3.0+11.sha-b481eab");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
