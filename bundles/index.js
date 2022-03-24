643f19af3372f051a3013151732d20abdfa9ade5
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:46:11 +0000

    aa29fc7b5c docs: update the public roadmap (#45415)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.0+11.sha-b481eab");
+var VERSION = new Version("13.3.0+12.sha-aa29fc7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
