80243f12a4b7d4fcacff21afda249fa2b9b00fc4
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:06:16 +0000

    0c28997739 build: disable payload size uploading within bazel (#45446)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.0+28.sha-88dbeda");
+var VERSION = new Version("13.3.0+31.sha-0c28997");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
