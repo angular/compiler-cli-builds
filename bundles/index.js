04a1061757ff8d73c05d78571098d33290010676
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:09:15 +0000

    96c7cc933e docs: deprecated relativeLinkResolution in the Router (#45308)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.9+3.sha-3143494");
+var VERSION = new Version("14.0.0-next.9+4.sha-96c7cc9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
