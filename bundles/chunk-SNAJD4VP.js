7f7f69fa399dd8d452fd04ec84156e57aecb7891
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:41:58 +0000

    c996b477a9 build: replace bazel devserver with shared dev-infra implementation (#45452)

--- bundles/chunk-SNAJD4VP.js
+++ bundles/chunk-SNAJD4VP.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.9+5.sha-d36fa11";
+var NGCC_VERSION = "14.0.0-next.9+7.sha-c996b47";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.9+5.sha-d36fa11");
+var VERSION = new Version("14.0.0-next.9+7.sha-c996b47");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
