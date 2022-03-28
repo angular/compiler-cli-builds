dc7bbe7da4ba1720109727b11f11e2dec582fbd8
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:56:08 +0000

    d43c0e973f fix(http): Throw error when headers are supplied in JSONP request (#45210)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.9+9.sha-b36dec6");
+var VERSION = new Version("14.0.0-next.9+10.sha-d43c0e9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.9+9.sha-b36dec6";
+export declare const NGCC_VERSION = "14.0.0-next.9+10.sha-d43c0e9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
