7270a83660fd2ce13ba3275c8619177717f94ab9
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 20:42:04 +0000

    e55e98dce5 build: update all non-major dependencies (#45374)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.9+28.sha-2142ffd");
+var VERSION = new Version("14.0.0-next.9+29.sha-e55e98d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.9+28.sha-2142ffd";
+export declare const NGCC_VERSION = "14.0.0-next.9+29.sha-e55e98d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
