2fb431af66031e2fb7efeae11c030f28c1643e80
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 00:33:41 +0000

    f03e313f24 fix(compiler): scope css keyframes in emulated view encapsulation (#42608)

--- bundles/chunk-SNAJD4VP.js
+++ bundles/chunk-SNAJD4VP.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.9+11.sha-bf98c64";
+var NGCC_VERSION = "14.0.0-next.9+12.sha-f03e313";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.9+11.sha-bf98c64";
+export declare const NGCC_VERSION = "14.0.0-next.9+12.sha-f03e313";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
