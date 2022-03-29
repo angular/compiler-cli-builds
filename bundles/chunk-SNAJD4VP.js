1b62ea6af02a2affe7586ca3c209b6f5ee9d3b86
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:43:47 +0000

    6e1fce529a test: update aio payload size golden to reflect Angular update (#45461)

--- bundles/chunk-SNAJD4VP.js
+++ bundles/chunk-SNAJD4VP.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.9+19.sha-16e635c";
+var NGCC_VERSION = "14.0.0-next.9+22.sha-6e1fce5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
