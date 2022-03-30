44a28fb5f1a7e9e821d580622a41b7f4c1609476
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:29:28 +0000

    e1e440d65a docs: add Angular university website to resources list (#45437)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.9+37.sha-82956dc";
+var NGCC_VERSION = "14.0.0-next.9+38.sha-e1e440d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
