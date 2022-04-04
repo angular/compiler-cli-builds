918a4b5e5824cbec197cebfceb9b5e8306364b5d
Andrew Kushnir <akushnir@google.com>
2022-04-04 22:11:07 +0000

    035ba83b16 build: update dependency entities to v4 (#45509)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.10+9.sha-5aeaedf";
+var NGCC_VERSION = "14.0.0-next.10+11.sha-035ba83";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
