b546eb39e7da6d695e45d695d3d654c30dfbb6d7
Andrew Kushnir <akushnir@google.com>
2022-04-05 16:06:36 +0000

    1c11a57155 fix(router): merge interited resolved data and static data (#45276) in layers

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.10+13.sha-a3f344f");
+var VERSION = new Version("14.0.0-next.10+14.sha-1c11a57");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
