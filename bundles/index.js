f71d05e06776cebf8e85f912332db4dcd6cd7b35
Andrew Kushnir <akushnir@google.com>
2022-04-05 15:59:04 +0000

    a3f344ff6e test(animations): add test for buildAnimationAst() function (#31107)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.10+10.sha-d56a537");
+var VERSION = new Version("14.0.0-next.10+13.sha-a3f344f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
