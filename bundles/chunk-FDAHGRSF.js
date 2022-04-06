2cb8b8371f1dcf543720c747b539442ba690110d
GitHub <noreply@github.com>
2022-04-06 16:14:34 +0000

    d4270d2423 docs: release notes for the v13.3.2 release (#45549)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.10+14.sha-1c11a57";
+var NGCC_VERSION = "14.0.0-next.10+15.sha-d4270d2";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
