bebb12c84ec728952c3600c6e98fdacdb7607ac0
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:19:25 +0000

    2a81e440f4 docs: remove out-of-order `<h3>` elements from footer (#45510)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.11+4.sha-5adfe8e";
+var NGCC_VERSION = "14.0.0-next.11+6.sha-2a81e44";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
