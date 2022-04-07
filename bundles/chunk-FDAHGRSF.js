c242db8367b9e4c773d1af64df2896af363ceb4b
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 22:20:12 +0000

    60b5a3de9d refactor(core): validate property (#45528)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.11+6.sha-2a81e44";
+var NGCC_VERSION = "14.0.0-next.11+7.sha-60b5a3d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
