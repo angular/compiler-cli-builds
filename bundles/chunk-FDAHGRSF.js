dd81880c4afc25a6c45d4695c39606a362932b7d
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:16:33 +0000

    b57f8bea61 test(docs-infra): ufix e2e test for `{@example}` dgeni tags (#45551)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.10+16.sha-f1a2816";
+var NGCC_VERSION = "14.0.0-next.11+2.sha-b57f8be";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.10+16.sha-f1a2816";
+export declare const NGCC_VERSION = "14.0.0-next.11+2.sha-b57f8be";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
