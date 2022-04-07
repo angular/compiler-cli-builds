37252d7a7a5ccc4e37c07b9ccd228c74e564471e
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 22:23:25 +0000

    208d81921d refactor(core): validate property (#45528)

--- bundles/chunk-5M5NTL4U.js
+++ bundles/chunk-5M5NTL4U.js
@@ -785,7 +785,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "13.3.2+3.sha-18f98e6");
+  const LATEST_VERSION_RANGE = getRange("<=", "13.3.2+4.sha-208d819");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -832,7 +832,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "13.3.2+3.sha-18f98e6") {
+    if (version === "13.3.2+4.sha-208d819") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.2+3.sha-18f98e6";
+export declare const NGCC_VERSION = "13.3.2+4.sha-208d819";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
