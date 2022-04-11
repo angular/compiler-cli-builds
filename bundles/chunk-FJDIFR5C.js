c505b6a4c751cd1e0cb5e9695789c98492d7d5d7
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 19:16:41 +0000

    f1630bb24d docs: Fix several typos in the angular documentation (#45573)

--- bundles/chunk-FJDIFR5C.js
+++ bundles/chunk-FJDIFR5C.js
@@ -792,7 +792,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.11+22.sha-9572bb2");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.11+23.sha-f1630bb");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -839,7 +839,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.11+22.sha-9572bb2") {
+    if (version === "14.0.0-next.11+23.sha-f1630bb") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
