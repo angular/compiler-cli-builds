e34f3a4b035cb1b43acf72966daff0219a4b9e95
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 16:39:05 +0000

    20fbb4c35c build: update github/codeql-action action to v2.1.8 (#45542)

--- bundles/chunk-FJDIFR5C.js
+++ bundles/chunk-FJDIFR5C.js
@@ -792,7 +792,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.11+15.sha-a6bf2c5");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.11+16.sha-20fbb4c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -839,7 +839,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.11+15.sha-a6bf2c5") {
+    if (version === "14.0.0-next.11+16.sha-20fbb4c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.11+15.sha-a6bf2c5");
+var VERSION = new Version("14.0.0-next.11+16.sha-20fbb4c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
