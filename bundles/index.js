e7618ef34f65482c3d673c3e6d19ebca8cf3c56c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 17:09:02 +0000

    9572bb2ded refactor(animations): use full ngDevMode check in animatable prop validation (#45570)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.11+20.sha-b13a453");
+var VERSION = new Version("14.0.0-next.11+22.sha-9572bb2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
