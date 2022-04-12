2dc86dcc49a47c3f27bb6c4ca1184b40ef50ac93
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 00:26:47 +0000

    d11d1c0b70 Revert "refactor(router): Add warning for `relativeLinkResolution: 'legacy'` (#45523)" (#45594)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.11+31.sha-3fceba4");
+var VERSION = new Version("14.0.0-next.11+32.sha-d11d1c0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
