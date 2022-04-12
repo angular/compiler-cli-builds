97402b1b163ec9021264c1b3d7242dedb1a200d7
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:44:25 +0000

    3c1695e399 docs: make tutorial instructions consistent (#45372)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.12+6.sha-820077d");
+var VERSION = new Version("14.0.0-next.12+9.sha-3c1695e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
