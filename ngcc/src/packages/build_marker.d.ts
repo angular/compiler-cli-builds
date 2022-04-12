053c447a96c00bce7d4d9562f1673fc1591aeff7
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:31:16 +0000

    79de026a20 docs: add powershell execution policies note (#45576)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.11+47.sha-6532328";
+export declare const NGCC_VERSION = "14.0.0-next.12+1.sha-79de026";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
