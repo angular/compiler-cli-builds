61d8ae8645a8cb75eb24981b3f8622b685d76812
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:20:44 +0000

    89d299105a feat(forms): Implement strict types for the Angular Forms package. (#43834)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.11+32.sha-d11d1c0");
+var VERSION = new Version("14.0.0-next.11+33.sha-89d2991");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.11+32.sha-d11d1c0";
+export declare const NGCC_VERSION = "14.0.0-next.11+33.sha-89d2991";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
