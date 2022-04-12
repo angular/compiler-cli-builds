f27a1467ae0ebf0286e66c662e14c95dda494374
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:47:11 +0000

    c49d23269c docs: new Pipes custom data doc (#45505)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.2+10.sha-5900f0e");
+var VERSION = new Version("13.3.2+15.sha-c49d232");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.2+10.sha-5900f0e";
+export declare const NGCC_VERSION = "13.3.2+15.sha-c49d232";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
