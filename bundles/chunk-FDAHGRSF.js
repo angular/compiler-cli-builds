53c489568bab1ace4b4bd93ff9abff5700f42d1c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:09 +0000

    eb2f4492eb docs(docs-infra): add missing period in `aio/README.md` (#45584)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.11+33.sha-89d2991";
+var NGCC_VERSION = "14.0.0-next.11+35.sha-eb2f449";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.11+33.sha-89d2991");
+var VERSION = new Version("14.0.0-next.11+35.sha-eb2f449");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
