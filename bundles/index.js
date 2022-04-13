8b9afea52613377d0aaae16a72131a49c96bba4b
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:10:49 +0000

    598b759d33 fix(compiler): avoid errors with extremely long instruction chains (#45574)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.12+17.sha-7bf1cf4");
+var VERSION = new Version("14.0.0-next.12+18.sha-598b759");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.12+17.sha-7bf1cf4";
+export declare const NGCC_VERSION = "14.0.0-next.12+18.sha-598b759";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
