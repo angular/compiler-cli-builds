939aebc4ead6bc5ec017f944573c6d5e4cd94fa0
GitHub <noreply@github.com>
2022-04-13 18:45:16 +0000

    cbfe6284a2 release: cut the v13.3.3 release (#45614)

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.2+24.sha-e50fd9e";
+var NGCC_VERSION = "13.3.2+25.sha-cbfe628";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.2+24.sha-e50fd9e";
+export declare const NGCC_VERSION = "13.3.2+25.sha-cbfe628";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
