6f7b5d5ce1a074909ba43a051188b1e5c7ebe401
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 00:12:29 +0000

    5fe59706fd docs: fix grammar (#45455) PR Close #45455

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.2+19.sha-fc145d0";
+var NGCC_VERSION = "13.3.2+23.sha-5fe5970";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.2+19.sha-fc145d0");
+var VERSION = new Version("13.3.2+23.sha-5fe5970");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
