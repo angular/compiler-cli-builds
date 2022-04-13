9fb8cb51176384d9ac85c5609037f4151914bfee
Dylan Hunn <dylhunn@gmail.com>
2022-04-13 22:52:39 +0000

    ec115a31b8 refactor(core): avoid referencing `PlatformRef` in bootstrap code (#45519)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.2+25.sha-cbfe628");
+var VERSION = new Version("13.3.3+1.sha-ec115a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
