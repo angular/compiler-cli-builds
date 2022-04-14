cfe3473f9efb9e5de086c4cbd2aee968b2449875
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:18:55 +0000

    6444a02620 docs: move old changelog entries to a separate file (#45638)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.13+6.sha-2f08b80");
+var VERSION = new Version("14.0.0-next.13+7.sha-6444a02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
