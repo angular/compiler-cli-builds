9f7a775b1cd6ee61e4c3e805f8677890028b2d07
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 00:47:36 +0000

    000363eec5 refactor(router): combine functions for getting loaded config (#45613)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+1.sha-e250db4";
+var NGCC_VERSION = "14.0.0-next.13+2.sha-000363e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.13+1.sha-e250db4");
+var VERSION = new Version("14.0.0-next.13+2.sha-000363e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
