1cd44a3b3aa7577806512e8b8eba48b9cd405b4b
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 23:47:37 +0000

    acbd2e9339 docs: fix typo (#45637)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.3+3.sha-a1c7418");
+var VERSION = new Version("13.3.3+4.sha-acbd2e9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.3+3.sha-a1c7418";
+export declare const NGCC_VERSION = "13.3.3+4.sha-acbd2e9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
