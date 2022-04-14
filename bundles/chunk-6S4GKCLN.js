1570b61c09a8f860cf20c9b6729a4fa035b7b1cd
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:19:00 +0000

    a1c741846d docs: move old changelog entries to a separate file (#45639)

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.3+1.sha-ec115a3";
+var NGCC_VERSION = "13.3.3+3.sha-a1c7418";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.3+1.sha-ec115a3");
+var VERSION = new Version("13.3.3+3.sha-a1c7418");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
