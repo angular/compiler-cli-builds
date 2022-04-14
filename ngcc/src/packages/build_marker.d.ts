f2c3ec547921190feb873dfd075033ea1a54c5f8
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:16:33 +0000

    f8a1ea0c11 fix(bazel): do not error if files part of `srcs` are outside of package (#45622)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.13+2.sha-000363e";
+export declare const NGCC_VERSION = "14.0.0-next.13+3.sha-f8a1ea0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
