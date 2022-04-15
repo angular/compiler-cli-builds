a2b5ebfa17df92b98c7311408c5a4960b2a45e9a
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:39:03 +0000

    0b6407241c fix(docs-infra): fix aio search-results issue with toolbar and notification (#45579)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+14.sha-c6e0e3f";
+var NGCC_VERSION = "14.0.0-next.13+18.sha-0b64072";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
