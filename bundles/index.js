9d30ec6f432e3bb19e2d3223a9aa5e26640a77f1
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:12:13 +0000

    d68333e508 fix(language-service): two-way binding completion should not remove the trailing quote (#45582)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.3+4.sha-acbd2e9");
+var VERSION = new Version("13.3.3+7.sha-d68333e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
