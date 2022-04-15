9b04432bf7a03a7d0a3bf0f1cff2cfd5a760a073
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 19:47:32 +0000

    96fd29c6d2 fix(router): validate lazy loaded configs (#45526)

--- bundles/chunk-FDAHGRSF.js
+++ bundles/chunk-FDAHGRSF.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+11.sha-f13295f";
+var NGCC_VERSION = "14.0.0-next.13+12.sha-96fd29c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
