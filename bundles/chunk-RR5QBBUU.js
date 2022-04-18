2ee12d3279886ba1fab2098bac3ee554bd32261d
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 23:22:48 +0000

    ea8256f43d refactor(router): Move config loader tracking to the RouterConfigLoader (#45656)

--- bundles/chunk-RR5QBBUU.js
+++ bundles/chunk-RR5QBBUU.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+30.sha-aa966fd";
+var NGCC_VERSION = "14.0.0-next.13+34.sha-ea8256f";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.13+30.sha-aa966fd";
+export declare const NGCC_VERSION = "14.0.0-next.13+34.sha-ea8256f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
