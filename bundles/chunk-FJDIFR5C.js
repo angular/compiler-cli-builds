8f1175363a35fec5716e704ca108631ab699a44b
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:32:41 +0000

    f5299a3b8f docs: update decorators proposal stage and link in glossary (#45669)

--- bundles/chunk-FJDIFR5C.js
+++ bundles/chunk-FJDIFR5C.js
@@ -792,7 +792,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.13+35.sha-63202b9");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.13+36.sha-f5299a3");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -839,7 +839,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.13+35.sha-63202b9") {
+    if (version === "14.0.0-next.13+36.sha-f5299a3") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
