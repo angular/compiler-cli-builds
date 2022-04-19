bff25e0f345af73e386f0683a878f252d3ca2c52
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:44:03 +0000

    1e8fe84c05 Fix typo (#45680) PR Close #45680

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.3+15.sha-ebf98c4";
+var NGCC_VERSION = "13.3.3+20.sha-1e8fe84";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
