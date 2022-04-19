e32413aa8adac60c45b4be2f588d20421673506f
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:32:50 +0000

    c6feb0a7ae build: don't assign jelbourn to most review categories (#45668)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.13+36.sha-f5299a3");
+var VERSION = new Version("14.0.0-next.13+37.sha-c6feb0a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
