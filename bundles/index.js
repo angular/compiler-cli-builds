6efccd608d9d054eba177cf1acd40366111e3aef
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:44:30 +0000

    5d155950e3 Fix typo (#45680) PR Close #45680

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.13+38.sha-ceffded");
+var VERSION = new Version("14.0.0-next.13+39.sha-5d15595");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
