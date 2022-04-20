4c323edcc5abf603387c019c8bb434dbe8590167
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:36:35 +0000

    0231a363fe build: preparation for primary branch rename in the Angular repos (#45681)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.13+39.sha-5d15595");
+var VERSION = new Version("14.0.0-next.13+41.sha-0231a36");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
