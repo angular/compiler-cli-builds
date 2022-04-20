729a9760130fc4ef8d447a725b764a449b469d49
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 18:50:13 +0000

    4e0957a4e1 feat(router): Add ability to specify providers on a Route (#45673)

--- bundles/chunk-OOBCA7BG.js
+++ bundles/chunk-OOBCA7BG.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+59.sha-ba45428";
+var NGCC_VERSION = "14.0.0-next.13+60.sha-4e0957a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
