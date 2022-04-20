b014cb33835497b7dbb7aa95d82b779e8536dcbc
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 16:18:01 +0000

    47e5b056ec test(http): Add + to encoding test (#45111)

--- bundles/chunk-OOBCA7BG.js
+++ bundles/chunk-OOBCA7BG.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.13+54.sha-b8d3389";
+var NGCC_VERSION = "14.0.0-next.13+56.sha-47e5b05";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
