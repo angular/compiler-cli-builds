7f8b0967bd8f5db9ee06d2f5b3275ccfd722e2a1
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:24:52 +0000

    3dee3d107b fix(docs-infra): fix placement of "Edit source" button on errors and diagnostics pages (#45634)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.14+8.sha-9292953");
+var VERSION = new Version("14.0.0-next.14+9.sha-3dee3d1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
