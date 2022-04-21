0d2b2ce7ed5f8e875278b757142592af09ec09b9
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:22:44 +0000

    9292953c56 fix(docs-infra): include `tslib` into SystemJS config in `upgrade-module` example app (#45706)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.14+7.sha-9e4c4bc");
+var VERSION = new Version("14.0.0-next.14+8.sha-9292953");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.14+7.sha-9e4c4bc";
+export declare const NGCC_VERSION = "14.0.0-next.14+8.sha-9292953";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
