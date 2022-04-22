f74663203f12a5d0d660869656e046d038fef6cb
Andrew Scott <atscott01@gmail.com>
2022-04-22 20:04:16 +0000

    63398e0eb4 fix(bazel): add this_is_bazel marker (#45728)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.4+2.sha-81611db");
+var VERSION = new Version("13.3.4+3.sha-63398e0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
