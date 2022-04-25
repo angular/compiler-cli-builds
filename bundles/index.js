046f55ad1a0686eb337faf3f7b4c32c0f8f59b31
Andrew Scott <atscott@google.com>
2022-04-25 22:54:16 +0000

    7992132be6 test(core): dynamic component creation and standalone injectors hierarchy (#45726)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.14+22.sha-31d7c3b");
+var VERSION = new Version("14.0.0-next.14+23.sha-7992132");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
