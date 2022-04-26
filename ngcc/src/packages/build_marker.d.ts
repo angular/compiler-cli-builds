bd5a0a7915c7912beb1b2df0459312c06000bc28
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:29:59 +0000

    3a645c6bfc build: update actions/checkout action to v3.0.2 (#45764)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.4+3.sha-63398e0";
+export declare const NGCC_VERSION = "13.3.4+4.sha-3a645c6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
