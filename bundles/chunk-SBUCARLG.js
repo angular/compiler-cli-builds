3b928620b74b4bf30548bf7702d592157d4cd3b0
Andrew Scott <atscott01@gmail.com>
2022-04-27 16:22:57 +0000

    2b5c2d6938 refactor(core): create a standalone injector during applicationBootstrap (#45766)

--- bundles/chunk-SBUCARLG.js
+++ bundles/chunk-SBUCARLG.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.14+30.sha-f1cc4a6";
+var NGCC_VERSION = "14.0.0-next.14+32.sha-2b5c2d6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.14+30.sha-f1cc4a6");
+var VERSION = new Version("14.0.0-next.14+32.sha-2b5c2d6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
