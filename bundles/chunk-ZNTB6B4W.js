cfb157ca3f10f55f33ccd98b56a91446f50c3583
Dylan Hunn <dylhunn@gmail.com>
2022-04-27 23:04:54 +0000

    2f5fd41a19 refactor(core): verify that standalone entities are not declared in NgModule (#45777)

--- bundles/chunk-ZNTB6B4W.js
+++ bundles/chunk-ZNTB6B4W.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.15+1.sha-f3eb7d9";
+var NGCC_VERSION = "14.0.0-next.15+4.sha-2f5fd41";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+1.sha-f3eb7d9");
+var VERSION = new Version("14.0.0-next.15+4.sha-2f5fd41");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
