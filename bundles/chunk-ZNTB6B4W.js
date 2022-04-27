37d4dd861370f76ecd6df08d03383c0024e341bc
Andrew Scott <atscott01@gmail.com>
2022-04-27 17:48:55 +0000

    4d6a1d6722 fix(compiler): scope css keyframes in emulated view encapsulation (#42608)

--- bundles/chunk-ZNTB6B4W.js
+++ bundles/chunk-ZNTB6B4W.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.14+42.sha-3d45484";
+var NGCC_VERSION = "14.0.0-next.14+43.sha-4d6a1d6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.14+42.sha-3d45484");
+var VERSION = new Version("14.0.0-next.14+43.sha-4d6a1d6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
