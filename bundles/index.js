b3b6e6c4747d9f2291218429b94da2a6d646e7ea
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 21:57:13 +0000

    d4fc12fa19 fix(router): Remove deprecated initialNavigation option (#45729)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-3a60063");
+var VERSION = new Version("14.0.0-next.15+sha-d4fc12f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
