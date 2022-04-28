c2088285ee042ccd4212853e643099bae558fe91
Joey Perrott <josephperrott@gmail.com>
2022-04-28 17:34:16 +0000

    b596a5087f build: update dev-infra to latest snapshot (#45800)

---
+++ bundles/linker/index.js
@@ -0,0 +1,28 @@
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+      const __ESM_IMPORT_META_URL__ = import.meta.url;
+    
+import {
+  DEFAULT_LINKER_OPTIONS,
+  FatalLinkerError,
+  FileLinker,
+  LinkerEnvironment,
+  assert,
+  isFatalLinkerError,
+  needsLinking
+} from "../chunk-CPLZ6CTJ.js";
+import "../chunk-WGBFSDVQ.js";
+import "../chunk-UMDDHI5B.js";
+import "../chunk-T7MYRXIE.js";
+import "../chunk-GMSUYBZP.js";
+export {
+  DEFAULT_LINKER_OPTIONS,
+  FatalLinkerError,
+  FileLinker,
+  LinkerEnvironment,
+  assert,
+  isFatalLinkerError,
+  needsLinking
+};
+//# sourceMappingURL=index.js.map
---
+++ bundles/ngcc/main-ngcc.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+      const __ESM_IMPORT_META_URL__ = import.meta.url;
+    
+import {
+  parseCommandLineOptions
+} from "../chunk-OLGEBPBY.js";
+import {
+  mainNgcc
+} from "../chunk-ZNTB6B4W.js";
+import "../chunk-R3C7RFJ4.js";
+import "../chunk-7464WID2.js";
+import "../chunk-RJMU2IDU.js";
+import "../chunk-2ZVQLOCC.js";
+import "../chunk-2IMT6JFI.js";
+import "../chunk-LX5Q27EF.js";
+import "../chunk-WGBFSDVQ.js";
+import "../chunk-UMDDHI5B.js";
+import "../chunk-T7MYRXIE.js";
+import "../chunk-R4NY3TJC.js";
+import "../chunk-GMSUYBZP.js";
+
+// bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/main-ngcc.mjs
+process.title = "ngcc";
+var startTime = Date.now();
+var options = parseCommandLineOptions(process.argv.slice(2));
+(async () => {
+  try {
+    await mainNgcc(options);
+    if (options.logger) {
+      const duration = Math.round((Date.now() - startTime) / 1e3);
+      options.logger.debug(`Run ngcc in ${duration}s.`);
+    }
+    process.exitCode = 0;
+  } catch (e) {
+    console.error(e.stack || e.message);
+    process.exit(typeof e.code === "number" ? e.code : 1);
+  }
+})();
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+//# sourceMappingURL=main-ngcc.js.map
