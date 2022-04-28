1a5c08f31319e8d235fb0b854ebf942c15803025
Joey Perrott <josephperrott@gmail.com>
2022-04-28 17:36:13 +0000

    b596a5087f build: update dev-infra to latest snapshot (#45800)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+6.sha-2941793");
+var VERSION = new Version("14.0.0-next.15+sha-b596a50");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
