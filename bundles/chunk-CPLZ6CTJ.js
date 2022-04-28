2757d58072624ff5087eda5ba3385066d0e5c9bb
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 17:09:10 +0000

    29417935de test(core): verify nested array in standalone component imports (#45794)

--- bundles/chunk-CPLZ6CTJ.js
+++ bundles/chunk-CPLZ6CTJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+5.sha-14c4979");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+6.sha-2941793");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.15+5.sha-14c4979") {
+    if (version === "14.0.0-next.15+6.sha-2941793") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
