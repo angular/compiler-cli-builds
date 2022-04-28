8b670c2351df80b3642a041e49dfce47c139737f
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 16:57:28 +0000

    14c4979a51 build(docs-infra): upgrade cli command docs sources to f258be5be (#45795)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+8.sha-a097be9");
+var VERSION = new Version("14.0.0-next.15+5.sha-14c4979");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
