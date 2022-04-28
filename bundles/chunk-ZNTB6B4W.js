f518b4a80ffb1c15b2b43ff13bcd06dd5e0b137f
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 16:55:37 +0000

    a097be9497 test(core): verify `importProvidersFrom` work correctly with `ModuleWithProviders` (#45787)

--- bundles/chunk-ZNTB6B4W.js
+++ bundles/chunk-ZNTB6B4W.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.15+7.sha-4e413d9";
+var NGCC_VERSION = "14.0.0-next.15+8.sha-a097be9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
