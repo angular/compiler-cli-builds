20c8fbea3a64ff9482174387fed5bb2d532e78e4
Dylan Hunn <dylhunn@gmail.com>
2022-04-29 21:37:08 +0000

    dff5586d52 feat(platform-server): implement `renderApplication` function (#45785)

--- bundles/chunk-CPLZ6CTJ.js
+++ bundles/chunk-CPLZ6CTJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-e1454ae");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-dff5586");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.15+sha-e1454ae") {
+    if (version === "14.0.0-next.15+sha-dff5586") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
