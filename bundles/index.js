d042bb443fd1f1a2e7fd21bd8a2fcd8043299c31
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 23:49:51 +0000

    9e6538c4ba fix(dev-infra): Fix pullapprove by commenting out the empty availability (#45846)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-d322052");
+var VERSION = new Version("14.0.0-next.15+sha-9e6538c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
