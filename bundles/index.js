cc5bc12dab37b28a9547afb73f95e6c173e538c7
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 16:41:25 +0000

    6662a97c61 feat(core): allow to throw on unknown elements in tests (#45479)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-8315974");
+var VERSION = new Version("14.0.0-next.15+sha-6662a97");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
