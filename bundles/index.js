6eb696db97eee19fba2562f2003be4b212e3f7ab
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:21:36 +0000

    ad5ffffc7b refactor(core): rename `INJECTOR_INITIALIZER` -> `ENVIRONMENT_INITIALIZER` (#45845)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-fce5063");
+var VERSION = new Version("14.0.0-next.15+sha-ad5ffff");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
