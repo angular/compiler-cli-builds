69e547ee5123ae5f972ec222a5c35c4a51eabd07
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:15:17 +0000

    22c71be94c fix(platform-server): update `renderApplication` to move `appId` to options (#45844)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-752ddbc");
+var VERSION = new Version("14.0.0-next.15+sha-22c71be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
