89c013ec223d52527474a6eefa47a29001b48890
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 16:45:54 +0000

    9c2f219a40 build: update ng-dev with updated clang-format version to fix formatting (#45834)

--- bundles/chunk-CPLZ6CTJ.js
+++ bundles/chunk-CPLZ6CTJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-6662a97");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-9c2f219");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.15+sha-6662a97") {
+    if (version === "14.0.0-next.15+sha-9c2f219") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/chunk-KYEYWKJW.js
+++ bundles/chunk-KYEYWKJW.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.15+sha-6662a97";
+var NGCC_VERSION = "14.0.0-next.15+sha-9c2f219";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
