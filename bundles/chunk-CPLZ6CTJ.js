06e03368f9c5e1dd43e6903dba74a159eb9f2817
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 16:53:06 +0000

    6a3ca0eb45 Revert "feat(core): allow to throw on unknown elements in tests (#45479)" (#45839)

--- bundles/chunk-CPLZ6CTJ.js
+++ bundles/chunk-CPLZ6CTJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-9c2f219");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.0-next.15+sha-6a3ca0e");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.0-next.15+sha-9c2f219") {
+    if (version === "14.0.0-next.15+sha-6a3ca0e") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-9c2f219");
+var VERSION = new Version("14.0.0-next.15+sha-6a3ca0e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
