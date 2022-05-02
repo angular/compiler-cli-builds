dd1b9491f4a5927ec153a4642e015ea8985f9b26
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:17:30 +0000

    38f2906422 docs(core): add public API docs for the ApplicationRef (#45836)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-22c71be");
+var VERSION = new Version("14.0.0-next.15+sha-38f2906");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
