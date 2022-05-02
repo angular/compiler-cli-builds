526473a9189399e0b44c4e5a4c832e3b740f2e89
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 23:08:34 +0000

    d322052db3 refactor(core): guard against `importProvidersFrom` in components (#45838)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-a521571");
+var VERSION = new Version("14.0.0-next.15+sha-d322052");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
