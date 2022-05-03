f318b6555846ccd081e683985e12f51676d193b1
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 17:51:49 +0000

    401dec46eb feat(core): update TestBed to recognize Standalone Components (#45809)

--- bundles/chunk-QCRIPJID.js
+++ bundles/chunk-QCRIPJID.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.15+sha-bb8d709";
+var NGCC_VERSION = "14.0.0-next.15+sha-401dec4";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
