99b66ea42f92f1628c555a4911cd1204303823d7
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 16:45:07 +0000

    9f558003c4 docs(service-worker): fix alert closing tag (#45797)

--- bundles/chunk-X5LTOQVW.js
+++ bundles/chunk-X5LTOQVW.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.15+sha-9e6538c";
+var NGCC_VERSION = "14.0.0-next.15+sha-9f55800";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-9e6538c");
+var VERSION = new Version("14.0.0-next.15+sha-9f55800");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
