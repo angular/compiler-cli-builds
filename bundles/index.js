9eed4d32f0d5fbb538ebc454ff9adbe910ffd123
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:06:30 +0000

    7005da9c0f ci: decrease the number of required `public-api` and `size-tracking` reviews (#45855)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-a667592");
+var VERSION = new Version("14.0.0-next.15+sha-7005da9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
