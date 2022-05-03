d6c91cc7f239d878d61745224a490a6243680017
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:05:55 +0000

    f9e0da6cf2 ci: decrease the number of required `public-api` and `size-tracking` reviews (#45855)

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.5+7.sha-8007946";
+var NGCC_VERSION = "13.3.5+8.sha-f9e0da6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
