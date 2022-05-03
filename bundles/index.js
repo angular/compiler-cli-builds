7d5f8c65e0a657f8e6ff6d207351bfacfc2748ee
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:26:36 +0000

    fa755b2a54 fix(core): prevent `BrowserModule` providers from being loaded twice (#45826)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-7005da9");
+var VERSION = new Version("14.0.0-next.15+sha-fa755b2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
