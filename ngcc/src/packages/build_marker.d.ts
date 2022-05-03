d7dbba101f1d70e4280b9c356c48d6ec3326699e
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 20:31:25 +0000

    410d81f0e2 refactor(core): disallow standalone components in importProvidersFrom calls (#45837)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.15+sha-8ba4ddc";
+export declare const NGCC_VERSION = "14.0.0-next.15+sha-410d81f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
