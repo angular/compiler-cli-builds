17dac868d6e3e23daad83e26c95ccb70d9c7a63b
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:46:06 +0000

    949927edda build: remove outdated/obsolete github PR utility scripts (#45868)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-b5834de");
+var VERSION = new Version("14.1.0-next.0+sha-949927e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
