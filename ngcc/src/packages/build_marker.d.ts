70f771a2ed98ecfdb9ba2ea5b01db46374946f50
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:01:12 +0000

    10691c626b fix(common): properly cast http param values to strings (#42643)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-next.15+sha-a2d5358";
+export declare const NGCC_VERSION = "14.0.0-next.15+sha-10691c6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
