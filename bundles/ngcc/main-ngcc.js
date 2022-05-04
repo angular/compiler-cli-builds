fc56cd0f5efa76b670cdd0edb58d62bafcbe1d87
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:29:56 +0000

    c988487f03 docs: release notes for the v13.3.6 release (#45874)

---
+++ bundles/ngcc/main-ngcc.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+      const __ESM_IMPORT_META_URL__ = import.meta.url;
+    
+import {
+  parseCommandLineOptions
+} from "../chunk-OLGEBPBY.js";
+import {
+  mainNgcc
+} from "../chunk-36EOQKNZ.js";
+import "../chunk-R3C7RFJ4.js";
+import "../chunk-OGPZOZRU.js";
+import "../chunk-6E6DHHHX.js";
+import "../chunk-HZUCTYDG.js";
+import "../chunk-2IMT6JFI.js";
+import "../chunk-LX5Q27EF.js";
+import "../chunk-WGBFSDVQ.js";
+import "../chunk-JDKMNZ6O.js";
+import "../chunk-T7MYRXIE.js";
+import "../chunk-R4NY3TJC.js";
+import "../chunk-GMSUYBZP.js";
+
+// bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/main-ngcc.mjs
+process.title = "ngcc";
+var startTime = Date.now();
+var options = parseCommandLineOptions(process.argv.slice(2));
+(async () => {
+  try {
+    await mainNgcc(options);
+    if (options.logger) {
+      const duration = Math.round((Date.now() - startTime) / 1e3);
+      options.logger.debug(`Run ngcc in ${duration}s.`);
+    }
+    process.exitCode = 0;
+  } catch (e) {
+    console.error(e.stack || e.message);
+    process.exit(typeof e.code === "number" ? e.code : 1);
+  }
+})();
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+//# sourceMappingURL=main-ngcc.js.map
