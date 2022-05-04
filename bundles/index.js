b13b28ad9add38455a3b26640684313d06189b7d
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:43:35 +0000

    b5834deaa0 build(docs-infra): upgrade cli command docs sources to b71844f36 (#45871)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-68a6a07");
+var VERSION = new Version("14.1.0-next.0+sha-b5834de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
