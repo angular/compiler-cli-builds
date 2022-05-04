8ac89db94f3a23a637efda4685212e1a92a429df
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:30:28 +0000

    e79260e445 release: cut the v14.0.0-next.16 release (#45875)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.15+sha-c988487");
+var VERSION = new Version("14.0.0-next.16+sha-e79260e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
