de6fcf5e06ea704e25e5e2717f7bee760ed3956b
Andrew Kushnir <akushnir@google.com>
2022-05-05 23:02:58 +0000

    86f0213303 ci: update the list of excluded paths in NgBot config (#45901)

--- bundles/chunk-36EOQKNZ.js
+++ bundles/chunk-36EOQKNZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.16+sha-58a8bd3";
+var NGCC_VERSION = "14.0.0-next.16+sha-86f0213";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
