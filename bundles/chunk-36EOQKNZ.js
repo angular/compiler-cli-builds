66cd6f8cc983aa17fffcfc05b62a35db651a3b71
Andrew Kushnir <akushnir@google.com>
2022-05-05 19:08:00 +0000

    3f9faad356 docs: fix a markdown syntax error in aio/content/marketing/analytics.md (#45898) PR Close #45898

--- bundles/chunk-36EOQKNZ.js
+++ bundles/chunk-36EOQKNZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.16+sha-dcea70f";
+var NGCC_VERSION = "14.0.0-next.16+sha-3f9faad";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
