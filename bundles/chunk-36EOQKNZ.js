63c562d7c1e51aba99850775938d8be3bf590920
Andrew Kushnir <akushnir@google.com>
2022-05-05 17:33:56 +0000

    0483c7487c build: update dependency @types/chrome to ^0.0.184 (#45849)

--- bundles/chunk-36EOQKNZ.js
+++ bundles/chunk-36EOQKNZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-81f255b";
+var NGCC_VERSION = "14.1.0-next.0+sha-0483c74";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-81f255b";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-0483c74";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
