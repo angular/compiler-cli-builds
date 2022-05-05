6e80132424813b6abd016a3ab6a7a924ed516746
Andrew Kushnir <akushnir@google.com>
2022-05-05 18:13:07 +0000

    d380bb49b7 refactor(core): rename internal bootstrap function (#45896)

--- bundles/chunk-36EOQKNZ.js
+++ bundles/chunk-36EOQKNZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-0483c74";
+var NGCC_VERSION = "14.1.0-next.0+sha-d380bb4";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
