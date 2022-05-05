8ecf8b95d86c79d48db991d42f9212faab6a6628
Andrew Kushnir <akushnir@google.com>
2022-05-05 23:02:47 +0000

    60ea078ce9 ci: update the list of excluded paths in NgBot config (#45901)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.6+4.sha-c989963";
+export declare const NGCC_VERSION = "13.3.6+5.sha-60ea078";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
