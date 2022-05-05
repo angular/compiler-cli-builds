2374c9fe87d5b8f775cc09eba9f7e88b6bbc7bbe
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:45:41 +0000

    136839054c refactor(devtools): remove support for v11 and older versions of Angular (#45883)

--- bundles/chunk-7XY4OMWJ.js
+++ bundles/chunk-7XY4OMWJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-be7955d");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-1368390");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-be7955d") {
+    if (version === "14.1.0-next.0+sha-1368390") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-be7955d");
+var VERSION = new Version("14.1.0-next.0+sha-1368390");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
