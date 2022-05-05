c54a25f795abe3d2099e72fc92e4bb45fa43d7eb
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:07:02 +0000

    c9899637b0 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.6+2.sha-719199c");
+var VERSION = new Version("13.3.6+4.sha-c989963");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.6+2.sha-719199c";
+export declare const NGCC_VERSION = "13.3.6+4.sha-c989963";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
