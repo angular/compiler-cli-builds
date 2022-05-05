30ba89c245e630eaf62386f0faa7db0b46363735
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:06:51 +0000

    58a8bd3df2 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- bundles/chunk-36EOQKNZ.js
+++ bundles/chunk-36EOQKNZ.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-next.16+sha-f6543c2";
+var NGCC_VERSION = "14.0.0-next.16+sha-58a8bd3";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
