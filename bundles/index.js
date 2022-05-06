0d3098b9073be6c9ad8ca131c5feefb25bc1736c
Andrew Kushnir <akushnir@google.com>
2022-05-06 17:10:11 +0000

    6644165e13 docs: remove redundant word (#45910) PR Close #45910

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.6+6.sha-f9f1f92");
+var VERSION = new Version("13.3.6+8.sha-6644165");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.6+6.sha-f9f1f92";
+export declare const NGCC_VERSION = "13.3.6+8.sha-6644165";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
