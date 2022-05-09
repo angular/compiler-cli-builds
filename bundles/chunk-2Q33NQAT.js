ef1bd167e264aed6074ebcf66f887354665e08cd
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:42:11 +0000

    b6156f41a6 docs(docs-infra): clarify toh-2 error message (#45878)

--- bundles/chunk-2Q33NQAT.js
+++ bundles/chunk-2Q33NQAT.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-e83775c";
+var NGCC_VERSION = "14.1.0-next.0+sha-b6156f4";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
