b7175856d57a676a86e55bf36bc0318e5fe6e5ae
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:51:01 +0000

    91b833a7f4 ci: enable the `aio_preview` CircleCI job (#45935)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-0f0d01b");
+var VERSION = new Version("14.1.0-next.0+sha-91b833a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
