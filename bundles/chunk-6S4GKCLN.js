62cd44387050d8a41bfbf4899f5bf7151e6157a8
Andrew Kushnir <akushnir@google.com>
2022-05-09 20:42:04 +0000

    73bbee2c0d perf(core): allow `checkNoChanges` mode to be tree-shaken in production (#45936)

--- bundles/chunk-6S4GKCLN.js
+++ bundles/chunk-6S4GKCLN.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.6+18.sha-02b99f6";
+var NGCC_VERSION = "13.3.6+19.sha-73bbee2";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.6+18.sha-02b99f6");
+var VERSION = new Version("13.3.6+19.sha-73bbee2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
