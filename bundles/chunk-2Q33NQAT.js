9ca8d33bdb0b9ca35fae9b20fa20eca41f3fb225
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:42:57 +0000

    af253cc2ea ci: enable Android 12 for tests on CI (#45926)

--- bundles/chunk-2Q33NQAT.js
+++ bundles/chunk-2Q33NQAT.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-b6156f4";
+var NGCC_VERSION = "14.1.0-next.0+sha-af253cc";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
