8bb2cd595e875208a07155ceb64ade7ce1a0ef28
Andrew Kushnir <akushnir@google.com>
2022-05-10 01:19:54 +0000

    43ba4ab9da fix(forms): Allow NonNullableFormBuilder to be injected. (#45904)

--- bundles/index.js
+++ bundles/index.js
@@ -71,7 +71,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-216a966");
+var VERSION = new Version("14.1.0-next.0+sha-43ba4ab");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
