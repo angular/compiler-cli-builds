58c9e9b9343b5c83531b5a4134d77b31a7b0c5a9
Andrew Kushnir <akushnir@google.com>
2022-05-10 19:54:06 +0000

    9f6fa5b746 fix(forms): Prevent FormBuilder from distributing unions to control types. (#45942)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-next.16+sha-7dd6450");
+var VERSION = new Version("14.0.0-next.16+sha-9f6fa5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
