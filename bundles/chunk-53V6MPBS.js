acfb4abcde82da0ce0c00ffbbd47adf211f99c69
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-11 17:54:58 +0000

    b882d03068 docs: release notes for the v14.0.0-rc.0 release

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-e0a1a42";
+var NGCC_VERSION = "14.1.0-next.0+sha-b882d03";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-e0a1a42";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-b882d03";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
