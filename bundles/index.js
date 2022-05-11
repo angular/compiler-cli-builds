38007b0d1374a77d655ef4ef2d930e7199210234
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:33:22 +0000

    6238f7895f docs: Add a guide for typed forms. (#45939)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-b882d03");
+var VERSION = new Version("14.1.0-next.0+sha-6238f78");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
