c3cad66ab0452b755c97df5d976d53f031712287
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-11 17:42:20 +0000

    e0a1a423fa docs: release notes for the v13.3.7 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-5e6c574";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-e0a1a42";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
