53aeca956d431593a2b718ed10926d060a39d862
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:40:31 +0000

    1c0d0f0579 refactor(bazel): add support for starlark's unused_inputs_list (#45946)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-rc.0+sha-c3c82b6";
+var NGCC_VERSION = "14.0.0-rc.0+sha-1c0d0f0";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
