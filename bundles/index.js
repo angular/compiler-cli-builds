2a84bd73d53ba0b5ce6fc4fe7b9f7cb1d9202b12
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-11 17:35:55 +0000

    72c11f33b5 release: cut the v13.3.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.6+26.sha-3626464");
+var VERSION = new Version("13.3.6+27.sha-72c11f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
