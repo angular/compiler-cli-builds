c6e0a6cc3cac3023cb2d4b05106d876ac3ac9155
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 16:20:08 +0000

    aea7b98a56 fix(docs-infra): do not detect a running job as failed (#45948)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-6375fa7");
+var VERSION = new Version("14.1.0-next.0+sha-aea7b98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
