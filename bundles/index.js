9a0c5683141c5fe039e7b7eb6b2f45300b7e53d7
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 22:18:54 +0000

    0db2841ea2 build: use clone instead of init for creating the builds commit changes (#45975)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-fa754cd");
+var VERSION = new Version("14.1.0-next.0+sha-0db2841");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
