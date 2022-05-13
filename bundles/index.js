879415ad9ffb3ebc85348bdbcf4faf9779fa814f
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 20:25:35 +0000

    3a468813ca fix(devtools): stop relying on `getAllAngularRootElements` in Angular DevTools' backend code. (#45983)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-a4deac3");
+var VERSION = new Version("14.1.0-next.0+sha-3a46881");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
