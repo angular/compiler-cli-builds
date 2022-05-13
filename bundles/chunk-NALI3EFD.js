6236a21b4f26c9180b0d3077f7f659183ce1cc8d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 22:22:21 +0000

    57adaded2a docs: add selin tunc to contributors (#45989)

--- bundles/chunk-NALI3EFD.js
+++ bundles/chunk-NALI3EFD.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.8+3.sha-c224ada";
+var NGCC_VERSION = "13.3.8+4.sha-57adade";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.8+3.sha-c224ada");
+var VERSION = new Version("13.3.8+4.sha-57adade");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
