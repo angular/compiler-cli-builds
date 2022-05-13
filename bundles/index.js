9e3599933586d8f316d70eba527364685a436e7c
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 20:24:30 +0000

    c224adaa21 docs: adds ngx-cookie-service library to resources list (#45955) PR Close #45955

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.8+2.sha-9a31c22");
+var VERSION = new Version("13.3.8+3.sha-c224ada");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
