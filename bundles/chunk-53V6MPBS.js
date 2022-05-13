12c16443a2e00a181c36d25cc0cbc763f01b76a1
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:26:58 +0000

    e1a93e4d7c build: manually unshallow the repo for builds publishing, use absolute paths (#45979)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-0db2841";
+var NGCC_VERSION = "14.1.0-next.0+sha-e1a93e4";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
