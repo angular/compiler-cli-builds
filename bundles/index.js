31204ab0f578422589f0de4decd3620284065653
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 19:14:45 +0000

    3f7ecec59b fix(core): set correct context for inject() for component ctors (#45991)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.0+sha-45fedb5");
+var VERSION = new Version("14.0.0-rc.0+sha-3f7ecec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
