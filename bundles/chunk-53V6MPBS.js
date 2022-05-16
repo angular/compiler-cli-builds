b7ef46d12d14f060b64455a73b31ec71b59b0a22
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 17:22:46 +0000

    cd04588643 refactor(core): tree-shake away a DI error message string in prod bundles (#45994)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-3242f75";
+var NGCC_VERSION = "14.1.0-next.0+sha-cd04588";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-3242f75");
+var VERSION = new Version("14.1.0-next.0+sha-cd04588");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
