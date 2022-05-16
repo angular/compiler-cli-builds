ba5569b73adf0b6303be3b92fcda31a7ef0431ed
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 17:24:14 +0000

    db1b6fe657 fix(language-service): Fix detection of Angular for v14+ projects (#45998)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-ca04ece");
+var VERSION = new Version("14.1.0-next.0+sha-db1b6fe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
