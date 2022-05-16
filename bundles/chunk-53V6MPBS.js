8e1f55c99496715fb5d06222960726117d89d718
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 16:56:19 +0000

    3242f75efe docs: add transparency to Ecosystem image (#45993)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-89b5248";
+var NGCC_VERSION = "14.1.0-next.0+sha-3242f75";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
