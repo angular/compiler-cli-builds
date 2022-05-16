51c0c2dd53df7076714b61d14848d27d4aeb64b1
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:23:59 +0000

    db15a0882e docs(devtools): add firefox extension references (#45985)

--- bundles/chunk-NALI3EFD.js
+++ bundles/chunk-NALI3EFD.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.8+6.sha-c85bff1";
+var NGCC_VERSION = "13.3.8+7.sha-db15a08";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.8+6.sha-c85bff1");
+var VERSION = new Version("13.3.8+7.sha-db15a08");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
