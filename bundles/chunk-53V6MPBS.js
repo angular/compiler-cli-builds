3209796e5683f429c89ccd1122f46a668a723d07
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 23:25:30 +0000

    6952a0a3e6 docs: add template overview doc (#45897)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-a7897ee";
+var NGCC_VERSION = "14.1.0-next.0+sha-6952a0a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-a7897ee");
+var VERSION = new Version("14.1.0-next.0+sha-6952a0a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
