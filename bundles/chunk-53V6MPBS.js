2f60a47810d0f18af494815e37daa42aca91adeb
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 18:46:08 +0000

    5a04963305 docs: explicitly specify a version in the CLI install command (#46020)

--- bundles/chunk-53V6MPBS.js
+++ bundles/chunk-53V6MPBS.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-rc.0+sha-338de1b";
+var NGCC_VERSION = "14.0.0-rc.0+sha-5a04963";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
