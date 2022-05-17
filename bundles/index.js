ea41746a94af2cfd0daa60b534f5c837ce697991
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 16:16:43 +0000

    35af7c133f build: update angular (#46016)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-6952a0a");
+var VERSION = new Version("14.1.0-next.0+sha-35af7c1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
