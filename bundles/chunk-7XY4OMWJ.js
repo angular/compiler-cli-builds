9b38f18cfecc3bc02214220191b9de52dffd2a6d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-18 16:14:55 +0000

    e92799c97e test(router): Add tests for initialNavigation: enabledBlocking (#45733)

--- bundles/chunk-7XY4OMWJ.js
+++ bundles/chunk-7XY4OMWJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-1e07a11");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-e92799c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-1e07a11") {
+    if (version === "14.1.0-next.0+sha-e92799c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/chunk-UMY2M5GZ.js
+++ bundles/chunk-UMY2M5GZ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-1e07a11";
+var NGCC_VERSION = "14.1.0-next.0+sha-e92799c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
