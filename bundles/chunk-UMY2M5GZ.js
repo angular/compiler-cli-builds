451922d57146a93dcff7cc27fbb87fcedd6a928e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-19 23:07:56 +0000

    2e96cede3e fix(forms): Warn on FormControls that are constructed with both options and asyncValidators.

--- bundles/chunk-UMY2M5GZ.js
+++ bundles/chunk-UMY2M5GZ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-rc.1+sha-2bc6535";
+var NGCC_VERSION = "14.0.0-rc.1+sha-2e96ced";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.1+sha-2bc6535");
+var VERSION = new Version("14.0.0-rc.1+sha-2e96ced");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
