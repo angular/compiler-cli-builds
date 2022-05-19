ec8b9b290a500a52f415f58aae7dfab70175dfba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-19 20:49:58 +0000

    cd0096df2c docs: fix inline JSDoc tags (@see --> @link) (#46040)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-0ff4eda");
+var VERSION = new Version("14.1.0-next.0+sha-cd0096d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
