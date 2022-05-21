b1af925ef01c216d79feecb0806bd67837bb9ba1
Joey Perrott <josephperrott@gmail.com>
2022-05-21 20:39:43 +0000

    ac7fdbd93e build: update saucelabs key (#46089)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "12.2.16+1.sha-68901bf.with-local-changes";
+export declare const NGCC_VERSION = "12.2.16+5.sha-ac7fdbd.with-local-changes";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
