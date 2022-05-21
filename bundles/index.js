7c5f2fe926dbeeff219f4077f20fac65cddb21e1
Joey Perrott <josephperrott@gmail.com>
2022-05-21 20:38:57 +0000

    1d02c09010 build: update saucelabs key (#46089)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.8+11.sha-f708479");
+var VERSION = new Version("13.3.9+2.sha-1d02c09");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
