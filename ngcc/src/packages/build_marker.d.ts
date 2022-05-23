302e36e200459a9f899f971f7f0f819a730ee9da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 21:18:22 +0000

    c3205d0962 feat(devtools): Create browser specific styles for chrome and firefox (#46037)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-f04a8ea";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-c3205d0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
