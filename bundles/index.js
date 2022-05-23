80900a703e9deb5a377015440fd7447d4bf5b280
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:41:48 +0000

    8704574f7e fix(core): restore NgModule state correctly after TestBed overrides (#46049)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-ae18f00");
+var VERSION = new Version("14.1.0-next.0+sha-8704574");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
