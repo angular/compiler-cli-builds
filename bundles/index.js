8f1fa8e63721a83cca99b1888f3f45c3e211cbfa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:32:37 +0000

    ae18f00023 docs: add guide to standalone components preview (#46058)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-76f5a55");
+var VERSION = new Version("14.1.0-next.0+sha-ae18f00");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
