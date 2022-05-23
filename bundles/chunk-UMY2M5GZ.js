129d64afb54196ac02293ab91e5e16d61784f519
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:21:23 +0000

    3172b4cc99 fix(core): produce proper error message for unknown props on `<ng-template>`s (#46068)

--- bundles/chunk-UMY2M5GZ.js
+++ bundles/chunk-UMY2M5GZ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-rc.1+sha-b11a939";
+var NGCC_VERSION = "14.0.0-rc.1+sha-3172b4c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.1+sha-b11a939");
+var VERSION = new Version("14.0.0-rc.1+sha-3172b4c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
