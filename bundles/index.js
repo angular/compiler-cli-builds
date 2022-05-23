09b6948ff4e712662330c538fe8de83938adc0ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 20:35:57 +0000

    e235c70d7d docs: update service-worker-config to more clearly describe behavior (#45909)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-985f8d7");
+var VERSION = new Version("14.1.0-next.0+sha-e235c70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
