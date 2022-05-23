fc06c1be46e5bade6bda687745f9b3db6e808526
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 20:37:50 +0000

    f83ee69e17 build(docs-infra): update Angular Material to v14.0.0-rc.0 in AIO (#45997)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-18b0ee0");
+var VERSION = new Version("14.1.0-next.0+sha-f83ee69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
