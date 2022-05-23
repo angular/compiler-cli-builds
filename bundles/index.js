8b5d05b93fb1f8492209a6db633f9cc9dd5bb3e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:20:35 +0000

    985f8d721e fix(core): produce proper error message for unknown props on `<ng-template>`s (#46068)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-863c32e");
+var VERSION = new Version("14.1.0-next.0+sha-985f8d7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
