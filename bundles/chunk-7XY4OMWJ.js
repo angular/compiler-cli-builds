626e7ff50d60500905b3f6224067913e6998fedc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 20:44:03 +0000

    0bb8c48ae7 build: fix unbound variable error in build artifact script (#46076)

--- bundles/chunk-7XY4OMWJ.js
+++ bundles/chunk-7XY4OMWJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-8461259");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-0bb8c48");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-8461259") {
+    if (version === "14.1.0-next.0+sha-0bb8c48") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
