3b5200159a24ec3200901d6e2a9da5a5147361c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 21:10:24 +0000

    281d4b7ace docs: fix angularfire2 hyperlink (#46046)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-e592cc7";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-281d4b7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
