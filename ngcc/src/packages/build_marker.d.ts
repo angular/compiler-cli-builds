54a24056365c06af9c110d637c130d696e13f0c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:46:03 +0000

    b1a4b305db fix(core): update unknown tag error for jit standalone components (#45920)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-dfba192";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-b1a4b30";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
