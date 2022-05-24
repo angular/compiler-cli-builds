04b09ddb9aca2d7935e58060f9f55b21ca22fd69
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 18:11:59 +0000

    8629f2d0af refactor(compiler-cli): fix "for to" typo in code comment (#46113)

--- bundles/chunk-4GIGG2NN.js
+++ bundles/chunk-4GIGG2NN.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-0b6ca5b";
+var NGCC_VERSION = "14.1.0-next.0+sha-8629f2d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
