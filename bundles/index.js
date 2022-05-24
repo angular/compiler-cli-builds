8b6e84e15761676e9f221bb17c95bad8b017f135
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 18:11:09 +0000

    4a7e724238 refactor(compiler-cli): fix "for to" typo in code comment (#46113)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.9+13.sha-123782b");
+var VERSION = new Version("13.3.9+17.sha-4a7e724");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
