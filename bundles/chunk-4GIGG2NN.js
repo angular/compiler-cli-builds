3300eaeacd578d1339aca3adfd5edf3247208d9c
Andrew Kushnir <akushnir@google.com>
2022-05-24 00:42:36 +0000

    58bbe5e63c ci: temporarily publish snapshots for `image-directive` branch

--- bundles/chunk-4GIGG2NN.js
+++ bundles/chunk-4GIGG2NN.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-c3205d0";
+var NGCC_VERSION = "14.1.0-next.0+sha-58bbe5e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
