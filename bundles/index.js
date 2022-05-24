598896f6ce6980fe6f3986d786f159f67fda18e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 17:58:28 +0000

    7efa09b401 perf(bazel): use allowedInputs to avoid fs.stat (#46069)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-c3205d0");
+var VERSION = new Version("14.1.0-next.0+sha-7efa09b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
