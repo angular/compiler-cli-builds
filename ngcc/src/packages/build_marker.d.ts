154e981c8936c8699f51f3ef825add6c974ca00d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 17:58:30 +0000

    53ca936366 feat(router): Add ability to create `UrlTree` from any `ActivatedRouteSnapshot` (#45877)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-c3205d0";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-53ca936";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
