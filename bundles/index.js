4b3d8621e20c52673a2bcfa54928f828fb850ee2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 19:36:10 +0000

    d2ea1ba876 build: update angular (#46120)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-1cc1583");
+var VERSION = new Version("14.1.0-next.0+sha-d2ea1ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-1cc1583";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-d2ea1ba";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
