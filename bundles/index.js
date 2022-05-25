4cc0ee9e121a7c5e2fd9a20475e680bdfd528034
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 18:56:17 +0000

    ab90d8dcc2 build: update to TypeScript 4.7 final (#46132)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.1+sha-05fd839");
+var VERSION = new Version("14.0.0-rc.1+sha-ab90d8d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
