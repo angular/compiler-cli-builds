043d15f3adbc6c0e35d4a40b4ea0c0dcf595421b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 19:39:55 +0000

    a1e5aad5dc fix(core): improve TestBed declarations standalone error message (#45999)

--- bundles/chunk-4GIGG2NN.js
+++ bundles/chunk-4GIGG2NN.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-d2ea1ba";
+var NGCC_VERSION = "14.1.0-next.0+sha-a1e5aad";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-d2ea1ba";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-a1e5aad";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
