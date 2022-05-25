3fac0ba4979103c14ab26b900ff1fe38191f32e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 18:43:02 +0000

    8db9930aa0 docs: update the Angular roadmap as of May 2022 (#46117)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-8629f2d");
+var VERSION = new Version("14.1.0-next.0+sha-8db9930");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
