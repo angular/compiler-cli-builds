c6664b5fe763ae339d4e87e9d88e29437876cfe2
Andrew Scott <atscott01@gmail.com>
2022-05-26 18:46:00 +0000

    1798ce99b6 build(docs-infra): be able to resolve root-relative image URLs in `getImageDimensions()` (#46138)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.2+sha-542d4c7");
+var VERSION = new Version("14.0.0-rc.2+sha-1798ce9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
