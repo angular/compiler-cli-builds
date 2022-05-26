95045a64f125f4a2a5aeefd62aa0e72f3626e322
Andrew Scott <atscott01@gmail.com>
2022-05-26 18:43:43 +0000

    bd6d82bcb7 fix(core): better error message when unknown property is present (#46147)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-cc0f381");
+var VERSION = new Version("14.1.0-next.0+sha-bd6d82b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
