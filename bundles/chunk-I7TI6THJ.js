39e06aa61eb7b44e288390db3bae260a432e55f2
Andrew Scott <atscott01@gmail.com>
2022-05-27 17:32:36 +0000

    f87aa2da8c build(docs-infra): upgrade cli command docs sources to 874ad2184 (#46157)

--- bundles/chunk-I7TI6THJ.js
+++ bundles/chunk-I7TI6THJ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-a31d821";
+var NGCC_VERSION = "14.1.0-next.0+sha-f87aa2d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-a31d821");
+var VERSION = new Version("14.1.0-next.0+sha-f87aa2d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
