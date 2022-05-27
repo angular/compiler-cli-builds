acb4ee9555fb5ed2f76a2d01ed0f125df0aa17ed
Andrew Scott <atscott01@gmail.com>
2022-05-27 19:05:06 +0000

    ba9f30c9a6 fix(core): include component name into unknown element/property error message (#46160)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.2+sha-5dee53b");
+var VERSION = new Version("14.0.0-rc.2+sha-ba9f30c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-rc.2+sha-5dee53b";
+export declare const NGCC_VERSION = "14.0.0-rc.2+sha-ba9f30c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
