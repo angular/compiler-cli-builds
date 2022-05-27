e4d517ab012f1bd4dc51316cec64a8050fedbb7e
Andrew Scott <atscott01@gmail.com>
2022-05-27 01:00:57 +0000

    6a4353cb40 docs: close lightbox div to fix page display (#46155)

--- bundles/chunk-7XY4OMWJ.js
+++ bundles/chunk-7XY4OMWJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-f0bf29a");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-6a4353c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-f0bf29a") {
+    if (version === "14.1.0-next.0+sha-6a4353c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-f0bf29a");
+var VERSION = new Version("14.1.0-next.0+sha-6a4353c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
