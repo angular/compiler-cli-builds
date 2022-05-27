c0b7dc6c3fd91e03bf25a7680a4f60ce66a930bf
Andrew Kushnir <akushnir@google.com>
2022-05-27 02:57:15 +0000

    6dbb14818a fix(common): reference `DOCUMENT` token inside a constructor

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.9+20.sha-aa6f0c0";
+export declare const NGCC_VERSION = "13.3.9+22.sha-6dbb148";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
