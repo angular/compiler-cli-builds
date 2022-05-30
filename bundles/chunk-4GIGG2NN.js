9d6f040e66ff87ed8131e55e38398c0b2c7a1cb2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-30 02:06:42 +0000

    9e3369cf25 test: update error codes used in the `NgOptimizedImage` tests

--- bundles/chunk-4GIGG2NN.js
+++ bundles/chunk-4GIGG2NN.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-7234871";
+var NGCC_VERSION = "14.1.0-next.0+sha-9e3369c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
