97b03b60c5abdfd1b296a0b1158897e371115466
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:17:48 +0000

    3af2cd5306 docs: fix template example (#46170)

--- bundles/chunk-I7TI6THJ.js
+++ bundles/chunk-I7TI6THJ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-a46f3a1";
+var NGCC_VERSION = "14.1.0-next.0+sha-3af2cd5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
