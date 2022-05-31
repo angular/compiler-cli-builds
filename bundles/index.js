ed5ff57f27cd1ae51fe40eebdca1597b809ccd75
Andrew Scott <atscott01@gmail.com>
2022-05-31 21:15:55 +0000

    9850ea499e docs: add the Standalone guide link to API docs of component/directive/pipe decorators (#46184)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-7f65089");
+var VERSION = new Version("14.1.0-next.0+sha-9850ea4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
