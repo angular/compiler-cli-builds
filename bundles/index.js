fa6abf989ce577ee99c118a6899aef24630ff6c1
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:22:24 +0000

    253ac15ae8 docs: replace broken  `<br/>` from service worker (#46193)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-efed24e");
+var VERSION = new Version("14.1.0-next.0+sha-253ac15");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
