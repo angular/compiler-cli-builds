596774deff29e780f0fe54321988bf7671e83e0c
Andrew Scott <atscott01@gmail.com>
2022-05-31 22:35:07 +0000

    0111f6c75e docs: update release and update guides for v14 (#46200)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.2+sha-c901934");
+var VERSION = new Version("14.0.0-rc.2+sha-0111f6c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
