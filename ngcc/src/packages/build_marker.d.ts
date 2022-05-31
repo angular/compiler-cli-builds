807f36f0492e4ce37f55a44629f0fb4c09746f61
Andrew Scott <atscott01@gmail.com>
2022-05-31 21:13:27 +0000

    7f65089de8 docs(core): improve `inject` function docs (#46168)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-a623c4f";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-7f65089";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
