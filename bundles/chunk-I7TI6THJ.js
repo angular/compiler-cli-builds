54f378f20313afef77825763a59027dacdfaf1b0
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:24:13 +0000

    a623c4f622 test: drop no longer needed postinstall step from integration test apps (#46182)

--- bundles/chunk-I7TI6THJ.js
+++ bundles/chunk-I7TI6THJ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-253ac15";
+var NGCC_VERSION = "14.1.0-next.0+sha-a623c4f";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
