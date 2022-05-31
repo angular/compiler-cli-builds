b92fc403f2d8db1ca23aa83f6a587b6059c747ce
Andrew Scott <atscott01@gmail.com>
2022-05-31 21:15:57 +0000

    c90193475f docs: add the Standalone guide link to API docs of component/directive/pipe decorators (#46184)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-rc.2+sha-ba9f30c";
+export declare const NGCC_VERSION = "14.0.0-rc.2+sha-c901934";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
