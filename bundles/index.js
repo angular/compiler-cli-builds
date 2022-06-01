41ceec4ea823795d3f688ad6fd7c15e4c6c1c395
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:26:54 +0000

    6d1e6a8bc7 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-1f01bcc");
+var VERSION = new Version("14.1.0-next.0+sha-6d1e6a8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
