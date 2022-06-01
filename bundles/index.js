5cea7fb6b8b13b546300175a1646947bfc0a37d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-01 23:28:36 +0000

    025903566e docs(core): mark the standalone APIs as Developer Preview (#46050)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-6d1e6a8");
+var VERSION = new Version("14.1.0-next.0+sha-0259035");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
