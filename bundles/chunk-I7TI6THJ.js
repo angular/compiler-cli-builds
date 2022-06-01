04dc467a41edd5d5fa286c12ebc69acbb7880442
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:26:48 +0000

    536939e138 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- bundles/chunk-I7TI6THJ.js
+++ bundles/chunk-I7TI6THJ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0-rc.3+sha-763c5f8";
+var NGCC_VERSION = "14.0.0-rc.3+sha-536939e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.3+sha-763c5f8");
+var VERSION = new Version("14.0.0-rc.3+sha-536939e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
