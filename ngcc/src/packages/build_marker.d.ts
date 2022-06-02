d164770b464443b90fcdec96237096588229c1be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 21:01:05 +0000

    0206c10f8e refactor(core): minor ComponentDef improvements (#46093)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-8fb737c";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-0206c10";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
