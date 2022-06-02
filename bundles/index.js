25faec0364d2ffc5b7c9ec12305f8bf6394a8e33
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 17:10:55 +0000

    6280a98ff1 release: cut the v14.0.0 release (#46223)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0-rc.3+sha-bf6b44b");
+var VERSION = new Version("14.0.0+sha-6280a98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.0-rc.3+sha-bf6b44b";
+export declare const NGCC_VERSION = "14.0.0+sha-6280a98";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
