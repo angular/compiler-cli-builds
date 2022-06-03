ba2f06f3e541bb796929bbecca5f984ac92d0027
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-03 17:10:24 +0000

    07b40849db docs: update navigation update-to-latest for v14 (#46246)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-f42f80b");
+var VERSION = new Version("14.1.0-next.0+sha-07b4084");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
