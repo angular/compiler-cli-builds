b939b81a2994f02e3efb915d6dfddd5b86ec3c4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 17:31:14 +0000

    cc183928e1 refactor(core): split RuntimeError unit test up (#46239)

--- bundles/chunk-SBOHXOMU.js
+++ bundles/chunk-SBOHXOMU.js
@@ -830,7 +830,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-d846bba");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-cc18392");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -877,7 +877,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-d846bba") {
+    if (version === "14.1.0-next.0+sha-cc18392") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
