5fc2d1ea07d23b6cc483e666311dcf20587a837d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:06:59 +0000

    9f5558cb2a docs: mark version 13 as LTS in releases guide (#46268)

--- bundles/chunk-I7TI6THJ.js
+++ bundles/chunk-I7TI6THJ.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.0+sha-dee2ba9";
+var NGCC_VERSION = "14.0.0+sha-9f5558c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0+sha-dee2ba9");
+var VERSION = new Version("14.0.0+sha-9f5558c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
