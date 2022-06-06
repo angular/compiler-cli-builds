9b540e1ec2cffeb52aca7e049c87037158c8174d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 17:30:37 +0000

    d846bba678 refactor(core): improve code around element validation (#46175)

--- bundles/chunk-5TH3HMQG.js
+++ bundles/chunk-5TH3HMQG.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-f12cf2b";
+var NGCC_VERSION = "14.1.0-next.0+sha-d846bba";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
