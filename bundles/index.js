d6772c834e4810e4244c7e5cdc101af483bbfb84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-08 20:25:20 +0000

    f0b94f5b48 release: cut the v14.0.1 release (#46308)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.0+sha-31d6f32");
+var VERSION = new Version("14.0.1+sha-f0b94f5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
