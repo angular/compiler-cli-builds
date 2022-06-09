de06f25a074fc42419b05f909794b9fd00625be5
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 16:46:35 +0000

    8b9c32d4dc fix(common): allow null in ngComponentOutlet (#46280)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-8115031");
+var VERSION = new Version("14.1.0-next.0+sha-8b9c32d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
