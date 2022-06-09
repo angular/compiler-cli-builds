44631e1f632fe395e0fa51a056a1f02387512ac1
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 21:04:46 +0000

    d6ee9f29e6 docs: update example hero ID (#46304)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-ef5cba3");
+var VERSION = new Version("14.0.1+sha-d6ee9f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
