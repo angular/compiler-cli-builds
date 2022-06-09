36d1f834b58e31201439934accf90f53e91fe7bf
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-09 18:08:27 +0000

    68fd8cbbd9 refactor(common): remove code duplication in loaders

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-40ea24a";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-68fd8cb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
