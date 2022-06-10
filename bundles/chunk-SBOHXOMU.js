c720a020f9324ad786bcf42c6334588f77cddaa0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-10 20:38:15 +0000

    b00b52d9e8 feat(common): add warnings re: image distortion

--- bundles/chunk-SBOHXOMU.js
+++ bundles/chunk-SBOHXOMU.js
@@ -830,7 +830,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-68fd8cb");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.0+sha-b00b52d");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -877,7 +877,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.0+sha-68fd8cb") {
+    if (version === "14.1.0-next.0+sha-b00b52d") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
