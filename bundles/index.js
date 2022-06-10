a1265a201ba2ae949692033a5c8e7182e9ce08c8
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 16:56:26 +0000

    adb21a2416 docs: add angular#bundler to Enforcing Trusted Types section (#45450)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-974da3f");
+var VERSION = new Version("14.1.0-next.0+sha-adb21a2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
