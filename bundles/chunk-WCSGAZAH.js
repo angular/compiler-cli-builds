b950d2acd03361bfd790ae126243e40f1c775bb2
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 18:17:40 +0000

    8ac3513b62 docs: Add kevindaviladev to contributors (#46330)

--- bundles/chunk-WCSGAZAH.js
+++ bundles/chunk-WCSGAZAH.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.1+sha-75c953f";
+var NGCC_VERSION = "14.0.1+sha-8ac3513";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
