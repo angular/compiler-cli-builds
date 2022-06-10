7316f9df9a3c690f1831337737ff652805a89956
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 16:30:00 +0000

    75c953f55b docs: add angular#bundler to Enforcing Trusted Types section (#45450)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-77ae8e1");
+var VERSION = new Version("14.0.1+sha-75c953f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
