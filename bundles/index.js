82c14da24ef906e8148ee97061864ff4424af6e9
Andrew Kushnir <akushnir@google.com>
2022-06-11 00:11:29 +0000

    9dd43f222d refactor(common): update the NgOptimizedImage code to the latest version

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.9+23.sha-872282c");
+var VERSION = new Version("13.3.11+sha-9dd43f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
