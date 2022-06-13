242245c30df035614d484b0158394d956421d1a2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-06-13 09:22:34 +0000

    374f345ce8 refactor(common): add missing format error call in NgOptimizedImage

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.0+sha-bf8bc1e");
+var VERSION = new Version("14.1.0-next.0+sha-374f345");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-bf8bc1e";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-374f345";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
