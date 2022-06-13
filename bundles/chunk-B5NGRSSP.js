28a89f8e971c5ba7fc5b6c1ec9dbf571b5d4849d
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 23:11:21 +0000

    72e6a948bb refactor(router): Update `recognize` to use `Observable` instead of `Promise` (#46021)

--- bundles/chunk-B5NGRSSP.js
+++ bundles/chunk-B5NGRSSP.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.0+sha-9ab3261";
+var NGCC_VERSION = "14.1.0-next.0+sha-72e6a94";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
