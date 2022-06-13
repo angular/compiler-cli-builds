24e87634123d249659019a9cd39ffd5eabbab34d
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 21:17:18 +0000

    9ab32616aa docs:  Adding a book "Angular from Scratch" (#46352)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.0+sha-d6880c9";
+export declare const NGCC_VERSION = "14.1.0-next.0+sha-9ab3261";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
