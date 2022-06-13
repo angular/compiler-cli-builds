1e586fcc7b0c04418f8a34c5b4fa028e244a8919
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 21:17:31 +0000

    b5d5f76c9d docs:  Adding a book "Angular from Scratch" (#46352)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-a0f3c81");
+var VERSION = new Version("14.0.1+sha-b5d5f76");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
