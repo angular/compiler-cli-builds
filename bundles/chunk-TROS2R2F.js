2430db07caab09ce70dda817c77a668632f25c7f
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:57:32 +0000

    a0f3c81bb3 build: update angular version of in-memory-web-api (#46344)

--- bundles/chunk-TROS2R2F.js
+++ bundles/chunk-TROS2R2F.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.1+sha-3fd8948";
+var NGCC_VERSION = "14.0.1+sha-a0f3c81";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
