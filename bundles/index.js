5d934ddb1f4010821d66a3ecf27fb7c5603474ee
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:18:25 +0000

    3fd8948b4a fix(core): Resolve forwardRef declarations for jit (#46334)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-b92c1a6");
+var VERSION = new Version("14.0.1+sha-3fd8948");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
