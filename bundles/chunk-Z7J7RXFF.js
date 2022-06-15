106d402662d68171c5cc8159aa20c82526dd2249
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-15 16:16:13 +0000

    ddc2e70a39 ci: override existing snapshot tag if publish snapshot job is restarted (#46366)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.1+sha-d2470cc";
+var NGCC_VERSION = "14.0.1+sha-ddc2e70";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-d2470cc");
+var VERSION = new Version("14.0.1+sha-ddc2e70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
