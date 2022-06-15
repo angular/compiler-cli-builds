40bc8dee14689771e5d89758b4856ccdf9e1272f
GitHub <noreply@github.com>
2022-06-15 17:46:32 +0000

    88e16553e8 release: cut the v14.0.2 release (#46375)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.1+sha-ddc2e70");
+var VERSION = new Version("14.0.2+sha-88e1655");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
