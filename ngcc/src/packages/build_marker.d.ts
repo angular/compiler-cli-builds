7434a299d019bbbb6d58de64be975b182836dc18
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:24:16 +0000

    ba4f3aabee docs: add standalone details to NG0302 (#46090)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.1+sha-d1e5ef5";
+export declare const NGCC_VERSION = "14.1.0-next.1+sha-ba4f3aa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
