e325cdc54c03e2154edbfb4cda880407793aec15
Andrew Kushnir <akushnir@google.com>
2022-06-16 16:52:48 +0000

    8fac2995fc refactor(common): align tree shakable error messages to the new format (#46382) Simplifying the tree shakable error messages with the new format and removing the errorMessage variables

--- bundles/chunk-7XY4OMWJ.js
+++ bundles/chunk-7XY4OMWJ.js
@@ -828,7 +828,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.0.2+sha-14b9628");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.0.2+sha-8fac299");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -875,7 +875,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.0.2+sha-14b9628") {
+    if (version === "14.0.2+sha-8fac299") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
