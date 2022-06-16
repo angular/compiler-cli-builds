0ecd96f3736b9d4b0da2b25dbb6d29b59fc7652a
Andrew Kushnir <akushnir@google.com>
2022-06-16 16:50:40 +0000

    07ea938aed refactor(common): align tree shakable error messages to the new format (#46382) Simplifying the tree shakable error messages with the new format and removing the errorMessage variables

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.1+sha-df1652e");
+var VERSION = new Version("14.1.0-next.1+sha-07ea938");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
