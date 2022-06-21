e6c5a9de4d07ce3864d41c8cdd1f714195a0186a
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:16:00 +0000

    6ad7b40a6f fix(platform-server): invalid style attribute being generated for null values (#46433)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.2+sha-8fac299");
+var VERSION = new Version("14.0.2+sha-6ad7b40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
