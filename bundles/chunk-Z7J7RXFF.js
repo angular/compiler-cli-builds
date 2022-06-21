b01652dac24f532c94f9a939f9d09aa50cd3854c
Andrew Kushnir <akushnir@google.com>
2022-06-21 20:00:58 +0000

    c086653655 fix(core): deduplicate imports of standalone components in JIT compiler (#46439)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.2+sha-79a973a";
+var NGCC_VERSION = "14.0.2+sha-c086653";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.2+sha-79a973a");
+var VERSION = new Version("14.0.2+sha-c086653");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
