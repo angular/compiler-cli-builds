81c504f4703d057353a83cbbb37b7538aee25b43
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:06:01 +0000

    e8a33e75f4 docs: remove old bazel schematics doc (#46436)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.1+sha-cdd19bd");
+var VERSION = new Version("14.1.0-next.1+sha-e8a33e7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
