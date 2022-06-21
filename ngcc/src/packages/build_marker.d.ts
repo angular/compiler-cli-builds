bddd53426aa2cceafc6b41eb27fa581075586f08
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:06:31 +0000

    ffedcdec08 build: replace copied `spec_bundle` rule in devtools with dev-infra rule (#46437)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.1+sha-e8a33e7";
+export declare const NGCC_VERSION = "14.1.0-next.1+sha-ffedcde";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
