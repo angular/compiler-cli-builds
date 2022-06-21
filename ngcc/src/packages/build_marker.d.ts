a39cef75a459264cd585ee3fa564e588bc94bf44
Andrew Kushnir <akushnir@google.com>
2022-06-21 21:39:19 +0000

    2512f65e51 docs: add dany paredes to GDE resources (#46411)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.1+sha-ddd6d66";
+export declare const NGCC_VERSION = "14.1.0-next.1+sha-2512f65";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
