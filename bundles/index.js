76b5a862ec401ed8664f8a60c003d7be6a5ddff9
Andrew Kushnir <akushnir@google.com>
2022-06-22 16:33:34 +0000

    fac6944ff7 docs: fix typos in several guides (#46453)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.1+sha-cce395a");
+var VERSION = new Version("14.1.0-next.1+sha-fac6944");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
