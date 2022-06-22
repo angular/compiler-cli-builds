d342ac2f58cf1a61fead65d6e29be9c5b92a978a
Andrew Kushnir <akushnir@google.com>
2022-06-22 16:32:13 +0000

    cce395a928 build: load ng-dev release package builder using CJS interop (#46454)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.1+sha-2512f65");
+var VERSION = new Version("14.1.0-next.1+sha-cce395a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
