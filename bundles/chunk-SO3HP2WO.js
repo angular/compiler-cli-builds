5620ff711650aa4b9fb94521f9eb4022f16638e9
Andrew Kushnir <akushnir@google.com>
2022-06-22 20:41:23 +0000

    cb57cdbbd4 fix(zone.js): do not silence uncaught promise rejections when the `finally` is called (#45449)

--- bundles/chunk-SO3HP2WO.js
+++ bundles/chunk-SO3HP2WO.js
@@ -830,7 +830,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.1+sha-fac6944");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.1+sha-cb57cdb");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -877,7 +877,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.1+sha-fac6944") {
+    if (version === "14.1.0-next.1+sha-cb57cdb") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
