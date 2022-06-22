d4cde469de8f0d69398ade8d3bf38a975006d20c
Andrew Kushnir <akushnir@google.com>
2022-06-22 21:53:56 +0000

    8722eddd1d build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.2+sha-2d8ad6c");
+var VERSION = new Version("14.0.2+sha-8722edd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
