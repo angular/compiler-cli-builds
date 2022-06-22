72c434b538b4447355bb10030d1747faf2a03422
Andrew Kushnir <akushnir@google.com>
2022-06-22 22:11:28 +0000

    63396a1ea7 build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- bundles/chunk-U2GR6UX6.js
+++ bundles/chunk-U2GR6UX6.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.1+sha-2d713f5";
+var NGCC_VERSION = "14.1.0-next.1+sha-63396a1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.1+sha-2d713f5");
+var VERSION = new Version("14.1.0-next.1+sha-63396a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
