6df801c43489cfcd4cb1c2e72ae4c75e0b73cf4c
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 23:56:04 +0000

    be43177514 refactor(core): make i18n error messages tree shakable (#46468) Make the error messages tree shakable from the production build to reduce the bundle size.

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.3+sha-51be9bb";
+export declare const NGCC_VERSION = "14.0.3+sha-be43177";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
