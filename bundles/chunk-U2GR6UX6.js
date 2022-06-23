bae20a6d27300c27eadb6569bb82570b4eeaa7ae
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:10:08 +0000

    d24d54bf69 refactor(platform-browser):remove unused code in dom sanitization service (#46383)

--- bundles/chunk-U2GR6UX6.js
+++ bundles/chunk-U2GR6UX6.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.2+sha-b417370";
+var NGCC_VERSION = "14.1.0-next.2+sha-d24d54b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
