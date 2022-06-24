c7c547eec8dee1316c674c7dac096ae65229dbfa
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:28:28 +0000

    dedbc2c5d5 refactor(forms): remove unused internal function (#46479)

--- bundles/chunk-SO3HP2WO.js
+++ bundles/chunk-SO3HP2WO.js
@@ -830,7 +830,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.2+sha-14e24ac");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.2+sha-dedbc2c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -877,7 +877,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.2+sha-14e24ac") {
+    if (version === "14.1.0-next.2+sha-dedbc2c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
