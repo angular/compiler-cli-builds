9784e869333aebcb85d307b4311a17a563c30ca2
Dylan Hunn <dylhunn@gmail.com>
2022-06-25 00:22:23 +0000

    e9e1d8595f docs(core): add circleci insights metrics widget (#46496)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.3+sha-f930fc2");
+var VERSION = new Version("14.0.3+sha-e9e1d85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.3+sha-f930fc2";
+export declare const NGCC_VERSION = "14.0.3+sha-e9e1d85";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
