189615b7ffb66c072278f0c04d33b53cf918bfc7
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 18:08:54 +0000

    e026a9f174 build: remove the previous release scripts as they are unused (#46516)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.2+sha-cb452e6";
+export declare const NGCC_VERSION = "14.1.0-next.2+sha-e026a9f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
