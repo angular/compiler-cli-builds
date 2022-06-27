077f48be1ffcd91fae2bff84b4599452bb9f186a
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 22:32:47 +0000

    8ed502a6a7 test(core): use TestBed & JIT instead of handwritten defs in providers_spec (#46527)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-21cce3a");
+var VERSION = new Version("14.1.0-next.2+sha-8ed502a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
