c58ff3e71d0fab681e711608976721b9207769ff
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 22:34:39 +0000

    2e59f9f612 test(core): convert listener TemplateFixture tests to TestBed (#46517)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-acf6dc9");
+var VERSION = new Version("14.1.0-next.2+sha-2e59f9f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
