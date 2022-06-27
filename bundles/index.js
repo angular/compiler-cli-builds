a4f2253c03c6300d4096f756a81364946b9710b9
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 23:00:20 +0000

    a3f7046380 test(core): replace hand-written instructions in tests with TestBed APIs (#46525)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-de171d8");
+var VERSION = new Version("14.1.0-next.2+sha-a3f7046");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
