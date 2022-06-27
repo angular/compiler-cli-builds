dac34d0f787bb827bb63a0ba9eea41d5dc70b796
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 18:15:29 +0000

    21cce3a82b docs: add how to pass array options to the CLI (#46505)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-09ceb4e");
+var VERSION = new Version("14.1.0-next.2+sha-21cce3a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
