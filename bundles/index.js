5a486608c7b8d824cbcc5dec1f4ddc6af4355f0b
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 23:11:11 +0000

    f355a24a12 test: avoid test fixture affecting zone in all web tests (#46511)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.3+sha-56eca95");
+var VERSION = new Version("14.0.3+sha-f355a24");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
