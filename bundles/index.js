82ab5281446c3ba707a055ff955c67813d635906
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 16:56:57 +0000

    ff071bbc88 test(core): replace hand-written instructions in component tests with TestBed (#46532)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-0775332");
+var VERSION = new Version("14.1.0-next.2+sha-ff071bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
