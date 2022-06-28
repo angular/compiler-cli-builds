db42e32c6e724b62fd711a2f24155c884ecf1b7c
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 18:35:39 +0000

    a322b7d694 refactor(core): disable renderer3 unless explicitly enabled by tests (#46530)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.2+sha-1c5f0cd";
+export declare const NGCC_VERSION = "14.1.0-next.2+sha-a322b7d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
