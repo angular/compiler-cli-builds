1ed3adccb7766bbc8c11036df18bf5e57f574e4e
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 07:48:19 +0000

    f86e0948f8 fix(core): Updates error to use RuntimeError code (#46526)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-d4c10d1");
+var VERSION = new Version("14.1.0-next.2+sha-f86e094");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
