55546aa80bd41dfff9c9c197d06ece31724861b6
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 16:54:09 +0000

    6c4d36f3ff docs: documentation text fix for compiler cli (#46541)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-196b64d");
+var VERSION = new Version("14.1.0-next.2+sha-6c4d36f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
