1e2215ba98bf9628c290d93261f959d54a9f406e
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 20:13:25 +0000

    ee39cf62bc fix(router): Ensure that new `RouterOutlet` instances work after old ones are destroyed (#46554)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-f6a096e");
+var VERSION = new Version("14.1.0-next.2+sha-ee39cf6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
