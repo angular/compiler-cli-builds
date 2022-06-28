423ee7da074ec2d862ed79c20266d9fd7fe59daa
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 20:17:33 +0000

    606d94299a fix(core): Update ngfor error code to be negative (#46555)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-a7597dd");
+var VERSION = new Version("14.1.0-next.2+sha-606d942");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
