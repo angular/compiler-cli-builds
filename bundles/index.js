b26bc29582e66500c6ed12459e461bf5b36effdf
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:29:52 +0000

    47c65ace5e build: update eslint dependencies (#46593) PR Close #46593

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-d429eca");
+var VERSION = new Version("14.1.0-next.2+sha-47c65ac");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
