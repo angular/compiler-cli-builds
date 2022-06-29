df5314dea3ee6938c58c31059b610ce9e7cd64cf
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 08:12:54 +0000

    1289425cf8 test: update golden symbols (#46582)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-6a53034");
+var VERSION = new Version("14.1.0-next.2+sha-1289425");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
