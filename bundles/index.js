1764a5bbd3ae187287c4346e8f81a624bc5f7a3d
Andrew Scott <atscott01@gmail.com>
2022-06-29 22:38:29 +0000

    a3bd65e2b8 fix(router): Ensure `APP_INITIALIZER` of `enabledBlocking` option completes (#46634)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.4+sha-88298ca");
+var VERSION = new Version("14.0.4+sha-a3bd65e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
