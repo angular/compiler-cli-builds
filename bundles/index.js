e79f8634558db1ee6c07caa9e368b22868222a6f
Andrew Scott <atscott01@gmail.com>
2022-06-29 21:48:39 +0000

    88298cab50 test(core): set parent injector in `createEnvironmentInjector` calls in tests (#46633)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.3+sha-55609aa");
+var VERSION = new Version("14.0.4+sha-88298ca");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.3+sha-55609aa";
+export declare const NGCC_VERSION = "14.0.4+sha-88298ca";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
