60961423298a299ddc7883332536f83468aeba36
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 18:35:46 +0000

    66baa8ccf1 docs: release notes for the v14.0.4 release (#46621)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-08733b0");
+var VERSION = new Version("14.1.0-next.2+sha-66baa8c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
