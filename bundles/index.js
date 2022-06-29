eef214fc388476146898b9747778a2510c74fd3a
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:41:07 +0000

    9d30298250 build: update bazel setup to v5.5.1 (#46543) PR Close #46543

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-19364cf");
+var VERSION = new Version("14.1.0-next.2+sha-9d30298");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
