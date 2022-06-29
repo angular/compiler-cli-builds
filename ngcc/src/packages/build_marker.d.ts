c560c6efffde2d61dad29971f5b295bc184455cd
Andrew Scott <atscott01@gmail.com>
2022-06-29 20:15:16 +0000

    7562421353 ci: do not run firefox components repo tests (#46624)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.3+sha-36f46c6";
+export declare const NGCC_VERSION = "14.1.0-next.3+sha-7562421";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
