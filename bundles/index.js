8389c41220a99b00f5afe606cfb652c4f196dc62
Andrew Scott <atscott01@gmail.com>
2022-06-29 21:13:21 +0000

    598239c35d build: update dependency rimraf to v3 (#46625)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.3+sha-daa6c24");
+var VERSION = new Version("14.1.0-next.3+sha-598239c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
