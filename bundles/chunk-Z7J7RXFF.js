fcc8bf9e2973a8a60c0ef1b362c9a6425421aeca
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:52:13 +0000

    55609aa564 ci: fix pullapprove `fnmatch` patterns due to recent changes (#46589)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.3+sha-72976c7";
+var NGCC_VERSION = "14.0.3+sha-55609aa";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
