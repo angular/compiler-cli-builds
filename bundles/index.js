4d43177da205a2c651dd7497f5b5a4e583f4c3d1
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:25:56 +0000

    b1ac3d9968 refactor: clean-up pre-CLDR 39 safety check (#46606)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.2+sha-957744f");
+var VERSION = new Version("14.1.0-next.2+sha-b1ac3d9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
