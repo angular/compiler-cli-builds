6f888e096c108a9b5eabb479a134555471aab124
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:01:01 +0000

    39c02f1c9d docs(router): mark title strategy as injectable (#46639)

--- bundles/chunk-U2GR6UX6.js
+++ bundles/chunk-U2GR6UX6.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.3+sha-8d2e5e6";
+var NGCC_VERSION = "14.1.0-next.3+sha-39c02f1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.3+sha-8d2e5e6";
+export declare const NGCC_VERSION = "14.1.0-next.3+sha-39c02f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
