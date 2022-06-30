5bddd82fef290e3f9866740dcce601938758bc60
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:55:40 +0000

    7fe81a9a0c refactor(router): Convert user-visible router errors to RuntimeError (#46630)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.4+sha-1bd0b56";
+var NGCC_VERSION = "14.0.4+sha-7fe81a9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
