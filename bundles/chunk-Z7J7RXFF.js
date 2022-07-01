688024fb32b967880f91e2a8e0ad4f71197049d2
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:41:08 +0000

    853b1c1bc9 docs: move section on subscribing to observable (#46655)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.4+sha-7fe81a9";
+var NGCC_VERSION = "14.0.4+sha-853b1c1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
