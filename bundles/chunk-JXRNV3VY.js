e83427c5dce16a9fb11df4fe3790d23599c11203
Andrew Scott <atscott01@gmail.com>
2022-07-01 20:43:58 +0000

    dd3e0968ec refactor(router): Remove custom error message for invalid guards (#46678)

--- bundles/chunk-JXRNV3VY.js
+++ bundles/chunk-JXRNV3VY.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.3+sha-915e82d";
+var NGCC_VERSION = "14.1.0-next.3+sha-dd3e096";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.3+sha-915e82d");
+var VERSION = new Version("14.1.0-next.3+sha-dd3e096");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
