06fd357195934e28ec06d162bbfa7eaa48a82bf7
Andrew Scott <atscott01@gmail.com>
2022-07-06 14:59:07 +0000

    5001a7fb02 refactor(router): Remove custom error message for invalid guards (#46680)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.4+sha-853b1c1");
+var VERSION = new Version("14.0.4+sha-5001a7f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
