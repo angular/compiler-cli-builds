22618bacaf4cbcc4dd9ee4cac8a195dcc069617e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 21:05:22 +0000

    818928c70d ci: remove renovate changes group (#46676)

--- bundles/chunk-Z7J7RXFF.js
+++ bundles/chunk-Z7J7RXFF.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.5+sha-e8c8b69";
+var NGCC_VERSION = "14.0.5+sha-818928c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.5+sha-e8c8b69");
+var VERSION = new Version("14.0.5+sha-818928c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
