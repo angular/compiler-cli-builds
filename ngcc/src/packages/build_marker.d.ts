9a1201816795199562755e1588a4443b7ac79084
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 21:04:36 +0000

    a588b2f712 ci: remove renovate changes group (#46676)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.4+sha-e9b5dac";
+export declare const NGCC_VERSION = "14.1.0-next.4+sha-a588b2f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
