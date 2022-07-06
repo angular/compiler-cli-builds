072f8bbfdf259941856bf5fc8ef0ec96f3e220fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 23:13:15 +0000

    ad2721a149 build: update cross-repo angular dependencies (#46607)

--- bundles/chunk-JXRNV3VY.js
+++ bundles/chunk-JXRNV3VY.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-42b6d23";
+var NGCC_VERSION = "14.1.0-next.4+sha-ad2721a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
