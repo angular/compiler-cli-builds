cbb39cc5c1e2949a94005ed39ec9eaf65f123ef7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-07 20:24:44 +0000

    71e606d3c3 feat(core): expose EnvironmentInjector on ApplicationRef (#46665)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-33ce388");
+var VERSION = new Version("14.1.0-next.4+sha-71e606d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
