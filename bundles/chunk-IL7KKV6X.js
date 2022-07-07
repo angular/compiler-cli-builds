14cf18813e12599c326fd04ca022f78da2900acf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-07 21:09:32 +0000

    b3c20add72 Revert "fix(animations): enable shadowElements to leave when their parent does (#46459)" (#46739)

--- bundles/chunk-IL7KKV6X.js
+++ bundles/chunk-IL7KKV6X.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-28b6ec5";
+var NGCC_VERSION = "14.1.0-next.4+sha-b3c20ad";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-next.4+sha-28b6ec5";
+export declare const NGCC_VERSION = "14.1.0-next.4+sha-b3c20ad";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
