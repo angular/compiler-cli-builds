f560dae52fb6aa6bc5a551773607acb8f3a720ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-07 20:52:21 +0000

    28b6ec515a docs: typo fix in destroy description (#46737)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-5a69903");
+var VERSION = new Version("14.1.0-next.4+sha-28b6ec5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
