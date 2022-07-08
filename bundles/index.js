8220baac937df5b703b4acf3d5cccb7c7e695aea
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 18:15:22 +0000

    ea0f07a858 docs(router): fix minor typos in tutorial (#46696) PR Close #46696

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-201248e");
+var VERSION = new Version("14.1.0-next.4+sha-ea0f07a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
