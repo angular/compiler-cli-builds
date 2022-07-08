6f41a9a46a71839377e4bf9882c00e0f3885ddd5
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 16:37:12 +0000

    8ade3b764f fixup! ci: use actual main branch for components repo unit test job (#46743)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.5+sha-72d1aff");
+var VERSION = new Version("14.0.5+sha-8ade3b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
