a9f53c9eb5c150ca8d0acbf7e9ed81b5192de747
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 22:00:52 +0000

    2808d889dc build: update dependency firebase-tools to v11 (#46779)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-7b9269c");
+var VERSION = new Version("14.1.0-next.4+sha-2808d88");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
