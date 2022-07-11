e37c6066e25c3c99471cf88fdbc6e187d4459962
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 20:20:11 +0000

    19e6d9ccd3 feat(core): import AsyncStackTaggingZone if available (#46693)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-b6a950c");
+var VERSION = new Version("14.1.0-next.4+sha-19e6d9c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
