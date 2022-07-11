e37ed65188585639e55c35c48c26be05c60ccd70
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:55:47 +0000

    97491997ff build(docs-infra): update versions of some docs examples dependencies (#46609)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.0.5+sha-ec267b4";
+export declare const NGCC_VERSION = "14.0.5+sha-9749199";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
