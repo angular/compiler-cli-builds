30ff1beff48c18c6129ea544739390d0c08c9991
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 22:02:24 +0000

    8271da9dbb docs: Remove content lint errors from tutorial topics (#46652)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-2808d88");
+var VERSION = new Version("14.1.0-next.4+sha-8271da9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
