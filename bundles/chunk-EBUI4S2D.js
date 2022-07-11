bb14db7cde0d46e121bc1e15f3b5ba52407111b8
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:52:01 +0000

    b9f5829509 build: Fix Angular CLI issue template link (#46756)

--- bundles/chunk-EBUI4S2D.js
+++ bundles/chunk-EBUI4S2D.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-a36853c";
+var NGCC_VERSION = "14.1.0-next.4+sha-b9f5829";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
