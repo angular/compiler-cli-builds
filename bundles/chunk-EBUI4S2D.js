c513146d59b23a7d61aec64a18705a5350ef0497
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:52:17 +0000

    a4b64bc483 build(docs-infra): update versions of some docs examples dependencies (#46609)

--- bundles/chunk-EBUI4S2D.js
+++ bundles/chunk-EBUI4S2D.js
@@ -898,7 +898,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-4190e9a";
+var NGCC_VERSION = "14.1.0-next.4+sha-a4b64bc";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-4190e9a");
+var VERSION = new Version("14.1.0-next.4+sha-a4b64bc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
