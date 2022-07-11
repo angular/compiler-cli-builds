78f19ea8fd3ca988ae5c051a5c762f6263c3fe2f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 20:19:16 +0000

    b6a950cd60 docs(core): add full example of directive with typed context (#46689)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-93f7311");
+var VERSION = new Version("14.1.0-next.4+sha-b6a950c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
