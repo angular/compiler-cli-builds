60b87c510440ec7a6cff2ce72674d4bbd28b2d2d
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 22:54:08 +0000

    862dd03764 refactor(animations): refactor non-animatable check to be timeline based (#46666)

--- bundles/chunk-M7DRLMSX.js
+++ bundles/chunk-M7DRLMSX.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.5+sha-99697da";
+var NGCC_VERSION = "14.0.5+sha-862dd03";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.5+sha-99697da");
+var VERSION = new Version("14.0.5+sha-862dd03");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
