ba4a5b44c63ffc2927e580faef2da711f243ba93
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 18:06:52 +0000

    93c65e7b14 feat(compiler-cli): add extended diagnostic for non-nullable optional chains (#46686)

--- bundles/chunk-5IHGVHNA.js
+++ bundles/chunk-5IHGVHNA.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-c0ca3fc";
+var NGCC_VERSION = "14.1.0-next.4+sha-93c65e7";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
