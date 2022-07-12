97ba6ec1c4eb310e64830607f4723512eb50977a
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 16:13:43 +0000

    c8b0127f31 build: remove unused `inline-package-json-deps.js` script (#46798)

--- bundles/chunk-KUYUFP3M.js
+++ bundles/chunk-KUYUFP3M.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-9f9e4f2";
+var NGCC_VERSION = "14.1.0-next.4+sha-c8b0127";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
