20f05c001245846a71adb1f958bd6ba8ee3714fb
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 19:27:06 +0000

    10495a8405 docs: Add missing period (#46799)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-f6859e8");
+var VERSION = new Version("14.1.0-next.4+sha-10495a8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
