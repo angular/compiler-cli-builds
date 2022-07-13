7f6387b88cd25f1d20d8692ea64c75a84672581a
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 17:28:17 +0000

    dbb0dfe707 build: lock file maintenance (#46691)

--- bundles/chunk-GDAOEMFN.js
+++ bundles/chunk-GDAOEMFN.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-336598a";
+var NGCC_VERSION = "14.1.0-next.4+sha-dbb0dfe";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
