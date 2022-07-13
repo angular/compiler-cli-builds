e85762da9dfdcbfa4123489945701688e1c433f6
GitHub <noreply@github.com>
2022-07-13 20:02:33 +0000

    27d855b9fb Update next branch to reflect new release-train "v14.2.0-next.0". (#46831)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-5308634");
+var VERSION = new Version("14.2.0-next.0+sha-27d855b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
