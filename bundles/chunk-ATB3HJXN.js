16d6a7f14c90027b8485979aa78268b854ff63fe
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 20:22:45 +0000

    4dca1842b3 docs: describe smallest typo, "an" -> "a" (#46812) PR Close #46812

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-rc.0+sha-442b16c");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-rc.0+sha-4dca184");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-rc.0+sha-442b16c") {
+    if (version === "14.1.0-rc.0+sha-4dca184") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-rc.0+sha-442b16c");
+var VERSION = new Version("14.1.0-rc.0+sha-4dca184");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
