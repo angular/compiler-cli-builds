36ae14716db8be34e621bbbc43e1688bf22d585f
GitHub <noreply@github.com>
2022-07-13 19:27:04 +0000

    5308634900 docs: release notes for the v14.0.6 release (#46828)

--- bundles/chunk-GDAOEMFN.js
+++ bundles/chunk-GDAOEMFN.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-next.4+sha-dbb0dfe";
+var NGCC_VERSION = "14.1.0-next.4+sha-5308634";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-dbb0dfe");
+var VERSION = new Version("14.1.0-next.4+sha-5308634");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
