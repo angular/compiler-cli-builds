8f755b946a7c8c971d70444328c0175bc835125c
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 00:22:01 +0000

    fd15744fca docs: add warning about lack of testing used by TOH (#46811)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.5+sha-862dd03");
+var VERSION = new Version("14.0.5+sha-fd15744");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
