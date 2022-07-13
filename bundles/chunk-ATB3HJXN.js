93641c7984fbbc77ae5442df634bdc151830e1a5
GitHub <noreply@github.com>
2022-07-13 19:52:55 +0000

    442b16c551 release: cut the v14.1.0-rc.0 release (#46830)

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-next.4+sha-5308634");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-rc.0+sha-442b16c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-next.4+sha-5308634") {
+    if (version === "14.1.0-rc.0+sha-442b16c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
