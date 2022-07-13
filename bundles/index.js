13962b81fcc805d9e4d779c705ae5dd458b26d97
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 16:50:19 +0000

    336598aedc refactor(docs-infra): update boilerplate to have proper `angular.js` types (#46819)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-next.4+sha-58d2555");
+var VERSION = new Version("14.1.0-next.4+sha-336598a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
