579527043c3dcfb154a64c7317c7308693197828
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 22:19:46 +0000

    d36a9e6bf3 refactor(core): improve an error message when `ENVIRONMENT_INITIALIZER` is not a multi provider (#46829)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-rc.0+sha-3f2d16c";
+export declare const NGCC_VERSION = "14.1.0-rc.0+sha-d36a9e6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
