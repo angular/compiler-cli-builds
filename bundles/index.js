d7f4ba057ee27d4b815c06bf7ae822c5110007ea
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 21:19:30 +0000

    7e03fc90d6 refactor(core): combine TestBed interface and implementation to avoid cycles (#46635)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-27d855b");
+var VERSION = new Version("14.2.0-next.0+sha-7e03fc9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
