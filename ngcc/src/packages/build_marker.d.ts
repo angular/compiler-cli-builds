915903f87fffeabd186918bf452386d61ee5548f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-14 22:45:27 +0000

    c0f023f602 fix(router): Ensure target `RouterStateSnapshot` is defined in `NavigationError` (#46842)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-26ea976";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-c0f023f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
