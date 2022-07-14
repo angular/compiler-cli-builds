58040688e9e826154cc22ba981af076dd8515e29
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-14 22:53:21 +0000

    71062e140f build: update cross-repo angular dependencies (#46834)

--- bundles/chunk-M7DRLMSX.js
+++ bundles/chunk-M7DRLMSX.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.6+sha-f8a25f7";
+var NGCC_VERSION = "14.0.6+sha-71062e1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
