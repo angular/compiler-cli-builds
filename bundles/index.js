782bba8da8bba7e8ec9a9457ba6ee296d9e2d114
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-14 21:26:58 +0000

    26ea97688c feat(router): Make router directives standalone (#46758)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-4a81fe7");
+var VERSION = new Version("14.2.0-next.0+sha-26ea976");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
