2001ac8e23b9dd19cbcc5e31eb96a99445359050
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 16:17:11 +0000

    571d7debb5 build: update cross-repo angular dependencies to 84dd092 (#46848)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-rc.0+sha-5e09cd1");
+var VERSION = new Version("14.1.0-rc.0+sha-571d7de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-rc.0+sha-5e09cd1";
+export declare const NGCC_VERSION = "14.1.0-rc.0+sha-571d7de";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
