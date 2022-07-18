ffb1b164e0129ec33f4783ab06b61fdcec74edda
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 17:01:16 +0000

    4a68b0f6ab docs: fix code style (#46862)

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-a0b4a92");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-4a68b0f");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.0-next.0+sha-a0b4a92") {
+    if (version === "14.2.0-next.0+sha-4a68b0f") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
