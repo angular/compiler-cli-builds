64ecd061fa81f3ef77e7a08ce028b76032dd86ab
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 17:00:58 +0000

    2ca404dbfd docs: fix code style (#46862)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-rc.0+sha-f9edb77");
+var VERSION = new Version("14.1.0-rc.0+sha-2ca404d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
