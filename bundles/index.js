ac302c4db8bc0bdbb9cd42e8dcd0626fb7eafbc8
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 22:19:01 +0000

    91b3ad1672 refactor(router): Update TitleStrategy to useFactory (#46876)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.0.6+sha-76badad");
+var VERSION = new Version("14.0.6+sha-91b3ad1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
