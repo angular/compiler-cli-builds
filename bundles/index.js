7e30470c9735fe8da779d77e5cd8eef97944b147
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-07-18 17:36:40 +0000

    5004b6ebf1 test: update tests to not run jasmine `done` function in sync-test zone from `describe` block

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-rc.0+sha-2ca404d");
+var VERSION = new Version("14.1.0-rc.0+sha-5004b6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0-rc.0+sha-2ca404d";
+export declare const NGCC_VERSION = "14.1.0-rc.0+sha-5004b6e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
