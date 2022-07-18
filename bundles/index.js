5687f5c5dffcfb4b0c1d2c9e4a55b6b93c8cf7ec
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 16:59:58 +0000

    a0b4a92552 build: fix forms size tracking numbers (#46871)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-557cf7d");
+var VERSION = new Version("14.2.0-next.0+sha-a0b4a92");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
