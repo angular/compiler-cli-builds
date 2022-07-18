a4cbd3a0ad160898870c70d3306556d9ca3027af
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 17:01:12 +0000

    c5c3cbc726 docs: fix code style (#46862)

--- bundles/chunk-M7DRLMSX.js
+++ bundles/chunk-M7DRLMSX.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.0.6+sha-031deb3";
+var NGCC_VERSION = "14.0.6+sha-c5c3cbc";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
