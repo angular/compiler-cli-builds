4951b87d66ae5712242a50dbfb749c4666173b82
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:54:36 +0000

    14081dc48d fix(core): Fix `runInContext` for `NgModuleRef` injector (#46877)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-e736f95";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-14081dc";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
