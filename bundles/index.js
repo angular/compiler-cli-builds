09ee973845ae9160ece5d31f0894f115c9b1b4c4
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:57:56 +0000

    0dc754a726 test: convert `source-map` core tests to use `async/await` instead of `fakeAsync` (#46888)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-14081dc");
+var VERSION = new Version("14.2.0-next.0+sha-0dc754a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-14081dc";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-0dc754a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
