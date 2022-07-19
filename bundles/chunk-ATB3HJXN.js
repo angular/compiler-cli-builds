c3430c054910f7c05b1115b064461bacc9b6d436
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:22:11 +0000

    0e3a2759b0 build: update github/codeql-action action to v2.1.16 (#46885)

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-6479cdb");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-0e3a275");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.0-next.0+sha-6479cdb") {
+    if (version === "14.2.0-next.0+sha-0e3a275") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/chunk-NDI3WTMG.js
+++ bundles/chunk-NDI3WTMG.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.0+sha-6479cdb";
+var NGCC_VERSION = "14.2.0-next.0+sha-0e3a275";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
