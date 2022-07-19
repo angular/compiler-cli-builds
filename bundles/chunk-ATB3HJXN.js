fbcf31ec2f9072f6f48be9fb27b7101d2af6948e
Andrew Kushnir <akushnir@google.com>
2022-07-19 16:59:10 +0000

    b431b5c6d6 test: convert `source-map` core tests to use `async/await` instead of `fakeAsync` (#46888)

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-rc.0+sha-88003e2");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0-rc.0+sha-b431b5c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0-rc.0+sha-88003e2") {
+    if (version === "14.1.0-rc.0+sha-b431b5c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0-rc.0+sha-88003e2");
+var VERSION = new Version("14.1.0-rc.0+sha-b431b5c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
