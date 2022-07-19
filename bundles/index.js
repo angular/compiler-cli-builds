8f7d458eb6f35494e887f8849bd3f648e1e0be8d
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:22:19 +0000

    e736f95884 build: update eslint dependencies to v5.30.7 (#46884)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-0e3a275");
+var VERSION = new Version("14.2.0-next.0+sha-e736f95");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
