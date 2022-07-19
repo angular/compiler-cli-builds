9cddb449fcc3323fdf9ec6649b22ca415926e683
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:20:57 +0000

    6479cdba43 build: update babel dependencies to v7.18.9 (#46883)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-bde925b");
+var VERSION = new Version("14.2.0-next.0+sha-6479cdb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
