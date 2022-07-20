04587468ae5f2b67c1c5c6ed390c6294657fe863
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-07-20 18:02:22 +0000

    793326deef release: cut the v14.1.0 release

--- bundles/chunk-MN3YDSGE.js
+++ bundles/chunk-MN3YDSGE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0-rc.0+sha-10d46b4";
+var NGCC_VERSION = "14.1.0+sha-793326d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
