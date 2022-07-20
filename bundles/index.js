4cf1d9663a3d5d8ea371748c21909f61756947c2
Andrew Kushnir <akushnir@google.com>
2022-07-20 18:52:53 +0000

    d583f85701 refactor(router): Update Router to be `providedIn: 'root'` (#46824)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-a0b2d36");
+var VERSION = new Version("14.2.0-next.0+sha-d583f85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
