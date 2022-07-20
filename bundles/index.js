8f2ec966cb9a15376208ca3051b572f574146462
Andrew Kushnir <akushnir@google.com>
2022-07-20 18:22:55 +0000

    a0b2d36415 refactor(platform-server): make `TransferState` standalone-friendly (#46879)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-473f65f");
+var VERSION = new Version("14.2.0-next.0+sha-a0b2d36");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
