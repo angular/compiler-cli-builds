ddd5fa48e26c69167ccb07c3d1457535911f03f1
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:05:51 +0000

    f55224a1f9 build: update cross-repo angular dependencies to cdaaa15 (#46853)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-0d192e8";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-f55224a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
