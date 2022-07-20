fbee5ea593a02a31a4df7930e683672ab90b788c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-07-20 21:47:14 +0000

    b5afd62a0b feat(common): explain why width/height is required

--- bundles/chunk-ATB3HJXN.js
+++ bundles/chunk-ATB3HJXN.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-186245a");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-b5afd62");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.0-next.0+sha-186245a") {
+    if (version === "14.2.0-next.0+sha-b5afd62") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-186245a");
+var VERSION = new Version("14.2.0-next.0+sha-b5afd62");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
