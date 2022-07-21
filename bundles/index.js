69e264f1ba843f9d6ea44b162ead8d3e8c17838f
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 12:34:44 +0000

    46c44de0cc refactor(platform-server): include TransferState providers into ServerModule (#46899)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-24f63fa");
+var VERSION = new Version("14.2.0-next.0+sha-46c44de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-24f63fa";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-46c44de";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
