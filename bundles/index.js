e5201335556678d94bfd4a1b715bab4f6de91be3
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 10:41:17 +0000

    24f63fa4e3 build: cut `zone.js` v0.11.7 (#46911)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-10289f1");
+var VERSION = new Version("14.2.0-next.0+sha-24f63fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
