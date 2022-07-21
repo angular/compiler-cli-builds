58ccc9b66257b962b1f4fea1f7dc9f191729c3b1
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 08:46:02 +0000

    3606917732 fix(core): improve the missing control flow directive message (#46903)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.0+sha-0cbbd6a";
+export declare const NGCC_VERSION = "14.1.0+sha-3606917";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
