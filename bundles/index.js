91952f6e5210c1985a40d8364295e1878bba7ac3
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 15:56:14 +0000

    52bbf7fd60 build: update doc approvers (#46939)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-d4c9523");
+var VERSION = new Version("14.2.0-next.0+sha-52bbf7f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
