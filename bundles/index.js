39e1939cb66f729bd1d66156b4563f826251372e
Andrew Kushnir <akushnir@google.com>
2022-08-01 20:12:43 +0000

    e2ab99b95e fix(common): allow null/undefined to be passed to ngClass input (#39280) (#46906)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-8600732");
+var VERSION = new Version("14.2.0-next.0+sha-e2ab99b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
