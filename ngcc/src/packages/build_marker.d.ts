0a592e2fde3dc15e1cde273213d5d4a7b0552d86
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:10:25 +0000

    af4f5df150 refactor(zone.js): remove leftover debugging code using `Error.stack` (#46989)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-99e5fa1";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-af4f5df";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
