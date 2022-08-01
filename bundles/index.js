082521a58279ffae357a7344ef760bb1beb7f8b4
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:09:22 +0000

    99e5fa157d test(service-worker): remove redundant ternary operator from `MockRequest` initialization (#46912)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-49e1912");
+var VERSION = new Version("14.2.0-next.0+sha-99e5fa1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
