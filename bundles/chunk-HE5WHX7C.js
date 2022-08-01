00218b0197697883bcab7f41fb251d80a6919e04
Andrew Kushnir <akushnir@google.com>
2022-08-01 18:43:17 +0000

    05f3f7445a fix(router): Use correct return type for provideRoutes function (#46941)

--- bundles/chunk-HE5WHX7C.js
+++ bundles/chunk-HE5WHX7C.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.0+sha-8cd95d4";
+var NGCC_VERSION = "14.1.0+sha-05f3f74";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
