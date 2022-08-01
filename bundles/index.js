a878df97abe95376826f0213a7f5045c901114d0
Andrew Kushnir <akushnir@google.com>
2022-08-01 21:37:40 +0000

    3f162f5e3f refactor(common): Align PathLocationStrategy constructor with default factory (#46929)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0+sha-d05d05e");
+var VERSION = new Version("14.1.0+sha-3f162f5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
