cf793e3d960d16c394962a6e7324756bc65f8284
Andrew Kushnir <akushnir@google.com>
2022-08-01 20:15:40 +0000

    f47c93ba3f build: update bazel setup to v5.5.3 (#47000)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-e2ab99b";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-f47c93b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
