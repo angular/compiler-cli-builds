c9c3cf6eff4b2dbe5e6d39a5348f4ecc8c48c747
Andrew Kushnir <akushnir@google.com>
2022-08-02 16:58:49 +0000

    d8cf78ba5e fix(router): Do not call preload method when not necessary (#47007)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-b757b13");
+var VERSION = new Version("14.2.0-next.0+sha-d8cf78b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
