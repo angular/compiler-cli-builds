45b330aa21564620a5d18d34d9874731a27a9da3
Andrew Kushnir <akushnir@google.com>
2022-08-02 19:17:51 +0000

    7243564499 build(docs-infra): ensure all boilerplate dependencies are in sync (#47009)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.0+sha-598b72b";
+var NGCC_VERSION = "14.2.0-next.0+sha-7243564";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
