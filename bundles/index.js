06dae6538f18401c5523fa2017b468036df01c38
Andrew Kushnir <akushnir@google.com>
2022-08-02 16:58:29 +0000

    b757b1387c build: update dev-infra packages and account for build-tooling split from `ng-dev` (#46976)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-1fe759d");
+var VERSION = new Version("14.2.0-next.0+sha-b757b13");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
