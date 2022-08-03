89c5f475911217c2d7887c73f920b2cc507e0a9b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-03 18:14:47 +0000

    6fabc0c679 release: cut the v14.2.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-71b4ccd");
+var VERSION = new Version("14.2.0-next.0+sha-6fabc0c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
