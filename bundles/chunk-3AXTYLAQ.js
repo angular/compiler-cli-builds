93ad71c0aa1fdb361c1436aeceec28babb558be0
Andrew Kushnir <akushnir@google.com>
2022-08-03 18:22:01 +0000

    3ee883e417 fix(docs-infra): do  not require `@ngModule` tag on standalone types (#47024)

--- bundles/chunk-3AXTYLAQ.js
+++ bundles/chunk-3AXTYLAQ.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.0+sha-efb8e3e");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.1+sha-3ee883e");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.0+sha-efb8e3e") {
+    if (version === "14.1.1+sha-3ee883e") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.0+sha-efb8e3e");
+var VERSION = new Version("14.1.1+sha-3ee883e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
