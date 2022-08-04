bb3e9cee9de812d33c36a406b86c3dd38c979d19
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 00:52:35 +0000

    f42a7c5234 docs(core): Add docs for keyboard event binding (#47022)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-7e491cf");
+var VERSION = new Version("14.2.0-next.0+sha-f42a7c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
