445bc1473ff5cc39a8f9f6cbf80d7b51f2cc386d
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 00:52:26 +0000

    d0f4f1d601 docs: update Vale installation instructions (#47031)

--- bundles/chunk-3AXTYLAQ.js
+++ bundles/chunk-3AXTYLAQ.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.1+sha-3ee883e");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.1+sha-d0f4f1d");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.1+sha-3ee883e") {
+    if (version === "14.1.1+sha-d0f4f1d") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
