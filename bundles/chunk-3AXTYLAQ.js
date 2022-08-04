10cd9b0a818d697d4f99602c88e0cfcff4098f65
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 21:42:13 +0000

    f23232ff66 feat(zone.js): Update to the simpler Async Stack Tagging v2 API (#46958)

--- bundles/chunk-3AXTYLAQ.js
+++ bundles/chunk-3AXTYLAQ.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-2b4d7f6");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.0+sha-f23232f");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.0-next.0+sha-2b4d7f6") {
+    if (version === "14.2.0-next.0+sha-f23232f") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
