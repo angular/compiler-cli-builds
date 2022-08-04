b6b576cc04f0823f0db8deb352523702df7b439e
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 20:24:33 +0000

    1d39ab6e6b build: update io_bazel_rules_sass digest to 70ffe01 (#47023)

--- bundles/chunk-HE5WHX7C.js
+++ bundles/chunk-HE5WHX7C.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.1+sha-d0f4f1d";
+var NGCC_VERSION = "14.1.1+sha-1d39ab6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
