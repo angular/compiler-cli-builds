518e3784f1b0a1363c1a421f805670238470a4e3
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:14:54 +0000

    c1ad37d161 refactor(core): drop unused fields from the `RootContext` (#46806)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-0b392af");
+var VERSION = new Version("14.2.0-next.0+sha-c1ad37d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
