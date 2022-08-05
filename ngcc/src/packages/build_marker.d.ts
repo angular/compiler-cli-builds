bded6a76a0769b8666d9947eb15c64191342c66b
Andrew Scott <atscott01@gmail.com>
2022-08-05 16:52:36 +0000

    c7fed38cf0 refactor(router): Cherry-pick Router services to be providedIn: 'root' (#47052)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.1+sha-1d39ab6";
+export declare const NGCC_VERSION = "14.1.1+sha-c7fed38";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
