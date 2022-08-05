e015fc770d8f7634f55314899f575b3e6691c057
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:22:31 +0000

    387f4a79f8 docs: update dependency injection content (#44466)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.1+sha-c7fed38");
+var VERSION = new Version("14.1.1+sha-387f4a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
