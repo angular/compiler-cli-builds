44233b93c6dfa31756180caf979fd1c9e0bc6cc4
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:22:37 +0000

    0920a15139 docs: update dependency injection content (#44466)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.0+sha-c1ad37d";
+var NGCC_VERSION = "14.2.0-next.0+sha-0920a15";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
