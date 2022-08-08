24abf5250187df1ba75bf4c11e2f3c0e326105ba
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:50:28 +0000

    951d02e1de docs: fix dependecy injection documentation page typos (#47057)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-7343ec7");
+var VERSION = new Version("14.2.0-next.0+sha-951d02e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
