5416868dd10b6c74394e73e249b1959a5eb679a9
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:50:50 +0000

    621c38813f refactor: improve disabled attribute warning (#47041)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.0+sha-dbed2cf";
+var NGCC_VERSION = "14.2.0-next.0+sha-621c388";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
