1cfdf4cb93e27d14d14216a8b6bcf317ac4d64dc
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 16:39:17 +0000

    561cd84901 docs: fix link in dependency injection guide (#47055) PR Close #47055

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-next.0+sha-0abb67a");
+var VERSION = new Version("14.2.0-next.0+sha-561cd84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
