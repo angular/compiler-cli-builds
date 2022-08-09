2a53d8b405040387fb805ab4f0aacb215121313f
Dylan Hunn <dylhunn@gmail.com>
2022-08-09 18:12:31 +0000

    8546b17ade build: update github/codeql-action action to v2.1.18 (#47015)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.0+sha-e4aee2b";
+var NGCC_VERSION = "14.2.0-next.0+sha-8546b17";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
