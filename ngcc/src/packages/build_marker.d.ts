86e2ca6b6550ac212dadc243b95552351c3c336c
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 18:01:36 +0000

    4e3fc7eac8 docs: clarify providers' injection in lazy loaded modules (#47094)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.0+sha-2a43bee";
+export declare const NGCC_VERSION = "14.2.0-next.0+sha-4e3fc7e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
