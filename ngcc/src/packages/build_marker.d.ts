7219b2877c0f772f771854c5b2b3a5bd182f93a9
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 16:21:58 +0000

    01f5598571 docs: remove duplicated sentence (#47126) PR Close #47126

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.1+sha-6454974";
+export declare const NGCC_VERSION = "14.2.0-next.1+sha-01f5598";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
