8ca45e2ccdc424f218b232ab6e543909a081ebc7
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 17:20:00 +0000

    0a99130f85 docs: add NG Poland 2022 and JS Poland 2022 to events (#47128) PR Close #47128

--- bundles/chunk-HE5WHX7C.js
+++ bundles/chunk-HE5WHX7C.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.1.2+sha-6a94157";
+var NGCC_VERSION = "14.1.2+sha-0a99130";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
