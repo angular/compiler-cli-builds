a7cbc5a14d96650217f55df4e92f3ecd3575deb4
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 18:34:36 +0000

    970e68ae38 docs: remove duplicated styles (#47143)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.2+sha-a5ca049";
+export declare const NGCC_VERSION = "14.1.2+sha-970e68a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
