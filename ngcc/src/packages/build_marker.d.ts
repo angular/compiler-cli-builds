a753ca828bf50d671f407132153a245938c5f173
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 10:14:50 +0000

    1329707327 docs: clarify use of unsubscribe with HttpClient (#47127)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0-next.1+sha-5509e35";
+export declare const NGCC_VERSION = "14.2.0-next.1+sha-1329707";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
