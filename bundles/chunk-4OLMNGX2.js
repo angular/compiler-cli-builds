999c3e50948bbd580bbb89f397117f3a65df05c8
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 16:28:34 +0000

    d4f44bde50 refactor(router): Provide easily patchable file for assigning relativeLinkResolution (#47136)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.1+sha-757684b";
+var NGCC_VERSION = "14.2.0-next.1+sha-d4f44bd";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
