6d6344733e15b803e4b691c694b5ad03472e88d0
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 13:44:49 +0000

    422323cee0 feat(router): improve typings for RouterLink boolean inputs (#47101)

--- bundles/chunk-4OLMNGX2.js
+++ bundles/chunk-4OLMNGX2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-next.1+sha-93289f9";
+var NGCC_VERSION = "14.2.0-next.1+sha-422323c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
