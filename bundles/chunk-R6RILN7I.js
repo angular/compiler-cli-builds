6ef53cf5991b8a2ada929e19cddb21bf46e716e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-08-17 15:29:14 +0000

    c83e861a3f release: cut the v14.2.0-rc.0 release

--- bundles/chunk-R6RILN7I.js
+++ bundles/chunk-R6RILN7I.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-next.1+sha-f2b9956");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.0-rc.0+sha-c83e861");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.0-next.1+sha-f2b9956") {
+    if (version === "14.2.0-rc.0+sha-c83e861") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
