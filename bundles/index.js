7a59e8c381e0d4e41499d783b30714d84dfffbde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-18 19:27:07 +0000

    a9c21c3f51 fix(service-worker): update golden `index.md` (#47044)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-19d7fb0");
+var VERSION = new Version("14.3.0-next.0+sha-a9c21c3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
