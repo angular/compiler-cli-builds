c32a835606c137ceb5311e842605a8bc0f2d5c95
Joey Perrott <josephperrott@gmail.com>
2022-08-22 13:22:14 +0000

    b13c991ce7 fix(docs-infra): avoid distorting contributor images (#47215)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-a360309");
+var VERSION = new Version("14.3.0-next.0+sha-b13c991");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
