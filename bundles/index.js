c5c842d4f3e664681489561b6c8dc791ccce9eac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:12:28 +0000

    2095a08781 docs: fix grammar issues in various markdown files (#47200)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-05f5e8a");
+var VERSION = new Version("14.3.0-next.0+sha-2095a08");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
