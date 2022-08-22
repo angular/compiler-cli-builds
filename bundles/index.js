e20ee1cd29d62b5aa38ba0b0dce856e909405863
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 23:33:06 +0000

    eaaa294e49 docs: update peek-a-boo example (#47219)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.1.3+sha-791fa9a");
+var VERSION = new Version("14.1.3+sha-eaaa294");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
