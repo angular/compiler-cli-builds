52919ca5934ef68d959d6170394bfa05feba556b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 17:46:52 +0000

    c467eb78d9 build: update all non-major dependencies (#47162)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.3.0-next.0+sha-d7c0039";
+export declare const NGCC_VERSION = "14.3.0-next.0+sha-c467eb7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
