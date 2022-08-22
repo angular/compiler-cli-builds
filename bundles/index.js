044d222e2bfd32f7ab6f313e3a59a66e8e903b43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 17:45:24 +0000

    d7c0039433 docs: Add Bob Watson as a member of the Angular team (#47217)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-b13c991");
+var VERSION = new Version("14.3.0-next.0+sha-d7c0039");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
