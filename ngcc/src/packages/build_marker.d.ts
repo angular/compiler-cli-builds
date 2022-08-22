f4b79649f7fb9a310c05113207b95e9ce627a20a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:09:27 +0000

    876ba77bcd refactor(docs-infra): fix TypeScript warnings due to property types (#47201)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.3.0-next.0+sha-b3879db";
+export declare const NGCC_VERSION = "14.3.0-next.0+sha-876ba77";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
