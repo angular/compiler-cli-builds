41226b3f19b05bb258cd1e2f3d35a44b8e65938a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 23:38:10 +0000

    4fdbd85d26 fix(common): consider density descriptors with multiple digits as valid (#47230)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-d34ae84");
+var VERSION = new Version("14.3.0-next.0+sha-4fdbd85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
