e4b88757e0aabcad5da47a08303517b900e8954e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 17:59:33 +0000

    78e72db19c ci: do not match dev-infra group for pullapprove owner changes (#47216)

--- bundles/chunk-4BAQBKNO.js
+++ bundles/chunk-4BAQBKNO.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-rc.0+sha-0dd6102";
+var NGCC_VERSION = "14.2.0-rc.0+sha-78e72db";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0-rc.0+sha-0dd6102");
+var VERSION = new Version("14.2.0-rc.0+sha-78e72db");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
