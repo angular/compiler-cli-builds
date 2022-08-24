e9b8dcfbdd4e8781d354fe08b368f17b599f6545
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-24 18:21:28 +0000

    46e8982c6a build: invalidate circleci cache to prune unused nested node modules (#47240)

--- bundles/chunk-3AXTYLAQ.js
+++ bundles/chunk-3AXTYLAQ.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.1.3+sha-09834f9");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.1.3+sha-46e8982");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.1.3+sha-09834f9") {
+    if (version === "14.1.3+sha-46e8982") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.1.3+sha-09834f9";
+export declare const NGCC_VERSION = "14.1.3+sha-46e8982";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
