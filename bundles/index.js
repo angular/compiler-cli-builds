e5dede5a0dae944bc832c69c96183464cba459ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-25 21:12:49 +0000

    3d7deabca6 build: update ng-dev version (#47258)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.3.0-next.0+sha-0afb895");
+var VERSION = new Version("14.3.0-next.0+sha-3d7deab");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
