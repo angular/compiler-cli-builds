6c7e51c2f0e1f6fabf708353129ad4bec332c14e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-25 21:48:24 +0000

    e9b3d2072b release: cut the v14.2.0 release

--- bundles/chunk-4BAQBKNO.js
+++ bundles/chunk-4BAQBKNO.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0-rc.0+sha-c5db867";
+var NGCC_VERSION = "14.2.0+sha-e9b3d20";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
