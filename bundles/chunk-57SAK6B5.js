b69399469a7b33be3857cb6e25f87ce352d42423
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-26 17:47:45 +0000

    20e1a2470b build: update to TypeScript 4.8 final (#47270) (#47278)

--- bundles/chunk-57SAK6B5.js
+++ bundles/chunk-57SAK6B5.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.0+sha-59a6fe7";
+var NGCC_VERSION = "14.2.0+sha-20e1a24";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
