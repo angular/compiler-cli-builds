4c1e2a5f7c52fed90895f313b836b80eed105124
GitHub <noreply@github.com>
2022-08-30 17:15:13 +0000

    0bb3aff01f build: update `CODE_OF_CONDUCT.md` to match the content of `angular/.github`

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.0+sha-7d3df47";
+export declare const NGCC_VERSION = "15.0.0-next.0+sha-0bb3aff";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
