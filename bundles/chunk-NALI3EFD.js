4d98f6bc493e3aa4cc9813e8f1df926a409b4f26
Joey Perrott <josephperrott@gmail.com>
2022-08-30 18:26:54 +0000

    3b91101bbc docs: update security guide (#47296)

--- bundles/chunk-NALI3EFD.js
+++ bundles/chunk-NALI3EFD.js
@@ -887,7 +887,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "13.3.11+sha-41c6f7a";
+var NGCC_VERSION = "13.3.11+sha-3b91101";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "13.3.11+sha-41c6f7a";
+export declare const NGCC_VERSION = "13.3.11+sha-3b91101";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
