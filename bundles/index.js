340ba7d4b26f1e614d97f1165076b1fb264e6d2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-02 02:37:32 +0000

    af975d5bef docs(http): replace http method return description (#47322)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0+sha-4ac5ea3");
+var VERSION = new Version("14.2.0+sha-af975d5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
