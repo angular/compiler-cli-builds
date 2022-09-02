501070c1d125e5225e95debac95efced19e1e1d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-02 02:33:12 +0000

    5dd72b7610 docs(http): replace http method return description (#47322)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.0+sha-0bb3aff";
+export declare const NGCC_VERSION = "15.0.0-next.0+sha-5dd72b7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
