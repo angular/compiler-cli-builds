b9dc0d6eff1976e39863348ba6a7e756c4895e97
Andrew Kushnir <akushnir@google.com>
2022-09-06 20:28:14 +0000

    bea953a5cf build: update dependency typescript to ~4.8.0 (#47293)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-cf0c53a");
+var VERSION = new Version("15.0.0-next.0+sha-bea953a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
