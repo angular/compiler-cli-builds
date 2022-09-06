e97cd76ef23a04ea5ae31cd9f26efccd365792fd
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:31:35 +0000

    b8fd0da47f docs(docs-infra): improve docs on updating angular.io and docs examples apps (#47250)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-28f277e");
+var VERSION = new Version("15.0.0-next.0+sha-b8fd0da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
