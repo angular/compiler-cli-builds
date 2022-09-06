00625bda7905a34aa3604000f030c4f1759beec1
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:58:50 +0000

    94ea8700d7 docs: fix linting errors in component overview guide (#47352)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-99c12c9");
+var VERSION = new Version("15.0.0-next.0+sha-94ea870");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
