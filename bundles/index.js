b74f747de1c9615a9633a575bf760f2d47059df9
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:25:48 +0000

    6091786696 fix(service-worker): include headers in requests for assets (#47260)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.0+sha-626340b");
+var VERSION = new Version("14.2.0+sha-6091786");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0+sha-626340b";
+export declare const NGCC_VERSION = "14.2.0+sha-6091786";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
