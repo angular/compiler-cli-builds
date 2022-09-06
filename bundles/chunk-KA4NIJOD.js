2cfb0176abaeb70ce8dc1a0fd180433197f9ce7d
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:30:35 +0000

    17d66d9302 docs: compress the bios image (#47244)

--- bundles/chunk-KA4NIJOD.js
+++ bundles/chunk-KA4NIJOD.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.0+sha-f8e78e3";
+var NGCC_VERSION = "15.0.0-next.0+sha-17d66d9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-f8e78e3");
+var VERSION = new Version("15.0.0-next.0+sha-17d66d9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
