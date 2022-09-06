9fc7daf2fa5cff2f071750b8dc943690368cf9f9
Andrew Kushnir <akushnir@google.com>
2022-09-06 18:49:01 +0000

    ab936dd407 build: update cross-repo angular dependencies to f2a04c8 (#47231)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.0+sha-e1bb638";
+export declare const NGCC_VERSION = "15.0.0-next.0+sha-ab936dd";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
