c61ec31d5acfa5f70c1efd6d51122c518d36fed7
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:20:25 +0000

    a0c3326485 fix(service-worker): only consider GET requests as navigation requests (#47263)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-b9f80d2");
+var VERSION = new Version("15.0.0-next.0+sha-a0c3326");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
