5981d7604e68caf89d77685b4ecf3570480d6bc6
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:47:46 +0000

    a46886500f docs: fix typo in image directive guide (#47313)

--- bundles/chunk-4ZCDRNBU.js
+++ bundles/chunk-4ZCDRNBU.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.0+sha-329fed7");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.0+sha-a468865");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.0.0-next.0+sha-329fed7") {
+    if (version === "15.0.0-next.0+sha-a468865") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
