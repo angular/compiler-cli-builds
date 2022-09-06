b5eacbf113137ab781bcbd3fa30d0f1c19ee1fa4
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:07:03 +0000

    9fbf810744 build: add zone.js to bug-report template for github (#47213)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-385471d");
+var VERSION = new Version("15.0.0-next.0+sha-9fbf810");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
