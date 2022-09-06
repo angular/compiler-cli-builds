0d1a9de38a02ac0d169dcc10b1371a9b68d307c9
Andrew Kushnir <akushnir@google.com>
2022-09-06 18:51:03 +0000

    1edb8e5c45 docs: add new words to vocabulary (#47344)

--- bundles/chunk-KA4NIJOD.js
+++ bundles/chunk-KA4NIJOD.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.0+sha-e810402";
+var NGCC_VERSION = "15.0.0-next.0+sha-1edb8e5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-e810402");
+var VERSION = new Version("15.0.0-next.0+sha-1edb8e5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
