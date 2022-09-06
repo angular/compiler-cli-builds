b42981fd3d14195a5a3dcfdc2d4d65d0b0c79f9f
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:42:36 +0000

    329fed78f3 docs: fix doc linter warnings in `app-shell.md` (#47282)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.0+sha-d077d58";
+export declare const NGCC_VERSION = "15.0.0-next.0+sha-329fed7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
