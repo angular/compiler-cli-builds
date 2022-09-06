aa41f990fc16738822e3868277686822be09a057
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:33:23 +0000

    33bb34b64e refactor(service-worker): refactored code to remove useless code (#47255)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-0d13cc7");
+var VERSION = new Version("15.0.0-next.0+sha-33bb34b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
