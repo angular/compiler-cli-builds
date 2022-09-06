b5ba241c046f2fb1ef41827026e9605afa8d3cbb
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:31:55 +0000

    0d13cc7848 docs: fixing typos and grammatical errors in several documentation files (#47254)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.0+sha-b8fd0da");
+var VERSION = new Version("15.0.0-next.0+sha-0d13cc7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
