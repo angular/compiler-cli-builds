645ab133914dc6533a6435a3963c22f990b19939
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-09-07 18:01:11 +0000

    e25349cd41 release: cut the v15.0.0-next.1 release

--- bundles/chunk-NX4VAKY6.js
+++ bundles/chunk-NX4VAKY6.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.0+sha-710109b";
+var NGCC_VERSION = "15.0.0-next.1+sha-e25349c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
