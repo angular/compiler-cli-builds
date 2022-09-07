cc8b3d64d8788c0fe96ea4ee165b43e2068f6e1d
Andrew Kushnir <akushnir@google.com>
2022-09-07 18:06:46 +0000

    6dec750e4b docs: fix linter errors for accessibility page (#47327)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-25c4e4c");
+var VERSION = new Version("15.0.0-next.1+sha-6dec750");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
