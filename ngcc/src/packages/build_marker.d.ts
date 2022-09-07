8494c14f6a7c89586e110314cb148bdbf196fdf8
Andrew Kushnir <akushnir@google.com>
2022-09-07 18:11:19 +0000

    16ba9aaaef build: update dependency typescript to ~4.8.0 (#47367)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.0+sha-0e35829";
+export declare const NGCC_VERSION = "14.2.1+sha-16ba9aa";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
