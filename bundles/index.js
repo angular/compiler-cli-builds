4ebd4f8acb13a70f8ff65ac67434b766a80d28f2
Dylan Hunn <dylhunn@gmail.com>
2022-09-08 16:51:46 +0000

    4e10a74941 fix(core): imply @Optional flag when a default value is provided (#47242)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-bd66f49");
+var VERSION = new Version("15.0.0-next.1+sha-4e10a74");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
