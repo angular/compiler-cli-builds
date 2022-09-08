80d11b1ee6a0d41e2dc22d5edc5ff8e4aea5c741
Dylan Hunn <dylhunn@gmail.com>
2022-09-08 16:51:37 +0000

    bd66f49261 docs: updates es version based on issue #47335 (#47379)

--- bundles/chunk-NX4VAKY6.js
+++ bundles/chunk-NX4VAKY6.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.1+sha-6dec750";
+var NGCC_VERSION = "15.0.0-next.1+sha-bd66f49";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
