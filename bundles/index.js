04076c53e202c3dc778f8cc9ced3eddb9ca9ba21
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:45:56 +0000

    ca7bf65933 fix(common): rename `rawSrc` -> `ngSrc` in NgOptimizedImage directive (#47362)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-710d1da");
+var VERSION = new Version("15.0.0-next.1+sha-ca7bf65");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
