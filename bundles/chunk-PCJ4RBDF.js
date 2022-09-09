34e44afbfc7360ec5d1d77d8e9d2094340687b18
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:47:46 +0000

    a61d553d73 refactor(common): drop unnecessary srcset sanitization (#47302)

--- bundles/chunk-PCJ4RBDF.js
+++ bundles/chunk-PCJ4RBDF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.1+sha-403b970";
+var NGCC_VERSION = "14.2.1+sha-a61d553";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
