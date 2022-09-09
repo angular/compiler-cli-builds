77eb2aa1c0236ca8d47def72488f2ff85f2747cd
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:21:13 +0000

    7e0de5b8be docs: fix typo in app.component.ts (#47384)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-c377f57");
+var VERSION = new Version("15.0.0-next.1+sha-7e0de5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
