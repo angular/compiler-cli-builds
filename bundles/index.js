48b23e4c50396e0dbe51df5c34cb1d7132291bb5
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:47:17 +0000

    678c7f1841 docs: Removed unnecessary blank line (#47329)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-9e2d3ed");
+var VERSION = new Version("15.0.0-next.1+sha-678c7f1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
