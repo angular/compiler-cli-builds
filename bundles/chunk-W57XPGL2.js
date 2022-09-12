4793dd053e54a9ebae768ed98b55d7db7c7001a4
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:43:48 +0000

    c26854b733 docs: update prompts in the doc issue template (#47389)

--- bundles/chunk-W57XPGL2.js
+++ bundles/chunk-W57XPGL2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.1+sha-f0f809c";
+var NGCC_VERSION = "15.0.0-next.1+sha-c26854b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-f0f809c");
+var VERSION = new Version("15.0.0-next.1+sha-c26854b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
