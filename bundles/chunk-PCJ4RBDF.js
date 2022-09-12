592798af9e7030760a218f64f607b659e0509a86
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:44:23 +0000

    4b7b1aee96 docs: update prompts in the doc issue template (#47389)

--- bundles/chunk-PCJ4RBDF.js
+++ bundles/chunk-PCJ4RBDF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.1+sha-2d7ce00";
+var NGCC_VERSION = "14.2.1+sha-4b7b1ae";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
