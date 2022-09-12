fec80982eeebd6569d617ca1b16dd9bc001e6ae5
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 19:48:38 +0000

    2d7ce00caa build(docs-infra): introduce new process for generating data for the events page (#45588)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.1+sha-c9bdf9b");
+var VERSION = new Version("14.2.1+sha-2d7ce00");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.1+sha-c9bdf9b";
+export declare const NGCC_VERSION = "14.2.1+sha-2d7ce00";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
