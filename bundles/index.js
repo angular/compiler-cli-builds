2e6d0e33913b4bae6a7e99a261656e18457271a5
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:43:02 +0000

    f0f809c561 docs: replace deprecated --outFile option for ng extract-i18n (#47397)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-0d4594a");
+var VERSION = new Version("15.0.0-next.1+sha-f0f809c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
