2fd82f0ee400990ef8dcdce749cb1f2956b5e56e
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:42:03 +0000

    0d4594a53e docs: fix angle brackets in details code block (#47400)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-c9241fd");
+var VERSION = new Version("15.0.0-next.1+sha-0d4594a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
