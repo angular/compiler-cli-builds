e0973678dcea946b31de90eed31928bad9bef53e
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 02:22:59 +0000

    ec9ee8e2bb docs(forms): correcting description verbs of formGroup methods  (#47399)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-c26854b");
+var VERSION = new Version("15.0.0-next.1+sha-ec9ee8e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
