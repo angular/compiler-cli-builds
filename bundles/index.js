861defdeba45c53c48eb68cef4d4ad1dacfdd58b
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 16:38:17 +0000

    bba5abd050 docs(core): fix viewProviders & providers typos in NodeInjectorFactory (#47408)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.1+sha-6c7cd6b");
+var VERSION = new Version("15.0.0-next.1+sha-bba5abd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
