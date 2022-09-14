00933acd7cd5729d3dbdf1a89d9ed1f967be6a04
Dylan Hunn <dylhunn@gmail.com>
2022-09-14 06:02:54 +0000

    69ecbd5bcc ci: update payload size for Animations integration test (#47424)

--- bundles/chunk-W57XPGL2.js
+++ bundles/chunk-W57XPGL2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.1+sha-7ec196e";
+var NGCC_VERSION = "15.0.0-next.1+sha-69ecbd5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
