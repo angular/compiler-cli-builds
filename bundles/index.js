e0b6be0b048cb8bad94329706988382b13cd4b07
Dylan Hunn <dylhunn@gmail.com>
2022-09-14 16:40:49 +0000

    a0cb5011ed ci: update payload size for cli-hello-world-lazy test (#47431)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.1+sha-bb06432");
+var VERSION = new Version("14.2.1+sha-a0cb501");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
