5d59f7cfb23a13e9bb02b5dbe0cb95960551df5a
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:39:25 +0000

    d961f1fb9b docs(docs-infra): add documentation on debugging the events generation process (#47449)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.2+sha-cc2d32a";
+export declare const NGCC_VERSION = "14.2.2+sha-d961f1f";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
