46b43ce2b1e6d31783b5abc689370e69186f2ed9
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:40:29 +0000

    deaa05a393 refactor(core): generate initial inputs from inputs map (#47228)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-8deedc5");
+var VERSION = new Version("15.0.0-next.2+sha-deaa05a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
