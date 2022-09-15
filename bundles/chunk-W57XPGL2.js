2eebd942030c604185c1c594e4d23ddcb9327060
GitHub <noreply@github.com>
2022-09-15 17:44:23 +0000

    1f88df0f8f build: update `CODE_OF_CONDUCT.md` to match the content of `angular/.github`

--- bundles/chunk-W57XPGL2.js
+++ bundles/chunk-W57XPGL2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.2+sha-deaa05a";
+var NGCC_VERSION = "15.0.0-next.2+sha-1f88df0";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
