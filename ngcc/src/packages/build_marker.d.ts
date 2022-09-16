eaf79bf0a1925b1f80f740719da32cd2ec76fabe
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-16 09:33:12 +0000

    5fb7ae20a2 fix(docs-infra): initialize events arrays in EventsComponent (#47458)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.2+sha-1f88df0";
+export declare const NGCC_VERSION = "15.0.0-next.2+sha-5fb7ae2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
