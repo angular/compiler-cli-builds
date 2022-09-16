56793ef169d2939c97f80be1ccbdf8260614e1de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-16 09:36:15 +0000

    db3b532950 fix(docs-infra): fix `Update AIO events` GitHub Action (#47452)

--- bundles/chunk-PCJ4RBDF.js
+++ bundles/chunk-PCJ4RBDF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.2+sha-d961f1f";
+var NGCC_VERSION = "14.2.2+sha-db3b532";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.2+sha-d961f1f");
+var VERSION = new Version("14.2.2+sha-db3b532");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
