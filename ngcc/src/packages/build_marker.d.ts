90a4333c28bd9dad8b4e26bd9c718a67a74a0698
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 17:16:42 +0000

    32cad55f0d feat(devtools): implement inspect functionality for directives (#47334)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.2+sha-150dd30";
+export declare const NGCC_VERSION = "15.0.0-next.2+sha-32cad55";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
