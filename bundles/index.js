e3c52a7bd1ac03b2f86c3f5e567738fb30a2a169
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:28:32 +0000

    14d5fc5700 docs: add issue template for Angular DevTools (#47383)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-247181f");
+var VERSION = new Version("15.0.0-next.2+sha-14d5fc5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
