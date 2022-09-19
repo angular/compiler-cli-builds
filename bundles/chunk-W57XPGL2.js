9a5ec21c0ad1e0199fd22934c5734638b08b77b5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:17:37 +0000

    b6c90e8e0b fix(devtools): fix positioning of property explorer expansion icon (#47446)

--- bundles/chunk-W57XPGL2.js
+++ bundles/chunk-W57XPGL2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.2+sha-a192c2d";
+var NGCC_VERSION = "15.0.0-next.2+sha-b6c90e8";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
