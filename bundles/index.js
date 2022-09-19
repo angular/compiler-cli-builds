a1bd7f3e412b6bb34232e7200c07f956422c81a7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 11:29:36 +0000

    14cb9fb005 test(docs-infra): add mocks for missing mat-icons (#47469)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-8654e5c");
+var VERSION = new Version("15.0.0-next.2+sha-14cb9fb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.2+sha-8654e5c";
+export declare const NGCC_VERSION = "15.0.0-next.2+sha-14cb9fb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
