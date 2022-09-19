c0ec6b1b87e90245acc171b1e1ebf787a9513fba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 11:31:10 +0000

    a192c2de73 fix(docs-infra): apply `text-top` instead of `top` for table alignment (#47457)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-14cb9fb");
+var VERSION = new Version("15.0.0-next.2+sha-a192c2d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
