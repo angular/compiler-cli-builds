5e5c57bb5722e7fb236043f6c24eebf11ce94071
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 07:57:46 +0000

    ad098d82a8 docs(docs-infra): add missing decorator in tutorial step (#47456) PR Close #47456

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-7f637e1");
+var VERSION = new Version("15.0.0-next.2+sha-ad098d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
