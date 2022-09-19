1a22cd42d9f207d046ce11d46f8013898965f3e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 16:36:26 +0000

    aaad830d24 ci: remove atscott from unavailable list (#47482)

--- bundles/chunk-PCJ4RBDF.js
+++ bundles/chunk-PCJ4RBDF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.2+sha-1dd5b42";
+var NGCC_VERSION = "14.2.2+sha-aaad830";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.2+sha-1dd5b42";
+export declare const NGCC_VERSION = "14.2.2+sha-aaad830";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
