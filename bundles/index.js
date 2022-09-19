a2eb09c3ff919f527662c05414b661af30407b58
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 17:15:32 +0000

    150dd3092b docs: add fatima amzil to contributors (#47097)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-8a3054c");
+var VERSION = new Version("15.0.0-next.2+sha-150dd30");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
