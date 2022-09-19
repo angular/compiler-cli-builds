c6d2601a0e2db5b62d5ea32177c61f79ed8f7fa9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 16:34:56 +0000

    8a3054c954 ci: remove atscott from unavailable list (#47482)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-7814da6");
+var VERSION = new Version("15.0.0-next.2+sha-8a3054c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
