c6428d3a28ce99da2a4abc0bb19f73c590489558
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 07:56:09 +0000

    7f637e1873 docs: move Image Directive guide link to the Feature Preview section (#47466)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-4eff4d3");
+var VERSION = new Version("15.0.0-next.2+sha-7f637e1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
