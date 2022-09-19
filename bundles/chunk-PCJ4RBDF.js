8cd0d70d0e217a7e50bdddb7dfd3ee618c7011a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:42:46 +0000

    1dd5b4250a build: update dependency google-closure-compiler to v20220905 (#47437)

--- bundles/chunk-PCJ4RBDF.js
+++ bundles/chunk-PCJ4RBDF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.2+sha-e563139";
+var NGCC_VERSION = "14.2.2+sha-1dd5b42";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
