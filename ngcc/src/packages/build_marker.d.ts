247b81e57cf5bcbc7f0d2f6ebf9db88516e33add
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-20 08:30:20 +0000

    bba2dae812 fix(animations): make sure that the useAnimation function delay is applied (#47468)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.2+sha-fe6d33c";
+export declare const NGCC_VERSION = "14.2.2+sha-bba2dae";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
