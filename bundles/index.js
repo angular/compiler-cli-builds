9ecad9ebe6fedc83e092f5ed0e73ce350c0d639f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 08:24:47 +0000

    06ca0dcf27 refactor(core): remove assumptions that component will be first in directives list (#47490)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-59aa2c0");
+var VERSION = new Version("15.0.0-next.2+sha-06ca0dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
