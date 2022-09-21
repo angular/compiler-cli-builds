09ac3bb062e75530c836671b60f40afc10536775
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 14:21:38 +0000

    8cef5dd7c1 build: update all non-major dependencies (#47487)

--- bundles/chunk-W57XPGL2.js
+++ bundles/chunk-W57XPGL2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.2+sha-06ca0dc";
+var NGCC_VERSION = "15.0.0-next.2+sha-8cef5dd";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.2+sha-06ca0dc");
+var VERSION = new Version("15.0.0-next.2+sha-8cef5dd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
