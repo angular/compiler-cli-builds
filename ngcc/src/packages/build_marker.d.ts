c4fa68c44e051766615c3c97ae51ea7f3f235365
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:23:36 +0000

    76f393b322 docs: visualize keyboard shortcut for search (#47507)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.3+sha-7e6fc10";
+export declare const NGCC_VERSION = "14.2.3+sha-76f393b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
