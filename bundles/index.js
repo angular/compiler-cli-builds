2e41d6f7d37c3e512efdf1b4bd586fafaa0b7f38
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:18:20 +0000

    ddef448cf3 docs: add v15 scheduled release dates (#47513)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-85330f3");
+var VERSION = new Version("15.0.0-next.3+sha-ddef448");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
