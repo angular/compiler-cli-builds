116a5337f3c3b311e9e632cafe264704fa927aa4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:16:05 +0000

    3fe21a67ca refactor(core): expose host directives to their host through DI (#47476)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-7a5ba93");
+var VERSION = new Version("15.0.0-next.3+sha-3fe21a6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
