aa033bd439fa5d96b07249c295ec38eaf379d27d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 22:07:53 +0000

    86372538ab refactor(zone.js): remove zone-async-tagging from zone.js (#47416)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-291a5b3");
+var VERSION = new Version("15.0.0-next.3+sha-8637253");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
