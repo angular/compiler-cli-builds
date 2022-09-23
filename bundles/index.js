e19eb7a09431d3f8ed352fdd04b0458e9dae11c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:17:55 +0000

    85330f3fd9 fix(core): update `isDevMode` to rely on `ngDevMode` (#47475)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-3fe21a6");
+var VERSION = new Version("15.0.0-next.3+sha-85330f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
