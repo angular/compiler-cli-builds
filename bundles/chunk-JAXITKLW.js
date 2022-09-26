6968790a5f49f5e91337a6a74013e1d1dfee9b26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-26 21:19:43 +0000

    85b5d125be build: fix rxjs 7 build failure (#47548)

--- bundles/chunk-JAXITKLW.js
+++ bundles/chunk-JAXITKLW.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.3+sha-f9e21d3";
+var NGCC_VERSION = "15.0.0-next.3+sha-85b5d12";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
