fd1d10e5d12941997c7cfc855a1f1f9f0be23ceb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 18:11:40 +0000

    002ee3226c refactor(core): support applying directives to the root component (#47530)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-6a88bad");
+var VERSION = new Version("15.0.0-next.3+sha-002ee32");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.3+sha-6a88bad";
+export declare const NGCC_VERSION = "15.0.0-next.3+sha-002ee32";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
