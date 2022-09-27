c64d48dff348d150526baecb9abe0f1203daf6b4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:33:49 +0000

    e7ee53c541 feat(language-service): support to fix invalid banana in box (#47393)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-120555a");
+var VERSION = new Version("15.0.0-next.3+sha-e7ee53c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
