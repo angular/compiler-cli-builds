fa7660e8df3edd3dacf22d732ba9baecdd65a249
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:36:05 +0000

    a4167c8854 docs: update what what the CLI collects in analytics (#47529)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-551c641");
+var VERSION = new Version("15.0.0-next.3+sha-a4167c8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
