4bc15375f1260de621ffaadf8cc5c147ec6b72c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:35:45 +0000

    551c641a64 docs: update broken link (#47532) PR Close #47532

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.3+sha-e7ee53c";
+export declare const NGCC_VERSION = "15.0.0-next.3+sha-551c641";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
