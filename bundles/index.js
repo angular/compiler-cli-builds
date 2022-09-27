071dbb846177ff40343c917cde26bb2e0ff98e82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:27:46 +0000

    120555a626 feat(core): support object-based DI flags in TestBed.inject() (#46761)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-85b5d12");
+var VERSION = new Version("15.0.0-next.3+sha-120555a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
