b0ae558ded69b432ebc010c8a892d4c3d2408b2d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-28 21:35:48 +0000

    2d35395b69 docs: release notes for the v14.2.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.3+sha-002ee32");
+var VERSION = new Version("15.0.0-next.3+sha-2d35395");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
