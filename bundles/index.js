8126754e8f4885a9fe2c3de5af1b561fa0a850b7
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:08:26 +0000

    d90e76054f build: update cross-repo angular dependencies (#47454)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-051f756");
+var VERSION = new Version("15.0.0-next.4+sha-d90e760");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.4+sha-051f756";
+export declare const NGCC_VERSION = "15.0.0-next.4+sha-d90e760";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
