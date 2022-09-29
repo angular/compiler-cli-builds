9527952d9d9f2283f98c1d55eff3bdced6c53292
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:08:18 +0000

    051f75648d fix(compiler): scope css keyframes in emulated view encapsulation (#42608)

--- bundles/chunk-JAXITKLW.js
+++ bundles/chunk-JAXITKLW.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.4+sha-76a8c68";
+var NGCC_VERSION = "15.0.0-next.4+sha-051f756";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-76a8c68");
+var VERSION = new Version("15.0.0-next.4+sha-051f756");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
