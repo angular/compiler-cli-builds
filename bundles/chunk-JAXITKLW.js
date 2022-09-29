b5bc60472aed931289e743c29ed058cfcd371f45
Andrew Kushnir <akushnir@google.com>
2022-09-29 18:09:57 +0000

    f0d6a0276b test(upgrade): demonstrate that upgraded component can be standalone (#47503)

--- bundles/chunk-JAXITKLW.js
+++ bundles/chunk-JAXITKLW.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.4+sha-7809e3a";
+var NGCC_VERSION = "15.0.0-next.4+sha-f0d6a02";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-7809e3a");
+var VERSION = new Version("15.0.0-next.4+sha-f0d6a02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
