24f754e93895a6c3724f2863c63a7623a51dd90b
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:06:44 +0000

    76a8c68cc1 refactor(core): add input and output filtering for host directives (#47536)

--- bundles/chunk-FHVSPC32.js
+++ bundles/chunk-FHVSPC32.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.4+sha-26ad19e");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.4+sha-76a8c68");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.0.0-next.4+sha-26ad19e") {
+    if (version === "15.0.0-next.4+sha-76a8c68") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-26ad19e");
+var VERSION = new Version("15.0.0-next.4+sha-76a8c68");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
