c8a6c51c0af4887d0dfe19cc4d3f558ddf1f2bcf
Andrew Kushnir <akushnir@google.com>
2022-09-29 23:11:36 +0000

    ec1105dd71 build: use shared circleci dev-infra for common circleci code (#47467)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-d90e760");
+var VERSION = new Version("15.0.0-next.4+sha-ec1105d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
