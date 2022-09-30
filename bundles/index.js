46206244fb65a4284797adcfe7e7ddbc4c19d168
Andrew Kushnir <akushnir@google.com>
2022-09-30 00:00:26 +0000

    89006b1025 fix(docs-infra): add punctuation to dr iq (#47525)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-d3c25f2");
+var VERSION = new Version("15.0.0-next.4+sha-89006b1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
