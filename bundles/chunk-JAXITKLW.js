76b1d9affbdbf34291a583dc3ae94d58531bb84c
Andrew Kushnir <akushnir@google.com>
2022-09-30 18:12:22 +0000

    ce59f1a14d test: delete bazel_ngtsc_plugin integration test (#47584)

--- bundles/chunk-JAXITKLW.js
+++ bundles/chunk-JAXITKLW.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.4+sha-382330c";
+var NGCC_VERSION = "15.0.0-next.4+sha-ce59f1a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.4+sha-382330c";
+export declare const NGCC_VERSION = "15.0.0-next.4+sha-ce59f1a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
