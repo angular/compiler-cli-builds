86226351651dcf753f486eec4c2986b0b523fa64
Andrew Kushnir <akushnir@google.com>
2022-09-30 18:10:43 +0000

    50f8928d56 refactor(core): add host directive definitions validation (#47589)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-752e224");
+var VERSION = new Version("15.0.0-next.4+sha-50f8928");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
