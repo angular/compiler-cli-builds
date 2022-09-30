6dfc1670b2ad6e5573ad1f180837711d63cd7b46
Andrew Kushnir <akushnir@google.com>
2022-09-30 18:25:23 +0000

    c025ef9c9e build(docs-infra): upgrade cli command docs sources to 6b533b661 (#47591)

--- bundles/chunk-NYGWZR47.js
+++ bundles/chunk-NYGWZR47.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.4+sha-771f2c3";
+var NGCC_VERSION = "14.2.4+sha-c025ef9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
