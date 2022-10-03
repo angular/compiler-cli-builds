5ef70d134c07393552edc149ac5ca244da560fb9
Andrew Kushnir <akushnir@google.com>
2022-10-03 17:25:58 +0000

    07d9a27da2 refactor(core): handle ngOnChanges in host directives (#47597)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-4ba2d3d");
+var VERSION = new Version("15.0.0-next.4+sha-07d9a27");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
