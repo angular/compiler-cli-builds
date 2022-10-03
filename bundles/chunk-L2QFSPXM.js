9bd3152de0969b2f177624f84252104a9400debb
Andrew Kushnir <akushnir@google.com>
2022-10-03 16:10:00 +0000

    0aed24c5b8 docs: add missing link to canMatch guard (#47602)

--- bundles/chunk-L2QFSPXM.js
+++ bundles/chunk-L2QFSPXM.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.4+sha-c025ef9");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.4+sha-0aed24c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.4+sha-c025ef9") {
+    if (version === "14.2.4+sha-0aed24c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.4+sha-c025ef9");
+var VERSION = new Version("14.2.4+sha-0aed24c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
