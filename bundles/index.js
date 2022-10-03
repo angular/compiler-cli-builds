c138bf8efa030f8d95b951e9d28d8ee4779344b5
Andrew Kushnir <akushnir@google.com>
2022-10-03 16:05:16 +0000

    9eb38917fe refactor(router): apply `href` value via renderer to further align RouterLink directives (#47605)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-ce59f1a");
+var VERSION = new Version("15.0.0-next.4+sha-9eb3891");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
