3bc47ba85c6707dc97c3b191de7dc47052f2ee9b
Andrew Kushnir <akushnir@google.com>
2022-10-04 19:11:18 +0000

    e3cef4a784 docs(core): deprecate `providedIn: NgModule` and `providedIn: 'any'` (#47616)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.4+sha-7ab10a5";
+export declare const NGCC_VERSION = "15.0.0-next.4+sha-e3cef4a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
