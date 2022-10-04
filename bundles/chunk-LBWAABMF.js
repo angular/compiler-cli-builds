e802b0f72d24bf889cdcb684406ebaa06abee400
Andrew Kushnir <akushnir@google.com>
2022-10-04 23:38:33 +0000

    ab4ef264ed refactor(router): drop own constructor from the `RouterLinkWithHref` class (#47619)

--- bundles/chunk-LBWAABMF.js
+++ bundles/chunk-LBWAABMF.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.4+sha-2919463";
+var NGCC_VERSION = "15.0.0-next.4+sha-ab4ef26";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
