91b6ffe356d5e4dc71314efcc08cbc32898f4294
Andrew Kushnir <akushnir@google.com>
2022-10-04 15:15:53 +0000

    c3f857975d feat(router): make RouterOutlet name an Input so it can be set dynamically (#46569)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.4+sha-07d9a27");
+var VERSION = new Version("15.0.0-next.4+sha-c3f8579");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
