acf1618f6995d90b672b72b6a2bc1b800f3afe8c
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-05 22:34:36 +0000

    f73ef21442 feat(router): merge `RouterLinkWithHref` into `RouterLink` (#47630)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-8df8c77");
+var VERSION = new Version("15.0.0-next.5+sha-f73ef21");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
