fca14b24e7af41c9becd02ceda43c8b4e04554e0
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 23:59:35 +0000

    ea16a98dfe fix(http): better handle unexpected `undefined` XSRF tokens (#47683)

--- bundles/chunk-7N6XKDID.js
+++ bundles/chunk-7N6XKDID.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.5+sha-8d1f28a";
+var NGCC_VERSION = "15.0.0-next.5+sha-ea16a98";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
