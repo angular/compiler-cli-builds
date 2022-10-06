144a5f5632525714c4c2616245f298382dc17b54
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:23:33 +0000

    5782d632f5 ci: cleanup PullApprove config file (#47611)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.5+sha-6f4f162";
+export declare const NGCC_VERSION = "14.2.5+sha-5782d63";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
