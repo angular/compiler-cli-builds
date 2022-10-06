8327b3c0d1e5da7ce05d61f61396920f8a028536
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 16:25:07 +0000

    cf88a3ce20 build: add atscott to unavailable list for pullapprove (#47658)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-f73ef21");
+var VERSION = new Version("15.0.0-next.5+sha-cf88a3c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
