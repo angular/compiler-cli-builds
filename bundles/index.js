ccf20731bfdd6efcdefcfef5909e0a53d1341557
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:22:02 +0000

    c09c1bb033 docs(http): fix copy-paste error in HttpClientJsonpModule docs (#47502)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-16c8f55");
+var VERSION = new Version("15.0.0-next.5+sha-c09c1bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
