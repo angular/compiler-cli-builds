de939560f6ca7591f9439afa6c93b6b76ad50398
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:42:17 +0000

    e3ef401c25 docs: add Marko to GDE resources (#47595)

--- bundles/chunk-NYGWZR47.js
+++ bundles/chunk-NYGWZR47.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.5+sha-197e8b4";
+var NGCC_VERSION = "14.2.5+sha-e3ef401";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
