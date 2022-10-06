7c76fcd5307559d39ab1076cc69ce24edbb79c07
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:16:33 +0000

    16c8f55663 feat(router): migrate `RouterLinkWithHref` references to `RouterLink` (#47599)

--- bundles/chunk-7N6XKDID.js
+++ bundles/chunk-7N6XKDID.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.5+sha-db28bad";
+var NGCC_VERSION = "15.0.0-next.5+sha-16c8f55";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
