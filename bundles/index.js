400db7cdf6801cad5a8b919c3d67a4be542ec34b
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 17:09:19 +0000

    6f4f16219b docs: fix linter errors for lazy-loading-ngmodules.md (#47645)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.5+sha-730af49");
+var VERSION = new Version("14.2.5+sha-6f4f162");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
