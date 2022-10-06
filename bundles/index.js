a7fa6fd929bfbdea034e126d50ebbc278e8abfa7
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 17:08:03 +0000

    fcea8fe377 docs: fix linter errors for libraries.md (#47668)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-5d002df");
+var VERSION = new Version("15.0.0-next.5+sha-fcea8fe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
