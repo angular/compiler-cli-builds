ad5349ccb8700e2cdfbc3eacf91a72a5f6254d86
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:20:14 +0000

    197e8b4963 docs: fixed typo in AsyncValidatorFn (#47465)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.5+sha-1095dc5";
+export declare const NGCC_VERSION = "14.2.5+sha-197e8b4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
