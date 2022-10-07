2a7e3e0dbef72931233ce4a0d7203aff34bb40b2
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 22:29:52 +0000

    07017a7bd3 feat(router): prevent `provideRouter()` from usage in @Component (#47669)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.5+sha-deb4cab";
+export declare const NGCC_VERSION = "15.0.0-next.5+sha-07017a7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
