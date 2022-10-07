8fce639ef76546403cdf1dae9e5219e929adb2d2
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 18:00:36 +0000

    cfd0cdbd9b docs: remove assorted lint errors (#47693)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.5+sha-9e14658";
+export declare const NGCC_VERSION = "14.2.5+sha-cfd0cdb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
