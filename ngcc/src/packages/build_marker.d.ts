e313eacecb14b52495032a044b3cf1d8afa101a0
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 21:53:33 +0000

    48b354a83e fix(compiler): update element schema (#47552)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.5+sha-bebef5f";
+export declare const NGCC_VERSION = "15.0.0-next.5+sha-48b354a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
