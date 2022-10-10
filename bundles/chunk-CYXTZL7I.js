4c3ad5f8f27573c4fac64a7033c5ef5b95a35fde
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 16:39:53 +0000

    4fde292bb5 feat(common): Add automatic srcset generation to ngOptimizedImage (#47547)

--- bundles/chunk-CYXTZL7I.js
+++ bundles/chunk-CYXTZL7I.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.5+sha-ed11a13";
+var NGCC_VERSION = "15.0.0-next.5+sha-4fde292";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-ed11a13");
+var VERSION = new Version("15.0.0-next.5+sha-4fde292");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
