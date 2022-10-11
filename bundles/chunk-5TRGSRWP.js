c5c342639c8acee92abb47d2b800170e8d030f78
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 16:18:06 +0000

    6be93cf4e4 docs: fix linter errors in prerendering.md (#47724)

--- bundles/chunk-5TRGSRWP.js
+++ bundles/chunk-5TRGSRWP.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.5+sha-f57bb38";
+var NGCC_VERSION = "14.2.5+sha-6be93cf";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
