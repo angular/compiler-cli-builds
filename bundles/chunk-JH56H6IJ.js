e32dbf9c114b0db44475bfdd74780a82c2b45841
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 16:17:21 +0000

    a41ff9413d docs(core): Improved README.md (#47700)

--- bundles/chunk-JH56H6IJ.js
+++ bundles/chunk-JH56H6IJ.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.5+sha-1b602fa");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.5+sha-a41ff94");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.0.0-next.5+sha-1b602fa") {
+    if (version === "15.0.0-next.5+sha-a41ff94") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
