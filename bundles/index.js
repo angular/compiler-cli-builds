6d71cf0eaffe8d16c5e21fe9de233f46d069ed7d
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 01:32:14 +0000

    1b602faee9 Revert "fix(compiler): update element schema" (#47726)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-ca2fa7a");
+var VERSION = new Version("15.0.0-next.5+sha-1b602fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
