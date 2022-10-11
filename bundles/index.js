94545343ef3dc32c718464060fd940f856b7c394
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 22:23:57 +0000

    bbbd4c3ac8 docs: fix linter errors for security.md (#47688)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.5+sha-23c06ee");
+var VERSION = new Version("15.0.0-next.5+sha-bbbd4c3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
