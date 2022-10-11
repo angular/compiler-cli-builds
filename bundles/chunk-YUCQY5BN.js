adfee7481be086677c4a6e0b4024037bbf86e4f1
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:41:19 +0000

    b618b5aa86 fix(zone.js): cancel tasks only when they are scheduled or running (#46435)

--- bundles/chunk-YUCQY5BN.js
+++ bundles/chunk-YUCQY5BN.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-next.5+sha-96b7fe9";
+var NGCC_VERSION = "15.0.0-next.5+sha-b618b5a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
