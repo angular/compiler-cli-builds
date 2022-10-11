7334794c855843a80f877d563d619a8ddde2c537
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:42:32 +0000

    39b72e208b fix(compiler): update element schema (#47552)

--- bundles/chunk-JH56H6IJ.js
+++ bundles/chunk-JH56H6IJ.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.5+sha-b618b5a");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.0.0-next.5+sha-39b72e2");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.0.0-next.5+sha-b618b5a") {
+    if (version === "15.0.0-next.5+sha-39b72e2") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
