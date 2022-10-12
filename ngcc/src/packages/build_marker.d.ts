14c098f79f02bd2609af326052e4e612682ad410
Joey Perrott <josephperrott@gmail.com>
2022-10-12 17:22:12 +0000

    09d5f51764 build: update labels to match standardized labels (#47747)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.5+sha-3fd176a";
+export declare const NGCC_VERSION = "14.2.5+sha-09d5f51";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
