8205cf4df0bad886d0a5624e0360d91e3a8a3ddb
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 16:54:56 +0000

    6c98ca4572 docs: update of documentation contrib. guide (#47381)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-ec97331";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-6c98ca4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
