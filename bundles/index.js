7123f7ed0b48cbc9f5eb4017b73901de46339aad
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 16:56:19 +0000

    2ca213747c docs: update of documentation contrib. guide (#47381)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.6+sha-24950dd");
+var VERSION = new Version("14.2.6+sha-2ca2137");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
