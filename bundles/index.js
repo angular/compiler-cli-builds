b46c390b8ffd65305391cf8270e2264be16aa20a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-10-13 10:02:20 +0000

    1b48e65e89 build: wire up new github action for identifying g3-affecting PRs (#47735)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.6+sha-93cc5b2");
+var VERSION = new Version("15.0.0-next.6+sha-1b48e65");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
