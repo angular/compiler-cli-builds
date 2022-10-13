34a7f52f0117981496a2a97b3820caddfbcbb372
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-10-13 10:03:19 +0000

    ec97331c8e build: wire up new github action for identifying g3-affecting PRs (#47735)

--- bundles/chunk-JH56H6IJ.js
+++ bundles/chunk-JH56H6IJ.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.0+sha-195a8d8");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.0+sha-ec97331");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.1.0-next.0+sha-195a8d8") {
+    if (version === "15.1.0-next.0+sha-ec97331") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
