74493bc0464e0b2b035d58483fa029e33423a0eb
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 19:29:22 +0000

    2e7e4024ca docs: Separated HTML elements with blank lines (#47331)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.6+sha-2ca2137");
+var VERSION = new Version("14.2.6+sha-2e7e402");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
