8bfdbdc2820d622b5e083ab85d131aaa468450b1
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 10:29:43 +0000

    cd25cb2f83 docs: fix linter errors for view-encapsulation.md (#47772)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-4bddd90";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-cd25cb2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
