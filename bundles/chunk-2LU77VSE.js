fcf86a3a9cb59f0bba2d02d2ac62963d5992f5e5
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 08:16:41 +0000

    46d33dd206 docs(docs-infra): fix errors page hover styles (#47770)

--- bundles/chunk-2LU77VSE.js
+++ bundles/chunk-2LU77VSE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.6+sha-2e7e402";
+var NGCC_VERSION = "14.2.6+sha-46d33dd";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
