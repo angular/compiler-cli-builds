8e73097182924a2ec49d87752303d67b93201499
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 08:16:52 +0000

    c9541f4c49 fix(localize): update ng add schematic to support Angular CLI version 15 (#47763)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-e0bda23");
+var VERSION = new Version("15.1.0-next.0+sha-c9541f4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
