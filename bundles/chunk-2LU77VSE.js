2f322d3f486df3188a8916efc38e544113e265c0
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:32:54 +0000

    15044737a6 docs: fix linter errors for accessibility.md (#47725)

--- bundles/chunk-2LU77VSE.js
+++ bundles/chunk-2LU77VSE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.6+sha-64d6a4f";
+var NGCC_VERSION = "14.2.6+sha-1504473";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
