64c3631bcd8c842ccc449c9eb52d7ddad2cf4c9f
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:31:44 +0000

    b136e9eea6 docs: declare NgOptimizedImage APIs as stable (#47794)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-c5ba762");
+var VERSION = new Version("15.1.0-next.0+sha-b136e9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
