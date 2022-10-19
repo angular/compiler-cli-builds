4ca50fc7be24ac08cb85a02cb8889cea01166566
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 22:11:49 +0000

    44c1770fc9 release: cut the v15.0.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-next.6+sha-79e9e8a");
+var VERSION = new Version("15.0.0-rc.0+sha-44c1770");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
