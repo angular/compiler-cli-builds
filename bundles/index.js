8ea6ed5eaaf6afaec611696c91b9d42e045b3a9c
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:44:17 +0000

    b31091b222 docs(common): update image directive docs (#47796)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-a471684");
+var VERSION = new Version("15.1.0-next.0+sha-b31091b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
