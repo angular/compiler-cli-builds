159387935449a051845b3c7387ed7c67b53b9fe7
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:41:30 +0000

    bdd4d14db9 refactor(common): drop deprecated selector from the NgOptimizedImage directive (#47798)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-ce5880f";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-bdd4d14";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
