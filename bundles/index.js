6ce4ed2883b9e2ec27d7bcb438c2d2af0f2626ec
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:26:36 +0000

    177c55686d docs: exclude lifecycle hooks of the NgOptimizedImage from docs (#47814)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-0f726bd");
+var VERSION = new Version("15.1.0-next.0+sha-177c556");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
