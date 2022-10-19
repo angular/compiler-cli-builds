aa89ef8b152d229a4f955be36389a58e06c4f388
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:32:52 +0000

    af8afee5bd fix(router): Delay router scroll event until navigated components have rendered (#47563)

--- bundles/chunk-JMDLIPDD.js
+++ bundles/chunk-JMDLIPDD.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-38ec156";
+var NGCC_VERSION = "15.1.0-next.0+sha-af8afee";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
