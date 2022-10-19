de27c08ec9de5940cafd8f26fbb99fb3823702d1
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:44:48 +0000

    f1ed796a68 docs: release notes for the v14.2.7 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-af8afee";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-f1ed796";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
