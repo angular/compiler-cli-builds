fa929872a4265c424dd5f7176091fe9dbbc932d5
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:43:48 +0000

    4d18ba1af7 release: cut the v14.2.7 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.6+sha-1504473";
+export declare const NGCC_VERSION = "14.2.7+sha-4d18ba1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
