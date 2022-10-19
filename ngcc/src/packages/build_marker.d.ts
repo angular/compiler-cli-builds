475a09c6cdb4b48601cb476b02de816f04a6ad64
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 18:31:38 +0000

    716352c1b0 docs: fix linter errors for accessibility.md (#47725)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-next.6+sha-420d6b3";
+export declare const NGCC_VERSION = "15.0.0-next.6+sha-716352c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
