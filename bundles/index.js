fb00c0497e18224c0722e0adfb0907bb1fb3cafa
Dylan Hunn <dylhunn@gmail.com>
2022-10-21 00:08:12 +0000

    3a18398d83 fix(common): Don't warn about image distortion is fill mode is enabled (#47824)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.0+sha-44c1770");
+var VERSION = new Version("15.0.0-rc.0+sha-3a18398");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
