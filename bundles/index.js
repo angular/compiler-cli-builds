54522e85ae20dac8de83488985e6d2f2e2b256e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 17:31:45 +0000

    4e46370a44 docs: fix linter errors in user-input.md (#47832)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-5f0b53c");
+var VERSION = new Version("15.1.0-next.0+sha-4e46370");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
