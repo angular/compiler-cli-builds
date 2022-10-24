244ad732ea70950fcb770fa02ef7a5f1042f30e1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:01:20 +0000

    6471eafde6 build: update recommended launch vscode config to use proper bazel dist path (#47831)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.0+sha-a014ca1";
+export declare const NGCC_VERSION = "15.0.0-rc.0+sha-6471eaf";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
