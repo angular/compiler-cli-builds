1ea98348ad61528b964d04e55e48b1a3f070eb90
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:00:51 +0000

    2f32ce071e docs: fixing copy inconsistencies in schematics description (#47453)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-b4c4a95");
+var VERSION = new Version("15.1.0-next.0+sha-2f32ce0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
