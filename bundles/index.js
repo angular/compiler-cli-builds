62972c4a2f5e1997713e92c6244e3fc163c28e0e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 12:36:28 +0000

    a99d9d67f3 fix(forms): don't mutate validators array (#47830)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.0+sha-6471eaf");
+var VERSION = new Version("15.0.0-rc.0+sha-a99d9d6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
