aa62324c649758b0d7bbb839697456b460c515a3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 08:46:57 +0000

    48853bef43 docs: update typescript configuration (#47842)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-d234fb4");
+var VERSION = new Version("15.1.0-next.0+sha-48853be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
