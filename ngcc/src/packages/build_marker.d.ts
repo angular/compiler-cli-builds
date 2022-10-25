0681f0905a09c67c7a71e939ff88525de34419ef
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 08:23:24 +0000

    c0903acbd4 Revert "fix(forms): don't mutate validators array (#47830)" (#47845)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.0+sha-588cd3e";
+export declare const NGCC_VERSION = "15.0.0-rc.0+sha-c0903ac";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
