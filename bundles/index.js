67504c7ec608b730dc5cdba21419628a9ca5e50b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:08:49 +0000

    54e32dad3e fix(platform-server): align server renderer interface with base renderer (#47868)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-48853be");
+var VERSION = new Version("15.1.0-next.0+sha-54e32da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
