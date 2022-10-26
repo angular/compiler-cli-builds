b2a24f01f4b8aec01ed40240f0323dbd66a83575
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:52:20 +0000

    f24da6baad release: cut the v15.0.0-rc.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.0+sha-ea7ff35");
+var VERSION = new Version("15.0.0-rc.1+sha-f24da6b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.0+sha-ea7ff35";
+export declare const NGCC_VERSION = "15.0.0-rc.1+sha-f24da6b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
