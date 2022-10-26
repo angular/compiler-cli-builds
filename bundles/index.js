0e955ab314a6797119afb6741b1e64da4d883ffb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 14:01:00 +0000

    d75e3da429 build: update bazel setup to v5.7.0 (#47436)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.0+sha-ebd2ddb");
+var VERSION = new Version("15.0.0-rc.0+sha-d75e3da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
