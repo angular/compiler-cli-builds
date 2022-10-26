918722e76cecb3013262a65f4be237b4bd53f425
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:44:43 +0000

    e78885ee8e docs: release notes for the v14.2.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-2acdb08");
+var VERSION = new Version("15.1.0-next.0+sha-e78885e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
