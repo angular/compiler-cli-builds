6a5f4fbf02d40c8ddb66528047e6977a2bde31f0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 12:30:27 +0000

    ebd2ddbbd1 build: update cross-repo angular dependencies (#47846)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.0+sha-df36537";
+export declare const NGCC_VERSION = "15.0.0-rc.0+sha-ebd2ddb";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
