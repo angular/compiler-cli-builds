6256f92e4a3e0c8150a5e52d79743d01ac40b68c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:12:38 +0000

    9d12a072b8 Revert "build: add atscott to unavailable list for pullapprove (#47658)" (#47877)

--- bundles/chunk-2LU77VSE.js
+++ bundles/chunk-2LU77VSE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.7+sha-a478ab1";
+var NGCC_VERSION = "14.2.7+sha-9d12a07";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.7+sha-a478ab1";
+export declare const NGCC_VERSION = "14.2.7+sha-9d12a07";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
