df75800983910633341117fc6fdfffc3cf649b5c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 16:21:15 +0000

    c7bc47c509 docs: update CLI related deprecation information (#47885)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.1+sha-96c0e42");
+var VERSION = new Version("15.0.0-rc.1+sha-c7bc47c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
