d072f691f86eb2e99d7978bd983ad682079dd241
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 16:20:34 +0000

    e9d78d080f docs: update CLI related deprecation information (#47885)

--- bundles/chunk-JMDLIPDD.js
+++ bundles/chunk-JMDLIPDD.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-2d085dc";
+var NGCC_VERSION = "15.1.0-next.0+sha-e9d78d0";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
