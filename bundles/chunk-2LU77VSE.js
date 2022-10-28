b2d7e4f37f75f552c808ba98401e3a2877f59aab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-10-28 09:59:30 +0000

    65a3338e54 docs: Linting corrections.  Restructuring or rewording sentences to be shorter and clearer. (#47897)

--- bundles/chunk-2LU77VSE.js
+++ bundles/chunk-2LU77VSE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.8+sha-8707475";
+var NGCC_VERSION = "14.2.8+sha-65a3338";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.8+sha-8707475");
+var VERSION = new Version("14.2.8+sha-65a3338");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
