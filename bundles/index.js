fa5580d62c4eafc31c7d19382b99d074d49e9679
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:02:08 +0000

    0d49fe01ea docs(core): update standalone docs for `provideRouter` (#47902)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-11288f2");
+var VERSION = new Version("15.1.0-next.0+sha-0d49fe0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
