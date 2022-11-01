67eccb70100bffae1058feef7f50fbc885fec43c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:06:36 +0000

    f7f354a1f7 docs: update roadmap as of q4 2022 (#47910)

--- bundles/chunk-2LU77VSE.js
+++ bundles/chunk-2LU77VSE.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "14.2.8+sha-65a3338";
+var NGCC_VERSION = "14.2.8+sha-f7f354a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
