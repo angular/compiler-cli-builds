e716b15386ec6b9a0379ddd3334da3fceed3470e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-02 16:31:54 +0000

    2d08965b1a fix(core): hardening rules related to the attribute order on iframe elements (#47935)

--- bundles/chunk-3KIUHCHT.js
+++ bundles/chunk-3KIUHCHT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-0bc4405";
+var NGCC_VERSION = "15.1.0-next.0+sha-2d08965";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
