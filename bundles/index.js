f241b6f30317c381f0aa2638dd88e6635e4a60d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-02 14:40:49 +0000

    d4b3c0b47c fix(core): hardening rules related to the attribute order on iframe elements (#47935)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.1+sha-e635747");
+var VERSION = new Version("15.0.0-rc.1+sha-d4b3c0b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
