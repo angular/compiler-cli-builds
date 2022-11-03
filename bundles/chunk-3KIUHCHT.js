875dc95cbb7e27c26a983b317d0500fd7ea8e43a
Andrew Kushnir <akushnir@google.com>
2022-11-03 18:37:51 +0000

    13b863a1bf Revert "fix(core): hardening rules related to the attribute order on iframe elements (#47935)" (#47959)

--- bundles/chunk-3KIUHCHT.js
+++ bundles/chunk-3KIUHCHT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-2d08965";
+var NGCC_VERSION = "15.1.0-next.0+sha-13b863a";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
