7837e30e455fafac3e556aa3be6f214059be155d
Andrew Kushnir <akushnir@google.com>
2022-11-04 00:21:32 +0000

    173dde7681 Revert "fix(core): hardening rules related to the attribute order on iframe elements (#47935)" (#47961)

--- bundles/chunk-3KIUHCHT.js
+++ bundles/chunk-3KIUHCHT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-rc.1+sha-d4b3c0b";
+var NGCC_VERSION = "15.0.0-rc.1+sha-173dde7";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.1+sha-d4b3c0b");
+var VERSION = new Version("15.0.0-rc.1+sha-173dde7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
