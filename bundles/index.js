a865c2c7e3f2617bf13e8f7dc298e15bbaa3d699
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:12:52 +0000

    febf29dd51 fix(http): rename `withLegacyInterceptors` to `withInterceptorsFromDi` (#47901)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-5b00e1f");
+var VERSION = new Version("15.1.0-next.0+sha-febf29d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
