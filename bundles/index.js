aa846e56198877cdc80cd29998e475945e6daba2
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:03:21 +0000

    32135b27a1 docs: update examples to reflect changes in Angular CLI version 15. (#47904)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-c9a6f95");
+var VERSION = new Version("15.1.0-next.0+sha-32135b2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
