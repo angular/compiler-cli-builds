299519b54d80a9a7f1f2c237b4699af10f6ed044
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:02:43 +0000

    c9a6f95c9d docs: update analytics gathering documentation (#47840)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-c82c1a5");
+var VERSION = new Version("15.1.0-next.0+sha-c9a6f95");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
