7168444b6768d2534ea07d5a0fe7ab3a2726cd35
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:08:32 +0000

    f01ea3c9dd docs: fix lint errors for change detection guides (#47952)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-ce8160e";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-f01ea3c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
