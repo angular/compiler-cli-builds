678bddfae6624a852b60fd7afd2ffba32cb9d0f6
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:04:32 +0000

    bd04173910 docs: fix missing word (#47876)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-760cd78");
+var VERSION = new Version("15.1.0-next.0+sha-bd04173");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
