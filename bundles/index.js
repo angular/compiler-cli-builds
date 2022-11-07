a8faa77c59ce42367659ab6a0ed55bec34636eb1
Andrew Kushnir <akushnir@google.com>
2022-11-07 20:51:05 +0000

    917816f672 refactor(core): remove duplicate createRenderer call (#47981)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-ea3374c");
+var VERSION = new Version("15.1.0-next.0+sha-917816f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
