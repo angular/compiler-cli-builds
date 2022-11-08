a6a06a6f6c3188a0670deaff2f2c0e8589075859
Andrew Kushnir <akushnir@google.com>
2022-11-08 20:56:28 +0000

    54d0981871 build: update dependency google-closure-compiler to v20221102 (#47992)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.2+sha-2efcc26");
+var VERSION = new Version("15.0.0-rc.2+sha-54d0981");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
