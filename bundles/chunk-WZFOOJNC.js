3dee42948a9b941e57474d8d04b78be37693944b
Andrew Kushnir <akushnir@google.com>
2022-11-08 18:54:02 +0000

    6438f69f28 docs(common): Merge two ngOptimizedImage docs into one (#47990)

--- bundles/chunk-WZFOOJNC.js
+++ bundles/chunk-WZFOOJNC.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-8e6ec72";
+var NGCC_VERSION = "15.1.0-next.0+sha-6438f69";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
