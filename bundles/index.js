b3000dbdb05fa57cba2f9d3d0dc8a996fb12f68c
Andrew Kushnir <akushnir@google.com>
2022-11-08 00:17:09 +0000

    604cdb7307 fix(forms): Improve a very commonly viewed error message by adding a guide. (#47969)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-917816f");
+var VERSION = new Version("15.1.0-next.0+sha-604cdb7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
