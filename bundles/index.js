280138631d0e592e9b63969f3dee67b56297d2c7
Andrew Kushnir <akushnir@google.com>
2022-11-08 19:52:02 +0000

    2efcc26bb6 docs(router): Update router docs to use guard functions instead of @Injectable (#47989)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.2+sha-a93a1a5");
+var VERSION = new Version("15.0.0-rc.2+sha-2efcc26");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
