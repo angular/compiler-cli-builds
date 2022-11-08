d761ab3d293a1ee806f7e794e15a6dcb4a42f1c3
Andrew Kushnir <akushnir@google.com>
2022-11-08 00:19:24 +0000

    2625dc1312 fix(forms): Improve a very commonly viewed error message by adding a guide. (#47969)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.2+sha-3e490fa");
+var VERSION = new Version("15.0.0-rc.2+sha-2625dc1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.2+sha-3e490fa";
+export declare const NGCC_VERSION = "15.0.0-rc.2+sha-2625dc1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
