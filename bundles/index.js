560cce38a880cabeeafd92afbeddc524ad5b1d7a
Andrew Kushnir <akushnir@google.com>
2022-11-09 18:12:31 +0000

    37a25486cf fix(core): add` zone.js` version `0.12.x` as a valid peer dependency (#48002)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.2+sha-28f289b");
+var VERSION = new Version("15.0.0-rc.2+sha-37a2548");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.2+sha-28f289b";
+export declare const NGCC_VERSION = "15.0.0-rc.2+sha-37a2548";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
