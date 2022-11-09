cbc995c26ef3af3b3283d1febb4f75a5f9ec0de0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-09 09:09:15 +0000

    28f289b825 fix(core): hardening attribute and property binding rules for <iframe> elements (#47964)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.2+sha-54d0981");
+var VERSION = new Version("15.0.0-rc.2+sha-28f289b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.2+sha-54d0981";
+export declare const NGCC_VERSION = "15.0.0-rc.2+sha-28f289b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
