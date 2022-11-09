8dea2afee4ca09d2a0db1f968688d4cbc7f39f3d
Andrew Kushnir <akushnir@google.com>
2022-11-09 19:15:21 +0000

    6ab9f03baf docs(router): small adjustments to router docs (#48009)

--- bundles/chunk-3KIUHCHT.js
+++ bundles/chunk-3KIUHCHT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-rc.2+sha-37a2548";
+var NGCC_VERSION = "15.0.0-rc.2+sha-6ab9f03";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
