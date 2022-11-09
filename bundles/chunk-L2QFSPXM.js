d59ee416fa0e0a2246246a8d07b184a65710efa5
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-11-09 20:03:45 +0000

    b358b61304 release: cut the v14.2.10 release

--- bundles/chunk-L2QFSPXM.js
+++ bundles/chunk-L2QFSPXM.js
@@ -827,7 +827,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "14.2.9+sha-a4312e1");
+  const LATEST_VERSION_RANGE = getRange("<=", "14.2.10+sha-b358b61");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -874,7 +874,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "14.2.9+sha-a4312e1") {
+    if (version === "14.2.10+sha-b358b61") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
