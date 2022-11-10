3421189078c20da1c7a8b954eb729bdc7fb91ab1
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-10 17:54:29 +0000

    449d29b701 fix(common): Add fetchpriority to ngOptimizedImage preloads (#48010)

--- bundles/chunk-3KIUHCHT.js
+++ bundles/chunk-3KIUHCHT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0-rc.3+sha-6a9b1e1";
+var NGCC_VERSION = "15.0.0-rc.3+sha-449d29b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
