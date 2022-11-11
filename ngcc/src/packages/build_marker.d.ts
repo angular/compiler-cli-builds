63f04933b4b4183beba3d22223c2bb0a38e1f3f0
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 19:47:03 +0000

    154ca82f7b docs(core): add an error details page for unsafe <iframe> bindings (#48027)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.3+sha-0c0550b";
+export declare const NGCC_VERSION = "15.0.0-rc.3+sha-154ca82";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
