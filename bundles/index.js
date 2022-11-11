ca188aba567d24b1663d56a185ade2bda6a82903
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 19:46:23 +0000

    ec8b52af69 docs(core): add an error details page for unsafe <iframe> bindings (#48027)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-85d9b61");
+var VERSION = new Version("15.1.0-next.0+sha-ec8b52a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
