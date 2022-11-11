88336ad815f595aba7ded99c864fd337d164ca03
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 17:09:32 +0000

    1248992631 docs(docs-infra): Fix typo in documentation for standalone components (#48026)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.10+sha-b358b61");
+var VERSION = new Version("14.2.10+sha-1248992");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.10+sha-b358b61";
+export declare const NGCC_VERSION = "14.2.10+sha-1248992";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
