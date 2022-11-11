5127fdd576411b5d029a36c9b23fefabf3146f5b
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 17:05:59 +0000

    85d9b61f9a docs(docs-infra): Fix typo in documentation for standalone components (#48026)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-1d1e33e");
+var VERSION = new Version("15.1.0-next.0+sha-85d9b61");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
