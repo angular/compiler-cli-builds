a0932a48610c664b2f831500d1895963054f389f
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-14 20:22:37 +0000

    4cfb06086b release: cut the v15.0.0-rc.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0-rc.3+sha-933d481");
+var VERSION = new Version("15.0.0-rc.4+sha-4cfb060");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.3+sha-933d481";
+export declare const NGCC_VERSION = "15.0.0-rc.4+sha-4cfb060";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
