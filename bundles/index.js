12923db25f8cde217b6f7f098c6c7f9316575783
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:45:16 +0000

    8602f16aa3 docs: remove version 12 from actively supported versions (#48041)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-d6dc6b6");
+var VERSION = new Version("15.1.0-next.0+sha-8602f16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-d6dc6b6";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-8602f16";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
