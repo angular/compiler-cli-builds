b35aedda99e3e8f2413d1878e80c48ffd742095e
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:48:20 +0000

    11809badb2 docs: remove version 12 from actively supported versions (#48041)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.10+sha-1248992");
+var VERSION = new Version("14.2.10+sha-11809ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "14.2.10+sha-1248992";
+export declare const NGCC_VERSION = "14.2.10+sha-11809ba";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
