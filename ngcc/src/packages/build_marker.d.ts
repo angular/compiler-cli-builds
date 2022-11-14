154942a025a303b3ec9620982e03ba2fe0584ff2
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:46:17 +0000

    933d481a0e docs: remove version 12 from actively supported versions (#48041)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0-rc.3+sha-a1b6610";
+export declare const NGCC_VERSION = "15.0.0-rc.3+sha-933d481";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
