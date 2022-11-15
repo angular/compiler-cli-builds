e49ae10956ef7cff717e450738cb0225349928c1
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-15 20:53:54 +0000

    fc4714113c docs: add host directives guide (#48057)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-38b2ed3";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-fc47141";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
