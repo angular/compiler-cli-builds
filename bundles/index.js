49838d37b54b9d565be4a6de7552d60f94b5ce3a
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-15 21:58:54 +0000

    aef353c143 fix(router): Ensure renavigating in component init works with enabledBlocking (#48066)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.10+sha-11809ba");
+var VERSION = new Version("14.2.10+sha-aef353c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
