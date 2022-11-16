3db82ba80d0a2daea9f03a50bef5df1945f7132d
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-16 18:11:32 +0000

    893ec32726 docs: release notes for the v15.0.0 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-174c0db";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-893ec32";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
