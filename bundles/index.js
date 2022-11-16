9dde72fd1093c8a1cc504b066c84060cafad951d
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-16 17:36:47 +0000

    0cd056abe7 release: cut the v14.2.11 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("14.2.10+sha-aef353c");
+var VERSION = new Version("14.2.11+sha-0cd056a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
