2699c08f4b6d87e2e0fa308941e98f4b016df014
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-17 01:14:53 +0000

    2551c78cc5 docs: add info about v15 changes and update process (#48095)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0+sha-bc063b2");
+var VERSION = new Version("15.0.0+sha-2551c78");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0+sha-bc063b2";
+export declare const NGCC_VERSION = "15.0.0+sha-2551c78";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
