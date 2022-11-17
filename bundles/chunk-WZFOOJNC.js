441c2e4a87b7ffc48d49fd6128397cfba452de1a
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:43:27 +0000

    6732618399 refactor(router): remove unused currentRawUrl property from transition (#48067)

--- bundles/chunk-WZFOOJNC.js
+++ bundles/chunk-WZFOOJNC.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-02b18dc";
+var NGCC_VERSION = "15.1.0-next.0+sha-6732618";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
