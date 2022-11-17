f0cf99410146e91ff40b223afc051efe98e8edc2
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:47:56 +0000

    80e3c28995 build: update dependency @nguniversal/builders to v15 (#48093)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-0a8b8a6");
+var VERSION = new Version("15.1.0-next.0+sha-80e3c28");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
