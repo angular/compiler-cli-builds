1475f0c87f39207ea2e836feb636928a6768a521
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:54:08 +0000

    f8f8928210 fix(devtools): prevent devTools to load when not text/html document (#48021)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-779a76f");
+var VERSION = new Version("15.1.0-next.0+sha-f8f8928");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
