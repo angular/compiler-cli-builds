e1cb7d27497840940f0546da23cc5a2a60fe257d
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:49:10 +0000

    79bd0f1e96 build(docs-infra): upgrade cli command docs sources to 6ccbc6b4c (#48076)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-80e3c28";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-79bd0f1";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
