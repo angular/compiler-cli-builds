7b52176c659e725f7f457be0e858900203f601c4
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 21:40:14 +0000

    0c3389ecd9 docs: extract XSS security doc URL into a constant (#48082)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.0+sha-b342e55";
+export declare const NGCC_VERSION = "15.0.0+sha-0c3389e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
