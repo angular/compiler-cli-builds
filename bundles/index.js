3823c6eca0c6bc2b50972bd9118225358f3fa6f5
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 20:33:42 +0000

    57d9296802 Revert "refactor(router): Simplify router defaults by making the defaults Injectable" (#48121)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-764fa3d");
+var VERSION = new Version("15.1.0-next.0+sha-57d9296");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-764fa3d";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-57d9296";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
