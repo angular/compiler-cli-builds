926ce8c3da39af0b7ad34654392306844a91ad3b
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 20:34:46 +0000

    bf724a257b docs: fix typo in image-directive.md (#48115)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0+sha-a8c33bf");
+var VERSION = new Version("15.0.0+sha-bf724a2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
