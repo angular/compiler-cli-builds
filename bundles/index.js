ed5339790bff9102ef587b2c283fd6e95ad04e16
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 22:02:49 +0000

    1976e37475 fix(router): restore 'history.state' on popstate even if navigationId missing (#48033)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-f376bf8");
+var VERSION = new Version("15.1.0-next.0+sha-1976e37");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
