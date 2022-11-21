bb044058180a192b2dd481771547809392e77fce
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 20:39:42 +0000

    4ea399a6d7 docs(core): add an error details page for unsafe <iframe> bindings (#48027) (#48029)

--- bundles/index.js
+++ bundles/index.js
@@ -70,7 +70,7 @@ import {
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("13.3.11+sha-3b91101");
+var VERSION = new Version("13.3.11+sha-4ea399a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
