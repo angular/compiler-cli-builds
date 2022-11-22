55b0f48c860b0ac9fff4d3f4564809b595641851
GitHub <noreply@github.com>
2022-11-22 22:32:46 +0000

    eccda3203c docs: fix errors in 12.2.17 changelog

--- bundles/chunk-45I77IIG.js
+++ bundles/chunk-45I77IIG.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-903917b";
+var NGCC_VERSION = "15.1.0-next.0+sha-eccda32";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
