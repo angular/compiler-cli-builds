8dddec95eddb15b5060655bce5c27d3b1ab30651
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:56:20 +0000

    83f0b7b7b2 refactor(router): Update RouterOutlet to not read internal route property (#48112)

--- bundles/chunk-S5EWJGD2.js
+++ bundles/chunk-S5EWJGD2.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-5127a4d";
+var NGCC_VERSION = "15.1.0-next.0+sha-83f0b7b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
