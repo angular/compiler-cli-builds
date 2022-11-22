28e096f9e651f372162acec2662fed6cc2be1e46
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 18:03:15 +0000

    c5d176ceb5 docs: add state of javascript banner (#48160)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-b0bc578");
+var VERSION = new Version("15.1.0-next.0+sha-c5d176c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
