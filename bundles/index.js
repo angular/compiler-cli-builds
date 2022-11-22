d3b00718c9b92a3dde210c7deeaf773d4523fba8
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:54:52 +0000

    7fbb53f52d docs(forms): fix typos in removeValidators and hasValidator usage notes (#48144)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-bc148c3");
+var VERSION = new Version("15.1.0-next.0+sha-7fbb53f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
