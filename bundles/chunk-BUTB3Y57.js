6a84683c02971badbf6d9126113bd1d9edd40b1e
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:57:55 +0000

    8ab50a13d7 docs: fix broken link in v15 deprecations table (#48157)

--- bundles/chunk-BUTB3Y57.js
+++ bundles/chunk-BUTB3Y57.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0+sha-cc8b76e";
+var NGCC_VERSION = "15.0.0+sha-8ab50a1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0+sha-cc8b76e");
+var VERSION = new Version("15.0.0+sha-8ab50a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
