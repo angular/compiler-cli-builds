76db7068d60ff8445b29ba47bb744961949fad50
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:58:22 +0000

    2e2135bf18 docs: add missing documentation setup instructions (#48174)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XYNRD7NE.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-4f03a9c");
+var VERSION = new Version("15.1.0-next.0+sha-2e2135b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
