86167d6917f33ecdd4b2d3202e093bb799c16710
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 00:41:15 +0000

    3596c44e0c docs: add brooke avery to GDE resources (#47923)

--- bundles/chunk-WOISJMS4.js
+++ bundles/chunk-WOISJMS4.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.0+sha-8600b30";
+var NGCC_VERSION = "15.0.0+sha-3596c44";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0+sha-8600b30");
+var VERSION = new Version("15.0.0+sha-3596c44");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
