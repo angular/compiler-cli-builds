6abfb42eea14b872ec30452a86189ee925173980
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 03:37:01 +0000

    332461bd0c feat(router): Add ability to override `onSameUrlNavigation` default per-navigation (#48050)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.0+sha-6a8ab30";
+export declare const NGCC_VERSION = "15.1.0-next.0+sha-332461b";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
