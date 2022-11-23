a483aa3291f959c77aac433942c0dc92620961c0
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 03:13:57 +0000

    cadc011f63 release: cut the v15.1.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-6acae14");
+var VERSION = new Version("15.1.0-next.0+sha-cadc011");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
