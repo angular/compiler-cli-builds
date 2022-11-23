40f9814d74368167fe4068b17d873d230faf26b6
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 03:23:31 +0000

    40c138c13d release: cut the v15.0.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.0+sha-3596c44");
+var VERSION = new Version("15.0.1+sha-40c138c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
