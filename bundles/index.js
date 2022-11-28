0c66c1ccddfce6d0fa09f99333815167ac13c437
Andrew Scott <atscott@google.com>
2022-11-28 17:30:18 +0000

    ad483007b4 refactor(platform-server): remove `Injector` dependency for `initDominoAdapter` factory (#48231)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-7368ee1");
+var VERSION = new Version("15.1.0-next.0+sha-ad48300");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
