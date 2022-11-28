a097d4633e1f89093eb6357321e4e6750182845e
Andrew Scott <atscott01@gmail.com>
2022-11-28 23:50:34 +0000

    2b55f4bf0d docs: update docs to use new build and test commands (#48266)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.0+sha-3f95427");
+var VERSION = new Version("15.1.0-next.0+sha-2b55f4b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
