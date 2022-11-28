8df5c099a668115ea67464cf40309358b8b2e563
Andrew Scott <atscott@google.com>
2022-11-28 17:32:08 +0000

    458d62b7b8 build: update angular version of in-memory-web-api (#48218)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.1+sha-cfd8702");
+var VERSION = new Version("15.0.1+sha-458d62b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
