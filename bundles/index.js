0a3153785d4692bb91c2127b0c4270affe6974c8
Andrew Scott <atscott01@gmail.com>
2022-11-28 21:26:40 +0000

    c0601db72c test(core): remove some ViewEngine related test code (#48238)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.1+sha-6eb2f76");
+var VERSION = new Version("15.0.1+sha-c0601db");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
