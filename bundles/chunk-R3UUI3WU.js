6f93750535155e0b068e8d24c5b9781753e8d11b
Andrew Scott <atscott01@gmail.com>
2022-11-29 17:57:27 +0000

    bec0007c82 docs: update date pipe default timezone injection token reference (#48274)

--- bundles/chunk-R3UUI3WU.js
+++ bundles/chunk-R3UUI3WU.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.0.1+sha-a192dea");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.0.1+sha-bec0007");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.0.1+sha-a192dea") {
+    if (version === "15.0.1+sha-bec0007") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
