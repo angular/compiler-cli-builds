9377b9160e69f4476fa622f0c03524ea28367f27
Andrew Scott <atscott01@gmail.com>
2022-11-29 20:46:08 +0000

    2067643896 build: update eslint dependencies (#48216)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.1+sha-bec0007");
+var VERSION = new Version("15.0.1+sha-2067643");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
