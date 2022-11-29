128212c55bc5a02b3acf632f18b1ac23e8f8254d
Joey Perrott <josephperrott@gmail.com>
2022-11-29 20:21:43 +0000

    535838d67b build(bazel): fix aio deploy script (#48265)

--- bundles/chunk-FCAD5QMI.js
+++ bundles/chunk-FCAD5QMI.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-ae712ef";
+var NGCC_VERSION = "15.1.0-next.0+sha-535838d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
