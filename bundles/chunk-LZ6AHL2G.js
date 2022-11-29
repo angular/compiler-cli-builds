3a66299dd4e3707b9494c85b0ae36a5316f60acb
Andrew Scott <atscott01@gmail.com>
2022-11-29 23:12:22 +0000

    82756e8a5f refactor(router): Move navigationId handling to the transition manager (#48257)

--- bundles/chunk-LZ6AHL2G.js
+++ bundles/chunk-LZ6AHL2G.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.1+sha-2067643";
+var NGCC_VERSION = "15.0.1+sha-82756e8";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
