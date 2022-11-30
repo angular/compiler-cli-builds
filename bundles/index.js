7eeb47a3dd98a2bc0851ce50c6c44d960db321c9
Andrew Scott <atscott01@gmail.com>
2022-11-30 16:46:37 +0000

    2a445923ee refactor(router): Remove DeprecatedLoadChildren (#48292)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.1+sha-82756e8");
+var VERSION = new Version("15.0.1+sha-2a44592");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
