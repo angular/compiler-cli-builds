caf7f1561afd88592d0f38d1f13b53fd59cce004
Andrew Scott <atscott01@gmail.com>
2022-11-30 17:44:46 +0000

    d933b17170 refactor(core): remove debug data structures (#48281)

--- bundles/chunk-LZ6AHL2G.js
+++ bundles/chunk-LZ6AHL2G.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.1+sha-2a44592";
+var NGCC_VERSION = "15.0.1+sha-d933b17";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
