71d050da2f2b9b96c74a7859f6d318bb7a95b783
Andrew Scott <atscott01@gmail.com>
2022-11-30 16:46:06 +0000

    d5aaf5523d refactor(router): Remove DeprecatedLoadChildren (#48292)

--- bundles/chunk-FCAD5QMI.js
+++ bundles/chunk-FCAD5QMI.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.0+sha-0ff5d97";
+var NGCC_VERSION = "15.1.0-next.0+sha-d5aaf55";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
