66b0561c61ee28e7b8b00141178d2386dc71b380
Andrew Kushnir <akushnir@google.com>
2022-12-01 17:56:12 +0000

    b620d9b316 docs: removed empty method (#48236)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-73ec0c6");
+var VERSION = new Version("15.0.2+sha-b620d9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
