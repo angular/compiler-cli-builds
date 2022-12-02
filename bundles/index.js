52a88575ce148349293a6871e18be9d116c63880
Andrew Kushnir <akushnir@google.com>
2022-12-02 23:14:48 +0000

    2352c2f143 build: attempt to avoid nodejs version flakiness in `test_win` job (#48326)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-7b7d5e1");
+var VERSION = new Version("15.1.0-next.1+sha-2352c2f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
