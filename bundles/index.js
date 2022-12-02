99da8790d4763eca8ee2d7a7bc766c812b4a83d0
Andrew Kushnir <akushnir@google.com>
2022-12-02 18:07:49 +0000

    ccff7dcb28 build: fix unnecessary large runfiles in AIO when composing boilerplate (#48329)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-6706fab");
+var VERSION = new Version("15.1.0-next.1+sha-ccff7dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
