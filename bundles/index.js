f6416447b796c035a2e71316163a3768e40dd11b
Andrew Kushnir <akushnir@google.com>
2022-12-02 17:17:14 +0000

    b53e1ed4ef build: enable progress messages for Bazel in CircleCI (#48316)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-7f36221");
+var VERSION = new Version("15.1.0-next.1+sha-b53e1ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
