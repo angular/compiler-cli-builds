6bb27cafa2e2d3595a077e2226df8828ad14eca2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 11:03:51 +0000

    d1a501e65b build: update actions/checkout digest (#48336)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.1+sha-c3136d6";
+export declare const NGCC_VERSION = "15.1.0-next.1+sha-d1a501e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
