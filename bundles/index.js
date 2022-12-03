c49f2411f0cdedd431042367c18c039274989dcf
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 16:38:26 +0000

    09fe75d0e9 build: update actions/checkout action to v3 (#48337)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-d1a501e");
+var VERSION = new Version("15.1.0-next.1+sha-09fe75d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
