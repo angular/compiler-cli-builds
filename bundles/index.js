77c2ebdd94c79b7d8da3a95c500f02de5d8d4a3e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 11:30:59 +0000

    95886d8aa9 build: set firebase deploy target when deploying AIO preview (#48338)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-6f79c2b");
+var VERSION = new Version("15.0.2+sha-95886d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
