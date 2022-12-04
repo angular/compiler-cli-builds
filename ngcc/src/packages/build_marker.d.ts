b584b9f9e3f5e420243aff79242541eade4051a5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 13:37:20 +0000

    43e8f4798f build: use npx for setting firebase deploy targets in AIO preview (#48346)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.1+sha-011ef66";
+export declare const NGCC_VERSION = "15.1.0-next.1+sha-43e8f47";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
