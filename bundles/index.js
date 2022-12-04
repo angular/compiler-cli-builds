d578f84eb878241fbf00d04d5943e0a2dd58c8be
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:33:44 +0000

    0d6b33c40f build: check out project before setting AIO preview targets (#48347)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-43e8f47");
+var VERSION = new Version("15.1.0-next.1+sha-0d6b33c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
