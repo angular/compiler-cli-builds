0289c87efd4f3960e271dfafff27fd27892d1e9c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 13:38:00 +0000

    09388eb9ab build: use npx for setting firebase deploy targets in AIO preview (#48346)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-95886d8");
+var VERSION = new Version("15.0.2+sha-09388eb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
