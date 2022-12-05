1906f25b17a9564197f0adc585f60c529e9f2a33
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:23:30 +0000

    0beeee4304 build: add more debug information for occasional windows flakiness (#48351)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-d2dcf95");
+var VERSION = new Version("15.1.0-next.1+sha-0beeee4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
