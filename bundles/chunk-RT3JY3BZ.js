9afab6ac73326ef5b3a347450f9ab71c01b092e8
Andrew Kushnir <akushnir@google.com>
2022-12-05 22:56:53 +0000

    cf51690341 refactor(core): remove several private utils and APIs (#48357)

--- bundles/chunk-RT3JY3BZ.js
+++ bundles/chunk-RT3JY3BZ.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.1+sha-8e52ca2";
+var NGCC_VERSION = "15.1.0-next.1+sha-cf51690";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
