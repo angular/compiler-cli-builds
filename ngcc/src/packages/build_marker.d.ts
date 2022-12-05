bf8da227f09a33d50e6abfe1fbe33be55c405575
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:23:34 +0000

    e44b2b3fe0 docs: removed empty methods (#48343)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.1+sha-0beeee4";
+export declare const NGCC_VERSION = "15.1.0-next.1+sha-e44b2b3";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
