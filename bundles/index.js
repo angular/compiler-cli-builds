047b0243e616114351c2e6ba3724ab5402522aa9
Andrew Kushnir <akushnir@google.com>
2022-12-05 23:19:04 +0000

    45cc5f49e4 build: switch new AIO preview action to use `aio: preview` label (#48345)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-50b1c2b");
+var VERSION = new Version("15.0.2+sha-45cc5f4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
