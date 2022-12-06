7e7b930ae18fd26d0c2a69cc7ae7ded84454eaa4
Andrew Kushnir <akushnir@google.com>
2022-12-06 18:37:51 +0000

    8656ac0df3 build: update dependency karma-jasmine-html-reporter to v2 (#48369)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-4c02395");
+var VERSION = new Version("15.1.0-next.1+sha-8656ac0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
