cd7eee129dd9a1f2f7b03d96d2415dfc8933a47d
Andrew Kushnir <akushnir@google.com>
2022-12-06 21:10:45 +0000

    c0d04175ff refactor(core): simplify array flatten logic (#48358)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-5e2cd07");
+var VERSION = new Version("15.0.2+sha-c0d0417");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.2+sha-5e2cd07";
+export declare const NGCC_VERSION = "15.0.2+sha-c0d0417";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
