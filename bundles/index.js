eabea3bcd862806c0ca63d7638b4ef64deb320b4
Andrew Kushnir <akushnir@google.com>
2022-12-06 17:41:07 +0000

    40d6a06615 refactor(core): remove old AJD workaround for G3 (#48363)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.2+sha-ac51e5c");
+var VERSION = new Version("15.0.2+sha-40d6a06");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
