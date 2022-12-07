1395e8f38566def31872844fe8d4888853e98722
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-12-07 22:15:40 +0000

    9e9b90cc39 docs: release notes for the v15.0.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.1+sha-f8ecc19");
+var VERSION = new Version("15.1.0-next.1+sha-9e9b90c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
