81f9b3d089e0c179783ff34a9c0d455efac737e8
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 23:27:13 +0000

    5af7ec244d refactor(router): use `RegExp` to check if a URL is absolute (#48406)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.3+sha-77ba4aa";
+export declare const NGCC_VERSION = "15.0.3+sha-5af7ec2";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
