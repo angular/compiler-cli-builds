262eb5d35467c94dc2de74feb84c6f3eafafbcbd
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 18:14:43 +0000

    ea00adf761 docs: updated local variables and removed empty methods (#48344)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-d99638f");
+var VERSION = new Version("15.1.0-next.2+sha-ea00adf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
