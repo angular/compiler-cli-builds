fa91cd06ac557a1eb8e2846c47f997187e063e2d
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 22:45:59 +0000

    6d9df7bbf9 docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47908)

--- bundles/chunk-EFL37X7Q.js
+++ bundles/chunk-EFL37X7Q.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.2+sha-4bf271a");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.2+sha-6d9df7b");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.1.0-next.2+sha-4bf271a") {
+    if (version === "15.1.0-next.2+sha-6d9df7b") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
