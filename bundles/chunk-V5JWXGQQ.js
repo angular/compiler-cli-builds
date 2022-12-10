f78d51a98b819cff93bea01fa91e04cd6365606a
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-10 00:22:50 +0000

    b5bb4c17d0 docs: update of landing page and backup pages (#47047)

--- bundles/chunk-V5JWXGQQ.js
+++ bundles/chunk-V5JWXGQQ.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.2+sha-4d2bac5";
+var NGCC_VERSION = "15.1.0-next.2+sha-b5bb4c1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-4d2bac5");
+var VERSION = new Version("15.1.0-next.2+sha-b5bb4c1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
