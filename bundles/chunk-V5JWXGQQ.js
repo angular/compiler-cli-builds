4ba9ed50da5a91441bfa7b3b6eefaf2cea5861f7
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 23:17:21 +0000

    5bc502fa2c docs: modify directive composition API guide (#48439)

--- bundles/chunk-V5JWXGQQ.js
+++ bundles/chunk-V5JWXGQQ.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.2+sha-6dd7575";
+var NGCC_VERSION = "15.1.0-next.2+sha-5bc502f";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
