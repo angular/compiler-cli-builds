ec0533d524e964efe118fdb9808f3d379feb3301
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 18:04:44 +0000

    f30d18a942 fix(common): Fix TestBed.overrideProvider type to include multi (#48424)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.3+sha-6279fec");
+var VERSION = new Version("15.0.3+sha-f30d18a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
