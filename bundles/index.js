ea14601b871f028fdee09835c1fb0939d5f413dc
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 16:51:15 +0000

    f58ad86e51 feat(router): Add feature provider for enabling hash navigation (#48301)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-abd11a6");
+var VERSION = new Version("15.1.0-next.2+sha-f58ad86");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
