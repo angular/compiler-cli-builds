46f231f2809b62db110784718159a4db786ef67b
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 22:27:25 +0000

    ff84c73603 docs(core): Document invalid multi token (#48267)

--- bundles/chunk-EFL37X7Q.js
+++ bundles/chunk-EFL37X7Q.js
@@ -849,7 +849,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.2+sha-279e7d4");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.1.0-next.2+sha-ff84c73");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -896,7 +896,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.1.0-next.2+sha-279e7d4") {
+    if (version === "15.1.0-next.2+sha-ff84c73") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-279e7d4");
+var VERSION = new Version("15.1.0-next.2+sha-ff84c73");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
