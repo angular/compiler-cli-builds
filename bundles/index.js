15b65b3c1f8e3eb081f8990270d71700b4eec1f9
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 23:30:37 +0000

    aabf8be033 docs: edit suggestion to meet style guide (#47979) PR Close #47979

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.3+sha-d181b93");
+var VERSION = new Version("15.0.3+sha-aabf8be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
