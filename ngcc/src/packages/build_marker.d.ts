8d21b67639de91b1efa2a2269ac178e249cd7009
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 21:57:25 +0000

    279e7d4465 refactor: cleanup polyfills by importing zone.js directly in angular.json (#48380)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.2+sha-2e65a2b";
+export declare const NGCC_VERSION = "15.1.0-next.2+sha-279e7d4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
