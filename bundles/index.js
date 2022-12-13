53854bdabf77b22d9881bbe6657a75a234b29d88
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 00:45:57 +0000

    70b2508d22 docs: add docgeni to tooling of resources list (#48061)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.3+sha-6c1064c");
+var VERSION = new Version("15.0.3+sha-70b2508");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
