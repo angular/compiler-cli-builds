6bba02dd4307bfe7c1fe9fa21b642d474ab8c219
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-14 01:16:07 +0000

    9291bd950f refactor(router): Add opt-in provider for upcoming router change (#47988)

--- bundles/chunk-4DUYFEVT.js
+++ bundles/chunk-4DUYFEVT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.3+sha-aabf8be";
+var NGCC_VERSION = "15.0.3+sha-9291bd9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -9,7 +9,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.0.3+sha-aabf8be";
+export declare const NGCC_VERSION = "15.0.3+sha-9291bd9";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
