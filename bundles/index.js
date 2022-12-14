93b2b5d457e65cf660f5201a789ac364338e42cc
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-12-14 20:21:46 +0000

    eb52d9eabd release: cut the v15.1.0-next.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-6ade57c");
+var VERSION = new Version("15.1.0-next.3+sha-eb52d9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
