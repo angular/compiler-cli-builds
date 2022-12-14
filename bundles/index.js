f6e1c20044b692c1cbd49a31e80c35f51ef1be27
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-14 01:15:04 +0000

    cf7e4e47fc docs: Add doc page for 'textAttributeNotBinding' extended diagnostic (#48307)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.2+sha-f6ac94c");
+var VERSION = new Version("15.1.0-next.2+sha-cf7e4e4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
