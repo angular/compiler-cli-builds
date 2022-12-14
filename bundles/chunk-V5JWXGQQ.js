e4b33762ffec35cc221f28b845707f4e980192bd
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-14 01:17:35 +0000

    81203c6ec1 refactor(router): Add opt-in provider for upcoming router change (#47988)

--- bundles/chunk-V5JWXGQQ.js
+++ bundles/chunk-V5JWXGQQ.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.2+sha-cf7e4e4";
+var NGCC_VERSION = "15.1.0-next.2+sha-81203c6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
