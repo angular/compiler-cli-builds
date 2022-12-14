8b66eeb9781dbdc65921fe230ec62e66840ebd15
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-12-14 19:36:41 +0000

    470738c8f7 release: cut the v15.0.4 release

--- bundles/chunk-4DUYFEVT.js
+++ bundles/chunk-4DUYFEVT.js
@@ -896,7 +896,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.3+sha-a2cb2b4";
+var NGCC_VERSION = "15.0.4+sha-470738c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
