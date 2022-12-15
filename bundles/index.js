851b11f7fc8d8715ac3d6806db945d4fb6a27cc3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-15 15:40:36 +0000

    e59406f5e7 build: enable new merge ready status action (#48494)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-NDREJTCS.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.3+sha-63debcc");
+var VERSION = new Version("15.1.0-next.3+sha-e59406f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
