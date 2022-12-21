82e473b40cba179406bca67f340d5c1401e7e940
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-21 13:34:39 +0000

    d768fb7fe5 docs: updated docs removed unnecessary imports and empty methods (#48513)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.4+sha-a3958a2");
+var VERSION = new Version("15.0.4+sha-d768fb7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
