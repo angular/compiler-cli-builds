778f915a4cd9486b09e4f138fb6620d2c0b64d65
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-02 12:36:02 +0000

    cc284afbbc fix(migrations): combine newly-added imports in import manager (#48620)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.3+sha-0ae80a7");
+var VERSION = new Version("15.1.0-next.3+sha-cc284af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
