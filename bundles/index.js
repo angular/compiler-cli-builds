271c899ebddf72e85bfa3ef591839127bc8b2c81
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 16:48:34 +0000

    caedef0f5b fix(compiler-cli): update `@babel/core` dependency and lock version (#48634)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.3+sha-e38bc6b");
+var VERSION = new Version("15.1.0-next.3+sha-caedef0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0-next.3+sha-e38bc6b";
+export declare const NGCC_VERSION = "15.1.0-next.3+sha-caedef0";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
