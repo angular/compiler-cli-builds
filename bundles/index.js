fed0e134dc31e69c9d9b9a0508724dc083ea4132
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 13:33:47 +0000

    87d2385994 build: switch from `ng-comp-dev` to canonical dev-infra preview Firebase project (#48630)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.4+sha-d7ff276");
+var VERSION = new Version("15.0.4+sha-87d2385");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
