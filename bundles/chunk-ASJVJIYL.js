fbad7125a504592679292b2101b224436e377ac7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 19:50:36 +0000

    aa51e9b217 build: update all non-major dependencies (#48462)

--- bundles/chunk-ASJVJIYL.js
+++ bundles/chunk-ASJVJIYL.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.3+sha-c5a8485";
+var NGCC_VERSION = "15.1.0-next.3+sha-aa51e9b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.3+sha-c5a8485");
+var VERSION = new Version("15.1.0-next.3+sha-aa51e9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
