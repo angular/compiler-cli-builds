2020ba11c3d682cc404c79c5d653647c01b18958
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 21:44:51 +0000

    009b0d0b3e docs: release notes for the v15.1.0-rc.0 release

--- bundles/chunk-ASJVJIYL.js
+++ bundles/chunk-ASJVJIYL.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-next.3+sha-5d1fbbb";
+var NGCC_VERSION = "15.2.0-next.0+sha-009b0d0";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-next.3+sha-5d1fbbb");
+var VERSION = new Version("15.2.0-next.0+sha-009b0d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
