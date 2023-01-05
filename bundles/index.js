ad22fa48163d9cbc2c4eec1a214d24778bdc598b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:17:15 +0000

    61fc47475d refactor(forms): cleanup type any in forms tests (#48624)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-rc.0+sha-28ea114");
+var VERSION = new Version("15.1.0-rc.0+sha-61fc474");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
