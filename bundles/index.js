eca70346c7850df8c787d82b336621a6bf831149
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-06 00:45:18 +0000

    8aa8b4b77c fix(forms): Form provider FormsModule.withConfig return a FormsModule (#48526)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-rc.0+sha-61fc474");
+var VERSION = new Version("15.1.0-rc.0+sha-8aa8b4b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
