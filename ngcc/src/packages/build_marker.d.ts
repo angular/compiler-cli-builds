8e3582d39bd7c5a4ef31b555e844af53a7565365
GitHub <noreply@github.com>
2023-01-06 09:02:48 +0000

    b6cc5f7eac build: update `SECURITY.md` to match the content of `angular/.github`

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.0+sha-bdf288d";
+export declare const NGCC_VERSION = "15.2.0-next.0+sha-b6cc5f7";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
