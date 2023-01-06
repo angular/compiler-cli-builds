44053c7b408a5a78ce0903bf26d337f5e3e154e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-06 00:44:31 +0000

    433dd5c66f fix(forms): Form provider FormsModule.withConfig return a FormsModule (#48526)

--- bundles/chunk-HLWHHA6H.js
+++ bundles/chunk-HLWHHA6H.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.0.4+sha-10de92c";
+var NGCC_VERSION = "15.0.4+sha-433dd5c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.0.4+sha-10de92c");
+var VERSION = new Version("15.0.4+sha-433dd5c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
