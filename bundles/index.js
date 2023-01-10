32f5f7ea7c9a7b150889b54bf4254255754c8866
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:14:25 +0000

    123c95209f docs(compiler-cli): fix commands to run compliance unit test (#48559)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-a3a4ca0");
+var VERSION = new Version("15.2.0-next.0+sha-123c952");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
