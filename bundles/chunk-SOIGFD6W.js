1d5cafdd2ecbfa27a063e7ee2dbb6f33de20fb10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:03:22 +0000

    99723fc64e release: cut the v15.1.0 release

--- bundles/chunk-SOIGFD6W.js
+++ bundles/chunk-SOIGFD6W.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.0-rc.0+sha-6a8ea29";
+var NGCC_VERSION = "15.1.0+sha-99723fc";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0-rc.0+sha-6a8ea29");
+var VERSION = new Version("15.1.0+sha-99723fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
