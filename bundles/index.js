773ffb4a6b4ed450871cbf664caa16fe7c19d56a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:04:49 +0000

    45029f111a docs: release notes for the v15.1.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-489243b");
+var VERSION = new Version("15.2.0-next.0+sha-45029f1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
