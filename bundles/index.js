65c875dad77a9b70f1c30b1a13ca718aa2b0d7e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 19:30:13 +0000

    df77bbc70b ci: use readonly github token for querying CLI snapshot repo (#48681)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0+sha-99723fc");
+var VERSION = new Version("15.1.0+sha-df77bbc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
