8c04a9127707e0863f8a1649b31e42c274b6f80b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:25:22 +0000

    579cbb20d9 refactor(platform-browser): prepare the code to use `MockPlatformLocation` by default (#48651)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-123c952");
+var VERSION = new Version("15.2.0-next.0+sha-579cbb2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
