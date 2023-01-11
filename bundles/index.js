cb2f105da7b4c52a1f106f8eb95cc2efe1b57cdc
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:17:44 +0000

    926c35f4ac docs: Deprecate class and InjectionToken and resolvers (#47924)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-7f68a70");
+var VERSION = new Version("15.2.0-next.0+sha-926c35f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
