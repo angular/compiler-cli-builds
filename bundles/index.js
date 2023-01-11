f46f3231653c416177725d6685fde9d24156ddd5
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:11:03 +0000

    fda96a6fc9 build: update dependency puppeteer-core to v19 (#48183)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-b0318f8");
+var VERSION = new Version("15.2.0-next.0+sha-fda96a6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
