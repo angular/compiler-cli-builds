d73f62fbfef970707d6e091da227255922d6bd59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-11 15:57:43 +0000

    7cfe97ca31 ci: set `fetch-depth: 0` in `update-events` and `update-cli-help` actions (#48698)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-2b83986");
+var VERSION = new Version("15.2.0-next.0+sha-7cfe97c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
