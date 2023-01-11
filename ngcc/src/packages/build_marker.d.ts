dd95026f07624baaaaad7469d187f2c9a7aa9edd
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:12:07 +0000

    61023b563d refactor(compiler): refactor the shadow css specs (#48443)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.0+sha-fda96a6";
+export declare const NGCC_VERSION = "15.2.0-next.0+sha-61023b5";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
