feac2b044910eddfee631f021db43af83bf92131
Andrew Kushnir <akushnir@google.com>
2023-01-12 20:02:50 +0000

    0b57e24eb6 docs: add first-app-lesson-13 example (#48512)

--- bundles/chunk-NEQOZOZQ.js
+++ bundles/chunk-NEQOZOZQ.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-next.0+sha-64967da");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-next.0+sha-0b57e24");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.2.0-next.0+sha-64967da") {
+    if (version === "15.2.0-next.0+sha-0b57e24") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
