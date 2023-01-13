dd4532ef97d4f2cc439df3e2379807bfd48cb01f
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:46:31 +0000

    d47bd480c9 build: update scorecard action dependencies (#48465)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-f03f20c");
+var VERSION = new Version("15.2.0-next.0+sha-d47bd48");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
