fbae0140393393593970e926b6256b00cba45bab
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:48:35 +0000

    914e7b052e build: update actions/cache digest to 58c146c (#48564)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-5b1c7c1");
+var VERSION = new Version("15.2.0-next.0+sha-914e7b0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
