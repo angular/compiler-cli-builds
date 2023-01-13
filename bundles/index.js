0ad47db80a04b8521953970adf33fe5a37d0a73b
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:45:23 +0000

    34dda890ed build: update io_bazel_rules_sass digest to 57ade09 (#48407)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-13f10de");
+var VERSION = new Version("15.2.0-next.0+sha-34dda89");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
