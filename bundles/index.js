e67c4026ce2300951276f6f12d5b107c4e5ddf51
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:38:41 +0000

    fb7b8a95f2 docs: Fix example in breaking changes to apply to correct sentence (#48758)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.0+sha-7840352");
+var VERSION = new Version("15.2.0-next.0+sha-fb7b8a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
