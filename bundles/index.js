21a506dfc77a580fc1c05ae39977630a328bf515
Andrew Kushnir <akushnir@google.com>
2023-01-17 19:08:05 +0000

    0f8a4f97d6 test: update size golden to reflect Angular cross dependency updates (#48747)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.0+sha-6686eac");
+var VERSION = new Version("15.1.0+sha-0f8a4f9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
