c705d173b4cca099418aace699d32fb53fe58b51
Andrew Kushnir <akushnir@google.com>
2023-01-18 21:46:31 +0000

    9e1e5ccdfe ci: increase payload size for an integration test app (#48772)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-fe6a2bd");
+var VERSION = new Version("15.2.0-next.1+sha-9e1e5cc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
