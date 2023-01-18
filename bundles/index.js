04504853f04e1f993ebc7da400a4962ebb931931
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:03:22 +0000

    42ca9c0b02 docs: update documentation to use `ng generate config` and `ng generate environments` (#48757)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-8377acd");
+var VERSION = new Version("15.2.0-next.1+sha-42ca9c0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
