7eb2f0eac7d2dc2541c84f4cb0831d345b682ee0
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:04:56 +0000

    fe6a2bd9e2 build: update dependency rimraf to v4 (#48726)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-3a688cb");
+var VERSION = new Version("15.2.0-next.1+sha-fe6a2bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
