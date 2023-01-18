41d804f65d97db95f439384f93e757e14af5d8f2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:28:42 +0000

    e3a8a5cc42 release: cut the v15.1.1 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.0+sha-0f8a4f9";
+export declare const NGCC_VERSION = "15.1.1+sha-e3a8a5c";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
