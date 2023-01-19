4c595eebb6cf08d92981ec3e0c5242e42ea3b2bc
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 17:53:55 +0000

    60a7214ab2 refactor(bazel): Extract the helper patchNgHostWithFileNameToModuleName to its own file for 1P use (#48739)

--- bundles/chunk-SOIGFD6W.js
+++ bundles/chunk-SOIGFD6W.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.1+sha-97f6535";
+var NGCC_VERSION = "15.1.1+sha-60a7214";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
