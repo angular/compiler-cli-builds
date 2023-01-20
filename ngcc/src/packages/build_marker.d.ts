b1cccd0652678a6f73ffdfd0d6a6737b5ea385b4
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 18:59:43 +0000

    92ab80d44a refactor(bazel): remove unnecessary banner stamping code (#48798)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.1+sha-6632a06";
+export declare const NGCC_VERSION = "15.1.1+sha-92ab80d";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
