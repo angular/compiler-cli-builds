98ba5e59db4b32a3162b9e9eb2c26f4a39a78fe3
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 18:55:31 +0000

    b6e3840ed4 refactor(bazel): remove unnecessary banner stamping code (#48798)

--- bundles/chunk-NEQOZOZQ.js
+++ bundles/chunk-NEQOZOZQ.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-next.1+sha-840530f");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-next.1+sha-b6e3840");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.2.0-next.1+sha-840530f") {
+    if (version === "15.2.0-next.1+sha-b6e3840") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-840530f");
+var VERSION = new Version("15.2.0-next.1+sha-b6e3840");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
