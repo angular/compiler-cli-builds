cc8018fcf803ad13871600f8ee0b184df631dc51
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 01:34:46 +0000

    2d0ca73d73 docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47907)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-bca76ed");
+var VERSION = new Version("15.2.0-next.1+sha-2d0ca73");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.1+sha-bca76ed";
+export declare const NGCC_VERSION = "15.2.0-next.1+sha-2d0ca73";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
