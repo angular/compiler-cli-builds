fb6fca455d8538d6ed02de8fb5a463fbb0ce924c
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 18:46:44 +0000

    c2cd0c548d fix(docs-infra): consistent table width with min-width:100% (#48815)

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.1+sha-3440778";
+var NGCC_VERSION = "15.2.0-next.1+sha-c2cd0c5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
