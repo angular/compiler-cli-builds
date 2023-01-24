23dff7f6d127c85210abedc118ea377523fda026
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 18:06:40 +0000

    98ccb57117 fix(compiler): handle css selectors with space after an escaped character. (#48558)

--- bundles/chunk-7CWE4INV.js
+++ bundles/chunk-7CWE4INV.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.1+sha-64416a6";
+var NGCC_VERSION = "15.1.1+sha-98ccb57";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.1+sha-64416a6");
+var VERSION = new Version("15.1.1+sha-98ccb57");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
