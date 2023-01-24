160709a52c665556e19e8674f28d409233f5451c
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:06:04 +0000

    4da1f2948c fix(compiler-cli): resolve deprecation warning (#48812)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-c03e6a6");
+var VERSION = new Version("15.2.0-next.1+sha-4da1f29");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
