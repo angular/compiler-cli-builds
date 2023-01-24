a52de570ac617970485ff8e2d16fb1a74877e2b1
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 21:26:22 +0000

    552cd3fb5c refactor: remove unnecessary test support check utilities (#47543)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.1+sha-067c4ce");
+var VERSION = new Version("15.1.1+sha-552cd3f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
