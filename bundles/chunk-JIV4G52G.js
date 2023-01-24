f960b38e32c403da676c1819a1620c9ce3ba82eb
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:05:57 +0000

    c03e6a6097 docs: Use of $any() has no runtime incidence. (#48785) Some users are woried about `$any()` as function calls are to be avoided in templates. This sentence clarifies the situation.

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.1+sha-a154db8";
+var NGCC_VERSION = "15.2.0-next.1+sha-c03e6a6";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.1+sha-a154db8";
+export declare const NGCC_VERSION = "15.2.0-next.1+sha-c03e6a6";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
