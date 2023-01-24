f4a9c7a50434bafdaac123b57e4311f87d757bbe
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 18:51:09 +0000

    402fcc57bb refactor(localize): bundle message digest algorithm directly in npm package (#48799)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-c2cd0c5");
+var VERSION = new Version("15.2.0-next.1+sha-402fcc5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
