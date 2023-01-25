e0bf107a9a1f645d6bdf162567ea47ca83ddd4da
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-01-25 17:25:21 +0000

    c91606b67e release: cut the v15.2.0-next.2 release

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.1+sha-e7318fc";
+var NGCC_VERSION = "15.2.0-next.2+sha-c91606b";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-e7318fc");
+var VERSION = new Version("15.2.0-next.2+sha-c91606b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
