c7a4e73cbe42392e89a4b26c703033f75f07f3c9
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 16:32:27 +0000

    e7318fc758 feat(core): add ng generate schematic to remove unnecessary modules (#48832)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.1+sha-1063404");
+var VERSION = new Version("15.2.0-next.1+sha-e7318fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
