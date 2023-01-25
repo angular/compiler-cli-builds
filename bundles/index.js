497c9dab8053aa938d80dc11996577c8da3eeaf9
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 19:51:25 +0000

    dedac8d3f7 feat(router): Add test helper for trigger navigations in tests (#48552)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.2+sha-a055196");
+var VERSION = new Version("15.2.0-next.2+sha-dedac8d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
