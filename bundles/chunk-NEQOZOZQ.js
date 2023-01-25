8901b1958e1accf3f9a3fb96d1dec2e91f527b1e
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-01-25 17:07:05 +0000

    0861471112 release: cut the v15.1.2 release

--- bundles/chunk-NEQOZOZQ.js
+++ bundles/chunk-NEQOZOZQ.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.1.1+sha-2403d3d");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.1.2+sha-0861471");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.1.1+sha-2403d3d") {
+    if (version === "15.1.2+sha-0861471") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
