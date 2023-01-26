847b043415eab1b06a3e35b16f52ae969bdb96fb
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 17:15:37 +0000

    ed8f70125e docs(core): add readme for standalone migration (#48848)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.2+sha-04e0ac3");
+var VERSION = new Version("15.2.0-next.2+sha-ed8f701");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
