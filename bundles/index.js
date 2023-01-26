0874b7cc301d8950a0a4e56a6605fa45533e93e3
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 22:23:45 +0000

    b4187548ca fix(animations): fix non-animatable warnings for easing (#48583)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.2+sha-33a636a");
+var VERSION = new Version("15.2.0-next.2+sha-b418754");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
