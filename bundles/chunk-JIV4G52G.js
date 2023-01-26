3a6d757334693f619776185865c7c6b935267e52
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 22:26:54 +0000

    e7a4fd3426 build: update cross-repo angular dependencies (#48843)

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.2+sha-b418754";
+var NGCC_VERSION = "15.2.0-next.2+sha-e7a4fd3";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
