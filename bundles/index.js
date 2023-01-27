07422306cd968470b9fab4a805de9c84e5e2f124
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 17:24:35 +0000

    d3a14d94a1 docs(docs-infra): update strongbrew resource to new website (#48340)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.2+sha-f82bdc4");
+var VERSION = new Version("15.2.0-next.2+sha-d3a14d9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
