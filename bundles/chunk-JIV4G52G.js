5ece11ddcf0bb132a22d1fc1ed48454d564c6104
Dylan Hunn <dylhunn@gmail.com>
2023-02-01 12:16:27 +0000

    2fc5b70fce fix(platform-server): insert transfer state `script` before other `script` tags (#48868)

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.2+sha-d3a14d9";
+var NGCC_VERSION = "15.2.0-next.2+sha-2fc5b70";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
