59599bea2fb8f5cea100ba909b253265af652010
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:36:42 +0000

    3cce18e434 release: cut the v15.2.0-next.3 release

--- bundles/chunk-JIV4G52G.js
+++ bundles/chunk-JIV4G52G.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.2+sha-65329a9";
+var NGCC_VERSION = "15.2.0-next.3+sha-3cce18e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.2+sha-65329a9";
+export declare const NGCC_VERSION = "15.2.0-next.3+sha-3cce18e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
