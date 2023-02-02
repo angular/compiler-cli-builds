23551e8f18e8c42afd9ff49830e10d4adf19ee44
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 22:00:38 +0000

    21b15e8b86 docs: making footer copyright year dynamic (#48811)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.2+sha-7f5d035");
+var VERSION = new Version("15.1.3+sha-21b15e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
