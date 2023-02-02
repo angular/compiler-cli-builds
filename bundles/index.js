9643d9169d08db222f9e502e480310b96c2d2e10
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:57:50 +0000

    c2bcf0b475 refactor(compiler): Remove strictStyling option for ShadowCss (#48824)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.3+sha-3a9bbad");
+var VERSION = new Version("15.2.0-next.3+sha-c2bcf0b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
