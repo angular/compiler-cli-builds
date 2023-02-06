0cf6a8d88cc9061645ff648ecc1b105f1fca8a0c
Andrew Scott <atscott@google.com>
2023-02-06 20:51:15 +0000

    a47721c2f1 refactor(migrations): add API to remap generated import names (#48974)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.3+sha-5a886c0");
+var VERSION = new Version("15.2.0-next.3+sha-a47721c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
