75656f8d5b72ae683e4573f638dd2b29e1154b4b
Andrew Scott <atscott@google.com>
2023-02-06 20:57:48 +0000

    03a946dda3 docs: Fixing typo error (#48891)

--- bundles/chunk-7CWE4INV.js
+++ bundles/chunk-7CWE4INV.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.3+sha-21b15e8";
+var NGCC_VERSION = "15.1.3+sha-03a946d";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.3+sha-21b15e8");
+var VERSION = new Version("15.1.3+sha-03a946d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
