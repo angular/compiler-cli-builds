87d076aedda42edeeeec6d180cb2dab4df0b188e
Andrew Scott <atscott@google.com>
2023-02-07 20:38:54 +0000

    5128ba036a refactor(router): Warn if a navigation will change in the upcoming v16 release (#48688)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.3+sha-4b8b5e3");
+var VERSION = new Version("15.2.0-next.3+sha-5128ba0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
