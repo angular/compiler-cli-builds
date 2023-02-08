c2b5f46fbb482999595e4a7cae4488b64b95bc13
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:20:26 +0000

    845ef7102b build: update github/codeql-action action to v2.2.2 (#48889)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.3+sha-5128ba0");
+var VERSION = new Version("15.2.0-next.3+sha-845ef71");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
