21131511931c1f8eebd1e89bc889073af7f4a155
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:31:59 +0000

    c7926b5773 fix(migrations): move standalone migrations into imports (#48987)

--- bundles/chunk-OHMNUCVF.js
+++ bundles/chunk-OHMNUCVF.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.3+sha-845ef71";
+var NGCC_VERSION = "15.2.0-next.3+sha-c7926b5";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
