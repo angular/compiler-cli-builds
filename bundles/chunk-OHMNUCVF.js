9404367c61a469067005301cfa5a20d577de20f2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:43:56 +0000

    642cc1c349 release: cut the v15.2.0-next.4 release

--- bundles/chunk-OHMNUCVF.js
+++ bundles/chunk-OHMNUCVF.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.3+sha-47387d4";
+var NGCC_VERSION = "15.2.0-next.4+sha-642cc1c";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
