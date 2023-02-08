205ed509dc014fb5df6b157d392f804871b86dbd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:33:06 +0000

    47387d495e docs: release notes for the v15.1.4 release

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.3+sha-bdbf21d";
+export declare const NGCC_VERSION = "15.2.0-next.3+sha-47387d4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
