ac580a9452958f7d9606a8e7bed4a46be39e2728
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:49:41 +0000

    993a43f87c test: update size goldens to reflect devkit update and terser (#48996)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-7af4ea6");
+var VERSION = new Version("15.2.0-next.4+sha-993a43f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
