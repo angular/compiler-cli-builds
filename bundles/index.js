303103808c35ba4022b450f2807ef0ee58d37de6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-10 19:33:56 +0000

    42619bd670 build: remove puppeteer, remaining usages of `webdriver-manager` and migrate AIO production test to Bazel (#49025)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-993a43f");
+var VERSION = new Version("15.2.0-next.4+sha-42619bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
