66753271341782d492b4c2682756105d1d871900
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:37:57 +0000

    7af4ea62e6 build: update actions/cache digest to 6998d13 (#49020)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-40c976c");
+var VERSION = new Version("15.2.0-next.4+sha-7af4ea6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
