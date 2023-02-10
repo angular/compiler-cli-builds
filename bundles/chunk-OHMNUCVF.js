8682d57fafb190938c100038b1b66fd2a892113e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:42:09 +0000

    865e67fcb4 build: remove unused webdriver-manager code (#49009)

--- bundles/chunk-OHMNUCVF.js
+++ bundles/chunk-OHMNUCVF.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.4+sha-4ac25b2";
+var NGCC_VERSION = "15.2.0-next.4+sha-865e67f";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
