0936972c6096c563c9a44776e16523682f177eb1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:38:09 +0000

    c9e2728c85 build: update actions/cache digest to 6998d13 (#49020)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.1.4+sha-5f2a3ed";
+export declare const NGCC_VERSION = "15.1.4+sha-c9e2728";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
