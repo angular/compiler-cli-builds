0e7d9a587591e0b25f292083af49a9d8b8c8d733
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 10:47:06 +0000

    59685614f8 fix(forms): Make radio buttons respect `[attr.disabled]` (#48864)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0-next.4+sha-c194867";
+export declare const NGCC_VERSION = "15.2.0-next.4+sha-5968561";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
