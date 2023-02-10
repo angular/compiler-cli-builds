bb7cedec9c90a295209f9a3855dc98c8fc93e09e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:34:57 +0000

    40c976c909 fix(migrations): use NgForOf instead of NgFor (#49022)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-5968561");
+var VERSION = new Version("15.2.0-next.4+sha-40c976c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
