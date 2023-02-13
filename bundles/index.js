ed2c3cf69cc8a59620e0f71d5a147310bafe56e1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 19:16:25 +0000

    c120fe5eab build: never cache monitoring bazel test target (#49039)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.4+sha-b022d07");
+var VERSION = new Version("15.1.4+sha-c120fe5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
