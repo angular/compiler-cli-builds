5773ee1a378bb7a12fb636be184d7049ef5e55d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 15:19:53 +0000

    b022d07344 build: update dependency eslint-plugin-jsdoc to v40 (#49037)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.4+sha-0091947");
+var VERSION = new Version("15.1.4+sha-b022d07");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
