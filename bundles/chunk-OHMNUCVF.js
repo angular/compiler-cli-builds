05f0d0ec133486e080b4943f18005cf6b09fc5c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 15:20:29 +0000

    ebae506d89 fix(migrations): use import remapper in root component (#49046)

--- bundles/chunk-OHMNUCVF.js
+++ bundles/chunk-OHMNUCVF.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.4+sha-0b725a8";
+var NGCC_VERSION = "15.2.0-next.4+sha-ebae506";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
