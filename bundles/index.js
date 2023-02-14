63324691c71465ca657bb264b6a2ba9edbd3c194
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 14:03:43 +0000

    1c5ae63811 refactor(docs-infra): Drop deprecated rxjs multicasting operators (#48969)

--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-31a2e21");
+var VERSION = new Version("15.2.0-next.4+sha-1c5ae63");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
