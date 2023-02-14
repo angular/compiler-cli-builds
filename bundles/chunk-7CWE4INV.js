a469e6025244b491ba7095f2a6328920bd5743e7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 18:02:52 +0000

    6534a5e3ca docs: remove former team members from bios (#48999)

--- bundles/chunk-7CWE4INV.js
+++ bundles/chunk-7CWE4INV.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.1.4+sha-f3e3f6e";
+var NGCC_VERSION = "15.1.4+sha-6534a5e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.4+sha-f3e3f6e");
+var VERSION = new Version("15.1.4+sha-6534a5e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
