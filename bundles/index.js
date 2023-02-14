b392e065e007cd4ff3458dbd58b4833e74b5dc21
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 17:21:44 +0000

    f3e3f6e455 build: update dependency json5 to v2.2.2 [security] (#48989)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.4+sha-17c3bd4");
+var VERSION = new Version("15.1.4+sha-f3e3f6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
