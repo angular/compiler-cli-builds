b9209d5b0508f7d16b92856d4c782319fb5932e2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-15 17:49:33 +0000

    44f13e2f2b docs: release notes for the v15.1.5 release

--- bundles/chunk-OHMNUCVF.js
+++ bundles/chunk-OHMNUCVF.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0-next.4+sha-2ca4972";
+var NGCC_VERSION = "15.2.0-next.4+sha-44f13e2";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -68,7 +68,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-next.4+sha-2ca4972");
+var VERSION = new Version("15.2.0-next.4+sha-44f13e2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
