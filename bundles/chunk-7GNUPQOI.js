efd4b362fb1c12cde5c5c73c720cd97696b193e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:51:02 +0000

    f3f139942c refactor(compiler): remove remaining usage of getMutableClone (#49070)

--- bundles/chunk-7GNUPQOI.js
+++ bundles/chunk-7GNUPQOI.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-0c7b9aa");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-f3f1399");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.0+sha-0c7b9aa") {
+    if (version === "16.0.0-next.0+sha-f3f1399") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-0c7b9aa");
+var VERSION = new Version("16.0.0-next.0+sha-f3f1399");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
