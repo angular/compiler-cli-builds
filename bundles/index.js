d0dd68905dc3491c6af55a6ffbd220d16ff826af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:21:04 +0000

    dea403e43f fix(platform-server): avoid duplicate TransferState info after renderApplication call (#49094)

--- bundles/index.js
+++ bundles/index.js
@@ -67,7 +67,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.1.5+sha-262895b");
+var VERSION = new Version("15.1.5+sha-dea403e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
