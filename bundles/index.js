2c1152ca793d8d838065d9d94cfdb54f20ecb3b4
Andrew Kushnir <akushnir@google.com>
2023-02-17 20:06:48 +0000

    c872426fa9 ci: update AIO payload size tracking (#49130)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-9b9c818");
+var VERSION = new Version("16.0.0-next.0+sha-c872426");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
