cc5a5e91d23654eff04a79ecbb19d8c576f812ae
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:25:39 +0000

    2d51b5f644 Revert "fix(platform-server): insert transfer state `script` before other `script` tags (#48868)" (#49112)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-ca53ade");
+var VERSION = new Version("16.0.0-next.0+sha-2d51b5f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
