39e4ba67419fad4c483601548fad9113d32170f0
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:19:55 +0000

    1f055b90b6 fix(router): Ensure anchor scrolling happens on ignored same URL navigations (#48025)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-48aa96e");
+var VERSION = new Version("16.0.0-next.0+sha-1f055b9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
