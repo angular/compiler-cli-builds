540c0f8ddce83242d088c566fb236bee61289ea1
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:27:10 +0000

    f12ba1c6dd refactor: remove unnecessary file (#49042)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-2d51b5f");
+var VERSION = new Version("16.0.0-next.0+sha-f12ba1c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
