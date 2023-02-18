62e3a13a8243df9e453a09d3ed6a6e063cc4d33c
Joey Perrott <josephperrott@gmail.com>
2023-02-18 02:53:27 +0000

    61878883ed build: update to latest remote build execution setup (#49133)

--- bundles/chunk-7GNUPQOI.js
+++ bundles/chunk-7GNUPQOI.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-rc.0+sha-8ed653b");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.2.0-rc.0+sha-6187888");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.2.0-rc.0+sha-8ed653b") {
+    if (version === "15.2.0-rc.0+sha-6187888") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-rc.0+sha-8ed653b");
+var VERSION = new Version("15.2.0-rc.0+sha-6187888");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
