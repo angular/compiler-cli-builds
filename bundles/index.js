30aed46c60ad7d1aa97513e6d9a181e970f4ed97
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-20 15:28:07 +0000

    0950ef6a49 ci: attempt to fix node windows job flakiness (#49142)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.0-rc.0+sha-6187888");
+var VERSION = new Version("15.2.0-rc.0+sha-0950ef6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
