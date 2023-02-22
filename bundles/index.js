4351cb90eb41237a38a3f178ff6827dc4884dc5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:45:23 +0000

    bc5ddabdcb feat(core): add Angular Signals to the public API (#49150)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-1e32709");
+var VERSION = new Version("16.0.0-next.0+sha-bc5ddab");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
