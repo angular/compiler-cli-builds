e9cb644006d971ac7dddc3b1700bf4e70cee9cb9
Andrew Kushnir <akushnir@google.com>
2023-02-23 19:01:24 +0000

    d7835e4f1a docs(router): fix code block closing ticks (#49185) PR Close #49185

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0+sha-7677963";
+export declare const NGCC_VERSION = "15.2.0+sha-d7835e4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
