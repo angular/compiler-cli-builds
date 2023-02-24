1ac8d88097b2b000e3addb1fbfffce42c6f5a10d
Andrew Kushnir <akushnir@google.com>
2023-02-24 19:01:57 +0000

    8542593c80 refactor(platform-browser): move TransferState init logic into its constructor (#49191)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.0+sha-d0fa598";
+export declare const NGCC_VERSION = "15.2.0+sha-8542593";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
