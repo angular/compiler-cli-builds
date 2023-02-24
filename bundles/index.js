178590090c95171f9823ce8775fe2fa4d301683c
Andrew Kushnir <akushnir@google.com>
2023-02-24 19:00:48 +0000

    74258f3652 refactor(platform-browser): move TransferState init logic into its constructor (#49191)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-76731ae");
+var VERSION = new Version("16.0.0-next.0+sha-74258f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
