2797f5b8aa8e845f1bd8660e656a24dc7a1a06d4
Andrew Kushnir <akushnir@google.com>
2023-02-24 16:33:51 +0000

    7dd19570e8 fix(migrations): delete barrel exports in standalone migration (#49176)

--- bundles/chunk-JEQIMWEA.js
+++ bundles/chunk-JEQIMWEA.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-79cdfeb");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-7dd1957");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.0+sha-79cdfeb") {
+    if (version === "16.0.0-next.0+sha-7dd1957") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-79cdfeb");
+var VERSION = new Version("16.0.0-next.0+sha-7dd1957");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
