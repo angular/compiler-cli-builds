2af6a853ae7912ba2ba029d370e027b8b6b45abf
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:49 +0000

    1d08a93a51 docs: change link for Angular Projects book (#49198) PR Close #49198

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-47ef9e8");
+var VERSION = new Version("16.0.0-next.0+sha-1d08a93");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
