36e0226fcfe05887bc2c1afdb94fb20b7db4520f
Andrew Kushnir <akushnir@google.com>
2023-02-27 18:27:07 +0000

    7e35a917c5 fix(router): add error message when using loadComponent with a NgModule (#49164)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-d420ab9");
+var VERSION = new Version("16.0.0-next.0+sha-7e35a91");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
