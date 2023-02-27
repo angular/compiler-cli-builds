50f47d278136dd15c3592f39efec9ec6a4b0b039
Andrew Kushnir <akushnir@google.com>
2023-02-27 23:35:58 +0000

    ca6ea0d2b7 docs: New document extracted from the original Communicating with backend services using HTTP document, which is to be retired. (#47937)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-459fbb3");
+var VERSION = new Version("16.0.0-next.0+sha-ca6ea0d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
