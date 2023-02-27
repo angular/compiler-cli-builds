74506ddd56a16537779cfa739a35c091d074bb43
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:40 +0000

    47ef9e83c7 build: cleanup unused dependencies (#49227)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-d1ac3aa");
+var VERSION = new Version("16.0.0-next.0+sha-47ef9e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
