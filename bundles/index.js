75515919668ea56520a6b55986047bfd990e5819
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:20:57 +0000

    27b986e119 docs: add new edition of Learning Angular book (#49199) PR Close #49199

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-74258f3");
+var VERSION = new Version("16.0.0-next.0+sha-27b986e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
