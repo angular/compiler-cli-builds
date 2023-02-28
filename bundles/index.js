b6679bf4afee5ada4909717689dba0012af1fe61
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:35:20 +0000

    8869289c09 refactor(platform-server): handle #24571 todos (#49233)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-75f48fe");
+var VERSION = new Version("16.0.0-next.0+sha-8869289");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
