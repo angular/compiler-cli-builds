90fd8b147d6f3489062b32a8ff216b6ad149df2e
Andrew Kushnir <akushnir@google.com>
2023-02-28 17:47:31 +0000

    9553a9a2a1 docs: New doc extract from original HTTP doc to be retired. (#48912)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-557ae66");
+var VERSION = new Version("16.0.0-next.0+sha-9553a9a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
