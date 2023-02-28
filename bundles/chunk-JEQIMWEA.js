a6e97e016292a61871fd2310b4b702d05a03d81e
Andrew Kushnir <akushnir@google.com>
2023-02-28 17:46:11 +0000

    557ae661ed docs: New doc extract from original HTTP doc to be retired. (#48908)

--- bundles/chunk-JEQIMWEA.js
+++ bundles/chunk-JEQIMWEA.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-a51b5cf");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.0+sha-557ae66");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.0+sha-a51b5cf") {
+    if (version === "16.0.0-next.0+sha-557ae66") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
