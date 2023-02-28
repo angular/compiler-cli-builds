87099d05229c9d5c53c1ec1c5530868385a2d876
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:41:07 +0000

    31f210bf2c fix(router): Router.createUrlTree should work with any ActivatedRoute (#48508)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.0+sha-8869289");
+var VERSION = new Version("16.0.0-next.0+sha-31f210b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
