221fb6afb1f59ebd1a756f8d4e491d1a0647bbef
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:00 +0000

    17abe6dc96 refactor(platform-server): remove deprecated `renderModuleFactory` (#49247)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-2beffeb");
+var VERSION = new Version("16.0.0-next.1+sha-17abe6d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
