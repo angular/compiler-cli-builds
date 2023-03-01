499bbe844f4b789f3d6810c680c6f984a7ff0b46
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:37:18 +0000

    4e9531f777 feat(core): add `mergeApplicationConfig` method (#49253)

--- bundles/chunk-JEQIMWEA.js
+++ bundles/chunk-JEQIMWEA.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.1+sha-41bed34");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.1+sha-4e9531f");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.1+sha-41bed34") {
+    if (version === "16.0.0-next.1+sha-4e9531f") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
