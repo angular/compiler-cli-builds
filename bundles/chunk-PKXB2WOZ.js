09fbdf316988482a9832deb17c9128c061c8db43
Andrew Kushnir <akushnir@google.com>
2023-03-01 01:29:32 +0000

    439b4d3a89 docs: clarify ActivatedRouter injection location (#49270)

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.0+sha-377d0c9";
+var NGCC_VERSION = "15.2.0+sha-439b4d3";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
