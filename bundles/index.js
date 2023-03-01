c43d8a63b4fea3832e174566a9b0aa20d3def489
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:36 +0000

    c41a21658c refactor(common): remove deprecated `XhrFactory` export from `http` entrypoint (#49251)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-17abe6d");
+var VERSION = new Version("16.0.0-next.1+sha-c41a216");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
