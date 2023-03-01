0428443cde73ec545c9c7d9de6e42206d7998299
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-01 22:54:44 +0000

    8beb8526be refactor: remove duplicate key from component metadata (#49065)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-6d9e979");
+var VERSION = new Version("16.0.0-next.1+sha-8beb852");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
