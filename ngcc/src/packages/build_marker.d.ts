1c67f5b1bd186a65303993a6320d179b7a297f9a
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:36:41 +0000

    bca3aed9e4 build: remove core-js-bundle from dependencies (#49259)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.1+sha-1749971";
+export declare const NGCC_VERSION = "15.2.1+sha-bca3aed";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
