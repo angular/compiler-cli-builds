428b5108f050beb7c03fb8268aac7c28d4f860d7
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:44:15 +0000

    030060e3dc refactor(core): use lower-level API to annotate `TransferState` class (#49272)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-4e9531f");
+var VERSION = new Version("16.0.0-next.1+sha-030060e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
