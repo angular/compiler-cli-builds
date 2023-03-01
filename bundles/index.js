fcd3be1bb8011d229291fdf99cca68c3f4c10bb7
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-01 22:54:33 +0000

    6d9e979be1 refactor(router): use native functions instead of custom utils (#49209)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-b5278cc");
+var VERSION = new Version("16.0.0-next.1+sha-6d9e979");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
