199223bd58512c071bd4a059414d3032d8c45c68
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 22:31:23 +0000

    13e675ea87 build: update all non-major dependencies (#49267)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.1+sha-de48c99";
+export declare const NGCC_VERSION = "15.2.1+sha-13e675e";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
