1c3b460f0e1dea18e7ad08a81a4db53476812181
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 21:32:47 +0000

    e7948d30c5 docs(router): Update component testing to user `RouterTestingHarness` (#48553)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-e9bda8a");
+var VERSION = new Version("16.0.0-next.1+sha-e7948d3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
