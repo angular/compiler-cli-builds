93695ad7d2bc94601985a83d361270337d0be987
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 22:07:49 +0000

    10c8eae6d4 refactor(migrations): expose current file in import remapper (#49288)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-675fe2f");
+var VERSION = new Version("16.0.0-next.1+sha-10c8eae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
