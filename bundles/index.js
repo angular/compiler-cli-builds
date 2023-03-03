934fc74994af28cb27f3858238ca74682052fd12
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 19:58:50 +0000

    594e68cc4b refactor(core): Remove ɵivyEnabled (#49296)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-47dda6e");
+var VERSION = new Version("16.0.0-next.1+sha-594e68c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
