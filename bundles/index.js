ae986446d1408df95442a9a9cdb193d91f911492
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 18:47:20 +0000

    1ba551b888 Revert "refactor(router): Remove zone code from router scroller (#49238)" (#49311)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-fce0602");
+var VERSION = new Version("16.0.0-next.1+sha-1ba551b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
