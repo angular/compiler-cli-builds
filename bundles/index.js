79cbff1a587c60be8c9d8deb08942671891e139e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 19:57:27 +0000

    47dda6e1da test: fix router testing example breaking due to non-existent stub (#49293)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.1+sha-1ba551b");
+var VERSION = new Version("16.0.0-next.1+sha-47dda6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
