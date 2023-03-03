17e40cd602520aec9b79cf5fb57261b2148cd378
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 17:01:36 +0000

    f321e65c46 refactor(common): remove `BrowserPlatformLocation` from private exports. (#49301)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.1+sha-13e675e");
+var VERSION = new Version("15.2.1+sha-f321e65");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
