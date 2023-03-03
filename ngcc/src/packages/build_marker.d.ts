55a68bff4b1e14a56c53769ecea2dd69dd88a76f
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 20:02:16 +0000

    b90b3299aa test: fix router testing example breaking due to non-existent stub (#49293)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.1+sha-f321e65";
+export declare const NGCC_VERSION = "15.2.1+sha-b90b329";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
