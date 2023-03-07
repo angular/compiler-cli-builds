c3f9843a832fd492874a3656c1f5a7ad0c6071e1
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 19:33:38 +0000

    88a0ff74ad docs(docs-infra): Split Error by categories & sort by code. (#49343)

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.1+sha-e0be553";
+var NGCC_VERSION = "15.2.1+sha-88a0ff7";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
