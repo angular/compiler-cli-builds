38245cfdb1c636fd1531c999d9aee15344428e1c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 23:58:56 +0000

    7e23883b5d refactor(dev-infra): remove obsolete todos (#49362)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.2+sha-f099df8";
+export declare const NGCC_VERSION = "15.2.2+sha-7e23883";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
