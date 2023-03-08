e08fbb14a1ac556aa812a4050a8a35b5a79c82d9
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-03-08 18:58:56 +0000

    75d5bfda1e release: cut the v15.2.2 release

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.1+sha-9937508";
+var NGCC_VERSION = "15.2.2+sha-75d5bfd";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
