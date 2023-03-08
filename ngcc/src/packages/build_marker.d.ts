0c0e578353ee8d3fd5fbf6cd2c86925190faf55c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 22:13:25 +0000

    f099df839e docs: update events (#49366)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.2+sha-c9e8804";
+export declare const NGCC_VERSION = "15.2.2+sha-f099df8";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
