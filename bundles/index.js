083428f405a2d259e8d1844fa9d287f15deb5af3
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 18:33:49 +0000

    5e7fc259ea feat(service-worker): add function to provide service worker (#48247)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-86fc4d3");
+var VERSION = new Version("16.0.0-next.2+sha-5e7fc25");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
