aa8dab87a45fc899612bff8a13f8c3ebb416c55e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 17:29:04 +0000

    0a3ac9d6ea docs: delete outdated link. (#48909)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-addd7f6");
+var VERSION = new Version("16.0.0-next.2+sha-0a3ac9d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
