38ced46a885f0ac010d299f357c07e5ecc96f6fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:41:52 +0000

    b20ee9d47c refactor(core): remove duplicate call to `getComponentId` (#49390)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-3d2351c");
+var VERSION = new Version("16.0.0-next.2+sha-b20ee9d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
