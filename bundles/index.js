9bf33cc1810f689322dfc5269429639e825fff3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:30:24 +0000

    5a2b6227b3 fix(zone.js): revert Mocha it.skip, describe.skip method patch (#49329)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-daaed89");
+var VERSION = new Version("16.0.0-next.2+sha-5a2b622");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
