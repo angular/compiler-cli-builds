7398ec72acb88f17a17655c8303a96630f009e02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:37:25 +0000

    4e098fa8a7 refactor(core): move Zone providers to a single provider function (#49373)

--- bundles/chunk-JEQIMWEA.js
+++ bundles/chunk-JEQIMWEA.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.2+sha-5a2b622");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.2+sha-4e098fa");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.2+sha-5a2b622") {
+    if (version === "16.0.0-next.2+sha-4e098fa") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
