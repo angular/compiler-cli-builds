38acc71af538d60c0cefebaed086cb689f8c4a19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:36:55 +0000

    6c189ed0f1 build: update io_bazel_rules_sass digest to aa03944 (#49397)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.2+sha-2df92f7";
+export declare const NGCC_VERSION = "15.2.2+sha-6c189ed";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
