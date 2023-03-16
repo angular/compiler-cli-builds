583dee9fac5fa905eb1c52d4f9b2f8d2d7e74d7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:10:40 +0000

    de0bdeea4c build: update github/codeql-action action to v2.2.6 (#49420)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-2d7f48c");
+var VERSION = new Version("16.0.0-next.2+sha-de0bdee");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
