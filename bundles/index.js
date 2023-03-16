2b8faa0bc5a6722cfd16ac1f78b7cb087226a75e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:35:06 +0000

    67743340a7 test(common): update async pipe tests to fix test errors (#49433)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-bf2a5d5");
+var VERSION = new Version("16.0.0-next.2+sha-6774334");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
