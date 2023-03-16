44d0181e67859edf73fd23f7a1c2141fd8245352
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:35:11 +0000

    250f60227f docs: Understanding communicating with http - insert updated links. (#49429)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.2+sha-6774334");
+var VERSION = new Version("16.0.0-next.2+sha-250f602");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
