db559df5c5e02ab932a7e5fed0c11286313238dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 07:54:00 +0000

    c807a324d1 build: update actions/checkout digest to 24cb908 (#49436)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.3+sha-3d71d9d");
+var VERSION = new Version("15.2.3+sha-c807a32");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
