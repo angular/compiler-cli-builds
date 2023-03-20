485f42aea42288341b9dfa8049cc6d7082218ceb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 12:43:31 +0000

    0f0c0a9452 docs(docs-infra): improve docs for @angular/localize package. (#49393)

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.3+sha-bae6b5c";
+var NGCC_VERSION = "15.2.3+sha-0f0c0a9";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.3+sha-bae6b5c");
+var VERSION = new Version("15.2.3+sha-0f0c0a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
