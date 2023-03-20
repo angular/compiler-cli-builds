8b945092356ebebca95af229bfb2378842320d6f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 15:31:24 +0000

    087f4412af fix(core): more accurate matching of classes during content projection (#48888)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.3+sha-0f0c0a9";
+export declare const NGCC_VERSION = "15.2.3+sha-087f441";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
