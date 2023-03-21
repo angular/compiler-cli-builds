57bca963e1eb34bfc0ccfaec46771e7d663d5b1b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 18:10:15 +0000

    a94b66f432 refactor(core): avoid `ngSkipHydration` on nodes other than component host ones (#49500)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.3+sha-a0c289c");
+var VERSION = new Version("16.0.0-next.3+sha-a94b66f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
