50cbd30e61e2097186656216e18ce258bc7fb988
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:54:39 +0000

    b5e68e42c1 build: exclude draft PRs from triage queue (#49515)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.3+sha-6c9886b");
+var VERSION = new Version("15.2.3+sha-b5e68e4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
