c5add611ae82d2ec2398cea3894fe493d77a1ffa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 19:41:30 +0000

    951a3474d6 test: increase size tracking threshold to 5K / 5% (#49520)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.3+sha-b5e68e4");
+var VERSION = new Version("15.2.3+sha-951a347");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
