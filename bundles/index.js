05654da7e63ba65cdeaa3827cd019e56a37d339b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 08:59:46 +0000

    7885f35fd6 refactor(core): adding hydration for content projection (#49454)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.3+sha-6ad4b91");
+var VERSION = new Version("16.0.0-next.3+sha-7885f35");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
