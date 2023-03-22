43204e1f7595235f42651876fb2c1b5a6e2f563e
Andrew Scott <atscott01@gmail.com>
2023-03-22 20:28:45 +0000

    901353ec7a release: cut the v15.2.4 release

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.3+sha-3bb7676";
+var NGCC_VERSION = "15.2.4+sha-901353e";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
