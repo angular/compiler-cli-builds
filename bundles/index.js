7d3f88298300abde8ae6333beb96309de9548474
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:34:36 +0000

    60d71484fd build: update eslint dependencies to v5.56.0 (#49507)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.3+sha-ab0b362");
+var VERSION = new Version("16.0.0-next.3+sha-60d7148");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
