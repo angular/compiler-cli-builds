f3aa02a8d4266db11e03c02b9c3139b3ba4fd27a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:19:36 +0000

    3bb76768a6 docs: remove ticks in code sample to generate components (#49525)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.3+sha-be2db59";
+export declare const NGCC_VERSION = "15.2.3+sha-3bb7676";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
