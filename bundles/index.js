f7b9f9d9db1ccb56bd768305a1a157dc4672a6f0
Andrew Scott <atscott01@gmail.com>
2023-03-22 17:20:38 +0000

    8ea1fb7396 refactor(core): exclude disconnected nodes from hydration (#49471)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.3+sha-ff9d3b0");
+var VERSION = new Version("16.0.0-next.3+sha-8ea1fb7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
