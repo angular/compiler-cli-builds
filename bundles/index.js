42986e5b90eeecd5851c0183a90db2f5e70c36e0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:57:08 +0000

    546b285ec1 fix(migrations): preserve trailing commas in code generated by standalone migration (#49533)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.3+sha-60d7148");
+var VERSION = new Version("16.0.0-next.3+sha-546b285");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
