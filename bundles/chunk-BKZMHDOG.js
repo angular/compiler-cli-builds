d5dd06e4e9bb69c46c9c6acba91a521619d16fcd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:18:08 +0000

    ab0b3622a5 docs: remove ticks in code sample to generate components (#49525)

--- bundles/chunk-BKZMHDOG.js
+++ bundles/chunk-BKZMHDOG.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.3+sha-69a6e09");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.3+sha-ab0b362");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.3+sha-69a6e09") {
+    if (version === "16.0.0-next.3+sha-ab0b362") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
