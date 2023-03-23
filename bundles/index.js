fdcdecbf4ea22fee31311be528036a9d0a9b43f2
Andrew Scott <atscott01@gmail.com>
2023-03-23 18:52:26 +0000

    73d2f3c866 fix(compiler): handle trailing comma in object literal (#49535)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-585e34b");
+var VERSION = new Version("16.0.0-next.4+sha-73d2f3c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
