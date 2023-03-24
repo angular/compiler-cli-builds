dea174573584b93f6ee137462d7c84e2f4a45da3
Andrew Scott <atscott01@gmail.com>
2023-03-24 17:39:45 +0000

    656a38899a build: update actions/checkout digest to 8f4b7f8 (#49577)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.4+sha-29c682c");
+var VERSION = new Version("15.2.4+sha-656a388");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.4+sha-29c682c";
+export declare const NGCC_VERSION = "15.2.4+sha-656a388";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
