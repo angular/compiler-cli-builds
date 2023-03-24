46dd87425c7e2190c69d803f736ff9f886bc873b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-24 13:05:47 +0000

    29c682ce5b ci: update github.com known host given recent RSA update (#49580)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.4+sha-3f8e4fb");
+var VERSION = new Version("15.2.4+sha-29c682c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
