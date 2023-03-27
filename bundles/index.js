8f46f4ab161542f23def9c69a72ce99c8afafb0e
Andrew Scott <atscott01@gmail.com>
2023-03-27 20:14:51 +0000

    5e5dac278d feat(migrations): Migration to remove `Router` guard and resolver interfaces (#49337)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-22688b8");
+var VERSION = new Version("16.0.0-next.4+sha-5e5dac2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
