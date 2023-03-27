9b248d226562343835bfcf80e944697928687d38
Andrew Scott <atscott01@gmail.com>
2023-03-27 18:39:17 +0000

    9b65b84cb9 feat(core): Mark components for check if they read a signal (#49153)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-1d63183");
+var VERSION = new Version("16.0.0-next.4+sha-9b65b84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
