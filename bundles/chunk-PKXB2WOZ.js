9fca11262ce9bfa5721206479c9d263dc7b4af88
Andrew Scott <atscott01@gmail.com>
2023-03-28 18:54:43 +0000

    077f6b4674 fix(compiler): do not unquote CSS values (#49460)

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.4+sha-6d6fc12";
+var NGCC_VERSION = "15.2.4+sha-077f6b4";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
