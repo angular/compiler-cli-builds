fd4e96f5a5ce7812e7730d7aaf21d7a90f7da080
Andrew Scott <atscott01@gmail.com>
2023-03-29 22:56:49 +0000

    f2ba19253d release: cut the v16.0.0-next.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-cbe6807");
+var VERSION = new Version("16.0.0-next.5+sha-f2ba192");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
