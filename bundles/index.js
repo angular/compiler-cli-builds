b35cd908c0c6783b5bd786fc8b4dc79a937e5a89
Andrew Scott <atscott01@gmail.com>
2023-03-29 22:44:28 +0000

    cbe6807a46 refactor(core): Add default implementation for WeakRef (#49639)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-3c7e637");
+var VERSION = new Version("16.0.0-next.4+sha-cbe6807");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
