e82c1a2657a562a52978165171070697192dd3d0
Andrew Scott <atscott01@gmail.com>
2023-03-29 21:51:27 +0000

    3c7e637374 fix(router): Route matching should only happen once when navigating (#49163)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-2d25a2d");
+var VERSION = new Version("16.0.0-next.4+sha-3c7e637");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
