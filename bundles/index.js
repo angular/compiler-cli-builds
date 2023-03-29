8d9c2f61193028b26ff8ff7b592b56458813f458
Andrew Scott <atscott01@gmail.com>
2023-03-29 20:01:59 +0000

    2d25a2de16 docs: release notes for the v15.2.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-0432d95");
+var VERSION = new Version("16.0.0-next.4+sha-2d25a2d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
