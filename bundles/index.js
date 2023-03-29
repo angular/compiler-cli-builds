41b97cd91af14640da8510f8fbb3a0ecbd26a50a
Andrew Scott <atscott01@gmail.com>
2023-03-29 18:56:20 +0000

    0432d952a1 Revert "fix(router): Route matching should only happen once when navigating (#49163)" (#49634)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-9c5fd50");
+var VERSION = new Version("16.0.0-next.4+sha-0432d95");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
