021ba29ddb5b048210dd7d8ab15c31f3adb1edd5
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:43:26 +0000

    e8e36811d5 fix(platform-browser): set nonce attribute in a platform compatible way (#49624)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-e994608");
+var VERSION = new Version("16.0.0-next.4+sha-e8e3681");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
