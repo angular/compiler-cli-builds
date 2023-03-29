7cf1fbceef20da50c7aefad187283e9605a5d67a
Andrew Scott <atscott01@gmail.com>
2023-03-29 17:57:23 +0000

    9c5fd50de4 feat(core): effects can optionally return a cleanup function (#49625)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-6bcdb68");
+var VERSION = new Version("16.0.0-next.4+sha-9c5fd50");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
