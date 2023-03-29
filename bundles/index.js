378278352698acfb34f3d407cbdb0dee3170005a
Andrew Scott <atscott01@gmail.com>
2023-03-29 00:13:43 +0000

    84a0fa3e8a refactor(core): Fixes hydration navigation and dom removal bugs (#49615)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-478c5ac");
+var VERSION = new Version("16.0.0-next.4+sha-84a0fa3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
