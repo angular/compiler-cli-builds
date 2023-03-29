5ccfd7b11f5e73b4928bece7979d61971ce0df77
Andrew Scott <atscott01@gmail.com>
2023-03-29 17:36:37 +0000

    6bcdb68e69 refactor(router): clear pending navigation task in a microtask (#49619)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.4+sha-c024574");
+var VERSION = new Version("16.0.0-next.4+sha-6bcdb68");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
