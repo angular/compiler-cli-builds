9bae3916c4dd2deb3bcebf9dfc681b23326e3c02
Andrew Scott <atscott01@gmail.com>
2023-03-30 16:48:14 +0000

    06e74a5583 refactor(core): run effects during change detection (#49641)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.5+sha-f2ba192");
+var VERSION = new Version("16.0.0-next.5+sha-06e74a5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
