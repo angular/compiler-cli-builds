6c58a4ad23c66a40ff0307ee2a805d6d3e0f948d
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:18:39 +0000

    08979e4589 docs: fix typo in toh-pg6.md (#49141)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.5+sha-35f4918";
+export declare const NGCC_VERSION = "15.2.5+sha-08979e4";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
