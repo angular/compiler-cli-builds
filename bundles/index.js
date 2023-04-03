86fbb86e5b44d10789eda1f5781bdd1620946edf
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 22:20:56 +0000

    7870fb07fe feat(platform-server): rename `provideServerSupport` to `provideServerRendering` (#49678)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-a5f1737");
+var VERSION = new Version("16.0.0-next.6+sha-7870fb0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
