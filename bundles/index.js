9446882540a53db13cb7968c397a498e206318a7
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 05:26:53 +0000

    b3d2e3312a docs: fix typo (#49664) PR Close #49664

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.5+sha-fa3909e");
+var VERSION = new Version("16.0.0-next.5+sha-b3d2e33");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
