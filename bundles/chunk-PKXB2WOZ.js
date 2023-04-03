e53dd121b5f6725f16959937062a1ebc1ceb95d1
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 05:35:15 +0000

    a6def648f5 docs: fix typo (#49664) PR Close #49664

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.5+sha-9b61379";
+var NGCC_VERSION = "15.2.5+sha-a6def64";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
