6b55af4af4c0e8f50689968a1a866cf6bea26f5c
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 22:19:32 +0000

    b35fa73968 feat(core): change the URL sanitization to only block javascript: URLs (#49659)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-f0978ad");
+var VERSION = new Version("16.0.0-next.6+sha-b35fa73");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
