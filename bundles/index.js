9e34c0bd51a5e251359db15bb5701680b8d0d0e8
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:29:09 +0000

    e7463dc80f docs: tweak hydration guide from feedback (#49705)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-edc3bb1");
+var VERSION = new Version("16.0.0-next.6+sha-e7463dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
