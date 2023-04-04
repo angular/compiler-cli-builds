a81babbcab1258cafe756854e17edbc05042d6a2
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:37:16 +0000

    4b673d5ed7 docs: Add error page for NG0504 error (#49689)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-c912294");
+var VERSION = new Version("16.0.0-next.6+sha-4b673d5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
