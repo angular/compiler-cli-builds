a6114f3992f7cb01f65cfb3ff2e50dcbef3e3052
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 05:30:07 +0000

    761e02d912 feat(platform-browser): add a public API function to enable non-destructive hydration (#49666)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-b2327f4");
+var VERSION = new Version("16.0.0-next.6+sha-761e02d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
