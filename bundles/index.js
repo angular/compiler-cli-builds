5ca35204bde6e4a78bbb39d7952590ea9aa993a2
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:34:51 +0000

    cb23864472 docs: update Universal guide with hydration information (#49694)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-e7463dc");
+var VERSION = new Version("16.0.0-next.6+sha-cb23864");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
