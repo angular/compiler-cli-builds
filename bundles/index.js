a8c13bf789f77ee4d694405f3754932ec58df82d
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:40:43 +0000

    bd82fbeec4 docs: fix typo (#49669)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.5+sha-a6def64");
+var VERSION = new Version("15.2.5+sha-bd82fbe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
