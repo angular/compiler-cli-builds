c4f8422e7e786a88b12ef7e1915b8c72ed478136
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:32:20 +0000

    719f6a05fd refactor(core): remove unnecessary `reflect-metadata` import. (#49673)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-8019fed");
+var VERSION = new Version("16.0.0-next.6+sha-719f6a0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
