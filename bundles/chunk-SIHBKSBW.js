1862b77fb588cc349b4e4f6129ec162410effe4f
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 17:04:53 +0000

    d497be7aca refactor(core): Use the nullish coalescing assignment in render3 functions (#49698)

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.6+sha-136ffbc");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.6+sha-d497be7");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.6+sha-136ffbc") {
+    if (version === "16.0.0-next.6+sha-d497be7") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-136ffbc");
+var VERSION = new Version("16.0.0-next.6+sha-d497be7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
