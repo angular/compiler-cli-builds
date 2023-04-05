696c39eb0168db771155b99233656e3712235d3c
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 18:06:13 +0000

    df1dfc4c17 fix(core): make sure that lifecycle hooks are not tracked (#49701)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-a4e749f");
+var VERSION = new Version("16.0.0-next.6+sha-df1dfc4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
