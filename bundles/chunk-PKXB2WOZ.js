036c3f1e2f3acfa9f8c1bc398723001b16c7c1c3
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 17:04:42 +0000

    da5f316ebf refactor(core): Use the nullish coalescing assignment in render3 functions (#49698)

--- bundles/chunk-PKXB2WOZ.js
+++ bundles/chunk-PKXB2WOZ.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.5+sha-cad7274";
+var NGCC_VERSION = "15.2.5+sha-da5f316";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.5+sha-cad7274";
+export declare const NGCC_VERSION = "15.2.5+sha-da5f316";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
