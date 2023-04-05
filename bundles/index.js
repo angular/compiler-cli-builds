f569702226d97820b573cd19e25a831d5c03225b
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 18:11:28 +0000

    1c16be4728 build: update domino digest to aa8de34 (#49716)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-df1dfc4");
+var VERSION = new Version("16.0.0-next.6+sha-1c16be4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
