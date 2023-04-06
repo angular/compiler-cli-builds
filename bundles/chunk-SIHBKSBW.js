20896570998d3413558704f1d61c02c1e454edb4
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:22:11 +0000

    ed8a595408 docs: Improve previous version navigation (#49063) In archive mode the link to the current doc will now point to same page. If the page doesn't exist anymore, it will show a contextualized warning message.

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-fe58f0a");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-ed8a595");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.0+sha-fe58f0a") {
+    if (version === "16.1.0-next.0+sha-ed8a595") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
