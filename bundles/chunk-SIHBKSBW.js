7f1846ed08cc5491bad2b110f964f5b24b16e381
Dylan Hunn <github@dylanhunn.com>
2023-04-06 03:16:36 +0000

    726f09c01e docs: release notes for the v16.0.0-next.7 release

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-next.6+sha-a747954");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-726f09c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-next.6+sha-a747954") {
+    if (version === "16.1.0-next.0+sha-726f09c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
