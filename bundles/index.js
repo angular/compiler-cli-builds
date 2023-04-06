ceeb6cb171824008e9999adb80800c01742544e6
Joey Perrott <josephperrott.github@gmail.com>
2023-04-06 17:57:23 +0000

    ec0b44da8c docs: release notes for the v13.4.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-726f09c");
+var VERSION = new Version("16.1.0-next.0+sha-ec0b44d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
