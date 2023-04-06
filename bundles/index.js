3d529d54cdf6a4200f47c53effaa33e8a667f2b7
Dylan Hunn <dylhunn@gmail.com>
2023-04-06 02:52:24 +0000

    40ed152d21 refactor(animations): remove #9100 todos. (#49407)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-9bd9a11");
+var VERSION = new Version("16.0.0-next.6+sha-40ed152");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
