635808f6e20133174762a7a88956bbea59f4532d
Dylan Hunn <github@dylanhunn.com>
2023-04-06 02:56:44 +0000

    a7479545db docs: release notes for the v15.2.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.6+sha-40ed152");
+var VERSION = new Version("16.0.0-next.6+sha-a747954");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
