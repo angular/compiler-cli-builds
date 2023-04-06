94fb1800371d36b9294628d1b23a7ecaa201c719
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:22:19 +0000

    c713d9fe66 docs: Improve previous version navigation (#49063) In archive mode the link to the current doc will now point to same page. If the page doesn't exist anymore, it will show a contextualized warning message.

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.7+sha-f148751");
+var VERSION = new Version("16.0.0-next.7+sha-c713d9f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
