23e40ceef087d5d1747c7d28bc28d8790ebb3762
Andrew Kushnir <akushnir@google.com>
2023-04-07 16:57:52 +0000

    bab36d0c57 refactor(core): add a warning when hydration annotation is missing in server response (#49743)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.7+sha-c713d9f");
+var VERSION = new Version("16.0.0-next.7+sha-bab36d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
