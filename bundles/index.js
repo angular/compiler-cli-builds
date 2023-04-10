5f77413e24849b669e87bfa1887b3dc6bd033f14
Andrew Kushnir <akushnir@google.com>
2023-04-10 16:19:26 +0000

    83262dc0f9 refactor(core): do not enable hydration when server response was incorrect (#49750)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-fe34de4");
+var VERSION = new Version("16.1.0-next.0+sha-83262dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
