1742d8832fae90da4003cfe51198ca490cbdec70
Andrew Kushnir <akushnir@google.com>
2023-04-11 17:38:09 +0000

    d5e02427ea build: create script to update integration lock files (#49787)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-ee816e1");
+var VERSION = new Version("16.1.0-next.0+sha-d5e0242");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
