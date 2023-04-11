470f60f9cb8543c26b5c257f4cde6ca736e6c502
Andrew Kushnir <akushnir@google.com>
2023-04-11 20:05:24 +0000

    ce38be03ce fix(core): allow async functions in effects (#49783)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-7ee542d");
+var VERSION = new Version("16.1.0-next.0+sha-ce38be0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
