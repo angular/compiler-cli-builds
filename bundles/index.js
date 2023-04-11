fca48d4bb8ec24dd301da1678a2e2b1858b28703
Andrew Kushnir <akushnir@google.com>
2023-04-11 19:02:00 +0000

    d980af6ff3 build(compiler): add a flag to conditionally build with the template pipeline (#48580)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.7+sha-687f138");
+var VERSION = new Version("16.0.0-next.7+sha-d980af6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
