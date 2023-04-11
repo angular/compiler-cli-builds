7efcde3b2cd1405016b7693cb36daae2498c8f23
Andrew Kushnir <akushnir@google.com>
2023-04-11 18:57:31 +0000

    4e41d127db build(compiler): add a flag to conditionally build with the template pipeline (#48580)

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-cf46dfb");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-4e41d12");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.0+sha-cf46dfb") {
+    if (version === "16.1.0-next.0+sha-4e41d12") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
