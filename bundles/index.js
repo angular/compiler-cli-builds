978fc690a4ecfff258cc88aff34137ff68c01932
Andrew Kushnir <akushnir@google.com>
2023-04-11 17:57:28 +0000

    0b3677e149 fix(http): ensure new cache state is returned on each request (#49749)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-8272b5b");
+var VERSION = new Version("16.1.0-next.0+sha-0b3677e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
