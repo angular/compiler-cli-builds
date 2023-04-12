d3aa1b149f30edb09e4fddd54f663a198162fa07
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:51:42 +0000

    d966fdd438 refactor(router): Ensure data is bound to components in change detection following navigation (#49741)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-c190fbb");
+var VERSION = new Version("16.1.0-next.0+sha-d966fdd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
