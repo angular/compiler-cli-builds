5ca1348326e194889081abf85669858f91e9e42d
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:53:18 +0000

    1dddb78786 fix(core): toObservable should allow writes to signals in the effect (#49769)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-d966fdd");
+var VERSION = new Version("16.1.0-next.0+sha-1dddb78");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
