edb2a25327b8e268e3a8f6caaee66c611d29286b
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:57:07 +0000

    fd13e11e75 test: improve testing of HTTP and transfer state cache (#49810)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-next.7+sha-15c91a5");
+var VERSION = new Version("16.0.0-next.7+sha-fd13e11");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
