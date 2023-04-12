3fd7004b2a944c8093233f4187dcd80c852cbd2d
Andrew Kushnir <akushnir@google.com>
2023-04-12 20:07:57 +0000

    4721c48a24 fix(core): error if document body is null (#49818)

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.0+sha-61f63cf");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.0+sha-4721c48");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-rc.0+sha-61f63cf") {
+    if (version === "16.0.0-rc.0+sha-4721c48") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
