868d0a4701297a246e32450f0febc38542dcbb82
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:40:07 +0000

    85c1a6463f build: update github/codeql-action action to v2.2.11 (#49775)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-1f7f759");
+var VERSION = new Version("16.1.0-next.0+sha-85c1a64");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
