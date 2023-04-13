cc3fd5e5e1ef14dfb3f6898fc0da4928e57267ea
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:19:19 +0000

    7fd0b678f3 refactor(core): avoid deep links into `@angular/core` (#49823)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-54f18c8");
+var VERSION = new Version("16.1.0-next.0+sha-7fd0b67");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
