4405ce239bfcda74653639274f3cc64b3da57273
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 21:04:07 +0000

    6ade3ec8c3 refactor(core): add asReadonly helper to writable signals (#49802)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.0+sha-87549af");
+var VERSION = new Version("16.0.0-rc.0+sha-6ade3ec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
