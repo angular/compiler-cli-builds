d816223f5596cf9d72e91cf14099eb0248586b16
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:22:00 +0000

    f266a05d2f refactor(core): drop IE workarounds (#49763)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.7+sha-1d761bd");
+var VERSION = new Version("15.2.7+sha-f266a05");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
