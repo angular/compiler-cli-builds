ffae17faf0645f628c5875391629b7c529d76aca
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:21:48 +0000

    970e8b40dc refactor(core): fix privately imported symbol from signals package (#49812)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.0+sha-4721c48");
+var VERSION = new Version("16.0.0-rc.0+sha-970e8b4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
