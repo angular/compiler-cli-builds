1df461e68ab005164c8cb7dc7ac5fc091ed8577b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 14:30:21 +0000

    31e1264faf ci: Remove environment files and EnableProdMode on integration tests (#49746)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.0+sha-c34d7e0");
+var VERSION = new Version("16.0.0-rc.1+sha-31e1264");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
