e6205523009106e4a16d54feb406ae97441c15f0
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 15:11:12 +0000

    cbca5817d8 fix(router): canceledNavigationResolution: 'computed' with redirects to the current URL (#49793)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-b4e83ca");
+var VERSION = new Version("16.1.0-next.0+sha-cbca581");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
