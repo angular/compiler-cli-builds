845edcdd9b991a8832482fda9226079050d9b89d
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-04-14 14:21:20 +0000

    09f9670f72 docs: release notes for the v16.0.0-rc.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-2c22e6f");
+var VERSION = new Version("16.1.0-next.0+sha-09f9670");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
