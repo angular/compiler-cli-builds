902b80f0f928cf37784d6d40b5cfd399a0680829
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 19:44:06 +0000

    449c011ae4 refactor(platform-server): simplify `_render` method (#49840)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-e594d1c");
+var VERSION = new Version("16.0.0-rc.1+sha-449c011");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
