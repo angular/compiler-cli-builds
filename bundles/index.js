00a6752f8d4e96b35b2d20c104d0876e4d828e23
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:06:05 +0000

    14b5a5d595 refactor(router): Do not access browserPageId if not computed resolution (#49850)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-3659e03");
+var VERSION = new Version("16.1.0-next.0+sha-14b5a5d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
