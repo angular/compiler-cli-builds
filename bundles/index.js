77e269e14fecf9768954139248325e72f800efcb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:47:47 +0000

    4ee286b518 build: update PullApprove config (#49878)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-2118ee9");
+var VERSION = new Version("16.0.0-rc.1+sha-4ee286b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
