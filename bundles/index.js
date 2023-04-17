a0a5aba2746a96c23155fe47963c97141968bbb8
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:22:27 +0000

    1017630d04 refactor(docs-infra): cleanup deprecated code (#49671)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.7+sha-a63d9a3");
+var VERSION = new Version("15.2.7+sha-1017630");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
