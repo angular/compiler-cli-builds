0910011d397c5cf3f42cbfad6a50edeec8f42be6
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:24:11 +0000

    14d27475f8 test: refactor platform-server tests to use async/await (#49855)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-449c011");
+var VERSION = new Version("16.0.0-rc.1+sha-14d2747");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
