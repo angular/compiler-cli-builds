740446fb06c0c8c4fc29435ad740a2a07ca79302
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:11:03 +0000

    a7dbb23bc8 refactor(compiler-cli): remove `incrementalDriver` on the compiler (#49869)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-3ba5dcc");
+var VERSION = new Version("16.1.0-next.0+sha-a7dbb23");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
