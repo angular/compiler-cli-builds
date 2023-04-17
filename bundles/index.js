fe84c8ab6ef18de46ab1724bd62a74a4844d7413
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:14:51 +0000

    ea4d9e8ef8 docs: fix typo in Hierarchical injectors doc (#49824)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-fd0c26e");
+var VERSION = new Version("16.1.0-next.0+sha-ea4d9e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
