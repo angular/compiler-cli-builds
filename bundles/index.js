00cf81385671a2d06c4752dba42bc6e3bd6c0feb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:00:58 +0000

    3c65305b10 refactor(router): remove targetPageId from transition (#49799)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-f6b9cf3");
+var VERSION = new Version("16.1.0-next.0+sha-3c65305");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
