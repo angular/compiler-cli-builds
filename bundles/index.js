633ccbc091436978741aeefee015f9176d30832b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 16:03:07 +0000

    a4c768cad6 build: update dependency eslint-plugin-jsdoc to v41 (#49885)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-28c789b");
+var VERSION = new Version("16.0.0-rc.1+sha-a4c768c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
