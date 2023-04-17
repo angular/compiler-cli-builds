bfe39355ed1b9cd920d2a7b6514554f8bb7f3aa8
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:47:30 +0000

    1d4cf95a94 docs(docs-infra): add lara newsom to GDE resources (#49620)

--- bundles/chunk-7GNUPQOI.js
+++ bundles/chunk-7GNUPQOI.js
@@ -848,7 +848,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "15.2.7+sha-79a8b3c");
+  const LATEST_VERSION_RANGE = getRange("<=", "15.2.7+sha-1d4cf95");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -895,7 +895,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "15.2.7+sha-79a8b3c") {
+    if (version === "15.2.7+sha-1d4cf95") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
