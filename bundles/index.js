8a82e881b266b6157e87afda5f03e9e7da54dfeb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:13:45 +0000

    fd0c26e430 docs: Fixed Typo (#49753)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-bf2e11c");
+var VERSION = new Version("16.1.0-next.0+sha-fd0c26e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
