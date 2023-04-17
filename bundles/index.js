3e2ba060eb99d88f0360b830e43b403947ed9596
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:16:34 +0000

    f9886e1e28 refactor(docs-infra): remove dart support & mentions (#49857)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-8c0285b");
+var VERSION = new Version("16.1.0-next.0+sha-f9886e1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
