9eb4e7ab744c2f6f94429f17028c03da083d31f4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 21:50:43 +0000

    1b13f18779 docs: add Artur Androsovych to GDE resources (#48520)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-209364d");
+var VERSION = new Version("16.0.0-rc.1+sha-1b13f18");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
