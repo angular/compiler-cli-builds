fc4ebf580243e1cf37dfe1dd25e8ae163cc8d794
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:32:03 +0000

    36ed47fdf4 docs: fix errors in toh-pt6.md (#49129)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-09332e7");
+var VERSION = new Version("16.1.0-next.0+sha-36ed47f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
