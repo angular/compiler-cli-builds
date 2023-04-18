6a462211cfdb2ca7f99ec18bc23e4bc4b2069137
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:22:36 +0000

    5e4797cd16 build: update dependency google-closure-compiler to v20230411 (#49892)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-ead10af");
+var VERSION = new Version("16.0.0-rc.1+sha-5e4797c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
