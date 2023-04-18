4ca041a5293b401dce30834b1329f9df24ceb24b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 17:18:03 +0000

    ead10aff41 refactor(compiler): next context merging in the template pipeline (#49797)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-27c624e");
+var VERSION = new Version("16.0.0-rc.1+sha-ead10af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
