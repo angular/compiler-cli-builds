8419357c9e6324da18d0e00143db434db42336a5
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 22:20:27 +0000

    318e3ed402 build: update angular/dev-infra digest to 1564c4e (#49916)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-1b13f18");
+var VERSION = new Version("16.0.0-rc.1+sha-318e3ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
