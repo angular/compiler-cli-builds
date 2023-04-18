1e82a746635669b00f6b38aa6423b38cf37994eb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:18:46 +0000

    1816c89326 refactor(platform-browser): cleanup references to dart (#49858)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-b7392f9");
+var VERSION = new Version("16.1.0-next.0+sha-1816c89");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
