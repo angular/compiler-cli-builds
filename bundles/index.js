4105e3a7465be973f2815b7b35dbcf4ec230aff5
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:50:59 +0000

    27c624e8e7 refactor(compiler): handle #24571 todos. (#49220)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-8219857");
+var VERSION = new Version("16.0.0-rc.1+sha-27c624e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
