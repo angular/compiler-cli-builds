e89f11e2afa5aa8c06a35062abc47e04b3db355d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 19:02:07 +0000

    d45aafb264 docs: add information on how to use index option in configuration (#49813)

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.1+sha-f8b95b9");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.1+sha-d45aafb");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-rc.1+sha-f8b95b9") {
+    if (version === "16.0.0-rc.1+sha-d45aafb") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-f8b95b9");
+var VERSION = new Version("16.0.0-rc.1+sha-d45aafb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
