fd0059ddcb380b433b48a9884046290019263227
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:43:35 +0000

    21b834fc36 test(core): adding extra tests for hydration of view containers located after <ng-content> slots (#49920)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-318e3ed");
+var VERSION = new Version("16.0.0-rc.1+sha-21b834f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
