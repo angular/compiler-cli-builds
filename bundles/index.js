44305d108b386d50a7c071a500e4dcdf74092697
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 18:20:40 +0000

    726d8a8a71 docs: update-to-version-14 linting fixes (#49839)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-9165942");
+var VERSION = new Version("16.1.0-next.0+sha-726d8a8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
