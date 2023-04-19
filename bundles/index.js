c8e5ed24b756c73168bff122158a1fdb2fd31755
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 18:29:34 +0000

    f8b95b9da6 fix(core): execute query setters in non-reactive context (#49906)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.1+sha-a3c3ab7");
+var VERSION = new Version("16.0.0-rc.1+sha-f8b95b9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
