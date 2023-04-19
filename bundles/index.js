88306bf9580b70eda2c48240e3c1d69a86754fe0
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:36:26 +0000

    efb3c68899 refactor(core): remove redundant `hydrationInfo` check (#49926)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-4175f6f");
+var VERSION = new Version("16.1.0-next.0+sha-efb3c68");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
