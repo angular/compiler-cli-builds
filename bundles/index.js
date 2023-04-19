fb55da4b0e887e271a336bdfb6a42fc3cbd3506d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 16:46:18 +0000

    2fff8fadbe fix(core): handle invalid classes in class array bindings (#49924)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.7+sha-0454158");
+var VERSION = new Version("15.2.7+sha-2fff8fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
