af1172da4132498388e5133de29e9f66608848cd
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:24:00 +0000

    0454158084 docs(core): add version ranges for Angular 14.3 (#48753)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.7+sha-296459a";
+export declare const NGCC_VERSION = "15.2.7+sha-0454158";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
