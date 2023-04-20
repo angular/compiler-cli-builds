344d710516ccfd9d9620af2998cc699026ba9b7d
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:05:10 +0000

    bbc2efcda2 refactor(core): hydrate components of the same type used with and without ngSkipHydration (#49943)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-da1ed98");
+var VERSION = new Version("16.1.0-next.0+sha-bbc2efc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
