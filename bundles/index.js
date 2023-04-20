b509cdb2f970cd56bce72220814497f6f52df758
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:08:18 +0000

    521e284214 build: update cross-repo angular dependencies to v16.0.0-rc.2 (#49941)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-bbc2efc");
+var VERSION = new Version("16.1.0-next.0+sha-521e284");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
