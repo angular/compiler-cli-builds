84618a5ab635f04b12ef581cc02eba11efd79dcb
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:06:39 +0000

    0f38b98274 docs: replaces "comp: docs" label to "area: docs" (#49952)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.8+sha-35690fd";
+export declare const NGCC_VERSION = "15.2.8+sha-0f38b98";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
