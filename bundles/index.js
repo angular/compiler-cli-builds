2b3c1332a9d9c7d6d57b2487c17e83494c500078
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 18:20:03 +0000

    6d028283d1 docs: fix typo in lazy-loading feature modules doc (#49972) PR Close #49972

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.8+sha-c17b84a");
+var VERSION = new Version("15.2.8+sha-6d02828");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
