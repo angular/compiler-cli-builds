da94a1c329ce5b4ee4ff3e358c350bf29b4d4548
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 19:09:20 +0000

    1ef6205a62 docs(core): correct description of actively supported versions (#49945)

--- bundles/chunk-EKN42M4J.js
+++ bundles/chunk-EKN42M4J.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.8+sha-6d02828";
+var NGCC_VERSION = "15.2.8+sha-1ef6205";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
