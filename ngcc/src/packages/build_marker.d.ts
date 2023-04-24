98d9a3daec62ac5d14f4ca3fc5ebbc007c1e565b
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:07:40 +0000

    c17b84a33b docs: applicability of NgModule providers (#49966)

--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.8+sha-898acfa";
+export declare const NGCC_VERSION = "15.2.8+sha-c17b84a";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
