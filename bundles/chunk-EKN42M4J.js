f9ee3f5d30f467d1407f10a0b7f9f0d0cde93a84
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:21 +0000

    bdf2aa1957 build: bump license year (#49984)

--- bundles/chunk-EKN42M4J.js
+++ bundles/chunk-EKN42M4J.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.8+sha-1ef6205";
+var NGCC_VERSION = "15.2.8+sha-bdf2aa1";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.8+sha-1ef6205");
+var VERSION = new Version("15.2.8+sha-bdf2aa1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
