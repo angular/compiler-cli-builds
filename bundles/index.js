8f1d23c1137e705e0f9c87157bc1bd8e16fbe60e
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:44 +0000

    f5dd3b4ede refactor(docs-infra): remove `semi` rule from eslintrc. (#49970)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-1090bf8");
+var VERSION = new Version("16.1.0-next.0+sha-f5dd3b4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
