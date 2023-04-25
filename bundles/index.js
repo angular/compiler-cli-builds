e733565d936fdcd8164ebd03396c1451edf10f5b
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:45:52 +0000

    fed4b9d6e9 docs: add v16 in version compatibility table (#49994)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-8683414");
+var VERSION = new Version("16.1.0-next.0+sha-fed4b9d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
