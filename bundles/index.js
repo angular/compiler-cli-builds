84c7de3ed4a5e2c7a6ee079b07bac9ccc88e1679
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:47:02 +0000

    9e0a651a5a build(elements): remove unecessary filegroup. (#49982)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.2+sha-022ed5a");
+var VERSION = new Version("16.0.0-rc.2+sha-9e0a651");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
