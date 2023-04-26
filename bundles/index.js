2d7fcb882a349755086d58ab559a542deeaa4f2b
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:42:46 +0000

    dd4fb18169 fix(docs-infra): add `work-break` to `a` tags in `.cli-option` (#50012)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.8+sha-bdf2aa1");
+var VERSION = new Version("15.2.8+sha-dd4fb18");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
