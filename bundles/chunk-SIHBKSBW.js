b1b617eeaccd775e15d4958199f395b4955a10a2
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 18:49:01 +0000

    2505018eaa refactor(core): Rename 'transplanted' view refresh flag and counters to be generic (#50000)

--- bundles/chunk-SIHBKSBW.js
+++ bundles/chunk-SIHBKSBW.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.2+sha-cc099e7");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0-rc.2+sha-2505018");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0-rc.2+sha-cc099e7") {
+    if (version === "16.0.0-rc.2+sha-2505018") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
