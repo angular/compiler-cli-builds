f535548121245883fee58751d14abb7b0dff2d87
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:42:16 +0000

    744f7ffb61 refactor(platform-server): avoid using `platformDynamicServer` when `ngJitMode` is disabled (#50007)

--- bundles/chunk-QS7CGVLD.js
+++ bundles/chunk-QS7CGVLD.js
@@ -842,7 +842,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-8a324c5");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-744f7ff");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -889,7 +889,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.0+sha-8a324c5") {
+    if (version === "16.1.0-next.0+sha-744f7ff") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
