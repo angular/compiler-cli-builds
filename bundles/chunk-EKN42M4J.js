cf5b1382a8d929c7dbcb541b3c2b5788fabe810c
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 18:40:19 +0000

    3599214526 build: update dependency patch-package to v7 (#50002)

--- bundles/chunk-EKN42M4J.js
+++ bundles/chunk-EKN42M4J.js
@@ -892,7 +892,7 @@ var ProgramBasedEntryPointFinder = class extends TracingEntryPointFinder {
 };
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/ngcc/src/packages/build_marker.mjs
-var NGCC_VERSION = "15.2.8+sha-dd4fb18";
+var NGCC_VERSION = "15.2.8+sha-3599214";
 function needsCleaning(packageJson) {
   return Object.values(packageJson.__processed_by_ivy_ngcc__ || {}).some((value) => value !== NGCC_VERSION);
 }
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.8+sha-dd4fb18";
+export declare const NGCC_VERSION = "15.2.8+sha-3599214";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
