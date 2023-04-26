9cec7d57f437060a460bc7d674a9c927923e2f02
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 19:50:47 +0000

    7eb7c69580 build: update io_bazel_rules_sass digest to 236d2fc (#50006)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-a8cf596");
+var VERSION = new Version("16.1.0-next.0+sha-7eb7c69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
