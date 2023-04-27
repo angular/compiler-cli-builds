885f171cd14d352f6ea0eec11847c3a0dec681a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-27 15:36:59 +0000

    9107e931ca fix(common): fix incorrectly reported distortion for padded images (#49889)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.8+sha-1584b32");
+var VERSION = new Version("15.2.8+sha-9107e93");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
