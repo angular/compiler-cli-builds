9a06bc532ff179b8da692061618c34b5dc93f018
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 16:35:59 +0000

    bfa6e6ad23 build: update cross-repo angular dependencies (#50067)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-cfab3ad");
+var VERSION = new Version("16.1.0-next.0+sha-bfa6e6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
