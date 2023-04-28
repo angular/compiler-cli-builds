f5836edf233a9421292d698ec44a39c78d627d10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:52:15 +0000

    6dc3add921 docs(core): primary Angular Signals documentation (#50053)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.3+sha-d5fb3ad");
+var VERSION = new Version("16.0.0-rc.3+sha-6dc3add");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
