079d91eff62539750651b8c51e2d8a55cee52766
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 17:45:35 +0000

    6928618fd7 release: cut the v16.0.0-rc.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.3+sha-952569c");
+var VERSION = new Version("16.0.0-rc.4+sha-6928618");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
