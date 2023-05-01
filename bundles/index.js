7c0b0deb7382f300e6ef79cc249fa19dde5bf900
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:55:03 +0000

    109eb88c6a docs: add v16 update guide (#50073)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-08b664d");
+var VERSION = new Version("16.1.0-next.0+sha-109eb88");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
