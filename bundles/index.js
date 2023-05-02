60fb586e8df611c7f5266f9fe603f965b065b1d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 14:30:36 +0000

    37443d8680 test: update payload golden for AIO to reflect reduction in size (#50107)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.4+sha-8080bf0");
+var VERSION = new Version("16.0.0-rc.4+sha-37443d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
