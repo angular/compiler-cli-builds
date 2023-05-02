df2751f1c7e1f58a4bb6061349aa162599dc3780
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 11:34:47 +0000

    8b34482e3d docs: Make links out of @see tags (#50098)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.8+sha-eb048fd");
+var VERSION = new Version("15.2.8+sha-8b34482");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- ngcc/src/packages/build_marker.d.ts
+++ ngcc/src/packages/build_marker.d.ts
@@ -8,7 +8,7 @@
 import { AbsoluteFsPath } from '../../../src/ngtsc/file_system';
 import { PackageJsonUpdater } from '../writing/package_json_updater';
 import { EntryPointPackageJson, PackageJsonFormatProperties } from './entry_point';
-export declare const NGCC_VERSION = "15.2.8+sha-eb048fd";
+export declare const NGCC_VERSION = "15.2.8+sha-8b34482";
 /**
  * Returns true if there is a format in this entry-point that was compiled with an outdated version
  * of ngcc.
