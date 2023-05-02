a2bd0425e2b9659df3379d1b54dad7ce28b2f53a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:16:44 +0000

    c68545e082 docs: cleanup team page (#50044)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-ee180a9");
+var VERSION = new Version("16.1.0-next.0+sha-c68545e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
