c7c0f732672ef7979b08378693622d70960999d6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:09:56 +0000

    ee180a9974 ci: Update pullapprove availability (#50070)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-90192c8");
+var VERSION = new Version("16.1.0-next.0+sha-ee180a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
