622fc2ae8c076c6b81d9ba0669141f31e268b542
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-03 08:57:33 +0000

    b381ad7d8a docs(docs-infra): Remove unused annotation template (#50114)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0-rc.4+sha-796273d");
+var VERSION = new Version("16.0.0-rc.4+sha-b381ad7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
