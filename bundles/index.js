ae08045d8299c9618c85862ebe1e6b8c7f797096
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-03 08:50:33 +0000

    2f360d51c9 build: update all non-major dependencies (#50100)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-47c093a");
+var VERSION = new Version("16.1.0-next.0+sha-2f360d5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
