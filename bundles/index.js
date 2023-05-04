cbdac85e1bf1987508b4f2cd482d3f570d443494
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:26:32 +0000

    adf3f6af88 docs: fix `takeUntilDestroyed` description's typo (#50134) PR Close #50134

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-ec968a9");
+var VERSION = new Version("16.1.0-next.0+sha-adf3f6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
