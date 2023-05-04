dd5880ee2be67eeabd971692048c7a7e0738f942
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 10:40:44 +0000

    2f03eefd5c refactor(platform-browser): Throw tree-shakable errors. (#49341)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0+sha-f28580a");
+var VERSION = new Version("16.0.0+sha-2f03eef");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
