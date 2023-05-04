bac4cff58cacc06778104a4046f90ad670c8606e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 06:49:45 +0000

    3c4b30711a docs: fix roadmap structure and remove redundant title (#50137)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-b373008");
+var VERSION = new Version("16.1.0-next.0+sha-3c4b307");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
