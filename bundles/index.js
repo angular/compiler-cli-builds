4857f64932b851dfcc5edaa06462e6cc937e9c8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:51:47 +0000

    e60b3d45b9 build: remove unused deps (#50116)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-f780d6f");
+var VERSION = new Version("16.1.0-next.0+sha-e60b3d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
