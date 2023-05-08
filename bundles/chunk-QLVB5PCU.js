10422c68d78ceed4f735c03676a1304d75b9bd9e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 17:04:32 +0000

    da7ff37dc9 Revert "docs(docs-infra): Remove unused annotation template (#50114)" (#50206)

--- bundles/chunk-QLVB5PCU.js
+++ bundles/chunk-QLVB5PCU.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0+sha-2f03eef");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.0+sha-da7ff37");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.0+sha-2f03eef") {
+    if (version === "16.0.0+sha-da7ff37") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.0+sha-2f03eef");
+var VERSION = new Version("16.0.0+sha-da7ff37");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
