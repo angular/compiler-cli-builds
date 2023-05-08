8d1d661afa701046a95dda150f9b0ef7897e9bc0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:52:14 +0000

    59837f4749 refactor(platform-server): import `xhr2` dynamically in the ServerXhr class (#50095)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-e60b3d4");
+var VERSION = new Version("16.1.0-next.0+sha-59837f4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
