c91b216d8cd638aa5d6e5425de9a1cb400d0caf6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 21:56:51 +0000

    dccfcb7a40 test(zone.js): update zone.js test for jasmine upgrade (#49914)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-478c70c");
+var VERSION = new Version("16.1.0-next.0+sha-dccfcb7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
