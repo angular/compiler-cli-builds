0e8258a3d401bbd6c69ef7766a9a4d2ea741e790
Andrew Kushnir <akushnir@google.com>
2023-05-10 17:45:36 +0000

    6945aff667 docs: fix typo on hydration in Preserve Whitespaces Configuration (#50236) PR Close #50236

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-e4ad067");
+var VERSION = new Version("16.1.0-next.0+sha-6945aff");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
