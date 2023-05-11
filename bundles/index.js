c84923b29f77656d617f7b6fc2518c597a20e1a5
Andrew Kushnir <akushnir@google.com>
2023-05-11 21:47:46 +0000

    4a7ebf4037 build: lock file maintenance (#49879)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-d5be23e");
+var VERSION = new Version("16.0.1+sha-4a7ebf4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
