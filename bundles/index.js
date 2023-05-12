deb31bfa373f3e2404ccaf2ede2b8310a865785a
Andrew Kushnir <akushnir@google.com>
2023-05-12 17:43:44 +0000

    21199f8086 fix(docs-infra): labels with links should have the same font weight (#50258)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-4a7ebf4");
+var VERSION = new Version("16.0.1+sha-21199f8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
