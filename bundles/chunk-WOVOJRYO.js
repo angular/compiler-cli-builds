25e65ad2cbe81e91ca3b7318c85bb359ecff0208
Andrew Kushnir <akushnir@google.com>
2023-05-15 17:58:24 +0000

    b65576af74 docs: add Enea to GDE contributors list (#50254)

--- bundles/chunk-WOVOJRYO.js
+++ bundles/chunk-WOVOJRYO.js
@@ -842,7 +842,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-a09f889");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-b65576a");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -889,7 +889,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.0+sha-a09f889") {
+    if (version === "16.1.0-next.0+sha-b65576a") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
