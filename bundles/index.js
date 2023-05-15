e55c43666bafc3c00b4613ae96a9de77202ea007
Andrew Kushnir <akushnir@google.com>
2023-05-15 17:11:12 +0000

    2d87aeecc1 docs: Glossary link to N (#50294)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-37acd37");
+var VERSION = new Version("16.0.1+sha-2d87aee");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
