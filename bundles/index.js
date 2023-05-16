71158a567b711be392e3ff48a93930c76437bc35
Andrew Kushnir <akushnir@google.com>
2023-05-16 20:48:06 +0000

    3eafb4b470 Revert "fix(core): add additional component metadata to component ID generation (#50203)" (#50335)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-b75625b");
+var VERSION = new Version("16.0.1+sha-3eafb4b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
