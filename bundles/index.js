e41e59821dedc124647be95243197cfb7ef0ab96
Andrew Kushnir <akushnir@google.com>
2023-05-16 17:02:32 +0000

    b75625ba49 docs: fix typo in security guide (#50324) PR Close #50324

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-bd4f64d");
+var VERSION = new Version("16.0.1+sha-b75625b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
