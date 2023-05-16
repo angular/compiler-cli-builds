95b38e2b20c83946db7258cc0936718fbdf46f6c
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:38:40 +0000

    165b8b647c fix(core): allow passing value of any type to `isSignal` function (#50035)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-fab7f39");
+var VERSION = new Version("16.1.0-next.0+sha-165b8b6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
