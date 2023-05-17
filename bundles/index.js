2eb8aed48002f30d89c5d947425e6bbcc327cc9d
Andrew Kushnir <akushnir@google.com>
2023-05-17 15:31:45 +0000

    bb4875682d docs: remove plnkr link from markForCheck example (#50328)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-c69ef4e");
+var VERSION = new Version("16.1.0-next.0+sha-bb48756");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
