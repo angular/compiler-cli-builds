cf42e9bc63ea5fb19d3926d2c9e4e2c03ad8f161
Andrew Kushnir <akushnir@google.com>
2023-05-17 16:03:05 +0000

    5bf1abffea refactor(core): Add a warning when `ApplicationRef.isStable` doesn't emit `true` (#50295)

--- bundles/chunk-2KHZXNEI.js
+++ bundles/chunk-2KHZXNEI.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.1+sha-656c953");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.1+sha-5bf1abf");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.1+sha-656c953") {
+    if (version === "16.0.1+sha-5bf1abf") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-656c953");
+var VERSION = new Version("16.0.1+sha-5bf1abf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
