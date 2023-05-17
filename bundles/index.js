b0ee8e8917e7d83ad9ca23fd24d8da5687d94ec2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-05-17 18:44:25 +0000

    d3eda34650 docs: release notes for the v16.0.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-c0ebe34");
+var VERSION = new Version("16.1.0-next.0+sha-d3eda34");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
