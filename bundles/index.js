5859ee941ae60170581d9f46a9056d599f0d3aa1
Andrew Kushnir <akushnir@google.com>
2023-05-17 16:46:33 +0000

    c1016d4e57 fix(core): add additional component metadata to component ID generation (#50340)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.1+sha-5bf1abf");
+var VERSION = new Version("16.0.1+sha-c1016d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
