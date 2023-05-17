f77900097f53b815095a6e5ffaab929b88e34c39
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-05-17 18:54:02 +0000

    70099a393c release: cut the v16.1.0-next.1 release

--- bundles/chunk-WOVOJRYO.js
+++ bundles/chunk-WOVOJRYO.js
@@ -842,7 +842,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.0+sha-d3eda34");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.1+sha-70099a3");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -889,7 +889,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.0+sha-d3eda34") {
+    if (version === "16.1.0-next.1+sha-70099a3") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.0+sha-d3eda34");
+var VERSION = new Version("16.1.0-next.1+sha-70099a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
