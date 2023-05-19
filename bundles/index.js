1e0eeb865ff929a3111d580feed6debd15b56ece
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:35:46 +0000

    00b4924bad docs: remove unused oninit method (#50353)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-b68306e");
+var VERSION = new Version("16.1.0-next.1+sha-00b4924");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
