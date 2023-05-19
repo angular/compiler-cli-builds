6d7272072e6ae4ed1d6f70e13c3c3a22967472db
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 16:55:57 +0000

    d963ecdf50 docs: added clarity in lifecycle event docs (#50145)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.2+sha-14588ab");
+var VERSION = new Version("16.0.2+sha-d963ecd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
