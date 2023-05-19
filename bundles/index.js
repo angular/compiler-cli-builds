13a137b0aa15f4151b9fdec3eba539f2efb8a28d
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 15:27:32 +0000

    24d803b0a2 docs: forwardref usage for standalone components (#50366)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-8b44ba3");
+var VERSION = new Version("16.1.0-next.1+sha-24d803b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
