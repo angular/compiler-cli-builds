08a7598f4c661d76fc674a4418cbae257fbdeceb
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 16:41:31 +0000

    d097844da9 docs: replace blossom emoji with tulip (#50349)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-24d803b");
+var VERSION = new Version("16.1.0-next.1+sha-d097844");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
