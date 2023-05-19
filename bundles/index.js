db59b35327ac04b09dcdce99bd9bd956a6634d4b
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:36:21 +0000

    46128feec2 docs(language-service): correction of the demonstration translation (#50359)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-3239160");
+var VERSION = new Version("16.1.0-next.1+sha-46128fe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
