17e63c4e6a8dde1c70faa379bc47305f6651ee8d
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 15:26:11 +0000

    8b44ba3170 fix(core): host directives incorrectly validating aliased bindings (#50364)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-46128fe");
+var VERSION = new Version("16.1.0-next.1+sha-8b44ba3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
