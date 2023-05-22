b0f5823689bab24683a0c9f91dd07c602e1044aa
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 17:51:11 +0000

    f40ecc025b docs: replaced the html tags with the updated ones (#50403)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.2+sha-5574e47");
+var VERSION = new Version("16.0.2+sha-f40ecc0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
