dda8a0f42dda8b426ae921e49cd8d714080871b4
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 18:38:32 +0000

    6ced5c8779 Revert "build: update babel dependencies to v7.21.9 (#50416)" (#50433)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-4dc528d");
+var VERSION = new Version("16.1.0-next.1+sha-6ced5c8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
