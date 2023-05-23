aceb7257dc6a8a506b44b921fff6185e82a315f9
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 15:48:38 +0000

    9054f95c6a refactor(core): Improve NG100 (#50391)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-c464b01");
+var VERSION = new Version("16.1.0-next.1+sha-9054f95");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
