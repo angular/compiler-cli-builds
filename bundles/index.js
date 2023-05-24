55f89ea546927b3817a99886ed023b0dbad4809b
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 14:18:36 +0000

    75fdbcb8f2 fix(core): fix `Self` flag inside embedded views with custom injectors (#50270)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-5fd4bf6");
+var VERSION = new Version("16.1.0-next.1+sha-75fdbcb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
