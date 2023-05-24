f876eec8a178df7087d2f85253e2cf43a4eab453
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 14:06:52 +0000

    24f7c1dc61 refactor(devtools): run spellcheck on devtools dir (#50445)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-4c01107");
+var VERSION = new Version("16.1.0-next.1+sha-24f7c1d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
