6bbcce1a1998b77bdc267966e863670d38ba6ed1
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-05-24 17:53:32 +0000

    c0a8c8fb4b release: cut the v16.0.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.2+sha-080bbd2");
+var VERSION = new Version("16.0.3+sha-c0a8c8f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
