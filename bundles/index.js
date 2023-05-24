e7796ea0a9812f95d7df4fd39f25e01aa1a18119
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 15:51:08 +0000

    2cdb4c5911 fix(http): create macrotask during request handling instead of load start (#50406)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.1+sha-75fdbcb");
+var VERSION = new Version("16.1.0-next.1+sha-2cdb4c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
