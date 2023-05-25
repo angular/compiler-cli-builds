0b611e8ccbc2a3dc2e0c4ba9e93841d50573ed36
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:54:25 +0000

    258c773188 docs: remove unused imports and improved the code (#50424)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.2+sha-06b498f");
+var VERSION = new Version("16.1.0-next.2+sha-258c773");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
