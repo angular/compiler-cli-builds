faa725962e19de39a33a511167d1f4cc276aa095
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:57:08 +0000

    fc6b10cb15 build: update dependency https-proxy-agent to v7 (#50456)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.3+sha-c0a8c8f");
+var VERSION = new Version("16.0.3+sha-fc6b10c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
