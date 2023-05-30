f194fefcf8aa7caf779fe9caf6c09ba61d9bcc08
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 16:31:01 +0000

    62e76d4d20 docs: update CLI command to include `--preserve-symlinks-main` (#50512)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.2+sha-6f5dabe");
+var VERSION = new Version("16.1.0-next.2+sha-62e76d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
