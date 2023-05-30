68a6b85f73592048c20a392e8538c619457737ef
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:23:56 +0000

    f26c9a2251 refactor(platform-browser): Remove BrowserDetection (#50411)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.3+sha-f517d18");
+var VERSION = new Version("16.0.3+sha-f26c9a2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
