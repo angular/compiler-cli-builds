19449c568a9790d93439d1d2c7a5ec44463ae159
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:56:15 +0000

    9e68a62512 docs: correct Signals documentation (#50518)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.3+sha-55244e6");
+var VERSION = new Version("16.0.3+sha-9e68a62");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
