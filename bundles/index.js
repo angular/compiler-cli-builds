7b2e8aa49a6a2b49a8327eba6ffbb7e3ef9f400c
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 17:13:04 +0000

    55244e6759 Revert "fix(zone.js): enable monkey patching of the `queueMicrotask()` API in node.js (#50467)" (#50529)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.3+sha-277a473");
+var VERSION = new Version("16.0.3+sha-55244e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
