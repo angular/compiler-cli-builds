c2935e7c91d43fd3f3b9fe7904931e72f8455659
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:03:19 +0000

    703b8fcac1 fix(compiler): do not remove comments in component styles (#50346)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.3+sha-9e68a62");
+var VERSION = new Version("16.0.4+sha-703b8fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
