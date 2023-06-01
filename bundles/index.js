90377cabd55053323fb9e8e7d37feb91eedd0e62
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 15:59:08 +0000

    019c2d2f14 docs: selector precisions (#50534)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.3+sha-84b6ef0");
+var VERSION = new Version("16.1.0-next.3+sha-019c2d2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
