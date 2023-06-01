e637864fa8a82d69adf4eb8fe4a6263b9f1f172b
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:02:48 +0000

    9847085448 refactor(compiler): support property interpolation in the template pipeline (#50118)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.3+sha-57d47b3");
+var VERSION = new Version("16.1.0-next.3+sha-9847085");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
