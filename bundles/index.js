3e027468c80ac627e8077ba731b480ba9e3ed728
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-02 21:16:23 +0000

    cf15c290f0 build: update to TypeScript 5.1 final (#50548)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.3+sha-540e643");
+var VERSION = new Version("16.1.0-next.3+sha-cf15c29");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
