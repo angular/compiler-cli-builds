29ae49c51d7c24b3a657cd333b3daecd1e2dca89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-02 21:24:01 +0000

    9d7991a216 fix(zone.js): patch entire promise in node (#50552)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.4+sha-703b8fc");
+var VERSION = new Version("16.0.4+sha-9d7991a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
