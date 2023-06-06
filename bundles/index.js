da8b6a7a52a9218b1680b5ce9cd4e733a5b75002
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-06 12:41:15 +0000

    21eb5ee840 fix(docs-infra): replace use of turbo on StackBlitz with npm (#50576)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("15.2.9+sha-2d68de5");
+var VERSION = new Version("15.2.9+sha-21eb5ee");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
