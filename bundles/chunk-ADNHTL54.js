62eb87c075e70a4487ed4f02063f36a4da4857fd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-06 12:29:25 +0000

    34989fda7a fix(docs-infra): replace use of turbo on StackBlitz with npm (#50576)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.3+sha-cb31dbc");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-next.3+sha-34989fd");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-next.3+sha-cb31dbc") {
+    if (version === "16.1.0-next.3+sha-34989fd") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
