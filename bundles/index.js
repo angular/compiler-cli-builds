b54ac39f7bf4bce757e8ace4ebad5c6abbb52d99
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 19:59:10 +0000

    0875b519b9 fix(platform-server): surface errors during rendering (#50587)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.3+sha-b3d2295");
+var VERSION = new Version("16.1.0-next.3+sha-0875b51");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
