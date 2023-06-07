7b39dca7dc59c6c0179a2be8626c6d45bc93c4bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:00:52 +0000

    3044822fc2 build: update dependency gulp-conventional-changelog to v3 (#50598)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-next.3+sha-0875b51");
+var VERSION = new Version("16.1.0-next.3+sha-3044822");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
