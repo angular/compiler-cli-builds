65022d83367f18b03eab579839c5eaecc982073a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-09 15:56:45 +0000

    a163d35c31 docs: removed unused methods (#50631)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-a1973a4");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-a163d35");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.0+sha-a1973a4") {
+    if (version === "16.2.0-next.0+sha-a163d35") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-a1973a4");
+var VERSION = new Version("16.2.0-next.0+sha-a163d35");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
