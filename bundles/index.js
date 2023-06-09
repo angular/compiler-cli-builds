938535701e60a05b7613849e0994fd8e20010b99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-09 15:56:51 +0000

    89c6592986 docs: removed unused methods (#50631)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.5+sha-992ce2d");
+var VERSION = new Version("16.0.5+sha-89c6592");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
