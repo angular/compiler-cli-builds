d95badcdbe40bd22b323b7986daea667acb1fb93
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 13:45:27 +0000

    d5a8ff39b9 build: Update Glob to fast-glob to remove Inflight due to memory leak (#50632)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-7166ed1");
+var VERSION = new Version("16.2.0-next.0+sha-d5a8ff3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
