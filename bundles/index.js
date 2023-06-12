a45a3f437e2872ab390fa2d4ebca1cd4390c728a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:35:26 +0000

    9860acecf0 docs(packaging): Clarify the necessity of dev-dependencies (#50619)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-0370905");
+var VERSION = new Version("16.2.0-next.0+sha-9860ace");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
