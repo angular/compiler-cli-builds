bb10332330c8a6ff9be43ea0d13609554e7e2f78
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:55:32 +0000

    33db5f934c docs: removed duplicated words (#50648)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.5+sha-080d62a");
+var VERSION = new Version("16.0.5+sha-33db5f9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
