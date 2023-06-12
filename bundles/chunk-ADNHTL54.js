1336e3d9e175799b80a1e5fa992f2af1cd14ee12
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:53:19 +0000

    8234738120 docs: removed duplicated words (#50648)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-rc.0+sha-2d66e93");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-rc.0+sha-8234738");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-rc.0+sha-2d66e93") {
+    if (version === "16.1.0-rc.0+sha-8234738") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
