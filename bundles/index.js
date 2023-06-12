a7ed4b54ded0f3e7825aa394ff7c098f351993b4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 17:32:46 +0000

    57f9efe200 docs: update schematic authoring guide to use new utilities (#50667)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.0-rc.0+sha-ecbdb24");
+var VERSION = new Version("16.1.0-rc.0+sha-57f9efe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
