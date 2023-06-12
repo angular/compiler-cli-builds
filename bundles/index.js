35bb9c814eea6f6f7837cce1b60bb4012995b8d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:54:16 +0000

    89a089bdeb docs: add missing navigation item (#50113)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.5+sha-8e9f920");
+var VERSION = new Version("16.0.5+sha-89a089b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
