739e9736c5ad04b959d4388feabe2d7147a4a269
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:39:04 +0000

    f7f15ae2a9 docs: added title attribute to the iframe tag (#50569)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-9860ace");
+var VERSION = new Version("16.2.0-next.0+sha-f7f15ae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
