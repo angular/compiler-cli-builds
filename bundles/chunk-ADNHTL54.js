cd3cbdca0317b978d1ba75ab56d6ef01ff65234a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 12:59:30 +0000

    fa3b7332a9 build: update actions/checkout action to v3.5.3 (#50689)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-rc.0+sha-b90f2b1");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0-rc.0+sha-fa3b733");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0-rc.0+sha-b90f2b1") {
+    if (version === "16.1.0-rc.0+sha-fa3b733") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
