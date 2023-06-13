d221c19dd51079308e1d34fa8ce0555b09756a65
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 15:14:32 +0000

    f1e350cb59 release: cut the v16.0.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.0.5+sha-c58bc97");
+var VERSION = new Version("16.0.6+sha-f1e350c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
