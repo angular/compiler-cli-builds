2fd817fb52544dda4bd575d1d2662398033b83db
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:31:52 +0000

    05ac0868c9 fix(core): avoid duplicated content during hydration while processing a component with i18n (#50644)

--- bundles/chunk-2KHZXNEI.js
+++ bundles/chunk-2KHZXNEI.js
@@ -841,7 +841,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.0.5+sha-50e1858");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.0.5+sha-05ac086");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -888,7 +888,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.0.5+sha-50e1858") {
+    if (version === "16.0.5+sha-05ac086") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
