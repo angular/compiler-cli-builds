8a100a6feec7661210a4acd946ffa3efdb6e4d3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 09:51:36 +0000

    ec9ee9e5bf docs: fix "After you finish" description of first-app-lesson-05.md (#50645)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-1400e55");
+var VERSION = new Version("16.2.0-next.0+sha-ec9ee9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
