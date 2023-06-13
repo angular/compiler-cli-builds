3ecc96309ba37143f79ea4e6f6152550a80f8d3e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:24:46 +0000

    ee073cdfae docs: update an example in the RouterEvent description (#50526)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-ad82f3a");
+var VERSION = new Version("16.2.0-next.0+sha-ee073cd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
