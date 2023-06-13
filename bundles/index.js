1dc2c70dc9068cae2eb0a901622866235faded00
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 15:24:05 +0000

    01d45df6d9 docs: release notes for the v16.1.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-2782fc2");
+var VERSION = new Version("16.2.0-next.0+sha-01d45df");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
