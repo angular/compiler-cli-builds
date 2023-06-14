96ff9e4697ffb3bc19b78e026964082e241c3604
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 14:56:27 +0000

    71360b3a3e fix(compiler-cli): libraries compiled with v16.1+ breaking with Angular framework v16.0.x (#50715)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0+sha-5e1d844");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0+sha-71360b3");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0+sha-5e1d844") {
+    if (version === "16.1.0+sha-71360b3") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
