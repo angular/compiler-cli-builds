5b1c6161b786087c9089e584ec6cf84e94bab7b0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 13:31:59 +0000

    6883a7ebf9 docs: update Angular CLI help [main] (#50711)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-8d42747");
+var VERSION = new Version("16.2.0-next.0+sha-6883a7e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
