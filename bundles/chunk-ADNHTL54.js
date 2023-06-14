e3dc286a7c9e1574ffc5c70a25597d927fa393c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 13:46:31 +0000

    0b14e4ef74 feat(router): exposes the `fixture` of the `RouterTestingHarness` (#50280)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-5dc7a99");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-0b14e4e");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.0+sha-5dc7a99") {
+    if (version === "16.2.0-next.0+sha-0b14e4e") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
