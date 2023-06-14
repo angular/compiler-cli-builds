a532c1824669ba1322d1b6b28d986ab241f1315b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 10:53:32 +0000

    9ff5572b9f refactor(core): handle #24571 todos. (#49221)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0+sha-0a72df6");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.0+sha-9ff5572");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.0+sha-0a72df6") {
+    if (version === "16.1.0+sha-9ff5572") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
