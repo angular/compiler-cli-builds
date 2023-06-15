6aa62067eaa9c48aa9ba4a08104877969b03c899
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-15 13:45:07 +0000

    1d3e96b7d9 test: mark ngtsc test target as flaky (#50718)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.1+sha-fd519b0");
+var VERSION = new Version("16.1.1+sha-1d3e96b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
