78a2b994f7da07afa2b69cd0743afae759cd1dc1
Andrew Kushnir <akushnir@google.com>
2023-06-20 20:50:06 +0000

    a126cbcf22 fix(http): use serializeBody to support JSON payload in FetchBackend (#50776) `HttpRequest.serializeBody` was used in HttpXhrBackend. `fetch` also needs to serialize request body.

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-64745a8");
+var VERSION = new Version("16.2.0-next.0+sha-a126cbc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
