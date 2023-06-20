a7ec35a093842b742fb52f5150e2d1a0f15e1a2e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 15:31:23 +0000

    f77bd0a2bd refactor: fix lint warning in compiler code by adding explicit `override` (#50772)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-a88ae41");
+var VERSION = new Version("16.2.0-next.0+sha-f77bd0a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
