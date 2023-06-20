1305c8676b156d4bd1ccc9787b926d6074b09374
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 16:15:44 +0000

    78ece3149e build: speed up remote execution by allowing more concurrent jobs (#50734)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.1+sha-60b160f");
+var VERSION = new Version("16.1.1+sha-78ece31");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
