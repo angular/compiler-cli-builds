575830d420b96635c3902f6345cbbd57218626f2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-06-21 18:45:51 +0000

    5c3407507a release: cut the v16.2.0-next.0 release

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-a99b6ff");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-5c34075");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.0+sha-a99b6ff") {
+    if (version === "16.2.0-next.0+sha-5c34075") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
