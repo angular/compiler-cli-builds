0dd298b38ed35c53d2a86917129d721e79a8febd
Andrew Kushnir <akushnir@google.com>
2023-06-21 19:03:50 +0000

    7d7fc48e58 refactor(core): tweak runtime deps tracker to accommodate JIT requirements. (#50606)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-4596577");
+var VERSION = new Version("16.2.0-next.0+sha-7d7fc48");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
