48103aa165cc78f5efcc2a28606eb791f89f4873
Andrew Kushnir <akushnir@google.com>
2023-06-21 19:10:27 +0000

    764c7fbd4e refactor(core): fix typo `Destory` -> `Destroy` (#50676)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-7d7fc48");
+var VERSION = new Version("16.2.0-next.0+sha-764c7fb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
