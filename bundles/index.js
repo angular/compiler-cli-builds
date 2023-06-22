12c575b607a9603575dcca464b242bb451b32c0c
Andrew Kushnir <akushnir@google.com>
2023-06-22 16:57:03 +0000

    a118e571b4 docs: fix typos in signals guide (#50789) PR Close #50789

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-66824b7");
+var VERSION = new Version("16.2.0-next.0+sha-a118e57");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
