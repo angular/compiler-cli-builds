0e894d4e84f5f995640447ea9a4bea380ee460c3
Andrew Kushnir <akushnir@google.com>
2023-06-22 16:51:08 +0000

    66824b7715 refactor: removed the unused declaration in the import statement (#50616)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-ba0ca42");
+var VERSION = new Version("16.2.0-next.0+sha-66824b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
