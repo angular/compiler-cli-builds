a920d655cedf8dc3eb5187f304c025b326a3338e
Andrew Kushnir <akushnir@google.com>
2023-06-23 21:29:06 +0000

    bdd8813451 build: print benchmark results at the end of `yarn benchmarks run` invocation (#50816)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-0ef4914");
+var VERSION = new Version("16.2.0-next.0+sha-bdd8813");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
