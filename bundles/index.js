bc57ef147dcd8b943dc74388c7dc20c763c60096
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:57:42 +0000

    232a78630e ci: re-enable RBE for http tests (#50741)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-e93708b");
+var VERSION = new Version("16.2.0-next.0+sha-232a786");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
