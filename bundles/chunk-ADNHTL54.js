8919fd4b9e9d497ed23de179a6be48120328541f
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:57:45 +0000

    0ef49141ea docs: invert logic for disabled in getting started docs (#50822)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-e93708b");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.0+sha-0ef4914");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.0+sha-e93708b") {
+    if (version === "16.2.0-next.0+sha-0ef4914") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
