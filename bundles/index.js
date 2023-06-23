aba112c05ad082c39f6da8c26625ef73ccb86283
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:46:42 +0000

    e93708b864 docs: update bio for devversion (#50831)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-9f2fb7c");
+var VERSION = new Version("16.2.0-next.0+sha-e93708b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
