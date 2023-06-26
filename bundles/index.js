40592fd98ec85d7e249345032623b200dc5ea6a2
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:50:24 +0000

    b09119b14b docs: Updating index.md to remove alert message which no longer applicable after version 15 (#50832)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-e699f1a");
+var VERSION = new Version("16.2.0-next.0+sha-b09119b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
