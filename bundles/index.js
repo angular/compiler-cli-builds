60449aae1e9d3a45deb3d63d705bd04f09082abe
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:14:34 +0000

    fb0a842ace build: update eslint dependencies to v5.60.1 (#50859)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-ad1265e");
+var VERSION = new Version("16.2.0-next.0+sha-fb0a842");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
