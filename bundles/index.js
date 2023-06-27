505128b5727f9df10fe6bf6e9c417bb4f6d70087
Andrew Kushnir <akushnir@google.com>
2023-06-27 13:41:43 +0000

    52f1dd9aaa refactor(animations): remove redundant castings (#50860)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.2+sha-ec1f6e3");
+var VERSION = new Version("16.1.2+sha-52f1dd9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
