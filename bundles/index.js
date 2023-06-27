27c6f00d1e4427f6d4ab4d36e93ff5d5a1d2c7cb
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:12:52 +0000

    77f0a2209a build: update all non-major dependencies (#50826)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-7aa913e");
+var VERSION = new Version("16.2.0-next.0+sha-77f0a22");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
