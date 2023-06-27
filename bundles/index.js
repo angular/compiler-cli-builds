1c31ba682771120aed3e46b0bf51c054ae8dae57
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:07:52 +0000

    7aa913e512 build: update actions/checkout digest to c85c95e (#50640)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.0+sha-0522f1b");
+var VERSION = new Version("16.2.0-next.0+sha-7aa913e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
