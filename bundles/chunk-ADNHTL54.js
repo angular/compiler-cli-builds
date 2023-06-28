c219892f7132ae1da939a896bc87f4297c16203a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-28 20:53:00 +0000

    4073cc04b3 build: lock file maintenance (#50877)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.1.3+sha-d9d70f0");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.1.3+sha-4073cc0");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.1.3+sha-d9d70f0") {
+    if (version === "16.1.3+sha-4073cc0") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
