567f900eff8922796cafdbf15ae82f378c92a333
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:51:42 +0000

    a19a87df6c fix(upgrade): allow for downgraded components to work with component-router (#50871)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-57c9399");
+var VERSION = new Version("16.2.0-next.1+sha-a19a87d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
