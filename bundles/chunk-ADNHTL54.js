a3333e8a0c6b165db92545d846e73112c9b25192
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:47:29 +0000

    57c9399ca6 refactor(compiler): save/restore view when listeners read references (#50834)

--- bundles/chunk-ADNHTL54.js
+++ bundles/chunk-ADNHTL54.js
@@ -852,7 +852,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-b66a16e");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-57c9399");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -899,7 +899,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.1+sha-b66a16e") {
+    if (version === "16.2.0-next.1+sha-57c9399") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
