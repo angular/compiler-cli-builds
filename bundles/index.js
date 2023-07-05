d28ff4875788d1b503540296f7e41556bfca9b09
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:14:48 +0000

    5d11844a7c docs: update link to the TS config. (#50933)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-941659f");
+var VERSION = new Version("16.2.0-next.1+sha-5d11844");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
