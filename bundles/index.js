e5e90c60dcb5d19cc88fb1f3bf26d7124a60e33f
Joey Perrott <josephperrott@gmail.com>
2023-07-11 19:14:31 +0000

    52749db653 ci: use frozen-lockfile in CI (#51012)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.4+sha-f31141c");
+var VERSION = new Version("16.1.4+sha-52749db");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
