caa6101089cb0e19e0738403c532f15cbdc5b2e7
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 17:27:16 +0000

    31419f6a3b perf(platform-browser): do not remove renderer from cache when `REMOVE_STYLES_ON_COMPONENT_DESTROY` is enabled. (#51005)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.4+sha-de01f75");
+var VERSION = new Version("16.1.4+sha-31419f6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
