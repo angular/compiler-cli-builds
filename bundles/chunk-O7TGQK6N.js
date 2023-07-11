9be9eeba834a7e4973546c50d9640b4a0d9c2b7f
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:40:57 +0000

    5d6ec0336b fix(core): handle `deref` returning `null` on `RefactiveNode`. (#50992)

--- bundles/chunk-O7TGQK6N.js
+++ bundles/chunk-O7TGQK6N.js
@@ -853,7 +853,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-253d756");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-5d6ec03");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -900,7 +900,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.1+sha-253d756") {
+    if (version === "16.2.0-next.1+sha-5d6ec03") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
