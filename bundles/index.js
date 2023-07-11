e0fe4552b660370efc3cd6dae362d494e3d483ad
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:41:54 +0000

    02cb0ba232 docs: fix typo in tutorial lesson 6 (#51002)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-16ce6e7");
+var VERSION = new Version("16.2.0-next.1+sha-02cb0ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
