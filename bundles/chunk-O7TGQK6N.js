16fdb5af9a360e444a7f16a27833d40ab3f4d80d
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:47:18 +0000

    1837efb9da feat(common): Allow ngSrc to be changed post-init (#50683) Remove thrown error when ngSrc is modified after an NgOptimizedImage image is initialized

--- bundles/chunk-O7TGQK6N.js
+++ bundles/chunk-O7TGQK6N.js
@@ -853,7 +853,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-a14bdfe");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-1837efb");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -900,7 +900,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.1+sha-a14bdfe") {
+    if (version === "16.2.0-next.1+sha-1837efb") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-a14bdfe");
+var VERSION = new Version("16.2.0-next.1+sha-1837efb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
