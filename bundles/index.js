03b8ab03d2a2158b37cd477ce0508268b454e913
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:45:09 +0000

    a14bdfe859 fix(animations): Ensure elements are removed from the cache after leave animation. (#50929)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-3dafc14");
+var VERSION = new Version("16.2.0-next.1+sha-a14bdfe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
