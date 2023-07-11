81a6eeea4f702ae2fa789ee62801823326dde131
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:48:13 +0000

    c27a1e61d6 feat(compiler): scope selectors in @scope queries (#50747)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-1837efb");
+var VERSION = new Version("16.2.0-next.1+sha-c27a1e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
