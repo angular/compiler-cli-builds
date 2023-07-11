da4244c66b90a966ca78500304afc746460303e4
Joey Perrott <josephperrott@gmail.com>
2023-07-11 18:14:33 +0000

    f31141c0d8 ci: remove GHA lint job from required statuses (#51011)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.4+sha-31419f6");
+var VERSION = new Version("16.1.4+sha-f31141c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
