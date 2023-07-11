72d5541ff0d9954ded4a03788cb72b91eda29a21
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:43:20 +0000

    1ec2aa902d docs: add missing jsdocs for static ngzone methods (#50924)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-5828eb9");
+var VERSION = new Version("16.2.0-next.1+sha-1ec2aa9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
