05897962370b8231083094fff529abde7148bf8e
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:45:09 +0000

    de01f75c21 refactor(core): Log a warning when multiple pipes match a name (#50389)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.4+sha-e35cc07");
+var VERSION = new Version("16.1.4+sha-de01f75");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
