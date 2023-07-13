83b61556546484256f76af8a5b33c0e807296378
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:48:36 +0000

    6cac41f039 build: update github/codeql-action action to v2.20.3 (#50930)

--- bundles/chunk-O7TGQK6N.js
+++ bundles/chunk-O7TGQK6N.js
@@ -853,7 +853,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-5bd530a");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.1+sha-6cac41f");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -900,7 +900,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.1+sha-5bd530a") {
+    if (version === "16.2.0-next.1+sha-6cac41f") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.1+sha-5bd530a");
+var VERSION = new Version("16.2.0-next.1+sha-6cac41f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
