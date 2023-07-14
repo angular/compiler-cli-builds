9bdc76b75867907485c21695c016ae40774e06e5
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 16:59:14 +0000

    8d636ad6a7 docs: updated the http tag with https (#50927)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-5a5bcd2");
+var VERSION = new Version("16.2.0-next.2+sha-8d636ad");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
