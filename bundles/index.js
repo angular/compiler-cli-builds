b236c3b9a4761dd496c8ebfc9dde8bbe7a5c1849
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 15:01:14 +0000

    743be79749 ci: migrate windows job to GHA (#51010)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-319aa76");
+var VERSION = new Version("16.2.0-next.2+sha-743be79");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
