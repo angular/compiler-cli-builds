ddadc76342b02845817378fbaf7c89e4bdee8073
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 15:03:03 +0000

    57ea8871a8 ci: mark devversion as unavailable for reviews (#51058)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-743be79");
+var VERSION = new Version("16.2.0-next.2+sha-57ea887");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
