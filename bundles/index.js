47cb24394c45aa63523984bf71e30d52a0cfd0d5
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:51:05 +0000

    ed1900eca5 refactor: add dylhunn to pullapprove (#51069)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.5+sha-3da03ca");
+var VERSION = new Version("16.1.5+sha-ed1900e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
