b2e6f45e52c677539c866227bb8e4376f2269f68
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:11:33 +0000

    0e8ca7bef5 docs: add privacy policy link at the bottom of the page (#51013)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-f1cb971");
+var VERSION = new Version("16.2.0-next.2+sha-0e8ca7b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
