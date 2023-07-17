fa74d9e3d1115e49918e86c4ca79462d6be1db92
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:18:00 +0000

    3da03cacae docs: add mention of paramsInheritanceStrategy in the router doc (#51051)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.5+sha-dea8dc0");
+var VERSION = new Version("16.1.5+sha-3da03ca");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
