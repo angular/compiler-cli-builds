9bdff8434301ded5e5514d407ccfa217e4a780e2
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:11:51 +0000

    b76088df9a build: update dependency lighthouse-logger to v2 (#51075)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-7f71d89");
+var VERSION = new Version("16.2.0-next.2+sha-b76088d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
