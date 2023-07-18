d9baa883c2dccf0f5791571fa845b7e373102125
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 21:16:12 +0000

    dd43615202 build: update all non-major dependencies (#50994)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-109737b");
+var VERSION = new Version("16.2.0-next.2+sha-dd43615");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
