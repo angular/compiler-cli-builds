e3b78238e52bb328299abeb3679fa1a62cac4bcf
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:19:17 +0000

    1ec3a2bc2b docs: fix typos and styling in lessons 7-14 (#51004)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-89c84bf");
+var VERSION = new Version("16.2.0-next.2+sha-1ec3a2b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
