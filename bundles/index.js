a64e9b0243be629a7b23fcab185a1fef43372e2e
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 17:23:05 +0000

    8fb9db9dad docs: Fix path to sample (#51076)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-24bf133");
+var VERSION = new Version("16.2.0-next.2+sha-8fb9db9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
