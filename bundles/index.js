6f81ecffcfbba27a962627a63e55d46bca071473
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 17:24:34 +0000

    ddf1769f2b docs(docs-infra): fix wrong links in first-app-lesson-13.md (#51094)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-8fb9db9");
+var VERSION = new Version("16.2.0-next.2+sha-ddf1769");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
