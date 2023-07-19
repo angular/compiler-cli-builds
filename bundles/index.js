325e7f09d1f355fe4f0df50019f51ed313514d3b
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-07-19 19:46:13 +0000

    05657cfde4 release: cut the v16.2.0-next.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.2+sha-16e132f");
+var VERSION = new Version("16.2.0-next.3+sha-05657cf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
