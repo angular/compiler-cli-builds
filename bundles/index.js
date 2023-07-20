6f6dd895a16b3dd2001a75048359e531e4e366d3
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-20 20:13:25 +0000

    47e51f61d4 docs(docs-infra): fix rendering of `Exported from` section on Firefox. (#51116)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.6+sha-46afa8a");
+var VERSION = new Version("16.1.6+sha-47e51f6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
