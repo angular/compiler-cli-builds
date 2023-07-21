65f5b55c4d641d1c3b483442e6fabf9f792cb59b
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:47:16 +0000

    4120a03805 docs: fix typos in first app tutorial (#51115) PR Close #51115

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.3+sha-0c9c825");
+var VERSION = new Version("16.2.0-next.3+sha-4120a03");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
