d6680d65062f0551d39f2a16cabc9e30c142d51a
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:04:35 +0000

    bfc6e482ac docs: Add links to the injection context guide (#51129)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.6+sha-c7f4298");
+var VERSION = new Version("16.1.6+sha-bfc6e48");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
