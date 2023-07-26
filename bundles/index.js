48efbddbf17a4ca212ba7385fdc31438c5ae5241
Andrew Kushnir <akushnir@google.com>
2023-07-26 18:30:56 +0000

    21299d95f5 fix(devtools): ensure that inspected component label is always in the viewport (#50656)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.7+sha-07969d1");
+var VERSION = new Version("16.1.7+sha-21299d9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
