70b7eda48af2288154125740f8cd4742d1196b8c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-07-26 16:17:31 +0000

    07969d1dc7 release: cut the v16.1.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.6+sha-03f9c93");
+var VERSION = new Version("16.1.7+sha-07969d1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
