d437ac864773b37a11c14e7befd89c277cd7cc8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-28 21:52:12 +0000

    cdaa2a8a9e feat(core): support Provider type in Injector.create (#49587)

--- bundles/chunk-DKWZY2MH.js
+++ bundles/chunk-DKWZY2MH.js
@@ -853,7 +853,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.4+sha-5061311");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0-next.4+sha-cdaa2a8");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -900,7 +900,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0-next.4+sha-5061311") {
+    if (version === "16.2.0-next.4+sha-cdaa2a8") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
