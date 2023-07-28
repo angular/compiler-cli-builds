e65dd6ac7d995e0ed2630b07707f041c269309f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-28 18:58:59 +0000

    50613117bd refactor(compiler): Ingest host attribute bindings in template pipeline (#51188)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.4+sha-1c553ee");
+var VERSION = new Version("16.2.0-next.4+sha-5061311");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
