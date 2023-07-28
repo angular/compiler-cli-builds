16aa7916e08dcaee098aae1b82bf196d5ec4cbad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-28 22:21:51 +0000

    c1052cf7a7 refactor(compiler): add support for sanitizing properties and attributes (#51156)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.4+sha-f71851a");
+var VERSION = new Version("16.2.0-next.4+sha-c1052cf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
