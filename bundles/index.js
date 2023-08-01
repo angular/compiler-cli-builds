fcf5db3f4389a70b963a9aff6df0f6901ef85f9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:37:22 +0000

    d86e637a75 docs: add lang attribute to html files (#51235)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.4+sha-388d1db");
+var VERSION = new Version("16.2.0-next.4+sha-d86e637");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
