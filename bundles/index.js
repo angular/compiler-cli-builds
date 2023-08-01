ae7cc783aa32ac284b7e33a23a0ddaf518f17d8a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 20:55:42 +0000

    2d52d5e62c refactor(compiler): support safe function calls (#51100)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.4+sha-8913d3e");
+var VERSION = new Version("16.2.0-next.4+sha-2d52d5e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
