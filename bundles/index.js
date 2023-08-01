8b3bd0ee1f12af07482e72a02453fb0d29717977
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 20:18:03 +0000

    8ffa85fb5d docs: fix warning about an unknown decorator (#51237)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.7+sha-7d579b9");
+var VERSION = new Version("16.1.7+sha-8ffa85f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
