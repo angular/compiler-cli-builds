aa73bc04865098b1e637e04c851d868da5d58e4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:08:18 +0000

    63b7f52008 docs: update the html tags (#51198)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-next.4+sha-71e803a");
+var VERSION = new Version("16.2.0-next.4+sha-63b7f52");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
