f105dea3b539f52785887e1eed1720fa41ef61e9
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 20:16:25 +0000

    e01549b26b refactor(core): introduce LView and LContainer utility functions (#51191)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0-rc.0+sha-cf62063");
+var VERSION = new Version("16.2.0-rc.0+sha-e01549b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
