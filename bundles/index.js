bc8149396fc7e8727d2a4564f8b2a1ccd86449cc
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:44:50 +0000

    4a8efbe287 refactor(animations): remove unecessary interface (#50662)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.1.8+sha-d4ffef2");
+var VERSION = new Version("16.1.8+sha-4a8efbe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
