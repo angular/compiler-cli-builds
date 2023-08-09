393578b05d065b2c3f28d6433e77c663900de630
Dylan Hunn <dylhunn@gmail.com>
2023-08-09 17:33:00 +0000

    dca6d6e0b8 refactor: add `dgp1130` to DevTools PullApprove config (#51301)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.3.0-next.0+sha-1eda1bd");
+var VERSION = new Version("16.3.0-next.0+sha-dca6d6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
