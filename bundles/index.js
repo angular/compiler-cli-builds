b0930ee77571e8ec981c39017ca445d1dd50c62a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:33:11 +0000

    1a56763b85 docs: add legend tag to fieldset (#51259)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.3.0-next.0+sha-79b3327");
+var VERSION = new Version("16.3.0-next.0+sha-1a56763");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
