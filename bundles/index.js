8973485c2488b0f0fe255888c36df543459a6121
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:31:40 +0000

    79b3327228 docs: Correct statement about app.module.ts generation with --standalone flag (#51307)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.3.0-next.0+sha-53687ef");
+var VERSION = new Version("16.3.0-next.0+sha-79b3327");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
