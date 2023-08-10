dc4e188f30a90ffcd66b16fc186f140561d02cb8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:46:49 +0000

    8f944e0752 ci: migrate framework tests to GHA (#51101)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-af0c8b8");
+var VERSION = new Version("16.2.0+sha-8f944e0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
