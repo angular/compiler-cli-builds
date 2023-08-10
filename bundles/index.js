21ecef7f12412ea0e0c78c3fcdb5b03a9979101a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 16:35:51 +0000

    232a8c1b8d fix(router): Apply named outlets to children empty paths not appearing in the URL (#51292)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-8f944e0");
+var VERSION = new Version("16.2.0+sha-232a8c1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
