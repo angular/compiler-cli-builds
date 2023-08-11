c5bae7985bfd342c385294fe280fa4952dd46e5b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 13:33:10 +0000

    f67812c220 release: switch the next branch to v17.0.0-next.0

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.3.0-next.0+sha-b2661fe");
+var VERSION = new Version("17.0.0-next.0+sha-f67812c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
