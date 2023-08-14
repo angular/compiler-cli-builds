552fcb74bafc01c6f1914b4ca06ef5b95ef778f7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 16:08:51 +0000

    951ef93aa6 docs(core): Fix minor typo in comment of injector_compatibility.ts (#51324) PR Close #51324

--- bundles/chunk-7WBWMZKZ.js
+++ bundles/chunk-7WBWMZKZ.js
@@ -855,7 +855,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0+sha-198834b");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.0+sha-951ef93");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -902,7 +902,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.0+sha-198834b") {
+    if (version === "16.2.0+sha-951ef93") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-198834b");
+var VERSION = new Version("16.2.0+sha-951ef93");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
