f1e01e67249b286399174582096d28adb8f84800
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 22:22:37 +0000

    074fd4a5ec build: update pullapprove (#51360)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-094d733");
+var VERSION = new Version("16.2.0+sha-074fd4a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
