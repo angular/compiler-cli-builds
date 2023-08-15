175347be40ba82f6b32d1a1a8cf6cb085792fa92
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:22:54 +0000

    328b3613b8 docs(common): revert format date_pipe (#51359)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-422d0d5");
+var VERSION = new Version("17.0.0-next.0+sha-328b361");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
