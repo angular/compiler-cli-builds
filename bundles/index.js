bf7b21312ffe6e94776728804bda0508c6fadaec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:04:46 +0000

    2174351385 docs: add the doc type attribute (#51332)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-d4ca651");
+var VERSION = new Version("17.0.0-next.0+sha-2174351");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
