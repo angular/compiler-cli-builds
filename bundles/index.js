f1ddac9bc2593a12d8fd0748d4ff40c4f7fc7f73
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 22:53:04 +0000

    da68bead56 docs(router): Clarify how to clear secondary routes (#51376)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-543b98e");
+var VERSION = new Version("16.2.0+sha-da68bea");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
