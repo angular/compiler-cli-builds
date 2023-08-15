5399768081f1c995e2337e578f0eb3de2dc376b2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:05:55 +0000

    252b111d1e docs: Improve host binding docs (#51326)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.0+sha-9f977e0");
+var VERSION = new Version("16.2.0+sha-252b111");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
