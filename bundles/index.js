8e804506e9b41d9978edb8f97ecb41a31f71835e
Andrew Kushnir <akushnir@google.com>
2023-08-17 17:30:25 +0000

    e6503930f1 docs: fix see also links. (#51379)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-5fb5f08");
+var VERSION = new Version("17.0.0-next.0+sha-e650393");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
