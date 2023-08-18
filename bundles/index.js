ddea16a2ac473227997f1fb234d147daa2de49dc
Andrew Kushnir <akushnir@google.com>
2023-08-18 17:16:12 +0000

    9152de1387 test(core): remove manually-written control flow instructions (#51380)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-de3be98");
+var VERSION = new Version("17.0.0-next.0+sha-9152de1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
