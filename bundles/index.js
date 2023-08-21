4ab3782e0dbe9540e9481cab620512bd55e9022a
Andrew Kushnir <akushnir@google.com>
2023-08-21 20:11:08 +0000

    38671f7c14 docs(animations): update animation docs to be standalone first (#51390)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-3a68647");
+var VERSION = new Version("17.0.0-next.0+sha-38671f7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
