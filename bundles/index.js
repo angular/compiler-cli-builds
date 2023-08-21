46f52ff23d266d82d095ded49178631567efeac3
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:35:10 +0000

    6dc2b22426 docs(service-worker): Miss a comma in sample (#3) (#51336)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.1+sha-39ace86");
+var VERSION = new Version("16.2.1+sha-6dc2b22");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
