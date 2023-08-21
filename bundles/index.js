6ea4c0507cf916128b01d4efd743b21ae6b6559e
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:22:55 +0000

    f724b8dd6b docs(docs-infra): fix margin on contribute page heading (#51393)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-9152de1");
+var VERSION = new Version("17.0.0-next.0+sha-f724b8d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
