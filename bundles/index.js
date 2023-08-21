3cd4e34247d7d9aee0e07813b88a3d8f79c10ec2
Andrew Kushnir <akushnir@google.com>
2023-08-21 20:13:02 +0000

    b0396e7164 fix(router): Ensure `canceledNavigationResolution: 'computed'` works on first page (#51441)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.1+sha-6dc2b22");
+var VERSION = new Version("16.2.1+sha-b0396e7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
