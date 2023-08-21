e62f7333ac3cb3b503cadfe21b5b39837e013182
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:26:30 +0000

    a9b3c006f8 fix(core): guard the jasmine hooks (#51394)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-5343486");
+var VERSION = new Version("17.0.0-next.0+sha-a9b3c00");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
