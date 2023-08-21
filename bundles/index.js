5ff522a43529253348e4add28de23e5447151e88
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:24:00 +0000

    53434864cb ci: remove myself from pullapprove unavailability list (#51437)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-f724b8d");
+var VERSION = new Version("17.0.0-next.0+sha-5343486");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
