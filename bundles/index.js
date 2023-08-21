ab5531233f5141e391985d32435f9c0cea9aa88d
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:43:42 +0000

    b7d25bbf47 docs: review Angular elements guide (#50043) PR Close #50043

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-119eaa5");
+var VERSION = new Version("17.0.0-next.0+sha-b7d25bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
