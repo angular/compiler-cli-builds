a1964c9092547dd852e7bff55f53b24f90445cc7
Andrew Kushnir <akushnir@google.com>
2023-08-22 18:51:26 +0000

    c9ce89ee5c ci: correctly check aio job results (#51464)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-36663e6");
+var VERSION = new Version("17.0.0-next.0+sha-c9ce89e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
