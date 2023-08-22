86b533d7974059fb981559132e310d42d1312204
Andrew Kushnir <akushnir@google.com>
2023-08-22 17:48:57 +0000

    fb296e4dd7 docs: add MSYS2 to DEVELOPER.md (#51459)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.1+sha-1574158");
+var VERSION = new Version("16.2.1+sha-fb296e4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
