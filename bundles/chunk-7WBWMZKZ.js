3dbe281f479afe63e2546cb3683f4038b2e992f3
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:16:09 +0000

    157415831c refactor(router): Have router subscribe to transition events to update state (#48427)

--- bundles/chunk-7WBWMZKZ.js
+++ bundles/chunk-7WBWMZKZ.js
@@ -855,7 +855,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "16.2.1+sha-4fe88cf");
+  const LATEST_VERSION_RANGE = getRange("<=", "16.2.1+sha-1574158");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -902,7 +902,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "16.2.1+sha-4fe88cf") {
+    if (version === "16.2.1+sha-1574158") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.1+sha-4fe88cf");
+var VERSION = new Version("16.2.1+sha-1574158");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
