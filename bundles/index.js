88c942af5b0452411abab69751203aa0b2d2bc92
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:12:26 +0000

    bd150a2550 docs: remove update reference (#51449)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.0+sha-d2346a6");
+var VERSION = new Version("17.0.0-next.0+sha-bd150a2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
