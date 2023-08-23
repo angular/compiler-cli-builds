172901ad5776ab732a12558c583930435f6b9f7c
Joey Perrott <josephperrott@gmail.com>
2023-08-23 22:54:57 +0000

    546a8e52dd build: provide CI_BUILD_URL env variable (#51488)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.2+sha-0289438");
+var VERSION = new Version("16.2.2+sha-546a8e5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
