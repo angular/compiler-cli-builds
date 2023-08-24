76e87e0c38ecdaf15da2cb81303f3c2fe56a081a
Joey Perrott <josephperrott@gmail.com>
2023-08-24 16:54:37 +0000

    554305976b docs(docs-infra): set meta description per page (#51487)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.2+sha-6f4a5c3");
+var VERSION = new Version("16.2.2+sha-5543059");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
