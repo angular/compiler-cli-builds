428a6757b04b6388124b9dad760ecffde053be71
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:25 +0000

    a28864d0f7 docs: migrate template guide and its code examples to standalone. (#51364)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.1+sha-dc4db80");
+var VERSION = new Version("17.0.0-next.1+sha-a28864d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
