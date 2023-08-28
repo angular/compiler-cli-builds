feab63e9a5d0a204982a8a3288f5bbd54e9811e7
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:18 +0000

    dc4db80f9a docs(docs-infra): remove inactive resource from community page (#51418)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.1+sha-9753347");
+var VERSION = new Version("17.0.0-next.1+sha-dc4db80");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
