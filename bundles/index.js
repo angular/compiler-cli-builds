77170fc94a10092ba86cbbeb8b7d04f92e89c521
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 21:37:56 +0000

    403833557b fix(docs-infra): escape the `.` character in regex (#51555)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.2+sha-97a4095");
+var VERSION = new Version("16.2.2+sha-4038335");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
