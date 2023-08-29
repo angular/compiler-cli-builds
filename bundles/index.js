656c370df060e0ac3dea46b2a56d9beccb427be5
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 00:06:08 +0000

    f7364ece87 docs: migrate binding-syntax code to standalone (#51479)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.1+sha-ba32d6f");
+var VERSION = new Version("17.0.0-next.1+sha-f7364ec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
