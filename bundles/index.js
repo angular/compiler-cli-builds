217025151ed3d1010215505f94f9878a4f870b03
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:42:34 +0000

    fa29eb84a3 docs: fix casing of a11y labels in main page (#51431) PR Close #51431

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.1+sha-8c77240");
+var VERSION = new Version("17.0.0-next.1+sha-fa29eb8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
