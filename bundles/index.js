facfe6ced6dbd98e3947878afa70ad8d0b95fca4
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 19:18:33 +0000

    6e5b865710 build: use pin for devcontainer base image (#51556)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.2+sha-0c7c852");
+var VERSION = new Version("16.2.2+sha-6e5b865");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
