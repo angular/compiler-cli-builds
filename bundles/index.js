10e8ac4c7d0d3596b40c1b9471d53a7104eee63c
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 00:10:56 +0000

    0f86a0b9e7 docs: migrate elements guide and code to standalone (#51455)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.1+sha-fa29eb8");
+var VERSION = new Version("17.0.0-next.1+sha-0f86a0b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
