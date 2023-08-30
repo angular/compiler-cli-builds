d5d852debf3034302695ccec5abb5ed41ab09384
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 19:54:36 +0000

    eab92167e5 build: enable video recording for saucelabs bazel tests (#51533)

--- bundles/chunk-DJV5J7EO.js
+++ bundles/chunk-DJV5J7EO.js
@@ -855,7 +855,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.2+sha-2c11b83");
+  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.2+sha-eab9216");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -902,7 +902,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "17.0.0-next.2+sha-2c11b83") {
+    if (version === "17.0.0-next.2+sha-eab9216") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
