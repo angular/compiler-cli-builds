2402e6a5d2f8c6fa9c9e379cfdb1fb31b1075334
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 19:55:02 +0000

    68d6fae734 build: enable video recording for saucelabs bazel tests (#51533)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-587093c");
+var VERSION = new Version("16.2.3+sha-68d6fae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
