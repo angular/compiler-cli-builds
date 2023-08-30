a6ab5f4deeefc967beaef8fa8660b094b9cee047
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 16:05:08 +0000

    dc4f77c851 docs(core): clarify InjectionToken usage (#51386)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.2+sha-c99624d");
+var VERSION = new Version("16.2.2+sha-dc4f77c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
