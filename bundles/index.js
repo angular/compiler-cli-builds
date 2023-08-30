4be1d66d70b93d3ab1c0e9afde1dc4d66d1c1d61
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 20:32:47 +0000

    a258823071 ci: migrate zonejs tests to gha (#51352)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-68d6fae");
+var VERSION = new Version("16.2.3+sha-a258823");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
