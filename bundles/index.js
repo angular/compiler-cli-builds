6dcc7388d799280756590875c8ef7a0853269f46
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:25:00 +0000

    055448e9c6 fix(docs-infra): fix incomplete escaping (#51604)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-3fd41a1");
+var VERSION = new Version("16.2.3+sha-055448e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
