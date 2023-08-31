73e3f442e2225dca723226d38e2c2eb24aa17a7e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 21:52:54 +0000

    3658e25f2e docs: migrate toh-pt0 to standalone (#51575)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-05e4dbb");
+var VERSION = new Version("17.0.0-next.2+sha-3658e25");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
