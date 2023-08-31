c6f24ea842b862753954358c8bbda31093e555ae
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 19:08:03 +0000

    0839885a50 refactor(core): make AfterRenderEventManager tree-shakable (#51541)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-a1bad49");
+var VERSION = new Version("17.0.0-next.2+sha-0839885");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
