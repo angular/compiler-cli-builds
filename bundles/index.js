decb2808a02d58dfb3b4b2546997c5600776cd46
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:09:38 +0000

    92f478ea7d docs: update router reference to use standalone (#51560)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-f464e39");
+var VERSION = new Version("17.0.0-next.2+sha-92f478e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
