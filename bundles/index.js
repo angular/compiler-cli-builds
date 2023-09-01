7c7ec2855ad7dedefb344774536f1e1de6115a7b
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:30:25 +0000

    201ab9d247 refactor(core): switch signals to a refcounting algorithm (#51226)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-b9ba6e6");
+var VERSION = new Version("17.0.0-next.2+sha-201ab9d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
