97b468a29144727a4b1e6b8ed7e4a02d50aded96
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 16:28:22 +0000

    8ce22ebae3 docs: fix missing information in createComponent (#51493)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-af9cd84");
+var VERSION = new Version("16.2.3+sha-8ce22eb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
