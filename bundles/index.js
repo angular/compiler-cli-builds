bf5f0a7a875a38f88fa43b4e18f75fd2e8576fde
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 17:19:15 +0000

    b37ba05804 docs: update the sentence to express its meaning (#51622)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-6e94e6d");
+var VERSION = new Version("17.0.0-next.2+sha-b37ba05");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
