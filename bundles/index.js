131bbd4aa3c949cb09feb72eb0fb3c39861c96ef
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 15:21:46 +0000

    fe1a50c63f docs(docs-infra): mixin generated same multiple css (#51528)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-dc4b4aa");
+var VERSION = new Version("17.0.0-next.2+sha-fe1a50c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
