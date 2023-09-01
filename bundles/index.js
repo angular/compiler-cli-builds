5dcb480c6d2ce567d2c52d4543598021776b5df7
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:50:48 +0000

    dc4b4aa57e feat(devtools): Display getters and setters in devtools property viewer (#49695)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-201ab9d");
+var VERSION = new Version("17.0.0-next.2+sha-dc4b4aa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
