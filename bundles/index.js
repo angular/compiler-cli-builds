fc06fb00c969ade0564f7a5275a5f5a820e46463
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:28:15 +0000

    b9ba6e6a16 refactor(core): remove duplicated access to TView (#51610)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-bb47f81");
+var VERSION = new Version("17.0.0-next.2+sha-b9ba6e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
