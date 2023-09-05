307c6525f94e8886a0fccb885adf31266f4ff958
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:26:19 +0000

    4b54947c97 fix(devtools): use the __ignore_ng_zone__ flag in devtools message bus' to prevent CD loop (#51339)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-2d96dc2");
+var VERSION = new Version("17.0.0-next.2+sha-4b54947");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
