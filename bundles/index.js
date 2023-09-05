7419863c5465834e658a3ae65c1bf4b590ed0701
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 17:03:26 +0000

    4721208d5a docs: remove mention of the string based lazy loading (#51651)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-fcfa173");
+var VERSION = new Version("16.2.3+sha-4721208");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
