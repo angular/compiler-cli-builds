8079404cf535fc619c5801b4618a2a4ea7ae22b5
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 15:12:42 +0000

    7fa17d00f6 test(compiler): fix failing tests (#51656)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-75ab0bd");
+var VERSION = new Version("17.0.0-next.2+sha-7fa17d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
