6712bf02f563b23770cabf5d740b7a32f49d41d1
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 20:58:25 +0000

    a734ca3e15 Revert "ci: disable all of the angular-robot settings (#51663)" (#51666)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.3+sha-c2fac37");
+var VERSION = new Version("16.2.3+sha-a734ca3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
