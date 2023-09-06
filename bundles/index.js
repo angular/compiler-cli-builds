c74f650eced891b084659e10b3134407d2af2017
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:42:09 +0000

    88a0af64fd perf(core): generate arrow functions for pure function calls (#51668)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-8ad21cc");
+var VERSION = new Version("17.0.0-next.2+sha-88a0af6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
