70c1d3b9a0ed09bf9cdd72e210aec4292c5aad44
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:42:03 +0000

    8ad21ccfb8 docs: Add info about hydration caching. (#51648)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.2+sha-970d68f");
+var VERSION = new Version("17.0.0-next.2+sha-8ad21cc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
