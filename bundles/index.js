888da8e1a38a3d05d88def94d6d4540c0e400b99
Andrew Kushnir <akushnir@google.com>
2023-09-07 20:59:16 +0000

    eb0137de8a build: migrate `project_id` option to use `bes_instance_name` (#51696)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-26af16c");
+var VERSION = new Version("17.0.0-next.3+sha-eb0137d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
