b1fe45442753e10bbc4930580a8028632366d00e
Andrew Kushnir <akushnir@google.com>
2023-09-07 17:13:37 +0000

    3f2d783f65 docs: migrate toh-pt3 to standalone (#51581)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-8cd9663");
+var VERSION = new Version("17.0.0-next.3+sha-3f2d783");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
