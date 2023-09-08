facbf57fba2ba23a8282cb772cec89eacb81c7b9
Andrew Kushnir <akushnir@google.com>
2023-09-08 22:00:53 +0000

    05762b9fff refactor(localize): ignore code duplication error with compiler package (#51500) (#51558)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-0d9fd6d");
+var VERSION = new Version("17.0.0-next.3+sha-05762b9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
