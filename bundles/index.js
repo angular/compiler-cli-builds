77f020fbabb1d9082de1755eaf350812a9767246
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:31:42 +0000

    f6ccb163aa docs: Migrate i18n guide pages and code to Standalone (#51589)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-44c7469");
+var VERSION = new Version("17.0.0-next.3+sha-f6ccb16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
