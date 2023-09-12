a46d2596c53f8342cf4165f80a1959788c4f2721
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:34:36 +0000

    65f7686769 docs: migrate reactive-forms to standalone (#51707)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-cbeef95");
+var VERSION = new Version("17.0.0-next.3+sha-65f7686");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
