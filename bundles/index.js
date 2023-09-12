ea7aefd7da59720e98e6df88db9fcb11192b22d3
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:32:59 +0000

    4e0a575262 docs: migrate forms-overview to standalone (#51699)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.3+sha-7b0bfc1");
+var VERSION = new Version("17.0.0-next.3+sha-4e0a575");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
