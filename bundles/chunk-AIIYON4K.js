b9fb24a7cd4f1b8496a440275143c864366b5b5c
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:59:17 +0000

    59aa0634f4 build: remove support for Node.js v16 (#51755)

--- bundles/chunk-AIIYON4K.js
+++ bundles/chunk-AIIYON4K.js
@@ -855,7 +855,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.4+sha-2a1723c");
+  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.4+sha-59aa063");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -902,7 +902,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "17.0.0-next.4+sha-2a1723c") {
+    if (version === "17.0.0-next.4+sha-59aa063") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-2a1723c");
+var VERSION = new Version("17.0.0-next.4+sha-59aa063");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
