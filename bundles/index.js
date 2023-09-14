4486a12c15eddc2c8eaa1112ff5dea0d34ef00e6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-14 09:33:01 +0000

    63b8108f66 docs: add revamped guide for dependency injection focused on standalone (#51572)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-c09015a");
+var VERSION = new Version("17.0.0-next.4+sha-63b8108");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
