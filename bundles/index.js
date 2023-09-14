84b5531f8e45b87af018f384955e045ef8dfa55a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-14 09:28:19 +0000

    c09015af08 docs: update Angular CLI help [main] (#51761)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-d91618f");
+var VERSION = new Version("17.0.0-next.4+sha-c09015a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
