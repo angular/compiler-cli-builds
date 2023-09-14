db59ebf5d8e93f02539728e386859679c1dccb49
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-14 11:00:30 +0000

    7b40d74fb4 refactor(core): let reactive node prototypes conform to their type (#51722) (#51772)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.5+sha-65d6915");
+var VERSION = new Version("16.2.5+sha-7b40d74");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
