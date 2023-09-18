419d8cdd6700319249e1f2f534fd1c9e7a9b6400
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:51:12 +0000

    545db6d22d refactor(core): support phases in afterRender and afterNextRender (#51559)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-e2416a2");
+var VERSION = new Version("17.0.0-next.4+sha-545db6d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
