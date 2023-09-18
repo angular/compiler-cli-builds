05a908848127cee8d2844cb5d6439761f53781ee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 10:06:57 +0000

    9bd1551c3f release: cut the zone.js-0.14.0 release (#51774)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-75380bf");
+var VERSION = new Version("17.0.0-next.4+sha-9bd1551");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
