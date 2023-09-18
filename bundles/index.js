89d24e622a704661595aaec1191086ae33ef0485
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 15:11:31 +0000

    873e80b869 refactor(core): fix NgModule compilation scope in deps tracker (#51791)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-3d06a81");
+var VERSION = new Version("17.0.0-next.4+sha-873e80b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
