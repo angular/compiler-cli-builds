62c9baa8707a543e6a0568c9c4ba17bb0b9f5457
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:56:59 +0000

    75380bf220 test(compiler): attempt to deflake windows tests (#51804)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-fe2fd7e");
+var VERSION = new Version("17.0.0-next.4+sha-75380bf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
