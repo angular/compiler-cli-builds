e19d99bfe8b5d723d32150d056fec43a2a22f512
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 15:10:19 +0000

    3d06a8133c refactor(core): flatten and resolve ng-module bootstrap info in local compilation mode (#51767)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-fb3e6d6");
+var VERSION = new Version("17.0.0-next.4+sha-3d06a81");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
