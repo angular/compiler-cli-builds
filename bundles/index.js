5a459d026b15be6cbffc4a4bfdbe4e3048859407
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:30:29 +0000

    8a2dc0f2e0 build: update actions/checkout action to v4 (#51793)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-e5a0116");
+var VERSION = new Version("17.0.0-next.4+sha-8a2dc0f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
