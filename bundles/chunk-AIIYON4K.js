3ec3c68144c09e3bedcd9bfee2a25e0002738400
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 16:49:08 +0000

    ba64e9c247 docs: update to latest version of material (#51785)

--- bundles/chunk-AIIYON4K.js
+++ bundles/chunk-AIIYON4K.js
@@ -855,7 +855,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.4+sha-1e6c055");
+  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.4+sha-ba64e9c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -902,7 +902,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "17.0.0-next.4+sha-1e6c055") {
+    if (version === "17.0.0-next.4+sha-ba64e9c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-1e6c055");
+var VERSION = new Version("17.0.0-next.4+sha-ba64e9c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
