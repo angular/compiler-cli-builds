0f6d250f8f4bdd8572f4c8e47e7c45323968c166
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 18:08:48 +0000

    bc296a17e5 docs: release notes for the v16.2.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.4+sha-34495b3");
+var VERSION = new Version("17.0.0-next.4+sha-bc296a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
