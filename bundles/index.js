e3261a0d6c052269f1ea01245ab1b98ae4990c0d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 16:43:22 +0000

    4ccd845081 refactor(router): Refactor loadChildren callback execution and privately export for SSR (#51818)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.5+sha-6e4d8f6");
+var VERSION = new Version("16.2.5+sha-4ccd845");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
