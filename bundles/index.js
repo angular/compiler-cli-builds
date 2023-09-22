4c0c6b9f16182736514e820e2cb18683887d97b0
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 16:58:13 +0000

    3c0577f991 perf(platform-browser): disable styles of removed components instead of removing (#51808)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.5+sha-dcf18dc");
+var VERSION = new Version("17.0.0-next.5+sha-3c0577f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
