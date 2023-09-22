92fad5ba16d80ed5105a0cffdd05f9895b81f469
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 17:00:37 +0000

    663703946e fix(bazel): allow setting `_enabledBlockTypes` angular compiler option (#51862)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.6+sha-6e6452d");
+var VERSION = new Version("16.2.6+sha-6637039");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
