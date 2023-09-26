ca8f8071a7e914298e5b1fbb6c124a6e621263e7
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:20:34 +0000

    dcaad169ec fix(service-worker): throw a critical error when `handleFetch` fails (#51885)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.5+sha-8be2c48");
+var VERSION = new Version("17.0.0-next.5+sha-dcaad16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
