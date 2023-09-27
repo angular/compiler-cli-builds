7e65fbdf98085e1f80b33bb6bf2e967b73c627fe
Dylan Hunn <github@dylanhunn.com>
2023-09-27 20:40:37 +0000

    423709634e docs: release notes for the v16.2.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.5+sha-f3c5b2e");
+var VERSION = new Version("17.0.0-next.5+sha-4237096");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
