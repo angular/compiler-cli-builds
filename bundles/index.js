7f25805858f4f3b2a03e899809c66eb3849290ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:01:30 +0000

    47e841c834 ci: move permissions for google-internal-tests into job definition (#51847)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.6+sha-81e7f5b");
+var VERSION = new Version("17.0.0-next.6+sha-47e841c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
