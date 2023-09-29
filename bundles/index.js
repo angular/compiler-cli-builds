6573105fe8e85b3abe1da3c42c95d86485f3cfb8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:04:12 +0000

    8d09e9e013 test: Update golden partial file (#51876)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.6+sha-47e841c");
+var VERSION = new Version("17.0.0-next.6+sha-8d09e9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
