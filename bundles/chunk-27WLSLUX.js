43a0a26ede0292486b583382da467445fdad6c02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 22:30:28 +0000

    6b6a44c473 refactor(core): add hydration support for built-in `for` (#51920)

--- bundles/chunk-27WLSLUX.js
+++ bundles/chunk-27WLSLUX.js
@@ -874,7 +874,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.6+sha-408d3b4");
+  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.6+sha-6b6a44c");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -921,7 +921,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "17.0.0-next.6+sha-408d3b4") {
+    if (version === "17.0.0-next.6+sha-6b6a44c") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
