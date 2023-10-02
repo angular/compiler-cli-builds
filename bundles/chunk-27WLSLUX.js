337eb59086a163fddb301f81ca3f2d3acc2bafd4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:44:38 +0000

    e368d81896 refactor(core): invoke `@defer (on idle)` callback in NgZone (#51975)

--- bundles/chunk-27WLSLUX.js
+++ bundles/chunk-27WLSLUX.js
@@ -874,7 +874,7 @@ var declarationFunctions = [
 ];
 function createLinkerMap(environment, sourceUrl, code) {
   const linkers = /* @__PURE__ */ new Map();
-  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.6+sha-c4d77fd");
+  const LATEST_VERSION_RANGE = getRange("<=", "17.0.0-next.6+sha-e368d81");
   linkers.set(\u0275\u0275ngDeclareDirective, [
     { range: LATEST_VERSION_RANGE, linker: new PartialDirectiveLinkerVersion1(sourceUrl, code) }
   ]);
@@ -921,7 +921,7 @@ var PartialLinkerSelector = class {
       throw new Error(`Unknown partial declaration function ${functionName}.`);
     }
     const linkerRanges = this.linkers.get(functionName);
-    if (version === "17.0.0-next.6+sha-c4d77fd") {
+    if (version === "17.0.0-next.6+sha-e368d81") {
       return linkerRanges[linkerRanges.length - 1].linker;
     }
     const declarationRange = getRange(">=", minVersion);
