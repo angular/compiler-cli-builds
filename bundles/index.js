cce11c07e5aca8157463eb08c3bb9ef641192a49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:47:14 +0000

    31edf79096 ci: add slack messages for failed aio deployments (#51956)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.6+sha-367b075");
+var VERSION = new Version("17.0.0-next.6+sha-31edf79");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
