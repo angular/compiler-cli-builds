76ab108228077818a8bcecdb3b6c9cba0653a385
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:28:55 +0000

    781bb91f43 build: lock file maintenance (#51838)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.7+sha-82e8e5e");
+var VERSION = new Version("16.2.7+sha-781bb91");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
