c4fbcf8727c92db58cc0aecc80306d6aa697d587
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:40:06 +0000

    4611b7032d docs: update events (#51966)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.6+sha-08482f2");
+var VERSION = new Version("17.0.0-next.6+sha-4611b70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
