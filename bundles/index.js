3a98fec6da0bbf1d3b05959fd11bf8d69c4d7223
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 16:02:13 +0000

    966ce9790a fix(service-worker): throw a critical error when handleFetch fails (#51960)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.7+sha-334fe60");
+var VERSION = new Version("16.2.7+sha-966ce97");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
