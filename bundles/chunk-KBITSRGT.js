21f48b1de705fd25a604b748a6b62f58eda2542b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 20:34:18 +0000

    c38846292b docs: release notes for the v15.2.10 release

--- bundles/chunk-KBITSRGT.js
+++ bundles/chunk-KBITSRGT.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.0-next.6+sha-a7fa253";
+var PLACEHOLDER_VERSION = "17.0.0-next.6+sha-c388462";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
