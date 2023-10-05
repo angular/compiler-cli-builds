faefba92d1854810ee74af135074e495819e3f00
Andrew Scott <atscott01@gmail.com>
2023-10-05 21:12:57 +0000

    5464bea73c build: update github/codeql-action action to v2.21.9 (#51992)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-caa8eb2");
+var VERSION = new Version("17.0.0-next.7+sha-5464bea");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
