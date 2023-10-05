0ef6d7f5f3f8c7a0cb7b759d608d0e94f62a54a9
Andrew Scott <atscott01@gmail.com>
2023-10-05 18:00:50 +0000

    2a10050bea ci: migrate snapshot publishing from CircleCI to GHA (#51957)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.8+sha-5122729");
+var VERSION = new Version("16.2.8+sha-2a10050");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
