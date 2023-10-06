26754b869747f8ae07dc6d931141626ed6be8892
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:51:57 +0000

    486cc611a5 refactor(core): add `focusin` event to `on hover` trigger of `@defer` block (#52059)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-f06d97e");
+var VERSION = new Version("17.0.0-next.7+sha-486cc61");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
