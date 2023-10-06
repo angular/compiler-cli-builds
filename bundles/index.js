63bb90214f41931a25d0864593a0d0ac8ab428ef
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:58:57 +0000

    17078a3fe1 fix(compiler): pipes used inside defer triggers not being picked up (#52071)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-486cc61");
+var VERSION = new Version("17.0.0-next.7+sha-17078a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
