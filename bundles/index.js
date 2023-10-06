3721c6f66b2b95d8e6dad531aa22bc86cf96deca
Andrew Scott <atscott01@gmail.com>
2023-10-06 20:10:15 +0000

    2e16b0e0ea ci: update `@defer` symbol test golden file (#52088)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-048f400");
+var VERSION = new Version("17.0.0-next.7+sha-2e16b0e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
