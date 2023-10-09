12a7abcf75e0255e83a7363ba6ec6c1c2bbc453d
Andrew Scott <atscott01@gmail.com>
2023-10-09 17:15:56 +0000

    28a5925f53 fix(common): use === operator to match NgSwitch cases (#51504)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-e25006b");
+var VERSION = new Version("17.0.0-next.7+sha-28a5925");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
