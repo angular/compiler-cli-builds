0fea7ddea340098b1f16d53a36081b99f0c49fd4
Andrew Scott <atscott01@gmail.com>
2023-10-09 21:51:13 +0000

    0ec66b85e6 refactor(core): Use flag instead of counter for dirty child transplanted views (#51515)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-11bb19c");
+var VERSION = new Version("17.0.0-next.7+sha-0ec66b8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
