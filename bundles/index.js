08da43b4713da9c08f0a8c32b3e211191aa01c1c
Andrew Scott <atscott01@gmail.com>
2023-10-09 19:45:48 +0000

    7368b8aaeb fix(core): host directive validation not picking up duplicate directives on component node (#52073)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-7bfe207");
+var VERSION = new Version("17.0.0-next.7+sha-7368b8a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
