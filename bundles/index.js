fe6abc8adf84171d6dfaa23e667aa8fb7ee5ca31
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:31:47 +0000

    2bfd28c396 docs: correct wording for Directive's decorator host property key description (#51977)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.8+sha-aeb7d03");
+var VERSION = new Version("16.2.8+sha-2bfd28c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
