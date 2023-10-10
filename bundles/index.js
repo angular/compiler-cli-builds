7433465c9140709e6edd8b3def84b7f23e75f601
Andrew Scott <atscott01@gmail.com>
2023-10-10 00:06:42 +0000

    b9a4941a32 build: update babel dependencies (#51898)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-c2f270c");
+var VERSION = new Version("17.0.0-next.7+sha-b9a4941");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
