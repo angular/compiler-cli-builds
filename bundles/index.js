a0dbbdebfdc2ddd2c1ce65739e1158e97c2b157c
Andrew Scott <atscott01@gmail.com>
2023-10-10 23:25:19 +0000

    fcc000e build: add targets for api doc generation (#52034)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-ddd7212");
+var VERSION = new Version("17.0.0-next.7+sha-fcc000e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
