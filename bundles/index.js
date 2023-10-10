de09173fc4c411e928c0276ae450f750cd015304
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:04:55 +0000

    5a4ccdf build: update cross-repo angular dependencies (#52133)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.7+sha-d487014");
+var VERSION = new Version("17.0.0-next.7+sha-5a4ccdf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
