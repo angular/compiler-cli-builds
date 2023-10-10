ee30dfef76822f82d60f95e9115c02f3ab4094aa
Andrew Scott <atscott01@gmail.com>
2023-10-10 22:40:51 +0000

    51a5baace3 fix(forms): reset() call with null values on nested group (#48830)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.8+sha-7d8b3f4");
+var VERSION = new Version("16.2.8+sha-51a5baa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
