8ab73defbb8c41ce24437635a1d72cfb58038572
Andrew Scott <atscott01@gmail.com>
2023-10-11 19:24:17 +0000

    c8f87c8eff docs: update events (#52053)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-fb10abc");
+var VERSION = new Version("16.2.9+sha-c8f87c8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
