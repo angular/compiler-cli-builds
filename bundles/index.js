b2df04d36b5856e7fd186c7f1f87154b7ccaf916
Andrew Scott <atscott01@gmail.com>
2023-10-11 16:21:44 +0000

    fcdae578fe docs(animations): fix typo in state name of keyframes example (#52155)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.8+sha-51a5baa");
+var VERSION = new Version("16.2.8+sha-fcdae57");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
