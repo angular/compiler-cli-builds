83ea2eca3503e3cd3bad9221012d6ba5e2ca1b9f
Andrew Scott <atscott01@gmail.com>
2023-10-11 17:03:43 +0000

    fb10abcf3e release: cut the v16.2.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.8+sha-a3136e0");
+var VERSION = new Version("16.2.9+sha-fb10abc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
