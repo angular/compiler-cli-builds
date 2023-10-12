6651c7614d093790608d9075cf2e97c2f27b73a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 17:20:27 +0000

    37c8fd7 fix(core): handle for alias with as in control flow migration (#52183)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-1c4a367");
+var VERSION = new Version("17.1.0-next.0+sha-37c8fd7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
