4d54f130e18d24c417925cdf1a57a94411eefdad
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 16:19:44 +0000

    0792424 refactor(animations): make `AnimationBuilder` tree-shakable (#52097)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-5c70148");
+var VERSION = new Version("17.0.0-next.8+sha-0792424");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
