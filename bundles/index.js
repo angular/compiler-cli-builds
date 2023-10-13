e037f5b6b829a12c28f9a04c3ef9d8be6bd7de72
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 11:53:06 +0000

    030a68b refactor(router): Introduce `StateManager` interface (#52171)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-6fd863d");
+var VERSION = new Version("17.0.0-next.8+sha-030a68b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
