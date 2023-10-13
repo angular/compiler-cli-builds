4e8d0700359c97368b1563ab4d537d18b31983c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 12:14:22 +0000

    d75eaa7375 refactor(core): remove unused helper for setting `LView` for reactive consumer (#52192)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-630e906");
+var VERSION = new Version("16.2.9+sha-d75eaa7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
