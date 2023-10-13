14d8ed78d1f4495bef0b37fc0f9a8936a53434fe
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 09:25:35 +0000

    e90f5e5 docs: Phase 2 Sample Code Conversions to Standalone (#51918)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-99e7629");
+var VERSION = new Version("17.0.0-next.8+sha-e90f5e5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
