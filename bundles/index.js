cb9adafd6c928dbfff945eae4d52bf3ebcd186aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:37:20 +0000

    d6070afdc2 docs: fix typo (#52219)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-37e7f2d");
+var VERSION = new Version("16.2.9+sha-d6070af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
