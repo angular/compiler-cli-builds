20dbd30c6ec320e498ab65378c0ef90fd3ed0a8b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:40:21 +0000

    e5720ed fix(core): handle if alias in control flow migration (#52181)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-81a287a");
+var VERSION = new Version("17.0.0-next.8+sha-e5720ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
