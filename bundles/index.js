f038950af389c79a43ba4b2864d591e0103f6c1e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 17:31:17 +0000

    6fe4f44 refactor(compiler): Fix i18n placeholders for slef-closing elements (#52195)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-e5720ed");
+var VERSION = new Version("17.0.0-next.8+sha-6fe4f44");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
