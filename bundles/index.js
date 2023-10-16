71be931a31e3eb27580bba5d3e753009225cc897
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 14:34:19 +0000

    81018d7562 docs: add missing next steps section (#52220)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-6997c52");
+var VERSION = new Version("16.2.9+sha-81018d7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
