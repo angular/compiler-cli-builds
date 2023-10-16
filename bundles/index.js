28c0c92e32c1e14cbace9990a238567c48f14ce0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 09:32:19 +0000

    4e4ad5a refactor(core): simplify hydration annotation key (#52207)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-262d4d5");
+var VERSION = new Version("17.1.0-next.0+sha-4e4ad5a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
