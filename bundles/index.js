ffdb589862ba7753289198dfe9bc417bdb6cc324
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 09:23:10 +0000

    c9cde3a perf(platform-browser): only append style element on creation (#52237)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-next.8+sha-35fd10a");
+var VERSION = new Version("17.0.0-next.8+sha-c9cde3a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
