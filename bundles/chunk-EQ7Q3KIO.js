d768271f473c9fd474dabb4d0d7a88d5c84be18c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 15:58:43 +0000

    83f5911 build: update all non-major dependencies (#51897)

--- bundles/chunk-EQ7Q3KIO.js
+++ bundles/chunk-EQ7Q3KIO.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-35b0691";
+var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-83f5911";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
