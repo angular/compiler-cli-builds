090fc644b4aabeb69542d1e57a6d7181e14e2044
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 15:44:00 +0000

    1a09d3e44b build: update io_bazel_rules_sass digest to 425ecaf (#52173)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-2820425");
+var VERSION = new Version("16.2.9+sha-1a09d3e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
