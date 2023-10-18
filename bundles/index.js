618c5d1e04142718d721c835d58a4302c7d5d5bf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 15:52:04 +0000

    55e41d876d build: update scorecard action dependencies (#52126)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-1a09d3e");
+var VERSION = new Version("16.2.9+sha-55e41d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
