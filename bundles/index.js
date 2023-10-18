bf4ccc3f9c1c8b6d3782db626c28bbaae86887f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 14:03:11 +0000

    2820425e38 build: update babel dependencies to v7.23.2 (#52255)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.9+sha-06ce49a");
+var VERSION = new Version("16.2.9+sha-2820425");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
