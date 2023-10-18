dbb06e4446efc9b431b24c1c3ca5503358647dae
Dylan Hunn <dylhunn@gmail.com>
2023-10-18 19:18:27 +0000

    b4c1ffb refactor(forms): Log a warning when `FormGroup` keys include a dot. (#50642)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.0+sha-866d0f4");
+var VERSION = new Version("17.0.0-rc.0+sha-b4c1ffb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
