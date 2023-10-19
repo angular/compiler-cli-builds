957b7161a271af5522b11d698fe7bd5d86b21bab
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 17:32:03 +0000

    3278966 fix(router): Ensure newly resolved data is inherited by child routes (#52167)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-8fff07c");
+var VERSION = new Version("17.1.0-next.0+sha-3278966");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
