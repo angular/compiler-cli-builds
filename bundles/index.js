a0942ee47cd2f20f581e3ba3015ceaa041581589
Dylan Hunn <dylhunn@gmail.com>
2023-10-23 19:13:20 +0000

    67b9720 refactor(core): use performance API for afterRender feature logging (#52235)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-1f66090");
+var VERSION = new Version("17.1.0-next.0+sha-67b9720");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
