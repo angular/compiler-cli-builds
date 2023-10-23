d74020f80a9a5710879769fca7a8bee98b40fe85
Dylan Hunn <dylhunn@gmail.com>
2023-10-23 20:47:31 +0000

    8239a8f19e docs: add event banner (#52334) PR Close #52334

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.10+sha-fbf49b5");
+var VERSION = new Version("16.2.10+sha-8239a8f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
