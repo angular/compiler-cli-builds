92a87cf5dc9a8cc68fdc0d5a7151f5d5b3120588
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 16:38:14 +0000

    fd88d6a7aa docs: update Angular CLI help [16.2.x] (#52335)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.10+sha-3687359");
+var VERSION = new Version("16.2.10+sha-fd88d6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
