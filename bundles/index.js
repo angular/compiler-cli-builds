537499f6d6311d968a7338fb1ba5d6fc7abe5455
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:41:50 +0000

    c9b1ddf fix(migrations): handle nested classes in control flow migration (#52309)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.0+sha-54fed68");
+var VERSION = new Version("17.0.0-rc.0+sha-c9b1ddf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
