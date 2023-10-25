89db93942c1acc52b08eaf4b87bb31324a75c186
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 18:09:46 +0000

    6858149 fix(router): `RouterTestingHarness` should throw if a component is expected but navigation fails (#52357)

--- bundles/chunk-YIBWGXHD.js
+++ bundles/chunk-YIBWGXHD.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.0-rc.0+sha-a803c89";
+var PLACEHOLDER_VERSION = "17.0.0-rc.0+sha-6858149";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
