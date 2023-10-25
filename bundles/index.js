8ef73dc0d54f32d55199cbf1c204e5060a787e6f
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 17:58:13 +0000

    a803c89 fix(core): load global utils before creating platform injector in the standalone case (#52365)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.0+sha-4c878f9");
+var VERSION = new Version("17.0.0-rc.0+sha-a803c89");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
