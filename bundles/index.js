aff74dc24552bbe76d8bed3341af8500f8a45ef9
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:45:59 +0000

    40f7536 release: cut the zone.js-0.14.1 release (#52374)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.0+sha-6858149");
+var VERSION = new Version("17.0.0-rc.1+sha-40f7536");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
