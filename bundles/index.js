9a98651968ec0da05e8159b9bce4856b665ef72c
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 23:57:15 +0000

    8580542 refactor: fix a number of typos throughout the codebase (#52249)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-57630c3");
+var VERSION = new Version("17.0.0-rc.1+sha-8580542");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
