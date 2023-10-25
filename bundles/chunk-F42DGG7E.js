7b71c6fd4c85dbf8fec284f43fd46d18f11e28f8
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 23:45:30 +0000

    31c7bc1 docs: fixed typos (#52297)

--- bundles/chunk-F42DGG7E.js
+++ bundles/chunk-F42DGG7E.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-16bf04b";
+var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-31c7bc1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
