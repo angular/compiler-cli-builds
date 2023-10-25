916e922237594146a8d06bcea32c4df805df1cd2
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 18:10:06 +0000

    b79b4aca91 fix(router): `RouterTestingHarness` should throw if a component is expected but navigation fails (#52357)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.10+sha-019a0f4");
+var VERSION = new Version("16.2.10+sha-b79b4ac");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
