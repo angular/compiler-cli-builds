504205c1ac5b8d1a1d818c556194cf67ee6b328b
Dylan Hunn <github@dylanhunn.com>
2023-10-25 21:32:01 +0000

    ee5b1cf076 release: cut the v16.2.11 release

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.10+sha-b79b4ac");
+var VERSION = new Version("16.2.11+sha-ee5b1cf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
