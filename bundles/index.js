fb13eef9871d05ba2b5156b634dd651eaebe66f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 21:17:56 +0000

    612986b build: update cross-repo angular dependencies (#52191)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-da1b71e");
+var VERSION = new Version("17.0.0-rc.1+sha-612986b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
