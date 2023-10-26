156941e5e63062b80617ab1d01b7e070616328dc
Dylan Hunn <dylhunn@gmail.com>
2023-10-26 16:45:04 +0000

    6a01d62 fix(migrations): fix broken migration when no control flow is present (#52399)

--- bundles/chunk-YIBWGXHD.js
+++ bundles/chunk-YIBWGXHD.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.0-rc.1+sha-576338a";
+var PLACEHOLDER_VERSION = "17.0.0-rc.1+sha-6a01d62";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
