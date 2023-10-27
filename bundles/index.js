736f864420df3b7ce9f565053d9f00995403ba2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:39:41 +0000

    5ff99f2f9e docs: add warning for localhost proxy (#51948) PR Close #51948

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.11+sha-38b4602");
+var VERSION = new Version("16.2.11+sha-5ff99f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
