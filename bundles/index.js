f675cbeee2ab6f231ba6a5ee30e16be9dfebf9e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:55:44 +0000

    a51bb6927d docs(docs-infra): fix typo in tap into the observable tutorial example (#52359)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.11+sha-5ff99f2");
+var VERSION = new Version("16.2.11+sha-a51bb69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
