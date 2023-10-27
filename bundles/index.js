829cedb9ddd04e8481c9c81931c8a51df45c3b42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:37:50 +0000

    becbcdc docs: add warning for localhost proxy (#51948) PR Close #51948

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-612986b");
+var VERSION = new Version("17.0.0-rc.1+sha-becbcdc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
