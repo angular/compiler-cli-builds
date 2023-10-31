62be8cb49cf548902c93b16f9b2562f9bbf94341
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:05:13 +0000

    e355f05 refactor(language-service): Add quick info for built in control flow/blocks (#52386)

--- bundles/chunk-YIBWGXHD.js
+++ bundles/chunk-YIBWGXHD.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.0-rc.1+sha-595009e";
+var PLACEHOLDER_VERSION = "17.0.0-rc.1+sha-e355f05";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
