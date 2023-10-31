857cfc9707482a4b9a089cbb30b63eeeecbc808a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 21:26:29 +0000

    696f003 fix(core): mutation bug in `getDependenciesFromInjectable` (#52450)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-b9ea2d6");
+var VERSION = new Version("17.0.0-rc.1+sha-696f003");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
