200ea478ed987a73d5f0472dc56f0867a0562f5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 15:14:31 +0000

    ddef3ac fix(core): effects wait for ngOnInit for their first run (#52473)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-935c181");
+var VERSION = new Version("17.0.0-rc.1+sha-ddef3ac");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
