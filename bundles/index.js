2b788bf925f739399ffb21248768721f52d295fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 23:30:15 +0000

    a6b8752 docs: update examples to use `buildTarget` instead of `browserTarget` (#52361)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-cce84d2");
+var VERSION = new Version("17.1.0-next.0+sha-a6b8752");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
