a08573c059405c682f65dff3927cbcbd3cf49c3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 20:28:47 +0000

    32f417f refactor(core): Do not refresh view if producers did not actually change (#52476)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-0d88e25");
+var VERSION = new Version("17.0.0-rc.1+sha-32f417f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
