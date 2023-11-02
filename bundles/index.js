6ed265097b1f160f72f164340729923f1acb04b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 18:10:16 +0000

    e092184a5c fix(common): apply fixed_srcset_width values only to fixed srcsets (#52486)

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.11+sha-64e4835");
+var VERSION = new Version("16.2.11+sha-e092184");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
