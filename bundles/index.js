17bb8e88ba44701f46bb7ef2403c662041336af3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 04:48:51 +0000

    356da9c refactor(core): add a flag for whether we skip logic in checkNoChanges (#52488)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-4e4c678");
+var VERSION = new Version("17.0.0-rc.1+sha-356da9c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
