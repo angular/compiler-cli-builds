fab5046505904059a50506b21980f4d205f74457
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:48:08 +0000

    cc13d6f build: update peter-evans/create-or-update-comment digest to 23ff157 (#52279)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.1+sha-0754122");
+var VERSION = new Version("17.0.0-rc.1+sha-cc13d6f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
