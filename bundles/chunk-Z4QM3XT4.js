818f2cc87479490cf4c59c4673ead749f29cf284
Andrew Scott <atscott01@gmail.com>
2023-11-03 17:58:26 +0000

    de13610 release: cut the zone.js-0.14.2 release (#52508)

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.0-rc.2+sha-95accb0";
+var PLACEHOLDER_VERSION = "17.0.0-rc.2+sha-de13610";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
