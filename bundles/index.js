1c8d9617b924d38b6b2cb46c6605f6dec117dbf4
Andrew Scott <atscott01@gmail.com>
2023-11-03 14:56:45 +0000

    236b448 docs: omit repetitions (#52413)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.2+sha-739c0d2");
+var VERSION = new Version("17.0.0-rc.2+sha-236b448");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
