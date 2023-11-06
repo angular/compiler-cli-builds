3fedeca6f54fdb9c167de6179e54589e1c05ea2c
Andrew Scott <atscott01@gmail.com>
2023-11-06 20:39:21 +0000

    a86661f refactor(devtools): update Angular logo for extension and loading animation (#52546)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.2+sha-590c618");
+var VERSION = new Version("17.0.0-rc.2+sha-a86661f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
