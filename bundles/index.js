2f8679178f8629668316e5f48df9c6ccaebeb1bd
Andrew Scott <atscott@google.com>
2023-11-06 19:15:04 +0000

    cab7e00 docs: add angular.dev (#52534)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0-rc.2+sha-2c3daeb");
+var VERSION = new Version("17.0.0-rc.2+sha-cab7e00");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
