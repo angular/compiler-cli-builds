702450dff9a7de4a8e322a86b2e8c8c180f029aa
Andrew Scott <atscott01@gmail.com>
2023-11-06 17:11:22 +0000

    f2fbe86 fix(migrations): Update regex to better match ng-templates (#52529)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-63777d2");
+var VERSION = new Version("17.1.0-next.0+sha-f2fbe86");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
