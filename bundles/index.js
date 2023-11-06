29b6c12ad8e4b1104f09cb5ef239658e978bb324
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:14:50 +0000

    499f09ac56 docs: add angular.dev banner (#52535) PR Close #52535

--- bundles/index.js
+++ bundles/index.js
@@ -69,7 +69,7 @@ import "./chunk-SRFZMXHZ.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("16.2.12+sha-ace0a68");
+var VERSION = new Version("16.2.12+sha-499f09a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
