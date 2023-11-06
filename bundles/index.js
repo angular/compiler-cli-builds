22e667a98c7586b00b1bc7e1b88edb317e6ac110
Andrew Scott <atscott01@gmail.com>
2023-11-06 23:59:10 +0000

    f887792 refactor(core): Add warning when signal equality is false for object.is (#52532)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-d01f371");
+var VERSION = new Version("17.1.0-next.0+sha-f887792");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
