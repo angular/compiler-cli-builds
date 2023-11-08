913236ab540b8066cd4e41ee3be525c839ad5a39
Andrew Scott <atscott01@gmail.com>
2023-11-08 16:57:32 +0000

    c745ca2 refactor(core): sanitize reactivity tests (#52632)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-3f2501e");
+var VERSION = new Version("17.1.0-next.0+sha-c745ca2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
