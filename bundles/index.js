ea6f89f3bb3831f346507d89698e5b7db4e0ee8f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 15:27:12 +0000

    fc6b2f2 docs: add the v17 blog post link to the changelog (#52675)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.0+sha-fecd141");
+var VERSION = new Version("17.0.0+sha-fc6b2f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
