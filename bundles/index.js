0890b65c926cf8e0eebc5ed9d5d14c57d019d752
Joey Perrott <josephperrott@gmail.com>
2023-11-09 16:02:35 +0000

    8c8ff63 docs: make homepade example accessible (#52578) PR Close #52578

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-645447d");
+var VERSION = new Version("17.1.0-next.0+sha-8c8ff63");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
