c2806ada928790b8df6188b07250382326636188
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 20:03:29 +0000

    b16fc26 fix(core): limit rate of markers invocations (#52742)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.1+sha-cfb42ee");
+var VERSION = new Version("17.0.1+sha-b16fc26");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
