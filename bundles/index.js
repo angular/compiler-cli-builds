d7a639e678ec6d3530febd8222a672c60562bc8a
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:23:45 +0000

    867161f docs: remove broken link pill in dynamic component page (#52744)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-f212736");
+var VERSION = new Version("17.1.0-next.0+sha-867161f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
