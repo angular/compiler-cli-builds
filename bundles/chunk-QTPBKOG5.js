f6ba6430eccb328756e9ecf03b3d119f000cf3c5
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 17:05:17 +0000

    55d2c42 refactor(core): delay applying component metadata until it's needed in tests (#52708)

--- bundles/chunk-QTPBKOG5.js
+++ bundles/chunk-QTPBKOG5.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-8c8ff63";
+var PLACEHOLDER_VERSION = "17.1.0-next.0+sha-55d2c42";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
