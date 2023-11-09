c777dcbf5ce80d07eb4da13285103238bfc8722a
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:41:25 +0000

    7d6752a docs(docs-infra): improve decorator deprecated property display in documentation (#52041)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.1+sha-96d554d");
+var VERSION = new Version("17.0.1+sha-7d6752a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
