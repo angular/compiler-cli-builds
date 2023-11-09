e1c2283757b69cbc364dcd5fab069e48b24719d8
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-11-09 20:14:51 +0000

    01efb1b release: cut the v17.0.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.1+sha-b16fc26");
+var VERSION = new Version("17.0.2+sha-01efb1b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
