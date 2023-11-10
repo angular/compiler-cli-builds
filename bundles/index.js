2d7c29efdb8cae0a17052ed09b894bebab4db831
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-10 17:07:46 +0000

    38aab6a refactor(compiler): Handle trailing spaces in ICU placeholders (#52698)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-c01af43");
+var VERSION = new Version("17.0.2+sha-38aab6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
