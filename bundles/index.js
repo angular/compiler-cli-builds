d8e2c0c6aa21cbd9acd0ef5d193915dc5356d8cd
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 18:30:28 +0000

    76aba9e refactor(core): Move booleans in LContainer to flags slot (#52338)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-30757bd");
+var VERSION = new Version("17.0.2+sha-76aba9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
