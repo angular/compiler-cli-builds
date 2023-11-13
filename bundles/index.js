37d3e1169dbde1e1c5fd9ddf95590535e9f24c43
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 19:03:31 +0000

    9e2c3bb docs(forms): replace fb with formBuilder (#52795)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-726530a");
+var VERSION = new Version("17.1.0-next.0+sha-9e2c3bb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
