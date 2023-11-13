93cfc6bf53dea544981d927d9b6b9f9f84306fcf
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 17:32:16 +0000

    30757bd refactor(animations): `EnvironmentProviders` for `provideAnimationsAsync` (#52862)

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.2+sha-f5872c9";
+var PLACEHOLDER_VERSION = "17.0.2+sha-30757bd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-f5872c9");
+var VERSION = new Version("17.0.2+sha-30757bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
