aa8c8b78ae280f0a290b775ffdcdd8c77c02e265
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 22:37:56 +0000

    d033540 fix(migrations): Add support for bound versions of NgIfElse and NgIfThenElse (#52869)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-c8ff0be");
+var VERSION = new Version("17.0.2+sha-d033540");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
