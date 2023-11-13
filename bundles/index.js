0e66a20761cec13e78ccf19e772286df00c663fd
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:35:13 +0000

    5ee11a7 fix(animations): prevent the AsyncAnimationRenderer from calling the delegate when there is no element. (#52570)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-cf86ae5");
+var VERSION = new Version("17.1.0-next.0+sha-5ee11a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
