17c35399feff08fd0bfa1a4d3d9b0aafd1c2a3a0
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 22:14:17 +0000

    c8ff0be ci: correct label for bazel-saucelabs job (#52873)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-a00beec");
+var VERSION = new Version("17.0.2+sha-c8ff0be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
