7383e1a3de8c96b8316fbb56f21e00a36062fec7
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 15:16:43 +0000

    b393e37 refactor(core): Simplify detectChangesInternal call signature (#52866)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-561cb1d");
+var VERSION = new Version("17.0.2+sha-b393e37");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
