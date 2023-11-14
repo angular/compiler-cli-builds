92887e83544af04c0f9dc72c1eb0ed8317f5c56a
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 22:06:11 +0000

    f8d5b84 docs: fixed unexpected closing tags for Receipt (#52914)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-0a976c8");
+var VERSION = new Version("17.1.0-next.0+sha-f8d5b84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
