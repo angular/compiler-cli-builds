cacdefdc3824ff48ad0fff3f4904203efcbb315a
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 19:21:11 +0000

    95e4806 docs: fix and improve @defer testing docs for adev (#52874)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-64d9e65");
+var VERSION = new Version("17.0.2+sha-95e4806");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
