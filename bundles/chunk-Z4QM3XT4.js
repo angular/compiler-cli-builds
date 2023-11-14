064b98ec513551057b910e100fd36d71fd245af1
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 21:11:32 +0000

    290e26a refactor(core): Remove detectChanges private export (#52875)

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.2+sha-95e4806";
+var PLACEHOLDER_VERSION = "17.0.2+sha-290e26a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
