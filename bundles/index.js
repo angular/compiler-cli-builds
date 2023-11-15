b5a50ee4a6981dffa9d65d4ccf1d01db371e8983
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 20:22:46 +0000

    151a544 release: bump DevTools version to 1.0.9 (#52938)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.2+sha-4e200bf");
+var VERSION = new Version("17.0.2+sha-151a544");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
