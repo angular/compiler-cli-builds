20991783e8070f61b8949a4f01ffc3c276092fcc
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 21:20:34 +0000

    98376f2 fix(compiler): changed after checked error in for loops (#52935)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.3+sha-f5307bf");
+var VERSION = new Version("17.0.3+sha-98376f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
