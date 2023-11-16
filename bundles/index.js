ee4e2b2c2adec1b6e0f607c7cc309e1cb561347a
Andrew Kushnir <akushnir@google.com>
2023-11-16 17:34:57 +0000

    6e29e85 refactor(migrations): Add optional template reformatting after migration (#52939)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.0+sha-ec2d6e7");
+var VERSION = new Version("17.1.0-next.0+sha-6e29e85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
