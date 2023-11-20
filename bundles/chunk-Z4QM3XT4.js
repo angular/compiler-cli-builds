0b201c4a51f158b57eff219ab7bb75a81eae0728
Andrew Kushnir <akushnir@google.com>
2023-11-20 18:51:14 +0000

    0fad36e fix(migrations): tweaks to formatting in control flow migration (#53058)

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.3+sha-291deac";
+var PLACEHOLDER_VERSION = "17.0.3+sha-0fad36e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.0.3+sha-291deac";
+export declare const PLACEHOLDER_VERSION = "17.0.3+sha-0fad36e";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
