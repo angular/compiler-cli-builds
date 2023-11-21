84131867ca0254048b817d18161cc855af2758a8
Andrew Kushnir <akushnir@google.com>
2023-11-21 18:36:04 +0000

    935fa8f build: update dependency globby to v14 (#52804)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.4+sha-dee50f1");
+var VERSION = new Version("17.0.4+sha-935fa8f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
