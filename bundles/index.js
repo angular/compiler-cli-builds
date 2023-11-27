d27e00d40d57d57687a02aaeeccd8535e6154b63
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 09:52:16 +0000

    6e1bf29 build: update dependency karma-jasmine-html-reporter to v2 (#52544)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.1+sha-c62b2da");
+var VERSION = new Version("17.1.0-next.1+sha-6e1bf29");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
