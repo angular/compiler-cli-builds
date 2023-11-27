8cff341e64d3575133d4fb8db8412f1d80f494b1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 14:54:09 +0000

    115dc23 build: upgrade node version for .devcontainer example file (#53157)

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.4+sha-4604be2";
+var PLACEHOLDER_VERSION = "17.0.4+sha-115dc23";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.4+sha-4604be2");
+var VERSION = new Version("17.0.4+sha-115dc23");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
