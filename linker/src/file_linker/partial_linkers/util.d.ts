3fad66755527c95e977002d1cb017c160296266e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:08:10 +0000

    dbd6f38 fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.0.4+sha-92a28a7";
+export declare const PLACEHOLDER_VERSION = "17.0.4+sha-dbd6f38";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
