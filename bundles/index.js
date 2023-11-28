482e0b7c3d641a669c3a25c9ea93714d623070dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 15:48:37 +0000

    f1b7d40 fix(migrations): fixes CF migration i18n ng-template offsets (#53212)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.1+sha-53912fd");
+var VERSION = new Version("17.1.0-next.1+sha-f1b7d40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
