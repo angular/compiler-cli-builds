b486c86727da1ad735be3f234b356cbfed90af41
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:07:45 +0000

    53912fd fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- bundles/chunk-OYZCQ7JQ.js
+++ bundles/chunk-OYZCQ7JQ.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.1+sha-4c1d69e";
+var PLACEHOLDER_VERSION = "17.1.0-next.1+sha-53912fd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
