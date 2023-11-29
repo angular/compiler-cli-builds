98c5cbaf35e6caa722cf989d44a6ba705f07c3e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 14:19:55 +0000

    fadfee4 fix(migrations): cf migration fix migrating empty switch default (#53237)

--- bundles/chunk-OYZCQ7JQ.js
+++ bundles/chunk-OYZCQ7JQ.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.1+sha-c9f8e75";
+var PLACEHOLDER_VERSION = "17.1.0-next.1+sha-fadfee4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
