bcd31ff9fdde0dbfcf40341a6fb8b4c98682cf9d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 17:22:54 +0000

    5f6323c release: cut the v17.0.5 release

--- bundles/chunk-Z4QM3XT4.js
+++ bundles/chunk-Z4QM3XT4.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.4+sha-6ae4088";
+var PLACEHOLDER_VERSION = "17.0.5+sha-5f6323c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
