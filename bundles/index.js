7064d1ece6ee83b8af758e25c2091045af1e19d5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 14:20:17 +0000

    5b9f896 fix(migrations): cf migration fix migrating empty switch default (#53237)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.4+sha-baf5830");
+var VERSION = new Version("17.0.4+sha-5b9f896");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
