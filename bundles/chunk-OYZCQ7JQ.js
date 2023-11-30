d9831b837c0864e45857d1feafbaf7093d3a8633
Dylan Hunn <dylhunn@gmail.com>
2023-11-30 17:45:14 +0000

    a429167 fix(devtools): use a shared angular detection code (#51569)

--- bundles/chunk-OYZCQ7JQ.js
+++ bundles/chunk-OYZCQ7JQ.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.2+sha-6462ddd";
+var PLACEHOLDER_VERSION = "17.1.0-next.2+sha-a429167";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
