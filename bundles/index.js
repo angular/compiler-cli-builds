6663be547a78a1883844631ed72cd35a077137a6
Dylan Hunn <dylhunn@gmail.com>
2023-12-01 17:23:21 +0000

    2998d48 fix(migrations): CF Migration - Fix case of aliases on i18n ng-templates preventing removal (#53299)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-4b23221");
+var VERSION = new Version("17.1.0-next.2+sha-2998d48");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
