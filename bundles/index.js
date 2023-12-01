e7286b2d244f45ed3e13e3a14826da986b76b59a
Dylan Hunn <dylhunn@gmail.com>
2023-12-01 17:17:42 +0000

    4b23221 fix(core): support hydration for cases when content is re-projected using ng-template (#53304)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-1940280");
+var VERSION = new Version("17.1.0-next.2+sha-4b23221");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
