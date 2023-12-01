096c82859db749cc8b3f44e2be42010592315fcb
Dylan Hunn <dylhunn@gmail.com>
2023-12-01 22:57:11 +0000

    acd6100 docs(compiler): Improve code comments (#53300)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-9423c19");
+var VERSION = new Version("17.1.0-next.2+sha-acd6100");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
