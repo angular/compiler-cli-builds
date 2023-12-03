17a1c236eb92e40012d6d48f7b2f236bf535fb1e
Dylan Hunn <dylhunn@gmail.com>
2023-12-03 23:02:25 +0000

    ac8eb5b Revert "fix(router): Ensure canMatch guards run on wildcard routes (#53239)" (#53339)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.5+sha-2174138");
+var VERSION = new Version("17.0.5+sha-ac8eb5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
