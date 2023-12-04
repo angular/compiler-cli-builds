063f533caaf7fdbf9951f0d6095fd3031e90ad13
Dylan Hunn <dylhunn@gmail.com>
2023-12-04 22:28:48 +0000

    12dfa9b test(compiler): Update golden partial file (#53327)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-d4b4236");
+var VERSION = new Version("17.1.0-next.2+sha-12dfa9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
