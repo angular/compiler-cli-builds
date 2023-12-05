94b94dda360b40df9b6a63603dec55c98de67502
Dylan Hunn <dylhunn@gmail.com>
2023-12-05 20:47:32 +0000

    6cd91c6 refactor(devtools): enables typescript strict option (#53340)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-1e3bcfe");
+var VERSION = new Version("17.1.0-next.2+sha-6cd91c6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
