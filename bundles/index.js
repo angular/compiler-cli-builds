90624ec3a94c34d6c9835cf5174665d822a16e30
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 01:18:17 +0000

    05f9c7b refactor(compiler): Initial support for i18n attributes (#53341)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.2+sha-6cd91c6");
+var VERSION = new Version("17.1.0-next.2+sha-05f9c7b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
