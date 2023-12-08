753d420c11894e6282a971dd4d20cf24ea4cc446
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 17:45:11 +0000

    a398c55 refactor(compiler): Fix out-of-order i18n issue (#53405)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.6+sha-17dbf8b");
+var VERSION = new Version("17.0.6+sha-a398c55");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
