c3328f6fffcc75c3d3a45cbf819a835f9caea80f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 23:06:01 +0000

    e3a6bf9 perf(core): optimize memory allocation when reconcilling lists (#52245)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.3+sha-f2245d1");
+var VERSION = new Version("17.1.0-next.3+sha-e3a6bf9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
