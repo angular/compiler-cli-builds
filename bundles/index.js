84e56f94ef05193a1fa5d4f042b1fd986c719767
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 22:12:22 +0000

    f9731ee build: remove unneeded graceful-fs package and resolution workaround (#53487)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.3+sha-cc75fab");
+var VERSION = new Version("17.1.0-next.3+sha-f9731ee");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
