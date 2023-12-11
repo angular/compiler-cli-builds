875428b1e316b28158ee43b447586e8490425059
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 22:13:09 +0000

    17610fa build: remove `incremental-dom` dependency (#53474)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.6+sha-9bb34a2");
+var VERSION = new Version("17.0.6+sha-17610fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
