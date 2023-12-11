e3bcef77be8b777e7c006d076818f7f9d7d8c420
Definitely not me <something@something.com>
2023-12-11 21:40:44 +0000

    9bb34a2 build: drop rxjs_umd_modules as it is unused (#53506)

--- bundles/chunk-JGZFZCLP.js
+++ bundles/chunk-JGZFZCLP.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.6+sha-1ea8a14";
+var PLACEHOLDER_VERSION = "17.0.6+sha-9bb34a2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.6+sha-1ea8a14");
+var VERSION = new Version("17.0.6+sha-9bb34a2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
