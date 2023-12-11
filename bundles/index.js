0cda94aab3421f6cddcc2a61b6a12652e7c299cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 19:01:44 +0000

    7c8f026 build: replace multimatch dependency with glob ignore option (#53463)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.3+sha-636b0d0");
+var VERSION = new Version("17.1.0-next.3+sha-7c8f026");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
