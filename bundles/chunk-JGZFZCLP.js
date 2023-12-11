1dc56bf97eca233eed4dce3107dd1e7c229fea11
Joey Perrott <josephperrott@gmail.com>
2023-12-11 19:27:04 +0000

    1ea8a14 refactor(docs-infra): move versions information into the application (#53501)

--- bundles/chunk-JGZFZCLP.js
+++ bundles/chunk-JGZFZCLP.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.6+sha-24d8aa6";
+var PLACEHOLDER_VERSION = "17.0.6+sha-1ea8a14";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
