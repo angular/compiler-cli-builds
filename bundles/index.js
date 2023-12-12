15c625fc576ed7d04ef109c1c375f7290dddf253
Definitely not me <something@something.com>
2023-12-12 23:52:49 +0000

    57ac806 feat(docs-infra): put the editor back in angular/angular (#53540)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.6+sha-9a91e0f");
+var VERSION = new Version("17.0.6+sha-57ac806");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
