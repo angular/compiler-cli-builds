06f0bbb551f6cee2617fa01ea7790bee2f4ae8f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-13 19:32:18 +0000

    629343f fix(core): Multiple subscribers to ApplicationRef.isStable should all see values (#53541)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.3+sha-5fa76c3");
+var VERSION = new Version("17.1.0-next.3+sha-629343f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
