e85dba04e1408184ada981ae15711e971f4a5ca7
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 17:25:03 +0000

    b394125 build: update rxjs build version to v7 (#53621)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.7+sha-de5c9ca");
+var VERSION = new Version("17.0.7+sha-b394125");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
