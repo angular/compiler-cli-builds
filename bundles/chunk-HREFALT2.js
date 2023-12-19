0686206069b483442a559153ded1ce9c9d156ce7
Andrew Scott <atscott01@gmail.com>
2023-12-19 21:27:18 +0000

    70de5c5 refactor(core): Use single source of truth for `ApplicationRef.isStable` (#53642)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.7+sha-ed936ba";
+var PLACEHOLDER_VERSION = "17.0.7+sha-70de5c5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.7+sha-ed936ba");
+var VERSION = new Version("17.0.7+sha-70de5c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
