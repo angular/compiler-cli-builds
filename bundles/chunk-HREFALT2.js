368ec8cd9d8dd948edfac9e7be6a4b5aa9caa8dc
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:26:39 +0000

    ed936ba fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638) (#53639)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.7+sha-eb7c29c";
+var PLACEHOLDER_VERSION = "17.0.7+sha-ed936ba";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
