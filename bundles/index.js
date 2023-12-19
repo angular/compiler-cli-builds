bc650aeadbb4481ec6c247bde2deb7b4988f09b7
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:47:21 +0000

    12181b9 refactor(core): Use single source of truth for `ApplicationRef.isStable` (#53576)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.4+sha-d49333e");
+var VERSION = new Version("17.1.0-next.4+sha-12181b9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
