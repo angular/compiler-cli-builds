22720383760ec9fb7f55ae1ec3061826dbc4511e
Andrew Scott <atscott01@gmail.com>
2023-12-19 19:06:45 +0000

    eb7c29c fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.7+sha-817dc1b");
+var VERSION = new Version("17.0.7+sha-eb7c29c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
