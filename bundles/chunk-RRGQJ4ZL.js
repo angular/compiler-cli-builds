2aa17640b880b77b5b81d4f02bb39e9a75b3f3ff
Andrew Scott <atscott01@gmail.com>
2023-12-19 18:04:21 +0000

    c16e9a7 refactor(core): No default for zone stableness token (#53505)

--- bundles/chunk-RRGQJ4ZL.js
+++ bundles/chunk-RRGQJ4ZL.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-502f300";
+var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-c16e9a7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
