3999e430029295df4e7004830d04d14e033a3df6
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:26:27 +0000

    d49333e fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638) (#53639)

--- bundles/chunk-RRGQJ4ZL.js
+++ bundles/chunk-RRGQJ4ZL.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-fb7c58c";
+var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-d49333e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.4+sha-fb7c58c");
+var VERSION = new Version("17.1.0-next.4+sha-d49333e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
