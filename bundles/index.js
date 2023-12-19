80c33e8b968172e07c3d4604672e8cff7a666fb7
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:25:31 +0000

    0696ab6 fix(router): Should not freeze original object used for route data (#53635)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.7+sha-b131c3b");
+var VERSION = new Version("17.0.7+sha-0696ab6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
