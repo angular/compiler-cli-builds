cb3669cc9211b5c38017356ec2cfe9796cd47c88
Andrew Scott <atscott01@gmail.com>
2023-12-20 15:59:42 +0000

    8d58595 refactor(core): Add scheduler abstraction and notify when necessary (#53499)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.4+sha-cc74ebf");
+var VERSION = new Version("17.1.0-next.4+sha-8d58595");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
