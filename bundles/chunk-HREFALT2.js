38d9985227c862d74ef08ef71c242bb30c52b9b9
Andrew Scott <atscott01@gmail.com>
2023-12-20 15:31:22 +0000

    c3250b0 refactor(compiler): Rework how ICU placeholders are handled (#53643)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.7+sha-70de5c5";
+var PLACEHOLDER_VERSION = "17.0.7+sha-c3250b0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
