35826d68e7724f946e62e065c0d236a29d825d91
Andrew Scott <atscott01@gmail.com>
2023-12-20 22:54:59 +0000

    c4de4e1 refactor(docs-infra): build adev application using local generated assets (#53511)

--- bundles/chunk-RRGQJ4ZL.js
+++ bundles/chunk-RRGQJ4ZL.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-8d58595";
+var PLACEHOLDER_VERSION = "17.1.0-next.4+sha-c4de4e1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
