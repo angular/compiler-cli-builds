7c8f28a8e71e1fe36fd4072be69ab0f878b35519
Andrew Scott <atscott01@gmail.com>
2023-12-20 15:28:35 +0000

    cc74ebf refactor(compiler): Rework how ICU placeholders are handled (#53643)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.4+sha-cafc3b0");
+var VERSION = new Version("17.1.0-next.4+sha-cc74ebf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
