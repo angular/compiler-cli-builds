8c7d6ad493d26326a5480d0928c3fe71e328cb63
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:30:12 +0000

    b87324d release: cut the v17.0.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.7+sha-257536e");
+var VERSION = new Version("17.0.8+sha-b87324d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
