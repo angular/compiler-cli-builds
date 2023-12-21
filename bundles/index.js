4edeb0e8ba7b5f838a14b4b122064f25e3a25378
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:38:37 +0000

    d315e2c release: cut the v17.1.0-next.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.4+sha-9e2e5d6");
+var VERSION = new Version("17.1.0-next.5+sha-d315e2c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
