bee64e173080cba34a7310ed96b64f24e2f7c8d9
Andrew Scott <atscott@google.com>
2024-01-03 21:33:25 +0000

    1c63edd refactor(router): remouve unused (#53750)

--- bundles/chunk-RRGQJ4ZL.js
+++ bundles/chunk-RRGQJ4ZL.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0-next.5+sha-58f2b74";
+var PLACEHOLDER_VERSION = "17.1.0-next.5+sha-1c63edd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
