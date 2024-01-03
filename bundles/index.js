6405c52f31265c6700ef46d6c1d99fe63bada5de
Andrew Scott <atscott@google.com>
2024-01-03 18:39:14 +0000

    7c19dc2 docs: fix incorrect glossary entry (#53609)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.8+sha-bd9f89d");
+var VERSION = new Version("17.0.8+sha-7c19dc2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
