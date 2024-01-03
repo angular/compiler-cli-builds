be30473d48332058bacbfeafa91f003cb4400414
Andrew Scott <atscott@google.com>
2024-01-03 19:34:10 +0000

    7bf11ea refactor(core): Use NoopNgZone in componentFixture (#53670)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.8+sha-7c19dc2";
+var PLACEHOLDER_VERSION = "17.0.8+sha-7bf11ea";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
