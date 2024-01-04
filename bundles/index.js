7ffcee45b08825a83777ed160c2e553114dab942
Andrew Scott <atscott@google.com>
2024-01-04 22:02:47 +0000

    5e84d9c refactor(router): replace `last` helper with native `Array.at(-1)` (#53749)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.5+sha-f4bd5a3");
+var VERSION = new Version("17.1.0-next.5+sha-5e84d9c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
