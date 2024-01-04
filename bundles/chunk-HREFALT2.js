26fc0998a26eec09bb5822c7a58e391b73fbc304
Andrew Scott <atscott@google.com>
2024-01-04 20:55:38 +0000

    91cb16f fix(platform-server): Do not delete global Event (#53659)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.8+sha-5f73737";
+var PLACEHOLDER_VERSION = "17.0.8+sha-91cb16f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
