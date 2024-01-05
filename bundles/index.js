f3143486412b8311e369e2f3557f4195084b3b5a
Andrew Scott <atscott@google.com>
2024-01-05 19:33:37 +0000

    a5a9b40 feat(router): Add transient info to RouterLink input (#53784)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.5+sha-5e84d9c");
+var VERSION = new Version("17.1.0-next.5+sha-a5a9b40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
