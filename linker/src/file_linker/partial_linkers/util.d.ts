1e204bef4080108e9f3f5a03ff3f6ac584fe3f0b
Andrew Scott <atscott@google.com>
2024-01-09 17:14:49 +0000

    a468a5e release: cut the zone.js-0.14.3 release (#53630)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.1.0-next.5+sha-e2b5988";
+export declare const PLACEHOLDER_VERSION = "17.1.0-next.5+sha-a468a5e";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
