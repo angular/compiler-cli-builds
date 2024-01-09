8f83fb9c01a99b777956698522600156b22b2bf3
Andrew Scott <atscott@google.com>
2024-01-09 21:27:26 +0000

    f4b1e64 refactor(zone.js): delete zone.js externs (#53445)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.8+sha-92fd6cc";
+var PLACEHOLDER_VERSION = "17.0.8+sha-f4b1e64";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
