705a328092277da5db595dc4f59886ac300cc803
Andrew Scott <atscott@google.com>
2024-01-09 20:33:21 +0000

    2dedc4a fix(compiler): generate less code for advance instructions (#53845)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.5+sha-2d7d4e2");
+var VERSION = new Version("17.1.0-next.5+sha-2dedc4a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
