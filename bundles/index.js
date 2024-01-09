a3e8579a34794d1d79058b8d2cf178068332e63d
Andrew Scott <atscott@google.com>
2024-01-09 16:56:06 +0000

    e2b5988 refactor(core): node removal should notify the scheduler (#53812)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-next.5+sha-1be6b0a");
+var VERSION = new Version("17.1.0-next.5+sha-e2b5988");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
