8d6f74fb5a12a2419d9e722474920707dff612b9
Andrew Scott <atscott@google.com>
2024-01-10 00:10:29 +0000

    b7cddf4 refactor(core): Move change detection scheduler implementation to core (#53579)

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.8+sha-f4b1e64";
+var PLACEHOLDER_VERSION = "17.0.8+sha-b7cddf4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.8+sha-f4b1e64");
+var VERSION = new Version("17.0.8+sha-b7cddf4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
