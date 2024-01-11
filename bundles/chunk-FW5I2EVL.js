c49fe984320bfb7ca0df6fc2aadb71bbb461bdbd
Andrew Scott <atscott@google.com>
2024-01-11 20:10:20 +0000

    1f8c53c fix(core): TestBed should still use the microtask queue to schedule effects (#53843)

--- bundles/chunk-FW5I2EVL.js
+++ bundles/chunk-FW5I2EVL.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-79fbc4e";
+var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-1f8c53c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
