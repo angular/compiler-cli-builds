18b65fa6b06173003d187079e63e43c259bf649a
Andrew Scott <atscott@google.com>
2024-01-11 20:11:50 +0000

    198e64a fix(core): TestBed should still use the microtask queue to schedule effects (#53843)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.9+sha-425a8d3");
+var VERSION = new Version("17.0.9+sha-198e64a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
