ee5765b6a1010497620426daac72eb4f144aab37
Dylan Hunn <dylhunn@gmail.com>
2024-01-12 18:32:31 +0000

    a2087a1 build: update cross-repo angular dependencies (#53606)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-1f8c53c");
+var VERSION = new Version("17.2.0-next.0+sha-a2087a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
