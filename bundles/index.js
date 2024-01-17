5c6dc0f393568e1874326605f8864c4e75f70f2f
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 03:23:11 +0000

    6f6ad02 ci: set up previews of adev (#53941)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-rc.0+sha-1112e91");
+var VERSION = new Version("17.1.0-rc.0+sha-6f6ad02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
