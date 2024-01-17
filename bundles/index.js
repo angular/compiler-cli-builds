53cf05616ee7e29c1e9ad04e45fc923e92bb774a
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:33 +0000

    6616019 refactor: migrate modules to prettier formatting (#53954)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0-rc.0+sha-4d4858f");
+var VERSION = new Version("17.1.0-rc.0+sha-6616019");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
