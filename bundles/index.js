82ab9e5042e885c8400408171f8e7393392bd9bb
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:23:41 +0000

    4373889 ci: use ng-dev-previews project for previews (#53958)

--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.9+sha-3363295");
+var VERSION = new Version("17.0.9+sha-4373889");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
