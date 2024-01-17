14d605148705ebe6db8835169a69665ff4ffeb47
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 22:12:33 +0000

    3c3a947 Revert "refactor(core): improve forwardRef typings"

--- bundles/chunk-HREFALT2.js
+++ bundles/chunk-HREFALT2.js
@@ -267,7 +267,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.0.9+sha-8ef626e";
+var PLACEHOLDER_VERSION = "17.0.9+sha-3c3a947";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -72,7 +72,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.0.9+sha-8ef626e");
+var VERSION = new Version("17.0.9+sha-3c3a947");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
