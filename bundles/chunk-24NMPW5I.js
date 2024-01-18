d9f4bb1b5a99021423ffb734976e10f83a29bedf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 17:12:35 +0000

    a057b61 ci: provide proper location for artifacts to allow for firebase to find the artifacts for deployment (#53960)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-c67647b";
+var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-a057b61";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
