398ae59702e37e39452afe27a0fdd42b94ee394d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 17:12:41 +0000

    8922a11 ci: provide proper location for artifacts to allow for firebase to find the artifacts for deployment (#53960)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0+sha-4e03410");
+var VERSION = new Version("17.1.0+sha-8922a11");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
