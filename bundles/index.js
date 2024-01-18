412dc164d506f4fe7b54f31d7c997fbeed81480d
Dylan Hunn <dylhunn@gmail.com>
2024-01-18 00:52:35 +0000

    e227275 refactor(devtools): Add support for signals. (#53269)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-b4c7167");
+var VERSION = new Version("17.2.0-next.0+sha-e227275");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
