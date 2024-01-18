6c290abbb292487d932bdcb9769bf960e90bed09
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 13:33:39 +0000

    4e03410 docs: add ankita (#52319)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0+sha-0616dc2";
+var PLACEHOLDER_VERSION = "17.1.0+sha-4e03410";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
