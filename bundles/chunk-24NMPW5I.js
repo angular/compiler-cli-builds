03984ee04f315f4ad436cf63c5a62e8716b6c4d9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:26:44 +0000

    62ad2f0 refactor: migrate scripts to prettier formatting (#53976)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.0+sha-424a26b";
+var PLACEHOLDER_VERSION = "17.1.0+sha-62ad2f0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
