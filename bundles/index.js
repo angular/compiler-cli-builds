744246c446e031e58e761fab5dbb594466a9843f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 19:13:16 +0000

    bbbe477 refactor: migrate animations to prettier formatting (#53977)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-711cb41");
+var VERSION = new Version("17.2.0-next.0+sha-bbbe477");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
