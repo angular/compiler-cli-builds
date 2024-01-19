24b9f432e2eac0db19f3c4041c77819d9c384331
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:16:33 +0000

    ee68df9 build: update all non-major dependencies (#53979)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-a057b61");
+var VERSION = new Version("17.2.0-next.0+sha-ee68df9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- src/bin/ngc.d.ts
+++ src/bin/ngc.d.ts
@@ -6,5 +6,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <reference types="reflect-metadata/Reflect" />
 import 'reflect-metadata';
