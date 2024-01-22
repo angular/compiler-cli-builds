5db8cbe06da1c186abfbdda664d54b7638973ac5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 18:49:18 +0000

    20811aa docs: move import statement of ngmodule from standalone component to module code #53952 (#54016)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.0+sha-df453c3");
+var VERSION = new Version("17.1.0+sha-20811aa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
