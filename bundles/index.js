d524bbc2b4af417f0bc5a8522d2d45cee38b8d82
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 16:03:41 +0000

    454d5e9 test(core): fix test with false-negative risk (#54025)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-d99bc8a");
+var VERSION = new Version("17.2.0-next.0+sha-454d5e9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
