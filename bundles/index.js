ea840f765540034d06890a0d76f8e8ba09784f03
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 18:08:13 +0000

    e603450 docs: fix & update MDN Shadow API link examples (#54040)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-98d545f");
+var VERSION = new Version("17.1.1+sha-e603450");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
