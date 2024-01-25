5ea3aef96be303dcc70034482e14c890811c9789
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:22:13 +0000

    293dc3b docs: fix reference to forwardRef in ForwardRefFn JSdoc (#53556)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-8dca5a1");
+var VERSION = new Version("17.1.1+sha-293dc3b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
