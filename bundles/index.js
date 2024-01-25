bcdb77f202a332af78e56d7e72e0ae175e332b8f
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:44:24 +0000

    d17ba9b ci: update adev approvers list (#54081)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-a4923f1");
+var VERSION = new Version("17.1.1+sha-d17ba9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
