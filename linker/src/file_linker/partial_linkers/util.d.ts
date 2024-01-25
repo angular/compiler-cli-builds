3fd12ffb2423b3511e50c0db312608cb9413fe13
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:39:26 +0000

    d8d6478 docs: testing-components-basics.md and components-basics.md links to css selector documentation are dead (#53586)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.1.1+sha-293dc3b";
+export declare const PLACEHOLDER_VERSION = "17.1.1+sha-d8d6478";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
