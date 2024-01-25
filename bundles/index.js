d9813f292a5fd9cf1296680e1e8943f4dda5e132
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 21:39:39 +0000

    9f18227 docs: bad redirection in core Injector page for providers and injection token links (#52775) PR Close #52775

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-9bb1884");
+var VERSION = new Version("17.2.0-next.0+sha-9f18227");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
