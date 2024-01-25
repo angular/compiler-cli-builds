4594af99341ec8c5d91ac66a35e1795fc721498f
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:23:16 +0000

    a4923f1 ci: add ben to reviewers for adev files (#54078)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-e603450";
+var PLACEHOLDER_VERSION = "17.1.1+sha-a4923f1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
