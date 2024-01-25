e9b960b7149928ddcfaf2dc492bb5bf1d6726a82
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:53:27 +0000

    ecd0daf docs(docs-infra): move comma in Angular Language Service section (#53759)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-d8d6478");
+var VERSION = new Version("17.1.1+sha-ecd0daf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
