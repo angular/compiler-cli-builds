8304742250076a534886463474251a53f3b10004
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:43:05 +0000

    4105ab5 docs: update i18n-common-merge.md (#52400)

--- bundles/chunk-EWSHQJRC.js
+++ bundles/chunk-EWSHQJRC.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-1a2b569";
+var PLACEHOLDER_VERSION = "17.2.0-next.0+sha-4105ab5";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
