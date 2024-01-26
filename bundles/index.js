f84a52a008ffbb0acef39a1ef4c9a0c76ce9d135
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:32:14 +0000

    1a2b569 docs: add 2 new diagnostics created recently. (#52329)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-89ab1c1");
+var VERSION = new Version("17.2.0-next.0+sha-1a2b569");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
