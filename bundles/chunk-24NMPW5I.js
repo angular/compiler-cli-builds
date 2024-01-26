00dd8173628ba5b0545e78ba3699373cd24c3b64
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:18:18 +0000

    9f9dd90 docs: Update update-to-version-16.md (#51857)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-4a5f37d";
+var PLACEHOLDER_VERSION = "17.1.1+sha-9f9dd90";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
