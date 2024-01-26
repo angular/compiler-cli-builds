8ae6673e32e12a9c00476e37bf0db01e99b9b6be
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 15:52:19 +0000

    0172477 docs(docs-infra): add if cases in templates to avoid internal marked parameters or methods to appear in the documentation (#52020)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-ecd0daf";
+var PLACEHOLDER_VERSION = "17.1.1+sha-0172477";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-ecd0daf");
+var VERSION = new Version("17.1.1+sha-0172477");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
