2facdc4c03c4d35043f0340bdb7d08a25ba1d83a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:02:15 +0000

    030d770 docs: update TypeScript version support details for Angular v15 and v16 (#54131)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-96ca49d");
+var VERSION = new Version("17.1.1+sha-030d770");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
