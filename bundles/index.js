b0ff4f1ff01f5c2d38a4c4a887bf6e8d5d822dca
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 19:35:49 +0000

    098543f docs: Update component-overview.md (#52282)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-9571287");
+var VERSION = new Version("17.2.0-next.0+sha-098543f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
