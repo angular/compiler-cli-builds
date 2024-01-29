f91c6450fc3b96f232022a09686d40c0d3a666c6
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 15:48:12 +0000

    96ca49d docs: update input/output guide links (#54135)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-5cd00f1";
+var PLACEHOLDER_VERSION = "17.1.1+sha-96ca49d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
