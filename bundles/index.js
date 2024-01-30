f406ab87fb001d3d4fd02748d85e169afc69fa51
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 20:13:56 +0000

    7001b5d refactor: migrate docs, examples, private, service worker and upgrade to prettier formatting (#54163)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-8d85de7");
+var VERSION = new Version("17.1.1+sha-7001b5d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
