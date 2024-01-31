47090e5f2354cebaa9593e48c718750a01b82137
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:39:08 +0000

    bd6d18b docs: Fix minor grammar mistake (#54094)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-3b218a7";
+var PLACEHOLDER_VERSION = "17.1.1+sha-bd6d18b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
