8b56d4e96a833281cbc399a3ae40c9060e7e2035
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:52:00 +0000

    948fb5f build: update slackapi/slack-github-action digest to 2a8087d (#52000)

--- bundles/chunk-24NMPW5I.js
+++ bundles/chunk-24NMPW5I.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.1+sha-ed8e7c7";
+var PLACEHOLDER_VERSION = "17.1.1+sha-948fb5f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.1.1+sha-ed8e7c7";
+export declare const PLACEHOLDER_VERSION = "17.1.1+sha-948fb5f";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
