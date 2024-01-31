d61809a5e84cd19a3f27bcf06f5243dcd4a4c78a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 20:16:56 +0000

    989d496 build: update cross-repo angular dependencies (#54178)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.2.0-next.1+sha-0a67085";
+export declare const PLACEHOLDER_VERSION = "17.2.0-next.1+sha-989d496";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
