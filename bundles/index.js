452238590bb92e2d7117508ad2e2c8faf4dc2279
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:27:43 +0000

    3b218a7 docs(core): fix example sections (#54121) PR Close #54121

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.1+sha-4367c44");
+var VERSION = new Version("17.1.1+sha-3b218a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
