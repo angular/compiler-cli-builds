22be893f4333e472bf48b0e9481261ece90d290d
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:02:18 +0000

    260d3ed fix(zone.js): patch `Response` methods returned by `fetch` (#50653)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.0+sha-6ec2f07");
+var VERSION = new Version("17.2.0-next.0+sha-260d3ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
