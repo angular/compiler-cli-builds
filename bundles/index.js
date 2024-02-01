24de753fde8e9d15ee2b01fee44d4dc139a62242
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 19:34:36 +0000

    de777af refactor: standardize doctype declaration to <!DOCTYPE html> (#51052)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-bc81440");
+var VERSION = new Version("17.2.0-next.1+sha-de777af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
