26ca7f298e463db1df9c20538dcc276a1068638f
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:52:46 +0000

    3cf612c fix(core): update imports to be compatible with rxjs 6 (#54193)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-908baec");
+var VERSION = new Version("17.2.0-next.1+sha-3cf612c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
