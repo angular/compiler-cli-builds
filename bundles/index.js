24f1e12f0e61ac94347fbd1a3fa837c6ae1c1e42
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 23:04:49 +0000

    bdf6927 docs:  remove outdated info about ng serve (#53890)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-32d0742");
+var VERSION = new Version("17.2.0-next.1+sha-bdf6927");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
