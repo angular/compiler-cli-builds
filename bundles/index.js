9e74b950081e3a8620bc3a1770955b94af2562d5
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 17:49:57 +0000

    46a2c5f ci: Add framework team to adev approvers (#54082)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-9e3d54c");
+var VERSION = new Version("17.1.2+sha-46a2c5f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
