e4ca024d1a1c987924b393f16e69da3435db05ed
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 20:57:41 +0000

    47c712e refactor(compiler): Show an error when a property binding duplicates an i18n attribute (#54063)

--- bundles/chunk-4ZDI4RVN.js
+++ bundles/chunk-4ZDI4RVN.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.2+sha-54deaae";
+var PLACEHOLDER_VERSION = "17.1.2+sha-47c712e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-54deaae");
+var VERSION = new Version("17.1.2+sha-47c712e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
