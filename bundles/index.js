2fdfb1eed09c97c80273abadaf9b21132504c919
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 22:05:13 +0000

    2ab536e docs: update Angular CLI help [main] (#54223)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-f600f0f");
+var VERSION = new Version("17.2.0-next.1+sha-2ab536e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
