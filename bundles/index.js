3a5c138122f0df7a94866e0698aac0170e611b25
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 14:56:17 +0000

    54deaae test(core): ExpressionChanged... error does not happen with signals (#54206)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-cd88f14");
+var VERSION = new Version("17.1.2+sha-54deaae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
