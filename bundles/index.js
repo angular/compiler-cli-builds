d028bac0be5d4cdc89768a2bed645f9b41fa6448
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 21:19:37 +0000

    f3f1f85 build: update dependency marked to v12 (#54296)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-c6896a3");
+var VERSION = new Version("17.1.2+sha-f3f1f85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
