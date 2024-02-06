6ff47a083cb94a36a3686417e756a583d6c37ef1
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 19:40:25 +0000

    2de7679 build: update dependency husky to v9 (#54289)

--- bundles/chunk-4ZDI4RVN.js
+++ bundles/chunk-4ZDI4RVN.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.2+sha-06391d1";
+var PLACEHOLDER_VERSION = "17.1.2+sha-2de7679";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
