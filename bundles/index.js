3006b22ad551427747f2dcb699893d20902c6815
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:50:55 +0000

    f303838 docs: replace event word with property for binding properties link (#54281)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-bc4a6a9");
+var VERSION = new Version("17.1.2+sha-f303838");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
