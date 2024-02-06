32238d9a34dc022907b7e268ccf005eca9119bd9
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 19:39:22 +0000

    c8a973e build: update dependency husky to v9 (#54289)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-4d29174");
+var VERSION = new Version("17.2.0-next.1+sha-c8a973e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
