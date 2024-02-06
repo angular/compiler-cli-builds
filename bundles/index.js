6e2da394e7601feec6457d02af18517c39f4e20b
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 20:39:04 +0000

    faf3b45 docs: add alejandrocuba.jpg file to images/bios/ (#53842)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-c8a973e");
+var VERSION = new Version("17.2.0-next.1+sha-faf3b45");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
