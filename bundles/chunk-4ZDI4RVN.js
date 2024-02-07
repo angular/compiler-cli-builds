2bc95242327c44442df258e1192256c042288ca4
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 15:51:36 +0000

    d695398 Revert "fix(router): Clear internal transition when navigation finalizes (#54261)" (#54317)

--- bundles/chunk-4ZDI4RVN.js
+++ bundles/chunk-4ZDI4RVN.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.1.2+sha-f3f1f85";
+var PLACEHOLDER_VERSION = "17.1.2+sha-d695398";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-f3f1f85");
+var VERSION = new Version("17.1.2+sha-d695398");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
