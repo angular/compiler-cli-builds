df67d55b70cce50063adc3a92e867dc98cca0cc2
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:16:48 +0000

    ca39926 refactor: migrate router to prettier formatting (#54319)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.3+sha-dd8490f");
+var VERSION = new Version("17.1.3+sha-ca39926");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
