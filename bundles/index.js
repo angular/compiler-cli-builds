3d2b2e91fba62faa03f4c06256222cdf00d7e0a5
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:23:49 +0000

    375e606 refactor(devtools): show tooltip for hydration icon on directive forest (#54326)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.3+sha-ca39926");
+var VERSION = new Version("17.1.3+sha-375e606");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
