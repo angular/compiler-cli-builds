25f27844b7d4b8820d87ae9e81bb0f0d4e42bc9a
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:50:20 +0000

    744e206 fix(core): Fix possible infinite loop with `markForCheck` by partially reverting #54074 (#54329)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.2+sha-63a9027");
+var VERSION = new Version("17.1.2+sha-744e206");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
