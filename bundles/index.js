08ad668748ce4df86b4789fbcf368acc1f0437b3
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-02-08 18:24:23 +0000

    c47bf63 docs: release notes for the v17.1.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-8dea3b5");
+var VERSION = new Version("17.2.0-next.1+sha-c47bf63");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
