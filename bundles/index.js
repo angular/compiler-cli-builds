c7edc3993882be059dbc562098444b8833fa104d
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:38:07 +0000

    adfc3f0 refactor(core): temporarily mark subscribe methods as deprecated (#54342)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-next.1+sha-38b01a3");
+var VERSION = new Version("17.2.0-next.1+sha-adfc3f0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
