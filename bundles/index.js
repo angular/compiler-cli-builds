56ab5018888d3bba0d2f53cf3d06ca8e01fed2e5
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-09 15:04:19 +0000

    2435291 docs(docs-infra): change deprecated modules' headers' style in the hover state (#54337)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.3+sha-375e606");
+var VERSION = new Version("17.1.3+sha-2435291");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
