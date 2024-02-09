cb50e1440c7f351614f7b9c599cc4b2a3542321c
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-09 15:04:52 +0000

    62b87b4 fix(core): do not crash for signal query that does not have any matches (#54353)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-rc.1+sha-7aa92fa");
+var VERSION = new Version("17.2.0-rc.1+sha-62b87b4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
