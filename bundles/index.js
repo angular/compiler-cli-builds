e27838ec37ac8c4aea9b467d169f1ef0df83445b
Andrew Kushnir <akushnir@google.com>
2024-02-12 19:07:53 +0000

    da93046 fix(core): show placeholder block on the server with immediate trigger (#54394)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.3+sha-017825b");
+var VERSION = new Version("17.1.3+sha-da93046");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
