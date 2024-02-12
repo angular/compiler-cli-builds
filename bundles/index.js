39b8ea9b39b1040f98a9d084250be3fc81d664ea
Andrew Kushnir <akushnir@google.com>
2024-02-12 23:43:19 +0000

    501c159 build: update node (#54399)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.0-rc.1+sha-277540c");
+var VERSION = new Version("17.2.0-rc.1+sha-501c159");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
