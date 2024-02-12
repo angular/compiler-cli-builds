972435c02801cf2ca9b121e9754a7b66d865beb3
Andrew Kushnir <akushnir@google.com>
2024-02-12 16:56:01 +0000

    017825b fix(zone.js): patch form-associated custom element callbacks (#50686)

--- bundles/index.js
+++ bundles/index.js
@@ -73,7 +73,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.1.3+sha-58365e0");
+var VERSION = new Version("17.1.3+sha-017825b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
