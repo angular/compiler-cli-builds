f392ae447463c6e3dc0bae702bf2028a4203296d
Andrew Kushnir <akushnir@google.com>
2024-02-12 23:40:40 +0000

    79001a7 build: update node (#54399)

--- bundles/chunk-OQU7V5PD.js
+++ bundles/chunk-OQU7V5PD.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.3.0-next.0+sha-383e093";
+var PLACEHOLDER_VERSION = "17.3.0-next.0+sha-79001a7";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.0+sha-383e093");
+var VERSION = new Version("17.3.0-next.0+sha-79001a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
