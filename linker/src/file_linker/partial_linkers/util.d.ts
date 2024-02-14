79d860b9a32074dfcef5b9782d5e0e0459b5189b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-02-14 18:59:47 +0000

    0b95d14 release: cut the v17.2.0 release

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.2.0-rc.1+sha-78b48cc";
+export declare const PLACEHOLDER_VERSION = "17.2.0+sha-0b95d14";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
