34c29fbcfe06ed5422f7a2f18244b96f93c6e890
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:27:23 +0000

    44a0ea4 docs: add guide for signal inputs (#54316)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.1+sha-3eda005");
+var VERSION = new Version("17.2.1+sha-44a0ea4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
