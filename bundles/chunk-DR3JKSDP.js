99d1db266c218e720f1a8b2d7c6dd9347bd5f59a
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:54:20 +0000

    96e9dbe ci: automatically deploy to angular.dev on every push to the main branch (#54436)

--- bundles/chunk-DR3JKSDP.js
+++ bundles/chunk-DR3JKSDP.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.3.0-next.0+sha-03acdbe";
+var PLACEHOLDER_VERSION = "17.3.0-next.0+sha-96e9dbe";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
