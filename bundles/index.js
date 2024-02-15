dd507f1bd1ad80364dff9b9c7a7d7659622fee4a
Andrew Kushnir <akushnir@google.com>
2024-02-15 20:26:24 +0000

    d9a1a7d fix(core): properly execute content queries for root components (#54457)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.0+sha-d181a36");
+var VERSION = new Version("17.3.0-next.0+sha-d9a1a7d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
