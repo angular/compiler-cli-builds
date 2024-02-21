45817d5b08df3f24e3d9639f234b4f58a0edee18
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:57:10 +0000

    e9246f7 refactor(docs-infra): correct location of hosted images (#54547)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.0+sha-554628e");
+var VERSION = new Version("17.3.0-next.0+sha-e9246f7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
