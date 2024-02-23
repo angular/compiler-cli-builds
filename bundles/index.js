334ee91a4af37103eda8b5231197f69cedc2f6f5
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 18:04:48 +0000

    3f95829 refactor(core): Allow dirty views to be refreshed in a loop internally (#54572)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.0+sha-707bfc9");
+var VERSION = new Version("17.3.0-next.0+sha-3f95829");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
