b9f145f4d712ef6c832dd8aec170c92f025d2d0d
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:36:01 +0000

    5712352 fix(core): collect providers from NgModules while rendering `@defer` block (#52881)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.2+sha-dcd6c8c");
+var VERSION = new Version("17.2.2+sha-5712352");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
