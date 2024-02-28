eabb0318019df90e4c5d6ef679d90991eb3ca949
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:24:52 +0000

    3f0e892 docs: add updating to v17 guide (#52410)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.3+sha-edc45d5");
+var VERSION = new Version("17.2.3+sha-3f0e892");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
