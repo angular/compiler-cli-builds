faf05d0adc357b8622bb657640c6a36bc33cb3f4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 11:53:04 +0000

    2e401c7 fix(docs-infra): include CLI reference docs in adev (#54591)

--- bundles/chunk-DXLOC6RC.js
+++ bundles/chunk-DXLOC6RC.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.3.0-next.1+sha-e923545";
+var PLACEHOLDER_VERSION = "17.3.0-next.1+sha-2e401c7";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
