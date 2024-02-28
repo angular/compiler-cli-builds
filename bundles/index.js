8830ed9022e37045eb382035e54f67a86e51b75d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:43:30 +0000

    c05bef0 docs: update versioning and deprecation policies (#51423)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.3+sha-3f0e892");
+var VERSION = new Version("17.2.3+sha-c05bef0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
