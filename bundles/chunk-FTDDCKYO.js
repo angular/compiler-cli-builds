e0fc0807bc374211685f979700328440159c1042
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 13:59:48 +0000

    37dcf8d refactor(core): remove unused `EffectRef` import statement (#53863)

--- bundles/chunk-FTDDCKYO.js
+++ bundles/chunk-FTDDCKYO.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.3+sha-ffbafc7";
+var PLACEHOLDER_VERSION = "17.2.3+sha-37dcf8d";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
