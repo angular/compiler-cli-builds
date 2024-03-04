78c2ca24b36d7f91af5cded9ba859b6d5637b496
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 18:13:10 +0000

    78e6911 fix(core): generic inference for signal inputs may break with `--strictFunctionTypes` (#54652)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.1+sha-5e32a77");
+var VERSION = new Version("17.3.0-next.1+sha-78e6911");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
