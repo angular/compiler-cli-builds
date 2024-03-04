4e5aed54bbbe81b7c3c897e3ba18a729fa20b6d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:41:34 +0000

    e3bdab1 refactor(core): Pending effects should make the application unstable (#53835)

--- bundles/chunk-FTDDCKYO.js
+++ bundles/chunk-FTDDCKYO.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.3+sha-342737e";
+var PLACEHOLDER_VERSION = "17.2.3+sha-e3bdab1";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.3+sha-342737e");
+var VERSION = new Version("17.2.3+sha-e3bdab1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
