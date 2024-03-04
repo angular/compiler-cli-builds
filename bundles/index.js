45ef5b221f5e081d97cfd4653a3e9299346b0eff
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 16:46:13 +0000

    6531e4c ci: Add primitives configurations for ng-dev tools (#54662)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.1+sha-07d5e42");
+var VERSION = new Version("17.3.0-next.1+sha-6531e4c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
