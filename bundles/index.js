f6aeafefaf64b77ae49da5e87c9b14058499a0f1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:15:48 +0000

    0ddae16 docs: queries as signals adev guide (#54505)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.1+sha-07173fd");
+var VERSION = new Version("17.3.0-next.1+sha-0ddae16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
