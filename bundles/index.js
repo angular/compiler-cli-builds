98182ecca54c1849fa62f267db81f28b0a80b3d2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-06 12:20:59 +0000

    e5885fa build: update ng-dev to avoid node-fetch usages (#54715)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0-next.1+sha-d4154f9");
+var VERSION = new Version("17.3.0-next.1+sha-e5885fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
