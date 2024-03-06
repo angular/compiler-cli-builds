ca1f9ec0a3cbb7badc6d0cdb9cf7e5df8587fb42
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-06 19:33:03 +0000

    50623ea release: cut the v17.2.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.3+sha-443e5f1");
+var VERSION = new Version("17.2.4+sha-50623ea");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
