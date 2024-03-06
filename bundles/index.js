28298bc5699781431b4e43234422ef9d160ecbbe
Andrew Scott <atscott@google.com>
2024-03-06 21:39:50 +0000

    d9c0a16 refactor(core): Fix internal test util types (#54736)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-fc0f47c");
+var VERSION = new Version("18.0.0-next.0+sha-d9c0a16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
