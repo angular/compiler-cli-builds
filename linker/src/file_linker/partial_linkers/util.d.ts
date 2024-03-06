464bd3b58e35df3176a319853db642fa99c0674c
GitHub <noreply@github.com>
2024-03-06 21:30:34 +0000

    fc0f47c release: bump the next branch to v18.0.0-next.0 (#54737)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.4.0-next.0+sha-781dbe2";
+export declare const PLACEHOLDER_VERSION = "18.0.0-next.0+sha-fc0f47c";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
