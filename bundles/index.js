952b41bc40b7f9479ce7e64bee71e5e870487ff3
Andrew Scott <atscott@google.com>
2024-03-07 19:17:57 +0000

    d888da4 fix(core): `ApplicationRef.tick` should respect OnPush for host bindings (#53718)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-8bf842d");
+var VERSION = new Version("18.0.0-next.0+sha-d888da4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
