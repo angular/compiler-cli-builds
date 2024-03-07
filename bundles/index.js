f688571d305ddd334fff99db56a723392979861e
Andrew Scott <atscott@google.com>
2024-03-07 20:46:24 +0000

    b9a0d02 refactor(core): refactor i18n node creation to prepare for hydration (#54722)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.4+sha-ea9c10e");
+var VERSION = new Version("17.2.4+sha-b9a0d02");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
