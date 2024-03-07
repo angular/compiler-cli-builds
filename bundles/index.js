323d77a9e5ff4d024e05a6b8398f8fdf61cfb458
Andrew Scott <atscott@google.com>
2024-03-07 21:11:26 +0000

    67bc88f fix(router): Navigations triggered by cancellation events should cancel previous navigation (#54710)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.4+sha-b9a0d02");
+var VERSION = new Version("17.2.4+sha-67bc88f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
