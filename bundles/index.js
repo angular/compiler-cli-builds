44d9e4289e69946201a516d134918a95fa81206c
Andrew Scott <atscott@google.com>
2024-03-07 19:12:11 +0000

    8bf842d docs: update content readme link (#53763)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-ef39107");
+var VERSION = new Version("18.0.0-next.0+sha-8bf842d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
