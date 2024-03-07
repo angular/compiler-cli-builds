491b9a8ba183a9d7a8765ccbedf0b609431a1680
Andrew Scott <atscott@google.com>
2024-03-07 19:11:45 +0000

    ea9c10e docs: update content readme link (#53763)

--- bundles/chunk-FTDDCKYO.js
+++ bundles/chunk-FTDDCKYO.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.2.4+sha-4d708b2";
+var PLACEHOLDER_VERSION = "17.2.4+sha-ea9c10e";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
