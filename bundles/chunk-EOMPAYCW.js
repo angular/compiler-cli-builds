72047ce2f529eaff3374e5f63827c7cb0088d532
Andrew Scott <atscott@google.com>
2024-03-08 00:18:26 +0000

    8180c39 build: fix size failure (#54756)

--- bundles/chunk-EOMPAYCW.js
+++ bundles/chunk-EOMPAYCW.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.0+sha-115ee88";
+var PLACEHOLDER_VERSION = "18.0.0-next.0+sha-8180c39";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
