5b3900f90fe3cc08f8e5f1117395b58746c8f9f7
Andrew Scott <atscott@google.com>
2024-03-11 19:51:41 +0000

    e27b505 fix(core): prevent infinite loops in clobbered elements check (#54425)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.4+sha-eff06ea");
+var VERSION = new Version("17.2.4+sha-e27b505");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
