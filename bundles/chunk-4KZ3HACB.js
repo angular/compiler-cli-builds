7927475f58709dab256c199860cf7165ea0f2ec6
Andrew Scott <atscott@google.com>
2024-03-11 19:52:01 +0000

    2909e98 fix(core): prevent infinite loops in clobbered elements check (#54425)

--- bundles/chunk-4KZ3HACB.js
+++ bundles/chunk-4KZ3HACB.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "17.3.0-rc.0+sha-619f3c8";
+var PLACEHOLDER_VERSION = "17.3.0-rc.0+sha-2909e98";
 var SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
