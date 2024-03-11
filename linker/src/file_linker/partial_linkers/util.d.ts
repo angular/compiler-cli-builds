30c917cf94529d59d2b255677a9933ac7c71c28e
Andrew Scott <atscott@google.com>
2024-03-11 16:23:55 +0000

    4799a3b fix(compiler): not catching for loop empty tracking expressions (#54772)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.2.4+sha-67bc88f";
+export declare const PLACEHOLDER_VERSION = "17.2.4+sha-4799a3b";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = false;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
