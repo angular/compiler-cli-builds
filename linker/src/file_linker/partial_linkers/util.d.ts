6a9b0a34620c0bbaba89a82beabc8976c5c4a5ee
Andrew Scott <atscott@google.com>
2024-03-11 20:44:29 +0000

    57e54e2 docs: bump to 2024 for copyright text (#54822)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.3.0-rc.0+sha-2909e98";
+export declare const PLACEHOLDER_VERSION = "17.3.0-rc.0+sha-57e54e2";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
