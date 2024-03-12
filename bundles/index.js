f0fd2090e204c5a885fadbf3cd9ea1ed2c589ced
Andrew Scott <atscott@google.com>
2024-03-12 20:17:14 +0000

    77329b7 docs: fix missing security guide in navigation (#54830)

--- bundles/index.js
+++ bundles/index.js
@@ -75,7 +75,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.2.4+sha-43ad12b");
+var VERSION = new Version("17.2.4+sha-77329b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
