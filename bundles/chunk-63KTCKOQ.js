71523bee7260c9481535d33115795be51b115585
Andrew Scott <atscott@google.com>
2024-03-12 17:30:34 +0000

    4ff94aa docs: improve in-depth guides information architecture (#54365)

--- bundles/chunk-63KTCKOQ.js
+++ bundles/chunk-63KTCKOQ.js
@@ -271,7 +271,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.0+sha-8735af0";
+var PLACEHOLDER_VERSION = "18.0.0-next.0+sha-4ff94aa";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
