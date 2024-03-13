9ec8072533b16844539b00e127dcfbcc61159830
Andrew Scott <atscott01@gmail.com>
2024-03-13 21:13:24 +0000

    1b19d2d docs: release notes for the v17.3.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-2d47329");
+var VERSION = new Version("18.0.0-next.0+sha-1b19d2d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
