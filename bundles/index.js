a0c384d7bfd4954274abab97270379cf7d16aca4
Andrew Scott <atscott@google.com>
2024-03-13 22:39:28 +0000

    700c052 fix(core): Update ApplicationRef.tick loop to only throw in dev mode (#54848)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-1b19d2d");
+var VERSION = new Version("18.0.0-next.0+sha-700c052");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
