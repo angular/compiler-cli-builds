641bbd226ca155a09a03c225a958942d16699974
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 23:43:25 +0000

    1f5ab96 refactor(core): allow passing an environment injector while creating a view (#54903)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.0+sha-860ecce");
+var VERSION = new Version("17.3.0+sha-1f5ab96");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
