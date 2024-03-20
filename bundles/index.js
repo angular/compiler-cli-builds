19d8f803eb01557af416d5dff0091bb805a17de3
Andrew Scott <atscott@google.com>
2024-03-20 16:20:44 +0000

    b3d045b fix(zone.js): Add 'declare' to each interface to prevent renaming (#54966)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.0+sha-2dced3a");
+var VERSION = new Version("18.0.0-next.0+sha-b3d045b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
