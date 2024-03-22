93187f60c7885366a21df0f215dc0de4e75520fd
Dylan Hunn <dylhunn@gmail.com>
2024-03-22 05:19:44 +0000

    1f7e81a refactor(http): use nullish coalescing operator in HttpRequest.clone() (#54939)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.1+sha-78d4ad2");
+var VERSION = new Version("17.3.1+sha-1f7e81a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
