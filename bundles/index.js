e02145692dfcb617db933e580a997b39a9ed4a5a
Dylan Hunn <dylhunn@gmail.com>
2024-03-25 18:22:08 +0000

    ee3bb81 refactor(common): request low quality placeholder images (#54899)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.1+sha-5bd188a");
+var VERSION = new Version("18.0.0-next.1+sha-ee3bb81");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
