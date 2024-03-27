d93fae9bb4e6d21c38c76368dbc09e3ddbc44ef2
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:03:28 +0000

    d1d9f55 docs: update compat table with 17.3 (#54882)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.1+sha-2ad5dcf");
+var VERSION = new Version("17.3.1+sha-d1d9f55");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
