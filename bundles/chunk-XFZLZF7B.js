72602758dcdcafd51643d3216f81f9a94a38aa81
Andrew Scott <atscott@google.com>
2024-03-28 19:17:40 +0000

    914e453 fix(core): test cleanup should not throw if Zone is not present (#55096)

--- bundles/chunk-XFZLZF7B.js
+++ bundles/chunk-XFZLZF7B.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.2+sha-ddbf6bb";
+var PLACEHOLDER_VERSION = "18.0.0-next.2+sha-914e453";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
