474b577170d2ae07623c444bbfcdfc6c9bb619b9
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:45:08 +0000

    6368871 fix(migrations): account for variables in imports initializer (#55081)

--- bundles/chunk-XFZLZF7B.js
+++ bundles/chunk-XFZLZF7B.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.1+sha-86a359b";
+var PLACEHOLDER_VERSION = "18.0.0-next.1+sha-6368871";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.1+sha-86a359b");
+var VERSION = new Version("18.0.0-next.1+sha-6368871");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
