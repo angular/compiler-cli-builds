30c1adabed5c28c15d73daa5c076f65c3fd26cf5
Andrew Scott <atscott@google.com>
2024-03-28 22:11:24 +0000

    878f581 docs: remove broken link from CONTRIBUTING.md (#54953)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.2+sha-9cef0d2");
+var VERSION = new Version("17.3.2+sha-878f581");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
