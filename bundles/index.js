a6eeb2995996d9aab44ac81259834f28cc82d199
Andrew Scott <atscott@google.com>
2024-03-29 16:50:29 +0000

    218c5bd build: update dependency @babel/core to v7.24.3 (#55038)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-112fae5");
+var VERSION = new Version("18.0.0-next.2+sha-218c5bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
