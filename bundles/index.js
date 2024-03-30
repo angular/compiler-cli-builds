9d731769ecbccd399813b7c476ff0c59ecb78f88
Andrew Scott <atscott@google.com>
2024-03-30 02:10:10 +0000

    a84beaa fix(docs-infra): scrolling experience in API Reference (#55133)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.2+sha-fd45d03");
+var VERSION = new Version("17.3.2+sha-a84beaa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
