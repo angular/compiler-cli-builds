61eaac7acb0c2d8cebc246402e69e26ab9f7a72b
Andrew Scott <atscott@google.com>
2024-04-01 17:04:27 +0000

    bc429ba ci: Update workflows to use latest dev infra hash (#55128)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.2+sha-a84beaa");
+var VERSION = new Version("17.3.2+sha-bc429ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
