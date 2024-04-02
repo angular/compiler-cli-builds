a3c6e6f7275f82406ed73fa758b2df9cff3c7c56
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 20:45:56 +0000

    894c1c5 refactor: apply prettier formatting to integration (#54653)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-ade0244");
+var VERSION = new Version("18.0.0-next.2+sha-894c1c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
