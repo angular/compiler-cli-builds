dd48d32544d8eb8936f6f259d0c3e2d2ff085aba
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 16:13:23 +0000

    666d646 feat(core): Add event delegation library to queue up events and replay them when the application is ready (#55121)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-840c375");
+var VERSION = new Version("18.0.0-next.2+sha-666d646");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
