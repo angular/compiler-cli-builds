b9d2d322c5b3c67ae75f23e7b55cf57d1aa46d27
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 18:37:34 +0000

    1991508 ci: bump ng-dev hashes (#55172)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-58942db");
+var VERSION = new Version("18.0.0-next.2+sha-1991508");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
