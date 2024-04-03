aa054fab828091f62c22fa4f2dd81a9ddcdab09b
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:31:34 +0000

    6f960af ci: fixes jsaction path name (#55199)

--- bundles/chunk-45SUUHP7.js
+++ bundles/chunk-45SUUHP7.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.2+sha-2656fd9";
+var PLACEHOLDER_VERSION = "18.0.0-next.2+sha-6f960af";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-2656fd9");
+var VERSION = new Version("18.0.0-next.2+sha-6f960af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
