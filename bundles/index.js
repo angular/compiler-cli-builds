ea3f1ba1abd09e4ba34d2c9d023e639699a45486
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-03 23:09:37 +0000

    05e7bf8 ci: jessicajaniuk is thePunderWoman (#55205)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.3+sha-16eba15");
+var VERSION = new Version("17.3.3+sha-05e7bf8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
