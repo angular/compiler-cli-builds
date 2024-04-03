88a9bbaf47f7fc066faf306d944be9d5dcf8d9ef
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 17:16:25 +0000

    1c736dc feat(forms): Unified Control State Change Events (#54579)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.2+sha-2ff74dc");
+var VERSION = new Version("18.0.0-next.2+sha-1c736dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
