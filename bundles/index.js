305c4f4c192c566488d243633cca42005dc4204f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 04:31:22 +0000

    9cb1d27 ci: pullapprove is case sensitive (#55220)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.3+sha-ee0412f");
+var VERSION = new Version("17.3.3+sha-9cb1d27");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
