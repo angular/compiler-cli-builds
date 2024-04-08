60abd71cf0bf7058d7dcf09529fd161bb3475f31
Andrew Scott <atscott@google.com>
2024-04-08 18:54:17 +0000

    87cdfaf docs: replace `code-example` with `docs-code` (#55186)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.3+sha-f8d5921");
+var VERSION = new Version("18.0.0-next.3+sha-87cdfaf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
