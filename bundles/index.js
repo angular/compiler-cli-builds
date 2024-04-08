12f29f18319eca7e5dfe9027c582ae8fc5e458df
Andrew Scott <atscott@google.com>
2024-04-08 18:40:39 +0000

    4d2a800 docs(docs-infra): move CLI docs to ADEV (#55242)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.3+sha-80d1094");
+var VERSION = new Version("17.3.3+sha-4d2a800");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
