c0ee2e2f2e707bb9b14700fba7bd88bdf9b1af29
Andrew Scott <atscott@google.com>
2024-04-08 20:46:38 +0000

    a7c1115 build: update io_bazel_rules_sass digest to 147f7fa (#55208)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.3+sha-9f06915");
+var VERSION = new Version("17.3.3+sha-a7c1115");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
