6fee03c0b3b487b228679e3274cf97a01713f9a5
Andrew Scott <atscott@google.com>
2024-04-09 19:22:00 +0000

    545fb0d docs(forms): validate email field with proper validator (#55142)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "17.3.3+sha-d0ae556";
+export declare const PLACEHOLDER_VERSION = "17.3.3+sha-545fb0d";
 export declare const SHOULD_USE_TEMPLATE_PIPELINE_FOR_LINKER = true;
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
