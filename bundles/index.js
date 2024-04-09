4280b4829773ed21d7eb414e5be6bb30a5a62380
Andrew Scott <atscott@google.com>
2024-04-09 18:27:24 +0000

    bc394de docs: fix input name (#55141)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.3+sha-3236111");
+var VERSION = new Version("18.0.0-next.3+sha-bc394de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
