7d0e5e1c1b3364e6a6f4af6789a6d63ceb402f4c
Andrew Scott <atscott@google.com>
2024-04-12 17:18:52 +0000

    f947dd5 docs: fix repeated note in "Add an Input to the component" section (#55306)

--- bundles/chunk-5YGSPVK5.js
+++ bundles/chunk-5YGSPVK5.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-5c29416";
+var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-f947dd5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
