2fabb2b212e70ae49c6d74b6cb8df7b7605ce3c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:41:19 +0000

    9bf9864 build(docs-infra): fix playground lezer parsing (#55349)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.4+sha-a3b1dc0");
+var VERSION = new Version("17.3.4+sha-9bf9864");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
