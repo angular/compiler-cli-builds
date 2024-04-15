a81d84e9059e21270f046244d0e276686eba0cfa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:15:59 +0000

    9f7b9ed refactor(core): warn about duplicated keys when using built-in @for (#55243)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-cfed97d");
+var VERSION = new Version("18.0.0-next.4+sha-9f7b9ed");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
