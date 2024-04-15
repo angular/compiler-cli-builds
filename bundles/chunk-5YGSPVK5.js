7baeeca831d7c355f4dd1d5bd7249515c5538146
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 09:13:46 +0000

    3af54d5 docs: fix links to output function (#55335)

--- bundles/chunk-5YGSPVK5.js
+++ bundles/chunk-5YGSPVK5.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-571a61b";
+var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-3af54d5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-571a61b");
+var VERSION = new Version("18.0.0-next.4+sha-3af54d5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
