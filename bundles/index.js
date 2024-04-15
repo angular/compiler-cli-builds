6f117dfc47179cbba308ea03a4cb890f0d6c7a3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 12:23:54 +0000

    cfed97d docs: update public APIs golden after doc changes (#55346)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-5eda3bf");
+var VERSION = new Version("18.0.0-next.4+sha-cfed97d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
