c0dbd2ac5b61513b5ebe6e3451fd262d4d7f6ef6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-16 08:29:23 +0000

    7d5bc1c fix(compiler): remove container index from conditional instruction (#55190)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-9afa2ea");
+var VERSION = new Version("18.0.0-next.4+sha-7d5bc1c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
