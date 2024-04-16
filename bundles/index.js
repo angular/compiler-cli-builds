9c83f988a95223b9876cfd2c650c2571b3514893
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-16 18:56:38 +0000

    f09c5a7 feat(core): Add zoneless change detection provider as experimental (#55329)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-3bc63ea");
+var VERSION = new Version("18.0.0-next.4+sha-f09c5a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
