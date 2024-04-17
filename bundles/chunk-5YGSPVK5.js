af9942dcd389de172146326593917e6ca95505f9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 08:41:39 +0000

    9f5e90e build: make JSA contract binary accessible (#55361)

--- bundles/chunk-5YGSPVK5.js
+++ bundles/chunk-5YGSPVK5.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-ff686f3";
+var PLACEHOLDER_VERSION = "18.0.0-next.4+sha-9f5e90e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
