01c7dd6093407dfc5cbf0fccca781a0bba49424a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-17 00:15:03 +0000

    a5623dc Revert "refactor(core): warn about duplicated keys when using built-in @for (#55243)" (#55373)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-e6425f7");
+var VERSION = new Version("18.0.0-next.4+sha-a5623dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
