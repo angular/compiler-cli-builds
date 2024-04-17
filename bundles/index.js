e2f5f910a508ce48b0ca615a6f401ba7329927e2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 09:33:50 +0000

    a34267b refactor(core): Switch stopPropagation default to false (#55351)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.4+sha-9f5e90e");
+var VERSION = new Version("18.0.0-next.4+sha-a34267b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
