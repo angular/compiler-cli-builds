a34dfb84260139de53de0f8981223d98b2557541
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-19 20:53:44 +0000

    b1dffa4 refactor: migrate platform-* to prettier formatting (#55423)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-a743d37");
+var VERSION = new Version("18.0.0-next.5+sha-b1dffa4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
