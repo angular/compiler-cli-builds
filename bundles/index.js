9f50bd78079c2f40970b615522d0e27db1130bc8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 19:06:32 +0000

    5cf14da fix(core): make `ActivatedRoute` inject correct instance inside `@defer` blocks (#55374)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.5+sha-2c66afe");
+var VERSION = new Version("17.3.5+sha-5cf14da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
