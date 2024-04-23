29c50f504815a8554d98aac373ee316d518e7f47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 19:27:44 +0000

    d34c033 refactor(common): Deprecate Local Data API functions (#54483)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-f914f6a");
+var VERSION = new Version("18.0.0-next.5+sha-d34c033");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
