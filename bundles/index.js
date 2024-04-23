45c8f5add3e7a9b88777895240cff3e2d3136d1a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 18:10:01 +0000

    3d84c98 refactor(core): Make some minor changes to facilitate testing and usage of event-dispatch. (#55411)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-f84d7dd");
+var VERSION = new Version("18.0.0-next.5+sha-3d84c98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
