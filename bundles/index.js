3cff415e8d16a0fa601dd0e035b2ac9e1d84528a
Andrew Kushnir <akushnir@google.com>
2024-04-23 23:12:48 +0000

    811fe00 refactor(core): Replay events from the event contract using the dispatcher. (#55467)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-2e2ca5e");
+var VERSION = new Version("18.0.0-next.5+sha-811fe00");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
