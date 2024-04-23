b7362c86e19b8fba362fcb7a28c197f5380f6a66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 20:20:54 +0000

    c773238 docs: Fix typo in waitForAsync example (#55407)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-5948193");
+var VERSION = new Version("18.0.0-next.5+sha-c773238");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
