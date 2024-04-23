184be0c6741dc0d50b599a6be80f2bbef13bbc08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 00:10:45 +0000

    f84d7dd test(core): Wait for zone stability before test completion (#55468)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.5+sha-9894278");
+var VERSION = new Version("18.0.0-next.5+sha-f84d7dd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
