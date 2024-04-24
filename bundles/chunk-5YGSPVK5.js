554d2f875a11b3629ddc5455a61f0543011358ca
Andrew Kushnir <akushnir@google.com>
2024-04-24 17:39:27 +0000

    9ab36cf refactor(animations): Ensure async animations applies changes when loaded in zoneless (#55132)

--- bundles/chunk-5YGSPVK5.js
+++ bundles/chunk-5YGSPVK5.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.5+sha-4bf3d89";
+var PLACEHOLDER_VERSION = "18.0.0-next.5+sha-9ab36cf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
