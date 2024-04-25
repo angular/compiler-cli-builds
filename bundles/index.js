7bc71581056637beb7a6c0f3c52811b0833c1791
Andrew Kushnir <akushnir@google.com>
2024-04-25 16:11:40 +0000

    a177abe fix(core): render hooks should not specifically run outside the Angular zone (#55399)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.6+sha-5ad2f5f");
+var VERSION = new Version("18.0.0-next.6+sha-a177abe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
