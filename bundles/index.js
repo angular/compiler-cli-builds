c71854c58648f70404daeb60bb04d889969b5bab
Andrew Kushnir <akushnir@google.com>
2024-04-25 21:36:09 +0000

    9ec0136 refactor(core): Do not duplicate change detection with run coalescing (part 2) (#55403)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.6+sha-a80fa8d");
+var VERSION = new Version("18.0.0-next.6+sha-9ec0136");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
