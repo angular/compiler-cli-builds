2b4f8b45c5f992b46f2323d20a2df461f7650a33
Andrew Kushnir <akushnir@google.com>
2024-04-25 15:16:20 +0000

    add11c6 build: make scripts for compatibility with Node.js 18.20+ (#55526)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.5+sha-5083d89");
+var VERSION = new Version("17.3.5+sha-add11c6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
