2010aa89b8df885fc3bc95bcb5319015dfc2d145
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:37:30 +0000

    f93e518 fix(migrations): resolve multiple structural issues with HttpClient migration (#55557)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.6+sha-7a97678");
+var VERSION = new Version("18.0.0-next.6+sha-f93e518");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
