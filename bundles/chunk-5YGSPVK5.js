a55b0396f352a68a1d6c6ba6074a4b978132507a
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:46:27 +0000

    be17de5 refactor(core): Permit disabling autoDetect for zoneless fixture (#55494)

--- bundles/chunk-5YGSPVK5.js
+++ bundles/chunk-5YGSPVK5.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-91b1f24";
+var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-be17de5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
