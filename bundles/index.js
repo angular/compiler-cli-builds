d58e7b4ad83d483bc1c60774a2eb2f22490e2032
Andrew Kushnir <akushnir@google.com>
2024-04-29 19:02:01 +0000

    0dcae69 refactor(compiler): add `handler` attribute to XMB output (#54865)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.6+sha-89dfb24");
+var VERSION = new Version("18.0.0-next.6+sha-0dcae69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
