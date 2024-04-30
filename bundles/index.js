8c19322882be045e8df0e1aff255723a19be8ff7
Andrew Kushnir <akushnir@google.com>
2024-04-30 22:44:56 +0000

    122478c refactor(core): Ensure animations are flushed before running render hooks (#55564)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-next.6+sha-354cd9c");
+var VERSION = new Version("18.0.0-next.6+sha-122478c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
