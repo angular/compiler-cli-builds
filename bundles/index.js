59aadcbcdb06698a249730412e63652666feca1d
Andrew Kushnir <akushnir@google.com>
2024-04-30 21:07:24 +0000

    266ad40 docs: Reference page anchor scroll consistency (#55566)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.6+sha-4261fd3");
+var VERSION = new Version("17.3.6+sha-266ad40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
