69fe0c7a6cd6d3cb5664b17bb20c450b4c615430
Andrew Kushnir <akushnir@google.com>
2024-05-01 00:06:15 +0000

    78e4af0 release: bump DevTools to 1.0.14 (#55545)

--- bundles/chunk-3UXY73FR.js
+++ bundles/chunk-3UXY73FR.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-next.6+sha-122478c";
+var PLACEHOLDER_VERSION = "18.0.0-next.6+sha-78e4af0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
