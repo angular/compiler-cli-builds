1206b914e08039896967629efa7ec9e76993db75
Andrew Kushnir <akushnir@google.com>
2024-05-01 00:06:10 +0000

    82bbcde release: bump DevTools to 1.0.14 (#55545)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.6+sha-266ad40");
+var VERSION = new Version("17.3.6+sha-82bbcde");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
