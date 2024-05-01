998795f18a2d9182f34fea63cf3d0649212f1b37
Andrew Kushnir <akushnir@google.com>
2024-05-01 19:00:21 +0000

    b9a997c refactor(core): Remove STOP_PROPAGATION define. (#55582)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-874d332");
+var VERSION = new Version("18.1.0-next.0+sha-b9a997c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
