e32d48a1b38362f504eb1e06f874c7a42dc7d15c
Andrew Kushnir <akushnir@google.com>
2024-05-02 18:17:57 +0000

    91b007e fix(compiler): add math elements to schema (#55631)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.0+sha-243f573");
+var VERSION = new Version("18.0.0-rc.0+sha-91b007e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
