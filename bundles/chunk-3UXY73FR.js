8459d6ad7dd14f4381cfaec6fa590707735ef032
Andrew Kushnir <akushnir@google.com>
2024-05-02 18:05:13 +0000

    243f573 Revert "fix(core): render hooks should not specifically run outside the Angular zone (#55399)" (#55624)

--- bundles/chunk-3UXY73FR.js
+++ bundles/chunk-3UXY73FR.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-rc.0+sha-36130b2";
+var PLACEHOLDER_VERSION = "18.0.0-rc.0+sha-243f573";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
