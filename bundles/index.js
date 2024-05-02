471e6158cbb4b93240ebd3928f1a65cebc94b387
Andrew Kushnir <akushnir@google.com>
2024-05-02 19:39:45 +0000

    afd87ac refactor(core): Turn off JSNAMESPACE_SUPPORT by default (#55598)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-2e891ad");
+var VERSION = new Version("18.1.0-next.0+sha-afd87ac");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
