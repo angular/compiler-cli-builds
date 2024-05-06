73240acbd1b2f031c29ca5a68eb2bd5866acf4d2
Andrew Kushnir <akushnir@google.com>
2024-05-06 23:07:59 +0000

    77ae5a3 refactor(devtools): remove git sha stamping from devtools build (#55694)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-8795374");
+var VERSION = new Version("18.1.0-next.0+sha-77ae5a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
