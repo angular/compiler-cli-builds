0f1deafaa7bf525c37da0acd8f63f4ed628e441c
Andrew Kushnir <akushnir@google.com>
2024-05-06 20:37:09 +0000

    7330b69 fix(core): TestBed should not override NgZone from initTestEnvironment (#55226)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-3e1d6e9");
+var VERSION = new Version("18.1.0-next.0+sha-7330b69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
