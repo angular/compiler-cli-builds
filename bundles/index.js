dfd6602bbc8e7c7cba178589a238bbb92daf4066
Andrew Scott <atscott@google.com>
2024-05-08 21:05:20 +0000

    f9aac83 refactor(core): a couple of minor changes to the early-event-contract (#55704)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.1+sha-84f1639");
+var VERSION = new Version("18.0.0-rc.1+sha-f9aac83");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "18.0.0-rc.1+sha-84f1639";
+export declare const PLACEHOLDER_VERSION = "18.0.0-rc.1+sha-f9aac83";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
