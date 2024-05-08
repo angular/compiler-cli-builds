a85a6b99bc127c5b7982bc89ae2a8335a95aa81f
Andrew Scott <atscott@google.com>
2024-05-08 18:16:39 +0000

    84f70cc release: bump Angular DevTools to version 1.0.16 (#55715)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("17.3.8+sha-99bf2cd");
+var VERSION = new Version("17.3.8+sha-84f70cc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
