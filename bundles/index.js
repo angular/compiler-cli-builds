631cb4ba8238299cf6506aad9dcfdfc1ac77776e
Andrew Scott <atscott@google.com>
2024-05-13 19:53:25 +0000

    9d1acd6 build: switch from rollup and terser to esbuild for creating contract bundle (#55705)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.1+sha-dc0c55c");
+var VERSION = new Version("18.0.0-rc.1+sha-9d1acd6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
