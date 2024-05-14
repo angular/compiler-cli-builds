7b8237ad7c19ced920c7072f132ecc837c2a1b83
Andrew Scott <atscott@google.com>
2024-05-14 16:43:39 +0000

    bfb5f2b refactor(core): Simplify event handler extraction logic. (#55747)

--- bundles/chunk-LHWY5IQU.js
+++ bundles/chunk-LHWY5IQU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.0-rc.1+sha-9d1acd6";
+var PLACEHOLDER_VERSION = "18.0.0-rc.1+sha-bfb5f2b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
