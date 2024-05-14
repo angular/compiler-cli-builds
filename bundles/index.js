985990894c86c66444f5b182184fdafa5fcc7239
Andrew Scott <atscott@google.com>
2024-05-14 18:17:56 +0000

    e205dcb ci: release angular.dev on each commit (#55792)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.1+sha-beedb68");
+var VERSION = new Version("18.0.0-rc.1+sha-e205dcb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
