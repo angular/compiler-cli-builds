b5f729a069865efbe2f0d7a7e4add12b314b8d9e
Andrew Scott <atscott@google.com>
2024-05-14 19:25:49 +0000

    56fb556 docs: fix production mode broken link (#55794)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-f736bea");
+var VERSION = new Version("18.1.0-next.0+sha-56fb556");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
