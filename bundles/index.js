65b3e686fcb79bf919e9b227885ab3ea332f1f8f
Andrew Scott <atscott@google.com>
2024-05-14 19:56:29 +0000

    6850ff1 ci: properly rely on adev for the adev-deploy job (#55797)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.1+sha-55d6942");
+var VERSION = new Version("18.0.0-rc.1+sha-6850ff1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
