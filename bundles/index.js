9f95007c9aaea07ac4df0f8bdcb9e1ad4dd1088d
Andrew Scott <atscott@google.com>
2024-05-14 18:53:11 +0000

    1fd63e9 refactor(core): deprecate `@Component.interpolation` (#55778)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.0-rc.1+sha-e205dcb");
+var VERSION = new Version("18.0.0-rc.1+sha-1fd63e9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
