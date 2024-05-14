6a1e9ea6b5d579873c2aba1f4cabc305f754af81
Andrew Scott <atscott@google.com>
2024-05-14 22:21:05 +0000

    f1e3ec2 refactor(core): Add an ActionResolver option to Dispatcher. (#55757)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-caedd10");
+var VERSION = new Version("18.1.0-next.0+sha-f1e3ec2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
