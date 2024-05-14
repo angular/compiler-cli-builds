c592b9b26e4a9334937b1769e22451e0f2db7d38
Andrew Scott <atscott@google.com>
2024-05-14 18:01:39 +0000

    af065cc build: update cross-repo angular dependencies (#55781)

--- bundles/chunk-3FAMHPS7.js
+++ bundles/chunk-3FAMHPS7.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-3ad120d";
+var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-af065cc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
