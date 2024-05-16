ae185145ab83363b71c3d20748856150b930d449
Andrew Scott <atscott@google.com>
2024-05-16 16:20:16 +0000

    f3b0e2f refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- bundles/chunk-3FAMHPS7.js
+++ bundles/chunk-3FAMHPS7.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-3d5c3d9";
+var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-f3b0e2f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.0+sha-3d5c3d9");
+var VERSION = new Version("18.1.0-next.0+sha-f3b0e2f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
