dce9d34a552e7140d6ff018216ae3a38da268eb0
Dylan Hunn <dylhunn@gmail.com>
2024-05-17 00:36:51 +0000

    1360110 release: cut the zone.js-0.14.6 release (#55845)

--- bundles/chunk-3FAMHPS7.js
+++ bundles/chunk-3FAMHPS7.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-400911e";
+var PLACEHOLDER_VERSION = "18.1.0-next.0+sha-1360110";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
