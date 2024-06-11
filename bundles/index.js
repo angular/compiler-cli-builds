7e85999e3789ee7bd63a1f128ef3851334e13ea8
Joey Perrott <josephperrott@gmail.com>
2024-06-11 17:15:35 +0000

    25a5373 docs(docs-infra): guard access to `navigator` for prerendering (#56333)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.1+sha-0287ae5");
+var VERSION = new Version("18.1.0-next.1+sha-25a5373");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
