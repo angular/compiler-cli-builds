ea905b8a99a566bd6f86769dd2387bfa5c592686
Joey Perrott <josephperrott@gmail.com>
2024-06-11 17:55:41 +0000

    65bf5ec ci: remove tmp directory after publishing artifacts (#56385)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.2+sha-5717ff6");
+var VERSION = new Version("18.0.2+sha-65bf5ec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
