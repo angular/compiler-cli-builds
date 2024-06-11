6ad66702e21d885405ff46b7bdf82955b7886b7f
Andrew Kushnir <akushnir@google.com>
2024-06-11 22:18:11 +0000

    dbd0fa0 fix(core): async EventEmitter should contribute to app stability (#56308)

--- bundles/chunk-46XI27YO.js
+++ bundles/chunk-46XI27YO.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.2+sha-5ec24c9";
+var PLACEHOLDER_VERSION = "18.0.2+sha-dbd0fa0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
