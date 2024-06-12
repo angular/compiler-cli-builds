27700b78e1e50afb0259edd8bd6bef4f436ba5b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 19:05:40 +0000

    70157aa docs(service-worker): fix formatting of directory structure example (#56412)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.1+sha-96d3ed8");
+var VERSION = new Version("18.1.0-next.1+sha-70157aa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
