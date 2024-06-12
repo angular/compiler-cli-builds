f8226b06346c3596183b737fa073852a21a900cc
Andrew Kushnir <akushnir@google.com>
2024-06-12 00:25:02 +0000

    160171e docs(docs-infra): generate valid HTML for the update guide. (#56390)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.1+sha-b3780b8");
+var VERSION = new Version("18.1.0-next.1+sha-160171e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
