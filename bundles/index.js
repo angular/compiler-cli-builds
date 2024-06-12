6f707c401960176866a3036e12f7da5b79cc1085
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:40:58 +0000

    f4b86bd docs: fix configure routes link (#56415)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.1+sha-5cc9e80");
+var VERSION = new Version("18.1.0-next.1+sha-f4b86bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
