9e654b4161c4965a756bd2123386b74cd6b4ca63
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 23:25:24 +0000

    38b8400 docs: release notes for the v18.0.3 release

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "18.1.0-next.1+sha-f4b86bd";
+export declare const PLACEHOLDER_VERSION = "18.1.0-next.1+sha-38b8400";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
