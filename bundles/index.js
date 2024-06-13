e92d833aeedf24c79b14d3fdfb2b2a3ed10f330a
Andrew Kushnir <akushnir@google.com>
2024-06-13 21:03:53 +0000

    1d3a752 feat(router): Set a different browser URL from the one for route matching (#53318)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.2+sha-fca5764");
+var VERSION = new Version("18.1.0-next.2+sha-1d3a752");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
