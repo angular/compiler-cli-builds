817b7779585255c2860dc64c809a74ac601a9eae
Andrew Kushnir <akushnir@google.com>
2024-06-14 20:54:40 +0000

    ee8ecfa ci: begin checking licenses of upstream dependencies to ensure they are approved (#56460)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.2+sha-d64ee6c");
+var VERSION = new Version("18.1.0-next.2+sha-ee8ecfa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
