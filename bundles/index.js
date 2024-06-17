ff9811a18e7e1e3be25f1aa2a01a4d72ded9923e
Andrew Kushnir <akushnir@google.com>
2024-06-17 15:58:57 +0000

    08038e8 docs: update security guide link to adev (#56469)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.2+sha-5a45d2b");
+var VERSION = new Version("18.1.0-next.2+sha-08038e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
