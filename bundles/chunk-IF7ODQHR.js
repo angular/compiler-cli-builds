4167beadf51e7e446104f92a495db611775842e3
Andrew Kushnir <akushnir@google.com>
2024-06-17 19:52:38 +0000

    841476d docs(docs-infra): Remove redundant API link (#56480)

--- bundles/chunk-IF7ODQHR.js
+++ bundles/chunk-IF7ODQHR.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.3+sha-1d48e72";
+var PLACEHOLDER_VERSION = "18.0.3+sha-841476d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.3+sha-1d48e72");
+var VERSION = new Version("18.0.3+sha-841476d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
