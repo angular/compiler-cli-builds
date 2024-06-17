76b38cb433093399ffed8babbdbd1ed8bba31094
Andrew Kushnir <akushnir@google.com>
2024-06-17 19:53:07 +0000

    67b2c33 fix(language-service): import the default exported component correctly (#56432)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.2+sha-89ec195");
+var VERSION = new Version("18.1.0-next.2+sha-67b2c33");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
