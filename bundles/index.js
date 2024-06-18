38e484fbf2b0ae146d75c2c213b8cba588d5ee72
Andrew Kushnir <akushnir@google.com>
2024-06-18 16:35:37 +0000

    775f451 docs: add `ng generate directive` page (#56417)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.2+sha-67b2c33");
+var VERSION = new Version("18.1.0-next.2+sha-775f451");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
