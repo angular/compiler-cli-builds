88048c7555ee3b691c68d7b88226433d4ccd55c2
Andrew Kushnir <akushnir@google.com>
2024-06-20 22:05:12 +0000

    be9e489 fix(core): improve support for i18n hydration of projected content (#56192)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.4+sha-2e58620");
+var VERSION = new Version("18.0.4+sha-be9e489");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
