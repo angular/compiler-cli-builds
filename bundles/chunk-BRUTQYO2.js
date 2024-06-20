0f71aafcee7c13229be81d47f975152c60352470
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-06-20 19:06:35 +0000

    34c3bd3 docs: release notes for the v18.0.4 release

--- bundles/chunk-BRUTQYO2.js
+++ bundles/chunk-BRUTQYO2.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.2+sha-0d52c6b";
+var PLACEHOLDER_VERSION = "18.1.0-next.2+sha-34c3bd3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
