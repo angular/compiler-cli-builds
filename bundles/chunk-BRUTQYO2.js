73d50bdc4680647c97f732d4644b2431b60ea876
Andrew Kushnir <akushnir@google.com>
2024-06-21 17:09:46 +0000

    49b2e65 docs: fix file structure link (#56542)

--- bundles/chunk-BRUTQYO2.js
+++ bundles/chunk-BRUTQYO2.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.3+sha-29ca6d1";
+var PLACEHOLDER_VERSION = "18.1.0-next.3+sha-49b2e65";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
