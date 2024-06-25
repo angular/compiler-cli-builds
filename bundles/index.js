888725df7a195bb83a8b677eafc66a72962ab7be
Dylan Hunn <dylhunn@gmail.com>
2024-06-25 16:21:43 +0000

    5be16d0 fix(core): prevent calling devMode only function on `@defer` error. (#56559)

--- bundles/index.js
+++ bundles/index.js
@@ -78,7 +78,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.3+sha-e81abdb");
+var VERSION = new Version("18.1.0-next.3+sha-5be16d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
