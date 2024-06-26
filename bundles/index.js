436c2ab7066af1417ecebddee74ea827539ee545
Dylan Hunn <dylhunn@gmail.com>
2024-06-26 19:44:25 +0000

    95f118b docs(docs-infra): fix asset path for learn angular #11 (#56587)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.4+sha-f49a2a4");
+var VERSION = new Version("18.0.4+sha-95f118b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
