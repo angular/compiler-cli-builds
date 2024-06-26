4a750aa9af0503c5e1f98b920f3eae5950f25a01
Dylan Hunn <dylhunn@gmail.com>
2024-06-26 15:58:49 +0000

    c19a4e7 docs: Move the background image migration to its own section. (#56588)

--- bundles/chunk-O4QUHILC.js
+++ bundles/chunk-O4QUHILC.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.3+sha-ccc8c80";
+var PLACEHOLDER_VERSION = "18.1.0-next.3+sha-c19a4e7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
