3e870d34436b504ed302d5f21a6609f9a3fd4b0b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 21:22:36 +0000

    ceaaa98 build: update to latest docs tooling to resolve missing answers (#56746)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-ed2665a");
+var VERSION = new Version("18.1.0-next.4+sha-ceaaa98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
