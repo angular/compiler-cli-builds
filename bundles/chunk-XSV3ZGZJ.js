971146d89d357acb121f2104e4bddd033f22799a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 17:49:20 +0000

    62970ca Revert "fix(common): Don't run preconnect assertion on the server. (#56213)" (#56740)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.5+sha-11f066b";
+var PLACEHOLDER_VERSION = "18.0.5+sha-62970ca";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-11f066b");
+var VERSION = new Version("18.0.5+sha-62970ca");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
