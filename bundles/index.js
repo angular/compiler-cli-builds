c8014994e1ef57f778ae6369ddb716ccfddf0e7d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 19:59:29 +0000

    fa9da56 docs: fix indentation on control flow documentation page (#56748)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-be6d9c5");
+var VERSION = new Version("18.0.5+sha-fa9da56");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
