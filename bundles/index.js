042921ab7ef321c067d24e19d2831b0a9c89131d
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:17:44 +0000

    a59496c release: cut the v18.0.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.4+sha-81e6a54");
+var VERSION = new Version("18.0.5+sha-a59496c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
