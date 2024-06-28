75bea0c18c2e169a8e1a0f5ee6a73a064758dfa9
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-28 18:42:28 +0000

    f25653e fix(common): typo in NgOptimizedImage warning (#56756)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-ceaaa98");
+var VERSION = new Version("18.1.0-next.4+sha-f25653e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
