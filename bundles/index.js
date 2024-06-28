afdff38c007d3113a54490ecbaa74334a122fa34
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-28 21:11:56 +0000

    ecd5e6b ci: update pullapprove primitives list (#56761)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-f25653e");
+var VERSION = new Version("18.1.0-next.4+sha-ecd5e6b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
