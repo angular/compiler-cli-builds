1dc26c3890c95c47c45a7a5e36c882b5ddd6966b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 13:47:53 +0000

    5dc6dec build: update docs infra package (#56758)

--- bundles/chunk-GLVFZWNB.js
+++ bundles/chunk-GLVFZWNB.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-next.4+sha-ecd5e6b";
+var PLACEHOLDER_VERSION = "18.1.0-next.4+sha-5dc6dec";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
