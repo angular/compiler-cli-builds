bd5860efdb22c4829fe699f8f6b40ff4c678e767
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 18:53:27 +0000

    b4cc8d4 ci: upload build artifacts on each pr run (#56787)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-f407e3c");
+var VERSION = new Version("18.1.0-next.4+sha-b4cc8d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
