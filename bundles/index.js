cee7f2cf3fc0d4896ba0cd44da7adc93c9d7d926
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 19:41:50 +0000

    9cbfc6b docs: update 18.1.0 release date (#56789)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-1156b19");
+var VERSION = new Version("18.0.5+sha-9cbfc6b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
