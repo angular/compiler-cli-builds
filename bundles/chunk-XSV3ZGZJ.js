77b51c2474508920829e855ee0832e6ee4080a8b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 13:47:59 +0000

    deedc47 build: update docs infra package (#56758)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.5+sha-387ca41";
+var PLACEHOLDER_VERSION = "18.0.5+sha-deedc47";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
