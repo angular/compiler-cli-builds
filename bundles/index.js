dae474fb7fb0ec98624e483b75a44ddb62152515
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 17:36:31 +0000

    551027e refactor(core): Set the jsaction cache directly instead of using jsaction attribute. (#56747)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-46b0292");
+var VERSION = new Version("18.1.0-next.4+sha-551027e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
