940582b51a808b04856a9b7b72210759f56e5a75
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-07-01 21:08:08 +0000

    e111e31 docs: release notes for the v18.1.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-next.4+sha-3d1bc5a");
+var VERSION = new Version("18.2.0-next.0+sha-e111e31");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
