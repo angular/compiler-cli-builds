f7fabac40ec26dfd2921ec9269be91eeb95784bf
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 20:37:59 +0000

    fdd8fb0 refactor(core): Add an ngOnDestroy to GlobalEventDelegation. (#56762)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-9cbfc6b");
+var VERSION = new Version("18.0.5+sha-fdd8fb0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
