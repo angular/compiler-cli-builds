3d26bd75308eec481a861e11faf172c7d2b101ba
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 16:48:37 +0000

    9acccb1 docs: Update interceptors.md to replace `public class` with `export class` in DI section (#56807)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-58be8e6");
+var VERSION = new Version("18.1.0-rc.0+sha-9acccb1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
