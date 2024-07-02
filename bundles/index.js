e3189a043ea9766b475048ece35755d941451d45
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 16:51:59 +0000

    c229916 build: update dependency typescript to v5.5.3 (#56802)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-4d81fdc");
+var VERSION = new Version("18.2.0-next.0+sha-c229916");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
