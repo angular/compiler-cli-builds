2ea28813b602c3c6d0c44b45729b46deabbfcdb0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 14:20:40 +0000

    8de9aca refactor(common): remove remaining code for NgSwitch equality patch (#56806)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-fdd8fb0");
+var VERSION = new Version("18.0.5+sha-8de9aca");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
