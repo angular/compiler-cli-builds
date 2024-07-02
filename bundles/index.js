63a5d9895874c3999aa7edbf553c462b09ad7192
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:38:28 +0000

    092bfe9 fix(core): support injection of object with null constructor. (#56553)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-c4384bf");
+var VERSION = new Version("18.1.0-rc.0+sha-092bfe9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
