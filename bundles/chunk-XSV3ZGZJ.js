9b1d8de7e91b1751b4c186240b84fc965792a7d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 15:31:27 +0000

    99de3b7 build: update all non-major dependencies (#56801)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.5+sha-8de9aca";
+var PLACEHOLDER_VERSION = "18.0.5+sha-99de3b7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-8de9aca");
+var VERSION = new Version("18.0.5+sha-99de3b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
