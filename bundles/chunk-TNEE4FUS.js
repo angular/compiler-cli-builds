b902af3a89ed366735c9d1449c10b7087d43e1e5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:01:43 +0000

    0f612dc docs: update playground app to use hello world for easier onboarding (#56813)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-rc.0+sha-9acccb1";
+var PLACEHOLDER_VERSION = "18.1.0-rc.0+sha-0f612dc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
