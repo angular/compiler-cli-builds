f349ff25d76aa6148da939d39b4aa8907fabf32b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:14:59 +0000

    b7d3ecc fix(router): routes should not get stale providers (#56798)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.5+sha-445dd96";
+var PLACEHOLDER_VERSION = "18.0.5+sha-b7d3ecc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
