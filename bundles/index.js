e06de36c8c4c5d5d829222bb33aa30d132b67f90
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:01:42 +0000

    68464a7 docs: update playground app to use hello world for easier onboarding (#56813)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.5+sha-0479c35");
+var VERSION = new Version("18.0.5+sha-68464a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
