af4bbcd86327256d24352943322fa7f0099b7083
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:45:34 +0000

    1ee64a5 refactor(common): Fire priority check on stable. (#56776)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-3235d6d");
+var VERSION = new Version("18.1.0-rc.0+sha-1ee64a5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
