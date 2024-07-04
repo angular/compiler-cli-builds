0efd0eed6465e41109f4292e02f6d99e328a6739
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 10:14:57 +0000

    95d7076 perf(http): execute `fetch` outside of Angular zone (#56820)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.6+sha-66bf467");
+var VERSION = new Version("18.0.6+sha-95d7076");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
