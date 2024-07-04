ea47a603fa2115973e9a36e00b1904b02066d405
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 08:00:19 +0000

    a2d93dc docs: Update image-optimization.md (#56819)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-4e36bf8");
+var VERSION = new Version("18.2.0-next.0+sha-a2d93dc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
