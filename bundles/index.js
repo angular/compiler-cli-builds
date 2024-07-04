c65a56e1e3a3a5823f6352b6a282ef3ffb1d67e3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 12:34:08 +0000

    1074134 docs: add window title to reference pages (#56840)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-ecc5644");
+var VERSION = new Version("18.1.0-rc.0+sha-1074134");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
