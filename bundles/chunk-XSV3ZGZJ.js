9d4a52e12b51633b8ebe9498d1b5aca543b1f92c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 10:32:16 +0000

    1517dde docs: fix broken link (#56848)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.6+sha-833fa2d";
+var PLACEHOLDER_VERSION = "18.0.6+sha-1517dde";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
