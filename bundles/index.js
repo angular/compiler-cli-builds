23d1cba4acbf2bb5d5543d1f9a50d2cf8b6e266e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 07:58:36 +0000

    176b26f docs: remove private symbol from docs (#56851)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-02da71c");
+var VERSION = new Version("18.2.0-next.0+sha-176b26f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
