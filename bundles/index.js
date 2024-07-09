12b60cf25f39116806032c27854a36bfa267431c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 16:22:31 +0000

    5f97d6a fix(migrations): Fix cf migration let condition semicolon order (#56913)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-2f8bf93");
+var VERSION = new Version("18.1.0-rc.0+sha-5f97d6a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
