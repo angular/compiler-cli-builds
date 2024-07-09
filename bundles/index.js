8ca00be25d1087372033a07c3dfdd477bec50d5f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 10:53:05 +0000

    0f4219c refactor(platform-server): Add private profiler. (#56274)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-f689214");
+var VERSION = new Version("18.1.0-rc.0+sha-0f4219c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
