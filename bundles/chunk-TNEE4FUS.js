f48696d04fe97d22ce154d9b2aca04521a7ae1cd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:51:44 +0000

    387e1cb fix(compiler): fix CSS animation rule scope (#56800)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-next.0+sha-02a31c5";
+var PLACEHOLDER_VERSION = "18.2.0-next.0+sha-387e1cb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-02a31c5");
+var VERSION = new Version("18.2.0-next.0+sha-387e1cb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
