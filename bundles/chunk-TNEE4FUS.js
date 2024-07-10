b99a6491c370c19a429e90b3272b68b41b4690f2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 09:13:29 +0000

    4d87b9e fix(core): rename the equality function option in toSignal (#56769) (#56922)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0-rc.0+sha-5f97d6a";
+var PLACEHOLDER_VERSION = "18.1.0-rc.0+sha-4d87b9e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0-rc.0+sha-5f97d6a");
+var VERSION = new Version("18.1.0-rc.0+sha-4d87b9e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
