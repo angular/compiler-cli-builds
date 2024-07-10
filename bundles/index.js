b13f1a854ae67420e031f804d0383801b94a1345
Andrew Scott <atscott@google.com>
2024-07-10 20:57:59 +0000

    a7b774c refactor(core): Refactor `EarlyEventContract` to prepare for using it as a container. (#56900)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0+sha-aedf832");
+var VERSION = new Version("18.1.0+sha-a7b774c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
