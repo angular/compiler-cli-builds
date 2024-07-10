5c33c59672c241f6fb6a95083bfd4f7914d1b991
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 16:21:40 +0000

    aedf832 docs: add overview page for performance section (#56828)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0+sha-e076eb2";
+var PLACEHOLDER_VERSION = "18.1.0+sha-aedf832";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
