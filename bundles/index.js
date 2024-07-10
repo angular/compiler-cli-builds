6b60f4739b635768d004154dac61b4e03a021549
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:45:57 +0000

    b6fb5b7 release: cut the v18.2.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-8fc2605");
+var VERSION = new Version("18.2.0-next.0+sha-b6fb5b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
