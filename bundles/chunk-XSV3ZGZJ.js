c62b8f963cebb8b5780e9006a3271b061f9108da
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 09:33:14 +0000

    1c2415e docs: Update components-scenarios.md (#56921)

--- bundles/chunk-XSV3ZGZJ.js
+++ bundles/chunk-XSV3ZGZJ.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.0.6+sha-a7d021f";
+var PLACEHOLDER_VERSION = "18.0.6+sha-1c2415e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
