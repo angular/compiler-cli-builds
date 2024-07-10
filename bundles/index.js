16ec041222845460ed36865246910af5d66f4e92
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:15:52 +0000

    619e181 docs: Update component testing scenarios (#56388)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.0.6+sha-d6fff45");
+var VERSION = new Version("18.0.6+sha-619e181");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
