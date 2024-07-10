e41edaf758b8ce7a1c4c59b40a99b43b308f4a9a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:55:48 +0000

    4b82b98 refactor(core): Remove unused A11Y_CLICK_SUPPORT define. (#56901)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.0+sha-729bf5b");
+var VERSION = new Version("18.1.0+sha-4b82b98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
