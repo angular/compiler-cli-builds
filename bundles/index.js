ff0ef439db36527411f2a9075cd7446d89465468
Andrew Scott <atscott@google.com>
2024-07-11 00:10:13 +0000

    dd6e548 docs: fixing links to guides on the Performance Overview page (#56942)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-d011c9f");
+var VERSION = new Version("18.2.0-next.0+sha-dd6e548");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
