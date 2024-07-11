8e688aeec8a014ccd4c37feda7daccd7439b7ec9
Andrew Scott <atscott@google.com>
2024-07-11 16:00:43 +0000

    e5f11dd build: fix rate limits when deploying angular.dev site (#56929)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-dd6e548");
+var VERSION = new Version("18.2.0-next.0+sha-e5f11dd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
