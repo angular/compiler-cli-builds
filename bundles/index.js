87b7b69000771ead2725700bb91bbc177e6c726f
Andrew Scott <atscott@google.com>
2024-07-12 00:08:15 +0000

    8f08d04 ci: add an entry for aio in the pullapprove config (#56952)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-0953d65");
+var VERSION = new Version("18.2.0-next.0+sha-8f08d04");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
