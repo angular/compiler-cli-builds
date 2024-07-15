00305c4cdfcc25840b795c7d012c6db2c6cae6dc
Andrew Scott <atscott@google.com>
2024-07-15 18:54:41 +0000

    ac9d9db build: update dependency inquirer to v10 (#56883)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-e958fa8");
+var VERSION = new Version("18.2.0-next.0+sha-ac9d9db");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
