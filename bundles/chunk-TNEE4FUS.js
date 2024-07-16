d2ae589734820bda28edbdd77f11f146373ee751
Andrew Scott <atscott@google.com>
2024-07-16 19:53:41 +0000

    4f11d6e fix(zone.js): support `Timeout.refresh` in Node.js (#56852)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.0+sha-a1cb9df";
+var PLACEHOLDER_VERSION = "18.1.0+sha-4f11d6e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
