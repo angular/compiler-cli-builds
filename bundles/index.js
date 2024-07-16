adc0e6e6cb382a240b725aaecbb5388ec5b07b41
Andrew Scott <atscott@google.com>
2024-07-16 19:55:02 +0000

    982f1b1 fix(zone.js): support `Timeout.refresh` in Node.js (#56852)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-2c4613a");
+var VERSION = new Version("18.2.0-next.0+sha-982f1b1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
