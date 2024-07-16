207f3ec5743a3995aa0e92d6833d8f981e85aa6e
Andrew Scott <atscott@google.com>
2024-07-16 16:58:18 +0000

    2c4613a fix(common): Don't run preconnect assertion on the server. (#56213)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-e5e1f49");
+var VERSION = new Version("18.2.0-next.0+sha-2c4613a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
