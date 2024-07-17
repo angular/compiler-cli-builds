d20b11e125849c70d41908c281e2fc001a25416b
GitHub <noreply@github.com>
2024-07-17 19:32:54 +0000

    59fc4de docs: release notes for the v17.3.12 release (#57023)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.0+sha-982f1b1");
+var VERSION = new Version("18.2.0-next.0+sha-59fc4de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
