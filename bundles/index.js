67aec4bf6adea13d0b4d062089f723e29e4060ed
Andrew Scott <atscott@google.com>
2024-07-18 19:10:05 +0000

    f3014c5 refactor(core): Move logic into early event contract files. (#56994)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.1+sha-bbabf54");
+var VERSION = new Version("18.1.1+sha-f3014c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
