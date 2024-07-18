c6df206440b088eb9f723cb538d4eb67d8679fb2
Andrew Scott <atscott@google.com>
2024-07-18 22:31:40 +0000

    f3a88ad docs(docs-infra): update tutorial deps to 18.1 (#57024)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.1+sha-425f44c");
+var VERSION = new Version("18.2.0-next.1+sha-f3a88ad");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
