6e116357e615f7cdf812ac028694882d1cfb7268
Andrew Scott <atscott@google.com>
2024-07-22 17:03:37 +0000

    ae15dd9 docs: fixing dead link (#57077)

--- bundles/chunk-F4R2DQ3M.js
+++ bundles/chunk-F4R2DQ3M.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.1+sha-7792aad";
+var PLACEHOLDER_VERSION = "18.1.1+sha-ae15dd9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
