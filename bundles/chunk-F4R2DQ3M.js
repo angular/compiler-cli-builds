08e79722fa0177402d524003c70e8fa7e258f6dc
Andrew Scott <atscott@google.com>
2024-07-22 17:57:09 +0000

    13bc8d7 docs: Update packages/router/src/router.ts (#57053)

--- bundles/chunk-F4R2DQ3M.js
+++ bundles/chunk-F4R2DQ3M.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-next.1+sha-aeaf07d";
+var PLACEHOLDER_VERSION = "18.2.0-next.1+sha-13bc8d7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "18.2.0-next.1+sha-aeaf07d";
+export declare const PLACEHOLDER_VERSION = "18.2.0-next.1+sha-13bc8d7";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
