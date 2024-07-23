e95d9c4f02a6577d6f02b79cb8c21ff9dbc71029
Andrew Scott <atscott@google.com>
2024-07-23 22:58:29 +0000

    1a606b0 refactor(core): Switch to using concat rather than spread operator for events. (#57103)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.1+sha-288a4fa");
+var VERSION = new Version("18.1.1+sha-1a606b0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
