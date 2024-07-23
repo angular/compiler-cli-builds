96390cfedea0973c301a7b6248836785bed92bc9
Andrew Scott <atscott@google.com>
2024-07-23 22:50:03 +0000

    43515a3 test(docs-infra): fix test with slight adjustment to error handling (#57104)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.1+sha-c578239");
+var VERSION = new Version("18.2.0-next.1+sha-43515a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
