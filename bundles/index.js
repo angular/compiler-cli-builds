8f8796e400276928b7641bed3db05b46d64ad18c
Andrew Scott <atscott@google.com>
2024-07-23 19:23:05 +0000

    23fa43d build: update all non-major dependencies (#57092)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.1+sha-c979dfd");
+var VERSION = new Version("18.1.1+sha-23fa43d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
