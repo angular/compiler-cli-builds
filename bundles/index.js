0151edc98b4a13f484a465be8ddc701097977a97
Andrew Scott <atscott@google.com>
2024-07-24 17:22:44 +0000

    8bb5937 docs(docs-infra): disable experimental decorators (#56257)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.1+sha-c969422");
+var VERSION = new Version("18.1.1+sha-8bb5937");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
