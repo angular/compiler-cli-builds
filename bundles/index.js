b7035b13ab32ae2d52a5a81356b15721b696f968
Dylan Hunn <dylhunn@gmail.com>
2024-07-29 21:00:34 +0000

    dd56270 build: update dependency typescript to v5.5.4 (#57090)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.2+sha-d108320");
+var VERSION = new Version("18.2.0-next.2+sha-dd56270");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
