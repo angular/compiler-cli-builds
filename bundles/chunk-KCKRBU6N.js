c362dcaf98288ca69ec349fc87240181c18ce100
Dylan Hunn <dylhunn@gmail.com>
2024-07-31 17:29:20 +0000

    89f28b6 release: cut the v18.2.0-next.3 release

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-next.2+sha-414d985";
+var PLACEHOLDER_VERSION = "18.2.0-next.3+sha-89f28b6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
