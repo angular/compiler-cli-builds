3cd5e64360fa9acff8f0bc1a67ceb6019b12ac13
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-01 19:21:54 +0000

    f125cd8 fix(docs-infra): replace the uses of replaceAll with replace using regex with g flag (#57232)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.3+sha-f5960d0");
+var VERSION = new Version("18.2.0-next.3+sha-f125cd8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
