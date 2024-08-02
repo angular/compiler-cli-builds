576506a41f81f714dce4d5cddd8ef9520d03cbb8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 14:26:32 +0000

    0c72976 refactor(core): Remove clickmod support from Angular. (#57201)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.3+sha-ad29621");
+var VERSION = new Version("18.1.3+sha-0c72976");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
