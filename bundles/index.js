c39b03945f5bd1cf7a362c673989de8259b717d8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:15:49 +0000

    6c76c91 feat(router): Add defaultQueryParamsHandling to router configuration (#57198)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.3+sha-2a915d1");
+var VERSION = new Version("18.2.0-next.3+sha-6c76c91");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
