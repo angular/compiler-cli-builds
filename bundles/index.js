20aa37b3b9a5c7191804313a9cf863d97159e1f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:51:04 +0000

    f7918f5 feat(core): Add 'flush' parameter option to fakeAsync to flush after the test (#57239)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.4+sha-4d0842d");
+var VERSION = new Version("18.2.0-next.4+sha-f7918f5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
