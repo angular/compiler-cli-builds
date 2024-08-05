f930ca47027ea10579b7cf08349f5175302cf44d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 20:28:48 +0000

    70e8b40 fix(zone.js): Update the default behavior of fakeAsync to flush after the test (#57240)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-next.4+sha-a55ecb9";
+var PLACEHOLDER_VERSION = "18.2.0-next.4+sha-70e8b40";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
