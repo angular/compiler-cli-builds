1122010fe85781aabad1beef02ae269aca75f0e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 16:03:04 +0000

    00a6c41 build: update dependency madge to v8 (#57257)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-next.3+sha-e2259c7";
+var PLACEHOLDER_VERSION = "18.2.0-next.3+sha-00a6c41";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.3+sha-e2259c7");
+var VERSION = new Version("18.2.0-next.3+sha-00a6c41");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
