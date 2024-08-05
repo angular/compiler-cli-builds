82e472a6226e81f4a9d06c9d4b52999edc6ace16
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:13:06 +0000

    3bdead1 refactor(docs-infra): migrate api-gen from dev-infra into the repo (#57241)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.3+sha-de99d3e");
+var VERSION = new Version("18.2.0-next.3+sha-3bdead1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
