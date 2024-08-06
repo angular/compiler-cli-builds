899524dd89f5277bc2a3abb8584a674c297b581d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-06 21:32:17 +0000

    7919982 feat(core): Add whenStable helper on ApplicationRef (#57190)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.0-next.4+sha-f9a97c7");
+var VERSION = new Version("18.2.0-next.4+sha-7919982");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
