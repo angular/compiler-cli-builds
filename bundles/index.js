36bd7b3441ebaffc3ed042626332ee2224df97ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-06 13:55:02 +0000

    8c029c6 build: update actions/upload-artifact action to v4.3.5 (#57271)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.3+sha-6b2a4c0");
+var VERSION = new Version("18.1.3+sha-8c029c6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
