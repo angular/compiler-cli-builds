666506bbcb39b921de82683e70fc510e3eca34c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 17:29:57 +0000

    6a99f83 fix(compiler): reduce chance of conflicts between generated factory and local variables (#57181)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.3+sha-bae54a1");
+var VERSION = new Version("18.1.3+sha-6a99f83");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
