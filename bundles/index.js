dcbff76c0b54769374a773b3bf8539dd6be4feac
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-07 18:56:41 +0000

    bdf8450 release: cut the v18.1.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.3+sha-69540d3");
+var VERSION = new Version("18.1.4+sha-bdf8450");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
