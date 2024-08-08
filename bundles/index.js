6703c57804c6a9d3c309de2d1522c029135aa08b
Andrew Kushnir <akushnir@google.com>
2024-08-08 17:51:21 +0000

    769b6e1 fix(core): Allow hybrid CD scheduling to support multiple "Angular zones" (#57267)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.3.0-next.0+sha-3439cc2");
+var VERSION = new Version("18.3.0-next.0+sha-769b6e1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
