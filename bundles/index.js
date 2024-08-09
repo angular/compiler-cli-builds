b78290f1975c356d93ccd4e3d63769c69605cd01
Andrew Kushnir <akushnir@google.com>
2024-08-09 15:14:05 +0000

    810f76f fix(core): take skip hydration flag into account while hydrating i18n blocks (#57299)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.4+sha-e39b22a");
+var VERSION = new Version("18.1.4+sha-810f76f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
