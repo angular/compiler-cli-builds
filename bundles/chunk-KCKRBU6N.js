3705ac20ba5ae6cb5a46513a82bc50534abcfb0b
Andrew Kushnir <akushnir@google.com>
2024-08-09 19:23:03 +0000

    448279f refactor(migrations): detect if an input is not narrowed and can be migrated (#57308) (#57323)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0-rc.0+sha-5558e27";
+var PLACEHOLDER_VERSION = "18.2.0-rc.0+sha-448279f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
