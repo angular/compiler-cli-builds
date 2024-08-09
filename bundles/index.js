1d8ac5e55a92b879f0a2aa512539119b810ccf23
Andrew Kushnir <akushnir@google.com>
2024-08-09 20:59:45 +0000

    37bf7de docs: reorder afterView and afterContent related lifecycle hooks (#57307)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-f271021");
+var VERSION = new Version("19.0.0-next.0+sha-37bf7de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
