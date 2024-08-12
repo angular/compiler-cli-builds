32404ea82b554b43f0d9c50d0cb9463b2da3ad09
Andrew Kushnir <akushnir@google.com>
2024-08-12 18:29:19 +0000

    468d3fb fix(core): rethrow errors during ApplicationRef.tick in TestBed (#57200)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-b279081");
+var VERSION = new Version("19.0.0-next.0+sha-468d3fb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
