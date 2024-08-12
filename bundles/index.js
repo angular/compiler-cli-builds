2ecf92cbf77bf94b2f8505b878d32d954af69bc3
Andrew Kushnir <akushnir@google.com>
2024-08-12 22:21:52 +0000

    d608b85 refactor(migrations): handle `jit: true` component templates in signal input migration (#57347)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-52673e6");
+var VERSION = new Version("19.0.0-next.0+sha-d608b85");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
