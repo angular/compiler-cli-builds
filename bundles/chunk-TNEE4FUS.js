80b24988cb1ae8aa1623aac7e3abdffd535b7000
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:47:57 +0000

    822db64 fix(core): skip hydration for i18n nodes that were not projected (#57356)

--- bundles/chunk-TNEE4FUS.js
+++ bundles/chunk-TNEE4FUS.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.1.4+sha-9814e9b";
+var PLACEHOLDER_VERSION = "18.1.4+sha-822db64";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.1.4+sha-9814e9b");
+var VERSION = new Version("18.1.4+sha-822db64");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
