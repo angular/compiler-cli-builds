6831b6dd85d198d536de0e2e831e8374b54afdf0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-08-14 16:53:04 +0000

    5537bf8 release: cut the v19.0.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-c1f1c02");
+var VERSION = new Version("19.0.0-next.0+sha-5537bf8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
