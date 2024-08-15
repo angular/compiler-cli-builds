9961aa0da228ff504171355a8acb469703055ee3
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 16:16:44 +0000

    58a79b6 fix(migrations): preserve type when using inject decorator (#57389)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-d4449fc");
+var VERSION = new Version("19.0.0-next.0+sha-58a79b6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
