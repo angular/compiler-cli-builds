84ff3c6ba90e7773a4f1997b2d361765160671a2
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 16:36:26 +0000

    3b0dca7 fix(core): Allow zoneless scheduler to run inside `fakeAsync` (#56932)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-58a79b6");
+var VERSION = new Version("19.0.0-next.0+sha-3b0dca7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
