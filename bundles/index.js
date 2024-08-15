fe4cbe6eb7b268a1929f9f1d06bd8ab8d22c727e
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 19:56:43 +0000

    9c73943 docs(docs-infra): move link tag for correct view (#57395)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-f03d274");
+var VERSION = new Version("19.0.0-next.0+sha-9c73943");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
