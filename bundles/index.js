f8222688932aba0a66507ec1a495abe387712e97
Dylan Hunn <dylhunn@gmail.com>
2024-08-16 15:25:14 +0000

    baa1254 docs: update CHANGELOG to drop a breaking change from 18.2.0 version (#57419)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-0bbbd78");
+var VERSION = new Version("19.0.0-next.0+sha-baa1254");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
