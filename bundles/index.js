7bcc6e48b4f3c6d4526b32886c82b4053599f324
Dylan Hunn <dylhunn@gmail.com>
2024-08-17 13:58:12 +0000

    368f36d refactor(migrations): framework to build batchable migrations (#57396)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-baa1254");
+var VERSION = new Version("19.0.0-next.0+sha-368f36d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
