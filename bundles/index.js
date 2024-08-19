f4a4f61f89512e8217b320f20083774beacf8cfe
Dylan Hunn <dylhunn@gmail.com>
2024-08-19 16:29:03 +0000

    f47901a docs(docs-infra): Fix heading parsing (#57386)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-368f36d");
+var VERSION = new Version("19.0.0-next.0+sha-f47901a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
