473147918ec5d9d2210d0bd6d34c9fce6412c642
Dylan Hunn <dylhunn@gmail.com>
2024-08-20 05:50:11 +0000

    df3e9c1 refactor(migrations): leverage tsurge for signal input migration (#57451)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.0+sha-0979ca0";
+var PLACEHOLDER_VERSION = "18.2.0+sha-df3e9c1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
