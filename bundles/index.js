b8951309ea661c0abf831df82f2d77852e4df3ed
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 18:35:18 +0000

    d1d4adc release: cut the zone.js-0.15.0 release (#57418)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-2ed52dd");
+var VERSION = new Version("19.0.0-next.0+sha-d1d4adc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
