3f1865e9bd31d7cde4011ae2469fa40badf53feb
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 20:21:53 +0000

    5cc63c2 build: update zone.js peer dependency for core package to 0.15.0 (#57431)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.0+sha-d1d4adc");
+var VERSION = new Version("19.0.0-next.0+sha-5cc63c2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
