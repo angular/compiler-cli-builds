d70a23f28cf3d81891fb9ef849e114be4e916349
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 21:51:20 +0000

    6d3a2af fix(core): Do not bubble capture events. (#57476)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.1+sha-223b785");
+var VERSION = new Version("18.2.1+sha-6d3a2af");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
