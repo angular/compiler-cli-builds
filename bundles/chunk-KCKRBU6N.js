04f489295e6b1b0e553ff090e02df6f487c06d08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 22:12:38 +0000

    226a67d fix(core): Schedulers run in zone above Angular rather than root (#57553)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-76b9e2b";
+var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-226a67d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.1+sha-76b9e2b");
+var VERSION = new Version("19.0.0-next.1+sha-226a67d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
