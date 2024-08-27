56857283e2f23cb07337393d1441a912681b0888
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 21:07:36 +0000

    76b9e2b Revert "refactor(migrations): initial migration logic for converting to signal queries (#57525)" (#57555)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-21445a2";
+var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-76b9e2b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
