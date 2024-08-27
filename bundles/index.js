215540a797a5102b3c1fb8f547994493974f1999
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 21:08:04 +0000

    eba3a0a Revert "refactor(migrations): initial migration logic for converting to signal queries (#57525)" (#57555)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.1+sha-5d2e243");
+var VERSION = new Version("18.2.1+sha-eba3a0a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
