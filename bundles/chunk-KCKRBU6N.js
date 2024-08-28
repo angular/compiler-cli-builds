fbabb709de0a0f49954b0bf8f6a87768a0745079
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 19:46:32 +0000

    013d02d Revert "fix(http): Dynamicaly call the global fetch implementation (#57531)" (#57571)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-b7f3baf";
+var PLACEHOLDER_VERSION = "19.0.0-next.1+sha-013d02d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
