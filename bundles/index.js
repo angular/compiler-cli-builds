10f870495cbbb4c0ad50f4de289e6e1bee14802d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 15:54:39 +0000

    adf3274 fix(devtools): correctly set environment injector path in the case where there are no element injectors (#57442)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.1+sha-eba3a0a");
+var VERSION = new Version("18.2.1+sha-adf3274");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
