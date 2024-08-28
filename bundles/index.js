67894c4e05e20ee876c5b001ae0dc3c8fb109fec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 16:21:35 +0000

    b7f3baf refactor(migrations): forward-fix conflicts with generic of `ProgramInfo`. (#57565)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.1+sha-774d983");
+var VERSION = new Version("19.0.0-next.1+sha-b7f3baf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
