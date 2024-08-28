6bc41e8723429ca751e6418caff6f5c7d4145872
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 21:36:47 +0000

    1853bbb release: cut the v18.2.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.1+sha-3ecf620");
+var VERSION = new Version("18.2.2+sha-1853bbb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
