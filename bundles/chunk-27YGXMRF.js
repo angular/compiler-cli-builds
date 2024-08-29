fea2cd2955b0f0483f4b07e51a37c2fa8fcee0a7
Andrew Kushnir <akushnir@google.com>
2024-08-29 22:24:26 +0000

    1d79cbd docs: update common-router-tasks to include sample dynamic fragment (#57590)

--- bundles/chunk-27YGXMRF.js
+++ bundles/chunk-27YGXMRF.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.2+sha-2ad9609";
+var PLACEHOLDER_VERSION = "19.0.0-next.2+sha-1d79cbd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
