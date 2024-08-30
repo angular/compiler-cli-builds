5cef9d99dad9b66481d7d458406afb0d58af13e0
Andrew Kushnir <akushnir@google.com>
2024-08-30 18:16:43 +0000

    3bafda2 docs(docs-infra): fix navigation between API pages (#57492)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.2+sha-5e42769");
+var VERSION = new Version("19.0.0-next.2+sha-3bafda2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
