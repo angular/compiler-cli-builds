7c707b2621685a7ef8a6df93677eae000943924c
Andrew Kushnir <akushnir@google.com>
2024-09-03 15:54:40 +0000

    2324d9b docs: Fix 'EnvironmentInjector' description (#57582)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.2+sha-b494e9b");
+var VERSION = new Version("18.2.2+sha-2324d9b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
