31221819cec7c3e1685c927765520e37ea5f2a22
Andrew Kushnir <akushnir@google.com>
2024-09-03 16:15:41 +0000

    c2892fe fix(http): Dynamicaly call the global fetch implementation (#57531)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.0.0-next.2+sha-36d8d19";
+export declare const PLACEHOLDER_VERSION = "19.0.0-next.2+sha-c2892fe";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
