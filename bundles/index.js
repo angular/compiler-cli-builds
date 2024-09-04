0778994f854d44cd39db997d0661246995aa61d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 21:04:25 +0000

    7a4199a refactor(devtools): use signal apis for directive forest (#56998)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.3+sha-f6c40f1");
+var VERSION = new Version("19.0.0-next.3+sha-7a4199a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
