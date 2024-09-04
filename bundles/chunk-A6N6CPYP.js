efc2bc2530a19c48c23ebe01864dcce5b07bcba3
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-09-04 15:32:05 +0000

    c19b02f docs: release notes for the v18.2.3 release

--- bundles/chunk-A6N6CPYP.js
+++ bundles/chunk-A6N6CPYP.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.2+sha-8da9fb4";
+var PLACEHOLDER_VERSION = "19.0.0-next.2+sha-c19b02f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.2+sha-8da9fb4");
+var VERSION = new Version("19.0.0-next.2+sha-c19b02f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
