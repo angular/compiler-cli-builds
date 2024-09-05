ebeb5f9b5a1a2023ebd2fbb4b3bbfac0e5fd6433
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 14:17:55 +0000

    68e5370 feat(migrations): remove complete calls for migrated outputs (#57671)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.3+sha-7a4199a");
+var VERSION = new Version("19.0.0-next.3+sha-68e5370");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
