5cd742423124bb5ae4113a05c5bbd9e7dbe3248e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 16:09:23 +0000

    74d3fbf docs: clarify behavior of ng serve with server.ts (#57658)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-004110c");
+var VERSION = new Version("18.2.3+sha-74d3fbf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
