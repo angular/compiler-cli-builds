a89000d2811deb76c8a30377b768f054de79635a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 17:27:54 +0000

    3bce676 refactor(devtools): use signal apis in visualizer and devtools-tabs (#57192)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.3+sha-74d3fbf";
+var PLACEHOLDER_VERSION = "18.2.3+sha-3bce676";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
