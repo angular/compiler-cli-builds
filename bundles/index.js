a5f36e1723f91c0337a578848f560afcccfa60dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 16:11:22 +0000

    0843f31 docs: Enable direct links to class/interface methods for `@link` (#57615)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-bb8e7ce");
+var VERSION = new Version("18.2.3+sha-0843f31");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
