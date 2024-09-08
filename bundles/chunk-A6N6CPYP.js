fc5a10a83a380416890a68fe6ab596391f278847
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-08 17:15:43 +0000

    4f52678 refactor(migrations): avoid flow node traversal limit by optimizing traversal (#57719)

--- bundles/chunk-A6N6CPYP.js
+++ bundles/chunk-A6N6CPYP.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.3+sha-2e60eb0";
+var PLACEHOLDER_VERSION = "19.0.0-next.3+sha-4f52678";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
