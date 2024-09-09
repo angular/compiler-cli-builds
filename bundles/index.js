341b2181dbfc029f528b4d46b7054ecbb89831ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:08:37 +0000

    4cb07e6 docs: extraneous backslashes removed (#57725)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-0843f31");
+var VERSION = new Version("18.2.3+sha-4cb07e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
