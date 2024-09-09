f13be931db826730352e49afc7c19adc1b53c426
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:21:19 +0000

    b619d69 fix(compiler): produce less noisy errors when parsing control flow (#57711)

--- bundles/chunk-KCKRBU6N.js
+++ bundles/chunk-KCKRBU6N.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.3+sha-4cb07e6";
+var PLACEHOLDER_VERSION = "18.2.3+sha-b619d69";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-4cb07e6");
+var VERSION = new Version("18.2.3+sha-b619d69");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
