2d3710540b805a268365980198521558c872f4f2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-09-09 18:19:49 +0000

    d58ffd6 release: cut the v19.0.0-next.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.3+sha-6fa1b38");
+var VERSION = new Version("19.0.0-next.4+sha-d58ffd6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
