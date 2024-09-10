41285ca5cb6a05b9385d3ba5dfbbda4a6fe93cd5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 16:36:41 +0000

    a919c6c build: update all non-major dependencies to ~5.3.0 (#57754)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-d0fe538");
+var VERSION = new Version("18.2.3+sha-a919c6c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
