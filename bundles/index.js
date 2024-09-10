57f417eb5fdbe66e9c66f3570cce4a79848d9e28
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 16:41:32 +0000

    685866d refactor(migrations): correct catalyst import in signal input migration (#57750)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.4+sha-95b8105");
+var VERSION = new Version("19.0.0-next.4+sha-685866d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
