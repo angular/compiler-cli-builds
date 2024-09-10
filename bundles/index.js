177d6eaf4801dafd420c2425944fc21b6e0f6dd0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:38:02 +0000

    bc03f53 docs: add examples for localize (#57534)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.4+sha-45575b5");
+var VERSION = new Version("19.0.0-next.4+sha-bc03f53");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
