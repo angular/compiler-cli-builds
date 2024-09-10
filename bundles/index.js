03b0820d227d01e4f56b24e0a2a0fbc3d28b1827
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:32:13 +0000

    c4b19c4 docs: fix link from zoneless to OnPush (#57760) PR Close #57760

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.3+sha-a919c6c");
+var VERSION = new Version("18.2.3+sha-c4b19c4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
