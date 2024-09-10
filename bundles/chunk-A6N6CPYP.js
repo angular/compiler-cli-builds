4cdfdcf9cb11fc8f203d59bc81e04b93c6ef1bd7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:52:01 +0000

    0ebb2be build: bump dev-infra dependencies (#57761)

--- bundles/chunk-A6N6CPYP.js
+++ bundles/chunk-A6N6CPYP.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.4+sha-bc03f53";
+var PLACEHOLDER_VERSION = "19.0.0-next.4+sha-0ebb2be";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.4+sha-bc03f53");
+var VERSION = new Version("19.0.0-next.4+sha-0ebb2be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
