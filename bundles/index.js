9d057103e734be0d4de4197ebd4ff997e9075a28
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-11 17:52:35 +0000

    73feb0e refactor(migrations): support reference migration in query migration (#57777)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.4+sha-c93b510");
+var VERSION = new Version("19.0.0-next.4+sha-73feb0e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
