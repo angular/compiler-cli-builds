19f3c6a9a9a9384fd6e9126fb4ee122835f33687
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-11 19:00:10 +0000

    ada67a3 docs: fix link of DEVELOPER.md in CONTRIBUTING.md (#57775)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.4+sha-73feb0e");
+var VERSION = new Version("19.0.0-next.4+sha-ada67a3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
