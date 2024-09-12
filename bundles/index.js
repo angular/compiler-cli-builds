78bd6881fac9f355e121105d5135774dd3d66820
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 09:48:38 +0000

    156f0d0 docs: fix example for defer on hover on an alternative element (#57788)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.4+sha-fab6462");
+var VERSION = new Version("18.2.4+sha-156f0d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
