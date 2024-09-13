be289e88139636f7db67b0e81f26d7826b2f7f7a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-13 08:52:48 +0000

    6144612 fix(migrations): account for explicit standalone: false in migration (#57803)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.5+sha-55e3a3a");
+var VERSION = new Version("19.0.0-next.5+sha-6144612");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
