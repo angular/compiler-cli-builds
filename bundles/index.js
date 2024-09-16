eded5a0508d9d2180920c6d47245e994ddc73a2e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 10:21:05 +0000

    e5adf92 feat(core): stabilize `@let` syntax (#57813)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.5+sha-6b066f3");
+var VERSION = new Version("19.0.0-next.5+sha-e5adf92");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
