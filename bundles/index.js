2be64e755851d03de9be4192647ad31cc7d6f40a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 14:35:00 +0000

    4231e8f fix(core): Handle @let declaration with array when `preparingForHydration` (#57816)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.5+sha-87e025a");
+var VERSION = new Version("19.0.0-next.5+sha-4231e8f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
