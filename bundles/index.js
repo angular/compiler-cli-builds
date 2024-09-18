9dd6f29e44925b23ea4f2160bb9949b217b667ee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 15:37:28 +0000

    f8666a9 docs: release notes for the v18.2.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.5+sha-029163c");
+var VERSION = new Version("19.0.0-next.5+sha-f8666a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
