319bc441a96647b2474f6d671848b887b7b986a3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 12:22:42 +0000

    9020a50 docs: Update Making HTTP requests best practices example (#57876)

--- bundles/chunk-R4DWHF2W.js
+++ bundles/chunk-R4DWHF2W.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-e598b08";
+var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-9020a50";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
