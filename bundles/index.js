2087817f167d2ae6fee617a050f5f8d7f9cb4204
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 09:18:38 +0000

    38de06b test: disable cache and analytics (#57863)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-4e890cc");
+var VERSION = new Version("19.0.0-next.6+sha-38de06b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
