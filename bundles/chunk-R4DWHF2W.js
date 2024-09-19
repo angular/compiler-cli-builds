433857ced53c60dd45d82bdb7aa87479be2e0e57
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 10:55:10 +0000

    e598b08 docs: fix closing tag slash location (#57405)

--- bundles/chunk-R4DWHF2W.js
+++ bundles/chunk-R4DWHF2W.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-163f4d0";
+var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-e598b08";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-163f4d0");
+var VERSION = new Version("19.0.0-next.6+sha-e598b08");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
