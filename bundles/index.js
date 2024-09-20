b779abdc1c67bcdfe3387f4dad44e599c8cc1c76
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 09:19:01 +0000

    e0ead92 build: update io_bazel_rules_sass digest to 8dac332 (#57892)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.5+sha-07a485f");
+var VERSION = new Version("18.2.5+sha-e0ead92");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
