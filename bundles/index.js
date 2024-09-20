f028c21e59a36acfbdc063948478da2c9493c2a7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 16:31:29 +0000

    a1f2298 feat(core): migrate ExperimentalPendingTasks to PendingTasks (#57533)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-a448f5a");
+var VERSION = new Version("19.0.0-next.6+sha-a1f2298");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
