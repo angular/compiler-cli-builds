650c881cb414b12c63a9a3b0ab52a03d5c9f7afe
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 15:04:52 +0000

    a448f5a ci: update ownership to include MarkTechson (#57885)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-3ac0a78");
+var VERSION = new Version("19.0.0-next.6+sha-a448f5a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
