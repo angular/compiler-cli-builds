346e5c748b0cce9ee7a06f3f7b8722c4f3a5862a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 12:28:43 +0000

    7b2fda1 refactor(platform-browser): reduce runtime code size of shared style host (#57834)

--- bundles/chunk-AHHP5JQB.js
+++ bundles/chunk-AHHP5JQB.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-04a4873";
+var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-7b2fda1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
