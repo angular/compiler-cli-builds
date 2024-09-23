b40407708366c5f64a95df48033fc4a02edb6250
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 15:25:50 +0000

    eb5651f fix(docs-infra): resolve Mermaid from Bazel root path (#57924)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-6c9abd3");
+var VERSION = new Version("19.0.0-next.6+sha-eb5651f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
