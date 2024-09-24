492dd07ce274d11a8dd7020d3175354003fb1681
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:11:57 +0000

    48216a2 refactor(router): Move view transitions to developer preview (#55600)

--- bundles/chunk-AHHP5JQB.js
+++ bundles/chunk-AHHP5JQB.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-5a04837";
+var PLACEHOLDER_VERSION = "19.0.0-next.6+sha-48216a2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
