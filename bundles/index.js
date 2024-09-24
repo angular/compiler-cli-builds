708466620c58c7926277c52b186577396426bc94
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 09:56:01 +0000

    997fe96 build: update dependency @rollup/plugin-commonjs to v28 (#57929)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.5+sha-613dd02");
+var VERSION = new Version("18.2.5+sha-997fe96");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
