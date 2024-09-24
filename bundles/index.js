3314ac43f481d2b1dcd36dd5cd2cb57c07f987f3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 12:51:49 +0000

    b035cae docs: use X link instead of twitter (#57608) PR Close #57608

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-b124e50");
+var VERSION = new Version("19.0.0-next.6+sha-b035cae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
