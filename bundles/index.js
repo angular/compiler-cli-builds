7c21e139c35a9b3ca647152a093eddd172fe96d8
Andrew Scott <atscott@google.com>
2024-09-25 17:09:46 +0000

    95e8fb4 refactor(migrations): do not insert TODO for skipped inputs (#57957)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.6+sha-2c8449a");
+var VERSION = new Version("19.0.0-next.6+sha-95e8fb4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
