99071e648830b2359bc13ceaecda4ca848e3c9cc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-09-27 07:54:46 +0000

    fbd5579 ci: update integration test payload size (#57986)

--- bundles/chunk-2TSCCC7T.js
+++ bundles/chunk-2TSCCC7T.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.7+sha-0aae371";
+var PLACEHOLDER_VERSION = "19.0.0-next.7+sha-fbd5579";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
