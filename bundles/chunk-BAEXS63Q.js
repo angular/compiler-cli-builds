03599461bda606d80a60bb917115d1acd965868c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-30 19:56:06 +0000

    add5c25 docs: set expiry date to the developer survey (#57981)

--- bundles/chunk-BAEXS63Q.js
+++ bundles/chunk-BAEXS63Q.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.6+sha-69b7cf5";
+var PLACEHOLDER_VERSION = "18.2.6+sha-add5c25";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
