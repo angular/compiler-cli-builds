3c32d271d4cbfd69c0a2e862ac748b1eb3bc7245
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 11:33:53 +0000

    9ce839f test(migrations): add test for statistic collection tsurge (#58019)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.7+sha-e77a163");
+var VERSION = new Version("19.0.0-next.7+sha-9ce839f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
