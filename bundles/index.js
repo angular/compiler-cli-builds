d9275d1f0d6e638d5271cff3e348bebc074385b5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 18:10:54 +0000

    00a79d0 refactor(migrations): properly migrate inputs marked as optional via question mark (#58031)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.7+sha-d60bb51");
+var VERSION = new Version("19.0.0-next.7+sha-00a79d0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
