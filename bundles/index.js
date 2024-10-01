597a428c4783d771a9e581d65aad5c560550440f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:36:52 +0000

    e77a163 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.7+sha-78bc349");
+var VERSION = new Version("19.0.0-next.7+sha-e77a163");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
