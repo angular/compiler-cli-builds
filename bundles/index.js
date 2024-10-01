c12a10a33c9b40cb8766a5211bb1be774f9181fe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 07:12:10 +0000

    b84e2d3 docs: add deferrable views tutorial (#57934)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.6+sha-b9d846d");
+var VERSION = new Version("18.2.6+sha-b84e2d3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
