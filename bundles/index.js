314a925c8b9fccc981bd268e84c86e591cd623d1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 12:36:39 +0000

    46b088a release: cut the v18.2.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.6+sha-46a2ad3");
+var VERSION = new Version("18.2.7+sha-46b088a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
