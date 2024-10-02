4d0670d13510d4133ce381e0d76411deb8ca667e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 15:15:00 +0000

    326cca2 refactor(migrations): eagerly migrate `Partial<T>` references in google3 (#58049)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-d48aac8");
+var VERSION = new Version("19.0.0-next.8+sha-326cca2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
