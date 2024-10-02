93eeecfcd3268c86a2758fa595b944a81c76ee13
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 11:31:12 +0000

    c48816e docs: remove whitespace (#58042) PR Close #58042

--- bundles/chunk-BAEXS63Q.js
+++ bundles/chunk-BAEXS63Q.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.6+sha-70591e7";
+var PLACEHOLDER_VERSION = "18.2.6+sha-c48816e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
