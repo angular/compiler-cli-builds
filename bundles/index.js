a5b3f20fe0bb51cb4273f99f40cad4612ef8b450
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-03 16:49:51 +0000

    9213216 refactor(migrations): do not accidentally detect read as incompatible write (#58054)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-5cbe02a");
+var VERSION = new Version("19.0.0-next.8+sha-9213216");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
