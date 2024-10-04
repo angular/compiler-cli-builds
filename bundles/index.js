e4257a32067cf0d976487109ff3e2ff83f694ef8
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:43:54 +0000

    0eb1c5c docs(common): Minor typo in code example (#58085)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-06600d2");
+var VERSION = new Version("19.0.0-next.8+sha-0eb1c5c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
