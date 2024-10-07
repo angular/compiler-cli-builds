60106d2fbe958bf62ddf7ed9a076a8f5f7419662
Andrew Kushnir <akushnir@google.com>
2024-10-07 16:30:49 +0000

    837af44 refactor(core): introduce debugName optional arg to ReactiveNode (#57710)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-6976349");
+var VERSION = new Version("19.0.0-next.8+sha-837af44");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
