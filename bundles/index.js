a76ff22b4d93ea2038b3aa560f9d5d888d73ef84
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-08 06:23:22 +0000

    7b13d34 build: update scorecard action dependencies (#58116)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.7+sha-bf83801");
+var VERSION = new Version("18.2.7+sha-7b13d34");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
