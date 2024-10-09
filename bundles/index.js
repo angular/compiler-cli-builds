e7570bbb9a3b3e9b1d1ac7d8b7e34ea23407fa8b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 15:46:04 +0000

    2d11314 refactor(migrations): improve statistic tracking of signal input migration (#58137)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-e00775a");
+var VERSION = new Version("19.0.0-next.8+sha-2d11314");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
