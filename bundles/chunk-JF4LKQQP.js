0901d0f3fdd6ccb3d9c3b36ec116650ba096ed9c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:29:40 +0000

    410ed69 docs(core): fix description about signal value equality (#56472)

--- bundles/chunk-JF4LKQQP.js
+++ bundles/chunk-JF4LKQQP.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.9+sha-6530c5e";
+var PLACEHOLDER_VERSION = "19.0.0-next.9+sha-410ed69";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
