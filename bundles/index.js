552df0d88a9d7a604a38040a0d2a2d6c88985800
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:53:01 +0000

    a9e440d refactor(core): drop the `Mutable` utility type. (#58124)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.8+sha-da0e6b1");
+var VERSION = new Version("18.2.8+sha-a9e440d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
