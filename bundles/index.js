92707878c4f49191e55d5e7512de146e3df9dd1e
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:21:42 +0000

    6530c5e release: cut the v19.0.0-next.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.8+sha-dff03d2");
+var VERSION = new Version("19.0.0-next.9+sha-6530c5e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
