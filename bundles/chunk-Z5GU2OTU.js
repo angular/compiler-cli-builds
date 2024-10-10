7a1f03421510ba5c4ed9540396530312de1122d6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 12:43:10 +0000

    4746794 docs: move JSDoc below constructor (#57187)

--- bundles/chunk-Z5GU2OTU.js
+++ bundles/chunk-Z5GU2OTU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.8+sha-a9e440d";
+var PLACEHOLDER_VERSION = "18.2.8+sha-4746794";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
