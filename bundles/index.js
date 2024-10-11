c6ba3614c9ad1f64d13a9c53e91b34d784b4e927
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 10:43:13 +0000

    b198593 build: update io_bazel_rules_sass digest to 583fc8c (#58155)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.9+sha-fc6c76a");
+var VERSION = new Version("19.0.0-next.9+sha-b198593");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
