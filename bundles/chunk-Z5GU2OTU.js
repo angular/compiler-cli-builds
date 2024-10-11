f2dd6e4413b3067934879a8c869c1569e594e5f7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 14:14:00 +0000

    6cd2028 ci: disable bazel-saucelabs job (#58162)

--- bundles/chunk-Z5GU2OTU.js
+++ bundles/chunk-Z5GU2OTU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.8+sha-d9bb74f";
+var PLACEHOLDER_VERSION = "18.2.8+sha-6cd2028";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
