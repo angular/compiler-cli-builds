555949841352a39f7fe426b26a4cc9b11b691790
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 11:28:35 +0000

    a59dbb7 refactor(migrations): assign incompatibility reasons for query migration (#58152)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.9+sha-b198593");
+var VERSION = new Version("19.0.0-next.9+sha-a59dbb7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
