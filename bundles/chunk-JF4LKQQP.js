0d44bf1d4a0d118c95e2f35fde9059bf3c0eb16f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-14 13:24:34 +0000

    9373c98 docs: remove word repetition (#58187) PR Close #58187

--- bundles/chunk-JF4LKQQP.js
+++ bundles/chunk-JF4LKQQP.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "19.0.0-next.9+sha-ba43408";
+var PLACEHOLDER_VERSION = "19.0.0-next.9+sha-9373c98";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.9+sha-ba43408");
+var VERSION = new Version("19.0.0-next.9+sha-9373c98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
