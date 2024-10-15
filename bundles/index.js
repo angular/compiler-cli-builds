49d01f6ba172f79c6adf6f6c55ff7ab0d4354136
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 12:57:08 +0000

    97c44a1 feat(core): Promote `takeUntilDestroyed` to stable. (#58200)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.9+sha-b4de7ac");
+var VERSION = new Version("19.0.0-next.9+sha-97c44a1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
