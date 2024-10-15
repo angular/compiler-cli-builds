e682800e611a1652f89ecc95e4b3d810a07580c7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 16:54:16 +0000

    06f45c3 docs: update browserslist config to include last 2 Android major versions (#58186)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.8+sha-4049d77");
+var VERSION = new Version("18.2.8+sha-06f45c3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
