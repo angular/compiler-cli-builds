68b112a241a475b6deab8b4f57f1df29834c9179
Andrew Kushnir <akushnir@google.com>
2024-10-15 20:07:51 +0000

    7f0b49d refactor(core): update effect error handling (#57952)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.9+sha-d760cbe");
+var VERSION = new Version("19.0.0-next.9+sha-7f0b49d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
