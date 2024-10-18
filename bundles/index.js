6c9859d7fbdfe12c523fcd848abc68f3553231ea
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 11:57:45 +0000

    dff4de0 feat(migrations): add a combined migration for all signals APIs (#58259)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-b542f15");
+var VERSION = new Version("19.0.0-next.10+sha-dff4de0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
