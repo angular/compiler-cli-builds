d339dc83459a9ec7221d9e332e1281ff0e67888f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 14:40:19 +0000

    01e6fc1 docs: update default asset paths (#58246)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-dff4de0");
+var VERSION = new Version("19.0.0-next.10+sha-01e6fc1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
