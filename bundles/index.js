8f27e9473573e3da6297b716d00c3439060ef382
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-10-19 15:50:18 +0000

    3924a70 build: update io_bazel_rules_sass digest to 6e23fb1 (#58270)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-01e6fc1");
+var VERSION = new Version("19.0.0-next.10+sha-3924a70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
