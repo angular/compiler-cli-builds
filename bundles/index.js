513f305aed1610e62029cc9a91939eb9cdb27477
Andrew Kushnir <akushnir@google.com>
2024-10-21 16:14:03 +0000

    2e3e9b1 refactor(platform-server): replace patch by CLI option. (#58256)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-3924a70");
+var VERSION = new Version("19.0.0-next.10+sha-2e3e9b1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
