b41e076f8ba2c80dab008e18d96ca68ef1c9d2f1
Andrew Kushnir <akushnir@google.com>
2024-10-21 19:10:15 +0000

    9544930 refactor(core): add incremental hydration infrastructure (#58193)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-2e3e9b1");
+var VERSION = new Version("19.0.0-next.10+sha-9544930");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
