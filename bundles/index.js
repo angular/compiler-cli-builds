25f8995f2a800fba5a2fe40d27a97950ad7bb91f
Andrew Kushnir <akushnir@google.com>
2024-10-22 16:49:35 +0000

    713a14f build: update actions/cache digest to 6849a64 (#58305)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.8+sha-7e933d8");
+var VERSION = new Version("18.2.8+sha-713a14f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
