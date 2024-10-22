2a706b0773a6d939b9cd64d0f6a6ec53e207e03d
Andrew Kushnir <akushnir@google.com>
2024-10-22 18:36:14 +0000

    2fb88a5 ci: temporarily disable adev tests (#58309)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "18.2.8+sha-713a14f";
+export declare const PLACEHOLDER_VERSION = "18.2.8+sha-2fb88a5";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
