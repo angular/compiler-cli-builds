5098f81b3b68cbfc772aa1cf9203885821cc21a5
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-10-23 20:18:29 +0000

    2d8ae84 docs: release notes for the v18.2.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.10+sha-65de20c");
+var VERSION = new Version("19.0.0-next.10+sha-2d8ae84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
