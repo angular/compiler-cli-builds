a10bc6e3a0c4bfbb3e3c0706455b592cce12ebfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:30:05 +0000

    2f1f525 Update queries.md (#58330)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-8af71c0");
+var VERSION = new Version("19.1.0-next.0+sha-2f1f525");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
