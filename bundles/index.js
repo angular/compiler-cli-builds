7d50c1d69af04cfef8c85a8d48d16b5979372c61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 16:31:33 +0000

    395cb34 refactor(core): cleanup incremental hydration code (#58363)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.11+sha-00b79f8");
+var VERSION = new Version("19.0.0-next.11+sha-395cb34");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
