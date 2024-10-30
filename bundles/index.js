006cd554cd92f5b711b453c8fc1b21d2ad332ef0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 19:00:23 +0000

    dcd16b4 refactor(migrations): Make the explicit standalone migration idempotent (#58418)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-next.11+sha-3230d78");
+var VERSION = new Version("19.0.0-next.11+sha-dcd16b4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
