ad3773365b40a38e9eefd946f3614210a1cc6066
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 16:31:03 +0000

    69dce38 Revert fix(compiler): transform pseudo selectors correctly for the encapsulated view. (#58417)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.9+sha-1f13a5e");
+var VERSION = new Version("18.2.9+sha-69dce38");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
