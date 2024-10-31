5e6aaea7759f9aac1aeeed0b80c5445ba95149d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-10-31 17:16:50 +0000

    600ef5e refactor(core): tree-shake defer block registry (#58424)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.0+sha-0ca14e2");
+var VERSION = new Version("19.0.0-rc.0+sha-600ef5e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
