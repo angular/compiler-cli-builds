d0327211ab82f99fefeece397fcc299f43c782c3
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 14:46:53 +0000

    9d9abf2 build: Update publish-build-artifacts to honor ORG (#58408)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.10+sha-a249d0b");
+var VERSION = new Version("18.2.10+sha-9d9abf2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
