b6f2cb86a8d816fc2021af5974acb1eded11fc80
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 15:49:51 +0000

    adbc27c refactor(zone.js): Change the type of `_taskCounts` to an IndexSignature that can have keys (#51739) named as the values of the `TaskType` type.

--- bundles/chunk-Z5GU2OTU.js
+++ bundles/chunk-Z5GU2OTU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.10+sha-9d9abf2";
+var PLACEHOLDER_VERSION = "18.2.10+sha-adbc27c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
