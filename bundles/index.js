146646eaef5269aacae7ec4126c4e2a6578ab0ab
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-04 16:20:49 +0000

    e6b05c7 docs: fix grammatical error in template binding documentation (#58491)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.0+sha-6e0af6d");
+var VERSION = new Version("19.0.0-rc.0+sha-e6b05c7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
