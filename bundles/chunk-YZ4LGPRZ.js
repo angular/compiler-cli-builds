48b1af23e6125ff91cf2e0922f6afba7f6d25f78
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-05 15:43:31 +0000

    85fa27d build: update dependency angular-split to v18 (#58510)

--- bundles/chunk-YZ4LGPRZ.js
+++ bundles/chunk-YZ4LGPRZ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.0-rc.0+sha-e7ac965";
+var PLACEHOLDER_VERSION = "19.0.0-rc.0+sha-85fa27d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
