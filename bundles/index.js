9167e41fa5fce52cdf4b5f88901de6215a54b5f3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-05 07:19:28 +0000

    90509f8 build: update all non-major dependencies (#58506)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.0+sha-a32e897");
+var VERSION = new Version("19.0.0-rc.0+sha-90509f8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
