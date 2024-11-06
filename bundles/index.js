158a6a3a47f4b8051efb6c057e68141d50a1543c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-06 06:06:57 +0000

    b0711d7 refactor(core): clean up standalone flag (#58478)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.0+sha-85fa27d");
+var VERSION = new Version("19.0.0-rc.0+sha-b0711d7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
