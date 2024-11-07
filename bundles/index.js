b667f8cc8e5919e7c72a48494113b72c64ddedc4
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 15:54:20 +0000

    65a0a1d docs(docs-infra): add support for `@remarks` (#58523)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.1+sha-0ec9533");
+var VERSION = new Version("19.0.0-rc.1+sha-65a0a1d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
