2dba937d6c6a520ceaa6b420a69a12059fc84513
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-07 12:32:11 +0000

    b371384 docs: add component HMR information to application build system (#58528)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-0642f45");
+var VERSION = new Version("19.1.0-next.0+sha-b371384");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
