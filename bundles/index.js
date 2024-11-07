db4edf94713e501c8da39d2331acdcaf5b0d4724
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 14:35:32 +0000

    0ec9533 refactor(core): always create new renderer when applying HMR metadata update (#58527)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.1+sha-10e6a25");
+var VERSION = new Version("19.0.0-rc.1+sha-0ec9533");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
