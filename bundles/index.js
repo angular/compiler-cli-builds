e412ceed229e892821b8d36edd43e41c8262b5c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 16:57:08 +0000

    7504323 ci: re-enable adev tests (#58574)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-086cb2c");
+var VERSION = new Version("19.1.0-next.0+sha-7504323");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
