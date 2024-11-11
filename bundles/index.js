93aba5d3842b1b6131c44d0acfe1dcefa83572f8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 17:57:50 +0000

    549c0ad docs: remove "getting started" from headers on API ref page (#58595)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.1+sha-d92001a");
+var VERSION = new Version("19.0.0-rc.1+sha-549c0ad");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
