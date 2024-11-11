4c42b8141e46ada0c26c99353dc81b35ef2c6257
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 15:36:56 +0000

    086cb2c fix(compiler-cli): report individual diagnostics for unused imports (#58589)

--- bundles/index.js
+++ bundles/index.js
@@ -33,7 +33,7 @@ import {
   isTsDiagnostic,
   performCompilation,
   readConfiguration
-} from "./chunk-QIPX3PSK.js";
+} from "./chunk-X5WDWHOA.js";
 import {
   angularJitApplicationTransform,
   getDownlevelDecoratorsTransform,
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-7263917");
+var VERSION = new Version("19.1.0-next.0+sha-086cb2c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
