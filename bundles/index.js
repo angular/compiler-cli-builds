b0b16cec82c6a7605367f1922fbcc56780d2c6c6
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 18:44:19 +0000

    26602fc ci: remove circleci as we no longer rely on it (#58615)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.1+sha-560282a");
+var VERSION = new Version("19.0.0-rc.1+sha-26602fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
