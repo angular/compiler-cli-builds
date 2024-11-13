97c497cce35b1c1f06629a9a96f16ec2fc74c72e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 18:54:15 +0000

    e1e2e3f build: update io_bazel_rules_sass digest to b442a44 (#58617)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0-rc.1+sha-4abb645");
+var VERSION = new Version("19.0.0-rc.1+sha-e1e2e3f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
