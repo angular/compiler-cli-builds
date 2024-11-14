1ad52c6aab2b8a8eca368c1ee8bbca3eca8009d9
Andrew Kushnir <akushnir@google.com>
2024-11-14 22:27:11 +0000

    a55341b refactor(core): add `REQUEST`, `RESPONSE_INIT` and `REQUEST_CONTEXT` tokens (#58669)

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.1.0-next.0+sha-64cfe18";
+var PLACEHOLDER_VERSION = "19.1.0-next.0+sha-a55341b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
