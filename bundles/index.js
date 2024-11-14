47aa78953a16075a4276a96a98f0c9fe75b6a6f0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-14 16:19:09 +0000

    666a2a4 docs: release notes for the v18.2.12 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-0dc8757");
+var VERSION = new Version("19.1.0-next.0+sha-666a2a4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
