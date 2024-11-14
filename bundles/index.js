3baa1e94a963779f7673e33ebf2ebcf0ce30bfbc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-14 16:46:06 +0000

    64cfe18 docs: remove `standalone: true` across content (#58660)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-666a2a4");
+var VERSION = new Version("19.1.0-next.0+sha-64cfe18");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
