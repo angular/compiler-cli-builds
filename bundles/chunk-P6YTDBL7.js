8416dfc5f0ff909a61504b4dc09d06b25a355afd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-15 11:58:39 +0000

    e33b195 fix(bazel): allow strictStandalone to be passed through tsconfig (#58683)

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.0-rc.2+sha-806a61b";
+var PLACEHOLDER_VERSION = "19.0.0-rc.2+sha-e33b195";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
