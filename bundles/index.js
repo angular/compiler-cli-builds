e295afed40bafcdc48119b42587e3c82988b4949
Andrew Kushnir <akushnir@google.com>
2024-11-19 02:30:36 +0000

    0274e6c docs: update links in the roadmap (#58728)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-257fedc");
+var VERSION = new Version("19.1.0-next.0+sha-0274e6c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
