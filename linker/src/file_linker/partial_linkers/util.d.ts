09132b33e571a9ed7f2fe2806409bbfca4039e9b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-20 22:12:23 +0000

    a3be5b0 docs(compiler): add strictStandalone option to angular compiler options (#58785)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.0.0+sha-28254ca";
+export declare const PLACEHOLDER_VERSION = "19.0.0+sha-a3be5b0";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
