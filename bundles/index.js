e4e3ed1950cf2e44231d7d7223ffb14f814a34d5
Andrew Kushnir <akushnir@google.com>
2024-11-20 00:39:27 +0000

    f4e2777 docs: add details on how `@defer` loads deps (#58763)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0+sha-a983865");
+var VERSION = new Version("19.0.0+sha-f4e2777");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
