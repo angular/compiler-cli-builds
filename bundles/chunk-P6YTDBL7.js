93b4868ee025e49856fba1c5e030f9c0dcb4c632
Andrew Kushnir <akushnir@google.com>
2024-11-20 20:03:11 +0000

    7c9f425 ci: update to latest actions from angular/dev-infra (#58781)

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.0+sha-678f333";
+var PLACEHOLDER_VERSION = "19.0.0+sha-7c9f425";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
