27a1989c0778ceddef92754d5bbd6afdae8873a1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-21 21:45:16 +0000

    d3491c7 fix(core): Prevents race condition of cleanup for incremental hydration (#58722)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0+sha-5ce1026");
+var VERSION = new Version("19.0.0+sha-d3491c7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
