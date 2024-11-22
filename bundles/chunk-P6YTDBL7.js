b7c8b33411f0ed5fc0e2b79bdb011fdd1d700656
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 19:41:46 +0000

    84f45ea fix(core): make component id generation more stable between client and server builds (#58813)

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.0+sha-671fdce";
+var PLACEHOLDER_VERSION = "19.0.0+sha-84f45ea";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
