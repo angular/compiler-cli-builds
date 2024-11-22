5be3e2821b17a8d9731d0579d4931be3cafcd0fa
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 20:36:57 +0000

    1bdc3f1 docs(docs-infra): changed default type to object (#58289)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-834c7c3");
+var VERSION = new Version("19.1.0-next.0+sha-1bdc3f1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
