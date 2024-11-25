ea07e0ca40fab005f0c69e8f79c0db0b2b10044d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 16:17:58 +0000

    03a14b5 refactor(core): invoke `setActiveConsumer` in `ɵɵdeferHydrateWhen` at the right time (#58864)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.0+sha-08b9452");
+var VERSION = new Version("19.0.0+sha-03a14b5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
