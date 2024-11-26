f5c44dbd56c6aa311fdb2d6dc34ff48c4018fd20
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-26 18:09:28 +0000

    e1e419a release: cut the v18.2.13 release

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.12+sha-c6cec5e");
+var VERSION = new Version("18.2.13+sha-e1e419a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
