15c6313ea67676a3f673b17fd749eeb471f61f6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 16:04:58 +0000

    30891d8 refactor(core): Consolidates shouldTrigger* methods down to one (#58833)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-4a874bf");
+var VERSION = new Version("19.1.0-next.0+sha-30891d8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
