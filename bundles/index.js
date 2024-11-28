d1b9b03b10c9018f16fc72275b43f9da456f7831
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 16:46:19 +0000

    3eedb98 fix(docs-infra): do not focus h1 from docs-top-level-banner (#57698)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.1+sha-dabbeb6");
+var VERSION = new Version("19.0.1+sha-3eedb98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
