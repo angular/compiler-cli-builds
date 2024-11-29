0e15c642491a05e5d77f03737913303f593e9efb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-29 14:01:53 +0000

    ed965bd docs: update two-way binding example in documentation (#58963)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-357166e");
+var VERSION = new Version("19.1.0-next.0+sha-ed965bd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
