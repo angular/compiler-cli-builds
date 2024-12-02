bf12329fd75bffd62a7e0577785c8894b945b389
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 14:18:11 +0000

    f72a1b1 ci: mark devversion as unavailable (#59007)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.1+sha-c453f5d");
+var VERSION = new Version("19.0.1+sha-f72a1b1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
