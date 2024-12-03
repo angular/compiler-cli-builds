0a4a0fab338cf89074d20e7e5581dbc2d22f81ce
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 11:26:23 +0000

    a107bf8 build: update dependency mocha to v11 (#59012)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.1+sha-bc5ca99");
+var VERSION = new Version("19.0.1+sha-a107bf8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
