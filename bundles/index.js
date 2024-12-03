766cc02c0b63c5020423044a47b08ce7ca3057a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 15:19:33 +0000

    a37af17 docs: remove extra spacing from DI token note (#59023)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-fa0a9a5");
+var VERSION = new Version("19.1.0-next.0+sha-a37af17");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
