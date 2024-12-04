c77226cb5e2fd6f361910491a8d1ea86495ee931
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 19:25:06 +0000

    4dfe7f1 release: cut the v19.0.3 release

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.0.2+sha-a7fb45f";
+export declare const PLACEHOLDER_VERSION = "19.0.3+sha-4dfe7f1";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
