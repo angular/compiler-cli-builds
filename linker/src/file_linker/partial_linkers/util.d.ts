4e74b4f9b66dd51159434eee03077a4d45354d69
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 14:31:06 +0000

    7d2308f feat(docs-infra): move members descriptions section to the center of the page (#58610)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.0.1+sha-0df7b1e";
+export declare const PLACEHOLDER_VERSION = "19.0.1+sha-7d2308f";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
