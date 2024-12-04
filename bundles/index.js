cfd3c8a30a4b47d34a121f39b7e4c156d0e88bc9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 11:34:57 +0000

    e894a5d feat(core): set kind field on template and effect nodes (#58865)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.0+sha-8d6ea5b");
+var VERSION = new Version("19.1.0-next.0+sha-e894a5d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
