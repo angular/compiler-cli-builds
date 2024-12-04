98890e5ed3673904a8904e76d9ffa965cae5f2bc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 17:11:53 +0000

    7ad1f27 feat(docs-infra): use angular-ts and angular-html shiki highlighting (#59004)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.2+sha-5f3ba06");
+var VERSION = new Version("19.0.2+sha-7ad1f27");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
