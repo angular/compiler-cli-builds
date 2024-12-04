ce3941108133e04985b2c2de796b7e10b0f79ad5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 11:47:01 +0000

    0df7b1e refactor(common): remove standalone: true (#58949)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.1+sha-b8a2ae0");
+var VERSION = new Version("19.0.1+sha-0df7b1e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
