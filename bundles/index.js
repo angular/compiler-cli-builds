ec5244f2b09b69711ceaaf7b5efbd6f5f5b6f123
Joey Perrott <josephperrott@gmail.com>
2024-12-05 15:22:12 +0000

    73dc59d ci: update to latest version of dev-infra actions (#59070)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.2+sha-4a030f7");
+var VERSION = new Version("19.1.0-next.2+sha-73dc59d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
