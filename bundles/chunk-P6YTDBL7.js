68228186a0c17f98284477200a5c82133084ddd2
Joey Perrott <josephperrott@gmail.com>
2024-12-05 15:22:14 +0000

    47e314f ci: update to latest version of dev-infra actions (#59070)

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.3+sha-4dfe7f1";
+var PLACEHOLDER_VERSION = "19.0.3+sha-47e314f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
