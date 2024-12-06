d5fbe0d6043954969e632f530515ca3623133fd2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-06 22:43:00 +0000

    ff03fc8 build: update actions/cache digest to 1bd1e32 (#59077)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.3+sha-34ded10");
+var VERSION = new Version("19.0.3+sha-ff03fc8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
