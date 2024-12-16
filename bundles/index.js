ad1bb1c2ff365e31e78de3d4ffd32c5e98285f84
Andrew Kushnir <akushnir@google.com>
2024-12-16 18:34:04 +0000

    9e0b50b refactor(core): add `ENABLE_ROOT_COMPONENT_BOOTSTRAP` token (#59133)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.3+sha-1f4ff2f");
+var VERSION = new Version("19.1.0-next.3+sha-9e0b50b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
