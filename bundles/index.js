960c5658beb53a3931c1d4ac1bdbdfba5c94fa90
Andrew Kushnir <akushnir@google.com>
2024-12-16 18:31:21 +0000

    4d74f86 refactor(core): add skipped defer blocks count to ngDevMode (#59188)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.4+sha-3793218");
+var VERSION = new Version("19.0.4+sha-4d74f86");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
