d38fc7f0e2b6631bc5299cfa0b50919309598878
Andrew Kushnir <akushnir@google.com>
2024-12-16 19:40:21 +0000

    b74e06e docs: Fix Dependency Injection Readme guide link (#59193) PR Close #59193

--- bundles/chunk-P6YTDBL7.js
+++ bundles/chunk-P6YTDBL7.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.0.4+sha-4d74f86";
+var PLACEHOLDER_VERSION = "19.0.4+sha-b74e06e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.0.4+sha-4d74f86";
+export declare const PLACEHOLDER_VERSION = "19.0.4+sha-b74e06e";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
