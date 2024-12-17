9ed5e3f46c81cf62fc726b1b2573e89ba1803a91
Andrew Kushnir <akushnir@google.com>
2024-12-17 00:37:13 +0000

    f0df010 docs(docs-infra): replace newline with <br> (#59156)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.4+sha-b476537");
+var VERSION = new Version("19.0.4+sha-f0df010");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
