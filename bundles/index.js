60539f9ca82b46f87ee3623b0887f5669d37bae9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-12-18 19:10:38 +0000

    f28359e release: cut the v19.1.0-next.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.3+sha-f2a0321");
+var VERSION = new Version("19.1.0-next.4+sha-f28359e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
