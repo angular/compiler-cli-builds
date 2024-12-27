8e1b6d6a97260cc984f738e1f809a8272ff78a66
Joey Perrott <josephperrott@gmail.com>
2024-12-27 15:01:29 +0000

    f8d22a9 fix(bazel): allow `ng_package` to work with `rules_js` dependencies (#59316)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-8c5db3c");
+var VERSION = new Version("19.1.0-next.4+sha-f8d22a9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
