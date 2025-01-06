b19748a3080082b0fdca4837fa5120c90c9ed9e4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 22:29:59 +0000

    8eb8af5 build: lock file maintenance (#59291)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-2563f39");
+var VERSION = new Version("19.1.0-next.4+sha-8eb8af5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
