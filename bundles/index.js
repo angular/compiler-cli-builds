2fad189be7228f92c7fb49a251bc2ac28a747466
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 16:28:17 +0000

    b0ab674 refactor(devtools): add split to the demo devtools app (#58818)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.5+sha-b98416c");
+var VERSION = new Version("19.0.5+sha-b0ab674");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
