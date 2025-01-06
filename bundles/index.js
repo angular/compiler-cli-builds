0c3f47d12b812e9f77950f12a86773c5729f1e83
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 20:03:16 +0000

    66f2aa3 docs: update copyright year (#59359)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.5+sha-144bccb");
+var VERSION = new Version("19.0.5+sha-66f2aa3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
