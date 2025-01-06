8099a8844dd89456ac55d9f4ac738d30f57feb9b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 22:35:43 +0000

    b22677d docs(changelog): removed repetitive point from CHANGELOG.md of 18.0.0 (#59374)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-8eb8af5");
+var VERSION = new Version("19.1.0-next.4+sha-b22677d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
