4073ac89717b653d50462ca4020d6af7adf1109a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 19:24:16 +0000

    144bccb fix(router): avoid component ID collisions with user code (#59300)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.5+sha-d7a34dc");
+var VERSION = new Version("19.0.5+sha-144bccb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
