94c9f092b4127c556c65d5e4466780ac7799df14
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 16:12:00 +0000

    9b8f699 refactor: remove unnecessary `// tslint:disable` rule flags in devtools (#59365)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-0e23f20");
+var VERSION = new Version("19.1.0-next.4+sha-9b8f699");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
