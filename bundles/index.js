a7c1f2e8dc6aa5faacbee32e5de87356500f4f9a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 15:36:40 +0000

    97039e8 ci: remove `devversion` from unavailable pullapprove list (#59391)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-b22677d");
+var VERSION = new Version("19.1.0-next.4+sha-97039e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
