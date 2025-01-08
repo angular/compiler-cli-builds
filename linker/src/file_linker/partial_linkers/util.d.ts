af2ceff825807c3e390366ef486ad7cf15d772c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 15:38:23 +0000

    a5fc962 fix(core): Don't run effects in check no changes pass (#58250)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.1.0-next.4+sha-a25396d";
+export declare const PLACEHOLDER_VERSION = "19.1.0-next.4+sha-a5fc962";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
