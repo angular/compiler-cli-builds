8657ffeee87e397d51262760e7dde7d97bfc0883
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 15:28:58 +0000

    a25396d fix(bazel): support setting `type: module` in source `package.json` (#59406)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-d9707e1");
+var VERSION = new Version("19.1.0-next.4+sha-a25396d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
