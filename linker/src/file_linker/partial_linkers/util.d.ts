d1554fe81182919af5a0ab14a233dbe2b2a0c9a3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 17:24:01 +0000

    5e8ee7a refactor(docs-infra): simplify Preview component (#59404)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.1.0-next.4+sha-5c0d688";
+export declare const PLACEHOLDER_VERSION = "19.1.0-next.4+sha-5e8ee7a";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
