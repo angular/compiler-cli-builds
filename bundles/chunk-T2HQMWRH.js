069d4ffd3ec4a69d4758eed4806ae29d1f26eb03
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-01-08 18:12:15 +0000

    c69f12f docs: release notes for the v19.0.6 release

--- bundles/chunk-T2HQMWRH.js
+++ bundles/chunk-T2HQMWRH.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.1.0-next.4+sha-5e8ee7a";
+var PLACEHOLDER_VERSION = "19.1.0-next.4+sha-c69f12f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
