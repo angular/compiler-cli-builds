8ccdf2f9e664472b7f40ce36266c66ddafd6d987
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-01-08 18:29:14 +0000

    77b24cd release: cut the v19.1.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-next.4+sha-c69f12f");
+var VERSION = new Version("19.1.0-rc.0+sha-77b24cd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
