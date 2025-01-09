e8958112d002331aa4a92cf4b351b2f64d755c37
kirjs <kirjs@users.noreply.github.com>
2025-01-09 17:43:03 +0000

    57a98c3 refactor(platform-browser): drop Hammer token names in production (#59438)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.6+sha-8b3c9aa");
+var VERSION = new Version("19.0.6+sha-57a98c3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
