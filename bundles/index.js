252cc829c95f302c16847c31a2e6dec8cc44faf3
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:37:50 +0000

    a55575d fix(devtools): fixing dark mode colors for devtools and router tree (#59329)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-adc6c09");
+var VERSION = new Version("19.2.0-next.0+sha-a55575d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
