c015cfe68af30fddf861a5609c3c2cdfca678340
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:00:53 +0000

    adc6c09 build: clean up pipeline-specific tests (#59450)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-854a561");
+var VERSION = new Version("19.2.0-next.0+sha-adc6c09");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
