8b9a61cc0dc85c6e859418407cf20fb596dc8608
kirjs <kirjs@users.noreply.github.com>
2025-01-09 17:33:06 +0000

    c281807 refactor(core): drop platform injection token names in production (#59400)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-rc.0+sha-c6b66e5");
+var VERSION = new Version("19.1.0-rc.0+sha-c281807");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
