342ddf674e564090b09240fd7cfa16a58a9410f9
kirjs <kirjs@users.noreply.github.com>
2025-01-09 20:46:29 +0000

    2ec7664 release: bump Angular DevTools version to 1.0.20 (#59454)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0-rc.0+sha-679d4d1");
+var VERSION = new Version("19.1.0-rc.0+sha-2ec7664");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
