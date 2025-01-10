0eb47d1cf709018df56590a18fad0d5482b017ac
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:00:51 +0000

    534368c docs: update component scenarios guide (#59461) PR Close #59461

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.6+sha-178c619");
+var VERSION = new Version("19.0.6+sha-534368c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
