f061ac73c2bf25af3d7b9718a1df81e88cc9d6c2
kirjs <kirjs@users.noreply.github.com>
2025-01-10 18:33:26 +0000

    7d5d693 fix(dev-infra): remove no longer necessary scope (#59472)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-49dbf09");
+var VERSION = new Version("19.2.0-next.0+sha-7d5d693");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
