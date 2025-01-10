644b87f8fa48da09d1371f383c7f4f688ddb0607
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:57:09 +0000

    8496fb1 fix(migrations): incorrect stats when migrating queries with best effort mode (#59463)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-17510be");
+var VERSION = new Version("19.2.0-next.0+sha-8496fb1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
