593935a8575e33e0f08306bff8073bd1e4843370
kirjs <kirjs@users.noreply.github.com>
2025-01-13 17:43:46 +0000

    411b4f5 refactor(common): prevent duplicating `Accept` header (#59467)

--- bundles/chunk-T2HQMWRH.js
+++ bundles/chunk-T2HQMWRH.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.1.0-rc.0+sha-de12b52";
+var PLACEHOLDER_VERSION = "19.1.0-rc.0+sha-411b4f5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
