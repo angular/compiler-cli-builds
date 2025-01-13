68259fbd87912aa80fce692ca3cb260307eac1d9
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:52:01 +0000

    64f1bd7 docs: add component harnesses guides (#59078)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.6+sha-fea5c27");
+var VERSION = new Version("19.0.6+sha-64f1bd7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
