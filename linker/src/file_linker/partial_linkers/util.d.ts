daf95032507a0b009cf43d8d37a6bc8c4b08e225
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:43:06 +0000

    cb30fec docs: fix typo example code in pipes documentation (#59312)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.0-next.0+sha-40e7904";
+export declare const PLACEHOLDER_VERSION = "19.2.0-next.0+sha-cb30fec";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
