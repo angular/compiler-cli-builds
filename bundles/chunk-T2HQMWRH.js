5f6ce0a03a9c23b278763a89c514c9c7b0fddce1
kirjs <kirjs@users.noreply.github.com>
2025-01-13 20:05:20 +0000

    47e48b4 refactor(docs-infra): lazy-load `EmbeddedTutorialManager` in code editor (#59505)

--- bundles/chunk-T2HQMWRH.js
+++ bundles/chunk-T2HQMWRH.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-065e118";
+var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-47e48b4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
