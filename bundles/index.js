d60ea91b9e8180db116015cd3b971d7f68439ecd
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:11:58 +0000

    8830537 build: update cross-repo angular dependencies (#59520)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-b9155b5");
+var VERSION = new Version("19.2.0-next.0+sha-8830537");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
