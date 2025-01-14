5db1af1f1650db6ad44bd8afc2540efba91d8f5b
kirjs <kirjs@users.noreply.github.com>
2025-01-14 16:19:33 +0000

    803358f docs(docs-infra): change API reference list items order to top-down (#58655)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-d038b3c");
+var VERSION = new Version("19.2.0-next.0+sha-803358f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
