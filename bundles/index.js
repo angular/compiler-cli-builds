585de6efed2abc7056da405c469dc053f5133144
kirjs <kirjs@users.noreply.github.com>
2025-01-14 16:27:25 +0000

    f893d07 fix(core): destroy renderer when replacing styles during HMR (#59514)

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.6+sha-bb35df4");
+var VERSION = new Version("19.0.6+sha-f893d07");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
