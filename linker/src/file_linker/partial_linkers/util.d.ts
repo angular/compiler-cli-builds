038a8a62f46232a748a39024a6ecfe2c1398f3c2
kirjs <kirjs@users.noreply.github.com>
2025-01-14 14:54:42 +0000

    9963a73 docs: update component HMR to fully stable (#59503)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.0-next.0+sha-412ac30";
+export declare const PLACEHOLDER_VERSION = "19.2.0-next.0+sha-9963a73";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
