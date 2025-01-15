244913615ebd78ecf7e6948d584ca253ef29fb6b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-01-15 19:50:06 +0000

    004b53a release: cut the v19.0.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -80,7 +80,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.0.6+sha-681f5e4");
+var VERSION = new Version("19.0.7+sha-004b53a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
