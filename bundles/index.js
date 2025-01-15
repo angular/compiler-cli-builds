02ffdd78c7232f6c02627ee437fbfebc25cd0be1
kirjs <kirjs@users.noreply.github.com>
2025-01-15 14:49:31 +0000

    e702934 docs: fix typos in let.md (#59526) PR Close #59526

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-eadf6a3");
+var VERSION = new Version("19.2.0-next.0+sha-e702934");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
