41c719566b18963ddd07e68aa2a23e9cba46f4b8
Andrew Kushnir <akushnir@google.com>
2025-01-15 20:28:11 +0000

    2e138e6 refactor(common): prevent duplicating `Content-Type` header (#59518)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-e2925cc");
+var VERSION = new Version("19.2.0-next.0+sha-2e138e6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
