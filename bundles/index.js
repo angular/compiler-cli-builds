c905d2a35fb0c21cdae39a3745fa5ca584a8c099
Andrew Kushnir <akushnir@google.com>
2025-01-16 00:52:43 +0000

    0d019e9 release: bump Angular DevTools version to 1.0.21 (#59548)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.0+sha-810ed6a");
+var VERSION = new Version("19.1.0+sha-0d019e9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
