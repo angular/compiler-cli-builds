d63463c52cb88da3cbddee08d82e92707487dbd2
Andrew Kushnir <akushnir@google.com>
2025-01-16 22:02:11 +0000

    f6e9776 fix(core): cleanup `_ejsa` when app is destroyed (#59492)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-b76be83");
+var VERSION = new Version("19.2.0-next.0+sha-f6e9776");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
