3c417fbb543b34987dbb796a6dfb12c29192a8fa
Andrew Kushnir <akushnir@google.com>
2025-01-17 18:05:55 +0000

    14791f3 fix(core): cleanup stash listener when app is destroyed (#59598)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-4e6017a");
+var VERSION = new Version("19.2.0-next.0+sha-14791f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
