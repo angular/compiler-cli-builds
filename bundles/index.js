b1d274a31573eeed93f256a042e8a7ab87f06361
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 08:15:46 +0000

    8dcd889 fix(compiler): update `@ng/component` URL to be relative (#59620)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.1+sha-95a05bb");
+var VERSION = new Version("19.1.1+sha-8dcd889");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
