061009a4a50634d1847ba5ef5b0cba2b4ef154e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 14:19:23 +0000

    2d8fa73 docs: release notes for the v19.1.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-394a683");
+var VERSION = new Version("19.2.0-next.0+sha-2d8fa73");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
