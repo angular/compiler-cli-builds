8b430df4b9ee8f3e20bac974d10ea86f73e1418f
Andrew Kushnir <akushnir@google.com>
2025-01-21 21:38:08 +0000

    c089d21 refactor(core): reuse directive instantiate logic (#59633)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-4f46b02");
+var VERSION = new Version("19.2.0-next.0+sha-c089d21");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
