1eb189c03738ace430d56bba066f7ea96e102fbe
Andrew Kushnir <akushnir@google.com>
2025-01-22 20:35:31 +0000

    ae1dfdf refactor(core): prevent duplicating `isRootView` checks (#59614)

--- bundles/chunk-BYC2LPQI.js
+++ bundles/chunk-BYC2LPQI.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-2ed633a";
+var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-ae1dfdf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
