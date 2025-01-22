2725537c538d9b8e6573e1379170b22b14ecbcdc
Andrew Kushnir <akushnir@google.com>
2025-01-22 20:36:34 +0000

    7dc5116 refactor(core): prevent duplicating `isRootView` checks (#59614)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-66d07e7");
+var VERSION = new Version("19.1.3+sha-7dc5116");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
