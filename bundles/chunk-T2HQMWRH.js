e5c59d7d9663605c68d559fd290cd8fae94742e3
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-01-22 19:46:08 +0000

    66d07e7 release: cut the v19.1.3 release

--- bundles/chunk-T2HQMWRH.js
+++ bundles/chunk-T2HQMWRH.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.1.2+sha-38c8868";
+var PLACEHOLDER_VERSION = "19.1.3+sha-66d07e7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.2+sha-38c8868");
+var VERSION = new Version("19.1.3+sha-66d07e7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
