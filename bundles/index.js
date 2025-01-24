f860635ebd897151f19b08183208e718c720be81
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 08:32:46 +0000

    4867b64 test: update golden files (#59693)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-cdb439b");
+var VERSION = new Version("19.1.3+sha-4867b64");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
