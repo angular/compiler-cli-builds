a7a649a5f8edbc86d933be2d63167a0f22a272aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 11:50:17 +0000

    edb8407 refactor(common): simplify null/undefined check in `keyvalue` pipe (#59696)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-434568b");
+var VERSION = new Version("19.2.0-next.0+sha-edb8407");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
