9f42c1f46733dcefaa4e0f227f4f36c94e91a6ec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 16:23:31 +0000

    10cdf0a refactor(common): tree-shake `PreloadLinkCreator` for client bundles (#59431)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-1685164");
+var VERSION = new Version("19.2.0-next.0+sha-10cdf0a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
