99aba3d78e38a423f8b308678c09a952a3f5295c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 10:33:57 +0000

    4574ece docs: replace word in using-component-harnesses.md (#59687) PR Close #59687

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-9ea6337");
+var VERSION = new Version("19.1.3+sha-4574ece");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
