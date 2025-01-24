fe8ef7107572f4d4db55c747796c4455c733c3ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 10:56:32 +0000

    f8b8a3e refactor(forms): wrap `_checkParentType` with `ngDevMode` (#59489)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-4574ece");
+var VERSION = new Version("19.1.3+sha-f8b8a3e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
