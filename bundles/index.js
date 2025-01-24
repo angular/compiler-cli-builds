dc744ed5d7abe7ee88358ce9f04d856736f4c5d9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 19:47:21 +0000

    7a328ba docs: update Angular CLI help [19.1.x] (#59706)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-b4b36ed");
+var VERSION = new Version("19.1.3+sha-7a328ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
