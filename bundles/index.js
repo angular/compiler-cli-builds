9aeafe8c804b4dc0c49072c5bb80d56644381f14
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 15:54:56 +0000

    068cf29 refactor(core): remove unused LifecycleHooksFeature (#59678) (#59735)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-0a9c03c");
+var VERSION = new Version("19.1.3+sha-068cf29");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
