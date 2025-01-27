5513198b5b1200a031b1fc9a126268ec367b586b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:34:04 +0000

    1c84cbc fix(platform-browser): Update pseudoevent created by createMouseSpecialEvent to populate `_originalEvent` property (#59690)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-170c052");
+var VERSION = new Version("19.1.3+sha-1c84cbc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
