a521332f6d0e7c8f40cbb5739b77f6eb191d6524
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:00:15 +0000

    6d6708c docs: fix typo in signals-interop.md (#59724) PR Close #59724

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-0c3b755");
+var VERSION = new Version("19.2.0-next.0+sha-6d6708c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
