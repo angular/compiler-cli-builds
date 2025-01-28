d054125348392f93490e9667c16c308604a46afa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 08:13:27 +0000

    d0dc8f7 build: update github/codeql-action action to v3.28.6 (#59744)

--- bundles/chunk-T2HQMWRH.js
+++ bundles/chunk-T2HQMWRH.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.1.3+sha-2c770a2";
+var PLACEHOLDER_VERSION = "19.1.3+sha-d0dc8f7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-2c770a2");
+var VERSION = new Version("19.1.3+sha-d0dc8f7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
