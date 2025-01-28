ab04bf424e4d79971527a7e448c4cc3b17b1e33a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 12:14:30 +0000

    522acbf fix(service-worker): add missing `rxjs` peer dependency (#59747)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-f3d6c49");
+var VERSION = new Version("19.1.3+sha-522acbf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
