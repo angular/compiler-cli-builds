53234e2971d34fac1be3c270cea1313beb8d83f5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 10:37:17 +0000

    cb1cb0c refactor(core): reuse host directive resolution logic (#59685)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-873870f");
+var VERSION = new Version("19.1.3+sha-cb1cb0c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
