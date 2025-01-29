9932568ccd25ac53e8d1741a20184e9af9f1a43a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 16:50:38 +0000

    79f0736 docs: fix spelling of 'static-site' (#59780) PR Close #59780

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.3+sha-68dbaf5");
+var VERSION = new Version("19.1.3+sha-79f0736");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
