a7195d489df23c3d3ac4678513bb0d83b81f707c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 16:56:04 +0000

    0c1ecb4 fix(elements): not setting initial value on signal-based input (#59773)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-9ed9c40");
+var VERSION = new Version("19.2.0-next.0+sha-0c1ecb4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
