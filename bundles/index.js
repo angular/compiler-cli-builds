4912be71caae776b029a9d1cd969a6e2a54477e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 18:58:02 +0000

    32512de docs: update Angular CLI help [18.2.x] (#59767)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.13+sha-3cbc8a5");
+var VERSION = new Version("18.2.13+sha-32512de");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
