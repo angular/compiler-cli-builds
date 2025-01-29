210114f18df548ccd4815b8c143d2af83b794eae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 23:23:23 +0000

    9723f03 release: cut the v19.2.0-next.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.0+sha-eb9dca3");
+var VERSION = new Version("19.2.0-next.1+sha-9723f03");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
