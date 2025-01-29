d553ea4b8365025279224c5cae44b038412843ef
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-01-29 12:34:56 +0000

    3cbc8a5 ci: update to latest version of dev-infra actions

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.13+sha-e1e419a");
+var VERSION = new Version("18.2.13+sha-3cbc8a5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
