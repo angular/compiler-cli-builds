329d752c33e149d27e285ec931fb7e9e19b3a72d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 18:59:11 +0000

    b5f9018 docs: fix programmatic-rendering example (#59768)

--- bundles/chunk-BYC2LPQI.js
+++ bundles/chunk-BYC2LPQI.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-0c1ecb4";
+var PLACEHOLDER_VERSION = "19.2.0-next.0+sha-b5f9018";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
