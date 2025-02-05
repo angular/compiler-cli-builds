580c82ff0c56ff212ef0a4956974b50ad683f1de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-05 23:10:02 +0000

    951155e fix(docs-infra): reset sandbox runtime on template change (#59844)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.1+sha-8ee91bc");
+var VERSION = new Version("19.2.0-next.1+sha-951155e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
