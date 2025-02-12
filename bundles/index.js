28ec037d50773bf18feec86b2103871d7647878d
Andrew Scott <atscott@google.com>
2025-02-12 17:45:30 +0000

    9366e84 build: update dependency saucelabs to v9 (#59801)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.5+sha-f9e5900");
+var VERSION = new Version("19.1.5+sha-9366e84");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
