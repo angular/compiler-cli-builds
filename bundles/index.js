106d976555db6e46b7572b0816586ac1c5a19401
Andrew Scott <atscott@google.com>
2025-02-12 18:04:25 +0000

    01f669a fix(compiler): handle tracking expressions requiring temporary variables (#58520)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.5+sha-b41a263");
+var VERSION = new Version("19.1.5+sha-01f669a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
