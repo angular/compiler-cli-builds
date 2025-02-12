790bf083146be361fe219fd9a9af40921cdcd735
Andrew Scott <atscott01@gmail.com>
2025-02-12 20:07:43 +0000

    20df2cd release: cut the v19.1.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.5+sha-01f669a");
+var VERSION = new Version("19.1.6+sha-20df2cd");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
