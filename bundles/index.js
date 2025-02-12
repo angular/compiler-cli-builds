df7e4502dead6ee8342ab09d4f9babe0716d95d3
Andrew Scott <atscott@google.com>
2025-02-12 18:52:53 +0000

    4f3ad98 refactor(devtools): styles management (#59589)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.2+sha-9e847fc");
+var VERSION = new Version("19.2.0-next.2+sha-4f3ad98");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
