b5a18a504cb973aa404758be8585c30a425ceb84
Andrew Scott <atscott@google.com>
2025-02-12 18:04:16 +0000

    9e847fc fix(compiler): handle tracking expressions requiring temporary variables (#58520)

--- bundles/chunk-BYC2LPQI.js
+++ bundles/chunk-BYC2LPQI.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0-next.2+sha-6f315fe";
+var PLACEHOLDER_VERSION = "19.2.0-next.2+sha-9e847fc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.2+sha-6f315fe");
+var VERSION = new Version("19.2.0-next.2+sha-9e847fc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
