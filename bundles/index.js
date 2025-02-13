8c033cddaa3eea0669de5a82379772426b17ad2b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 22:40:22 +0000

    1b4fc8e ci: fix flakey incremental hydration test (#59945)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.6+sha-3fd69d7");
+var VERSION = new Version("19.1.6+sha-1b4fc8e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
