e8d875565dd559e11c8a1abdb9cdd3880d84d8ac
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 15:08:56 +0000

    9f09214 docs(docs-infra): replace the WebGL home page animation with a CSS+JS version of it (#59865)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.2+sha-adf2a0a");
+var VERSION = new Version("19.2.0-next.2+sha-9f09214");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
