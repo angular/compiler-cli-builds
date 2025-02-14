fdb9f51c01762f2a85eb33b54fe0f953a18df04f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 18:05:29 +0000

    47d5e1e build: update dependency jsdom to v26 (#59444)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.3+sha-5ca2fa5");
+var VERSION = new Version("19.2.0-next.3+sha-47d5e1e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
