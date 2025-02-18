73b24aea3b86bb282368877794663ab17edf762a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 15:49:27 +0000

    c076b57 refactor(core): move LContainer manipulation logic to its own file (#59856)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.3+sha-ea5b737");
+var VERSION = new Version("19.2.0-next.3+sha-c076b57");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
