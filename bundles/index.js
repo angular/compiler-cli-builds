67469ab040bafb5a41a792e74469571d02aa1f3d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 19:34:17 +0000

    cf36951 fix(forms): Fix typing on `FormRecord`. (#59993)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-next.3+sha-1cd3a7d");
+var VERSION = new Version("19.2.0-next.3+sha-cf36951");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
