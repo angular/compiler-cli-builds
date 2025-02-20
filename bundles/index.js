29491c5f45ba58d58958eb023f9ea5287d72439e
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:02:10 +0000

    c513e5d refactor(devtools): fix component inspector highlighting (#59995)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.3.0-next.0+sha-7a8e8f3");
+var VERSION = new Version("19.3.0-next.0+sha-c513e5d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
