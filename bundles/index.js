353dbdc089fc3ec63b8414243f721328979c3fc9
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:08:58 +0000

    af5155f docs: Fix typo ')' in banner-initial.component.spec.ts (#60032)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.3.0-next.0+sha-b7dd5a1");
+var VERSION = new Version("19.3.0-next.0+sha-af5155f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
