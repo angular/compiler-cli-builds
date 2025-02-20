450e6654223e9ab2c59314f6a83882748e74693c
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:27:22 +0000

    7a8e8f3 fix(benchpress): Ensure future-proof correct initialization order (#60025)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.3.0-next.0+sha-af5155f");
+var VERSION = new Version("19.3.0-next.0+sha-7a8e8f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
