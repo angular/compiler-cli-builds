bc40e998162cd571348b242afeab8eb3e1ca4378
kirjs <kirjs@users.noreply.github.com>
2025-02-24 15:20:27 +0000

    17c84e5 docs: use `@angular/build` directly in example/tutorial projects (#60046)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.3.0-next.0+sha-628ab40");
+var VERSION = new Version("19.3.0-next.0+sha-17c84e5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
