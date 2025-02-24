ed4b19e5f1dd22608e3226ef558849007ea50d08
kirjs <kirjs@users.noreply.github.com>
2025-02-24 17:01:33 +0000

    e601bdc fix(core): capture stack for HMR errors (#60067)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.3.0-next.0+sha-4853129");
+var VERSION = new Version("19.3.0-next.0+sha-e601bdc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
