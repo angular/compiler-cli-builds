9015ebac4b4d674c57700eb17e4436470ed3d86a
kirjs <kirjs@users.noreply.github.com>
2025-02-25 15:55:17 +0000

    94117f1 build: update all non-major dependencies (#60093)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.7+sha-7a88ce5");
+var VERSION = new Version("19.1.7+sha-94117f1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
