52fc1793e6292f21853c44dbc4fa29aa639f3483
kirjs <kirjs@users.noreply.github.com>
2025-02-25 15:30:02 +0000

    7a88ce5 docs: update security guide (#59885) PR Close #59885

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.7+sha-c611c8d");
+var VERSION = new Version("19.1.7+sha-7a88ce5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
