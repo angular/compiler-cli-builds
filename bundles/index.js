6776008e054bdc33310a9411549ea87fae3e95fe
kirjs <kirjs@users.noreply.github.com>
2025-02-25 16:16:13 +0000

    af2e79e refactor(core): remove performance mark feature for signals (#59991)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-f2d5cf7");
+var VERSION = new Version("20.0.0-next.0+sha-af2e79e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
