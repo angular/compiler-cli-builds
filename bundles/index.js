03502f9d4c1b3f864d231d50d9a6b7b1a16b701f
kirjs <kirjs@users.noreply.github.com>
2025-02-25 19:18:38 +0000

    c4126c7 refactor(forms): tree-shake `_checkParentType` in production (#60041)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-rc.0+sha-7d66360");
+var VERSION = new Version("19.2.0-rc.0+sha-c4126c7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
