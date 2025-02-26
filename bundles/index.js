fddd0882ad136c17be0f8f9ad487de9f054b30a9
kirjs <kirjs@users.noreply.github.com>
2025-02-26 17:40:38 +0000

    b070100 fix(devtools): minor UI/UX improvements of the Injector Providers panel (#59531)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-911a210");
+var VERSION = new Version("20.0.0-next.0+sha-b070100");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
