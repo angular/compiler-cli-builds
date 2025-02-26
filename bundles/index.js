7bd16f8c26b1f2782ab51ad9e5c658964b102be8
Kirill Cherkashin <kirill@kcherkashin.org>
2025-02-26 19:16:20 +0000

    a8a3534 release: cut the v19.1.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.7+sha-41ef1cb");
+var VERSION = new Version("19.1.8+sha-a8a3534");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.1.7+sha-41ef1cb";
+export declare const PLACEHOLDER_VERSION = "19.1.8+sha-a8a3534";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
