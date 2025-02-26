64aca99569428fdf1daf661844e6fcaa567e5ffe
Kirill Cherkashin <kirill@kcherkashin.org>
2025-02-26 21:58:11 +0000

    ab2ee22 release: cut the v20.0.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-ef18755");
+var VERSION = new Version("20.0.0-next.0+sha-ab2ee22");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
