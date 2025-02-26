3bbfccbfe7ea9f1541a568e0046c37fcb6febe04
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:15:58 +0000

    5de4b0c refactor(core): inline `depPath` in `throwCyclicDependencyError` to be dropped (#60084)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0-rc.0+sha-fca2132");
+var VERSION = new Version("19.2.0-rc.0+sha-5de4b0c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
