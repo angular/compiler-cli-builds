2be700f3753846e9f9e103b94915db934030ec41
kirjs <kirjs@users.noreply.github.com>
2025-02-26 17:06:42 +0000

    41ef1cb docs(docs-infra): remove explicit `dectectChanges()` (#60098)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.1.7+sha-567d48f");
+var VERSION = new Version("19.1.7+sha-41ef1cb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
