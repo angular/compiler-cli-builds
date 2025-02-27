b9f81abeec9381dd6d4d8fae94ea4e90476b4055
Miles Malerba <mmalerba@google.com>
2025-02-27 18:31:03 +0000

    b09f4a5 docs(docs-infra): add a tooltip to show the full text when nav item text is lengthy (#59038)

--- bundles/chunk-J7S5XIBF.js
+++ bundles/chunk-J7S5XIBF.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0+sha-e549bde";
+var PLACEHOLDER_VERSION = "19.2.0+sha-b09f4a5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
