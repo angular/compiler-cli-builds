77fdd786469e755fb0e8a89a07ec55ede4159a47
Miles Malerba <mmalerba@google.com>
2025-02-28 18:53:41 +0000

    78b27a8 refactor(core): Move getCurrentInjector/setCurrentInjector to primitives package. (#60090)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-af02914");
+var VERSION = new Version("20.0.0-next.0+sha-78b27a8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
