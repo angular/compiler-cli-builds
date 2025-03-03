56a5cfcd22869aa7d58e360a112b5c5ec3bef830
Miles Malerba <mmalerba@google.com>
2025-03-03 22:26:07 +0000

    cd31bfc docs: change broke link (#60189)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-2717416");
+var VERSION = new Version("19.2.0+sha-cd31bfc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
