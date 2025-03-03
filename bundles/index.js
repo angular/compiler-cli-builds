cbf13b26a2966009a026d7b25e61dfee4d1c1ec7
Miles Malerba <mmalerba@google.com>
2025-03-03 17:20:07 +0000

    c2de5f6 fix(common): clean up `onUrlChange` listener when root scope is destroyed (#60004)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-1dd9447");
+var VERSION = new Version("19.2.0+sha-c2de5f6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
