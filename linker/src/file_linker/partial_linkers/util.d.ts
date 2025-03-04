19305860cba0bc8acdf23f595308834c3fe70526
Miles Malerba <mmalerba@google.com>
2025-03-04 22:14:48 +0000

    fbbe5d5 docs(migrations): Fix indentation in documentation for signal inputs migration (#60204) PR Close #60204

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.0+sha-31bbbe9";
+export declare const PLACEHOLDER_VERSION = "19.2.0+sha-fbbe5d5";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
