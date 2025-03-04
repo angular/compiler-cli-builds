bccb65a9c2a05f82a5d4cf498e082e8e2c283911
Miles Malerba <mmalerba@google.com>
2025-03-04 22:26:26 +0000

    a64a575 refactor(docs-infra): AppComponent cleanups (#58897)

--- bundles/chunk-J7S5XIBF.js
+++ bundles/chunk-J7S5XIBF.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.0+sha-fbbe5d5";
+var PLACEHOLDER_VERSION = "19.2.0+sha-a64a575";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
