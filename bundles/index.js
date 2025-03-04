8257b7b81ada4e0b10dd3f2bb1f4b08334335bc2
Miles Malerba <mmalerba@google.com>
2025-03-04 17:33:50 +0000

    0007d20 docs: fix the wrong way to get a signal value (#59194)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-2e03a86");
+var VERSION = new Version("20.0.0-next.0+sha-0007d20");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
