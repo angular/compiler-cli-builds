aafa2bc29b590652a154e3c015ed121341896d34
Miles Malerba <mmalerba@google.com>
2025-03-04 19:43:32 +0000

    8579ff8 docs: Remove misleading default value DEFAULT_CURRENCY_CODE (#59977)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-aff6d3d");
+var VERSION = new Version("19.2.0+sha-8579ff8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
