bd93a02f13642aad831928515c154bdd9ecaee61
Miles Malerba <mmalerba@google.com>
2025-03-05 16:59:46 +0000

    65d2ca9 refactor(devtools): ignore `ng.applyChanges` if not supported (#60207)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-a2db87e");
+var VERSION = new Version("19.2.0+sha-65d2ca9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
