888186d21dd041838345fb9c9a4f20c44d3fbe2b
Miles Malerba <mmalerba@google.com>
2025-03-05 17:00:45 +0000

    9068163 docs: update Angular CLI help [main] (#60216)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-4a8d0b7");
+var VERSION = new Version("20.0.0-next.0+sha-9068163");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
