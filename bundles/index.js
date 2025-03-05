403fdc3c4abe386df0940ef281b9473a802c3617
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-05 10:10:15 +0000

    a2db87e docs(docs-infra): fix secondary menu (#60212)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-a64a575");
+var VERSION = new Version("19.2.0+sha-a2db87e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
