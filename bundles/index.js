189bcb1f4b904a6ec11f274ce0e96a135c1d26c7
Miles Malerba <mmalerba@google.com>
2025-03-05 19:26:24 +0000

    c492db4 release: cut the v20.0.0-next.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.0+sha-8eebec9");
+var VERSION = new Version("20.0.0-next.1+sha-c492db4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
