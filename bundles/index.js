4e7be57117f0ba4702ddd36d54b9f824d4071827
Andrew Kushnir <akushnir@google.com>
2025-03-05 21:53:17 +0000

    ac94604 refactor(core): Remove the optional flag from Injector and all casts. (#60154)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.1+sha-1d3b914");
+var VERSION = new Version("20.0.0-next.1+sha-ac94604");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
