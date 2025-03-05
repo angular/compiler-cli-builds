79066a563b51da76946d83c24ddf7437a03fac24
Miles Malerba <mmalerba@google.com>
2025-03-05 19:17:24 +0000

    d9e022d release: cut the v19.2.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.0+sha-48d899f");
+var VERSION = new Version("19.2.1+sha-d9e022d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
