13cc28c37b8ba791e048b55a5d719ad9b443f4c2
Andrew Kushnir <akushnir@google.com>
2025-03-06 19:33:12 +0000

    0c10547 docs: update Angular CLI help [19.2.x] (#60249)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-48dc0d6");
+var VERSION = new Version("19.2.1+sha-0c10547");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
