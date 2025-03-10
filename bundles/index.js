f904a141af853569764c7bb8fb407def03fc000b
Andrew Kushnir <akushnir@google.com>
2025-03-10 18:48:54 +0000

    15c4296 fix(platform-server): add missing peer dependency for `rxjs` (#60308)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-23ca885");
+var VERSION = new Version("19.2.1+sha-15c4296");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
