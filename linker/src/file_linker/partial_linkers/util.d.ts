c49bd901df3acd9e9dc45e8721b11ff5b2f1e3c5
Andrew Kushnir <akushnir@google.com>
2025-03-10 15:22:46 +0000

    338818c fix(core): Surface errors from `ApplicationRef.tick` to callsite (#60102)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.0-next.1+sha-a02e270";
+export declare const PLACEHOLDER_VERSION = "20.0.0-next.1+sha-338818c";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
