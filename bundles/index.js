dccd756e4ac4b2110561d2c56b1d783267c47149
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:39:06 +0000

    44155c9 refactor(bazel): remove primary_bundle_name as attr for ng_package (#60306)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-5e2f312");
+var VERSION = new Version("19.2.1+sha-44155c9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
