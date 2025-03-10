e923254519aa668c39a04a56a553c3fa5583efcc
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:29:03 +0000

    5e2f312 fix(docs-infra): terminal light mode text color (#60246)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-bde1b37");
+var VERSION = new Version("19.2.1+sha-5e2f312");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
