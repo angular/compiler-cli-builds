274cfe4bac4edef17220dd12d47762a2fa0252a9
Andrew Kushnir <akushnir@google.com>
2025-03-10 21:04:19 +0000

    f2a8006 refactor(core): replace `PLATFORM_ID` in `initTransferState` (#60299)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.1+sha-0cff9a1");
+var VERSION = new Version("20.0.0-next.1+sha-f2a8006");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
