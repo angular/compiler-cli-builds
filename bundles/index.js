576730efc3ef89718d50d37efe797e2a24fe83bc
Andrew Kushnir <akushnir@google.com>
2025-03-11 16:32:59 +0000

    e59432c refactor(devtools): refactors property view drag-and-drop behavior (#60286)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-30f6db3");
+var VERSION = new Version("19.2.1+sha-e59432c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
