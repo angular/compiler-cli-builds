18f340673b42877d2aa0ffccf95ba53f043f7e92
Andrew Kushnir <akushnir@google.com>
2025-03-11 21:32:01 +0000

    705fb0d docs: add mention of GH issue for devPreview feedback (#60010)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-b6f22a7");
+var VERSION = new Version("19.2.1+sha-705fb0d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
