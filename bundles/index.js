4c21e312298109ba98ac6b7db282cc3fba6aff4c
Andrew Kushnir <akushnir@google.com>
2025-03-11 18:38:39 +0000

    8be6e38 refactor(core): replace internal usages of InjectFlags (#60318)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.1+sha-9a124c8");
+var VERSION = new Version("20.0.0-next.1+sha-8be6e38");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
