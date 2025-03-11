61e9e824867a48a22493819b97cb0df03b4a43ec
Andrew Kushnir <akushnir@google.com>
2025-03-11 20:10:17 +0000

    b6f22a7 refactor(core): inline standalone check to be dropped (#60305)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.1+sha-f9b6bf9");
+var VERSION = new Version("19.2.1+sha-b6f22a7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
