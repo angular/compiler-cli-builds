b4b3eb018249a0d7afe53f7c993bcc58d5b01253
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:09:20 +0000

    8caf9d7 docs: added short explanation about event reply (#60349)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-8b888b2");
+var VERSION = new Version("19.2.2+sha-8caf9d7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
