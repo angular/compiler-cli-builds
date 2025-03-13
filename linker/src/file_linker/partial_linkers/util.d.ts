2b397b84a848d7dc2b0e62ca71c1dcfb982ef281
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 14:56:17 +0000

    8b888b2 docs: note that CSS is also taken into account in the max bundle size (#60362)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.2+sha-4a30a4e";
+export declare const PLACEHOLDER_VERSION = "19.2.2+sha-8b888b2";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
