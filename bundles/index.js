ad829b7bbfd509d254e3b9cb331800f5dedc2c26
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-17 11:28:16 +0000

    bb7e948 refactor(core): Add recommendation for string tokens on `@Inject` (#60326)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.2+sha-1668b9f");
+var VERSION = new Version("20.0.0-next.2+sha-bb7e948");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
