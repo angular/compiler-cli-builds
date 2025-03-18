e1825190e066e67cc6265aa3e1891870c4ce630a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 17:22:01 +0000

    7d47678 refactor: remove peer dependency on core from compiler (#60437)

--- bundles/chunk-J7S5XIBF.js
+++ bundles/chunk-J7S5XIBF.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.2+sha-9291d52";
+var PLACEHOLDER_VERSION = "19.2.2+sha-7d47678";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-9291d52");
+var VERSION = new Version("19.2.2+sha-7d47678");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
