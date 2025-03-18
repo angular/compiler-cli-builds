1401bc3855c22ed436ad91de0951688dcd922b5e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:37:38 +0000

    9291d52 refactor(platform-server): expose internal `render` method for extensibility (#60416)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-9dffdd7");
+var VERSION = new Version("19.2.2+sha-9291d52");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
