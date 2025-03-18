669d5dc6bdb2932001d28edf27a553a4d35ea412
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:17:24 +0000

    b4f3352 docs: fix typo (#60431) PR Close #60431

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-2364be9");
+var VERSION = new Version("19.2.2+sha-b4f3352");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
