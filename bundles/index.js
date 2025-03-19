b2a728daa71d6ca89a2d451c7ae97ba594309ded
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 17:45:45 +0000

    c7cacbf ci: fix flakey defer test (#60461)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.2+sha-7e65b9f");
+var VERSION = new Version("20.0.0-next.2+sha-c7cacbf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
