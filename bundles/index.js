7972fc80c1571c26ec3f032b32c342e4465cdb4e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 14:58:23 +0000

    13a8709 fix(core): catch hydration marker with implicit body tag (#60429)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-9fea601");
+var VERSION = new Version("19.2.2+sha-13a8709");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
