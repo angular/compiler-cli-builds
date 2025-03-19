5ac4cc38930c2b4cf253ecd9be6ca75d7529f847
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 19:33:00 +0000

    2be0089 release: cut the v19.2.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.2+sha-2466fc9");
+var VERSION = new Version("19.2.3+sha-2be0089");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
