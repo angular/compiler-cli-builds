8280f486477c8693115198fd2cf0fee200012b72
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:39:21 +0000

    70bdb88 fix(platform-browser): ensure `platformBrowserTesting` includes `platformBrowser` providers (#60480)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.3+sha-911ad40");
+var VERSION = new Version("20.0.0-next.3+sha-70bdb88");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
