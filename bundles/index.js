cbd92bdf6190e9af0e2d95ba3c6fbed70db52a0a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-20 07:46:35 +0000

    164bb3f build: disable RBE for compliance partial goldens (#60473)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.3+sha-f901a4c");
+var VERSION = new Version("20.0.0-next.3+sha-164bb3f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
