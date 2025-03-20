1f16e14b225875884b322f8ba2f83543bb31a725
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:03:12 +0000

    911ad40 build: fix type checking issues in test code (#60481)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.3+sha-164bb3f");
+var VERSION = new Version("20.0.0-next.3+sha-911ad40");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
