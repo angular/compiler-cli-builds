5b846c08462a9a3413aa4ff69a36f2a05ae90260
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-24 07:01:30 +0000

    dcfa8a0 docs(docs-infra): disable shared-docs tests. (#60520)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.3+sha-13d1c8a");
+var VERSION = new Version("20.0.0-next.3+sha-dcfa8a0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
