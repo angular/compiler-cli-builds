0fd732daf3b89fb2178d31220a11010066d2d983
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 14:13:33 +0000

    9ac4057 refactor(core): specify reactive node kind for linked signal (#60451)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.3+sha-d6a1a3a");
+var VERSION = new Version("19.2.3+sha-9ac4057");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
