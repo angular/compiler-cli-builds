8ba6a1560f023ddde37ef786be7d247937045793
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-25 18:04:31 +0000

    6d3849f refactor(core): updates to golden files (#60227)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.3+sha-7499b74");
+var VERSION = new Version("20.0.0-next.3+sha-6d3849f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
