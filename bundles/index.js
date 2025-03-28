9849255e50329efd086fbcea03fa7770ff9f0e98
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 18:51:23 +0000

    cbd6ec8 build: remove obsolete bundling test apps (#60591)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-d15fda3");
+var VERSION = new Version("20.0.0-next.4+sha-cbd6ec8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
