f7e1b6ecb2742e17df55a9466981d8fe2c8e87b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 11:57:01 +0000

    958e98e fix(router): Add missing types to transition (#60307)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.4+sha-f4c4b10");
+var VERSION = new Version("19.2.4+sha-958e98e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
