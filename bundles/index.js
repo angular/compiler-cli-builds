05c115cbfe6091a88c0a36e6e8627794c7e3e04a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 21:42:18 +0000

    ea62a4f fix(language-service): Update adapter to log instead of throw errors (#60651)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-97556e6");
+var VERSION = new Version("20.0.0-next.4+sha-ea62a4f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
