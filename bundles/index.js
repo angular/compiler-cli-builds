f5d60cdf526cb8e8a4ebfca3b7f1d43aaf530905
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:04:05 +0000

    b222288 docs: remove todo comment (#59928)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.4+sha-f3361fb");
+var VERSION = new Version("19.2.4+sha-b222288");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
