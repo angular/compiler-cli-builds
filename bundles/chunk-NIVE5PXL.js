fc437b90616d1f16079f02d84e90441d8be5efc6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 14:48:27 +0000

    52dfce6 build: lock file maintenance (#60632)

--- bundles/chunk-NIVE5PXL.js
+++ bundles/chunk-NIVE5PXL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-next.4+sha-9c106f4";
+var PLACEHOLDER_VERSION = "20.0.0-next.4+sha-52dfce6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
