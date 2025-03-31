471c7a6aa773d3c9c3739a118e88e848b0c2e780
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:32:52 +0000

    51c303a refactor(core): remove TODOs referencing #24571 (#60648)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-4e478af");
+var VERSION = new Version("20.0.0-next.4+sha-51c303a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
