3ead3e7249ad60212961919162150dd19ecf8fb3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:23:15 +0000

    4e478af docs(docs-infra): add search via queryparam all adev routes (#57567)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.0-next.4+sha-302cb83";
+export declare const PLACEHOLDER_VERSION = "20.0.0-next.4+sha-4e478af";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
