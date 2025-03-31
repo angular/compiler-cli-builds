001b95d1818c8e708f63e3ec79d57c4f1f4780e6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:52:24 +0000

    1fed7dc refactor(router): drop special handling of the OutletInjector (#58351)

--- bundles/chunk-NIVE5PXL.js
+++ bundles/chunk-NIVE5PXL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-next.4+sha-79d502f";
+var PLACEHOLDER_VERSION = "20.0.0-next.4+sha-1fed7dc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
