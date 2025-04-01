8e90fdf41fee7563f86859349c6ab9db1199c291
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 12:24:27 +0000

    b3c7282 Revert "refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)" (#60669)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.4+sha-e816d2d";
+export declare const PLACEHOLDER_VERSION = "19.2.4+sha-b3c7282";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
