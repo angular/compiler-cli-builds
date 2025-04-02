af36b1c5a281847ccde476178d054c45eedd02e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:25:43 +0000

    4f86f3f docs(docs-infra): fix algolia icon in the search dialog on safari (#60702)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-5f7f046");
+var VERSION = new Version("20.0.0-next.4+sha-4f86f3f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
