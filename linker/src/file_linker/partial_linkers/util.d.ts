f50544fdda673bc841786ae91f64823725b1d939
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 18:32:43 +0000

    1c7b356 fix(core): release `hasPendingTasks` observers (#59723)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.0-next.4+sha-0bb4bd6";
+export declare const PLACEHOLDER_VERSION = "20.0.0-next.4+sha-1c7b356";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
