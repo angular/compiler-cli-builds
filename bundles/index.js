2ffb37931315d1d6c930891bd9871092a353b388
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:04:08 +0000

    316fabb docs: add a note about styles collision resolution (#59266) PR Close #59266

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-d3fec85");
+var VERSION = new Version("20.0.0-next.4+sha-316fabb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
