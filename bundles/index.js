5853547253a4be97fbc25dcc03b5651f0be9811f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:21:07 +0000

    6acce7c fix(core): fixes #592882 ng generate @angular/core:signal-queries-migration (#60688)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.4+sha-b6897bb");
+var VERSION = new Version("20.0.0-next.4+sha-6acce7c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
