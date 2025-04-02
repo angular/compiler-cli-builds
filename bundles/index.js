525f5b67e5435a90657d3e181a853d3a9d0fe7ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:04:25 +0000

    0c5b697 docs: add a note about styles collision resolution (#59266) PR Close #59266

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.4+sha-f90dc69");
+var VERSION = new Version("19.2.4+sha-0c5b697");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
