dce8f56a79b1b732d6deaa43a67313c3e6d21eee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:06:37 +0000

    635fb00 refactor(service-worker): pull less RxJS symbols (#60657)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.4+sha-8f68d1b";
+export declare const PLACEHOLDER_VERSION = "19.2.4+sha-635fb00";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
