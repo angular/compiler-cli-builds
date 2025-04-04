3ecdbff4647ee7becf5c0505c9731b4620e6a3a8
Andrew Scott <atscott@google.com>
2025-04-04 17:41:06 +0000

    8d050b5 feat(core): stabilize linkedSignal API (#60741)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.5+sha-3ea0163");
+var VERSION = new Version("20.0.0-next.5+sha-8d050b5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
