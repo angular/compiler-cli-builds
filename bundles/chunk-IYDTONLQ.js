be0055e6390cef7913f215071db9d4adbdd5eb70
Andrew Scott <atscott@google.com>
2025-04-07 20:39:35 +0000

    35de6f6 build: update io_bazel_rules_sass digest to b4800f3 (#60737)

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.5+sha-3441f7b";
+var PLACEHOLDER_VERSION = "19.2.5+sha-35de6f6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
