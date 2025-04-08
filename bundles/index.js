1ba838a35a94c73471bf07765d4ad2b3f4e742f6
Andrew Scott <atscott@google.com>
2025-04-08 17:25:12 +0000

    56db79a refactor(core): use stream in rxResource instead of loader (#59910)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.5+sha-8b9df44");
+var VERSION = new Version("19.2.5+sha-56db79a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
