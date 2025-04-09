6a72476e433187e7dd919dc3772c1ddaff8e8ac2
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:31:39 +0000

    54f9269 release: cut the v19.2.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.5+sha-fc946c5");
+var VERSION = new Version("19.2.6+sha-54f9269");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
