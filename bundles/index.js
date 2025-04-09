8519aead1d1354e8cc363443a8747b9cefa21703
Andrew Scott <atscott@google.com>
2025-04-09 13:59:55 +0000

    df42976 build: update github/codeql-action action to v3.28.15 (#60780)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.5+sha-b8d9f95");
+var VERSION = new Version("20.0.0-next.5+sha-df42976");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
