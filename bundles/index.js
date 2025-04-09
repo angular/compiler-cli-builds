e22a3ebacbcff85491f742fb29adfc7b698fa32e
Andrew Scott <atscott@google.com>
2025-04-09 18:14:12 +0000

    d99935a docs: update guide on referencing component children (#60787)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.5+sha-880ebf4");
+var VERSION = new Version("19.2.5+sha-d99935a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
