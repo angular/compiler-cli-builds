3bedd8431ee19c1bcf3bae28f9e6fa9f76d59316
kirjs <kirjs@users.noreply.github.com>
2025-04-10 17:37:35 +0000

    b144126 fix(core): inject migration: replace param with this. (#60713)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.6+sha-77c6041");
+var VERSION = new Version("19.2.6+sha-b144126");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
