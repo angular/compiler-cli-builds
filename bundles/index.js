688ccd3c619ee9bf7961a27ce23e052f1813f290
kirjs <kirjs@users.noreply.github.com>
2025-04-11 21:25:19 +0000

    4fec563 docs: Restructure example build rules (#60778)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.6+sha-b144126");
+var VERSION = new Version("19.2.6+sha-4fec563");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
