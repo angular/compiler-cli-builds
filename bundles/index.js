b57bdbe6dda816d89718d28f26813d28652620a6
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:28:09 +0000

    b210d3c refactor(devtools): switch to components tab whenever the inspector is used (#60694)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.6+sha-4fec563");
+var VERSION = new Version("19.2.6+sha-b210d3c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
