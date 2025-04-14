6647b6fd3017e93318befc74aadab64f6f5fabd9
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:44:45 +0000

    84d359c refactor(compiler): add support for the new search element (#54945)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.6+sha-c5ca597");
+var VERSION = new Version("20.0.0-next.6+sha-84d359c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
