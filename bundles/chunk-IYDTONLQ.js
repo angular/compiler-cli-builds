d791218e4f4e85fc1ae3f42d92f53a57109f685e
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:06:16 +0000

    c80f24d docs: fix missing card-title tag in example of fallback-content section (#60855)

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.6+sha-c0e4ff1";
+var PLACEHOLDER_VERSION = "19.2.6+sha-c80f24d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.6+sha-c0e4ff1");
+var VERSION = new Version("19.2.6+sha-c80f24d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
