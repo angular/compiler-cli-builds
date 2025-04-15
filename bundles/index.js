10a948e05c82305dfe4302def82a45d7170eaaf1
kirjs <kirjs@users.noreply.github.com>
2025-04-15 21:33:20 +0000

    070c09f fix(docs-infra): prevent processing node_modules .md files (#60872)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.6+sha-9171401");
+var VERSION = new Version("19.2.6+sha-070c09f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
