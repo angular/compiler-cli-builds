0ba0e6e6f078f764b13e7b2f52b109e9d2fed5cb
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:07:56 +0000

    cbbea70 fix(common): issue a warning instead of an error when `NgOptimizedImage` exceeds the preload limit (#60879)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.6+sha-f22c72b");
+var VERSION = new Version("20.0.0-next.6+sha-cbbea70");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
