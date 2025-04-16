c78d614ac028ee72307c6e2933a4aa8e9b105361
Kirill Cherkashin <kirill@kcherkashin.org>
2025-04-16 20:47:08 +0000

    2c1f348 release: cut the v19.2.7 release

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.6+sha-c90eea2";
+var PLACEHOLDER_VERSION = "19.2.7+sha-2c1f348";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
