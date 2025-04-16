eac3ff28cbf57c56601d8d8bf95b0c3c34f9594e
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:56:12 +0000

    bf8492b feat(core): stabilize withI18nSupport() api (#60889)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.6+sha-cbbea70");
+var VERSION = new Version("20.0.0-next.6+sha-bf8492b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
