6c3f8b08c4436d5a77fc498132a0b4f50f08afe7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-17 09:30:57 +0000

    763e3ca release: bump Angular DevTools version to 1.0.28 (#60893)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.7+sha-2c1f348");
+var VERSION = new Version("19.2.7+sha-763e3ca");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
