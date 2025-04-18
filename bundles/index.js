560a074fdae3819c8fd1d7a28c1fd3bb2ce9d11f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 19:19:38 +0000

    037dede refactor(devtools): improve components tab side pane UI (#60901)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.7+sha-3a6121f");
+var VERSION = new Version("20.0.0-next.7+sha-037dede");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
