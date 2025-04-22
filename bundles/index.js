fb5a5a277b033f3d4406c49c9bf8426155064e33
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 11:10:53 +0000

    be99562 fix(forms): make NgForm emit FormSubmittedEvent and FormResetEvent (#60887)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.7+sha-e1b0e94");
+var VERSION = new Version("20.0.0-next.7+sha-be99562");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
