4751e15d0113c166effbd96af9d781c530e9f264
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 19:34:54 +0000

    cebb9d2 refactor(devtools): disable unsupported features (#60585)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.7+sha-6c2fbad");
+var VERSION = new Version("20.0.0-next.7+sha-cebb9d2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
