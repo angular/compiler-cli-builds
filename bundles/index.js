bedb553768cee523931d6fe42f464d3029e9ee05
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:15:17 +0000

    5555e9f docs: update docs centered on NgModules (#60933)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.7+sha-763e3ca");
+var VERSION = new Version("19.2.7+sha-5555e9f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
