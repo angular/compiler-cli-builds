02ebd9c28e72a25041ff7f886afa9fc45438af6f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 14:34:11 +0000

    adac95c refactor(core): mark `makeEnvironmentProviders` as public. (#60956)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.7+sha-ea4a211");
+var VERSION = new Version("19.2.7+sha-adac95c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
