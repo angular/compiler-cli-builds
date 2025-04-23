606957a1fdffd9c43eb03804fe0deab31e7a2350
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 13:25:25 +0000

    a93b6e8 refactor(router): remove `Compiler` injectee from `RouterPreloader` (#60945)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.7+sha-62de7d9");
+var VERSION = new Version("20.0.0-next.7+sha-a93b6e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
