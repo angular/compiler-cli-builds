0f7df93011fafd70fddb8bbab9dc7f4b7aeea300
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 15:24:34 +0000

    fd8f5c6 release: cut the v20.0.0-next.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.7+sha-a93b6e8");
+var VERSION = new Version("20.0.0-next.8+sha-fd8f5c6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
