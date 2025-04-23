12ed2cc4da127cada0cce80aa8f05f8c0c7bb2b2
Miles Malerba <mmalerba@google.com>
2025-04-23 21:41:52 +0000

    5b0ec4a release: bump Angular DevTools version to 1.0.30 (#60992)

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.8+sha-5696451";
+var PLACEHOLDER_VERSION = "19.2.8+sha-5b0ec4a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
