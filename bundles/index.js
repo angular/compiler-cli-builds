542be17abda3006af7274f32e97904eaa707a13e
Miles Malerba <mmalerba@google.com>
2025-04-24 16:36:35 +0000

    06ada46 docs(docs-infra): fix github links for for previews (#60994)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-c6b41e1");
+var VERSION = new Version("20.0.0-next.8+sha-06ada46");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
