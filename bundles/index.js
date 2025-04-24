44a213e3a91f98abb83f26c275b2efcd88877cbc
Miles Malerba <mmalerba@google.com>
2025-04-24 16:50:14 +0000

    e6c14f9 docs: remove old pipes docs. (#60996)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-06ada46");
+var VERSION = new Version("20.0.0-next.8+sha-e6c14f9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
