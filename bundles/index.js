453ad7d8c9fe6433060199bfb9e996a16b78dff2
Miles Malerba <mmalerba@google.com>
2025-04-24 22:19:20 +0000

    ccc5cc0 feat(http): add keepalive support for fetch requests (#60621)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-e711f99");
+var VERSION = new Version("20.0.0-next.8+sha-ccc5cc0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
