3e6a54f1373bf7f66892466d8b481f5d93d63c1e
Miles Malerba <mmalerba@google.com>
2025-04-28 15:24:49 +0000

    54465e8 fix(docs-infra): use proper API for style overrides (#61008)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.8+sha-aca1f45");
+var VERSION = new Version("19.2.8+sha-54465e8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
