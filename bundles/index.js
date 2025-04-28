afe8bd862db16aceb76154122aa0eadfee444308
Miles Malerba <mmalerba@google.com>
2025-04-28 17:07:37 +0000

    578041b docs(docs-infra): fix function api ref header width (#61022)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.8+sha-0203e0b");
+var VERSION = new Version("19.2.8+sha-578041b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
