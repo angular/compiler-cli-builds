d4f40e5f32af8aef3747557f013399d9a50e0fbf
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-30 10:26:42 +0000

    867f389 Revert "build: Run browsers tests without `platform-browser-dynamic` (#60937)" (#61059)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-8d82a39");
+var VERSION = new Version("20.0.0-next.8+sha-867f389");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
