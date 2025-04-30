f1d0566dc220d84c42e6f327cde183713a975b7d
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:08:04 +0000

    400dbc5 fix(core): properly handle app stabilization with defer blocks (#61056)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.9+sha-a2eaea0";
+export declare const PLACEHOLDER_VERSION = "19.2.9+sha-400dbc5";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
