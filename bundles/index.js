21561eac90bf45a6a0bd4ac0de8980d233fe0ccd
Andrew Kushnir <akushnir@google.com>
2025-04-30 22:18:13 +0000

    33a336f build: update cross-repo angular dependencies (#61069)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-c0ae032");
+var VERSION = new Version("20.1.0-next.0+sha-33a336f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
