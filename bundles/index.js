0b3cb0eb564491133f439588a9487b8c3e835053
Miles Malerba <mmalerba@google.com>
2025-04-30 17:32:44 +0000

    a2eaea0 release: cut the v19.2.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.8+sha-d67ced6");
+var VERSION = new Version("19.2.9+sha-a2eaea0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
