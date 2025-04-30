73415b61a4cef6508d773a76db71521b8a56e1f0
Miles Malerba <mmalerba@google.com>
2025-04-30 15:59:20 +0000

    dbb8702 fix(core): call DestroyRef on destroy callback if view is destroyed [patch] (#61061)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.8+sha-a554208");
+var VERSION = new Version("19.2.8+sha-dbb8702");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
