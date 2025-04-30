c6ad0a6c2534b51de4bb772d651f624535a6a0e0
Miles Malerba <mmalerba@google.com>
2025-04-30 17:46:30 +0000

    c1e46c5 docs: release notes for the v20.0.0-next.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-829379e");
+var VERSION = new Version("20.1.0-next.0+sha-c1e46c5");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
