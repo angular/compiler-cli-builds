9517fc4f37b31224db4e3fe58f41164fe350e084
Miles Malerba <mmalerba@google.com>
2025-04-30 05:13:37 +0000

    e6a3427 feat(devtools): defer blocks  support (#60629)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.8+sha-3261d14");
+var VERSION = new Version("20.0.0-next.8+sha-e6a3427");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
