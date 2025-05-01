992d116fc44466b8d5ff8ae2d98b46682f4de90d
Andrew Kushnir <akushnir@google.com>
2025-05-01 15:52:34 +0000

    d2cb0b9 refactor(core): tree-shake `getDirectiveDef` error (#61075)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-33a336f");
+var VERSION = new Version("20.1.0-next.0+sha-d2cb0b9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
