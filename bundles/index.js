f242672409a3436a3eb584bef3c67fef5192b01b
Andrew Kushnir <akushnir@google.com>
2025-05-02 15:04:01 +0000

    782c6e1 docs: ViewContainerRef.createComponent jsdoc update (#61097)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.9+sha-bbdb166");
+var VERSION = new Version("20.0.0-next.9+sha-782c6e1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
