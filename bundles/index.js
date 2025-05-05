841cc8a8f419e9269757442dd270fd07d1b73aca
Andrew Kushnir <akushnir@google.com>
2025-05-05 15:54:03 +0000

    d8532bc docs(router): update typing for skipLocationChange in RedirectCommand example (#61119)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-1e79d47");
+var VERSION = new Version("20.1.0-next.0+sha-d8532bc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
