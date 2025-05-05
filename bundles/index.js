bb6d84265b676225f3768177c379f8afeda81fae
Andrew Kushnir <akushnir@google.com>
2025-05-05 16:30:49 +0000

    ba5a3c0 build: update rules_angular digest to 3ba9d67 (#61124)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.9+sha-cc48424");
+var VERSION = new Version("19.2.9+sha-ba5a3c0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
