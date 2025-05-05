28bc50fec6a445b15d8a3efb85abdf252afffd9d
Andrew Kushnir <akushnir@google.com>
2025-05-05 16:03:12 +0000

    e7f5aa2 refactor(core): Remove use of private export PendingTasksInternal where possible (#61049)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-d8532bc");
+var VERSION = new Version("20.1.0-next.0+sha-e7f5aa2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
