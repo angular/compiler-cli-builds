58025051f9453dff205e367281c8168b64b6e019
Andrew Kushnir <akushnir@google.com>
2025-05-06 21:17:57 +0000

    4b365c7 docs: fix non-working link  (#61131)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.9+sha-6cd5d8e");
+var VERSION = new Version("20.0.0-next.9+sha-4b365c7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
