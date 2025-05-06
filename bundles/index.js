34a86f8ff27816cb9f40d570c55203fc18cab78d
Andrew Kushnir <akushnir@google.com>
2025-05-06 17:32:21 +0000

    c3ff01b docs: fix whitespace after a link (#61136)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-583b9a7");
+var VERSION = new Version("20.1.0-next.0+sha-c3ff01b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
