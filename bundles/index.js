5aae4f7b0d8d5ff5290adf2d6f1ae32d24a02e5e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-05-07 17:58:15 +0000

    ea87eb4 release: cut the v20.0.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.9+sha-803c94c");
+var VERSION = new Version("20.0.0-rc.0+sha-ea87eb4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
