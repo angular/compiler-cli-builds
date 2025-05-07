c9f82304ffa22eca1ff1ec6866acd4f04e31dbd6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-07 08:50:11 +0000

    803c94c ci: enable Renovate on `main` and `20.0.x` branches (#61160)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-next.9+sha-4b365c7");
+var VERSION = new Version("20.0.0-next.9+sha-803c94c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
