233631443a253a64e376a614da14c66dfe17905d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:47:32 +0000

    2f7e4eb ci: remove redundant dash in workflow file (#61206)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-1dece6c");
+var VERSION = new Version("20.0.0-rc.0+sha-2f7e4eb");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
