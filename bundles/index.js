a9d3e197ffbc580fbd943e347acb1da448bcd542
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:03:48 +0000

    23ee85d docs:  update the description of example (#61205) PR Close #61205

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-1aeb71c");
+var VERSION = new Version("20.0.0-rc.0+sha-23ee85d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
