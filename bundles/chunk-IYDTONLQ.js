967e1b89b551028d8c380e1e3fa9e4e1baf993a9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:43:58 +0000

    3b9440b ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.10+sha-6255954";
+var PLACEHOLDER_VERSION = "19.2.10+sha-3b9440b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.10+sha-6255954";
+export declare const PLACEHOLDER_VERSION = "19.2.10+sha-3b9440b";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
