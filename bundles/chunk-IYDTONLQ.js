daeabb2146c2ecc8c71ed60c5c24eef991b8466a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:03:38 +0000

    5b60442 docs:  update the description of example (#61205) PR Close #61205

--- bundles/chunk-IYDTONLQ.js
+++ bundles/chunk-IYDTONLQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.10+sha-684af21";
+var PLACEHOLDER_VERSION = "19.2.10+sha-5b60442";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-684af21");
+var VERSION = new Version("19.2.10+sha-5b60442");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
