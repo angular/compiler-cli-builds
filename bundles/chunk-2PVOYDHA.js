dc3984cc4d50cf0cb7be5d76ab94611ffc2fa970
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:47:41 +0000

    6f4307f ci: remove redundant dash in workflow file (#61206)

--- bundles/chunk-2PVOYDHA.js
+++ bundles/chunk-2PVOYDHA.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.0-next.0+sha-50993be";
+var PLACEHOLDER_VERSION = "20.1.0-next.0+sha-6f4307f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
