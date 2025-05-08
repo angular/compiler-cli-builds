a02ca64ceede6106b85e0bc4bd0b84ebe415c302
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:35:56 +0000

    1dece6c ci: add workflow to sync non-default branches (#61201)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-9f43232");
+var VERSION = new Version("20.0.0-rc.0+sha-1dece6c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
