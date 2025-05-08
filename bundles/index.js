dcde23a359b945838d7eb9a516b97c807c2c10d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:33:48 +0000

    655954a ci: fix step name (#61213)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-3b9440b");
+var VERSION = new Version("19.2.10+sha-655954a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
