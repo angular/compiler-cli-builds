fc9dfb87dd801f26483265a6eb6d67e83e74ae2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 19:03:30 +0000

    5f46be0 ci: update workflow to clone repo with history (#61223)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-7820f4e");
+var VERSION = new Version("19.2.10+sha-5f46be0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
