01d52f54f9d1243860c9727b41e955c9f88bca1e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:43:05 +0000

    bad2870 ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-2f7e4eb");
+var VERSION = new Version("20.0.0-rc.0+sha-bad2870");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
