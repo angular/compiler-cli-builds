f8b43efea72c114eec70dd115bd696c3b5cf1b3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-09 17:40:12 +0000

    210f981 docs: change supported versions when v20 releases (#61238)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-5b77cf5");
+var VERSION = new Version("20.0.0-rc.0+sha-210f981");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
