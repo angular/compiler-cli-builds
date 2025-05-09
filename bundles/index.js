940b6846cc2a7509d173f4ddbd3f807eef13d345
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:38:35 +0000

    84b66f3 build: migrate adev shared-docs package to use ts_project (#61217)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-6cd2df0");
+var VERSION = new Version("19.2.10+sha-84b66f3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
