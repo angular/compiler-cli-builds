cd6449065eeb721d2fc0800a6c688e59337784fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 22:33:24 +0000

    dd25f3d refactor(core): remove `USE_RUNTIME_DEPS_TRACKER_FOR_JIT` flag. (#61265)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-b4c383b");
+var VERSION = new Version("20.1.0-next.0+sha-dd25f3d");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
