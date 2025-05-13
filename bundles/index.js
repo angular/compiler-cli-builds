e90c2581be454a6e5f0e52d1d147037e91249968
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-13 23:41:57 +0000

    41db650 docs: update references to the cdk/testing apis (#60853)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-1b4b44e");
+var VERSION = new Version("20.0.0-rc.0+sha-41db650");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
