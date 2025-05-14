41a77d06ff055f6191d082e04908ed91c3e70961
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 13:42:11 +0000

    ac77e30 docs: document the use of `this` in template expression syntax (#61250)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-7440a16");
+var VERSION = new Version("19.2.10+sha-ac77e30");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
