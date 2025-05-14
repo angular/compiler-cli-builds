c85aa3f3c3a853a32feeece665598329b27fb3c4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-14 10:51:38 +0000

    7440a16 build: bump domino to `93e720f143d0296dd2726ffbcf4fc12283363a7b` (#61302)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-5ea32b5");
+var VERSION = new Version("19.2.10+sha-7440a16");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
