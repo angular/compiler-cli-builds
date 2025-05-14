7b990d4205d45e017ca1ab96520e0775f5f352ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:41:13 +0000

    592a754 build: migrate upgrade to use ng_project instead of ng_module (#61320)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.0+sha-da59776");
+var VERSION = new Version("20.0.0-rc.0+sha-592a754");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
