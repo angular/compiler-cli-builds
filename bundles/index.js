b642b12f70e29a4a0744b92a92e69048ad69a97f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:19:59 +0000

    82e5449 refactor: replace port `4206` usage with `4209` (#61362)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-1191e62");
+var VERSION = new Version("19.2.11+sha-82e5449");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
