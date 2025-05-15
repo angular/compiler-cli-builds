eaf303d0e3abed626b65d2b689b029243f73477e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-15 12:56:34 +0000

    f2d6e59 refactor: explicitly ensure `ngDevMode` types are available (#61365)

--- bundles/chunk-GJGMYRKJ.js
+++ bundles/chunk-GJGMYRKJ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.10+sha-dfd068d";
+var PLACEHOLDER_VERSION = "19.2.10+sha-f2d6e59";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
