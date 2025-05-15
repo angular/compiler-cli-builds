a71a93d9ee7485a4bc29d16e6940f48504980ca6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 15:32:30 +0000

    ae503b7 release: cut the v19.2.11 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.10+sha-f2d6e59");
+var VERSION = new Version("19.2.11+sha-ae503b7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
