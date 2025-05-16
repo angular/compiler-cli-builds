2b81352283687576391dbe3d6297fad377d8d043
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:50:28 +0000

    11589d4 ci: streamline adev CDK APIs and CLI help workflows (#61396)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-94f5a4b");
+var VERSION = new Version("19.2.11+sha-11589d4");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
