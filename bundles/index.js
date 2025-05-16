cc27dc1a3fb9582188ee2cc1c3c4948b0944791b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:44:00 +0000

    378606e docs: fix incomplete sentence (#61391)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-c0fb34b");
+var VERSION = new Version("20.0.0-rc.1+sha-378606e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
