0df890a75e80f396961491046175a34fd8a162d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:39:00 +0000

    337c7b8 ci: update step name in workflow (#61393)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-648d42f");
+var VERSION = new Version("20.0.0-rc.1+sha-337c7b8");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
