d1484b254af67dc4295c3b0a57081be5c19a2891
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:20:52 +0000

    a0be4c1 docs: fix Routed components example (#61278)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-fb65ad1");
+var VERSION = new Version("20.1.0-next.0+sha-a0be4c1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
