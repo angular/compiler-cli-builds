30da2eb6dd052b8db5e84317ff8ef066146a4ac1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:01:00 +0000

    94f5a4b fix(core): Testing should not throw when Zone does not patch test FW APIs (#61376)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-bd2a5b9");
+var VERSION = new Version("19.2.11+sha-94f5a4b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
