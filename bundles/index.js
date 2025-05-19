b4b4e5a2df9a7e4472221d7f6d3edd3b11180d57
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 13:34:45 +0000

    c5177a6 refactor(core): re-introduce TestBed.flushEffects()" (#61462)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-76100ec");
+var VERSION = new Version("20.0.0-rc.1+sha-c5177a6");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
