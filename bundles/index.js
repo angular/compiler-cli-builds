bf6931809cdb3ae25f947f663364dfb9b8473fc0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:42:36 +0000

    76100ec build: `--config=debug` should not disable sandbox for all actions (#61468)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-a30fc96");
+var VERSION = new Version("20.0.0-rc.1+sha-76100ec");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
