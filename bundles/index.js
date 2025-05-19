b8ff2c5654b2cb652f6ec19fbe989f3b53a41e04
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 11:27:55 +0000

    1582610 ci: replace `yarn ng-dev misc update-generated-files` with separate update commands for specific targets (#61467)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-5d35288");
+var VERSION = new Version("20.0.0-rc.1+sha-1582610");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
