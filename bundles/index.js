6b1525fbf5e89047ca4dc5c25a9e5f4c4b0c1fcc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 20:12:49 +0000

    9ce79be docs: update v17 as EOL for current documentation (#61532)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-bb31f6f");
+var VERSION = new Version("19.2.11+sha-9ce79be");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
