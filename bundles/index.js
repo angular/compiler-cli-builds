01a9606b9e8e3f4832365abc4d08d2563fa8e034
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 11:34:38 +0000

    2e7032e build: replace `platform-browser-dynamic` with  `platform-browser` (#61497)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-c3e2690");
+var VERSION = new Version("20.1.0-next.0+sha-2e7032e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
