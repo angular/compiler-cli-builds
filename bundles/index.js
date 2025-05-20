c8a96a36cb4d55755bdeebf861bc3b1cf1abd8aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 20:14:38 +0000

    3b564e7 docs: update Angular CLI help (#61527)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-7a44727");
+var VERSION = new Version("20.0.0-rc.1+sha-3b564e7");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
