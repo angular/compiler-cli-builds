2b002a618cf9dedc5260c061401bff3c5b32159e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:14:08 +0000

    2796f18 test: set the `noCheck` TS compiler option in declaration-only emission compliance tests (#61470)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-fb0f5aa");
+var VERSION = new Version("20.1.0-next.0+sha-2796f18");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
