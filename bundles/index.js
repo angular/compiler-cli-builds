f224fde7f846102d1de0c8e1de7378e458fe5f56
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 13:56:50 +0000

    6218b38 build: update cross-repo angular dependencies (#61509)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.1+sha-16d7a7b");
+var VERSION = new Version("20.0.0-rc.1+sha-6218b38");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
