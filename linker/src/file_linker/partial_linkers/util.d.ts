ccdfe852f87beb350d915ed1ee5bebc69a079756
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 14:22:01 +0000

    8f65223 fix(core): update min Node.js support to 20.19, 22.12, and 24.0 (#61499)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.1.0-next.0+sha-6ba9adc";
+export declare const PLACEHOLDER_VERSION = "20.1.0-next.0+sha-8f65223";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
