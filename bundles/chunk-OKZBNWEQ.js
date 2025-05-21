a39496d0550d732d800e20ec27ef1f4a7773aba3
Andrew Scott <atscott@google.com>
2025-05-21 19:12:49 +0000

    6e79eaf fix(core): reading resource value after reload in the error state (#61441)

--- bundles/chunk-OKZBNWEQ.js
+++ bundles/chunk-OKZBNWEQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-rc.1+sha-0503362";
+var PLACEHOLDER_VERSION = "20.0.0-rc.1+sha-6e79eaf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
