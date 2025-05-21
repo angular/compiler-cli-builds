5138356605964741ac2d0f81359cf8668d78d447
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 16:11:43 +0000

    fc2483e refactor(compiler): avoid duplication between `FactoryTarget` type (#61571)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-21e5f07");
+var VERSION = new Version("19.2.11+sha-fc2483e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
