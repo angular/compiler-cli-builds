a75faa398bbcf440f5c4eb5e27bbe2d471a25f36
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:33:23 +0000

    6f6e91d build: add comment explaining path maps (#61584)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "19.2.11+sha-3a8bbe9";
+export declare const PLACEHOLDER_VERSION = "19.2.11+sha-6f6e91d";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
