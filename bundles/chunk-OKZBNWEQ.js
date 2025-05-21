7c9a4917d2b05f40b5dd994329946af900166367
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 11:13:37 +0000

    edc694d ci: temporarily disable saucelabs for push jobs (#61558)

--- bundles/chunk-OKZBNWEQ.js
+++ bundles/chunk-OKZBNWEQ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-rc.1+sha-4e5d6de";
+var PLACEHOLDER_VERSION = "20.0.0-rc.1+sha-edc694d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
