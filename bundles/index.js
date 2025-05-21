bbf4b8f69ba14a9edcf244f8aa64dcb6232b7e73
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:44:56 +0000

    b6d7b4b build: remove the "restamping" substitutions from adev shared-docs package (#61585)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-6f6e91d");
+var VERSION = new Version("19.2.11+sha-b6d7b4b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
