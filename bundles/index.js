8dcc3fc8d570ffc8e14bcc4fd408cd866f65a647
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 10:15:40 +0000

    ef01d3c refactor(core): Merge R3TemplateRef implementation and TemplateRef interface (#61455)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-7f6bb91");
+var VERSION = new Version("20.1.0-next.0+sha-ef01d3c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
