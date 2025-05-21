9deeb43eb005238e373d2b23f8bc6dc85d1fe6fe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:14:44 +0000

    8f9b05e fix(core): cleanup testability subscriptions (#61261)

--- bundles/chunk-GJGMYRKJ.js
+++ bundles/chunk-GJGMYRKJ.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.11+sha-0104a69";
+var PLACEHOLDER_VERSION = "19.2.11+sha-8f9b05e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
