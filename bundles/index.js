58b2ec013bb73ea75aa0cb2f2c1d56c3f5e321e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:37:07 +0000

    44bb328 fix(compiler): avoid conflicts between HMR code and local symbols (#61550)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-8f9b05e");
+var VERSION = new Version("19.2.11+sha-44bb328");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
