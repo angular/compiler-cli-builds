c04a13784cba380de0f01607aa0492ed7538ee3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:13:19 +0000

    3a8bbe9 build: migrate language service to use rules_js (#61578)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.11+sha-fc2483e");
+var VERSION = new Version("19.2.11+sha-3a8bbe9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
