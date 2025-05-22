aa267ea2f318bcd238d66a8ccc89e41aa791550a
Andrew Scott <atscott@google.com>
2025-05-22 20:46:32 +0000

    eb4abc2 refactor(zone.js): Add a `withProxyZone` helper that might be used for unpatched test frameworks (#61626)

--- bundles/chunk-FWTFIN5W.js
+++ bundles/chunk-FWTFIN5W.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-7e5abb3";
+var PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-eb4abc2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-7e5abb3";
+export declare const PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-eb4abc2";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
