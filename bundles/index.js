7bbb7ee97f548fc0f01485debc95cb69eff0c016
Andrew Scott <atscott@google.com>
2025-05-22 03:49:41 +0000

    5f1bd07 fix(zone.js): classes that extend Error should retain cause property (#61599)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.12+sha-2f5912e");
+var VERSION = new Version("19.2.12+sha-5f1bd07");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
