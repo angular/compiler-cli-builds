743926a99c3797e354210b5bc60d262d05ac2eb8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 10:34:09 +0000

    9e1cd49 fix(migrations): preserve comments when removing unused imports (#61674)

--- bundles/index.js
+++ bundles/index.js
@@ -83,7 +83,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("19.2.13+sha-61c731a");
+var VERSION = new Version("19.2.13+sha-9e1cd49");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
