294df08d83c066e876bff467353fcdd9d7bba89a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 12:30:14 +0000

    c11527f refactor(compiler): remove unused code (#61668)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-ca26b23");
+var VERSION = new Version("20.1.0-next.0+sha-c11527f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
