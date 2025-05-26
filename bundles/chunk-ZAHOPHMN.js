a02c78ec37511fc1e3ceb7e0cb3fe646361ba692
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 10:03:37 +0000

    d366a98 build: update tools directory to use rules_js for building (#61657)

--- bundles/chunk-ZAHOPHMN.js
+++ bundles/chunk-ZAHOPHMN.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-556413d";
+var PLACEHOLDER_VERSION = "20.0.0-rc.2+sha-d366a98";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
