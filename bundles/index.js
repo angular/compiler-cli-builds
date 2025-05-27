6247e3f852da73694a6b9ca42743aa1e43bb5d81
Andrew Scott <atscott01@gmail.com>
2025-05-27 23:07:49 +0000

    d19669c docs: release notes for the v20.0.0-rc.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-fe03aa0");
+var VERSION = new Version("20.1.0-next.0+sha-d19669c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
