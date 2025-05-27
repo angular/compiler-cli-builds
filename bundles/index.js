4a7b9eead358c45a201dddc8d8270cc520b74723
Andrew Scott <atscott@google.com>
2025-05-27 18:39:17 +0000

    87659ba docs(docs-infra): limit file interactions on tutorials. (#61696)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.2+sha-cc5a393");
+var VERSION = new Version("20.0.0-rc.2+sha-87659ba");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
