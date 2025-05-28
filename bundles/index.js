0264c1a25e6b58564619a7381c7646fb89c538ea
Andrew Scott <atscott@google.com>
2025-05-28 17:34:38 +0000

    2c8de90 docs: update version picker for v20 (#61727)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-55306bd");
+var VERSION = new Version("20.0.0+sha-2c8de90");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
