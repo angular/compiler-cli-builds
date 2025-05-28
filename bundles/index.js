ce7839db38cae8c0dac94e749607dfa0c123cc81
Andrew Scott <atscott@google.com>
2025-05-28 19:34:01 +0000

    a0d599e docs: link to v20 blog post in changelog (#61734)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-aed8a77");
+var VERSION = new Version("20.0.0+sha-a0d599e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
