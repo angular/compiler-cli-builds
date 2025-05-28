dfb1f1a86229f29384c4a7c7b62dd5b66aaa4f7b
Andrew Scott <atscott01@gmail.com>
2025-05-28 13:58:56 +0000

    40e2b5b release: cut the v20.0.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0-rc.3+sha-0502cf2");
+var VERSION = new Version("20.0.0+sha-40e2b5b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
