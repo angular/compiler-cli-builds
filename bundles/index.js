652dc96e179b3dac4853f489d0eb13f37d9fdd2e
Andrew Scott <atscott@google.com>
2025-05-28 16:48:15 +0000

    8cf7e23 docs: update tutorials and playground to v20 (#61726)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-62be7fa");
+var VERSION = new Version("20.1.0-next.0+sha-8cf7e23");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
