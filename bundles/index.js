7aaf6f14615b543560e8aed85250e918135663b0
kirjs <kirts@google.com>
2025-05-29 22:27:07 +0000

    6113fae docs: fix copy-paste error on defer page in interaction section (#61751) PR Close #61751

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-1802d36");
+var VERSION = new Version("20.0.0+sha-6113fae");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
