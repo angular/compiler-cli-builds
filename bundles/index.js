c2c2066764181b9546bd6cc0ca1d4dd58f8bf6c9
Andrew Scott <atscott@google.com>
2025-05-29 00:51:01 +0000

    04b6881 docs: setup redirect for zoneless (#61737)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-9b514b7");
+var VERSION = new Version("20.1.0-next.0+sha-04b6881");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
