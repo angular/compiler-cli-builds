f07aca800e42149761cbfb48656a491ee85ba5d1
kirjs <kirts@google.com>
2025-06-02 13:47:38 +0000

    744a1fa docs: point v20 banner to blog (#61824)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-a70f03a");
+var VERSION = new Version("20.1.0-next.0+sha-744a1fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
