951573923aa65102ee16c0b548b904420d394f6e
kirjs <kirts@google.com>
2025-06-02 20:12:35 +0000

    42f07ce docs: Use signal function to read value in CustomDialog example (programmatic-rendering.md) (#61821)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-fd5a049");
+var VERSION = new Version("20.1.0-next.0+sha-42f07ce");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
