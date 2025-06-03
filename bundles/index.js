dde288d6afef8809fb8f0ff6309147007d6c7b75
kirjs <kirts@google.com>
2025-06-03 20:04:23 +0000

    b93fa22 fix(service-worker): prevent duplicate fetches during concurrent update checks (#61443)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-939ca07");
+var VERSION = new Version("20.0.0+sha-b93fa22");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
