d3d826141bf1b042755f969097724e1e39e592df
kirjs <kirts@google.com>
2025-06-03 11:21:38 +0000

    0ea831c refactor(migrations): keep the control flow migration as `ng generate`. (#61773)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-7c4f74a");
+var VERSION = new Version("20.0.0+sha-0ea831c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
