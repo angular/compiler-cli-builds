b30a93d2289e5a7784249b27838c694f2dc37611
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-06-04 06:38:15 +0000

    eb62a58 docs: open llms files in new tab (#61851)

--- bundles/index.js
+++ bundles/index.js
@@ -84,7 +84,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.0+sha-2bf7249");
+var VERSION = new Version("20.0.0+sha-eb62a58");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
