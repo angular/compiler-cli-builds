fdfcdb4cafa2979e15da63efa78d065770f20525
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 13:00:34 +0000

    4f89f6e refactor(platform-browser): drop `isPlatformServer` in `SharedStylesHost` (#61685)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-e90bfc5");
+var VERSION = new Version("20.1.0-next.0+sha-4f89f6e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
