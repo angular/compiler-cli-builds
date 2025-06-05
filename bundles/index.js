b3958bffe7f50121c5ce34f3b07669bb7d2bdade
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 11:41:10 +0000

    ddfb94c ci: atomatically add target labels based on base branch (#61894)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-935ce0e");
+var VERSION = new Version("20.1.0-next.0+sha-ddfb94c");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
