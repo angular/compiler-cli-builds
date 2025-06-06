f7779fea3f30ddcf8625cdd261d6d0d72cb0ed89
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 09:04:16 +0000

    0576724 feat(devtools): add getSignalNestedProperties (#61917)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-78c417a");
+var VERSION = new Version("20.1.0-next.0+sha-0576724");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
