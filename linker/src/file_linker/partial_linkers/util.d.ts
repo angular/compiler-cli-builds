7e868f61d10e7ecc450c860283574e5743bdb91d
Andrew Kushnir <akushnir@google.com>
2025-06-06 22:27:40 +0000

    74b2aa6 test: use zone from npm instead of the locally built package for modules testing (#61948)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.2+sha-a76f64b";
+export declare const PLACEHOLDER_VERSION = "20.0.2+sha-74b2aa6";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
