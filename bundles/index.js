4492a188efa56b6c6bc50737e26a9f9655380461
Andrew Kushnir <akushnir@google.com>
2025-06-06 01:57:41 +0000

    71017f2 fix(docs-infra): fix wrong `visibleLines` numbers in `docs-code` blocks (#61909)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.1+sha-33bd215");
+var VERSION = new Version("20.0.1+sha-71017f2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
