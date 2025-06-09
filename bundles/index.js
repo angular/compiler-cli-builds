91cbc48c515ab95088ace85fae665486f5bff9eb
Andrew Kushnir <akushnir@google.com>
2025-06-09 16:51:33 +0000

    a3c6926 fix(devtools): material components background colors (#61969)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-ef10aa4");
+var VERSION = new Version("20.1.0-next.0+sha-a3c6926");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
