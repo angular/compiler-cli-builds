6e39425161af4d49abf0ca82cff61640f124f9d4
Andrew Kushnir <akushnir@google.com>
2025-06-10 23:01:17 +0000

    eef6faf ci: update to latest version of dev-infra actions (#61990)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.2+sha-9d6db41";
+var PLACEHOLDER_VERSION = "20.0.2+sha-eef6faf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
