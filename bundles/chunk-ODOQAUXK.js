5512d2f5a7267070e0a17694c87a5ce67d8fe3fb
Andrew Kushnir <akushnir@google.com>
2025-06-10 22:57:19 +0000

    6f6db99 ci: update to latest version of dev-infra actions (#61991)

--- bundles/chunk-ODOQAUXK.js
+++ bundles/chunk-ODOQAUXK.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "19.2.14+sha-9317b1c";
+var PLACEHOLDER_VERSION = "19.2.14+sha-6f6db99";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
