f61996377f232be640544ea17019c96e2dab4529
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 08:51:27 +0000

    ce7f2e3 build: update all non-major dependencies (#61953)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.0+sha-0bd7c7d");
+var VERSION = new Version("20.1.0-next.0+sha-ce7f2e3");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
