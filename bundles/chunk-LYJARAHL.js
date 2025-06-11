8203074e33a4ea70ecdb2e9784f3e154a068c74a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 09:45:48 +0000

    06979c6 docs: navigate-to-routes.md to include RouterLink in imports. (#61965)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.2+sha-e73f248";
+var PLACEHOLDER_VERSION = "20.0.2+sha-06979c6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
