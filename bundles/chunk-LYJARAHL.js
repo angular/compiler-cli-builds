6881eb365e226ad30ae511130e5aed7e5e7f16d9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 11:51:06 +0000

    b24d540 build: update dependency google-closure-compiler to v20250609 (#62009)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.3+sha-e343cdf";
+var PLACEHOLDER_VERSION = "20.0.3+sha-b24d540";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
