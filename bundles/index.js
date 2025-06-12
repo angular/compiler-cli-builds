9d5a847631569c08270418d5230ec69dbddb9275
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 11:05:07 +0000

    e343cdf fix(core): Fixes template outlet hydration (#62012)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.3+sha-43ef2ea");
+var VERSION = new Version("20.0.3+sha-e343cdf");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
