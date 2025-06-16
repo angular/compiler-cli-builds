a8d3b82678c4a66a9ac71c797c56f138c1c46709
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-16 08:34:22 +0000

    8403bc7 build: migrate to usages of @devinfra// instead of @npm//@angular/build-tooling (#62050)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.3+sha-f69ec36";
+export declare const PLACEHOLDER_VERSION = "20.0.3+sha-8403bc7";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
