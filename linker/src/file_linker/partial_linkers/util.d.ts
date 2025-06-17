106cb7950adf7040f6385a8601bcdd5c116de4aa
Joey Perrott <josephperrott@gmail.com>
2025-06-17 23:53:01 +0000

    04e0d4f build: update the ts_integrity expected for the typescript version in WORKSPACE (#62109)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.3+sha-1c73f54";
+export declare const PLACEHOLDER_VERSION = "20.0.3+sha-04e0d4f";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
