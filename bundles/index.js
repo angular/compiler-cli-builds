fb3bf8c36eca896f93b0a3968b4c8f7d308118f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:31:02 +0000

    89efd27 refactor(devtools): create devtools signals view (#61919)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.1+sha-132fd3c");
+var VERSION = new Version("20.1.0-next.1+sha-89efd27");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
