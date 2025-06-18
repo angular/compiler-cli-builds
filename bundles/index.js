5ee40128654c64a24577ea29eb9e2c2ae61dc10a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 16:10:44 +0000

    f201df9 build: migrate to using new jasmine_test (#62106)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.3+sha-b40613f");
+var VERSION = new Version("20.0.3+sha-f201df9");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
