df6cb0b493fa1bfc14578565d6018173bbdd69d2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 12:37:00 +0000

    144c429 fix(upgrade): Make zoneless work with hybrid apps (#61660)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.4+sha-8752014");
+var VERSION = new Version("20.0.4+sha-144c429");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
