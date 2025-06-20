df1adf1d87acafe956349ffcd3848df775974613
Andrew Kushnir <akushnir@google.com>
2025-06-20 20:16:07 +0000

    f11b3f9 docs: update Angular CLI help (#62166)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.1.0-next.2+sha-f8a90c9";
+export declare const PLACEHOLDER_VERSION = "20.1.0-next.2+sha-f11b3f9";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
