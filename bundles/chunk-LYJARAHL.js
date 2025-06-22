f6616a88dcd9d1cd146af662e0c518f3dffe6c44
Andrew Kushnir <akushnir@google.com>
2025-06-22 16:25:19 +0000

    f8a6537 Revert "docs: embeded best practices in page (#62173)" (#62193)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.4+sha-35ce544";
+var PLACEHOLDER_VERSION = "20.0.4+sha-f8a6537";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
