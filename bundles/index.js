13411aab535e3f0c8fa0e121b6333514b652c823
Andrew Kushnir <akushnir@google.com>
2025-06-22 16:24:54 +0000

    4c14503 Revert "docs: embeded best practices in page (#62173)" (#62193)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.2+sha-152ff0c");
+var VERSION = new Version("20.1.0-next.2+sha-4c14503");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
