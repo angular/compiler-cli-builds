5d51a698eef179ca09ec1b5d7875edf2357d1e81
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 15:14:49 +0000

    9d20d6f Revert "test(devtools): revive cypress tests (#61972)" (#62243)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.4+sha-a42169a");
+var VERSION = new Version("20.0.4+sha-9d20d6f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
