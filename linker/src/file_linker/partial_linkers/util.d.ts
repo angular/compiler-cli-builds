7246b753a1bcd60025ddca117ca210ff832dcf78
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 12:35:45 +0000

    65c59dd fix(router): handle scrollRestoration error in restricted environments (#62186)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.0.4+sha-9b01773";
+export declare const PLACEHOLDER_VERSION = "20.0.4+sha-65c59dd";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
