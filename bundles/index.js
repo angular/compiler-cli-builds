2348672bad0af0a66e06073eea2605636eece69a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:58:18 +0000

    b489f9f refactor(language-service): add methods for decoding classifications to public api (#62251)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.2+sha-73269cf");
+var VERSION = new Version("20.1.0-next.2+sha-b489f9f");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
