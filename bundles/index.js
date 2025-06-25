bf0cd8a8a8bede81a6741570cd38393ae4c076ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 11:34:01 +0000

    c43f632 docs: fix deprecation message on ngIf/ngSwitch (#62149)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.4+sha-c63a256");
+var VERSION = new Version("20.0.4+sha-c43f632");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
