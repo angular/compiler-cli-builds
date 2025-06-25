15e08d7983ce239999306cd56bb63a39402c7376
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:25:37 +0000

    c63a256 docs: add llms context files entries (#62224)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.4+sha-767e1dd";
+var PLACEHOLDER_VERSION = "20.0.4+sha-c63a256";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
