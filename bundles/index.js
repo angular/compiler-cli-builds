96332e976e352490bd4aa05b7da17982d12a6fd8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 14:45:34 +0000

    402eaa1 docs: fix deferrable views doc links (#62293)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-13875d0");
+var VERSION = new Version("20.1.0-next.3+sha-402eaa1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
