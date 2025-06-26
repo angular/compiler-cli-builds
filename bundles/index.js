a361c1fe3cbd06d70c554c7e5cc141e124a9185a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 08:31:15 +0000

    c5c9b0b build: update rules_browsers digest to 0952071 (#62279)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-85fe323");
+var VERSION = new Version("20.1.0-next.3+sha-c5c9b0b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
