00567fb89ef5d9a2fa111072ccf6b23994f87ccb
Joey Perrott <josephperrott@gmail.com>
2025-06-27 22:34:57 +0000

    8d58150 build: migrate modules to use `@rules_browsers` protractor rules (#62345)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.5+sha-0866077");
+var VERSION = new Version("20.0.5+sha-8d58150");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
