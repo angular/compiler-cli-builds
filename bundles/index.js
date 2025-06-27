a85335ed658f11ffbb20fd92cfe061fd4e7321b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:26:40 +0000

    f10e1bc fix(devtools): profiler records only the first frames from the stream (#62335)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-f943505");
+var VERSION = new Version("20.1.0-next.3+sha-f10e1bc");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
