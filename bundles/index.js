df0001648958113e24f33c37fcbef0c6f7b643ec
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:07:01 +0000

    5ccb81e build: update dependency marked to v16 (#62320)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-df1a61f");
+var VERSION = new Version("20.1.0-next.3+sha-5ccb81e");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
