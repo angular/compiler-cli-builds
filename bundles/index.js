f387a296ea19a76203729773e11d634ba01d63dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 12:02:10 +0000

    c0532f0 docs: add page NG8115 (uninvokedTrackFunction) (#61962)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.5+sha-82cdd5c");
+var VERSION = new Version("20.0.5+sha-c0532f0");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
