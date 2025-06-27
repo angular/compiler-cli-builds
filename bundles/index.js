e04ea4cd18cd5fb3535926204b4a93cd7a08c7e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:37:14 +0000

    b2afa11 docs: Update documentation http making request (#62336)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-f10e1bc");
+var VERSION = new Version("20.1.0-next.3+sha-b2afa11");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
