8f2f55d8daf2aaa1b952286922fedc78f430544e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:12:52 +0000

    e1856da docs: update router read-route-state.md file (#62367)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-08e2d9d");
+var VERSION = new Version("20.1.0-next.3+sha-e1856da");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
