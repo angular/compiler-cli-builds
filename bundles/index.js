8293523fc9f645685e8ea47d4841ef5608f70c4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:23:27 +0000

    85d6474 refactor(core): update NG0303 error message (#62363)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.5+sha-e145a83");
+var VERSION = new Version("20.0.5+sha-85d6474");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
