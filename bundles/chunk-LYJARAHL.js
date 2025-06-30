35a895698613e43263138b347e323566ec0e981d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 09:32:58 +0000

    7c82f26 build: lock file maintenance (#62369)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.5+sha-4bc8187";
+var PLACEHOLDER_VERSION = "20.0.5+sha-7c82f26";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.5+sha-4bc8187");
+var VERSION = new Version("20.0.5+sha-7c82f26");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
