7a16b0ec822947033a0a74acdf9c1c488230e0ea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 08:19:24 +0000

    b952982 build: update github/codeql-action action to v3.29.2 (#62392)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-e7ff105");
+var VERSION = new Version("20.1.0-next.3+sha-b952982");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
