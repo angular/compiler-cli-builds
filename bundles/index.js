dbd8da6b7ec21fee2cf9bed2cfd09aaf2b688ddf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 09:41:30 +0000

    1ea65b1 docs: update breaking change for nullish coalesching + parenthesis (#62382)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-next.3+sha-b25fb8a");
+var VERSION = new Version("20.1.0-next.3+sha-1ea65b1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
