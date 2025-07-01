ba74e990489a04e335596dca267cdb512dac3cf2
Andrew Kushnir <akushnir@google.com>
2025-07-01 19:28:07 +0000

    fb1bc99 ci: fix incorrect base branch in renovate config (#62406)

--- bundles/chunk-FPHHL4UV.js
+++ bundles/chunk-FPHHL4UV.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0-next.0+sha-4ac0147";
+var PLACEHOLDER_VERSION = "20.2.0-next.0+sha-fb1bc99";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
