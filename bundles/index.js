b7de11e55219f78d009780fa9003daf1d8ef0c8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 16:18:23 +0000

    d1d131a build: migrate devtools to use packaged version of @angular/* packages (#62413)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-rc.0+sha-42a3dbb");
+var VERSION = new Version("20.1.0-rc.0+sha-d1d131a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
