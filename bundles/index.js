69b5b0e9bfa664a12cb350319a974fe38bc12e1a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 14:57:07 +0000

    0cdb02a docs: Update documentation httpResource with credentials option (#62415)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-8fbe558");
+var VERSION = new Version("20.2.0-next.0+sha-0cdb02a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
