c5571f11c621c10629b7fdac25c52792855f4e26
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 07:56:51 +0000

    e0e78fe Revert "fix(service-worker): update service worker to handle seeking better for videos (#60029)" (#62422)

--- bundles/chunk-LYJARAHL.js
+++ bundles/chunk-LYJARAHL.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.0.6+sha-5766ba6";
+var PLACEHOLDER_VERSION = "20.0.6+sha-e0e78fe";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.6+sha-5766ba6");
+var VERSION = new Version("20.0.6+sha-e0e78fe");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
