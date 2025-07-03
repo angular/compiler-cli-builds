8cc115fb850edac6ef80645e825af87f3b280cb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:20:21 +0000

    99069c2 refactor(docs-infra): removed unused imports (#62441)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-e39bf3c");
+var VERSION = new Version("20.2.0-next.0+sha-99069c2");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
