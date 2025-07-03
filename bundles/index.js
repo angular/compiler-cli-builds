75c89033d52edafba51f8790c7aa10c2c87dba10
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 08:41:51 +0000

    08e6e7b docs: display deprecation message for top most symbol (#62427)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-99069c2");
+var VERSION = new Version("20.2.0-next.0+sha-08e6e7b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
