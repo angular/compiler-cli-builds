58c04128b9a9a4f8c1f933d38d368d1418568199
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:44:54 +0000

    fca058a refactor(migrations): ensure tsurge can properly emit references in g3 (#62447)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0-rc.0+sha-f5f80d8");
+var VERSION = new Version("20.1.0-rc.0+sha-fca058a");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
