792c3ed61c0f40ede1fcddd1c7a9344b7d40f84e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 08:55:31 +0000

    9ba5903 refactor(devtools): remove the standalone demo (#62455)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-720717e");
+var VERSION = new Version("20.2.0-next.0+sha-9ba5903");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
