c5f5756c88d508f18b932089042bb09d7ccdf6e4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:00:28 +0000

    fec1206 docs: update comment (#62482) PR Close #62482

--- bundles/chunk-FPHHL4UV.js
+++ bundles/chunk-FPHHL4UV.js
@@ -311,7 +311,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.0-rc.0+sha-98ecf80";
+var PLACEHOLDER_VERSION = "20.1.0-rc.0+sha-fec1206";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
