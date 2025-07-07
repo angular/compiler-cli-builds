2b87d66469db7d4358cdcb838f81ea4374023de7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 16:23:06 +0000

    633d01b refactor(compiler): ensure compiler passes closure conformance checks (#62500)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-c6a720b");
+var VERSION = new Version("20.2.0-next.0+sha-633d01b");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
