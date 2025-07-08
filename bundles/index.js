d77e936fc0d074909d334e1978bc68183beb8f48
Andrew Kushnir <akushnir@google.com>
2025-07-08 20:35:46 +0000

    365ca13 test: rename legacy animations integration tests (#62527)

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-6953166");
+var VERSION = new Version("20.2.0-next.0+sha-365ca13");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
