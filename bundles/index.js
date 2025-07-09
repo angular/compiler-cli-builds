3987c8d7532f408c8283cf7338f80c8959eab707
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-07-09 19:06:36 +0000

    b2076fa release: cut the v20.0.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-KPQ72R34.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.0.6+sha-ce958e0");
+var VERSION = new Version("20.0.7+sha-b2076fa");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.js
 var NgTscPlugin = class {
