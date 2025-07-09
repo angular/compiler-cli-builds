d29a87cb8b0ecfeddfa60656887e78b8f027d8d3
Andrew Kushnir <akushnir@google.com>
2025-07-09 21:46:26 +0000

    9c45c32 fix(router): ensure preloaded components are properly activated (#62502)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.0+sha-9d5e248");
+var VERSION = new Version("20.2.0-next.0+sha-9c45c32");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
