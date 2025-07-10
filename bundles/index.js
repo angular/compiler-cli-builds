3260dac40775160f968c8ee19fdad8cc3f8a1a35
Andrew Kushnir <akushnir@google.com>
2025-07-10 00:00:24 +0000

    b4ae55a build: use rollup directory instead of rollup through @bazel/rollup (#62556)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0+sha-6274ea7");
+var VERSION = new Version("20.1.0+sha-b4ae55a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
