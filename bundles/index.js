1522c023fc63dc495492d556799da65243962e46
Andrew Kushnir <akushnir@google.com>
2025-07-10 19:59:38 +0000

    aea0a70 ci: adjust renovate teams (#62578)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0+sha-6549418");
+var VERSION = new Version("20.1.0+sha-aea0a70");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
