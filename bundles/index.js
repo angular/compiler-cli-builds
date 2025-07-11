7576f9d8854987cb3602cb31cd63f51a1614efaf
Andrew Kushnir <akushnir@google.com>
2025-07-11 17:52:26 +0000

    02dce8e ci: add to public-api approvers list (#62591)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0+sha-4f1e88e");
+var VERSION = new Version("20.1.0+sha-02dce8e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
