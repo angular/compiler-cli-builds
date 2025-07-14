6500e7292338a34600b2c75ffe014689cdf7f224
Andrew Kushnir <akushnir@google.com>
2025-07-14 21:25:15 +0000

    f202160 docs: add dedicated data resolvers router guide (#62250)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0+sha-5949373");
+var VERSION = new Version("20.1.0+sha-f202160");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
