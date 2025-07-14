0e92830f60a81517385c1d093469d9d1a3ad9dd2
Andrew Kushnir <akushnir@google.com>
2025-07-14 22:56:33 +0000

    a81f0fa fix(core): InputBinding marks component a dirty. (#62613)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.0+sha-74beeec");
+var VERSION = new Version("20.1.0+sha-a81f0fa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
