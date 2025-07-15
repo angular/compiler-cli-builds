4e22269c86b2bbe8a8c12549c7cc151840ac0cc5
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:43:06 +0000

    acf8b74 build: update all non-major dependencies (#62643)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.0+sha-72605a4";
+var PLACEHOLDER_VERSION = "20.1.0+sha-acf8b74";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
