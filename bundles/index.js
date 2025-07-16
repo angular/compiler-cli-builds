00ab2fe344776ab73eb4d61266d394639cc62444
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:20:12 +0000

    f5f8f76 release: bump Angular DevTools version to 1.0.39 (#62670)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-a484a11");
+var VERSION = new Version("20.2.0-next.1+sha-f5f8f76");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
