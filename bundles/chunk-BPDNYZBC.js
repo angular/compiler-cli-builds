a02d36b396b7e053a7c2b304b319618103cff044
Kirill Cherkashin <kirjs@google.com>
2025-07-17 22:21:35 +0000

    23d5877 build: migrate to new toolchain usage for api goldens (#62688)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0-next.1+sha-6f52e97";
+var PLACEHOLDER_VERSION = "20.2.0-next.1+sha-23d5877";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
