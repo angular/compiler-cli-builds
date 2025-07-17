e9590544be5f8d5010fa93d1e0f0270291ef34e9
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:12:58 +0000

    dcab8c9 refactor(devtools): rearrange main toolbar menu buttons (#62684)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.1+sha-1930e99";
+var PLACEHOLDER_VERSION = "20.1.1+sha-dcab8c9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
