aa4085597f6f677024858e419c65b1d110a62dc7
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:29:12 +0000

    8ad10fd fix(compiler): fix detection of directive deps in JIT (#62666)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.1.1+sha-dcab8c9";
+export declare const PLACEHOLDER_VERSION = "20.1.1+sha-8ad10fd";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
