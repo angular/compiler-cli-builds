76fe7e3ab7cebfdd4a6283391e820e0eac58278f
Kirill Cherkashin <kirjs@google.com>
2025-07-18 14:27:08 +0000

    c36de9e build: update cross-repo angular dependencies (#62651)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.2+sha-409287a");
+var VERSION = new Version("20.1.2+sha-c36de9e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
