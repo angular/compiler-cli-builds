6d1400d3e8d3d8778306a13327cb5f6690099872
Kirill Cherkashin <kirjs@google.com>
2025-07-18 11:28:20 +0000

    448a7f7 refactor(core): Use promise.finally for task removal (#62674)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-d3b770d");
+var VERSION = new Version("20.2.0-next.1+sha-448a7f7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
