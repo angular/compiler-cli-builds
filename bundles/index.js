2a1e2355856d1a076b3c8b9bf726adcc8cc114df
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:24:36 +0000

    aee5260 fix(devtools): stop reseting currentlyMatchedIndex when a node is selected in the component explorer (#62727)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.2+sha-70d0c10");
+var VERSION = new Version("20.1.2+sha-aee5260");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
