c050676cd082cfc924296cd768302b88ff73fe4d
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:42:26 +0000

    45d3eef test(devtools): bump cypress timeout to 10 seconds (#62726)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.2+sha-f041042");
+var VERSION = new Version("20.1.2+sha-45d3eef");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
