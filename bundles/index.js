bd8872f52a328b26446613165a6b397a895172f1
Kirill Cherkashin <kirjs@google.com>
2025-07-22 16:38:45 +0000

    91bdd17 build: update @angular/ng-dev digest to 75a2aee (#62745)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.2+sha-db8f7ce");
+var VERSION = new Version("20.1.2+sha-91bdd17");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
