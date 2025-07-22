40a1d0a6df687f45133a07c07346e16e3277254e
Kirill Cherkashin <kirjs@google.com>
2025-07-22 14:25:39 +0000

    4ac6171 feat(service-worker): Adds support for updateViaCache in provideServiceWorker (#62721)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-a2f366f");
+var VERSION = new Version("20.2.0-next.1+sha-4ac6171");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
