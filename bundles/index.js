f93f689512908ae4e9d7c99eeb5925dafd40d47f
Kirill Cherkashin <kirjs@google.com>
2025-07-22 18:26:30 +0000

    0a9f55d build: update dependency angular-split to v20 (#62740)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-92e03e1");
+var VERSION = new Version("20.2.0-next.1+sha-0a9f55d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
