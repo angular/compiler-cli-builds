101a695cb26ba303c1637380bd7746d43b2020ba
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-23 21:26:35 +0000

    da2700c release: cut the v20.2.0-next.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-8344572");
+var VERSION = new Version("20.2.0-next.2+sha-da2700c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
