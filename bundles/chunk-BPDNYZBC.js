2e5d08129a20cc45b60ede6f40a8c98d8749a1d0
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-23 21:01:18 +0000

    d53649c release: cut the v20.1.3 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.2+sha-006eaf3";
+var PLACEHOLDER_VERSION = "20.1.3+sha-d53649c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
