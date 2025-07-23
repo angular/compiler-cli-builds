e3000698d6b12048b9041d270dbddcd1582a541c
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:49:14 +0000

    7f9d37d build: update bazel rules_browsers dependency (#62772)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-063b5e1");
+var VERSION = new Version("20.2.0-next.1+sha-7f9d37d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
