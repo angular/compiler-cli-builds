556cb350afccf5f516a32e11fd2472282b9b5b9d
Jan Martin <jan.krems@gmail.com>
2025-07-23 16:32:18 +0000

    6398baa release: bump Angular DevTools version to 1.0.40 (#62767)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.2+sha-2fd1f7b";
+var PLACEHOLDER_VERSION = "20.1.2+sha-6398baa";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
