054bb648501f8d6778947219b9f4cc1ed838af90
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-23 21:03:29 +0000

    8344572 docs: release notes for the v20.1.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.1+sha-7f9d37d");
+var VERSION = new Version("20.2.0-next.1+sha-8344572");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
