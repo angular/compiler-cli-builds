6665ab64e3498032dd67a5ced0c7405ad1666063
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 11:10:20 +0000

    94b0ef1 build: update github/codeql-action action to v3.29.4 (#62789)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.2+sha-92b0802");
+var VERSION = new Version("20.2.0-next.2+sha-94b0ef1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
