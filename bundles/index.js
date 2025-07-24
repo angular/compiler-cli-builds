07e8ad3c814b1a1ebaf998ea846a77cc9bb2cc4f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 16:20:23 +0000

    b11e050 docs(docs-infra): correct typos in adev and wording in resource api (#62801) PR Close #62801

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.2+sha-61eeb9b");
+var VERSION = new Version("20.2.0-next.2+sha-b11e050");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
