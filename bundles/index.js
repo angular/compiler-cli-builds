7b687d178a377b2bc6b958800e97dab5643b05c5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 08:15:28 +0000

    c19d0ae build: use ts_config rule from rules_ts only and always load from common location (#62808)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.2+sha-b11e050");
+var VERSION = new Version("20.2.0-next.2+sha-c19d0ae");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
