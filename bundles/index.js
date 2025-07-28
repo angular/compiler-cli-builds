c903f3b5bb1a7beeb077b7299c3160a89c325cda
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-28 15:11:46 +0000

    92c2d2a fix(core): update symbols for new signals api (#62284)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.2+sha-3b759db");
+var VERSION = new Version("20.2.0-next.2+sha-92c2d2a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
