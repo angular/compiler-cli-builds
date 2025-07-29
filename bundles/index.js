aaf3f66648b4136a8fff12e6fe0b939170357ce1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 10:01:07 +0000

    f0fca61 refactor(common): replace aio links to adev (#62839)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.3+sha-cd256ef");
+var VERSION = new Version("20.1.3+sha-f0fca61");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
