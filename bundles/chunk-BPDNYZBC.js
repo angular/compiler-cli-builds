3eae26b66d64eab839fd7d7475f6fcb851b84a0c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 10:06:18 +0000

    55e575a docs(docs-infra): increase until timeout (#62894)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.3+sha-a2e3bfc";
+var PLACEHOLDER_VERSION = "20.1.3+sha-55e575a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
