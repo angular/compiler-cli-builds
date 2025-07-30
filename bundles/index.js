4bcd0450df43c870a3915a1b48a2e7b7eb42f8ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 10:40:36 +0000

    320de4e refactor(core): deprecate animations field on component interface (#62895)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.2+sha-9d05a8a");
+var VERSION = new Version("20.2.0-next.2+sha-320de4e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
