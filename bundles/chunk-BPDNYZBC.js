917378584d8eee8c34cc8532c323c85f71805a39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 09:42:44 +0000

    a2e3bfc build: update all non-major dependencies (#62892)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.3+sha-25c6b15";
+var PLACEHOLDER_VERSION = "20.1.3+sha-a2e3bfc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
