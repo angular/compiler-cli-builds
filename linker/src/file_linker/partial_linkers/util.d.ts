02f197efeb78cfa804db446ba1d570e927b3e1d9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-31 08:44:58 +0000

    d08f9ce docs: release notes for the v20.1.4 release

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.2.0-next.2+sha-e7d748e";
+export declare const PLACEHOLDER_VERSION = "20.2.0-next.2+sha-d08f9ce";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
