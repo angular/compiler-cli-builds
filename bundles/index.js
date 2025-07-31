686c3f8b0a7bc78c7172b0f6ea84d40b16a38489
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-31 10:41:28 +0000

    0777222 build: update dependency shiki to v3.9.0 (#62921)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.4+sha-c92a306");
+var VERSION = new Version("20.1.4+sha-0777222");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
