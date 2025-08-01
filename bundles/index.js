b12d460606abb51b31fb36beaff2ae2d8197c7d9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-01 10:42:38 +0000

    17b6a5b build: update pnpm to v10.14.0 (#62938)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.3+sha-e5d6fb5");
+var VERSION = new Version("20.2.0-next.3+sha-17b6a5b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
