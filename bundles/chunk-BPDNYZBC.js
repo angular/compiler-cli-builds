c5373e2a36e21a9fa4b8a303794d48a7627c3eb7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:26:03 +0000

    c2b48be build: move to pnpm version 9.15.9 (#62951)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.4+sha-f700bd9";
+var PLACEHOLDER_VERSION = "20.1.4+sha-c2b48be";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.4+sha-f700bd9");
+var VERSION = new Version("20.1.4+sha-c2b48be");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
