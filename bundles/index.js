827212dfcd0cdac86933fe3eb27ebf93f956ae4b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 12:44:04 +0000

    a2b4988 build: replace `docs` script with `adev` (#62944)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.3+sha-17b6a5b");
+var VERSION = new Version("20.2.0-next.3+sha-a2b4988");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
