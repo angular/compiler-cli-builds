b43c27a1cafce11c9e47c6e8eea18f8c20d8a46b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:12:54 +0000

    ae443f8 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.4+sha-216d883");
+var VERSION = new Version("20.1.4+sha-ae443f8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
