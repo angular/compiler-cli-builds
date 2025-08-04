46f2f387d5cf8bc7641bce85c5aea2c2852f75f8
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 10:28:18 +0000

    6597ac0 fix(core): fix support for space separated strings in leave animations (#62979)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0-next.3+sha-cbd300d";
+var PLACEHOLDER_VERSION = "20.2.0-next.3+sha-6597ac0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
