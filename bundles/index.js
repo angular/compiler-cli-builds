672f62a5a5600d3fac8063029d16534ae0a9d3fa
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 16:07:06 +0000

    ddf779e Revert "feat(router): add a `currentNavigation` signal to the `Router` service. (#62971)" (#63010)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.3+sha-5802f80");
+var VERSION = new Version("20.2.0-next.3+sha-ddf779e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
