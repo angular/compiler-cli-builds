9eff4941f24208603b4afd5fd38405f16da8dad4
Andrew Scott <atscott01@gmail.com>
2025-08-06 19:09:08 +0000

    9365a7f docs: release notes for the v20.1.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.4+sha-ecc86e2");
+var VERSION = new Version("20.2.0-next.4+sha-9365a7f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
