80252b12de4555261189d7cba3533b977f96ace9
Andrew Scott <atscott01@gmail.com>
2025-08-06 19:15:17 +0000

    90b0227 release: cut the v20.2.0-next.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.4+sha-9365a7f");
+var VERSION = new Version("20.2.0-next.5+sha-90b0227");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
