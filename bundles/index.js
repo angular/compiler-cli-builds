8ff5ab0369c76e059e43aad5e15428acfc4ae805
Andrew Scott <atscott@google.com>
2025-08-06 16:58:47 +0000

    687c374 feat(router): add a currentNavigation signal to the Router service. (#63011) This new signal property is convenient to derive a `isNavigating` state.

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-next.4+sha-d24d574");
+var VERSION = new Version("20.2.0-next.4+sha-687c374");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
