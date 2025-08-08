8586ef1f46713fe4971347b8e3dea0d9705d33cd
Andrew Scott <atscott@google.com>
2025-08-08 18:10:16 +0000

    63aa602 docs(docs-infra): add next page reference (#62566)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.6+sha-322042c");
+var VERSION = new Version("20.1.6+sha-63aa602");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
