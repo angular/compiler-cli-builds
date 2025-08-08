620c38d9335bef673dde943833eeb40a3d762254
Andrew Scott <atscott@google.com>
2025-08-08 18:11:34 +0000

    3b214d2 feat(service-worker): Logs unhandled promise rejections in service worker (#63059)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.2.0-next.6+sha-6a11846";
+export declare const PLACEHOLDER_VERSION = "20.2.0-next.6+sha-3b214d2";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
