80eab09a7b57dc9b0c87431e40d2b87c20a7ee41
Andrew Scott <atscott@google.com>
2025-08-11 19:30:42 +0000

    d47ea29 refactor(devtools): keep TreeVisualizer snapped node on focus (#62982)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0-next.0+sha-baa31fb");
+var VERSION = new Version("20.3.0-next.0+sha-d47ea29");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
