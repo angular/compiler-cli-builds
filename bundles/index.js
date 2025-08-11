76cd46b22b71ca32e989a876fdcc74b6c4001678
Andrew Scott <atscott@google.com>
2025-08-11 22:47:36 +0000

    af247ca refactor(core): remove temporary resource setter function. (#63097)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.0+sha-499fa8f");
+var VERSION = new Version("20.2.0-rc.0+sha-af247ca");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
