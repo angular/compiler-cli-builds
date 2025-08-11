a063634c16b6a9aea8b1478a19ff5200d3bd8305
Andrew Scott <atscott@google.com>
2025-08-11 19:19:42 +0000

    fdce7e2 docs: remove router testing from the testing scenarios guide (#63078)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.1.6+sha-d9e3790";
+export declare const PLACEHOLDER_VERSION = "20.1.6+sha-fdce7e2";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
