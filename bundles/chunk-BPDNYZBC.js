a6c12d776cde6ad0c43af5d25c6048c4b0828ec0
Andrew Scott <atscott@google.com>
2025-08-11 19:05:49 +0000

    d9e3790 fix(compiler): incorrect spans for AST inside input value with leading space (#63082)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.6+sha-53297c0";
+var PLACEHOLDER_VERSION = "20.1.6+sha-d9e3790";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.6+sha-53297c0");
+var VERSION = new Version("20.1.6+sha-d9e3790");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
