21b0c837d40c33b19b454db747a7342f53110582
Andrew Scott <atscott@google.com>
2025-08-11 18:08:44 +0000

    53297c0 ci: several fixes for renovate (#63083)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.6+sha-b72ef65");
+var VERSION = new Version("20.1.6+sha-53297c0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
