c703ea1ca68773764e95b4661605dee17fbe5b2d
Andrew Scott <atscott@google.com>
2025-08-11 17:39:21 +0000

    b72ef65 build: update to the latest version of devinfra and rename npm2 workspace (#63093)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.6+sha-5fd7942");
+var VERSION = new Version("20.1.6+sha-b72ef65");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
