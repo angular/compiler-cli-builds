6c5de9a535c9ea904ed83b0d96e1144b935c81c2
Andrew Scott <atscott@google.com>
2025-08-12 16:43:46 +0000

    0e3465d docs: add missing supported `!=` operator (#63108)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.2.0-rc.0+sha-856dd50";
+export declare const PLACEHOLDER_VERSION = "20.2.0-rc.0+sha-0e3465d";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
