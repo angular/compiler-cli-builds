641caf61bb041a1ad94a592434250cebfe7525b8
Andrew Scott <atscott@google.com>
2025-08-12 18:04:47 +0000

    b13c3fc build: update actions/checkout action to v5 (#63115)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.0+sha-31df4d2");
+var VERSION = new Version("20.2.0-rc.0+sha-b13c3fc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
