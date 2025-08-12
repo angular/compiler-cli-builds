a1bd520a3f1ca836ff5c61762b05e0845d005781
Andrew Scott <atscott@google.com>
2025-08-12 16:57:26 +0000

    31df4d2 build: update cross-repo angular dependencies (#63088)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0-rc.0+sha-0e3465d";
+var PLACEHOLDER_VERSION = "20.2.0-rc.0+sha-31df4d2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
