8f38a15878e1f32247c29aecc6b98951dc499299
Andrew Scott <atscott@google.com>
2025-08-13 19:57:22 +0000

    ced0357 build: update cross-repo angular dependencies (#63137)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.0+sha-d9f0e6b");
+var VERSION = new Version("20.2.0-rc.0+sha-ced0357");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
