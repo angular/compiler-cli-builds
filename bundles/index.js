5c770284569ff26c9fc856a265d4435eb2e5e978
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:09 +0000

    55cd8c2 docs: add rxjs-interop section for `rxResource` (#63125)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-21b0fd3");
+var VERSION = new Version("21.0.0-next.0+sha-55cd8c2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
