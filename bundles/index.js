68e78a210e322fdfa8753a87ff6af49baf29df45
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:08 +0000

    5129dae ci: redirect adev to `index.csr.html` (#63168)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-d733c69");
+var VERSION = new Version("21.0.0-next.0+sha-5129dae");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
