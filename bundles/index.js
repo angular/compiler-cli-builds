28a8f5547d495b3590e74e8ece346cf52128758f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 20:06:56 +0000

    5e084e4 docs(docs-infra): enhance method name following the guideline (#63164)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.7+sha-7ee4965");
+var VERSION = new Version("20.1.7+sha-5e084e4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
