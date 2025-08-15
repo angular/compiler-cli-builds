ca81cf4bfcd31310bb7b5bfb5d9d5c249399e33d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 11:46:00 +0000

    d8ac27f docs(docs-infra): replace Input by signal one (#63169)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0-rc.1+sha-de6b641";
+var PLACEHOLDER_VERSION = "20.2.0-rc.1+sha-d8ac27f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-de6b641");
+var VERSION = new Version("20.2.0-rc.1+sha-d8ac27f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
