3a13f2a62d05d103470bd9e8023bf9c14aebcc71
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 07:58:56 +0000

    5b3933f fix(compiler): error when ng-content fallback has translated children (#63156)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-5129dae");
+var VERSION = new Version("21.0.0-next.0+sha-5b3933f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
