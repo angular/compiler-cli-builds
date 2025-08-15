bd76c96b7bcdd0ba2e367dbfe83abf51f13873d3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 10:35:51 +0000

    de6b641 docs: add --yes option to mcp commands (#63173)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-124d44f");
+var VERSION = new Version("20.2.0-rc.1+sha-de6b641");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
