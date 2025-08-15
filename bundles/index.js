af8a7957a36a8ec1d3ad88651ee5c8a6b485dd71
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 11:54:18 +0000

    3f88fbd docs(docs-infra): replace deprecated router method (#63183)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-6f91972");
+var VERSION = new Version("21.0.0-next.0+sha-3f88fbd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
