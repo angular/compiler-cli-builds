1c28cbc12a13ddf020ed60e625388e4da0ec0cb0
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:18:06 +0000

    ff01e33 docs: revert remove of `build-and-link` (#63187)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-3f88fbd");
+var VERSION = new Version("21.0.0-next.0+sha-ff01e33");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
