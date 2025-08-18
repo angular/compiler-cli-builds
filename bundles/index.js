0f33ff7ec6e8b477e3c1b3d032836e554241f859
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:24:58 +0000

    d0cf624 build: update cross-repo angular dependencies (#63220)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-1aa0a88");
+var VERSION = new Version("21.0.0-next.0+sha-d0cf624");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
