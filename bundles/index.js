0e88096b2e37bee06d04be8314a780e4458198a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 16:02:54 +0000

    7b51728 fix(compiler): fixes animation event host bindings not firing (#63217)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-5be80d3");
+var VERSION = new Version("20.2.0-rc.1+sha-7b51728");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
