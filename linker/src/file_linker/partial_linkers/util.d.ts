7740439c0b07d50078883bc9e662d33cd583876a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 12:45:46 +0000

    a7cfb42 fix(core): ensure animate events do not have duplicate elements (#63216)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.0+sha-6c8d000";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.0+sha-a7cfb42";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
