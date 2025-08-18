c2bb25b3bd10d9feb530f4a27f8796b3fff635fd
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 08:10:28 +0000

    5d31e20 docs(docs-infra): remove @adev/constant path (#63213)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.1.7+sha-a9bab9e";
+var PLACEHOLDER_VERSION = "20.1.7+sha-5d31e20";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
