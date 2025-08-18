70e31759d57970dccc49d6e908c0ddc9063c44ab
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:47:26 +0000

    856c5f0 build: remove extra comma in JSON (#63212)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-ff01e33");
+var VERSION = new Version("21.0.0-next.0+sha-856c5f0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
