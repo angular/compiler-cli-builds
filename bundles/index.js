1aa75a7190361c6896da3cc24f977b70d8285e90
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:46:27 +0000

    5be80d3 fix(devtools): DOM traversal bug (#62719)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-1e3748b");
+var VERSION = new Version("20.2.0-rc.1+sha-5be80d3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
