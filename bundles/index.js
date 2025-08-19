e81d4d8ab6b973f379fe54b37f0fb8a4d3a8d4e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:04:35 +0000

    a8c5baf docs: fix typo in SSR section (#63224) PR Close #63224

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-7b51728");
+var VERSION = new Version("20.2.0-rc.1+sha-a8c5baf");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
