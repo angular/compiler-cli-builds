a017395536316d01acdeec726f94f0d759a0c50a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 12:36:39 +0000

    72ae106 docs: use correct closing quote in `input` and `model` import usage notes (#63237)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-9425179");
+var VERSION = new Version("21.0.0-next.0+sha-72ae106");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
