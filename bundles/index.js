e25241da7b4bc2169c8b37b247a242539ad9eff6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:34:38 +0000

    8164b22 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0-rc.1+sha-966d95d");
+var VERSION = new Version("20.2.0-rc.1+sha-8164b22");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
