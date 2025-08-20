e001c3205fd634fa35d7d6b2ea539c3a0dd03cf4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:27:07 +0000

    edbbbb1 refactor(core): remove `NgProbeToken` class (#63279)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-6d88ce1";
+var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-edbbbb1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
