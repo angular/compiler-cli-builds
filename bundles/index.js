5aad6bf3caa918a7b3a2f44e0c10421e76190f51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:56:44 +0000

    086ae5e build: update all non-major dependencies (#63264)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-eeeaadc");
+var VERSION = new Version("21.0.0-next.0+sha-086ae5e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
