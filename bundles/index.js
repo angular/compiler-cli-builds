2bb66aed9acbf73ee368145dbe9a15bce145c283
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:33:37 +0000

    c935d42 docs: remove redundant Angular docs link in tailwind guide (#63205)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-DWRM7PIK.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.1.7+sha-683ae03");
+var VERSION = new Version("20.1.7+sha-c935d42");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
