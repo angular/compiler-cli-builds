08603841fb8384239baf20d76a4027db878f064b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:18:59 +0000

    9655ea7 build: update dependency playwright-core to v1.55.0 (#63273)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.2.0-rc.1+sha-d76a911";
+export declare const PLACEHOLDER_VERSION = "20.2.0-rc.1+sha-9655ea7";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
