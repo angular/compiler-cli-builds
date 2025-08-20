5104b4c69ea3b05df552c5a44578b531be502c05
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 13:08:18 +0000

    eb01830 docs: remove IsolatedShadowDom from the 20.2 release (#63281)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.0+sha-9e172e0");
+var VERSION = new Version("20.2.0+sha-eb01830");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
