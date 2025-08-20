3a28d93a319f988a26263aca38cbe6eaa2a9f0a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:03:44 +0000

    11c6385 build: enforce frozen lockfile mode (#63260)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.0+sha-a6130e0";
+var PLACEHOLDER_VERSION = "20.2.0+sha-11c6385";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
