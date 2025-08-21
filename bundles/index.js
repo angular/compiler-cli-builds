97601b3ebc0d98eba094109372be9c80868a01d7
Andrew Kushnir <akushnir@google.com>
2025-08-21 17:20:12 +0000

    7d6ae95 docs(docs-infra): fix mermaid rendering (#63311)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-0dd95c5");
+var VERSION = new Version("21.0.0-next.0+sha-7d6ae95");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
