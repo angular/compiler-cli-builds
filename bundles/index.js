cb335cca663692fe33f225609da023e5d1bf8dff
Andrew Kushnir <akushnir@google.com>
2025-08-21 18:46:48 +0000

    69df2c0 build: update dependency cypress to v15 (#63303)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-7d6ae95");
+var VERSION = new Version("21.0.0-next.0+sha-69df2c0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
