a7223ac273503d80008e1c69ace4c32842f3eb45
Andrew Kushnir <akushnir@google.com>
2025-08-21 21:17:09 +0000

    6f541b8 build: update dependency rollup to v4.47.1 (#63304)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-69df2c0";
+var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-6f541b8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
