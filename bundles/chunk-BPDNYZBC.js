ebb25d1cdf001cd50c482c68a107a1db51fe463f
Andrew Kushnir <akushnir@google.com>
2025-08-21 21:16:08 +0000

    040cba0 build: update cross-repo angular dependencies (#63319)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.1+sha-6213c7d";
+var PLACEHOLDER_VERSION = "20.2.1+sha-040cba0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
