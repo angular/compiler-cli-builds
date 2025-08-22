dba7767d5b80cbc5094838e83f061db14a098d6e
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:30:52 +0000

    983cc2f ci: add myself to review fw-general (#63337)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-a842272");
+var VERSION = new Version("21.0.0-next.0+sha-983cc2f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
