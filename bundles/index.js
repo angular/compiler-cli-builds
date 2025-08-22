d4bbb978ecaceb078b03be8155e531fd688b71cf
Andrew Kushnir <akushnir@google.com>
2025-08-22 21:48:32 +0000

    3df1dcc refactor: various build and import specificer fixes for strict deps (#63323)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-983cc2f");
+var VERSION = new Version("21.0.0-next.0+sha-3df1dcc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
