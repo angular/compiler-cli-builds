841bb1e61d816939e4762cdd81cb94f54dfaab47
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:25:31 +0000

    5af3222 docs: update paths and references to animations guides (#63368)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.0+sha-1e3196b");
+var VERSION = new Version("21.0.0-next.0+sha-5af3222");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
