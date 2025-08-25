4536c10d1dd5b2d1ac41456a94d91d0b9d7ab5f1
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:21:58 +0000

    650ad1f docs: add important service worker note (#63364)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.1+sha-10b0927");
+var VERSION = new Version("20.2.1+sha-650ad1f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
