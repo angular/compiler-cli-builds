fa3c6980fb44074fab729086acf3d91658f574cc
Andrew Kushnir <akushnir@google.com>
2025-08-25 17:54:00 +0000

    454a269 build: update cross-repo angular dependencies (#63339)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.1+sha-f881082");
+var VERSION = new Version("20.2.1+sha-454a269");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
