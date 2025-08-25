3c33ddadc94256426b8c6df4e3c2fcc8c71300ca
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:21:44 +0000

    1e3196b docs: add important service worker note (#63364)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-3df1dcc";
+var PLACEHOLDER_VERSION = "21.0.0-next.0+sha-1e3196b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
