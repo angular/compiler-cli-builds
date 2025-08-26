19037cc3c28ed93c0d611f820c5d49cb2cff1c23
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:44:22 +0000

    c9b0f45 fix(devtools): prevent profiler bars flickering after change detection (#63350)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.1+sha-15395a5");
+var VERSION = new Version("20.2.1+sha-c9b0f45");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
