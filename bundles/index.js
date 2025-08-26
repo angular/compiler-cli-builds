e245bd53b20f969a1313e9a356750f5411783dae
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:56:47 +0000

    23d1ace refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.1+sha-e220a61");
+var VERSION = new Version("20.2.1+sha-23d1ace");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
