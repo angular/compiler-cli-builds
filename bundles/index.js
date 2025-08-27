390d54ea69d0c6a8b35af2e520910e978f4edc00
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:33:29 +0000

    06c5367 docs: update to new standalone default behavior (#63329)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-6e0b66d");
+var VERSION = new Version("20.2.2+sha-06c5367");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
