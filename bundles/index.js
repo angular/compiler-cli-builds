72bb9ed5a2dcf07958aaa47d09714db6b59e14fd
Miles Malerba <mmalerba@google.com>
2025-08-27 23:13:36 +0000

    cd748aa docs: remove the ng-class migration entry (#63430)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-fce5602");
+var VERSION = new Version("20.2.2+sha-cd748aa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
