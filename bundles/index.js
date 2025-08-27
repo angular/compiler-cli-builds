0992eed4db3152296bca16a2b78af16f7c9a1a8c
Miles Malerba <mmalerba@google.com>
2025-08-27 23:59:39 +0000

    a6a46e9 docs(docs-infra): tooltip on hover and message after copied (#63167)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-9de2f07");
+var VERSION = new Version("20.2.2+sha-a6a46e9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
