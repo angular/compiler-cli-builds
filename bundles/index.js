fcca64f7d9fa6b9f4d5d0c603e890cce506701d1
Miles Malerba <mmalerba@google.com>
2025-08-27 23:21:41 +0000

    9de2f07 docs: add customizing route behavior guide for router (#63398)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-cd748aa");
+var VERSION = new Version("20.2.2+sha-9de2f07");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
