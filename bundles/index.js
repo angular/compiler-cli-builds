29bf913960048bbce101430107fffb29d6090868
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:15:37 +0000

    f868465 fix(upgrade): Remove deprecated UpgradeAdapter (#61659)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-478eebe");
+var VERSION = new Version("21.0.0-next.1+sha-f868465");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
