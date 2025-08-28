d1c83fc521d0d84284a1cb17d7e5091299743d2c
Miles Malerba <mmalerba@google.com>
2025-08-28 15:53:32 +0000

    a1d1cdf build: remove `rulesJsInteropMode` (#63448)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-07e6788");
+var VERSION = new Version("21.0.0-next.1+sha-a1d1cdf");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
