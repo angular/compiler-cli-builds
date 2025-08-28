53375a27108f41b3cf88b08e25c3e9596ac8c3d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:12:17 +0000

    fe533cd docs: update Angular CLI help (#63420)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.2+sha-421edfe";
+var PLACEHOLDER_VERSION = "20.2.2+sha-fe533cd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-421edfe");
+var VERSION = new Version("20.2.2+sha-fe533cd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
