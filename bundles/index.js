b59010cfbff8879dbfcae468a4aa117c69920156
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:15:24 +0000

    114906d fix(core): Fix cancellation of animation enter classes (#63442)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.2+sha-2e05dea");
+var VERSION = new Version("20.2.2+sha-114906d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
