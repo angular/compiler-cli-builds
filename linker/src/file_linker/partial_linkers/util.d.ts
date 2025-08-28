c459edbd3908d1f2ba8182f796211f8b50504a70
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:09:52 +0000

    2e05dea ci: remove old options from workflows (#63441)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.2.2+sha-690405b";
+export declare const PLACEHOLDER_VERSION = "20.2.2+sha-2e05dea";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
