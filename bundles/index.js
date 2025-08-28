62abfd0b42ee8163335b849f2c3bbf49bd89c921
Miles Malerba <mmalerba@google.com>
2025-08-28 15:39:20 +0000

    07e6788 feat(http): Add reponseType property to HttpResponse and HttpErrorResponse (#63043)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-ed3d1f2");
+var VERSION = new Version("21.0.0-next.1+sha-07e6788");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
