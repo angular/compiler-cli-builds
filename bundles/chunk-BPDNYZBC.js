3bb7fa4c35d6a162333c38eaa63f8a43de4088c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:14:26 +0000

    ed3d1f2 fix(core): Fix cancellation of animation enter classes (#63442)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.1+sha-99cf028";
+var PLACEHOLDER_VERSION = "21.0.0-next.1+sha-ed3d1f2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
