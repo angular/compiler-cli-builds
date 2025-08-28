c3077ed504654535ca7476a10b55f97fc86881b7
Miles Malerba <mmalerba@google.com>
2025-08-28 16:06:03 +0000

    b8314bd feat(forms): add experimental signal-based forms (#63408)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-a1d1cdf");
+var VERSION = new Version("21.0.0-next.1+sha-b8314bd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
