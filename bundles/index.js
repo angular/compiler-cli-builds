e4bc17810b9e80339fdb897e3be4d92a473f1d1f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:48:53 +0000

    1abf6d5 build: update cross-repo angular dependencies to 06d3af5 (#63476)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.3+sha-c357a03");
+var VERSION = new Version("20.2.3+sha-1abf6d5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
