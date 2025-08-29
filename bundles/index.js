4f499e292f3f6796eb1728349b7a0e0c65743c40
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:23:05 +0000

    3fb02ed build: update google-github-actions/auth action to v3 (#63461)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-a3938d5");
+var VERSION = new Version("21.0.0-next.1+sha-3fb02ed");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
