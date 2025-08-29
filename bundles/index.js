58a65886a38c6635c55bffeacfb2aa836acee790
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:05:38 +0000

    c357a03 docs(docs-infra): makes signal readonly (#63449)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.3+sha-64ddde2");
+var VERSION = new Version("20.2.3+sha-c357a03");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
