50e9d3a7b7a2a9f06a907f520d57333f18f4460a
Miles Malerba <mmalerba@google.com>
2025-08-29 21:09:48 +0000

    e75b04f docs(docs-infra): properties should be readonly (#63492)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-bbb46e5");
+var VERSION = new Version("21.0.0-next.1+sha-e75b04f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
