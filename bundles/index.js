74f7b67b8144c0c4e62bb9c3fbf67dd614fa69f2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 11:56:46 +0000

    0a82138 fix(compiler): fixes regression with event parsing and animate prefix (#63470)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-1a26fd3");
+var VERSION = new Version("21.0.0-next.1+sha-0a82138");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
