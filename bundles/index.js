24b0e7a677222c3174c78b5fe5148e50e207a690
Miles Malerba <mmalerba@google.com>
2025-08-29 22:14:51 +0000

    16fa983 ci: add more team members as approvers (#63495)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-db95a5c");
+var VERSION = new Version("21.0.0-next.1+sha-16fa983");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
