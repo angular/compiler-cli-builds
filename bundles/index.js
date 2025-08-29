c922b744f5a148886ee8bee435d5ed08a76dd8cc
Miles Malerba <mmalerba@google.com>
2025-08-29 16:02:14 +0000

    6e1599d refactor(router): Add internal flag to control async vs rxjs recognize (#62994)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-dcd81d9");
+var VERSION = new Version("21.0.0-next.1+sha-6e1599d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
