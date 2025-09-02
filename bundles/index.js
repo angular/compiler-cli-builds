b2fbbed133c2afd577342901ffa7cab10153a151
Miles Malerba <mmalerba@google.com>
2025-09-02 18:06:58 +0000

    92e09ad fix(core): Remove ignoreChangesOutsideZone option (#62700)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-1e6d311");
+var VERSION = new Version("21.0.0-next.1+sha-92e09ad");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
