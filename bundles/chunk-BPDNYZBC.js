6232c2b97cdc63c82b1d126f0d001ff93010ccd4
Miles Malerba <mmalerba@google.com>
2025-09-02 16:40:16 +0000

    3b42314 docs(docs-infra): export class (#63506)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.3+sha-15fa2ce";
+var PLACEHOLDER_VERSION = "20.2.3+sha-3b42314";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
