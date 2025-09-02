4584b70655e1ebbf3c9f82746da49af74890749c
Miles Malerba <mmalerba@google.com>
2025-09-02 16:47:03 +0000

    e63608d docs(docs-infra): signal property should be readonly (#63166)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-668ced9");
+var VERSION = new Version("21.0.0-next.1+sha-e63608d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
