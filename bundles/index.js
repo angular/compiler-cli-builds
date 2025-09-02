75878bfc5c941581766b0833e3a43796b87fc0e6
Miles Malerba <mmalerba@google.com>
2025-09-02 17:11:47 +0000

    1e6d311 build: update cross-repo angular dependencies (#63548)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-7aa9350");
+var VERSION = new Version("21.0.0-next.1+sha-1e6d311");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
