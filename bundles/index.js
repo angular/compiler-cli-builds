5bcfb79064e08cd360eea9058c284e26ef816f6e
Miles Malerba <mmalerba@google.com>
2025-09-02 19:42:35 +0000

    4fb8c46 docs: update Angular CLI help (#63552)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-4c36659");
+var VERSION = new Version("21.0.0-next.1+sha-4fb8c46");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
