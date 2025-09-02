f06a21c0e3b2047f719183fcdf22e449526dbeee
Miles Malerba <mmalerba@google.com>
2025-09-02 18:03:09 +0000

    dc64f3e fix(core): Fixed inject migration schematics for migrate destructured properties (#62832)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.3+sha-957b367";
+var PLACEHOLDER_VERSION = "20.2.3+sha-dc64f3e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
