17658161bb165d6f052a137f25ff846fe93feeb2
Miles Malerba <mmalerba@google.com>
2025-09-03 16:19:11 +0000

    0c2255b refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.3+sha-f4a2d0a";
+var PLACEHOLDER_VERSION = "20.2.3+sha-0c2255b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
