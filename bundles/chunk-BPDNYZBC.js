4a9a7c0aff1135b4813bbc90a6a285b7f8282d9f
Andrew Scott <atscott@google.com>
2025-09-03 16:55:33 +0000

    320f1d6 build: update @angular/ng-dev digest to 435c59b (#63571)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.2.3+sha-991785f";
+var PLACEHOLDER_VERSION = "20.2.3+sha-320f1d6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.3+sha-991785f");
+var VERSION = new Version("20.2.3+sha-320f1d6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
