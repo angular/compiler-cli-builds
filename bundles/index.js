94b7beef71e86ab62401babafaf70225dbd8d59e
Miles Malerba <mmalerba@google.com>
2025-09-03 16:19:05 +0000

    49d5de4 refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.1+sha-e6949ad");
+var VERSION = new Version("21.0.0-next.1+sha-49d5de4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
