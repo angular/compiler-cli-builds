767f136e85a4ff9cf70e68875aa038d17ca815c7
Miles Malerba <mmalerba@google.com>
2025-09-03 03:55:48 +0000

    475ed59 refactor(core): export zoneless provider token privately (#63558)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.1+sha-6148a40";
+var PLACEHOLDER_VERSION = "21.0.0-next.1+sha-475ed59";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
