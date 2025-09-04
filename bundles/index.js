97936c1555750639b59ea3637608b41295dffaae
Andrew Scott <atscott@google.com>
2025-09-04 16:26:52 +0000

    a3f808d fix: remove refresh button from transfer state tab (#63592)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.4+sha-951701d");
+var VERSION = new Version("20.2.4+sha-a3f808d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
