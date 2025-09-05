8fb2228a0187cbe1c5b98efb59d3df4da836d995
Andrew Scott <atscott@google.com>
2025-09-05 21:03:57 +0000

    bacda4f refactor(forms): add support for binding number fields to date inputs (#63585)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.2+sha-c0791e1");
+var VERSION = new Version("21.0.0-next.2+sha-bacda4f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
