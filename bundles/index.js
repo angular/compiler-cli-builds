4e0e2ac7a782740c8cfab3615f146b00ee627e51
Andrew Scott <atscott@google.com>
2025-09-05 20:11:34 +0000

    f18980c docs(docs-infra): fix mini share icon URL. (#63586)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.4+sha-c3303a5");
+var VERSION = new Version("20.2.4+sha-f18980c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
