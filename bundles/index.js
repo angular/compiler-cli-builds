e7f1031e47b2509cf82242da4ab98561588f13c2
Andrew Scott <atscott@google.com>
2025-09-08 19:49:14 +0000

    a6ed784 build: properly override the repo for rules_angular (#63584)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.4+sha-d124fba");
+var VERSION = new Version("20.2.4+sha-a6ed784");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
