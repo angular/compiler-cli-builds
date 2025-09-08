45983a32abdb4a0309ce34d5ec37baa827ed5a8a
Andrew Scott <atscott@google.com>
2025-09-08 21:25:54 +0000

    c8c4399 docs: remove installation (#63508)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-00a1806";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-c8c4399";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
