ee70129900807048574a3493df12154dfed62eba
Andrew Scott <atscott@google.com>
2025-09-08 23:34:42 +0000

    17d5479 docs: Update Language Service IDE list (#63647)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-2fd0b57";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-17d5479";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
