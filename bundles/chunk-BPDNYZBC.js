e5505ef16068dfcdf1c0c9f28101b38498087809
Andrew Scott <atscott@google.com>
2025-09-08 21:56:33 +0000

    efb9474 docs: should not conditionally include `<ng-content>` in IMPORTANT block (#63580)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-c8c4399";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-efb9474";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
