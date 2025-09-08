639b1cdcfc0f1b7109ed7fa81b3d81b14dab7a0a
Andrew Scott <atscott@google.com>
2025-09-08 21:56:18 +0000

    a857f72 docs: should not conditionally include `<ng-content>` in IMPORTANT block (#63580)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.4+sha-9943230");
+var VERSION = new Version("20.2.4+sha-a857f72");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
