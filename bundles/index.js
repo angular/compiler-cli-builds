1e0d52f55a0ab98fa019fb51e6919fed818ad94f
Andrew Scott <atscott@google.com>
2025-09-08 21:02:35 +0000

    00a1806 refactor(forms): add validateStandardSchema to public api (#63616)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.2+sha-8e8a3fe");
+var VERSION = new Version("21.0.0-next.2+sha-00a1806");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
