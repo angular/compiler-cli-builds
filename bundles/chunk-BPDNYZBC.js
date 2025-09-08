020a2fb8c06f1a70770c234336732c4b0c0e6498
Andrew Scott <atscott@google.com>
2025-09-08 17:18:05 +0000

    e7fbee0 docs(docs-infra): quote title attributes in links (#63578)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-4f256d3";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-e7fbee0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
