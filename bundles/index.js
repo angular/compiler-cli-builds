e5b71ed66eff9b49c063f625a8e9c148201a7257
Andrew Scott <atscott@google.com>
2025-09-09 17:48:39 +0000

    6117cce feat(core): introduce `BootstrapContext` for improved server bootstrapping (#63636)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0-next.0+sha-90e798b");
+var VERSION = new Version("20.3.0-next.0+sha-6117cce");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
