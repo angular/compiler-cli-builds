4bee8cd5c73852df94cdca42aebcc75b00a74713
Andrew Scott <atscott@google.com>
2025-09-09 20:00:36 +0000

    9ffc83e docs: add signals tutorial (#62750)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-6d12f10";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-9ffc83e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
