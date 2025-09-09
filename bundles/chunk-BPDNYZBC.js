d99a80ba5ba20f907e0847b4745e4f7955f2f610
Andrew Scott <atscott@google.com>
2025-09-09 21:44:57 +0000

    c357650 refactor(core): Update tests for zoneless by default (#63668)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-0571b33";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-c357650";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
