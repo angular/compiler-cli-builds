69e54f72f3969c170de7b281fa3d0a6206a9fb5c
Andrew Scott <atscott@google.com>
2025-09-09 17:36:10 +0000

    90e798b build: prepare exceptional minor branch: 20.3.x

--- bundles/index.js
+++ bundles/index.js
@@ -28,7 +28,7 @@ import {
   isTsDiagnostic,
   performCompilation,
   readConfiguration
-} from "./chunk-6UJHDDQR.js";
+} from "./chunk-ES5JHT4S.js";
 import {
   ConsoleLogger,
   LogLevel
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0-next.0+sha-11a54d1");
+var VERSION = new Version("20.3.0-next.0+sha-90e798b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
