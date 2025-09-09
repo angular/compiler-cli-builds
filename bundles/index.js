254d2fa269f493ab14aa4281587da20b7a91f4ca
Andrew Scott <atscott@google.com>
2025-09-09 21:32:27 +0000

    739b091 docs: add missing component harness docs (#63620)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.2.4+sha-0214e98");
+var VERSION = new Version("20.2.4+sha-739b091");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
