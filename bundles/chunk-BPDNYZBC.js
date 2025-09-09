0286ee6fa70ed44457074ffd6664821ec677493c
Andrew Scott <atscott@google.com>
2025-09-09 19:13:08 +0000

    dce31fb docs: add missing drag & drop docs (#63619)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-28926ba";
+var PLACEHOLDER_VERSION = "21.0.0-next.2+sha-dce31fb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
