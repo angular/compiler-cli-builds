937eab7fb44e4c49441d8bc372e4c9396eac2a7f
Andrew Scott <atscott@google.com>
2025-09-09 19:13:45 +0000

    3fb7a12 docs: add missing drag & drop docs (#63619)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.0-next.0+sha-6117cce";
+export declare const PLACEHOLDER_VERSION = "20.3.0-next.0+sha-3fb7a12";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
