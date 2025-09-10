de3e1eddd4a6656d6602782444af6858cb885c84
Andrew Scott <atscott@google.com>
2025-09-10 14:32:12 +0000

    08f01ac build: address issues with bazelrc file (#63684)

--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.13+sha-32512de");
+var VERSION = new Version("18.2.13+sha-08f01ac");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
