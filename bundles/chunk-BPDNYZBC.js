b68f493819c3892f43768f94cbd7e621788f6708
Andrew Scott <atscott@google.com>
2025-09-10 18:18:22 +0000

    fde699b docs: release notes for the v19.2.15 release (#63699)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-9487dc6";
+var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-fde699b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
