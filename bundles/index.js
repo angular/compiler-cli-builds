49a535507f132c67c214e943eb58622a7563dfd3
Andrew Scott <atscott01@gmail.com>
2025-09-10 14:59:55 +0000

    34c5a03 release: cut the v20.3.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0-next.0+sha-7a7725e");
+var VERSION = new Version("20.3.0-rc.0+sha-34c5a03");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
