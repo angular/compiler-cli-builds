468b768a65da6df094c7687ebe910d31b7f432b9
Andrew Scott <atscott01@gmail.com>
2025-09-10 18:54:28 +0000

    98db109 release: cut the v18.2.14 release

--- bundles/chunk-Z5GU2OTU.js
+++ bundles/chunk-Z5GU2OTU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.13+sha-1d86de1";
+var PLACEHOLDER_VERSION = "18.2.14+sha-98db109";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.13+sha-1d86de1");
+var VERSION = new Version("18.2.14+sha-98db109");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
