a7fbed5e8cf02405e7dd31dd73d9c9236a2a1962
Andrew Scott <atscott@google.com>
2025-09-10 18:47:47 +0000

    1d86de1 build: ignore node_modules (#63701)

--- bundles/chunk-Z5GU2OTU.js
+++ bundles/chunk-Z5GU2OTU.js
@@ -299,7 +299,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/linker/src/file_linker/partial_linkers/util.mjs
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
-var PLACEHOLDER_VERSION = "18.2.13+sha-08f01ac";
+var PLACEHOLDER_VERSION = "18.2.13+sha-1d86de1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -79,7 +79,7 @@ import "./chunk-XI2RTGAL.js";
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/version.mjs
 import { Version } from "@angular/compiler";
-var VERSION = new Version("18.2.13+sha-08f01ac");
+var VERSION = new Version("18.2.13+sha-1d86de1");
 
 // bazel-out/k8-fastbuild/bin/packages/compiler-cli/src/ngtsc/tsc_plugin.mjs
 var NgTscPlugin = class {
