0f23138529dcc672c74da0a577d6ef343584c30a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:57:15 +0000

    663f48c refactor(core): Split consumerBefore/AfterComputation (#62549)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-a8fae2a");
+var VERSION = new Version("21.0.0-next.3+sha-663f48c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
