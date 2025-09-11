4e66a686ea3d65c4ddc3123888ade28d37baa9cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:43:54 +0000

    c36cd73 ci: correctly set branchName for the CDK (#63718)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-32ff9eb");
+var VERSION = new Version("20.3.0+sha-c36cd73");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
