60c0d331dcfcd5782f0c4c0e217fc974e96edf5e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 16:59:05 +0000

    bd54106 refactor(router): update error checks extending error (#63487)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-a53a15c");
+var VERSION = new Version("21.0.0-next.3+sha-bd54106");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
