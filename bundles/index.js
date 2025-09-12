d4e69e293da2bc2338d300a81002f8718f45af38
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:48:43 +0000

    85994fb build: update branches and versioning information (#63772)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-62ad4db");
+var VERSION = new Version("21.0.0-next.3+sha-85994fb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
