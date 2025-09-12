b1c80a8a2efc1967cf4ee06bcec9bef2da3e80c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:08:12 +0000

    9de50b1 docs(docs-infra): move the navigation related files (#63729)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-895f0fb");
+var VERSION = new Version("20.3.0+sha-9de50b1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
