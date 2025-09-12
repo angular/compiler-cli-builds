a7571ec5824770eb725e976612363bd96f6fd705
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:54:24 +0000

    895f0fb docs: update Creating service with CLI (#63714)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-3ec8a5c");
+var VERSION = new Version("20.3.0+sha-895f0fb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
