5cd9d7ed02abecb07cd11a793b7c436161aab08f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:26:55 +0000

    3ec8a5c fix(core): Prevent leave animations on a move operation (#63745)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-4ee3993");
+var VERSION = new Version("20.3.0+sha-3ec8a5c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
