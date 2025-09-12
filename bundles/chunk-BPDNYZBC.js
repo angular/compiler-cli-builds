74a87f8cd9dce1add7dfc114eaf0c308d73c410c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:52:40 +0000

    f8cea65 refactor(devtools): property-tab folder structure (#63759)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-5fd6022";
+var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-f8cea65";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
