9db6738da4e6100f4ff130a8d567809ce32d04e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:44:46 +0000

    62ad4db refactor(core): Reorganize and cleanup animations code (#63775)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-bd2a84b");
+var VERSION = new Version("21.0.0-next.3+sha-62ad4db");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
