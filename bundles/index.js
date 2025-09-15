66e28bd0659a66f919d2b61ef09a4b13e0aa0053
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:38:06 +0000

    653089d test(platform-server): add test for transfer state data in nested defer blocks (#63801)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-13e4255");
+var VERSION = new Version("20.3.0+sha-653089d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
