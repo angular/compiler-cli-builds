3a1837b4537fecf777917455df12b30725f64f13
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:07:03 +0000

    cd36189 ci: update pullapprove public-api rules (#63737)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.0+sha-38c9921";
+var PLACEHOLDER_VERSION = "20.3.0+sha-cd36189";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
