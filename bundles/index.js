932a2e415b4ca2ba0b8ae23f849f90ec3d433c88
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:45:03 +0000

    cfe0f03 refactor(devtools): add a close button to the injector-provides pane (#63808)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-d2163f3");
+var VERSION = new Version("21.0.0-next.3+sha-cfe0f03");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-d2163f3";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-cfe0f03";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
