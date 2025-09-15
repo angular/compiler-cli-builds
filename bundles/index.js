ccfb2a8b6394668471d63be2cae38ecaaa65811d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:28:40 +0000

    420703f fix(docs-infra): fix guide content width (#63798)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-2b440f4");
+var VERSION = new Version("21.0.0-next.3+sha-420703f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
