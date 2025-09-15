e3687b9b58d57ba7f86a822e4ee0760dd90995a6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:28:59 +0000

    13e4255 fix(docs-infra): fix guide content width (#63798)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.0+sha-c536035";
+var PLACEHOLDER_VERSION = "20.3.0+sha-13e4255";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
