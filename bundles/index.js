ba7a56882cacddf6cfd5eaeba11d102cf6c760d3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:49:35 +0000

    233094e docs: fix detail about `linkedSignal`'s previous value. (#63811)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-b05552f");
+var VERSION = new Version("20.3.0+sha-233094e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
