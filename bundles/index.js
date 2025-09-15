fdb74aad2040e62ea0b3603ba2658a7a07a3213a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:12:53 +0000

    2473931 docs: Update outputs.md to demonstrate how to use event data (#63732) PR Close #63732

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-de8be97");
+var VERSION = new Version("21.0.0-next.3+sha-2473931");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
