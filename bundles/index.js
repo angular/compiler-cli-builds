bac8df4ebf09b7635e9f716358639e3fd4e22081
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:50:58 +0000

    7fb5a80 fix(compiler): Add support for `aria-invalid` (#63748)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-a2ea2ec");
+var VERSION = new Version("20.3.0+sha-7fb5a80");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
