ba560e0050a72280c00b145a1617c00e7e18d823
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 22:20:28 +0000

    0d028e0 refactor(platform-browser): Remove zonejs compatibility detector (#63847)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-1fec2a2");
+var VERSION = new Version("21.0.0-next.3+sha-0d028e0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
