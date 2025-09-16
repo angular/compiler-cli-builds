5b4001d88302481eae257f1c85c9576dc6ccb46c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 16:25:01 +0000

    049b672 docs(docs-infra): new home page (#63662)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-6e54bdf");
+var VERSION = new Version("20.3.0+sha-049b672");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
