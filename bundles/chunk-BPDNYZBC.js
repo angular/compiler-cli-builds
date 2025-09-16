3487c353c0e5b44174464de2a8220f451e1e4e96
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:21:28 +0000

    e43d9b9 docs(docs-infra): Use port 4201 for adev (#63834)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-a943e7c";
+var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-e43d9b9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-a943e7c");
+var VERSION = new Version("21.0.0-next.3+sha-e43d9b9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
