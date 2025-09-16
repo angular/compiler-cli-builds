b346bda5301f96d082e526c7101d15f1655526cc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:13:22 +0000

    27b2a4a refactor(core): Update async-test.ts to use globalThis polyfill (#57505)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-c80d9ba");
+var VERSION = new Version("20.3.0+sha-27b2a4a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
