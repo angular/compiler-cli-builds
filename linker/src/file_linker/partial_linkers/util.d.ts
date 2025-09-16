3103970507ffa7f6dd6334b3c0530015021d906c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 19:00:28 +0000

    d5e00f9 refactor(core): Make the zoneless-by-default change a flag flip (#63382)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-07b4c12";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-d5e00f9";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
