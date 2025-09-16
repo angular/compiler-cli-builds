d5003c825b3d0d9ba58d8e33de9a24973c03f131
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 18:57:17 +0000

    dd6e1c3 refactor(core): prevent input migration from introducing a breaking change (#63547)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-f612437");
+var VERSION = new Version("20.3.0+sha-dd6e1c3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
