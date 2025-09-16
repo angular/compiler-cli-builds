55b2908dc4fd88731535bf50937e26bb743a149c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 16:25:02 +0000

    d53ac26 docs(docs-infra): new home page (#63662)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-4133b08";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.3+sha-d53ac26";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
