fe66f5c725ed74b040548c2cd822f04ee5af9371
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 20:52:01 +0000

    1fec2a2 build: remove bazel flag usage for adev (#63845)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-1352fbd";
+var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-1fec2a2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
