80a9aa52547881035c171031c099820435884741
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:41:01 +0000

    79a818b build: update cross-repo angular dependencies (#63877)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.0+sha-8843707";
+var PLACEHOLDER_VERSION = "20.3.0+sha-79a818b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
