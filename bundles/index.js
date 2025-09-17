0d10131607d6f4fbfe50c0f8bbbf9a4c817d65eb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:04:04 +0000

    423cc17 refactor(common): trim undefined from locale data. (#63520)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.0+sha-1c7106a");
+var VERSION = new Version("20.3.0+sha-423cc17");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
