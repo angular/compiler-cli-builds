eec5506018a7f37d4a1e4238dc3eb38ced9550e8
Jan Martin <jan.krems@gmail.com>
2025-09-17 18:00:41 +0000

    7d3919e release: bump Angular DevTools version to 1.2.1 (#63879)

--- bundles/chunk-KZHANLEJ.js
+++ bundles/chunk-KZHANLEJ.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-dee6ef8";
+var PLACEHOLDER_VERSION = "21.0.0-next.3+sha-7d3919e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.3+sha-dee6ef8");
+var VERSION = new Version("21.0.0-next.3+sha-7d3919e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
