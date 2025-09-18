c26edbbf756f3627a5058e46f1f6ea0a0103293d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 15:02:46 +0000

    5cb1bbb build: update rules_sass digest to 4a54e0e (#63898)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-950ffd0";
+var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-5cb1bbb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
