39e3c18d1f1631c9c4ec65deb468bede954aff7a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:32:45 +0000

    b04e6b1 refactor(core): drop the usage of `PromiseSettledResult<void>` (#63955)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-de18875");
+var VERSION = new Version("20.3.1+sha-b04e6b1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
