371875ffbb5c86ea262b839abea4e21e8f5957a1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:54:27 +0000

    d0c7ea8 refactor(core): fix MockRenderer in unit tests. (#63936)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-fcec9a2");
+var VERSION = new Version("20.3.1+sha-d0c7ea8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
