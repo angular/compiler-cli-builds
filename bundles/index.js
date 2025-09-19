c210320a988d6dd52f17800d7913266a94c7a2f0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:40:54 +0000

    3c66209 ci: optimize adev CI workflows (#63941)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-ba40153");
+var VERSION = new Version("20.3.1+sha-3c66209");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
