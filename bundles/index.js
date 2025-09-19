a575bceba40d61d5a5921a279f29aa031fa09445
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:24:37 +0000

    e835c35 docs: Add categories to form apis (#63938)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.4+sha-0a4ad98");
+var VERSION = new Version("21.0.0-next.4+sha-e835c35");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
