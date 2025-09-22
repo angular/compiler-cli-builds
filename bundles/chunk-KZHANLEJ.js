49bb46ff12341b1503d3657c15784d8401c62d3e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:44:49 +0000

    a2d98cc docs: add missing `signal` function (#63974)

--- bundles/chunk-KZHANLEJ.js
+++ bundles/chunk-KZHANLEJ.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-e88f484";
+var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-a2d98cc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
