1b8c5a77538a9e456e60328b4b91a2b4f3be7d1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:45:02 +0000

    ca104b3 docs: add missing `signal` function (#63974)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-64a3675");
+var VERSION = new Version("20.3.1+sha-ca104b3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
