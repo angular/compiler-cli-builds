30b3aabd3e387ee4827b6a5340b6098c973b93a1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 14:25:34 +0000

    9515a70 fix(core): fix narrowing of `Resource.hasValue()` (#63994)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-2d07610");
+var VERSION = new Version("20.3.1+sha-9515a70");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
