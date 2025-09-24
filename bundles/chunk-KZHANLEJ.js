15ab776696c5a9e39009ffc5f59571bc7232de1d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-24 23:26:50 +0000

    b1d9373 docs: release notes for the v20.3.2 release

--- bundles/chunk-KZHANLEJ.js
+++ bundles/chunk-KZHANLEJ.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-057eff6";
+var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-b1d9373";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
