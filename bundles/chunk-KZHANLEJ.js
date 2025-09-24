40f1c19d805fba9a7d0011955cad78fe4495676e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:30:54 +0000

    090c0bb build: update dependency vsce to v2 (#64054)

--- bundles/chunk-KZHANLEJ.js
+++ bundles/chunk-KZHANLEJ.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-e8beaa4";
+var PLACEHOLDER_VERSION = "21.0.0-next.4+sha-090c0bb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
