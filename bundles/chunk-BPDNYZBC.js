c47c5459a98d47553011de74682f331c0ba825cd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 20:27:09 +0000

    f04a78a docs(docs-infra): scroll offset for headings (#64039)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.1+sha-ce6c7d9";
+var PLACEHOLDER_VERSION = "20.3.1+sha-f04a78a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
