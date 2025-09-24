a6a86915bbe657d553dffec2322e12a8c7407e4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:54:20 +0000

    7a7bc27 ci: use release config when deploying adev (#64030)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.4+sha-3a9b8d8");
+var VERSION = new Version("21.0.0-next.4+sha-7a7bc27");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
