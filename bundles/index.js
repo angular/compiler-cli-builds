fe882d5e34f5eb155943ebaa80611757d5ea61aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 18:34:48 +0000

    0b2018d docs(docs-infra): align docs-step number to the heading (#64035)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.4+sha-c0d88c3");
+var VERSION = new Version("21.0.0-next.4+sha-0b2018d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
