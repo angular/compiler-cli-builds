c71176e6eb9e52e014745411a05e0d4676a95631
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:28:28 +0000

    522587d docs: not external link (#64025)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.1+sha-71955dc");
+var VERSION = new Version("20.3.1+sha-522587d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
