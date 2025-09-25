bbf60bc0df84b7b4c1056dc572a775557b22e5f7
kirjs <kirts@google.com>
2025-09-25 18:19:14 +0000

    1dee0d3 build: add `.vscode` files back to the `.gitignore` (#64059)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-fe0e5ab");
+var VERSION = new Version("21.0.0-next.5+sha-1dee0d3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
