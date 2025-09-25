f440e7accfe32f04bb1badb169377c34be031a9c
kirjs <kirts@google.com>
2025-09-25 14:46:32 +0000

    9fd8d40 ci: authenticate to Bazel RBE (#64070)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-45e3f77");
+var VERSION = new Version("21.0.0-next.5+sha-9fd8d40");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
