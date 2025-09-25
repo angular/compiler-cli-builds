ade0886da6483c87e0a302a0e3da7bb9753ca7f5
kirjs <kirts@google.com>
2025-09-25 20:33:31 +0000

    e2fd79b docs: add warning to `AbstractControl.setErrors` (#64063)

--- bundles/chunk-KZHANLEJ.js
+++ bundles/chunk-KZHANLEJ.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.5+sha-6fee508";
+var PLACEHOLDER_VERSION = "21.0.0-next.5+sha-e2fd79b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
