4dd0ee25414bcbe73407a2255c58a4d42030d731
kirjs <kirts@google.com>
2025-09-25 19:02:05 +0000

    36c4b3f build: update all github actions (#64082)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.2+sha-542cd00";
+export declare const PLACEHOLDER_VERSION = "20.3.2+sha-36c4b3f";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
