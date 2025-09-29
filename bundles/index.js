e06c75e49ad2ce39afdcc5f8afeff720b3224201
kirjs <kirts@google.com>
2025-09-29 20:51:12 +0000

    3598855 docs(docs-infra): clean up code and use inferred types where possible (#64139)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-5c50e0b");
+var VERSION = new Version("20.3.2+sha-3598855");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
