ea8aeb0e4d520e2ab32f34af92a1f786025f0b05
kirjs <kirts@google.com>
2025-09-29 13:56:26 +0000

    e6c0d7e build: update github/codeql-action action to v3.30.5 (#64121)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-c0bb528");
+var VERSION = new Version("20.3.2+sha-e6c0d7e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
