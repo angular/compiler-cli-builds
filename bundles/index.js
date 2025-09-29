2fe229dfdbb846b90de30155e87230104cd7a38d
kirjs <kirts@google.com>
2025-09-29 13:28:01 +0000

    4952110 docs: remove note about node.js 17+ (#64129)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-c680927");
+var VERSION = new Version("20.3.2+sha-4952110");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
