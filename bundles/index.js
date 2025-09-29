5aa8af9f33d460b93abaebb430104a53e6876d4c
kirjs <kirts@google.com>
2025-09-29 17:14:22 +0000

    824bab5 build: update cross-repo angular dependencies (#64093)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-78cee8e");
+var VERSION = new Version("21.0.0-next.5+sha-824bab5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
