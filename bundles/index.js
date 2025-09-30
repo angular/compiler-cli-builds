70babe2da34de23a0dd507769dea0864d18b58fd
kirjs <kirts@google.com>
2025-09-30 19:36:18 +0000

    31bc9e4 fix(migrations): skip migration for inputs with 'this' references (#64142)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-c3a949a");
+var VERSION = new Version("21.0.0-next.5+sha-31bc9e4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
