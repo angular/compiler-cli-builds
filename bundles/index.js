dfb8696d20fc218455130ca8f1849cc1a5d13fea
kirjs <kirts@google.com>
2025-09-30 20:07:25 +0000

    159be56 fix(compiler): recover template literals with broken expressions (#64150)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-31bc9e4");
+var VERSION = new Version("21.0.0-next.5+sha-159be56");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
