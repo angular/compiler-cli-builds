6adb79a6cad65a0e8dfcc19d6d40dcf376656dff
kirjs <kirts@google.com>
2025-10-01 15:57:08 +0000

    1b102c2 build: update dependency @types/jsdom to v27 (#64165)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-2f6caef");
+var VERSION = new Version("21.0.0-next.5+sha-1b102c2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
