dd2d99e6ec3ee6cd7ce21e251c65994931cff6d0
kirjs <kirts@google.com>
2025-10-01 13:06:13 +0000

    24c979d docs: add allowed-host mention (#64145)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-f51ab32");
+var VERSION = new Version("20.3.2+sha-24c979d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
