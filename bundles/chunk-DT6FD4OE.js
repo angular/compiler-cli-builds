5e75448807c6b9d376a8ce17f1a42963c3158a06
kirjs <kirts@google.com>
2025-10-01 17:17:14 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.5+sha-0537083";
+var PLACEHOLDER_VERSION = "21.0.0-next.5+sha-ee7d49a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
