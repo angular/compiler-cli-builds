43d469a21ce61dda2a44816ab58d4d11f49139df
kirjs <kirts@google.com>
2025-10-01 14:49:33 +0000

    cc8f5e1 build: update ossf/scorecard-action action to v2.4.3 (#64166)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-0e928fb");
+var VERSION = new Version("20.3.2+sha-cc8f5e1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
