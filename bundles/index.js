518257c031024372a71f4922f8a52df1a9d8ead6
kirjs <kirts@google.com>
2025-10-01 16:48:32 +0000

    0537083 Revert "feat(common):  Support of optional keys for the KeyValue pipe (#48814)" (#64179)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-1b102c2");
+var VERSION = new Version("21.0.0-next.5+sha-0537083");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
