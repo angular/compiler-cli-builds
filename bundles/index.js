12b85f38e6fcdb6ffcb3727703c1ca5567ff2ca9
Miles Malerba <mmalerba@google.com>
2025-10-01 22:45:50 +0000

    d892aab Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.5+sha-be0455a");
+var VERSION = new Version("21.0.0-next.5+sha-d892aab");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
