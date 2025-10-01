c753d4c3a1a22df09d7af4acc5d15fa13995f996
kirjs <kirts@google.com>
2025-10-01 13:14:15 +0000

    0e928fb fix(core): Fixes animations in conjunction with content projection (#63776)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-24c979d");
+var VERSION = new Version("20.3.2+sha-0e928fb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
