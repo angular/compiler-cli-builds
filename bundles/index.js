fc98968d8ebfe095c5d3c5dc4d945406566ec970
Kirill Cherkashin <kirill@kcherkashin.org>
2025-10-02 12:11:00 +0000

    c314ccb release: cut the v20.3.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.2+sha-a453b1d");
+var VERSION = new Version("20.3.3+sha-c314ccb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
