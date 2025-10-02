b790051a7412d1d0d149b01ff799e6a6e9550146
Miles Malerba <mmalerba@google.com>
2025-10-02 20:28:21 +0000

    4704863 build: update peter-evans/create-or-update-comment action to v5 (#64211)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.6+sha-8146fc2");
+var VERSION = new Version("21.0.0-next.6+sha-4704863");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
