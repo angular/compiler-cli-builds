50c9147198631d8c6899669e356b33a1ede058b4
Miles Malerba <mmalerba@google.com>
2025-10-02 15:04:15 +0000

    b687d9c docs: update website URL in license banners  (#64183)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.3+sha-c314ccb");
+var VERSION = new Version("20.3.3+sha-b687d9c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
