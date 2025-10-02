51051bfd7c781786951d4f1bad4193873efd7061
Miles Malerba <mmalerba@google.com>
2025-10-02 15:17:54 +0000

    76fe559 fix(core): handle undefined CSS time values in parseCssTimeUnitsToMs function (#64181)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.3+sha-b687d9c";
+var PLACEHOLDER_VERSION = "20.3.3+sha-76fe559";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
