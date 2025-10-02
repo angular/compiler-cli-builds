bb529e74f6a21b443ebdef6d9bcc3e7e1a52313f
Miles Malerba <mmalerba@google.com>
2025-10-02 14:57:13 +0000

    50866b6 build: update all non-major dependencies (#64191)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.6+sha-6169fa5");
+var VERSION = new Version("21.0.0-next.6+sha-50866b6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
