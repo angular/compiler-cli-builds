6e296a7524841512a9b35f9a7633d949cab8e48e
Miles Malerba <mmalerba@google.com>
2025-10-03 20:31:43 +0000

    548ea02 fix(core): ensure missing leave animations don't queue leave animations (#64226)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.6+sha-ab780a7");
+var VERSION = new Version("21.0.0-next.6+sha-548ea02");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
