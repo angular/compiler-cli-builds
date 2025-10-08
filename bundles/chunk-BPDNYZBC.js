a486f0b8a09c8cbcc16c3cd2b9beb1b43564cfcf
Miles Malerba <mmalerba@google.com>
2025-10-08 14:53:39 +0000

    28b0d53 release: cut the v20.3.4 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.3+sha-11d83d1";
+var PLACEHOLDER_VERSION = "20.3.4+sha-28b0d53";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
