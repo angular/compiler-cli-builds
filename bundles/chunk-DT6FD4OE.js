b49f865da10a6c95069c4e168241a47e0a778a51
Miles Malerba <mmalerba@google.com>
2025-10-08 00:28:04 +0000

    a15688c build: update pnpm to v10.18.1 (#64264)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.6+sha-ac0eb23";
+var PLACEHOLDER_VERSION = "21.0.0-next.6+sha-a15688c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
