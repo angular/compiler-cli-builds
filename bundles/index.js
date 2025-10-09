ccf609592d35eb4d0c1fdaaab194e9732c2b3bb3
Andrew Kushnir <akushnir@google.com>
2025-10-09 03:11:52 +0000

    e873c22 build: clean up language service integration tests and Bazel dependencies (#64271)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-808d6ae");
+var VERSION = new Version("21.0.0-next.7+sha-e873c22");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
