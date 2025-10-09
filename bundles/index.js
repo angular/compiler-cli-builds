43d2cbb357d2f9bb18346a460bc3fc1bfee8d6fc
Andrew Kushnir <akushnir@google.com>
2025-10-09 12:29:20 +0000

    f521c1f build: update github/codeql-action action to v3.30.7 (#64284)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-e873c22");
+var VERSION = new Version("21.0.0-next.7+sha-f521c1f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
