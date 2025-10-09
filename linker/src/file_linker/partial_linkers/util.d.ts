8ac67060f4f1f211d134f71ccd4f06056c9e261d
Andrew Kushnir <akushnir@google.com>
2025-10-09 18:16:31 +0000

    6e4bcc7 fix(router): Scroll restoration should use instant scroll behavior for traversals (#64299)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.4+sha-fa25dad";
+export declare const PLACEHOLDER_VERSION = "20.3.4+sha-6e4bcc7";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
