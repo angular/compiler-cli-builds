6c2b8009644ef2213bf675a484957d744131e57d
Andrew Kushnir <akushnir@google.com>
2025-10-10 15:17:16 +0000

    2ecbc4e docs: add a callout that adding/removing to formArray doesn't not mark dirty (#64337)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.7+sha-249fbb3";
+var PLACEHOLDER_VERSION = "21.0.0-next.7+sha-2ecbc4e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
