76f3d37d066dd2ce74025829737d0852fa358bef
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:32:05 +0000

    09a927a ci: remove `cache-node-modules` from `checkout-and-setup-node` (#64387)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.7+sha-62cda78";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.7+sha-09a927a";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
