f42cf0e9a0a05110cd8d6c808c9822b39a4e39af
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:01:31 +0000

    c0510cd docs: Removes standalone flag from drag-drop examples (#64395)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-2a7a5de");
+var VERSION = new Version("21.0.0-next.7+sha-c0510cd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
