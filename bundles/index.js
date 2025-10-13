69dbbe216e6b20c2411f2038989a03b3c0351c26
Andrew Kushnir <akushnir@google.com>
2025-10-13 20:08:56 +0000

    32f86d3 perf(forms): optimize `[field]` binding instructions (#64351)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-6a96844");
+var VERSION = new Version("21.0.0-next.7+sha-32f86d3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
