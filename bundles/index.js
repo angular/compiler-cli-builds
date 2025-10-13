65a18e444e32b74ee25020057b4353f138f44f56
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:32:37 +0000

    51066cc ci: remove `cache-node-modules` from `checkout-and-setup-node` (#64387)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.4+sha-b4b19b6");
+var VERSION = new Version("20.3.4+sha-51066cc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
