c8ab6d610ebe6cca76b5d48191c907b72a2eec59
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:05:19 +0000

    b4b19b6 refactor(compiler): Improve pipe in track expression error (#64321)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.4+sha-9cbe5b6";
+export declare const PLACEHOLDER_VERSION = "20.3.4+sha-b4b19b6";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
