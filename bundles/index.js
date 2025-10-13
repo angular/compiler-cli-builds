80333b1247aac58f41246950a272fef0301722cd
Andrew Kushnir <akushnir@google.com>
2025-10-13 17:04:38 +0000

    eee8eab docs: improve clarity of self-closing tag migration example (#64388)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-09a927a");
+var VERSION = new Version("21.0.0-next.7+sha-eee8eab");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
