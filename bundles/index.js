781fa0537d6382fab0ce58d00a2aba0c7417716b
Andrew Kushnir <akushnir@google.com>
2025-10-14 19:01:40 +0000

    5b0363c refactor(core):  warning when `hydration` trigger is used without hydration being enabled (#64185)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.4+sha-11b006e");
+var VERSION = new Version("20.3.4+sha-5b0363c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
