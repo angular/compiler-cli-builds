6cc976b6fbb67a409cccd6f2d80f24c73c8832b3
Andrew Kushnir <akushnir@google.com>
2025-10-14 16:26:32 +0000

    1cb1fff docs: Add mentions of the `autoCsp` option (#64290)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.4+sha-a2773b5";
+export declare const PLACEHOLDER_VERSION = "20.3.4+sha-1cb1fff";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
