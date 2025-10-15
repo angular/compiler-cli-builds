2bb7c608e5c06fca91b86a66e76d29061dd8b639
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:19:37 +0000

    013effd release: cut the v21.0.0-next.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.7+sha-e3fa24d");
+var VERSION = new Version("21.0.0-next.8+sha-013effd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
