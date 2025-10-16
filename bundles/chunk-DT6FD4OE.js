c8a2aa446db92b7fba9069208fce0b4f26fc7d2b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:45:56 +0000

    6af231e refactor(devtools): Replace console warnings with snackbar notifications (#64458)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-a7ba2f5";
+var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-6af231e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
