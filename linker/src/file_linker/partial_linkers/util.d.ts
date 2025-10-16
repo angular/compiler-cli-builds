bf1d9d77a7bb083ad34762ecf8497fdbaff4ab31
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 18:59:29 +0000

    95e1632 refactor(platform-browser): remove unecessary export. (#64469)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-next.8+sha-b7c47d8";
+export declare const PLACEHOLDER_VERSION = "21.0.0-next.8+sha-95e1632";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
