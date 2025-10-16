6e529d087d924a7f653c99fe5bd3650fa1356f1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:58:16 +0000

    2d159af docs: Add docs for EVENT_MANAGER_PLUGINS (#64453)

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.5+sha-136e3de";
+var PLACEHOLDER_VERSION = "20.3.5+sha-2d159af";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
