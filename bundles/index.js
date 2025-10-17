76cca668f128f5dfca24845b8ee6c0553b19a16e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 18:29:30 +0000

    891f180 fix(compiler): correctly compile long numeric HTML entities (#64297)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.6+sha-0f81ef4");
+var VERSION = new Version("20.3.6+sha-891f180");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
