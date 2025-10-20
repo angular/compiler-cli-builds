a5d0fb4a448d0d37f9f31135b2bb22312a9d707a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 16:07:15 +0000

    e4e6b92 build: lock file maintenance (#64534)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.6+sha-4073dc5");
+var VERSION = new Version("20.3.6+sha-e4e6b92");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
