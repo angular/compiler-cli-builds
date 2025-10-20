d92a9eabf99a9a273edaaaedf023f2bf2d42cb98
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 17:14:54 +0000

    25d45ba refactor(router): Use promise array for loadComponent (#64450)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.8+sha-fad6e13");
+var VERSION = new Version("21.0.0-next.8+sha-25d45ba");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
