8fcff6a311696f7144b26582087c2937005a1e9e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 18:46:02 +0000

    f6a73f1 fix(router): Respect custom `UrlSerializer` handling of query parameters (#64449)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.8+sha-7fb7511");
+var VERSION = new Version("21.0.0-next.8+sha-f6a73f1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
