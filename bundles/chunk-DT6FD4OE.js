2d8d5d7b7505a0c5952277b8007fbfdbf4aeb157
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 16:53:06 +0000

    2b257b3 fix(animations): account for `Element.animate` exceptions (#64506)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-196fa50";
+var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-2b257b3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
