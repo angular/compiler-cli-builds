0a2e4ea6d2bdc900f2254d7d4affcf5b85059cd3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 18:13:01 +0000

    70332b0 fix(core): pass element removal property through in all locations (#64565)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.8+sha-fd9af2a");
+var VERSION = new Version("21.0.0-next.8+sha-70332b0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
