d2c8f2823c2e6e4e01b6b1ac9d80d2489956c690
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 16:40:13 +0000

    9d48e53 Revert "feat(router): allow router outlet to be set on ng-container (#64562)" (#64584)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.8+sha-9fef846");
+var VERSION = new Version("21.0.0-next.8+sha-9d48e53");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
