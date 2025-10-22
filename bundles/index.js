6c9ea85f4e9f0f3125766a8831b9b69025c6d451
Andrew Scott <atscott01@gmail.com>
2025-10-22 23:30:11 +0000

    292b9e3 refactor(zone.js): Improve missing proxy zone error for jest imported (#64497) test functions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.9+sha-de2ee6e");
+var VERSION = new Version("21.0.0-next.9+sha-292b9e3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
