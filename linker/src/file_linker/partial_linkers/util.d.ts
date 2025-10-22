3913533b84893bab5261983765588f5a6112bb1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:18:34 +0000

    c85be3b build: update cross-repo angular dependencies to v20.2.10 (#64591)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.6+sha-b05d8d9";
+export declare const PLACEHOLDER_VERSION = "20.3.6+sha-c85be3b";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
