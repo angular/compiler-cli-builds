1ce6aab8bd388679643246cb1618fa3c94b45925
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:31:41 +0000

    a3e91b2 refactor(forms): Add a test better documenting behavior of the error function in built-in validators (#63993)

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-e039c6b";
+var PLACEHOLDER_VERSION = "21.0.0-next.8+sha-a3e91b2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
