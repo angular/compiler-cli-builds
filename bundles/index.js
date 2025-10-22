5ee8efb3ef2291016c1fe6852cced74e0d761724
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-10-22 21:28:55 +0000

    2b51a83 release: cut the v21.0.0-next.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.8+sha-fcf7eee");
+var VERSION = new Version("21.0.0-next.9+sha-2b51a83");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
