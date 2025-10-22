bb85081034f71d38895d2e4652e954477d28db39
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-10-22 21:09:44 +0000

    fbb0056 release: cut the v20.3.7 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.6+sha-2fad4d4");
+var VERSION = new Version("20.3.7+sha-fbb0056");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
