fbdadb99b0ff657dc3eda3a278e9a00112bed19d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 17:07:18 +0000

    e9f55a8 Revert "build: refactor location of best-practices.md"

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-next.9+sha-5099ed4";
+var PLACEHOLDER_VERSION = "21.0.0-next.9+sha-e9f55a8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
