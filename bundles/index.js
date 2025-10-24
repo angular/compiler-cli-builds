d6eee2971825de8d8647dd25ef39241c6a05f3e3
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 17:06:16 +0000

    620e2c4 Revert "build: refactor location of best-practices.md"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-d7e5aea");
+var VERSION = new Version("21.1.0-next.0+sha-620e2c4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
