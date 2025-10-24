855ae6c3c0185897bc4fe09a2f87ef66bfdd880f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 16:54:14 +0000

    07d91d7 docs(docs-infra): Support linking to individual playground templates

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.7+sha-b97a677");
+var VERSION = new Version("20.3.7+sha-07d91d7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
