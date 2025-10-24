df0277028cc51a045581af96fae802fc9ec29a44
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 19:32:05 +0000

    9e8b4eb fix(docs-infra): fix broken adev tests

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.9+sha-e9f55a8");
+var VERSION = new Version("21.0.0-next.9+sha-9e8b4eb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
