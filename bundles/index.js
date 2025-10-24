cfc0f2cd19226f4d5581f0ce09862ef847766107
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 07:42:02 +0000

    0d38cbf docs: update Angular CDK apis (#64643)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-e47ef3e");
+var VERSION = new Version("21.1.0-next.0+sha-0d38cbf");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
