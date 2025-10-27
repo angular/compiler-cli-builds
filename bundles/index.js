e11641a3995eca1e152296533a4b61046a8c5924
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:26:20 +0000

    8511759 build: update rules_angular digest to 1f1aa3e

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.9+sha-04dd75b");
+var VERSION = new Version("21.0.0-next.9+sha-8511759");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
