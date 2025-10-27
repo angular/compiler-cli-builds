5d3b66d78499ea6494745166209673c7fe01bce8
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 18:48:51 +0000

    ca3fd78 fix(core): Clear lView from IcuIteratorState when stack is empty to prevent memory leak

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.7+sha-14b5590";
+var PLACEHOLDER_VERSION = "20.3.7+sha-ca3fd78";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
