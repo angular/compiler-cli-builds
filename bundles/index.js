3d9a97ea021bd3460740af9cd065c9086b7639e9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 15:59:42 +0000

    d1d5c72 docs(docs-infra): allow cors for json files

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-next.9+sha-4f21ea7");
+var VERSION = new Version("21.0.0-next.9+sha-d1d5c72");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
