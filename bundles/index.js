08cf150105c34b2afc9092b13a9d7bce44ebada3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 15:01:12 +0000

    766559e fix(docs-infra): update guide navigation to remain active with query params

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.7+sha-92d1897");
+var VERSION = new Version("20.3.7+sha-766559e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
