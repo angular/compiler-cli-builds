061c42d9517a85dd3de62496f83fe2fe673a54f5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 14:11:09 +0000

    92d1897 docs: align adev best practices with llm-eval

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.7+sha-f96df92");
+var VERSION = new Version("20.3.7+sha-92d1897");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
