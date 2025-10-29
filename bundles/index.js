4f073f5b4f2dd9c4339a5049e35066f656a341cc
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 13:14:37 +0000

    9123e3d fix(docs-infra): fix broken adev tests (#64761)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.7+sha-2edc82a");
+var VERSION = new Version("20.3.7+sha-9123e3d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
