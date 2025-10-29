7627b32bb27ee538b03b17e831b851494c6b369a
Andrew Scott <atscott01@gmail.com>
2025-10-29 20:36:08 +0000

    76adbbc refactor(core): expose isWritableSignal to the publicApi

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-662f0e5";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-76adbbc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
