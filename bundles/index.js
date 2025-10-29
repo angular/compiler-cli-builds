065bb8758764cbbec21497943c9f822ae80d9266
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-29 17:15:52 +0000

    77bc6ee release: cut the v20.3.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.7+sha-9123e3d");
+var VERSION = new Version("20.3.8+sha-77bc6ee");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
