dee05892f520c22f134d372fd9c9a05de58514bc
Andrew Scott <atscott@google.com>
2025-10-29 20:36:50 +0000

    20b4b2c refactor(core): expose isWritableSignal to the publicApi

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.0+sha-981fabc");
+var VERSION = new Version("21.0.0-rc.0+sha-20b4b2c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
