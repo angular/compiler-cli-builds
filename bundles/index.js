2fed7bb110bf04837e9cb1f7de26a992adf176d4
Andrew Scott <atscott@google.com>
2025-10-30 19:43:13 +0000

    136883a docs(devtools): Removes comment about enum deprecation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.9+sha-a832b1c");
+var VERSION = new Version("20.3.9+sha-136883a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
