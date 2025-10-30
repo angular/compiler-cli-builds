eb2141aa795b94717fc1d65a4269b6d7a2d1f31e
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:28:33 +0000

    58f921d docs: add essential signal forms guide (#64665)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.0+sha-90452f5");
+var VERSION = new Version("21.0.0-rc.0+sha-58f921d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
