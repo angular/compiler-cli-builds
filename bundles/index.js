b335d8d364db5a070e52653e938231d255222d83
Andrew Scott <atscott01@gmail.com>
2025-10-30 18:20:05 +0000

    29d6eba docs: update Angular Aria apis (#64782)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-56f26f1");
+var VERSION = new Version("21.1.0-next.0+sha-29d6eba");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
