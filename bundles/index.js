3094d006fa481e7d63e48f9a87fbfc46e23262b6
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:22:32 +0000

    26f4944 ci: refactor cross-repo documentation update scripts (#64792)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.0+sha-d23bb3c");
+var VERSION = new Version("21.0.0-rc.0+sha-26f4944");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
