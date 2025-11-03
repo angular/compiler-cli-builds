0af934569a9772e1e36efe3c5b924bd39c2f9504
Andrew Scott <atscott01@gmail.com>
2025-11-03 20:22:53 +0000

    32a166b build: update bazel dependencies

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-5d75d0e";
+var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-32a166b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
