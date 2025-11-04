435ba4ea5ffc38c9431140c069090996e8664ed9
Andrew Scott <atscott@google.com>
2025-11-04 00:30:25 +0000

    34f53aa docs: remove standalone: true since it's now the default

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.0+sha-a702c87");
+var VERSION = new Version("21.0.0-rc.0+sha-34f53aa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
