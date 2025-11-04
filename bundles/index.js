55ca3678a62bfdd80e29bc3fc2b8afa267a040a0
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:00:31 +0000

    7f16902 refactor(router): Update pre/post wildcard matching to retain 0 or more segment match meaning

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-8f49af9");
+var VERSION = new Version("21.1.0-next.0+sha-7f16902");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
