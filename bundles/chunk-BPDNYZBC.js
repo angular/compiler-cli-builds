f4415c52ba0bae1f005876a761cdca673e7b2034
Andrew Scott <atscott@google.com>
2025-11-04 23:29:00 +0000

    a45e6b2 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.9+sha-c374e80";
+var PLACEHOLDER_VERSION = "20.3.9+sha-a45e6b2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
