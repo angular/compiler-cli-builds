831d2a6d3a03478565a1fd51701edae22d919cf9
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:25:14 +0000

    192eb35 ci: update handle for jkrems/hybrist

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.9+sha-65c7805");
+var VERSION = new Version("20.3.9+sha-192eb35");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
