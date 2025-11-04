f6b8d86277501e8172a980adbaf5bfde20ee1ffb
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:43:53 +0000

    0a0fe44 feat(docs-infra): generate the sitemap for angular.dev

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "20.3.9+sha-dd7cb3d";
+export declare const PLACEHOLDER_VERSION = "20.3.9+sha-0a0fe44";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
