3a6203ec2b5e4a48de19388d66ce86a6b7bf7754
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:09:37 +0000

    dd7cb3d docs: update Angular CLI help

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.9+sha-1b6d0cf");
+var VERSION = new Version("20.3.9+sha-dd7cb3d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
