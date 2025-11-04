a97dc218565a29c7e25728e3ea82199f83a3dd9b
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:41:16 +0000

    b61847f docs: simplify tutorial signals with self-closing component tags

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-c4c7fb8");
+var VERSION = new Version("21.1.0-next.0+sha-b61847f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
