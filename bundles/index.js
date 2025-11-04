f3871320c89fceffbd0ab6969e41a6367d67eb8f
Andrew Scott <atscott@google.com>
2025-11-04 23:01:22 +0000

    c374e80 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.9+sha-43f7f41");
+var VERSION = new Version("20.3.9+sha-c374e80");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
