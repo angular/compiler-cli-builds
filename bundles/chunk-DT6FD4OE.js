430efb715f4ca41e8d74cf9cd79e571ea68fca5c
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:04:28 +0000

    a702c87 build: update cross-repo angular dependencies

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-6b3fdf3";
+var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-a702c87";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
