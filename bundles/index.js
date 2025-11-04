ffb71b50d527721ece7ec84291239f7f85ca0d8d
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:29:44 +0000

    5b210e9 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-c277214");
+var VERSION = new Version("21.1.0-next.0+sha-5b210e9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
