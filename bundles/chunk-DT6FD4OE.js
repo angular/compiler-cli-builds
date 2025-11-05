378ee33fadb495f274d167af197993a9b462c4f0
Andrew Scott <atscott01@gmail.com>
2025-11-05 19:32:41 +0000

    24f9d76 release: cut the v21.0.0-rc.1 release

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-01c5ba5";
+var PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-24f9d76";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
