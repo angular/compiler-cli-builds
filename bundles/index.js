0bc40efbd87d4cbf2011874237032ab0870ac5a6
Andrew Scott <atscott01@gmail.com>
2025-11-05 00:14:45 +0000

    41be02d perf(forms): implement change detection for field control bindings

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-5b210e9");
+var VERSION = new Version("21.1.0-next.0+sha-41be02d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
