8259d3edd646237497f69a1fd3d985ccb6b83a4a
Andrew Scott <atscott01@gmail.com>
2025-11-05 18:39:18 +0000

    098ed8c release: cut the v20.3.10 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.9+sha-22d190c");
+var VERSION = new Version("20.3.10+sha-098ed8c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
