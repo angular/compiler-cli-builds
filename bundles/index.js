9b75532a7ec4319c1e4e80fbca37fc652d9059bd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-05 22:32:16 +0000

    a54bea5 build: update all github actions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-e1dfe32");
+var VERSION = new Version("21.1.0-next.0+sha-a54bea5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
