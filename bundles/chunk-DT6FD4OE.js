bd58303d65925d66ca02437bb01d856c5acbe1aa
Andrew Scott <atscott@google.com>
2025-11-05 17:53:37 +0000

    01c5ba5 docs(docs-infra): fix heading layout on mobile

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-d3d2a34";
+var PLACEHOLDER_VERSION = "21.0.0-rc.0+sha-01c5ba5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
