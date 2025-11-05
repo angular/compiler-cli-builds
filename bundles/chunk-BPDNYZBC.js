b6c2db5f7f79c2dcec96c1f916d7919006946f3e
Andrew Scott <atscott@google.com>
2025-11-05 19:50:31 +0000

    ab94cd2 docs: Add the missing function call to use the signal's actual value

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.10+sha-098ed8c";
+var PLACEHOLDER_VERSION = "20.3.10+sha-ab94cd2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
