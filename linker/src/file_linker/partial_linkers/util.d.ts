d16e5df76550cdb7c806b4f978d1e0583ba2d662
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 16:44:56 +0000

    ca3ef38 refactor(common): Removes unused imports to clean up dependencies

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0-next.0+sha-e87f423";
+export declare const PLACEHOLDER_VERSION = "21.1.0-next.0+sha-ca3ef38";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
