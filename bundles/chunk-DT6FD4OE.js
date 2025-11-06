cd11d24e681d73a5f86523bafc777d142522a289
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:08:22 +0000

    a3c2fe8 Revert "refactor(common): Removes unused imports to clean up dependencies"

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-26995e2";
+var PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-a3c2fe8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.1+sha-26995e2");
+var VERSION = new Version("21.0.0-rc.1+sha-a3c2fe8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
