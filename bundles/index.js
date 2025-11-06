3553e31d3e5200431431774fa4fa3846e266f1ef
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 15:57:12 +0000

    26995e2 fix(docs-infra): properly expose the prerendered routes for sitemap generation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.1+sha-f0ffd9b");
+var VERSION = new Version("21.0.0-rc.1+sha-26995e2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
