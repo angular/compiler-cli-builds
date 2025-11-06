855fa71557537711043d9936c5994260f356c42a
GitHub <noreply@github.com>
2025-11-06 17:09:05 +0000

    25e13e9 build: add a dev-app

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-ca3ef38");
+var VERSION = new Version("21.1.0-next.0+sha-25e13e9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
