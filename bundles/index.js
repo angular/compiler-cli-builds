7e028872efd156bca514a3b3cf946ba3bc77b013
Andrew Kushnir <akushnir@google.com>
2025-11-07 19:40:13 +0000

    0f55443 refactor: exclude tsconfig.json from VSIX package contents (#64991)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-680c3c7");
+var VERSION = new Version("21.1.0-next.0+sha-0f55443");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
