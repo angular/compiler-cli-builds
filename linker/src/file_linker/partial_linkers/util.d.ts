9e708b8319f422d30867a1d111037d0ce3fec14b
Andrew Kushnir <akushnir@google.com>
2025-11-07 21:01:37 +0000

    647e640 refactor(core): Extract NavigateEvent cancel steps based on recent spec update

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-781a329";
+export declare const PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-647e640";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
