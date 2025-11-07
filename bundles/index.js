7a76b6b7bbb2eea2e427222894704ce3f4067997
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:28:30 +0000

    0552e72 docs(docs-infra): use self-closing syntax for component examples

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.1+sha-46c1a80");
+var VERSION = new Version("21.0.0-rc.1+sha-0552e72");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
