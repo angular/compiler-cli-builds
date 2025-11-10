215557e3a67cbe6406e0d47a45d0f1eb78001083
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:04:44 +0000

    b20bf5d docs: add shell language to CLI installation examples

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.10+sha-fedb083");
+var VERSION = new Version("20.3.10+sha-b20bf5d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
