f9e3578d592d661c6477c9edda69a9a5fb3de70c
Andrew Kushnir <akushnir@google.com>
2025-11-10 20:15:15 +0000

    5047849 fix(common): remove placeholder image listeners once view is removed

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.10+sha-2ad6b72";
+var PLACEHOLDER_VERSION = "20.3.10+sha-5047849";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
