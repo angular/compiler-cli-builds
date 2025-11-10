427046f10a605a7dffc2308e8003c5e34fd6181d
Andrew Kushnir <akushnir@google.com>
2025-11-10 17:58:17 +0000

    650af71 refactor(http): migrate XSRF classes to use inject() function

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-afa2ac2";
+var PLACEHOLDER_VERSION = "21.0.0-rc.1+sha-650af71";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
