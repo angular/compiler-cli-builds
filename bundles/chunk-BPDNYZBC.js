07cc679fd8151d0946b1df1bfbd857435e033f79
Andrew Kushnir <akushnir@google.com>
2025-11-11 16:36:42 +0000

    0baa28b docs: correct mermaid code block formatting

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.10+sha-e0407c0";
+var PLACEHOLDER_VERSION = "20.3.10+sha-0baa28b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
