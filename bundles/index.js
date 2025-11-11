e118c0223aaad8cf02f2948db6255d5a5052c3e6
Andrew Kushnir <akushnir@google.com>
2025-11-11 22:15:50 +0000

    c05cf41 refactor(core): render additional WAI-ARIA 1.3 property bindings as attributes

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.1+sha-7811d95");
+var VERSION = new Version("21.0.0-rc.1+sha-c05cf41");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
