873308f39983e3e846fa5fd73c844c430e5633d9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 16:39:21 +0000

    0770387 docs: correct mermaid code block formatting

--- bundles/chunk-DT6FD4OE.js
+++ bundles/chunk-DT6FD4OE.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-ec9dc94";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-0770387";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
