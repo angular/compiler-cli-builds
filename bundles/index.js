4e34a93d4fb8222a210d96c9c9b3ae90a8c3ac71
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 23:06:50 +0000

    31ca56a docs: Fixed mermaid diagram node label contrast in dark mode

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-be37129");
+var VERSION = new Version("21.0.0-rc.2+sha-31ca56a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
