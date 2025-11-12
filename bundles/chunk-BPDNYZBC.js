ad6be875c3d689f79de7886099c320875dad0490
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:51:58 +0000

    2e7623c docs: correct example for RouteReuseStrategy getRouteKey

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.11+sha-3737724";
+var PLACEHOLDER_VERSION = "20.3.11+sha-2e7623c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
