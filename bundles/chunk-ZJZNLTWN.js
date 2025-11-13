8cd66a3e6cc7165d3e44534718aed9efa534554e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:17:07 +0000

    4b0f208 ci: use hardcoded global approvers group

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-482e137";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-4b0f208";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
