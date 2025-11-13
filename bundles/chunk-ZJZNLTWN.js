4868d2924c341c173acfce8acbbfb439b92032e1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 17:22:56 +0000

    67fbd5f fix(core): SSR error in signal forms

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-31ca56a";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-67fbd5f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
