9370289c7b48d3d7ef63bebd3918005283ab769b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:49:45 +0000

    f689269 Revert "fix(compiler): support one additional level of nesting in :host()"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.11+sha-fdd267a");
+var VERSION = new Version("20.3.11+sha-f689269");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
