76d8efb2fe021f834ae2407364855680528101c8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 18:03:36 +0000

    06bc146 docs: adds guide references to router APIs

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.11+sha-3a8fb09";
+var PLACEHOLDER_VERSION = "20.3.11+sha-06bc146";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
