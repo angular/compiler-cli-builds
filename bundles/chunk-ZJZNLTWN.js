dcd40b5b2e4e388b0ce23909f1c8658d9775b7b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:08:15 +0000

    482e137 docs(docs-infra): improve theme picker accessibility and add animations

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-16d51f6";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-482e137";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-16d51f6");
+var VERSION = new Version("21.0.0-rc.2+sha-482e137");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
