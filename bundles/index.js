695f209641315e5e862aad1393bf45697554b1a6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:04:00 +0000

    fdd267a ci: use hardcoded global approvers group

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.11+sha-70da36f");
+var VERSION = new Version("20.3.11+sha-fdd267a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
