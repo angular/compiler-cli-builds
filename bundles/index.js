1b5119cabb0c2e3cc692d3cd404dc9423ddc7560
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 18:07:31 +0000

    16d51f6 refactor(core): flip ngDevMode condition for formatter

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-2406172");
+var VERSION = new Version("21.0.0-rc.2+sha-16d51f6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
