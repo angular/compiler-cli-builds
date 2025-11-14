7b029d9090de7a1d149aac45924ce8830c4ff020
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 17:44:15 +0000

    1464d02 docs(docs-infra): backport docs changes from `21.0.x`

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-fc1ef79";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-1464d02";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
