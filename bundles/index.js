d089394baf5b8c374a70d5375a22111cbcb89964
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 00:01:25 +0000

    afeba79 docs(docs-infra): hide file tabs when code example is hidden

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-750da17");
+var VERSION = new Version("21.0.0-rc.2+sha-afeba79");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
