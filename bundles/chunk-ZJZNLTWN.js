cf200c6f1d9fb0c80c91e2e28167342eb2afd652
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 23:34:00 +0000

    a315afb docs: add formArray clear() example clarifies array manipulation

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-e1a7c35";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-a315afb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
