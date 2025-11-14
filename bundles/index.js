e0f6e8076eb6d0f535f2299e46eae06459016fd7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:57:47 +0000

    911999e docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-f550f4a");
+var VERSION = new Version("21.0.0-rc.2+sha-911999e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
