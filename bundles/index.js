953888b9c892e4734158b7b91711c7fe766a7e7d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:35:31 +0000

    e0a6dad docs: Uses the self-closing tag syntax

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-35e7b9a");
+var VERSION = new Version("20.3.12+sha-e0a6dad");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
