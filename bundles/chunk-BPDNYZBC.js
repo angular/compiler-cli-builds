b5ca58802f555d3d02341e464878792212160b4c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:16:09 +0000

    39f9e23 release: cut the v20.3.12 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.11+sha-01719a5";
+var PLACEHOLDER_VERSION = "20.3.12+sha-39f9e23";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.11+sha-01719a5");
+var VERSION = new Version("20.3.12+sha-39f9e23");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
