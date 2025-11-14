eabd93c3a6570557fd2eb7baf7707d14bdfc0154
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 00:00:48 +0000

    01719a5 docs(docs-infra): hide file tabs when code example is hidden

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.11+sha-f689269";
+var PLACEHOLDER_VERSION = "20.3.11+sha-01719a5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
