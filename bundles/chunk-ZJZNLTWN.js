e616f305a76aa302cc0930a0a03adf64a15b844b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:05:29 +0000

    c2a52fd docs: add aria toolbar component guide

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-afeba79";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-c2a52fd";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
