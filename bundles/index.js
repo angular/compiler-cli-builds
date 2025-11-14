59959db06d2b1b2c610b591843b96eff48f63e79
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 20:27:28 +0000

    3228cce docs(docs-infra): enable tabs layout

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-8a5b284");
+var VERSION = new Version("21.0.0-rc.2+sha-3228cce");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
