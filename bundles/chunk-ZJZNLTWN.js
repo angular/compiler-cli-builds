023b111f58fe679e222c699311b2d00dc4779529
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 19:30:30 +0000

    8a5b284 docs: add draft of aria component guides

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-966bfae";
+var PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-8a5b284";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
