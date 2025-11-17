a5c1b9f3d533c14f868209c07044a9467f0ff95c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:25:03 +0000

    a8cea23 docs: use the new format for the X account

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.12+sha-68c5556";
+var PLACEHOLDER_VERSION = "20.3.12+sha-a8cea23";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
