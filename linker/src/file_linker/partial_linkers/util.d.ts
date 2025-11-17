d8c5eec293a1eb1098accc4fd58b17a9ec91e7ca
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:35:45 +0000

    cc621ea ci: remove cache usage from pr.yml

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-a7dddcc";
+export declare const PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-cc621ea";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
