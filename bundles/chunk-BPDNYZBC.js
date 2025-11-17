4a7beb2e27f831b15ba83558e6708ef3be2c900f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:03:44 +0000

    f7fcef1 docs(docs-infra): simplify file headers in <docs-code> blocks

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.12+sha-e0a6dad";
+var PLACEHOLDER_VERSION = "20.3.12+sha-f7fcef1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-e0a6dad");
+var VERSION = new Version("20.3.12+sha-f7fcef1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
