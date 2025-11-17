7f24e40618f903950a123a143ceba687d8f783d5
GitHub <noreply@github.com>
2025-11-17 16:25:54 +0000

    73295b1 docs: use the new format for the X account

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-599b72e";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-73295b1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
