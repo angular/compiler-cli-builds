d4ad571ea975c4ba7957ef332be6b8a3ec03ddb3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:43:22 +0000

    a966ff1 refactor(core): let the profiler handle asymmetric events leniently

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.12+sha-4a1a703";
+var PLACEHOLDER_VERSION = "20.3.12+sha-a966ff1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
