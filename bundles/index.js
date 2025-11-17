107ecc363a65f9e2dd4f9cb44684c337a5163abf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:46:38 +0000

    755b360 docs: replace legacy <docs-code> with fenced code blocks with highlight metadata

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-2e00074");
+var VERSION = new Version("20.3.12+sha-755b360");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
