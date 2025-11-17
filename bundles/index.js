ed77437dc4106e89d60460bdb9e27a9fabc911d2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 21:42:14 +0000

    f279aab release: cut the v21.0.0-rc.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-e8f238a");
+var VERSION = new Version("21.0.0-rc.3+sha-f279aab");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0-rc.2+sha-e8f238a";
+export declare const PLACEHOLDER_VERSION = "21.0.0-rc.3+sha-f279aab";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
