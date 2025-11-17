55f2eb22757ce4f949653c873ac0d31804d27ab3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 21:44:10 +0000

    cbd77b9 docs: release notes for the v21.0.0-rc.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-3661ae1");
+var VERSION = new Version("21.1.0-next.0+sha-cbd77b9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
