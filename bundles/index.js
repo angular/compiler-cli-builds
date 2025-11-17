445ef2cca87ba4c469f3344e731dfe7a6a188e69
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 19:21:03 +0000

    6551b65 docs: update examples to latest Angular

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.2+sha-b7e9183");
+var VERSION = new Version("21.0.0-rc.2+sha-6551b65");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
