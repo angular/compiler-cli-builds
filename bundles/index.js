f0816750e2b2e8626f1dbf7100305e1a2d46e71f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 19:02:43 +0000

    5e15553 ci: remove cache usage from ci.yml

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-85777ef");
+var VERSION = new Version("20.3.12+sha-5e15553");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
