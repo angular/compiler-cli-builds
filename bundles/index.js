9c64b2f49016eb8b72ff454b02f3f85ae655a481
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:32:47 +0000

    4a1a703 docs(docs-infra): set max-height to code example

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-daae263");
+var VERSION = new Version("20.3.12+sha-4a1a703");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
