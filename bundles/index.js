490d029eced3194dea7b0587188e6327d26157aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:13:39 +0000

    820bb39 Revert "refactor(core): let the profiler handle asymmetric events leniently"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.12+sha-a966ff1");
+var VERSION = new Version("20.3.12+sha-820bb39");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
