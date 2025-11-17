ec68ad0d2158da1de99979d8f21d255d2f8a7ba3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 22:43:16 +0000

    81b6e45 docs(docs-infra): add advanced recommendations

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.12+sha-967e111";
+var PLACEHOLDER_VERSION = "20.3.12+sha-81b6e45";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
