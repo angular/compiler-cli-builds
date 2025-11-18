407f9e46c5ee456901335fd2bfa712720831e8ff
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 19:21:29 +0000

    e43b4da build: update cross-repo angular dependencies

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0-rc.3+sha-56ba2c8";
+var PLACEHOLDER_VERSION = "21.0.0-rc.3+sha-e43b4da";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
