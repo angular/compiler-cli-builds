da8d35f35305659fb9d3c4c70c54ea935b068ea9
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-18 16:33:14 +0000

    ed0647d refactor(devtools): add matcher and runGuardsAndResolvers support in router viewer

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-f799fc8";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-ed0647d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
