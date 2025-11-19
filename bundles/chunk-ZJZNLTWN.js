6653b9cc64227a4e84fe7a906a133a55683b8760
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 20:13:11 +0000

    28810e0 build: bump adev version

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-fe5c9a1";
+var PLACEHOLDER_VERSION = "21.0.0+sha-28810e0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
