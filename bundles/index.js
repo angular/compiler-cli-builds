e55eeedceab11726becf9cfe7c2bafa0b3827a64
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 16:18:01 +0000

    17b7d56 build: force angular cli to run in non-TTY mode for adev and devapp

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.3+sha-28c1a2d");
+var VERSION = new Version("21.0.0-rc.3+sha-17b7d56");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
