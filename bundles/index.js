55062ac9ae2a3e8c778abc42a4e8fbed814f9bcd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 20:09:34 +0000

    fe5c9a1 release: bump version of in memory web api

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-63a95c7");
+var VERSION = new Version("21.0.0+sha-fe5c9a1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
