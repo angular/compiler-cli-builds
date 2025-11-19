703ee53f1ba1c937077ea22558384fda7133b568
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 17:44:24 +0000

    edcbe22 release: cut the v20.3.13 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.12+sha-24c999a";
+var PLACEHOLDER_VERSION = "20.3.13+sha-edcbe22";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
