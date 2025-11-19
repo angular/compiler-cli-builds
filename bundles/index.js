1d4ac6d345a5058ebc672f9ced64854e281d8619
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:22:49 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0-rc.3+sha-17b7d56");
+var VERSION = new Version("21.0.0-rc.3+sha-e69855a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
