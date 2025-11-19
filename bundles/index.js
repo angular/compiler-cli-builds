8251dc2b185481286c39b15d0da6920f60a1354b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:56:58 +0000

    38f4ea1 docs: add signal forms validation guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-e9ba63a");
+var VERSION = new Version("21.1.0-next.0+sha-38f4ea1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
