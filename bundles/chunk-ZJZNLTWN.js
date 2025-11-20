8eff538f9290b92c429c1c0d3bc973e9795f307a
kirjs <kirts@google.com>
2025-11-20 20:04:23 +0000

    b9181f1 docs: prioritize ng add for Tailwind CSS and clarify manual steps

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-82c085d";
+var PLACEHOLDER_VERSION = "21.0.0+sha-b9181f1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
