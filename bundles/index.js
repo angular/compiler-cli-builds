587556bd5660216ce7236c86c383c20fdb1b0ed5
kirjs <kirts@google.com>
2025-11-20 18:31:22 +0000

    7219759 docs: add v21 event game

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-cd86de1");
+var VERSION = new Version("21.0.0+sha-7219759");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
