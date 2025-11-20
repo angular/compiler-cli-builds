0992f45ce1fd33e8745c3cd0d06285d7a56670ff
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 22:08:51 +0000

    1ab461c refactor(router): Store route injector on ActivatedRoute instance

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-2ecdb22");
+var VERSION = new Version("21.1.0-next.0+sha-1ab461c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
