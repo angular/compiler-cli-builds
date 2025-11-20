893ad34b1cb882852f1f863dd3564ab56bbdb71f
kirjs <kirts@google.com>
2025-11-20 21:46:16 +0000

    65c39ea docs(core): remove outdated regex template restriction in best-practices.md

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-b9181f1");
+var VERSION = new Version("21.0.0+sha-65c39ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
