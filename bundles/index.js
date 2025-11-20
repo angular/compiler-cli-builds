59cd99c15ac23e868f148089a9aaa426fdaa075b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 18:31:30 +0000

    b8fa9cc docs: add v21 event game

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-be6f2f0");
+var VERSION = new Version("21.1.0-next.0+sha-b8fa9cc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
