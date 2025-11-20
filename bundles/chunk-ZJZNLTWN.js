5f36d1830b167651f923dc156a399fa72b824a9d
kirjs <kirts@google.com>
2025-11-20 22:06:59 +0000

    70fb1d6 ci: try to resolve vscode extension e2e flakes

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-da97099";
+var PLACEHOLDER_VERSION = "21.0.0+sha-70fb1d6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
