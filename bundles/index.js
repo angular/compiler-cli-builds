dbe7093825db61a25b6b3ccb913b196adf9ee40e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:38:10 +0000

    38a354f docs: Adds signal type checking documentation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-f884c18");
+var VERSION = new Version("21.1.0-next.0+sha-38a354f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
