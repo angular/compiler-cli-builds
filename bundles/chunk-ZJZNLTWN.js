956b520096134d38d4ecb26b0f3a359c53f2b18a
kirjs <kirts@google.com>
2025-11-21 21:38:12 +0000

    a70a191 docs: Adds signal type checking documentation

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-5170fd4";
+var PLACEHOLDER_VERSION = "21.0.0+sha-a70a191";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
