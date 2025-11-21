8452afe1dc837dee4e92df618bd5e791a4cd22fc
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 20:46:02 +0000

    67d3c25 refactor(devtools): don't query defer nodes

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-88546ab");
+var VERSION = new Version("21.1.0-next.0+sha-67d3c25");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
