0d572d07f4f7ca2a4a971b67ac9658976ccc3609
kirjs <kirts@google.com>
2025-11-21 16:00:03 +0000

    1cdcd5a docs: fix incorrect Signal Forms link

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-053bf98");
+var VERSION = new Version("21.0.0+sha-1cdcd5a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
