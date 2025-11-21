9a59938ff4c732473ecf4d2f47f90b34f25adbe7
kirjs <kirts@google.com>
2025-11-21 17:29:37 +0000

    1dfce39 docs: move aria tabs into content organization category

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-bc34083");
+var VERSION = new Version("21.0.0+sha-1dfce39");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
