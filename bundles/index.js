0531711efd9fe2aee6e25f8a9d8082c85c7fa862
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:11:48 +0000

    48a05dd docs: fix v21 event header image

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-72b1f7f");
+var VERSION = new Version("21.1.0-next.0+sha-48a05dd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
