ee511f59150a3b12122c1147c44b4ece69ca424f
kirjs <kirts@google.com>
2025-11-24 20:33:59 +0000

    70507b8 fix(core): debug data causing memory leak for root effects

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.0+sha-4845a33");
+var VERSION = new Version("21.0.0+sha-70507b8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
