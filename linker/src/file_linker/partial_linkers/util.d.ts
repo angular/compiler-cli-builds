a187126a6d05e7024eef66ea7db654237c254969
kirjs <kirts@google.com>
2025-11-24 17:22:46 +0000

    f73cfd5 ci: adding myself as optional to the compiler scope

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.0+sha-9852033";
+export declare const PLACEHOLDER_VERSION = "21.0.0+sha-f73cfd5";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
