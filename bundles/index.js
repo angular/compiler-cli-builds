c7fda9ab77f670953bf02be4fcb63caf3b5e1a06
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:44:17 +0000

    a4f50bd fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-b26fe73");
+var VERSION = new Version("21.1.0-next.0+sha-a4f50bd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
