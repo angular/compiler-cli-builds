a70b612188b2d4265cd4797c3863aad90bacc5ba
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:58:08 +0000

    0276479 fix(http): prevent XSRF token leakage to protocol-relative URLs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.13+sha-edcbe22");
+var VERSION = new Version("20.3.13+sha-0276479");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
