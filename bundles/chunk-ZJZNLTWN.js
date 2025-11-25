890a9342c00648bcc606b7822eb99d0a35766129
kirjs <kirts@google.com>
2025-11-25 15:24:58 +0000

    477df38 docs: improve core package API documentation with additional reference links

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-8acf5d2";
+var PLACEHOLDER_VERSION = "21.0.0+sha-477df38";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
