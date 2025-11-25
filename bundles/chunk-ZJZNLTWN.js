d8cd13da02d1e6c392374926b6e16b8fa6e77bcc
GitHub <noreply@github.com>
2025-11-25 15:38:50 +0000

    c70e246 feat(forms): add DI option for classes on `Field` directive

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-67d41e9";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-c70e246";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
