a67cee0035b72eb8f92722299d19aa31867e191c
kirjs <kirts@google.com>
2025-11-25 15:55:07 +0000

    e3f5f34 refactor(forms): Make reset take value

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.0+sha-9aa3e92";
+var PLACEHOLDER_VERSION = "21.0.0+sha-e3f5f34";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
