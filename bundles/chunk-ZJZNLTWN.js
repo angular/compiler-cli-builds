fed7ca436b521a48d7298962096b4af1f9f9f8c3
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:29:16 +0000

    4c0ba06 release: cut the v21.1.0-next.0 release

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-6aaddba";
+var PLACEHOLDER_VERSION = "21.1.0-next.0+sha-4c0ba06";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
