74f2d0a87b9d041d88da44d90c34a88b08230b1e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:17:45 +0000

    136e923 release: cut the v20.3.14 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.13+sha-0276479");
+var VERSION = new Version("20.3.14+sha-136e923");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
