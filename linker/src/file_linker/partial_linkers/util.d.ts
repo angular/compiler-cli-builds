17f1689d9ebbb00c8645549fe05245ff25ad29eb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 16:43:38 +0000

    3b1fa82 fix(core): use injected `DOCUMENT` for `CSP_NONCE`

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0-next.0+sha-5bfa027";
+export declare const PLACEHOLDER_VERSION = "21.1.0-next.0+sha-3b1fa82";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
