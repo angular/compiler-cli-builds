1fc114028c4ae18b1c8a1183c3e0ee69a279a21f
kirjs <kirts@google.com>
2025-11-26 18:47:01 +0000

    4207d89 docs(docs-infra): fix visible lines/regions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.1+sha-1260d1a");
+var VERSION = new Version("21.0.1+sha-4207d89");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
