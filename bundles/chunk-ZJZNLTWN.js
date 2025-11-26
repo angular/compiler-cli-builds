541baedd179033af0cb6956b90b28977240b7a6f
kirjs <kirts@google.com>
2025-11-26 21:06:01 +0000

    fec1f1a docs: update code snippets in reference doocumentation

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.1+sha-99b6341";
+var PLACEHOLDER_VERSION = "21.0.1+sha-fec1f1a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
