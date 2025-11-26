33611aedbea3a9b91a04cbc1390fd1d6884d6ace
kirjs <kirts@google.com>
2025-11-26 20:51:41 +0000

    99b6341 docs(docs-infra): fix form custom control example uses syntax

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.1+sha-4207d89");
+var VERSION = new Version("21.0.1+sha-99b6341");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
