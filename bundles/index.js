4c371c4b7dbb58c516b757aee7d24abbf32ff54e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:54:44 +0000

    8c4a152 release: cut the v21.0.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.1+sha-78fd159");
+var VERSION = new Version("21.0.2+sha-8c4a152");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
