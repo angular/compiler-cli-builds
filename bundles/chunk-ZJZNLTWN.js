4a80e41e5d1fcb492add10257c94dfd686e9196e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:41:59 +0000

    893fbdf docs: update syntax highlighting in component documentation

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.2+sha-706d762";
+var PLACEHOLDER_VERSION = "21.0.2+sha-893fbdf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
