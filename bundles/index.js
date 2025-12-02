b466d8549f5d8bd48d789d872b39720a14a27378
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 15:50:08 +0000

    a0ad5d4 test(router): Add tests for support of path params before/after a wildcard

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-e30e61b");
+var VERSION = new Version("21.1.0-next.0+sha-a0ad5d4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
