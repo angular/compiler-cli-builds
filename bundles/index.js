d9e93c320aedc6d8651baa5e87fc00d5fee1b0de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 11:09:46 +0000

    c0e0010 fix(devtools): hide profiler node details when the frame is changed

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.2+sha-2935fef");
+var VERSION = new Version("21.0.2+sha-c0e0010");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
