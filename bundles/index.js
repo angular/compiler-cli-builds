9e471cf5d99703f5c4de5908d6d9d4c57c031307
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:51:24 +0000

    2935fef build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.2+sha-893fbdf");
+var VERSION = new Version("21.0.2+sha-2935fef");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
