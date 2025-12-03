9d96d4bf6b7022e28f6aac6ccc749fd5466b5222
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:58:04 +0000

    0f50ea8 docs: prevent showing typescript diagnostic errors until the node runtime reaches a ready state

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.2+sha-c112ef1");
+var VERSION = new Version("21.0.2+sha-0f50ea8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
