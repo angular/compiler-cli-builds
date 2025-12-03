6a753bf215494806fe915d5df1eaebcbc6fae800
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:28:05 +0000

    13c0d45 release: cut the v21.1.0-next.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-ffd7a9b");
+var VERSION = new Version("21.1.0-next.1+sha-13c0d45");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
