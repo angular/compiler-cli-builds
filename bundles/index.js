3270dc74a5049dd653006fccf77f715a4a7f4153
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:16:17 +0000

    42fbb64 build: update bazel dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.2+sha-8e837cc");
+var VERSION = new Version("21.0.2+sha-42fbb64");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
