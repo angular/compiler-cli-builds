c8397c0fa9e8ee21373efc524aa700f4bc900ee9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:23:59 +0000

    ffd7a9b docs: release notes for the v21.0.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.0+sha-7d1e502");
+var VERSION = new Version("21.1.0-next.0+sha-ffd7a9b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
