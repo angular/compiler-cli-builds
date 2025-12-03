68f6cf2de5c0a8c5251c4ac144ee3139df885722
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:09:35 +0000

    8e837cc docs: rename visibleRegion to region

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.2+sha-e95d2e3");
+var VERSION = new Version("21.0.2+sha-8e837cc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
