96c4685949e6a9e2a309fc02d517e7dd1bc2adae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:29:30 +0000

    eebcaf4 ci: use defined ng-dev config for deploying docs site

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.1+sha-7f8ffa3";
+var PLACEHOLDER_VERSION = "21.1.0-next.1+sha-eebcaf4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
