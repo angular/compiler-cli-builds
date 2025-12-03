f680f0972b3712dbbdb169b2932e5960cdb3d344
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 20:36:45 +0000

    565c022 build: disable assistant_to_the_branch_manager on forks

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.3+sha-34caae5";
+var PLACEHOLDER_VERSION = "21.0.3+sha-565c022";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-34caae5");
+var VERSION = new Version("21.0.3+sha-565c022");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
