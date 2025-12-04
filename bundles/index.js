bfb29098bc0bf16263a397a3991e029fc0bdd989
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-04 19:49:57 +0000

    28e7546 feat(docs-infra): forward preview error locations

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-c3bd22d");
+var VERSION = new Version("21.0.3+sha-28e7546");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
