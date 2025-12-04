561f15930d9a3862fb750041057b383a82ccf2ea
GitHub <noreply@github.com>
2025-12-04 19:38:23 +0000

    5be3304 refactor(forms): Break logic.ts into separate files

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.1+sha-b96f65a");
+var VERSION = new Version("21.1.0-next.1+sha-5be3304");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
