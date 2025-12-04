88c1fd826ba100be8c363588b31a4ea90c1582d3
GitHub <noreply@github.com>
2025-12-04 19:30:18 +0000

    c3bd22d build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-bf1c12c");
+var VERSION = new Version("21.0.3+sha-c3bd22d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
