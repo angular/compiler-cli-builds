a62c49d6d8907da4cc20544b3db108847c1c516f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:04:15 +0000

    7e1e396 docs: add documentation for `HTTP_TRANSFER_CACHE_ORIGIN_MAP`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.1+sha-28017c1");
+var VERSION = new Version("21.1.0-next.1+sha-7e1e396");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
