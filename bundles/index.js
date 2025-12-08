8778b13cb66950fea387c41f22d0f0ef9bab1969
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:28:15 +0000

    70e024e docs: add attribute binding reference in accessibility guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.1+sha-7e1e396");
+var VERSION = new Version("21.1.0-next.1+sha-70e024e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
