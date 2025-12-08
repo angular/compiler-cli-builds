9e2235538984df1ba7a433dbbee49081b80a3b5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 18:09:24 +0000

    253dc95 feat(docs-infra): add complexity level badge

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0-next.1+sha-dd47c57";
+export declare const PLACEHOLDER_VERSION = "21.1.0-next.1+sha-253dc95";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
