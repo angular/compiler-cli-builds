98e9d3c20275b631dde7781f9ba1a4e9eb86b579
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 16:28:34 +0000

    01ab257 refactor(core): match spec updates for FakeNavigation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-96bb4c6");
+var VERSION = new Version("21.0.3+sha-01ab257");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
