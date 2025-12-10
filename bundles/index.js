03b8115d03f64f6eb97e229ae5cc566f427dc6e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 17:36:04 +0000

    ac6f34a fix(docs-infra): parse linenums attribute from triple backtick metadata

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-01ab257");
+var VERSION = new Version("21.0.3+sha-ac6f34a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
