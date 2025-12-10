0fe809de928a077925309f347ba1a82a6dc3175f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:45:14 +0000

    67a5a65 docs: fix typos and clarify root effect

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.3+sha-fe581f3";
+var PLACEHOLDER_VERSION = "21.0.3+sha-67a5a65";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
