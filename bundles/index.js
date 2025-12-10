7c84418c933580d164abaae909881fa7ed9efb0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:24:29 +0000

    497c0e3 refactor(core): Add transition.to property to FakeNavigation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-2dd971b");
+var VERSION = new Version("21.0.3+sha-497c0e3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
