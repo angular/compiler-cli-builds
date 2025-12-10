b56358fba13fc95aa3907cb8489dab5ed22f801b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 16:28:02 +0000

    d355205 refactor(core): match spec updates for FakeNavigation

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.1+sha-9ace7d7";
+var PLACEHOLDER_VERSION = "21.1.0-next.1+sha-d355205";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
