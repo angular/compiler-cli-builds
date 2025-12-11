58d2447b23052b6584a7235fe29ddedbadd46e96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:47:58 +0000

    bf86f53 docs: release notes for the v21.0.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.2+sha-d0097f7");
+var VERSION = new Version("21.1.0-next.2+sha-bf86f53");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
