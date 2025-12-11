a6fa33a2678ab40e002477d2cec78d620bb5104e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:37:01 +0000

    d0097f7 fix(forms): fix signal forms type error

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0-next.2+sha-4f6014a";
+export declare const PLACEHOLDER_VERSION = "21.1.0-next.2+sha-d0097f7";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
