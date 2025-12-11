dc54a2858d913f8911ff52b271e579abc3654eea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 19:27:31 +0000

    69d243a fix(core): avoid false-positive deprecation when using `InjectionToken` with factory only

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.4+sha-fa4e6e6");
+var VERSION = new Version("21.0.4+sha-69d243a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
