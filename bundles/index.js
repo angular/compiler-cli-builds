e8a81a6fa6ce24e497c7ddb28df3480fe7ada7ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 01:03:46 +0000

    1636451 release: cut the v21.1.0-next.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.1+sha-d343ff3");
+var VERSION = new Version("21.1.0-next.2+sha-1636451");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
