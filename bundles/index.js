91e2ceb471262f486ce844dfa00b5cab95b87f32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:52:58 +0000

    fa4e6e6 release: cut the v21.0.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.3+sha-c937da4");
+var VERSION = new Version("21.0.4+sha-fa4e6e6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
