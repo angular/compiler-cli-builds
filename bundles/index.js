8a790ba80c0c037169493806f89615523accc959
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 18:09:51 +0000

    d6b02a7 docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.5+sha-27bb376");
+var VERSION = new Version("21.0.5+sha-d6b02a7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
