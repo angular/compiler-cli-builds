f456d8d77420bb9fb3c0ee2f4744c4bc8adf85c2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 18:14:44 +0000

    be5dbf4 docs: update `guidelines.md` to match `best-practices.md`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.3+sha-480c4dc");
+var VERSION = new Version("21.1.0-next.3+sha-be5dbf4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
