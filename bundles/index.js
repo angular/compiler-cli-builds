00edcdd457a6c0207e3fda274c63db82712ef763
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 18:04:32 +0000

    23fbe5c docs: replace `@debugging` tag with proper Markdown syntax

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.3+sha-b5205c7");
+var VERSION = new Version("21.1.0-next.3+sha-23fbe5c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
