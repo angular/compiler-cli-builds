36b1178b71bd91251271b1e5c6e2dc797d665c47
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 19:49:06 +0000

    44d4439 refactor(forms): add signal forms to the type tests

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.3+sha-a2aa8fe";
+var PLACEHOLDER_VERSION = "21.1.0-next.3+sha-44d4439";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.3+sha-a2aa8fe");
+var VERSION = new Version("21.1.0-next.3+sha-44d4439");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
