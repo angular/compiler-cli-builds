a84e65a06b6d9688a76fab1347022588cffbd9d2
Andrew Kushnir <akushnir@google.com>
2025-12-16 23:02:38 +0000

    feed89e ci: reformat files

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.5+sha-729b964");
+var VERSION = new Version("21.0.5+sha-feed89e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
