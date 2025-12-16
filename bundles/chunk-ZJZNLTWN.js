2b4009d17c92dbca74b4a4706377ff87f1cee364
GitHub <noreply@github.com>
2025-12-16 18:30:11 +0000

    ae0c590 refactor(forms): rename field to fieldTree in FieldContext and ValidationError

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.3+sha-bf2e508";
+var PLACEHOLDER_VERSION = "21.1.0-next.3+sha-ae0c590";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
