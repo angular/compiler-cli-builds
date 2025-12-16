d97e9ac12abc772841ffa10193e4bd6a8473bbef
Andrew Kushnir <akushnir@google.com>
2025-12-16 17:45:51 +0000

    0a5fd6b refactor(core): conditionally include debug names based on `ngDevMode`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.5+sha-61a4648");
+var VERSION = new Version("21.0.5+sha-0a5fd6b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
