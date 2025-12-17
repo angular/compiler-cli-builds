fbae7645524abc8f2e5fef22ce6e3ec56a0de30e
Andrew Kushnir <akushnir@google.com>
2025-12-17 01:42:21 +0000

    a4d2de8 Revert "fix(core): explicitly cast signal node value to String"

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.5+sha-0eb623c";
+export declare const PLACEHOLDER_VERSION = "21.0.5+sha-a4d2de8";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
