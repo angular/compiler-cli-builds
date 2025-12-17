b4399878c6b03d8a76f646aaf25525f8ae52ea05
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:44:38 +0000

    dd38869 docs(docs-infra): leverage the search on 404

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.5+sha-a4d2de8");
+var VERSION = new Version("21.0.5+sha-dd38869");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
