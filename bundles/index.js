46572d940de0adee7f39fefe2c2be6bd4818b248
Andrew Kushnir <akushnir@google.com>
2025-12-17 00:45:28 +0000

    0eb623c fix(core): explicitly cast signal node value to String

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.5+sha-feed89e");
+var VERSION = new Version("21.0.5+sha-0eb623c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
