eb0da3817788f1f3c44dcb5e90f2f4696eb40a67
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 21:18:42 +0000

    fe30b2f docs: release notes for the v21.0.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.3+sha-868cb7c");
+var VERSION = new Version("21.1.0-next.3+sha-fe30b2f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
