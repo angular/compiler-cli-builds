cd6386da99ce7678639a71ee184a0d2e52c50fb8
Andrew Kushnir <akushnir@google.com>
2025-12-17 22:42:58 +0000

    0edbee4 fix(core): explicitly cast signal node value to String

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.6+sha-c8d14d7";
+var PLACEHOLDER_VERSION = "21.0.6+sha-0edbee4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
