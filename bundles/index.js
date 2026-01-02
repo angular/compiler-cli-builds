3f850eff432c67ff97930c4a12c4cd5ffee68283
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 13:28:15 +0000

    71a4373 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-f8b6a3f");
+var VERSION = new Version("21.0.6+sha-71a4373");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
