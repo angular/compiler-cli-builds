cfd529e4027246f439014b2facb22c400768e3e3
kirjs <kirts@google.com>
2026-01-05 17:12:31 +0000

    5e6a9ba refactor(router): Simplifies unsubscribe logic

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-73a3fd5");
+var VERSION = new Version("21.0.6+sha-5e6a9ba");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
