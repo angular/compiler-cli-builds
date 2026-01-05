ef212eb01b89ae3e34f47ccc887b3db30a1d06c1
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 20:25:53 +0000

    1530814 docs(docs-infra): Add package filter to the API list

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.4+sha-5edceff");
+var VERSION = new Version("21.1.0-next.4+sha-1530814");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
