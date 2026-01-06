ff7a99a4c64ca4c7b3300b72f7c1ed9caace966e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 15:38:51 +0000

    b93d5ec docs(forms): add documentation links to Signal Forms API

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0-next.4+sha-f05d0fe";
+export declare const PLACEHOLDER_VERSION = "21.1.0-next.4+sha-b93d5ec";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
