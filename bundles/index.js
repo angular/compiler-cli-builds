332de54f43c25a06a840134abd32a332f9849a1e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 00:33:16 +0000

    b16c2f5 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-8f26a52");
+var VERSION = new Version("21.0.6+sha-b16c2f5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
