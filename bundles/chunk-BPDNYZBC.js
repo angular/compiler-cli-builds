4ae9986f77dacea3be3a5aeb3d465b929657bb61
GitHub <noreply@github.com>
2026-01-06 20:58:08 +0000

    c2c2b4a fix(core): sanitize sensitive attributes on SVG script elements

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.15+sha-4755bbd";
+var PLACEHOLDER_VERSION = "20.3.15+sha-c2c2b4a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
