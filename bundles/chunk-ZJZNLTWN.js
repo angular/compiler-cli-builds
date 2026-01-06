b8cd2424686725ea2ae5f82ba7f1365f4694e266
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 20:35:05 +0000

    e2e5b66 build: update cross-repo angular dependencies

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-next.4+sha-82a2de2";
+var PLACEHOLDER_VERSION = "21.1.0-next.4+sha-e2e5b66";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
