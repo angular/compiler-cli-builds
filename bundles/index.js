71b36053f20f50367aeef82cce6f77962fa9ccba
kirjs <kirts@google.com>
2026-01-07 19:03:04 +0000

    c3f3ebe docs: remove Component suffix from examples in DI & Drag and Drop

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-61370dc");
+var VERSION = new Version("21.0.6+sha-c3f3ebe");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
