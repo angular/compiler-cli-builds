e7dd7be24a099c58243311716217b786b8e4139e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 16:50:14 +0000

    97fd1de Revert "refactor(router): Add support for precommitHandler in Navigation integration"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-next.4+sha-b17f93b");
+var VERSION = new Version("21.1.0-next.4+sha-97fd1de");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
