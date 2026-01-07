e4e8e1e5d326deb8cb3ecfbbe12b2d07d8494738
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-07 00:32:54 +0000

    4161161 Revert "fix(router): Ensure createUrlTree does not reuse segments of input"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-e3fba18");
+var VERSION = new Version("21.0.6+sha-4161161");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
