d356616c9e3ff41818157638d0f7f08a255a9aae
kirjs <kirts@google.com>
2026-01-07 17:43:18 +0000

    7c74674 fix(core): avoid leaking view data in animations

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.6+sha-7738eb9");
+var VERSION = new Version("21.0.6+sha-7c74674");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
