80393c821e3984612c4903707126f8fbc7ea1ae8
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 19:33:03 +0000

    5239e47 fix(core): handle cancelled traversals in fake navigation

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.7+sha-8ff2776";
+export declare const PLACEHOLDER_VERSION = "21.0.7+sha-5239e47";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
