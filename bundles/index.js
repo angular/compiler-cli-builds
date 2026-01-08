c156eb9787718219ebfe0de8b0ab9dc296ddfc24
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 19:25:24 +0000

    8ff2776 fix(docs-infra): improve dark mode button hover contrast for WCAG compliance

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.7+sha-0dead0f");
+var VERSION = new Version("21.0.7+sha-8ff2776");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
