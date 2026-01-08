92eede95c6b3d084563dc7bc8a30e81781f770ce
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 21:32:40 +0000

    696adbc refactor(core): Use the provided Document value rather than global in FakeNavigation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-a0904c8");
+var VERSION = new Version("21.0.8+sha-696adbc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
