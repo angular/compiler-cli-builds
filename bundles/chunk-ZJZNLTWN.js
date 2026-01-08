d44e1f8bf2eca162b315bb6143aa4c79555b1426
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:46:04 +0000

    a0904c8 release: cut the v21.0.8 release

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.7+sha-5239e47";
+var PLACEHOLDER_VERSION = "21.0.8+sha-a0904c8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.7+sha-5239e47");
+var VERSION = new Version("21.0.8+sha-a0904c8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
