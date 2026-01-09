44e8c9ed9effaf366d305c732b57cc805c909e3a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 16:26:58 +0000

    f41d721 refactor(devtools): style the profiler dialogs to match the current design

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-rc.0+sha-5dd2f2b");
+var VERSION = new Version("21.1.0-rc.0+sha-f41d721");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
