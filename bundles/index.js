5e1cd39a21427a231414baaa46cc6b03aa8d5e46
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:42:34 +0000

    a6efb77 docs(docs-infra): avoid persistent AbortSignal listener in wait debounce in search

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-a63f6d0");
+var VERSION = new Version("21.0.8+sha-a6efb77");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
