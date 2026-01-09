630958c194a852730f466fcdbdb5d972960e77d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 15:51:28 +0000

    baee12d Revert "docs: remove incorrectly listed operators in "Supported operators" table (Expression Syntax Guide) (#62092)"

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.8+sha-696adbc";
+var PLACEHOLDER_VERSION = "21.0.8+sha-baee12d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
