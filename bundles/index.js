59cfb83cbb09f7abc98744400495ab3658261e03
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 16:02:40 +0000

    3a65814 docs(docs-infra): improve label fallback logic in search results

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-3ccd5a4");
+var VERSION = new Version("21.2.0-next.0+sha-3a65814");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
