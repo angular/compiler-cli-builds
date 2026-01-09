3b840df536cfba85cb0d4a4aec44851a174ecefb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:35:14 +0000

    da364d2 refactor(router): Add support for precommitHandler in Navigation integration

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-4fb24ca";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-da364d2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
