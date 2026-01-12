8fff1031a3eaf207f43205821df26807dd0a8629
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:34:02 +0000

    77226dd docs: change language section to "community translations"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-49e18e5");
+var VERSION = new Version("21.0.8+sha-77226dd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
