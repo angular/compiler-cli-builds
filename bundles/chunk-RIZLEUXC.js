d6384520f4f0b2d32cb2e93fe5c0ff09d10b249b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:57:22 +0000

    82d556a fix(forms): Ensure the control instruction comes after the other bindings

--- bundles/chunk-RIZLEUXC.js
+++ bundles/chunk-RIZLEUXC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-6a9d4d5";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-82d556a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
