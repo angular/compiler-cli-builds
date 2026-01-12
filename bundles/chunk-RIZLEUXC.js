6de0704d85739662d563273bc135a9f1110edc95
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 22:57:03 +0000

    d5caac6 build: fix changelog generation for last rev

--- bundles/chunk-RIZLEUXC.js
+++ bundles/chunk-RIZLEUXC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-82d556a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-d5caac6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
