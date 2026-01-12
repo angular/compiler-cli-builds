d477aae16f3dd9322361b46a0c27bb5387d03cc0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:47:02 +0000

    6a9d4d5 docs: replace old https://angular.io/license with https://angular.dev/license

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.8+sha-7902344";
+var PLACEHOLDER_VERSION = "21.0.8+sha-6a9d4d5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-7902344");
+var VERSION = new Version("21.0.8+sha-6a9d4d5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
