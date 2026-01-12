3f69317325719b90abbf7084ee4281e26c1cfcad
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:53:54 +0000

    74e99fa docs(docs-infra): prevent from showing duplicate usage blocks in functions API docs

--- bundles/chunk-RIZLEUXC.js
+++ bundles/chunk-RIZLEUXC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-08d0ac7";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-74e99fa";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-08d0ac7");
+var VERSION = new Version("21.0.8+sha-74e99fa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
