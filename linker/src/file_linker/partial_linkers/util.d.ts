e090842d049bfea4a821fbcf4f360c3178ec16d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:54:09 +0000

    49e18e5 fix(docs-infra): improve symbolic green contrast for WCAG compliance

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.8+sha-dfd8c52";
+export declare const PLACEHOLDER_VERSION = "21.0.8+sha-49e18e5";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
