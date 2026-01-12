fcbcab91c3ab3bff247ff1dd502373794c8fa59a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 21:45:43 +0000

    1b4dcc0 docs: replace old https://angular.io/license with https://angular.dev/license

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-4d983e9");
+var VERSION = new Version("21.2.0-next.0+sha-1b4dcc0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
