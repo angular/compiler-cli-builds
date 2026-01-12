a1c26e39e6777adfbdda17455de0b4c735ab8580
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:36:11 +0000

    dfd8c52 docs: update testing guide to include Vitest and improve authGuard test setup

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-5b0e705");
+var VERSION = new Version("21.0.8+sha-dfd8c52");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
