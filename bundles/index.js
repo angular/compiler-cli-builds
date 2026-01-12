3c8399c2bf2433d242b629032da085797acf9c15
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:22:39 +0000

    5b0e705 docs: improve separation between enter and leave example

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-615a77b");
+var VERSION = new Version("21.0.8+sha-5b0e705");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
