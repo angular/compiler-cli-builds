22d95ffa8f913504430648b5a701aa0797be7fe2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:47:08 +0000

    5ad2e1b docs: replace old https://angular.io/license with https://angular.dev/license

--- bundles/chunk-TTZRVNVM.js
+++ bundles/chunk-TTZRVNVM.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.0-rc.0+sha-1f47e88";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.0-rc.0+sha-5ad2e1b";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0-rc.0+sha-1f47e88";
+var PLACEHOLDER_VERSION = "21.1.0-rc.0+sha-5ad2e1b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
