9b75355eba1f01ab8c4c2df9949d7833b13dff81
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:11:02 +0000

    7902344 docs: remove Component suffix in animations, HTTP, and SSR guides

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.0.8+sha-74e99fa");
+var VERSION = new Version("21.0.8+sha-7902344");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
