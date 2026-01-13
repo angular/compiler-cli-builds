1fceb34ea9dcf103602fcaa8c97c38b9b1489f27
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:49:07 +0000

    9a764e0 refactor(core): remove ng when invoking `enableProdMode`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-rc.0+sha-9c1a5a5");
+var VERSION = new Version("21.1.0-rc.0+sha-9a764e0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
