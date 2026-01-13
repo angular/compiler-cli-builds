362747ce0b3da16c30d0a089b03abf6d89b92973
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 21:12:23 +0000

    de9277b build: Update vscode release script

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.8+sha-492b106";
+var PLACEHOLDER_VERSION = "21.0.8+sha-de9277b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
