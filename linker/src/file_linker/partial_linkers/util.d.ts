c3fe29efa3a63c48a2236efbc8f249b43ab14705
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 17:09:06 +0000

    01e5bab docs: update OnChanges API reference w/ signal input

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.0.8+sha-8f4cc84";
+export declare const PLACEHOLDER_VERSION = "21.0.8+sha-01e5bab";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
