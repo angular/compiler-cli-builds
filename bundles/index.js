9a1ae7227b72d529e096390d1ac8b23dd92ada0e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 17:09:58 +0000

    871fa1b docs: update OnChanges API reference w/ signal input

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-6e6a022");
+var VERSION = new Version("21.2.0-next.0+sha-871fa1b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
