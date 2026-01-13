ad2eba7f31e19f0c4793d5ccf9d11fe4c9f61bae
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 19:14:44 +0000

    b95753e fix(vscode-extension): add syntax highlighting for arrow functions

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-871fa1b";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-b95753e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-871fa1b");
+var VERSION = new Version("21.2.0-next.0+sha-b95753e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
