b4b0abbf2fe161694b0a90f7863192809c36d08a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:49:49 +0000

    0159268 docs: release notes for the v21.1.0 release

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-d12e6f0";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-0159268";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-d12e6f0");
+var VERSION = new Version("21.2.0-next.0+sha-0159268");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
