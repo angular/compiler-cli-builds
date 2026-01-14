31fbef727841d81a3207732151c8f330e108c903
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:30:29 +0000

    3f13db8 ci: update algolia synonym updater to use node directly rather than tsx

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-724e917");
+var VERSION = new Version("21.2.0-next.0+sha-3f13db8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
