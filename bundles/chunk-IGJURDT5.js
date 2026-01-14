978551aa2a347d0a6fe9421aa3601d0d7b982617
Douglas Parker <dgp1130@users.noreply.github.com>
2026-01-14 22:19:15 +0000

    61614f6 release: bump Angular DevTools version to 1.8.0

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-fa4bcf1";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-61614f6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
