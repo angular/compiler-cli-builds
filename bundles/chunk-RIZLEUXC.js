17a02e00b57907e2cf986dcc8f99fd8582802232
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:24:20 +0000

    79273c1 Revert "docs(docs-infra): use Signals Forms focusBoundControl"

--- bundles/chunk-RIZLEUXC.js
+++ bundles/chunk-RIZLEUXC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-d710ebe";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.0.8+sha-79273c1";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
