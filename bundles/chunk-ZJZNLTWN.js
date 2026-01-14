b397151714c7e4b93f6952c1da3d1c452b3c98bf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:34:23 +0000

    147ea13 release: cut the v21.0.9 release

--- bundles/chunk-ZJZNLTWN.js
+++ bundles/chunk-ZJZNLTWN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.0.8+sha-79273c1";
+var PLACEHOLDER_VERSION = "21.0.9+sha-147ea13";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
