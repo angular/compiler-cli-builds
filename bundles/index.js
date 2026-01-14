9fc9c78e78e89f40497988512c26f41be480ca40
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:31:53 +0000

    1ea5c97 feat(forms): allow focusing bound control from field state

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0-rc.0+sha-654ed65");
+var VERSION = new Version("21.1.0-rc.0+sha-1ea5c97");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
