98b4c85931b6b6b17491b8cd3ae4344a1e4e2178
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-14 23:09:38 +0000

    ae4e2e0 build: update cross-repo angular dependencies to v21.1.0

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0+sha-7f99907";
+var PLACEHOLDER_VERSION = "21.1.0+sha-ae4e2e0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
