1bd72f04e3a50eda6b6f68394306efc25cf35d86
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:35:55 +0000

    d12e6f0 docs: release notes for the v21.0.9 release

--- bundles/chunk-BBKK2QPO.js
+++ bundles/chunk-BBKK2QPO.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.0+sha-3f13db8";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.0+sha-d12e6f0";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-3f13db8");
+var VERSION = new Version("21.2.0-next.0+sha-d12e6f0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
