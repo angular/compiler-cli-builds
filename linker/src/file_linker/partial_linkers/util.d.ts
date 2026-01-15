8693e05812a1838da97009a33ec4f46cd6e402fc
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 19:12:58 +0000

    0643f54 docs: clarify that FESM can depend on shared chunks

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.0+sha-c0446da";
+export declare const PLACEHOLDER_VERSION = "21.1.0+sha-0643f54";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
