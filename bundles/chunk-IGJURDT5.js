5ed8c38c7511aadc11f842daa3ea8ec64e711e8c
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 19:45:10 +0000

    417c385 docs: update directive name from `[field]` to `[formField]` in signal-forms documentation

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-87f4797";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-417c385";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-87f4797");
+var VERSION = new Version("21.2.0-next.0+sha-417c385");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
