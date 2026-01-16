f5cc003795a20eb4a0bd4740b4b0df385e52583e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 21:32:48 +0000

    5d46554 docs: Add section on `animate.leave` for clarity

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-e55260f-with-local-changes");
+var VERSION = new Version("21.2.0-next.0+sha-5d46554-with-local-changes");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
