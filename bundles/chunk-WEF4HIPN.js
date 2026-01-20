2e2ab5e5ba82ff1145c0ad2a1ec1fc18e158dc9d
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:40:52 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.0+sha-6b34e5f-with-local-changes";
+var PLACEHOLDER_VERSION = "21.1.0+sha-82593de";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
