bc528a182131fd360435f1168bbd257b01933678
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 18:31:50 +0000

    0fa8099 refactor: Delete counterproductive aria-label

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-b275206");
+var VERSION = new Version("21.2.0-next.0+sha-0fa8099");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
