56716d97a3cad04b24b09921f26ed9962b987ccf
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:35:33 +0000

    6284126 refactor(common): cleanup unused code in doc regions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-94a8f79");
+var VERSION = new Version("21.2.0-next.0+sha-6284126");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
