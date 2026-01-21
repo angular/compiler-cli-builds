2613867be1e0432df04e405159c3732ff30aae39
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 20:50:37 +0000

    643f0f4 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.0+sha-1884226");
+var VERSION = new Version("21.1.0+sha-643f0f4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
