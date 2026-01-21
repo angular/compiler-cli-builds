7a26f51ebd7da57fe8e2ca429e0178a7b0f24121
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 01:20:47 +0000

    6a56204 fix(docs-infra): use focus-visible for focus styling

--- bundles/chunk-TTZRVNVM.js
+++ bundles/chunk-TTZRVNVM.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.0+sha-62d8473";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.0+sha-6a56204";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
