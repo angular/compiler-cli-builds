c4d5bdd92934280eca7319a6e52bc810aa1b5a48
Andrew Scott <atscott01@gmail.com>
2026-01-22 20:00:58 +0000

    5d6e534 build(language-service): upgrade LSP library to v9.0.1 (LSP 3.17)

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.0-next.0+sha-405fa59";
+export declare const PLACEHOLDER_VERSION = "21.2.0-next.0+sha-5d6e534";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
