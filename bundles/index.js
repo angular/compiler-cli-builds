57b1b684a6cd6a31130cc91bf650aa6846f06805
Andrew Scott <atscott@google.com>
2026-01-22 23:06:05 +0000

    458e21e docs(docs-infra): Exempts more symbols from automatic linking

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.1+sha-e4c9ff7");
+var VERSION = new Version("21.1.1+sha-458e21e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
