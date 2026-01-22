ae238f911f49d65d1fd8f87b13f3d36cd30a631d
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:17:47 +0000

    e4c9ff7 build: Update prettier to 3.8

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.1+sha-c9043fb";
+var PLACEHOLDER_VERSION = "21.1.1+sha-e4c9ff7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
