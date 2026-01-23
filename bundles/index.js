0d346acd4b9ab7267f69bec4987836fb47dd29c3
Andrew Scott <atscott01@gmail.com>
2026-01-23 01:06:45 +0000

    ddd3198 docs: update signal form tutorials to use `FormField`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-d1135f5");
+var VERSION = new Version("21.2.0-next.0+sha-ddd3198");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
