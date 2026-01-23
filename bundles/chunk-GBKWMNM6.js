e15483a7d46a19b8216973d62049215f2f045d26
Andrew Scott <atscott01@gmail.com>
2026-01-23 00:40:20 +0000

    d9e7ab3 build: update all github actions

--- bundles/chunk-GBKWMNM6.js
+++ bundles/chunk-GBKWMNM6.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.1+sha-458e21e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.1+sha-d9e7ab3";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
