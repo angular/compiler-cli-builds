397ac786c511939a4e2bbce131214f9163d976e6
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:50:14 +0000

    c921786 docs: Updates form documentation (#66732)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.0+sha-8efee1c");
+var VERSION = new Version("21.2.0-next.0+sha-c921786");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
