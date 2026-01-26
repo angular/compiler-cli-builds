7d024444b5267763a03f3e05213906906c5ef936
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:38:54 +0000

    b4227fb docs: apply prefer/avoid blocks in UI with comments

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.1+sha-4742c70");
+var VERSION = new Version("21.1.1+sha-b4227fb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
