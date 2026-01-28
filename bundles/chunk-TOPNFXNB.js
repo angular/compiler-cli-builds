6c03b7fa8091c787a5b630a66ee29ace0ba2029b
GitHub <noreply@github.com>
2026-01-28 18:42:47 +0000

    0424710 refactor(devtools): snap to signal graph nodes selected from the properties panel

--- bundles/chunk-TOPNFXNB.js
+++ bundles/chunk-TOPNFXNB.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.0+sha-a4f6d40";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.0+sha-0424710";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
