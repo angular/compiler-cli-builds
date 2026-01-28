671b4c3965484542a2143b85fff2cde3147a5c56
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:12:43 +0000

    6601f06 test(common): enables zoneless change detection in tests

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.1+sha-fe69342";
+var PLACEHOLDER_VERSION = "21.1.1+sha-6601f06";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
