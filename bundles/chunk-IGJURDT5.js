bb391e8d8ebdf5f1d042e3a69f9aeed306a819b1
GitHub <noreply@github.com>
2026-01-28 00:13:02 +0000

    4448356 test(common): enables zoneless change detection in tests

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-960de62";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-4448356";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
