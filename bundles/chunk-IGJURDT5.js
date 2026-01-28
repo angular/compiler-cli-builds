075ce703b82a643fcaef13b7a6670a8c8000a29b
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:02:28 +0000

    bea4cb7 build: update dependency @actions/github to v8

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-4123ebf";
+var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-bea4cb7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
