e422b6119b429dad3daee972859fe6b7f6f0420f
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:53:56 +0000

    5a6fefb release: cut the v21.2.0-next.1 release

--- bundles/chunk-IGJURDT5.js
+++ bundles/chunk-IGJURDT5.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.0+sha-571747d";
+var PLACEHOLDER_VERSION = "21.2.0-next.1+sha-5a6fefb";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
