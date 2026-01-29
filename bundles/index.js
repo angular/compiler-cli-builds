b5b48a5f66640d8df6d42c3a143c4b04a39404fd
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:37:15 +0000

    74e7681 docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-a67e007");
+var VERSION = new Version("21.2.0-next.1+sha-74e7681");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
