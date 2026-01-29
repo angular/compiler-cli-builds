27cbc9932ab41947cdd30965d205f88a21425394
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:12:14 +0000

    0a4c647 refactor(core): Remove unsued properties in differ factories

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.2+sha-cfe7340");
+var VERSION = new Version("21.1.2+sha-0a4c647");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
