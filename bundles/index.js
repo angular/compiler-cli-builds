3cb387630f2bab2af7e3bdf1aa13a11c9b3972ec
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:04:53 +0000

    0e04233 docs: Add missing link in HostListener documentation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-d072791");
+var VERSION = new Version("21.2.0-next.1+sha-0e04233");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
