55ee418451856ff2c72ede879562c70bd97cd24f
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:35:24 +0000

    8dfbb8b build: update cross-repo angular dependencies

--- bundles/chunk-GBKWMNM6.js
+++ bundles/chunk-GBKWMNM6.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.2+sha-0a4c647";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.2+sha-8dfbb8b-with-local-changes";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
