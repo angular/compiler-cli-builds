5b4e84cb1f8e507791e76372f241b146360c24aa
GitHub <noreply@github.com>
2026-01-30 17:18:26 +0000

    26d1215 refactor(forms): convert `FieldState.controlValue` to a `WritableSignal`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-af76b9d");
+var VERSION = new Version("21.2.0-next.1+sha-26d1215");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
