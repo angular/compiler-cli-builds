f0f9993c76a8e9f3b6788f915ca417669cd2f175
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 00:11:39 +0000

    b327d72 refactor(language-service): initial use of type-only imports and standard server types

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.1+sha-74e7681";
+var PLACEHOLDER_VERSION = "21.2.0-next.1+sha-b327d72";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
