e96d7e868b50ac80a510ec6776869970349f4cd4
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 19:16:10 +0000

    8ab433a fix(core): export DirectiveWithBindings

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.1+sha-a64a90d";
+var PLACEHOLDER_VERSION = "21.2.0-next.1+sha-8ab433a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
