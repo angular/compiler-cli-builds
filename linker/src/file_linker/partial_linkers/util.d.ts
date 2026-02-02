7780c43939c8cf142db95cf6f90dfbeb9eda0ebf
GitHub <noreply@github.com>
2026-02-02 18:09:26 +0000

    3d09d8e docs: update zoneless guide

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.0-next.1+sha-8d5210c";
+export declare const PLACEHOLDER_VERSION = "21.2.0-next.1+sha-3d09d8e";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
