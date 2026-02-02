9d2c05fe2d005f945861333bd28a5dfe3c408e10
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:22:24 +0000

    a64a90d docs(animations): fix small typo in animations migrations docs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-3d09d8e");
+var VERSION = new Version("21.2.0-next.1+sha-a64a90d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
