548f11f9c6219b02879ceaabf9dbb27922caf97e
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 00:36:20 +0000

    5a0f272 Revert "feat(router): adds browserUrl input support to router links"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-68ba9c4");
+var VERSION = new Version("21.2.0-next.1+sha-5a0f272");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
