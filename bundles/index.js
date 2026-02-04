4a49aa4d4c62610041b6f479a43b75d5700ac9c5
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 22:29:35 +0000

    680d99b refactor(forms): work around internal issue

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-cab5ddd");
+var VERSION = new Version("21.2.0-next.1+sha-680d99b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
