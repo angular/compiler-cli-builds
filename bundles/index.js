b80ec6457816538ff1fb7b8b51aac5c7cf98de2a
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:49:41 +0000

    8c21866 feat(language-service): add linked editing ranges for HTML tag synchronization

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.1+sha-7dfbacb");
+var VERSION = new Version("21.2.0-next.1+sha-8c21866");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
