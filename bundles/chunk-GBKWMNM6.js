c0e4770428c48ce7f5af148e311ee9091e5776a6
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:02:11 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- bundles/chunk-GBKWMNM6.js
+++ bundles/chunk-GBKWMNM6.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.3+sha-70668be";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.1.3+sha-0729181";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
