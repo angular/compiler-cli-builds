660d9a4308c309bb20f86e07178fd26398e8cc61
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 18:24:46 +0000

    460dbbb docs: remove non-existant method

--- bundles/chunk-IKNUNOJT.js
+++ bundles/chunk-IKNUNOJT.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.2+sha-15c71fb";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.2+sha-460dbbb";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
