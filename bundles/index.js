27ba07536b23265e82642ff7725d3a08cba723d4
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 23:56:43 +0000

    760910e Revert "refactor(service-worker): remove zone-based testing utilities"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.2+sha-51cc914");
+var VERSION = new Version("21.2.0-next.2+sha-760910e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
