41f0c2181a1d4a63f7fa880e97ba042e3967c74c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:21:04 +0000

    e53c8ab fix(core): Fix flakey test due to document injection

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.2+sha-cb1163e";
+var PLACEHOLDER_VERSION = "21.2.0-next.2+sha-e53c8ab";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
