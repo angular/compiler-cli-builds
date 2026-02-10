c53883694acf57912c037759c2275b1d3cd3bf1f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 17:48:45 +0000

    18003a3 feat(common): add an 'outlet' injector option for ngTemplateOutlet

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.2+sha-0f47fda");
+var VERSION = new Version("21.2.0-next.2+sha-18003a3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
