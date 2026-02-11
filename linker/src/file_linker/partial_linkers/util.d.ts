aa38dea174d2a20c64c41683229721514467f187
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:46:28 +0000

    17f1927 refactor(core): replacing usages of `Default` strategy with `Eager`

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.0-next.3+sha-f67818a";
+export declare const PLACEHOLDER_VERSION = "21.2.0-next.3+sha-17f1927";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
