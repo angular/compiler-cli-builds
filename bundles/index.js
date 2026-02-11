406668bd933a34d55f5c1f73263bf3aeb06446aa
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 21:47:19 +0000

    a4fceda build: update pnpm to v10.29.2

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.3+sha-3606902");
+var VERSION = new Version("21.2.0-next.3+sha-a4fceda");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
