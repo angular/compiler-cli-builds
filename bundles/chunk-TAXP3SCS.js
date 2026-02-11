ba424022abd478cb5b3fb226f6bd1774651b1d6c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-11 18:02:33 +0000

    39cff9c release: cut the v21.2.0-next.3 release

--- bundles/chunk-TAXP3SCS.js
+++ bundles/chunk-TAXP3SCS.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.2+sha-ad81510";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-next.3+sha-39cff9c";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
