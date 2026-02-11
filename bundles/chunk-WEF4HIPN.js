c68c089c7c3b1f379cf4d0ec3fb1330711b5f93f
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 23:43:08 +0000

    937e98f build: update pnpm to v10.29.2

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.4+sha-4ef0f1f";
+var PLACEHOLDER_VERSION = "21.1.4+sha-937e98f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
