c698830d32d1aa6bd8d529ab831ecdd2a8fc97c9
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:06:10 +0000

    4937d16 ci: update pullapprove reviewer list

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.3+sha-a4fceda");
+var VERSION = new Version("21.2.0-next.3+sha-4937d16");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
