e0cda2fff4f2eb163da4ac6c4178fae02160fbec
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:15:25 +0000

    f67818a docs: update cross-repo adev docs

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-next.3+sha-4937d16";
+var PLACEHOLDER_VERSION = "21.2.0-next.3+sha-f67818a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
