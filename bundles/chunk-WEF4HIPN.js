245d132056442a2fc7b6551ee98f98caeb3ddf60
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 19:21:56 +0000

    7a8ba5c build: update cross-repo angular dependencies

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.4+sha-65a8df1";
+var PLACEHOLDER_VERSION = "21.1.4+sha-7a8ba5c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
