449c93e43e70f1ebdb403354433d51527d5572e7
Matthew Beck <me@mattrb.com>
2026-02-13 00:58:25 +0000

    55b501a refactor(common): improve image directive typings

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.1.4+sha-5cf357b";
+export declare const PLACEHOLDER_VERSION = "21.1.4+sha-55b501a";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
