13ad0591fb05b74821dfc9bbc2be749cbde7433e
Matthew Beck <me@mattrb.com>
2026-02-13 23:47:04 +0000

    670dee9 docs: fix formatting in pipes.md example

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.4+sha-4f94463";
+var PLACEHOLDER_VERSION = "21.1.4+sha-670dee9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
