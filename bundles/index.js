e84daedf3daaa87c18c47a39128dc434c27b3933
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:25:15 +0000

    50c5353 build: update pnpm to v10.29.3

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-55b501a");
+var VERSION = new Version("21.1.4+sha-50c5353");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
