4b5659bb5cf00d668e4e86e8d6a3c86282fa3888
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 20:30:54 +0000

    4f94463 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-5a86528");
+var VERSION = new Version("21.1.4+sha-4f94463");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
