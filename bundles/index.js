0eb6cdd234faf8811400d2c8e5ba6bf8aa735ddf
Matt Beck <mattrb@google.com>
2026-02-17 16:29:11 +0000

    24d00e9 docs: typo in documentation

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-a3588a0");
+var VERSION = new Version("21.1.4+sha-24d00e9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
