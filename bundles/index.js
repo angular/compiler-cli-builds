8199f3c6d24c8625597323c2d8ec186bdc8b5ac9
Matt Beck <mattrb@google.com>
2026-02-17 19:37:48 +0000

    9c1ec6c fix(zone.js): support passthrough of Promise.try API

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-fc873cd");
+var VERSION = new Version("21.1.4+sha-9c1ec6c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
