00994742c6c95ec4166cacc47534f675ae286769
GitHub <noreply@github.com>
2026-02-18 23:53:18 +0000

    7f33cbe release: bump VSCode extension version to 21.2.0 (#67134)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-dd630ef");
+var VERSION = new Version("21.2.0-rc.0+sha-7f33cbe");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
