1b445d56e1fe8549aa55aaa097281348150b4a8d
Matthew Beck <me@mattrb.com>
2026-02-18 19:10:06 +0000

    a4eb347 docs: add signal forms async operations guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-3c494b4");
+var VERSION = new Version("21.1.4+sha-a4eb347");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
