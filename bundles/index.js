a8c78070312755758e58f55631ba980c772ddf01
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:45:30 +0000

    13fd2b1 release: cut the v21.2.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.3+sha-cb8e974");
+var VERSION = new Version("21.2.0-rc.0+sha-13fd2b1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
