8c879fda3c7bea23479ff159deefd6405780f722
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 17:33:59 +0000

    c36385c refactor(docs-infra): Add writing guide skill

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-next.3+sha-22a2090");
+var VERSION = new Version("21.2.0-next.3+sha-c36385c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
