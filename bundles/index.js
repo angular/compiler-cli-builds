7abc8a88ac31fedf00236d30f1398ef6ee2bf405
Matthew Beck <me@mattrb.com>
2026-02-18 15:18:14 +0000

    25ebceb docs: fix custom control metadata example in signal forms guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.4+sha-76d1a37");
+var VERSION = new Version("21.1.4+sha-25ebceb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
