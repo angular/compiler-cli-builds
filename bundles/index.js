46bcb28cae4621da796aa6fcf254dae780dde5d5
Andrew Scott <atscott@google.com>
2026-02-19 00:30:56 +0000

    f58f9c8 release: cut the zone.js-0.16.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-7f33cbe");
+var VERSION = new Version("21.2.0-rc.0+sha-f58f9c8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
