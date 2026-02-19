9047257098d20ef35bf32b2deb9901f86a349339
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 16:59:29 +0000

    210ddf9 docs(docs-infra): fixed missing gray color palette definitions for light and dark modes

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.5+sha-debcae7";
+var PLACEHOLDER_VERSION = "21.1.5+sha-210ddf9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
