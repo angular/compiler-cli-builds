e1886c83659d44835e6fe232fbadd01c05a37a1a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 19:28:46 +0000

    5cea357 docs: update changelog to note no changes in 21.1.5 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-79f1cd4");
+var VERSION = new Version("21.2.0-rc.0+sha-5cea357");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
