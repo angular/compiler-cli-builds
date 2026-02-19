ed1e112be0b05a948b3fdde4db360a168e2d4468
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 17:16:32 +0000

    a5a1829 docs: improve lazy loading of homepage examples

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.3.0-next.0+sha-a3731a1";
+export declare const PLACEHOLDER_VERSION = "21.3.0-next.0+sha-a5a1829";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
