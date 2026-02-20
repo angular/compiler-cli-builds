1c4aded293d465824feb5d88284720b432c2e88a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:04:53 +0000

    f68078b docs: add section on reactive context and async operations

--- bundles/chunk-IQF227JC.js
+++ bundles/chunk-IQF227JC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.3.0-next.0+sha-bc473cf";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.3.0-next.0+sha-f68078b";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.3.0-next.0+sha-bc473cf");
+var VERSION = new Version("21.3.0-next.0+sha-f68078b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
