5f6a6c95ac17bc93bd4f8847da56f3b3569bba63
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 19:06:30 +0000

    0f4ba4a ci: update ng-dev config and pullapprove

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.5+sha-866da08");
+var VERSION = new Version("21.1.5+sha-0f4ba4a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
