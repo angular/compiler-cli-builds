e61245ee214ac7b72a3923d6a8d685531855b13b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:21:49 +0000

    3c4deaa refactor(common): log a warning when a JsonPipe receives a signal

--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-c0cb604";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-3c4deaa";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-c0cb604");
+var VERSION = new Version("21.2.0-rc.0+sha-3c4deaa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
