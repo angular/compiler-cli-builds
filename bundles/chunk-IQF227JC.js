4a38cd1f3c70d38078f77259799ef26f9058bd3d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 16:49:04 +0000

    cfdbb7c ci: bump target version to target 22.0.0-next.0

--- bundles/chunk-IQF227JC.js
+++ bundles/chunk-IQF227JC.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.3.0-next.0+sha-c672f92";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-cfdbb7c";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.3.0-next.0+sha-c672f92");
+var VERSION = new Version("22.0.0-next.0+sha-cfdbb7c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
