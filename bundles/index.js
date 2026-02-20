b1f028f9d14e3398ef113f9b70792ce4e655e8d3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:10:53 +0000

    67b22d5 docs: update NgTemplateOutlet documentation with injector

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.3.0-next.0+sha-f68078b");
+var VERSION = new Version("21.3.0-next.0+sha-67b22d5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
