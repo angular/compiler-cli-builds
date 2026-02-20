65daddf0507c9a7b62b57a31a6e98188ae485c2a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 19:06:01 +0000

    8737289 ci: update ng-dev config and pullapprove

--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-631f60d";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-8737289";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-631f60d");
+var VERSION = new Version("21.2.0-rc.0+sha-8737289");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
