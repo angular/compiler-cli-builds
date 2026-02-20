62989452302d3dfc77445def586d9c7cd17b31b5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:07:23 +0000

    390efd5 docs: add new debugging and troubleshooting di guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-c2cedd1");
+var VERSION = new Version("21.2.0-rc.0+sha-390efd5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
