5a1b46bed037cdbc8285324738f258dd9bf46ad4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-23 18:30:56 +0000

    5ac1c02 refactor(core): remove outdated TODO comments

--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-9a3f5cb";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-5ac1c02";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
