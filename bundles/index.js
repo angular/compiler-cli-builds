f7eb189eeb45d7e75fdf53df3ee100fc215df41e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 18:32:50 +0000

    a8dcb28 refactor(core): remove outdated TODO comments

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.5+sha-0596a5b");
+var VERSION = new Version("21.1.5+sha-a8dcb28");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
