e8bcc128ff9190ccf624677eab379aec64820661
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 17:30:32 +0000

    5c4d9b2 docs: consolidate validation rules for `Host` and `X-Forwarded-Host` headers into a single point.

--- bundles/chunk-WEF4HIPN.js
+++ bundles/chunk-WEF4HIPN.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.1.5+sha-4e6ac9c";
+var PLACEHOLDER_VERSION = "21.1.5+sha-5c4d9b2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
