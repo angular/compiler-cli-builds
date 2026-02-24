15d1f01934fe66774c8a2993df8a64fa31e93973
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:05:43 +0000

    0101922 docs: fix roadmap introduction text

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.1.5+sha-5c4d9b2");
+var VERSION = new Version("21.1.5+sha-0101922");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
