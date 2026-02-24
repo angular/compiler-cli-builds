5603a5ce41513eec7a2f569e223e3224a5c22413
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 19:06:43 +0000

    9dc3ca4 docs: add docs for `@switch` Exhaustive type checking

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.0+sha-8af73eb";
+var PLACEHOLDER_VERSION = "22.0.0-next.0+sha-9dc3ca4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
