0d03868ad23d2f065a386c9710b165e5204a8beb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:17:01 +0000

    63cb3f4 build: update bazel dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-787cd87");
+var VERSION = new Version("21.2.0-rc.0+sha-63cb3f4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
