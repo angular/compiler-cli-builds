89407712ea86b0d99d7ac7c13ea9928cca5f181d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:08:43 +0000

    957466d docs(docs-infra): scroll home features section into view on tab change

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.0+sha-01a1795");
+var VERSION = new Version("22.0.0-next.0+sha-957466d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
