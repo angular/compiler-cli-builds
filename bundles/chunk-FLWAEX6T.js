2205f892d3ed4e38311b268554bca17de1b4d399
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:59:07 +0000

    d3ab1ea build: lock file maintenance

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0-rc.0+sha-fe7b26e";
+var PLACEHOLDER_VERSION = "21.2.0-rc.0+sha-d3ab1ea";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0-rc.0+sha-fe7b26e");
+var VERSION = new Version("21.2.0-rc.0+sha-d3ab1ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
