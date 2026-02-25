3a83b2e45c91796d00ea01168e58db76e405bd1e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 18:52:33 +0000

    e437980 build: update cross-repo angular dependencies

--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-d3ab1ea";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0-rc.0+sha-e437980";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
