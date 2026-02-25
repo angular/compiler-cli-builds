2b7f2b302cae583a7e1e89a8a342712dc37f1186
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 19:36:55 +0000

    b7f03a9 docs: release notes for the v21.2.0 release

--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-0b6ac7f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-b7f03a9";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
