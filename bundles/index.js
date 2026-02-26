ee03675210558a650f1b5ede27948cbb2e9ed37d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 20:48:36 +0000

    9c0587f build: update to latest sha of dev-infra repo

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0+sha-ac06941");
+var VERSION = new Version("21.2.0+sha-9c0587f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
