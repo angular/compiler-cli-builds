f79ccd7adea00e1fbc222b1585bbd0e586b1e243
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 18:03:12 +0000

    b4f68ac build: update repository locations for bazel rules

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.0+sha-d0afeba";
+var PLACEHOLDER_VERSION = "22.0.0-next.0+sha-b4f68ac";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
