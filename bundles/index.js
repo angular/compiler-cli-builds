0f742cbe6bc9591d5aad7f0e21e55f1bd3993acd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:03:09 +0000

    3d3e6cb build: update repository locations for bazel rules

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0+sha-2158bf8");
+var VERSION = new Version("21.2.0+sha-3d3e6cb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
