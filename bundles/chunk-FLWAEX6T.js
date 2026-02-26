b359b9ee0c66f153f88390d50654e40dfd246d40
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 15:59:42 +0000

    f23f3e7 docs: add mention of identity tracking for field iterations

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0+sha-661e800";
+var PLACEHOLDER_VERSION = "21.2.0+sha-f23f3e7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
