6c9ec9d9c0e62874e0eaa620f235bbfb1e77c343
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 16:25:07 +0000

    95f12c8 docs: add info around route level services

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.0+sha-4c4a3db");
+var VERSION = new Version("22.0.0-next.0+sha-95f12c8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
