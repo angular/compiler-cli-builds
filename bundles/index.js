88f493518f4159afc71cf77a820001aa1ab40e70
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 18:38:27 +0000

    6585e4f build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.0+sha-078da3c");
+var VERSION = new Version("22.0.0-next.0+sha-6585e4f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
