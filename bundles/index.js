2ed949f3ed00ed3fff14d57b92a67df079627bd5
Douglas Parker <dgp1130@users.noreply.github.com>
2026-02-26 01:49:21 +0000

    01eadde release: bump Angular DevTools version to 1.12.0

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.0+sha-a57ebc9");
+var VERSION = new Version("22.0.0-next.0+sha-01eadde");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
