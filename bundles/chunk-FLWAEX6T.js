f2aa28acc3fa1b77e40c56d3e4062daeccc989ed
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:52:54 +0000

    23cb9a4 refactor(forms): address feedback

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.0+sha-fc5f9d0";
+var PLACEHOLDER_VERSION = "21.2.0+sha-23cb9a4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/chunk-IG22BDVK.js
+++ bundles/chunk-IG22BDVK.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0+sha-fc5f9d0";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.0+sha-23cb9a4";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
