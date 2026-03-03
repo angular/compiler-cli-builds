ea23c4dd1946ab1b0341b1a4a890f628d4b35baf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:53:58 +0000

    657d532 build: update dependency preact-render-to-string to v6.6.6

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0+sha-796b9a7");
+var VERSION = new Version("21.2.0+sha-657d532");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
