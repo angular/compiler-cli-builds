8a34802eb81ff62b0bfe094d34d2aa071a86ac01
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 16:27:51 +0000

    9e64147 fix(core): prevent child animation elements from being orphaned

--- bundles/chunk-5KB2MYHH.js
+++ bundles/chunk-5KB2MYHH.js
@@ -230,7 +230,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-0c40212";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.0+sha-9e64147";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
