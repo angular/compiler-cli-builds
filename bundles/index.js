d4346f836fbed3ad116935f9c6e0de2a0ad2c792
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 16:27:14 +0000

    b4ec3cc fix(core): prevent child animation elements from being orphaned

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.0+sha-84e79f5");
+var VERSION = new Version("21.2.0+sha-b4ec3cc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
