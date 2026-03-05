8fb0f989cf7e1e067addd476a926698986081d38
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:23:29 +0000

    dd464e5 ci: remove redundant `actions/checkout` steps from dev-infra workflows

--- bundles/chunk-ENV4GMOB.js
+++ bundles/chunk-ENV4GMOB.js
@@ -230,7 +230,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.1+sha-8217ef9";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.1+sha-dd464e5";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
