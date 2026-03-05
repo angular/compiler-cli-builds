4b6ff3bc1c768a908f97e40350ace69967156790
Andrew Scott <atscott01@gmail.com>
2026-03-05 18:20:10 +0000

    9ab80f4 docs: update http client for `withXhr`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.0+sha-e5f99ed");
+var VERSION = new Version("22.0.0-next.0+sha-9ab80f4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
