e601ec356d8fdf5b9f343522b056884d6721b995
Andrew Scott <atscott01@gmail.com>
2026-03-06 19:05:06 +0000

    759ee22 build: update all github actions to v7

--- bundles/index.js
+++ bundles/index.js
@@ -81,7 +81,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-cc64def");
+var VERSION = new Version("22.0.0-next.1+sha-759ee22");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
