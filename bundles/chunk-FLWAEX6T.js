2e4b89eecba3232d8a000954eb583f7b1647e39e
Andrew Scott <atscott01@gmail.com>
2026-03-06 17:54:28 +0000

    e7d1d8d refactor(core): add tracing for component creation

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.1+sha-c822bf8";
+var PLACEHOLDER_VERSION = "21.2.1+sha-e7d1d8d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.1+sha-c822bf8");
+var VERSION = new Version("21.2.1+sha-e7d1d8d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
