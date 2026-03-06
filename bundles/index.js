789f589561c8379247f629837e1f3d075bc434ee
Andrew Scott <atscott01@gmail.com>
2026-03-06 23:40:34 +0000

    276c12a build: update dependency node to v22.22.1

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.1+sha-104ac11");
+var VERSION = new Version("21.2.1+sha-276c12a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
