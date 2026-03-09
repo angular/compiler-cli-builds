9b710cb40612a8e6832eab2b4c9134fff1fa9ff2
Andrew Scott <atscott@google.com>
2026-03-09 17:52:15 +0000

    bd72571 refactor(devtools): prevent spamming the message bus

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.1+sha-628f3f9";
+var PLACEHOLDER_VERSION = "21.2.1+sha-bd72571";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
