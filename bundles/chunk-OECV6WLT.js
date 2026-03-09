64fdfbd26ce3f758a2d07f010996288c48175741
Andrew Scott <atscott@google.com>
2026-03-09 17:27:35 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- bundles/chunk-OECV6WLT.js
+++ bundles/chunk-OECV6WLT.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.1+sha-276c12a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.1+sha-79ac570";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
