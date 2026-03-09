a957b7c1ae4af6f0cc4232fe21bbc043b0fd4bdf
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:46:58 +0000

    71b8159 test(forms): cover transformedValue without FormField context

--- bundles/index.js
+++ bundles/index.js
@@ -82,7 +82,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-bf93ff8");
+var VERSION = new Version("22.0.0-next.1+sha-71b8159");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
