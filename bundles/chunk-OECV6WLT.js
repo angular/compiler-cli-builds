6a5220651def7c34abb6551755cac7a193ec723e
Andrew Scott <atscott@google.com>
2026-03-09 17:36:00 +0000

    628f3f9 docs: add Turkish community translation of Angular documentation

--- bundles/chunk-OECV6WLT.js
+++ bundles/chunk-OECV6WLT.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.1+sha-79ac570";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.1+sha-628f3f9";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
