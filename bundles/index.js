3e0c80b94a701bb3026c67c4909b971460affda2
Andrew Scott <atscott01@gmail.com>
2026-03-10 18:26:11 +0000

    8630319 fix(core): sanitize translated attribute bindings with interpolations

--- bundles/index.js
+++ bundles/index.js
@@ -83,7 +83,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-1866bf5");
+var VERSION = new Version("22.0.0-next.1+sha-8630319");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
