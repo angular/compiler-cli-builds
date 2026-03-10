bdfa88f54c0a8682af73c48a3740d978e1e4ac47
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:08:36 +0000

    9794e75 docs: fix first-app code getHousingLocationById housing.service.ts

--- bundles/chunk-XCV6QTVK.js
+++ bundles/chunk-XCV6QTVK.js
@@ -230,7 +230,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.1+sha-2049698";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.1+sha-9794e75";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
