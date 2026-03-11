707ce3f8aae2827e6ab5c265c1d4ec7a052ccde5
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:20:04 +0000

    4c532bd build: lock file maintenance

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-66e72ef");
+var VERSION = new Version("22.0.0-next.1+sha-4c532bd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
