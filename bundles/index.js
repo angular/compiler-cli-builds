78b9ff956ed3dc7a4a6075f68d36b4f43fc2783d
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:46:01 +0000

    3c2d95a refactor(forms): normalize experimental version tags in signals api

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-b401c18");
+var VERSION = new Version("22.0.0-next.1+sha-3c2d95a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
