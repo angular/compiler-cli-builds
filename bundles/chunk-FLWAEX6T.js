609f6a88b2fabc2e9b884a48e967fead5b61c1c1
Andrew Scott <atscott01@gmail.com>
2026-03-11 20:40:49 +0000

    7907e98 test: remove duplicate tests

--- bundles/chunk-FLWAEX6T.js
+++ bundles/chunk-FLWAEX6T.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.3+sha-e759365";
+var PLACEHOLDER_VERSION = "21.2.3+sha-7907e98";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
