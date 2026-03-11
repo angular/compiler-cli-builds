de3c2e23edb0e38e8e57d3bc5091200c88d4ad0c
Andrew Scott <atscott01@gmail.com>
2026-03-11 20:15:58 +0000

    cfb8aff release: cut the v22.0.0-next.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.1+sha-dea3241");
+var VERSION = new Version("22.0.0-next.2+sha-cfb8aff");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
