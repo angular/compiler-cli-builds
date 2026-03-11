cf97f7a630a3e026d39dce6a9f2e7cf020320372
Andrew Scott <atscott01@gmail.com>
2026-03-11 22:00:48 +0000

    0ab344c docs: release notes for the vscode extension 21.2.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.2+sha-3d25953");
+var VERSION = new Version("22.0.0-next.2+sha-0ab344c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
