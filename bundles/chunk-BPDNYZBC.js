c0adf90f6f02c36e255a9eefbad0cb97ba7afb3e
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 18:36:50 +0000

    02fbf08 fix(compiler): disallow translations of iframe src

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.17+sha-043125c";
+var PLACEHOLDER_VERSION = "20.3.17+sha-02fbf08";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
