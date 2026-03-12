90fd0f8d1d62cc510815fbf69a4c2ef94910a77f
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:04:51 +0000

    8017617 release: cut the v21.2.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.3+sha-ed2d324");
+var VERSION = new Version("21.2.4+sha-8017617");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
