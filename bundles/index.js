e7ab4f73f87cd1c08bbe2f97d51e90d6d76b5156
Matthew Beck <me@mattrb.com>
2026-03-12 23:44:57 +0000

    6f41efc refactor(core): Update determineLongestAnimation to account for playback rate

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.4+sha-bfb8b17");
+var VERSION = new Version("21.2.4+sha-6f41efc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
