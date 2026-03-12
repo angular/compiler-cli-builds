401a1462edf2bc5df0264fc0644074dc8a8097aa
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:32:21 +0000

    432a3de docs: release notes for the v20.3.18 release

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.3+sha-4cbc1a1");
+var VERSION = new Version("22.0.0-next.3+sha-432a3de");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
