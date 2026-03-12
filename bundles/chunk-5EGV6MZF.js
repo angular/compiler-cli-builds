0a3e70a5a86232e0c8e1370ba08678afdb684d14
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 23:42:04 +0000

    17d8a88 refactor(core): Update determineLongestAnimation to account for playback rate

--- bundles/chunk-5EGV6MZF.js
+++ bundles/chunk-5EGV6MZF.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.3+sha-89c9a4d";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.3+sha-17d8a88";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/chunk-HFKTR5QY.js
+++ bundles/chunk-HFKTR5QY.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.3+sha-89c9a4d";
+var PLACEHOLDER_VERSION = "22.0.0-next.3+sha-17d8a88";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
