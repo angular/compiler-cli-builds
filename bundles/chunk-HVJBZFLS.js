ae42bd5131986c1793e6bd8b2dfd543bc7a495ce
Matthew Beck <me@mattrb.com>
2026-03-17 20:06:22 +0000

    a8f80c1 refactor(core): declare explicit reactive node prototypes types

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.4+sha-67e0ba7";
+var PLACEHOLDER_VERSION = "21.2.4+sha-a8f80c1";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.4+sha-67e0ba7");
+var VERSION = new Version("21.2.4+sha-a8f80c1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
