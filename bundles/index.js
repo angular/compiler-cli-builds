f1316f1ec4781790a3f91fe2e3474fcf50926432
Matthew Beck (Berry) <me@mattrb.com>
2026-03-17 20:07:56 +0000

    1eaf920 refactor(core): declare explicit reactive node prototypes types

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.3+sha-9769560");
+var VERSION = new Version("22.0.0-next.3+sha-1eaf920");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
