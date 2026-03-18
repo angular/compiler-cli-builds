25d80ddfe723d3b5901d656f5403d3a61b409b3e
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 15:47:57 +0000

    0e496c1 docs(docs-infra): improve semantic HTML in update guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.4+sha-737777e");
+var VERSION = new Version("21.2.4+sha-0e496c1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
