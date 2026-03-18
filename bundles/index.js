23cf2d8ab939f558ae7a183aa98ef71b4a745a05
Matthew Beck <me@mattrb.com>
2026-03-18 17:36:58 +0000

    851ef77 Revert "refactor(core): Ensure determineLongestAnimation is run synchronously after style applies"

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.4+sha-0e496c1");
+var VERSION = new Version("21.2.4+sha-851ef77");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
