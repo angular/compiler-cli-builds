d6ff36dc22903439acc63cc936b925737e562568
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 20:16:38 +0000

    f02be58 docs: update cross-repo adev docs

--- bundles/chunk-BCQGTZBZ.js
+++ bundles/chunk-BCQGTZBZ.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.3+sha-890c973";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.3+sha-f02be58";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
