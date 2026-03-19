f44d7158a442e1129b0b0609719f4d0f1ceb3334
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 22:11:30 +0000

    c9e6263 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.4+sha-61967d7");
+var VERSION = new Version("22.0.0-next.4+sha-c9e6263");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
