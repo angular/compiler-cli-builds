c6bb27a19b95eb80e76967315c3f8e52fcf54446
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 22:59:06 +0000

    ad0156e fix(core): fixes a regression with animate.leave and reordering

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.5+sha-aa3220a");
+var VERSION = new Version("21.2.5+sha-ad0156e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
