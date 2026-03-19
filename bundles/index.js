2a1d0158711b7cccfa56eb4d45f7f64b2fce4e1d
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:01:54 +0000

    836bf20 build: exclude formating commit

--- bundles/index.js
+++ bundles/index.js
@@ -85,7 +85,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.4+sha-41b1410");
+var VERSION = new Version("22.0.0-next.4+sha-836bf20");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
