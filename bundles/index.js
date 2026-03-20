d6bb271c2a2e1f0bfbcaf3fc49721bb4252a9859
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-20 22:59:35 +0000

    a21be36 refactor: prepare for required `changeDetection` prop on G3.

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.5+sha-7aaa8b9");
+var VERSION = new Version("21.2.5+sha-a21be36");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
