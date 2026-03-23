49dbb3508a57491aebec46fe8619e9fc727c5b21
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 21:55:38 +0000

    c7a1892 fix(devtools): fix incorrect logic in destroy function for ChromeMessageBus

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.5+sha-b658acc";
+export declare const PLACEHOLDER_VERSION = "21.2.5+sha-c7a1892";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
