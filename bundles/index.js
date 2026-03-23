cceb765ba163a5db245e810f3fdbddf74eefd602
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:31:27 +0000

    b658acc docs: fix js doc of signal forms ignoreValidators option.

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.5+sha-8818095");
+var VERSION = new Version("21.2.5+sha-b658acc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
