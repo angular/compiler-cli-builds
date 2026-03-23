603b63efe9e6535d2f6d4239d388c9ad3b69dc1d
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 16:55:38 +0000

    a821e06 ci: support publishing dev-skills to snapshot repository

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.5+sha-a21be36";
+var PLACEHOLDER_VERSION = "21.2.5+sha-a821e06";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
