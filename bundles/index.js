f65b1a5c154e7dcb322f9f0c57bb0852c2019d36
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 22:42:43 +0000

    050b14b docs: fix incorrect getHarnesses() method to getAllHarnesses and broken link

--- bundles/index.js
+++ bundles/index.js
@@ -86,7 +86,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.4+sha-d9712e6");
+var VERSION = new Version("22.0.0-next.4+sha-050b14b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
