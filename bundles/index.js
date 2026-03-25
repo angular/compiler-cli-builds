02c604d3e30062abae9dc298b082efdd830e760b
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 20:13:48 +0000

    a6c49e0 refactor(language-service): Export template target from API

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.5+sha-2c7d3b5");
+var VERSION = new Version("21.2.5+sha-a6c49e0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
