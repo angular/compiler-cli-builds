faafb164d8895f0df8445ca54045036867db427b
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 21:26:20 +0000

    24e52d4 feat(forms): add debounce option to validateAsync and validateHttp

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.4+sha-b5af15a";
+var PLACEHOLDER_VERSION = "22.0.0-next.4+sha-24e52d4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -86,7 +86,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.4+sha-b5af15a");
+var VERSION = new Version("22.0.0-next.4+sha-24e52d4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
