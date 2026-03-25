b74afc35f8e0c5ad640c35fd6121c983f3ba9828
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 22:51:51 +0000

    f187d23 docs: release notes for the v21.2.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -86,7 +86,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.4+sha-24e52d4");
+var VERSION = new Version("22.0.0-next.4+sha-f187d23");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
