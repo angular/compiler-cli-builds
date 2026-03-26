ef48c0e85ff8400a5909c0d4c71b037361ca221c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:41:04 +0000

    13f050d test: construct local `Date` objects to fix timezone flakiness

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-d8239c7");
+var VERSION = new Version("21.2.6+sha-13f050d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
