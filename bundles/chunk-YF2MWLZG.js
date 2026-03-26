b116bc3285c117d7873657774b90ed24f0fabcda
Andrew Scott <atscott01@gmail.com>
2026-03-26 21:40:51 +0000

    b453c0c refactor(language-service): Update getTcbNodesOfTemplateAtPosition to be usable without compiler (#67898)

--- bundles/chunk-YF2MWLZG.js
+++ bundles/chunk-YF2MWLZG.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.6+sha-807cabd";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.6+sha-b453c0c";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
