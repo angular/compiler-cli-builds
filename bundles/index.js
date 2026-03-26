144775ef612489cf2b2123bb0f4038c0d7cd8f59
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:52:12 +0000

    807cabd fix(dev-infra): improve PR review skill handling of git worktrees

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-13f050d");
+var VERSION = new Version("21.2.6+sha-807cabd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
