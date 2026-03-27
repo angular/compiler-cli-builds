770eafe1bac31149268859db96736c1c41cb0b5e
GitHub <noreply@github.com>
2026-03-27 16:23:57 +0000

    9d79ec6 docs(docs-infra): introduce a custom UrlSerializer

--- bundles/chunk-SSEPJ4ZG.js
+++ bundles/chunk-SSEPJ4ZG.js
@@ -230,7 +230,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.5+sha-fcd0bb0";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.5+sha-9d79ec6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-fcd0bb0");
+var VERSION = new Version("22.0.0-next.5+sha-9d79ec6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
