4d528005c3220b1f9ae7ec9faca31a75cc8bd018
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 13:07:18 +0000

    c8902de build: update all github actions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-b453c0c");
+var VERSION = new Version("21.2.6+sha-c8902de");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
