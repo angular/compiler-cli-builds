ea9f4933916c76ee890a644ad54a68644fd8cc4b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:49:32 +0000

    c950299 build: update pnpm to v10.33.0

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.0-next.5+sha-0f960a5";
+export declare const PLACEHOLDER_VERSION = "22.0.0-next.5+sha-c950299";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
