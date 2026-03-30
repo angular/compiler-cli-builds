4d6bcf68697cb4add1a7e3bf27726a4828bfc148
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 08:48:21 +0000

    c6f73f4 docs: update url from `<base href>`

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-86bbc94");
+var VERSION = new Version("22.0.0-next.5+sha-c6f73f4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
