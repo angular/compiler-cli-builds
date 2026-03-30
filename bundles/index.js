a3bb528b2ec947cbbe801af09a7c7af1d84b8b4b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:20:20 +0000

    0f960a5 docs(docs-infra): sanitize markdown tooltip in Code editor

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-60930f8");
+var VERSION = new Version("22.0.0-next.5+sha-0f960a5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
