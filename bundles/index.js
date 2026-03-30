9d37e6a0568e31e4fd8640087da86b5874463ff9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 10:51:20 +0000

    cf7846b build: update pnpm to v10.33.0

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-9ea8cb6");
+var VERSION = new Version("21.2.6+sha-cf7846b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
