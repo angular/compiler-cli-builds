1368a0360bd444d673cdfd7b82b737e19dfeda88
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:17:27 +0000

    efbba94 docs: align list formatting and improve emphasis

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-94b1b53");
+var VERSION = new Version("21.2.6+sha-efbba94");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
