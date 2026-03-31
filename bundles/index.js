c0cbd61c024038202410fd649d0a787809b2cdb6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 10:00:25 +0000

    1a67858 docs: modernize CLI reference for build and test

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-c950299");
+var VERSION = new Version("22.0.0-next.5+sha-1a67858");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
