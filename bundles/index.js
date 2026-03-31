c0f0cdf0f8f6f65444e6afaae524d24615938ad0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 11:58:10 +0000

    895c576 refactor(forms): use strict equality for pending status getter

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-5fd4f0f");
+var VERSION = new Version("21.2.6+sha-895c576");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
