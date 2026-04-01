88a7a31ff7558aa5637c684bef6e68ad13493e8c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 06:44:30 +0000

    7ba8929 test: fix flakiness in `image-directive` e2e tests

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-ef7679b");
+var VERSION = new Version("22.0.0-next.5+sha-7ba8929");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
