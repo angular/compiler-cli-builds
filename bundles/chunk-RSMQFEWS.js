15064419417fe057e081b801c39e9784d016b158
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 19:02:04 +0000

    0b08e29 test(core): refactors test to use `timeout` utility

--- bundles/chunk-RSMQFEWS.js
+++ bundles/chunk-RSMQFEWS.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.6+sha-f363ab4";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.7+sha-0b08e29";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
