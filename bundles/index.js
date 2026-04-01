578c33e5595d23d5e311def78012435211b7f7dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 16:37:54 +0000

    436c5df build: update cross-repo angular dependencies to v22.0.0-next.4

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.5+sha-1f3ac4a");
+var VERSION = new Version("22.0.0-next.5+sha-436c5df");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
