072d56c447b41647dae3792382a11dcf59096002
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 07:31:39 +0000

    f916531 docs(docs-infra): improve angular-new-app skill

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.6+sha-895c576");
+var VERSION = new Version("21.2.6+sha-f916531");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
