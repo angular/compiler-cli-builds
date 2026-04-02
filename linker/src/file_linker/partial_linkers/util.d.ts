34b7c62633300f5e21bec4ce2b5e501f43674642
Andrew Scott <atscott01@gmail.com>
2026-04-02 22:07:32 +0000

    30f63fc refactor(core): address review comments on NG0750 error message

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.0-next.6+sha-8216d34";
+export declare const PLACEHOLDER_VERSION = "22.0.0-next.6+sha-30f63fc";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
