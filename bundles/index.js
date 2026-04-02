3c2b6d9440de52b7a733981ba58992e86857b119
Andrew Scott <atscott01@gmail.com>
2026-04-02 22:54:17 +0000

    164cf98 docs(docs-infra): Enhances update guide layout responsiveness

--- bundles/index.js
+++ bundles/index.js
@@ -90,7 +90,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-30f63fc");
+var VERSION = new Version("22.0.0-next.6+sha-164cf98");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
