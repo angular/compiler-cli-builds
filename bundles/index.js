98a065d6f7470a186172805e4703e3a2a0dbf3c0
Andrew Scott <atscott01@gmail.com>
2026-04-02 20:05:26 +0000

    05bcc60 build: update all github actions

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.7+sha-9f61910");
+var VERSION = new Version("21.2.7+sha-05bcc60");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
