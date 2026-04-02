fa295e0570835799e505ee8ed83fed43b65951d0
Andrew Scott <atscott01@gmail.com>
2026-04-02 18:53:16 +0000

    11f047f build: update all non-major dependencies

--- bundles/chunk-SSEPJ4ZG.js
+++ bundles/chunk-SSEPJ4ZG.js
@@ -230,7 +230,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.6+sha-36966ba";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.6+sha-11f047f";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
