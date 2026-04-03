e1123170beade1c1e5d206dd259ac6513ec39cd6
Andrew Scott <atscott01@gmail.com>
2026-04-03 17:24:30 +0000

    394ad0c fix(forms): allow late-bound input types for signals forms

--- bundles/index.js
+++ bundles/index.js
@@ -89,7 +89,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-9c55fcb");
+var VERSION = new Version("22.0.0-next.6+sha-394ad0c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
