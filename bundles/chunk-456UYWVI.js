9a0e57e6922ad218467c1c514aacf7103d1d531d
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:34:48 +0000

    47cd604 build: update bazel dependencies

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-789c2cd";
+var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-47cd604";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
