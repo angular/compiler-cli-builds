ecd7c51c59a702c32ab4c93168d48f425dc1faa3
Andrew Scott <atscott01@gmail.com>
2026-04-03 23:50:55 +0000

    4a174b8 test(platform-server): fix race condition in incremental hydration test

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-b4a3abd";
+var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-4a174b8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
