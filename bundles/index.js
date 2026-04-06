4fa739db50798fec1a3e5d95833c94c3ffb77051
Andrew Scott <atscott01@gmail.com>
2026-04-06 19:37:48 +0000

    fd95735 refactor(compiler-cli): fix failing tests

--- bundles/index.js
+++ bundles/index.js
@@ -89,7 +89,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-f3c471e");
+var VERSION = new Version("22.0.0-next.6+sha-fd95735");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
