aea907220037c2ba502443aa0b14dd47db71be8d
Andrew Scott <atscott01@gmail.com>
2026-04-06 16:28:02 +0000

    ecae525 Revert "refactor(core): remove ComponentFactoryResolver & ComponentFactory from the api surface"

--- bundles/index.js
+++ bundles/index.js
@@ -89,7 +89,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-4a174b8");
+var VERSION = new Version("22.0.0-next.6+sha-ecae525");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
