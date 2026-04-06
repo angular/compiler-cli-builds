d01a03b67a659879bbf805203c67b9b3fa6b9abf
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:41:01 +0000

    33b001d fix(devtools): clean up removed directive entries in IdentityTracker to prevent memory leak

--- bundles/index.js
+++ bundles/index.js
@@ -89,7 +89,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-2cb67c0");
+var VERSION = new Version("22.0.0-next.6+sha-33b001d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
