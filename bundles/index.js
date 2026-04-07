07c5ffcd95d4190ee72f1afa3e6176dcaf21f082
Andrew Scott <atscott01@gmail.com>
2026-04-07 16:56:45 +0000

    1c9c453 docs(docs-infra): fix v21 event video not responsive on mobile

--- bundles/index.js
+++ bundles/index.js
@@ -51,6 +51,7 @@ import {
   OutOfBadDiagnosticCategory,
   RegistryDomSchemaChecker,
   SymbolBuilder,
+  SymbolKind,
   TcbGenericContextBehavior,
   TsCreateProgramDriver,
   createHostElement,
@@ -90,7 +91,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-236d6d4");
+var VERSION = new Version("22.0.0-next.6+sha-1c9c453");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
@@ -219,6 +220,7 @@ export {
   RegistryDomSchemaChecker,
   SOURCE,
   SymbolBuilder,
+  SymbolKind,
   TcbGenericContextBehavior,
   UNKNOWN_ERROR_CODE,
   VERSION,
--- private/hybrid_analysis.d.ts
+++ private/hybrid_analysis.d.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 export { generateTypeCheckBlock } from '../src/ngtsc/typecheck/src/type_check_block';
-export { type TypeCheckingConfig, type TcbComponentMetadata, type TcbTypeCheckBlockMetadata, type TcbTypeParameter, type TypeCheckId, type TcbDirectiveMetadata, type TemplateDiagnostic, type TcbReferenceMetadata, type SourceMapping, type OutOfBandDiagnosticRecorder, type DomSchemaChecker, OutOfBadDiagnosticCategory, } from '../src/ngtsc/typecheck/api';
+export { type TypeCheckingConfig, type TcbComponentMetadata, type TcbTypeCheckBlockMetadata, type TcbTypeParameter, type TypeCheckId, type TcbDirectiveMetadata, type TemplateDiagnostic, type TcbReferenceMetadata, type SourceMapping, type OutOfBandDiagnosticRecorder, type DomSchemaChecker, OutOfBadDiagnosticCategory, SymbolKind, } from '../src/ngtsc/typecheck/api';
 export { RegistryDomSchemaChecker } from '../src/ngtsc/typecheck/src/dom';
 export { Environment } from '../src/ngtsc/typecheck/src/environment';
 export { TcbGenericContextBehavior } from '../src/ngtsc/typecheck/src/ops/context';
