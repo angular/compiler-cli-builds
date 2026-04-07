317cbd99244bbeca9b1f9adf7519f2195f77b7eb
Andrew Scott <atscott01@gmail.com>
2026-04-07 18:33:30 +0000

    77f1ca0 fix(core): handle missing serialized container hydration data

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.0-next.6+sha-2ff9db3";
+export declare const PLACEHOLDER_VERSION = "22.0.0-next.6+sha-77f1ca0";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
