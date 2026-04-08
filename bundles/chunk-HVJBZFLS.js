c192f7caad3184b340a6ffdc6d58da62c11fa91c
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:38:19 +0000

    057cc6d fix(core): remove obsolete iOS cursor pointer hack in event delegation

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.7+sha-dea76c7";
+var PLACEHOLDER_VERSION = "21.2.7+sha-057cc6d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
