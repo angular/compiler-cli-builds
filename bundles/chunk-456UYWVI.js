82b96d741427216d07883d64d8582a4c01f76ea4
Andrew Scott <atscott01@gmail.com>
2026-04-08 19:46:14 +0000

    f95d757 docs: release notes for the v21.2.8 release

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-51c3a11";
+var PLACEHOLDER_VERSION = "22.0.0-next.6+sha-f95d757";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
