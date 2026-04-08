ebc920db2f440009555df49ba56a3380e2e58f75
Douglas Parker <dgp1130@users.noreply.github.com>
2026-04-08 19:24:09 +0000

    51c3a11 release: bump Angular DevTools version to 1.14.1

--- bundles/index.js
+++ bundles/index.js
@@ -91,7 +91,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.6+sha-6fee651");
+var VERSION = new Version("22.0.0-next.6+sha-51c3a11");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
