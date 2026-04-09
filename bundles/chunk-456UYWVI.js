d05c92ed92d3edcd728d3f393688181b8380374d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:18:18 +0000

    b9265cc docs: update typescript version requirement for angular 22

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.7+sha-1a16a40";
+var PLACEHOLDER_VERSION = "22.0.0-next.7+sha-b9265cc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
