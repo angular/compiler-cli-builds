a16d3a3900ac22ac7624d38c1e565e7c2e7f199d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 12:46:05 +0000

    db9f9a8 build: update all non-major dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -91,7 +91,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-5516769");
+var VERSION = new Version("22.0.0-next.7+sha-db9f9a8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
