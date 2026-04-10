5173ac8f6211aca8e12f0029274b2dd9effbd1f2
kirjs <kirts@google.com>
2026-04-10 13:53:13 +0000

    b52a626 refactor: add flaky tests workflow

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-dc95814");
+var VERSION = new Version("21.2.8+sha-b52a626");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
