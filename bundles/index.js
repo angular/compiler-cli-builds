b0da1a31cb1733e60e515a9a601ff9dd4a66e661
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 19:06:31 +0000

    a89b565 refactor(language-service): Avoid deep import of comments helper

--- bundles/index.js
+++ bundles/index.js
@@ -91,7 +91,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-e453848");
+var VERSION = new Version("22.0.0-next.7+sha-a89b565");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
