add805e970298fa3f1911150c94ac2f892f6682c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 08:03:20 +0000

    0304228 docs: add documentation for NG1002

--- bundles/index.js
+++ bundles/index.js
@@ -91,7 +91,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-836094c");
+var VERSION = new Version("22.0.0-next.7+sha-0304228");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
