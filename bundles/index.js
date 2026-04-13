e339a96ba87e05d7448733f9c8255ede56195c25
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 10:29:45 +0000

    f25c7ce docs(forms): update signal forms migration guide

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-5f74e7e");
+var VERSION = new Version("22.0.0-next.7+sha-f25c7ce");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
