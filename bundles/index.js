30bf837266ad0376d69c2b04ad12b8f8580775ec
kirjs <kirts@google.com>
2026-04-13 10:29:46 +0000

    b72b6b4 docs(forms): update signal forms migration guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-3ed14c6");
+var VERSION = new Version("21.2.8+sha-b72b6b4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
