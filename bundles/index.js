be24d83820a4cd7b023fd96b68e5efc45cea12a5
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:40:46 +0000

    5f74e7e docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-973ede6");
+var VERSION = new Version("22.0.0-next.7+sha-5f74e7e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
