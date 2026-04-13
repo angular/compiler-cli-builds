d52428111fc5d78fd37933f2d6678ddbb26bd47a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 11:17:48 +0000

    8ce9cc4 feat(core): register AI runtime debugging tools

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-70368ea");
+var VERSION = new Version("22.0.0-next.7+sha-8ce9cc4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
