6f6e3ecd615db2e926721831396309d5a0926334
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 18:03:44 +0000

    eb2b06f docs: draft PR spam policy addendum

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-0454d4c");
+var VERSION = new Version("22.0.0-next.7+sha-eb2b06f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
