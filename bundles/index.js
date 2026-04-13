c403e7335341434cb7e79cb5f7819fbb99ecc557
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:28:48 +0000

    973ede6 refactor: Mobile layout api reference Fix mobile layout shift in API reference

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-59513b7");
+var VERSION = new Version("22.0.0-next.7+sha-973ede6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
