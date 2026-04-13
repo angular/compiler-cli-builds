eefeb4b1056c358c7669b81104a1b5de3c7edfad
kirjs <kirts@google.com>
2026-04-13 18:04:09 +0000

    aa5d237 docs: draft PR spam policy addendum

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-540536c");
+var VERSION = new Version("21.2.8+sha-aa5d237");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
