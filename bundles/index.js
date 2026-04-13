89790509f548a1ba3c25754ce02d41277aa97316
kirjs <kirts@google.com>
2026-04-13 08:06:06 +0000

    e2e7211 docs(docs-infra): consolidate tab menu margins for phone breakpoint

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-b351d49");
+var VERSION = new Version("21.2.8+sha-e2e7211");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
