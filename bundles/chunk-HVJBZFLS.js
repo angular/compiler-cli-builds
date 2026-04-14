386d8f279856517ecc35bf6cf088525461fb6d42
kirjs <kirts@google.com>
2026-04-14 15:35:01 +0000

    c3d69ae fix(docs-infra): prevent inline code wrapping in CLI reference table

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.8+sha-684e9fd";
+var PLACEHOLDER_VERSION = "21.2.8+sha-c3d69ae";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-684e9fd");
+var VERSION = new Version("21.2.8+sha-c3d69ae");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
