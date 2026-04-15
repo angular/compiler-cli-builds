f7c17418ba08a3acacf0d65f84163a3b5e66c4e2
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 14:59:25 +0000

    8b4581d build: lock file maintenance

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-e32159b");
+var VERSION = new Version("22.0.0-next.7+sha-8b4581d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
