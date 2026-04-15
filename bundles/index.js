94760d23bc0667240fcc84be9fbe673e6f8e5650
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 19:47:28 +0000

    79c9818 docs(router): document .. traversal and relativeTo pitfalls in router.navigate()

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-034b12f");
+var VERSION = new Version("22.0.0-next.7+sha-79c9818");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
