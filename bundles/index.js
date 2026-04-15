7fbda437ca83ba308eda719c1ee6af8919990cd4
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 14:31:41 +0000

    b2cff79 docs(docs-infra): add background to playground template dropdown

--- bundles/index.js
+++ bundles/index.js
@@ -105,7 +105,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.7+sha-75f2cb8");
+var VERSION = new Version("22.0.0-next.7+sha-b2cff79");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
