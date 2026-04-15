2b47d1c2bb8254e00ef55712070797d4b2899462
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 22:33:44 +0000

    3579ea0 docs: release notes for the v19.2.21 release

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.8+sha-9bead58");
+var VERSION = new Version("22.0.0-next.8+sha-3579ea0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
