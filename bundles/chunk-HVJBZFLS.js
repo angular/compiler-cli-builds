0d66a46257482057a718c602be7d83ee2db1fb85
kirjs <kirts@google.com>
2026-04-15 14:31:43 +0000

    175343d docs(docs-infra): add background to playground template dropdown

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.8+sha-c3d69ae";
+var PLACEHOLDER_VERSION = "21.2.8+sha-175343d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
