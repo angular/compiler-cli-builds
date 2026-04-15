62651f09cc21c22ba69f330c6d3458d7375e16ad
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 21:40:54 +0000

    e14d5ea release: cut the v21.2.9 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.8+sha-528a93a");
+var VERSION = new Version("21.2.9+sha-e14d5ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
