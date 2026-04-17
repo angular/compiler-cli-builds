7b50317ccecead5574605c2a60847a5e40a13d5e
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 21:42:38 +0000

    d771a65 refactor: Improve hydration mismatch errors for third-party scripts

--- bundles/chunk-USW6NVU3.js
+++ bundles/chunk-USW6NVU3.js
@@ -231,7 +231,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-67e4d19";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-d771a65";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
