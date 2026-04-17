bba8b485e6cf9df0206380424eab5f3ab3541033
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 18:05:08 +0000

    67e4d19 build: update all github actions

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.8+sha-3579ea0");
+var VERSION = new Version("22.0.0-next.8+sha-67e4d19");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
