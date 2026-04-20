d2f36732061d554467874507ebbef9ecc84cfe87
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:36:29 +0000

    be74909 docs: improve clarity in dynamic forms guide

--- bundles/chunk-O3K5OHL7.js
+++ bundles/chunk-O3K5OHL7.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.9+sha-810fb73";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.9+sha-be74909";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.9+sha-810fb73");
+var VERSION = new Version("21.2.9+sha-be74909");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
