24f59860a3504bfc1d38aa169189bae52975b62f
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:36:38 +0000

    a718e18 docs: improve clarity in dynamic forms guide

--- bundles/chunk-456UYWVI.js
+++ bundles/chunk-456UYWVI.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.8+sha-a0d4563";
+var PLACEHOLDER_VERSION = "22.0.0-next.8+sha-a718e18";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
