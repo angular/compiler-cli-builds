d133d1955c2e6f1c93813eae7850b93eb09aec0a
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:02:09 +0000

    c9215b3 Revert "refactor(core): complete removal of deprecated `createNgModuleRef` alias"

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.9+sha-e5b93ea";
+var PLACEHOLDER_VERSION = "21.2.9+sha-c9215b3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.9+sha-e5b93ea");
+var VERSION = new Version("21.2.9+sha-c9215b3");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
