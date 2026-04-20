93d1d7dee38c3f50179b3a90136874b80eb25c16
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:36:41 +0000

    d27e2c2 docs: warn against storing secrets in environment files

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.8+sha-c04c0b9");
+var VERSION = new Version("22.0.0-next.8+sha-d27e2c2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
