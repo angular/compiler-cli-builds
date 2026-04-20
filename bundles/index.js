3b4b4cd7307b75f905348f74ff200535a7349db3
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:58:59 +0000

    e5b93ea docs: fix wording in reactive forms guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.9+sha-ce883d9");
+var VERSION = new Version("21.2.9+sha-e5b93ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
