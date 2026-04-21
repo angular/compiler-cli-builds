3e2c3d46243f6889386ecf9d75e73353356a3a18
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 17:32:10 +0000

    4dc7bf5 build: lock file maintenance

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.9+sha-751e4af");
+var VERSION = new Version("21.2.9+sha-4dc7bf5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
