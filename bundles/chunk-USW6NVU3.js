c9ece8e0412f88d869c60c63f6c7b9be6da3521a
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 16:07:51 +0000

    fa6a3d2 docs: update builder docs

--- bundles/chunk-USW6NVU3.js
+++ bundles/chunk-USW6NVU3.js
@@ -231,7 +231,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-6eff439";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-fa6a3d2";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
