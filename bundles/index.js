0013a73c04fc41904988a7c892efd174bbc321cd
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 17:15:49 +0000

    b395173 fix(migrations): fix NgClass leaving trailing comma after removal

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.8+sha-09b9a62");
+var VERSION = new Version("22.0.0-next.8+sha-b395173");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
