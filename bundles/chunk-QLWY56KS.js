535d74dadf4878618de293f2e84927c08d246c6f
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 21:48:53 +0000

    7f3f3d7 ci: remove remainings of saucelabs tests

--- bundles/chunk-QLWY56KS.js
+++ bundles/chunk-QLWY56KS.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-2c141c0";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.8+sha-7f3f3d7";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.8+sha-2c141c0");
+var VERSION = new Version("22.0.0-next.8+sha-7f3f3d7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
