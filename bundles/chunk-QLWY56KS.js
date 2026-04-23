aab9695edb5798f59e22c724414f72fa8f35c4bf
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 19:49:56 +0000

    5b4453b docs: update cross-repo adev docs

--- bundles/chunk-QLWY56KS.js
+++ bundles/chunk-QLWY56KS.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-6120d31";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-5b4453b";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
