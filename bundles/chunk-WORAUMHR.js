88e9377e7884b7dea1dfa3f687b800f6b699714e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 18:47:08 +0000

    6120d31 docs: link Angular CLI in installation prerequisites

--- bundles/chunk-WORAUMHR.js
+++ bundles/chunk-WORAUMHR.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.9+sha-c64ee96";
+var PLACEHOLDER_VERSION = "22.0.0-next.9+sha-6120d31";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
