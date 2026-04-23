0162d08d697ef4187463c41d93df6c0b0f8cca8c
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 21:21:19 +0000

    218e2d0 build: update cross-repo angular dependencies to v22.0.0-next.6

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.9+sha-5b4453b");
+var VERSION = new Version("22.0.0-next.9+sha-218e2d0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
