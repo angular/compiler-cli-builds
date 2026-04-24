0c3f6080582cf77ce53306a474034424af672b9b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 23:03:46 +0000

    08930e6 docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.9+sha-ac9babe");
+var VERSION = new Version("22.0.0-next.9+sha-08930e6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
