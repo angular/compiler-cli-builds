35e95aafbb999015634e3fe5309f18e48f181fcb
Alon Mishne <amishne@users.noreply.github.com>
2026-04-24 17:39:56 +0000

    6662724 docs(docs-infra): improve Playground card on installation page

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.10+sha-8ce8b93";
+var PLACEHOLDER_VERSION = "21.2.10+sha-6662724";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.10+sha-8ce8b93");
+var VERSION = new Version("21.2.10+sha-6662724");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
