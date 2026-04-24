5149b5df22c9046ec227239ee619706259c9a812
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 17:17:45 +0000

    adbd1ab docs: update service documentation to include `@Service` decorator details

--- bundles/chunk-QLWY56KS.js
+++ bundles/chunk-QLWY56KS.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-9203aca";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-adbd1ab";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
