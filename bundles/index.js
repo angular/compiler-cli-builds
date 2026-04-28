f616588f79dfb999016af476385c926225c5c386
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 19:51:25 +0000

    8f8972b feat(migrations): model + output migrations becomes input + linkedSignal

--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.9+sha-5dfe37d");
+var VERSION = new Version("22.0.0-next.9+sha-8f8972b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
