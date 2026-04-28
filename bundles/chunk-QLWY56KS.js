2efc95f8efc03cc975d0f670e1c4079d7ad5ffbf
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 20:13:32 +0000

    ef38017 build: move devtools only deps out of the workspace

--- bundles/chunk-QLWY56KS.js
+++ bundles/chunk-QLWY56KS.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-8f8972b";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.9+sha-ef38017";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -101,7 +101,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.9+sha-8f8972b");
+var VERSION = new Version("22.0.0-next.9+sha-ef38017");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
