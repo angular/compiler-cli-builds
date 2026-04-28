b069ef7d1050b7091e6bf085c07f6697777c0f18
Alon Mishne <amishne@users.noreply.github.com>
2026-04-28 00:24:56 +0000

    a3d14fc docs: remove duplicated text

--- bundles/chunk-O3K5OHL7.js
+++ bundles/chunk-O3K5OHL7.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.10+sha-27da56e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.10+sha-a3d14fc";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
