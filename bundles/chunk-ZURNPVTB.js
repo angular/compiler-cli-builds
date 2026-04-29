99c4deef46ca2fbc6c71d6899d89ae2b11633798
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 22:46:47 +0000

    662560e docs: release notes for the v21.2.11 release

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.9+sha-13c0422";
+var PLACEHOLDER_VERSION = "22.0.0-next.9+sha-662560e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
