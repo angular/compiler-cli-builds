88464c173ccd1562c3bdfa22cfc725de2f75f255
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-04-30 23:19:32 +0000

    2b62458 ci: update PullApprove config

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.10+sha-ebffd80";
+var PLACEHOLDER_VERSION = "22.0.0-next.10+sha-2b62458";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-ebffd80");
+var VERSION = new Version("22.0.0-next.10+sha-2b62458");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
