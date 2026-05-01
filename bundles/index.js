fb58da57e78e5909bdd40e1ab80197e75d5039f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-01 18:12:23 +0000

    a4145de Revert "fix(language-service): Add support for `@Input` with transforms"

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-2b62458");
+var VERSION = new Version("22.0.0-next.10+sha-a4145de");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
