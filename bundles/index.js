1adef9b76c51cca139dbe8a382f81a16385308f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-04 20:15:55 +0000

    4f048e7 build: update dependency typescript to v6.0.3

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-c84642a");
+var VERSION = new Version("22.0.0-next.10+sha-4f048e7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
