ef4342c224b95b882718875d7bb12a17554bca0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 19:52:46 +0000

    8a7f955 docs: correct "Angular JS" to "AngularJS"

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-708631f");
+var VERSION = new Version("22.0.0-next.10+sha-8a7f955");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
