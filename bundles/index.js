5851bee1f2a7e9f4540fa51b21e5d798ac627ad1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 23:37:21 +0000

    3524de2 fix(forms): Add support for range type with outside of native bounds

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-f81fa6e");
+var VERSION = new Version("22.0.0-next.10+sha-3524de2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
