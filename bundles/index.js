d31ce5fbc63077b8c0d6389bb32ebdcb8be863c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 18:22:59 +0000

    708631f fix(forms): prohibit concurrent submits in signal forms

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.10+sha-fc52633");
+var VERSION = new Version("22.0.0-next.10+sha-708631f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
