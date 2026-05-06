612277b5ebeeb187cc92aefb0b95c8c1d7f6de50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 23:50:26 +0000

    e3dc87c release: cut the zone.js-0.16.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-853a91e");
+var VERSION = new Version("22.1.0-next.0+sha-e3dc87c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
