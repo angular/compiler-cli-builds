6d3ab1ca1cf9090d5736e5098b920a550965658f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:55:31 +0000

    5316753 release: cut the v21.2.12 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.11+sha-9e38ed7");
+var VERSION = new Version("21.2.12+sha-5316753");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
