f1ae7c066e13bc85ae45bfed6df89e947c996c21
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 00:14:09 +0000

    9e38ed7 fix(core): sanitizer typings

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.11+sha-1aeebbe");
+var VERSION = new Version("21.2.11+sha-9e38ed7");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
