f68ff1d7be1f5722ea369ec30aaddcf89f53355f
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:51:14 +0000

    609f930 docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.11+sha-a3d4631");
+var VERSION = new Version("22.0.0-next.11+sha-609f930");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
