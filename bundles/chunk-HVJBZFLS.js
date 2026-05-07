4a07ac94738f5223d80c1d4aef8355ac6d044882
Matthew Beck <me@mattrb.com>
2026-05-07 22:16:54 +0000

    2a1e6ec docs: normalize product name casing across docs

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.12+sha-bca94ee";
+var PLACEHOLDER_VERSION = "21.2.12+sha-2a1e6ec";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.12+sha-bca94ee");
+var VERSION = new Version("21.2.12+sha-2a1e6ec");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
