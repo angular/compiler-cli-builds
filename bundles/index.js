0aa117dd9d14eba92c1f98cb5cb63aa623a3cb9a
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:28:24 +0000

    f584840 fix(platform-server): add `allowedHosts` option to `renderModule` and `renderApplication`

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.20+sha-4aee744");
+var VERSION = new Version("20.3.20+sha-f584840");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
