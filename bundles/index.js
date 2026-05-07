96f9a78b7d36243da79ad6a351f1925c9d99feeb
Matthew Beck <me@mattrb.com>
2026-05-07 22:03:29 +0000

    bca94ee docs: Update error display for password field in signal forms playground

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.12+sha-5316753");
+var VERSION = new Version("21.2.12+sha-bca94ee");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
