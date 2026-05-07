5eec3e38e27665edde188e9cc14d2d20d8df664c
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:03:11 +0000

    2fcfffb docs: Update error display for password field in signal forms playground

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-f1738b5");
+var VERSION = new Version("22.1.0-next.0+sha-2fcfffb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
