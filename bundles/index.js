38cdb3872a3b0f07ad1f20991e820d6bd22ba20e
Matthew Beck <me@mattrb.com>
2026-05-08 21:16:41 +0000

    0b7192f fix(platform-server): forward BEFORE_APP_SERIALIZED errors to ErrorHandler

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.12+sha-f9a58c1");
+var VERSION = new Version("21.2.12+sha-0b7192f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.12+sha-f9a58c1";
+export declare const PLACEHOLDER_VERSION = "21.2.12+sha-0b7192f";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
