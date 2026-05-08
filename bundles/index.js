0d668c314d69f1204c55400a18cc32329644af21
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 16:05:14 +0000

    80632a9 build: update dependency bazel to v8.7.0

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-38b2bed");
+var VERSION = new Version("22.1.0-next.0+sha-80632a9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
