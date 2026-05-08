4f166555757989ad14870db006ff492d5b616939
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 16:13:32 +0000

    1d621ea build: update pnpm to v10.33.4

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-0d02c6a");
+var VERSION = new Version("22.0.0-next.12+sha-1d621ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
