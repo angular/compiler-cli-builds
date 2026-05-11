b2942d767ae314fa431b27d15601cc80c54cc96e
Matthew Beck <me@mattrb.com>
2026-05-11 18:54:30 +0000

    5a6e946 docs: remove experimental warnings from resource/signal forms documentation

--- bundles/chunk-G6TWEH7Q.js
+++ bundles/chunk-G6TWEH7Q.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.12+sha-119a19e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-next.12+sha-5a6e946";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
