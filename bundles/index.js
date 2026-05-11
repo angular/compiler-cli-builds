6f1445a74172478968cc9619f1564dc1fb993ecf
Matthew Beck <me@mattrb.com>
2026-05-11 19:14:34 +0000

    c93d158 docs: add new signal forms field metadata guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.12+sha-0b7192f");
+var VERSION = new Version("21.2.12+sha-c93d158");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
