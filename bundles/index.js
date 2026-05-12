a0b0b275db9f47c4aea875b5d58da83ccc6d1f0f
Matthew Beck <me@mattrb.com>
2026-05-12 17:55:10 +0000

    7ab78d5 ci: mark devversion as unavailable

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.12+sha-49a133a");
+var VERSION = new Version("21.2.12+sha-7ab78d5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
