2945d7ccfe18f805f1d0eff3154f4cc7bb9e1502
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:58:22 +0000

    a617967 refactor(http): update HTTP resource options APIs to stable

--- bundles/chunk-G6TWEH7Q.js
+++ bundles/chunk-G6TWEH7Q.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.0+sha-62710ad";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.0+sha-a617967";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
