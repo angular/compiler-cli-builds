c42a3c1c2d7139c30328ce25b8f4b43b51e487ba
Matthew Beck <me@mattrb.com>
2026-05-12 17:57:46 +0000

    cc0fa6e refactor(http): update HTTP resource options APIs to stable

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-e9d1c7e");
+var VERSION = new Version("22.0.0-next.12+sha-cc0fa6e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
