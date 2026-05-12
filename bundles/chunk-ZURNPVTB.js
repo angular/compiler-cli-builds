9fb7bb63f8b155fc1722deaaa17eb29c3f5fb60e
Matthew Beck <me@mattrb.com>
2026-05-12 21:43:42 +0000

    7dc1017 fix(compiler): simplify handling of colon host with a selector list

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.12+sha-a4ec2b4";
+var PLACEHOLDER_VERSION = "22.0.0-next.12+sha-7dc1017";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-a4ec2b4");
+var VERSION = new Version("22.0.0-next.12+sha-7dc1017");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
