dbe96eb09777e9b4a4cda62a3898ed50972fc3a2
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 18:11:28 +0000

    2c0e45b build: update cross-repo angular dependencies (22.0.x)

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-cc0fa6e");
+var VERSION = new Version("22.0.0-next.12+sha-2c0e45b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
