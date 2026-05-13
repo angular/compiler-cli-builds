21bf7dfc1c64468ce712b4deb09e703ee6e54f6f
Matthew Beck <me@mattrb.com>
2026-05-13 22:44:09 +0000

    639a785 docs: add version 22 to the browser support reference table

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-1f0c1ab");
+var VERSION = new Version("22.0.0-next.12+sha-639a785");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
