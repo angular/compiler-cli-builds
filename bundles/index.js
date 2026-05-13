f18fc666a30e34b48820fbf2f1434f3f91df8d15
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 23:39:27 +0000

    5ca0c5a release: cut the v22.0.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-next.12+sha-0d55de8");
+var VERSION = new Version("22.0.0-rc.0+sha-5ca0c5a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
