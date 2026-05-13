0d21dfadf942ab84f5bde7468ab8271db40b28b9
Matthew Beck <me@mattrb.com>
2026-05-13 19:29:24 +0000

    1f0c1ab refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-next.12+sha-ccb7d42";
+var PLACEHOLDER_VERSION = "22.0.0-next.12+sha-1f0c1ab";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
