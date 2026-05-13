036606bfe6900132c08335b7781036060c86fa2a
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 19:28:39 +0000

    ec63947 refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-06f6dec");
+var VERSION = new Version("22.1.0-next.0+sha-ec63947");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
