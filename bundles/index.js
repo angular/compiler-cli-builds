3116e9f7bc01811c63509603e3c7bdd01b4e549a
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 18:54:10 +0000

    defb172 build: update dependency node to v22.22.3

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.0+sha-4dd9839");
+var VERSION = new Version("22.0.0-rc.0+sha-defb172");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
