76d2a80cd25b892cbbbdddd24644e4fe00a02b43
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 19:17:10 +0000

    da82f24 refactor(forms): add `provideExperimentalWebMcpForms`

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.0+sha-defb172");
+var VERSION = new Version("22.0.0-rc.0+sha-da82f24");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
