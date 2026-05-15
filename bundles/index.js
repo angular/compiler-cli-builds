b7070ad19007510c3ae60c1d8f63f2e64d6fa738
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 19:23:44 +0000

    9a7dedc docs: use when in signal forms rule examples

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-2959d6b");
+var VERSION = new Version("22.1.0-next.0+sha-9a7dedc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
