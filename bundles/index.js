becac9d4786756125a01d3807296a3bdb29ac75e
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 20:44:03 +0000

    c2160cd docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.0+sha-08d5017");
+var VERSION = new Version("22.0.0-rc.0+sha-c2160cd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
