78fe8c2e2d2c1106c9e03fc8d8ae9c5d745e2548
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 21:09:47 +0000

    49113ac fix(core): visit ICU expressions in signal migration schematics

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.13+sha-027c7c0");
+var VERSION = new Version("21.2.13+sha-49113ac");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
