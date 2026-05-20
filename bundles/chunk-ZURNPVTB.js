1ae28cba92489e47d93eb863c8384d7c2a197fa8
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 17:57:05 +0000

    cbddb5c build: update bazel dependencies

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-rc.0+sha-83026cf";
+var PLACEHOLDER_VERSION = "22.0.0-rc.0+sha-cbddb5c";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
