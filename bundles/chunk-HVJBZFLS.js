e83b89221abb6ea2311cfc6213a3353dd076d852
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 17:41:04 +0000

    0c7f70e fix(docs-infra): make absolute angular.dev hrefs relative in CLI option descriptions

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.13+sha-1ef4ea3";
+var PLACEHOLDER_VERSION = "21.2.13+sha-0c7f70e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
