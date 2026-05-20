f24a05e60993fb6b8cdf3f0982a74ee94e48c850
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 20:42:14 +0000

    a91315c docs: document content projection limitations

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.0+sha-7f1cfcb");
+var VERSION = new Version("22.0.0-rc.0+sha-a91315c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
