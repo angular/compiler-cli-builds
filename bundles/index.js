28a73f062b9ea4a21f2dcd0b9f8689e1541cc616
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 20:59:50 +0000

    1b6f780 docs: fix grammar slips on pipes guide

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.13+sha-232b21d");
+var VERSION = new Version("21.2.13+sha-1b6f780");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
