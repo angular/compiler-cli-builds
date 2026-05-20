df0a1fa746fb5ce38f83d87c663cd686e550107c
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 01:39:11 +0000

    5993c47 test(core): remove obsolete SVG script sanitization translation test

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.0+sha-1f71ebd");
+var VERSION = new Version("22.0.0-rc.0+sha-5993c47");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
