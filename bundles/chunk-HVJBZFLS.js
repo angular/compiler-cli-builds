b433d59442e6a2e5db01e406518568065fe20ac0
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 21:18:36 +0000

    9627e02 docs: link to ng new reference from installation guide

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.13+sha-1b6f780";
+var PLACEHOLDER_VERSION = "21.2.13+sha-9627e02";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
