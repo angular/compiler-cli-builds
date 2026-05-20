411075b93f44835688371dc7a7f23f497871479c
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:29:11 +0000

    1d6e71d docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.13+sha-0c7f70e";
+var PLACEHOLDER_VERSION = "21.2.13+sha-1d6e71d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
