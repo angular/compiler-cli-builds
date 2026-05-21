7262f50d3a0392b380d0991b94695b42112b9f8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-21 20:29:13 +0000

    dfbe7a6 build: update github/codeql-action action to v4.35.5

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-1563aae");
+var VERSION = new Version("22.1.0-next.0+sha-dfbe7a6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
