04eaac534e94473bfeac026460d18c777d9fbb71
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 19:29:43 +0000

    b25964b fix(vscode-extension): disable language server in untrusted workspaces

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-rc.1+sha-c08f22a";
+var PLACEHOLDER_VERSION = "22.0.0-rc.1+sha-b25964b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.1+sha-c08f22a");
+var VERSION = new Version("22.0.0-rc.1+sha-b25964b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
