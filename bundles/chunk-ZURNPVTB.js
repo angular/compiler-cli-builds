9a6a8914dc35deb694fd34aaba8b3ce44649f99a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:05:45 +0000

    d8c871e refactor(vscode-extension): Remove effectively dead code

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-dfbe7a6";
+var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-d8c871e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
