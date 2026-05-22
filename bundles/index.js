8063ce6eddbc0ff9e5811b1516b589d40f7dd363
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:05:08 +0000

    711f873 refactor(vscode-extension): Remove effectively dead code

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.14+sha-3fe8562");
+var VERSION = new Version("21.2.14+sha-711f873");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
