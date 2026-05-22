4c4235a8bc03fc120898b1308b108d01943c378d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:14:01 +0000

    6d8b156 fix(vscode-extension): restrict jsdoc markdown trust and harden document opening

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.14+sha-711f873");
+var VERSION = new Version("21.2.14+sha-6d8b156");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
