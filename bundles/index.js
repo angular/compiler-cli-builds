e6d38551169a0f2843083a54f70bf5fcd5a84b41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 22:56:56 +0000

    161c79d docs: release notes for the v19.2.23 release

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-34090cb");
+var VERSION = new Version("22.1.0-next.0+sha-161c79d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
