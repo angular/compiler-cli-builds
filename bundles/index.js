b56072c8e3e343cd840ed171fbba911877401cb8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 21:25:53 +0000

    34090cb fix(http): exclude withCredentials requests from transfer cache

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-6388675");
+var VERSION = new Version("22.1.0-next.0+sha-34090cb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
