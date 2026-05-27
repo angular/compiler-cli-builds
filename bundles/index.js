f1aa7e7beea05e6fc71483be58d4dad8eb49bbf0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 17:38:05 +0000

    37e8aad fix(platform-server): prevent SSRF bypasses via backslash URLs in HttpClient

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.14+sha-525e160");
+var VERSION = new Version("21.2.14+sha-37e8aad");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
