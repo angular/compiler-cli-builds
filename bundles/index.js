15ca652420436f35e997e19627770dd6cbd59d03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 17:41:48 +0000

    3d135ce fix(common): add upper bounds for digitsInfo

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.21+sha-591fa53");
+var VERSION = new Version("20.3.21+sha-3d135ce");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
