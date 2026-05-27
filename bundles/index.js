cad22896cc6417a480178b2972c54a153a3c1b85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:35:12 +0000

    d9c38e5 docs: fix typos in source code comments

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.1+sha-2200b4a");
+var VERSION = new Version("22.0.0-rc.1+sha-d9c38e5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
