522e52aff8949b72df0efcbd7c123beb90889ce9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:12:37 +0000

    e617fa0 fix(service-worker): Preserves HTTP cache mode in asset group requests

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.21+sha-3d135ce";
+var PLACEHOLDER_VERSION = "20.3.21+sha-e617fa0";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
