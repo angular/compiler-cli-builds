01d10a9ee54bf8b21092d19dc5580921ada0a728
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:04:22 +0000

    2b17b2d refactor(language-server): drop duplicate isAngularCore helpers in session

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.14+sha-37e8aad");
+var VERSION = new Version("21.2.14+sha-2b17b2d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
