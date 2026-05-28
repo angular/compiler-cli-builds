fda354bf6e7b262c965de187aea5601917fba373
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-28 16:58:35 +0000

    cea6588 release: cut the v21.2.15 release

--- bundles/chunk-XMX6JBER.js
+++ bundles/chunk-XMX6JBER.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.14+sha-6b8202e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.15+sha-cea6588";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.14+sha-6b8202e");
+var VERSION = new Version("21.2.15+sha-cea6588");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
