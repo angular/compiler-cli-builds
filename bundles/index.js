42fa9688325f9bebca8eeb274d3e981102bf8ddf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:52:40 +0000

    7b4b067 docs: fix grammar issues in resource guide

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.1+sha-d88b796");
+var VERSION = new Version("22.0.0-rc.1+sha-7b4b067");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
