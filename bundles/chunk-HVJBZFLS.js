7771200af627aed398b84296aa249f423c05f2f0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:52:19 +0000

    8538bdc docs: fix grammar issues in resource guide

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.14+sha-582a417";
+var PLACEHOLDER_VERSION = "21.2.14+sha-8538bdc";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
