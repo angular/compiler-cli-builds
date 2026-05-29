eb42caf81b465af8342aaff759ba490d601dd716
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 10:26:26 +0000

    5f0c9f8 docs: update cross-repo adev docs

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-rc.2+sha-c2153b1";
+var PLACEHOLDER_VERSION = "22.0.0-rc.2+sha-5f0c9f8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
