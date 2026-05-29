2e6eec5fdb05a33ce75f09833cfefa19cb4fe2eb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:05:27 +0000

    7e38336 fix(core): use Object.create(null) for LOCALE_DATA as a hardening measure

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.15+sha-f6d8e64");
+var VERSION = new Version("21.2.15+sha-7e38336");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.15+sha-f6d8e64";
+export declare const PLACEHOLDER_VERSION = "21.2.15+sha-7e38336";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
