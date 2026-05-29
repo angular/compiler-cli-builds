2461ccc53faf2eeab72c6616a67d83670c537bbc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 11:25:31 +0000

    f6d8e64 fix(common): only strip a literal /index.html suffix from URLs

--- bundles/chunk-XMX6JBER.js
+++ bundles/chunk-XMX6JBER.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.15+sha-9b7d0e5";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.15+sha-f6d8e64";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.15+sha-9b7d0e5");
+var VERSION = new Version("21.2.15+sha-f6d8e64");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
