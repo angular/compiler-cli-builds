47638a1708012c4f54763e4b964f15e1c17ebc04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:24:26 +0000

    15f7f8a docs: fix esbuild and TensorFlow branding on overview page

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.2+sha-d82c364");
+var VERSION = new Version("22.0.0-rc.2+sha-15f7f8a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
