60db1d3e90a0b4c73115ba2d5b9dd7b9cf74e619
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 17:03:28 +0000

    ae12a09 docs(core): fix typos and grammar in dependency injection guides

--- bundles/chunk-3NUMKC7G.js
+++ bundles/chunk-3NUMKC7G.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-rc.2+sha-012609a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-rc.2+sha-ae12a09";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.2+sha-012609a");
+var VERSION = new Version("22.0.0-rc.2+sha-ae12a09");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
