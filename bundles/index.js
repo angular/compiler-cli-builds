10aaafa89cac7761eb2bbfea7e11abd23b443546
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:14:55 +0000

    0b19c92 fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-ac47524");
+var VERSION = new Version("22.1.0-next.0+sha-0b19c92");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
