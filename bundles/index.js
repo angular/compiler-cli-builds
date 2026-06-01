918bab7a49cd02de9abd855ba00152152f9b5f0c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:47:52 +0000

    6ca433e fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.23+sha-8680b51");
+var VERSION = new Version("20.3.23+sha-6ca433e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
