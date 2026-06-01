f88e56466a8e4a2c7b9a9ac98239d65caabc03fc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:01:58 +0000

    1cd4f54 fix(docs-infra): readable contrast for DEV/EXP api badges in light mode

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.15+sha-69c0d48");
+var VERSION = new Version("21.2.15+sha-1cd4f54");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
