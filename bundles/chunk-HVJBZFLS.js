37809f98b0ccb6cb775402331914b9e0821f8cb7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:23:59 +0000

    9f6fda6 docs: fix esbuild and TensorFlow branding on overview page

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.15+sha-1cd4f54";
+var PLACEHOLDER_VERSION = "21.2.15+sha-9f6fda6";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
