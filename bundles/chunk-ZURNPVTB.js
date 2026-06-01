3c5cad64c0ee81e887d59bff320865078dd946a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:42:21 +0000

    012609a docs(docs-infra): simplify code block styles

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0-rc.2+sha-e834c37";
+var PLACEHOLDER_VERSION = "22.0.0-rc.2+sha-012609a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0-rc.2+sha-e834c37");
+var VERSION = new Version("22.0.0-rc.2+sha-012609a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
