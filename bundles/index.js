0189beda95e422a6eb5a6eb8aa67cef90f5e33f3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 20:13:58 +0000

    3cc9e2b docs: release notes for the v22.0.0-rc.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-0b19c92");
+var VERSION = new Version("22.1.0-next.0+sha-3cc9e2b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
