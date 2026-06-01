440aee0b4375ea667538e39e9417afe968733d00
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 09:39:12 +0000

    69c0d48 fix(docs-infra): round up media queries

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.15+sha-7e38336");
+var VERSION = new Version("21.2.15+sha-69c0d48");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
