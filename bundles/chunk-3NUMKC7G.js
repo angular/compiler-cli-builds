3e413b5a3f292f208df93978fdc3700e5b838cfa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:40:33 +0000

    2891f7e fix(compiler): move projection attributes into constants

--- bundles/chunk-3NUMKC7G.js
+++ bundles/chunk-3NUMKC7G.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-rc.2+sha-15f7f8a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0-rc.2+sha-2891f7e";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
