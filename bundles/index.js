bdef5a6911cafea862791dfb6daa06c8419341c0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 11:36:53 +0000

    e695379 fix(core): harden inherit definition feature against polluted prototypes

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-ebc7dbe");
+var VERSION = new Version("22.1.0-next.0+sha-e695379");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
