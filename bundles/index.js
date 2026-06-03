da0328ff1423b2480c862fb06c84252fb9d5f7c4
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:34:15 +0000

    1523061 fix(core): harden TransferState restoration against DOM clobbering

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.16+sha-03188dd");
+var VERSION = new Version("21.2.16+sha-1523061");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
