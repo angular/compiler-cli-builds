b295d11f088d4a3481c76650e0f69a686d61597f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 12:37:24 +0000

    03188dd release: cut the v21.2.16 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.15+sha-736c4ab");
+var VERSION = new Version("21.2.16+sha-03188dd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
