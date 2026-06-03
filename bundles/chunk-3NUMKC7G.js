f7413430d31473966f7f1e2dd1732175d47fd57f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:30:15 +0000

    da07b3c build: update cross-repo angular dependencies

--- bundles/chunk-3NUMKC7G.js
+++ bundles/chunk-3NUMKC7G.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0+sha-4e42420";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0+sha-da07b3c";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
