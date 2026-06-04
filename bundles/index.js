41603a5ec21f03ea154a611ac3201794b78ee68a
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:44:36 +0000

    a786862 fix(core): use Object.hasOwn to handle null-prototype objects in toStylingKeyValueArray

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-8da1198");
+var VERSION = new Version("22.1.0-next.0+sha-a786862");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
