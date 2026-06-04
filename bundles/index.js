0e36c5151b0da7d5534f5a2ec3f5f0a06ef0d515
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:31:36 +0000

    8da1198 build: update cross-repo angular dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-c5293c4");
+var VERSION = new Version("22.1.0-next.0+sha-8da1198");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
