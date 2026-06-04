b310dbd0fa6d2df2bbc4599e79f7d68b5c53da93
Andrew Scott <atscott01@gmail.com>
2026-06-04 17:12:05 +0000

    c5293c4 refactor(core): Also throw an error on iframe attributes set to `undefined`

--- bundles/chunk-5K7YOMBF.js
+++ bundles/chunk-5K7YOMBF.js
@@ -233,7 +233,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.0+sha-6bde84f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.0+sha-c5293c4";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
