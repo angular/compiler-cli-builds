53fafcaafbe9f53b18bb82519c7d1e1a24749cd6
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:27:09 +0000

    a68ec70 fix(compiler): sanitize two-way properties

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.24+sha-566ad05";
+var PLACEHOLDER_VERSION = "20.3.24+sha-a68ec70";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.24+sha-566ad05");
+var VERSION = new Version("20.3.24+sha-a68ec70");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
