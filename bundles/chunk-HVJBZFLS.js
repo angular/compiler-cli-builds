c6e34078ab0586dc1bfaa2ac9f09c27d865a3c02
GitHub <noreply@github.com>
2026-06-05 21:32:24 +0000

    a810a31 fix(http): Rejects non-HTTP(S) URLs in JSONP requests

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.16+sha-3551074";
+var PLACEHOLDER_VERSION = "21.2.16+sha-a810a31";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
