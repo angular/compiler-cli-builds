2223f40cd855e4b0fed9030f77a109b7ab8f0a6b
Andrew Scott <atscott01@gmail.com>
2026-06-08 19:20:14 +0000

    b3748e9 fix(core): correct container anchor collection order to match DOM layout

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-51724b4");
+var VERSION = new Version("22.1.0-next.0+sha-b3748e9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
