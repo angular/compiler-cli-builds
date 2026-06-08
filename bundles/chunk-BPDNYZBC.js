2bb557b749b44dfa151e089a50bcea7edfacd3cf
Andrew Scott <atscott01@gmail.com>
2026-06-08 22:48:19 +0000

    06be298 fix(http): preserve empty referrer option in HttpRequest

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.24+sha-9f443bc";
+var PLACEHOLDER_VERSION = "20.3.24+sha-06be298";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
