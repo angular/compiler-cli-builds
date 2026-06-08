1dc1bf6ae17e58f47cc1e95573622472b9a3180d
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:29:47 +0000

    86a56dc fix(common): Limits date format string length

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.16+sha-b9d2938");
+var VERSION = new Version("21.2.16+sha-86a56dc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
