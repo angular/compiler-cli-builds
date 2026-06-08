0a12e726d0587c3a75ec4386bf79ad733c938019
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:30:02 +0000

    e7dae12 fix(docs-infra): avoid code copy button overlap

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0+sha-167bd4c";
+var PLACEHOLDER_VERSION = "22.0.0+sha-e7dae12";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-167bd4c");
+var VERSION = new Version("22.0.0+sha-e7dae12");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
