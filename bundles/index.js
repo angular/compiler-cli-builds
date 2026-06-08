89cd212585f89ba527e90257227ae0cb54ec8446
Andrew Scott <atscott01@gmail.com>
2026-06-08 18:05:40 +0000

    a816848 ci: update devtools reviewers list

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-11b206b");
+var VERSION = new Version("22.1.0-next.0+sha-a816848");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
