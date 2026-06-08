468d472a870fd0166d98cdbe89f9d693cf9a7244
Andrew Scott <atscott01@gmail.com>
2026-06-08 17:23:45 +0000

    11b206b fix(core): introduce disposal mechanism for Angular views in foreign `@content`

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-255151a";
+var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-11b206b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
