8ecd628eafafed8885ed311128691f1f3d12f0ff
Andrew Scott <atscott01@gmail.com>
2026-06-08 19:54:50 +0000

    61dbc2b feat(devtools): improve transfer state tab

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-a99b7e4");
+var VERSION = new Version("22.0.0+sha-61dbc2b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
