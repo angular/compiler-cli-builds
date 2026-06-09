851dfa3b736a3b1168ea0e23b168f89cbcb39823
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:31:35 +0000

    4d24f46 fix(docs-infra): load cross-origin video embeds under COEP credentialless

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-4289c4c");
+var VERSION = new Version("22.0.0+sha-4d24f46");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
