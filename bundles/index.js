60f3dc400f2e45eac6703d1b216cd99c8cf6331e
Andrew Scott <atscott01@gmail.com>
2026-06-09 21:04:49 +0000

    a704b08 docs: add Signal Forms and v22 guidance to AI best-practices and llms.txt

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-43a0e28");
+var VERSION = new Version("22.0.0+sha-a704b08");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
