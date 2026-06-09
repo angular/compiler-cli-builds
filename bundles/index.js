ba53829e4b037c6e5536702e1a11b5190a331a10
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:04:11 +0000

    8ddda4d fix(docs-infra): improve version picker dropdown positioning

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-96b6419");
+var VERSION = new Version("22.0.0+sha-8ddda4d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
