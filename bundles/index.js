72008c5b7f911c3745590435c3983e2249004125
Andrew Scott <atscott01@gmail.com>
2026-06-09 23:26:50 +0000

    d1539a8 refactor(core): Fix DirectiveDefinition interface to allow abstract classes

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-7541f30");
+var VERSION = new Version("22.1.0-next.0+sha-d1539a8");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
