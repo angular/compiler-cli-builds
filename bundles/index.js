953944545fd6135bb76a742d309fe0af29ac090b
Andrew Scott <atscott01@gmail.com>
2026-06-09 16:53:15 +0000

    547d85a ci: run benchmark comparison in isolated worktree and harden security

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-e6ae250");
+var VERSION = new Version("22.1.0-next.0+sha-547d85a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
