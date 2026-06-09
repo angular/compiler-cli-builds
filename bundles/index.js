3d1d96fefeb392fa1378974c9ca6d791b8c0ada9
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:35:43 +0000

    cdcea80 fix(core): require WebMCP tool descriptions

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-f908a3e");
+var VERSION = new Version("22.0.0+sha-cdcea80");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
