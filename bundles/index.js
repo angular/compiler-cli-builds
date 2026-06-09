bcc90f7a238e5a92fe355e4fff609338e39ea4b2
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:10:41 +0000

    f908a3e refactor(core): harden change store access

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-8ddda4d");
+var VERSION = new Version("22.0.0+sha-f908a3e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
