606cc7dcae1261469be363e52c8c1c646e48e070
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:48:26 +0000

    3dd35c2 fix(common): escape CSS string-terminating characters in escapeCssUrl

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-c121407");
+var VERSION = new Version("22.1.0-next.0+sha-3dd35c2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
