866607219b4b1a58c6fa2bb5ed8c295b1713398a
GitHub <noreply@github.com>
2026-06-09 17:12:31 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.16+sha-86a56dc");
+var VERSION = new Version("21.2.16+sha-13fb0af");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
