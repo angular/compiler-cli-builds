1ef4e1e925886e680cb2a3e4b277db1cb615e1ff
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:08:19 +0000

    7e94ab9 docs: release notes for the v21.2.17 release

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-daadb38";
+var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-7e94ab9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
