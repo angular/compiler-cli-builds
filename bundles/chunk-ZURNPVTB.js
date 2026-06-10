d2fbeb9936f34412d64292f9a99c692977bb2461
Andrew Scott <atscott01@gmail.com>
2026-06-10 00:16:22 +0000

    6e3d51d refactor(migrations): Improve `safeNavigationMigration` heuristic

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.0+sha-01ea640";
+var PLACEHOLDER_VERSION = "22.0.0+sha-6e3d51d";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
