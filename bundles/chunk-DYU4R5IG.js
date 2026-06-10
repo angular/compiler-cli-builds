ef31e4b607ce6da5856195360bdebd8d004a77da
Andrew Scott <atscott01@gmail.com>
2026-06-10 17:04:53 +0000

    6cc54e5 fix(forms): remove animationstart listener on component destroy to prevent memory leak

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-f06b96d";
+var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-6cc54e5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
