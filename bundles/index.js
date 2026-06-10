1695a80e975535ae90ac109096f1104e11595a5a
Andrew Scott <atscott01@gmail.com>
2026-06-10 02:33:30 +0000

    85d2d10 fix(forms): harden FormGroup control lookups against prototype shadowing

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-6e3d51d");
+var VERSION = new Version("22.0.0+sha-85d2d10");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
