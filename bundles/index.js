3532e13b44a0f2f96a7292e77630a7e2b7bf4222
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:45:38 +0000

    c092a00 fix(http): pass down the `reportUploadProgress` and `reportDownloadProgress` on post/patch requests

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-274d1d2");
+var VERSION = new Version("22.1.0-next.0+sha-c092a00");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
