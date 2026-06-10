8827aabc4c1fe6caa026abe9b0ab2f831313515c
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:20:55 +0000

    8b32559 release: cut the v22.0.1 release

--- bundles/chunk-3NUMKC7G.js
+++ bundles/chunk-3NUMKC7G.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.0+sha-2dd65d2";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.1+sha-8b32559";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.0+sha-2dd65d2");
+var VERSION = new Version("22.0.1+sha-8b32559");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
