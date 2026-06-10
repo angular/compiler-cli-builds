779c1b8da2f06c35477b3bbcf01dc2d8c7c26e02
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:16:09 +0000

    fbe807c fix(vscode-extension): resolve relative workspace tsdk paths to absolute

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-54112d9");
+var VERSION = new Version("22.1.0-next.0+sha-fbe807c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
