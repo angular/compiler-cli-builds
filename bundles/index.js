4f421fa55556a635f21c0fce42a9ddb5aabf2051
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:46:16 +0000

    6f3e832 release: cut the v22.1.0-next.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-9f4b3dc");
+var VERSION = new Version("22.1.0-next.0+sha-6f3e832");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
