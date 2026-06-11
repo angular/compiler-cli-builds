805f7e9570e1aa7f69b5c7f1c9e5ba1b421214e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-11 16:52:10 +0000

    21bdff5 fix(zone.js): harden zoneSymbolEventNames against __proto__ key (defense-in-depth)

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-cbcf31b");
+var VERSION = new Version("22.0.1+sha-21bdff5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
