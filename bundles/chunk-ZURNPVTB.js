1156177ddcea2e89deadacfa786cfdc70c741049
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-11 16:33:34 +0000

    cbcf31b build: update cross-repo angular dependencies

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.1+sha-4d0091b";
+var PLACEHOLDER_VERSION = "22.0.1+sha-cbcf31b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-4d0091b");
+var VERSION = new Version("22.0.1+sha-cbcf31b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
