1886e7785f7597c3fd572ef678e2e02187ab0b0a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:23:03 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-15314c1");
+var VERSION = new Version("22.0.1+sha-1d97355");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
