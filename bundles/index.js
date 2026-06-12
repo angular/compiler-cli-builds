63f97e4cde1c4eb26f3c01426eaaa5d5c2043cf7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-12 15:31:54 +0000

    d45e318 build: update pnpm to v10.34.2

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-e74b884");
+var VERSION = new Version("22.0.1+sha-d45e318");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
