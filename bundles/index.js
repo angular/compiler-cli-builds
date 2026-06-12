df1ac283bd543407dfbf21615c0b9a3f4d6f860a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 16:34:27 +0000

    15314c1 fix(migrations): migration skip any target are not build or test

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-b7aa8dc");
+var VERSION = new Version("22.0.1+sha-15314c1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
