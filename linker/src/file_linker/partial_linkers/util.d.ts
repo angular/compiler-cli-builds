48fa49ac944c495efd093fdc547c1b39ff4bd696
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-15 16:05:50 +0000

    50e7f3a fix(docs-infra): center social and theme menus under their triggers on tablet

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.1.0-next.0+sha-40f7fbd";
+export declare const PLACEHOLDER_VERSION = "22.1.0-next.0+sha-50e7f3a";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
