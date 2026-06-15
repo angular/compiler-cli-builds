6fafb3ef09746b9949163cb7b1646d6c21991766
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-15 16:35:25 +0000

    417a407 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- bundles/index.js
+++ bundles/index.js
@@ -102,7 +102,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-50e7f3a");
+var VERSION = new Version("22.1.0-next.0+sha-417a407");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
