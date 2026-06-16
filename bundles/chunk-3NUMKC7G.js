638517b04ca3c821b97dc2f2a774c94784f79db0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:54:17 +0000

    f2c5045 docs: use the @Service decorator in the learn-angular DI tutorial

--- bundles/chunk-3NUMKC7G.js
+++ bundles/chunk-3NUMKC7G.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.1+sha-93d0a5f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.1+sha-f2c5045";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.1+sha-93d0a5f");
+var VERSION = new Version("22.0.1+sha-f2c5045");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
