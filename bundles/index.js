2c7703a9692fd369fc33ea2c80c79dce7534a7ee
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 15:37:35 +0000

    28cb15a fix(core): prevent unsubscribe during emit from throwing off other listeners

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-383a42a");
+var VERSION = new Version("22.1.0-next.0+sha-28cb15a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
