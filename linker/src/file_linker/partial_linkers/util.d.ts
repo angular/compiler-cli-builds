bc21bddc1e08c80488530f70e88311229c4ef6a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 16:15:25 +0000

    b32ee7c fix(core): treat iframe credentialless as security-sensitive

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.1+sha-f2c5045";
+export declare const PLACEHOLDER_VERSION = "22.0.1+sha-b32ee7c";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
