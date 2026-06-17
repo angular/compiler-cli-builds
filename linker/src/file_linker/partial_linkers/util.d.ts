9f22ad6a1d91e2211df30e6c6c8377e885ff02d6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 21:14:03 +0000

    aac8577 release: cut the v22.0.2 release

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.1+sha-1d0256b";
+export declare const PLACEHOLDER_VERSION = "22.0.2+sha-aac8577";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
