9ca57bbc7b4a9bf453799bed6d04818db9d53e44
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 21:22:03 +0000

    302dd0f release: cut the v22.1.0-next.1 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.0+sha-f540400");
+var VERSION = new Version("22.1.0-next.1+sha-302dd0f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
