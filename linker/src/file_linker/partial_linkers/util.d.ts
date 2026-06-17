03a1577cb3d10c9a11be3122986b6e896ab6d406
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 21:03:06 +0000

    1d0256b build: update cross-repo angular dependencies to 1ce5d68

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.0.1+sha-52f5501";
+export declare const PLACEHOLDER_VERSION = "22.0.1+sha-1d0256b";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
