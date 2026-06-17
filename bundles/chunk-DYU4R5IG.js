a7fc6ef814cc278229030b92de0036a842d842dd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 18:50:18 +0000

    1e79dd3 refactor(router): Add handling for ActivatedRoute-scoped injector

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-ea18ab2";
+var PLACEHOLDER_VERSION = "22.1.0-next.0+sha-1e79dd3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
