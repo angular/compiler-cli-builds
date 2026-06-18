b772a987e1b4ef1eb9ab414bf4f04084ef2c6edf
kirjs <kirts@google.com>
2026-06-18 14:52:30 +0000

    ece8ba4 docs: use the @Service decorator in the first-app and signals tutorials

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.2+sha-291d574";
+var PLACEHOLDER_VERSION = "22.0.2+sha-ece8ba4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
