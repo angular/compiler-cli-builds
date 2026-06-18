b2b3785c7130a83cddedd71920597e9de5bdadbd
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 14:57:14 +0000

    ffcc8ee fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.1+sha-e7a4e25");
+var VERSION = new Version("22.1.0-next.1+sha-ffcc8ee");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
