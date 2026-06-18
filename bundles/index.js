a2b9161a4f32425bc9048d8fe4ae2b9904ea0c43
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 18:15:18 +0000

    e77a8a0 fix(docs-infra): align the page title with its edit action

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.1+sha-a849b6f");
+var VERSION = new Version("22.1.0-next.1+sha-e77a8a0");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
