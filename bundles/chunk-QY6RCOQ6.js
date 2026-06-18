4fe32d8d5e27cfa017d5034c715f9e506a8eba81
kirjs <kirts@google.com>
2026-06-18 16:39:01 +0000

    3e7c83b Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-73880c5";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-3e7c83b";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
