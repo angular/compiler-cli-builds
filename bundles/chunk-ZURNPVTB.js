d16bd2048b17b9b99ece64621b527cac3132300c
kirjs <kirts@google.com>
2026-06-18 14:56:56 +0000

    73880c5 fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.2+sha-ece8ba4";
+var PLACEHOLDER_VERSION = "22.0.2+sha-73880c5";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
