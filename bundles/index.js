7d8d34af629fbb251673bb5eafaf3742d812f17e
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-19 09:41:59 +0000

    aaac1bc fix(devtools): restore build under stricter ts_project deps

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.2+sha-c6b0b87");
+var VERSION = new Version("22.0.2+sha-aaac1bc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
