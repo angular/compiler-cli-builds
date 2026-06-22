5fc53d8808dfe6d600bd5d0af6e64842bf601b55
kirjs <kirts@google.com>
2026-06-22 20:54:02 +0000

    d817e56 docs: show selected menu item visually in context menu example

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.2+sha-59ee51e";
+var PLACEHOLDER_VERSION = "22.0.2+sha-d817e56";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
