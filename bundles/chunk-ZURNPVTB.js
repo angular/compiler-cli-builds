8a89166ee2149be6d39fc99ef3ca3ba77e0e8c31
kirjs <kirts@google.com>
2026-06-22 20:35:49 +0000

    59ee51e docs(forms): update package docs for Signal Forms graduation

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.2+sha-a477a69";
+var PLACEHOLDER_VERSION = "22.0.2+sha-59ee51e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
