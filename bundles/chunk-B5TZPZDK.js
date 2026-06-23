84e1d05dccea5638515e4db40b9fd6b706da6259
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 18:21:42 +0000

    06e9da4 docs: document support for standard forwarded proxy header

--- bundles/chunk-B5TZPZDK.js
+++ bundles/chunk-B5TZPZDK.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.1+sha-91d168e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.1+sha-06e9da4";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.1+sha-91d168e");
+var VERSION = new Version("22.1.0-next.1+sha-06e9da4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
