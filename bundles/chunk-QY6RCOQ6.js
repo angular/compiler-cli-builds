2814812cbd36ceee1d26353c011d54f0bac3262d
kirjs <kirts@google.com>
2026-06-23 15:35:46 +0000

    f598dcf docs: add error guide for NG05101

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-7766dc0";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-f598dcf";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
