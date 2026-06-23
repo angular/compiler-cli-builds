93a2226f69c14d36cf553fac24313083d935ae93
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 18:21:02 +0000

    50f252e build: update bazel dependencies

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-2799304";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.2+sha-50f252e";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
