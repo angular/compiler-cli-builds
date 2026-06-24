3839adc1a377ea7ffeb081d795557a3c2ce0ffe8
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 14:54:29 +0000

    295dad7 build: update cross-repo angular dependencies

--- bundles/chunk-B5TZPZDK.js
+++ bundles/chunk-B5TZPZDK.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.1+sha-d7d6a08";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.1+sha-295dad7";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
