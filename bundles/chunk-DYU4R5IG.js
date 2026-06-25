5cd5fa41f78a424a48d736d36bf6c7ef6e000b4d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-25 15:39:03 +0000

    28a90e3 release: cut the v22.1.0-next.2 release

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.1+sha-7f2d34a";
+var PLACEHOLDER_VERSION = "22.1.0-next.2+sha-28a90e3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
