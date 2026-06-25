278aeee692e511de3184879eaff3f37d6f693441
Alan Agius <alan.agius4@gmail.com>
2026-06-25 15:15:53 +0000

    5ba6443 release: cut the v22.0.3 release

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.2+sha-f4f7f37";
+var PLACEHOLDER_VERSION = "22.0.3+sha-5ba6443";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.2+sha-f4f7f37");
+var VERSION = new Version("22.0.3+sha-5ba6443");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
