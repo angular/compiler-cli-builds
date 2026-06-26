008ab65e906b8f0f84c3b53edc555e4e2220a671
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 16:23:55 +0000

    26b0c71 fix(migrations): resolve migration failure when tsconfig specifies rootDir

--- bundles/chunk-B5TZPZDK.js
+++ bundles/chunk-B5TZPZDK.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.2+sha-28a90e3";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.2+sha-26b0c71";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.2+sha-28a90e3");
+var VERSION = new Version("22.1.0-next.2+sha-26b0c71");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
