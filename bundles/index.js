15a5eb159e34ed55303e2885aa2f7bf1b062650c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:55:05 +0000

    e653a7b docs(forms): correct signal field access in form model example

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.3+sha-1659952");
+var VERSION = new Version("22.1.0-next.3+sha-e653a7b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
