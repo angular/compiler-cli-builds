569961793f21ed5159c48385d0fd4222e052787a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 00:38:57 +0000

    a720094 refactor(core): format `@content` blocks consistently (#69502)

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.3+sha-74803c7";
+var PLACEHOLDER_VERSION = "22.1.0-next.3+sha-a720094";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.3+sha-74803c7");
+var VERSION = new Version("22.1.0-next.3+sha-a720094");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
