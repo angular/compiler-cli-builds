6bad98c37fba4d3e7494201ba50aba22a6162905
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 00:48:09 +0000

    5a693ba fix(core): reject dynamic script host elements

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.17+sha-6bcce11";
+var PLACEHOLDER_VERSION = "21.2.17+sha-5a693ba";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.17+sha-6bcce11");
+var VERSION = new Version("21.2.17+sha-5a693ba");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
