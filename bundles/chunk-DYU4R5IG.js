4665c4960495d6cba591f61fbb0a5a4877517d70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 01:07:14 +0000

    c4cb66e docs: improve introductory resource example

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.3+sha-d7f7061";
+var PLACEHOLDER_VERSION = "22.1.0-next.3+sha-c4cb66e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
