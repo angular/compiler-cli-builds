251c48e53d223cfb44cb2f57aca24776b85b333d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 21:14:37 +0000

    b126dc9 release: cut the v22.1.0-next.4 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.3+sha-c4fa24a");
+var VERSION = new Version("22.1.0-next.4+sha-b126dc9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.1.0-next.3+sha-c4fa24a";
+export declare const PLACEHOLDER_VERSION = "22.1.0-next.4+sha-b126dc9";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
