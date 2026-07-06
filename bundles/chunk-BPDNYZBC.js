e84fa9dfaa79f9e5592ea1bca3b94ea1a859a0ff
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-06 20:41:25 +0000

    406aaa3 fix(compiler-cli): update babel dependencies to latest v7

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.25+sha-8eb7aea";
+var PLACEHOLDER_VERSION = "20.3.25+sha-406aaa3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
