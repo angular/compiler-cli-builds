ad98edd8d41c38b294833594b3a342d66cbffab9
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-06 21:07:03 +0000

    8d22cc9 fix(compiler-cli): update babel dependencies to latest v7

--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "21.2.17+sha-5a693ba";
+export declare const PLACEHOLDER_VERSION = "21.2.17+sha-8d22cc9";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
