4218b1ef2ba186ceb5dcdd1519800ab00009d282
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 16:41:27 +0000

    e878d7c docs(ai-tutor): add troubleshooting for MCP setup issues

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-2a05de4");
+var VERSION = new Version("22.0.5+sha-e878d7c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
