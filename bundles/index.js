384612922eff93bcb584cec037416ba60b940cbc
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 16:59:26 +0000

    e839891 fix(docs-infra): disable nav-list tooltip on mobile

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-e878d7c");
+var VERSION = new Version("22.0.5+sha-e839891");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
