297b6a45cf46b81900c9f33fe3d7c8dc89382f5f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:20:15 +0000

    91df739 fix(http): prevent caching of responses with Set-Cookie headers

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.17+sha-8d22cc9";
+var PLACEHOLDER_VERSION = "21.2.17+sha-91df739";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/chunk-XMX6JBER.js
+++ bundles/chunk-XMX6JBER.js
@@ -229,7 +229,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.17+sha-8d22cc9";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "21.2.17+sha-91df739";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
