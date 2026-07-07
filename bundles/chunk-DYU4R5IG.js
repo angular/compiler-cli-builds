c83ac09c4c712b589bfb2ff0eac67bba7a747c44
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 16:59:31 +0000

    5ec0b16 fix(docs-infra): disable nav-list tooltip on mobile

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-e91046f";
+var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-5ec0b16";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
