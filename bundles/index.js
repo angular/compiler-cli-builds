6aac50a6b6ece6be8f3e12144ec4e66a2064a94f
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 23:51:29 +0000

    4314f72 docs: fix import of `AdminPage` in Angular routing documentation. #69670

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-f4d2069");
+var VERSION = new Version("22.0.5+sha-4314f72");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
