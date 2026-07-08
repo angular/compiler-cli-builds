34a672b8eede8926e339cf5d664d2c0216ea9857
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:33:24 +0000

    c0eaaed refactor(core): cleanup Meta service

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-39ea1c5";
+var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-c0eaaed";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.4+sha-39ea1c5");
+var VERSION = new Version("22.1.0-next.4+sha-c0eaaed");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
