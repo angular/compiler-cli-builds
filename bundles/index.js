10e7bfb0887a8557270402523867163a50b22d27
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 21:42:58 +0000

    89e2c77 release: cut the v22.0.6 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-d1aaf1a");
+var VERSION = new Version("22.0.6+sha-89e2c77");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
