dfd1cd4714098ac6651e2e9079a9021a63e705b0
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:15:09 +0000

    4d62760 release: cut the v20.3.26 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.25+sha-baa093b";
+var PLACEHOLDER_VERSION = "20.3.26+sha-4d62760";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
