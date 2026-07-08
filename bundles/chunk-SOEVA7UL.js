b59347cb0d3de4cad711d2b13dfc086b65fb3bfb
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 17:31:39 +0000

    c1829f6 docs(docs-infra): Add build-time validation for API and guide links using route manifest

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.4+sha-42862f7";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.4+sha-c1829f6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
