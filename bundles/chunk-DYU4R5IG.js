c8da46951bc16eb638f77fa60700f4fb4d5cc3cd
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 20:08:10 +0000

    25b7afa Revert "fix(forms): allow multiple async validators"

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-3ac2c7b";
+var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-25b7afa";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
