9a609e55794d0274ef5b9938622cbcbc4921f54c
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:21:14 +0000

    baa093b fix(service-worker): preserve referrer policy in asset requests

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.25+sha-b963f61";
+var PLACEHOLDER_VERSION = "20.3.25+sha-baa093b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.25+sha-b963f61");
+var VERSION = new Version("20.3.25+sha-baa093b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
