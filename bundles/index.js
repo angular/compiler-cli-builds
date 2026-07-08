4aa1f2c35a5d08d262d423ac20513388261377a5
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 19:50:59 +0000

    d1aaf1a feat(docs-infra): add Angie mascot to the not found page

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-2cb034f");
+var VERSION = new Version("22.0.5+sha-d1aaf1a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
