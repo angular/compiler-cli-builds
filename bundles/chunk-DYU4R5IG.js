892ae579804d12683c434815061397d0cfd7e5c9
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 15:59:00 +0000

    42862f7 docs: update formatting

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-95b7385";
+var PLACEHOLDER_VERSION = "22.1.0-next.4+sha-42862f7";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
