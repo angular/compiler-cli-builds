f4a783e3d986a392815e8508774ecb4f0e986ea3
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 19:25:23 +0000

    2cb034f docs: clarify runtime component comparison in elements guide

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.5+sha-ab18f5f";
+var PLACEHOLDER_VERSION = "22.0.5+sha-2cb034f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.5+sha-ab18f5f");
+var VERSION = new Version("22.0.5+sha-2cb034f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
