e27bb4c9e8ad9471db0665da09298c0447178557
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 19:26:32 +0000

    eac04cf docs: clarify runtime component comparison in elements guide

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.4+sha-c0eaaed");
+var VERSION = new Version("22.1.0-next.4+sha-eac04cf");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
