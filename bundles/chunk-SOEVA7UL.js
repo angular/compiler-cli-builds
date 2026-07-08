922216d3e7fbe76d8c0461be6ba3dbb7fdc038c1
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:11:34 +0000

    57616af docs: release notes for the v21.2.18 release

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.5+sha-248d4e5";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.5+sha-57616af";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.5+sha-248d4e5");
+var VERSION = new Version("22.1.0-next.5+sha-57616af");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
