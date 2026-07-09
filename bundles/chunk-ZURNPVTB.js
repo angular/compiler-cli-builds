d8c670244e28b3f8470a4fec3c56f5d5020de847
Matthew Beck <me@mattrb.com>
2026-07-09 19:40:16 +0000

    8ce1fcf fix(localize): use Object.hasOwn for placeholder lookup in translate

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.6+sha-3a9351f";
+var PLACEHOLDER_VERSION = "22.0.6+sha-8ce1fcf";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
