ec33687940988342222d4e7bd72dad127c5ed596
Matthew Beck <me@mattrb.com>
2026-07-09 19:38:04 +0000

    2a4f582 fix(localize): use Object.hasOwn for placeholder lookup in translate

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.5+sha-cf6d693";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.5+sha-2a4f582";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
