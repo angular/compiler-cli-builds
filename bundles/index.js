350164327ad23556325dc84785a445c176a02cce
Matthew Beck <me@mattrb.com>
2026-07-09 19:19:07 +0000

    6fdde4f docs: show ng-add schema files in schematics-for-libraries guide

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-cd6b903");
+var VERSION = new Version("22.0.6+sha-6fdde4f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
