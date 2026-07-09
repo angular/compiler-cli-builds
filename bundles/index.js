856d97a877eef71404aafb0404a26c053bc74822
Matthew Beck <me@mattrb.com>
2026-07-09 17:06:31 +0000

    aa03cb9 test(language-service): empty template literal interpolation

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-89e2c77");
+var VERSION = new Version("22.0.6+sha-aa03cb9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
