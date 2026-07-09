268ac368be3755c390787ee1a185651fe73e8b4c
Matthew Beck <me@mattrb.com>
2026-07-09 23:15:25 +0000

    97c830b ci: fix workflows broken by renamed command

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-8ce1fcf");
+var VERSION = new Version("22.0.6+sha-97c830b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
