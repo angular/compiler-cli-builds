fd6bdd9c39d91014469fa7c3f353c6fbbe7256c4
Matthew Beck <me@mattrb.com>
2026-07-09 19:08:17 +0000

    c92d995 feat(docs-infra): add Angie to the embedded editor loading steps

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.5+sha-53ca8c2");
+var VERSION = new Version("22.1.0-next.5+sha-c92d995");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
