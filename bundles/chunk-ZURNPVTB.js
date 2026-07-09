eb1cee3b4046aef85c1c656c683cf2c71b66fcbe
Matthew Beck <me@mattrb.com>
2026-07-09 19:08:54 +0000

    cd6b903 feat(docs-infra): add Angie to the embedded editor loading steps

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.6+sha-aa03cb9";
+var PLACEHOLDER_VERSION = "22.0.6+sha-cd6b903";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
