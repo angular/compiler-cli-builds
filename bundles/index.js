88cf5f8f04c9582ce2530b5fc19da7266dc9e43d
Matthew Beck <me@mattrb.com>
2026-07-10 18:11:05 +0000

    c429036 fix(docs-infra): avoid search dialog empty-state flicker while re-querying

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-c82ae67");
+var VERSION = new Version("22.0.6+sha-c429036");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
