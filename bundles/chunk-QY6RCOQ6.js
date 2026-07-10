137e5c23b166151ad48b648e2cd3be69d04e802b
Matthew Beck <me@mattrb.com>
2026-07-10 21:29:55 +0000

    cfb77d6 docs: update cross-repo adev docs

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.6+sha-e6272f2";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.6+sha-cfb77d6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
