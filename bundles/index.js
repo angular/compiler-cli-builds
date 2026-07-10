9f9258b2db8c112390181778d86642ed43e2d455
Matthew Beck <me@mattrb.com>
2026-07-10 01:04:12 +0000

    c82ae67 Revert "fix(core): allow static attributes for explicit input transforms"

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-97c830b");
+var VERSION = new Version("22.0.6+sha-c82ae67");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
