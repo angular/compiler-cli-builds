daf02637e4dfbf33977c71268d5ee079deca84cc
Matthew Beck <me@mattrb.com>
2026-07-13 20:05:24 +0000

    7658a81 docs: Add mention that about signal narrowing on `@switch` blocks

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-ac5f476");
+var VERSION = new Version("22.0.6+sha-7658a81");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
