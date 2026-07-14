c607afd243d7018356585f3a2176d3ea12341d57
Matthew Beck <me@mattrb.com>
2026-07-14 18:09:20 +0000

    fb6b354 fix(platform-browser): prevent ReDoS in SOURCEMAP_URL_REGEXP

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.5+sha-6d043f8");
+var VERSION = new Version("22.1.0-next.5+sha-fb6b354");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
