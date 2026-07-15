9ef69a1cb6ecd8297b58e49178ff574072321654
Matthew Beck <me@mattrb.com>
2026-07-15 19:10:24 +0000

    0ae6d81 fix(core): preserve explicit input transform write type

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.5+sha-d997a96");
+var VERSION = new Version("22.1.0-next.5+sha-0ae6d81");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
