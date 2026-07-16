a202e9cfb372a0df1b09d128a5ecc671ceeea4c4
Matthew Beck <me@mattrb.com>
2026-07-16 00:49:45 +0000

    3b82aea ci: harden workflow against expression injection

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.6+sha-64d6d47");
+var VERSION = new Version("22.0.6+sha-3b82aea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
