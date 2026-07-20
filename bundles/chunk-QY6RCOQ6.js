52e32d5a7d1c9f5cf20deeaebee7d239bd544660
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:29:36 +0000

    e02a0de refactor(devtools): add alt text to the empty-state screen illustrations

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.7+sha-1ecd9c3";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.7+sha-e02a0de";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.7+sha-1ecd9c3");
+var VERSION = new Version("22.0.7+sha-e02a0de");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
