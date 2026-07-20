5c4409636a98940f87987f5c2ec0c3ee221ee2c6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 14:08:35 +0000

    3ff6100 fix(docs-infra): only claim a search on the 404 page when results exist

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.7+sha-9d40f8a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.7+sha-3ff6100";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.7+sha-9d40f8a");
+var VERSION = new Version("22.0.7+sha-3ff6100");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
