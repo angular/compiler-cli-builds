d425460d42dbed12492b538deeaea37e21acc379
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:29:40 +0000

    00699f3 refactor(devtools): add alt text to the empty-state screen illustrations

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.6+sha-d6cad90";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-next.6+sha-00699f3";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
