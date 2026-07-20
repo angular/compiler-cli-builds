6e0a08112619120012ae1a3786d9650745496699
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:55:36 +0000

    d6cad90 docs: fix broken link to Angular AI Skills page

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.6+sha-f032f5a");
+var VERSION = new Version("22.1.0-next.6+sha-d6cad90");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
