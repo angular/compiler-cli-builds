91b0e20239e6140b26b7c27c5761ecb433329a0f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:45:39 +0000

    3192dcc fix(http): prevent transfer cache key collisions

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.6+sha-00699f3");
+var VERSION = new Version("22.1.0-next.6+sha-3192dcc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
