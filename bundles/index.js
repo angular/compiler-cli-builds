9b88c34a465dcc44b7feadd0fedbb63966fb90c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 08:45:17 +0000

    e14ead1 Revert "fix(forms): preserve intermediate number values in signal forms"

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.7+sha-f8df06d");
+var VERSION = new Version("22.0.7+sha-e14ead1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
