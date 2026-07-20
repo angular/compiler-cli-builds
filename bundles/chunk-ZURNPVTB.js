e03b2bb25fda5f853476ee68813f18772ffe2203
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:55:10 +0000

    1ecd9c3 docs: fix broken link to Angular AI Skills page

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.7+sha-e00e291";
+var PLACEHOLDER_VERSION = "22.0.7+sha-1ecd9c3";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
