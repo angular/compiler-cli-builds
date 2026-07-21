ad8dca2bcd52c303d5b160e2f98d91709b80b901
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 10:12:55 +0000

    2bc2146 build: update pnpm to v11.15.1

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.0-next.6+sha-eb3e480";
+var PLACEHOLDER_VERSION = "22.1.0-next.6+sha-2bc2146";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.6+sha-eb3e480");
+var VERSION = new Version("22.1.0-next.6+sha-2bc2146");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
