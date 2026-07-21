6bdbb60d702fd8e9232c9e6796dc5a0809596da6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 15:26:27 +0000

    fb68711 feat(docs-infra): react with Angie in the playground minigame

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.7+sha-d302c7a");
+var VERSION = new Version("22.0.7+sha-fb68711");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
