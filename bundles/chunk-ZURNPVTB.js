5289bf1467863ca46a4c904c07bba3c484e4f47e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:38:27 +0000

    548ef47 release: cut the v22.0.8 release

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.7+sha-45eef01";
+var PLACEHOLDER_VERSION = "22.0.8+sha-548ef47";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
