46e6fb157b1b8be286a81fb1dd7e4aeea65df304
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 09:56:46 +0000

    6371f0b docs: add skills for Angular pipes

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.6+sha-49672c4");
+var VERSION = new Version("22.1.0-next.6+sha-6371f0b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
