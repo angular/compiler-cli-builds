eb1ea76fa3acd23e194dd26f341935b82662cf7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:38:54 +0000

    21c45a8 fix(zone.js): vitest patching of describe/it curried calls

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.7+sha-e62905e";
+var PLACEHOLDER_VERSION = "22.0.7+sha-21c45a8";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
