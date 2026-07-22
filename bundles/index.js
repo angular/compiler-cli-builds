0c2072d4fba40489bc6b4a84521786a21d6436ed
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:38:34 +0000

    e23541b fix(zone.js): vitest patching of describe/it curried calls

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-next.6+sha-6557df5");
+var VERSION = new Version("22.1.0-next.6+sha-e23541b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
