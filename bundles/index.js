5b2c968b6409b9532ae778194b5bf28bd9412a30
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 12:30:53 +0000

    45eef01 fix(docs-infra): fail the guide build when a markdown file starts with a BOM

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.0.7+sha-bc3ff3b");
+var VERSION = new Version("22.0.7+sha-45eef01");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
