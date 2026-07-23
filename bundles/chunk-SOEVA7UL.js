5b0ac902d6e46813e5670069bf03b553ac0e7200
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-23 19:08:01 +0000

    e779309 release: bump Angular DevTools version to 1.18.0

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.0+sha-3bf2430";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.0+sha-e779309";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
