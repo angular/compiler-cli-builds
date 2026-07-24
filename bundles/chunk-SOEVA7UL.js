12ba958cab1113a16342009ba8dfbc1b57416ddc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 21:03:16 +0000

    5d5b2ea fix(migrations): correctly detect `then`/`else` keywords in control flow migration

--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-rc.0+sha-a99fb91";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.0-rc.0+sha-5d5b2ea";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-rc.0+sha-a99fb91");
+var VERSION = new Version("22.1.0-rc.0+sha-5d5b2ea");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
