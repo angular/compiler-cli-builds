0cea06e98b9ad0aa1cab4b484ea6d4bb37a109f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:33:07 +0000

    125a794 docs: use https for external links in adev

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-rc.0+sha-1b85b3c");
+var VERSION = new Version("22.1.0-rc.0+sha-125a794");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
