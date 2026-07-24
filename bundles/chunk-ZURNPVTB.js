45728c075bc68b5176e9afa9809f5cb88f409b46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:32:51 +0000

    433276b docs: use https for external links in adev

--- bundles/chunk-ZURNPVTB.js
+++ bundles/chunk-ZURNPVTB.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.0.8+sha-548ef47";
+var PLACEHOLDER_VERSION = "22.0.8+sha-433276b";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
