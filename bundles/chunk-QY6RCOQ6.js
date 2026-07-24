8e38524978dd3d5d7ef693e63de42944c6de4c15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:41:55 +0000

    0650966 docs: modernize directives guides (#69822)

--- bundles/chunk-QY6RCOQ6.js
+++ bundles/chunk-QY6RCOQ6.js
@@ -232,7 +232,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.8+sha-433276b";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.0.8+sha-0650966";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
