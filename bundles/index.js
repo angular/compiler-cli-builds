a9ed71bb02db5700e851ff0c88b153d7b059080c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:54:00 +0000

    098d28f docs: fix self-referential link in defer block description

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0-rc.0+sha-a7d67f9");
+var VERSION = new Version("22.1.0-rc.0+sha-098d28f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
