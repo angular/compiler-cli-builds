fe1362bd82711d798b784ee2452fa8e5c7c8b92b
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:57:54 +0000

    c6e4d5a release: cut the v22.2.0-next.0 release

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.2.0-next.0+sha-9386c89";
+var PLACEHOLDER_VERSION = "22.2.0-next.0+sha-c6e4d5a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/chunk-SOEVA7UL.js
+++ bundles/chunk-SOEVA7UL.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.0+sha-9386c89";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.0+sha-c6e4d5a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
