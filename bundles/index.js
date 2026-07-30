7a07b1f5cdb316aaf1adf626e62fe956dfaf6a65
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-30 16:13:32 +0000

    6deb6af refactor(forms): modernize reactive forms tests to rely on `whenStable`

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.0+sha-d068fc1");
+var VERSION = new Version("22.2.0-next.0+sha-6deb6af");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
