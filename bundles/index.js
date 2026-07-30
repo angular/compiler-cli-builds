3db2612bfe7c7457f9d334a1b338c70e081a446a
GitHub <noreply@github.com>
2026-07-30 15:50:26 +0000

    2f96c80 fix(core): sanitize host bindings on concrete hosts

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.27+sha-72a8512");
+var VERSION = new Version("20.3.27+sha-2f96c80");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
