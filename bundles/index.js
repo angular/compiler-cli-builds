cd9acf91231e2aa29f307d7327fb0eacdbc3dff5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 17:04:24 +0000

    01eeee4 docs: replace Firebase Studio references with Antigravity

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0+sha-949de9c");
+var VERSION = new Version("22.1.0+sha-01eeee4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
