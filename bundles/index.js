ad91c82659863859a17c7a6edb0214e94b95900c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 15:52:10 +0000

    644455b refactor(core): update acceptance test

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0+sha-c4b1432");
+var VERSION = new Version("22.1.0+sha-644455b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
