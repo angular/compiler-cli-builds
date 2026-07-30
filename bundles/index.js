ad23d627ab7e3578ffe2d7bacd456a74f83851e6
Alan Agius <alan.agius4@gmail.com>
2026-07-30 07:40:26 +0000

    892ea7b release: bump Angular DevTools version to 1.19.0

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.0+sha-1a2bcb2");
+var VERSION = new Version("22.2.0-next.0+sha-892ea7b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
