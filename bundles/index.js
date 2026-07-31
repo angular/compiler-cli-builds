7b3111c569198a79e623c813d28893929baf7961
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 22:40:46 +0000

    3d0067a refactor(animations): remove outdated comment

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.0+sha-aa6d318");
+var VERSION = new Version("22.1.0+sha-3d0067a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
