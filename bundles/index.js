97fe8ad7ad0b335bee10d3ec6be2b02a4d9b697b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-03 16:51:21 +0000

    337053e refactor(compiler): remove unused code and exports

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.0+sha-fb698d1");
+var VERSION = new Version("22.2.0-next.0+sha-337053e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
