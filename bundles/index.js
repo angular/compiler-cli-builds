faf6816bff259642e004bd59694d2f1dcc6b97fe
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 23:10:20 +0000

    eaa0d0b build: update all github actions

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.1+sha-7c15737");
+var VERSION = new Version("22.1.1+sha-eaa0d0b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
