22b9c4e92dc8e8eb508ac4407acba38cbca3e5ff
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:47:41 +0000

    596356e docs: update cross-repo adev docs

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.1+sha-4d985a1";
+var PLACEHOLDER_VERSION = "22.1.1+sha-596356e";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.1+sha-4d985a1");
+var VERSION = new Version("22.1.1+sha-596356e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
