d354ea39670d0841dad64324b58526cf018455d2
Alon Mishne <amishne@users.noreply.github.com>
2026-08-10 22:42:51 +0000

    0df9d5e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- bundles/chunk-FW4HJL5T.js
+++ bundles/chunk-FW4HJL5T.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.1+sha-596356e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.1+sha-0df9d5e";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
