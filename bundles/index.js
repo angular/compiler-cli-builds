72b8f9ecd869eef06afb4e851eee3fcfa885e380
Alon Mishne <amishne@users.noreply.github.com>
2026-08-11 19:14:03 +0000

    2f82601 fix(router): limit protocol-relative URL handling to serialization

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.1+sha-65e2aed");
+var VERSION = new Version("22.1.1+sha-2f82601");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
