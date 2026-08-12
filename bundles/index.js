6de5eea45ebc27f0e772e89e25bb462a5da6714e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-12 18:19:48 +0000

    3d32db6 docs: clarify animate.leave child animation behavior

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.1+sha-435f8b2");
+var VERSION = new Version("22.2.0-next.1+sha-3d32db6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
