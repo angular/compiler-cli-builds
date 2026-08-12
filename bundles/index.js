d6d16e49281dcc124908a979b7a4760065b356b4
GitHub <noreply@github.com>
2026-08-12 22:43:22 +0000

    3032e7f release: bump VSCode extension version to 22.1.0 (#70171)

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.1+sha-299607d");
+var VERSION = new Version("22.1.1+sha-3032e7f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
