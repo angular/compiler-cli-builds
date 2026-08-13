718e59843666ff14cbf8cffdac92337ea59761ca
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 17:21:53 +0000

    044e69c docs: release notes for the v20.3.28 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.1+sha-16e42c3");
+var VERSION = new Version("22.2.0-next.1+sha-044e69c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
