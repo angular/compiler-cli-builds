b7c75e9d7378f340ebb2affda20eb0525723e42f
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 18:54:09 +0000

    40d23fa release: cut the v22.2.0-next.2 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.1+sha-044e69c");
+var VERSION = new Version("22.2.0-next.2+sha-40d23fa");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
