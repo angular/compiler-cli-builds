1804735e2a839945aea1101fa9fbc7295c85ca08
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 17:13:17 +0000

    1d576ee release: cut the v20.3.28 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.27+sha-e4c416c");
+var VERSION = new Version("20.3.28+sha-1d576ee");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
