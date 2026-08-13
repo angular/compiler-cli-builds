e358ff2c069d6fdb0ef291a5ba38387835b00e65
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-13 16:03:41 +0000

    0a708e9 docs: note SafeResourceUrl audio src change

--- bundles/chunk-DYU4R5IG.js
+++ bundles/chunk-DYU4R5IG.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.1+sha-910f391";
+var PLACEHOLDER_VERSION = "22.1.1+sha-0a708e9";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
