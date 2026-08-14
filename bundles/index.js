90a84d92ff73d2a1bcc00afa506e33d60a55a1cf
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:28:24 +0000

    0cd243d build: resolve CI failures

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.2+sha-a9f8fb2");
+var VERSION = new Version("22.1.2+sha-0cd243d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
