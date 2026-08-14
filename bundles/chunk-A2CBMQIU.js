934dcfde79c554430648ea357ca05936c1016089
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-14 15:39:18 +0000

    c6e4a36 fix(core): throw a descriptive error instead of crashing when a hydration node is missing

--- bundles/chunk-A2CBMQIU.js
+++ bundles/chunk-A2CBMQIU.js
@@ -237,7 +237,7 @@ var COMPILER_ERRORS_WITH_GUIDES = /* @__PURE__ */ new Set([
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.2+sha-d3d3bc6";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.2+sha-c6e4a36";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
