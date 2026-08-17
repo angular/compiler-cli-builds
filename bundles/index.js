e7a055814dc6fc171ca65db4628587c7f24d82c1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 17:53:10 +0000

    789c9c2 docs: update Angular MCP skill reference

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.2+sha-ec678c0");
+var VERSION = new Version("22.2.0-next.2+sha-789c9c2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
