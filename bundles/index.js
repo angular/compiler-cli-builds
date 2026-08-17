2ace9d2ce8ae87dfbfbe6e0fc04dfb5af6caa143
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:50:11 +0000

    7538744 fix(platform-browser): disallow event handler attributes in Meta

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.28+sha-1d576ee");
+var VERSION = new Version("20.3.28+sha-7538744");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
