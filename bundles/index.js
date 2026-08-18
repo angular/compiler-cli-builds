299faed5fa1ea37d3ca00a800dd5e7db6e92a622
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 22:14:09 +0000

    abe019d fix(router): pass correct component to canDeactivate for named outlets in componentless parent routes

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.2+sha-49f4e15");
+var VERSION = new Version("22.1.2+sha-abe019d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
