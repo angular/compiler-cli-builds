c6f76875c4a522726916c9682d87183e13e932e5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 19:14:01 +0000

    2720362 feat(router): add containsTree as public API

--- bundles/chunk-G5WVUNWP.js
+++ bundles/chunk-G5WVUNWP.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.2+sha-d364c83";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.2+sha-2720362";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
