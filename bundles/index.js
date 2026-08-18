d4c0e345bf3189fceeb5bf281701752504587582
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 23:04:09 +0000

    94f0b9a fix(compiler): preserve &ngsp; between sibling control flow blocks

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.2+sha-abe019d");
+var VERSION = new Version("22.1.2+sha-94f0b9a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
