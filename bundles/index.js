a373d11da0a831331e44af25a2b4429e6128504e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 18:16:49 +0000

    7e39b4a docs: update signal forms skill to match the current implementation

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.2+sha-80a04eb");
+var VERSION = new Version("22.2.0-next.2+sha-7e39b4a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
