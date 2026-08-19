108de7a679b2e02bf4857e02294afa17eb8e06da
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 15:36:08 +0000

    84a210e refactor(router): add support for non-blocking router resources (#70211)

--- bundles/chunk-G5WVUNWP.js
+++ bundles/chunk-G5WVUNWP.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.2+sha-83450d2";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.2+sha-84a210e";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
