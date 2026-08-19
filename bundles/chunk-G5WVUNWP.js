e746c63a622edd919a2270acb05f9cff95a591e5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 18:19:40 +0000

    8aa5046 build: update cross-repo angular dependencies to v22.1.3

--- bundles/chunk-G5WVUNWP.js
+++ bundles/chunk-G5WVUNWP.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.2+sha-84a210e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.2+sha-8aa5046";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.2+sha-84a210e");
+var VERSION = new Version("22.1.2+sha-8aa5046");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
