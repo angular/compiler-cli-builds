05494ef48a4c5e7fefb79e02f70476883be25b8a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 19:52:23 +0000

    391c378 refactor(core): pass AbortSignal to WebMCP tools to handle cancellation

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.2+sha-8aa5046");
+var VERSION = new Version("22.1.2+sha-391c378");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
