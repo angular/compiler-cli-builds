d3816ea9d90a63c38b16ea242c5e81a176f3ab15
leonsenft <leonsenft@users.noreply.github.com>
2026-08-24 17:14:12 +0000

    87af7a4 docs: add self-closing tags guideline to components reference

--- bundles/chunk-G5WVUNWP.js
+++ bundles/chunk-G5WVUNWP.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.3+sha-64e02d8";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.3+sha-87af7a4";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
