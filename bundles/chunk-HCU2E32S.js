cfb2260b0d4c03c8042e6c94f89ab4dd02de4f74
leonsenft <leonsenft@users.noreply.github.com>
2026-08-24 22:38:54 +0000

    7c752d4 fix(core): prevent TransferState prototype pollution

--- bundles/chunk-HCU2E32S.js
+++ bundles/chunk-HCU2E32S.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.3+sha-ba3bc47";
+var PLACEHOLDER_VERSION = "22.1.3+sha-7c752d4";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
