70f1aa0bfbb1e33346fd9e4aac501c3287147528
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:48:15 +0000

    d953d2e docs: add missing tap import and AppConfig interface to environment configuration reference

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.3+sha-719e1ad");
+var VERSION = new Version("22.2.0-next.3+sha-d953d2e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
