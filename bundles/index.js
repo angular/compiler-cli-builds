412c4eb306dfb243194a87acf2d3fc927a397bfc
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:51:09 +0000

    73d8bbd fix(platform-server): update domino to latest version

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.21+sha-992d627");
+var VERSION = new Version("21.2.21+sha-73d8bbd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
