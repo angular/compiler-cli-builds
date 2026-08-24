d65684340ad9d63352e2d8bcdff3b914d23998ab
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:49:18 +0000

    89b2056 fix(platform-server): update domino to latest version

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.29+sha-77f7b60";
+var PLACEHOLDER_VERSION = "20.3.29+sha-89b2056";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
