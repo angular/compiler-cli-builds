3f5140a955fbfe41679057367544b1d942ea4634
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 22:27:57 +0000

    ddfd8a8 refactor(docs-infra): remove example configs for tooling that no longer exists

--- bundles/chunk-BKKGWDSR.js
+++ bundles/chunk-BKKGWDSR.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-3848b7f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-ddfd8a8";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
