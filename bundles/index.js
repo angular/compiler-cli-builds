e8de8391a495c06c09aa4c93a0b122f51d1f9cbf
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 17:12:51 +0000

    5aa6d97 fix(platform-server): avoid stripping unicode whitespace during url resolution

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.21+sha-73d8bbd");
+var VERSION = new Version("21.2.21+sha-5aa6d97");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
