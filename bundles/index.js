143a9ce00b3dea7e976bf77bdeafe62c76b71e9b
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 18:34:18 +0000

    3848b7f docs: clarify onSameUrlNavigation reload behavior

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.3+sha-07605ba");
+var VERSION = new Version("22.2.0-next.3+sha-3848b7f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
