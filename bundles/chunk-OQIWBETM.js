09fe6d183d6c92634b1008c876a0d39c01f089d6
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 22:57:30 +0000

    2e6a057 docs: update cross-repo adev docs

--- bundles/chunk-OQIWBETM.js
+++ bundles/chunk-OQIWBETM.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-c1ac4b7";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-2e6a057";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
