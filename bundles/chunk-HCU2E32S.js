192b927f08d2a33af8f35891b7a8a91fb882e7a6
Alan Agius <alan.agius4@gmail.com>
2026-08-26 08:00:50 +0000

    1d0945f release: bump Angular DevTools version to 1.21.0

--- bundles/chunk-HCU2E32S.js
+++ bundles/chunk-HCU2E32S.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.2.0-next.3+sha-915a03a";
+var PLACEHOLDER_VERSION = "22.2.0-next.3+sha-1d0945f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
