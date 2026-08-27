796f82f83e1326a946ef2c5e012a0dac211f51e6
Matthew Beck <me@mattrb.com>
2026-08-27 14:57:39 +0000

    11e9949 build: update bazel dependencies

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.4+sha-8983809");
+var VERSION = new Version("22.1.4+sha-11e9949");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
