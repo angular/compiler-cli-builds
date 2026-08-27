e78693b2a731f42af82a5a099ed100edbffd88f9
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-27 00:17:02 +0000

    a2b4379 docs: release notes for the v22.1.4 release

--- bundles/chunk-OQIWBETM.js
+++ bundles/chunk-OQIWBETM.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-9b70174";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-a2b4379";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
