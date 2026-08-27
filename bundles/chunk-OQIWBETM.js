d647775a42a1640886083cdc8dbb742f17b55ed4
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-27 00:21:45 +0000

    133cafd release: cut the v22.2.0-next.4 release

--- bundles/chunk-OQIWBETM.js
+++ bundles/chunk-OQIWBETM.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.3+sha-a2b4379";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.4+sha-133cafd";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
