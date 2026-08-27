51fbf6ade2374c9a168344315123b052c57e40e4
Matthew Beck <me@mattrb.com>
2026-08-27 20:53:07 +0000

    ad22e89 refactor(devtools): point the .gitignore comment at live GitHub docs

--- bundles/chunk-OQIWBETM.js
+++ bundles/chunk-OQIWBETM.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.4+sha-6f1dc26";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.4+sha-ad22e89";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
