db2ca7688e712699504a97da2ed07064ee377a04
Matthew Beck <me@mattrb.com>
2026-08-29 01:12:26 +0000

    3b26567 docs: update cross-repo adev docs

--- bundles/chunk-HCU2E32S.js
+++ bundles/chunk-HCU2E32S.js
@@ -456,7 +456,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.4+sha-60f8a78";
+var PLACEHOLDER_VERSION = "22.1.4+sha-3b26567";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.4+sha-60f8a78");
+var VERSION = new Version("22.1.4+sha-3b26567");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
