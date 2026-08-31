1d8b3ade405f24e489691922da98601e0b46baa0
Matthew Beck <me@mattrb.com>
2026-08-31 22:18:23 +0000

    0904f90 fix(core): cancel stale debounce timers to prevent timer leaks

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.4+sha-b3bb36a");
+var VERSION = new Version("22.2.0-next.4+sha-0904f90");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
