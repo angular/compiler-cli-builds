5918bf6ba3ab374d89c71008b770be8f4f79a87a
Matthew Beck <me@mattrb.com>
2026-08-31 18:37:35 +0000

    341bc0d fix(docs-infra): reject unclosed paired docs elements

--- bundles/chunk-UMFPUGY4.js
+++ bundles/chunk-UMFPUGY4.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.4+sha-ffbb7e6";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.4+sha-341bc0d";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.4+sha-ffbb7e6");
+var VERSION = new Version("22.1.4+sha-341bc0d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
