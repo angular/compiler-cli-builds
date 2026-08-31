34928e7868f5f91bfe7c53e0c8b6e241b55c5330
Matthew Beck <me@mattrb.com>
2026-08-31 18:31:04 +0000

    46b02cc docs: correct stale visibleLines ranges in the first-app tutorial

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.4+sha-563850e");
+var VERSION = new Version("22.2.0-next.4+sha-46b02cc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
