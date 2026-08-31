40ab52447c922b53f9259905b4fa842abcd504ff
Matthew Beck <me@mattrb.com>
2026-08-31 18:30:58 +0000

    ffbb7e6 docs: correct stale visibleLines ranges in the first-app tutorial

--- bundles/chunk-UMFPUGY4.js
+++ bundles/chunk-UMFPUGY4.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.4+sha-3b26567";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.4+sha-ffbb7e6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.4+sha-3b26567");
+var VERSION = new Version("22.1.4+sha-ffbb7e6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
