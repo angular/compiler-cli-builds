8a615b56897d4c629d52282bf5630de0dbf02d3d
Matthew Beck <me@mattrb.com>
2026-09-02 21:27:21 +0000

    0a1bbbe release: cut the zone.js-0.16.3 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.4+sha-b2903de");
+var VERSION = new Version("22.1.4+sha-0a1bbbe");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
