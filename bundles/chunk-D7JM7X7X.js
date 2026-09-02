2bf90e48eeae1ff51813bf94ddb696e36ade9a95
Matthew Beck <me@mattrb.com>
2026-09-02 21:42:00 +0000

    45148da docs: correct openFiles entries that point at missing files

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.4+sha-124bf44";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.4+sha-45148da";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
