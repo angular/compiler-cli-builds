dba0d2a187b92571a96a20e7172e2fa9ffc670ca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:54:59 +0000

    ef48630 test(core): replace act with actAsync

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.5+sha-af219ee");
+var VERSION = new Version("22.1.5+sha-ef48630-with-local-changes");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
