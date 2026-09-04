3929a127c0825a8874fcf705c073bca25f37ee4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:31:05 +0000

    6f5406c fix(docs-infra): correct example viewer tab state and line numbering

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.5+sha-dfe7be4");
+var VERSION = new Version("22.1.5+sha-6f5406c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
