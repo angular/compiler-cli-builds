dc3674ed1c2b9491740020508d626018af66f1cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:32:31 +0000

    bae170d build: update bazel dependencies

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-74c1716";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-bae170d";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.5+sha-74c1716");
+var VERSION = new Version("22.2.0-next.5+sha-bae170d");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
