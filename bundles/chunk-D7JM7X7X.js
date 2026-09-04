f28f910d8ac3dc2a229a131bf021e5f8854f6b0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:57:14 +0000

    9a58353 build: update cross-repo angular dependencies

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-bae170d";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-9a58353";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
