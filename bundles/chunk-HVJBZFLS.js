586de487860fe45506816a27c4f163d47d022957
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:01:02 +0000

    81385ea release: cut the v21.2.23 release

--- bundles/chunk-HVJBZFLS.js
+++ bundles/chunk-HVJBZFLS.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "21.2.22+sha-fc2e8fb";
+var PLACEHOLDER_VERSION = "21.2.23+sha-81385ea";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
