6450301ca1bec94d2794005af85f0eba3bacb925
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 16:17:03 +0000

    679c50d refactor(router): Remove ActivatedRouteSnapshot from resourceContext

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-25228d0";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.5+sha-679c50d";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
