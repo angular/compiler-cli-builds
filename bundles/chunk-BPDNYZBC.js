c35c2e1a1c9fd551b00cfabdae56c9c62ac2470e
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:07:22 +0000

    a700a91 release: cut the v20.3.31 release

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.30+sha-3db26e2";
+var PLACEHOLDER_VERSION = "20.3.31+sha-a700a91";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.30+sha-3db26e2");
+var VERSION = new Version("20.3.31+sha-a700a91");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
