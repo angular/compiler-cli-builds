07f3955c4751f4c6738a54146f6bd45ebda987d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:33:52 +0000

    3db26e2 fix(platform-server): update domino to latest version

--- bundles/chunk-BPDNYZBC.js
+++ bundles/chunk-BPDNYZBC.js
@@ -455,7 +455,7 @@ import { compileDirectiveFromMetadata, makeBindingParser, ParseLocation, ParseSo
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "20.3.30+sha-4ee0421";
+var PLACEHOLDER_VERSION = "20.3.30+sha-3db26e2";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-G7GFT6BU.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("20.3.30+sha-4ee0421");
+var VERSION = new Version("20.3.30+sha-3db26e2");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
