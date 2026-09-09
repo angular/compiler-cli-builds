7ae47fb5599d4f9867442a7022d631a16816c266
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:38:10 +0000

    8576f16 fix(docs-infra): highlight home page code for the resolved theme

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.5+sha-679c50d");
+var VERSION = new Version("22.2.0-next.7+sha-8576f16");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
