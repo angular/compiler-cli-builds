f214af192cdeb9c00b6688aea531fef595134535
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:34:18 +0000

    fc2e8fb fix(platform-server): update domino to latest version

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.22+sha-4c0bc43");
+var VERSION = new Version("21.2.22+sha-fc2e8fb");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
