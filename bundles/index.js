5f44a7ddeadeff1b484e51b9177a64bf31df2461
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:14:24 +0000

    29f9dd5 refactor(devtools): update angular optimization plugin for JavaScriptTransformer API

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-b9fb456");
+var VERSION = new Version("22.2.0-next.7+sha-29f9dd5");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
