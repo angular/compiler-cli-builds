8b5b8b08217f977a04a5c12a3642648e6590994b
GitHub <noreply@github.com>
2026-09-10 18:45:43 +0000

    1905dc6 release: bump VSCode extension version to 22.1.1 (#70666)

--- bundles/chunk-NPM4D5UO.js
+++ bundles/chunk-NPM4D5UO.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-da8dac6";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-1905dc6";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
