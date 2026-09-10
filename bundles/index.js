b64149904c748aac55bdb138f0e0bf05ba71d354
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:02:36 +0000

    b9fb456 docs(forms): close the unterminated code fences in the compatForm docs

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-9090a75");
+var VERSION = new Version("22.2.0-next.7+sha-b9fb456");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
