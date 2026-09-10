0d3cd6ec95e5343a5e690114298eb5f3d39f023b
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:32:18 +0000

    14dbbf9 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-8ca879e";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-14dbbf9";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-8ca879e");
+var VERSION = new Version("22.2.0-next.7+sha-14dbbf9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
