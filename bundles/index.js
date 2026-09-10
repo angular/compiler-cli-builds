c6053a42c790a3d7f4ce6e4097c33244338cd728
Andrew Scott <atscott01@gmail.com>
2026-09-10 23:33:33 +0000

    23e5d30 build: update pnpm to v11.26.0

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.6+sha-14d6999");
+var VERSION = new Version("22.1.6+sha-23e5d30");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
