5c85bdd978e08e3636ef0f444d6d047b146d2925
Andrew Scott <atscott01@gmail.com>
2026-09-11 21:45:53 +0000

    2dcdf9a fix(router): mark router_resource module-level symbols as side-effect free

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-c097c01";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-2dcdf9a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-c097c01");
+var VERSION = new Version("22.2.0-next.7+sha-2dcdf9a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
