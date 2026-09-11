031a3c24332a3665cfab6c66f5ba230bb0bf0c22
Andrew Scott <atscott01@gmail.com>
2026-09-11 23:08:03 +0000

    fc2f6fd docs: stop the disabled aria toolbar radio group from changing selection

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-f2cf65a");
+var VERSION = new Version("22.2.0-next.7+sha-fc2f6fd");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
