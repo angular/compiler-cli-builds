82bb69dafffae7a55f2aa84339346cc3f1cbf502
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:14:19 +0000

    8a837f6 Revert "fix(compiler): wrap `@for` collection expression before appending non-null assertion"

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.6+sha-23e5d30");
+var VERSION = new Version("22.1.6+sha-8a837f6");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
