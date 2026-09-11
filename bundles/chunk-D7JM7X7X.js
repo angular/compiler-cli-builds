040f308d04a687fb0ffc8f0b6219354ced626319
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:25:08 +0000

    f2cf65a fix(vscode-extension): handle template literals when detecting supported decorator fields

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-2dcdf9a";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-f2cf65a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
