f164c082865b79656bdac02da15a26096abf9db2
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:22:54 +0000

    376b71e docs(forms): document what required() considers empty

--- bundles/chunk-NPM4D5UO.js
+++ bundles/chunk-NPM4D5UO.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-4b5571c";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-376b71e";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.6+sha-4b5571c");
+var VERSION = new Version("22.1.6+sha-376b71e");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
