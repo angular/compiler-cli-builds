56f7680cd60ddc95e728f4d9bd7b0ae362c830d8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 19:10:40 +0000

    c45cc9a docs: release notes for the v22.1.7 release

--- bundles/chunk-OWYB6QWT.js
+++ bundles/chunk-OWYB6QWT.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-312e1d8";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-c45cc9a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-312e1d8");
+var VERSION = new Version("22.2.0-next.7+sha-c45cc9a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
