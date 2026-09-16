236ed948186dc886535f0d04c856c6176c3867e3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 19:20:55 +0000

    379bdbe release: cut the v22.2.0-rc.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-c45cc9a");
+var VERSION = new Version("22.2.0-rc.0+sha-379bdbe");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
