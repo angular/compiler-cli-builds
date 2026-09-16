d4ba7c52086e09698d00fdb46f5048368f286282
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:50:31 +0000

    f73f059 docs: fix API links that point at missing member anchors

--- bundles/chunk-NPM4D5UO.js
+++ bundles/chunk-NPM4D5UO.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-24ca9d4";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.6+sha-f73f059";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
