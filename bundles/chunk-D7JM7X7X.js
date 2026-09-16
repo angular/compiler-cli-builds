9e42a082f102ef6cdf5d334aadaa822f925e327c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:09:49 +0000

    5cdae0a docs: remove outdated useFactory and expression lowering guidance

--- bundles/chunk-D7JM7X7X.js
+++ bundles/chunk-D7JM7X7X.js
@@ -147,7 +147,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-430a8eb";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-next.7+sha-5cdae0a";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-next.7+sha-430a8eb");
+var VERSION = new Version("22.2.0-next.7+sha-5cdae0a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
