3bf6456a254071efc764220ce3a4db3e6e80da71
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 15:58:16 +0000

    80f9e3b test(core): restore longer durations in animation tests

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-379bdbe");
+var VERSION = new Version("22.2.0-rc.0+sha-80f9e3b");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
