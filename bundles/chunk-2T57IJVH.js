35599a7b8b10230f4c214d47ecc199977848481b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 21:24:03 +0000

    0b632de refactor(platform-browser): type shadow DOM renderer fields

--- bundles/chunk-2T57IJVH.js
+++ bundles/chunk-2T57IJVH.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-03754b3";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-0b632de";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
