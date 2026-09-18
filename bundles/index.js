a03b4321f2bbab3b97d46ce1398bcb504334d75e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-18 16:24:27 +0000

    7bf58f9 docs: update cross-repo adev docs

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.7+sha-f15be33");
+var VERSION = new Version("22.1.7+sha-7bf58f9");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
--- linker/src/file_linker/partial_linkers/util.d.ts
+++ linker/src/file_linker/partial_linkers/util.d.ts
@@ -7,7 +7,7 @@
  */
 import { MaybeForwardRefExpression, outputAst as o, R3DeclareDependencyMetadata, R3DependencyMetadata, R3Reference } from '@angular/compiler';
 import { AstObject, AstValue } from '../../ast/ast_value';
-export declare const PLACEHOLDER_VERSION = "22.1.7+sha-f15be33";
+export declare const PLACEHOLDER_VERSION = "22.1.7+sha-7bf58f9";
 export declare function wrapReference<TExpression>(wrapped: o.WrappedNodeExpr<TExpression>): R3Reference;
 /**
  * Parses the value of an enum from the AST value's symbol name.
