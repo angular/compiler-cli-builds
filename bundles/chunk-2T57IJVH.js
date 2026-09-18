7f991a6f0fc3beefee05217ebd317f23d71d3bf3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-18 16:13:40 +0000

    f15be33 fix(docs-infra): render the signature of decorators without members

--- bundles/chunk-2T57IJVH.js
+++ bundles/chunk-2T57IJVH.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-0b632de";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-f15be33";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
