06df460460b1e034416cecba57c84a997c88fb41
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 16:55:23 +0000

    f39db77 docs: fix stray markup in the image optimization and zoneless guides

--- bundles/chunk-UHTQAPWI.js
+++ bundles/chunk-UHTQAPWI.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.7+sha-c08ba52";
+var PLACEHOLDER_VERSION = "22.1.7+sha-f39db77";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
