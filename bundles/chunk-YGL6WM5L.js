059d7137ee47b415fb914af1ff607ef123293852
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:33:03 +0000

    7a58550 fix(forms): create controls before listeners

--- bundles/chunk-YGL6WM5L.js
+++ bundles/chunk-YGL6WM5L.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-51f056f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-7a58550";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
