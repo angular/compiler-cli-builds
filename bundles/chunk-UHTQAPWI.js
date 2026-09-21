5dea4620b9262eaf875ed5d51c3ec4b71960343e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 19:15:25 +0000

    6647a07 docs: update `disabledReasons` and `transformedValue` examples

--- bundles/chunk-UHTQAPWI.js
+++ bundles/chunk-UHTQAPWI.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-0ee60b4";
+var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-6647a07";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/chunk-YGL6WM5L.js
+++ bundles/chunk-YGL6WM5L.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.3.0-next.0+sha-0ee60b4";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.3.0-next.0+sha-6647a07";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
