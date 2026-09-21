917e832e2f71867eebe2c95d9ded41b5bdb2d80d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:15:26 +0000

    aa2cabb refactor(compiler): add method to emitter context

--- bundles/chunk-2T57IJVH.js
+++ bundles/chunk-2T57IJVH.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-f39db77";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-aa2cabb";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
