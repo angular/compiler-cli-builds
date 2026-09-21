bb77e62bcb1f58a83faeb0ec5058240dc6d79b10
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 16:56:21 +0000

    adea161 docs: fix stray markup in the image optimization and zoneless guides

--- bundles/chunk-YGL6WM5L.js
+++ bundles/chunk-YGL6WM5L.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-8dbd6e4";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-adea161";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
