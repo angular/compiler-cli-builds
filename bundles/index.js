f28b5ff5369ac4c53e9345427dcad1fac7822d9e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 19:38:10 +0000

    602799c fix(dev-infra): handle missing arguments and gh failures in PR review script

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-1828e70");
+var VERSION = new Version("22.2.0-rc.0+sha-602799c");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
