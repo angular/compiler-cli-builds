38c655efeca5e3612a04a4c80c161449ff9b8927
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:14:55 +0000

    330083f fix(platform-server): preserve Unicode whitespace in ServerXhr URLs

--- bundles/chunk-I3LOGI4D.js
+++ bundles/chunk-I3LOGI4D.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.7+sha-71f84a8";
+var PLACEHOLDER_VERSION = "22.1.7+sha-330083f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
