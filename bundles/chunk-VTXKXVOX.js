695fdb2deb8b4f3e5271b72b4ffdf217d735c6f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:25:03 +0000

    726368b docs: correct the adev writing guide skill

--- bundles/chunk-VTXKXVOX.js
+++ bundles/chunk-VTXKXVOX.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-330083f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-726368b";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
