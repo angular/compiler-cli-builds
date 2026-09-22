45366e87bb68b732d59e2e271ee536166a3f1c8f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:54:05 +0000

    59e2c2f docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- bundles/chunk-VTXKXVOX.js
+++ bundles/chunk-VTXKXVOX.js
@@ -144,7 +144,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-726368b";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.1.7+sha-59e2c2f";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.7+sha-726368b");
+var VERSION = new Version("22.1.7+sha-59e2c2f");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
