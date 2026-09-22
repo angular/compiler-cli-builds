93d451e53652ce6906d6fb7a51c23a8eb809d6cb
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:25:14 +0000

    ad15967 docs: correct the adev writing guide skill

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-5c927fb");
+var VERSION = new Version("22.2.0-rc.0+sha-ad15967");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
