2b4f8d7efeb16e9b57f76332e6f964f5fa995e16
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 23:10:37 +0000

    efe48df docs(core): fix three JSDoc rendering bugs on the API pages

--- bundles/chunk-ICX2MHOI.js
+++ bundles/chunk-ICX2MHOI.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-8287b3f";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0-rc.0+sha-efe48df";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-8287b3f");
+var VERSION = new Version("22.2.0-rc.0+sha-efe48df");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
