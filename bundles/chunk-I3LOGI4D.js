4ac43b7d22870d712e6ca95e173255db77fb5b2d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:38:16 +0000

    1cd8e9a release: cut the v22.1.8 release

--- bundles/chunk-I3LOGI4D.js
+++ bundles/chunk-I3LOGI4D.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.1.7+sha-2cab6f5";
+var PLACEHOLDER_VERSION = "22.1.8+sha-1cd8e9a";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.7+sha-2cab6f5");
+var VERSION = new Version("22.1.8+sha-1cd8e9a");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
