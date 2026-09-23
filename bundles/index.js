393d5ac5bdf13c3a2f5b68787a056fe6a5b0b038
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:39:37 +0000

    42ce791 docs: release notes for the v22.1.8 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.3.0-next.0+sha-573293c");
+var VERSION = new Version("22.3.0-next.0+sha-42ce791");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
