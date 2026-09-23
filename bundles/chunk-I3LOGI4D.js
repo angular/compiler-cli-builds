6e0fd9c7b72dbf16036d0cd724ed199e50184608
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 15:59:37 +0000

    344b10f refactor(router): clean up route resource implementation and test helpers

--- bundles/chunk-I3LOGI4D.js
+++ bundles/chunk-I3LOGI4D.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-95e8dec";
+var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-344b10f";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
