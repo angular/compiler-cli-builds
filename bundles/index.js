02ff14a75d58c3ab3c74f1da4f030c580dac00c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 15:48:00 +0000

    a2fedc1 docs(upgrade): add maintenance mode note

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.1.7+sha-282f9fe");
+var VERSION = new Version("22.1.7+sha-a2fedc1");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
