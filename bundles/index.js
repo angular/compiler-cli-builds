6c0380a2e1275f692f62092bb2d34962efdd8461
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:51:30 +0000

    e65f114 release: cut the v21.2.24 release

--- bundles/index.js
+++ bundles/index.js
@@ -77,7 +77,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("21.2.23+sha-03872a8");
+var VERSION = new Version("21.2.24+sha-e65f114");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
