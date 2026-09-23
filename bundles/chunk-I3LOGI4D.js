657bcc29f2179e05aa6fba37bd9d6cf7c56e4b59
Andrew Scott <atscott01@gmail.com>
2026-09-23 20:02:46 +0000

    1ba9b86 refactor(router): give internal error classes an explicit name

--- bundles/chunk-I3LOGI4D.js
+++ bundles/chunk-I3LOGI4D.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.2.0+sha-84975ce";
+var PLACEHOLDER_VERSION = "22.2.0+sha-1ba9b86";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0+sha-84975ce");
+var VERSION = new Version("22.2.0+sha-1ba9b86");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
