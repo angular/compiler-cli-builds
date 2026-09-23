dc60702688b716713657311bd7efe2a1b8209bda
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 16:00:04 +0000

    52c33bc refactor(router): clean up route resource implementation and test helpers

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-369d1f4");
+var VERSION = new Version("22.2.0-rc.0+sha-52c33bc");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
