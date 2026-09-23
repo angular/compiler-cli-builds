139208b9ccec443600073e53c1f574f3633e9a63
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 18:00:20 +0000

    fc187d4 release: cut the v22.2.0 release

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0-rc.0+sha-52c33bc");
+var VERSION = new Version("22.2.0+sha-fc187d4");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
