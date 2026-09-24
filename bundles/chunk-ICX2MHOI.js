78ec5b7a463038b7659138743c3ff03bcc5989aa
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:09:16 +0000

    5b52d19 build: update all non-major dependencies

--- bundles/chunk-ICX2MHOI.js
+++ bundles/chunk-ICX2MHOI.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.3.0-next.0+sha-742fd44";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.3.0-next.0+sha-5b52d19";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.3.0-next.0+sha-742fd44");
+var VERSION = new Version("22.3.0-next.0+sha-5b52d19");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
