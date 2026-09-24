58cf396f25111975ddb0895abc6cb8c541c85e8f
Andrew Scott <atscott01@gmail.com>
2026-09-24 18:37:47 +0000

    742fd44 build: update cross-repo angular dependencies

--- bundles/chunk-I3LOGI4D.js
+++ bundles/chunk-I3LOGI4D.js
@@ -457,7 +457,7 @@ import semver2 from "semver";
 // packages/compiler-cli/linker/src/file_linker/partial_linkers/util.js
 import { createMayBeForwardRefExpression, outputAst as o2 } from "@angular/compiler";
 import semver from "semver";
-var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-28ad39a";
+var PLACEHOLDER_VERSION = "22.3.0-next.0+sha-742fd44";
 function wrapReference(wrapped) {
   return { value: wrapped, type: wrapped };
 }
