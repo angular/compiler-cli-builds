c38272c6ec5aede107812a90491589138adc085a
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:17:53 +0000

    870f200 fix(docs-infra): render one card per API member

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.2.0+sha-5db6fc4");
+var VERSION = new Version("22.2.0+sha-870f200");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
