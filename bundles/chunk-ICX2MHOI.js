1832bc49c20be93a3cd0faaee301dca99a52576b
GitHub <noreply@github.com>
2026-09-25 17:52:06 +0000

    5db6fc4 release: bump VSCode extension version to 22.2.0 (#70950)

--- bundles/chunk-ICX2MHOI.js
+++ bundles/chunk-ICX2MHOI.js
@@ -148,7 +148,7 @@ var ErrorCode;
 import { VERSION } from "@angular/compiler";
 var DOC_PAGE_BASE_URL = (() => {
   const full = VERSION.full;
-  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0+sha-21851fc";
+  const isPreRelease = full.includes("-next") || full.includes("-rc") || full === "22.2.0+sha-5db6fc4";
   const prefix = isPreRelease ? "next" : `v${VERSION.major}`;
   return `https://${prefix}.angular.dev`;
 })();
