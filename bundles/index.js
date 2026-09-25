0b75fc9ad29ec2e10679ee0acf8974b61e034f4c
GitHub <noreply@github.com>
2026-09-25 18:04:04 +0000

    e4fc745 docs: release notes for the vscode extension 22.2.0 release (#70951)

--- bundles/index.js
+++ bundles/index.js
@@ -106,7 +106,7 @@ import "./chunk-IEBNHER4.js";
 
 // packages/compiler-cli/src/version.js
 import { Version } from "@angular/compiler";
-var VERSION = new Version("22.3.0-next.0+sha-846c73d");
+var VERSION = new Version("22.3.0-next.0+sha-e4fc745");
 
 // packages/compiler-cli/private/tooling.js
 var GLOBAL_DEFS_FOR_TERSER = {
